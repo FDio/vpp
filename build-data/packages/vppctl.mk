c72cbf80d0674a745fef2effb3c2f6dbcf5bf98e
Ed Warnicke <eaw@cisco.com>
2015-12-15 15:08:54 -0800

    Add vppctl as a simple cli interface to vpp
    This patch provides a pretty simple hack on top
    of vpe_api_test to give users a cli interface
    to vpp.
    
    Example:
    
    sudo vppctl show api client
    sudo vppctl show int
    
    It has the advantage that users can use bash shell scripting
    and history with it.
    
    Change-Id: I621dd8e29269e14a83b4887f20adcfbe5f946b99
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

---
+++ build-data/packages/vppctl.mk
@@ -0,0 +1,11 @@
+vppctl_configure_depend =			\
+	vppinfra-install			\
+	dpdk-install				\
+	svm-install				\
+	vlib-api-install			\
+	vlib-install				\
+	vnet-install				\
+	vpp-install				\
+	vpp-api-test-install
+
+
