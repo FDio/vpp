2df2e3d7d5226891e974207aa10d1d6e69418cbf
Ole Troan <ot@cisco.com>
2016-03-02 10:01:43 +0100

    TAP: Jumbo-frame support.
    Change-Id: I3a0726d7645f775738253d0a47ee04d94d138c9a
    Signed-off-by: Ole Troan <ot@cisco.com>

--- vlib/vlib/buffer_funcs.h
+++ vlib/vlib/buffer_funcs.h
@@ -506,6 +506,7 @@ vlib_buffer_chain_append_data_with_alloc(vlib_main_t *vm,
                              vlib_buffer_t *first,
                              vlib_buffer_t **last,
                              void * data, u16 data_len);
+void vlib_buffer_chain_validate(vlib_main_t *vm, vlib_buffer_t *first);
 
 format_function_t format_vlib_buffer, format_vlib_buffer_and_data, format_vlib_buffer_contents;
 
