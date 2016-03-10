eca40c88cc7f2c382c0fc2e582e23178cec0867c
rangan <rangan@cisco.com>
2016-03-10 15:51:58 +0530

    * Added support to configure trace type as a configuration parameter.   This can be any combination of nodeid, interface indices, timestamp and   appdata. This configuration is passed through packet header by encap node   to all other nodes. Rewrite buffer is resized accordingly. Trace function    modified accordingly. * Added CLI 'show ioam summary' command to display various configuration. * Added CLI 'set ioam visapp'   IOAM application interacts with a 'visualization app' to display the   parameters in a GUI. This has some limiations in range of values it can   accept. IOAM-VPP applcation will 'shape' the values if it need to   interact with this app. A CLI 'set ioam visapp <true|false>' is added   to enable / disable this shaping. * Added CLI 'clear ioam rewrite'
    Change-Id: Ib8939b82ef333c21683d5a92e0384374e14f8a2b
    Signed-off-by: rangan <rangan@cisco.com>

--- vnet/vnet/ip/ip6_hop_by_hop_packet.h
+++ vnet/vnet/ip/ip6_hop_by_hop_packet.h
@@ -15,6 +15,17 @@
 #ifndef __included_ip6_hop_by_hop_packet_h__
 #define __included_ip6_hop_by_hop_packet_h__
 
+#define BIT_NONE            0x00
+#define BIT_0               0x01
+#define BIT_1               0x02
+#define BIT_2               0x04
+#define BIT_3               0x08
+#define BIT_4               0x10
+#define BIT_5               0x20
+#define BIT_6               0x40
+#define BIT_7               0x80
+#define BIT_8               0x0100
+
 typedef struct {
   /* Protocol for next header */
   u8 protocol;
@@ -39,29 +50,152 @@ typedef struct {
 } ip6_hop_by_hop_option_t;
 
 /* $$$$ IANA banana constants */
-#define HBH_OPTION_TYPE_IOAM_DATA_LIST 1
+#define HBH_OPTION_TYPE_IOAM_TRACE_DATA_LIST 1
 #define HBH_OPTION_TYPE_IOAM_PROOF_OF_WORK 2
+#define HBH_OPTION_TYPE_IOAM_EDGE_TO_EDGE 3
 
+/*
 typedef struct {
   u32 ttl_node_id;
   u16 ingress_if;
   u16 egress_if;
   u32 timestamp;
   u32 app_data;
-} ioam_data_list_element_t;
+} ioam_trace_data_list_element_t;
+*/
+
+#define    BIT_TTL_NODEID       BIT_0
+#define    BIT_ING_INTERFACE    BIT_1 
+#define    BIT_EGR_INTERFACE    BIT_2 
+#define    BIT_TIMESTAMP        BIT_3 
+#define    BIT_APPDATA          BIT_4 
+#define    TRACE_TYPE_MASK  (BIT_0 | BIT_1 | BIT_2 | BIT_3 | BIT_4)
+
+/*
+     0x00011111  iOAM-trace-type is 0x00011111 then the format of node
+        data is:
+  
+          0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |   Hop_Lim     |              node_id                          |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |     ingress_if_id             |         egress_if_id          |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         +                           timestamp                           +
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |                            app_data                           |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  
+*/
+#define   TRACE_TYPE_IF_TS_APP   0x1f
+typedef struct {
+  u32 ttl_node_id;
+  u16 ingress_if;
+  u16 egress_if;
+  u32 timestamp;
+  u32 app_data;
+} ioam_trace_if_ts_app_t;
+
+/*
+     0x00000111  iOAM-trace-type is 0x00000111 then the format is:
+  
+          0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |   Hop_Lim     |              node_id                          |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |     ingress_if_id             |         egress_if_id          |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  
+*/
+
+#define   TRACE_TYPE_IF   0x03
+typedef struct {
+  u32 ttl_node_id;
+  u16 ingress_if;
+  u16 egress_if;
+} ioam_trace_if_t;
+
+/*
+     0x00001001  iOAM-trace-type is 0x00001001 then the format is:
+  
+          0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |   Hop_Lim     |              node_id                          |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         +                           timestamp                           +
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  
+*/
+
+#define   TRACE_TYPE_TS   0x09
+typedef struct {
+  u32 ttl_node_id;
+  u32 timestamp;
+} ioam_trace_ts_t;
+
+/*
+     0x00010001  iOAM-trace-type is 0x00010001 then the format is:
+  
+  
+          0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |   Hop_Lim     |              node_id                          |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |                            app_data                           |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  
+*/
+
+
+#define   TRACE_TYPE_APP   0x11
+typedef struct {
+  u32 ttl_node_id;
+  u32 app_data;
+} ioam_trace_app_t;
+
+/*
+  
+     0x00011001  iOAM-trace-type is 0x00011001 then the format is:
+  
+          0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |   Hop_Lim     |              node_id                          |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         +                           timestamp                           +
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |                            app_data                           |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+*/
+
+#define   TRACE_TYPE_TS_APP   0x19
+typedef struct {
+  u32 ttl_node_id;
+  u32 timestamp;
+  u32 app_data;
+} ioam_trace_ts_app_t;
+
 
 typedef CLIB_PACKED(struct {
   ip6_hop_by_hop_option_t hdr;
+  u8 ioam_trace_type;
   u8 data_list_elts_left;
-  ioam_data_list_element_t elts[0];
+  u32 elts[0]; /* Variable type. So keep it generic */
 }) ioam_trace_option_t;
 
 typedef CLIB_PACKED(struct {
   ip6_hop_by_hop_option_t hdr;
   u8 pow_type;
-  u8 reserved;
-  u32 random[2];
-  u32 cumulative[2];
+#define PROFILE_ID_MASK 0xF
+  u8 reserved_profile_id; /* 4 bits reserved, 4 bits to carry profile id */
+  u64 random;
+  u64 cumulative;
 }) ioam_pow_option_t;
+
+typedef CLIB_PACKED(struct {
+  ip6_hop_by_hop_option_t hdr;
+  u8 e2e_type;
+  u8 reserved;
+  u32 e2e_data;
+}) ioam_e2e_option_t;
 
 #endif /* __included_ip6_hop_by_hop_packet_h__ */
