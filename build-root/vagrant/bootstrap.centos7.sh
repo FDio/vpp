c841eac7730c3e80fc14006481edb5d9221bf45c
Ed Warnicke <eaw@cisco.com>
2016-03-22 16:10:07 -0500

    Fix vagrant centos flavor to work with top level make file
    Change-Id: I89044ed27130a036536ed33aba847034ed15ad7d
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- build-root/vagrant/bootstrap.centos7.sh
+++ build-root/vagrant/bootstrap.centos7.sh
@@ -3,59 +3,35 @@
 yum check-update
 yum update -y
 
-# Install build tools
-yum groupinstall 'Development Tools' -y
-yum install openssl-devel -y
-yum install glibc-static -y
+# Install dependencies
+cd /vpp
+make install-dep
 
-# Install development tools
-yum install gdb -y
-yum install gdbserver -y
+# Build rpms
+make bootstrap
+make pkg-rpm
 
-# Install jdk and maven
-yum install -y java-1.8.0-openjdk-devel
+# Install rpms
 
-# Install EPEL
-yum install -y epel-release
+(cd build-root/;sudo rpm -Uvh *.rpm)
 
-# Install components to build Ganglia modules
-yum install -y apr-devel
-yum install -y --enablerepo=epel libconfuse-devel
-yum install -y --enablerepo=epel ganglia-devel
+# Disable all ethernet interfaces other than the default route
+# interface so VPP will use those interfaces.  The VPP auto-blacklist
+# algorithm prevents the use of any physical interface contained in the
+# routing table (i.e. "route --inet --inet6") preventing the theft of
+# the management ethernet interface by VPP from the kernel.
+for intf in $(ls /sys/class/net) ; do
+    if [ -d /sys/class/net/$intf/device ] && 
+        [ "$(route --inet --inet6 | grep default | grep $intf)" == "" ] ; then
+        ifconfig $intf down
+    fi
+done
 
-# PCIutils
-yum install -y pciutils
-
-# Load the uio kernel module
+# Install uio-pci-generic
 modprobe uio_pci_generic
 
-echo uio_pci_generic >> /etc/modules-load.d/uio_pci_generic.conf
+# Start vpp
+service vpp start
 
-# Setup for hugepages using upstart so it persists across reboots
-sysctl -w vm.nr_hugepages=1024
-echo "vm.nr_hugepages=1024" >> /etc/sysctl.conf
-mkdir -p /mnt/huge
-echo "hugetlbfs       /mnt/huge  hugetlbfs       defaults        0 0" >> /etc/fstab
-mount /mnt/huge
-
-# Setup the vpp code
-cd ~vagrant/
-sudo -u vagrant mkdir git
-cd git/
-
-# Check if git exists and remove it before attempting clone, else clone ineffective when "reload --provision"
-[ -d vpp ] && rm -rf vpp
-sudo -H -u vagrant git clone /vpp
-cd vpp
-
-# Initial vpp build
-if [ -d build-root ]; then
-  # Bootstrap vpp
-  cd build-root/
-  sudo -H -u vagrant ./bootstrap.sh
-
-  # Build vpp
-  sudo -H -u vagrant make PLATFORM=vpp TAG=vpp_debug install-packages
-  cd ~vagrant/
-  cat /vagrant/README
-fi
+# cat README
+cat /vagrant/README
