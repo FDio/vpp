58261d9f3e5a7506614702e70532660fbe52f3ce
Keith Burns (alagalah) <alagalah@gmail.com>
2016-03-25 09:38:50 -0700

    Initial commit for IP4 SR multicast
    - remove all config files before publish
    - grep and remove all "alagalah" comments
    --- they are TODO/Qs to reviewers
    
    Change-Id: Icd2fe6a2cf65c09906e82ed1afbb0eae8df79452
    Signed-off-by: Keith Burns (alagalah) <alagalah@gmail.com>

--- vnet/vnet/sr/sr.h
+++ vnet/vnet/sr/sr.h
@@ -37,6 +37,9 @@ typedef struct {
   /* src, dst address */
   ip6_sr_tunnel_key_t key;
 
+  /* optional tunnel name */
+  u8 * name;
+
   /* mask width for FIB entry */
   u32 dst_mask_width;
 
@@ -49,6 +52,10 @@ typedef struct {
 
   /* The actual ip6 sr header */
   u8 * rewrite;
+
+  /* Indicates that this tunnel is part of a policy comprising
+     of multiple tunnels. */
+  u32 policy_index;
 } ip6_sr_tunnel_t;
 
 typedef struct {
@@ -62,6 +69,12 @@ typedef struct {
   u32 dst_mask_width;
   u32 rx_table_id;
   u32 tx_table_id;
+
+  /* optional name argument - for referencing SR tunnel/policy by name */
+  u8 * name;
+
+  /* optional policy name */
+  u8 * policy_name;
 
   /* segment list, when inserting an ip6 SR header*/
   ip6_address_t *segments;
@@ -83,12 +96,30 @@ typedef struct {
 } ip6_sr_add_del_tunnel_args_t;
 
 typedef struct {
+  /* name of policy */
+  u8 * name;
+  
+  /* vector to SR tunnel index */
+  u32 * tunnel_indices;
+
+} ip6_sr_policy_t;
+
+typedef struct {
   /* pool of tunnel instances, sr entry only */
   ip6_sr_tunnel_t *tunnels;
 
   /* find an sr "tunnel" by its outer-IP src/dst */
   uword * tunnel_index_by_key;
-  
+
+  /* find an sr "tunnel" by its name */
+  uword * tunnel_index_by_name;
+
+  /* policy pool */
+  ip6_sr_policy_t * policies;
+
+  /* find a policy by name */
+  uword * policy_index_by_policy_name;
+
   /* ip6-lookup next index for imposition FIB entries */
   u32 ip6_lookup_sr_next_index;
 
