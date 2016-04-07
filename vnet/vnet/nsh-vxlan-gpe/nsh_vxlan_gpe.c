639b4bded649dd510b7c1c288bda413ce1d98613
Hongjun Ni <hongjun.ni@intel.com>
2016-04-06 17:54:39 -0700

    Change encap-fib-id and decap-fib-id within create_nsh_vxlan_gpe_tunnel_command
    Change-Id: Ied5275fcea21ae2e0b346931f9e2f520a73afd67
    Signed-off-by: Hongjun Ni <hongjun.ni@intel.com>

--- vnet/vnet/nsh-vxlan-gpe/nsh_vxlan_gpe.c
+++ vnet/vnet/nsh-vxlan-gpe/nsh_vxlan_gpe.c
@@ -514,7 +514,7 @@ VLIB_CLI_COMMAND (create_nsh_vxlan_gpe_tunnel_command, static) = {
   .short_help = 
   "nsh vxlan tunnel src <ip4-addr> dst <ip4-addr>" 
   "    c1 <nn> c2 <nn> c3 <nn> c4 <nn> spi <nn> si <nn> vni <nn>\n"
-  "    [encap-fib-id <nn>] [decap-fib-id <nn>] [o-bit <1|0>] [c-bit <1|0>]\n"
+  "    [encap-vrf-id <nn>] [decap-vrf-id <nn>] [o-bit <1|0>] [c-bit <1|0>]\n"
   "    [md-type <nn>][next-ip4][next-ip6][next-ethernet][next-nsh]\n"
   "    [tlv <xx>][decap-next [ip4|ip6|ethernet|nsh-encap]][del]\n",
   .function = nsh_vxlan_gpe_add_del_tunnel_command_fn,
