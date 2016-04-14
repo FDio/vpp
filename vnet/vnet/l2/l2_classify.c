102b0c1e9705fa85b4069cc89e9e948606560fd3
rangan <rangan@cisco.com>
2016-04-14 10:09:12 +0530

    Took care of code review comments. Extended the fix to other files.
    Change-Id: I24e688884cd5896b317efb53e1d193f7d363715a
    Signed-off-by: rangan <rangan@cisco.com>

--- vnet/vnet/l2/l2_classify.c
+++ vnet/vnet/l2/l2_classify.c
@@ -275,6 +275,7 @@ l2_classify_node_fn (vlib_main_t * vm,
           h0 = vlib_buffer_get_current(b0);
           table_index0 = vnet_buffer(b0)->l2_classify.table_index;
           e0 = 0;
+          vnet_buffer(b0)->l2_classify.opaque_index = ~0;
 
           if (PREDICT_TRUE(table_index0 != ~0))
             {
