65da8c881b589746e9a734709c505f573a8d000b
Chris Luke <chrisy@flirble.org>
2016-04-25 14:26:55 -0400

    Track number of ethernet vlans in a frame
    Adds flags to the packet buffer to track the number of VLANs in
    the current Ethernet frame. We use two bits to signify 0, 1 or
    2 VLANs. The value 3 signififies an unknown quantity of VLANs,
    which includes "three or more" which is not widely supported.
    We place the bits in the vlib_buffer section; that is not the
    opaque section, so that all subordinate nodes can use it.
    
    For background, see the discussion thread at
    https://lists.fd.io/pipermail/vpp-dev/2016-March/000354.html
    
    The helper macro ethernet_buffer_header_size(buffer) uses
    these bits stored in "buffer" to calculate the Ethernet header
    size.
    
    The macro ethernet_buffer_set_vlan_count(buffer, count) sets the
    appropriate bit values based on the number in "count".
    
    By current frame we are referring to the case where a packet
    that arrives from the wire is carrying an encapsulated Ethernet
    packet. Once decapsulated that inner packet becomes the current
    frame.
    
    There are two places where this value is set; For most Ethernet
    frames this will be in the "ethernet-input" node when that node
    parses the Ethernet header. The second place is whenever
    vnet_update_l2_len() is used to update the layer 2 opaque data.
    Typically this function is used by nodes just before they send
    a packet into l2-input.
    
    Primarily this VLAN counter is used by nodes below "ethernet-
    input" and "l2-input" to determine where the start of the
    current Ethernet header is. There is opaque data set by
    "ethernet-input" storing the offset of the current Ethernet
    header but, since this is opaque, it's not usable by downstream
    nodes. Previously several nodes have made assumptions regarding
    the location of the Ethernet header, including that it is always
    at the start of the packet buffer (incorrect when we have
    encapsulated packets) or that it is exactly
    sizeof(ethernet_header_t) away (incorrect when we have VLAN tags.)
    
    One notable case where this functionality is required is in
    ip6_neighbor when it generates a response to a received neighbor
    soliciation request; it reuses the incoming Ethernet header
    in-situ and thus needs to reliably know where that header begins.
    
    Change-Id: I00e4b9ced5ef814a776020c395d1774aba6185b3
    Signed-off-by: Chris Luke <chrisy@flirble.org>

--- vnet/vnet/buffer.h
+++ vnet/vnet/buffer.h
@@ -52,6 +52,15 @@
 #define LOG2_HGSHM_BUFFER_USER_INDEX_VALID  LOG2_VLIB_BUFFER_FLAG_USER(3)
 #define VNET_HGSHM_BUFFER_USER_INDEX_VALID  (1 << LOG2_HGSHM_BUFFER_USER_INDEX_VALID)
 
+/* VLAN header flags */
+#define LOG2_ETH_BUFFER_VLAN_1_DEEP LOG2_VLIB_BUFFER_FLAG_USER(4)
+#define LOG2_ETH_BUFFER_VLAN_2_DEEP LOG2_VLIB_BUFFER_FLAG_USER(5)
+#define ETH_BUFFER_VLAN_1_DEEP (1 << LOG2_ETH_BUFFER_VLAN_1_DEEP)
+#define ETH_BUFFER_VLAN_2_DEEP (1 << LOG2_ETH_BUFFER_VLAN_2_DEEP)
+#define ETH_BUFFER_VLAN_BITS (ETH_BUFFER_VLAN_1_DEEP | \
+                              ETH_BUFFER_VLAN_2_DEEP)
+
+
 #define foreach_buffer_opaque_union_subtype     \
 _(ethernet)                                     \
 _(ip)                                           \
