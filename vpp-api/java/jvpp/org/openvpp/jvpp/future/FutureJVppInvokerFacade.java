be39a5ab6705d8bf49ddfa7180c91213b127b652
Tibor Sirovatka <tsirovat@cisco.com>
2016-05-13 10:17:51 +0200

    Changed JVPP interface for construction and connectivity
    Simplified construction, autoconnected; possible connect/close
    See updated sample test cases with changed interface usage
    
    Change-Id: Ib53e855880bc414868aa2b9bb8f5df086917e375
    Signed-off-by: Tibor Sirovatka <tsirovat@cisco.com>

--- vpp-api/java/jvpp/org/openvpp/jvpp/future/FutureJVppInvokerFacade.java
+++ vpp-api/java/jvpp/org/openvpp/jvpp/future/FutureJVppInvokerFacade.java
@@ -49,6 +49,10 @@ public class FutureJVppInvokerFacade implements FutureJVppInvoker {
         this.requests = Objects.requireNonNull(requestMap, "Null requestMap");
     }
 
+    protected final Map<Integer, CompletableFuture<? extends JVppReply<?>>> getRequests() {
+        return this.requests;
+    }
+
     // TODO use Optional in Future, java8
 
     @Override
