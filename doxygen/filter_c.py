dadbf61b14b139b845fa3c6840bf7e2458a213d1
Chris Luke <chrisy@flirble.org>
2016-05-14 12:17:12 -0400

    VPP-62 Add a doxy filter to enable vpe.api doc
    This makes Doxygen think the API definitions are structs which is close
    enough to be able to document the API methods.
    
    It also has logic to create an indexed API page but that's disabled for
    now because it duplicates the "brief" text twice in the struct doc.
    
    Fixes a minor line numbering issue in filter_c.py.
    
    Change-Id: If380160b73e7c10d999b35a76f55d0e27cbc91cc
    Signed-off-by: Chris Luke <chrisy@flirble.org>

--- doxygen/filter_c.py
+++ doxygen/filter_c.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+# Copyright (c) 2016 Comcast Cable Communications Management, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Filter for .c files to make various preprocessor tricks Doxygenish
 
 import sys, re
 
@@ -31,7 +46,7 @@ patterns = [
 
 with open(sys.argv[1]) as fd:
     for line in fd:
-        str = line
+        str = line[:-1] # filter \n
         for p in patterns:
             str = p[0].sub(p[1], str)
-        sys.stdout.write(str)
+        sys.stdout.write(str+"\n")
