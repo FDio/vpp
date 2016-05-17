f842342e7a31b376ee947f199d7bc473f7b0c517
Dave Barach <dave@barachs.net>
2016-05-17 16:08:49 -0400

    vpp net-copy plugin
    Change-Id: I238c4dffc2a4c0d26c3675a2f3860867475d522b
    Signed-off-by: Dave Barach <dave@barachs.net>

---
+++ netcp-plugin/netcp/netcp.h
@@ -0,0 +1,114 @@
+
+/*
+ * netcp.h - skeleton vpp engine plug-in header file 
+ *
+ * Copyright (c) <current-year> <your-organization>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef __included_netcp_h__
+#define __included_netcp_h__
+
+#include <vnet/vnet.h>
+#include <vnet/ip/ip.h>
+#include <vnet/ethernet/ethernet.h>
+
+#include <vppinfra/hash.h>
+#include <vppinfra/error.h>
+#include "netcp_packet.h"
+
+typedef enum {
+  NETCP_STATE_START = 0,
+  NETCP_STATE_SENT_SEND_FILE,
+  NETCP_STATE_DATA,
+  NETCP_STATE_RESEND,
+} netcp_state_t;
+
+typedef struct {
+  union {
+    ip4_address_t ip4;
+    ip4_address_t ip6;
+  };
+} netcp_addr_t;
+
+typedef struct {
+  /* from, to addresses */
+  netcp_addr_t from;
+  netcp_addr_t to;
+
+  /* src, dst filenames */
+  u8 * src_file;
+  u8 * dst_file;
+  u64 size_in_bytes;
+
+  /* Current place in src file */
+  u64 my_current_offset;
+  u64 their_currrent_offset;
+
+  /* Session state variables */
+  netcp_state_t state;
+  u32 session_id;
+  u32 retry_count;
+  f64 retry_timer;
+  
+  /* mmap address */
+  u8 * map_addr;
+
+  /* properties */
+  u8 is_sender;
+  u8 is_ip4;
+
+  /* mmap address, size */
+  void * addr;
+  u16 segment_size;
+
+  /* encap string */
+  u8 * rewrite;
+} netcp_session_t;  
+
+typedef struct {
+  netcp_session_t * sessions;
+  uword * session_by_id;
+
+  f64 process_sleep_timer;
+
+  u32 random_seed;
+
+  /* API message ID base */
+  u16 msg_id_base;
+
+  /* node indices */
+  u32 ip4_lookup_index;
+  u32 ip6_lookup_index;
+  u16 segment_size;
+
+  /* convenience */
+  vlib_main_t * vlib_main;
+  vnet_main_t * vnet_main;
+  ethernet_main_t * ethernet_main;
+} netcp_main_t;
+
+netcp_main_t netcp_main;
+
+vlib_node_registration_t netcp_node;
+
+typedef CLIB_PACKED (struct {
+  ip4_header_t ip;
+  netcp_header_t netcp;
+}) ip4_and_netcp_header_t;
+
+void unmap_file (u8 * filename, u8 * addr, u64 size);
+u8 * map_file (u8 *filename, u64 *sizep, int is_write);
+
+#define NETCP_PROCESS_EVENT_SET_TIMER 1
+
+#endif /* __included_netcp_h__ */
