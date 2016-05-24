7aaa01421da6029e1db1d778ceafe9809b25d04f
Ole Troan <ot@cisco.com>
2016-05-24 11:56:58 +0200

    IPv6 HBH: Refactor code. Separate out RFC2460 HBH handling and the more experimental           IOAM code. Support dynamically adding options. By default           only process HBH if explicitly configured to. Otherwise we'll just set           ourselves up to be a victim of DOS.
    Change-Id: I41cdfdc00aeaa0cf568e4463440b89be761b6b7d
    Signed-off-by: Ole Troan <ot@cisco.com>

--- vnet/vnet/ip/ip6_hop_by_hop_packet.h
+++ vnet/vnet/ip/ip6_hop_by_hop_packet.h
@@ -32,15 +32,14 @@ typedef struct {
 #define HBH_OPTION_TYPE_DISCARD_UNKNOWN_ICMP (0x2 << 6)
 #define HBH_OPTION_TYPE_DISCARD_UNKNOWN_ICMP_NOT_MCAST (0x3 << 6)
 #define HBH_OPTION_TYPE_DATA_CHANGE_ENROUTE (1<<5)
-#define HBH_OPTION_TYPE_MASK (0x1F)
   u8 type;
   /* Length in octets of the option data field */
   u8 length;
 } ip6_hop_by_hop_option_t;
 
 /* $$$$ IANA banana constants */
-#define HBH_OPTION_TYPE_IOAM_TRACE_DATA_LIST 27
-#define HBH_OPTION_TYPE_IOAM_PROOF_OF_WORK 28
+#define HBH_OPTION_TYPE_IOAM_TRACE_DATA_LIST 59 /* Third highest bit set (change en-route) */
+#define HBH_OPTION_TYPE_IOAM_PROOF_OF_WORK 60   /* Third highest bit set (change en-route) */
 #define HBH_OPTION_TYPE_IOAM_EDGE_TO_EDGE 29
 
 /*
