26f9bbc8232099fd5539f99a5e028713bb52d3db
Maros Marsalek <mmarsale@cisco.com>
2016-05-31 17:45:16 +0200

    VPP-119: JVpp notifications
    - add notification DTOs to JVpp
    - add notification callbacks
    - add notification registry
    - provide/implement notification registry from future and callback facades
    
    Change-Id: I1060ef2ec8ba1eb2e8cff279c93b73aa7c9f9aee
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>

---
+++ vpp-api/java/jvpp/org/openvpp/jvpp/notification/NotificationRegistryProviderContext.java
@@ -0,0 +1,20 @@
+package org.openvpp.jvpp.notification;
+
+/**
+ * Base class for notification aware JVpp facades
+ */
+public abstract class NotificationRegistryProviderContext {
+
+    private final NotificationRegistryImpl notificationRegistry = new NotificationRegistryImpl();
+
+    public final NotificationRegistry getNotificationRegistry() {
+        return notificationRegistry;
+    }
+
+    /**
+     * Get instance of notification callback. Can be used to propagate notifications from JVpp facade
+     */
+    protected final GlobalNotificationCallback getNotificationCallback() {
+        return notificationRegistry;
+    }
+}
