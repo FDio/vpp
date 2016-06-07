c633741842b50a9d53d2bdd3fdeeb81fe077c065
Hongjun Ni <hongjun.ni@intel.com>
2016-06-08 01:07:12 +0800

    Add vxlan-gpe tunnel to sw_interface_dump
    Change-Id: I3375d6c9c7e18ab46896552614369b9d77ee6004
    Signed-off-by: Hongjun Ni <hongjun.ni@intel.com>

--- vpp-api-test/vat/api_format.c
+++ vpp-api-test/vat/api_format.c
@@ -3098,6 +3098,13 @@ int api_sw_interface_dump (vat_main_t * vam)
     strncpy ((char *) mp->name_filter, "lo", sizeof(mp->name_filter)-1);
     S;
 
+
+    /* and vxlan-gpe tunnel interfaces */
+    M(SW_INTERFACE_DUMP, sw_interface_dump);
+    mp->name_filter_valid = 1;
+    strncpy ((char *) mp->name_filter, "vxlan_gpe", sizeof(mp->name_filter)-1);
+    S;
+
     /* and vxlan tunnel interfaces */
     M(SW_INTERFACE_DUMP, sw_interface_dump);
     mp->name_filter_valid = 1;
