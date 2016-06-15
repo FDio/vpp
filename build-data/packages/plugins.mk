3b3688f9d3a640a169e9c7fc858d36ab3aeb147e
Ole Troan <ot@cisco.com>
2016-06-15 14:29:08 +0200

    6rd: Move to plugin
    - Change toplevel plugins make target. Now builds all plugins under
      plugins/. (Apart from sample-plugin).
    - Move sixrd code to plugins directory and make necessary changes to
      make it a plugin
    - Remove 6rd hooks from IP lookup code
    
    Change-Id: I447e92e3bee240cd8de01d0abac2e1708e8c27d1
    Signed-off-by: Ole Troan <ot@cisco.com>

---
+++ build-data/packages/plugins.mk
@@ -0,0 +1,38 @@
+plugins_configure_depend = 			\
+    vppinfra-install 				\
+    vlib-api-install 				\
+    vlib-install 
+
+plugins_CPPFLAGS = $(call installed_includes_fn, 	\
+    vppinfra 					\
+    vlib 					\
+    vlib-api)
+
+plugins_LDFLAGS = $(call installed_libs_fn, 	\
+    vppinfra					\
+    vlib					\
+    vlib-api)
+
+ifeq ($($(PLATFORM)_enable_tests),yes)
+plugins_configure_args += --enable-tests
+endif
+
+# Platform dependent configure flags
+plugins_configure_args += $(plugins_configure_args_$(PLATFORM))
+
+# include & link with openssl only if needed
+ifneq ($($(PLATFORM)_uses_openssl),no)
+plugins_CPPFLAGS += $(call installed_includes_fn, openssl)
+plugins_LDFLAGS += $(call installed_libs_fn, openssl)
+endif
+
+ifneq ($($(PLATFORM)_uses_dpdk),no)
+ifeq ($($(PLATFORM)_uses_external_dpdk),yes)
+plugins_CPPFLAGS += -I$($(PLATFORM)_dpdk_inc_dir)
+plugins_LDFLAGS += -L$($(PLATFORM)_dpdk_lib_dir)
+else
+plugins_configure_depend += dpdk-install
+plugins_CPPFLAGS += $(call installed_includes_fn, dpdk)
+plugins_LDFLAGS += $(call installed_libs_fn, dpdk)
+endif
+endif
