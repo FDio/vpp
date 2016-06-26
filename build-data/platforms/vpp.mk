7789acdef8b9cacda5e8d49ddfcdab566cfadff5
Dave Barach <dave@barachs.net>
2016-06-26 10:42:08 -0400

    Simple ip4 NAT plugin
    Change-Id: Iffe77bf2a05ced41474540ff54a842101aad7c41
    Signed-off-by: Dave Barach <dave@barachs.net>

--- build-data/platforms/vpp.mk
+++ build-data/platforms/vpp.mk
@@ -38,6 +38,10 @@ vnet_configure_args_vpp = --with-dpdk
 # Set these parameters carefully. The vlib_buffer_t is 128 bytes, i.e.
 vlib_configure_args_vpp = --with-pre-data=128
 
+# Enable plugins here, and no place else, via multiple --enable-XXX-plugin
+# stanzas.
+plugins_configure_args_vpp = --with-dpdk --enable-sixrd-plugin 
+
 # DPDK configuration parameters
 # vpp_uses_external_dpdk = yes
 # vpp_dpdk_inc_dir = /usr/include/dpdk
