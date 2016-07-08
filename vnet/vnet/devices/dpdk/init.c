30a04171b60c5458c2bec427a8e6d5a958295fae
Damjan Marion <damarion@cisco.com>
2016-07-08 17:36:50 +0200

    dpdk: disable CRC stripping
    Change-Id: I7006a3068c22e18e62c84abb8cc6124695b9ca44
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- vnet/vnet/devices/dpdk/init.c
+++ vnet/vnet/devices/dpdk/init.c
@@ -54,7 +54,7 @@ static struct rte_eth_conf port_conf_template = {
     .header_split   = 0, /**< Header Split disabled */
     .hw_ip_checksum = 0, /**< IP checksum offload disabled */
     .hw_vlan_filter = 0, /**< VLAN filtering disabled */
-    .hw_strip_crc   = 1, /**< CRC stripped by hardware */
+    .hw_strip_crc   = 0, /**< CRC stripped by hardware */
   },
   .txmode = {
     .mq_mode = ETH_MQ_TX_NONE,
