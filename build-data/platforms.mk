178a563d379068cc5a86e8ed68bfeadb510b4663
Ed Warnicke <eaw@cisco.com>
2016-07-11 10:29:41 -0700

    DO NOT MERGE. Small changes to fascilitate plugin jars. WIP
    Change-Id: I2254f90b2c3e423563bb91bf70877979f1e86a6b
    Signed-off-by: Ed Warnicke <eaw@cisco.com>
    Signed-off-by: Marek Gradzki <mgradzki@cisco.com>

--- build-data/platforms.mk
+++ build-data/platforms.mk
@@ -56,8 +56,14 @@ install-deb: $(patsubst %,%-find-source,$(ROOT_PACKAGES))
 	   >> deb/debian/vpp.install ;					\
 									\
 	: dev package needs a couple of additions ;			\
-        echo ../build-tool-native/vppapigen/vppapigen /usr/bin		\
-           >> deb/debian/vpp-dev.install ;				\
+    echo ../build-tool-native/vppapigen/vppapigen /usr/bin		\
+       >> deb/debian/vpp-dev.install ;				\
+    echo ../../vpp-api/java/jvpp/gen/jvpp_gen.py /usr/bin    \
+       >> deb/debian/vpp-dev.install ;				 \
+    for i in $$(ls ../vpp-api/java/jvpp/gen/jvppgen/*.py); do \
+    	echo ../$${i} /usr/lib/python2.7/dist-packages/jvppgen \
+    	   >> deb/debian/vpp-dev.install; \
+   	done; \
 									\
 	: generate changelog;						\
 	./scripts/generate-deb-changelog 				\
