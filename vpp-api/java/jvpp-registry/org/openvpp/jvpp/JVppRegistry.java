1c7815eb0cbf8635532ecfcd00798de4732988be
Marek Gradzki <mgradzki@cisco.com>
2016-07-26 15:28:22 +0200

    VPP-205: jvpp plugin support.
    Splits jvpp into two jars
    jvpp-registry.jar - base jvpp functionality
    jvpp-core.jar - Java wrapper for vpe.api
    
    Plugins can be generated the same way jvpp-core.jar is.
    Example (nsh):
    https://gerrit.fd.io/r/#/c/2118/
    
    Change-Id: I2254f90b2c3e423563bb91bf70877979f1e90a7d
    Signed-off-by: Marek Gradzki <mgradzki@cisco.com>

---
+++ vpp-api/java/jvpp-registry/org/openvpp/jvpp/JVppRegistry.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openvpp.jvpp;
+
+import org.openvpp.jvpp.callback.JVppCallback;
+
+/**
+ * The registry that stores callbacks.
+ */
+public interface JVppRegistry extends AutoCloseable {
+    /**
+     * Registers callback for the given plugin.
+     *
+     * @param name     plugin name
+     * @param callback callback provided by the plugin
+     * @throws NullPointerException     if name or callback is null
+     * @throws IllegalArgumentException if plugin was already registered
+     */
+    void register(JVpp jvpp, JVppCallback callback);
+
+    /**
+     * Unregisters callback for the given plugin.
+     *
+     * @param name plugin name
+     * @throws NullPointerException     if name is null
+     * @throws IllegalArgumentException if plugin was not registered
+     */
+    void unregister(String name);
+
+    /**
+     * Returns callback registered for the plugin.
+     *
+     * @param name plugin name
+     * @return callback provided by the plugin
+     * @throws NullPointerException     if name is null
+     * @throws IllegalArgumentException if plugin was not registered
+     */
+    JVppCallback get(String name);
+
+    /**
+     * Sends control ping. Reply handler calls
+     * @param clazz
+     * @return
+     */
+    int controlPing(Class<? extends JVpp> clazz) throws VppInvocationException;
+}
