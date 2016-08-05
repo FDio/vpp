fb54c3c8687bed574cc83168ab13a376970bee73
Ed Warnicke <eaw@cisco.com>
2016-08-05 11:43:58 -0700

    DO NOT MERGE (yet) - Checkstyle script to check for new checkstyle breakage
    Change-Id: I5b73e02f5996d8a01ec2e46741affbc6d62da6c9
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

---
+++ build-root/scripts/checkstyle.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+VPP_DIR=`dirname $0`/../../
+EXIT_CODE=0
+
+# Check to make sure we have indent.  Exit if we don't with an error message, but
+# don't *fail*.
+command -v indent
+if [ $? != 0 ]; then
+    echo "Cound not find required commend \"indent\".  Checkstyle aborted"
+    exit ${EXIT_CODE}
+fi
+
+cd ${VPP_DIR}
+for i in `git diff --name-only HEAD^1`; do
+    grep -q "fd.io coding-style-patch-verification: ON" ${i}
+    if [ $? == 0 ]; then
+        echo "checkstyle ${i}"
+        indent ${i}
+        if [ $? != 0 ]; then
+            EXIT_CODE=1
+        fi
+    else
+        echo "Not checkstyle ${i}"
+    fi
+done
+exit ${EXIT_CODE}
