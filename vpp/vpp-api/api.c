98cefc72136bb0ae25be39487d3d01c31b335529
AkshayaNadahalli <anadahal@cisco.com>
2016-08-09 13:38:04 +0530

    Adding Sequence Number - Per Packet Counter(PPC) support for iOAM6. - Added support in classifier session to identify a flow to be iOAM6 encap/decap - PPC as part of iOAM6 E2E header is created as a plugin. - On creation of a classifier session with command classify session <options> <ioam-encap/ioam-decap> <flow-name>, ioam creates its local flow DB and stores an index to local DB in classifier opaque index. On a packet matching a classify session, opaque index is stored in opaque1 of vlib_buffer_ti(under l2_classify). This data will be used by iOAM to add per flow data. - Command 'show ioam e2e' can be used to see e2e Seqno data.
    Pending:
    - On deleteion of classify session, correspnding data in ioam needs to be cleaned up.
    
    Change-Id: Ib7605de45aecff25d684d099b525f8dc96ee7038
    Signed-off-by: AkshayaNadahalli <anadahal@cisco.com>

--- vpp/vpp-api/api.c
+++ vpp/vpp-api/api.c
@@ -7048,7 +7048,8 @@ vl_api_ioam_enable_t_handler (vl_api_ioam_enable_t * mp)
 
   /* Ignoring the profile id as currently a single profile
    * is supported */
-  error = ip6_ioam_enable (mp->trace_enable, mp->pow_enable, mp->trace_ppc);
+  error = ip6_ioam_enable (mp->trace_enable, mp->pow_enable,
+			   mp->seqno, mp->analyse);
   if (error)
     {
       clib_error_report (error);
