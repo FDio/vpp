07d2f84907e776383572fa5cc89071d90a8be36b
Vengada Govindan <venggovi@cisco.com>
2016-08-25 10:34:34 -0700

    Move iOAM Trace as a plugin
    Change-Id: I78322e91485e5429871bd356f668b8ed90f4265a
    Signed-off-by: Vengada Govindan <venggovi@cisco.com>

---
+++ plugins/ioam-plugin/ioam/lib-trace/trace_util.c
@@ -0,0 +1,221 @@
+/*
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <vnet/vnet.h>
+#include <stdint.h>
+#include <time.h>
+#include <string.h>
+#include <vppinfra/mem.h>
+#include "trace_util.h"
+
+trace_main_t trace_main;
+
+extern ip6_hop_by_hop_ioam_main_t ip6_hop_by_hop_ioam_main;
+
+static int
+trace_profile_cleanup (trace_profile * profile)
+{
+  int rv;
+  ip6_hop_by_hop_ioam_main_t *hm = &ip6_hop_by_hop_ioam_main;
+
+  memset (profile, 0, sizeof (trace_profile));
+  profile->trace_tsp = TSP_MICROSECONDS;	/* Micro seconds */
+  hm->options_size[HBH_OPTION_TYPE_IOAM_TRACE_DATA_LIST] = 0;
+  if (0 !=
+      (rv =
+       ip6_ioam_set_rewrite (&hm->rewrite, hm->has_trace_option,
+			     hm->has_pot_option, hm->has_ppc_option)))
+    return (-1);
+  return 0;
+
+}
+
+static int
+trace_main_profiles_reset (void)
+{
+  int rv;
+
+  trace_main_t *sm = &trace_main;
+  rv = trace_profile_cleanup (&(sm->profile));
+  return (rv);
+}
+
+int
+trace_util_init (void)
+{
+  int rv;
+
+  rv = trace_main_profiles_reset ();
+  return (rv);
+}
+
+
+int
+trace_profile_create (trace_profile * profile, u8 trace_type, u8 num_elts,
+		      u32 trace_tsp, u32 node_id, u32 app_data)
+{
+  u32 trace_size = 0;
+  int rv;
+  ip6_hop_by_hop_ioam_main_t *hm = &ip6_hop_by_hop_ioam_main;
+
+  if (profile && !profile->valid)
+    {
+      //rv = trace_profile_cleanup (profile);
+      profile->trace_type = trace_type;
+      profile->num_elts = num_elts;
+      profile->trace_tsp = trace_tsp;
+      profile->node_id = node_id;
+      profile->app_data = app_data;
+      profile->valid = 1;
+
+      if (ioam_trace_get_sizeof_handler (&trace_size) < 0)
+	return (-1);
+
+      hm->options_size[HBH_OPTION_TYPE_IOAM_TRACE_DATA_LIST] = trace_size;
+
+      if (hm->has_trace_option)
+	{
+	  if (0 !=
+	      (rv =
+	       ip6_ioam_set_rewrite (&hm->rewrite, hm->has_trace_option,
+				     hm->has_pot_option, hm->has_ppc_option)))
+	    return (-1);
+
+	}
+      return (0);
+    }
+
+  return (-1);
+}
+
+
+
+clib_error_t *
+clear_trace_profile_command_fn (vlib_main_t * vm,
+				unformat_input_t * input,
+				vlib_cli_command_t * cmd)
+{
+
+  trace_main_profiles_reset ();
+  return 0;
+}
+
+void
+clear_trace_profiles (void)
+{
+  clear_trace_profile_command_fn (0, 0, 0);
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND(clear_trace_profile_command) =
+{
+.path = "clear ioam-trace profile",
+.short_help = "clear ioam-trace profile [<index>|all]",
+.function = clear_trace_profile_command_fn,
+};
+/* *INDENT-ON* */
+
+static clib_error_t *
+set_trace_profile_command_fn (vlib_main_t * vm,
+			      unformat_input_t * input,
+			      vlib_cli_command_t * cmd)
+{
+  u8 trace_type = 0;
+  u8 num_elts = 0;
+  u32 node_id = 0;
+  u32 app_data = 0;
+  u32 trace_tsp = 0;
+  trace_profile *profile = NULL;
+  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (input, "trace-type 0x%x", &trace_type));
+      else if (unformat (input, "trace-elts %d", &num_elts));
+      else if (unformat (input, "trace-tsp %d", &trace_tsp));
+      else if (unformat (input, "node-id 0x%x", &node_id));
+      else if (unformat (input, "app-data 0x%x", &app_data));
+      else
+	return clib_error_return (0, "unknown input `%U'",
+				  format_unformat_error, input);
+    }
+  profile = trace_profile_find ();
+  if (profile)
+    {
+      trace_profile_create (profile, trace_type, num_elts, trace_tsp,
+			    node_id, app_data);
+    }
+  return 0;
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (set_trace_profile_command) =
+{
+.path = "set ioam-trace profile",.short_help = "set ioam-trace \
+                  trace-type <0x1f|0x3|0x9|0x11|0x19> trace-elts <nn> trace-tsp <0|1|2|3> node-id <node id in hex> app-data <app_data in hex>",.function = set_trace_profile_command_fn,};
+
+/* *INDENT-ON* */
+
+static clib_error_t *
+show_trace_profile_command_fn (vlib_main_t * vm,
+			       unformat_input_t * input,
+			       vlib_cli_command_t * cmd)
+{
+  trace_profile *p = NULL;
+  u8 *s = 0;
+  p = trace_profile_find ();
+  if (p->valid == 0)
+    return 0;
+  s = format (s, " HOP BY HOP OPTIONS - TRACE CONFIG - \n");
+  s = format (s, "                        Trace Type : 0x%x (%d)\n",
+	      p->trace_type, p->trace_type);
+  s =
+    format (s, "         Trace timestamp precision : %d (%s)\n",
+	    p->trace_tsp,
+	    (p->trace_tsp ==
+	     TSP_SECONDS) ? "Seconds" : ((p->trace_tsp ==
+					  TSP_MILLISECONDS) ?
+					 "Milliseconds"
+					 : (((p->trace_tsp ==
+					      TSP_MICROSECONDS) ?
+					     "Microseconds" :
+					     "Nanoseconds"))));
+  s = format (s, "                Num of trace nodes : %d\n", p->num_elts);
+  s =
+    format (s, "                           Node-id : 0x%x (%d)\n",
+	    p->node_id, p->node_id);
+  s =
+    format (s, "                          App Data : 0x%x (%d)\n",
+	    p->app_data, p->app_data);
+  if (!(p && p->valid))
+    {
+      s = format (s, "\nTrace configuration not valid\n");
+    }
+  vlib_cli_output (vm, "%v", s);
+  vec_free (s);
+  return 0;
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (show_trace_profile_command) =
+{
+.path = "show ioam-trace profile",.short_help =
+    "show ioam-trace profile",.function = show_trace_profile_command_fn,};
+/* *INDENT-ON* */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
---
+++ plugins/ioam-plugin/ioam/lib-trace/trace_util.h
@@ -0,0 +1,250 @@
+/*
+ * trace_util.h -- Trace Profile Utility header
+ *
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef include_vnet_trace_util_h
+#define include_vnet_trace_util_h
+
+#include <vnet/ip/ip6_hop_by_hop.h>
+#define debug_ioam debug_ioam_fn
+
+
+/**
+ * Usage:
+ *
+ * On any node that participates in iOAM Trace.
+ *
+ * Step 1: Initialize this library by calling trace_init()
+ * Step 2: Setup a trace  profile that contains all the parameters needed to compute cumulative:
+ *         Call these functions:
+ *         trace_profile_find
+ *         trace_profile_create
+ * Step 2a: On initial node enable the profile to be used:
+ *          trace_profile_set_active / trace_profile_get_active will return the profile
+ * Step 4: TBD
+ *         trace_validate
+ *
+ */
+
+typedef struct trace_profile_
+{
+  u8 valid:1;
+  u8 trace_type;
+  u8 num_elts;
+  /* Configured node-id */
+  u32 node_id;
+  u32 app_data;
+  u32 trace_tsp;
+} trace_profile;
+
+typedef struct
+{
+  /* Name of the default profile list in use */
+  trace_profile profile;
+
+  /* API message ID base */
+  u16 msg_id_base;
+
+  /* convenience */
+  vlib_main_t *vlib_main;
+  vnet_main_t *vnet_main;
+} trace_main_t;
+
+extern trace_main_t trace_main;
+
+/*
+ * Initialize Trace profile
+ */
+int trace_util_init (void);
+
+
+/*
+ * Find a trace profile by ID
+ */
+always_inline trace_profile *trace_profile_find (void);
+
+always_inline trace_profile *
+trace_profile_find (void)
+{
+  trace_main_t *sm = &trace_main;
+
+  return (&(sm->profile));
+}
+
+
+/* setup and clean up profile */
+int trace_profile_create (trace_profile * profile, u8 trace_type, u8 num_elts,
+			  u32 trace_tsp, u32 node_id, u32 app_data);
+
+void clear_trace_profiles (void);
+
+
+
+#define    BIT_TTL_NODEID       (1<<0)
+#define    BIT_ING_INTERFACE    (1<<1)
+#define    BIT_EGR_INTERFACE    (1<<2)
+#define    BIT_TIMESTAMP        (1<<3)
+#define    BIT_APPDATA          (1<<4)
+#define    TRACE_TYPE_MASK      0x1F	/* Mask of all above bits */
+
+/*
+     0x00011111  iOAM-trace-type is 0x00011111 then the format of node
+        data is:
+
+          0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |   Hop_Lim     |              node_id                          |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |     ingress_if_id             |         egress_if_id          |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         +                           timestamp                           +
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |                            app_data                           |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+*/
+#define   TRACE_TYPE_IF_TS_APP   0x1f
+typedef struct
+{
+  u32 ttl_node_id;
+  u16 ingress_if;
+  u16 egress_if;
+  u32 timestamp;
+  u32 app_data;
+} ioam_trace_if_ts_app_t;
+
+/*
+     0x00000111  iOAM-trace-type is 0x00000111 then the format is:
+
+          0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |   Hop_Lim     |              node_id                          |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |     ingress_if_id             |         egress_if_id          |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+*/
+
+#define   TRACE_TYPE_IF   0x03
+typedef struct
+{
+  u32 ttl_node_id;
+  u16 ingress_if;
+  u16 egress_if;
+} ioam_trace_if_t;
+
+/*
+     0x00001001  iOAM-trace-type is 0x00001001 then the format is:
+
+          0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |   Hop_Lim     |              node_id                          |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         +                           timestamp                           +
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+*/
+
+#define   TRACE_TYPE_TS   0x09
+typedef struct
+{
+  u32 ttl_node_id;
+  u32 timestamp;
+} ioam_trace_ts_t;
+
+/*
+     0x00010001  iOAM-trace-type is 0x00010001 then the format is:
+
+
+          0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |   Hop_Lim     |              node_id                          |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |                            app_data                           |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+*/
+
+
+#define   TRACE_TYPE_APP   0x11
+typedef struct
+{
+  u32 ttl_node_id;
+  u32 app_data;
+} ioam_trace_app_t;
+
+/*
+
+     0x00011001  iOAM-trace-type is 0x00011001 then the format is:
+
+          0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |   Hop_Lim     |              node_id                          |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         +                           timestamp                           +
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         |                            app_data                           |
+         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+*/
+
+#define   TRACE_TYPE_TS_APP   0x19
+typedef struct
+{
+  u32 ttl_node_id;
+  u32 timestamp;
+  u32 app_data;
+} ioam_trace_ts_app_t;
+
+
+/* *INDENT-OFF* */
+typedef CLIB_PACKED(struct {
+  ip6_hop_by_hop_option_t hdr;
+  u8 ioam_trace_type;
+  u8 data_list_elts_left;
+  u32 elts[0]; /* Variable type. So keep it generic */
+}) ioam_trace_option_t;
+/* *INDENT-ON* */
+
+
+static inline u8
+fetch_trace_data_size (u8 trace_type)
+{
+  u8 trace_data_size = 0;
+
+  if (trace_type == TRACE_TYPE_IF_TS_APP)
+    trace_data_size = sizeof (ioam_trace_if_ts_app_t);
+  else if (trace_type == TRACE_TYPE_IF)
+    trace_data_size = sizeof (ioam_trace_if_t);
+  else if (trace_type == TRACE_TYPE_TS)
+    trace_data_size = sizeof (ioam_trace_ts_t);
+  else if (trace_type == TRACE_TYPE_APP)
+    trace_data_size = sizeof (ioam_trace_app_t);
+  else if (trace_type == TRACE_TYPE_TS_APP)
+    trace_data_size = sizeof (ioam_trace_ts_app_t);
+
+  return trace_data_size;
+}
+
+int ioam_trace_get_sizeof_handler (u32 * result);
+#endif
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
