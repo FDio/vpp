cf909a06674ba8b656ebade0df45460ec88b630f
Ole Troan <ot@cisco.com>
2016-09-12 22:00:32 +0200

    DO NOT MERGE: Python API: Preparation for RPM/DEB packaging.
    Repackage the Python API binding to include all
    necessary modules in a single Python package.
    
    Change-Id: I5e35141d413bfb1aad650217e1ca07d85646c349
    Signed-off-by: Ole Troan <ot@cisco.com>

--- vpp-api/python/tests/test_base.py
+++
@@ -1,7 +0,0 @@
-# Manipulate sys.path to allow tests be run inside the build environment.
-import os, sys, glob
-scriptdir = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.dirname(glob.glob(scriptdir+'/../../../build-root/install*/vpp-api/lib64/vpp_api.so')[0]))
-sys.path.append(os.path.dirname(glob.glob(scriptdir+'/../../../build-root/install*/vlib-api/vlibmemory/memclnt.py')[0]))
-sys.path.append(os.path.dirname(glob.glob(scriptdir+'/../../../build-root/install*/vpp/vpp-api/vpe.py')[0]))
-sys.path.append(glob.glob(scriptdir+'/../../../build-root/install*/plugins/vpp_papi_plugins')[0])
--- vpp-api/python/vpp_papi/__init__.py
+++ vpp-api/python/vpp_papi/__init__.py
@@ -1,4 +1,3 @@
 __import__('pkg_resources').declare_namespace(__name__)
 from . vpp_papi import *
 
-
