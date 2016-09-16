b82e9814179de5506c19dd37c9238b32711f80f8
Maros Marsalek <mmarsale@cisco.com>
2016-09-16 16:17:58 +0200

    HONEYCOMB-185 DTOs generated by JVpp improvements:
    - hashCode & equals
    - toString
    - constructor
    
    Change-Id: I5f8bc8868c216862a307dcc8f6b423f0ce29e7b5
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>

--- vpp-api/java/jvpp/gen/jvppgen/dto_gen.py
+++ vpp-api/java/jvpp/gen/jvppgen/dto_gen.py
@@ -40,7 +40,7 @@ field_template = Template("""    public $type $name;\n""")
 send_template = Template("""    @Override
     public int send(final $base_package.JVpp jvpp) throws org.openvpp.jvpp.VppInvocationException {
         return (($plugin_package.JVpp${plugin_name})jvpp).$method_name($args);
-    }\n""")
+    }""")
 
 
 def generate_dtos(func_list, base_package, plugin_package, plugin_name, dto_package, inputfile):
@@ -58,15 +58,13 @@ def generate_dtos(func_list, base_package, plugin_package, plugin_name, dto_pack
         if util.is_ignored(func['name']) or util.is_control_ping(camel_case_dto_name):
             continue
 
-        fields = ""
-        for t in zip(func['types'], func['args']):
-            # for retval don't generate dto field in Reply
-            field_name = util.underscore_to_camelcase(t[1])
-            if util.is_reply(camel_case_dto_name) and util.is_retval_field(field_name):
-                continue
-            fields += field_template.substitute(type=util.jni_2_java_type_mapping[t[0]],
-                                                name=field_name)
-        methods = ""
+        fields = generate_dto_fields(camel_case_dto_name, func)
+        # FIXME HONEYCOMB-185 fix other code in jvpp and in HC + tests here to use constructor + getters
+        methods = generate_dto_constructor(camel_case_dto_name, func)
+        methods += generate_dto_getters(func)
+        methods += generate_dto_hash(func)
+        methods += generate_dto_equals(camel_case_dto_name, func)
+        methods += generate_dto_tostring(camel_case_dto_name, func)
         base_type = ""
 
         # Generate request/reply or dump/dumpReply even if structure can be used as notification
@@ -77,17 +75,18 @@ def generate_dtos(func_list, base_package, plugin_package, plugin_name, dto_pack
                 if util.is_details(camel_case_dto_name):
                     # FIXME assumption that dump calls end with "Dump" suffix. Not enforced in vpe.api
                     base_type += "JVppReply<%s.%s.%s>" % (plugin_package, dto_package, request_dto_name + "Dump")
-                    generate_dump_reply_dto(request_dto_name, base_package, plugin_package, dto_package, camel_case_dto_name,
-                                            camel_case_method_name, func)
+                    # FIXME HONEYCOMB-185 add constructor etc. to dump reply aggregator
+                    generate_dump_reply_dto(request_dto_name, base_package, plugin_package, dto_package,
+                                            camel_case_dto_name, camel_case_method_name, func, "")
                 else:
                     base_type += "JVppReply<%s.%s.%s>" % (plugin_package, dto_package, request_dto_name)
             else:
                 args = "" if fields is "" else "this"
-                methods = send_template.substitute(method_name=camel_case_method_name,
-                                                   base_package=base_package,
-                                                   plugin_package=plugin_package,
-                                                   plugin_name=plugin_name,
-                                                   args=args)
+                methods += send_template.substitute(method_name=camel_case_method_name,
+                                                    base_package=base_package,
+                                                    plugin_package=plugin_package,
+                                                    plugin_name=plugin_name,
+                                                    args=args)
                 if util.is_dump(camel_case_dto_name):
                     base_type += "JVppDump"
                     description = "dump request DTO"
@@ -95,24 +94,156 @@ def generate_dtos(func_list, base_package, plugin_package, plugin_name, dto_pack
                     base_type += "JVppRequest"
                     description = "request DTO"
 
-            write_dto_file(base_package, plugin_package, base_type, camel_case_dto_name, description, dto_package, dto_path, fields, func,
-                           inputfile, methods)
+            write_dto_file(base_package, plugin_package, base_type, camel_case_dto_name, description, dto_package,
+                           dto_path, fields, func, inputfile, methods)
 
         # for structures that are also used as notifications, generate dedicated notification DTO
         if util.is_notification(func["name"]):
             base_type = "JVppNotification"
             description = "notification DTO"
+            methods = generate_dto_tostring(camel_case_dto_name, func)
             camel_case_dto_name = util.add_notification_suffix(camel_case_dto_name)
-            methods = ""
             dto_path = os.path.join(dto_package, camel_case_dto_name + ".java")
-            write_dto_file(base_package, plugin_package, base_type, camel_case_dto_name, description, dto_package, dto_path, fields, func,
-                           inputfile, methods)
+            write_dto_file(base_package, plugin_package, base_type, camel_case_dto_name, description, dto_package,
+                           dto_path, fields, func, inputfile, methods)
 
     flush_dump_reply_dtos(inputfile)
 
 
-def write_dto_file(base_package, plugin_package, base_type, camel_case_dto_name, description, dto_package, dto_path, fields, func,
-                   inputfile, methods):
+field_getter_template = Template("""    public $type get$getter_name() {
+        return $field_name;
+    }\n\n""")
+
+
+def generate_dto_getters(func):
+    getters = ""
+    for t in zip(func['types'], func['args']):
+        # for retval don't generate dto field in Reply
+        field_name = util.underscore_to_camelcase(t[1])
+        field_name_capital = util.underscore_to_camelcase_upper(t[1])
+        if util.is_retval_field(field_name):
+            continue
+        getters += field_getter_template.substitute(type=util.jni_2_java_type_mapping[t[0]],
+                                                    getter_name=field_name_capital,
+                                                    field_name=field_name)
+    return getters
+
+
+def generate_dto_fields(camel_case_dto_name, func):
+    fields = ""
+    for t in zip(func['types'], func['args']):
+        # for retval don't generate dto field in Reply
+        field_name = util.underscore_to_camelcase(t[1])
+        if util.is_reply(camel_case_dto_name) and util.is_retval_field(field_name):
+            continue
+        fields += field_template.substitute(type=util.jni_2_java_type_mapping[t[0]],
+                                            name=field_name)
+    return fields
+
+
+tostring_field_template = Template("""                \"$field_name=\" + $field_name +\n""")
+tostring_array_field_template = Template("""                \"$field_name=\" + java.util.Arrays.toString($field_name) +\n""")
+tostring_template = Template("""    @Override
+    public String toString() {
+        return "$cls_name{" +
+$fields_tostring        "}";
+    }\n\n""")
+
+
+def generate_dto_tostring(camel_case_dto_name, func):
+    tostring_fields = ""
+    for t in zip(func['types'], func['args']):
+        field_name = util.underscore_to_camelcase(t[1])
+        # for retval don't generate dto field in Reply
+        if util.is_retval_field(field_name):
+            continue
+
+        # handle array types
+        if util.is_array(util.jni_2_java_type_mapping[t[0]]):
+            tostring_fields += tostring_array_field_template.substitute(field_name=field_name)
+        else:
+            tostring_fields += tostring_field_template.substitute(field_name=field_name)
+
+    return tostring_template.substitute(cls_name=camel_case_dto_name,
+                                        fields_tostring=tostring_fields)
+
+
+equals_field_template = Template("""        if (!java.util.Objects.equals(this.$field_name, other.$field_name)) {
+            return false;
+        }\n""")
+equals_template = Template("""    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final $cls_name other = ($cls_name) o;
+
+$comparisons
+        return true;
+    }\n\n""")
+
+
+def generate_dto_equals(camel_case_dto_name, func):
+    equals_fields = ""
+    for t in zip(func['types'], func['args']):
+        field_name = util.underscore_to_camelcase(t[1])
+        # for retval don't generate dto field in Reply
+        if util.is_retval_field(field_name):
+            continue
+
+        equals_fields += equals_field_template.substitute(field_name=field_name)
+
+    return equals_template.substitute(cls_name=camel_case_dto_name,
+                                      comparisons=equals_fields)
+
+
+hash_template = Template("""    @Override
+    public int hashCode() {
+        return java.util.Objects.hash($fields);
+    }\n\n""")
+
+
+def generate_dto_hash(func):
+    hash_fields = ""
+    for t in zip(func['types'], func['args']):
+        field_name = util.underscore_to_camelcase(t[1])
+        # for retval don't generate dto field in Reply
+        if util.is_retval_field(field_name):
+            continue
+
+        hash_fields += field_name + ", "
+
+    return hash_template.substitute(fields=hash_fields[:-2])
+
+
+constructor_field_template = Template("""        this.$field_name = $field_name;\n""")
+constructor_template = Template("""    public $cls_name($params) {
+$assignments    }\n\n""")
+
+
+def generate_dto_constructor(camel_case_dto_name, func):
+    params = ""
+    assignments = ""
+    for t in zip(func['types'], func['args']):
+        field_name = util.underscore_to_camelcase(t[1])
+        # for retval don't generate dto field in Reply
+        if util.is_retval_field(field_name):
+            continue
+
+        params += "%s %s, " % (util.jni_2_java_type_mapping[t[0]], field_name)
+        assignments += constructor_field_template.substitute(field_name=field_name)
+
+    return constructor_template.substitute(cls_name=camel_case_dto_name,
+                                           params=params[:-2],
+                                           assignments=assignments)
+
+
+def write_dto_file(base_package, plugin_package, base_type, camel_case_dto_name, description, dto_package, dto_path,
+                   fields, func, inputfile, methods):
     dto_file = open(dto_path, 'w')
     dto_file.write(dto_template.substitute(inputfile=inputfile,
                                            description=description,
@@ -130,6 +261,7 @@ def write_dto_file(base_package, plugin_package, base_type, camel_case_dto_name,
 
 dump_dto_suffix = "ReplyDump"
 dump_reply_artificial_dtos = {}
+
 
 # Returns request name or special one from unconventional_naming_rep_req map
 def get_request_name(camel_case_dto_name, func_name):
@@ -157,8 +289,8 @@ def flush_dump_reply_dtos(inputfile):
         dto_file.close()
 
 
-def generate_dump_reply_dto(request_dto_name, base_package, plugin_package, dto_package, camel_case_dto_name, camel_case_method_name,
-                            func):
+def generate_dump_reply_dto(request_dto_name, base_package, plugin_package, dto_package, camel_case_dto_name,
+                            camel_case_method_name, func, methods):
     base_type = "JVppReplyDump<%s.%s.%s, %s.%s.%s>" % (
         plugin_package, dto_package, util.remove_reply_suffix(camel_case_dto_name) + "Dump",
         plugin_package, dto_package, camel_case_dto_name)
@@ -174,7 +306,7 @@ def generate_dump_reply_dto(request_dto_name, base_package, plugin_package, dto_
         dump_reply_artificial_dtos[request_dto_name] = ({'docs': util.api_message_to_javadoc(func),
                                                          'cls_name': cls_name,
                                                          'fields': fields,
-                                                         'methods': "",
+                                                         'methods': methods,
                                                          'plugin_package': plugin_package,
                                                          'base_package': base_package,
                                                          'base_type': base_type,
