708ddf0699b8a9e8c67d51b858e00892f78dd67c
David Hotham <david.hotham@metaswitch.com>
2016-09-20 13:34:00 +0000

    Handle ARP proxy after tunnelling
    Only rewind the buffer back to the start of the ethernet header,
    rather than all the way.  We don't want to undo earlier
    decapsulation.
    
    Change-Id: I5f4b183eb3e8df690695ce3a97e55f3b0579a266
    Signed-off-by: David Hotham <david.hotham@metaswitch.com>

--- vnet/vnet/ethernet/arp.c
+++ vnet/vnet/ethernet/arp.c
@@ -1116,12 +1116,13 @@ arp_input (vlib_main_t * vm, vlib_node_runtime_t * node, vlib_frame_t * frame)
 		    /*
 		     * Rewind buffer, direct code above not to
 		     * think too hard about it.
-		     * $$$ is the answer ever anything other than
-		     * vlib_buffer_reset(..)?
 		     */
 		    ifa0 = 0;
 		    if_addr0 = &proxy_src;
-		    vlib_buffer_reset (p0);
+		    i32 ethernet_start =
+		      vnet_buffer (p0)->ethernet.start_of_ethernet_header;
+		    i32 rewind = p0->current_data - ethernet_start;
+		    vlib_buffer_advance (p0, -rewind);
 		    n_proxy_arp_replies_sent++;
 		    goto send_reply;
 		  }
