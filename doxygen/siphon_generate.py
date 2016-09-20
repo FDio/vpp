178bc1f9781f51be42e269eac49470a9b9af5373
Chris Luke <chrisy@flirble.org>
2016-09-20 09:20:42 -0400

    Attempt to use the C preprocessor before parsing for CLI initializers; VPP-417
    Change-Id: I41c6b8bebe8a8c306584fcf9d8259624ae46d045
    Signed-off-by: Chris Luke <chrisy@flirble.org>

--- doxygen/siphon_generate.py
+++ doxygen/siphon_generate.py
@@ -17,19 +17,71 @@
 # off into another file for later parsing; ostensibly to generate
 # documentation from struct initializer data.
 
-import os, sys, re, argparse, json
+import os, sys, re, argparse, json, subprocess
 
 DEFAULT_OUTPUT = "build-root/docs/siphons"
 DEFAULT_PREFIX = os.getcwd()
+DEFAULT_CPP = "cpp"
+DEFAULT_CPPARGS = ""
+DEFAULT_USECPP = True
 
 ap = argparse.ArgumentParser()
 ap.add_argument("--output", '-o', metavar="directory", default=DEFAULT_OUTPUT,
         help="Output directory for .siphon files [%s]" % DEFAULT_OUTPUT)
 ap.add_argument("--input-prefix", metavar="path", default=DEFAULT_PREFIX,
         help="Prefix to strip from input pathnames [%s]" % DEFAULT_PREFIX)
+ap.add_argument("--cpp", metavar="path", default=DEFAULT_CPP,
+        help="Path to the C pre-processor [%s]" % DEFAULT_CPP)
+ap.add_argument("--cppargs", metavar="args", default=DEFAULT_CPPARGS,
+        help="Arguments to pass to the C pre-processor [%s]" % DEFAULT_CPPARGS)
+_usecpp = ap.add_mutually_exclusive_group(required=False)
+_usecpp.add_argument("--usecpp", action='store_true',  dest='usecpp',
+        help="Use the C pre-processor [%s]" % DEFAULT_USECPP)
+_usecpp.add_argument("--no-usecpp", action='store_false', dest='usecpp',
+        help="Do not use the C pre-processor")
+ap.set_defaults(usecpp=DEFAULT_USECPP)
 ap.add_argument("input", nargs='+', metavar="input_file",
         help="Input C source files")
 args = ap.parse_args()
+
+# If usecpp==True then validate the pre-processor works
+if args.usecpp:
+    cpp = args.cpp
+    if not os.path.isfile(cpp):
+        # Check if it's in the PATH
+        for path in os.environ['PATH'].split(os.pathsep):
+            file = os.path.join(path, cpp)
+            if os.path.isfile(file):
+                cpp = file
+                args.cpp = file
+                break
+    if not os.path.isfile(cpp):
+        print "C Pre-processor '%s' does not exist." % cpp
+        sys.exit(1)
+    if not os.access(cpp, os.X_OK):
+        print "C Pre-processor '%s' is not executable." % cpp
+        sys.exit(1)
+    # Spawn CPP
+    cmd = [ cpp, '-x', 'c' ]
+    if len(args.cppargs):
+        cmd += args.cppargs.split(" ")
+    p = subprocess.Popen(cmd, bufsize=-1, close_fds=True, executable=cpp,
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE, cwd="/")
+    # Send some input to CPP to test if #define works
+    (p_stdout, p_stderr) = p.communicate(input="#define test true\ntest\n")
+    if p.returncode != 0:
+        print "C Pre-processor '%s' does not appear to work." % cpp
+        sys.exit(1)
+    # Read back from the process until we see "test" which indicates CPP
+    # works as expected
+    found = False
+    for line in iter(p_stdout.splitlines()):
+        if line == "true":
+            found = True
+    if not found:
+        print "C Pre-processor '%s' does not appear to work." % cpp
+        sys.exit(1)
+
 
 """Patterns that match the start of code blocks we want to siphon"""
 siphon_patterns = [
@@ -51,6 +103,9 @@ siphon_block_directive = re.compile("(%s)\s*([a-zA-Z0-9_:]+)\s+(.*)\s*(%s)" % \
 
 """Matches the start of an initializer block"""
 siphon_initializer = re.compile("\s*=")
+
+"""Matches a directive from the C pre-processor"""
+cpp_directive = re.compile('^# ([0-9]+) "([^"]+)"')
 
 """
 count open and close braces in str
@@ -129,70 +184,82 @@ for filename in files:
     # Open the file and explore its contents...
     sys.stderr.write("Siphoning from %s...\n" % filename)
     directives = {}
-    with open(filename) as fd:
-        siphon = None
-        close_siphon = None
-        siphon_block = ""
-        in_block = False
-        line_num = 0
-        siphon_line = 0
 
-        for line in fd:
-            line_num += 1
-            str = line[:-1] # filter \n
+    siphon = None
+    close_siphon = None
+    siphon_block = ""
+    in_block = False
+    line_num = 0
+    siphon_line = 0
 
-            """See if there is a block directive and if so extract it"""
-            def process_block_directive(str, directives):
-                m = siphon_block_directive.search(str)
+    fd = None
+    cpppipe = None
+    if args.usecpp:
+        # Spawn CPP
+        cmd = [ args.cpp, '-x', 'c', '-C', '-w' ]
+        if len(args.cppargs):
+            cmd += args.cppargs.split(" ")
+        cmd.append(filename)
+        sys.stderr.write("cmd: '%s'\n" % " ".join(cmd))
+        cpppipe = subprocess.Popen(cmd,
+            bufsize=-1,
+            close_fds=True,
+            executable=args.cpp,
+            stdin=None,
+            stdout=subprocess.PIPE)
+        fd = cpppipe.stdout
+    else:
+        fd = open(filename)
+
+    for line in fd:
+        line_num += 1
+        str = line[:-1] # filter \n
+
+        if args.usecpp and len(str) and str[0] == '#':
+            # Look for pre-processor directives
+
+            m = cpp_directive.search(str)
+            if m is not None:
+                # Update the line number
+                line_num = int(m.group(1))
+                # If the filename is not us, skip to the next line
+                cppfile = m.group(2)
+                if cppfile is not None:
+                    if cppfile != filename:
+                        continue
+
+
+        """See if there is a block directive and if so extract it"""
+        def process_block_directive(str, directives):
+            m = siphon_block_directive.search(str)
+            if m is not None:
+                k = m.group(2)
+                v = m.group(3).strip()
+                directives[k] = v
+                # Return only the parts we did not match
+                return str[0:m.start(1)] + str[m.end(4):]
+
+            return str
+
+        def process_block_prefix(str):
+            if str.startswith(" * "):
+                str = str[3:]
+            elif str == " *":
+                str = ""
+            return str
+            
+        if not in_block:
+            # See if the line contains the start of a siphon doc block
+            m = siphon_block_start.search(str)
+            if m is not None:
+                in_block = True
+                t = m.group(1)
+
+                # Now check if the block closes on the same line
+                m = siphon_block_stop.search(t)
                 if m is not None:
-                    k = m.group(2)
-                    v = m.group(3).strip()
-                    directives[k] = v
-                    # Return only the parts we did not match
-                    return str[0:m.start(1)] + str[m.end(4):]
-
-                return str
-
-            def process_block_prefix(str):
-                if str.startswith(" * "):
-                    str = str[3:]
-                elif str == " *":
-                    str = ""
-                return str
-                
-            if not in_block:
-                # See if the line contains the start of a siphon doc block
-                m = siphon_block_start.search(str)
-                if m is not None:
-                    in_block = True
                     t = m.group(1)
-
-                    # Now check if the block closes on the same line
-                    m = siphon_block_stop.search(t)
-                    if m is not None:
-                        t = m.group(1)
-                        in_block = False
-
-                    # Check for directives
-                    t = process_block_directive(t, directives)
-
-                    # Filter for normal comment prefixes
-                    t = process_block_prefix(t)
-
-                    # Add what is left
-                    siphon_block += t
-
-                    # Skip to next line
-                    continue
-
-            else:
-                # Check to see if we have an end block marker
-                m = siphon_block_stop.search(str)
-                if m is not None:
                     in_block = False
-                    t = m.group(1)
-                else:
-                    t = str
 
                 # Check for directives
                 t = process_block_directive(t, directives)
@@ -201,115 +268,144 @@ for filename in files:
                 t = process_block_prefix(t)
 
                 # Add what is left
-                siphon_block += t + "\n"
+                siphon_block += t
 
                 # Skip to next line
                 continue
 
-
-            if siphon is None:
-                # Look for blocks we need to siphon
-                for p in siphon_patterns:
-                    if p[0].match(str):
-                        siphon = [ p[1], str + "\n", 0 ]
-                        siphon_line = line_num
-
-                        # see if we have an initializer
-                        m = siphon_initializer.search(str)
-                        if m is not None:
-                            # count the braces on this line
-                            (count, index) = count_braces(str[m.start():])
-                            siphon[2] = count
-                            # TODO - it's possible we have the initializer all on the first line
-                            # we should check for it, but also account for the possibility that
-                            # the open brace is on the next line
-                            #if count == 0:
-                            #    # braces balanced
-                            #    close_siphon = siphon
-                            #    siphon = None
-                        else:
-                            # no initializer: close the siphon right now
-                            close_siphon = siphon
-                            siphon = None
+        else:
+            # Check to see if we have an end block marker
+            m = siphon_block_stop.search(str)
+            if m is not None:
+                in_block = False
+                t = m.group(1)
             else:
-                # See if we should end the siphon here - do we have balanced
-                # braces?
-                (count, index) = count_braces(str, count=siphon[2], found=True)
-                if count == 0:
-                    # braces balanced - add the substring and close the siphon
-                    siphon[1] += str[:index+1] + ";\n"
-                    close_siphon = siphon
-                    siphon = None
+                t = str
+
+            # Check for directives
+            t = process_block_directive(t, directives)
+
+            # Filter for normal comment prefixes
+            t = process_block_prefix(t)
+
+            # Add what is left
+            siphon_block += t + "\n"
+
+            # Skip to next line
+            continue
+
+
+        if siphon is None:
+            # Look for blocks we need to siphon
+            for p in siphon_patterns:
+                if p[0].match(str):
+                    siphon = [ p[1], str + "\n", 0 ]
+                    siphon_line = line_num
+
+                    # see if we have an initializer
+                    m = siphon_initializer.search(str)
+                    if m is not None:
+                        # count the braces on this line
+                        (count, index) = count_braces(str[m.start():])
+                        siphon[2] = count
+                        # TODO - it's possible we have the initializer all on the first line
+                        # we should check for it, but also account for the possibility that
+                        # the open brace is on the next line
+                        #if count == 0:
+                        #    # braces balanced
+                        #    close_siphon = siphon
+                        #    siphon = None
+                    else:
+                        # no initializer: close the siphon right now
+                        close_siphon = siphon
+                        siphon = None
+        else:
+            # See if we should end the siphon here - do we have balanced
+            # braces?
+            (count, index) = count_braces(str, count=siphon[2], found=True)
+            if count == 0:
+                # braces balanced - add the substring and close the siphon
+                siphon[1] += str[:index+1] + ";\n"
+                close_siphon = siphon
+                siphon = None
+            else:
+                # add the whole string, move on
+                siphon[2] = count
+                siphon[1] += str + "\n"
+
+        if close_siphon is not None:
+            # Write the siphoned contents to the right place
+            siphon_name = close_siphon[0]
+
+            # Copy directives for the file
+            details = {}
+            for key in directives:
+                if ":" in key:
+                    (sn, label) = key.split(":")
+                    if sn == siphon_name:
+                        details[label] = directives[key]
                 else:
-                    # add the whole string, move on
-                    siphon[2] = count
-                    siphon[1] += str + "\n"
+                    details[key] = directives[key]
 
-            if close_siphon is not None:
-                # Write the siphoned contents to the right place
-                siphon_name = close_siphon[0]
+            # Copy details for this block
+            details['file'] = filename
+            details['line_start'] = siphon_line
+            details['line_end'] = line_num
+            details['siphon_block'] = siphon_block.strip()
 
-                # Copy directives for the file
-                details = {}
-                for key in directives:
-                    if ":" in key:
-                        (sn, label) = key.split(":")
-                        if sn == siphon_name:
-                            details[label] = directives[key]
-                    else:
-                        details[key] = directives[key]
+            # Some defaults
+            if "group" not in details:
+                if "group_label" in details:
+                    # use the filename since group labels are mostly of file scope
+                    details['group'] = details['file']
+                else:
+                    details['group'] = directory
 
-                # Copy details for this block
-                details['file'] = filename
-                details['line_start'] = siphon_line
-                details['line_end'] = line_num
-                details['siphon_block'] = siphon_block.strip()
+            if "group_label" not in details:
+                details['group_label'] = details['group']
 
-                # Some defaults
-                if "group" not in details:
-                    if "group_label" in details:
-                        # use the filename since group labels are mostly of file scope
-                        details['group'] = details['file']
-                    else:
-			details['group'] = directory
+            details["block"] = close_siphon[1]
 
-                if "group_label" not in details:
-                    details['group_label'] = details['group']
+            # Store the item
+            output[siphon_name]['items'].append(details)
 
-                details["block"] = close_siphon[1]
+            # All done
+            close_siphon = None
+            siphon_block = ""
 
-                # Store the item
-                output[siphon_name]['items'].append(details)
+    # Update globals
+    for key in directives.keys():
+        if ':' not in key:
+            continue
 
-                # All done
-                close_siphon = None
-                siphon_block = ""
+        if filename.endswith("/dir.dox"):
+            # very special! use the parent directory name
+            l = directory
+        else:
+            l = filename
 
-        # Update globals
-        for key in directives.keys():
-            if ':' not in key:
-                continue
+        (sn, label) = key.split(":")
 
-            if filename.endswith("/dir.dox"):
-                # very special! use the parent directory name
-                l = directory
-            else:
-                l = filename
+        if sn not in output:
+            output[sn] = {}
+        if 'global' not in output[sn]:
+            output[sn]['global'] = {}
+        if l not in output[sn]['global']:
+            output[sn]['global'][l] = {}
+        if 'file' not in output[sn]:
+            output[sn]['file'] = "%s/%s.siphon" % (args.output, sn)
+        if 'items' not in output[sn]:
+            output[sn]['items'] = []
 
-            (sn, label) = key.split(":")
+        output[sn]['global'][l][label] = directives[key]
 
-            if sn not in output:
-                output[sn] = {}
-            if 'global' not in output[sn]:
-                output[sn]['global'] = {}
-            if l not in output[sn]['global']:
-                output[sn]['global'][l] = {}
-            if 'file' not in output[sn]:
-                output[sn]['file'] = "%s/%s.siphon" % (args.output, sn)
-            if 'items' not in output[sn]:
-                output[sn]['items'] = []
-
-            output[sn]['global'][l][label] = directives[key]
+    if args.usecpp:
+        # Wait for the thing to finish, complain if there were errors
+        cpppipe.wait()
+        if cpppipe.returncode != 0:
+            sys.stderr.write("Error running C pre-processor on file '%s'." %
+                filename)
+            sys.exit(1)
 
 
 # Write out the data
