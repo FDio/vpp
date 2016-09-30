f2c6ed1925efa46fc9f17d0b7cad738c71067eb4
Damjan Marion <damarion@cisco.com>
2016-09-30 10:53:30 +0200

    vat: add pg interfaces to sw_interface_dump (VPP-451)
    Change-Id: I976a44dcfb32b1132c14363174239333315b2c6f
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- vpp-api-test/vat/api_format.c
+++ vpp-api-test/vat/api_format.c
@@ -4371,6 +4371,11 @@ api_sw_interface_dump (vat_main_t * vam)
   strncpy ((char *) mp->name_filter, "lo", sizeof (mp->name_filter) - 1);
   S;
 
+  /* and packet-generator interfaces */
+  M (SW_INTERFACE_DUMP, sw_interface_dump);
+  mp->name_filter_valid = 1;
+  strncpy ((char *) mp->name_filter, "pg", sizeof (mp->name_filter) - 1);
+  S;
 
   /* and vxlan-gpe tunnel interfaces */
   M (SW_INTERFACE_DUMP, sw_interface_dump);
