153202125e80ffe95fcb3339cb549c33ba2852a8
Alexander Popovsky (apopovsk) <apopovsk@cisco.com>
2016-10-05 22:31:23 -0700

    VPP-395 Add udp-punt node(s) and API
    Uses existing UDP local API in order to register
    requested UDP port punt to the host.
    
    CLI: set punt udp [del] <port>
    API: punt protocol <l4-protocol> [ip <ver>] [port <l4-port>] [del]
    * Only UDP (l4-protocol = 17) is supported at this time
    
    Change-Id: I9232af1c891d1ed174d77f3e0dfe60c4b9d85e40
    Signed-off-by: Alex Popovsky <apopovsk@cisco.com>

--- vnet/vnet/ip/punt.h
+++ vnet/vnet/ip/punt.h
@@ -1,7 +1,5 @@
 /*
- * udp_error.def: udp errors
- *
- * Copyright (c) 2013-2016 Cisco and/or its affiliates.
+ * Copyright (c) 2016 Cisco and/or its affiliates.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at:
@@ -15,6 +13,31 @@
  * limitations under the License.
  */
 
-udp_error (NONE, "no error")
-udp_error (NO_LISTENER, "no listener for dst port")
-udp_error (LENGTH_ERROR, "UDP packets with length errors")
+/**
+ * @file
+ * @brief Definitions for punt infrastructure.
+ */
+#ifndef included_punt_h
+#define included_punt_h
+
+typedef enum
+{
+#define punt_error(n,s) PUNT_ERROR_##n,
+#include <vnet/ip/punt_error.def>
+#undef punt_error
+  PUNT_N_ERROR,
+} punt_error_t;
+
+
+clib_error_t *vnet_punt_add_del (vlib_main_t * vm, u8 ipv,
+				 u8 protocol, u16 port, int is_add);
+
+#endif /* included_punt_h */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
