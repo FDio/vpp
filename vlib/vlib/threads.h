47e907b1cd832c1d66038519c42f41ddddc517a0
Pavel Kotucek <pkotucek@cisco.com>
2016-10-07 08:37:28 +0200

    vpp_lite: add cpu pinning support (VPP-467)
    Proper cpu pinning in vpp_lite platform, like in normal vpp image.
    Extended “show threads” command to show propper information.
    Changed handling of coreID and socketID for threads in "show threads"
    CLI, pthread_getaffinity is used instead of info stored in DPDK.
    
    Change-Id: Ic8299ec5e284472bb10a37a95fadeed57b6edae8
    Signed-off-by: Pavel Kotucek <pkotucek@cisco.com>

--- vlib/vlib/threads.h
+++ vlib/vlib/threads.h
@@ -105,7 +105,8 @@ typedef struct
   u64 barrier_sync_count;
 
   long lwp;
-  int dpdk_lcore_id;
+  int lcore_id;
+  pthread_t thread_id;
 } vlib_worker_thread_t;
 
 vlib_worker_thread_t *vlib_worker_threads;
