7733ea4c207dfec732bfc9bb0827d55705ac3c60
Marek Zavodsky <mazavods@gmail.com>
2016-10-12 04:38:21 -0400

    Complete lawful intercept node, fixes VPP-186
    Created separate static LI nodes for ip4 and ip6 listening on specific classifier.
    Added cli to show LI
    Added binary API for LI.
    
    Fixed: vnet/vnet/ip/ip4_forward.c where fib was not correctly resolved for TX interface.
    
    Change-Id: If87e61b1dec231af4edce9744effadabb053f7a0
    Signed-off-by: Marek Zavodsky <mazavods@gmail.com>

--- vpp/vpp-api/custom_dump.c
+++ vpp/vpp-api/custom_dump.c
@@ -2280,6 +2280,50 @@ static void *vl_api_pg_enable_disable_t_print
   FINISH;
 }
 
+static void *
+vl_api_li_create_t_print (vl_api_li_create_t * mp, void *handle)
+{
+  u8 *s;
+
+  ip4_address_t src_ip4_addr;
+  ip4_address_t collector;
+  src_ip4_addr.data_u32 = mp->src_ip4_addr;
+  collector.data_u32 = mp->collector;
+
+  s = format (0, "SCRIPT: li_create ");
+  s = format (s, "src_ip4_addr %U ", format_ip4_address, &src_ip4_addr);
+  s = format (s, "collector %U ", format_ip4_address, &collector);
+  s = format (s, "port %u ", ntohl (mp->port));
+
+  FINISH;
+}
+
+static void *
+vl_api_li_delete_t_print (vl_api_li_delete_t * mp, void *handle)
+{
+  u8 *s;
+
+
+  ip4_address_t collector;
+  collector.data_u32 = mp->collector;
+
+  s = format (0, "SCRIPT: li_delete ");
+  s = format (s, "collector %U ", format_ip4_address, &collector);
+  s = format (s, "port %u ", ntohl (mp->port));
+
+  FINISH;
+}
+
+static void *
+vl_api_li_dump_t_print (vl_api_li_dump_t * mp, void *handle)
+{
+  u8 *s;
+
+  s = format (0, "SCRIPT: li_dump ");
+
+  FINISH;
+}
+
 static void *vl_api_ip_source_and_port_range_check_add_del_t_print
   (vl_api_ip_source_and_port_range_check_add_del_t * mp, void *handle)
 {
@@ -2958,6 +3002,9 @@ _(GET_NEXT_INDEX, get_next_index)                                       \
 _(PG_CREATE_INTERFACE,pg_create_interface)                              \
 _(PG_CAPTURE, pg_capture)                                               \
 _(PG_ENABLE_DISABLE, pg_enable_disable)                                 \
+_(LI_CREATE, li_create)                                                 \
+_(LI_DELETE, li_delete)                                                 \
+_(LI_DUMP, li_dump)                                                     \
 _(POLICER_ADD_DEL, policer_add_del)                                     \
 _(POLICER_DUMP, policer_dump)                                           \
 _(POLICER_CLASSIFY_SET_INTERFACE, policer_classify_set_interface)       \
