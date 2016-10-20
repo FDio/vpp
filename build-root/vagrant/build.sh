520cd4848fd547a7444490aac3178daf3e16c785
Billy McFall <bmcfall@redhat.com>
2016-10-20 08:50:11 -0400

    VPP-504: Permissions Error on Centos Vagrant VPP VM
    Change-Id: I5e63538e0ba2395fb13f99f0c7c2ca3123a34e59
    Signed-off-by: Billy McFall <bmcfall@redhat.com>

--- build-root/vagrant/build.sh
+++ build-root/vagrant/build.sh
@@ -68,7 +68,7 @@ $SUDOCMD make bootstrap
 if [ $DISTRIB_ID == "Ubuntu" ]; then
     $SUDOCMD make pkg-deb
 elif [ $DISTRIB_ID == "CentOS" ]; then
-    (cd $VPP_DIR/vnet ;aclocal; automake -a)
+    (cd $VPP_DIR/vnet ;$SUDOCMD aclocal;$SUDOCMD automake -a)
     $SUDOCMD make pkg-rpm
 fi
 
