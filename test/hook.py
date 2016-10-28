0be2aa2111cda74f3494ca35eb0c24dd1e435f24
Klement Sekera <ksekera@cisco.com>
2016-10-28 13:20:27 +0200

    tidy run flags for test framework add stepping capability to test framework fix test dependencies, adjust colors and log levels add help file for test framework move test framework documentation to build-root remove test documentation dependency on python api
    Change-Id: Ia9678aa2532ecb4cb33736aedb4a31aa3f2a3f93
    Signed-off-by: Klement Sekera <ksekera@cisco.com>

--- test/hook.py
+++ test/hook.py
@@ -1,13 +1,17 @@
 import signal
 import os
 import pexpect
-from logging import *
+import traceback
+from log import *
 
 
 class Hook(object):
     """
     Generic hooks before/after API/CLI calls
     """
+
+    def __init__(self, logger):
+        self.logger = logger
 
     def before_api(self, api_name, api_args):
         """
@@ -17,7 +21,8 @@ class Hook(object):
         @param api_name: name of the API
         @param api_args: tuple containing the API arguments
         """
-        debug("API: %s (%s)" % (api_name, api_args))
+        self.logger.debug("API: %s (%s)" %
+                          (api_name, api_args), extra={'color': RED})
 
     def after_api(self, api_name, api_args):
         """
@@ -35,7 +40,7 @@ class Hook(object):
 
         @param cli: CLI string
         """
-        debug("CLI: %s" % (cli))
+        self.logger.debug("CLI: %s" % (cli), extra={'color': RED})
 
     def after_cli(self, cli):
         """
@@ -54,6 +59,7 @@ class PollHook(Hook):
     def __init__(self, testcase):
         self.vpp_dead = False
         self.testcase = testcase
+        self.logger = testcase.logger
 
     def spawn_gdb(self, gdb_path, core_path):
         gdb_cmdline = gdb_path + ' ' + self.testcase.vpp_bin + ' ' + core_path
@@ -74,11 +80,12 @@ class PollHook(Hook):
                 self.spawn_gdb(gdb_path, core_path)
                 return
             else:
-                error("Debugger '%s' does not exist or is not an executable.." %
-                      gdb_path)
+                self.logger.error(
+                    "Debugger '%s' does not exist or is not an executable.." %
+                    gdb_path)
 
-        critical('core file present, debug with: gdb ' +
-                 self.testcase.vpp_bin + ' ' + core_path)
+        self.logger.critical('core file present, debug with: gdb ' +
+                             self.testcase.vpp_bin + ' ' + core_path)
 
     def poll_vpp(self):
         """
@@ -97,7 +104,7 @@ class PollHook(Hook):
             msg = "VPP subprocess died unexpectedly with returncode %d [%s]" % (
                 self.testcase.vpp.returncode,
                 signaldict[abs(self.testcase.vpp.returncode)])
-            critical(msg)
+            self.logger.critical(msg)
             core_path = self.testcase.tempdir + '/core'
             if os.path.isfile(core_path):
                 self.on_crash(core_path)
@@ -126,3 +133,92 @@ class PollHook(Hook):
         """
         super(PollHook, self).after_cli(cli)
         self.poll_vpp()
+
+
+class StepHook(PollHook):
+    """ Hook which requires user to press ENTER before doing any API/CLI """
+
+    def __init__(self, testcase):
+        self.skip_stack = None
+        self.skip_num = None
+        self.skip_count = 0
+        super(StepHook, self).__init__(testcase)
+
+    def skip(self):
+        if self.skip_stack is None:
+            return False
+        stack = traceback.extract_stack()
+        counter = 0
+        skip = True
+        for e in stack:
+            if counter > self.skip_num:
+                break
+            if e[0] != self.skip_stack[counter][0]:
+                skip = False
+            if e[1] != self.skip_stack[counter][1]:
+                skip = False
+            counter += 1
+        if skip:
+            self.skip_count += 1
+            return True
+        else:
+            print("%d API/CLI calls skipped in specified stack "
+                  "frame" % self.skip_count)
+            self.skip_count = 0
+            self.skip_stack = None
+            self.skip_num = None
+            return False
+
+    def user_input(self):
+        print('number\tfunction\tfile\tcode')
+        counter = 0
+        stack = traceback.extract_stack()
+        for e in stack:
+            print('%02d.\t%s\t%s:%d\t[%s]' % (counter, e[2], e[0], e[1], e[3]))
+            counter += 1
+        print(single_line_delim)
+        print("You can enter a number of stack frame chosen from above")
+        print("Calls in/below that stack frame will be not be stepped anymore")
+        print(single_line_delim)
+        while True:
+            choice = raw_input("Enter your choice, if any, and press ENTER to "
+                               "continue running the testcase...")
+            if choice == "":
+                choice = None
+            try:
+                if choice is not None:
+                    num = int(choice)
+            except:
+                print("Invalid input")
+                continue
+            if choice is not None and (num < 0 or num >= len(stack)):
+                print("Invalid choice")
+                continue
+            break
+        if choice is not None:
+            self.skip_stack = stack
+            self.skip_num = num
+
+    def before_cli(self, cli):
+        """ Wait for ENTER before executing CLI """
+        if self.skip():
+            print("Skip pause before executing CLI: %s" % cli)
+        else:
+            print(double_line_delim)
+            print("Test paused before executing CLI: %s" % cli)
+            print(single_line_delim)
+            self.user_input()
+        super(StepHook, self).before_cli(cli)
+
+    def before_api(self, api_name, api_args):
+        """ Wait for ENTER before executing API """
+        if self.skip():
+            print("Skip pause before executing API: %s (%s)"
+                  % (api_name, api_args))
+        else:
+            print(double_line_delim)
+            print("Test paused before executing API: %s (%s)"
+                  % (api_name, api_args))
+            print(single_line_delim)
+            self.user_input()
+        super(StepHook, self).before_api(api_name, api_args)
