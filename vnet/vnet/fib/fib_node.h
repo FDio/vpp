4fffb75b051f1f00be3c4207ebce804dbd1fcc43
John Lo <loj@cisco.com>
2016-11-07 18:30:47 -0500

    VXLAN tunnel encap forwarding optimization with FIB 2.0
    Optimize VXLAN encap node so that vxlan4-encap node is used for IP4
    and vxlan6-encap node is used for IP6 underlay. Also stack the VXLAN
    encap nodes to the appropriate FIB IP4 or IP6 load-balance node
    instead of ip4/ip6-lookup node to save IP lookup operation.
    
    For VXLAN decap node, check VXLAN header FLAGS field for each packet
    and remove the code to support decap-next for IP4 or IP6. These decap-
    next values were intended for experimentation purposes and not needed
    any more since VXLAN-GPE tunnel is supported. The decap-next field is
    still kept in API for backward compatibility and its value has no
    effect. Decap next for both vxlan4-decap and vxlan6-decap nodes is
    always l2-input node.
    
    Change-Id: I8ac95774946549ec403ab691f999df0c006b460f
    Signed-off-by: John Lo <loj@cisco.com>

--- vnet/vnet/fib/fib_node.h
+++ vnet/vnet/fib/fib_node.h
@@ -38,6 +38,7 @@ typedef enum fib_node_type_t_ {
     FIB_NODE_TYPE_LISP_GPE_FWD_ENTRY,
     FIB_NODE_TYPE_LISP_ADJ,
     FIB_NODE_TYPE_GRE_TUNNEL,
+    FIB_NODE_TYPE_VXLAN_TUNNEL,
     /**
      * Marker. New types before this one. leave the test last.
      */
@@ -57,6 +58,7 @@ typedef enum fib_node_type_t_ {
     [FIB_NODE_TYPE_LISP_GPE_FWD_ENTRY] = "lisp-gpe-fwd-entry", \
     [FIB_NODE_TYPE_LISP_ADJ] = "lisp-adj", \
     [FIB_NODE_TYPE_GRE_TUNNEL] = "gre-tunnel", \
+    [FIB_NODE_TYPE_VXLAN_TUNNEL] = "vxlan-tunnel", \
 }
 
 /**
