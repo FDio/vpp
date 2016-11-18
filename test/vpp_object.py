bbd1f61c34e3e1597a475173711c62677020982d
Klement Sekera <ksekera@cisco.com>
2016-11-18 10:00:59 +0100

    make test: add support for config auto-cleanup
    Define an interface VppObject, containing methods for adding, removing
    and querying the existence of object in VPP config.
    
    Define VppObjectRegistry, which automatically checks for any left-over
    configuration in VPP after test case finishes.
    
    Make VppTestCase inherit the VppObjectRegistry behaviour and add the
    implementation of necessary methods to VppPGInterface.
    
    Change-Id: Ic06cd2743b58e3f097d04474a886f9f21d8a8aed
    Signed-off-by: Klement Sekera <ksekera@cisco.com>

---
+++ test/vpp_object.py
@@ -0,0 +1,61 @@
+from abc import abstractmethod
+
+
+class VppObject(object):
+    """abstract vpp object"""
+
+    @abstractmethod
+    def add_vpp_config(self):
+        """Add the configuration for this object to vpp"""
+        pass
+
+    @abstractmethod
+    def query_vpp_config(self):
+        """Query the vpp configuration
+
+        :return: True if the object is configured"""
+        pass
+
+    @abstractmethod
+    def remove_vpp_config(self):
+        """Remove the configuration for this object from vpp"""
+        pass
+
+
+class VppObjectRegistry(object):
+    """Class which handles automatic configuration cleanup."""
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @classmethod
+    def register(cls, o):
+        """Register an object in the registry."""
+        if not hasattr(cls, "_object_registry"):
+            cls._object_registry = []
+        cls._object_registry.append(o)
+
+    @classmethod
+    def remove_vpp_config(cls):
+        """Remove configuration (if present) for all objects in the registry."""
+        if not hasattr(cls, "_object_registry"):
+            cls.logger.info("No objects registered for auto-cleanup.")
+            return
+        cls.logger.info("Removing VPP configuration for registered objects")
+        for o in reversed(cls._object_registry):
+            if o.query_vpp_config():
+                cls.logger.info("Removing %s", o)
+                o.remove_vpp_config()
+            else:
+                cls.logger.info("Skipping %s, configuration not present", o)
+        failed = []
+        for o in cls._object_registry:
+            if o.query_vpp_config():
+                failed.append(o)
+        if failed:
+            cls.logger.error("Couldn't remove configuration for object(s):")
+            for x in failed:
+                cls.logger.error(repr(x))
+            raise Exception("Couldn't remove configuration for object(s): %s" %
+                            (", ".join(str(x) for x in failed)))
