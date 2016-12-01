7eb1fd3fc5cda5046c2bc8d09028c74c48b3c13f
AkshayaNadahalli <anadahal@cisco.com>
2016-12-01 16:33:51 +0530

    Analyse node - Initial Draft
    Change-Id: I6facb9ef8553a21464f9a2e612706f152badbb68
    Signed-off-by: AkshayaNadahalli <anadahal@cisco.com>

---
+++ plugins/ioam-plugin/ioam/analyse/ip6/ip6_ioam_analyse.c
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <ioam/analyse/ioam_analyse.h>
+#include <ioam/export-common/ioam_export.h>
+#include <ioam/analyse/ip6/ip6_ioam_analyse.h>
+#include <ioam/analyse/ioam_summary_export.h>
+#include <vnet/ip/ip.h>
+#include <ioam/ipfixcollector/ipfixcollector.h>
+
+
+static clib_error_t *
+set_ioam_analyse_command_fn (vlib_main_t *vm, unformat_input_t *input,
+                             vlib_cli_command_t *cmd)
+{
+  //int rv;
+  int is_export = 0;
+  int is_add = 1;
+  int remote_listen = 0;
+  ipfix_client_add_del_t ipfix_reg;
+
+  strncpy((char *)ipfix_reg.client_name, "ip6-hbh-analyse-remote",
+          strlen("ip6-hbh-analyse-remote"));
+  ipfix_reg.client_node = analyse_node_remote.index;
+  ipfix_reg.ipfix_setid = IPFIX_IOAM_EXPORT_ID;
+
+  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat(input, "export-ipfix-collector"))
+        is_export = 1;
+      else if (unformat(input, "disable"))
+        is_add = 0;
+      else if (unformat(input, "listen-ipfix"))
+        remote_listen = 1;
+      else
+        break;
+    }
+
+  if (is_add)
+    {
+      if (is_export)
+        (void) ioam_flow_create (0);
+
+      ip6_ioam_analyse_register_handlers();
+      if (remote_listen)
+        {
+          ipfix_reg.del = 0;
+          ipfix_collector_reg_flow(vm, &ipfix_reg);
+        }
+      else
+        {
+          ioam_export_set_next_node(&ioam_export_main,
+                                    (u8 *) "ip6-hbh-analyse-local");
+        }
+    }
+  else
+    {
+      if (is_export)
+        (void) ioam_flow_create (1);
+
+      //memset((void *) &ioam_analyser_main, 0, sizeof(ioam_analyser_main));
+      ip6_ioam_analyse_unregister_handlers();
+      if (remote_listen)
+        {
+          ipfix_reg.del = 1;
+          ipfix_collector_reg_flow(vm, &ipfix_reg);
+        }
+      else
+        ioam_export_reset_next_node(&ioam_export_main);
+    }
+  return 0;
+}
+
+VLIB_CLI_COMMAND (set_ioam_analyse_command, static) = {
+    .path = "set ioam analyse",
+    .short_help = "set ioam analyse [export-ipfix-collector] [disable] [listen-ipfix]",
+    .function = set_ioam_analyse_command_fn,
+};
+
+static clib_error_t *
+show_ioam_analyse_cmd_fn (vlib_main_t * vm, unformat_input_t * input,
+                          vlib_cli_command_t * cmd)
+{
+  ip6_ioam_analyser_main_t *am = &ioam_analyser_main;
+  ioam_analyser_data_t *record = NULL;
+  u8 i;
+  u8 *s = 0;
+
+  vec_reset_length(s);
+  s = format (0, "iOAM Analyse Information: \n");
+  vec_foreach_index(i, am->aggregated_data)
+  {
+    record = am->aggregated_data + i;
+    if (record->is_free)
+      continue;
+
+    s = format (s, "Flow Number: %u\n", i);
+    s = print_analyse_flow(s, record);
+    s = format(s, "\n");
+  }
+  vlib_cli_output (vm, "%v", s);
+
+  vec_free(s);
+
+  return 0;
+}
+
+VLIB_CLI_COMMAND (ip6_show_ioam_ipfix_cmd, static) =
+{
+  .path = "show ioam analyse ",
+  .short_help = "show ioam analyser information",
+  .function = show_ioam_analyse_cmd_fn,
+};
+
+static clib_error_t *
+ioam_analyse_init (vlib_main_t *vm)
+{
+  ip6_ioam_analyser_main_t *am = &ioam_analyser_main;
+  u16 i;
+
+  vec_validate_aligned(am->aggregated_data,
+                       200,
+                       CLIB_CACHE_LINE_BYTES);
+  vec_foreach_index(i, am->aggregated_data)
+  {
+    ioam_analyse_init_data(am->aggregated_data + i);
+  }
+
+  return 0;
+}
+
+VLIB_INIT_FUNCTION(ioam_analyse_init);
---
+++ plugins/ioam-plugin/ioam/ipfixcollector/ipfixcollector.c
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vnet/ip/ip.h>
+#include <vnet/plugin/plugin.h>
+#include <vnet/ip/udp.h>
+#include <ioam/ipfixcollector/ipfixcollector.h>
+
+ipfix_collector_main_t ipfix_collector_main;
+
+int
+ipfix_collector_reg_flow (vlib_main_t * vm, ipfix_client_add_del_t * info)
+{
+  ipfix_collector_main_t *cm = &ipfix_collector_main;
+  uword *p = NULL;
+  int i;
+  ipfix_client *client = 0;
+
+  if ((!info) || ('\0' == info->client_name[0]))
+    return IPFIX_COLLECTOR_ERR_INVALID_PARAM;
+
+  p = hash_get (cm->client_reg_table, info->ipfix_setid);
+  client = p ? pool_elt_at_index (cm->client_reg_pool, (*p)) : NULL;
+
+  if (info->del)
+    {
+      if (!client)
+	return 0;		//There is no registered handler, so send success
+
+      hash_unset (cm->client_reg_table, info->ipfix_setid);
+      pool_put (cm->client_reg_pool, client);
+      return 0;
+    }
+
+  if (client)
+    return IPFIX_COLLECTOR_ERR_REG_EXISTS;
+
+  pool_get (cm->client_reg_pool, client);
+  i = client - cm->client_reg_pool;
+  strncpy ((char *) client->client_name, (char *) info->client_name,
+	   IPFIX_COLLECTOR_CLIENT_NAME_MAX - 1);
+  client->client_node = info->client_node;
+  client->client_next_node = vlib_node_add_next (vm,
+						 ipfix_collector_node.index,
+						 client->client_node);
+  client->set_id = info->ipfix_setid;
+
+  hash_set (cm->client_reg_table, info->ipfix_setid, i);
+  return 0;
+}
+
+/**
+ * @brief plugin-api required function
+ * @param vm vlib_main_t * vlib main data structure pointer
+ * @param h vlib_plugin_handoff_t * handoff structure
+ * @param from_early_init int notused
+ *
+ * <em>Notes:</em>
+ * This routine exists to convince the vlib plugin framework that
+ * we haven't accidentally copied a random .dll into the plugin directory.
+ *
+ * Also collects global variable pointers passed from the vpp engine
+ */
+clib_error_t *
+vlib_plugin_register (vlib_main_t * vm, vnet_plugin_handoff_t * h,
+		      int from_early_init)
+{
+  ipfix_collector_main_t *cm = &ipfix_collector_main;
+  clib_error_t *error = 0;
+  cm->vlib_main = vm;
+  cm->vnet_main = h->vnet_main;
+
+  return error;
+}
+
+static clib_error_t *
+ipfix_collector_init (vlib_main_t * vm)
+{
+  clib_error_t *error = 0;
+  ipfix_collector_main_t *cm = &ipfix_collector_main;
+
+  cm->client_reg_pool = NULL;
+  cm->client_reg_table = hash_create (0, sizeof (uword));
+
+  udp_register_dst_port (vm,
+			 UDP_DST_PORT_ipfix,
+			 ipfix_collector_node.index, 1 /* is_ip4 */ );
+  return error;
+}
+
+VLIB_INIT_FUNCTION (ipfix_collector_init);
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
