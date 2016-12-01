696e924995c51c279ecc053772fa2662b47571ae
AkshayaNadahalli <anadahal@cisco.com>
2016-12-01 16:33:51 +0530

    VPP as IP-Fix collector and data analyser for iOAM
    New nodes added:
    ipfix-collector: Listens on IP-FIX port 4739 and distributes packets to registered nodes based on IP-FIX setID.
    ip6-hbh-analyse-remote: Registers with ipfix-collector to receive IP-FIX records for iOAM, analyses and exports summarised data.
    ip6-hbh-analyse-local: Registers with ip6-export node to get iOAM records, analyses and exports summarised data.
    
    Change-Id: I6facb9ef8553a21464f9a2e612706f152badbb68
    Signed-off-by: AkshayaNadahalli <anadahal@cisco.com>

---
+++ src/plugins/ioam/analyse/ip6/ip6_ioam_analyse.c
@@ -0,0 +1,162 @@
+/*
+ * Copyright (c) 2017 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <ioam/analyse/ioam_analyse.h>
+#include <ioam/export-common/ioam_export.h>
+#include <ioam/analyse/ip6/ip6_ioam_analyse.h>
+#include <ioam/analyse/ioam_summary_export.h>
+#include <vnet/ip/ip.h>
+#include <ioam/ipfixcollector/ipfixcollector.h>
+
+static clib_error_t *
+ioam_analyse_enable_disable (vlib_main_t * vm,
+			     int is_add, int is_export, int remote_listen)
+{
+  ipfix_client_add_del_t ipfix_reg;
+  clib_error_t *rv = 0;
+
+  ipfix_reg.client_name = format (0, "ip6-hbh-analyse-remote");
+  ipfix_reg.client_node = analyse_node_remote.index;
+  ipfix_reg.ipfix_setid = IPFIX_IOAM_EXPORT_ID;
+
+  if (is_export)
+    {
+      rv = ioam_flow_create (!is_add);
+      if (rv)
+	goto ret;
+    }
+
+  if (is_add)
+    {
+      ip6_ioam_analyse_register_handlers ();
+      if (remote_listen)
+	{
+	  ipfix_reg.del = 0;
+	  ipfix_collector_reg_setid (vm, &ipfix_reg);
+	}
+      else
+	{
+	  ioam_export_set_next_node (&ioam_export_main,
+				     (u8 *) "ip6-hbh-analyse-local");
+	}
+    }
+  else
+    {
+      ip6_ioam_analyse_unregister_handlers ();
+      if (remote_listen)
+	{
+	  ipfix_reg.del = 1;
+	  ipfix_collector_reg_setid (vm, &ipfix_reg);
+	}
+      else
+	ioam_export_reset_next_node (&ioam_export_main);
+    }
+
+ret:
+  vec_free (ipfix_reg.client_name);
+  return rv;
+}
+
+static clib_error_t *
+set_ioam_analyse_command_fn (vlib_main_t * vm, unformat_input_t * input,
+			     vlib_cli_command_t * cmd)
+{
+  int is_export = 0;
+  int is_add = 1;
+  int remote_listen = 0;
+
+  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (input, "export-ipfix-collector"))
+	is_export = 1;
+      else if (unformat (input, "disable"))
+	is_add = 0;
+      else if (unformat (input, "listen-ipfix"))
+	remote_listen = 1;
+      else
+	break;
+    }
+
+  return (ioam_analyse_enable_disable (vm, is_add, is_export, remote_listen));
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (set_ioam_analyse_command, static) = {
+  .path = "set ioam analyse",
+  .short_help = "set ioam analyse [export-ipfix-collector] [disable] [listen-ipfix]",
+  .function = set_ioam_analyse_command_fn,
+};
+/* *INDENT-ON* */
+
+static clib_error_t *
+show_ioam_analyse_cmd_fn (vlib_main_t * vm, unformat_input_t * input,
+			  vlib_cli_command_t * cmd)
+{
+  ip6_ioam_analyser_main_t *am = &ioam_analyser_main;
+  ioam_analyser_data_t *record = NULL;
+  u8 i;
+  u8 *s = 0;
+
+  vec_reset_length (s);
+  s = format (0, "iOAM Analyse Information: \n");
+  vec_foreach_index (i, am->aggregated_data)
+  {
+    record = am->aggregated_data + i;
+    if (record->is_free)
+      continue;
+
+    s = format (s, "Flow Number: %u\n", i);
+    s = print_analyse_flow (s, record);
+    s = format (s, "\n");
+  }
+  vlib_cli_output (vm, "%v", s);
+
+  vec_free (s);
+
+  return 0;
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (ip6_show_ioam_ipfix_cmd, static) = {
+  .path = "show ioam analyse ",
+  .short_help = "show ioam analyser information",
+  .function = show_ioam_analyse_cmd_fn,
+};
+/* *INDENT-ON* */
+
+static clib_error_t *
+ioam_analyse_init (vlib_main_t * vm)
+{
+  ip6_ioam_analyser_main_t *am = &ioam_analyser_main;
+  u16 i;
+
+  vec_validate_aligned (am->aggregated_data, 50, CLIB_CACHE_LINE_BYTES);
+  vec_foreach_index (i, am->aggregated_data)
+  {
+    ioam_analyse_init_data (am->aggregated_data + i);
+  }
+
+  return 0;
+}
+
+VLIB_INIT_FUNCTION (ioam_analyse_init);
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
---
+++ src/plugins/ioam/lib-e2e/ioam_seqno_lib.h
@@ -0,0 +1,201 @@
+/*
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef PLUGINS_IOAM_PLUGIN_IOAM_LIB_E2E_IOAM_SEQNO_LIB_H_
+#define PLUGINS_IOAM_PLUGIN_IOAM_LIB_E2E_IOAM_SEQNO_LIB_H_
+
+#include <vppinfra/types.h>
+
+#define SEQ_CHECK_VALUE 0x80000000	/* for seq number wraparound detection */
+
+#define SEQNO_WINDOW_SIZE 2048
+#define SEQNO_WINDOW_ARRAY_SIZE 64
+
+typedef struct seqno_bitmap_
+{
+  u32 window_size;
+  u32 array_size;
+  u32 mask;
+  u32 pad;
+  u64 highest;
+  u64 array[SEQNO_WINDOW_ARRAY_SIZE];	/* Will be alloc to array_size */
+} seqno_bitmap;
+
+typedef struct seqno_rx_info_
+{
+  u64 rx_packets;
+  u64 lost_packets;
+  u64 reordered_packets;
+  u64 dup_packets;
+  seqno_bitmap bitmap;
+} seqno_rx_info;
+
+/* This structure is 64-byte aligned */
+typedef struct ioam_seqno_data_
+{
+  union
+  {
+    u32 seq_num;		/* Useful only for encap node */
+    seqno_rx_info seqno_rx;
+  };
+} ioam_seqno_data;
+
+static inline void
+BIT_SET (u64 * p, u32 n)
+{
+  p[n >> 5] |= (1 << (n & 31));
+}
+
+static inline int
+BIT_TEST (u64 * p, u32 n)
+{
+  return p[n >> 5] & (1 << (n & 31));
+}
+
+static void
+BIT_CLEAR (u64 * p, u64 start, int num_bits, u32 mask)
+{
+  int n, t;
+  int start_index = (start >> 5);
+  int mask_index = (mask >> 5);
+
+  start_index &= mask_index;
+  if (start & 0x1f)
+    {
+      int start_bit = (start & 0x1f);
+
+      n = (1 << start_bit) - 1;
+      t = start_bit + num_bits;
+      if (t < 32)
+	{
+	  n |= ~((1 << t) - 1);
+	  p[start_index] &= n;
+	  return;
+	}
+      p[start_index] &= n;
+      start_index = (start_index + 1) & mask_index;
+      num_bits -= (32 - start_bit);
+    }
+  while (num_bits >= 32)
+    {
+      p[start_index] = 0;
+      start_index = (start_index + 1) & mask_index;
+      num_bits -= 32;
+    }
+  n = ~((1 << num_bits) - 1);
+  p[start_index] &= n;
+}
+
+static inline u8
+seqno_check_wraparound (u32 a, u32 b)
+{
+  if ((a != b) && (a > b) && ((a - b) > SEQ_CHECK_VALUE))
+    {
+      return 1;
+    }
+  return 0;
+}
+
+/*
+ * Function to analyze the PPC value recevied.
+ *     - Updates the bitmap with received sequence number
+ *     - counts the received/lost/duplicate/reordered packets
+ */
+inline static void
+ioam_analyze_seqno (seqno_rx_info * seqno_rx, u64 seqno)
+{
+  int diff;
+  static int peer_dead_count;
+  seqno_bitmap *bitmap = &seqno_rx->bitmap;
+
+  seqno_rx->rx_packets++;
+
+  if (seqno > bitmap->highest)
+    {				/* new larger sequence number */
+      peer_dead_count = 0;
+      diff = seqno - bitmap->highest;
+      if (diff < bitmap->window_size)
+	{
+	  if (diff > 1)
+	    {			/* diff==1 is *such* a common case it's a win to optimize it */
+	      BIT_CLEAR (bitmap->array, bitmap->highest + 1, diff - 1,
+			 bitmap->mask);
+	      seqno_rx->lost_packets += diff - 1;
+	    }
+	}
+      else
+	{
+	  seqno_rx->lost_packets += diff - 1;
+	  memset (bitmap->array, 0, bitmap->array_size * sizeof (u64));
+	}
+      BIT_SET (bitmap->array, seqno & bitmap->mask);
+      bitmap->highest = seqno;
+      return;
+    }
+
+  /* we've seen a bigger seq number before */
+  diff = bitmap->highest - seqno;
+  if (diff >= bitmap->window_size)
+    {
+      if (seqno_check_wraparound (bitmap->highest, seqno))
+	{
+	  memset (bitmap->array, 0, bitmap->array_size * sizeof (u64));
+	  BIT_SET (bitmap->array, seqno & bitmap->mask);
+	  bitmap->highest = seqno;
+	  return;
+	}
+      else
+	{
+	  peer_dead_count++;
+	  if (peer_dead_count > 25)
+	    {
+	      peer_dead_count = 0;
+	      memset (bitmap->array, 0, bitmap->array_size * sizeof (u64));
+	      BIT_SET (bitmap->array, seqno & bitmap->mask);
+	      bitmap->highest = seqno;
+	    }
+	  //ppc_rx->reordered_packets++;
+	}
+      return;
+    }
+
+  if (BIT_TEST (bitmap->array, seqno & bitmap->mask))
+    {
+      seqno_rx->dup_packets++;
+      return;			/* Already seen */
+    }
+  seqno_rx->reordered_packets++;
+  seqno_rx->lost_packets--;
+  BIT_SET (bitmap->array, seqno & bitmap->mask);
+  return;
+}
+
+u8 *show_ioam_seqno_analyse_data_fn (u8 * s, seqno_rx_info * rx);
+
+u8 *show_ioam_seqno_cmd_fn (u8 * s, ioam_seqno_data * seqno_data, u8 enc);
+
+void ioam_seqno_init_data (ioam_seqno_data * data);
+
+void ioam_seqno_init_rx_info (seqno_rx_info * data);
+
+#endif /* PLUGINS_IOAM_PLUGIN_IOAM_LIB_E2E_IOAM_SEQNO_LIB_H_ */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
