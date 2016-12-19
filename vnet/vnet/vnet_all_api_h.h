9cd20aab6e1711f8696469e96533d615d29b6459
Pavel Kotucek <pkotucek@cisco.com>
2016-12-19 14:47:09 +0100

    API refactoring : vxlan-gpe
    Change-Id: Ia31de797e0ff79ceff010b496af30f5196de9fd6
    Signed-off-by: Pavel Kotucek <pkotucek@cisco.com>

--- vnet/vnet/vnet_all_api_h.h
+++ vnet/vnet/vnet_all_api_h.h
@@ -35,6 +35,7 @@
 #include <vnet/span/span.api.h>
 #include <vnet/ip/ip.api.h>
 #include <vnet/unix/tap.api.h>
+#include <vnet/vxlan-gpe/vxlan_gpe.api.h>
 
 /*
  * fd.io coding-style-patch-verification: ON
