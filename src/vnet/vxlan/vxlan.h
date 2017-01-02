12802ad7bbe802ae1aba3b528d0edeb6ee7dd4f8
Eyal Bari <ebari@cisco.com>
2017-01-02 14:29:21 +0200

    vxlan fix mcast tunnel delete
    Change-Id: I15f7ff1e957718e808bfad811895deaacb85d2a3
    Signed-off-by: Eyal Bari <ebari@cisco.com>

--- src/vnet/vxlan/vxlan.h
+++ src/vnet/vxlan/vxlan.h
@@ -138,7 +138,7 @@ typedef enum {
 } vxlan_input_error_t;
 
 typedef struct {
-  ip46_address_t ip;
+  ip46_address_t * ip;
   fib_node_index_t fib_entry_index;
   adj_index_t mcast_adj_index;
 } mcast_remote_t;
