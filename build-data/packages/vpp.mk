57f16255437a90c582831b5a29656d32e5f87129
Damjan Marion <damarion@cisco.com>
2017-01-09 20:24:50 +0100

    Add own dpdk packaging
    Change-Id: I6aa2a6709241d99ce734c29e47487eb456907351
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- build-data/packages/vpp.mk
+++ build-data/packages/vpp.mk
@@ -19,8 +19,9 @@ vpp_CPPFLAGS += -I$($(PLATFORM)_dpdk_inc_dir)
 vpp_LDFLAGS += -L$($(PLATFORM)_dpdk_lib_dir)
 else
 vpp_configure_depend += dpdk-install
-vpp_CPPFLAGS += $(call installed_includes_fn, dpdk)
+vpp_CPPFLAGS += $(call installed_includes_fn, dpdk)/dpdk
 vpp_LDFLAGS += $(call installed_libs_fn, dpdk)
+vpp_CPPFLAGS += -I/usr/include/dpdk
 endif
 ifeq ($($(PLATFORM)_uses_dpdk_cryptodev),yes)
 vpp_configure_args += --with-dpdk-crypto
