0e6715c0408b41780e2277730018acc2f09700fa
AkshayaNadahalli <anadahal@cisco.com>
2017-02-07 23:59:54 +0530

    Fix source address reachability check for ip6 local packets
    Currently ip6 local check fails with error - source lookup miss if
    route to source of packet is over a dpo object such as load balance -
    U/ECMP, recurssive route etc.
    So unless packet source is of a directly connected neibhor or has
    route with both interface and nexthop specified, it will be dropped.
    Fix is to check urpf list and if at least one link exists in the list,
    then allow packets to be processed, else drop.
    
    Change-Id: Id426311bb63bab506754a79409c602fdb6d0f190
    Signed-off-by: AkshayaNadahalli <anadahal@cisco.com>

--- src/vnet/fib/ip6_fib.h
+++ src/vnet/fib/ip6_fib.h
@@ -20,6 +20,7 @@
 #include <vnet/ip/format.h>
 #include <vnet/fib/fib_entry.h>
 #include <vnet/fib/fib_table.h>
+#include <vnet/fib/fib_urpf_list.h>
 #include <vnet/ip/lookup.h>
 #include <vnet/dpo/load_balance.h>
 
@@ -65,6 +66,27 @@ u32 ip6_fib_table_fwding_lookup(ip6_main_t * im,
 extern void ip6_fib_table_walk(u32 fib_index,
                                fib_table_walk_fn_t fn,
                                void *ctx);
+
+/**
+ * @biref returns number of links on which src is reachable.
+ */
+always_inline int
+ip6_src_check_for_packet (ip6_main_t * im,
+		       vlib_buffer_t * b,
+		       ip6_header_t * i)
+{
+  const load_balance_t *lb0;
+  index_t lbi;
+
+  lbi = ip6_fib_table_fwding_lookup_with_if_index(
+      im,
+      vnet_buffer (b)->sw_if_index[VLIB_RX],
+      &i->src_address);
+
+  lb0 = load_balance_get(lbi);
+
+  return (fib_urpf_check_size (lb0->lb_urpf));
+}
 
 /**
  * @biref return the DPO that the LB stacks on.
