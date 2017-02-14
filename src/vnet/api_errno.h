5fc9bd18e7a78fd3e6ddf6815df5aebf4b56de95
Neale Ranns <nranns@cisco.com>
2017-02-13 23:20:04 -0800

    Changing the IP table for an interface is an error if the interface already has an address configured (VPP-601)
    Change-Id: I311fc264f73dd3b2b3ce9d7d1c33cd0515b36c4a
    Signed-off-by: Neale Ranns <nranns@cisco.com>

--- src/vnet/api_errno.h
+++ src/vnet/api_errno.h
@@ -102,7 +102,8 @@ _(URI_FIFO_CREATE_FAILED, -109, "URI FIFO segment create failed")       \
 _(LISP_RLOC_LOCAL, -110, "RLOC address is local")                       \
 _(BFD_EAGAIN, -111, "BFD object cannot be manipulated at this time")	\
 _(INVALID_GPE_MODE, -112, "Invalid GPE mode")                           \
-_(LISP_GPE_ENTRIES_PRESENT, -113, "LISP GPE entries are present")
+_(LISP_GPE_ENTRIES_PRESENT, -113, "LISP GPE entries are present")       \
+_(ADDRESS_FOUND_FOR_INTERFACE, -114, "Address found for interface")
 
 typedef enum
 {
