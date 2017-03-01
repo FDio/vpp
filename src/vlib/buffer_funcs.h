2af0087f6ece556bab524dacd311d3410907cf58
Dave Barach <dave@barachs.net>
2017-03-01 11:38:02 -0500

    Fix buffer template copy
    Change-Id: If451c9cb68719fc816999b0330b9be3a0169176a
    Signed-off-by: Dave Barach <dave@barachs.net>

--- src/vlib/buffer_funcs.h
+++ src/vlib/buffer_funcs.h
@@ -781,6 +781,7 @@ vlib_buffer_init_for_free_list (vlib_buffer_t * dst,
 				vlib_buffer_free_list_t * fl)
 {
   vlib_buffer_t *src = &fl->buffer_init_template;
+  u64 *d0, *s0;
 
   /* Make sure vlib_buffer_t is cacheline aligned and sized */
   ASSERT (STRUCT_OFFSET_OF (vlib_buffer_t, cacheline0) == 0);
@@ -792,14 +793,23 @@ vlib_buffer_init_for_free_list (vlib_buffer_t * dst,
   /* Make sure buffer template is sane. */
   ASSERT (fl->index == fl->buffer_init_template.free_list_index);
 
+  d0 = (u64 *) dst;
+  s0 = (u64 *) src;
+
+  d0[0] = s0[0];
+  d0[1] = s0[1];
+
+  /* Not in the first 16 octets. */
+  dst->n_add_refs = src->n_add_refs;
+
   /* Make sure it really worked. */
-#define _(f) dst->f = src->f
+#define _(f) ASSERT (dst->f == src->f);
   _(current_data);
   _(current_length);
   _(flags);
   _(free_list_index);
 #undef _
-  dst->total_length_not_including_first_buffer = 0;
+  ASSERT (dst->total_length_not_including_first_buffer == 0);
   ASSERT (dst->n_add_refs == 0);
 }
 
@@ -821,19 +831,34 @@ vlib_buffer_init_two_for_free_list (vlib_buffer_t * dst0,
 				    vlib_buffer_free_list_t * fl)
 {
   vlib_buffer_t *src = &fl->buffer_init_template;
+  u64 *d0, *d1, *s0;
 
   /* Make sure buffer template is sane. */
   ASSERT (fl->index == fl->buffer_init_template.free_list_index);
 
+  d0 = (u64 *) dst0;
+  d1 = (u64 *) dst1;
+  s0 = (u64 *) src;
+
+  d0[0] = s0[0];
+  d1[0] = s0[0];
+
+  d0[1] = s0[1];
+  d1[1] = s0[1];
+  /* Not in the first 16 octets. */
+  dst0->n_add_refs = src->n_add_refs;
+  dst1->n_add_refs = src->n_add_refs;
+
   /* Make sure it really worked. */
-#define _(f) dst0->f = src->f;  dst1->f = src->f
+#define _(f) ASSERT (dst0->f == src->f);  ASSERT( dst1->f == src->f)
   _(current_data);
   _(current_length);
   _(flags);
   _(free_list_index);
 #undef _
-  dst0->total_length_not_including_first_buffer = 0;
-  dst1->total_length_not_including_first_buffer = 0;
+
+  ASSERT (dst0->total_length_not_including_first_buffer == 0);
+  ASSERT (dst1->total_length_not_including_first_buffer == 0);
   ASSERT (dst0->n_add_refs == 0);
   ASSERT (dst1->n_add_refs == 0);
 }
