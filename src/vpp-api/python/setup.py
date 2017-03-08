0c39f1400eabbdb7f11974968edd0ecdd4b9d68f
Ole Troan <ot@cisco.com>
2017-03-08 12:02:24 +0100

    Python API: Change from cPython to CFFI.
    Change-Id: I03e52466fb3f909ae52b8fba601168f3eadbd972
    Signed-off-by: Ole Troan <ot@cisco.com>

--- src/vpp-api/python/setup.py
+++ src/vpp-api/python/setup.py
@@ -18,18 +18,12 @@ except ImportError:
     from distutils.core import setup, Extension
 
 setup (name = 'vpp_papi',
-       version = '1.3',
+       version = '1.4',
        description = 'VPP Python binding',
        author = 'Ole Troan',
        author_email = 'ot@cisco.com',
        test_suite = 'tests',
        packages=['vpp_papi'],
-       ext_modules = [
-           Extension(
-               'vpp_api',
-               sources = ['vpp_papi/pneum_wrap.c'],
-               libraries = ['pneum'],
-           )],
        long_description = '''VPP Python language binding.''',
        zip_safe = True,
 )
