948c276635609da7c003dd860e24df02fd2dbc53
Ole Troan <ot@cisco.com>
2017-03-13 13:39:52 +0100

    Flowperpkt: Stateful flows and IPv6, L4 recording
    Change-Id: I67839281623721bf42f0a918a53356143d9dc78a
    Signed-off-by: Ole Troan <ot@cisco.com>
    Signed-off-by: Pavel Kotucek <pkotucek@cisco.com>

--- test/test_flowperpkt.py
+++ test/test_flowperpkt.py
@@ -1,46 +1,128 @@
 #!/usr/bin/env python
 
 import unittest
+import time
+import struct
 
 from framework import VppTestCase, VppTestRunner
+from vpp_object import VppObject
+from vpp_pg_interface import CaptureTimeoutError
+from util import Host, ppp
 
 from scapy.packet import Raw
 from scapy.layers.l2 import Ether
 from scapy.layers.inet import IP, UDP
 
+from ipfix import IPFIX, Set, Template, Data, IPFIXDecoder
 
-class TestFlowperpkt(VppTestCase):
+class VppIPFIX(VppObject):
+    """VPP IPFIX"""
+
+    def __init__(self, test, intf, active=0,
+                 passive=0, timeout=20):
+        self._test = test
+        self._intf = intf
+        self._active = active
+        self._passive = passive
+        self._timeout = timeout
+        self._configured = False
+
+    def add_vpp_config(self):
+        self._test.vapi.set_ipfix_exporter(
+            collector_address=self._test.pg0.remote_ip4n,
+            src_address=self._test.pg0.local_ip4n,
+            path_mtu=512,
+            template_interval=self._timeout)
+        self._test.vapi.ppcli("flowperpkt params record l2 l3 l4 active %s "
+                            "passive %s" %(self._active, self._passive))
+        self._test.vapi.ppcli("flowperpkt feature add-del %s l2" % self._intf)
+        self._test.vapi.cli("ipfix flush")
+        self._configured = True
+
+    def remove_vpp_config(self):
+        self._test.vapi.cli("set ipfix exporter collector 0.0.0.0")
+        self._test.vapi.cli("flowperpkt feature add-del %s l2 disable" %
+                            self._intf)
+        self._configured = False
+
+    def object_id(self):
+        return "ipfix-collector-%s" % (self._src, self.dst)
+
+    def query_vpp_config(self):
+        return self._configured
+
+    def verify_templates(self, decoder=None, timeout=1):
+        p = self._test.wait_for_cflow_packet(self._test.pg0, 2, timeout)
+        self._test.assertTrue(p.haslayer(IPFIX))
+        if decoder is not None and p.haslayer(Template):
+            decoder.add_template(p.getlayer(Template))
+        p = self._test.wait_for_cflow_packet(self._test.pg0, 2)
+        self._test.assertTrue(p.haslayer(IPFIX))
+        if decoder is not None and p.haslayer(Template):
+            decoder.add_template(p.getlayer(Template))
+        p = self._test.wait_for_cflow_packet(self._test.pg0, 2)
+        self._test.assertTrue(p.haslayer(IPFIX))
+        if decoder is not None and p.haslayer(Template):
+            decoder.add_template(p.getlayer(Template))
+
+class TestFlowPerPkt(VppTestCase):
     """ Flow-per-packet plugin: test both L2 and IP4 reporting """
+
+    # Test variables
+    active_timer = 5
+    passive_timer = 30
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Perform standard class setup (defined by class method setUpClass in
+        class VppTestCase) before running the test case, set test case related
+        variables and configure VPP.
+        """
+        super(TestFlowPerPkt, cls).setUpClass()
+
+        try:
+            # Create pg interfaces
+            cls.create_pg_interfaces(range(3))
+
+            # Packet sizes
+            cls.pg_if_packet_sizes = [64, 512, 1518, 9018]
+
+            # Create BD with MAC learning and unknown unicast flooding disabled
+            # and put interfaces to this BD
+            cls.vapi.bridge_domain_add_del(bd_id=1, uu_flood=1, learn=1)
+            cls.vapi.sw_interface_set_l2_bridge(cls.pg1._sw_if_index, bd_id=1)
+            cls.vapi.sw_interface_set_l2_bridge(cls.pg2._sw_if_index, bd_id=1)
+
+            # Set up all interfaces
+            for i in cls.pg_interfaces:
+                i.admin_up()
+                i.config_ip4()
+                if i!=cls.pg0:
+                    i.resolve_arp()
+            cls.pg0.configure_ipv4_neighbors()
+
+        except Exception:
+            super(TestFlowPerPkt, cls).tearDownClass()
+            raise
 
     def setUp(self):
         """
-        Set up
-
-        **Config:**
-            - create three PG interfaces
         """
-        super(TestFlowperpkt, self).setUp()
+        super(TestFlowPerPkt, self).setUp()
 
-        self.create_pg_interfaces(range(3))
+    def tearDown(self):
+         """
+         """
 
-        self.pg_if_packet_sizes = [150]
-
-        self.interfaces = list(self.pg_interfaces)
-
-        for intf in self.interfaces:
-            intf.admin_up()
-            intf.config_ip4()
-            intf.resolve_arp()
-
-    def create_stream(self, src_if, dst_if, packet_sizes):
+    def create_stream(self, src_if, dst_if):
         """Create a packet stream to tickle the plugin
 
         :param VppInterface src_if: Source interface for packet stream
         :param VppInterface src_if: Dst interface for packet stream
-        :param list packet_sizes: Sizes to test
         """
         pkts = []
-        for size in packet_sizes:
+        for size in self.pg_if_packet_sizes:
             info = self.create_packet_info(src_if, dst_if)
             payload = self.info_to_payload(info)
             p = (Ether(src=src_if.remote_mac, dst=src_if.local_mac) /
@@ -51,6 +133,32 @@ class TestFlowperpkt(VppTestCase):
             self.extend_packet(p, size)
             pkts.append(p)
         return pkts
+
+    def create_ipfix(self, collector, watched, active=0, passive=0,
+                     collect="l2 l3 l4", datapath="l2", template_timout=20):
+        """Create a IPFIX collector and setup flowperpkt feature
+
+        :param VppInterface collector: interfcae used as colector
+        :param VppInterface watched: which interface is monitored
+        :param int active: flowperpkt active timer
+        :param int passive: flowperpkt passive timer
+        :param str collect: flowperpkt - what to collect
+        :param str datapath: data path
+        """
+        self.vapi.set_ipfix_exporter(collector_address=collector.remote_ip4n,
+                                     src_address=collector.local_ip4n,
+                                     path_mtu=512,
+                                     template_interval=template_timout)
+        self.vapi.cli("flowperpkt params record %s active %d passive %d" % (
+            collect, active, passive))
+        self.vapi.cli("flowperpkt feature add-del %s %s" % (watched, datapath))
+        # poke the flow reporting process
+        self.vapi.cli("ipfix flush")
+
+        # template packet should arrive immediately
+        self.wait_for_cflow_packet(self.pg0)
+        self.wait_for_cflow_packet(self.pg0)
+        self.wait_for_cflow_packet(self.pg0)
 
     @staticmethod
     def compare_with_mask(payload, masked_expected_data):
@@ -98,6 +206,8 @@ class TestFlowperpkt(VppTestCase):
                          % (collector_if.name, collector_if.sw_if_index))
         # expecting 4 packets on collector interface based on traffic on other
         # interfaces
+
+        self.wait_for_cflow_packet(15)
         capture = collector_if.get_capture(4)
 
         for p in capture:
@@ -131,43 +241,282 @@ class TestFlowperpkt(VppTestCase):
         self.assertTrue(found_l2_template_packet,
                         "L2 template packet not found")
 
-    def test_L3_fpp(self):
-        """ Flow per packet L3 test """
+    def verify_cflow_data(self, decoder, capture, cflow):
+        octets = 0
+        packets = 0
+        for p in capture:
+            octets += p[IP].len
+            packets += 1
+        #print octets
+        if cflow.haslayer(Data):
+            #print cflow[Data].data.encode('hex')
+            data = decoder.decode_data_set(cflow.getlayer(Set))
+            #print data
+            for record in data:
+                #print record[1]
+                #print record[1].encode('hex')
+                #print int(record[1].encode('hex'), 16)
+                #print struct.pack("i", 11040)
+                #print "%16.0x" % 11040
+                #print "{0:0{1}x}".format(11040, 16)
+                #self.assertEqual(record[1], struct.pack("!H", 11040))
+                #, struct.unpack("HH", record[1])
+                self.assertEqual(int(record[1].encode('hex'), 16), octets)
+                self.assertEqual(int(record[2].encode('hex'), 16), packets)
 
-        # Configure an ipfix report on the [nonexistent] collector
-        # 172.16.3.2, as if it was connected to the pg2 interface
-        # Install a FIB entry, so the exporter's work won't turn into
-        # an ARP request
+            #IP(src=src_if.remote_ip4, dst=dst_if.remote_ip4)
 
+    def wait_for_cflow_packet(self, collector_intf, set_id=2, timeout=1,
+                             expected=True):
+        """ wait for CFLOW packet and verify its correctness
+
+        :param timeout: how long to wait
+
+        :returns: tuple (packet, time spent waiting for packet)
+        """
+        self.logger.info("IPFIX: Waiting for CFLOW packet")
+        deadline = time.time() + timeout
+        counter = 0
+        while True:
+            counter += 1
+            # sanity check
+            self.assert_in_range(counter, 0, 100, "number of packets ignored")
+            time_left = deadline - time.time()
+            try:
+                if time_left < 0 and expected:
+                    raise CaptureTimeoutError(
+                          "Packet did not arrive within timeout")
+                p = collector_intf.wait_for_packet(timeout=time_left)
+            except CaptureTimeoutError:
+                if expected:
+                    raise CaptureTimeoutError(
+                          "Packet did not arrive within timeout")
+                else:
+                    return
+            self.assertEqual(p[Set].setID, set_id)
+            self.logger.debug(ppp("IPFIX: Got packet:", p))
+            break
+        return p
+
+    @unittest.skip('')
+    def test_0000_verify_templates(self):
+        """ FFP receive template data packets
+        """
+
+        self.logger.info("FFP_TEST_START_0001")
         self.pg_enable_capture(self.pg_interfaces)
-        self.pg2.configure_ipv4_neighbors()
-        self.vapi.set_ipfix_exporter(collector_address=self.pg2.remote_ip4n,
-                                     src_address=self.pg2.local_ip4n,
-                                     path_mtu=1450,
-                                     template_interval=1)
 
-        # Export flow records for all pkts transmitted on pg1
-        self.vapi.cli("flowperpkt feature add-del pg1")
-        self.vapi.cli("flowperpkt feature add-del pg1 l2")
+        ipfix = VppIPFIX(test=self, intf='pg2', timeout=1)
+        ipfix.add_vpp_config()
 
-        # Arrange to minimally trace generated ipfix packets
-        self.vapi.cli("trace add flowperpkt-ipv4 10")
-        self.vapi.cli("trace add flowperpkt-l2 10")
+        # template packet should arrive immediately
+        ipfix.verify_templates()
 
-        # Create a stream from pg0 -> pg1, which causes
-        # an ipfix packet to be transmitted on pg2
+        self.logger.info("FFP_TEST_FINISH_0001")
 
-        pkts = self.create_stream(self.pg0, self.pg1,
-                                  self.pg_if_packet_sizes)
-        self.pg0.add_stream(pkts)
+    #@unittest.skip('')
+    def test_0001(self):
+        """ FFP no timer
+        """
+        self.logger.info("FFP_TEST_START_0001")
+        self.pg_enable_capture(self.pg_interfaces)
+
+        ipfix = VppIPFIX(test=self, intf='pg2', timeout=20, active=0)
+        ipfix.add_vpp_config()
+
+        ipfix_decoder = IPFIXDecoder()
+        # template packet should arrive immediately
+        ipfix.verify_templates(ipfix_decoder)
+
+        pkts = self.create_stream(self.pg1, self.pg2)
+        self.pg1.add_stream(pkts)
+
         self.pg_start()
+        self.pg2.get_capture()
 
-        # Flush the ipfix collector, so we don't need any
-        # asinine time.sleep(5) action
+        # make sure the one packet we expect actually showed up
+        self.wait_for_cflow_packet(self.pg0, 256, 6)
+
+        self.logger.info("FFP_TEST_FINISH_0001")
+
+    @unittest.skip('')
+    def test_0002(self):
+        """ FFP timer=10s, less than template timer
+        """
+        self.logger.info("FFP_TEST_START_0002")
+        self.pg_enable_capture(self.pg_interfaces)
+
+        ipfix = VppIPFIX(test=self, intf='pg2', timeout=20, active=10)
+        ipfix.add_vpp_config()
+
+        ipfix_decoder = IPFIXDecoder()
+        # template packet should arrive immediately
+        ipfix.verify_templates(ipfix_decoder)
+
+        pkts = self.create_stream(self.pg1, self.pg2)
+        self.pg_enable_capture(self.pg_interfaces)
+        self.pg1.add_stream(pkts)
+
+        self.pg_start()
+        capture = self.pg2.get_capture()
+
+        # make sure the one packet we expect actually showed up
+        cflow = self.wait_for_cflow_packet(self.pg0, 256, 15)
+        self.verify_cflow_data(ipfix_decoder, capture, cflow)
+
+        self.logger.info("FFP_TEST_FINISH_0002")
+
+    @unittest.skip('')
+    def test_0003(self):
+        """ FFP timer=30s, greater than template timer
+        """
+        self.logger.info("FFP_TEST_START_0003")
+        self.pg_enable_capture(self.pg_interfaces)
+
+        ipfix = VppIPFIX(test=self, intf='pg2', timeout=20, active=30)
+        ipfix.add_vpp_config()
+
+        # template packet should arrive immediately
+        ipfix.verify_templates()
+
+        pkts = self.create_stream(self.pg1, self.pg2)
+        self.pg1.add_stream(pkts)
+
+        self.pg_start()
+        capture = self.pg2.get_capture()
+
         self.vapi.cli("ipfix flush")
+        # next set of template packet should arrive after 20 seconds
+        ipfix_decoder = IPFIXDecoder()
+        # template packet should arrive in 20 s
+        ipfix.verify_templates(ipfix_decoder, timeout=20)
 
-        # Make sure the 4 pkts we expect actually showed up
-        self.verify_ipfix(self.pg2)
+        # make sure the one packet we expect actually showed up
+        cflow = self.wait_for_cflow_packet(self.pg0, 256, 15)
+        self.verify_cflow_data(ipfix_decoder, capture, cflow)
+
+        self.logger.info("FFP_TEST_FINISH_0003")
+
+    @unittest.skip('')
+    def test_0004(self):
+        """ FFP sent packet after first cflow packet arrived
+        """
+        self.logger.info("FFP_TEST_START_0004")
+        self.pg_enable_capture(self.pg_interfaces)
+
+        ipfix = VppIPFIX(test=self, intf='pg2', timeout=120, active=10)
+        ipfix.add_vpp_config()
+
+        ipfix_decoder = IPFIXDecoder()
+        # template packet should arrive immediately
+        ipfix.verify_templates(ipfix_decoder)
+
+        pkts = self.create_stream(self.pg1, self.pg2)
+        self.pg1.add_stream(pkts)
+        self.pg_start()
+        self.pg2.get_capture()
+
+        # make sure the one packet we expect actually showed up
+        self.wait_for_cflow_packet(self.pg0, 256, 15)
+
+        self.pg_enable_capture([self.pg2])
+
+        self.pg1.add_stream(pkts)
+        self.pg_start()
+        self.pg2.get_capture()
+        # make sure the one packet we expect actually showed up
+        self.wait_for_cflow_packet(self.pg0, 256, 30)
+
+        self.logger.info("FFP_TEST_FINISH_0004")
+
+    @unittest.skip('')
+    def test_0005(self):
+        """ FFP disable IPFIX after first packets
+        """
+        self.logger.info("FFP_TEST_START_0005")
+        self.pg_enable_capture(self.pg_interfaces)
+
+        ipfix = VppIPFIX(test=self, intf='pg2', timeout=120, active=10)
+        ipfix.add_vpp_config()
+
+        ipfix_decoder = IPFIXDecoder()
+        # template packet should arrive immediately
+        ipfix.verify_templates(ipfix_decoder)
+
+        pkts = self.create_stream(self.pg1, self.pg2)
+        self.pg1.add_stream(pkts)
+        self.pg_start()
+        self.pg2.get_capture()
+
+        # make sure the one packet we expect actually showed up
+        self.wait_for_cflow_packet(self.pg0, 256, 15)
+
+        # disble IPFIX
+        self.vapi.cli("set ipfix exporter collector 0.0.0.0")
+
+        self.pg_enable_capture([self.pg2])
+
+        self.pg1.add_stream(pkts)
+        self.pg_start()
+        self.pg2.get_capture()
+        # make sure no one packet arrived
+        self.wait_for_cflow_packet(self.pg0, 256, 60, expected=False)
+        self.pg0.get_capture(len(pkts))
+
+        self.logger.info("FFP_TEST_FINISH_0005")
+
+    @unittest.skip('')
+    def test_0006(self):
+        """ FFP disable IPFIX after first packet and re enable after few
+        packets
+        """
+        self.logger.info("FFP_TEST_START_0006")
+        self.pg_enable_capture(self.pg_interfaces)
+
+        ipfix = VppIPFIX(test=self, intf='pg2', timeout=120, active=10)
+        ipfix.add_vpp_config()
+
+        ipfix_decoder = IPFIXDecoder()
+        # template packet should arrive immediately
+        ipfix.verify_templates(ipfix_decoder)
+
+        pkts = self.create_stream(self.pg1, self.pg2)
+        self.pg1.add_stream(pkts)
+        self.pg_start()
+        self.pg2.get_capture()
+
+        # make sure the one packet we expect actually showed up
+        self.wait_for_cflow_packet(self.pg0, 256, 15)
+
+        # disble IPFIX
+        self.vapi.cli("set ipfix exporter collector 0.0.0.0")
+        #self.vapi.set_ipfix_exporter(collector_address="0.0.0.0")
+
+        self.pg_enable_capture([self.pg2])
+
+        self.pg1.add_stream(pkts)
+        self.pg_start()
+        self.pg2.get_capture()
+        # make sure no one packet arrived in 1 minute
+        self.wait_for_cflow_packet(self.pg0, 256, 60, expected=False)
+        self.pg0.get_capture(len(pkts))
+
+        # enable IPFIX
+        self.vapi.set_ipfix_exporter(
+            collector_address=self.pg0.remote_ip4n,
+            src_address=self.pg0.local_ip4n,
+            path_mtu=512,
+            template_interval=120)
+
+        self.pg_enable_capture([self.pg2])
+        self.pg1.add_stream(pkts)
+        self.pg_start()
+        self.pg2.get_capture()
+
+        # make sure the one packet we expect actually showed up
+        self.wait_for_cflow_packet(self.pg0, 256, 60)
+
+        self.logger.info("FFP_TEST_FINISH_0006")
 
 if __name__ == '__main__':
     unittest.main(testRunner=VppTestRunner)
