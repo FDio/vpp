00b31c019e26a7951424c69ab07a8fe702abe73e
Damjan Marion <damarion@cisco.com>
2017-05-15 18:35:37 +0200

    dpdk: Enable hardware CRC strip for Intel VFs
    Change-Id: I60a7795761fe74ae5d57dacd03c343ddb77434d4
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- src/plugins/dpdk/device/init.c
+++ src/plugins/dpdk/device/init.c
@@ -365,6 +365,10 @@ dpdk_lib_init (dpdk_main_t * dm)
 	    case VNET_DPDK_PMD_IGBVF:
 	    case VNET_DPDK_PMD_IXGBEVF:
 	    case VNET_DPDK_PMD_I40EVF:
+	      xd->port_type = VNET_DPDK_PORT_TYPE_ETH_VF;
+	      xd->port_conf.rxmode.hw_strip_crc = 1;
+	      break;
+
 	    case VNET_DPDK_PMD_THUNDERX:
 	      xd->port_type = VNET_DPDK_PORT_TYPE_ETH_VF;
 	      break;
