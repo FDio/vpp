f766729eccae44f0057ce5a01e5ca9c1a8995be0
Christophe Fontaine <christophe.fontaine@enea.com>
2017-05-31 11:27:19 +0200

    Remove calls to crc_u32 and add clib_crc32c for armv8+crc
    crc_u32 was not defined for non x86_64 with SSE4.2 processors.
    
    Calls to "crc_u32" are removed and replaced by either a call to
    clib_crc32c or a call to clib_xxhash, as the result is not used
    as a check value but as a hash.
    
    Change-Id: I3af4d68e2e5ebd0c9b0a6090f848d043cb0f20a2
    Signed-off-by: Christophe Fontaine <christophe.fontaine@enea.com>

--- src/vppinfra/crc32.h
+++ src/vppinfra/crc32.h
@@ -17,6 +17,7 @@
 #define __included_crc32_h__
 
 #if __SSE4_2__
+#define clib_crc32c_uses_intrinsics
 #include <x86intrin.h>
 
 static_always_inline u32
@@ -45,8 +46,31 @@ clib_crc32c (u8 * s, int len)
   return v;
 }
 
-#endif
+#elif __ARM_FEATURE_CRC32
+#define clib_crc32c_with_intrinsics
+#include <arm_acle.h>
 
+static_always_inline u32
+clib_crc32c (u8 * s, int len)
+{
+  u32 v = 0;
+
+  for (; len >= 8; len -= 8, s += 8)
+    v = __crc32cd (v, *((u64 *) s));
+
+  for (; len >= 4; len -= 4, s += 4)
+    v = __crc32cw (v, *((u32 *) s));
+
+  for (; len >= 2; len -= 2, s += 2)
+    v = __crc32ch (v, *((u16 *) s));
+
+  for (; len >= 1; len -= 1, s += 1)
+    v = __crc32cb (v, *((u8 *) s));
+
+  return v;
+}
+
+#endif
 #endif /* __included_crc32_h__ */
 
 /*
