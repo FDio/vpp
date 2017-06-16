23b3b974acc739746889ab0975b2f10af33a918a
Damjan Marion <damarion@cisco.com>
2017-06-16 22:06:00 +0200

    memif: add ip mode
    In IP mode memif interface is L3 point-to-point interfaces and
    we don't pass l2 header. There is no l2 header rewrite operation and
    received packets are sent straight to ip4-input / ip6-input nodes.
    
    Change-Id: I4177f3fce3004da7ecf14d235006ae053fcf3f09
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- src/plugins/memif/node.c
+++ src/plugins/memif/node.c
@@ -30,7 +30,8 @@
 #include <memif/memif.h>
 #include <memif/private.h>
 
-#define foreach_memif_input_error
+#define foreach_memif_input_error \
+  _(NOT_IP, "not ip packet")
 
 typedef enum
 {
@@ -200,16 +201,33 @@ memif_copy_buffer_from_rx_ring (vlib_main_t * vm, memif_if_t * mif,
   return (total_bytes);
 }
 
+
+static_always_inline u32
+memif_next_from_ip_hdr (vlib_node_runtime_t * node, vlib_buffer_t * b)
+{
+  u8 *ptr = vlib_buffer_get_current (b);
+  u8 v = *ptr & 0xf0;
+
+  if (PREDICT_TRUE (v == 0x40))
+    return VNET_DEVICE_INPUT_NEXT_IP4_NCS_INPUT;
+  else if (PREDICT_TRUE (v == 0x60))
+    return VNET_DEVICE_INPUT_NEXT_IP6_INPUT;
+
+  b->error = node->errors[MEMIF_INPUT_ERROR_NOT_IP];
+  return VNET_DEVICE_INPUT_NEXT_DROP;
+}
+
 static_always_inline uword
 memif_device_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 			   vlib_frame_t * frame, memif_if_t * mif,
-			   memif_ring_type_t type, u16 qid)
+			   memif_ring_type_t type, u16 qid,
+			   memif_interface_mode_t mode)
 {
   vnet_main_t *vnm = vnet_get_main ();
   memif_ring_t *ring;
   memif_queue_t *mq;
   u16 head;
-  u32 next_index = VNET_DEVICE_INPUT_NEXT_ETHERNET_INPUT;
+  u32 next_index;
   uword n_trace = vlib_get_trace_count (vm, node);
   memif_main_t *nm = &memif_main;
   u32 n_rx_packets = 0;
@@ -227,8 +245,14 @@ memif_device_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
   ring_size = 1 << mq->log2_ring_size;
   mask = ring_size - 1;
 
-  if (mif->per_interface_next_index != ~0)
-    next_index = mif->per_interface_next_index;
+  if (mode == MEMIF_INTERFACE_MODE_IP)
+    {
+      next_index = VNET_DEVICE_INPUT_NEXT_IP6_INPUT;
+    }
+  else
+    {
+      next_index = VNET_DEVICE_INPUT_NEXT_ETHERNET_INPUT;
+    }
 
   n_free_bufs = vec_len (nm->rx_buffers[thread_index]);
   if (PREDICT_FALSE (n_free_bufs < ring_size))
@@ -308,6 +332,24 @@ memif_device_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 	  to_next += 2;
 	  n_left_to_next -= 2;
 
+
+	  if (mode == MEMIF_INTERFACE_MODE_IP)
+	    {
+	      next0 = memif_next_from_ip_hdr (node, first_b0);
+	      next1 = memif_next_from_ip_hdr (node, first_b1);
+	    }
+	  else if (mode == MEMIF_INTERFACE_MODE_ETHERNET)
+	    {
+	      if (PREDICT_FALSE (mif->per_interface_next_index != ~0))
+		next0 = next1 = mif->per_interface_next_index;
+	      else
+		/* redirect if feature path
+		 * enabled */
+		vnet_feature_start_device_input_x2 (mif->sw_if_index,
+						    &next0, &next1,
+						    first_b0, first_b1);
+	    }
+
 	  /* trace */
 	  VLIB_BUFFER_TRACE_TRAJECTORY_INIT (first_b0);
 	  VLIB_BUFFER_TRACE_TRAJECTORY_INIT (first_b1);
@@ -343,11 +385,6 @@ memif_device_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 		}
 	    }
 
-	  /* redirect if feature path enabled */
-	  vnet_feature_start_device_input_x2 (mif->sw_if_index,
-					      &next0, &next1, first_b0,
-					      first_b1);
-
 	  /* enqueue */
 	  vlib_validate_buffer_enqueue_x2 (vm, node, next_index, to_next,
 					   n_left_to_next, first_bi0,
@@ -367,6 +404,21 @@ memif_device_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 						     &n_free_bufs, &first_b0,
 						     &first_bi0, &bi0,
 						     &num_slots);
+
+	  if (mode == MEMIF_INTERFACE_MODE_IP)
+	    {
+	      next0 = memif_next_from_ip_hdr (node, first_b0);
+	    }
+	  else if (mode == MEMIF_INTERFACE_MODE_ETHERNET)
+	    {
+	      if (PREDICT_FALSE (mif->per_interface_next_index != ~0))
+		next0 = mif->per_interface_next_index;
+	      else
+		/* redirect if feature path
+		 * enabled */
+		vnet_feature_start_device_input_x1 (mif->sw_if_index,
+						    &next0, first_b0);
+	    }
 
 	  /* trace */
 	  VLIB_BUFFER_TRACE_TRAJECTORY_INIT (first_b0);
@@ -390,10 +442,6 @@ memif_device_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 	  to_next[0] = first_bi0;
 	  to_next += 1;
 	  n_left_to_next--;
-
-	  /* redirect if feature path enabled */
-	  vnet_feature_start_device_input_x1 (mif->sw_if_index, &next0,
-					      first_b0);
 
 	  /* enqueue */
 	  vlib_validate_buffer_enqueue_x1 (vm, node, next_index, to_next,
@@ -433,11 +481,27 @@ memif_input_fn (vlib_main_t * vm, vlib_node_runtime_t * node,
 	(mif->flags & MEMIF_IF_FLAG_CONNECTED))
       {
 	if (mif->flags & MEMIF_IF_FLAG_IS_SLAVE)
-	  n_rx += memif_device_input_inline (vm, node, frame, mif,
-					     MEMIF_RING_M2S, dq->queue_id);
+	  {
+	    if (mif->mode == MEMIF_INTERFACE_MODE_IP)
+	      n_rx += memif_device_input_inline (vm, node, frame, mif,
+						 MEMIF_RING_M2S, dq->queue_id,
+						 MEMIF_INTERFACE_MODE_IP);
+	    else
+	      n_rx += memif_device_input_inline (vm, node, frame, mif,
+						 MEMIF_RING_M2S, dq->queue_id,
+						 MEMIF_INTERFACE_MODE_ETHERNET);
+	  }
 	else
-	  n_rx += memif_device_input_inline (vm, node, frame, mif,
-					     MEMIF_RING_S2M, dq->queue_id);
+	  {
+	    if (mif->mode == MEMIF_INTERFACE_MODE_IP)
+	      n_rx += memif_device_input_inline (vm, node, frame, mif,
+						 MEMIF_RING_S2M, dq->queue_id,
+						 MEMIF_INTERFACE_MODE_IP);
+	    else
+	      n_rx += memif_device_input_inline (vm, node, frame, mif,
+						 MEMIF_RING_S2M, dq->queue_id,
+						 MEMIF_INTERFACE_MODE_ETHERNET);
+	  }
       }
   }
 
