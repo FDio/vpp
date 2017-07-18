95e9209c43cbbf856d44bb48039e7eb0315c242f
Matej Perina <mperina@cisco.com>
2017-07-18 13:59:41 +0200

    jvpp: introducing callback api and future api tests for all plugins (VPP-591)
    test can be run with:
    make test TEST=test_jvpp
    
    memory_shared.c:
    declaring and assigning variable in if statement
    makes it usage outside statement impossible. Looks like memory
    space assigned to variable declared in statement is freed when
    statement ends
    svm.c:
    - fixed case when root path can have a "/" at beggining
    - added option for test to operate over shared memory space
    with /vpe-api name and not create new one with name consisting of
    root path and region name which would require root permisions
    
    Change-Id: Iff1170dc6a5c1be134c152f2757c7ab9b919a8ed
    Signed-off-by: Matej Perina <mperina@cisco.com>

---
+++ src/vpp-api/java/jvpp-core/io/fd/vpp/jvpp/core/test/CallbackApiTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.fd.vpp.jvpp.core.test;
+
+import io.fd.vpp.jvpp.AbstractCallbackApiTest;
+import io.fd.vpp.jvpp.core.JVppCoreImpl;
+
+import java.util.logging.Logger;
+
+public class CallbackApiTest extends AbstractCallbackApiTest {
+
+    private static Logger LOG = Logger.getLogger(CallbackApiTest.class.getName());
+
+
+    public static void main(String[] args) throws Exception {
+        LOG.info("Testing ControlPing using Java callback API for core plugin");
+        testControlPing(args[0], new JVppCoreImpl());
+    }
+}
---
+++ src/vpp-api/java/jvpp-nat/io/fd/vpp/jvpp/nat/test/Readme.txt
@@ -0,0 +1,4 @@
+release version:
+sudo java -cp build-vpp-native/vpp/vpp-api/java/jvpp-registry-17.10.jar:build-vpp_debug-native/vpp/vpp-api/java/jvpp-nat-17.10.jar io.fd.vpp.jvpp.nat.test.[test-name]
+debug version:
+sudo java -cp build-vpp_debug-native/vpp/vpp-api/java/jvpp-registry-17.10.jar:build-vpp_debug-native/vpp/vpp-api/java/jvpp-nat-17.10.jar io.fd.vpp.jvpp.nat.test.[test-name]
