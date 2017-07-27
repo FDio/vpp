2b2361d955d5d6190aa1ea0bf2b0e150b449fc00
Steven <sluong@cisco.com>
2017-07-27 14:54:44 -0700

    devices: Display VhostEthernet instead of UnknownEthernet for dpdk net_vhost driver
    Added a new macro VNET_DPDK_PORT_TYPE_VHOST_ETHER for dpdk net_vhost driver
    to display VhostEthernet instead of UnknownEthernet.
    
    Change-Id: Ie407d83ebbbd60e4d11be79f0ebc44e2867a5f3b
    Signed-off-by: Steven <sluong@cisco.com>

--- src/plugins/dpdk/device/format.c
+++ src/plugins/dpdk/device/format.c
@@ -213,6 +213,10 @@ format_dpdk_device_name (u8 * s, va_list * args)
       device_name = "VirtioUser";
       break;
 
+    case VNET_DPDK_PORT_TYPE_VHOST_ETHER:
+      device_name = "VhostEthernet";
+      break;
+
     default:
     case VNET_DPDK_PORT_TYPE_UNKNOWN:
       device_name = "UnknownEthernet";
@@ -316,6 +320,9 @@ format_dpdk_device_type (u8 * s, va_list * args)
 
     case VNET_DPDK_PMD_THUNDERX:
       dev_type = "Cavium ThunderX";
+
+    case VNET_DPDK_PMD_VHOST_ETHER:
+      dev_type = "VhostEthernet";
       break;
 
     default:
