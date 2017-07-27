cebee96f479b8b5969002452e2cbdc8c97214fc7
Florin Coras <fcoras@cisco.com>
2017-07-27 00:34:12 -0700

    Make ip csum configurable in vlib buffer functions
    Also fixes csum computation for lisp control plane 4o6 encapsulated control
    messages.
    
    Change-Id: I991e0b5c0d16dc51e0b5bdc79e1d752270b34765
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/vnet/lisp-cp/packets.h
+++ src/vnet/lisp-cp/packets.h
@@ -19,10 +19,11 @@
 #define IP_DF 0x4000		/* don't fragment */
 
 void *pkt_push_ip (vlib_main_t * vm, vlib_buffer_t * b, ip_address_t * src,
-		   ip_address_t * dst, u32 proto);
+		   ip_address_t * dst, u32 proto, u8 csum_offload);
 
 void *pkt_push_udp_and_ip (vlib_main_t * vm, vlib_buffer_t * b, u16 sp,
-			   u16 dp, ip_address_t * sip, ip_address_t * dip);
+			   u16 dp, ip_address_t * sip, ip_address_t * dip,
+			   u8 cksum_offload);
 
 void *pkt_push_ecm_hdr (vlib_buffer_t * b);
 
