86ee27e853edcf3ec19b45859b1d9db1d1501a1a
Florin Coras <fcoras@cisco.com>
2017-08-14 22:33:41 -0700

    tcp: improve builtin http server
    Additionally:
    - remove opaques from stream_session_t
    - ensure first segment manager is only used once per app.
    
    Change-Id: I143d1fdb8effc88815ef969b78122ba3ac29e06e
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/vnet/session/application.h
+++ src/vnet/session/application.h
@@ -86,6 +86,7 @@ typedef struct _application
   uword *listeners_table;
 
   u32 first_segment_manager;
+  u8 first_segment_manager_in_use;
 
   /** Segment manager properties. Shared by all segment managers */
   segment_manager_properties_t sm_properties;
