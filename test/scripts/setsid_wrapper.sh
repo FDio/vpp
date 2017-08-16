8712ada6c3a84b2f75a25457ec6d477f3683787c
Klement Sekera <ksekera@cisco.com>
2017-08-16 16:38:10 +0200

    make test: fix broken passing of return value
    Change-Id: I2cb83caaf55ca9a29c06d71c6d20f8273ec062b3
    Signed-off-by: Klement Sekera <ksekera@cisco.com>

--- test/scripts/setsid_wrapper.sh
+++ test/scripts/setsid_wrapper.sh
@@ -3,10 +3,10 @@
 if [[ "$1" == "1" ]]
 then
 	setsid scripts/run_in_venv_with_cleanup.sh $*
-	pid=$!
 else
 	setsid scripts/run_in_venv_with_cleanup.sh $* &
 	pid=$!
 	trap "echo setsid_wrapper.sh: got signal, killing child pid ${pid}; kill ${pid}; sleep .1;" SIGINT SIGTERM
-	wait
+	wait ${pid}
+	exit $?
 fi
