89a08ebecc09b9e7124f12b3275b7fa444d23676
Dave Wallace <dwallacelf@gmail.com>
2017-08-17 12:40:27 -0400

    TEST PATCH: Break ping test to verify fail archive.
    Change-Id: I26ad213e35d6fbcf8ff8acf7bc0c7631edc4985b
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- test/scripts/compress_failed.sh
+++ test/scripts/compress_failed.sh
@@ -2,20 +2,36 @@
 
 if [ "$(ls -A ${VPP_TEST_FAILED_DIR})" ]
 then
-	if [ "${COMPRESS_FAILED_TEST_LOGS}" == "yes" ]
-	then
-		echo -n "Compressing files in temporary directories from failed test runs..."
-		cd ${VPP_TEST_FAILED_DIR}
-		for d in *
-		do
-			cd ${d}
-			find . ! -path . -print0 | xargs -0 -n1 gzip
-			cd ${VPP_TEST_FAILED_DIR}
-		done
-		echo "done."
-	else
-		echo "Not compressing files in temporary directories from failed test runs."
-	fi
+    if [ "${COMPRESS_FAILED_TEST_LOGS}" == "yes" ]
+    then
+	echo -n "Compressing files in temporary directories from failed test runs... "
+	cd ${VPP_TEST_FAILED_DIR}
+	for d in *
+	do
+	    cd ${d}
+	    find . ! -path . -print0 | xargs -0 -n1 gzip
+	    cd ${VPP_TEST_FAILED_DIR}
+	done
+	echo "done."
+        if [ -n "$WORKSPACE" ]
+        then
+            echo "Copying failed test logs into build log archive directory ($WORKSPACE/archives)... "
+            for failed_test in $(ls $VPP_TEST_FAILED_DIR)
+            do
+                mkdir -p $WORKSPACE/archives/$failed_test
+                cp -a $VPP_TEST_FAILED_DIR/$failed_test/* $WORKSPACE/archives/$failed_test
+            done
+	    echo "done."
+        fi
+        
+    else
+	echo "Not compressing files in temporary directories from failed test runs."
+    fi
 else
-	echo "No symlinks to failed tests' temporary directories found in ${VPP_TEST_FAILED_DIR}."
+    echo "No symlinks to failed tests' temporary directories found in ${VPP_TEST_FAILED_DIR}."
 fi
+
+# This script gets run only if there was a 'make test' failure,
+# so return failure error status so that the build results are
+# recorded correctly.
+exit 1
