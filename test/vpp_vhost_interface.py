3eeb0035bc0094929e70887e786f1946d3d0d7b1
Steven <sluong@cisco.com>
2017-08-30 14:36:45 -0700

    vhost: add vhost interface add/delete/dump API test cases to make test
    The vhost binary APIs for add/delete/dump interface were available long ago.
    But no unit tests were ever added in make test. This patch is to retrofit
    the missing unit tests.
    
    Change-Id: I489521b5ae359a1168ac5880a1f13a5f7e93ce4a
    Signed-off-by: Steven <sluong@cisco.com>

---
+++ test/vpp_vhost_interface.py
@@ -0,0 +1,42 @@
+from vpp_object import VppObject
+from vpp_interface import VppInterface
+
+
+class VppVhostInterface(VppInterface):
+    """VPP vhost interface."""
+
+    def __init__(self, test, sock_filename, is_server=0, renumber=0,
+                 custom_dev_instance=0, use_custom_mac=0, mac_address='',
+                 tag=''):
+
+        """ Create VPP Vhost interface """
+        self._test = test
+        super(VppVhostInterface, self)
+        self.is_server = is_server
+        self.sock_filename = sock_filename
+        self.renumber = renumber
+        self.custom_dev_instance = custom_dev_instance
+        self.use_custom_mac = use_custom_mac
+        self.mac_address = mac_address
+        self.tag = tag
+        self._sw_if_index = 0
+
+    def add_vpp_config(self):
+        r = self.test.vapi.create_vhost_user_if(self.is_server,
+                                                self.sock_filename,
+                                                self.renumber,
+                                                self.custom_dev_instance,
+                                                self.use_custom_mac,
+                                                self.mac_address,
+                                                self.tag)
+        self._sw_if_index = r.sw_if_index
+
+    def remove_vpp_config(self):
+        self.test.vapi.delete_vhost_user_if(self.sw_if_index)
+
+    def is_interface_config_in_dump(self, dump):
+        for i in dump:
+            if i.sw_if_index == self.sw_if_index:
+                return True
+        else:
+            return False
