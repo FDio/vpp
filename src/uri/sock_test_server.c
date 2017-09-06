ea39253fe215ba8ca57a79a280b98ab0def9f369
Dave Wallace <dwallacelf@gmail.com>
2017-09-06 01:20:02 -0400

    Fix session connect_* api message handling.
    Change-Id: If7fd125989c90240de12953658d10007b9eb4f07
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- src/uri/sock_test_server.c
+++ src/uri/sock_test_server.c
@@ -345,7 +345,7 @@ main (int argc, char **argv)
   memset (&servaddr, 0, sizeof (servaddr));
 
   servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = htons (INADDR_ANY);
+  servaddr.sin_addr.s_addr = htonl (INADDR_ANY);
   servaddr.sin_port = htons (port);
 
 #ifdef VCL_TEST
