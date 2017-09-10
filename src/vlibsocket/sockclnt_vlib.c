f25f4b37992ad466a9c31ace58381c2c1cb3e899
Dave Barach <dave@barachs.net>
2017-09-10 15:04:27 -0400

    Repair vlib API socket server
    - Teach vpp_api_test to send/receive API messages over sockets
    - Add memfd-based shared memory
    - Add api messages to create memfd-based shared memory segments
    - vpp_api_test supports both socket and shared memory segment connections
    - vpp_api_test pivot from socket to shared memory API messaging
    - add socket client support to libvlibclient.so
    - dead client reaper sends ping messages, container-friendly
    - dead client reaper falls back to kill (<pid>, 0) live checking
      if e.g. a python app goes silent for tens of seconds
    - handle ping messages in python client support code
    - teach show api ring about pairwise shared-memory segments
    
    We'll need this work to implement proper host-stack client isolation
    
    Change-Id: Ic23b65f75c854d0393d9a2e9d6b122a9551be769
    Signed-off-by: Dave Barach <dave@barachs.net>
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- src/vlibsocket/sockclnt_vlib.c
+++ src/vlibsocket/sockclnt_vlib.c
@@ -29,23 +29,23 @@
 #include <sys/stat.h>
 
 #include <vlibmemory/api.h>
-#include <vlibsocket/api.h>
 
-#include <vlibsocket/vl_socket_msg_enum.h>
+
+#include <vlibmemory/vl_memory_msg_enum.h>
 
 #define vl_typedefs		/* define message structures */
-#include <vlibsocket/vl_socket_api_h.h>
+#include <vlibmemory/vl_memory_api_h.h>
 #undef vl_typedefs
 
 /* instantiate all the print functions we know about */
 #define vl_print(handle, ...) vlib_cli_output (handle, __VA_ARGS__)
 #define vl_printfun
-#include <vlibsocket/vl_socket_api_h.h>
+#include <vlibmemory/vl_memory_api_h.h>
 #undef vl_printfun
 
 /* instantiate all the endian swap functions we know about */
 #define vl_endianfun
-#include <vlibsocket/vl_socket_api_h.h>
+#include <vlibmemory/vl_memory_api_h.h>
 #undef vl_endianfun
 
 static void
