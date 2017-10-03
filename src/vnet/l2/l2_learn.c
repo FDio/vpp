c5c77ae50a2483fc3d7dbdb495d55f8a4f0aba5a
John Lo <loj@cisco.com>
2017-10-03 17:39:00 -0400

    Update L2FIB entry timestamp only if BD aging enabled (VPP-1002)
    Change L2 learning path so it update stale timestamp in MAC entry
    only if aging is enabled on the BD for the MAC entry.
    
    Change-Id: I849154fe7ad2c6c68d6a94a66ca9345f6a98bc07
    Signed-off-by: John Lo <loj@cisco.com>

--- src/vnet/l2/l2_learn.c
+++ src/vnet/l2/l2_learn.c
@@ -136,7 +136,7 @@ l2learn_process (vlib_node_runtime_t * node,
       /* Entry in L2FIB with matching sw_if_index matched - normal fast path */
       u32 dtime = timestamp - result0->fields.timestamp;
       u32 dsn = result0->fields.sn.as_u16 - vnet_buffer (b0)->l2.l2fib_sn;
-      u32 check = dtime | dsn;
+      u32 check = (dtime && vnet_buffer (b0)->l2.bd_age) || dsn;
 
       if (PREDICT_TRUE (check == 0))
 	return;
