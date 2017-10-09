9af66726ebe2c46d364d7b3d3b4e65349d7429f9
Keith Burns (alagalah) <alagalah@gmail.com>
2017-10-09 08:52:59 -0700

    Initial push of vcl-ldpreload to extras
    Change-Id: I4317757258ed6a65b8fae1377f17db39375282ac
    Signed-off-by: Keith Burns (alagalah) <alagalah@gmail.com>

---
+++ extras/vcl-ldpreload/env.sh
@@ -0,0 +1,5 @@
+export VCL_BASE=$PWD/../../..
+export VPP_DIR=$VCL_BASE/vpp
+export LDP_DIR=$VCL_BASE/vpp/extras
+export WS_ROOT=$VCL_BASE/vpp
+export VCL_LDPRELOAD_LIB_DIR=$LDP_DIR/vcl-ldpreload/src/.libs
