a7102eae10894c7a2a6780116fe5363f53eee45c
Dave Barach <dave@barachs.net>
2017-10-10 17:53:14 -0400

    DNS name resolver
    Change-Id: I48c373d5c05d7108ccd814d4055caf8c75ca10b7
    Signed-off-by: Dave Barach <dave@barachs.net>

--- src/vnet/vnet_all_api_h.h
+++ src/vnet/vnet_all_api_h.h
@@ -60,6 +60,7 @@
 #include <vnet/policer/policer.api.h>
 #include <vnet/ethernet/p2p_ethernet.api.h>
 #include <vnet/tcp/tcp.api.h>
+#include <vnet/dns/dns.api.h>
 
 /*
  * fd.io coding-style-patch-verification: ON
