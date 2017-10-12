4cf8ce438d93aae46184572ec76c058f6c29521c
Ole Troan <ot@cisco.com>
2017-10-12 13:06:35 +0200

    VPPAPIGEN: vppapigen replacement in Python PLY. [EXPERIMENTAL]
    This is a version of the VPP API generator in Python PLY. It supports
    the existing language, and has a plugin architecture for generators.
    Currently C and JSON are supported.
    
    Changes:
     - vl_api_version to option version = "major.minor.patch"
     - enum support
     - Added error checking and reporting
     - import support (removed the C pre-processor)
    
    Planned features:
     - services (tying request/reply together)
     - unions
     - bool, text
     - array support (including length)
     - proto3 output plugin
    
    Change-Id: Ifa289966c790e1b1a8e2938a91e69331e3a58bdf
    Signed-off-by: Ole Troan <ot@cisco.com>

---
+++ src/tools/vppapigen/JSON.py
@@ -0,0 +1,43 @@
+#### JSON generation ####
+import json
+
+def walk_enums(s):
+    r = []
+    for e in s:
+        d = []
+        d.append(e.name)
+        for b in e.block:
+            d.append(b)
+        r.append(d)
+    return r
+
+def walk_defs(s):
+    r = []
+    for t in s:
+        d = []
+        d.append(t.name)
+        for b in t.block:
+            if len(b) == 3:
+                if type(b[2]) is str:
+                        b.insert(2, 0)
+            d.append(b)
+        if t.crc:
+            c = {}
+            c['crc'] = "{0:#0{1}x}".format(t.crc,10)
+            d.append(c)
+
+        r.append(d)
+    return r
+
+#
+# Plugin entry point
+#
+def run(filename, s, file_crc):
+    j = {}
+
+    j['types'] = walk_defs(s['typedefs'])
+    j['messages'] = walk_defs(s['defines'])
+    j['enums'] = walk_enums(s['enums'])
+    j['vl_api_version'] = hex(file_crc)
+        #return json.dumps(j, indent=4, separators=(',', ': '))
+    return json.dumps(j, indent=4, separators=(',', ': '))
