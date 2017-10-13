51d0fc1d8ac7183acaa4918d9097c252497861b2
Damjan Marion <damarion@cisco.com>
2017-10-13 18:29:53 +0200

    virtio: fast TAP interfaces with vhost-net backend
    Changes for this version:
    Introduce tapv2 API and undo all changes to existing API. This allows
    applications to migrate to tapv2 API first and then we can deprecate the
    old tap API later.
    
    Changes for this version:
    rebase to pickup 9467 and remove the changes in pppoe
    
    Changes for this version:
    Introduce tap delete and show tap cli
    Add binary API for tap create, tap delete, and dump tap sw interfaces
    EOL for tapcli.c and tuntap.c
    
    Change-Id: Ided667356d5c6fb9648eb34685aabd6b16a598b7
    Signed-off-by: Damjan Marion <damarion@cisco.com>

---
+++ src/vnet/devices/virtio/tap.h
@@ -0,0 +1,56 @@
+/*
+ *------------------------------------------------------------------
+ * Copyright (c) 2017 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *------------------------------------------------------------------
+ */
+
+#ifndef _VNET_DEVICES_VIRTIO_TAP_H_
+#define _VNET_DEVICES_VIRTIO_TAP_H_
+
+#ifndef MIN
+#define MIN(x,y) (((x)<(y))?(x):(y))
+#endif
+
+typedef struct
+{
+  u8 *name;
+  u8 *net_ns;
+  u8 hw_addr_set;
+  u8 hw_addr[6];
+  u16 rx_ring_sz;
+  u16 tx_ring_sz;
+  /* return */
+  u32 sw_if_index;
+} tap_create_if_args_t;
+
+/** TAP interface details struct */
+typedef struct
+{
+  u32 sw_if_index;
+  u8 dev_name[64];
+} tap_interface_details_t;
+
+int tap_create_if (vlib_main_t * vm, tap_create_if_args_t * args);
+int tap_delete_if (vlib_main_t * vm, u32 sw_if_index);
+int tap_dump_ifs (tap_interface_details_t ** out_tapids);
+
+#endif /* _VNET_DEVICES_VIRTIO_TAP_H_ */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
