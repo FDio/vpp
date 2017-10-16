2e7e32240e8ec1a32f3552d38f9a62f86b0ad09e
Neale Ranns <neale.ranns@cisco.com>
2017-10-16 04:20:13 -0700

    VPP Object Model (VOM)
    The VOM is a C++ library for use by clients/agents of VPP for programming
    state. It uses the binary APIs to do so. Various other common client side
    functions are also provided. Please see om.hpp for a more detailed description.
    
    Change-Id: Ib756bfe99817093815a9e26ccf464aa5583fc523
    Signed-off-by: Neale Ranns <neale.ranns@cisco.com>

---
+++ src/vpp-api/vom/logger.hpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2017 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __VOM_LOGGER_H__
+#define __VOM_LOGGER_H__
+
+#include <boost/log/sources/logger.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/trivial.hpp>
+
+using levels = boost::log::trivial::severity_level;
+
+namespace VOM
+{
+    /**
+     * We use the boost logging library.
+     * the expectation is the client configures the sinks as it desires
+     */
+    typedef boost::log::sources::severity_logger_mt<
+        boost::log::trivial::severity_level>
+        log_t;
+
+    /**
+     * Return a log object into which VPP objects can write
+     */
+    log_t &logger();
+};
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
+
+#endif
