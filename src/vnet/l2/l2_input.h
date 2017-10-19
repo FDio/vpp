b14f7608cbbef64c0dc84abc19a9f3aaa342912e
John Lo <loj@cisco.com>
2017-10-19 14:16:07 -0400

    Cleanup l2-input.h to test gerrit push via https
    Change-Id: I8b798eb1db0c12a98fd718eb74bf59e6e98713fb
    Signed-off-by: John Lo <loj@cisco.com>

--- src/vnet/l2/l2_input.h
+++ src/vnet/l2/l2_input.h
@@ -113,10 +113,9 @@ l2input_bd_config (u32 bd_index)
  _(RW,            "l2-rw")                      \
  _(LEARN,         "l2-learn")                   \
  _(VTR,           "l2-input-vtr")               \
- _(VPATH,         "vpath-input-l2")             \
  _(ACL,           "l2-input-acl")               \
  _(POLICER_CLAS,  "l2-policer-classify")	\
- _(INPUT_CLASSIFY, "l2-input-classify")         \
+ _(INPUT_CLASSIFY,"l2-input-classify")          \
  _(SPAN,          "span-l2-input")
 
 /* Feature bitmap positions */
