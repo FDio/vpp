17aa409557df1098e2d67ad7e804df66aaae47f2
Neale Ranns <nranns@cisco.com>
2017-10-21 10:53:20 -0700

    BIER
    - see draft-ietf-bier-mpls-encapsulation-10
    - midpoint, head and tail functions
    - supported payload protocols; IPv4 and IPv6 only.
    
    Change-Id: I59d7363bb6fdfdce8e4016a68a9c8f5a5e5791cb
    Signed-off-by: Neale Ranns <nranns@cisco.com>

---
+++ src/vnet/bier/bier_disp_lookup_node.c
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vnet/bier/bier_disp_table.h>
+#include <vnet/bier/bier_hdr_inlines.h>
+
+/**
+ * @brief A struct to hold tracing information for the MPLS label imposition
+ * node.
+ */
+typedef struct bier_disp_lookup_trace_t_
+{
+    /**
+     * BIER source BP used in the lookup - host order
+     */
+    bier_bp_t bp;
+    /**
+     * BIER disp table
+     */
+    index_t bdti;
+} bier_disp_lookup_trace_t;
+
+/**
+ * Next nodes from BIER disposition lookup
+ */
+typedef enum bier_disp_lookup_next_t_
+{
+    BIER_DISP_LOOKUP_NEXT_DROP,
+    BIER_DISP_LOOKUP_NEXT_DISPATCH,
+} bier_disp_lookup_next_t;
+#define BIER_DISP_LOOKUP_N_NEXT (BIER_DISP_LOOKUP_NEXT_DISPATCH+1)
+
+always_inline uword
+bier_disp_lookup_inline (vlib_main_t * vm,
+                         vlib_node_runtime_t * node,
+                         vlib_frame_t * from_frame)
+{
+    u32 n_left_from, next_index, * from, * to_next;
+
+    from = vlib_frame_vector_args (from_frame);
+    n_left_from = from_frame->n_vectors;
+
+    next_index = node->cached_next_index;
+
+    while (n_left_from > 0)
+    {
+        u32 n_left_to_next;
+
+        vlib_get_next_frame(vm, node, next_index, to_next, n_left_to_next);
+
+        while (n_left_from > 0 && n_left_to_next > 0)
+        {
+            const bier_hdr_t *hdr0;
+            vlib_buffer_t * b0;
+            u32 bdei0, bdti0;
+            u32 next0, bi0;
+
+            bi0 = from[0];
+            to_next[0] = bi0;
+            from += 1;
+            to_next += 1;
+            n_left_from -= 1;
+            n_left_to_next -= 1;
+
+            b0 = vlib_get_buffer (vm, bi0);
+
+            bdti0 = vnet_buffer(b0)->ip.adj_index[VLIB_TX];
+            hdr0 = vlib_buffer_get_current(b0);
+
+            /*
+             * lookup - source is in network order.
+             */
+            bdei0 = bier_disp_table_lookup(bdti0, bier_hdr_get_src_id(hdr0));
+
+            if (PREDICT_FALSE(INDEX_INVALID == bdei0))
+            {
+                next0 = BIER_DISP_LOOKUP_NEXT_DROP;
+            }
+            else
+            {
+                next0 = BIER_DISP_LOOKUP_NEXT_DISPATCH;
+            }
+
+            vnet_buffer(b0)->ip.adj_index[VLIB_TX] = bdei0;
+
+            if (PREDICT_FALSE(b0->flags & VLIB_BUFFER_IS_TRACED))
+            {
+                bier_disp_lookup_trace_t *tr =
+                    vlib_add_trace (vm, node, b0, sizeof (*tr));
+                tr->bp = clib_net_to_host_u16(bier_hdr_get_src_id(hdr0));
+                tr->bdti = bdti0;
+            }
+
+            vlib_validate_buffer_enqueue_x1(vm, node, next_index, to_next,
+                                            n_left_to_next, bi0, next0);
+        }
+        vlib_put_next_frame (vm, node, next_index, n_left_to_next);
+    }
+    return from_frame->n_vectors;
+}
+
+static u8 *
+format_bier_disp_lookup_trace (u8 * s, va_list * args)
+{
+    CLIB_UNUSED (vlib_main_t * vm) = va_arg (*args, vlib_main_t *);
+    CLIB_UNUSED (vlib_node_t * node) = va_arg (*args, vlib_node_t *);
+    bier_disp_lookup_trace_t * t;
+
+    t = va_arg (*args, bier_disp_lookup_trace_t *);
+    s = format (s, "tbl:%d src:%d", t->bdti, t->bp);
+
+    return (s);
+}
+
+static uword
+bier_disp_lookup (vlib_main_t * vm,
+                  vlib_node_runtime_t * node,
+                  vlib_frame_t * frame)
+{
+    return (bier_disp_lookup_inline(vm, node, frame));
+}
+
+VLIB_REGISTER_NODE (bier_disp_lookup_node) = {
+    .function = bier_disp_lookup,
+    .name = "bier-disp-lookup",
+    .vector_size = sizeof (u32),
+
+    .format_trace = format_bier_disp_lookup_trace,
+    .n_next_nodes = BIER_DISP_LOOKUP_N_NEXT,
+    .next_nodes = {
+        [BIER_DISP_LOOKUP_NEXT_DROP] = "bier-drop",
+        [BIER_DISP_LOOKUP_NEXT_DISPATCH] = "bier-disp-dispatch",
+    }
+};
+VLIB_NODE_FUNCTION_MULTIARCH (bier_disp_lookup_node, bier_disp_lookup)
--- src/vnet/fib/fib_test.h
+++ src/vnet/fib/fib_test.h
@@ -17,16 +17,24 @@
 #define __FIB_TEST_H__
 
 #include <vnet/fib/fib_types.h>
+#include <vnet/mpls/mpls_types.h>
+#include <vnet/fib/fib_types.h>
+#include <vnet/mpls/packet.h>
+#include <vnet/dpo/load_balance.h>
+#include <vnet/adj/adj_types.h>
+#include <vnet/dpo/replicate_dpo.h>
 
 typedef enum fib_test_lb_bucket_type_t_ {
     FT_LB_LABEL_O_ADJ,
     FT_LB_LABEL_STACK_O_ADJ,
     FT_LB_LABEL_O_LB,
     FT_LB_O_LB,
-    FT_LB_SPECIAL,
-    FT_LB_ADJ,
     FT_LB_INTF,
     FT_LB_L2,
+    FT_LB_BIER_TABLE,
+    FT_LB_BIER_FMASK,
+    FT_LB_DROP,
+    FT_LB_ADJ,
 } fib_test_lb_bucket_type_t;
 
 typedef struct fib_test_lb_bucket_t_ {
@@ -68,6 +76,13 @@ typedef struct fib_test_lb_bucket_t_ {
 	{
 	    index_t adj;
 	} special;
+        struct
+        {
+            union {
+                index_t table;
+                index_t fmask;
+            };
+	} bier;
     };
 } fib_test_lb_bucket_t;
 
@@ -105,9 +120,14 @@ extern int fib_test_validate_lb_v(const load_balance_t *lb,
                                   u16 n_buckets,
                                   va_list *ap);
 
+extern int fib_test_validate_lb(const dpo_id_t *dpo,
+				u16 n_buckets,
+				...);
+
 extern int fib_test_validate_entry(fib_node_index_t fei,
                                    fib_forward_chain_type_t fct,
                                    int n_buckets,
                                    ...);
+
 
 #endif
