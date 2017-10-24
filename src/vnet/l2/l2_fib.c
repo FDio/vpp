3ae7e84f418e20e92bebd74f96ed6897c7fdd67e
Gabriel Ganne <gabriel.ganne@enea.com>
2017-10-24 10:24:57 +0200

    fix l2fib_init() invalid read of size 2
    l2fib_make_key() casts and reads the input mac_address as u64, therefore
    if the mac_address is declared with 6 Bytes only, we trigger an invalid
    read on the las two Bytes.
    
    l2fib_make_key() does a 16 bits shift to discard those values, so this
    invalid read is of no consequence other than to raise an error.
    
    l2fib_init() seems to be the only place in the module where the mac
    address is on 6 Bytes only; the other declarations of mac addresses
    are as u64.
    
    Change-Id: I38646fe60073093d25cdf135185d4c96136d55d0
    Signed-off-by: Gabriel Ganne <gabriel.ganne@enea.com>

--- src/vnet/l2/l2_fib.c
+++ src/vnet/l2/l2_fib.c
@@ -1221,7 +1221,7 @@ l2fib_init (vlib_main_t * vm)
 {
   l2fib_main_t *mp = &l2fib_main;
   l2fib_entry_key_t test_key;
-  u8 test_mac[6];
+  u8 test_mac[8];
 
   mp->vlib_main = vm;
   mp->vnet_main = vnet_get_main ();
