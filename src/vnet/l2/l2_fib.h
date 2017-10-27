12f90968af53f466bf1489125c25afe9bd5458ff
Mohsin Kazmi <sykazmi@cisco.com>
2017-10-27 21:28:07 +0200

    l2fib: MAC: Fix uint64 to u8 byte array
    As per proposal on the mailing list, this patch fixes
    the represntation of MAC address in VPP API calls for
       ·  L2fib_add_del
       ·  L2_fib_table_details
    
    Change-Id: I31e17efd1a6314cded69666e693cb8fc33158d02
    Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>

--- src/vnet/l2/l2_fib.h
+++ src/vnet/l2/l2_fib.h
@@ -371,24 +371,24 @@ l2fib_lookup_4 (BVT (clib_bihash) * mac_table,
 void l2fib_clear_table (void);
 
 void
-l2fib_add_entry (u64 mac,
+l2fib_add_entry (u8 * mac,
 		 u32 bd_index,
 		 u32 sw_if_index, u8 static_mac, u8 drop_mac, u8 bvi_mac);
 
 static inline void
-l2fib_add_fwd_entry (u64 mac, u32 bd_index, u32 sw_if_index, u8 static_mac,
+l2fib_add_fwd_entry (u8 * mac, u32 bd_index, u32 sw_if_index, u8 static_mac,
 		     u8 bvi_mac)
 {
   l2fib_add_entry (mac, bd_index, sw_if_index, static_mac, 0, bvi_mac);
 }
 
 static inline void
-l2fib_add_filter_entry (u64 mac, u32 bd_index)
+l2fib_add_filter_entry (u8 * mac, u32 bd_index)
 {
   l2fib_add_entry (mac, bd_index, ~0, 1, 1, 0);
 }
 
-u32 l2fib_del_entry (u64 mac, u32 bd_index);
+u32 l2fib_del_entry (u8 * mac, u32 bd_index);
 
 void l2fib_start_ager_scan (vlib_main_t * vm);
 
