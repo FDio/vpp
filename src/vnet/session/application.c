b56b7d4c631893001872b7ef0a42bda74f7d3853
Marco Varlese <marco.varlese@suse.com>
2017-10-30 18:17:21 +0100

    DRAFT: SCTP implementation
    This patch contains:
    * SCTP chunks definition as per RFC4960;
    * Helper functions to set/get values to/from the corresponding chunks;
    * CRC32c functionality to calculate checksum as per RFC4960 for both
    IPv4 and IPv6;
    * Partially completed INIT sequence;
    * Hooks to the session/application layers;
    
    Change-Id: I1b55c455ab400be9513f4e094dadfc3181d2ebc9
    Signed-off-by: Marco Varlese <marco.varlese@suse.com>

--- src/vnet/session/application.c
+++ src/vnet/session/application.c
@@ -610,6 +610,8 @@ application_setup_proxy (application_t * app)
     application_start_stop_proxy (app, TRANSPORT_PROTO_TCP, 1);
   if (transports & (1 << TRANSPORT_PROTO_UDP))
     application_start_stop_proxy (app, TRANSPORT_PROTO_UDP, 1);
+  if (transports & (1 << TRANSPORT_PROTO_SCTP))
+    application_start_stop_proxy (app, TRANSPORT_PROTO_SCTP, 1);
 }
 
 void
@@ -621,6 +623,8 @@ application_remove_proxy (application_t * app)
     application_start_stop_proxy (app, TRANSPORT_PROTO_TCP, 0);
   if (transports & (1 << TRANSPORT_PROTO_UDP))
     application_start_stop_proxy (app, TRANSPORT_PROTO_UDP, 0);
+  if (transports & (1 << TRANSPORT_PROTO_SCTP))
+    application_start_stop_proxy (app, TRANSPORT_PROTO_SCTP, 0);
 }
 
 u8 *
