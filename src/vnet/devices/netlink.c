9f945f27c074f2096fa57215aad3641537a0793f
Damjan Marion <damarion@cisco.com>
2017-11-21 16:22:55 +0100

    tap_v2: add option to specify namespace by PID or full path
    to specify pid use format "pid:12345"
    
    Change-Id: I1079312b6bfea4cb970143d42ef64a69edcad406
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- src/vnet/devices/netlink.c
+++ src/vnet/devices/netlink.c
@@ -94,14 +94,27 @@ vnet_netlink_set_if_namespace (int ifindex, char *net_ns)
   clib_error_t *err;
   int ns_fd;
   u8 *s;
-  s = format (0, "/var/run/netns/%s%c", net_ns, 0);
+
+  if (strncmp (net_ns, "pid:", 4) == 0)
+    {
+      int pid = atoi (net_ns + 4);
+      return vnet_netlink_set_if_attr (ifindex, IFLA_NET_NS_PID, &pid,
+				       sizeof (int));
+    }
+
+  if (net_ns[0] == '/')
+    s = format (0, "%s%c", net_ns, 0);
+  else
+    s = format (0, "/var/run/netns/%s%c", net_ns, 0);
+
   ns_fd = open ((char *) s, O_RDONLY);
   vec_free (s);
+
   if (ns_fd == -1)
     return clib_error_return (0, "namespace '%s' doesn't exist", net_ns);
 
-  err =
-    vnet_netlink_set_if_attr (ifindex, IFLA_NET_NS_FD, &ns_fd, sizeof (int));
+  err = vnet_netlink_set_if_attr (ifindex, IFLA_NET_NS_FD, &ns_fd,
+				  sizeof (int));
   close (ns_fd);
   return err;
 }
