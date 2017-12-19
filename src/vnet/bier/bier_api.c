6b37e5dd145e5f9d938c467166ba78b4d7faa7b0
Dave Barach <dave@barachs.net>
2017-12-19 08:26:27 -0500

    VPP-1032: fix coverity warning in bier
    Change-Id: I0f8d2d9be78d4d936ed61538aee1b7207871347b
    Signed-off-by: Dave Barach <dave@barachs.net>

--- src/vnet/bier/bier_api.c
+++ src/vnet/bier/bier_api.c
@@ -236,11 +236,11 @@ vl_api_bier_route_add_del_t_handler (vl_api_bier_route_add_del_t * mp)
 
     if (mp->br_is_add)
     {
-        bier_table_route_add(&bti, ntohs(mp->br_bp), brpaths);
+        bier_table_route_add(&bti, bp, brpaths);
     }
     else
     {
-        bier_table_route_remove(&bti, ntohs(mp->br_bp), brpaths);
+        bier_table_route_remove(&bti, bp, brpaths);
     }
 
 done:
