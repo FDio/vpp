033079a27e6a0a6e40212bc5dff8298880bb8f74
Steven <sluong@cisco.com>
2017-12-20 12:43:01 -0800

    lacp: Add LACP protocol
    Add bond-input/bond-output statistics, interface admin up/down
    
    Add feature arc for bond-input and bond-output
    
    A very rudimentary implementation of LACP based on 802.3ad standard.
    Just the framework. Still lots of TODOs.
    
    Change-Id: I06581d3b87635972f9f0e1ec50b67560fc13e26c
    Signed-off-by: Steven <sluong@cisco.com>

---
+++ src/vnet/bonding/lacp/input.c
@@ -0,0 +1,218 @@
+/*
+ * Copyright (c) 2017 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define _GNU_SOURCE
+#include <vnet/bonding/node.h>
+#include <vnet/bonding/lacp/node.h>
+#include <vnet/bonding/lacp/rx_machine.h>
+
+static int
+lacp_packet_scan (vlib_main_t * vm, bond_main_t * bm, lacp_neighbor_t * n)
+{
+  lacp_pdu_t *lacpdu = (lacp_pdu_t *) n->last_rx_pkt;
+
+  if (lacpdu->subtype != LACP_SUBTYPE)
+    return LACP_ERROR_UNSUPPORTED;
+
+  /*
+   * According to the spec, no checking on the version number and tlv types.
+   * But we may check the tlv lengths.
+   */
+  if ((lacpdu->actor.tlv_length != sizeof (lacp_actor_partner_t)) ||
+      (lacpdu->partner.tlv_length != sizeof (lacp_actor_partner_t)) ||
+      (lacpdu->collector.tlv_length != sizeof (lacp_collector_t)) ||
+      (lacpdu->terminator.tlv_length != 0))
+    return (LACP_ERROR_BAD_TLV);
+
+  lacp_machine_dispatch (&lacp_rx_machine, vm, n, LACP_RX_EVENT_PDU_RECEIVED,
+			 &n->rx_state);
+
+  return LACP_ERROR_NONE;
+}
+
+/*
+ * lacp input routine
+ */
+lacp_error_t
+lacp_input (vlib_main_t * vm, vlib_buffer_t * b0, u32 bi0)
+{
+  bond_main_t *bm = &bond_main;
+  lacp_neighbor_t *n;
+  uword *p, nbytes;
+  lacp_error_t e;
+
+  p = hash_get (bm->neighbor_by_sw_if_index,
+		vnet_buffer (b0)->sw_if_index[VLIB_RX]);
+
+  if (p == 0)
+    {
+      return LACP_ERROR_DISABLED;
+    }
+  else
+    {
+      n = pool_elt_at_index (bm->neighbors, p[0]);
+    }
+
+  /*
+   * typical clib idiom. Don't repeatedly allocate and free
+   * the per-neighbor rx buffer. Reset its apparent length to zero
+   * and reuse it.
+   */
+
+  if (n->last_rx_pkt)
+    _vec_len (n->last_rx_pkt) = 0;
+
+  /* lacp disabled on this interface, we're done */
+  if (n->disabled)
+    return LACP_ERROR_DISABLED;
+
+  /*
+   * Make sure the per-neighbor rx buffer is big enough to hold
+   * the data we're about to copy
+   */
+  vec_validate (n->last_rx_pkt, vlib_buffer_length_in_chain (vm, b0) - 1);
+
+  /*
+   * Coalesce / copy e the buffer chain into the per-neighbor
+   * rx buffer
+   */
+  nbytes = vlib_buffer_contents (vm, bi0, n->last_rx_pkt);
+  ASSERT (nbytes <= vec_len (n->last_rx_pkt));
+
+  if (nbytes < sizeof (lacp_pdu_t))
+    return LACP_ERROR_TOO_SMALL;
+
+  /* Actually scan the packet */
+  e = lacp_packet_scan (vm, bm, n);
+
+  if (e == LACP_ERROR_NONE)
+    {
+      n->last_heard = vlib_time_now (vm);
+    }
+
+  return e;
+}
+
+/*
+ * setup neighbor hash table
+ */
+static clib_error_t *
+lacp_init (vlib_main_t * vm)
+{
+  clib_error_t *error;
+
+  if ((error = vlib_call_init_function (vm, lacp_periodic_init)))
+    return error;
+
+  return 0;
+}
+
+VLIB_INIT_FUNCTION (lacp_init);
+
+/*
+ * packet trace format function, very similar to
+ * lacp_packet_scan except that we call the per TLV format
+ * functions instead of the per TLV processing functions
+ */
+u8 *
+lacp_input_format_trace (u8 * s, va_list * args)
+{
+  CLIB_UNUSED (vlib_main_t * vm) = va_arg (*args, vlib_main_t *);
+  CLIB_UNUSED (vlib_node_t * node) = va_arg (*args, vlib_node_t *);
+  lacp_input_trace_t *t = va_arg (*args, lacp_input_trace_t *);
+  lacp_pdu_t *lacpdu = &t->lacpdu;
+  int i, len;
+  u8 *p;
+  lacp_state_struct *state_entry;
+
+  s = format (s, "Length: %d\n", t->len);
+  if (t->len >= sizeof (lacp_pdu_t))
+    {
+      if ((lacpdu->subtype == LACP_SUBTYPE) && (lacpdu->version_number == 1))
+	s = format (s, "  LACPv1\n");
+      else
+	s = format (s, "  Subtype %u, Version %u\n", lacpdu->subtype,
+		    lacpdu->version_number);
+      s = format (s, "  Actor Information TLV: length %u\n",
+		  lacpdu->actor.tlv_length);
+      s = format (s, "    System %U\n", format_ethernet_address,
+		  lacpdu->actor.port_info.system);
+      s = format (s, "    System priority %u\n",
+		  ntohs (lacpdu->actor.port_info.system_priority));
+      s = format (s, "    Key %u\n", ntohs (lacpdu->actor.port_info.key));
+      s = format (s, "    Port priority %u\n",
+		  ntohs (lacpdu->actor.port_info.port_priority));
+      s = format (s, "    Port number %u\n",
+		  ntohs (lacpdu->actor.port_info.port_number));
+      s = format (s, "    State 0x%x\n", lacpdu->actor.port_info.state);
+      state_entry = (lacp_state_struct *) & lacp_state_array;
+      while (state_entry->str)
+	{
+	  if (lacpdu->actor.port_info.state & (1 << state_entry->bit))
+	    s = format (s, "      %s (%d)\n", state_entry->str,
+			state_entry->bit);
+	  state_entry++;
+	}
+
+      s = format (s, "  Partner Information TLV: length %u\n",
+		  lacpdu->partner.tlv_length);
+      s = format (s, "    System %U\n", format_ethernet_address,
+		  lacpdu->partner.port_info.system);
+      s = format (s, "    System priority %u\n",
+		  ntohs (lacpdu->partner.port_info.system_priority));
+      s = format (s, "    Key %u\n", ntohs (lacpdu->partner.port_info.key));
+      s = format (s, "    Port priority %u\n",
+		  ntohs (lacpdu->partner.port_info.port_priority));
+      s = format (s, "    Port number %u\n",
+		  ntohs (lacpdu->partner.port_info.port_number));
+      s = format (s, "    State 0x%x\n", lacpdu->partner.port_info.state);
+      state_entry = (lacp_state_struct *) & lacp_state_array;
+      while (state_entry->str)
+	{
+	  if (lacpdu->partner.port_info.state & (1 << state_entry->bit))
+	    s = format (s, "      %s (%d)\n", state_entry->str,
+			state_entry->bit);
+	  state_entry++;
+	}
+    }
+
+  if (t->len > sizeof (lacp_pdu_t))
+    len = sizeof (lacp_pdu_t);
+  else
+    len = t->len;
+  p = (u8 *) lacpdu;
+  for (i = 0; i < len; i++)
+    {
+      if ((i % 16) == 0)
+	{
+	  if (i)
+	    s = format (s, "\n");
+	  s = format (s, "  0x%04x: ", i);
+	}
+      if ((i % 2) == 0)
+	s = format (s, " ");
+      s = format (s, "%02x", p[i]);
+    }
+
+  return s;
+}
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
---
+++ src/vnet/bonding/lacp/ptx_machine.c
@@ -0,0 +1,217 @@
+/*
+ * Copyright (c) 2017 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define _GNU_SOURCE
+
+#include <vnet/bonding/lacp/node.h>
+#include <vnet/bonding/lacp/ptx_machine.h>
+#include <vnet/bonding/lacp/tx_machine.h>
+
+/*
+ *  LACP State = UNKNOWN
+ */
+static lacp_fsm_state_t lacp_ptx_state_begin[] = {
+  {LACP_ACTION_NO_PERIODIC, LACP_PTX_STATE_NO_PERIODIC},	// event 0 BEGIN
+  {LACP_ACTION_NO_PERIODIC, LACP_PTX_STATE_NO_PERIODIC},	// event 1 NO_PERIODIC
+  {LACP_ACTION_NO_PERIODIC, LACP_PTX_STATE_NO_PERIODIC},	// event 2 START_PERIODIC
+  {LACP_ACTION_NO_PERIODIC, LACP_PTX_STATE_NO_PERIODIC},	// event 3 LONG_TIMEOUT
+  {LACP_ACTION_NO_PERIODIC, LACP_PTX_STATE_NO_PERIODIC},	// event 4 TIMER_EXPIRED
+  {LACP_ACTION_NO_PERIODIC, LACP_PTX_STATE_NO_PERIODIC},	// event 5 SHORT_TIMEOUT
+};
+
+/*
+ *  LACP State = NO_PERIODIC
+ */
+static lacp_fsm_state_t lacp_ptx_state_no_periodic[] = {
+  {LACP_ACTION_NO_PERIODIC, LACP_PTX_STATE_NO_PERIODIC},	// event 0 BEGIN
+  {LACP_ACTION_NO_PERIODIC, LACP_PTX_STATE_NO_PERIODIC},	// event 1 NO_PERIODIC
+  {LACP_ACTION_FAST_PERIODIC, LACP_PTX_STATE_FAST_PERIODIC},	// event 2 START_PERIODIC
+  {LACP_NOACTION, LACP_PTX_STATE_NO_PERIODIC},	// event 3 LONG_TIMEOUT
+  {LACP_NOACTION, LACP_PTX_STATE_NO_PERIODIC},	// event 4 TIMER_EXPIRED
+  {LACP_NOACTION, LACP_PTX_STATE_NO_PERIODIC},	// event 5 SHORT_TIMEOUT
+};
+
+/*
+ *  LACP State = SLOW_PERIODIC
+ */
+static lacp_fsm_state_t lacp_ptx_state_slow_periodic[] = {
+  {LACP_ACTION_NO_PERIODIC, LACP_PTX_STATE_NO_PERIODIC},	// event 0 BEGIN
+  {LACP_ACTION_NO_PERIODIC, LACP_PTX_STATE_NO_PERIODIC},	// event 1 NO_PERIODIC
+  {LACP_ACTION_TIMER_EXPIRED, LACP_PTX_STATE_PERIODIC_TX},	// event 2 START_PERIODIC
+  {LACP_ACTION_TIMER_EXPIRED, LACP_PTX_STATE_PERIODIC_TX},	// event 3 LONG_TIMEOUT
+  {LACP_ACTION_TIMER_EXPIRED, LACP_PTX_STATE_PERIODIC_TX},	// event 4 TIMER_EXPIRED
+  {LACP_ACTION_TIMER_EXPIRED, LACP_PTX_STATE_PERIODIC_TX},	// event 5 SHORT_TIMEOUT
+};
+
+/*
+ *  LACP State = FAST_PERIODIC
+ */
+static lacp_fsm_state_t lacp_ptx_state_fast_periodic[] = {
+  {LACP_ACTION_NO_PERIODIC, LACP_PTX_STATE_NO_PERIODIC},	// event 0 BEGIN
+  {LACP_ACTION_NO_PERIODIC, LACP_PTX_STATE_NO_PERIODIC},	// event 1 NO_PERIODIC
+  {LACP_ACTION_FAST_PERIODIC, LACP_PTX_STATE_FAST_PERIODIC},	// event 2 START_PERIODIC
+  {LACP_ACTION_FAST_PERIODIC, LACP_PTX_STATE_FAST_PERIODIC},	// event 3 LONG_TIMEOUT
+  {LACP_ACTION_TIMER_EXPIRED, LACP_PTX_STATE_PERIODIC_TX},	// event 4 TIMER_EXPIRED
+  {LACP_ACTION_FAST_PERIODIC, LACP_PTX_STATE_FAST_PERIODIC},	// event 5 SHORT_TIMEOUT
+};
+
+/*
+ *  LACP State = PERIODIC_TX
+ */
+static lacp_fsm_state_t lacp_ptx_state_periodic_tx[] = {
+  {LACP_ACTION_NO_PERIODIC, LACP_PTX_STATE_NO_PERIODIC},	// event 0 BEGIN
+  {LACP_ACTION_NO_PERIODIC, LACP_PTX_STATE_NO_PERIODIC},	// event 1 NO_PERIODIC
+  {LACP_ACTION_TIMER_EXPIRED, LACP_PTX_STATE_PERIODIC_TX},	// event 2 START_PERIODIC
+  {LACP_ACTION_FAST_PERIODIC, LACP_PTX_STATE_FAST_PERIODIC},	// event 3 LONG_TIMEOUT
+  {LACP_ACTION_TIMER_EXPIRED, LACP_PTX_STATE_PERIODIC_TX},	// event 4 TIMER_EXPIRED
+  {LACP_ACTION_FAST_PERIODIC, LACP_PTX_STATE_FAST_PERIODIC},	// event 5 SHORT_TIMEOUT
+};
+
+
+static lacp_fsm_machine_t lacp_ptx_fsm_table[] = {
+  {lacp_ptx_state_begin},
+  {lacp_ptx_state_no_periodic},
+  {lacp_ptx_state_slow_periodic},
+  {lacp_ptx_state_fast_periodic},
+  {lacp_ptx_state_periodic_tx},
+};
+
+lacp_machine_t lacp_ptx_machine = {
+  lacp_ptx_fsm_table,
+  lacp_ptx_debug_func,
+};
+
+int
+lacp_ptx_action_no_periodic (void *p1, void *p2)
+{
+  vlib_main_t *vm = (vlib_main_t *) p1;
+  lacp_neighbor_t *n = (lacp_neighbor_t *) p2;
+
+  n->periodic_timer = 0.0;
+
+  if (n->lacp_enabled || n->port_enabled ||
+      ((n->actor.state & LACP_STATE_LACP_ACTIVITY) &&
+       (n->partner.state & LACP_STATE_LACP_ACTIVITY)))
+    lacp_machine_dispatch (&lacp_ptx_machine, vm, n,
+			   LACP_PTX_EVENT_START_PERIODIC, &n->ptx_state);
+
+  return 0;
+}
+
+int
+lacp_ptx_action_slow_periodic (void *p1, void *p2)
+{
+  vlib_main_t *vm = (vlib_main_t *) p1;
+  lacp_neighbor_t *n = (lacp_neighbor_t *) p2;
+
+  n->periodic_timer = vlib_time_now (vm) + LACP_SLOW_PERIODIC_TIMER;
+
+  return 0;
+}
+
+int
+lacp_ptx_action_fast_periodic (void *p1, void *p2)
+{
+  vlib_main_t *vm = (vlib_main_t *) p1;
+  lacp_neighbor_t *n = (lacp_neighbor_t *) p2;
+
+  n->periodic_timer = vlib_time_now (vm) + LACP_FAST_PERIODIC_TIMER;
+
+  return 0;
+}
+
+int
+lacp_ptx_action_timer_expired (void *p1, void *p2)
+{
+  vlib_main_t *vm = (vlib_main_t *) p1;
+  lacp_neighbor_t *n = (lacp_neighbor_t *) p2;
+
+  n->ntt = 1;
+  lacp_machine_dispatch (&lacp_tx_machine, vm, n, LACP_TX_EVENT_NTT,
+			 &n->tx_state);
+  if (n->partner.state & LACP_STATE_LACP_TIMEOUT)
+    lacp_machine_dispatch (&lacp_ptx_machine, vm, n,
+			   LACP_PTX_EVENT_SHORT_TIMEOUT, &n->ptx_state);
+  else
+    lacp_machine_dispatch (&lacp_ptx_machine, vm, n,
+			   LACP_PTX_EVENT_LONG_TIMEOUT, &n->ptx_state);
+
+  return 0;
+}
+
+static u8 *
+format_ptx_event (u8 * s, va_list * args)
+{
+  static lacp_event_struct lacp_ptx_event_array[] = {
+#define _(b, s, n) {.bit = b, .str = #s, },
+    foreach_lacp_ptx_event
+#undef _
+    {.str = NULL}
+  };
+  int e = va_arg (*args, int);
+  lacp_event_struct *event_entry =
+    (lacp_event_struct *) & lacp_ptx_event_array;
+
+  if (e >= (sizeof (lacp_ptx_event_array) / sizeof (*event_entry)))
+    s = format (s, "Bad event %d", e);
+  else
+    s = format (s, "%s", event_entry[e].str);
+
+  return s;
+}
+
+u8 *
+format_ptx_sm_state (u8 * s, va_list * args)
+{
+  static lacp_state_struct lacp_ptx_sm_state_array[] = {
+#define _(b, s, n) {.bit = b, .str = #s, },
+    foreach_lacp_ptx_sm_state
+#undef _
+    {.str = NULL}
+  };
+  int state = va_arg (*args, int);
+  lacp_state_struct *state_entry =
+    (lacp_state_struct *) & lacp_ptx_sm_state_array;
+
+  if (state >= (sizeof (lacp_ptx_sm_state_array) / sizeof (*state_entry)))
+    s = format (s, "Bad state %d", state);
+  else
+    s = format (s, "%s", state_entry[state].str);
+
+  return s;
+}
+
+void
+lacp_ptx_debug_func (int event, int state, lacp_fsm_state_t * transition)
+{
+  clib_warning ("PTX: event %U, old state %U, new state %U", format_ptx_event,
+		event, format_ptx_sm_state, state, format_ptx_sm_state,
+		transition->next_state);
+}
+
+void
+lacp_init_ptx_machine (vlib_main_t * vm, lacp_neighbor_t * n)
+{
+  lacp_machine_dispatch (&lacp_ptx_machine, vm, n, LACP_PTX_EVENT_BEGIN,
+			 &n->ptx_state);
+}
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
