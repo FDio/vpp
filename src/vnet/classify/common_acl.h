a660a4d66fee6a90e30c92175e9c09ff9a14126e
Andrew Yourtchenko <ayourtch@gmail.com>
2017-12-21 18:58:43 +0100

    DO_NOT_MERGE: VPP-1088: classifier-based outacl + macip-egress ACL enhancement
    The requirements from VPP-1088 will be implemented as two parts:
    1) acl plugin macip acl programming the destination simultaneously in the reverse direction, when using is_permit==3
    2) vnet adding the outbound acl using classifier tables, similar to inbound acl
    
    It compiles and does not break the existing unit tests. And that is all that is known at this point.
    
    Change-Id: I3d7883ed45b1cdf98d7303771bcc75951dff38f0
    Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>

---
+++ src/vnet/classify/common_acl.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2015 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __included_vnet_common_acl_h__
+#define __included_vnet_common_acl_h__
+
+typedef enum
+{
+    ACL_NEXT_INDEX_DENY,
+    ACL_NEXT_INDEX_N_NEXT,
+} acl_next_index_t;
+
+#endif
