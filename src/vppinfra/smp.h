00d67707a9d5ece51dfbb82e57d6790fc691756d
Kevin Wang <kevin.wang@arm.com>
2018-01-09 10:42:07 +0800

    vppinfra: use __atomic to take replace of __sync builtins
    In smp.h, some clib functions still use the legacy "__sync" builtins.
    This will decrease performance at least on arm platform.
    
    Change-Id: I212a8f4e18e7b970492996259a84cf8cd3c919af
    Signed-off-by: Kevin Wang <kevin.wang@arm.com>

--- src/vppinfra/smp.h
+++ src/vppinfra/smp.h
@@ -41,9 +41,10 @@
 #include <vppinfra/cache.h>
 #include <vppinfra/os.h>	/* for os_panic */
 
-#define clib_smp_compare_and_swap(addr,new,old) __sync_val_compare_and_swap(addr,old,new)
-#define clib_smp_swap(addr,new) __sync_lock_test_and_set(addr,new)
-#define clib_smp_atomic_add(addr,increment) __sync_fetch_and_add(addr,increment)
+#define clib_smp_compare_and_swap(addr, new, old) \
+	__atomic_compare_exchange_n(addr, &old, new, 0, __ATOMIC_RELAXED, __ATOMIC_RELAXED)
+#define clib_smp_swap(addr, new) __atomic_exchange_n(addr, new, __ATOMIC_RELAXED)
+#define clib_smp_atomic_add(addr, increment) __atomic_fetch_add(addr, increment, __ATOMIC_RELAXED)
 
 #if defined (i386) || defined (__x86_64__)
 #define clib_smp_pause() do { asm volatile ("pause"); } while (0)
