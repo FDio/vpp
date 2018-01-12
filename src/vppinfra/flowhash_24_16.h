0724107170e2364788a8c1ba886c7a8fbbf984a9
Pierre Pfister <ppfister@cisco.com>
2018-01-12 09:41:16 +0100

    Add flowhash hash table to vppinfra
    This hash table intends to provide an alternative to the widely
    used bihash table in places where either:
    - Hash entry timeout is required
    - The hash table data does not fit in CPU cache
    
    Although the bihash table is very fast, each lookup requires
    accessing two cache lines in a serialized fashion. It works fine
    when the hash table is in cache, but hits a wall when it does not.
    
    The 'flowhash' table uses a simplified design (at the cost of a
    less good bucket auto-scaling) where each access only requires
    a single memory lookup (in the absence of collision). The hash
    table also uses a reduced number of registers.
    
    In practice, a VPP node implementing a stateful feature would
    typically:
    - prefetch buffer metadata (in-cache)
    - prefetch packet header (in-cache)
    - compute hash & prefetch hash bucket (possibly in RAM)
    - read/write key and value from bucket
    
    Using this hash table, it is possible to pipeline accesses in a way
    that does not exhaust CPU's line field buffers, even when the
    requested value is located in RAM (i.e. not in cache).
    
    Measurements showed it was possible to scale to tens of millions
    of flows (with a full 5-tuple matching and 32B value, i.e. 1
    cache line per flow) with no performance degradation when
    the hash table grows to the point it doesn't fit in cache anymore.
    
    I have used this table in a couple of non-open-sourced projects,
    but think it might be useful to lb, nat, and possibly other VPP
    subsystems.
    
    More information in the .h file.
    
    Change-Id: I2b13dde0eabd868b75da1cedbfca0bf74d705102
    Signed-off-by: Pierre Pfister <ppfister@cisco.com>

---
+++ src/vppinfra/flowhash_24_16.h
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2018 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SRC_VPPINFRA_FLOWHASH_24_16_H_
+#define SRC_VPPINFRA_FLOWHASH_24_16_H_
+
+#ifdef __included_flowhash_template_h__
+#undef __included_flowhash_template_h__
+#endif
+
+#if defined (__SSE4_2__)
+#include <immintrin.h>
+#endif
+
+#include <vppinfra/clib.h>
+
+typedef struct {
+  u64 as_u64[3];
+} flowhash_skey_24_16_t;
+
+typedef struct {
+  u64 as_u64[3];
+} flowhash_lkey_24_16_t;
+
+typedef struct {
+  u64 as_u64[2];
+} flowhash_value_24_16_t;
+
+#define FLOWHASH_TYPE _24_16
+#include <vppinfra/flowhash_template.h>
+#undef FLOWHASH_TYPE
+
+#if __SSE4_2__ && !defined (__i386__)
+static_always_inline
+u32 flowhash_hash_24_16(flowhash_lkey_24_16_t *k)
+{
+  /* crc32 costs multiple cycles, but multiple operations can be performed
+   * in parallel. We can still achieve decent hash properties by splitting
+   * the computation in two. */
+  u32 val0 = 0;
+  u32 val1 = 0;
+  val0 = _mm_crc32_u64(val0, k->as_u64[0]);
+  val1 = _mm_crc32_u64(val1, k->as_u64[0] >> 32);
+  val0 = _mm_crc32_u64(val0, k->as_u64[1]);
+  val1 = _mm_crc32_u64(val1, k->as_u64[1] >> 32);
+  val0 = _mm_crc32_u64(val0, k->as_u64[2]);
+  val1 = _mm_crc32_u64(val1, k->as_u64[2] >> 32);
+  return val0 ^ val1;
+}
+#else
+static_always_inline
+u32 flowhash_hash_24_16(flowhash_lkey_24_16_t *k)
+{
+  u64 val = 0;
+  val ^= k->as_u64[0];
+  val ^= k->as_u64[1];
+  val ^= k->as_u64[2];
+  return (u32)clib_xxhash (val);
+}
+#endif
+
+static_always_inline
+u8 flowhash_cmp_key_24_16(flowhash_skey_24_16_t *a,
+                          flowhash_lkey_24_16_t *b)
+{
+  u8 val = 0;
+  val |= (a->as_u64[0] != b->as_u64[0]);
+  val |= (a->as_u64[1] != b->as_u64[1]);
+  val |= (a->as_u64[2] != b->as_u64[2]);
+  return val;
+}
+
+static_always_inline
+void flowhash_cpy_key_24_16(flowhash_skey_24_16_t *dst,
+			    flowhash_lkey_24_16_t *src)
+{
+  dst->as_u64[0] = src->as_u64[0];
+  dst->as_u64[1] = src->as_u64[1];
+  dst->as_u64[2] = src->as_u64[2];
+}
+
+#endif /* SRC_VPPINFRA_FLOWHASH_24_16_H_ */
