ddb12e89f680f5e19b25c3440ccb9717ee514142
Damjan Marion <damarion@cisco.com>
2018-01-18 01:42:48 +0100

    tapv2: add GSO support
    Change-Id: I6bb1484d12afa37fdbf4c668a45d5636ceb1a132
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- src/vnet/devices/virtio/device.c
+++ src/vnet/devices/virtio/device.c
@@ -131,6 +131,8 @@ add_buffer_to_slot (vlib_main_t * vm, virtio_vring_t * vring, u32 bi,
   d = &vring->desc[next];
   vlib_buffer_t *b = vlib_get_buffer (vm, bi);
 
+  memset (vlib_buffer_get_current (b) - hdr_sz, 0, hdr_sz);
+
   if (PREDICT_TRUE ((b->flags & VLIB_BUFFER_NEXT_PRESENT) == 0))
     {
       d->addr = pointer_to_uword (vlib_buffer_get_current (b)) - hdr_sz;
