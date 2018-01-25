0c28a595854dd144d70b505a8ceb4ae05900201d
Florin Coras <fcoras@cisco.com>
2018-01-25 06:34:42 -0800

    session: move builtin apps to their own folder
    This consolidates builtin apps under session-apps folder. It also
    removes duplicate builtin echo server/client implementations.
    
    Change-Id: I75ed879399c5aa9b75b1eb38b33aedf69dd8df3f
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/vnet/session-apps/echo_server.c
+++ src/vnet/session-apps/echo_server.c
@@ -49,90 +49,89 @@ typedef struct
   u32 **rx_retries;
 
   vlib_main_t *vlib_main;
-} builtin_server_main_t;
+} echo_server_main_t;
 
-builtin_server_main_t builtin_server_main;
+echo_server_main_t echo_server_main;
 
 int
-builtin_session_accept_callback (stream_session_t * s)
+echo_server_session_accept_callback (stream_session_t * s)
 {
-  builtin_server_main_t *bsm = &builtin_server_main;
+  echo_server_main_t *esm = &echo_server_main;
 
-  bsm->vpp_queue[s->thread_index] =
+  esm->vpp_queue[s->thread_index] =
     session_manager_get_vpp_event_queue (s->thread_index);
   s->session_state = SESSION_STATE_READY;
-  bsm->byte_index = 0;
-  vec_validate (bsm->rx_retries[s->thread_index], s->session_index);
-  bsm->rx_retries[s->thread_index][s->session_index] = 0;
+  esm->byte_index = 0;
+  vec_validate (esm->rx_retries[s->thread_index], s->session_index);
+  esm->rx_retries[s->thread_index][s->session_index] = 0;
   return 0;
 }
 
 void
-builtin_session_disconnect_callback (stream_session_t * s)
+echo_server_session_disconnect_callback (stream_session_t * s)
 {
-  builtin_server_main_t *bsm = &builtin_server_main;
+  echo_server_main_t *esm = &echo_server_main;
   vnet_disconnect_args_t _a, *a = &_a;
 
   a->handle = session_handle (s);
-  a->app_index = bsm->app_index;
+  a->app_index = esm->app_index;
   vnet_disconnect_session (a);
 }
 
 void
-builtin_session_reset_callback (stream_session_t * s)
+echo_server_session_reset_callback (stream_session_t * s)
 {
   clib_warning ("Reset session %U", format_stream_session, s, 2);
   stream_session_cleanup (s);
 }
 
-
 int
-builtin_session_connected_callback (u32 app_index, u32 api_context,
-				    stream_session_t * s, u8 is_fail)
+echo_server_session_connected_callback (u32 app_index, u32 api_context,
+					stream_session_t * s, u8 is_fail)
 {
   clib_warning ("called...");
   return -1;
 }
 
 int
-builtin_add_segment_callback (u32 client_index, const ssvm_private_t * sp)
+echo_server_add_segment_callback (u32 client_index, const ssvm_private_t * sp)
 {
   clib_warning ("called...");
   return -1;
 }
 
 int
-builtin_redirect_connect_callback (u32 client_index, void *mp)
+echo_server_redirect_connect_callback (u32 client_index, void *mp)
 {
   clib_warning ("called...");
   return -1;
 }
 
 void
-test_bytes (builtin_server_main_t * bsm, int actual_transfer)
+test_bytes (echo_server_main_t * esm, int actual_transfer)
 {
   int i;
   u32 my_thread_id = vlib_get_thread_index ();
 
   for (i = 0; i < actual_transfer; i++)
     {
-      if (bsm->rx_buf[my_thread_id][i] != ((bsm->byte_index + i) & 0xff))
+      if (esm->rx_buf[my_thread_id][i] != ((esm->byte_index + i) & 0xff))
 	{
-	  clib_warning ("at %lld expected %d got %d", bsm->byte_index + i,
-			(bsm->byte_index + i) & 0xff,
-			bsm->rx_buf[my_thread_id][i]);
+	  clib_warning ("at %lld expected %d got %d", esm->byte_index + i,
+			(esm->byte_index + i) & 0xff,
+			esm->rx_buf[my_thread_id][i]);
 	}
     }
-  bsm->byte_index += actual_transfer;
+  esm->byte_index += actual_transfer;
 }
 
 /*
  * If no-echo, just read the data and be done with it
  */
 int
-builtin_server_rx_callback_no_echo (stream_session_t * s)
+echo_server_builtin_server_rx_callback_no_echo (stream_session_t * s)
 {
-  builtin_server_main_t *bsm = &builtin_server_main;
+  echo_server_main_t *esm = &echo_server_main;
   u32 my_thread_id = vlib_get_thread_index ();
   int actual_transfer;
   svm_fifo_t *rx_fifo;
@@ -142,20 +141,20 @@ builtin_server_rx_callback_no_echo (stream_session_t * s)
   do
     {
       actual_transfer =
-	svm_fifo_dequeue_nowait (rx_fifo, bsm->rcv_buffer_size,
-				 bsm->rx_buf[my_thread_id]);
+	svm_fifo_dequeue_nowait (rx_fifo, esm->rcv_buffer_size,
+				 esm->rx_buf[my_thread_id]);
     }
   while (actual_transfer > 0);
   return 0;
 }
 
 int
-builtin_server_rx_callback (stream_session_t * s)
+echo_server_rx_callback (stream_session_t * s)
 {
   u32 n_written, max_dequeue, max_enqueue, max_transfer;
   int actual_transfer;
   svm_fifo_t *tx_fifo, *rx_fifo;
-  builtin_server_main_t *bsm = &builtin_server_main;
+  echo_server_main_t *esm = &echo_server_main;
   session_fifo_event_t evt;
   u32 thread_index = vlib_get_thread_index ();
 
@@ -189,37 +188,37 @@ builtin_server_rx_callback (stream_session_t * s)
 	  evt.fifo = rx_fifo;
 	  evt.event_type = FIFO_EVENT_BUILTIN_RX;
 
-	  q = bsm->vpp_queue[thread_index];
+	  q = esm->vpp_queue[thread_index];
 	  if (PREDICT_FALSE (q->cursize == q->maxsize))
 	    clib_warning ("out of event queue space");
 	  else if (svm_queue_add (q, (u8 *) & evt, 0))
 	    clib_warning ("failed to enqueue self-tap");
 
-	  if (bsm->rx_retries[thread_index][s->session_index] == 500000)
+	  if (esm->rx_retries[thread_index][s->session_index] == 500000)
 	    {
 	      clib_warning ("session stuck: %U", format_stream_session, s, 2);
 	    }
-	  if (bsm->rx_retries[thread_index][s->session_index] < 500001)
-	    bsm->rx_retries[thread_index][s->session_index]++;
+	  if (esm->rx_retries[thread_index][s->session_index] < 500001)
+	    esm->rx_retries[thread_index][s->session_index]++;
 	}
 
       return 0;
     }
 
-  _vec_len (bsm->rx_buf[thread_index]) = max_transfer;
+  _vec_len (esm->rx_buf[thread_index]) = max_transfer;
 
   actual_transfer = svm_fifo_dequeue_nowait (rx_fifo, max_transfer,
-					     bsm->rx_buf[thread_index]);
+					     esm->rx_buf[thread_index]);
   ASSERT (actual_transfer == max_transfer);
 
-//  test_bytes (bsm, actual_transfer);
+//  test_bytes (esm, actual_transfer);
 
   /*
    * Echo back
    */
 
   n_written = svm_fifo_enqueue_nowait (tx_fifo, actual_transfer,
-				       bsm->rx_buf[thread_index]);
+				       esm->rx_buf[thread_index]);
 
   if (n_written != max_transfer)
     clib_warning ("short trout!");
@@ -230,7 +229,7 @@ builtin_server_rx_callback (stream_session_t * s)
       evt.fifo = tx_fifo;
       evt.event_type = FIFO_EVENT_APP_TX;
 
-      if (svm_queue_add (bsm->vpp_queue[s->thread_index],
+      if (svm_queue_add (esm->vpp_queue[s->thread_index],
 			 (u8 *) & evt, 0 /* do wait for mutex */ ))
 	clib_warning ("failed to enqueue tx evt");
     }
@@ -241,35 +240,35 @@ builtin_server_rx_callback (stream_session_t * s)
   return 0;
 }
 
-static session_cb_vft_t builtin_session_cb_vft = {
-  .session_accept_callback = builtin_session_accept_callback,
-  .session_disconnect_callback = builtin_session_disconnect_callback,
-  .session_connected_callback = builtin_session_connected_callback,
-  .add_segment_callback = builtin_add_segment_callback,
-  .redirect_connect_callback = builtin_redirect_connect_callback,
-  .builtin_server_rx_callback = builtin_server_rx_callback,
-  .session_reset_callback = builtin_session_reset_callback
+static session_cb_vft_t echo_server_session_cb_vft = {
+  .session_accept_callback = echo_server_session_accept_callback,
+  .session_disconnect_callback = echo_server_session_disconnect_callback,
+  .session_connected_callback = echo_server_session_connected_callback,
+  .add_segment_callback = echo_server_add_segment_callback,
+  .redirect_connect_callback = echo_server_redirect_connect_callback,
+  .builtin_server_rx_callback = echo_server_rx_callback,
+  .session_reset_callback = echo_server_session_reset_callback
 };
 
 /* Abuse VPP's input queue */
 static int
 create_api_loopback (vlib_main_t * vm)
 {
-  builtin_server_main_t *bsm = &builtin_server_main;
+  echo_server_main_t *esm = &echo_server_main;
   api_main_t *am = &api_main;
   vl_shmem_hdr_t *shmem_hdr;
 
   shmem_hdr = am->shmem_hdr;
-  bsm->vl_input_queue = shmem_hdr->vl_input_queue;
-  bsm->my_client_index =
-    vl_api_memclnt_create_internal ("tcp_test_server", bsm->vl_input_queue);
+  esm->vl_input_queue = shmem_hdr->vl_input_queue;
+  esm->my_client_index = vl_api_memclnt_create_internal ("echo_server",
+							 esm->vl_input_queue);
   return 0;
 }
 
 static int
-server_attach (u8 * appns_id, u64 appns_flags, u64 appns_secret)
+echo_server_attach (u8 * appns_id, u64 appns_flags, u64 appns_secret)
 {
-  builtin_server_main_t *bsm = &builtin_server_main;
+  echo_server_main_t *esm = &echo_server_main;
   u64 options[APP_OPTIONS_N_OPTIONS];
   vnet_app_attach_args_t _a, *a = &_a;
   u32 segment_size = 512 << 20;
@@ -277,25 +276,25 @@ server_attach (u8 * appns_id, u64 appns_flags, u64 appns_secret)
   memset (a, 0, sizeof (*a));
   memset (options, 0, sizeof (options));
 
-  if (bsm->no_echo)
-    builtin_session_cb_vft.builtin_server_rx_callback =
-      builtin_server_rx_callback_no_echo;
+  if (esm->no_echo)
+    echo_server_session_cb_vft.builtin_server_rx_callback =
+      echo_server_builtin_server_rx_callback_no_echo;
   else
-    builtin_session_cb_vft.builtin_server_rx_callback =
-      builtin_server_rx_callback;
+    echo_server_session_cb_vft.builtin_server_rx_callback =
+      echo_server_rx_callback;
 
-  if (bsm->private_segment_size)
-    segment_size = bsm->private_segment_size;
+  if (esm->private_segment_size)
+    segment_size = esm->private_segment_size;
 
-  a->api_client_index = bsm->my_client_index;
-  a->session_cb_vft = &builtin_session_cb_vft;
+  a->api_client_index = esm->my_client_index;
+  a->session_cb_vft = &echo_server_session_cb_vft;
   a->options = options;
   a->options[APP_OPTIONS_SEGMENT_SIZE] = segment_size;
-  a->options[APP_OPTIONS_RX_FIFO_SIZE] = bsm->fifo_size;
-  a->options[APP_OPTIONS_TX_FIFO_SIZE] = bsm->fifo_size;
-  a->options[APP_OPTIONS_PRIVATE_SEGMENT_COUNT] = bsm->private_segment_count;
+  a->options[APP_OPTIONS_RX_FIFO_SIZE] = esm->fifo_size;
+  a->options[APP_OPTIONS_TX_FIFO_SIZE] = esm->fifo_size;
+  a->options[APP_OPTIONS_PRIVATE_SEGMENT_COUNT] = esm->private_segment_count;
   a->options[APP_OPTIONS_PREALLOC_FIFO_PAIRS] =
-    bsm->prealloc_fifos ? bsm->prealloc_fifos : 1;
+    esm->prealloc_fifos ? esm->prealloc_fifos : 1;
 
   a->options[APP_OPTIONS_FLAGS] = APP_OPTIONS_FLAGS_IS_BUILTIN;
   if (appns_id)
@@ -310,31 +309,44 @@ server_attach (u8 * appns_id, u64 appns_flags, u64 appns_secret)
       clib_warning ("failed to attach server");
       return -1;
     }
-  bsm->app_index = a->app_index;
+  esm->app_index = a->app_index;
   return 0;
 }
 
 static int
-server_listen ()
+echo_server_detach (void)
 {
-  builtin_server_main_t *bsm = &builtin_server_main;
+  echo_server_main_t *esm = &echo_server_main;
+  vnet_app_detach_args_t _da, *da = &_da;
+  int rv;
+
+  da->app_index = esm->app_index;
+  rv = vnet_application_detach (da);
+  esm->app_index = ~0;
+  return rv;
+}
+
+static int
+echo_server_listen ()
+{
+  echo_server_main_t *esm = &echo_server_main;
   vnet_bind_args_t _a, *a = &_a;
   memset (a, 0, sizeof (*a));
-  a->app_index = bsm->app_index;
-  a->uri = bsm->server_uri;
+  a->app_index = esm->app_index;
+  a->uri = esm->server_uri;
   return vnet_bind_uri (a);
 }
 
 static int
-server_create (vlib_main_t * vm, u8 * appns_id, u64 appns_flags,
-	       u64 appns_secret)
+echo_server_create (vlib_main_t * vm, u8 * appns_id, u64 appns_flags,
+		    u64 appns_secret)
 {
-  builtin_server_main_t *bsm = &builtin_server_main;
+  echo_server_main_t *esm = &echo_server_main;
   vlib_thread_main_t *vtm = vlib_get_thread_main ();
   u32 num_threads;
   int i;
 
-  if (bsm->my_client_index == (u32) ~ 0)
+  if (esm->my_client_index == (u32) ~ 0)
     {
       if (create_api_loopback (vm))
 	{
@@ -344,55 +356,60 @@ server_create (vlib_main_t * vm, u8 * appns_id, u64 appns_flags,
     }
 
   num_threads = 1 /* main thread */  + vtm->n_threads;
-  vec_validate (builtin_server_main.vpp_queue, num_threads - 1);
-  vec_validate (bsm->rx_buf, num_threads - 1);
-  vec_validate (bsm->rx_retries, num_threads - 1);
+  vec_validate (echo_server_main.vpp_queue, num_threads - 1);
+  vec_validate (esm->rx_buf, num_threads - 1);
+  vec_validate (esm->rx_retries, num_threads - 1);
 
   for (i = 0; i < num_threads; i++)
-    vec_validate (bsm->rx_buf[i], bsm->rcv_buffer_size);
+    vec_validate (esm->rx_buf[i], esm->rcv_buffer_size);
 
-  if (server_attach (appns_id, appns_flags, appns_secret))
+  if (echo_server_attach (appns_id, appns_flags, appns_secret))
     {
       clib_warning ("failed to attach server");
       return -1;
     }
-  if (server_listen ())
+  if (echo_server_listen ())
     {
       clib_warning ("failed to start listening");
+      if (echo_server_detach ())
+	clib_warning ("failed to detach");
       return -1;
     }
   return 0;
 }
 
 static clib_error_t *
-server_create_command_fn (vlib_main_t * vm, unformat_input_t * input,
-			  vlib_cli_command_t * cmd)
+echo_server_create_command_fn (vlib_main_t * vm, unformat_input_t * input,
+			       vlib_cli_command_t * cmd)
 {
-  builtin_server_main_t *bsm = &builtin_server_main;
+  echo_server_main_t *esm = &echo_server_main;
   u8 server_uri_set = 0, *appns_id = 0;
   u64 tmp, appns_flags = 0, appns_secret = 0;
+  char *default_uri = "tcp://0.0.0.0/1234";
   int rv;
 
-  bsm->no_echo = 0;
-  bsm->fifo_size = 64 << 10;
-  bsm->rcv_buffer_size = 128 << 10;
-  bsm->prealloc_fifos = 0;
-  bsm->private_segment_count = 0;
-  bsm->private_segment_size = 0;
-  vec_free (bsm->server_uri);
+  esm->no_echo = 0;
+  esm->fifo_size = 64 << 10;
+  esm->rcv_buffer_size = 128 << 10;
+  esm->prealloc_fifos = 0;
+  esm->private_segment_count = 0;
+  esm->private_segment_size = 0;
+  vec_free (esm->server_uri);
 
   while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
     {
-      if (unformat (input, "no-echo"))
-	bsm->no_echo = 1;
-      else if (unformat (input, "fifo-size %d", &bsm->fifo_size))
-	bsm->fifo_size <<= 10;
-      else if (unformat (input, "rcv-buf-size %d", &bsm->rcv_buffer_size))
+      if (unformat (input, "uri %s", &esm->server_uri))
+	server_uri_set = 1;
+      else if (unformat (input, "no-echo"))
+	esm->no_echo = 1;
+      else if (unformat (input, "fifo-size %d", &esm->fifo_size))
+	esm->fifo_size <<= 10;
+      else if (unformat (input, "rcv-buf-size %d", &esm->rcv_buffer_size))
 	;
-      else if (unformat (input, "prealloc-fifos %d", &bsm->prealloc_fifos))
+      else if (unformat (input, "prealloc-fifos %d", &esm->prealloc_fifos))
 	;
       else if (unformat (input, "private-segment-count %d",
-			 &bsm->private_segment_count))
+			 &esm->private_segment_count))
 	;
       else if (unformat (input, "private-segment-size %U",
 			 unformat_memory_size, &tmp))
@@ -400,10 +417,8 @@ server_create_command_fn (vlib_main_t * vm, unformat_input_t * input,
 	  if (tmp >= 0x100000000ULL)
 	    return clib_error_return
 	      (0, "private segment size %lld (%llu) too large", tmp, tmp);
-	  bsm->private_segment_size = tmp;
+	  esm->private_segment_size = tmp;
 	}
-      else if (unformat (input, "uri %s", &bsm->server_uri))
-	server_uri_set = 1;
       else if (unformat (input, "appns %_%v%_", &appns_id))
 	;
       else if (unformat (input, "all-scope"))
@@ -416,49 +431,50 @@ server_create_command_fn (vlib_main_t * vm, unformat_input_t * input,
       else if (unformat (input, "secret %lu", &appns_secret))
 	;
       else
-	return clib_error_return (0, "unknown input `%U'",
+	return clib_error_return (0, "failed: unknown input `%U'",
 				  format_unformat_error, input);
     }
 
   vnet_session_enable_disable (vm, 1 /* turn on TCP, etc. */ );
 
   if (!server_uri_set)
-    bsm->server_uri = (char *) format (0, "tcp://0.0.0.0/1234%c", 0);
-
-  rv = server_create (vm, appns_id, appns_flags, appns_secret);
-  vec_free (appns_id);
-  switch (rv)
     {
-    case 0:
-      break;
-    default:
-      return clib_error_return (0, "server_create returned %d", rv);
+      clib_warning ("No uri provided! Using default: %s", default_uri);
+      esm->server_uri = (char *) format (0, "%s%c", default_uri, 0);
+    }
+
+  rv = echo_server_create (vm, appns_id, appns_flags, appns_secret);
+  vec_free (appns_id);
+  if (rv)
+    {
+      vec_free (esm->server_uri);
+      return clib_error_return (0, "failed: server_create returned %d", rv);
     }
 
   return 0;
 }
 
 /* *INDENT-OFF* */
-VLIB_CLI_COMMAND (server_create_command, static) =
+VLIB_CLI_COMMAND (echo_server_create_command, static) =
 {
-  .path = "test tcp server",
-  .short_help = "test tcp server [no echo][fifo-size <mbytes>] "
+  .path = "test echo server",
+  .short_help = "test echo server proto <proto> [no echo][fifo-size <mbytes>]"
       "[rcv-buf-size <bytes>][prealloc-fifos <count>]"
       "[private-segment-count <count>][private-segment-size <bytes[m|g]>]"
       "[uri <tcp://ip/port>]",
-  .function = server_create_command_fn,
+  .function = echo_server_create_command_fn,
 };
 /* *INDENT-ON* */
 
 clib_error_t *
-builtin_tcp_server_main_init (vlib_main_t * vm)
+echo_server_main_init (vlib_main_t * vm)
 {
-  builtin_server_main_t *bsm = &builtin_server_main;
-  bsm->my_client_index = ~0;
+  echo_server_main_t *esm = &echo_server_main;
+  esm->my_client_index = ~0;
   return 0;
 }
 
-VLIB_INIT_FUNCTION (builtin_tcp_server_main_init);
+VLIB_INIT_FUNCTION (echo_server_main_init);
 
 /*
 * fd.io coding-style-patch-verification: ON
