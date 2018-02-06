79a88fc91f78ca2848f7a59e5358172cee099703
Damjan Marion <damarion@cisco.com>
2018-02-06 19:01:28 +0100

    Fix clang implicit conversion errors
    Change-Id: I1771a1cca2a4bc394677b2a18f14c47f0633fa77
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- src/vnet/ethernet/node.c
+++ src/vnet/ethernet/node.c
@@ -289,14 +289,15 @@ static_always_inline int
 ethernet_frame_is_any_tagged (u16 type0, u16 type1)
 {
 #if __SSE4_2__
-  const __m128i ethertype_mask = _mm_set_epi16 (ETHERNET_TYPE_VLAN,
-						ETHERNET_TYPE_DOT1AD,
-						ETHERNET_TYPE_VLAN_9100,
-						ETHERNET_TYPE_VLAN_9200,
+  const __m128i ethertype_mask = _mm_set_epi16 ((u16) ETHERNET_TYPE_VLAN,
+						(u16) ETHERNET_TYPE_DOT1AD,
+						(u16) ETHERNET_TYPE_VLAN_9100,
+						(u16) ETHERNET_TYPE_VLAN_9200,
 						/* duplicate for type1 */
-						ETHERNET_TYPE_VLAN,
-						ETHERNET_TYPE_DOT1AD,
-						ETHERNET_TYPE_VLAN_9100,
+						(u16) ETHERNET_TYPE_VLAN,
+						(u16) ETHERNET_TYPE_DOT1AD,
+						(u16) ETHERNET_TYPE_VLAN_9100,
+						(u16)
 						ETHERNET_TYPE_VLAN_9200);
 
   __m128i r =
