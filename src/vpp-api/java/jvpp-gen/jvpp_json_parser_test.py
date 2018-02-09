43647af179c2921d5370d490e6b40b1946a44fa3
Marek Gradzki <mgradzki@cisco.com>
2018-02-09 13:42:12 +0100

    JVPP API GEN 2
    Change-Id: Ic58f642e2941182b10fd6b151ab4e567a461518c
    Signed-off-by: Marek Gradzki <mgradzki@cisco.com>

---
+++ src/vpp-api/java/jvpp-gen/jvpp_json_parser_test.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python2
+#
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import logging
+import sys
+import unittest
+
+from jvpp_json_parser import JsonParser, Dump, Request, Reply, Details
+
+
+# Generating json file from api file:
+# python ~/vpp/src/tools/vppapigen/vppapigen.py --input test.api --pluginpath ~/vpp/src/tools/vppapigen/
+# --output test.api.json JSON
+#
+
+class JsonParserTest(unittest.TestCase):
+    def setUp(self):
+        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+        self.logger = logging.getLogger("JVPP PARSER TEST")
+
+    def testParse(self):
+        # todo update test to include fields and types
+        jsonparser = JsonParser(self.logger, ["test.api.json"], "core")
+        m1 = Request("foo_request", "foo_request_reply", "0x4199095a", [])
+        m2 = Reply("foo_request_reply", "foo_request", "0xe8d4e804", [])
+        m3 = Dump("foo_dump", "foo_details", "0x8ac76db6", [])
+        m4 = Details("foo_details", "foo_dump", "0xe18f0c91", [])
+
+        self.assertMessage(m1, jsonparser.messages)
+        self.assertMessage(m2, jsonparser.messages)
+        self.assertMessage(m3, jsonparser.messages)
+        self.assertMessage(m4, jsonparser.messages)
+
+    def assertMessage(self, msg, messages):
+        self.assertIn(msg, messages, "%s was not found in [%s]" % (msg, " ".join([str(x) for x in messages])))
+
+
+if __name__ == '__main__':
+    unittest.main()
