3081564d2b82d320989b4d047f5716129b45eec6
Pablo Camarillo <pcamaril@cisco.com>
2018-02-15 00:34:59 +0100

    SRv6 to GTP mapping
    Change-Id: Id458181d6f96b0f7584c6fcf5ab4e899197de24e
    Signed-off-by: Pablo Camarillo <pcamaril@cisco.com>
    Signed-off-by: pcamaril <pcamaril@cisco.com>

--- src/plugins/srv6-am/am.c
+++ src/plugins/srv6-am/am.c
@@ -211,6 +211,7 @@ srv6_am_init (vlib_main_t * vm)
 				      keyword_str,
 				      def_str,
 				      params_str,
+              128,
 				      &sm->srv6_am_dpo_type,
 				      format_srv6_am_localsid,
 				      unformat_srv6_am_localsid,
