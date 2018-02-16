b5a86eed8bd889f5c8986e5fa569a50b997ccd77
Dave Wallace <dwallacelf@gmail.com>
2018-02-16 18:26:11 -0500

    VCL: config api prefix using env var.
    - Configure vpp api prefix using VCL_API_PREFIX
      environment variable.
    - Prepend api prefix to vpp api filename when
      connecting to vpp.
    - Fix stale vcl_cfg ptr after heap allocation.
    - Cleanup misleading warning messages wrt.
      reading the vcl config file.
    
    Change-Id: I908c9b567ff4f6f0c21ae43a4627a1a3202a3290
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- test/scripts/socket_test.sh
+++ test/scripts/socket_test.sh
@@ -520,6 +520,9 @@ write_script_header() {
     if [ -n "$VCL_CONFIG" ] ; then
         echo "export VCL_CONFIG=${vcl_config_dir}${vcl_config}" >> $1
     fi
+    if [ -n "$VCL_API_PREFIX" ] ; then
+        echo "export VCL_API_PREFIX=$VCL_API_PREFIX" >> $1
+    fi
     if [ -n "$VCL_DEBUG" ] ; then
         echo "export VCL_DEBUG=$VCL_DEBUG" >> $1
     fi
