d0d8f3ae2cda96acd938ae1961ea61299af101a8
Dave Wallace <dwallacelf@gmail.com>
2018-02-20 12:39:37 -0500

    make test: Add VCL thru hoststack test case.
    Change-Id: I7deb1fa4624cc1b7d7bc2f68034d4ce7589d4ef1
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- test/test_vcl.py
+++ test/test_vcl.py
@@ -9,22 +9,28 @@ from threading import Thread
 from log import single_line_delim
 from framework import VppTestCase, running_extended_tests, \
     running_on_centos, VppTestRunner, Worker
+from vpp_ip_route import VppIpTable, VppIpRoute, VppRoutePath
 
 
-class VCLTestCase(VppTestCase):
-    """ VPP Communications Library Test """
+class VCLCUTTHRUTestCase(VppTestCase):
+    """ VCL Cut Thru Test """
 
     server_addr = "127.0.0.1"
     server_port = "22000"
 
     @classmethod
     def setUpClass(cls):
-        super(VCLTestCase, cls).setUpClass()
-
-        cls.vapi.session_enable_disable(is_enabled=1)
+        super(VCLCUTTHRUTestCase, cls).setUpClass()
 
     def setUp(self):
-        super(VCLTestCase, self).setUp()
+        super(VCLCUTTHRUTestCase, self).setUp()
+
+        self.vapi.session_enable_disable(is_enabled=1)
+
+    def tearDown(self):
+        self.vapi.session_enable_disable(is_enabled=0)
+
+        super(VCLCUTTHRUTestCase, self).tearDown()
 
     def test_vcl_cutthru(self):
         """ run VCL cut-thru test """
@@ -37,7 +43,6 @@ class VCLTestCase(VppTestCase):
         executable = "%s/vcl_test_server" % vcl_exe_dir
         worker_server = Worker([executable, self.server_port], self.logger)
         worker_server.env["VCL_API_PREFIX"] = self.shm_prefix
-#        worker_server.env["VCL_DEBUG"] = "2"
         worker_server.env["VCL_APP_SCOPE_LOCAL"] = "true"
         worker_server.start()
         executable = "%s/vcl_test_client" % vcl_exe_dir
@@ -45,7 +50,6 @@ class VCLTestCase(VppTestCase):
             [executable, self.server_addr, self.server_port,
              "-E", "Hello, world!", "-X"], self.logger)
         worker_client.env["VCL_API_PREFIX"] = self.shm_prefix
-#        worker_client.env["VCL_DEBUG"] = "2"
         worker_client.env["VCL_APP_SCOPE_LOCAL"] = "true"
         worker_client.start()
         worker_client.join(timeout)
@@ -68,6 +72,103 @@ class VCLTestCase(VppTestCase):
                 "Timeout! Client worker did not finish in %ss" % timeout)
         self.assert_equal(worker_client.result, 0, "Binary test return code")
 
+
+class VCLTHRUHSTestcase(VppTestCase):
+    """ VCL Thru Hoststack Test """
+
+    server_port = "22000"
+
+    @classmethod
+    def setUpClass(cls):
+        super(VCLTHRUHSTestcase, cls).setUpClass()
+
+    def setUp(self):
+        super(VCLTHRUHSTestcase, self).setUp()
+
+        self.vapi.session_enable_disable(is_enabled=1)
+        self.create_loopback_interfaces(range(2))
+
+        table_id = 0
+
+        for i in self.lo_interfaces:
+            i.admin_up()
+
+            if table_id != 0:
+                tbl = VppIpTable(self, table_id)
+                tbl.add_vpp_config()
+
+            i.set_table_ip4(table_id)
+            i.config_ip4()
+            table_id += 1
+
+        # Configure namespaces
+        self.vapi.app_namespace_add(namespace_id="0",
+                                    sw_if_index=self.loop0.sw_if_index)
+        self.vapi.app_namespace_add(namespace_id="1",
+                                    sw_if_index=self.loop1.sw_if_index)
+
+    def tearDown(self):
+        for i in self.lo_interfaces:
+            i.unconfig_ip4()
+            i.set_table_ip4(0)
+            i.admin_down()
+
+        self.vapi.session_enable_disable(is_enabled=0)
+        super(VCLTHRUHSTestcase, self).tearDown()
+
+    def test_vcl_thru_hoststack(self):
+        """ run VCL thru hoststack test """
+        # Add inter-table routes
+        ip_t01 = VppIpRoute(self, self.loop1.local_ip4, 32,
+                            [VppRoutePath("0.0.0.0",
+                                          0xffffffff,
+                                          nh_table_id=1)])
+        ip_t10 = VppIpRoute(self, self.loop0.local_ip4, 32,
+                            [VppRoutePath("0.0.0.0",
+                                          0xffffffff,
+                                          nh_table_id=0)], table_id=1)
+        ip_t01.add_vpp_config()
+        ip_t10.add_vpp_config()
+
+        timeout = 20
+        var = "VPP_TEST_BUILD_DIR"
+        build_dir = os.getenv(var, None)
+        self.assertIsNotNone(build_dir,
+                             "Environment variable `%s' not set" % var)
+        vcl_exe_dir = "%s/vpp/.libs" % build_dir
+        executable = "%s/vcl_test_server" % vcl_exe_dir
+        worker_server = Worker([executable, self.server_port], self.logger)
+        worker_server.env["VCL_API_PREFIX"] = self.shm_prefix
+        worker_server.env["VCL_APP_SCOPE_GLOBAL"] = "true"
+        worker_server.env["VCL_APP_NAMESPACE_ID"] = "0"
+        worker_server.start()
+        executable = "%s/vcl_test_client" % vcl_exe_dir
+        worker_client = Worker(
+            [executable, self.loop0.local_ip4, self.server_port,
+             "-E", "Hello, world!", "-X"], self.logger)
+        worker_client.env["VCL_API_PREFIX"] = self.shm_prefix
+        worker_client.env["VCL_APP_SCOPE_GLOBAL"] = "true"
+        worker_client.env["VCL_APP_NAMESPACE_ID"] = "1"
+        worker_client.start()
+        worker_client.join(timeout)
+        self.logger.info("Client worker result is `%s'" % worker_client.result)
+        error = False
+        if worker_client.result is None:
+            try:
+                error = True
+                self.logger.error(
+                    "Timeout! Client worker did not finish in %ss" % timeout)
+                os.killpg(os.getpgid(worker_client.process.pid),
+                          signal.SIGTERM)
+                worker_client.join()
+            except:
+                raise Exception("Couldn't kill client worker-spawned process")
+        if error:
+            os.killpg(os.getpgid(worker_server.process.pid), signal.SIGTERM)
+            worker_server.join()
+            raise Exception(
+                "Timeout! Client worker did not finish in %ss" % timeout)
+        self.assert_equal(worker_client.result, 0, "Binary test return code")
 
 if __name__ == '__main__':
     unittest.main(testRunner=VppTestRunner)
