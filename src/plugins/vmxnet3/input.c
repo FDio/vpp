0eac18c7fe72caaeb94b020dc0b463491e47702e
Damjan Marion <damarion@cisco.com>
2018-03-18 08:01:27 -0700

    vmxnet device driver
    - multi-versioning
    - binary API
    
    DBGvpp# sh int
    sh int
                  Name               Idx    State  MTU (L3/IP4/IP6/MPLS)     Counter          Count
    local0                            0     down          0/0/0/0
    DBGvpp# binary-api vmxnet3_create 0000:0b:00.0 rx-queue-size 128 tx-queue-size 128
    binary-api vmxnet3_create 0000:0b:00.0 rx-queue-size 128 tx-queue-size 128
    0: created vmxnet3 with sw_if_index 1
    DBGvpp# set interface ip address VMXNET30/b/0/0 10.1.1.1/24
    set interface ip address VMXNET30/b/0/0 10.1.1.1/24
    DBGvpp#
    DBGvpp#
    DBGvpp# set interface state VMXNET30/b/0/0 up
    set interface state VMXNET30/b/0/0 up
    DBGvpp# sh int
    sh int
                  Name               Idx    State  MTU (L3/IP4/IP6/MPLS)     Counter          Count
    VMXNET30/b/0/0                    1      up          9000/0/0/0     rx packets                     2
                                                                        rx bytes                     120
                                                                        drops                          2
                                                                        ip4                            2
    local0                            0     down          0/0/0/0
    DBGvpp# ping 10.1.1.2
    ping 10.1.1.2
    64 bytes from 10.1.1.2: icmp_seq=2 ttl=64 time=.1556 ms
    64 bytes from 10.1.1.2: icmp_seq=3 ttl=64 time=.1410 ms
    64 bytes from 10.1.1.2: icmp_seq=4 ttl=64 time=.0931 ms
    64 bytes from 10.1.1.2: icmp_seq=5 ttl=64 time=.0929 ms
    
    Statistics: 5 sent, 4 received, 20% packet loss
    DBGvpp# binary-api vmxnet3_dump
    binary-api vmxnet3_dump
    0: Sending ping id=-1
    0: vl_msg_api_handler_with_vm_node:514: no handler for msg id 65535
    0: VMXNET30/b/0/0: sw_if_index 1 mac 00:50:56:99:73:88
       version: 1
       PCI Address: 0000:0b:00.0
       RX Queue 0
        size 128 fill 120 consume 8 produce 128
       TX Queue 0
        size 128 consume 5 produce 6
       state up
    binary-api: vmxnet3_dump error: Misc
    DBGvpp# binary-api vmxnet3_delete sw_if_index 1
    binary-api vmxnet3_delete sw_if_index 1
    DBGvpp# sh int
    sh int
                  Name               Idx    State  MTU (L3/IP4/IP6/MPLS)     Counter          Count
    local0                            0     down          0/0/0/0
    DBGvpp#
    
    Change-Id: I85fbc86f2d8532b017bc4271612d17e24e498e4d
    Signed-off-by: Damjan Marion <damarion@cisco.com>

---
+++ src/plugins/vmxnet3/input.c
@@ -0,0 +1,183 @@
+/*
+ *------------------------------------------------------------------
+ * Copyright (c) 2018 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *------------------------------------------------------------------
+ */
+
+#include <vlib/vlib.h>
+#include <vlib/unix/unix.h>
+#include <vlib/pci/pci.h>
+#include <vnet/ethernet/ethernet.h>
+#include <vnet/devices/devices.h>
+
+#include <vmxnet3/vmxnet3.h>
+
+#define foreach_vmxnet3_input_error \
+  _(BUFFER_ALLOC, "buffer alloc error") \
+  _(RX_PACKET_ERROR, "Rx packet errors")
+
+typedef enum
+{
+#define _(f,s) VMXNET3_INPUT_ERROR_##f,
+  foreach_vmxnet3_input_error
+#undef _
+    VMXNET3_INPUT_N_ERROR,
+} vmxnet3_input_error_t;
+
+static __clib_unused char *vmxnet3_input_error_strings[] = {
+#define _(n,s) s,
+  foreach_vmxnet3_input_error
+#undef _
+};
+
+static_always_inline uword
+vmxnet3_device_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
+			     vlib_frame_t * frame, vmxnet3_device_t * vd,
+			     u16 qid)
+{
+  vnet_main_t *vnm = vnet_get_main ();
+  uword n_trace = vlib_get_trace_count (vm, node);
+  u32 n_rx_packets = 0, n_rx_bytes = 0;
+  vmxnet3_rx_comp *rx_comp;
+  u32 comp_idx;
+  u32 desc_idx;
+  u32 generation;
+  vmxnet3_rxq_t *rxq;
+  u16 mask;
+  u32 thr_idx = vlib_get_thread_index ();
+  u32 buffer_indices[VLIB_FRAME_SIZE], *bi;
+  u16 nexts[VLIB_FRAME_SIZE], *next;
+
+  rxq = vec_elt_at_index (vd->rxqs, qid);
+  mask = rxq->size - 1;
+  bi = buffer_indices;
+  next = nexts;
+  while (1)
+    {
+      vlib_buffer_t *b0;
+
+      comp_idx = rxq->consume & mask;
+      if (rxq->consume & rxq->size)
+	generation = 0;
+      else
+	generation = VMXNET3_RXCF_GEN;
+      rx_comp = &rxq->rx_comp[comp_idx];
+      if (generation != (rx_comp->flags & VMXNET3_RXCF_GEN))
+	break;
+
+      rxq->consume++;
+      desc_idx = rx_comp->index & mask;
+      rxq->fill--;
+
+      b0 = vlib_get_buffer (vm, rxq->bufs[desc_idx]);
+      vnet_buffer (b0)->sw_if_index[VLIB_RX] = vd->sw_if_index;
+      vnet_buffer (b0)->sw_if_index[VLIB_TX] = (u32) ~0;
+      b0->current_length = rx_comp->len;
+      b0->flags = VLIB_BUFFER_TOTAL_LENGTH_VALID;
+
+      bi[0] = rxq->bufs[desc_idx];
+      bi++;
+
+      // FIXME
+      // next maybe VNET_DEVICE_INPUT_NEXT_IP6_INPUT or
+      // VNET_DEVICE_INPUT_NEXT_IP4_NCS_INPUT
+      next[0] = VNET_DEVICE_INPUT_NEXT_ETHERNET_INPUT;
+      next++;
+
+      n_rx_packets++;
+      n_rx_bytes += rx_comp->len;
+    }
+
+  if (PREDICT_FALSE ((n_trace = vlib_get_trace_count (vm, node))))
+    {
+      u32 n_left = n_rx_packets;
+
+      bi = buffer_indices;
+      next = nexts;
+      while (n_trace && n_left)
+	{
+	  vlib_buffer_t *b;
+	  vmxnet3_input_trace_t *tr;
+
+	  b = vlib_get_buffer (vm, bi[0]);
+	  vlib_trace_buffer (vm, node, next[0], b, /* follow_chain */ 0);
+	  tr = vlib_add_trace (vm, node, b, sizeof (*tr));
+	  tr->next_index = next[0];
+	  tr->hw_if_index = vd->hw_if_index;
+
+	  n_trace--;
+	  n_left--;
+	  bi++;
+	  next++;
+	}
+      vlib_set_trace_count (vm, node, n_trace);
+    }
+
+  if (PREDICT_TRUE (n_rx_packets))
+    {
+      vlib_buffer_enqueue_to_next (vm, node, buffer_indices, nexts,
+				   n_rx_packets);
+      vlib_increment_combined_counter
+	(vnm->interface_main.combined_sw_if_counters +
+	 VNET_INTERFACE_COUNTER_RX, thr_idx,
+	 vd->hw_if_index, n_rx_packets, n_rx_bytes);
+    }
+
+  vmxnet3_rxq_refill (vm, vd, rxq);
+
+  return n_rx_packets;
+}
+
+VLIB_NODE_FN (vmxnet3_input_node) (vlib_main_t * vm,
+                                   vlib_node_runtime_t * node,
+				   vlib_frame_t * frame)
+{
+  u32 n_rx = 0;
+  vmxnet3_main_t *vmxm = &vmxnet3_main;
+  vnet_device_input_runtime_t *rt = (void *) node->runtime_data;
+  vnet_device_and_queue_t *dq;
+
+  foreach_device_and_queue (dq, rt->devices_and_queues)
+  {
+    vmxnet3_device_t *vd;
+    vd = vec_elt_at_index (vmxm->devices, dq->dev_instance);
+    if ((vd->flags & VMXNET3_DEVICE_F_ADMIN_UP) == 0)
+      continue;
+    n_rx += vmxnet3_device_input_inline (vm, node, frame, vd, dq->queue_id);
+  }
+  return n_rx;
+}
+
+#ifndef CLIB_MARCH_VARIANT
+/* *INDENT-OFF* */
+VLIB_REGISTER_NODE (vmxnet3_input_node) = {
+  .name = "vmxnet3-input",
+  .sibling_of = "device-input",
+  .format_trace = format_vmxnet3_input_trace,
+  .type = VLIB_NODE_TYPE_INPUT,
+  .state = VLIB_NODE_STATE_DISABLED,
+  .n_errors = VMXNET3_INPUT_N_ERROR,
+  .error_strings = vmxnet3_input_error_strings,
+};
+#endif
+
+/* *INDENT-ON* */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
