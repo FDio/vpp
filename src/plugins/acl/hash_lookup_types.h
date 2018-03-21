555f1efed74e4ce0b1410c3ac2963445f108e8cf
Andrew Yourtchenko <ayourtch@gmail.com>
2018-03-21 19:53:39 +0100

    acl-plugin: implement ACL lookup contexts for "ACL as a service" use by other plugins
    This code implements the functionality required for other plugins wishing
    to perform ACL lookups in the contexts of their choice, rather than only
    in the context of the interface in/out.
    
    The lookups are the stateless ACLs - there is no concept of "direction"
    within the context, hence no concept of "connection" either.
    
    The plugins need to include the
    
    The file acl_lookup_context.md has more info.
    
    Change-Id: I91ba97428cc92b24d1517e808dc2fd8e56ea2f8d
    Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>

--- src/plugins/acl/hash_lookup_types.h
+++ src/plugins/acl/hash_lookup_types.h
@@ -38,9 +38,8 @@ typedef struct {
 typedef struct {
   /* The mask types present in this ACL */
   uword *mask_type_index_bitmap;
-  /* hash ACL applied on these interfaces */
-  u32 *inbound_sw_if_index_list;
-  u32 *outbound_sw_if_index_list;
+  /* hash ACL applied on these lookup contexts */
+  u32 *lc_index_list;
   hash_ace_info_t *rules;
 } hash_acl_info_t;
 
@@ -68,6 +67,10 @@ typedef struct {
    * number of hits on this entry
    */
   u64 hitcount;
+  /*
+   * acl position in vector of ACLs within lookup context
+   */
+  u32 acl_position;
   /*
    * Action of this applied ACE
    */
