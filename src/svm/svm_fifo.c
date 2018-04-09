3ac1d3ebb5e417df49a30441b7b2689a55039a48
Florin Coras <fcoras@cisco.com>
2018-04-09 09:24:52 -0700

    udp: refactor
    Change-Id: I2960c0d2d246cb166005f545794ec31fe0d546dd
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/svm/svm_fifo.c
+++ src/svm/svm_fifo.c
@@ -459,7 +459,7 @@ svm_fifo_enqueue_internal (svm_fifo_t * f, u32 max_bytes,
   f->ooos_newest = OOO_SEGMENT_INVALID_INDEX;
 
   if (PREDICT_FALSE (cursize == f->nitems))
-    return -2;			/* fifo stuffed */
+    return SVM_FIFO_FULL;
 
   nitems = f->nitems;
 
@@ -615,6 +615,20 @@ svm_fifo_enqueue_with_offset (svm_fifo_t * f,
 						copy_from_here);
 }
 
+void
+svm_fifo_overwrite_head (svm_fifo_t *f, u8 *data, u32 len)
+{
+  u32 first_chunk;
+  ASSERT (len <= f->nitems);
+  if (len < f->nitems - f->head)
+    clib_memcpy (&f->data[f->head], data, len);
+  else
+    {
+      first_chunk = len - (f->nitems - f->head);
+      clib_memcpy (&f->data[f->head], data, first_chunk);
+      clib_memcpy (f->data, data + first_chunk, len - first_chunk);
+    }
+}
 
 static int
 svm_fifo_dequeue_internal (svm_fifo_t * f, u32 max_bytes, u8 * copy_here)
