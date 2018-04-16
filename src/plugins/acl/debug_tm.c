2001b1b6361f8722ea04e95849e9ce07202e83d4
Andrew Yourtchenko <ayourtch@gmail.com>
2018-04-16 11:28:40 +0200

    TEST: add the tm matching to ACLs
    First version of integrating Valerio's code implementing TupleMerge
    
    Change-Id: I3d7e2585cb88d4037fb5771ef52c4c0f825733e8
    Signed-off-by: Valerio Bruschi <valerio.bruschi@students.uniroma2.eu>
    Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>

---
+++ src/plugins/acl/debug_tm.c
@@ -0,0 +1,276 @@
+/*
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <stddef.h>
+#include <netinet/in.h>
+
+#include <vlibapi/api.h>
+#include <vlibmemory/api.h>
+#include <vlibsocket/api.h>
+
+#include <vlib/vlib.h>
+#include <vnet/vnet.h>
+#include <vnet/pg/pg.h>
+#include <vppinfra/error.h>
+#include <vnet/plugin/plugin.h>
+#include <acl/acl.h>
+
+#include "hash_lookup.h"
+
+
+
+
+
+
+clib_error_t*
+acl_describe_partition (vlib_main_t * vm,
+                                   unformat_input_t * i,
+                                   vlib_cli_command_t * cmd){
+
+    clib_error_t *error = 0;
+    acl_main_t * sm = &acl_main;
+    u32 sw_if_index = ~0;
+    u32 acl_index = ~0;
+    u32 *inacls = 0;
+    u32 *outacls = 0;
+    u8 is_input = 0;
+    u8 verbose = 0;
+
+//  acl_interface_set_acl_list <intfc> | sw_if_index <if-idx> input [acl-idx list] output [acl-idx list]
+
+    /* Parse args required to build the message */
+    while (unformat_check_input (i) != UNFORMAT_END_OF_INPUT) {
+        if (unformat (i, "sw_if_index %d", &sw_if_index))
+            ;
+        else if (unformat (i, "%d", &acl_index))
+          {
+            if(is_input)
+              vec_add1(inacls, htonl(acl_index));
+            else
+              vec_add1(outacls, htonl(acl_index));
+          }
+        else if (unformat (i, "acl %d", &acl_index))
+            ;
+        else if (unformat (i, "input"))
+            is_input = 1;
+        else if (unformat (i, "output"))
+            is_input = 0;
+        else if (unformat (i, "verbose"))
+            verbose = 1;
+        else
+            break;
+    }
+
+    if (sw_if_index == ~0) {
+	    return error = clib_error_return(0, "missing interface name / explicit sw_if_index number \n");
+    }
+
+    hash_applied_mask_info_t **hash_applied_mask_pool = is_input ? vec_elt_at_index(sm->input_hash_applied_mask_pool_by_sw_if_index, sw_if_index) :
+	    vec_elt_at_index(sm->output_hash_applied_mask_pool_by_sw_if_index,  sw_if_index);
+
+
+    ace_mask_type_entry_t *mte;
+
+    u32 order_index=0;
+    u32 priority=0;
+    u32 mask_type_index=0;
+    vlib_cli_output(vm, "mask type: 'id' (best_priority) - max collisions hosted, number of entries");
+
+    for(order_index = 0; order_index < pool_len((*hash_applied_mask_pool)); order_index++) {
+	    hash_applied_mask_info_t *minfo = vec_elt_at_index((*hash_applied_mask_pool), order_index);
+	    mask_type_index = minfo->mask_type_index;
+
+	    mte = vec_elt_at_index(sm->ace_mask_type_pool, mask_type_index);
+	    priority = minfo->max_priority;
+
+	    u32 max_collisions = minfo->max_collisions;
+	    u32 num_entries = minfo->num_entries;
+
+	    if(verbose){
+		    vlib_cli_output(vm, "Mask type: %d (%d)", mask_type_index, priority);
+		    print_mask(&mte->mask);
+	    }
+
+	    vlib_cli_output(vm, "Mask type: %d (%d) - %d %d", mask_type_index, priority, max_collisions, num_entries);
+    }
+
+
+
+    return error;
+}
+
+
+
+
+
+clib_error_t*
+acl_show_collision (vlib_main_t * vm,
+                                   unformat_input_t * i,
+                                   vlib_cli_command_t * cmd){
+
+    clib_error_t *error = 0;
+    acl_main_t * sm = &acl_main;
+    u32 sw_if_index = ~0;
+    u32 acl_index = ~0;
+    u32 *inacls = 0;
+    u32 *outacls = 0;
+    u8 is_input = 0;
+    u8 verbose = 0;
+
+//  acl_interface_set_acl_list <intfc> | sw_if_index <if-idx> input [acl-idx list] output [acl-idx list]
+
+    /* Parse args required to build the message */
+    while (unformat_check_input (i) != UNFORMAT_END_OF_INPUT) {
+        if (unformat (i, "sw_if_index %d", &sw_if_index))
+            ;
+        else if (unformat (i, "%d", &acl_index))
+          {
+            if(is_input)
+              vec_add1(inacls, htonl(acl_index));
+            else
+              vec_add1(outacls, htonl(acl_index));
+          }
+        else if (unformat (i, "acl %d", &acl_index))
+            ;
+        else if (unformat (i, "input"))
+            is_input = 1;
+        else if (unformat (i, "output"))
+            is_input = 0;
+        else if (unformat (i, "verbose"))
+            verbose = 1;
+        else
+            break;
+    }
+
+    if (sw_if_index == ~0) {
+	    return error = clib_error_return(0, "missing interface name / explicit sw_if_index number \n");
+    }
+
+
+    applied_hash_ace_entry_t **applied_hash_aces = is_input ? vec_elt_at_index(sm->input_hash_entry_vec_by_sw_if_index, sw_if_index)
+	    : vec_elt_at_index(sm->output_hash_entry_vec_by_sw_if_index, sw_if_index);
+
+
+    vlib_cli_output(vm,"Collisions in mask: 'applied-id' ('number of collisions in this entry) - 'ace-id' (entry in the hash)");
+ 
+    u32 hash_ace_info;
+    for(hash_ace_info=0; hash_ace_info < vec_len(*applied_hash_aces); hash_ace_info++) {
+
+	    applied_hash_ace_entry_t *pae = vec_elt_at_index((*applied_hash_aces), hash_ace_info);
+
+	    if(pae->prev_applied_entry_index == ~0){
+	    u32 applied_mask_type_index = pae->mask_type_index;
+	    u64 collisions = pae->collision;
+
+	    ace_mask_type_entry_t *mte = vec_elt_at_index(sm->ace_mask_type_pool, applied_mask_type_index);
+
+
+	    vlib_cli_output(vm,"Collisions in mask: %d (%ld) - ace: %d", applied_mask_type_index, 
+			    collisions, pae->ace_index);
+
+	    if(verbose){
+		    print_mask(&mte->mask);
+	    }
+	}
+
+    }
+
+
+
+    return error;
+}
+
+
+
+
+
+
+clib_error_t*
+acl_compare_partition (vlib_main_t * vm,
+                                   unformat_input_t * i,
+                                   vlib_cli_command_t * cmd){
+
+    clib_error_t *error = 0;
+    acl_main_t * sm = &acl_main;
+    u32 sw_if_index = ~0;
+    u32 acl_index = ~0;
+    u32 *inacls = 0;
+    u32 *outacls = 0;
+    u8 is_input = 0;
+    u8 verbose = 0;
+
+//  acl_interface_set_acl_list <intfc> | sw_if_index <if-idx> input [acl-idx list] output [acl-idx list]
+
+    /* Parse args required to build the message */
+    while (unformat_check_input (i) != UNFORMAT_END_OF_INPUT) {
+        if (unformat (i, "sw_if_index %d", &sw_if_index))
+            ;
+        else if (unformat (i, "%d", &acl_index))
+          {
+            if(is_input)
+              vec_add1(inacls, htonl(acl_index));
+            else
+              vec_add1(outacls, htonl(acl_index));
+          }
+        else if (unformat (i, "acl %d", &acl_index))
+            ;
+        else if (unformat (i, "input"))
+            is_input = 1;
+        else if (unformat (i, "output"))
+            is_input = 0;
+        else if (unformat (i, "verbose"))
+            verbose = 1;
+        else
+            break;
+    }
+
+    if (sw_if_index == ~0) {
+	    return error = clib_error_return(0, "missing interface name / explicit sw_if_index number \n");
+    }
+
+    hash_acl_info_t *ha = vec_elt_at_index(sm->hash_acl_infos, acl_index);
+
+    applied_hash_ace_entry_t **applied_hash_aces = is_input ? vec_elt_at_index(sm->input_hash_entry_vec_by_sw_if_index, sw_if_index)
+	    : vec_elt_at_index(sm->output_hash_entry_vec_by_sw_if_index, sw_if_index);
+
+
+    vlib_cli_output(vm,"Comparing mask: 'base_id' ('applied-id') - 'ace'");
+
+    u32 hash_ace_info;
+    for(hash_ace_info=0; hash_ace_info < vec_len(*applied_hash_aces); hash_ace_info++) {
+
+	    applied_hash_ace_entry_t *pae = vec_elt_at_index((*applied_hash_aces), hash_ace_info);
+
+	    u32 base_mask_type_index = (vec_elt_at_index((ha->rules), pae->hash_ace_info_index))->base_mask_type_index;
+	    u32 applied_mask_type_index = pae->mask_type_index;
+
+	    ace_mask_type_entry_t *mte1 = vec_elt_at_index(sm->ace_mask_type_pool, base_mask_type_index);
+	    ace_mask_type_entry_t *mte2 = vec_elt_at_index(sm->ace_mask_type_pool, applied_mask_type_index);
+
+
+	    vlib_cli_output(vm,"Comparing mask: %d (applied:%d) - ace: %d", base_mask_type_index, applied_mask_type_index, 
+			    pae->ace_index);
+
+	    if(verbose){
+		    print_mask(&mte1->mask);
+		    print_mask(&mte2->mask);
+	    }
+
+    }
+
+
+
+    return error;
+}
+
