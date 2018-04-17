3d195dbfe412c0c722d09aa6d9c5496992c167a0
eyal bari <royalbee@gmail.com>
2018-04-17 11:20:27 +0300

    vxlan offload POC
    using flow infra
    no ipv6 handling
    
    Change-Id: I70e61f792ef8e3f007d03d7df70e97ea4725b101
    Signed-off-by: Eyal Bari <ebari@cisco.com>

--- src/plugins/dpdk/device/flow.c
+++ src/plugins/dpdk/device/flow.c
@@ -22,6 +22,7 @@
 #include <vnet/ip/ip.h>
 #include <vnet/ethernet/ethernet.h>
 #include <vnet/ethernet/arp_packet.h>
+#include <vnet/vxlan/vxlan.h>
 #include <dpdk/device/dpdk.h>
 
 #include <dpdk/device/dpdk_priv.h>
@@ -43,6 +44,19 @@ dpdk_flow_add_n_touple (dpdk_device_t * xd, vnet_flow_t * f,
   struct rte_flow_action_mark mark = { 0 };
   struct rte_flow_item *item, *items = 0;
   struct rte_flow_action *action, *actions = 0;
+
+  enum
+  {
+    vxlan_hdr_sz = sizeof (vxlan_header_t),
+    raw_sz = sizeof (struct rte_flow_item_raw)
+  };
+
+  union
+  {
+    struct rte_flow_item_raw item;
+    u8 val[raw_sz + vxlan_hdr_sz];
+  } raw[2];
+
   u16 src_port, dst_port, src_port_mask, dst_port_mask;
   u8 protocol;
   int rv = 0;
@@ -50,6 +64,9 @@ dpdk_flow_add_n_touple (dpdk_device_t * xd, vnet_flow_t * f,
   if (f->actions & (~xd->supported_flow_actions))
     return VNET_FLOW_ERROR_NOT_SUPPORTED;
 
+  memset (raw, 0, sizeof raw);
+
+  /* Match items */
   /* Ethernet */
   vec_add2 (items, item, 1);
   item->type = RTE_FLOW_ITEM_TYPE_ETH;
@@ -57,10 +74,13 @@ dpdk_flow_add_n_touple (dpdk_device_t * xd, vnet_flow_t * f,
   item->mask = any_eth + 1;
 
   /* VLAN */
-  vec_add2 (items, item, 1);
-  item->type = RTE_FLOW_ITEM_TYPE_VLAN;
-  item->spec = any_vlan;
-  item->mask = any_vlan + 1;
+  if (f->type != VNET_FLOW_TYPE_IP4_VXLAN)
+    {
+      vec_add2 (items, item, 1);
+      item->type = RTE_FLOW_ITEM_TYPE_VLAN;
+      item->spec = any_vlan;
+      item->mask = any_vlan + 1;
+    }
 
   /* IP */
   vec_add2 (items, item, 1);
@@ -81,11 +101,10 @@ dpdk_flow_add_n_touple (dpdk_device_t * xd, vnet_flow_t * f,
       dst_port_mask = t6->dst_port.mask;
       protocol = t6->protocol;
     }
-  else
+  else if (f->type == VNET_FLOW_TYPE_IP4_N_TUPLE)
     {
       vnet_flow_ip4_n_tuple_t *t4 = &f->ip4_n_tuple;
-      ASSERT (f->type == VNET_FLOW_TYPE_IP4_N_TUPLE);
-      ip4[0].hdr.src_addr = t4->src_addr.mask.as_u32;
+      ip4[0].hdr.src_addr = t4->src_addr.addr.as_u32;
       ip4[1].hdr.src_addr = t4->src_addr.mask.as_u32;
       ip4[0].hdr.dst_addr = t4->dst_addr.addr.as_u32;
       ip4[1].hdr.dst_addr = t4->dst_addr.mask.as_u32;
@@ -94,10 +113,32 @@ dpdk_flow_add_n_touple (dpdk_device_t * xd, vnet_flow_t * f,
       item->mask = ip4 + 1;
 
       src_port = t4->src_port.port;
-      dst_port = t4->dst_port.mask;
+      dst_port = t4->dst_port.port;
       src_port_mask = t4->src_port.mask;
       dst_port_mask = t4->dst_port.mask;
       protocol = t4->protocol;
+    }
+  else if (f->type == VNET_FLOW_TYPE_IP4_VXLAN)
+    {
+      vnet_flow_ip4_vxlan_t *v4 = &f->ip4_vxlan;
+      ip4[0].hdr.src_addr = v4->src_addr.as_u32;
+      ip4[1].hdr.src_addr = -1;
+      ip4[0].hdr.dst_addr = v4->dst_addr.as_u32;
+      ip4[1].hdr.dst_addr = -1;
+      item->type = RTE_FLOW_ITEM_TYPE_IPV4;
+      item->spec = ip4;
+      item->mask = ip4 + 1;
+
+      dst_port = v4->dst_port;
+      dst_port_mask = -1;
+      src_port = 0;
+      src_port_mask = 0;
+      protocol = IP_PROTOCOL_UDP;
+    }
+  else
+    {
+      rv = VNET_FLOW_ERROR_NOT_SUPPORTED;
+      goto done;
     }
 
   /* Layer 4 */
@@ -128,13 +169,39 @@ dpdk_flow_add_n_touple (dpdk_device_t * xd, vnet_flow_t * f,
       goto done;
     }
 
-  /* The End */
+  /* Tunnel */
+  if (f->type == VNET_FLOW_TYPE_IP4_VXLAN)
+    {
+      vec_add2 (items, item, 1);
+
+      u32 vni = f->ip4_vxlan.vni;
+      vxlan_header_t spec_hdr = {
+	.flags = VXLAN_FLAGS_I,
+	.vni_reserved = clib_host_to_net_u32 (vni << 8)
+      };
+      vxlan_header_t mask_hdr = {
+	.flags = 0xff,
+	.vni_reserved = clib_host_to_net_u32 (((u32) - 1) << 8)
+      };
+
+      STATIC_ASSERT (sizeof spec_hdr <= (sizeof raw[0].val - raw_sz), "");
+      clib_memcpy (raw[0].val + raw_sz, &spec_hdr, vxlan_hdr_sz);
+      raw[0].item.relative = 1;
+      raw[0].item.length = vxlan_hdr_sz;
+      clib_memcpy (raw[1].val + raw_sz, &mask_hdr, vxlan_hdr_sz);;
+      item->type = RTE_FLOW_ITEM_TYPE_RAW;
+      item->spec = raw;
+      item->mask = raw + 1;
+    }
+
   vec_add2 (items, item, 1);
   item->type = RTE_FLOW_ITEM_TYPE_END;
 
+  /* Actions */
   vec_add2 (actions, action, 1);
   action->type = RTE_FLOW_ACTION_TYPE_PASSTHRU;
 
+  ASSERT (f->actions & VNET_FLOW_ACTION_REDIRECT_TO_NODE);
   vec_add2 (actions, action, 1);
   mark.id = fe->mark;
   action->type = RTE_FLOW_ACTION_TYPE_MARK;
@@ -147,8 +214,16 @@ dpdk_flow_add_n_touple (dpdk_device_t * xd, vnet_flow_t * f,
 				&xd->last_flow_error);
 
   if (!fe->handle)
-    rv = VNET_FLOW_ERROR_NOT_SUPPORTED;
-
+    {
+      clib_warning ("flow create failed");
+      clib_warning ("%-25s: %d\n", "last DPDK error type",
+		    xd->last_flow_error.type);
+      clib_warning ("%-25s: %s\n", "last DPDK error message",
+		    xd->last_flow_error.message ? xd->
+		    last_flow_error.message : "n/a");
+      rv = VNET_FLOW_ERROR_NOT_SUPPORTED;
+      goto done;
+    }
 done:
   vec_free (items);
   vec_free (actions);
@@ -164,7 +239,7 @@ dpdk_flow_ops_fn (vnet_main_t * vnm, vnet_flow_dev_op_t op, u32 dev_instance,
   dpdk_device_t *xd = vec_elt_at_index (dm->devices, dev_instance);
   dpdk_flow_entry_t *fe;
   dpdk_flow_lookup_entry_t *fle = 0;
-  int rv;
+  int rv = 0;
 
   if (op == VNET_FLOW_DEV_OP_DEL_FLOW)
     {
@@ -172,13 +247,15 @@ dpdk_flow_ops_fn (vnet_main_t * vnm, vnet_flow_dev_op_t op, u32 dev_instance,
 
       fe = vec_elt_at_index (xd->flow_entries, *private_data);
 
-      if ((rv = rte_flow_destroy (xd->device_index, fe->handle,
-				  &xd->last_flow_error)))
+      rv = rte_flow_destroy (xd->device_index, fe->handle,
+			     &xd->last_flow_error);
+      if (rv != 0)
 	return VNET_FLOW_ERROR_INTERNAL;
 
       memset (fe, 0, sizeof (*fe));
       pool_put (xd->flow_entries, fe);
-      return 0;
+
+      goto disable_hw_offload_support;
     }
 
   if (op != VNET_FLOW_DEV_OP_ADD_FLOW)
@@ -208,10 +285,17 @@ dpdk_flow_ops_fn (vnet_main_t * vnm, vnet_flow_dev_op_t op, u32 dev_instance,
   else
     fe->mark = 0;
 
+  if ((xd->flags & DPDK_DEVICE_FLAG_RX_FLOW_OFFLOAD) == 0)
+    {
+      xd->flags |= DPDK_DEVICE_FLAG_RX_FLOW_OFFLOAD;
+      dpdk_device_setup (xd);
+    }
+
   switch (flow->type)
     {
     case VNET_FLOW_TYPE_IP4_N_TUPLE:
     case VNET_FLOW_TYPE_IP6_N_TUPLE:
+    case VNET_FLOW_TYPE_IP4_VXLAN:
       if ((rv = dpdk_flow_add_n_touple (xd, flow, fe)))
 	goto done;
       break;
@@ -220,17 +304,22 @@ dpdk_flow_ops_fn (vnet_main_t * vnm, vnet_flow_dev_op_t op, u32 dev_instance,
       goto done;
     }
 
-
   *private_data = fe - xd->flow_entries;
 
   /* install entry in the lookup table */
-  memset (fle, ~1, sizeof (*fle));
+  memset (fle, -1, sizeof (*fle));
   if (flow->actions & VNET_FLOW_ACTION_MARK)
     fle->flow_id = flow->mark_flow_id;
-  if (flow->actions & VNET_FLOW_ACTION_REDIRECT_TO_NODE)
-    fle->next_index = flow->redirect_device_input_next_index;
   if (flow->actions & VNET_FLOW_ACTION_BUFFER_ADVANCE)
     fle->buffer_advance = flow->buffer_advance;
+  if (flow->actions & VNET_FLOW_ACTION_REDIRECT_TO_NODE)
+    {
+      vnet_hw_interface_t *hw =
+	vnet_get_hw_interface (dm->vnet_main, xd->hw_if_index);
+      fle->next_index =
+	vlib_node_add_next (dm->vlib_main, hw->input_node_index,
+			    flow->redirect_node_index);
+    }
 
 done:
   if (rv)
@@ -243,6 +332,14 @@ done:
 	  pool_put (xd->flow_lookup_entries, fle);
 	}
     }
+disable_hw_offload_support:
+  if ((xd->flags & DPDK_DEVICE_FLAG_RX_FLOW_OFFLOAD) != 0
+      && pool_elts (xd->flow_entries) == 0)
+    {
+      xd->flags &= ~DPDK_DEVICE_FLAG_RX_FLOW_OFFLOAD;
+      dpdk_device_setup (xd);
+    }
+
   return rv;
 }
 
