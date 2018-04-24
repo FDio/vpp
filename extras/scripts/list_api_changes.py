0f306fa865d95d257c48d994b5c1fd0daa8ede49
Chris Luke <chrisy@flirble.org>
2018-04-24 00:02:37 -0400

    VPP 18.04 release notes
    - Notes for the 18.04 release
    - Fixes for table layout of previous API summary
    - Update list_api_changes.py script
    
    Change-Id: Id99ed4df2e76e2704f949ee940eedf9ede7e8f4b
    Signed-off-by: Chris Luke <chrisy@flirble.org>

--- extras/scripts/list_api_changes.py
+++ extras/scripts/list_api_changes.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python
 import os, fnmatch, subprocess
-starttag = 'v18.01-rc0'
-endtag = 'v18.01-rc2'
+
+starttag = 'v18.04-rc0'
+endtag = 'v18.04-rc2'
+emit_md = True
 apifiles = []
+
 for root, dirnames, filenames in os.walk('.'):
     for filename in fnmatch.filter(filenames, '*.api'):
         apifiles.append(os.path.join(root, filename))
+
 for f in apifiles:
     commits = subprocess.check_output(['git', 'log',
                                        '--oneline', starttag + '..' + endtag,
                                        f])
     if commits:
-        print f
-        print commits
+        if f[0:2] == './':
+            f = f[2:]
+        if emit_md:
+            print "| @c %s ||" % f
+            print "| ------- | ------- |"
+            for line in commits.splitlines():
+                parts = line.strip().split()
+                commit = parts[0]
+                message = " ".join(parts[1:]).replace("|", "\|")
+                print "| [%s](https://gerrit.fd.io/r/gitweb?" \
+                        "p=vpp.git;a=commit;h=%s) | %s |" % (
+                                commit, commit, message)
+            print
+        else:
+            print f
+            print commits
