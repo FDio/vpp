2b2c709a9c86e4c846c29edc570747ca18932d63
Neale Ranns <neale.ranns@cisco.com>
2018-05-01 05:17:55 -0700

    FIB API updates
    Enhance the route add/del APIs to take a set of paths rather than just one.
    Most unicast routing protocols calcualte all the available paths in one
    run of the algorithm so updating all the paths at once is beneficial for the client.
    two knobs control the behaviour:
      is_multipath - if set the the set of paths passed will be added to those
                     that already exist, otherwise the set will replace them.
      is_add - add or remove the set
    
    is_add=0, is_multipath=1 and an empty set, results in deleting the route.
    
    It is also considerably faster to add multiple paths at once, than one at a time:
    
    vat# ip_add_del_route 1.1.1.1/32 count 100000 multipath via 10.10.10.11
    100000 routes in .572240 secs, 174751.80 routes/sec
    vat# ip_add_del_route 1.1.1.1/32 count 100000 multipath via 10.10.10.12
    100000 routes in .528383 secs, 189256.54 routes/sec
    vat# ip_add_del_route 1.1.1.1/32 count 100000 multipath via 10.10.10.13
    100000 routes in .757131 secs, 132077.52 routes/sec
    vat# ip_add_del_route 1.1.1.1/32 count 100000 multipath via 10.10.10.14
    100000 routes in .878317 secs, 113854.12 routes/sec
    
    vat# ip_route_add_del 1.1.1.1/32 count 100000 multipath via 10.10.10.11 via 10.10.10.12 via 10.10.10.13 via 10.10.10.14
    100000 routes in .900212 secs, 111084.93 routes/sec
    
    Change-Id: I416b93f7684745099c1adb0b33edac58c9339c1a
    Signed-off-by: Neale Ranns <neale.ranns@cisco.com>
    Signed-off-by: Ole Troan <ot@cisco.com>

--- test/vpp_mpls_tunnel_interface.py
+++ test/vpp_mpls_tunnel_interface.py
@@ -13,50 +13,31 @@ class VppMPLSTunnelInterface(VppInterface):
         self.t_paths = paths
         self.is_multicast = is_multicast
         self.is_l2 = is_l2
+        self.encoded_paths = []
+        for path in self.t_paths:
+            self.encoded_paths.append(path.encode())
 
     def add_vpp_config(self):
-        sw_if_index = 0xffffffff
-        for path in self.t_paths:
-            lstack = path.encode_labels()
-
-            reply = self.test.vapi.mpls_tunnel_add_del(
-                sw_if_index,
-                1,  # IPv4 next-hop
-                path.nh_addr,
-                path.nh_itf,
-                path.nh_table_id,
-                path.weight,
-                next_hop_via_label=path.nh_via_label,
-                next_hop_out_label_stack=lstack,
-                next_hop_n_out_labels=len(lstack),
-                is_multicast=self.is_multicast,
-                l2_only=self.is_l2)
-            sw_if_index = reply.sw_if_index
-            self.tunnel_index = reply.tunnel_index
-        self.set_sw_if_index(sw_if_index)
+        reply = self.test.vapi.mpls_tunnel_add_del(
+            0xffffffff,
+            self.encoded_paths,
+            is_multicast=self.is_multicast,
+            l2_only=self.is_l2)
+        self.set_sw_if_index(reply.sw_if_index)
+        self.tunnel_index = reply.tunnel_index
         self._test.registry.register(self, self._test.logger)
 
     def remove_vpp_config(self):
-        for path in self.t_paths:
-            lstack = path.encode_labels()
-
-            self.test.vapi.mpls_tunnel_add_del(
-                self.sw_if_index,
-                1,  # IPv4 next-hop
-                path.nh_addr,
-                path.nh_itf,
-                path.nh_table_id,
-                path.weight,
-                next_hop_via_label=path.nh_via_label,
-                next_hop_out_label_stack=lstack,
-                next_hop_n_out_labels=len(lstack),
-                is_add=0)
+        reply = self.test.vapi.mpls_tunnel_add_del(
+            self.sw_if_index,
+            self.encoded_paths,
+            is_add=0)
 
     def query_vpp_config(self):
         dump = self._test.vapi.mpls_tunnel_dump()
         for t in dump:
-            if self.sw_if_index == t.mt_sw_if_index and \
-               self.tunnel_index == t.mt_tunnel_index:
+            if self.sw_if_index == t.mt_tunnel.mt_sw_if_index and \
+               self.tunnel_index == t.mt_tunnel.mt_tunnel_index:
                 return True
         return False
 
