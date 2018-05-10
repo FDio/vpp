ee6ddceaf54fa055ac3fc3997156f5ee0c9ca4ae
Sachin Saxena <sachin.saxena@nxp.com>
2018-05-10 16:22:09 +0530

    Fix broken compilation for non-numa aware platforms
     - The dpdk plugin always looks for libnuma library during compilation.
       For non-numa aware platforms compilation breaks, if third party
       libnuma lib is not available.
     - Issue is more severe with Cross Compilation scenario where one has to
       download and cross compile libnuma-dev package even when target platofrom
       is NUMA disabled.
       Like when cross compiling for ARM platforms, Linaro tool-chain doesn't have
       libnuma by default.
    
    Change-Id: Ib85b3188b787c23ba33b47e3f6123c74fd37190e
    Signed-off-by: Sachin Saxena <sachin.saxena@nxp.com>

--- build-data/platforms/vpp.mk
+++ build-data/platforms/vpp.mk
@@ -36,6 +36,7 @@ vpp_root_packages = vpp vom
 # vpp_dpdk_lib_dir = /usr/lib
 # vpp_dpdk_shared_lib = yes
 
+# Use '--without-libnuma' for non-numa aware architecture
 vpp_configure_args_vpp =
 
 # load balancer plugin is not portable on 32 bit platform
