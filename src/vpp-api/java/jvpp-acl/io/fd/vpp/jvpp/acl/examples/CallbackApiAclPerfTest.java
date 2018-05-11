62c36447d69728325777063f4d48c87da88a9b69
Marek Gradzki <mgradzki@cisco.com>
2018-05-11 16:22:27 +0200

    ACL performance test example
    Change-Id: I62ece018f4479231de80a823e8448f26661f3f19
    Signed-off-by: Marek Gradzki <mgradzki@cisco.com>

---
+++ src/vpp-api/java/jvpp-acl/io/fd/vpp/jvpp/acl/examples/CallbackApiAclPerfTest.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2018 Cisco and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.fd.vpp.jvpp.acl.examples;
+
+import io.fd.vpp.jvpp.JVpp;
+import io.fd.vpp.jvpp.JVppRegistry;
+import io.fd.vpp.jvpp.JVppRegistryImpl;
+import io.fd.vpp.jvpp.VppCallbackException;
+import io.fd.vpp.jvpp.acl.dto.*;
+import io.fd.vpp.jvpp.acl.types.*;
+import io.fd.vpp.jvpp.acl.JVppAclImpl;
+import io.fd.vpp.jvpp.acl.callback.AclAddReplaceReplyCallback;
+import java.util.logging.Logger;
+
+public class CallbackApiAclPerfTest {
+
+    private static final Logger LOG = Logger.getLogger(CallbackApiAclPerfTest.class.getName());
+    private static final short MAX_PORT_NUMBER = (short) 65535;
+
+    /**
+     * @param args - for running for one sec requires no parameter - for running for set amount of requests requires one
+     *             parameters, desired REQUEST amount
+     * @throws Exception if arguments aren't String representations of numbers
+     */
+    public static void main(String[] args) throws Exception {
+        final int aclCount = Integer.parseUnsignedInt(args[0]);
+        final int aclSize = Integer.parseUnsignedInt(args[1]);
+        int trials = 5;
+        if (args.length >= 3) {
+            trials = Integer.parseUnsignedInt(args[2]);
+        }
+        testInvokeCounter(aclCount, aclSize, trials);
+    }
+
+    /**
+     * @param setCount true = run with set amount of requests, false = run for 1 sec
+     * @param count    number of requests with which test should be run
+     */
+    private static void testInvokeCounter(final int aclCount, final int aclSize, final int trials) throws Exception {
+        LOG.info("Testing callback API Invocation Counter");
+        try (final JVppRegistry registry = new JVppRegistryImpl("CallbackApiAclPerfTest");
+             final JVpp jvpp = new JVppAclImpl()) {
+            TestCallback callback = new TestCallback(aclCount);
+            registry.register(jvpp, callback);
+
+            final AclAddReplace request = createAclAddRequest(aclSize);
+
+            for (int i = 0; i < trials; i++) {
+                LOG.info("Starting invocations");
+                callback.reset();
+                long time = System.nanoTime();
+                for (int x = 0; x < aclCount; x++) {
+                    jvpp.send(request);
+                }
+                long timeAfter = callback.getTime();
+                LOG.info(String.format("Invocations took %f ms (%f invocations/s)", (timeAfter - time) / 1000000.0,
+                    aclCount * (1000000000.0 / (timeAfter - time))));
+            }
+
+            Thread.sleep(1000);
+            LOG.info("Disconnecting...");
+        }
+        Thread.sleep(1000);
+    }
+
+    private static AclAddReplace createAclAddRequest(final int aclSize) {
+        AclAddReplace request = new AclAddReplace();
+        request.aclIndex = -1; // define new one
+        request.count = aclSize;
+        request.r = createAclRules(aclSize);
+        return request;
+    }
+
+    private static AclRule[] createAclRules(final int aclSize) {
+        final AclRule[] rules = new AclRule[aclSize];
+        long ip = 0x01000000; // 1.0.0.0
+        for (int i = 0; i < aclSize; ++i) {
+            rules[i] = new AclRule();
+            rules[i].isIpv6 = 0;
+            rules[i].isPermit = 1;
+            rules[i].srcIpAddr = getIp((int) ip++);
+            rules[i].srcIpPrefixLen = 32;
+            rules[i].dstIpAddr = getIp((int) ip++);
+            rules[i].dstIpPrefixLen = 32;
+            rules[i].dstportOrIcmpcodeFirst = 0;
+            rules[i].dstportOrIcmpcodeLast = MAX_PORT_NUMBER;
+            rules[i].srcportOrIcmptypeFirst = 0;
+            rules[i].srcportOrIcmptypeLast = MAX_PORT_NUMBER;
+            rules[i].proto = 17; // UDP
+        }
+        return rules;
+    }
+
+    private static byte[] getIp(final int i) {
+        int b1 = (i >> 24) & 0xff;
+        int b2 = (i >> 16) & 0xff;
+        int b3 = (i >> 8) & 0xff;
+        int b4 = i & 0xff;
+        return new byte[] {(byte) b1, (byte) b2, (byte) b3, (byte) b4};
+    }
+
+    static class TestCallback implements AclAddReplaceReplyCallback {
+
+        private int replyCounter = 0;
+        private int count;
+        private long time = 0;
+        private boolean stop = false;
+
+        public TestCallback(int count) throws Exception {
+            this.count = count;
+        }
+
+        public int getReplyCounter() {
+            return replyCounter;
+        }
+
+        public void reset() {
+            replyCounter = 0;
+            time = 0;
+            stop = false;
+        }
+
+        public boolean stop() {
+            this.stop = true;
+            return false;
+        }
+
+        /* actual method called from VPP
+           not thread safe but since there's only one VPP thread listening for requests and calling
+           this method it's OK
+         */
+        @Override
+        public void onAclAddReplaceReply(final AclAddReplaceReply msg) {
+            if (stop) {
+                return;
+            }
+            replyCounter++;
+            if (replyCounter == count) {
+                time = System.nanoTime();
+            }
+        }
+
+        @Override
+        public void onError(VppCallbackException ex) {
+            System.out.printf("Received onError exception: call=%s, context=%d, retval=%d%n", ex.getMethodName(),
+                ex.getCtxId(), ex.getErrorCode());
+        }
+
+        public long getTime() throws Exception {
+            while (time == 0) {
+                Thread.sleep(1000);
+            }
+            return time;
+        }
+    }
+}
