83cd0960bd49bdc601a19f691728837079f960a6
Jakub Grajciar <jgrajcia@cisco.com>
2018-05-14 11:15:08 +0200

    igmp: IGMP Proxy
    Change-Id: If2ecd550eae9d9dd3eca46db66fc3da549d45b11
    Signed-off-by: Jakub Grajciar <jgrajcia@cisco.com>

--- test/test_igmp.py
+++ test/test_igmp.py
@@ -289,7 +289,12 @@ class TestIgmp(VppTestCase):
             self.config_list[-1].add_vpp_config()
 
         for config in self.config_list:
-            dump = self.vapi.igmp_dump(config.sw_if_index)
+            cfg_dump = config.query_vpp_config()
+            self.assertTrue(cfg_dump)
+            self.assertEqual(cfg_dump[0].sw_if_index, config.sw_if_index)
+            self.assertTrue(
+                cfg_dump[0].flags, IgmpConfigFlags.CLI_API_CONFIGURED.value)
+            dump = config.query_sg_config()
             self.assertTrue(dump)
             self.assertEqual(len(dump), len(config.sg_list))
             for idx, e in enumerate(dump):
@@ -303,6 +308,223 @@ class TestIgmp(VppTestCase):
         dump = self.vapi.igmp_dump()
         self.assertFalse(dump)
 
+
+class TestIgmpProxy(VppTestCase):
+
+    """ IGMP Proxy Test Case """
+
+    def setUp(self):
+        super(TestIgmpProxy, self).setUp()
+
+        self.create_pg_interfaces(range(2))
+
+        for pg in self.pg_interfaces:
+            pg.admin_up()
+            pg.config_ip4()
+            pg.resolve_arp()
+
+    def tearDown(self):
+        for pg in self.pg_interfaces:
+            self.vapi.igmp_clear_interface(pg.sw_if_index)
+            pg.unconfig_ip4()
+            pg.admin_down()
+        super(TestIgmpProxy, self).tearDown()
+
+    def send(self, ti, pkts):
+        ti.add_stream(pkts)
+        self.pg_enable_capture(self.pg_interfaces)
+        self.pg_start()
+
+    def verify_report(self, p, gaddr, saddr):
+        ip = p[IP]
+        self.assertEqual(ip.dst, "224.0.0.22")
+        self.assertEqual(ip.proto, 2)
+        igmp = p[IGMPv3]
+        self.assertEqual(igmp.type, 0x22)
+        self.assertEqual(igmp.gaddr, gaddr)
+        group = igmp[IGMP3gr]
+        self.assertEqual(igmp.saddr, saddr)
+
+    def test_igmp_proxy(self):
+        """ IGMP Poxy enable/disable """
+
+        proxy = VppIgmpProxy(self, self.pg0._sw_if_index)
+        proxy1 = VppIgmpProxy(self, self.pg1._sw_if_index)
+
+        proxy.add_vpp_config()
+        with self.vapi.expect_negative_api_retval():
+            proxy1.add_vpp_config()
+
+        cfg_dump = proxy.query_vpp_config()
+        self.assertTrue(cfg_dump)
+        self.assertEqual(len(cfg_dump), 1)
+        dump = proxy.query_sg_config()
+        self.assertFalse(dump)
+
+        for idx, e in enumerate(cfg_dump):
+            self.assertEqual(e.sw_if_index, proxy.sw_if_index)
+            self.assertTrue(e.flags & IgmpConfigFlags.CLI_API_CONFIGURED.value)
+            self.assertTrue(e.flags & IgmpConfigFlags.PROXY_ENABLED.value)
+            self.assertEqual(e.proxy_addr, proxy.addr)
+
+        proxy.remove_vpp_config()
+        dump = proxy.query_vpp_config()
+        self.assertFalse(dump)
+
+    def test_igmp_proxy_join(self):
+        """ IGMP send 'join/leave' upstream """
+
+        #
+        # pg0 -> proxy
+        # pg1 -> send membership report
+        #
+        proxy = VppIgmpProxy(self, self.pg0._sw_if_index)
+        proxy.add_vpp_config()
+
+        # pg1 sends join IGMP 'join'
+        p_join = (Ether(dst=self.pg1.local_mac, src=self.pg1.remote_mac) /
+                  IP(src=self.pg1.remote_ip4, dst='224.0.0.22', tos=0xc0) /
+                  IGMPv3() /
+                  IGMPv3mr(numgrp=1) /
+                  IGMPv3gr(rtype=3, maddr="224.1.1.1", srcaddrs=["10.1.1.1"]))
+
+        self.send(self.pg1, p_join)
+
+        capture = self.pg0.get_capture(1, timeout=1)
+
+        p = capture[0]
+        ip = p[IP]
+        self.assertEqual(ip.dst, "224.0.0.22")
+        self.assertEqual(ip.proto, 2)
+        igmp = p[IGMPv3]
+        self.assertEqual(igmp.type, 0x22)
+        self.assertEqual(len(igmp.records), 1)
+        gr = igmp.records[0]
+        self.assertEqual(gr.rtype, 3)
+        self.assertEqual(gr.maddr, "224.1.1.1")
+        self.assertEqual(len(gr.srcaddrs), 1)
+        self.assertEqual(gr.srcaddrs[0], "10.0.1.1")
+
+        # pg1 sends join IGMP 'join'
+        p_join = (Ether(dst=self.pg1.local_mac, src=self.pg1.remote_mac) /
+                  IP(src=self.pg1.remote_ip4, dst='224.0.0.22', tos=0xc0) /
+                  IGMPv3() /
+                  IGMPv3mr(numgrp=1) /
+                  IGMPv3gr(rtype=3, maddr="224.1.1.1", srcaddrs=["10.1.1.2"]))
+
+        self.send(self.pg1, p_join)
+        self.sleep(1)
+        self.pg0.assert_nothing_captured()
+
+        # pg2 sends IGMP 'leave'
+        p_leave = (Ether(dst=self.pg1.local_mac, src=self.pg1.remote_mac) /
+                   IP(src=self.pg1.remote_ip4, dst='224.0.0.22', tos=0xc0) /
+                   IGMPv3() /
+                   IGMPv3mr(numgrp=1) /
+                   IGMPv3gr(rtype=6, maddr="224.1.1.1", srcaddrs=["10.1.1.1"]))
+
+        self.send(self.pg1, p_leave)
+        self.sleep(1)
+        self.pg0.assert_nothing_captured()
+
+        # pg2 sends IGMP 'leave'
+        p_leave = (Ether(dst=self.pg1.local_mac, src=self.pg1.remote_mac) /
+                   IP(src=self.pg1.remote_ip4, dst='224.0.0.22', tos=0xc0) /
+                   IGMPv3() /
+                   IGMPv3mr(numgrp=1) /
+                   IGMPv3gr(rtype=6, maddr="224.1.1.1", srcaddrs=["10.1.1.2"]))
+
+        self.send(self.pg1, p_leave)
+
+        capture = self.pg0.get_capture(1, timeout=1)
+
+        p = capture[0]
+        ip = p[IP]
+        self.assertEqual(ip.dst, "224.0.0.22")
+        self.assertEqual(ip.proto, 2)
+        igmp = p[IGMPv3]
+        self.assertEqual(igmp.type, 0x22)
+        self.assertEqual(len(igmp.records), 1)
+        gr = igmp.records[0]
+        self.assertEqual(gr.rtype, 6)
+        self.assertEqual(gr.maddr, "224.1.1.1")
+        self.assertEqual(len(gr.srcaddrs), 1)
+        self.assertEqual(gr.srcaddrs[0], "10.0.1.1")
+
+        proxy.remove_vpp_config()
+
+    def test_igmp_proxy_mfib(self):
+        """ IGMP add/del mfib entries """
+
+        mfib_entries_n = len(self.vapi.ip_mfib_dump())
+
+        #
+        # pg0 -> proxy
+        # pg1 -> send membership report
+        #
+        proxy = VppIgmpProxy(self, self.pg0._sw_if_index)
+        proxy.add_vpp_config()
+        grp_addr = socket.inet_pton(socket.AF_INET, "224.1.1.1")
+
+        # pg1 sends join IGMP 'join'
+        p_join = (Ether(dst=self.pg1.local_mac, src=self.pg1.remote_mac) /
+                  IP(src=self.pg1.remote_ip4, dst='224.0.0.22', tos=0xc0) /
+                  IGMPv3() /
+                  IGMPv3mr(numgrp=1) /
+                  IGMPv3gr(rtype=3, maddr="224.1.1.1", srcaddrs=["10.1.1.1"]))
+
+        self.send(self.pg1, p_join)
+
+        #
+        # new mfib entry was added
+        #
+        mfib_entries = self.vapi.ip_mfib_dump()
+        self.assertTrue(mfib_entries_n + 1 == len(mfib_entries))
+        mfib_entries_n = len(mfib_entries)
+        #
+        # check added entry
+        #
+        mfib_entry = mfib_entries[-1]
+        self.assertEqual(mfib_entry.grp_address, grp_addr)
+
+        # pg1 sends join IGMP 'join'
+        p_join = (Ether(dst=self.pg1.local_mac, src=self.pg1.remote_mac) /
+                  IP(src=self.pg1.remote_ip4, dst='224.0.0.22', tos=0xc0) /
+                  IGMPv3() /
+                  IGMPv3mr(numgrp=1) /
+                  IGMPv3gr(rtype=3, maddr="224.1.1.1", srcaddrs=["10.1.1.2"]))
+
+        self.send(self.pg1, p_join)
+
+        self.assertTrue(mfib_entries_n == len(self.vapi.ip_mfib_dump()))
+
+        # pg1 sends IGMP 'leave'
+        p_leave = (Ether(dst=self.pg1.local_mac, src=self.pg1.remote_mac) /
+                   IP(src=self.pg1.remote_ip4, dst='224.0.0.22', tos=0xc0) /
+                   IGMPv3() /
+                   IGMPv3mr(numgrp=1) /
+                   IGMPv3gr(rtype=6, maddr="224.1.1.1", srcaddrs=["10.1.1.1"]))
+
+        self.send(self.pg1, p_leave)
+
+        self.assertTrue(mfib_entries_n == len(self.vapi.ip_mfib_dump()))
+
+        # pg1 sends IGMP 'leave'
+        p_leave = (Ether(dst=self.pg1.local_mac, src=self.pg1.remote_mac) /
+                   IP(src=self.pg1.remote_ip4, dst='224.0.0.22', tos=0xc0) /
+                   IGMPv3() /
+                   IGMPv3mr(numgrp=1) /
+                   IGMPv3gr(rtype=6, maddr="224.1.1.1", srcaddrs=["10.1.1.2"]))
+
+        self.send(self.pg1, p_leave)
+
+        #
+        # remove igmp entry
+        #
+        self.assertTrue(mfib_entries_n - 1 == len(self.vapi.ip_mfib_dump()))
+        mfib_entries_n = len(self.vapi.ip_mfib_dump())
+
+        proxy.remove_vpp_config()
 
 if __name__ == '__main__':
     unittest.main(testRunner=VppTestRunner)
