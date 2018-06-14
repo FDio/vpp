8d4c5ffff0f207f083f455ec29c1c66a21ba7e7e
Dave Barach <dave@barachs.net>
2018-06-14 18:52:46 -0400

    configurable per-dispatch-cycle sleep
    Workaround for lack of driver interrupt support. Also quite handy for
    home gateway, laptop/vagrant, other use-cases not requiring maximum
    vectors/second for proper operation.
    
    Change-Id: Ifc4b98112450664beef67b89ab8a6940a3bf24b5
    Signed-off-by: Dave Barach <dave@barachs.net>

--- src/plugins/dpdk/device/dpdk.h
+++ src/plugins/dpdk/device/dpdk.h
@@ -433,9 +433,6 @@ typedef struct
   f64 link_state_poll_interval;
   f64 stat_poll_interval;
 
-  /* Sleep for this many usec after each device poll */
-  u32 poll_sleep_usec;
-
   /* convenience */
   vlib_main_t *vlib_main;
   vnet_main_t *vnet_main;
