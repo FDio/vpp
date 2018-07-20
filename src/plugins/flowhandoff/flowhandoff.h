62c7b73547cffe554cc00056c5b8d86147b4d53c
Damjan Marion <damarion@cisco.com>
2018-07-20 21:47:42 +0200

    Flow Handoff Plugin
    Sample plugin which:
      - for each incoming flow calculate 5-tuple key
      - do flow lookup
      - if lookup fails, create new flow and store thread index, forward localy
      - if lookup succeeds, forward packet based on thread index (local or handover)
    
    Change-Id: I97376c5ad1adcabdb9e4fd6330a82067b9631e96
    Signed-off-by: Damjan Marion <damarion@cisco.com>

---
+++ src/plugins/flowhandoff/flowhandoff.h
@@ -0,0 +1,37 @@
+/*
+ * flowhandoff.h - ipfix probe plug-in header file
+ *
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef __included_flowhandoff_h__
+#define __included_flowhandoff_h__
+
+typedef struct
+{
+  clib_bihash_16_8_t table4;
+  u32 frame_queue_index;
+} flowhandoff_main_t;
+
+extern flowhandoff_main_t flowhandoff_main;
+
+
+#endif
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
