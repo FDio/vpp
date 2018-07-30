ca0105f9af9b53139486b420d9169c75d2b0e5e7
Damjan Marion <damarion@cisco.com>
2018-07-30 12:45:07 +0200

    cmake
    Change-Id: Ibc59323e849810531dd0963e85493efad3b86857
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- build-data/packages/vpp.mk
+++ build-data/packages/vpp.mk
@@ -12,40 +12,8 @@
 # limitations under the License.
 
 vpp_source = src
-
-ifeq ($($(PLATFORM)_dpdk_shared_lib),yes)
-vpp_configure_args = --enable-dpdk-shared
-else
-vpp_configure_args =
-endif
-
-# Platform dependent configure flags
-vpp_configure_args += $(vpp_configure_args_$(PLATFORM))
-
-
-vpp_CPPFLAGS =
-vpp_LDFLAGS =
-
-ifneq ($($(PLATFORM)_uses_dpdk),no)
-ifeq ($($(PLATFORM)_uses_external_dpdk),yes)
-vpp_CPPFLAGS += -I$($(PLATFORM)_dpdk_inc_dir)
-vpp_LDFLAGS += -L$($(PLATFORM)_dpdk_lib_dir)
-else
-vpp_configure_depend += dpdk-install
-vpp_CPPFLAGS += $(call installed_includes_fn, dpdk)/dpdk
-vpp_LDFLAGS += $(call installed_libs_fn, dpdk)
-vpp_CPPFLAGS += -I/usr/include/dpdk
-endif
-ifeq ($($(PLATFORM)_uses_dpdk_mlx5_pmd),yes)
-vpp_configure_args += --with-dpdk-mlx5-pmd
-endif
-ifeq ($($(PLATFORM)_uses_dpdk_mlx4_pmd),yes)
-vpp_configure_args += --with-dpdk-mlx4-pmd
-endif
-else
-vpp_configure_args += --disable-dpdk-plugin
-endif
-
-ifeq ($($(PLATFORM)_enable_tests),yes)
-vpp_configure_args += --enable-tests
-endif
+vpp_configure = \
+  cd $(PACKAGE_BUILD_DIR) && \
+  cmake \
+    -DCMAKE_INSTALL_PREFIX:PATH=$(PACKAGE_INSTALL_DIR) \
+    $(call find_source_fn,$(PACKAGE_SOURCE))
