1e8926cd007584f82e98b8dc9c20c206e7789999
Dave Barach <dave@barachs.net>
2018-08-17 18:29:07 -0400

    Clean up packet tracer, especially "clear trace"
    There are multiple trace enablement schemes. It's easy to end up in
    vlib_add_trace with tracing disabled insofar as the packet tracer is
    concerned. When that happens, return the address of a per-system
    dummy trace record.
    
    Change-Id: I929391b8be4ed57e26e291afdc509a15f09a3160
    Signed-off-by: Dave Barach <dave@barachs.net>

--- src/vlib/trace.h
+++ src/vlib/trace.h
@@ -80,7 +80,7 @@ typedef struct
   u32 filter_count;
 
   /* set on trace add, cleared on clear trace */
-  u32 trace_active_hint;
+  u32 trace_enable;
 
   /* Per node trace counts. */
   vlib_trace_node_t *nodes;
