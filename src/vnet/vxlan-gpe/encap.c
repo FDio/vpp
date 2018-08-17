0140dd911f39ca1a1e832d7233257c36ff7d846c
Zhiyong Yang <zhiyong.yang@intel.com>
2018-08-17 04:32:39 -0400

    vxlan-gpe: quad-loop optimization
    Change-Id: Ib273db97d854bedc1cc63fabe429693eaa5db1ef
    Signed-off-by: Zhiyong Yang <zhiyong.yang@intel.com>

--- src/vnet/vxlan-gpe/encap.c
+++ src/vnet/vxlan-gpe/encap.c
@@ -167,61 +167,79 @@ vxlan_gpe_encap (vlib_main_t * vm,
 
       vlib_get_next_frame (vm, node, next_index, to_next, n_left_to_next);
 
-      while (n_left_from >= 4 && n_left_to_next >= 2)
+      while (n_left_from >= 8 && n_left_to_next >= 4)
 	{
-	  u32 bi0, bi1;
-	  vlib_buffer_t *b0, *b1;
-	  u32 next0, next1;
-	  u32 sw_if_index0, sw_if_index1, len0, len1;
-	  vnet_hw_interface_t *hi0, *hi1;
-	  vxlan_gpe_tunnel_t *t0, *t1;
-	  u8 is_ip4_0, is_ip4_1;
+	  u32 bi0, bi1, bi2, bi3;
+	  vlib_buffer_t *b0, *b1, *b2, *b3;
+	  u32 next0, next1, next2, next3;
+	  u32 len0, len1, len2, len3;
+	  u32 sw_if_index0, sw_if_index1, sw_if_index2, sw_if_index3;
+	  vnet_hw_interface_t *hi0, *hi1, *hi2, *hi3;
+	  vxlan_gpe_tunnel_t *t0, *t1, *t2, *t3;
+	  u8 is_ip4_0, is_ip4_1, is_ip4_2, is_ip4_3;
 
 	  next0 = next1 = VXLAN_GPE_ENCAP_NEXT_IP4_LOOKUP;
+	  next2 = next3 = VXLAN_GPE_ENCAP_NEXT_IP4_LOOKUP;
 
 	  /* Prefetch next iteration. */
 	  {
-	    vlib_buffer_t *p2, *p3;
+	    vlib_buffer_t *p4, *p5, *p6, *p7;
 
-	    p2 = vlib_get_buffer (vm, from[2]);
-	    p3 = vlib_get_buffer (vm, from[3]);
+	    p4 = vlib_get_buffer (vm, from[4]);
+	    p5 = vlib_get_buffer (vm, from[5]);
+	    p6 = vlib_get_buffer (vm, from[6]);
+	    p7 = vlib_get_buffer (vm, from[7]);
 
-	    vlib_prefetch_buffer_header (p2, LOAD);
-	    vlib_prefetch_buffer_header (p3, LOAD);
+	    vlib_prefetch_buffer_header (p4, LOAD);
+	    vlib_prefetch_buffer_header (p5, LOAD);
+	    vlib_prefetch_buffer_header (p6, LOAD);
+	    vlib_prefetch_buffer_header (p7, LOAD);
 
-	    CLIB_PREFETCH (p2->data, 2 * CLIB_CACHE_LINE_BYTES, LOAD);
-	    CLIB_PREFETCH (p3->data, 2 * CLIB_CACHE_LINE_BYTES, LOAD);
+	    CLIB_PREFETCH (p4->data, CLIB_CACHE_LINE_BYTES, LOAD);
+	    CLIB_PREFETCH (p5->data, CLIB_CACHE_LINE_BYTES, LOAD);
+	    CLIB_PREFETCH (p6->data, CLIB_CACHE_LINE_BYTES, LOAD);
+	    CLIB_PREFETCH (p7->data, CLIB_CACHE_LINE_BYTES, LOAD);
 	  }
 
 	  bi0 = from[0];
 	  bi1 = from[1];
+	  bi2 = from[2];
+	  bi3 = from[3];
 	  to_next[0] = bi0;
 	  to_next[1] = bi1;
-	  from += 2;
-	  to_next += 2;
-	  n_left_to_next -= 2;
-	  n_left_from -= 2;
+	  to_next[2] = bi2;
+	  to_next[3] = bi3;
+	  from += 4;
+	  to_next += 4;
+	  n_left_to_next -= 4;
+	  n_left_from -= 4;
 
 	  b0 = vlib_get_buffer (vm, bi0);
 	  b1 = vlib_get_buffer (vm, bi1);
+	  b2 = vlib_get_buffer (vm, bi2);
+	  b3 = vlib_get_buffer (vm, bi3);
 
 	  /* 1-wide cache? */
 	  sw_if_index0 = vnet_buffer (b0)->sw_if_index[VLIB_TX];
 	  sw_if_index1 = vnet_buffer (b1)->sw_if_index[VLIB_TX];
-	  hi0 =
-	    vnet_get_sup_hw_interface (vnm,
-				       vnet_buffer (b0)->sw_if_index
-				       [VLIB_TX]);
-	  hi1 =
-	    vnet_get_sup_hw_interface (vnm,
-				       vnet_buffer (b1)->sw_if_index
-				       [VLIB_TX]);
+	  sw_if_index2 = vnet_buffer (b2)->sw_if_index[VLIB_TX];
+	  sw_if_index3 = vnet_buffer (b3)->sw_if_index[VLIB_TX];
+
+	  hi0 = vnet_get_sup_hw_interface (vnm, sw_if_index0);
+	  hi1 = vnet_get_sup_hw_interface (vnm, sw_if_index1);
+	  hi2 = vnet_get_sup_hw_interface (vnm, sw_if_index2);
+	  hi3 = vnet_get_sup_hw_interface (vnm, sw_if_index3);
+
 
 	  t0 = pool_elt_at_index (ngm->tunnels, hi0->dev_instance);
 	  t1 = pool_elt_at_index (ngm->tunnels, hi1->dev_instance);
+	  t2 = pool_elt_at_index (ngm->tunnels, hi2->dev_instance);
+	  t3 = pool_elt_at_index (ngm->tunnels, hi3->dev_instance);
 
 	  is_ip4_0 = (t0->flags & VXLAN_GPE_TUNNEL_IS_IPV4);
 	  is_ip4_1 = (t1->flags & VXLAN_GPE_TUNNEL_IS_IPV4);
+	  is_ip4_2 = (t2->flags & VXLAN_GPE_TUNNEL_IS_IPV4);
+	  is_ip4_3 = (t3->flags & VXLAN_GPE_TUNNEL_IS_IPV4);
 
 	  if (PREDICT_TRUE (is_ip4_0 == is_ip4_1))
 	    {
@@ -234,17 +252,37 @@ vxlan_gpe_encap (vlib_main_t * vm,
 	      vxlan_gpe_encap_one_inline (ngm, b1, t1, &next1, is_ip4_1);
 	    }
 
+	  if (PREDICT_TRUE (is_ip4_2 == is_ip4_3))
+	    {
+	      vxlan_gpe_encap_two_inline (ngm, b2, b3, t2, t3, &next2, &next3,
+					  is_ip4_2);
+	    }
+	  else
+	    {
+	      vxlan_gpe_encap_one_inline (ngm, b2, t2, &next2, is_ip4_2);
+	      vxlan_gpe_encap_one_inline (ngm, b3, t3, &next3, is_ip4_3);
+	    }
+
 	  /* Reset to look up tunnel partner in the configured FIB */
 	  vnet_buffer (b0)->sw_if_index[VLIB_TX] = t0->encap_fib_index;
 	  vnet_buffer (b1)->sw_if_index[VLIB_TX] = t1->encap_fib_index;
+	  vnet_buffer (b2)->sw_if_index[VLIB_TX] = t2->encap_fib_index;
+	  vnet_buffer (b3)->sw_if_index[VLIB_TX] = t3->encap_fib_index;
+
 	  vnet_buffer (b0)->sw_if_index[VLIB_RX] = sw_if_index0;
 	  vnet_buffer (b1)->sw_if_index[VLIB_RX] = sw_if_index1;
-	  pkts_encapsulated += 2;
+	  vnet_buffer (b2)->sw_if_index[VLIB_RX] = sw_if_index2;
+	  vnet_buffer (b3)->sw_if_index[VLIB_RX] = sw_if_index3;
+
+	  pkts_encapsulated += 4;
 
 	  len0 = vlib_buffer_length_in_chain (vm, b0);
-	  len1 = vlib_buffer_length_in_chain (vm, b0);
-	  stats_n_packets += 2;
-	  stats_n_bytes += len0 + len1;
+	  len1 = vlib_buffer_length_in_chain (vm, b1);
+	  len2 = vlib_buffer_length_in_chain (vm, b2);
+	  len3 = vlib_buffer_length_in_chain (vm, b3);
+
+	  stats_n_packets += 4;
+	  stats_n_bytes += len0 + len1 + len2 + len3;
 
 	  /* Batch stats increment on the same vxlan tunnel so counter is not
 	     incremented per packet. Note stats are still incremented for deleted
@@ -280,6 +318,37 @@ vxlan_gpe_encap (vlib_main_t * vm,
 						   1, len1);
 		}
 	    }
+	  if (PREDICT_FALSE ((sw_if_index2 != stats_sw_if_index)
+			     || (sw_if_index3 != stats_sw_if_index)))
+	    {
+	      stats_n_packets -= 2;
+	      stats_n_bytes -= len0 + len1;
+	      if (sw_if_index2 == sw_if_index1)
+		{
+		  if (stats_n_packets)
+		    vlib_increment_combined_counter
+		      (im->combined_sw_if_counters +
+		       VNET_INTERFACE_COUNTER_TX, thread_index,
+		       stats_sw_if_index, stats_n_packets, stats_n_bytes);
+		  stats_sw_if_index = sw_if_index2;
+		  stats_n_packets = 2;
+		  stats_n_bytes = len2 + len3;
+		}
+	      else
+		{
+		  vlib_increment_combined_counter (im->combined_sw_if_counters
+						   +
+						   VNET_INTERFACE_COUNTER_TX,
+						   thread_index, sw_if_index2,
+						   1, len2);
+		  vlib_increment_combined_counter (im->combined_sw_if_counters
+						   +
+						   VNET_INTERFACE_COUNTER_TX,
+						   thread_index, sw_if_index3,
+						   1, len3);
+		}
+	    }
+
 
 	  if (PREDICT_FALSE (b0->flags & VLIB_BUFFER_IS_TRACED))
 	    {
@@ -295,9 +364,24 @@ vxlan_gpe_encap (vlib_main_t * vm,
 	      tr->tunnel_index = t1 - ngm->tunnels;
 	    }
 
-	  vlib_validate_buffer_enqueue_x2 (vm, node, next_index, to_next,
-					   n_left_to_next, bi0, bi1, next0,
-					   next1);
+	  if (PREDICT_FALSE (b2->flags & VLIB_BUFFER_IS_TRACED))
+	    {
+	      vxlan_gpe_encap_trace_t *tr =
+		vlib_add_trace (vm, node, b2, sizeof (*tr));
+	      tr->tunnel_index = t2 - ngm->tunnels;
+	    }
+
+	  if (PREDICT_FALSE (b3->flags & VLIB_BUFFER_IS_TRACED))
+	    {
+	      vxlan_gpe_encap_trace_t *tr = vlib_add_trace (vm, node, b3,
+							    sizeof (*tr));
+	      tr->tunnel_index = t3 - ngm->tunnels;
+	    }
+
+	  vlib_validate_buffer_enqueue_x4 (vm, node, next_index, to_next,
+					   n_left_to_next, bi0, bi1, bi2, bi3,
+					   next0, next1, next2, next3);
+
 	}
 
       while (n_left_from > 0 && n_left_to_next > 0)
