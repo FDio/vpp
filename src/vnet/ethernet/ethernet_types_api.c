f5bd048709f44e5d78ee937fafaa9beaeedb3e6d
Neale Ranns <neale.ranns@cisco.com>
2018-08-29 06:37:18 -0700

    Introduce a mac_address_t on the API and in VPP
    Change-Id: I05d6c2cb5d34de469eb050e4ee10dc6b954c986d
    Signed-off-by: Neale Ranns <neale.ranns@cisco.com>

---
+++ src/vnet/ethernet/ethernet_types_api.c
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2018 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vnet/ethernet/ethernet_types_api.h>
+
+#define vl_typedefs		/* define message structures */
+#include <vnet/vnet_all_api_h.h>
+#undef vl_typedefs
+
+#define vl_endianfun		/* define message structures */
+#include <vnet/vnet_all_api_h.h>
+#undef vl_endianfun
+
+/* instantiate all the print functions we know about */
+#define vl_print(handle, ...) vlib_cli_output (handle, __VA_ARGS__)
+#define vl_printfun
+#include <vnet/vnet_all_api_h.h>
+#undef vl_printfun
+
+void
+mac_address_decode (const vl_api_mac_address_t * in, mac_address_t * out)
+{
+  mac_address_from_bytes (out, in->bytes);
+}
+
+void
+mac_address_encode (const mac_address_t * in, vl_api_mac_address_t * out)
+{
+}
+
+extern u8 *
+format_vl_api_mac_address (u8 * s, va_list * args)
+{
+  vl_api_mac_address_t *mac = va_arg (args, vl_api_mac_address_t *);
+
+  return (format (s, "%U", format_mac_address, mac->bytes));
+}
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
