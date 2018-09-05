338a56f4fd78bcbe62fdcf9634f5d9dd4ad3167a
Neale Ranns <neale.ranns@cisco.com>
2018-09-05 15:42:26 -0700

    GBP Endpoint Updates
    - common types on the API
    - endpoints keyed in various ways for DP lookup
    
    Change-Id: If7ec0bbc5cea71fd0983fe78987d147ec1bd7ec8
    Signed-off-by: Neale Ranns <neale.ranns@cisco.com>

--- test/vpp_ip.py
+++ test/vpp_ip.py
@@ -4,6 +4,7 @@
 """
 
 from ipaddress import ip_address
+from socket import AF_INET, AF_INET6
 
 
 class IpAddressFamily:
@@ -14,14 +15,32 @@ class IpAddressFamily:
 INVALID_INDEX = 0xffffffff
 
 
-def compare_ip_address(api_address, py_address):
+def compare_ip_address_union(api_address_u, py_address):
     if 4 is py_address.version:
-        if py_address.packed == api_address.ip4.address:
+        if py_address.packed == api_address_u.ip4.address:
             return True
     else:
-        if py_address.packed == api_address.ip6.address:
+        if py_address.packed == api_address_u.ip6.address:
             return True
     return False
+
+
+def compare_ip_address(api_address, vpp_address):
+    if 4 is vpp_address.version:
+        return (api_address.af == IpAddressFamily.ADDRESS_IP4 and
+                compare_ip_address_union(api_address.un,
+                                         vpp_address.addr.ip_addr))
+    else:
+        return (api_address.af == IpAddressFamily.ADDRESS_IP6 and
+                compare_ip_address_union(api_address.un,
+                                         vpp_address.addr.ip_addr))
+    return False
+
+
+def compare_ip_prefix(api_prefix, vpp_prefix):
+    return (api_prefix.address_length == vpp_prefix.length and
+            compare_ip_address(api_prefix.address,
+                               vpp_prefix.addr))
 
 
 class VppIpAddressUnion():
@@ -29,16 +48,8 @@ class VppIpAddressUnion():
         self.addr = addr
         self.ip_addr = ip_address(unicode(self.addr))
 
-    @property
-    def version(self):
-        return self.ip_addr.version
-
-    @property
-    def address(self):
-        return self.addr
-
     def encode(self):
-        if self.ip_addr.version is 6:
+        if self.version is 6:
             return {
                 'ip6': {
                     'address': self.ip_addr.packed
@@ -50,6 +61,25 @@ class VppIpAddressUnion():
                     'address': self.ip_addr.packed
                 },
             }
+
+    @property
+    def version(self):
+        return self.ip_addr.version
+
+    @property
+    def address(self):
+        return self.addr
+
+    @property
+    def length(self):
+        if self.version is 6:
+            return 128
+        else:
+            return 32
+
+    @property
+    def bytes(self):
+        return self.ip_addr.packed
 
 
 class VppIpAddress():
@@ -69,8 +99,31 @@ class VppIpAddress():
             }
 
     @property
+    def bytes(self):
+        return self.addr.bytes
+
+    @property
     def address(self):
         return self.addr.address
+
+    @property
+    def length(self):
+        return self.addr.length
+
+    @property
+    def version(self):
+        return self.addr.version
+
+    @property
+    def is_ip6(self):
+        return (self.version == 6)
+
+    @property
+    def af(self):
+        if self.version == 6:
+            return AF_INET6
+        else:
+            return AF_INET
 
 
 class VppIpPrefix():
@@ -90,6 +143,13 @@ class VppIpPrefix():
     @property
     def address(self):
         return self.addr.address
+
+    @property
+    def length(self):
+        return self.len
+
+    def __str__(self):
+        return "%s/%d" % (self.address, self.length)
 
 
 class VppIpMPrefix():
