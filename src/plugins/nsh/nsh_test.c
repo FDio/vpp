096eb28d0b25094853c680584136e8c14d9473eb
Ole Troan <ot@cisco.com>
2018-09-11 11:26:12 +0200

    VAT: plugin load errors
    - NSH plugin overrode vat_api_hookup()
    - Missing format function in mactime.
    
    Change-Id: I2dcbc2522d8f525d223c2ee3bb667111d1b3b78e
    Signed-off-by: Ole Troan <ot@cisco.com>

--- src/plugins/nsh/nsh_test.c
+++ src/plugins/nsh/nsh_test.c
@@ -365,7 +365,8 @@ _(nsh_entry_dump, "")   \
 _(nsh_add_del_map, "nsp <nn> nsi <nn> [del] mapped-nsp <nn> mapped-nsi <nn> [encap-gre-intf <nn> | encap-vxlan-gpe-intf <nn> | encap-none]")  \
 _(nsh_map_dump, "")
 
-void vat_api_hookup (vat_main_t *vam)
+static void
+nsh_vat_api_hookup (vat_main_t *vam)
 {
     nsh_test_main_t * sm = &nsh_test_main;
     /* Hook up handlers for replies from the data plane plug-in */
@@ -403,7 +404,7 @@ clib_error_t * vat_plugin_register (vat_main_t *vam)
   sm->msg_id_base = vl_client_get_first_plugin_msg_id ((char *) name);
 
   if (sm->msg_id_base != (u16) ~0)
-    vat_api_hookup (vam);
+    nsh_vat_api_hookup (vam);
 
   vec_free(name);
 
