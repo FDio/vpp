0466dfd3d9711db4894e44e2474579ca91f307b9
Damjan Marion <damarion@cisco.com>
2018-09-12 12:12:36 +0200

    cmake: create cmake VPP module, update sample-plugin so it uses it
    Change-Id: I32e91ba8e55797ffe169f98b09bdb42caa5c7de2
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- build-data/packages/sample-plugin.mk
+++ build-data/packages/sample-plugin.mk
@@ -17,3 +17,29 @@ sample-plugin_configure_depend = vpp-install
 sample-plugin_CPPFLAGS = $(call installed_includes_fn, vpp)
 sample-plugin_LDFLAGS = $(call installed_libs_fn, vpp)
 sample-plugin_PATH = $(call package_install_dir_fn,vpp)/bin
+
+ifneq ($(shell which cmake3),)
+CMAKE?=cmake3
+else
+CMAKE?=cmake
+endif
+
+sample-plugin_cmake_args ?=
+sample-plugin_cmake_args += -DCMAKE_INSTALL_PREFIX:PATH=$(PACKAGE_INSTALL_DIR)
+sample-plugin_cmake_args += -DCMAKE_C_FLAGS="$($(TAG)_TAG_CFLAGS)"
+sample-plugin_cmake_args += -DCMAKE_SHARED_LINKER_FLAGS="$($(TAG)_TAG_LDFLAGS)"
+sample-plugin_cmake_args += -DCMAKE_PREFIX_PATH:PATH="$(PACKAGE_INSTALL_DIR)/../vpp"
+
+# Use devtoolset on centos 7
+ifneq ($(wildcard /opt/rh/devtoolset-7/enable),)
+sample-plugin_cmake_args += -DCMAKE_PROGRAM_PATH:PATH="/opt/rh/devtoolset-7/root/bin"
+endif
+
+sample-plugin_configure = \
+  cd $(PACKAGE_BUILD_DIR) && \
+  $(CMAKE) -G Ninja $(sample-plugin_cmake_args) \
+  $(call find_source_fn,$(PACKAGE_SOURCE))$(PACKAGE_SUBDIR)
+
+sample-plugin_build = $(CMAKE) --build $(PACKAGE_BUILD_DIR) -- $(MAKE_PARALLEL_FLAGS)
+
+sample-plugin_install = $(CMAKE) --build $(PACKAGE_BUILD_DIR) -- install
