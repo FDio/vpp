921bf765ae51e8c25ef3859437118b5265d8e7c5
Andrew Yourtchenko <ayourtch@gmail.com>
2018-09-27 21:44:18 +0200

    DRAFT: vnet, acl-plugin: create l2 feature arc and use it
    This is a draft of a task to decouple the plumbing
    of the ACL plugin in L2 path from using L2 classifier
    entries.
    
    Since having "personal" ACL plugin nodes in VNET seemed
    wasteful, I have rather implemented the generic
    "L2 feature arc" nodes on input and output - so as
    to not have any coupling with ACL plugin.
    
    These nodes fork the traffic into three feature
    arcs: ip4, ip6 and nonip, which later join
    to continue the usual L2 processing.
    The vnet_l2_feature_enable_disable() function
    with the same signature as
    vnet_feature_enable_disable() takes care of
    enabling the L2 datapath feature bits as needed, when
    the features are enabled/disabled.
    
    This draft includes "wholesale" change
    together with the acl plugin plumbing and
    necessary additions (a new node for ethertype whitelisting,
    previously done using classifier entries).
    
    Even with the simple single loop implementation,
    avoiding l2 classifier allows to get the performance
    on mostly-established sessions 7.28 mpps vs.
    6.74 mpps with using the classifier. A simple
    quad-loop optimization makes this number 7.3-7.4.
    
    The final result will be split into three parts:
    - l2 feature arc "proper"
    - acl plugin testcase improvement
    - acl plugin l2 feature arc "plumbing" switch
    
    Change-Id: I0a136823dd5ce4ade1d0f41dfa17adfaf4187116
    Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>

---
+++ src/vnet/l2/l2_in_out_feat_arc.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2015 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __included_vnet_in_out_feat_arc_h__
+#define __included_vnet_in_out_feat_arc_h__
+
+#include <vlib/vlib.h>
+#include <vnet/vnet.h>
+#include <vnet/classify/vnet_classify.h>
+
+typedef enum
+{
+  FEAT_ARC_NEXT_INDEX_DROP,
+  FEAT_ARC_NEXT_INDEX_N_NEXT,
+} in_out_feat_arc_next_index_t;
+
+typedef enum
+{
+  IN_OUT_FEAT_ARC_TABLE_IP4,
+  IN_OUT_FEAT_ARC_TABLE_IP6,
+  IN_OUT_FEAT_ARC_TABLE_L2,
+  IN_OUT_FEAT_ARC_N_TABLES,
+} in_out_feat_arc_table_id_t;
+
+typedef enum
+{
+  IN_OUT_FEAT_ARC_INPUT_TABLE_GROUP,
+  IN_OUT_FEAT_ARC_OUTPUT_TABLE_GROUP,
+  IN_OUT_FEAT_ARC_N_TABLE_GROUPS
+} in_out_feat_arc_table_group_id_t;
+
+typedef struct
+{
+  /* convenience variables */
+  vlib_main_t *vlib_main;
+  vnet_main_t *vnet_main;
+  vnet_classify_main_t *vnet_classify_main;
+    vnet_config_main_t
+    *
+    vnet_config_main[IN_OUT_FEAT_ARC_N_TABLE_GROUPS]
+    [IN_OUT_FEAT_ARC_N_TABLES];
+} in_out_feat_arc_main_t;
+
+int vnet_l2_feature_enable_disable (const char *arc_name,
+				    const char *node_name, u32 sw_if_index,
+				    int enable_disable, void *feature_config,
+				    u32 n_feature_config_bytes);
+
+
+
+#endif /* __included_vnet_in_out_feat_arc_h__ */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
