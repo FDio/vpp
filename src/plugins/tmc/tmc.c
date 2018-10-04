767efc5385fe909c880269be0cb003bac162c3fd
Neale Ranns <neale.ranns@cisco.com>
2018-10-04 06:40:30 -0700

    tmc: TCP MSS clamping plugin
    Type: feature
    
    Configure TCP MSS clamping on an interface as follows:
    set interface tcp-mss-clamp <interface-name> mss <size>
    
    Change-Id: I45b04e50a0b70a33e14a9066f981c651292ebffb
    Signed-off-by: Neale Ranns <neale.ranns@cisco.com>
    Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>
    Signed-off-by: Miklos Tirpak <miklos.tirpak@gmail.com>

---
+++ src/plugins/tmc/tmc.c
@@ -0,0 +1,222 @@
+/*
+ * tmc.c - skeleton vpp engine plug-in
+ *
+ * Copyright (c) 2018 Cisco and/or its affiliates
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vnet/vnet.h>
+#include <vnet/plugin/plugin.h>
+#include <tmc/tmc.h>
+
+tmc_main_t tmc_main;
+
+/* Action function shared between message handler and debug CLI */
+
+static void
+tmc_enable_disable_feat (u32 sw_if_index, int enable)
+{
+  vnet_feature_enable_disable ("ip4-unicast", "tcp-mss-clamping-ip4-in",
+			       sw_if_index, enable, 0, 0);
+  vnet_feature_enable_disable ("ip6-unicast", "tcp-mss-clamping-ip6-in",
+			       sw_if_index, enable, 0, 0);
+  vnet_feature_enable_disable ("ip4-output", "tcp-mss-clamping-ip4-out",
+			       sw_if_index, enable, 0, 0);
+  vnet_feature_enable_disable ("ip6-output", "tcp-mss-clamping-ip6-out",
+			       sw_if_index, enable, 0, 0);
+}
+
+int
+tmc_enable (u32 sw_if_index, u16 mss)
+{
+  tmc_main_t *tm = &tmc_main;
+  int rv = 0;
+
+  vec_validate_init_empty (tm->max_mss, sw_if_index, TMC_UNSET);
+
+  if (TMC_UNSET == tm->max_mss[sw_if_index])
+    {
+      tmc_enable_disable_feat (sw_if_index, 1);
+    }
+
+  tm->max_mss[sw_if_index] = mss;
+
+  return rv;
+}
+
+int
+tmc_disable (u32 sw_if_index)
+{
+  tmc_main_t *tm = &tmc_main;
+  int rv = 0;
+
+  if (sw_if_index >= vec_len (tm->max_mss))
+    return VNET_API_ERROR_INVALID_SW_IF_INDEX;
+
+  if (TMC_UNSET != tm->max_mss[sw_if_index])
+    {
+      tmc_enable_disable_feat (sw_if_index, 0);
+    }
+  tm->max_mss[sw_if_index] = TMC_UNSET;
+
+  return rv;
+}
+
+int
+tmc_get_mss (u32 sw_if_index, u16 * mss)
+{
+  tmc_main_t *tm = &tmc_main;
+  int rv = 0;
+
+  if (vec_len (tm->max_mss) > sw_if_index
+      && TMC_UNSET != tm->max_mss[sw_if_index])
+    {
+      *mss = tm->max_mss[sw_if_index];
+    }
+  else
+    {
+      rv = VNET_API_ERROR_FEATURE_DISABLED;
+    }
+  return rv;
+}
+
+static clib_error_t *
+tmc_enable_command_fn (vlib_main_t * vm,
+		       unformat_input_t * input, vlib_cli_command_t * cmd)
+{
+  u32 mss = ~0, sw_if_index = ~0;
+  int rv, is_enable = 1;
+
+  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (input, "disable"))
+	is_enable = 0;
+      else if (unformat (input, "enable"))
+	is_enable = 1;
+      else if (unformat (input, "mss %d", &mss))
+	is_enable = 1;
+      else if (unformat (input, "%U", unformat_vnet_sw_interface,
+			 vnet_get_main (), &sw_if_index))
+	;
+      else
+	break;
+    }
+
+  if (sw_if_index == ~0)
+    return clib_error_return (0, "Please specify an interface...");
+  if (is_enable)
+    {
+      if (mss == ~0)
+	return clib_error_return (0,
+				  "Please specify the Max Segment Size...");
+      if (mss >= TMC_UNSET)
+	return clib_error_return (0, "Invalid Max Segment Size");
+    }
+
+  if (is_enable)
+    rv = tmc_enable (sw_if_index, mss);
+  else
+    rv = tmc_disable (sw_if_index);
+
+  if (rv)
+    return clib_error_return (0, "Failed: %d = %U",
+			      rv, format_vnet_api_errno, rv);
+
+  return (NULL);
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (tmc_enable_disable_command, static) =
+{
+  .path = "set interface tcp-mss-clamp",
+  .short_help = "set interface tcp-mss-clamp [enable|disable] <interface-name> mss <size>",
+  .function = tmc_enable_command_fn,
+};
+/* *INDENT-ON* */
+
+static clib_error_t *
+tmc_show_command_fn (vlib_main_t * vm,
+		     unformat_input_t * input, vlib_cli_command_t * cmd)
+{
+  tmc_main_t *tm = &tmc_main;
+  u32 sw_if_index = ~0;
+  u32 ii;
+
+  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (input, "%U", unformat_vnet_sw_interface,
+		    vnet_get_main (), &sw_if_index))
+	;
+      else
+	break;
+    }
+
+  if (sw_if_index == ~0)
+    {
+      vec_foreach_index (ii, tm->max_mss)
+      {
+	if (TMC_UNSET != tm->max_mss[ii])
+	  {
+	    u32 mss = tm->max_mss[ii];
+	    vlib_cli_output (vm, "%U: %d",
+			     format_vnet_sw_if_index_name,
+			     vnet_get_main (), ii, mss);
+	  }
+      }
+    }
+  else
+    {
+      u16 mss16;
+      if (tmc_get_mss (sw_if_index, &mss16) == 0)
+	{
+	  u32 mss = mss16;
+	  vlib_cli_output (vm, "%U: %d",
+			   format_vnet_sw_if_index_name,
+			   vnet_get_main (), sw_if_index, mss);
+	}
+      else
+	{
+	  vlib_cli_output (vm, "%U: disabled",
+			   format_vnet_sw_if_index_name,
+			   vnet_get_main (), sw_if_index);
+	}
+    }
+
+  return (NULL);
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (tmc_show_command, static) =
+{
+  .path = "show interface tcp-mss-clamp",
+  .short_help = "show interface tcp-mss-clamp [interface-name]",
+  .long_help = "show TCP MSS clamping configurations",
+  .function = tmc_show_command_fn,
+};
+/* *INDENT-ON* */
+
+static clib_error_t *
+tmc_init (vlib_main_t * vm)
+{
+  return NULL;
+}
+
+VLIB_INIT_FUNCTION (tmc_init);
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
---
+++ src/plugins/tmc/tmc_api.c
@@ -0,0 +1,101 @@
+/*
+ * tmc.c - skeleton vpp engine plug-in
+ *
+ * Copyright (c) <current-year> <your-organization>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vnet/vnet.h>
+#include <vnet/plugin/plugin.h>	/* VLIB_PLUGIN_REGISTER */
+#include <tmc/tmc.h>
+#include <tmc/tmc.api_enum.h>
+#include <tmc/tmc.api_types.h>
+#include <vlibapi/api.h>
+#include <vlibmemory/api.h>
+#include <vpp/app/version.h>	/* VPP_BUILD_VER */
+
+
+#define REPLY_MSG_ID_BASE tm->msg_id_base
+#include <vlibapi/api_helper_macros.h>
+
+/* API message handler */
+static void vl_api_tmc_enable_disable_t_handler
+  (vl_api_tmc_enable_disable_t * mp)
+{
+  tmc_main_t *tm = &tmc_main;
+  vl_api_tmc_enable_disable_reply_t *rmp;
+  int rv;
+
+  VALIDATE_SW_IF_INDEX (mp);
+
+  if (mp->enable)
+    rv = tmc_enable (ntohl (mp->sw_if_index), ntohs (mp->mss));
+  else
+    rv = tmc_disable (ntohl (mp->sw_if_index));
+
+  BAD_SW_IF_INDEX_LABEL;
+  REPLY_MACRO (VL_API_TMC_ENABLE_DISABLE_REPLY);
+}
+
+static void
+vl_api_tmc_get_mss_t_handler (vl_api_tmc_get_mss_t * mp)
+{
+  tmc_main_t *tm = &tmc_main;
+  vl_api_tmc_get_mss_reply_t *rmp;
+  int rv;
+  u16 mss = 0;
+
+  VALIDATE_SW_IF_INDEX (mp);
+
+  rv = tmc_get_mss (ntohl (mp->sw_if_index), &mss);
+
+  BAD_SW_IF_INDEX_LABEL;
+  /* *INDENT-OFF* */
+  REPLY_MACRO2 (VL_API_TMC_GET_MSS_REPLY,
+  ({
+    rmp->mss = htons (mss);
+  }));
+  /* *INDENT-ON* */
+}
+
+/* API definitions */
+#include <vnet/format_fns.h>
+#include <tmc/tmc.api.c>
+
+/* Set up the API message handling tables */
+static clib_error_t *
+tmc_api_hookup (vlib_main_t * vm)
+{
+  tmc_main_t *tm = &tmc_main;
+
+  tm->msg_id_base = setup_message_id_table ();
+  return 0;
+}
+
+VLIB_API_INIT_FUNCTION (tmc_api_hookup);
+
+/* *INDENT-OFF* */
+VLIB_PLUGIN_REGISTER () =
+{
+  .version = VPP_BUILD_VER,
+  .description = "TCP MSS clamping (tmc) plugin",
+};
+/* *INDENT-ON* */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
