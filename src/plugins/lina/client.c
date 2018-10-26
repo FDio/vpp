53cc4de43e9c2d23cb01cbc696370b319f4c17e8
Damjan Marion <damarion@cisco.com>
2018-10-26 14:22:40 +0200

    lina
    Change-Id: Idd13fa2611960011868644a79c82530046956d19
    Signed-off-by: Damjan Marion <damarion@cisco.com>

---
+++ src/plugins/lina/client.c
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) 2018 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vppinfra/format.h>
+#include <vppinfra/socket.h>
+#include "shared.h"
+
+typedef struct
+{
+  u8 *filename;
+  int verbose;
+} test_main_t;
+
+test_main_t test_main;
+
+clib_error_t *
+test_palloc (test_main_t * tm)
+{
+  clib_socket_t _sock = { 0 }, *sock = &_sock;
+  clib_error_t *err;
+  lina_msg_t msg;
+  int i, fds[LINA_SHM_MAX_REGIONS];
+  void *shm[LINA_SHM_MAX_REGIONS];
+  lina_shm_hdr_t *shm_hdr;
+
+  fformat (stdout, "Connecting to %s ...\n", tm->filename);
+
+  /* connect to unix domain socket */
+  sock->config = (char *) tm->filename;
+  sock->flags = CLIB_SOCKET_F_IS_CLIENT | CLIB_SOCKET_F_SEQPACKET;
+
+  if ((err = clib_socket_init (sock)))
+    return err;
+
+  if ((err = clib_socket_recvmsg (sock, &msg, sizeof (lina_msg_t), fds,
+				  LINA_SHM_MAX_REGIONS)))
+    return err;
+
+  fformat (stdout, "Connected to instance %u ...\n", msg.instance);
+
+  for (i = 0; i < msg.n_regions; i++)
+    {
+      if ((shm[i] = mmap (0, msg.region_size[i], PROT_READ | PROT_WRITE,
+			  MAP_SHARED, fds[i], 0)) == MAP_FAILED)
+	return clib_error_return_unix (0, "mmap");
+      fformat (stdout, "region %u size %u fd %u mapped at %p...\n", i,
+	       msg.region_size[i], fds[i], shm[i]);
+    }
+
+  shm_hdr = shm[0];
+  if (shm_hdr->cookie != LINA_SHM_HDR_COOKIE)
+    return clib_error_return (0, "bad cookie 0x%x", shm_hdr->cookie);
+
+  fformat (stdout, "%u rings, ring size is %u...\n", shm_hdr->n_rings,
+	   1 << shm_hdr->log2_ring_sz);
+
+  u32 ring = 0;
+  u32 last_head[shm_hdr->n_rings];
+  u32 mask = (1 << shm_hdr->log2_ring_sz) - 1;
+
+  memset (last_head, 0, sizeof (last_head));
+  while (1)
+    {
+      lina_shm_ring_hdr_t *ring_hdr;
+
+      ring_hdr = lina_get_shm_ring (shm_hdr, ring);
+      while (ring_hdr->head != last_head[ring])
+	{
+	  lina_shm_desc_t *d;
+	  u8 *data;
+	  d = lina_get_shm_desc (shm_hdr, ring, last_head[ring]);
+	  fformat (stdout, "ring %u desc %u region %u offset %u len %u\n",
+		   ring, last_head[ring], d->region, d->offset, d->length);
+
+	  data = shm[d->region] + d->offset;
+	  fformat (stderr, "%U\n", format_hexdump, data, d->length > 128 ?
+		   128 : d->length);
+
+	  /* set action for packet */
+	  d->action = LINA_SHM_DESC_ACTION_FORWARD;
+
+	  last_head[ring] = (last_head[ring] + 1) & mask;
+	}
+
+      __builtin_ia32_sfence ();
+      ring_hdr->tail = last_head[ring];
+
+      ring++;
+      if (ring >= shm_hdr->n_rings)
+	ring = 0;
+    }
+
+  clib_socket_close (sock);
+
+  return 0;
+}
+
+clib_error_t *
+test_palloc_main (unformat_input_t * i)
+{
+  test_main_t *tm = &test_main;
+  clib_error_t *error;
+
+  while (unformat_check_input (i) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (i, "file %s", &tm->filename))
+	;
+      else if (unformat (i, "verbose"))
+	tm->verbose = 1;
+      else
+	return clib_error_return (0, "unknown input '%U'",
+				  format_unformat_error, i);
+    }
+
+  error = test_palloc (tm);
+
+  return error;
+}
+
+#ifdef CLIB_UNIX
+int
+main (int argc, char *argv[])
+{
+  unformat_input_t i;
+  int rv = 0;
+  clib_error_t *error;
+
+  clib_mem_init (0, 3ULL << 30);
+
+  unformat_init_command_line (&i, argv);
+  error = test_palloc_main (&i);
+  if (error)
+    {
+      clib_error_report (error);
+      rv = 1;
+    }
+  unformat_free (&i);
+
+  return rv;
+}
+#endif /* CLIB_UNIX */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
---
+++ src/plugins/lina/init.c
@@ -0,0 +1,132 @@
+/*
+ *------------------------------------------------------------------
+ * Copyright (c) 2017 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *------------------------------------------------------------------
+ */
+
+
+#define _GNU_SOURCE
+#include <stdint.h>
+#include <net/if.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/uio.h>
+#include <sys/mman.h>
+#include <sys/prctl.h>
+#include <sys/eventfd.h>
+#include <inttypes.h>
+#include <limits.h>
+
+#include <vlib/vlib.h>
+#include <vlib/unix/unix.h>
+#include <vppinfra/linux/syscall.h>
+#include <vnet/plugin/plugin.h>
+#include <vnet/ethernet/ethernet.h>
+#include <lina/shared.h>
+#include <lina/lina.h>
+
+lina_main_t lina_main;
+
+void
+lina_create_instance (vlib_main_t * vm, lina_create_instance_args_t * a)
+{
+  vnet_main_t *vnm = vnet_get_main ();
+  lina_main_t *lm = &lina_main;
+  lina_instance_t *lin;
+  vlib_physmem_map_t *pm;
+  u32 i, n_rings;
+
+  pool_get_aligned_zero (lm->instances, lin, CLIB_CACHE_LINE_BYTES);
+  lin->index = lin - lm->instances;
+  lin->listener_filename = format (0, "%s%c", a->filename, 0);
+
+  lin->log2_ring_sz = min_log2 (a->ring_size);
+
+  if ((1 << lin->log2_ring_sz) != a->ring_size)
+    {
+      a->error = clib_error_return (0, "ring size must be power of 2");
+      goto error;
+    }
+
+  /* allocate shared memory */
+  n_rings = vec_len (vlib_mains);
+  lin->shm_size = sizeof (lina_shm_hdr_t) +
+    n_rings * sizeof (lina_shm_ring_hdr_t) +
+    (n_rings << lin->log2_ring_sz) * sizeof (lina_shm_desc_t);
+
+  a->error = vlib_physmem_shared_map_create (vm, "lina", lin->shm_size, 0,
+					     -1, &lin->shm_map_index);
+  pm = vlib_physmem_get_map (vm, lin->shm_map_index);
+  lin->fd = pm->fd;
+  lin->shm_hdr = pm->base;
+  lin->shm_size = pm->n_pages << pm->log2_page_size;
+
+  clib_memset (lin->shm_hdr, 0, lin->shm_size);
+  lin->shm_hdr->cookie = LINA_SHM_HDR_COOKIE;
+  lin->shm_hdr->n_rings = vec_len (vlib_mains);
+  lin->shm_hdr->log2_ring_sz = lin->log2_ring_sz;
+  if ((a->error = lina_socket_listener_create (vm, lin)))
+    goto error;
+
+  for (i = 0; i < n_rings; i++)
+    {
+      lina_ring_t *ring;
+      vec_add2 (lin->rings, ring, 1);
+      ring->hdr = lina_get_shm_ring (lin->shm_hdr, i);
+      ring->descs = lina_get_shm_desc (lin->shm_hdr, i, 0);
+      vec_validate_aligned (ring->buffer_indices,
+			    (1 << lin->log2_ring_sz) - 1,
+			    CLIB_CACHE_LINE_BYTES);
+    }
+
+  vnet_hw_interface_rx_redirect_to_node (vnm, a->hw_if_index,
+					 lina_enqueue_node.index);
+
+  return;
+
+error:
+  pool_put (lm->instances, lin);
+}
+
+static clib_error_t *
+lina_init (vlib_main_t * vm)
+{
+  lina_main_t *mm = &lina_main;
+
+  clib_memset (mm, 0, sizeof (lina_main_t));
+
+  mm->log_class = vlib_log_register_class ("lina", 0);
+
+  return 0;
+}
+
+VLIB_INIT_FUNCTION (lina_init);
+
+/* *INDENT-OFF* */
+VLIB_PLUGIN_REGISTER () = {
+    .version = "1.0",
+    .description = "lina enqueue/dequeue plugin",
+};
+/* *INDENT-ON* */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
