e18e58dd03a5656e442c7467d61235430638261f
Mohsin Kazmi <sykazmi@cisco.com>
2018-10-30 19:24:34 +0100

    vom: Add igmp 'host' support in vom
    Change-Id: Ibdb19d21b8ec7fb340a057e32df207b7723dba9b
    Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>

---
+++ extras/vom/vom/igmp_binding_cmds.cpp
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2018 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "vom/igmp_binding_cmds.hpp"
+
+namespace VOM {
+namespace igmp_binding_cmds {
+
+bind_cmd::bind_cmd(HW::item<bool>& item, const handle_t& itf)
+  : rpc_cmd(item)
+  , m_itf(itf)
+{
+}
+
+bool
+bind_cmd::operator==(const bind_cmd& other) const
+{
+  return (m_itf == other.m_itf);
+}
+
+rc_t
+bind_cmd::issue(connection& con)
+{
+  msg_t req(con.ctx(), std::ref(*this));
+
+  auto& payload = req.get_request().get_payload();
+  payload.sw_if_index = m_itf.value();
+  payload.enable = 1;
+  payload.mode = 1;
+
+  VAPI_CALL(req.execute());
+
+  return (wait());
+}
+
+std::string
+bind_cmd::to_string() const
+{
+  std::ostringstream s;
+  s << "igmp-bind: " << m_hw_item.to_string() << " itf:" << m_itf.to_string();
+
+  return (s.str());
+}
+
+unbind_cmd::unbind_cmd(HW::item<bool>& item, const handle_t& itf)
+  : rpc_cmd(item)
+  , m_itf(itf)
+{
+}
+
+bool
+unbind_cmd::operator==(const unbind_cmd& other) const
+{
+  return (m_itf == other.m_itf);
+}
+
+rc_t
+unbind_cmd::issue(connection& con)
+{
+  msg_t req(con.ctx(), std::ref(*this));
+
+  auto& payload = req.get_request().get_payload();
+  payload.sw_if_index = m_itf.value();
+  payload.enable = 0;
+  payload.mode = 1;
+
+  VAPI_CALL(req.execute());
+
+  wait();
+  m_hw_item.set(rc_t::NOOP);
+
+  return rc_t::OK;
+}
+
+std::string
+unbind_cmd::to_string() const
+{
+  std::ostringstream s;
+  s << "igmp-unbind: " << m_hw_item.to_string() << " itf:" << m_itf.to_string();
+
+  return (s.str());
+}
+
+}; // namespace igmp_binding_cmds
+}; // namespace VOM
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "mozilla")
+ * End:
+ */
---
+++ extras/vom/vom/igmp_listen_cmds.hpp
@@ -0,0 +1,162 @@
+/*
+ * Copyright (c) 2018 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __VOM_IGMP_LISTEN_CMDS_H__
+#define __VOM_IGMP_LISTEN_CMDS_H__
+
+#include "vom/dump_cmd.hpp"
+#include "vom/igmp_listen.hpp"
+#include "vom/rpc_cmd.hpp"
+
+#include <vapi/igmp.api.vapi.hpp>
+
+namespace VOM {
+namespace igmp_listen_cmds {
+
+/**
+ * A functor class that binds the igmp group to the interface
+ */
+class listen_cmd : public rpc_cmd<HW::item<bool>, vapi::Igmp_listen>
+{
+public:
+  /**
+   * Constructor
+   */
+  listen_cmd(HW::item<bool>& item,
+             const handle_t& itf,
+             const boost::asio::ip::address& gaddr,
+             const igmp_listen::src_addrs_t& saddrs);
+
+  /**
+   * Issue the command to VPP/HW
+   */
+  rc_t issue(connection& con);
+  /**
+   * convert to string format for debug purposes
+   */
+  std::string to_string() const;
+
+  /**
+   * Comparison operator - only used for UT
+   */
+  bool operator==(const listen_cmd& i) const;
+
+private:
+  /**
+   * Reference to the interface to bind to
+   */
+  const handle_t& m_itf;
+
+  /**
+   * The igmp group to bind
+   */
+  const boost::asio::ip::address& m_gaddr;
+
+  /**
+   * The igmp srouce specific addresses to listen them
+   */
+  const igmp_listen::src_addrs_t& m_saddrs;
+};
+
+/**
+ * A cmd class that Unbinds igmp group from an interface
+ */
+class unlisten_cmd : public rpc_cmd<HW::item<bool>, vapi::Igmp_listen>
+{
+public:
+  /**
+   * Constructor
+   */
+  unlisten_cmd(HW::item<bool>& item,
+               const handle_t& itf,
+               const boost::asio::ip::address& gaddr);
+
+  /**
+   * Issue the command to VPP/HW
+   */
+  rc_t issue(connection& con);
+
+  /**
+   * convert to string format for debug purposes
+   */
+  std::string to_string() const;
+
+  /**
+   * Comparison operator - only used for UT
+   */
+  bool operator==(const unlisten_cmd& i) const;
+
+private:
+  /**
+   * Reference to the interface to unbind
+   */
+  const handle_t& m_itf;
+
+  /**
+   * The igmp group to unlisten
+   */
+  const boost::asio::ip::address& m_gaddr;
+};
+
+/**
+ * A cmd class that Dumps all the igmp configs
+ */
+class dump_cmd : public VOM::dump_cmd<vapi::Igmp_dump>
+{
+public:
+  /**
+   * Constructor
+   */
+  dump_cmd(const handle_t& itf);
+  dump_cmd(const dump_cmd& d);
+
+  /**
+   * Issue the command to VPP/HW
+   */
+  rc_t issue(connection& con);
+  /**
+   * convert to string format for debug purposes
+   */
+  std::string to_string() const;
+
+  /**
+   * Comparison operator - only used for UT
+   */
+  bool operator==(const dump_cmd& i) const;
+
+private:
+  /**
+   * HW reutrn code
+   */
+  HW::item<bool> item;
+
+  /**
+   * The interface to get the igmp config for
+   */
+  const handle_t& m_itf;
+};
+
+}; // namespace igmp_listen_cmds
+}; // namespace VOM
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "mozilla")
+ * End:
+ */
+
+#endif
