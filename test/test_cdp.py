e711f987f87b6e9c13a1b2756d9b1e5f4e917a6b
Filip Varga <fivarga@cisco.com>
2018-10-31 07:58:05 +0100

    cdp scapy protocol & cdp unit tests
    Change-Id: Ieb362523f81f7ae3e1a9dceb341c499ff1f402c8
    Signed-off-by: Filip Varga <fivarga@cisco.com>

---
+++ test/test_cdp.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+""" CDP tests """
+
+from cdp import CDPVersion, CDP, TLVType, \
+    TLV, CustomTLV 
+from scapy.layers.l2 import Dot3, LLC, SNAP
+from framework import VppTestCase
+from scapy.all import raw
+from re import compile
+from time import sleep
+from util import ppp
+import platform
+
+
+""" TestCDP is a subclass of  VPPTestCase classes.
+
+CDP test.
+
+"""
+
+
+class TestCDP(VppTestCase):
+    """ CDP Test Case """
+
+    nen_ptr = compile("not enabled")
+    cdp_ptr = compile("^([-\.\w]+)\s+([-\.\w]+)\s+([-\.\w]+)\s+([-\.\w]+)$")
+    err_ptr = compile("^([\d]+)\s+([-\w]+)\s+([ -\.\w)(]+)$")
+
+    @property
+    def device_id(self):
+        return platform.node()
+
+    @property
+    def version(self):
+        return platform.release()
+
+    @property
+    def port_id(self):
+        return self.interface.name
+
+    @property
+    def platform(self):
+        return platform.system()
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestCDP, cls).setUpClass()
+        try:
+            cls.create_pg_interfaces(range(1))
+            cls.interface = cls.pg_interfaces[0]
+
+            cls.interface.admin_up()
+            cls.interface.config_ip4()
+            cls.interface.resolve_arp()
+
+        except Exception:
+            super(TestCDP, cls).tearDownClass()
+            raise
+
+    def test_enable_cdp(self):
+        self.logger.info(self.vapi.cli("cdp enable"))
+        ret = self.vapi.cli("show cdp")
+        self.logger.info(ret)
+        not_enabled = self.nen_ptr.search(ret)
+        self.assertFalse(not_enabled, "CDP isn't enabled")
+
+    def test_send_cdp_packet(self):
+        self.logger.info(self.vapi.cli("cdp enable"))
+        self.send_packet(self.create_packet())
+
+        neighbors = list(self.show_cdp())
+        self.assertTrue(neighbors, "CDP didn't register neighbor")
+
+        port, system = neighbors[0]
+
+        self.assert_equal(port, self.port_id, "CDP received invalid port id")
+        self.assert_equal(system, self.device_id, "CDP received invalid device id")
+
+    def test_send_cdp_bad_packet(self):
+        self.logger.info(self.vapi.cli("cdp enable"))
+        self.send_packet(self.create_bad_packet(3, "."))
+
+        errors = list(self.show_errors())
+        self.assertTrue(errors)
+
+        expected_errors = False
+        for count, node, reason in errors:
+            if (node == u'cdp-input' and
+                    reason == u'cdp packets with bad TLVs' and
+                    int(count) == 1):
+
+                expected_errors = True
+                break
+        self.assertTrue(expected_errors, "CDP didn't drop bad packet")
+
+    def send_packet(self, packet):
+        self.logger.debug(ppp("Sending packet:", packet))
+        self.interface.add_stream(packet)
+        self.pg_start()
+
+    def create_base_packet(self):
+        packet = (Dot3(src=self.interface.remote_mac,
+                       dst="01:00:0c:cc:cc:cc") /
+                  LLC(dsap=0xaa, ssap=0xaa, ctrl=0x03) /
+                  SNAP(OUI=0x0c, code=0x2000) /
+                  CDP(version=2, ttl=180))
+        return packet
+
+    def create_packet(self):
+        packet = (self.create_base_packet() /
+                  TLV(type=TLVType.device_id, value=self.device_id) /
+                  TLV(type=TLVType.version, value=self.version) /
+                  TLV(type=TLVType.port_id, value=self.port_id) /
+                  TLV(type=TLVType.platform, value=self.platform))
+        return packet
+
+    def create_bad_packet(self, tl=4, tv=""):
+        packet = (self.create_base_packet() /
+                  CustomTLV(type=TLVType.device_id,
+                            length=tl,
+                            value=tv))
+        return packet
+
+    def process_cli(self, exp, ptr):
+        for line in self.vapi.cli(exp).split('\n')[1:]:
+            m = ptr.match(line.strip())
+            if m:
+                yield m.groups()
+
+    def show_cdp(self):
+        for pack in self.process_cli("show cdp", self.cdp_ptr):
+            try:
+                port, system, _, _ = pack
+            except ValueError:
+                pass
+            else:
+                yield port, system
+
+    def show_errors(self):
+        for pack in self.process_cli("show errors", self.err_ptr):
+            try:
+                count, node, reason = pack
+            except ValueError:
+                pass
+            else:
+                yield count, node, reason
+
