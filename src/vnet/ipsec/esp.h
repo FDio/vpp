ca684a6d16d134a21b31908d4e9b16d95cea6778
Klement Sekera <ksekera@cisco.com>
2018-11-06 17:24:08 +0100

    ipsec: support buffer chains
    Change-Id: Ic7a08e94647a5c382d52f37753482d057eb9b5c2
    Signed-off-by: Klement Sekera <ksekera@cisco.com>

--- src/vnet/ipsec/esp.h
+++ src/vnet/ipsec/esp.h
@@ -276,42 +276,146 @@ ipsec_proto_init ()
     }
 }
 
-always_inline unsigned int
-hmac_calc (ipsec_integ_alg_t alg,
-	   u8 * key,
-	   int key_len,
-	   u8 * data, int data_len, u8 * signature, u8 use_esn, u32 seq_hi)
+always_inline int
+esp_cipher_cbc (vlib_main_t * vm, ipsec_crypto_alg_t alg, vlib_buffer_t * ib,
+		u32 ib_offset, vlib_buffer_t * ob, u8 * key,
+		u8 * iv, vlib_buffer_t ** last_ob, u32 * empty_buffers,
+		int is_encrypt)
 {
   ipsec_proto_main_t *em = &ipsec_proto_main;
   u32 thread_index = vlib_get_thread_index ();
-#if OPENSSL_VERSION_NUMBER >= 0x10100000L
-  HMAC_CTX *ctx = em->per_thread_data[thread_index].hmac_ctx;
-#else
-  HMAC_CTX *ctx = &(em->per_thread_data[thread_index].hmac_ctx);
-#endif
-  const EVP_MD *md = NULL;
-  unsigned int len;
-
-  ASSERT (alg < IPSEC_INTEG_N_ALG);
-
-  if (PREDICT_FALSE (em->ipsec_proto_main_integ_algs[alg].md == 0))
-    return 0;
-
-  if (PREDICT_FALSE (alg != em->per_thread_data[thread_index].last_integ_alg))
+  EVP_CIPHER_CTX *ctx;
+  if (is_encrypt)
     {
-      md = em->ipsec_proto_main_integ_algs[alg].md;
-      em->per_thread_data[thread_index].last_integ_alg = alg;
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+      ctx = em->per_thread_data[thread_index].encrypt_ctx;
+#else
+      ctx = &(em->per_thread_data[thread_index].encrypt_ctx);
+#endif
+    }
+  else
+    {
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+      ctx = em->per_thread_data[thread_index].decrypt_ctx;
+#else
+      ctx = &(em->per_thread_data[thread_index].decrypt_ctx);
+#endif
+    }
+  const EVP_CIPHER *cipher = NULL;
+  int out_len;
+
+  if (PREDICT_FALSE (em->ipsec_proto_main_crypto_algs[alg].type == 0))
+    {
+      return 0;
     }
 
-  HMAC_Init_ex (ctx, key, key_len, md, NULL);
+  if (is_encrypt)
+    {
+      if (PREDICT_FALSE
+	  (alg != em->per_thread_data[thread_index].last_encrypt_alg))
+	{
+	  cipher = em->ipsec_proto_main_crypto_algs[alg].type;
+	  em->per_thread_data[thread_index].last_encrypt_alg = alg;
+	}
+    }
+  else
+    {
+      if (PREDICT_FALSE
+	  (alg != em->per_thread_data[thread_index].last_decrypt_alg))
+	{
+	  cipher = em->ipsec_proto_main_crypto_algs[alg].type;
+	  em->per_thread_data[thread_index].last_decrypt_alg = alg;
+	}
+    }
+  const int block_size = em->ipsec_proto_main_crypto_algs[alg].block_size;
 
-  HMAC_Update (ctx, data, data_len);
+  if (!EVP_CipherInit_ex (ctx, cipher, NULL, key, iv, is_encrypt))
+    {
+      return 0;
+    }
 
-  if (PREDICT_TRUE (use_esn))
-    HMAC_Update (ctx, (u8 *) & seq_hi, sizeof (seq_hi));
-  HMAC_Final (ctx, signature, &len);
+  EVP_CIPHER_CTX_set_padding (ctx, 0);
 
-  return em->ipsec_proto_main_integ_algs[alg].trunc_size;
+  vlib_buffer_t *first_ob = ob;
+  u32 total_length = ob->current_length;
+  do
+    {
+      size_t in_length = ib->current_length - ib_offset;
+      const size_t cipher_could_write = in_length + block_size - 1;
+      size_t space_left_in_buffer =
+	VLIB_BUFFER_DATA_SIZE - ((u8 *) vlib_buffer_get_current (ob) +
+				 ob->current_length - ob->data);
+      if (space_left_in_buffer < block_size)
+	{
+	  u32 last_empty_buffer = vec_len (empty_buffers) - 1;
+	  u32 ebi = empty_buffers[last_empty_buffer];
+	  vlib_buffer_t *eb = vlib_get_buffer (vm, ebi);
+	  eb->current_data = 0;
+	  eb->current_length = 0;
+	  vlib_buffer_chain_buffer (vm, ob, ebi);
+	  vlib_prefetch_buffer_with_index (vm,
+					   empty_buffers[last_empty_buffer -
+							 1], STORE);
+	  _vec_len (empty_buffers) = last_empty_buffer;
+	  ob = eb;
+	  space_left_in_buffer =
+	    VLIB_BUFFER_DATA_SIZE - ((u8 *) vlib_buffer_get_current (ob) +
+				     ob->current_length - ob->data);
+	}
+      if (space_left_in_buffer < cipher_could_write)
+	{
+	  in_length -= cipher_could_write - space_left_in_buffer;
+	}
+
+      if (!EVP_CipherUpdate
+	  (ctx, vlib_buffer_get_current (ob) + ob->current_length,
+	   &out_len, vlib_buffer_get_current (ib) + ib_offset, in_length))
+	{
+	  return 0;
+	}
+      total_length += out_len;
+      ob->current_length += out_len;
+      ib_offset += in_length;
+
+      if (ib_offset == ib->current_length
+	  && ib->flags & VLIB_BUFFER_NEXT_PRESENT)
+	{
+	  ib = vlib_get_buffer (vm, ib->next_buffer);
+	  ib_offset = 0;
+	}
+    }
+  while (ib_offset < ib->current_length
+	 || ib->flags & VLIB_BUFFER_NEXT_PRESENT);
+
+
+  u8 dummy[block_size + 1];
+  if (!EVP_CipherFinal_ex (ctx, dummy, &out_len))
+    {
+      return 0;
+    }
+  if (out_len != 0)
+    {
+      /* this really shouldn't happen, because padding is disabled */
+      return 0;
+    }
+
+  if (ob != first_ob)
+    {
+      first_ob->total_length_not_including_first_buffer =
+	total_length - first_ob->current_length;
+      first_ob->flags |= VLIB_BUFFER_TOTAL_LENGTH_VALID;
+#if 0
+      u32 x = vlib_buffer_length_in_chain (vm, first_ob);
+      first_ob->flags &= ~VLIB_BUFFER_TOTAL_LENGTH_VALID;
+      u32 y = vlib_buffer_length_in_chain (vm, first_ob);
+      ASSERT (x == y);
+#endif
+    }
+  if (last_ob)
+    {
+      *last_ob = ob;
+    }
+  return 1;
 }
 
 #endif /* __ESP_H__ */
