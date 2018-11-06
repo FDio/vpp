6fe7df0313aaf0777c7e175b64ea32da752fe7c3
jdenisco <jdenisco@cisco.com>
2018-11-06 12:32:33 -0500

    docs: Add version and build date
    Change-Id: I6fd53d3ef12e4b6f1af1e5605c09938bce324ec9
    Signed-off-by: jdenisco <jdenisco@cisco.com>

--- docs/scripts/sphinx-make.sh
+++ docs/scripts/sphinx-make.sh
@@ -8,6 +8,11 @@ then
     pip install -r $DOCS_DIR/etc/requirements.txt
 else
     source $VENV_DIR/bin/activate;
+    VERSION=`source $WS_ROOT/src/scripts/version`
+    TM=`TZ=GMT date`
+    sed -ie "s/**VPP Version:\*\* .*/**VPP Version:** $VERSION/" $DOCS_DIR/about.rst
+    sed -ie "s/**Built on:\*\* .*/**Built on:** $TM/" $DOCS_DIR/about.rst
+    rm $DOCS_DIR/about.rste
     make -C $DOCS_DIR $1
 fi
 
