17164fc59a045f9cb223bccefaa71508c3dc0ea5
juraj.linkes <juraj.linkes@pantheon.tech>
2018-12-03 14:08:07 +0100

    Rename VPP_TEST_FAILED_DIR to FAILED_DIR
    15999 renamed VPP_TEST_FAILED_DIR, but not in all files. Fix this in the
    one remaining file.
    
    Change-Id: I159cdc86bfd5dd33af74256830873a8d081f1a73
    Signed-off-by: juraj.linkes <juraj.linkes@pantheon.tech>

--- test/scripts/compress_failed.sh
+++ test/scripts/compress_failed.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-if [ "$(ls -A ${VPP_TEST_FAILED_DIR})" ]
+if [ "$(ls -A ${FAILED_DIR})" ]
 then
     if [ "${COMPRESS_FAILED_TEST_LOGS}" == "yes" ]
     then
 	echo -n "Compressing files in temporary directories from failed test runs... "
-	cd ${VPP_TEST_FAILED_DIR}
+	cd ${FAILED_DIR}
 	for d in *
 	do
 	    cd ${d}
 	    find . ! -path . -print0 | xargs -0 -n1 gzip
-	    cd ${VPP_TEST_FAILED_DIR}
+	    cd ${FAILED_DIR}
 	done
 	echo "done."
         if [ -n "$WORKSPACE" ]
         then
             echo "Copying failed test logs into build log archive directory ($WORKSPACE/archives)... "
-            for failed_test in $(ls $VPP_TEST_FAILED_DIR)
+            for failed_test in $(ls $FAILED_DIR)
             do
                 mkdir -p $WORKSPACE/archives/$failed_test
-                cp -a $VPP_TEST_FAILED_DIR/$failed_test/* $WORKSPACE/archives/$failed_test
+                cp -a $FAILED_DIR/$failed_test/* $WORKSPACE/archives/$failed_test
             done
 	    echo "done."
         fi
-        
+
     else
 	echo "Not compressing files in temporary directories from failed test runs."
     fi
 else
-    echo "No symlinks to failed tests' temporary directories found in ${VPP_TEST_FAILED_DIR}."
+    echo "No symlinks to failed tests' temporary directories found in ${FAILED_DIR}."
 fi
 
 # This script gets run only if there was a 'make test' failure,
