78c8f8f35333271d11d1b4001086051733f96562
jackiechen1985 <xiaobo.chen@tieto.com>
2018-12-06 03:10:31 +0800

    Fix VPP-1515 IPSec receive packet error in transport mode with udp encap
    Change-Id: Ife66395b89e1e9f9206666e5f0fd441b3c241bb2
    Signed-off-by: jackiechen1985 <xiaobo.chen@tieto.com>

--- src/vnet/ipsec/esp_decrypt.c
+++ src/vnet/ipsec/esp_decrypt.c
@@ -277,9 +277,19 @@ esp_decrypt_inline (vlib_main_t * vm,
 		    }
 		  else
 		    {
-		      ih4 =
-			(ip4_header_t *) ((u8 *) esp0 -
-					  sizeof (ip4_header_t));
+		      if (sa0->udp_encap)
+			{
+			  ih4 =
+			    (ip4_header_t *) ((u8 *) esp0 -
+					      sizeof (udp_header_t) -
+					      sizeof (ip4_header_t));
+			}
+		      else
+			{
+			  ih4 =
+			    (ip4_header_t *) ((u8 *) esp0 -
+					      sizeof (ip4_header_t));
+			}
 		      oh4 = vlib_buffer_get_current (o_b0);
 		      ip_hdr_size = sizeof (ip4_header_t);
 		    }
