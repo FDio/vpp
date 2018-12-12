56c6aeaeb1fc8af88b28a344b3d9c61a16014903
Michal Cmarada <mcmarada@cisco.com>
2018-12-12 10:43:32 +0100

    add support for strings in Japi
     - converts vl_api_string_t to String in Java and back
     - simplifies the use of strings in API calls
       (no more byte array conversions for Strings in Java)
    
    Change-Id: I45a363bef8ecb3378524e213e0c76f6c2c914224
    Signed-off-by: Michal Cmarada <mcmarada@cisco.com>

---
+++ extras/japi/java/jvpp-core/jvpp_core.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2018 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef VPP_JVPP_CORE_H
+#define VPP_JVPP_CORE_H
+
+#endif //VPP_JVPP_CORE_H
+
+// /**
+// * Host to network byte order conversion for string type. Converts String in Java to VPP string type.
+// * typedef struct
+// * {
+// *   u32 length;
+// *   u8 buf[0];
+// * } __attribute__ ((packed)) vl_api_string_t;
+// */
+static void _host_to_net_string(JNIEnv * env, jstring javaString, vl_api_string_t * _net)
+{
+    const char *nativeString = (*env)->GetStringUTFChars(env, javaString, 0);
+    int len = strlen(nativeString);
+
+    vl_api_string_t * vl_api_string = (vl_api_string_t *)calloc(1, (sizeof(vl_api_string_t) + len * sizeof(u8)));
+    if (NULL == vl_api_string)
+        return NULL;
+
+    vl_api_string->length = len;
+    memcpy(vl_api_string->buf, nativeString, len);
+
+    _net = vl_api_string;
+    (*env)->ReleaseStringUTFChars(env, javaString, nativeString);
+}
+
+//
+// /**
+// * Network to host byte order conversion for string type. Converts VPP string type to String in Java
+// * typedef struct
+// * {
+// *   u32 length;
+// *   u8 buf[0];
+// * } __attribute__ ((packed)) vl_api_string_t;
+// */
+static jstring _net_to_host_string(JNIEnv * env, const vl_api_string_t * _net)
+{
+    jstring jstr = (*env)->NewStringUTF(env, (char *)_net->buf);
+
+    return jstr;
+}
