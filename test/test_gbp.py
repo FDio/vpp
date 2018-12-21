27af5d8f1f5ee38e767ee4296bb9cb7debbae35f
juraj.linkes <juraj.linkes@pantheon.tech>
2018-12-21 10:17:03 +0100

    Enable GBP tests for ARM
    The underlying failure has been fixed.
    
    Change-Id: If2ca92a17cfed1c2d7253e46ab161f301f847f29
    Signed-off-by: juraj.linkes <juraj.linkes@pantheon.tech>

--- test/test_gbp.py
+++ test/test_gbp.py
@@ -2,8 +2,7 @@
 
 import unittest
 
-from framework import VppTestCase, VppTestRunner, is_skip_aarch64_set, \
-    is_platform_aarch64
+from framework import VppTestCase, VppTestRunner
 from vpp_object import VppObject
 from vpp_neighbor import VppNeighbor
 from vpp_ip_route import VppIpRoute, VppRoutePath, VppIpTable, \
@@ -1836,8 +1835,6 @@ class TestGBP(VppTestCase):
         self.logger.info(self.vapi.cli("sh int"))
         self.logger.info(self.vapi.cli("sh gbp vxlan"))
 
-    @unittest.skipIf(is_skip_aarch64_set and is_platform_aarch64,
-                     "test doesn't work on aarch64")
     def test_gbp_learn_vlan_l2(self):
         """ GBP L2 Endpoint w/ VLANs"""
 
@@ -2006,8 +2003,6 @@ class TestGBP(VppTestCase):
         self.pg2.unconfig_ip4()
         self.pg3.unconfig_ip4()
 
-    @unittest.skipIf(is_skip_aarch64_set and is_platform_aarch64,
-                     "test doesn't work on aarch64")
     def test_gbp_learn_l3(self):
         """ GBP L3 Endpoint Learning """
 
