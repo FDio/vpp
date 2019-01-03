6fd3d7dd82214db4f6adfffb1688529aa6b683d4
Jon Loeliger <jdl@netgate.com>
2019-01-03 12:56:02 -0600

    VXLAN: Prevent duplicate bypass graph nodes.
    Change-Id: I68cc509b594b09751ff5e0e09bbca187a4a88edd
    Signed-off-by: Jon Loeliger <jdl@netgate.com>

--- src/vnet/vxlan/vxlan.h
+++ src/vnet/vxlan/vxlan.h
@@ -172,6 +172,10 @@ typedef struct
   /* Mapping from sw_if_index to tunnel index */
   u32 *tunnel_index_by_sw_if_index;
 
+  /* graph node state */
+  uword *bm_ip4_bypass_enabled_by_sw_if;
+  uword *bm_ip6_bypass_enabled_by_sw_if;
+
   /* convenience */
   vlib_main_t *vlib_main;
   vnet_main_t *vnet_main;
