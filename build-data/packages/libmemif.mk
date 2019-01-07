58e7527d659241b4796b8e33ab951e7016b73508
Mauro Sardara <msardara+fdio@cisco.com>
2019-01-07 11:46:19 +0100

    Update copyright year of modified files to 2019.
    Change-Id: I884899f4560c0b537200472986fd2e08a163fec3
    Signed-off-by: Mauro Sardara <msardara+fdio@cisco.com>

--- build-data/packages/libmemif.mk
+++ build-data/packages/libmemif.mk
@@ -1,4 +1,4 @@
-# Copyright (c) 2017-2018 Cisco and/or its affiliates.
+# Copyright (c) 2019 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
