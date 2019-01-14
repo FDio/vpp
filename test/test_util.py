a2504f71150b1b86ee446302252f41efc3082847
Paul Vinciguerra <pvinci@vinciconsulting.com>
2019-01-13 16:09:10 -0800

    VTL:  Allow running simple unittest.TestCases.
    It came to my attention that Ole added a simple test in:
    https://gerrit.fd.io/r/#/c/16381/ and the framework forced him
    to launch an instance of VPP to test the formatting of a mac address.
    
    This change allows the test framework to run standard unittest.TestCases
    without the need to spawn a VPP instance.
    
    Change-Id: I56651ab27c4c6bf920081a526f168a743d643201
    Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

--- test/test_util.py
+++ test/test_util.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-"""Test framework utilitty functions tests"""
+"""Test framework utility functions tests"""
 
 import unittest
-from framework import VppTestCase, VppTestRunner
+from framework import VppTestRunner
 from vpp_papi import mac_pton, mac_ntop
 
 
-class TestUtil (VppTestCase):
+class TestUtil (unittest.TestCase):
     """ MAC to binary and back """
     def test_mac_to_binary(self):
         mac = 'aa:bb:cc:dd:ee:ff'
@@ -14,6 +14,7 @@ class TestUtil (VppTestCase):
         mac2 = mac_ntop(b)
         self.assertEqual(type(mac), type(mac2))
         self.assertEqual(mac2, mac)
+
 
 if __name__ == '__main__':
     unittest.main(testRunner=VppTestRunner)
