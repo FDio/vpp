252ac7575452931d3f20eaae8d9d0b3fb2796066
Mohsin Kazmi <sykazmi@cisco.com>
2019-02-12 21:56:49 +0100

    vom: Add support for vtr in xconnect
    Change-Id: Ib168a1d20c4609e4e64f472f75e9463161638e3e
    Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>

--- extras/vom/vom/l2_binding_cmds.cpp
+++ extras/vom/vom/l2_binding_cmds.cpp
@@ -121,50 +121,6 @@ unbind_cmd::to_string() const
 
   return (s.str());
 }
-
-set_vtr_op_cmd::set_vtr_op_cmd(HW::item<l2_binding::l2_vtr_op_t>& item,
-                               const handle_t& itf,
-                               uint16_t tag)
-  : rpc_cmd(item)
-  , m_itf(itf)
-  , m_tag(tag)
-{
-}
-
-bool
-set_vtr_op_cmd::operator==(const set_vtr_op_cmd& other) const
-{
-  return (
-    (m_hw_item.data() == other.m_hw_item.data() && m_itf == other.m_itf) &&
-    (m_tag == other.m_tag));
-}
-
-rc_t
-set_vtr_op_cmd::issue(connection& con)
-{
-  msg_t req(con.ctx(), std::ref(*this));
-
-  auto& payload = req.get_request().get_payload();
-  payload.sw_if_index = m_itf.value();
-  payload.vtr_op = m_hw_item.data().value();
-  payload.push_dot1q = 1;
-  payload.tag1 = m_tag;
-
-  VAPI_CALL(req.execute());
-
-  return (wait());
-}
-
-std::string
-set_vtr_op_cmd::to_string() const
-{
-  std::ostringstream s;
-  s << "L2-set-vtr-op: " << m_hw_item.to_string()
-    << " itf:" << m_itf.to_string() << " tag:" << m_tag;
-
-  return (s.str());
-}
-
 }; // namespace l2_binding_cmds
 }; // namespace VOM
 
