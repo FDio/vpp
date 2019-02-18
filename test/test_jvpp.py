8864c4e5ac7bd8c1066278a1e19a53847b4a044e
Paul Vinciguerra <pvinci@vinciconsulting.com>
2019-02-18 06:55:45 -0800

    Tests: fix TypeErrors in test/test_jvpp.py
    Fixes:
    TypeError: __init__() takes at least 3 arguments (2 given).
    
    Due to a pycodestyle bug.
    
    Change-Id: I0f52086447d0ff87814e3ec79a4232c7a44e1b31
    Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

--- test/test_jvpp.py
+++ test/test_jvpp.py
@@ -16,6 +16,7 @@ REGISTRY_JAR_PREFIX = "jvpp-registry"
 @unittest.skipUnless(running_extended_tests, "part of extended tests")
 class TestJVpp(VppTestCase):
     """ JVPP Core Test Case """
+    DEFAULT_TIMEOUT = 10
 
     @classmethod
     def setUpClass(cls):
@@ -25,80 +26,75 @@ class TestJVpp(VppTestCase):
     def tearDownClass(cls):
         super(TestJVpp, cls).tearDownClass()
 
-    def invoke_for_jvpp_core(self, api_jar_name, test_class_name):
-        self.jvpp_connection_test(api_jar_name=api_jar_name,
-                                  test_class_name=test_class_name,
-                                  timeout=10)
-
     def test_vpp_core_callback_api(self):
         """ JVPP Core Callback Api Test Case """
-        self.invoke_for_jvpp_core(api_jar_name="jvpp-core",
+        self.jvpp_connection_test(api_jar_name="jvpp-core",
                                   test_class_name="io.fd.vpp.jvpp.core.test."
                                                   "CallbackApiTest")
 
     def test_vpp_core_future_api(self):
         """JVPP Core Future Api Test Case"""
-        self.invoke_for_jvpp_core(api_jar_name="jvpp-core",
+        self.jvpp_connection_test(api_jar_name="jvpp-core",
                                   test_class_name="io.fd.vpp.jvpp.core.test."
                                                   "FutureApiTest")
 
     def test_vpp_acl_callback_api(self):
         """ JVPP Acl Callback Api Test Case """
-        self.invoke_for_jvpp_core(api_jar_name="jvpp-acl",
+        self.jvpp_connection_test(api_jar_name="jvpp-acl",
                                   test_class_name="io.fd.vpp.jvpp.acl.test."
                                                   "CallbackApiTest")
 
     def test_vpp_acl_future_api(self):
         """JVPP Acl Future Api Test Case"""
-        self.invoke_for_jvpp_core(api_jar_name="jvpp-acl",
+        self.jvpp_connection_test(api_jar_name="jvpp-acl",
                                   test_class_name="io.fd.vpp.jvpp.acl.test."
                                                   "FutureApiTest")
 
     def test_vpp_ioamexport_callback_api(self):
         """ JVPP Ioamexport Callback Api Test Case """
-        self.invoke_for_jvpp_core(api_jar_name="jvpp-ioamexport",
+        self.jvpp_connection_test(api_jar_name="jvpp-ioamexport",
                                   test_class_name="io.fd.vpp.jvpp.ioamexport."
                                                   "test.CallbackApiTest")
 
     def test_vpp_ioamexport_future_api(self):
         """JVPP Ioamexport Future Api Test Case"""
-        self.invoke_for_jvpp_core(api_jar_name="jvpp-ioamexport",
+        self.jvpp_connection_test(api_jar_name="jvpp-ioamexport",
                                   test_class_name="io.fd.vpp.jvpp.ioamexport."
                                                   "test.FutureApiTest")
 
     def test_vpp_ioampot_callback_api(self):
         """ JVPP Ioampot Callback Api Test Case """
-        self.invoke_for_jvpp_core(api_jar_name="jvpp-ioampot",
+        self.jvpp_connection_test(api_jar_name="jvpp-ioampot",
                                   test_class_name="io.fd.vpp.jvpp.ioampot."
                                                   "test.CallbackApiTest")
 
     def test_vpp_ioampot_future_api(self):
         """JVPP Ioampot Future Api Test Case"""
-        self.invoke_for_jvpp_core(api_jar_name="jvpp-ioampot",
+        self.jvpp_connection_test(api_jar_name="jvpp-ioampot",
                                   test_class_name="io.fd.vpp.jvpp.ioampot."
                                                   "test.FutureApiTest")
 
     def test_vpp_ioamtrace_callback_api(self):
         """ JVPP Ioamtrace Callback Api Test Case """
-        self.invoke_for_jvpp_core(api_jar_name="jvpp-ioamtrace",
+        self.jvpp_connection_test(api_jar_name="jvpp-ioamtrace",
                                   test_class_name="io.fd.vpp.jvpp.ioamtrace."
                                                   "test.CallbackApiTest")
 
     def test_vpp_ioamtrace_future_api(self):
         """JVPP Ioamtrace Future Api Test Case"""
-        self.invoke_for_jvpp_core(api_jar_name="jvpp-ioamtrace",
+        self.jvpp_connection_test(api_jar_name="jvpp-ioamtrace",
                                   test_class_name="io.fd.vpp.jvpp.ioamtrace."
                                                   "test.FutureApiTest")
 
     def test_vpp_snat_callback_api(self):
         """ JVPP Snat Callback Api Test Case """
-        self.invoke_for_jvpp_core(api_jar_name="jvpp-nat",
+        self.jvpp_connection_test(api_jar_name="jvpp-nat",
                                   test_class_name="io.fd.vpp.jvpp.nat.test."
                                                   "CallbackApiTest")
 
     def test_vpp_snat_future_api(self):
         """JVPP Snat Future Api Test Case"""
-        self.invoke_for_jvpp_core(api_jar_name="jvpp-nat",
+        self.jvpp_connection_test(api_jar_name="jvpp-nat",
                                   test_class_name="io.fd.vpp.jvpp.nat.test."
                                                   "FutureApiTest")
 
@@ -106,7 +102,8 @@ class TestJVpp(VppTestCase):
         return os.path.join(install_dir, API_FILES_PATH,
                             "{0}-{1}.jar".format(jar_name, version))
 
-    def jvpp_connection_test(self, api_jar_name, test_class_name, timeout):
+    def jvpp_connection_test(self, api_jar_name, test_class_name,
+                             timeout=DEFAULT_TIMEOUT):
         install_dir = os.getenv('VPP_BUILD_DIR')
         self.logger.info("Install directory : {0}".format(install_dir))
 
@@ -143,10 +140,12 @@ class TestJVpp(VppTestCase):
         self.logger.info("Process output : {0}{1}".format(os.linesep, out))
 
         if self.process.returncode != 0:
-            raise subprocess.CalledProcessError(
+            # leave the redundant () of pycodestyle (2.4.0) will complain.
+            raise subprocess.CalledProcessError((
                 "Command {0} failed with return code: {1}.{2}"
                 "Process error output: {2}{3}"
-                .format(command, self.process.returncode, os.linesep, err))
+                .format(command, self.process.returncode,
+                        os.linesep, err)))  # noqa: E502
 
     def tearDown(self):
         self.logger.info("Tearing down jvpp test")
