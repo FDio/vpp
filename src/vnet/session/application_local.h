5f9192899656fbb22bdc4934d410e7f243c8e4c8
Florin Coras <fcoras@cisco.com>
2019-02-22 13:11:38 -0800

    session: refactor local/cut-through listens
    Introduce a cut-through transport as replacement to local sessions. This
    first patch removes the per app local listener pools and switches local
    listeners to sessions with cut-through transport.
    
    Change-Id: I98b20c25bc5df5f4110bf66f76c650a973032618
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/vnet/session/application_local.h
+++ src/vnet/session/application_local.h
@@ -20,9 +20,7 @@
 #include <vnet/session/application.h>
 
 local_session_t *application_local_listen_session_alloc (application_t * app);
-void application_local_listen_session_free (application_t * app,
-					    local_session_t * ll);
-void application_local_listener_session_endpoint (local_session_t * ll,
+void application_local_listener_session_endpoint (session_t * ll,
 						  session_endpoint_t * sep);
 
 local_session_t *app_worker_local_session_alloc (app_worker_t * app_wrk);
@@ -41,7 +39,7 @@ int app_worker_local_session_cleanup (app_worker_t * client_wrk,
 int app_worker_local_session_connect_notify (local_session_t * ls);
 int app_worker_local_session_connect (app_worker_t * client,
 				      app_worker_t * server,
-				      local_session_t * ls, u32 opaque);
+				      session_t * ls, u32 opaque);
 int app_worker_local_session_disconnect (u32 app_or_wrk,
 					 local_session_t * ls);
 int app_worker_local_session_disconnect_w_index (u32 app_or_wrk,
@@ -52,22 +50,6 @@ void app_worker_format_local_connects (app_worker_t * app, int verbose);
 
 void mq_send_local_session_disconnected_cb (u32 app_or_wrk,
 					    local_session_t * ls);
-
-always_inline local_session_t *
-application_get_local_listen_session (application_t * app, u32 session_index)
-{
-  return pool_elt_at_index (app->local_listen_sessions, session_index);
-}
-
-always_inline local_session_t *
-application_get_local_listener_w_handle (session_handle_t handle)
-{
-  u32 server_index, session_index;
-  application_t *app;
-  local_session_parse_handle (handle, &server_index, &session_index);
-  app = application_get (server_index);
-  return application_get_local_listen_session (app, session_index);
-}
 
 always_inline u8
 application_local_session_listener_has_transport (local_session_t * ls)
