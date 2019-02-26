bad0bcd442417f943f2205112128dabeab36bf34
Damjan Marion <damarion@cisco.com>
2019-02-26 17:15:48 +0100

    Fix crash in barrier sync when vlib_worker_threads is zero
    Change-Id: I6819dd9dbfc15c17740bdb98b51bdd639ef8c4d2
    Signed-off-by: Damjan Marion <damarion@cisco.com>
    (cherry picked from commit 8343ee5665942353e57ee074da62b9d07c1c510b)

--- src/vlib/threads.h
+++ src/vlib/threads.h
@@ -201,9 +201,10 @@ u32 vlib_frame_queue_main_init (u32 node_index, u32 frame_queue_nelts);
 #define BARRIER_SYNC_TIMEOUT (1.0)
 #endif
 
-#define vlib_worker_thread_barrier_sync(X) {vlib_worker_threads[0].barrier_caller=__FUNCTION__;vlib_worker_thread_barrier_sync_int(X);}
+#define vlib_worker_thread_barrier_sync(X) {vlib_worker_thread_barrier_sync_int(X, __FUNCTION__);}
 
-void vlib_worker_thread_barrier_sync_int (vlib_main_t * vm);
+void vlib_worker_thread_barrier_sync_int (vlib_main_t * vm,
+					  const char *func_name);
 void vlib_worker_thread_barrier_release (vlib_main_t * vm);
 void vlib_worker_thread_node_refork (void);
 
