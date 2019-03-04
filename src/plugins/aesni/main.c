7448608d6dd9c2e7fd00de26865349148ba69107
Damjan Marion <damarion@cisco.com>
2019-03-04 12:37:55 +0100

    crypto: scheduler and intel plugin
    Change-Id: I4c3fcccf55c36842b7b48aed260fef2802b5c54b
    Signed-off-by: Damjan Marion <damarion@cisco.com>

---
+++ src/plugins/aesni/main.c
@@ -0,0 +1,68 @@
+/*
+ *------------------------------------------------------------------
+ * Copyright (c) 2019 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *------------------------------------------------------------------
+ */
+
+#include <vlib/vlib.h>
+#include <vnet/plugin/plugin.h>
+#include <vnet/crypto/crypto.h>
+#include <x86intrin.h>
+#include <aesni/aesni.h>
+
+clib_error_t *
+crypto_intel_init (vlib_main_t * vm)
+{
+  vnet_crypto_register_ops_handler (vm, VNET_CRYPTO_ALG_AES_128_CBC,
+				    VNET_CRYPTO_OP_ENCRYPT,
+				    aesni_ops_enc_aes_cbc_128);
+
+  vnet_crypto_register_ops_handler (vm, VNET_CRYPTO_ALG_AES_192_CBC,
+				    VNET_CRYPTO_OP_ENCRYPT,
+				    aesni_ops_enc_aes_cbc_192);
+
+  vnet_crypto_register_ops_handler (vm, VNET_CRYPTO_ALG_AES_256_CBC,
+				    VNET_CRYPTO_OP_ENCRYPT,
+				    aesni_ops_enc_aes_cbc_256);
+
+  vnet_crypto_register_ops_handler (vm, VNET_CRYPTO_ALG_AES_128_CBC,
+				    VNET_CRYPTO_OP_DECRYPT,
+				    aesni_ops_dec_aes_cbc_128);
+
+  vnet_crypto_register_ops_handler (vm, VNET_CRYPTO_ALG_AES_192_CBC,
+				    VNET_CRYPTO_OP_DECRYPT,
+				    aesni_ops_dec_aes_cbc_192);
+
+  vnet_crypto_register_ops_handler (vm, VNET_CRYPTO_ALG_AES_256_CBC,
+				    VNET_CRYPTO_OP_DECRYPT,
+				    aesni_ops_dec_aes_cbc_256);
+
+  vnet_crypto_register_async_queue_handler (vm, VNET_CRYPTO_ALG_AES_128_CBC,
+					    VNET_CRYPTO_OP_ENCRYPT,
+					    aesni_queue_enc_aes_cbc_128);
+  vnet_crypto_register_async_queue_handler (vm, VNET_CRYPTO_ALG_AES_128_CBC,
+					    VNET_CRYPTO_OP_DECRYPT,
+					    aesni_queue_dec_aes_cbc_128);
+  return 0;
+}
+
+VLIB_INIT_FUNCTION (crypto_intel_init);
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
