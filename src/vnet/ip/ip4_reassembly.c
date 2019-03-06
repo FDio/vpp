d2ce323ebb575d1268b625ba6b94b6024a2134f4
Klement Sekera <ksekera@cisco.com>
2019-03-06 11:54:13 +0100

    reassembly: improve type safety
    Change-Id: I6f3dff73163392e97f636b8aeba35868420534fd
    Signed-off-by: Klement Sekera <ksekera@cisco.com>

--- src/vnet/ip/ip4_reassembly.c
+++ src/vnet/ip/ip4_reassembly.c
@@ -359,8 +359,7 @@ ip4_reass_find_or_create (vlib_main_t * vm, ip4_reass_main_t * rm,
   ip4_reass_t *reass = NULL;
   f64 now = vlib_time_now (rm->vlib_main);
 
-  if (!clib_bihash_search_16_8
-      (&rm->hash, (clib_bihash_kv_16_8_t *) kv, (clib_bihash_kv_16_8_t *) kv))
+  if (!clib_bihash_search_16_8 (&rm->hash, &kv->kv, &kv->kv))
     {
       if (vm->thread_index != kv->v.thread_index)
 	{
@@ -400,13 +399,13 @@ ip4_reass_find_or_create (vlib_main_t * vm, ip4_reass_main_t * rm,
       ++rt->reass_n;
     }
 
-  reass->key.as_u64[0] = ((clib_bihash_kv_16_8_t *) kv)->key[0];
-  reass->key.as_u64[1] = ((clib_bihash_kv_16_8_t *) kv)->key[1];
+  reass->key.as_u64[0] = kv->kv.key[0];
+  reass->key.as_u64[1] = kv->kv.key[1];
   kv->v.reass_index = (reass - rt->pool);
   kv->v.thread_index = vm->thread_index;
   reass->last_heard = now;
 
-  if (clib_bihash_add_del_16_8 (&rm->hash, (clib_bihash_kv_16_8_t *) kv, 1))
+  if (clib_bihash_add_del_16_8 (&rm->hash, &kv->kv, 1))
     {
       ip4_reass_free (rm, rt, reass);
       reass = NULL;
