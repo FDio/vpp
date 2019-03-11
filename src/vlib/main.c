733106ab05a98a1c1b3f10aa5d9c0313934430f7
Dave Barach <dbarach@cisco.com>
2019-03-11 09:57:38 -0400

    Perf tune handoff queue polling
    Change-Id: I5cfa0f6eee67156bf87907fcf8a39f16d68a0905
    Signed-off-by: Dave Barach <dave@barachs.net>

--- src/vlib/main.c
+++ src/vlib/main.c
@@ -1679,6 +1679,7 @@ vlib_main_or_worker_loop (vlib_main_t * vm, int is_main)
   u64 cpu_time_now;
   vlib_frame_queue_main_t *fqm;
   u32 *last_node_runtime_indices = 0;
+  u32 frame_queue_check_counter = 0;
 
   /* Initialize pending node vector. */
   if (is_main)
@@ -1734,8 +1735,26 @@ vlib_main_or_worker_loop (vlib_main_t * vm, int is_main)
       if (!is_main)
 	{
 	  vlib_worker_thread_barrier_check ();
-	  vec_foreach (fqm, tm->frame_queue_mains)
-	    vlib_frame_queue_dequeue (vm, fqm);
+	  if (PREDICT_FALSE (vm->check_frame_queues +
+			     frame_queue_check_counter))
+	    {
+	      u32 processed = 0;
+
+	      if (vm->check_frame_queues)
+		{
+		  frame_queue_check_counter = 100;
+		  vm->check_frame_queues = 0;
+		}
+
+	      vec_foreach (fqm, tm->frame_queue_mains)
+		processed += vlib_frame_queue_dequeue (vm, fqm);
+
+	      /* No handoff queue work found? */
+	      if (processed)
+		frame_queue_check_counter = 100;
+	      else
+		frame_queue_check_counter--;
+	    }
 	  if (PREDICT_FALSE (vm->worker_thread_main_loop_callback != 0))
 	    ((void (*)(vlib_main_t *)) vm->worker_thread_main_loop_callback)
 	      (vm);
