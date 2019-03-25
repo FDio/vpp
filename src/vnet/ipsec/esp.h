8b3abf7141e9a29313bbae0597d227ca3d22bbb8
Damjan Marion <damarion@cisco.com>
2019-03-25 15:54:40 +0100

    ipsec: esp-decrypt rework
    Change-Id: Icf83c876d0880d1872b84e0a3d34be654b76149f
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- src/vnet/ipsec/esp.h
+++ src/vnet/ipsec/esp.h
@@ -57,6 +57,7 @@ typedef CLIB_PACKED (struct {
 #define ESP_WINDOW_SIZE		(64)
 #define ESP_SEQ_MAX		(4294967295UL)
 #define ESP_MAX_BLOCK_SIZE	(16)
+#define ESP_MAX_IV_SIZE		(16)
 #define ESP_MAX_ICV_SIZE	(16)
 
 u8 *format_esp_header (u8 * s, va_list * args);
