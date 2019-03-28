95d788f5b79ac6e6aafc2de2214aea1a76c5f851
Damjan Marion <damarion@cisco.com>
2019-03-28 10:58:59 +0100

    ipsec: USE_EXTENDED_SEQ_NUM -> USE_ESN
    Change-Id: Ib828ea5106f3ae280e4ce233f2462dee363580b7
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- src/vnet/ipsec/esp.h
+++ src/vnet/ipsec/esp.h
@@ -184,7 +184,7 @@ esp_replay_advance_esn (ipsec_sa_t * sa, u32 seq)
 always_inline int
 esp_seq_advance (ipsec_sa_t * sa)
 {
-  if (PREDICT_TRUE (ipsec_sa_is_set_USE_EXTENDED_SEQ_NUM (sa)))
+  if (PREDICT_TRUE (ipsec_sa_is_set_USE_ESN (sa)))
     {
       if (PREDICT_FALSE (sa->seq == ESP_SEQ_MAX))
 	{
@@ -224,7 +224,7 @@ hmac_calc (vlib_main_t * vm, ipsec_sa_t * sa, u8 * data, int data_len,
   op->dst = signature;
   op->hmac_trunc_len = sa->integ_trunc_size;
 
-  if (ipsec_sa_is_set_USE_EXTENDED_SEQ_NUM (sa))
+  if (ipsec_sa_is_set_USE_ESN (sa))
     {
       u32 seq_hi = clib_host_to_net_u32 (sa->seq_hi);
 
