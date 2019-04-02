10123e23c6b987d174bab48b4b5cab64d94208b8
Neale Ranns <nranns@cisco.com>
2019-04-02 08:13:33 +0000

    ipsec: bind an SA to a worker
    the sequence number increment and the anti-replay window
    checks must be atomic. Given the vector nature of VPP we
    can't simply use atomic increments for sequence numbers,
    since a vector on thread 1 with lower sequence numbers could
    be 'overtaken' by packets on thread 2 with higher sequence
    numbers.
    The anti-replay logic requires a critical section, not just
    atomics, and we don't want that.
    So when the SA see the first packet it is bound to that worker
    all subsequent packets, that arrive on a different worker,
    are subject to a handoff.
    
    Type: feature
    
    Change-Id: Ia20a8645fb50622ea6235ab015a537f033d531a4
    Signed-off-by: Neale Ranns <nranns@cisco.com>

--- src/vnet/ipsec/ipsec_sa.c
+++ src/vnet/ipsec/ipsec_sa.c
@@ -163,6 +163,8 @@ ipsec_sa_add_and_lock (u32 id,
   sa->protocol = proto;
   sa->flags = flags;
   sa->salt = salt;
+  sa->encrypt_thread_index = (vlib_num_workers ())? ~0 : 0;
+  sa->decrypt_thread_index = (vlib_num_workers ())? ~0 : 0;
   if (integ_alg != IPSEC_INTEG_ALG_NONE)
     {
       ipsec_sa_set_integ_alg (sa, integ_alg);
