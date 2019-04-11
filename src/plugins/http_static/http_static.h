74f9de0d5836995d5f079f1ded00a6781ca5879a
Dave Barach <dave@barachs.net>
2019-04-11 17:06:21 -0400

    Static html server
    Good enough to serve the vpp sphinx and doxygen docs. Knows about
    html, css, and javascript files.
    
    Change-Id: Ib18c19f07f35f91ba935ea26ed7be406dacf2205
    Signed-off-by: Dave Barach <dave@barachs.net>

---
+++ src/plugins/http_static/http_static.h
@@ -0,0 +1,56 @@
+
+/*
+ * http_static.h - skeleton vpp engine plug-in header file
+ *
+ * Copyright (c) <current-year> <your-organization>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef __included_http_static_h__
+#define __included_http_static_h__
+
+#include <vnet/vnet.h>
+#include <vnet/ip/ip.h>
+#include <vnet/ethernet/ethernet.h>
+
+#include <vppinfra/hash.h>
+#include <vppinfra/error.h>
+#include <vppinfra/time_range.h>
+
+typedef struct
+{
+  /* API message ID base */
+  u16 msg_id_base;
+
+  /* convenience */
+  vlib_main_t *vlib_main;
+  vnet_main_t *vnet_main;
+} http_static_main_t;
+
+extern http_static_main_t http_static_main;
+
+extern vlib_node_registration_t http_static_node;
+
+/* Periodic function events */
+#define HTTP_STATIC_EVENT1 1
+#define HTTP_STATIC_EVENT2 2
+#define HTTP_STATIC_EVENT_PERIODIC_ENABLE_DISABLE 3
+
+#endif /* __included_http_static_h__ */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
