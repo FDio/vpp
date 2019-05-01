abfd778a92a0050178e966e9f29dfc1497b8ca58
jdenisco <jdenisco@cisco.com>
2019-05-01 15:27:40 -0400

    Add the packages for 19.04
    Change-Id: I167bb7d86bbb5cba0b265978b4f8faf271b77411
    Signed-off-by: jdenisco <jdenisco@cisco.com>

--- extras/vpp_config/setup.py
+++ extras/vpp_config/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="vpp_config",
-      version="19.01.4",
+      version="19.04.2",
       author="John DeNisco",
       author_email="jdenisco@cisco.com",
       description="VPP Configuration Utility",
