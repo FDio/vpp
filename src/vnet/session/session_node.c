9c1aef6026992d252a09aea3a3ec5af4cea1436b
Florin Coras <fcoras@cisco.com>
2019-05-07 16:28:16 -0700

    session: send tx events when data is dequeued
    Change-Id: Ib8cb19361c42e38e3f68d7147358378fff161eb1
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/vnet/session/session_node.c
+++ src/vnet/session/session_node.c
@@ -792,6 +792,9 @@ session_tx_fifo_read_and_snd_i (vlib_main_t * vm, vlib_node_runtime_t * node,
       else if (svm_fifo_max_dequeue_cons (ctx->s->tx_fifo) > 0)
 	if (svm_fifo_set_event (ctx->s->tx_fifo))
 	  vec_add1 (wrk->pending_event_vector, *e);
+
+      if (svm_fifo_needs_tx_ntf (ctx->s->tx_fifo, ctx->max_len_to_snd ))
+        session_dequeue_notify (ctx->s);
     }
   return SESSION_TX_OK;
 }
@@ -819,11 +822,30 @@ session_tx_fifo_dequeue_internal (vlib_main_t * vm,
 				  session_event_t * e, int *n_tx_pkts)
 {
   session_t *s = wrk->ctx.s;
+  int n_sent, n_deq;
+  svm_fifo_t *f;
 
   if (PREDICT_FALSE (s->session_state >= SESSION_STATE_TRANSPORT_CLOSED))
     return 0;
-  svm_fifo_unset_event (s->tx_fifo);
-  return transport_custom_tx (session_get_transport_proto (s), s);
+
+  f = s->tx_fifo;
+  n_deq = svm_fifo_max_dequeue_cons (f);
+  svm_fifo_unset_event (f);
+
+  n_sent = transport_custom_tx (session_get_transport_proto (s), s);
+  if ((n_sent < 0))
+    return SESSION_TX_NO_DATA;
+
+  if (!n_sent)
+    {
+      n_sent = (int) svm_fifo_max_dequeue_cons (f) - n_deq;
+      n_sent = clib_max (n_sent, 0);
+    }
+
+  if (svm_fifo_needs_tx_ntf (s->tx_fifo, n_sent))
+    session_dequeue_notify (s);
+
+  return 0;
 }
 
 always_inline session_t *
@@ -905,9 +927,8 @@ session_queue_node_fn (vlib_main_t * vm, vlib_node_runtime_t * node,
 
   for (i = 0; i < n_events; i++)
     {
-      session_t *s;		/* $$$ prefetch 1 ahead maybe */
       session_event_t *e;
-      u8 need_tx_ntf;
+      session_t *s;
 
       e = &fifo_events[i];
       switch (e->event_type)
@@ -933,14 +954,15 @@ session_queue_node_fn (vlib_main_t * vm, vlib_node_runtime_t * node,
 	   * different nodes */
 	  rv = (smm->session_tx_fns[s->session_type]) (vm, node, wrk, e,
 						       &n_tx_packets);
-	  if (PREDICT_TRUE (rv == SESSION_TX_OK))
-	    {
-	      need_tx_ntf = svm_fifo_needs_tx_ntf (s->tx_fifo,
-						   wrk->ctx.max_len_to_snd);
-	      if (PREDICT_FALSE (need_tx_ntf))
-		session_dequeue_notify (s);
-	    }
-	  else if (PREDICT_FALSE (rv == SESSION_TX_NO_BUFFERS))
+//	  if (PREDICT_TRUE (rv == SESSION_TX_OK))
+//	    {
+//	      need_tx_ntf = svm_fifo_needs_tx_ntf (s->tx_fifo,
+//						   wrk->ctx.max_len_to_snd);
+//	      if (PREDICT_FALSE (need_tx_ntf))
+//		session_dequeue_notify (s);
+//	    }
+//	  else
+	  if (PREDICT_FALSE (rv == SESSION_TX_NO_BUFFERS))
 	    {
 	      vlib_node_increment_counter (vm, node->node_index,
 					   SESSION_QUEUE_ERROR_NO_BUFFER, 1);
