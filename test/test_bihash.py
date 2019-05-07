4b2bdf636d1a596461010d9dbbe83059be630a78
Dave Barach <dave@barachs.net>
2019-05-07 10:30:18 -0400

    Improve lcov reported code coverage stats
    Remove unused code from the vppinfra build
    Add a bihash test case
    
    Change-Id: Ia930309efa28620dd1c0d69aaec432e2d8bd962c
    Signed-off-by: Dave Barach <dave@barachs.net>

--- test/test_bihash.py
+++ test/test_bihash.py
@@ -25,7 +25,7 @@ class TestBihash(VppTestCase):
 
     def test_bihash_unittest(self):
         """ Bihash Add/Del Test """
-        error = self.vapi.cli("test bihash ")
+        error = self.vapi.cli("test bihash careful 0 verbose 0")
 
         if error:
             self.logger.critical(error)
@@ -34,11 +34,32 @@ class TestBihash(VppTestCase):
     def test_bihash_thread(self):
         """ Bihash Thread Test """
 
-        error = self.vapi.cli("test bihash threads 2 nbuckets 64000")
+        error = self.vapi.cli("test bihash threads 2 nbuckets"
+                              + " 64000 careful 0 verbose 0")
 
         if error:
             self.logger.critical(error)
             self.assertNotIn('failed', error)
+
+    def test_bihash_vec64(self):
+        """ Bihash vec64 Test """
+
+        error = self.vapi.cli("test bihash vec64")
+
+        if error:
+            self.logger.critical(error)
+            self.assertNotIn('failed', error)
+
+    def test_bihash_coverage(self):
+        """ Improve Code Coverage """
+
+        error = self.vapi.cli("test bihash nitems 10 ncycles 3" +
+                              "search 2 careful 1 verbose 2 non-random-keys")
+
+        if error:
+            self.logger.critical(error)
+            self.assertNotIn('failed', error)
+
 
 if __name__ == '__main__':
     unittest.main(testRunner=VppTestRunner)
