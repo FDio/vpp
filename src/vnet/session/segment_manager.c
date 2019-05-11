6f8513c95c4784d727cd6aebd16f8c581362cb7e
Florin Coras <fcoras@cisco.com>
2019-05-11 16:55:53 -0700

    svm: track fifo segment memory and improve fifo alloc
    Change-Id: If18dba7ab856272c9f565d36ac36365139793e0b
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/vnet/session/segment_manager.c
+++ src/vnet/session/segment_manager.c
@@ -102,7 +102,7 @@ segment_manager_add_segment (segment_manager_t * sm, u32 segment_size)
     }
 
   /*
-   * Allocate fifo segment and lock if needed
+   * Allocate fifo segment and grab lock if needed
    */
   if (vlib_num_workers ())
     clib_rwlock_writer_lock (&sm->segments_rwlock);
@@ -110,13 +110,14 @@ segment_manager_add_segment (segment_manager_t * sm, u32 segment_size)
   pool_get_zero (sm->segments, fs);
 
   /*
-   * Initialize ssvm segment and svm fifo private header
+   * Allocate ssvm segment
    */
   segment_size = segment_size ? segment_size : props->add_segment_size;
   page_size = clib_mem_get_page_size ();
   /* Protect against segment size u32 wrap */
   segment_size = clib_max (segment_size + page_size - 1, segment_size);
   segment_size = segment_size & ~(page_size - 1);
+
   if (props->segment_type != SSVM_SEGMENT_PRIVATE)
     {
       seg_name = format (0, "%d-%d%c", getpid (), smm->seg_name_counter++, 0);
@@ -147,6 +148,9 @@ segment_manager_add_segment (segment_manager_t * sm, u32 segment_size)
       goto done;
     }
 
+  /*
+   * Initialize fifo segment
+   */
   fifo_segment_init (fs);
 
   /*
@@ -761,6 +765,7 @@ segment_manager_alloc_queue (fifo_segment_t * segment,
 
   oldheap = ssvm_push_heap (segment->ssvm.sh);
   q = svm_msg_q_alloc (cfg);
+  fifo_segment_update_free_bytes (segment);
   ssvm_pop_heap (oldheap);
 
   if (props->use_mq_eventfd)
