ab21eb32262af0985362e746fdd496d141b2f23c
Dave Wallace <dwallacelf@gmail.com>
2019-05-15 21:41:23 -0400

    make test: Add QUIC serial transfer test
    Change-Id: I7ed68dd199daa31269d45d67e0a4fa632e1a08d1
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- test/test_quic.py
+++ test/test_quic.py
@@ -193,6 +193,42 @@ class QUICInternalEchoIPv4TestCase(QUICTestCase):
                                          ["no-output", "mbytes", "10"])
 
 
+class QUICInternalSerialEchoIPv4TestCase(QUICTestCase):
+    """ QUIC Internal Serial Echo IPv4 Transfer Test Cases """
+
+    @classmethod
+    def setUpClass(cls):
+        super(QUICInternalSerialEchoIPv4TestCase, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        super(QUICInternalSerialEchoIPv4TestCase, cls).tearDownClass()
+
+    def setUp(self):
+        super(QUICInternalSerialEchoIPv4TestCase, self).setUp()
+        self.thru_host_stack_ipv4_setup()
+
+    def tearDown(self):
+        self.thru_host_stack_ipv4_tear_down()
+        super(QUICInternalSerialEchoIPv4TestCase, self).tearDown()
+
+    def show_commands_at_teardown(self):
+        self.logger.debug(self.vapi.cli("show session verbose 2"))
+
+    @unittest.skipUnless(running_extended_tests, "part of extended tests")
+    def test_quic_serial_internal_transfer(self):
+        """ QUIC serial internal echo client/server transfer """
+
+        client_args = (self.client_echo_test_args +
+                       ["no-output", "mbytes", "10"])
+        self.internal_ipv4_transfer_test(self.server_echo_test_args,
+                                         client_args)
+        self.start_internal_echo_client(client_args)
+        self.start_internal_echo_client(client_args)
+        self.start_internal_echo_client(client_args)
+        self.start_internal_echo_client(client_args)
+
+
 class QUICInternalEchoIPv4MultiStreamTestCase(QUICTestCase):
     """ QUIC Internal Echo IPv4 Transfer Test Cases """
 
