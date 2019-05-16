aa1127f21f1713028ba76ea1102c0d3c790b8ef0
Nathan Skrzypczak <nathan.skrzypczak@gmail.com>
2019-05-16 14:38:44 +0200

    vcl: add QUIC support
    Type: feature
    
    * Adds the concept of a "connectable listener" : a session that
     can be both connected and accepted on.
    * vppcom_session_is_connectable_listener (fd) that tells if the fd
    is a connectable listener
    * vppcom_session_listener (fd) that gives you the listener's fd
    that accepted the session (if any)
    * vppcom_session_n_accepted (fd) that gives the number
    of sessions a listener accepted.
    
    Change-Id: Id89d67d8339fb15a7cf7e00a9c5448175eca04fc
    Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

--- src/vcl/vppcom.h
+++ src/vcl/vppcom.h
@@ -16,6 +16,7 @@
 #ifndef included_vppcom_h
 #define included_vppcom_h
 
+#include <string.h>
 #include <netdb.h>
 #include <errno.h>
 #include <sys/poll.h>
@@ -44,6 +45,8 @@ extern "C"
 #define VPPCOM_ENV_APP_SCOPE_GLOBAL          	"VCL_APP_SCOPE_GLOBAL"
 #define VPPCOM_ENV_VPP_API_SOCKET           	"VCL_VPP_API_SOCKET"
 
+#define INVALID_HANDLE ((u64)~0)
+
 typedef enum
 {
   VPPCOM_PROTO_TCP = 0,
@@ -51,7 +54,8 @@ typedef enum
   VPPCOM_PROTO_SCTP,
   VPPCOM_PROTO_NONE,
   VPPCOM_PROTO_TLS,
-  VPPCOM_PROTO_UDPC
+  VPPCOM_PROTO_UDPC,
+  VPPCOM_PROTO_QUIC,
 } vppcom_proto_t;
 
 static inline char *
@@ -76,11 +80,26 @@ vppcom_proto_str (vppcom_proto_t proto)
     case VPPCOM_PROTO_UDPC:
       proto_str = "UDPC";
       break;
+    case VPPCOM_PROTO_QUIC:
+      proto_str = "QUIC";
+      break;
     default:
       proto_str = "UNKNOWN";
       break;
     }
   return proto_str;
+}
+
+static inline int
+vcl_proto_is_dgram (uint8_t proto)
+{
+  return proto == VPPCOM_PROTO_UDP || proto == VPPCOM_PROTO_UDPC;
+}
+
+static inline int
+vcl_proto_is_accepted_session_listener (uint8_t proto)
+{
+  return proto == VPPCOM_PROTO_QUIC;
 }
 
 typedef enum
@@ -95,6 +114,7 @@ typedef struct vppcom_endpt_t_
   uint8_t is_ip4;
   uint8_t *ip;
   uint16_t port;
+  uint64_t transport_opts;
 } vppcom_endpt_t;
 
 typedef uint32_t vcl_session_handle_t;
@@ -254,6 +274,8 @@ extern int vppcom_session_accept (uint32_t session_handle,
 
 extern int vppcom_session_connect (uint32_t session_handle,
 				   vppcom_endpt_t * server_ep);
+extern int vppcom_session_stream_connect (uint32_t session_handle,
+					  uint32_t parent_session_handle);
 extern int vppcom_session_read (uint32_t session_handle, void *buf, size_t n);
 extern int vppcom_session_write (uint32_t session_handle, void *buf,
 				 size_t n);
@@ -294,6 +316,10 @@ extern int vppcom_session_tls_add_key (uint32_t session_handle, char *key,
 				       uint32_t key_len);
 extern int vppcom_data_segment_copy (void *buf, vppcom_data_segments_t ds,
 				     uint32_t max_bytes);
+extern int vppcom_unformat_proto (uint8_t * proto, char *proto_str);
+extern int vppcom_session_is_connectable_listener (uint32_t session_handle);
+extern int vppcom_session_listener (uint32_t session_handle);
+extern int vppcom_session_n_accepted (uint32_t session_handle);
 
 /**
  * Request from application to register a new worker
