6159df5ef61e7a8846ff7497b7dbbddb0cce170c
Andrew Yourtchenko <ayourtch@gmail.com>
2019-05-21 11:16:46 +0000

    make: add the first part of dot-release automation script + installability docker-based test
    Change-Id: Id4026e03e0bf9af20771d71d6d094c89e7ef8955
    Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

---
+++ extras/release-mgmt/docker-tests/start.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -eu
+
+/usr/bin/vpp unix { cli-listen /run/vpp/cli.sock } 2> /dev/null
+
+sleep 1
+
+VPP_CHECK_VERSION="$(cat /VPP_CHECK_VERSION.txt)"
+echo "Checking the running VPP for version v${VPP_CHECK_VERSION}"
+
+vppctl show version
+VPP_SHOW_VER=$(vppctl show version | cut -d' ' -f2)
+if [ "$VPP_SHOW_VER" != "v$VPP_CHECK_VERSION" ] ; then
+  echo "WRONG VPP version: $VPP_SHOW_VER"
+  echo
+  exit 1
+fi
+
+WRONG_VER="$(vppctl show plugins | grep -v ${VPP_CHECK_VERSION} | grep -v Description | grep -v 'Plugin path is' | tr -d '[\r\n]' )"
+if [ -n "$WRONG_VER" ] ; then
+  echo "Plugins with wrong version:"
+  vppctl show plugins | gawk -e "!/${VPP_CHECK_VERSION}/{print \$0}"
+  echo
+  exit 2
+fi
+
+
+
+
