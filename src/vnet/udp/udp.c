3df5682e845ee71d3671a06c153f04b1b382a6fd
Dave Wallace <dwallacelf@gmail.com>
2019-05-22 18:56:57 -0400

    fix show session verbose crash with udp transport
    Type: fix
    
    Change-Id: Ib3a2777317f8c57e91ce43820ad7ca5d10ac8677
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- src/vnet/udp/udp.c
+++ src/vnet/udp/udp.c
@@ -238,6 +238,7 @@ format_udp_session (u8 * s, va_list * args)
 u8 *
 format_udp_half_open_session (u8 * s, va_list * args)
 {
+  u32 __clib_unused tci = va_arg (*args, u32);
   clib_warning ("BUG");
   return 0;
 }
@@ -246,6 +247,7 @@ u8 *
 format_udp_listener_session (u8 * s, va_list * args)
 {
   u32 tci = va_arg (*args, u32);
+  u32 __clib_unused verbose = va_arg (*args, u32);
   udp_connection_t *uc = udp_listener_get (tci);
   return format (s, "%U", format_udp_connection, uc);
 }
