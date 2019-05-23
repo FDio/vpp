3d88fbc6ae7fe5351d46044255cc6e7a64cad5bb
Paul Vinciguerra <pvinci@vinciconsulting.com>
2019-05-23 17:41:22 -0400

    API: Fix missing attribute in memclnt_delete.
    logging(DEBUG) under vpp_papi identified:
      'memclnt_delete' object has no attribute 'context'
    
    (What should the api version be bumped to in this case?)
    
    {0} test.api.test_memclnt_api.TestMemclntApiSingleThread.test_memclnt_delete [0.000575s] ... FAILED
    
    Captured traceback:
    ~~~~~~~~~~~~~~~~~~
        Traceback (most recent call last):
          File "/vpp/test/api/test_memclnt_api.py", line 36, in test_memclnt_delete
            do_cleanup=1)
          File "/vpp/.tox/py27/local/lib/python2.7/site-packages/vpp_papi/vpp_papi.py", line 101, in __call__
            return self._func(**kwargs)
          File "/vpp/.tox/py27/local/lib/python2.7/site-packages/vpp_papi/vpp_papi.py", line 416, in f
            return self._call_vpp(i, msg, multipart, **kwargs)
          File "/vpp/.tox/py27/local/lib/python2.7/site-packages/vpp_papi/vpp_papi.py", line 620, in _call_vpp
            self.validate_args(msgdef, kwargs)
          File "/vpp/.tox/py27/local/lib/python2.7/site-packages/vpp_papi/vpp_papi.py", line 588, in validate_args
            .format(list(d), msg.name))
        vpp_papi.vpp_papi.VPPValueError: Invalid argument ['context'] to memclnt_delete
    
    Change-Id: I6b986588048250b6039097073036094f9f5b8679
    Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

---
+++ test/test_memclnt_api.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+#  Copyright (c) 2019. Vinci Consulting Corp. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import logging
+import time
+import unittest
+
+import framework
+
+
+class TestMemclntApiSingleThread(framework.VppTestCase):
+    """TestMemclntApiSingleThread"""
+    VPP_TIMEOUT = 10
+
+    @classmethod
+    def setUpClass(cls):
+        cls.start_time_ticks = time.time()
+        super(TestMemclntApiSingleThread, cls).setUpClass()
+
+    def test_memclnt_delete(self):
+        """test_memclnt_delete()"""
+        rv = self.vapi.memclnt_delete(index=1,
+                                      handle=1,
+                                      do_cleanup=1)
+        self.assertZero(rv.retval)
