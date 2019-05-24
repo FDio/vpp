6af82438144fca93af4df4c2808827dbbc392699
John Lo <loj@cisco.com>
2019-05-24 07:16:12 -0400

    Fix congestion recovery check
    Change-Id: Ia9347c8cb6175c6bec3f9c27a7e0a71c4e58fe28
    Signed-off-by: John Lo <loj@cisco.com>

--- src/vnet/tcp/tcp_input.c
+++ src/vnet/tcp/tcp_input.c
@@ -1526,8 +1526,10 @@ tcp_rcv_ack (tcp_worker_ctx_t * wrk, tcp_connection_t * tc, vlib_buffer_t * b,
       /* When we entered cong recovery, we reset snd_nxt to snd_una. Seems
        * peer still has the data so accept the ack */
       if (tcp_in_cong_recovery (tc)
-	  && seq_leq (vnet_buffer (b)->tcp.ack_number,
-		      tc->snd_una + tc->snd_wnd))
+	  && (seq_leq (vnet_buffer (b)->tcp.ack_number,
+		       tc->snd_una + tc->snd_wnd)
+	      || seq_leq (vnet_buffer (b)->tcp.ack_number,
+			  tc->snd_congestion)))
 	{
 	  tc->snd_nxt = vnet_buffer (b)->tcp.ack_number;
 	  if (seq_gt (tc->snd_nxt, tc->snd_una_max))
