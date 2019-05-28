a24ea902f3de5d8aa47de71d16ea6c2c7aa69892
Chenmin Sun <chenmin.sun@intel.com>
2019-05-28 23:05:46 +0800

    dpdk: bump DPDK version to 19.05
    Change-Id: I8b1f916b18df3bba205875a05ad95b1a41806dac
    Signed-off-by: Chenmin Sun <chenmin.sun@intel.com>

--- build/external/packages/dpdk.mk
+++ build/external/packages/dpdk.mk
@@ -21,12 +21,13 @@ DPDK_MLX5_PMD                ?= n
 DPDK_TAP_PMD                 ?= n
 DPDK_FAILSAFE_PMD            ?= n
 
-DPDK_VERSION                 ?= 19.02
+DPDK_VERSION                 ?= 19.05
 DPDK_BASE_URL                ?= http://fast.dpdk.org/rel
 DPDK_TARBALL                 := dpdk-$(DPDK_VERSION).tar.xz
 DPDK_TAR_URL                 := $(DPDK_BASE_URL)/$(DPDK_TARBALL)
 DPDK_18.11_TARBALL_MD5_CKSUM := 04b86f4a77f4f81a7fbd26467dd2ea9f
 DPDK_19.02_TARBALL_MD5_CKSUM := 23944a2cdee061aa4bd72ebe7d836db0
+DPDK_19.05_TARBALL_MD5_CKSUM := fe22ad1bab1539945119047b0fdf1105
 MACHINE=$(shell uname -m)
 
 # replace dot with space, and if 3rd word exists we deal with stable dpdk rel
@@ -220,9 +221,9 @@ $(B)/custom-config: $(B)/.dpdk-patch.ok Makefile
 	$(call set,RTE_LIBRTE_GRO,n)
 	$(call set,RTE_LIBRTE_KNI,n)
 	$(call set,RTE_LIBRTE_BPF,n)
-	$(call set,RTE_LIBRTE_RAWDEV,n)
-	$(call set,RTE_LIBRTE_PMD_IFPGA_RAWDEV,n)
-	$(call set,RTE_LIBRTE_IFPGA_BUS,n)
+	$(call set,RTE_LIBRTE_RAWDEV,y)
+	$(call set,RTE_LIBRTE_PMD_IFPGA_RAWDEV,y)
+	$(call set,RTE_LIBRTE_IFPGA_BUS,y)
 	$(call set,RTE_LIBRTE_BBDEV,n)
 	$(call set,RTE_LIBRTE_BBDEV_NULL,n)
 	$(call set,RTE_TEST_PMD,n)
