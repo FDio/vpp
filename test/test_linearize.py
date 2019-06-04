0355574e2917c8ac7e309d18bdfec73df21dab8e
Klement Sekera <ksekera@cisco.com>
2019-06-04 21:14:26 +0200

    buffer_funcs: fix crash this change is being made to fix a crash
    Ticket: N/A
    Type: fix
    Fixes: f883f6a1132ad4bb7aa9d9a79d420274fbcf3b64
    
    Change-Id: Ia4ede823f673780e0c30d075b091db42e183650d
    Signed-off-by: Klement Sekera <ksekera@cisco.com>

---
+++ test/test_linearize.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from framework import VppTestCase
+
+class TestLinearize(VppTestCase):
+    """ Buffer Linearization """
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestLinearize, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        super(TestLinearize, cls).tearDownClass()
+
+    def setUp(self):
+        super(TestLinearize, self).setUp()
+
+    def tearDown(self):
+        super(TestLinearize, self).tearDown()
+
+    def test_linearize(self):
+        self.vapi.cli("test linearize")
