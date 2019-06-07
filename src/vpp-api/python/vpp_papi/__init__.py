ae8819f0a426953aa7ebf97c2e26940525b55fb1
Paul Vinciguerra <pvinci@vinciconsulting.com>
2019-06-07 13:35:37 -0400

    misc: vpp_papi- add tests, clean up pep8
    Type: test
    
    Change-Id: Ic81bd4a9eba3c89a746e7a9b9e471b59cd87fa40
    Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

--- src/vpp-api/python/vpp_papi/__init__.py
+++ src/vpp-api/python/vpp_papi/__init__.py
@@ -1,1 +1,10 @@
-from .vpp_papi import *
+from .vpp_papi import FuncWrapper, VPP, VppApiDynamicMethodHolder  # noqa: F401
+from .vpp_papi import VppEnum, VppEnumType  # noqa: F401
+from .vpp_papi import VPPIOError, VPPRuntimeError, VPPValueError  # noqa: F401
+from .vpp_papi import VPPApiClient  # noqa: F401
+from . macaddress import MACAddress, mac_pton, mac_ntop  # noqa: F401
+
+# sorted lexicographically
+from .vpp_serializer import BaseTypes  # noqa: F401
+from .vpp_serializer import VPPEnumType, VPPType, VPPTypeAlias  # noqa: F401
+from .vpp_serializer import VPPMessage, VPPUnionType  # noqa: F401
