22eeb0d1866d4c60c5158ae52744581e14f4902f
Ole Troan <ot@cisco.com>
2019-06-17 20:37:48 +0200

    api: add new stream message convention
    Instead of having to wrap dump/detail calls in control ping, send details messages in between a normal
    reply / request pair. As expressed in the below service statement.
    
    Example:
    
    service {
      rpc map_domain_dump returns map_domain_dump_reply
        stream map_domain_details;
    };
    
    autoreply define map_domain_dump
    {
      u32 client_index;
      u32 context;
    };
    
    define map_domain_details
    {
      u32 context;
      u32 domain_index;
      vl_api_ip6_prefix_t ip6_prefix;
      vl_api_ip4_prefix_t ip4_prefix;
      vl_api_ip6_prefix_t ip6_src;
      u8 ea_bits_len;
      u8 psid_offset;
      u8 psid_length;
      u8 flags;
      u16 mtu;
      string tag[limit=64];
    };
    
    Change-Id: Iad9f6b41b0ef886adb584c97708dd91cf552749e
    Type: make
    Signed-off-by: Ole Troan <ot@cisco.com>

--- src/plugins/map/map_api.c
+++ src/plugins/map/map_api.c
@@ -103,15 +103,11 @@ vl_api_map_add_del_rule_t_handler (vl_api_map_add_del_rule_t * mp)
 static void
 vl_api_map_domain_dump_t_handler (vl_api_map_domain_dump_t * mp)
 {
-  vl_api_map_domain_details_t *rmp;
   map_main_t *mm = &map_main;
   map_domain_t *d;
   map_domain_extra_t *de;
   vl_api_registration_t *reg;
   u32 map_domain_index;
-
-  if (pool_elts (mm->domains) == 0)
-    return;
 
   reg = vl_api_client_index_to_registration (mp->client_index);
   if (!reg)
@@ -124,6 +120,7 @@ vl_api_map_domain_dump_t_handler (vl_api_map_domain_dump_t * mp)
     de = vec_elt_at_index(mm->domain_extras, map_domain_index);
 
     /* Make sure every field is initiated (or don't skip the clib_memset()) */
+    vl_api_map_domain_details_t *rmp;
     rmp = vl_msg_api_alloc (sizeof (*rmp) + vec_len(de->tag));
 
     rmp->_vl_msg_id = htons(VL_API_MAP_DOMAIN_DETAILS + mm->msg_id_base);
@@ -146,6 +143,9 @@ vl_api_map_domain_dump_t_handler (vl_api_map_domain_dump_t * mp)
     vl_api_send_msg (reg, (u8 *) rmp);
   }));
   /* *INDENT-ON* */
+  vl_api_map_domain_dump_reply_t *rmp;
+  int rv = 0;
+  REPLY_MACRO (VL_API_MAP_DOMAIN_DUMP_REPLY);
 }
 
 static void
