118be9d8c0bd8a79ddb32b4cd4f012cc2a4a634a
Ole Troan <ot@cisco.com>
2019-06-17 20:37:48 +0200

    api: add new stream message convention
    Instead of having to wrap dump/detail calls in control ping, send details messages in between a normal
    reply / request pair. As expressed in the below service statement.
    
    Example:
    
    service {
      rpc map_domain_dump returns map_domain_dump_reply
        stream map_domain_details;
    };
    
    autoreply define map_domain_dump
    {
      u32 client_index;
      u32 context;
    };
    
    define map_domain_details
    {
      u32 context;
      u32 domain_index;
      vl_api_ip6_prefix_t ip6_prefix;
      vl_api_ip4_prefix_t ip4_prefix;
      vl_api_ip6_prefix_t ip6_src;
      u8 ea_bits_len;
      u8 psid_offset;
      u8 psid_length;
      u8 flags;
      u16 mtu;
      string tag[limit=64];
    };
    
    Change-Id: Iad9f6b41b0ef886adb584c97708dd91cf552749e
    Type: infra
    Signed-off-by: Ole Troan <ot@cisco.com>

--- src/tools/vppapigen/vppapigen_json.py
+++ src/tools/vppapigen/vppapigen_json.py
@@ -20,6 +20,8 @@ def walk_services(s):
         d = {'reply': e.reply}
         if e.stream:
             d['stream'] = True
+        if e.stream_message:
+            d['stream_msg'] = e.stream_message
         if e.events:
             d['events'] = e.events
         r[e.caller] = d
