d46055e868b7e687f50866f8aa1cefc177836bc7
Klement Sekera <ksekera@cisco.com>
2019-06-24 11:52:49 +0000

    ip: refactor reassembly
    this is a preparation step for introducing other reassembly types
    
    Type: refactor
    
    Change-Id: I197e299dbd729b00eead31667913b8ceff915d63
    Signed-off-by: Klement Sekera <ksekera@cisco.com>
    (cherry picked from commit 896c896a3c4ed563a270281ff8b50fd253f4833f)

--- src/vnet/ip/reass/ip6_full_reass.h
+++ src/vnet/ip/reass/ip6_full_reass.h
@@ -15,35 +15,35 @@
 
 /**
  * @file
- * @brief IPv4 Reassembly.
+ * @brief IPv6 Reassembly.
  *
- * This file contains the source code for IPv4 reassembly.
+ * This file contains the source code for IPv6 reassembly.
  */
 
-#ifndef __included_ip4_reassembly_h__
-#define __included_ip4_reassembly_h__
+#ifndef __included_ip6_full_reass_h__
+#define __included_ip6_full_reass_h__
 
 #include <vnet/api_errno.h>
 #include <vnet/vnet.h>
 
 /**
- * @brief set ip4 reassembly configuration
+ * @brief set ip6 reassembly configuration
  */
-vnet_api_error_t ip4_reass_set (u32 timeout_ms, u32 max_reassemblies,
-				u32 max_reassembly_length,
-				u32 expire_walk_interval_ms);
+vnet_api_error_t ip6_full_reass_set (u32 timeout_ms, u32 max_reassemblies,
+				     u32 max_reassembly_length,
+				     u32 expire_walk_interval_ms);
 
 /**
- * @brief get ip4 reassembly configuration
+ * @brief get ip6 reassembly configuration
  */
-vnet_api_error_t ip4_reass_get (u32 * timeout_ms, u32 * max_reassemblies,
-				u32 * max_reassembly_length,
-				u32 * expire_walk_interval_ms);
+vnet_api_error_t ip6_full_reass_get (u32 * timeout_ms, u32 * max_reassemblies,
+				     u32 * max_reassembly_length,
+				     u32 * expire_walk_interval_ms);
 
-vnet_api_error_t ip4_reass_enable_disable (u32 sw_if_index,
-					   u8 enable_disable);
+vnet_api_error_t ip6_full_reass_enable_disable (u32 sw_if_index,
+						u8 enable_disable);
 
-#endif /* __included_ip4_reassembly_h */
+#endif /* __included_ip6_full_reass_h */
 
 /*
  * fd.io coding-style-patch-verification: ON
