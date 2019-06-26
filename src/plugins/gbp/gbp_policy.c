ae5f77587d4ac7d9e18def393e2bb52012e57fb4
Benoît Ganne <bganne@cisco.com>
2019-06-26 13:36:51 +0200

    gbp: add anonymous l3-out external interfaces
    So far, GBP l3-out packets classification & policy relied on programmed
    EP. All traffic to/from l3-out must go through a known EP.
    This patch introduces a new feature where l3-out next-hops are only
    known by their subnets (l3-out prefixes). As there are no longer known
    EPs to program, an interface must be configured as external anonymous
    l3-out. Packets classification & policy on this interface will rely on
    the external subnets programmed in the BD VRF.
    Note that contrary to all other interfaces in a GBP BD, external
    anonymous l3-out interfaces have BD L2 learning turned on and rely on
    ARP/ND.
    
    Type: feature
    
    Change-Id: Ieedb29dff4e967d08c4301e82d06bff450a63e5f
    Signed-off-by: Benoît Ganne <bganne@cisco.com>

--- src/plugins/gbp/gbp_policy.c
+++ src/plugins/gbp/gbp_policy.c
@@ -24,21 +24,27 @@ gbp_policy_init (vlib_main_t * vm)
   gbp_policy_main_t *gpm = &gbp_policy_main;
   clib_error_t *error = 0;
 
-  vlib_node_t *node = vlib_get_node_by_name (vm, (u8 *) "gbp-policy-port");
-
   /* Initialize the feature next-node indexes */
+  vlib_node_t *node = vlib_get_node_by_name (vm, (u8 *) "gbp-policy-port");
   feat_bitmap_init_next_nodes (vm,
 			       node->index,
 			       L2OUTPUT_N_FEAT,
 			       l2output_get_feat_names (),
-			       gpm->l2_output_feat_next[1]);
+			       gpm->l2_output_feat_next[GBP_POLICY_PORT]);
 
   node = vlib_get_node_by_name (vm, (u8 *) "gbp-policy-mac");
   feat_bitmap_init_next_nodes (vm,
 			       node->index,
 			       L2OUTPUT_N_FEAT,
 			       l2output_get_feat_names (),
-			       gpm->l2_output_feat_next[0]);
+			       gpm->l2_output_feat_next[GBP_POLICY_MAC]);
+
+  node = vlib_get_node_by_name (vm, (u8 *) "gbp-policy-lpm");
+  feat_bitmap_init_next_nodes (vm,
+			       node->index,
+			       L2OUTPUT_N_FEAT,
+			       l2output_get_feat_names (),
+			       gpm->l2_output_feat_next[GBP_POLICY_LPM]);
 
   return error;
 }
