FDio/vpp

commit 097fa66b986f06281f603767d321ab13ab6c88c3

fib: fib api updates
Enhance the route add/del APIs to take a set of paths rather than just one.
Most unicast routing protocols calcualte all the available paths in one
run of the algorithm so updating all the paths at once is beneficial for the client.
two knobs control the behaviour:
  is_multipath - if set the the set of paths passed will be added to those
                 that already exist, otherwise the set will replace them.
  is_add - add or remove the set

is_add=0, is_multipath=1 and an empty set, results in deleting the route.

It is also considerably faster to add multiple paths at once, than one at a time:

vat# ip_add_del_route 1.1.1.1/32 count 100000 multipath via 10.10.10.11
100000 routes in .572240 secs, 174751.80 routes/sec
vat# ip_add_del_route 1.1.1.1/32 count 100000 multipath via 10.10.10.12
100000 routes in .528383 secs, 189256.54 routes/sec
vat# ip_add_del_route 1.1.1.1/32 count 100000 multipath via 10.10.10.13
100000 routes in .757131 secs, 132077.52 routes/sec
vat# ip_add_del_route 1.1.1.1/32 count 100000 multipath via 10.10.10.14
100000 routes in .878317 secs, 113854.12 routes/sec

vat# ip_route_add_del 1.1.1.1/32 count 100000 multipath via 10.10.10.11 via 10.10.10.12 via 10.10.10.13 via 10.10.10.14
100000 routes in .900212 secs, 111084.93 routes/sec

Change-Id: I416b93f7684745099c1adb0b33edac58c9339c1a
Signed-off-by: Neale Ranns <neale.ranns@cisco.com>
Signed-off-by: Ole Troan <ot@cisco.com>
Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

extras/vom/vom/route_cmds.cpp
@@ -15,6 +15,7 @@
 
 #include <sstream>
 
+#include <vom/api_types.hpp>
 #include <vom/route_api_types.hpp>
 #include <vom/route_cmds.hpp>
 
@@ -22,14 +23,14 @@ namespace VOM {
 namespace route {
 namespace ip_route_cmds {
 
-update_cmd::update_cmd(HW::item<bool>& item,
+update_cmd::update_cmd(HW::item<handle_t>& item,
                        table_id_t id,
                        const prefix_t& prefix,
-                       const path& path)
-  : rpc_cmd(item)
+                       const path_list_t& pl)
+  : srpc_cmd(item)
   , m_id(id)
   , m_prefix(prefix)
-  , m_path(path)
+  , m_pl(pl)
 {
 }
 
@@ -37,23 +38,26 @@ bool
 update_cmd::operator==(const update_cmd& other) const
 {
   return ((m_prefix == other.m_prefix) && (m_id == other.m_id) &&
-          (m_path == other.m_path));
+          (m_pl == other.m_pl));
 }
 
 rc_t
 update_cmd::issue(connection& con)
 {
-  msg_t req(con.ctx(), 0, std::ref(*this));
+  msg_t req(con.ctx(), m_pl.size(), std::ref(*this));
 
   auto& payload = req.get_request().get_payload();
 
-  payload.table_id = m_id;
+  payload.route.table_id = m_id;
   payload.is_add = 1;
   payload.is_multipath = 1;
 
-  m_prefix.to_vpp(&payload.is_ipv6, payload.dst_address,
-                  &payload.dst_address_length);
-  to_vpp(m_path, payload);
+  payload.route.table_id = m_id;
+  payload.route.prefix = to_api(m_prefix);
+
+  uint32_t ii = 0;
+  for (auto& p : m_pl)
+    to_api(p, payload.route.paths[ii++]);
 
   VAPI_CALL(req.execute());
 
@@ -65,27 +69,26 @@ update_cmd::to_string() const
 {
   std::ostringstream s;
   s << "ip-route-create: " << m_hw_item.to_string() << " table-id:" << m_id
-    << " prefix:" << m_prefix.to_string() << " paths:" << m_path.to_string();
+    << " prefix:" << m_prefix.to_string() << " paths:";
+  for (auto p : m_pl)
+    s << p.to_string() << " ";
 
   return (s.str());
 }
 
-delete_cmd::delete_cmd(HW::item<bool>& item,
+delete_cmd::delete_cmd(HW::item<handle_t>& item,
                        table_id_t id,
-                       const prefix_t& prefix,
-                       const path& path)
+                       const prefix_t& prefix)
   : rpc_cmd(item)
   , m_id(id)
   , m_prefix(prefix)
-  , m_path(path)
 {
 }
 
 bool
 delete_cmd::operator==(const delete_cmd& other) const
 {
-  return ((m_prefix == other.m_prefix) && (m_id == other.m_id) &&
-          (m_path == other.m_path));
+  return ((m_prefix == other.m_prefix) && (m_id == other.m_id));
 }
 
 rc_t
@@ -94,12 +97,12 @@ delete_cmd::issue(connection& con)
   msg_t req(con.ctx(), 0, std::ref(*this));
 
   auto& payload = req.get_request().get_payload();
-  payload.table_id = m_id;
+  payload.route.table_id = m_id;
   payload.is_add = 0;
+  payload.is_multipath = 0;
 
-  m_prefix.to_vpp(&payload.is_ipv6, payload.dst_address,
-                  &payload.dst_address_length);
-  to_vpp(m_path, payload);
+  payload.route.table_id = m_id;
+  payload.route.prefix = to_api(m_prefix);
 
   VAPI_CALL(req.execute());
 
@@ -114,25 +117,32 @@ delete_cmd::to_string() const
 {
   std::ostringstream s;
   s << "ip-route-delete: " << m_hw_item.to_string() << " id:" << m_id
-    << " prefix:" << m_prefix.to_string() << " paths:" << m_path.to_string();
+    << " prefix:" << m_prefix.to_string();
 
   return (s.str());
 }
 
-dump_v4_cmd::dump_v4_cmd()
+dump_cmd::dump_cmd(route::table_id_t id, const l3_proto_t& proto)
+  : m_id(id)
+  , m_proto(proto)
 {
 }
 
 bool
-dump_v4_cmd::operator==(const dump_v4_cmd& other) const
+dump_cmd::operator==(const dump_cmd& other) const
 {
   return (true);
 }
 
 rc_t
-dump_v4_cmd::issue(connection& con)
+dump_cmd::issue(connection& con)
 {
   m_dump.reset(new msg_t(con.ctx(), std::ref(*this)));
+
+  auto& payload = m_dump->get_request().get_payload();
+
+  payload.table.table_id = m_id;
+  payload.table.is_ip6 = m_proto.is_ipv6();
 
   VAPI_CALL(m_dump->execute());
 
@@ -142,45 +152,19 @@ dump_v4_cmd::issue(connection& con)
 }
 
 std::string
-dump_v4_cmd::to_string() const
+dump_cmd::to_string() const
 {
   return ("ip-route-v4-dump");
 }
 
-dump_v6_cmd::dump_v6_cmd()
-{
-}
-
-bool
-dump_v6_cmd::operator==(const dump_v6_cmd& other) const
-{
-  return (true);
-}
-
-rc_t
-dump_v6_cmd::issue(connection& con)
-{
-  m_dump.reset(new msg_t(con.ctx(), std::ref(*this)));
-
-  VAPI_CALL(m_dump->execute());
-
-  wait();
-
-  return rc_t::OK;
-}
-
-std::string
-dump_v6_cmd::to_string() const
-{
-  return ("ip-route-v6-dump");
-}
 } // namespace ip_route_cmds
 } // namespace route
 } // namespace vom
-  /*
-   * fd.io coding-style-patch-verification: ON
-   *
-   * Local Variables:
-   * eval: (c-set-style "mozilla")
-   * End:
-   */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "mozilla")
+ * End:
+ */

src/vnet/fib/fib_api.h
@@ -17,61 +17,38 @@
 #define __FIB_API_H__
 
 #include <vnet/fib/fib_types.h>
+#include <vnet/fib/fib_entry.h>
 
-int
-add_del_route_check (fib_protocol_t table_proto,
-		     u32 table_id,
-		     u32 next_hop_sw_if_index,
-		     dpo_proto_t next_hop_table_proto,
-		     u32 next_hop_table_id,
-                     u8 is_rpf_id,
-		     u32 * fib_index, u32 * next_hop_fib_index);
-
-int
-add_del_route_t_handler (u8 is_multipath,
-			 u8 is_add,
-			 u8 is_drop,
-			 u8 is_unreach,
-			 u8 is_prohibit,
-			 u8 is_local,
-			 u8 is_multicast,
-			 u8 is_classify,
-			 u32 classify_table_index,
-			 u8 is_resolve_host,
-			 u8 is_resolve_attached,
-			 u8 is_interface_rx,
-                         u8 is_rpf_id,
-                         u8 is_dvr,
-                         u8 is_source_lookup,
-                         u8 is_udp_encap,
-			 u32 fib_index,
-			 const fib_prefix_t * prefix,
-			 dpo_proto_t next_hop_proto,
-			 const ip46_address_t * next_hop,
-                         u32 next_hop_id,
-			 u32 next_hop_sw_if_index,
-			 u8 next_hop_fib_index,
-			 u16 next_hop_weight,
-			 u16 next_hop_preference,
-			 mpls_label_t next_hop_via_label,
-			 fib_mpls_label_t * next_hop_out_label_stack);
-
+/**
+ * Forward declare the API type, no need to include the generated api headers
+ */
 struct _vl_api_fib_path;
+struct _vl_api_fib_prefix;
 
-extern void fib_api_path_encode (const fib_route_path_encode_t * api_rpath,
-                                 struct _vl_api_fib_path *out);
+/**
+ * Encode and decode functions from the API types to internal types
+ */
+extern void fib_api_path_encode(const fib_route_path_t * api_rpath,
+                                struct _vl_api_fib_path *out);
+extern int fib_api_path_decode(struct _vl_api_fib_path *in,
+                               fib_route_path_t *out);
 
-void
-fib_prefix_to_api (const fib_prefix_t *pfx,
-                   u8 address[16],
-                   u8 *length,
-                   u8 *is_ip6);
+extern int fib_api_table_id_decode(fib_protocol_t fproto,
+                                   u32 table_id,
+                                   u32 *fib_index);
 
+/**
+ * Adding routes from the API
+ */
+extern void fib_api_route_add_del (u8 is_add,
+                                   u8 is_multipath,
+                                   u32 fib_index,
+                                   const fib_prefix_t * prefix,
+                                   fib_entry_flag_t entry_flags,
+                                   fib_route_path_t *rpaths);
 
-struct _vl_api_fib_path;
+extern u8* format_vl_api_fib_path(u8 * s, va_list * args);
 
-extern int fib_path_api_parse(const struct _vl_api_fib_path *in,
-                              fib_route_path_t *out);
 
 extern fib_protocol_t fib_proto_from_api_address_family (int af);
 extern int fib_proto_to_api_address_family (fib_protocol_t fproto);

commit be00d7f1dad4dd38adec2569a169994d041cacba

fib: allow route delete with no paths and multipath=0 to remove the whole route
Type: fix
Fixes: 097fa66b

Change-Id: I017ab5797670eb278c27c6e306cd8cadaacddf9d
Signed-off-by: Neale Ranns <nranns@cisco.com>

extras/vom/vom/route_cmds.cpp
@@ -31,8 +31,7 @@ update_cmd::update_cmd(HW::item<handle_t>& item,
   , m_id(id)
   , m_prefix(prefix)
   , m_pl(pl)
-{
-}
+{}
 
 bool
 update_cmd::operator==(const update_cmd& other) const
@@ -82,8 +81,7 @@ delete_cmd::delete_cmd(HW::item<handle_t>& item,
   : rpc_cmd(item)
   , m_id(id)
   , m_prefix(prefix)
-{
-}
+{}
 
 bool
 delete_cmd::operator==(const delete_cmd& other) const
@@ -97,10 +95,11 @@ delete_cmd::issue(connection& con)
   msg_t req(con.ctx(), 0, std::ref(*this));
 
   auto& payload = req.get_request().get_payload();
-  payload.route.table_id = m_id;
   payload.is_add = 0;
   payload.is_multipath = 0;
 
+  payload.route.table_id = m_id;
+  payload.route.n_paths = 0;
   payload.route.table_id = m_id;
   payload.route.prefix = to_api(m_prefix);
 
@@ -125,8 +124,7 @@ delete_cmd::to_string() const
 dump_cmd::dump_cmd(route::table_id_t id, const l3_proto_t& proto)
   : m_id(id)
   , m_proto(proto)
-{
-}
+{}
 
 bool
 dump_cmd::operator==(const dump_cmd& other) const

src/vnet/fib/fib_api.c
@@ -449,7 +449,7 @@ fib_api_path_encode (const fib_route_path_t * rpath,
     }
 }
 
-void
+int
 fib_api_route_add_del (u8 is_add,
                        u8 is_multipath,
                        u32 fib_index,
@@ -457,36 +457,46 @@ fib_api_route_add_del (u8 is_add,
                        fib_entry_flag_t entry_flags,
                        fib_route_path_t *rpaths)
 {
-  if (is_multipath)
+    if (is_multipath)
     {
-      /* Iterative path add/remove */
-      if (is_add)
-	fib_table_entry_path_add2 (fib_index,
-				   prefix,
-				   FIB_SOURCE_API,
-                                   entry_flags,
-                                   rpaths);
-      else
-	fib_table_entry_path_remove2 (fib_index,
-				      prefix,
-                                      FIB_SOURCE_API,
-                                      rpaths);
+        if (vec_len(rpaths) == 0)
+            return (VNET_API_ERROR_NO_PATHS_IN_ROUTE);
+
+        /* Iterative path add/remove */
+        if (is_add)
+            fib_table_entry_path_add2 (fib_index,
+                                       prefix,
+                                       FIB_SOURCE_API,
+                                       entry_flags,
+                                       rpaths);
+        else
+            fib_table_entry_path_remove2 (fib_index,
+                                          prefix,
+                                          FIB_SOURCE_API,
+                                          rpaths);
     }
-  else
+    else
     {
-      if (is_add)
-        /* path replacement */
-	fib_table_entry_update (fib_index,
-                                prefix,
-                                FIB_SOURCE_API,
-                                entry_flags,
-                                rpaths);
-      else
-        /* entry delete */
-	fib_table_entry_delete (fib_index,
-                                prefix,
-                                FIB_SOURCE_API);
+        if (is_add)
+        {
+            if (vec_len(rpaths) == 0)
+                return (VNET_API_ERROR_NO_PATHS_IN_ROUTE);
+
+            /* path replacement */
+            fib_table_entry_update (fib_index,
+                                    prefix,
+                                    FIB_SOURCE_API,
+                                    entry_flags,
+                                    rpaths);
+        }
+        else
+            /* entry delete */
+            fib_table_entry_delete (fib_index,
+                                    prefix,
+                                    FIB_SOURCE_API);
     }
+
+    return (0);
 }
 
 u8*

src/vnet/fib/fib_api.h
@@ -40,12 +40,12 @@ extern int fib_api_table_id_decode(fib_protocol_t fproto,
 /**
  * Adding routes from the API
  */
-extern void fib_api_route_add_del (u8 is_add,
-                                   u8 is_multipath,
-                                   u32 fib_index,
-                                   const fib_prefix_t * prefix,
-                                   fib_entry_flag_t entry_flags,
-                                   fib_route_path_t *rpaths);
+extern int fib_api_route_add_del (u8 is_add,
+                                  u8 is_multipath,
+                                  u32 fib_index,
+                                  const fib_prefix_t * prefix,
+                                  fib_entry_flag_t entry_flags,
+                                  fib_route_path_t *rpaths);
 
 extern u8* format_vl_api_fib_path(u8 * s, va_list * args);
 

src/vnet/ip/ip_api.c
@@ -667,13 +667,8 @@ ip_route_add_del_t_handler (vl_api_ip_route_add_del_t * mp, u32 * stats_index)
   if (0 != rv)
     goto out;
 
-  if (0 == mp->route.n_paths)
-    {
-      rv = VNET_API_ERROR_NO_PATHS_IN_ROUTE;
-      goto out;
-    }
-
-  vec_validate (rpaths, mp->route.n_paths - 1);
+  if (0 != mp->route.n_paths)
+    vec_validate (rpaths, mp->route.n_paths - 1);
 
   for (ii = 0; ii < mp->route.n_paths; ii++)
     {
@@ -690,9 +685,9 @@ ip_route_add_del_t_handler (vl_api_ip_route_add_del_t * mp, u32 * stats_index)
 	goto out;
     }
 
-  fib_api_route_add_del (mp->is_add,
-			 mp->is_multipath,
-			 fib_index, &pfx, entry_flags, rpaths);
+  rv = fib_api_route_add_del (mp->is_add,
+			      mp->is_multipath,
+			      fib_index, &pfx, entry_flags, rpaths);
 
   if (mp->is_add && 0 == rv)
     *stats_index = fib_table_entry_get_stats_index (fib_index, &pfx);

src/vnet/mpls/mpls_api.c
@@ -192,13 +192,13 @@ mpls_route_add_del_t_handler (vnet_main_t * vnm,
 	goto out;
     }
 
-  fib_api_route_add_del (mp->mr_is_add,
-			 mp->mr_is_multipath,
-			 fib_index,
-			 &pfx,
-			 (mp->mr_route.mr_is_multicast ?
-			  FIB_ENTRY_FLAG_MULTICAST :
-			  FIB_ENTRY_FLAG_NONE), rpaths);
+  rv = fib_api_route_add_del (mp->mr_is_add,
+			      mp->mr_is_multipath,
+			      fib_index,
+			      &pfx,
+			      (mp->mr_route.mr_is_multicast ?
+			       FIB_ENTRY_FLAG_MULTICAST :
+			       FIB_ENTRY_FLAG_NONE), rpaths);
 
   if (mp->mr_is_add && 0 == rv)
     *stats_index = fib_table_entry_get_stats_index (fib_index, &pfx);

test/vpp_ip_route.py
@@ -428,6 +428,7 @@ class VppIpRoute(VppObject):
         self.prefix = VppIpPrefix(dest_addr, dest_addr_len)
         self.register = register
         self.stats_index = None
+        self.modified = False
 
         self.encoded_paths = []
         for path in self.paths:
@@ -444,6 +445,7 @@ class VppIpRoute(VppObject):
         self.encoded_paths = []
         for path in self.paths:
             self.encoded_paths.append(path.encode())
+        self.modified = True
 
         self._test.vapi.ip_route_add_del(route={'table_id': self.table_id,
                                                 'prefix': self.prefix.encode(),
@@ -468,14 +470,26 @@ class VppIpRoute(VppObject):
             self._test.registry.register(self, self._test.logger)
 
     def remove_vpp_config(self):
-        self._test.vapi.ip_route_add_del(route={'table_id': self.table_id,
-                                                'prefix': self.prefix.encode(),
-                                                'n_paths': len(
-                                                    self.encoded_paths),
-                                                'paths': self.encoded_paths,
-                                                },
-                                         is_add=0,
-                                         is_multipath=0)
+        # there's no need to issue different deletes for modified routes
+        # we do this only to test the two different ways to delete routes
+        # eiter by passing all the paths to remove and mutlipath=1 or
+        # passing no paths and multipath=0
+        if self.modified:
+            self._test.vapi.ip_route_add_del(
+                route={'table_id': self.table_id,
+                       'prefix': self.prefix.encode(),
+                       'n_paths': len(
+                           self.encoded_paths),
+                       'paths': self.encoded_paths},
+                is_add=0,
+                is_multipath=1)
+        else:
+            self._test.vapi.ip_route_add_del(
+                route={'table_id': self.table_id,
+                       'prefix': self.prefix.encode(),
+                       'n_paths': 0},
+                is_add=0,
+                is_multipath=0)
 
     def query_vpp_config(self):
         return find_route(self._test,