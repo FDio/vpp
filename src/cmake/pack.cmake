1fffa1d281759249d2c20d153e64d96da991e42e
YohanPipereau <ypiperea@cisco.com>
2019-07-19 17:53:18 +0200

    build: Append build number for cpack packages
    add_vpp_packaging maccro circumvents scripts/version causing:
    1. build number not being appended to the package
    2. invalid package name format (due to other errors)
    
    Change-Id: I2b9a985357a3f3bd501be6db6ca638d4430f4bbb
    Type: make
    Fixes: def35a2352c9a54f748d301ffa47a446d25a83e0
    Signed-off-by: YohanPipereau <ypiperea@cisco.com>

--- src/cmake/pack.cmake
+++ src/cmake/pack.cmake
@@ -15,14 +15,10 @@
 # DEB Packaging
 ##############################################################################
 
-macro(add_vpp_packaging name)
-  cmake_parse_arguments(ARG
-    ""
-    "NAME;DESCRIPION;VENDOR"
-    ""
-    ${ARGN}
-  )
-
+# name, vendor, description are maccros, arguments are in ${ARGN}
+# args require more parsing and useful cmake_parse_arguments is not defined
+# only for cmake >= 3.5
+macro(add_vpp_packaging name vendor description)
   # parse /etc/os-release
   file(READ "/etc/os-release" os_version)
   string(REPLACE "\n" ";" os_version ${os_version})
@@ -33,63 +29,53 @@ macro(add_vpp_packaging name)
     set(OS_${_name} ${_value})
   endforeach()
 
-  # extract version from git
+  # scripts/version provides ALSO the build number
   execute_process(
-    COMMAND git describe --long --match v*
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    OUTPUT_VARIABLE VER
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/../..
+    COMMAND scripts/version
+    OUTPUT_VARIABLE VPP_VERSION
     OUTPUT_STRIP_TRAILING_WHITESPACE
   )
-  string(REGEX REPLACE "v(.*)-([0-9]+)-(g[0-9a-f]+)" "\\1;\\2;\\3" VER ${VER})
-  list(GET VER 0 tag)
-  string(REPLACE "-" "~" tag ${tag})
-  list(GET VER 1 commit_num)
-  list(GET VER 2 commit_name)
+  set(deb_ver "${VPP_VERSION}")
+  set(rpm_ver "${VPP_VERSION}")
 
-  #define DEB and RPM version numbers
-  if(${commit_num} EQUAL 0)
-    set(deb_ver "${tag}")
-    set(rpm_ver "${tag}")
-  else()
-    set(deb_ver "${tag}~${commit_num}~${commit_name}")
-    set(rpm_ver "${tag}~${commit_num}_${commit_name}")
-  endif()
-
-  get_cmake_property(components COMPONENTS)
+  set(CPACK_PACKAGE_NAME ${name})
+  set(CPACK_STRIP_FILES OFF)
+  set(CPACK_PACKAGE_VENDOR "${vendor}")
+  set(CPACK_COMPONENTS_IGNORE_GROUPS 1)
+  set(CPACK_${CPACK_GENERATOR}_COMPONENT_INSTALL ON)
+  set(CPACK_${type}_PACKAGE_DESCRIPTION "${description}")
+  set(CPACK_${type}_PACKAGE_RELEASE 1)
 
   if(OS_ID_LIKE MATCHES "debian")
     set(CPACK_GENERATOR "DEB")
     set(type "DEBIAN")
     set(CPACK_PACKAGE_VERSION "${deb_ver}")
     set(CPACK_DEBIAN_PACKAGE_MAINTAINER "VPP Team")
-    set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
-    foreach(lc ${components})
-      string(TOUPPER ${lc} uc)
-      set(CPACK_DEBIAN_${uc}_PACKAGE_NAME "${lc}")
-    endforeach()
+    execute_process(
+      COMMAND dpkg --print-architecture
+      OUTPUT_VARIABLE CPACK_DEBIAN_PACKAGE_ARCHITECTURE
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    #CPACK_DEBIAN_FILE_NAME in cmake >= 3.6
+    set(CPACK_PACKAGE_FILE_NAME ${CPACK_PACKAGE_NAME}_${CPACK_PACKAGE_VERSION}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE})
   elseif(OS_ID_LIKE MATCHES "rhel")
     set(CPACK_GENERATOR "RPM")
     set(type "RPM")
     set(CPACK_PACKAGE_VERSION "${rpm_ver}")
-    set(CPACK_RPM_FILE_NAME RPM-DEFAULT)
-    foreach(lc ${components})
-      string(TOUPPER ${lc} uc)
-      if(${lc} MATCHES ".*-dev")
-	set(CPACK_RPM_${uc}_DEBUGINFO_PACKAGE ON)
-	set(lc ${lc}el)
-      endif()
-      set(CPACK_RPM_${uc}_PACKAGE_NAME "${lc}")
-    endforeach()
+    execute_process(
+      COMMAND uname -m
+      OUTPUT_VARIABLE CPACK_RPM_PACKAGE_ARCHITECTURE
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    #CPACK_DEBIAN_FILE_NAME in cmake >= 3.6
+    set(CPACK_PACKAGE_FILE_NAME ${CPACK_PACKAGE_NAME}_${CPACK_PACKAGE_VERSION}_${CPACK_RPM_PACKAGE_ARCHITECTURE})
   endif()
 
   if(CPACK_GENERATOR)
-    set(CPACK_PACKAGE_NAME ${ARG_NAME})
-    set(CPACK_STRIP_FILES OFF)
-    set(CPACK_PACKAGE_VENDOR "${ARG_VENDOR}")
-    set(CPACK_COMPONENTS_IGNORE_GROUPS 1)
-    set(CPACK_${CPACK_GENERATOR}_COMPONENT_INSTALL ON)
-    set(CPACK_${type}_PACKAGE_DESCRIPTION "${ARG_DESCRIPTION}")
-    set(CPACK_${type}_PACKAGE_RELEASE 1)
     include(CPack)
+  else()
+    message(ERROR "CPACK_GENERATOR must be set")
   endif()
+
 endmacro()
