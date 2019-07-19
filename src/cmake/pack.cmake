aca174383af80abf33719fb4cb15e220b0cbb4fa
YohanPipereau <ypiperea@cisco.com>
2019-07-19 17:53:18 +0200

    build: Append build number for cpack packages
    add_vpp_packaging maccro was circumventing scripts/version causing:
    1. build number not being appended to the package
    2. invalid package name format (due to other errors)
    
    Now add_vpp_packaging also takes a VERSION argument as input.
    It can either be output from scripts/version for internal projects or
    any version for external projects leveraging this maccro.
    
    Change-Id: I2b9a985357a3f3bd501be6db6ca638d4430f4bbb
    Type: make
    Fixes: def35a2352c9a54f748d301ffa47a446d25a83e0
    Signed-off-by: YohanPipereau <ypiperea@cisco.com>

--- src/cmake/pack.cmake
+++ src/cmake/pack.cmake
@@ -11,14 +11,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-##############################################################################
-# DEB Packaging
-##############################################################################
+# cmake_parse_arguments used to be a module and is now built-in since 3.4
+if (CMAKE_VERSION VERSION_LESS "3.4")
+include(CmakeParseArguments)
+endif()
 
-macro(add_vpp_packaging name)
+# name, vendor, description are maccros, arguments are in ${ARGN}
+macro(add_vpp_packaging)
   cmake_parse_arguments(ARG
     ""
-    "NAME;DESCRIPION;VENDOR"
+    "NAME;DESCRIPTION;VENDOR;VERSION"
     ""
     ${ARGN}
   )
@@ -33,63 +35,51 @@ macro(add_vpp_packaging name)
     set(OS_${_name} ${_value})
   endforeach()
 
-  # extract version from git
-  execute_process(
-    COMMAND git describe --long --match v*
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    OUTPUT_VARIABLE VER
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
-  string(REGEX REPLACE "v(.*)-([0-9]+)-(g[0-9a-f]+)" "\\1;\\2;\\3" VER ${VER})
-  list(GET VER 0 tag)
-  string(REPLACE "-" "~" tag ${tag})
-  list(GET VER 1 commit_num)
-  list(GET VER 2 commit_name)
-
-  #define DEB and RPM version numbers
-  if(${commit_num} EQUAL 0)
-    set(deb_ver "${tag}")
-    set(rpm_ver "${tag}")
-  else()
-    set(deb_ver "${tag}~${commit_num}~${commit_name}")
-    set(rpm_ver "${tag}~${commit_num}_${commit_name}")
+  if ("${ARG_VERSION}" STREQUAL "")
+      message(FATAL_ERROR "VPP version unset in add_vpp_packaging")
   endif()
+  message(STATUS "VPP version:" ${ARG_VERSION})
 
-  get_cmake_property(components COMPONENTS)
+  set(deb_ver "${ARG_VERSION}")
+  set(rpm_ver "${ARG_VERSION}")
+
+  set(CPACK_PACKAGE_NAME ${ARG_NAME})
+  set(CPACK_STRIP_FILES OFF)
+  set(CPACK_PACKAGE_VENDOR "${ARG_VENDOR}")
+  set(CPACK_COMPONENTS_IGNORE_GROUPS 1)
+  set(CPACK_${CPACK_GENERATOR}_COMPONENT_INSTALL ON)
+  set(CPACK_${type}_PACKAGE_DESCRIPTION "${ARG_DESCRIPTION}")
+  set(CPACK_${type}_PACKAGE_RELEASE 1)
 
   if(OS_ID_LIKE MATCHES "debian")
     set(CPACK_GENERATOR "DEB")
     set(type "DEBIAN")
     set(CPACK_PACKAGE_VERSION "${deb_ver}")
     set(CPACK_DEBIAN_PACKAGE_MAINTAINER "VPP Team")
-    set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
-    foreach(lc ${components})
-      string(TOUPPER ${lc} uc)
-      set(CPACK_DEBIAN_${uc}_PACKAGE_NAME "${lc}")
-    endforeach()
+    execute_process(
+      COMMAND dpkg --print-architecture
+      OUTPUT_VARIABLE CPACK_DEBIAN_PACKAGE_ARCHITECTURE
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    #CPACK_DEBIAN_FILE_NAME in cmake >= 3.6
+    set(CPACK_PACKAGE_FILE_NAME ${CPACK_PACKAGE_NAME}_${CPACK_PACKAGE_VERSION}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE})
   elseif(OS_ID_LIKE MATCHES "rhel")
     set(CPACK_GENERATOR "RPM")
     set(type "RPM")
     set(CPACK_PACKAGE_VERSION "${rpm_ver}")
-    set(CPACK_RPM_FILE_NAME RPM-DEFAULT)
-    foreach(lc ${components})
-      string(TOUPPER ${lc} uc)
-      if(${lc} MATCHES ".*-dev")
-	set(CPACK_RPM_${uc}_DEBUGINFO_PACKAGE ON)
-	set(lc ${lc}el)
-      endif()
-      set(CPACK_RPM_${uc}_PACKAGE_NAME "${lc}")
-    endforeach()
+    execute_process(
+      COMMAND uname -m
+      OUTPUT_VARIABLE CPACK_RPM_PACKAGE_ARCHITECTURE
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    #CPACK_DEBIAN_FILE_NAME in cmake >= 3.6
+    set(CPACK_PACKAGE_FILE_NAME ${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}.${CPACK_RPM_PACKAGE_ARCHITECTURE})
   endif()
 
   if(CPACK_GENERATOR)
-    set(CPACK_PACKAGE_NAME ${ARG_NAME})
-    set(CPACK_STRIP_FILES OFF)
-    set(CPACK_PACKAGE_VENDOR "${ARG_VENDOR}")
-    set(CPACK_COMPONENTS_IGNORE_GROUPS 1)
-    set(CPACK_${CPACK_GENERATOR}_COMPONENT_INSTALL ON)
-    set(CPACK_${type}_PACKAGE_DESCRIPTION "${ARG_DESCRIPTION}")
-    set(CPACK_${type}_PACKAGE_RELEASE 1)
     include(CPack)
+  else()
+    message(ERROR "CPACK_GENERATOR must be set")
   endif()
+
 endmacro()
