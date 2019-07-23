FDio/vpp

commit 3b7261978ee4ffdc1e92336e708ae05e2be25f71

udp: fix connection flags
Change-Id: Ib69f9bd7970aeb2ee6a1c114d38dcb7f8698dc6d
Type: fix
Fixes: c754239
Signed-off-by: Aloys Augustin <aloaugus@cisco.com>

src/vnet/udp/udp.h
@@ -36,9 +36,9 @@ typedef enum
 
 typedef enum
 {
-  UDP_CONN_F_CONNECTED,		/**< connected mode */
-  UDP_CONN_F_OWNS_PORT,		/**< port belong to conn (UDPC) */
-  UDP_CONN_F_CLOSING,		/**< conn closed with data */
+  UDP_CONN_F_CONNECTED = 1 << 0,	/**< connected mode */
+  UDP_CONN_F_OWNS_PORT = 1 << 1,	/**< port belong to conn (UDPC) */
+  UDP_CONN_F_CLOSING = 2 << 2,		/**< conn closed with data */
 } udp_conn_flags_t;
 
 typedef struct

commit 5c4db54bed0251c6f11631c4aefae127c2e11ee5

udp: fix type in udp connectinon flags
Type: fix
Fixes: 3b726197

Change-Id: Ib515f0995e5c837349ebcad5f63fbd1b2a197e13
Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

src/vnet/udp/udp.h
@@ -38,7 +38,7 @@ typedef enum
 {
   UDP_CONN_F_CONNECTED = 1 << 0,	/**< connected mode */
   UDP_CONN_F_OWNS_PORT = 1 << 1,	/**< port belong to conn (UDPC) */
-  UDP_CONN_F_CLOSING = 2 << 2,		/**< conn closed with data */
+  UDP_CONN_F_CLOSING = 1 << 2,		/**< conn closed with data */
 } udp_conn_flags_t;
 
 typedef struct