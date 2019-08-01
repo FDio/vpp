FDio/vpp

commit 039cbfe254be998f7311bc4638bb262b44efac19

QoS recording and marking
Change-Id: Ie5a50def4ec1e4a3b3404a8b6ab9ec248bc16744
Signed-off-by: Neale Ranns <nranns@cisco.com>

src/vnet/ethernet/packet.h
@@ -91,6 +91,15 @@ typedef struct
   u16 type;
 } ethernet_vlan_header_t;
 
+always_inline void
+ethernet_vlan_header_set_priority_net_order (ethernet_vlan_header_t * h,
+					     u8 prio)
+{
+  u8 *bytes = (u8 *) (&h->priority_cfi_and_id);
+
+  bytes[0] &= 0x1f;
+  bytes[0] |= (prio & 0x7) << 5;
+}
 
 /* VLAN with ethertype first and vlan id second */
 typedef struct

src/plugins/nat/nat64.h
@@ -377,9 +377,6 @@ u32 nat64_get_worker_in2out (ip6_address_t * addr);
  */
 u32 nat64_get_worker_out2in (ip4_header_t * ip);
 
-#define u8_ptr_add(ptr, index) (((u8 *)ptr) + index)
-#define u16_net_add(u, val) clib_host_to_net_u16(clib_net_to_host_u16(u) + (val))
-
 #endif /* __included_nat64_h__ */
 
 /*

src/plugins/nat/nat66.h
@@ -75,9 +75,6 @@ int nat66_static_mapping_add_del (ip6_address_t * l_addr,
 				  ip6_address_t * e_addr, u32 vrf_id,
 				  u8 is_add);
 
-#define u8_ptr_add(ptr, index) (((u8 *)ptr) + index)
-#define u16_net_add(u, val) clib_host_to_net_u16(clib_net_to_host_u16(u) + (val))
-
 #endif /* __included_nat66_h__ */
 
 /*

src/vnet.am
@@ -348,7 +348,7 @@ libvnet_la_SOURCES +=				\
  vnet/ip/ip_frag.c				\
  vnet/ip/ip.c					\
  vnet/ip/ip_init.c				\
- vnet/ip/ip_in_out_acl.c				\
+ vnet/ip/ip_in_out_acl.c			\
  vnet/ip/lookup.c				\
  vnet/ip/ping.c					\
  vnet/ip/punt_api.c				\
@@ -1196,6 +1196,19 @@ libvnet_la_SOURCES +=                    \
   vnet/util/radix.c			     \
   vnet/util/refcount.c			 \
   vnet/util/trajectory.c
+
+########################################
+# QoS
+########################################
+
+libvnet_la_SOURCES +=      	                \
+  vnet/qos/qos_types.c				\
+  vnet/qos/qos_api.c				\
+  vnet/qos/qos_egress_map.c			\
+  vnet/qos/qos_record.c				\
+  vnet/qos/qos_mark.c
+
+API_FILES += vnet/qos/qos.api
 
 ########################################
 # BIER

src/vnet/buffer.c
@@ -38,6 +38,10 @@ format_vnet_buffer (u8 * s, va_list * args)
   if (b->flags & VNET_BUFFER_F_L4_HDR_OFFSET_VALID)
     a = format (a, "l4-hdr-offset %d ", vnet_buffer (b)->l4_hdr_offset);
 
+  if (b->flags & VNET_BUFFER_F_QOS_DATA_VALID)
+    a = format (a, "qos %d.%d ",
+		vnet_buffer2 (b)->qos.bits, vnet_buffer2 (b)->qos.source);
+
   s = format (s, "%U", format_vlib_buffer, b);
   if (a)
     s = format (s, "\n%U%v", format_white_space, indent, a);

src/vnet/buffer.h
@@ -63,7 +63,8 @@
   _(15, L3_HDR_OFFSET_VALID, 0)				\
   _(16, L4_HDR_OFFSET_VALID, 0)				\
   _(17, FLOW_REPORT, "flow-report")			\
-  _(18, IS_DVR, "dvr")
+  _(18, IS_DVR, "dvr")                                  \
+  _(19, QOS_DATA_VALID, 0)
 
 #define VNET_BUFFER_FLAGS_VLAN_BITS \
   (VNET_BUFFER_F_VLAN_1_DEEP | VNET_BUFFER_F_VLAN_2_DEEP)
@@ -186,6 +187,8 @@ typedef struct
       u8 ttl;
       u8 exp;
       u8 first;
+      /* Rewrite length */
+      u32 save_rewrite_length;
       /*
        * BIER - the nubmer of bytes in the header.
        *  the len field inthe header is not authoritative. It's the
@@ -362,16 +365,29 @@ STATIC_ASSERT (sizeof (vnet_buffer_opaque_t) <=
 /* Full cache line (64 bytes) of additional space */
 typedef struct
 {
+  /**
+   * QoS marking data that needs to persist from the recording nodes
+   * (nominally in the ingress path) to the marking node (in the
+   * egress path)
+   */
+  struct
+  {
+    u8 bits;
+    u8 source;
+  } qos;
+
+  u8 __unused[2];
+
   union
   {
-#if VLIB_BUFFER_TRACE_TRAJECTORY > 0
-    /* buffer trajectory tracing */
     struct
     {
+#if VLIB_BUFFER_TRACE_TRAJECTORY > 0
+      /* buffer trajectory tracing */
       u16 *trajectory_trace;
-    };
 #endif
-    u32 unused[12];
+    };
+    u32 unused[11];
   };
 } vnet_buffer_opaque2_t;
 

src/vnet/ip/ip.h
@@ -65,6 +65,9 @@
 #include <vnet/ip/icmp6.h>
 #include <vnet/classify/vnet_classify.h>
 
+#define u8_ptr_add(ptr, index) (((u8 *)ptr) + index)
+#define u16_net_add(u, val) clib_host_to_net_u16(clib_net_to_host_u16(u) + (val))
+
 /* Per protocol info. */
 typedef struct
 {

src/vnet/ip/ip6_packet.h
@@ -360,6 +360,7 @@ ip6_set_traffic_class_network_order (ip6_header_t * ip6, u8 dscp)
 {
   u32 tmp =
     clib_net_to_host_u32 (ip6->ip_version_traffic_class_and_flow_label);
+  tmp &= 0xf00fffff;
   tmp |= (dscp << 20);
   ip6->ip_version_traffic_class_and_flow_label = clib_host_to_net_u32 (tmp);
 }

commit 4790cea15ef6e389cc31ccad999273ccab27c44c

ethernet: change to mark the CFI bit in the L2 header.
Type: fix
Fixes: 039cbfe2

Signed-off-by: Prashant Maheshwari <pmahesh2@cisco.com>
Change-Id: Idca91c73758824688dd6ce61df994be66753d838

src/vnet/ethernet/packet.h
@@ -142,8 +142,8 @@ ethernet_vlan_header_set_priority_net_order (ethernet_vlan_header_t * h,
 {
   u8 *bytes = (u8 *) (&h->priority_cfi_and_id);
 
-  bytes[0] &= 0x1f;
-  bytes[0] |= (prio & 0x7) << 5;
+  bytes[0] &= 0x0f;
+  bytes[0] |= (prio & 0xf) << 4;
 }
 
 always_inline u8
@@ -151,7 +151,7 @@ ethernet_vlan_header_get_priority_net_order (ethernet_vlan_header_t * h)
 {
   u8 *bytes = (u8 *) (&h->priority_cfi_and_id);
 
-  return (bytes[0] >> 5);
+  return (bytes[0] >> 4);
 }
 
 /* VLAN with ethertype first and vlan id second */