2dd990af463efe8c7f0217fdace4d5edddb72666
Nathan Skrzypczak <nathan.skrzypczak@gmail.com>
2019-08-05 13:43:31 +0200

    session: Refactor echo client apps
    Type: refactor
    
    Change-Id: Iaef9091e1d057110530255e644fad1c298418966
    Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

--- src/plugins/hs_apps/sapi/tcp_echo.c
+++ src/plugins/hs_apps/sapi/tcp_echo.c
@@ -17,6 +17,7 @@
 #include <signal.h>
 
 #include <vnet/session/application_interface.h>
+#include <hs_apps/sapi/echo_common.c>
 #include <vlibmemory/api.h>
 
 #include <vpp/api/vpe_msg_enum.h>
@@ -117,9 +118,6 @@ typedef struct
 
   u32 configured_segment_size;
 
-  /* VNET_API_ERROR_FOO -> "Foo" hash table */
-  uword *error_string_by_error_number;
-
   u8 *connect_test_data;
   pthread_t *client_thread_handles;
   u32 *thread_args;
@@ -151,34 +149,6 @@ echo_main_t echo_main;
 #else
 #define NITER 4000000
 #endif
-
-static u8 *
-format_api_error (u8 * s, va_list * args)
-{
-  echo_main_t *em = &echo_main;
-  i32 error = va_arg (*args, u32);
-  uword *p;
-
-  p = hash_get (em->error_string_by_error_number, -error);
-
-  if (p)
-    s = format (s, "%s", p[0]);
-  else
-    s = format (s, "%d", error);
-  return s;
-}
-
-static void
-init_error_string_table (echo_main_t * em)
-{
-  em->error_string_by_error_number = hash_create (0, sizeof (uword));
-
-#define _(n,v,s) hash_set (em->error_string_by_error_number, -v, s);
-  foreach_vnet_api_error;
-#undef _
-
-  hash_set (em->error_string_by_error_number, 99, "Misc");
-}
 
 static void handle_mq_event (session_event_t * e);
 
@@ -964,89 +934,6 @@ vl_api_unbind_uri_reply_t_handler (vl_api_unbind_uri_reply_t * mp)
     clib_warning ("returned %d", ntohl (mp->retval));
 
   em->state = STATE_START;
-}
-
-u8 *
-format_ip4_address (u8 * s, va_list * args)
-{
-  u8 *a = va_arg (*args, u8 *);
-  return format (s, "%d.%d.%d.%d", a[0], a[1], a[2], a[3]);
-}
-
-u8 *
-format_ip6_address (u8 * s, va_list * args)
-{
-  ip6_address_t *a = va_arg (*args, ip6_address_t *);
-  u32 i, i_max_n_zero, max_n_zeros, i_first_zero, n_zeros, last_double_colon;
-
-  i_max_n_zero = ARRAY_LEN (a->as_u16);
-  max_n_zeros = 0;
-  i_first_zero = i_max_n_zero;
-  n_zeros = 0;
-  for (i = 0; i < ARRAY_LEN (a->as_u16); i++)
-    {
-      u32 is_zero = a->as_u16[i] == 0;
-      if (is_zero && i_first_zero >= ARRAY_LEN (a->as_u16))
-	{
-	  i_first_zero = i;
-	  n_zeros = 0;
-	}
-      n_zeros += is_zero;
-      if ((!is_zero && n_zeros > max_n_zeros)
-	  || (i + 1 >= ARRAY_LEN (a->as_u16) && n_zeros > max_n_zeros))
-	{
-	  i_max_n_zero = i_first_zero;
-	  max_n_zeros = n_zeros;
-	  i_first_zero = ARRAY_LEN (a->as_u16);
-	  n_zeros = 0;
-	}
-    }
-
-  last_double_colon = 0;
-  for (i = 0; i < ARRAY_LEN (a->as_u16); i++)
-    {
-      if (i == i_max_n_zero && max_n_zeros > 1)
-	{
-	  s = format (s, "::");
-	  i += max_n_zeros - 1;
-	  last_double_colon = 1;
-	}
-      else
-	{
-	  s = format (s, "%s%x",
-		      (last_double_colon || i == 0) ? "" : ":",
-		      clib_net_to_host_u16 (a->as_u16[i]));
-	  last_double_colon = 0;
-	}
-    }
-
-  return s;
-}
-
-/* Format an IP46 address. */
-u8 *
-format_ip46_address (u8 * s, va_list * args)
-{
-  ip46_address_t *ip46 = va_arg (*args, ip46_address_t *);
-  ip46_type_t type = va_arg (*args, ip46_type_t);
-  int is_ip4 = 1;
-
-  switch (type)
-    {
-    case IP46_TYPE_ANY:
-      is_ip4 = ip46_address_is_ip4 (ip46);
-      break;
-    case IP46_TYPE_IP4:
-      is_ip4 = 1;
-      break;
-    case IP46_TYPE_IP6:
-      is_ip4 = 0;
-      break;
-    }
-
-  return is_ip4 ?
-    format (s, "%U", format_ip4_address, &ip46->ip4) :
-    format (s, "%U", format_ip6_address, &ip46->ip6);
 }
 
 static void
