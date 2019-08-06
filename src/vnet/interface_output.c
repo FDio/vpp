08f2a5dae9663eafa09685ebebf672aa6b0b7ae0
Florin Coras <fcoras@cisco.com>
2019-08-06 13:48:50 -0700

    interface: fix checksum flags
    Type:fix
    Fixes:4208a4c
    
    Change-Id: I7077788cd14f539b06ead5dc65d8411940438bae
    Signed-off-by: Florin Coras <fcoras@cisco.com>
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- src/vnet/interface_output.c
+++ src/vnet/interface_output.c
@@ -224,6 +224,9 @@ calc_checksums (vlib_main_t * vm, vlib_buffer_t * b)
 	    ip6_tcp_udp_icmp_compute_checksum (vm, b, ip6, &bogus);
 	}
     }
+  b->flags &= ~VNET_BUFFER_F_OFFLOAD_TCP_CKSUM;
+  b->flags &= ~VNET_BUFFER_F_OFFLOAD_UDP_CKSUM;
+  b->flags &= ~VNET_BUFFER_F_OFFLOAD_IP_CKSUM;
 }
 
 static_always_inline u16
