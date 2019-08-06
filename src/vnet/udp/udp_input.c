e6c43aec29187fdec5df099e432088080a548eaa
Florin Coras <fcoras@cisco.com>
2019-08-06 11:13:55 -0700

    udp: fix frame flush in input
    Flush frames only for current thread instead of all.
    
    Type:fix
    
    Change-Id: I1c767542d3f40c081a79031c1ccd23ed0c5a00f3
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/vnet/udp/udp_input.c
+++ src/vnet/udp/udp_input.c
@@ -284,7 +284,8 @@ udp46_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
       vlib_put_next_frame (vm, node, next_index, n_left_to_next);
     }
 
-  errors = session_main_flush_all_enqueue_events (TRANSPORT_PROTO_UDP);
+  errors = session_main_flush_enqueue_events (TRANSPORT_PROTO_UDP,
+					      my_thread_index);
   udp_input_inc_counter (vm, is_ip4, UDP_ERROR_EVENT_FIFO_FULL, errors);
   return frame->n_vectors;
 }
