FDio/vpp

commit ae9118055f6236b71145ace400464aa709265ff0

avf: request queues support
Change-Id: I3a4529ff8dae70da99ec95c39ddf788c9498f478
Signed-off-by: Jakub Grajciar <jgrajcia@cisco.com>

src/plugins/avf/device.c
@@ -661,6 +661,46 @@ retry:
 }
 
 clib_error_t *
+avf_request_queues (vlib_main_t * vm, avf_device_t * ad, u16 num_queue_pairs)
+{
+  virtchnl_vf_res_request_t res_req = { 0 };
+  clib_error_t *error;
+  u32 rstat;
+  int n_retry = 20;
+
+  res_req.num_queue_pairs = num_queue_pairs;
+
+  error = avf_send_to_pf (vm, ad, VIRTCHNL_OP_REQUEST_QUEUES, &res_req,
+			  sizeof (virtchnl_vf_res_request_t), &res_req,
+			  sizeof (virtchnl_vf_res_request_t));
+
+  /*
+   * if PF respondes, the request failed
+   * else PF initializes restart and avf_send_to_pf returns an error
+   */
+  if (!error)
+    {
+      return clib_error_return (0, "requested more than %u queue pairs",
+				res_req.num_queue_pairs);
+    }
+
+retry:
+  vlib_process_suspend (vm, 10e-3);
+  rstat = avf_get_u32 (ad->bar0, AVFGEN_RSTAT);
+
+  if ((rstat == VIRTCHNL_VFR_COMPLETED) || (rstat == VIRTCHNL_VFR_VFACTIVE))
+    goto done;
+
+  if (--n_retry == 0)
+    return clib_error_return (0, "reset failed (timeout)");
+
+  goto retry;
+
+done:
+  return NULL;
+}
+
+clib_error_t *
 avf_device_init (vlib_main_t * vm, avf_device_t * ad,
 		 avf_create_if_args_t * args)
 {
@@ -672,8 +712,15 @@ avf_device_init (vlib_main_t * vm, avf_device_t * ad,
 
   avf_adminq_init (vm, ad);
 
-  if ((error = avf_device_reset (vm, ad)))
-    return error;
+  /* request more queues only if we need them */
+  if ((error = avf_request_queues (vm, ad, tm->n_vlib_mains)))
+    {
+      /* we failed to get more queues, but still we want to proceed */
+      clib_error_free (error);
+
+      if ((error = avf_device_reset (vm, ad)))
+	return error;
+    }
 
   avf_adminq_init (vm, ad);
 
@@ -767,6 +814,11 @@ avf_process_one_device (vlib_main_t * vm, avf_device_t * ad, int is_irq)
     return;
 
   ASSERT (ad->error == 0);
+
+  /* do not process device in reset state */
+  r = avf_get_u32 (ad->bar0, AVFGEN_RSTAT);
+  if (r != VIRTCHNL_VFR_VFACTIVE)
+    return;
 
   r = avf_get_u32 (ad->bar0, AVF_ARQLEN);
   if ((r & 0xf0000000) != (1ULL << 31))

src/plugins/avf/cli.c
@@ -104,7 +104,7 @@ avf_delete_command_fn (vlib_main_t * vm, unformat_input_t * input,
 
   hw = vnet_get_sup_hw_interface (vnm, sw_if_index);
   if (hw == NULL || avf_device_class.index != hw->dev_class_index)
-    return clib_error_return (0, "not a AVF interface");
+    return clib_error_return (0, "not an AVF interface");
 
   ad = pool_elt_at_index (am->devices, hw->dev_instance);
 

src/plugins/avf/virtchnl.h
@@ -132,6 +132,13 @@ typedef enum
   VIRTCHNL_VSI_SRIOV = 6,
 } virtchnl_vsi_type_t;
 
+typedef enum
+{
+  VIRTCHNL_VFR_INPROGRESS = 0,
+  VIRTCHNL_VFR_COMPLETED,
+  VIRTCHNL_VFR_VFACTIVE,
+} virtchnl_vfr_states_t;
+
 typedef struct
 {
   u16 vsi_id;
@@ -354,6 +361,12 @@ typedef struct
 } virtchnl_rss_lut_t;
 
 STATIC_ASSERT_SIZEOF (virtchnl_rss_lut_t, 6);
+
+/* VIRTCHNL_OP_REQUEST_QUEUES */
+typedef struct
+{
+  u16 num_queue_pairs;
+} virtchnl_vf_res_request_t;
 
 #endif /* AVF_VIRTCHNL_H */
 

commit 9ef500382e0a4b595faa0734779085951e9939be

avf: properly set irq queue masks
Type: fix
Fixes: ae91180

Change-Id: I9c55c3fdf41089d337b998e2e28abb23d81525ef
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/plugins/avf/device.c
@@ -693,7 +693,8 @@ avf_op_config_irq_map (vlib_main_t * vm, avf_device_t * ad)
 
   imi->vecmap[0].vector_id = 1;
   imi->vecmap[0].vsi_id = ad->vsi_id;
-  imi->vecmap[0].rxq_map = 1;
+  imi->vecmap[0].rxq_map = (1 << ad->n_rx_queues) - 1;
+  imi->vecmap[0].txq_map = (1 << ad->n_tx_queues) - 1;
 
   avf_log_debug (ad, "config_irq_map: vsi_id %u vector_id %u rxq_map %u",
 		 ad->vsi_id, imi->vecmap[0].vector_id,