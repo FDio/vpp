dddc7cc3d4eef2f69fecddfce8fa8ed45c5329ea
Július Milan <julius.milan@pantheon.tech>
2019-08-28 20:40:08 +0200

    af_xdp: add RX/TX functionality
    Type: feature
    
    Signed-off-by: Július Milan <julius.milan@pantheon.tech>
    Change-Id: I2b2854b678e7819f4d399749970731a91823bcfe

--- src/plugins/af_xdp/format.c
+++ src/plugins/af_xdp/format.c
@@ -29,7 +29,7 @@ format_af_xdp_device_name (u8 * s, va_list * args)
   af_xdp_main_t *am = &af_xdp_main;
   af_xdp_device_t *ad = vec_elt_at_index (am->devices, i);
 
-  s = format (s, "af_xdp-%u", ad->dev_instance);
+  s = format (s, "xdp-%s/q%d", ad->ifname, ad->queue_id);
   return s;
 }
 
