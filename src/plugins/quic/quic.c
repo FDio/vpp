55a0891244b2644bcab8b9b63f91fd1bebace7b4
Mathias Raoul <mathias.raoul@gmail.com>
2019-09-10 19:18:51 +0200

    quic: add crypto operations batching
    Type: feature
    
    Signed-off-by: MathiasRaoul <mathias.raoul@gmail.com>
    Change-Id: Idd19f0d3d63586f355e6e1507106f7b68121429f

--- src/plugins/quic/quic.c
+++ src/plugins/quic/quic.c
@@ -429,6 +429,8 @@ quic_send_packets (quic_ctx_t * ctx)
       if ((err = quicly_send (conn, packets, &num_packets)))
 	goto quicly_error;
 
+      quic_crypto_process ();
+
       for (i = 0; i != num_packets; ++i)
 	{
 	  if ((err = quic_send_datagram (udp_session, packets[i])))
@@ -948,7 +950,8 @@ quic_store_quicly_ctx (application_t * app, u8 is_client)
   ptls_ctx->random_bytes = ptls_openssl_random_bytes;
   ptls_ctx->get_time = &ptls_get_time;
   ptls_ctx->key_exchanges = ptls_openssl_key_exchanges;
-  ptls_ctx->cipher_suites = qm->quic_ciphers[qm->default_cipher];
+  //ptls_ctx->cipher_suites = qm->quic_ciphers[qm->default_cipher];
+  ptls_ctx->cipher_suites = quic_crypto_cipher_suites;
   ptls_ctx->certificates.list = NULL;
   ptls_ctx->certificates.count = 0;
   ptls_ctx->esni = NULL;
@@ -990,6 +993,10 @@ quic_store_quicly_ctx (application_t * app, u8 is_client)
   quicly_ctx->cid_encryptor =
     quicly_new_default_cid_encryptor (&ptls_openssl_bfecb,
 				      &ptls_openssl_sha256, key_vec);
+
+  quicly_ctx->crypto_codec = quic_new_crypto_codec ();
+  //quicly_ctx->crypto_codec = quicly_new_default_crypto_codec ();
+
   if (is_client)
     return;
   if (app->tls_key != NULL && app->tls_cert != NULL)
@@ -2088,7 +2095,7 @@ quic_app_rx_callback (session_t * udp_session)
   int rv = 0;
   app = application_get_if_valid (app_index);
   u32 thread_index = vlib_get_thread_index ();
-  quic_rx_packet_ctx_t packets_ctx[16];
+  quic_rx_packet_ctx_t packets_ctx[QUIC_RCV_MAX_BATCH_PACKETS];
 
   if (!app)
     {
@@ -2108,7 +2115,7 @@ quic_app_rx_callback (session_t * udp_session)
 	}
 
       u32 fifo_offset = 0;
-      u32 max_packets = 16;
+      u32 max_packets = QUIC_RCV_MAX_BATCH_PACKETS;
       for (int i = 0; i < max_packets; i++)
 	{
 	  quic_process_one_rx_packet (udp_session_handle,
@@ -2116,6 +2123,8 @@ quic_app_rx_callback (session_t * udp_session)
 				      &fifo_offset, &max_packets, i,
 				      &packets_ctx[i]);
 	}
+
+      quic_decrypt_process ();
 
       for (int i = 0; i < max_packets; i++)
 	{
