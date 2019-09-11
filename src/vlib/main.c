FDio/vpp

commit 9af7e2e87e6a11fb69309fc9ce4bf8432acbc4e3

fix pcap_write function
when use pcap cli to capture pcakets into two files rx01.pcap && rx02.pcap,
the first time:
1)pcap rx trace on max 100 intfc any file rx01.pcap
2)......the process of capture data to buffer......
3)pcap rx trace off

the second time:
4)pcap rx trace on max 100 intfc any file rx02.pcap
5)......the process of capture data to buffer......
6)pcap rx trace off

the pcap_write function bug in this two lines
pm->n_packets_captured = 0;
if (pm->n_packets_captured >= pm->n_packets_to_capture) referring to calling pcap_close()
will result in that the twice pcap cli both writes the packets
into rx01.pcap, but nothing into rx02.pcap. Beside, the rx02.pcap
file will not be created.

solution: separate the pcap_close() out of pcap_write()

Change-Id: Iedeb46f9cf0a4cb12449fd75a4014f95f3bb3fa8
Signed-off-by: Jack Xu <jack.c.xu@ericsson.com>

src/vlib/main.c
@@ -2204,6 +2204,8 @@ pcap_dispatch_trace_command_internal (vlib_main_t * vm,
 		{
 		  pm->n_packets_to_capture = pm->n_packets_captured;
 		  error = pcap_write (pm);
+		  if (pm->file_descriptor >= 0)
+		    pcap_close (pm);
 		  if (error)
 		    clib_error_report (error);
 		  else

src/vnet/interface_cli.c
@@ -1743,6 +1743,8 @@ pcap_trace_command_internal (vlib_main_t * vm,
 		  vm->pcap[rx_tx].pcap_main.n_packets_to_capture =
 		    vm->pcap[rx_tx].pcap_main.n_packets_captured;
 		  error = pcap_write (&vm->pcap[rx_tx].pcap_main);
+		  if (vm->pcap[rx_tx].pcap_main.file_descriptor >= 0)
+		    pcap_close (&vm->pcap[rx_tx].pcap_main);
 		  if (error)
 		    clib_error_report (error);
 		  else

src/vnet/interface_output.c
@@ -1439,6 +1439,8 @@ pcap_drop_trace_command_fn (vlib_main_t * vm,
 		  im->pcap_main.n_packets_to_capture =
 		    im->pcap_main.n_packets_captured;
 		  error = pcap_write (&im->pcap_main);
+		  if (im->pcap_main.file_descriptor >= 0)
+		    pcap_close (&im->pcap_main);
 		  if (error)
 		    clib_error_report (error);
 		  else

src/vnet/pg/output.c
@@ -79,7 +79,10 @@ pg_output (vlib_main_t * vm, vlib_node_runtime_t * node, vlib_frame_t * frame)
     }
   if (pif->pcap_file_name != 0)
     pcap_write (&pif->pcap_main);
-
+  if (pif->pcap_main.file_descriptor >= 0
+      && pif->pcap_main.n_packets_captured >=
+      pif->pcap_main.n_packets_to_capture)
+    pcap_close (&pif->pcap_main);
 
   vlib_buffer_free (vm, vlib_frame_vector_args (frame), n_buffers);
   if (PREDICT_FALSE (pif->lockp != 0))

src/vppinfra/pcap.c
@@ -157,9 +157,6 @@ pcap_write (pcap_main_t * pm)
       pm->n_pcap_data_written = 0;
     }
 
-  if (pm->n_packets_captured >= pm->n_packets_to_capture)
-    pcap_close (pm);
-
 done:
   if (error)
     {

src/vppinfra/pcap_funcs.h
@@ -22,6 +22,9 @@ clib_error_t *pcap_write (pcap_main_t * pm);
 /** Read data from file. */
 clib_error_t *pcap_read (pcap_main_t * pm);
 
+/** Close the file created by pcap_write function. */
+clib_error_t *pcap_close (pcap_main_t * pm);
+
 /**
  * @brief Add packet
  *

commit cd57376d6df3e4acf1db527b88f692e72e19f60c

pcap: use the correct condition for checking if the pcap fd is open
The 9af7e2e87e used a comparison that fd is >= 0 to check that
the pcap needs closing. While the pcap_close() function does
reset the file descriptor to -1, the freshly initialized structure
has it equal to 0.

This causes the VPP to close stdin if the packets are being seen
on pg interface without the capture file being opened.
This triggers the vpp attempting to read from STDIN
(another bug), which results in running out of memory.

Change-Id: I11d61422701500a9b3e0dd52d59383f297d57f54
Type: fix
Fixes: 9af7e2e87e
Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>

src/vlib/main.c
@@ -2254,7 +2254,7 @@ vlib_pcap_dispatch_trace_configure (vlib_pcap_dispatch_trace_args_t * a)
 	  vlib_cli_output (vm, "Write %d packets to %s, and stop capture...",
 			   pm->n_packets_captured, pm->file_name);
 	  error = pcap_write (pm);
-	  if (pm->file_descriptor >= 0)
+	  if (pm->flags & PCAP_MAIN_INIT_DONE)
 	    pcap_close (pm);
 	  /* Report I/O errors... */
 	  if (error)

src/vnet/interface_cli.c
@@ -1766,7 +1766,7 @@ vnet_pcap_dispatch_trace_configure (vnet_pcap_dispatch_trace_args_t * a)
 	  vlib_cli_output (vm, "Write %d packets to %s, and stop capture...",
 			   pm->n_packets_captured, pm->file_name);
 	  error = pcap_write (pm);
-	  if (pm->file_descriptor >= 0)
+	  if (pm->flags & PCAP_MAIN_INIT_DONE)
 	    pcap_close (pm);
 	  /* Report I/O errors... */
 	  if (error)

src/vnet/interface_output.c
@@ -1441,7 +1441,7 @@ pcap_drop_trace_command_fn (vlib_main_t * vm,
 		  im->pcap_main.n_packets_to_capture =
 		    im->pcap_main.n_packets_captured;
 		  error = pcap_write (&im->pcap_main);
-		  if (im->pcap_main.file_descriptor >= 0)
+		  if (im->pcap_main.flags & PCAP_MAIN_INIT_DONE)
 		    pcap_close (&im->pcap_main);
 		  if (error)
 		    clib_error_report (error);

src/vnet/pg/output.c
@@ -79,7 +79,7 @@ pg_output (vlib_main_t * vm, vlib_node_runtime_t * node, vlib_frame_t * frame)
     }
   if (pif->pcap_file_name != 0)
     pcap_write (&pif->pcap_main);
-  if (pif->pcap_main.file_descriptor >= 0
+  if ((pif->pcap_main.flags & PCAP_MAIN_INIT_DONE)
       && pif->pcap_main.n_packets_captured >=
       pif->pcap_main.n_packets_to_capture)
     pcap_close (&pif->pcap_main);