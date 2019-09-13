e2f4fc108291da9abfe4d8f968abd58c3d52a8a3
Nathan Skrzypczak <nathan.skrzypczak@gmail.com>
2019-09-13 17:14:57 +0200

    session: fix msg freeing on error
    Type: fix
    
    Change-Id: I0807f84737d5e98e69e9ed27a1de6813b2ddd138
    Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

--- src/svm/message_queue.c
+++ src/svm/message_queue.c
@@ -129,6 +129,7 @@ svm_msg_q_lock_and_alloc_msg_w_ring (svm_msg_q_t * mq, u32 ring_index,
       *msg = svm_msg_q_alloc_msg_w_ring (mq, ring_index);
       if (PREDICT_FALSE (svm_msg_q_msg_is_invalid (msg)))
 	{
+	  svm_msg_q_free_msg (mq, msg);
 	  svm_msg_q_unlock (mq);
 	  return -2;
 	}
