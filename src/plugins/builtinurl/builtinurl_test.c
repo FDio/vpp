91b9d1bc6f0ea9d0c02e5f9fafdf0792da3a2d47
Dave Barach <dave@barachs.net>
2019-09-17 09:47:35 -0400

    builtinurl: initial working attempt
    Note that the builtin URLs are disabled by default. To activate,
    "builtinurl enable" or use the builtinurl_enable API.
    
    See .../extras/http/sample.md for some Hugo-friendly .md w/ embedded
    Javascript that accesses the builtin URLs.
    
    Type: feature
    
    Signed-off-by: Dave Barach <dave@barachs.net>
    Change-Id: I6d82d9292c41d6d2d90be73ba8a1a043fb20c986

---
+++ src/plugins/builtinurl/builtinurl_test.c
@@ -0,0 +1,149 @@
+/*
+ * builtinurl.c - skeleton vpp-api-test plug-in
+ *
+ * Copyright (c) 2019 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <vat/vat.h>
+#include <vlibapi/api.h>
+#include <vlibmemory/api.h>
+#include <vppinfra/error.h>
+#include <stdbool.h>
+
+uword unformat_sw_if_index (unformat_input_t * input, va_list * args);
+
+/* Declare message IDs */
+#include <builtinurl/builtinurl_msg_enum.h>
+
+/* define message structures */
+#define vl_typedefs
+#include <builtinurl/builtinurl_all_api_h.h>
+#undef vl_typedefs
+
+/* declare message handlers for each api */
+
+#define vl_endianfun		/* define message structures */
+#include <builtinurl/builtinurl_all_api_h.h>
+#undef vl_endianfun
+
+/* instantiate all the print functions we know about */
+#define vl_print(handle, ...)
+#define vl_printfun
+#include <builtinurl/builtinurl_all_api_h.h>
+#undef vl_printfun
+
+/* Get the API version number. */
+#define vl_api_version(n,v) static u32 api_version=(v);
+#include <builtinurl/builtinurl_all_api_h.h>
+#undef vl_api_version
+
+
+typedef struct
+{
+  /* API message ID base */
+  u16 msg_id_base;
+  vat_main_t *vat_main;
+} builtinurl_test_main_t;
+
+builtinurl_test_main_t builtinurl_test_main;
+
+#define __plugin_msg_base builtinurl_test_main.msg_id_base
+#include <vlibapi/vat_helper_macros.h>
+
+#define foreach_standard_reply_retval_handler   \
+_(builtinurl_enable_reply)
+
+#define _(n)                                            \
+    static void vl_api_##n##_t_handler                  \
+    (vl_api_##n##_t * mp)                               \
+    {                                                   \
+        vat_main_t * vam = builtinurl_test_main.vat_main;   \
+        i32 retval = ntohl(mp->retval);                 \
+        if (vam->async_mode) {                          \
+            vam->async_errors += (retval < 0);          \
+        } else {                                        \
+            vam->retval = retval;                       \
+            vam->result_ready = 1;                      \
+        }                                               \
+    }
+foreach_standard_reply_retval_handler;
+#undef _
+
+/*
+ * Table of message reply handlers, must include boilerplate handlers
+ * we just generated
+ */
+#define foreach_vpe_api_reply_msg                                       \
+_(BUILTINURL_ENABLE_REPLY, builtinurl_enable_reply)
+
+
+static int
+api_builtinurl_enable_disable (vat_main_t * vam)
+{
+  vl_api_builtinurl_enable_t *mp;
+  int ret;
+
+  /* Construct the API message */
+  M (BUILTINURL_ENABLE, mp);
+
+  /* send it... */
+  S (mp);
+
+  /* Wait for a reply... */
+  W (ret);
+  return ret;
+}
+
+/*
+ * List of messages that the api test plugin sends,
+ * and that the data plane plugin processes
+ */
+#define foreach_vpe_api_msg \
+_(builtinurl_enable_disable, "")
+
+static void
+builtinurl_api_hookup (vat_main_t * vam)
+{
+  builtinurl_test_main_t *btmp = &builtinurl_test_main;
+  /* Hook up handlers for replies from the data plane plug-in */
+#define _(N,n)                                                  \
+    vl_msg_api_set_handlers((VL_API_##N + btmp->msg_id_base),     \
+                           #n,                                  \
+                           vl_api_##n##_t_handler,              \
+                           vl_noop_handler,                     \
+                           vl_api_##n##_t_endian,               \
+                           vl_api_##n##_t_print,                \
+                           sizeof(vl_api_##n##_t), 1);
+  foreach_vpe_api_reply_msg;
+#undef _
+
+  /* API messages we can send */
+#define _(n,h) hash_set_mem (vam->function_by_name, #n, api_##n);
+  foreach_vpe_api_msg;
+#undef _
+
+  /* Help strings */
+#define _(n,h) hash_set_mem (vam->help_by_name, #n, h);
+  foreach_vpe_api_msg;
+#undef _
+}
+
+VAT_PLUGIN_REGISTER (builtinurl);
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
