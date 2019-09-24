21e64613c965abb05325df1c761d17fecd70f13e
MathiasRaoul <mathias.raoul@gmail.com>
2019-09-24 12:22:53 +0000

    quic: create quic_error_def
    create quic_error_def, add internal counter for Tx/Rx Packets
    
    Type: feature
    
    Change-Id: I25aeeed49fc569315296a73c5595c2e2e302434f
    Signed-off-by: MathiasRaoul <mathias.raoul@gmail.com>

--- src/plugins/quic/quic.c
+++ src/plugins/quic/quic.c
@@ -30,6 +30,11 @@
 
 #include <quicly/defaults.h>
 
+static char *quic_error_strings[] = {
+#define quic_error(n,s) s,
+#include "quic_error.def"
+#undef quic_error
+};
 
 static quic_main_t quic_main;
 static void quic_update_timer (quic_ctx_t * ctx);
@@ -391,8 +396,14 @@ quic_send_datagram (session_t * udp_session, quicly_datagram_t * packet)
       return QUIC_ERROR_FULL_FIFO;
     }
 
+<<<<<<< HEAD
   quic_increment_counter (QUIC_ERROR_TX_PACKETS, 1);
 
+=======
+  vlib_main_t *vm = vlib_get_main ();
+  vlib_node_increment_counter (vm, quic_input_node.index,
+			       QUIC_ERROR_TX_PACKETS, 1);
+>>>>>>> ddba5ef68... quic: create quic_error_def
   return 0;
 }
 
@@ -2033,7 +2044,9 @@ quic_process_one_rx_packet (u64 udp_session_handle,
       return 1;
     }
 
-  quic_increment_counter (QUIC_ERROR_RX_PACKETS, 1);
+  vlib_main_t *vm = vlib_get_main ();
+  vlib_node_increment_counter (vm, quic_input_node.index,
+			       QUIC_ERROR_RX_PACKETS, 1);
 
   rv = 0;
   quic_build_sockaddr (sa, &salen, &ph.rmt_ip, ph.rmt_port, ph.is_ip4);
@@ -2375,6 +2388,24 @@ VLIB_PLUGIN_REGISTER () =
   .version = VPP_BUILD_VER,
   .description = "Quic transport protocol",
   .default_disabled = 1,
+};
+
+static uword
+quic_node_fn (vlib_main_t * vm, vlib_node_runtime_t * node,
+	      vlib_frame_t * frame)
+{
+  return 0;
+}
+
+/* *INDENT-OFF* */
+VLIB_REGISTER_NODE (quic_input_node) =
+{
+  .function = quic_node_fn,
+  .name = "quic-input",
+  .vector_size = sizeof (u32),
+  .type = VLIB_NODE_TYPE_INTERNAL,
+  .n_errors = ARRAY_LEN (quic_error_strings),
+  .error_strings = quic_error_strings,
 };
 /* *INDENT-ON* */
 
