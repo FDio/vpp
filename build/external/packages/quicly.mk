eaa83f79e6f8ef067091523950ac4606bd8e5664
MathiasRaoul <mathias.raoul@gmail.com>
2019-10-02 14:56:15 +0000

    quic: update quicly to v0.0.5
    -Update quicly patches
    -Remove event logger (removed from quicly).
    -Update quicly functions calls
    
    Type: feature
    
    Signed-off-by: MathiasRaoul <mathias.raoul@gmail.com>
    Change-Id: I0355befeb5ce961928f17225c83890ba87cf6ebc

--- build/external/packages/quicly.mk
+++ build/external/packages/quicly.mk
@@ -11,9 +11,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-quicly_version := 0.0.3-vpp
+quicly_version := 0.0.5-vpp
 quicly_tarball := quicly_$(quicly_version).tar.gz
-quicly_tarball_md5sum := fa4be6d9039967b380bbf14c96e34cdc
+quicly_tarball_md5sum := 47f6d6324412efcbd058cce1b1ebc045
 quicly_tarball_strip_dirs := 1
 quicly_url := https://github.com/vpp-quic/quicly/releases/download/v$(quicly_version)/quicly_$(quicly_version).tar.gz
 
