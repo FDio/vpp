43242b40e29104c17c9d6f6a1fc7c8ac327112f2
Tetsuya Murakami <tetsuya.mrk@gmail.com>
2019-10-03 16:05:01 -0700

    support QFI mapping

--- src/plugins/srv6-mobile/node.c
+++ src/plugins/srv6-mobile/node.c
@@ -337,7 +337,7 @@ VLIB_NODE_FN (srv6_end_m_gtp4_e) (vlib_main_t * vm,
 
 		  hdr0->gtpu.ext->nextexthdr = GTPU_EXTHDR_PDU_SESSION;
 
-		  sess = (gtpu_pdu_session_t *)(((char *)hdr0) + sizeof(ipv4_gtpu_header_t) + sizeof(gtpu_exthdr_t));
+		  sess = (gtpu_pdu_session_t *)(((char *)hdr0) + sizeof(ip4_gtpu_header_t) + sizeof(gtpu_exthdr_t));
 		  sess->exthdrlen = 1;
 		  sess->type = 0;
 		  sess->spare = 0;
