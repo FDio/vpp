10441be971f759d6e9b78c7a4f21122e28aa99ab
Srikanth A <srakula@cisco.com>
2019-10-02 17:48:58 -0700

    tcp: custom checksum calculations for Ipv4/Ipv6
    Type: feature
    
    Based on the configuration, we can disable checksum offload capability
    and calculate checksum while pushing the TCP & IP header.
    This saves some cycles when VPP stack is used in legacy hardware devices.
    
    Signed-off-by: Srikanth A <srakula@cisco.com>
    Change-Id: Ic1b3fcf3040917e47ee65263694ebf7437ac5668

--- src/vnet/tcp/tcp.h
+++ src/vnet/tcp/tcp.h
@@ -120,6 +120,7 @@ extern timer_expiration_handler tcp_timer_retransmit_syn_handler;
   _(RATE_SAMPLE, "Conn does rate sampling")	\
   _(TRACK_BURST, "Track burst")			\
   _(ZERO_RWND_SENT, "Zero RWND sent")		\
+  _(NO_CSUM_OFFLOAD, "No Checksum Offload")     \
 
 typedef enum _tcp_connection_flag_bits
 {
@@ -1233,6 +1234,7 @@ vlib_buffer_push_tcp_net_order (vlib_buffer_t * b, u16 sp, u16 dp, u32 seq,
   th->window = wnd;
   th->checksum = 0;
   th->urgent_pointer = 0;
+  vnet_buffer (b)->l4_hdr_offset = (u8 *) th - b->data;
   return th;
 }
 
