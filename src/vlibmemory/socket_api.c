498e43d1850a8c2202ee792792a7265ea0f09bba
Vratko Polak <vrpolak@cisco.com>
2019-10-07 15:10:07 +0200

    api: fix vl_socket_write_ready
    When decrementing, use the number of bytes actually sent.
    Handle the case when the number of bytes sent is zero.
    
    Type: fix
    
    Change-Id: If042d479d5cb1177c52dff1bb6d1cfbba339fd6f
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- src/vlibmemory/socket_api.c
+++ src/vlibmemory/socket_api.c
@@ -386,7 +386,7 @@ vl_socket_write_ready (clib_file_t * uf)
 {
   clib_file_main_t *fm = &file_main;
   vl_api_registration_t *rp;
-  int n;
+  int bytes_sent;
 
   u32 reg_index = uf->private_data;
   if (is_being_removed_reg_index (reg_index))
@@ -403,8 +403,8 @@ vl_socket_write_ready (clib_file_t * uf)
   while (remaining_bytes > 0)
     {
       bytes_to_send = remaining_bytes > 4096 ? 4096 : remaining_bytes;
-      n = write (uf->file_descriptor, p, bytes_to_send);
-      if (n < 0)
+      bytes_sent = write (uf->file_descriptor, p, bytes_to_send);
+      if (bytes_sent <= 0)
 	{
 	  if (errno == EAGAIN)
 	    {
@@ -416,8 +416,8 @@ vl_socket_write_ready (clib_file_t * uf)
 	  vl_socket_request_remove_reg_index (reg_index);
 	  return 0;
 	}
-      remaining_bytes -= bytes_to_send;
-      p += bytes_to_send;
+      remaining_bytes -= bytes_sent;
+      p += bytes_sent;
     }
 
   vec_delete (rp->output_vector, total_bytes - remaining_bytes, 0);
