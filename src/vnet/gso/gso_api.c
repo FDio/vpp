1ad19bc45d1c3487c33b6e4a1d7a0314d501b155
Mohsin Kazmi <sykazmi@cisco.com>
2019-10-08 19:42:38 +0200

    gso: Add gso feature arc
    Type: refactor
    
    Change-Id: Ib4a8e7e64093b2db1deda6663325080bc8337605
    Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>

---
+++ src/vnet/gso/gso_api.c
@@ -0,0 +1,71 @@
+/*
+ *------------------------------------------------------------------
+ * gso_api.c - Generic Segmentation Offload api
+ *
+ * Copyright (c) 2019 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *------------------------------------------------------------------
+ */
+
+#include <vlibmemory/api.h>
+#include <vnet/gso/gso.h>
+#include <vnet/vnet.h>
+
+#include <vnet/gso/gso.api_enum.h>
+#include <vnet/gso/gso.api_types.h>
+
+#define REPLY_MSG_ID_BASE gm->msg_id_base
+#include <vlibapi/api_helper_macros.h>
+
+static void
+  vl_api_feature_gso_enable_disable_t_handler
+  (vl_api_feature_gso_enable_disable_t * mp)
+{
+  gso_main_t *gm = &gso_main;
+  vl_api_feature_gso_enable_disable_reply_t *rmp;
+  int rv = 0;
+
+  VALIDATE_SW_IF_INDEX (mp);
+
+  rv =
+    vnet_sw_interface_gso_enable_disable (ntohl (mp->sw_if_index),
+					  mp->enable_disable);
+
+  BAD_SW_IF_INDEX_LABEL;
+
+  REPLY_MACRO (VL_API_FEATURE_GSO_ENABLE_DISABLE_REPLY);
+}
+
+#include <vnet/gso/gso.api.c>
+
+static clib_error_t *
+feature_gso_api_hookup (vlib_main_t * vm)
+{
+  gso_main_t *gm = &gso_main;
+  /*
+   * Set up the (msg_name, crc, message-id) table
+   */
+  gm->msg_id_base = setup_message_id_table ();
+
+  return 0;
+}
+
+VLIB_API_INIT_FUNCTION (feature_gso_api_hookup);
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
