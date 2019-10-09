3a693c0008ec4ece1abb6e99888b3cbe58a12480
Ole Troan <ot@cisco.com>
2019-10-09 13:33:19 +0200

    ip: functional interface to ip fragmentation
    This provides a functional interface to IP fragmentation.
    Allowing external features to fragment. Supports
    arbitrary encap size, for e.g. MPLS or inner fragmentation
    of tunnels.
    
    Type: fix
    Signed-off-by: Ole Troan <ot@cisco.com>
    Change-Id: Ia89ecec8ee3cbe2416edbe87630fdb714898c2a8
    Signed-off-by: Ole Troan <ot@cisco.com>

--- src/vnet/ip/ip4_forward.c
+++ src/vnet/ip/ip4_forward.c
@@ -2312,8 +2312,8 @@ ip4_mtu_check (vlib_buffer_t * b, u16 packet_len,
 	  /* IP fragmentation */
 	  ip_frag_set_vnet_buffer (b, adj_packet_bytes,
 				   (is_midchain ?
-				    IP4_FRAG_NEXT_IP4_REWRITE_MIDCHAIN :
-				    IP4_FRAG_NEXT_IP4_REWRITE), 0);
+				    IP_FRAG_NEXT_IP_REWRITE_MIDCHAIN :
+				    IP_FRAG_NEXT_IP_REWRITE), 0);
 	  *next = IP4_REWRITE_NEXT_FRAGMENT;
 	}
     }
