81928f3ca36db7fd08948e507717a59d3324468f
Andrew Yourtchenko <ayourtch@gmail.com>
2019-10-13 18:56:03 +0000

    tests: make RA tests run on VPP time
    the IPv6 RA tests take timing into the account,
    but the time inside VPP may go slightly differently compared
    to the time inside the driving python thread,
    if the machine running the tests is heavily loaded.
    Make a sleep function which sleeps "on VPP time" and use it.
    
    Change-Id: I3b34b0164f6e0ec7a619b92ee308089a4a8935e3
    Type: test
    Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>

--- test/framework.py
+++ test/framework.py
@@ -745,6 +745,20 @@ class VppTestCase(unittest.TestCase):
                                 if name != cap_name]
 
     @classmethod
+    def get_vpp_time(cls):
+        return float(cls.vapi.cli('show clock').replace("Time now ", ""))
+
+    @classmethod
+    def sleep_on_vpp_time(cls, sec):
+        """ Sleep according to time in VPP world """
+        # On a busy system with many processes
+        # we might end up with VPP time being slower than real world
+        # So take that into account when waiting for VPP to do something
+        start_time = cls.get_vpp_time()
+        while cls.get_vpp_time() - start_time < sec:
+            cls.sleep(0.1)
+
+    @classmethod
     def pg_start(cls):
         """ Remove any zombie captures and enable the packet generator """
         # how long before capture is allowed to be deleted - otherwise vpp
