072051b97a8ce1b8e2794d60aa05fda82fe22882
Ole Troan <ot@cisco.com>
2019-10-21 19:52:06 +0200

    classify: tests to support python3
    Type: fix
    Signed-off-by: Ole Troan <ot@cisco.com>
    Change-Id: I8af6ee6e5239f0836978baa063a18a01c610279f

--- test/run_tests.py
+++ test/run_tests.py
@@ -760,7 +760,7 @@ if __name__ == '__main__':
                                 % (min_req_shm >> 20))
             else:
                 extra_shm = shm_free - min_req_shm
-                shm_max_processes += extra_shm / shm_per_process
+                shm_max_processes += extra_shm // shm_per_process
             concurrent_tests = min(cpu_count(), shm_max_processes)
             print('Found enough resources to run tests with %s cores'
                   % concurrent_tests)
