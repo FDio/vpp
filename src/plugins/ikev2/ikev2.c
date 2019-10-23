cb3cfe876b3bbe2f360e3b32a43a060bdcb8bf9e
Filip Tehlar <ftehlar@cisco.com>
2019-10-23 11:11:42 +0000

    ikev2: fix traffic selector match
    Type: fix
    
    Change-Id: I81ab3dcd03f397b3d275da6cfa094e048ad92f95
    Signed-off-by: Filip Tehlar <ftehlar@cisco.com>

--- src/plugins/ikev2/ikev2.c
+++ src/plugins/ikev2/ikev2.c
@@ -1308,7 +1308,7 @@ ikev2_sa_match_ts (ikev2_sa_t * sa)
       {
         if (ikev2_ts_cmp(p_tsi, ts))
           {
-            tsi = vec_dup(ts);
+            vec_add1 (tsi, ts[0]);
             break;
           }
       }
@@ -1317,7 +1317,7 @@ ikev2_sa_match_ts (ikev2_sa_t * sa)
       {
         if (ikev2_ts_cmp(p_tsr, ts))
           {
-            tsr = vec_dup(ts);
+            vec_add1 (tsr, ts[0]);
             break;
           }
       }
