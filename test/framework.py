23ebf2b6236a0eb7ce0d7f3d25176bc469bdce78
Paul Vinciguerra <pvinci@vinciconsulting.com>
2019-10-28 09:04:07 -0400

    tests: remove python2isms from framework.py
    Type: test
    
    Change-Id: I5d8880a80128cc388d370b34dd6b7dae61f830c8
    Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

--- test/framework.py
+++ test/framework.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
 import gc
 import sys
 import os
 import select
 import signal
+import subprocess
 import unittest
 import tempfile
 import time
@@ -37,19 +37,6 @@ from util import ppp, is_core_present
 from scapy.layers.inet import IPerror, TCPerror, UDPerror, ICMPerror
 from scapy.layers.inet6 import ICMPv6DestUnreach, ICMPv6EchoRequest
 from scapy.layers.inet6 import ICMPv6EchoReply
-
-if os.name == 'posix' and sys.version_info[0] < 3:
-    # using subprocess32 is recommended by python official documentation
-    # @ https://docs.python.org/2/library/subprocess.html
-    import subprocess32 as subprocess
-else:
-    import subprocess
-
-#  Python2/3 compatible
-try:
-    input = raw_input
-except NameError:
-    pass
 
 PASS = 0
 FAIL = 1
@@ -309,9 +296,9 @@ class VppTestCase(unittest.TestCase):
 
     @staticmethod
     def get_least_used_cpu():
-        cpu_usage_list = [set(range(psutil.cpu_count()))]
-        vpp_processes = [p for p in psutil.process_iter(attrs=['pid', 'name'])
-                         if 'vpp_main' == p.info['name']]
+        cpu_usage_list = [set(range(os.cpu_count()))]
+        vpp_processes = (p for p in psutil.process_iter(attrs=['pid', 'name'])
+                         if 'vpp_main' == p.info['name'])
         for vpp_process in vpp_processes:
             for cpu_usage_set in cpu_usage_list:
                 try:
