0d8d3f60616a8ff623637a5d98c36b73f8bc03e3
Aleksander Djuric <aleksander.djuric@gmail.com>
2019-10-31 14:35:21 +0300

    interface: shmemioerror while getting name_filter arg
    Type: fix
    
    Signed-off-by: Aleksander Djuric <aleksander.djuric@gmail.com>
    Change-Id: I5e0eb7024d208040d79e9d6db863f41e2ecf4ee6
    Signed-off-by: Ole Troan <ot@cisco.com>

--- src/vnet/interface_api.c
+++ src/vnet/interface_api.c
@@ -358,9 +358,8 @@ vl_api_sw_interface_dump_t_handler (vl_api_sw_interface_dump_t * mp)
 
   if (mp->name_filter_valid)
     {
-      filter =
-	format (0, ".*%s", vl_api_string_len (&mp->name_filter),
-		vl_api_from_api_string (&mp->name_filter), 0);
+      filter = vl_api_from_api_to_vec (&mp->name_filter);
+      vec_add1 (filter, 0);	/* Ensure it's a C string for strcasecmp() */
     }
 
   char *strcasestr (char *, char *);	/* lnx hdr file botch */
