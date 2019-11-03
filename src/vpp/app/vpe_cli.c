6d7913dbada06f44ae95c2a8139e6dabd31c24f1
Paul Vinciguerra <pvinci@vinciconsulting.com>
2019-11-03 09:50:15 -0500

    vpp: fix .short_help for "ip virtual"
    Type: fix
    
    "# <feature-name>: <subject>
    
    Change-Id: I8b6b6b8c70faec7cd95e1842259e907fb9587017
    Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

--- src/vpp/app/vpe_cli.c
+++ src/vpp/app/vpe_cli.c
@@ -122,7 +122,7 @@ done:
 /* *INDENT-OFF* */
 VLIB_CLI_COMMAND (virtual_ip_cmd_fn_command, static) = {
   .path = "ip virtual",
-  .short_help = "ip virtual <addr> <interface> [mac <Mi>]+",
+  .short_help = "ip virtual <addr> <interface> [mac <Mi>]+ [next-hop <ip4_address>]+",
   .function = virtual_ip_cmd_fn_command_fn,
 };
 /* *INDENT-ON* */
