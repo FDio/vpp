841e2af17cd59e5969d515a4c0bc30b183a7c181
Tetsuya Murakami <tetsuya.mrk@gmail.com>
2019-11-06 11:05:51 -0800

    srv6-mobile
    Type: feature
    
    Plug-in for IPv6 Segment Routing Mobile
    
    This funcion was developed by
    
    Signed-off-by: Satoru Matsuchima <satoru.matsushima@gmail.com>
    Signed-off-by: Filip Varga <filipvarga89@gmail.com>
    Signed-off-by: Tetsuya Murakami <tetsuya.mrk@gmail.com>
    
    Signed-off-by: Tetsuya Murakami <tetsuya.mrk@gmail.com>
    Change-Id: Ie995adc73d8f8d444339aab71619c3599e69f12d
    Signed-off-by: Tetsuya Murakami <tetsuya.mrk@gmail.com>

--- src/vnet/srv6/sr.h
+++ src/vnet/srv6/sr.h
@@ -54,6 +54,14 @@
 
 #define SR_SEGMENT_LIST_WEIGHT_DEFAULT 1
 
+/* *INDENT-OFF* */
+typedef struct
+{
+  ip6_header_t ip;
+  ip6_sr_header_t sr;
+} __attribute__ ((packed)) ip6srv_combo_header_t;
+/* *INDENT-ON* */
+
 /**
  * @brief SR Segment List (SID list)
  */
@@ -69,6 +77,12 @@ typedef struct
   dpo_id_t bsid_dpo;				/**< DPO for Encaps/Insert for BSID */
   dpo_id_t ip6_dpo;				/**< DPO for Encaps/Insert IPv6 */
   dpo_id_t ip4_dpo;				/**< DPO for Encaps IPv6 */
+
+  u8 is_tmap;				     /**< T.M.Tmap dynamically compute node ID */
+  u16 sr_prefixlen;
+  ip6_address_t local_prefix;		/**< T.M.Tmap localsid prefix */
+  u16 localsid_len;
+
 } ip6_sr_sl_t;
 
 /* SR policy types */
@@ -94,6 +108,10 @@ typedef struct
   u32 fib_table;			/**< FIB table */
 
   u8 is_encap;				/**< Mode (0 is SRH insert, 1 Encaps) */
+  u8 is_tmap;			    /**< T.M.Tmap dynamically compute node ID */
+
+  u16 sr_prefixlen;
+  u16 localsid_len;
 } ip6_sr_policy_t;
 
 /**
@@ -102,6 +120,8 @@ typedef struct
 typedef struct
 {
   ip6_address_t localsid;		/**< LocalSID IPv6 address */
+
+  u16 localsid_len;
 
   char end_psp;					/**< Combined with End.PSP? */
 
@@ -140,6 +160,8 @@ typedef struct
   u8 *def_str;								/**< Behavior definition (i.e. Endpoint with cross-connect) */
 
   u8 *params_str;							/**< Behavior parameters (i.e. <oif> <IP46next_hop>) */
+
+  u8 prefix_length;
 
   dpo_type_t dpo;							/**< DPO type registration */
 
@@ -250,7 +272,8 @@ extern void sr_dpo_unlock (dpo_id_t * dpo);
 extern int
 sr_localsid_register_function (vlib_main_t * vm, u8 * fn_name,
 			       u8 * keyword_str, u8 * def_str,
-			       u8 * params_str, dpo_type_t * dpo,
+			       u8 * params_str, u8 prefix_length,
+			       dpo_type_t * dpo,
 			       format_function_t * ls_format,
 			       unformat_function_t * ls_unformat,
 			       sr_plugin_callback_t * creation_fn,
@@ -258,15 +281,16 @@ sr_localsid_register_function (vlib_main_t * vm, u8 * fn_name,
 
 extern int
 sr_policy_add (ip6_address_t * bsid, ip6_address_t * segments,
-	       u32 weight, u8 behavior, u32 fib_table, u8 is_encap);
-extern int
-sr_policy_mod (ip6_address_t * bsid, u32 index, u32 fib_table,
-	       u8 operation, ip6_address_t * segments, u32 sl_index,
-	       u32 weight);
+	       u32 weight, u8 behavior, u32 fib_table, u8 is_encap,
+	       u8 is_gtp4_removal, u16 sr_prefixlen,
+	       ip6_address_t * gtp4_localsid_prefix, u16 localsid_len);
+extern int sr_policy_mod (ip6_address_t * bsid, u32 index, u32 fib_table,
+			  u8 operation, ip6_address_t * segments,
+			  u32 sl_index, u32 weight);
 extern int sr_policy_del (ip6_address_t * bsid, u32 index);
 
 extern int
-sr_cli_localsid (char is_del, ip6_address_t * localsid_addr,
+sr_cli_localsid (char is_del, ip6_address_t * localsid_addr, u16 prefixlen,
 		 char end_psp, u8 behavior, u32 sw_if_index,
 		 u32 vlan_index, u32 fib_table, ip46_address_t * nh_addr,
 		 void *ls_plugin_mem);
