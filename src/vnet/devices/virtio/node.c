FDio/vpp

commit 8389fb9112bcf96def69539fa1de13a7a08923f5

virtio: fast TAP interfaces with vhost-net backend
Change-Id: Ided667356d5c6fb9648eb34685aabd6b16a598b7
Signed-off-by: Damjan Marion <damarion@cisco.com>
Signed-off-by: Steven Luong <sluong@cisco.com>

src/vnet/devices/virtio/node.c
@@ -1,0 +1,302 @@
+/*
+ *------------------------------------------------------------------
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *------------------------------------------------------------------
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <net/if.h>
+#include <linux/if_tun.h>
+#include <sys/ioctl.h>
+#include <linux/virtio_net.h>
+#include <linux/vhost.h>
+#include <sys/eventfd.h>
+
+#include <vlib/vlib.h>
+#include <vlib/unix/unix.h>
+#include <vnet/ethernet/ethernet.h>
+#include <vnet/devices/devices.h>
+#include <vnet/feature/feature.h>
+#include <vnet/devices/virtio/virtio.h>
+
+
+#define foreach_virtio_input_error \
+  _(UNKNOWN, "unknown")
+
+typedef enum
+{
+#define _(f,s) TAP_INPUT_ERROR_##f,
+  foreach_virtio_input_error
+#undef _
+    TAP_INPUT_N_ERROR,
+} virtio_input_error_t;
+
+static char *virtio_input_error_strings[] = {
+#define _(n,s) s,
+  foreach_virtio_input_error
+#undef _
+};
+
+typedef struct
+{
+  u32 next_index;
+  u32 hw_if_index;
+  u16 ring;
+  u16 len;
+  struct virtio_net_hdr_v1 hdr;
+} virtio_input_trace_t;
+
+static u8 *
+format_virtio_input_trace (u8 * s, va_list * args)
+{
+  CLIB_UNUSED (vlib_main_t * vm) = va_arg (*args, vlib_main_t *);
+  CLIB_UNUSED (vlib_node_t * node) = va_arg (*args, vlib_node_t *);
+  virtio_input_trace_t *t = va_arg (*args, virtio_input_trace_t *);
+  u32 indent = format_get_indent (s);
+
+  s = format (s, "virtio: hw_if_index %d next-index %d vring %u len %u",
+	      t->hw_if_index, t->next_index, t->ring, t->len);
+  s = format (s, "\n%Uhdr: flags 0x%02x gso_type 0x%02x hdr_len %u "
+	      "gso_size %u csum_start %u csum_offset %u num_buffers %u",
+	      format_white_space, indent + 2,
+	      t->hdr.flags, t->hdr.gso_type, t->hdr.hdr_len, t->hdr.gso_size,
+	      t->hdr.csum_start, t->hdr.csum_offset, t->hdr.num_buffers);
+  return s;
+}
+
+static_always_inline void
+virtio_refill_vring (vlib_main_t * vm, virtio_vring_t * vring)
+{
+  const int hdr_sz = sizeof (struct virtio_net_hdr_v1);
+  u16 used, next, avail, n_slots, n_alloc;
+  u16 sz = vring->size;
+  u16 mask = sz - 1;
+  int i;
+
+  used = vring->desc_in_use;
+
+  if (sz - used < sz / 8)
+    return;
+
+  n_slots = sz - used;
+  next = vring->desc_next;
+  avail = vring->avail->idx;
+  n_alloc = vlib_buffer_alloc (vm, &vring->buffers[next], n_slots);
+
+  if (PREDICT_FALSE (n_alloc < n_slots))
+    n_slots = n_alloc;
+
+  i = next + n_slots - sz;
+  if (PREDICT_FALSE (i > 0))
+    clib_memcpy (vring->buffers, &vring->buffers[sz], i * sizeof (u32));
+
+  while (n_slots)
+    {
+      struct vring_desc *d = &vring->desc[next];;
+      vlib_buffer_t *b = vlib_get_buffer (vm, vring->buffers[next]);
+      d->addr = pointer_to_uword (vlib_buffer_get_current (b)) - hdr_sz;
+      d->len = VLIB_BUFFER_DATA_SIZE + hdr_sz;
+      d->flags = VRING_DESC_F_WRITE;
+      vring->avail->ring[avail & mask] = next;
+      avail++;
+      next = (next + 1) & mask;
+      n_slots--;
+      used++;
+    }
+  CLIB_MEMORY_STORE_BARRIER ();
+  vring->avail->idx = avail;
+  vring->desc_next = next;
+  vring->desc_in_use = used;
+
+  if ((vring->used->flags & VIRTIO_RING_FLAG_MASK_INT) == 0)
+    {
+      u64 b = 1;
+      CLIB_UNUSED (int r) = write (vring->kick_fd, &b, sizeof (b));
+    }
+}
+
+static_always_inline uword
+virtio_device_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
+			    vlib_frame_t * frame, virtio_if_t * vif, u16 qid)
+{
+  vnet_main_t *vnm = vnet_get_main ();
+  u32 thread_index = vlib_get_thread_index ();
+  uword n_trace = vlib_get_trace_count (vm, node);
+  virtio_vring_t *vring = vec_elt_at_index (vif->vrings, 0);
+  u32 next_index = VNET_DEVICE_INPUT_NEXT_ETHERNET_INPUT;
+  const int hdr_sz = sizeof (struct virtio_net_hdr_v1);
+  u32 *to_next = 0;
+  u32 n_rx_packets = 0;
+  u32 n_rx_bytes = 0;
+  u16 mask = vring->size - 1;
+  u16 last = vring->last_used_idx;
+  u16 n_left = vring->used->idx - last;
+
+  if (n_left == 0)
+    goto refill;
+
+  while (n_left)
+    {
+      u32 n_left_to_next;
+      u32 next0 = next_index;
+      vlib_get_next_frame (vm, node, next_index, to_next, n_left_to_next);
+
+      while (n_left && n_left_to_next)
+	{
+	  u16 num_buffers;
+	  struct vring_used_elem *e = &vring->used->ring[last & mask];
+	  struct virtio_net_hdr_v1 *hdr;
+	  u16 slot = e->id;
+	  u16 len = e->len - hdr_sz;
+	  u32 bi0 = vring->buffers[slot];
+	  vlib_buffer_t *b0 = vlib_get_buffer (vm, bi0);
+	  hdr = vlib_buffer_get_current (b0) - hdr_sz;
+	  num_buffers = hdr->num_buffers;
+
+	  b0->current_data = 0;
+	  b0->current_length = len;
+	  b0->total_length_not_including_first_buffer = 0;
+	  b0->flags = VLIB_BUFFER_TOTAL_LENGTH_VALID;
+	  vnet_buffer (b0)->sw_if_index[VLIB_RX] = vif->sw_if_index;
+	  vnet_buffer (b0)->sw_if_index[VLIB_TX] = (u32) ~ 0;
+
+	  /* if multisegment packet */
+	  if (PREDICT_FALSE (num_buffers > 1))
+	    {
+	      vlib_buffer_t *pb, *cb;
+	      pb = b0;
+	      while (num_buffers > 1)
+		{
+		  last++;
+		  e = &vring->used->ring[last & mask];
+		  u32 cbi = vring->buffers[e->id];
+		  cb = vlib_get_buffer (vm, cbi);
+
+		  /* current buffer */
+		  cb->current_data = -hdr_sz;
+		  cb->current_length = e->len;
+
+		  /* previous buffer */
+		  pb->next_buffer = cbi;
+		  pb->flags |= VLIB_BUFFER_NEXT_PRESENT;
+
+		  /* first buffer */
+		  b0->total_length_not_including_first_buffer += e->len;
+
+		  pb = cb;
+		  vring->desc_in_use--;
+		  num_buffers--;
+		  n_left--;
+		}
+	    }
+
+	  if (PREDICT_FALSE (vif->per_interface_next_index != ~0))
+	    next0 = vif->per_interface_next_index;
+	  else
+	    /* redirect if feature path enabled */
+	    vnet_feature_start_device_input_x1 (vif->sw_if_index, &next0, b0);
+	  /* trace */
+	  VLIB_BUFFER_TRACE_TRAJECTORY_INIT (b0);
+
+	  if (PREDICT_FALSE (n_trace > 0))
+	    {
+	      virtio_input_trace_t *tr;
+	      vlib_trace_buffer (vm, node, next0, b0,
+				 /* follow_chain */ 0);
+	      vlib_set_trace_count (vm, node, --n_trace);
+	      tr = vlib_add_trace (vm, node, b0, sizeof (*tr));
+	      tr->next_index = next0;
+	      tr->hw_if_index = vif->hw_if_index;
+	      tr->len = len;
+	      clib_memcpy (&tr->hdr, hdr, hdr_sz);
+	    }
+
+	  /* enqueue buffer */
+	  to_next[0] = bi0;
+	  vring->desc_in_use--;
+	  to_next += 1;
+	  n_left_to_next--;
+	  n_left--;
+	  last++;
+
+	  /* enqueue */
+	  vlib_validate_buffer_enqueue_x1 (vm, node, next_index, to_next,
+					   n_left_to_next, bi0, next0);
+
+	  /* next packet */
+	  n_rx_packets++;
+	  n_rx_bytes += len;
+	}
+      vlib_put_next_frame (vm, node, next_index, n_left_to_next);
+    }
+  vring->last_used_idx = last;
+
+  vlib_increment_combined_counter (vnm->interface_main.combined_sw_if_counters
+				   + VNET_INTERFACE_COUNTER_RX, thread_index,
+				   vif->hw_if_index, n_rx_packets,
+				   n_rx_bytes);
+
+refill:
+  virtio_refill_vring (vm, vring);
+
+  return n_rx_packets;
+}
+
+static uword
+virtio_input_fn (vlib_main_t * vm, vlib_node_runtime_t * node,
+		 vlib_frame_t * frame)
+{
+  u32 n_rx = 0;
+  virtio_main_t *nm = &virtio_main;
+  vnet_device_input_runtime_t *rt = (void *) node->runtime_data;
+  vnet_device_and_queue_t *dq;
+
+  foreach_device_and_queue (dq, rt->devices_and_queues)
+  {
+    virtio_if_t *mif;
+    mif = vec_elt_at_index (nm->interfaces, dq->dev_instance);
+    if (mif->flags & VIRTIO_IF_FLAG_ADMIN_UP)
+      {
+	n_rx += virtio_device_input_inline (vm, node, frame, mif,
+					    dq->queue_id);
+      }
+  }
+
+  return n_rx;
+}
+
+/* *INDENT-OFF* */
+VLIB_REGISTER_NODE (virtio_input_node) = {
+  .function = virtio_input_fn,
+  .name = "virtio-input",
+  .sibling_of = "device-input",
+  .format_trace = format_virtio_input_trace,
+  .type = VLIB_NODE_TYPE_INPUT,
+  .state = VLIB_NODE_STATE_INTERRUPT,
+  .n_errors = TAP_INPUT_N_ERROR,
+  .error_strings = virtio_input_error_strings,
+};
+
+VLIB_NODE_FUNCTION_MULTIARCH (virtio_input_node, virtio_input_fn)
+/* *INDENT-ON* */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

src/vat/api_format.c
@@ -1694,6 +1694,74 @@ static void vl_api_tap_delete_reply_t_handler_json
   vam->result_ready = 1;
 }
 
+static void
+vl_api_tap_create_v2_reply_t_handler (vl_api_tap_create_v2_reply_t * mp)
+{
+  vat_main_t *vam = &vat_main;
+  i32 retval = ntohl (mp->retval);
+  if (vam->async_mode)
+    {
+      vam->async_errors += (retval < 0);
+    }
+  else
+    {
+      vam->retval = retval;
+      vam->sw_if_index = ntohl (mp->sw_if_index);
+      vam->result_ready = 1;
+    }
+
+}
+
+static void vl_api_tap_create_v2_reply_t_handler_json
+  (vl_api_tap_create_v2_reply_t * mp)
+{
+  vat_main_t *vam = &vat_main;
+  vat_json_node_t node;
+
+  vat_json_init_object (&node);
+  vat_json_object_add_int (&node, "retval", ntohl (mp->retval));
+  vat_json_object_add_uint (&node, "sw_if_index", ntohl (mp->sw_if_index));
+
+  vat_json_print (vam->ofp, &node);
+  vat_json_free (&node);
+
+  vam->retval = ntohl (mp->retval);
+  vam->result_ready = 1;
+
+}
+
+static void
+vl_api_tap_delete_v2_reply_t_handler (vl_api_tap_delete_v2_reply_t * mp)
+{
+  vat_main_t *vam = &vat_main;
+  i32 retval = ntohl (mp->retval);
+  if (vam->async_mode)
+    {
+      vam->async_errors += (retval < 0);
+    }
+  else
+    {
+      vam->retval = retval;
+      vam->result_ready = 1;
+    }
+}
+
+static void vl_api_tap_delete_v2_reply_t_handler_json
+  (vl_api_tap_delete_v2_reply_t * mp)
+{
+  vat_main_t *vam = &vat_main;
+  vat_json_node_t node;
+
+  vat_json_init_object (&node);
+  vat_json_object_add_int (&node, "retval", ntohl (mp->retval));
+
+  vat_json_print (vam->ofp, &node);
+  vat_json_free (&node);
+
+  vam->retval = ntohl (mp->retval);
+  vam->result_ready = 1;
+}
+
 static void vl_api_mpls_tunnel_add_del_reply_t_handler
   (vl_api_mpls_tunnel_add_del_reply_t * mp)
 {
@@ -5378,6 +5446,9 @@ _(TAP_CONNECT_REPLY, tap_connect_reply)					\
 _(TAP_MODIFY_REPLY, tap_modify_reply)					\
 _(TAP_DELETE_REPLY, tap_delete_reply)					\
 _(SW_INTERFACE_TAP_DETAILS, sw_interface_tap_details)                   \
+_(TAP_CREATE_V2_REPLY, tap_create_v2_reply)				\
+_(TAP_DELETE_V2_REPLY, tap_delete_v2_reply)				\
+_(SW_INTERFACE_TAP_V2_DETAILS, sw_interface_tap_v2_details)             \
 _(IP_ADD_DEL_ROUTE_REPLY, ip_add_del_route_reply)			\
 _(IP_TABLE_ADD_DEL_REPLY, ip_table_add_del_reply)			\
 _(IP_MROUTE_ADD_DEL_REPLY, ip_mroute_add_del_reply)			\
@@ -7721,6 +7792,142 @@ api_tap_delete (vat_main_t * vam)
 
   /* Construct the API message */
   M (TAP_DELETE, mp);
+
+  mp->sw_if_index = ntohl (sw_if_index);
+
+  /* send it... */
+  S (mp);
+
+  /* Wait for a reply... */
+  W (ret);
+  return ret;
+}
+
+static int
+api_tap_create_v2 (vat_main_t * vam)
+{
+  unformat_input_t *i = vam->input;
+  vl_api_tap_create_v2_t *mp;
+  u8 mac_address[6];
+  u8 random_mac = 1;
+  u8 name_set = 0;
+  u8 *tap_name;
+  u8 *net_ns;
+  u8 net_ns_set = 0;
+  int ret;
+  int rx_ring_sz = 0, tx_ring_sz = 0;
+
+  memset (mac_address, 0, sizeof (mac_address));
+
+  /* Parse args required to build the message */
+  while (unformat_check_input (i) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (i, "hw-addr %U", unformat_ethernet_address, mac_address))
+	{
+	  random_mac = 0;
+	}
+      else if (unformat (i, "name %s", &tap_name))
+	name_set = 1;
+      else if (unformat (i, "host-ns %s", &net_ns))
+	net_ns_set = 1;
+      else if (unformat (i, "rx-ring-size %d", &rx_ring_sz))
+	;
+      else if (unformat (i, "tx-ring-size %d", &tx_ring_sz))
+	;
+      else
+	break;
+    }
+
+  if (name_set == 0)
+    {
+      errmsg ("missing tap name. ");
+      return -99;
+    }
+  if (vec_len (tap_name) > 63)
+    {
+      errmsg ("tap name too long. ");
+      return -99;
+    }
+  if (vec_len (net_ns) > 63)
+    {
+      errmsg ("host name space too long. ");
+      return -99;
+    }
+  if (!is_pow2 (rx_ring_sz))
+    {
+      errmsg ("rx ring size must be power of 2. ");
+      return -99;
+    }
+  if (rx_ring_sz > 32768)
+    {
+      errmsg ("rx ring size must be 32768 or lower. ");
+      return -99;
+    }
+  if (!is_pow2 (tx_ring_sz))
+    {
+      errmsg ("tx ring size must be power of 2. ");
+      return -99;
+    }
+  if (tx_ring_sz > 32768)
+    {
+      errmsg ("tx ring size must be 32768 or lower. ");
+      return -99;
+    }
+
+  vec_add1 (tap_name, 0);
+  vec_add1 (net_ns, 0);
+
+  /* Construct the API message */
+  M (TAP_CREATE_V2, mp);
+
+  mp->use_random_mac = random_mac;
+  clib_memcpy (mp->mac_address, mac_address, 6);
+  clib_memcpy (mp->tap_name, tap_name, vec_len (tap_name));
+  mp->net_ns_set = net_ns_set;
+  mp->rx_ring_sz = rx_ring_sz;
+  mp->tx_ring_sz = tx_ring_sz;
+  if (net_ns)
+    clib_memcpy (mp->net_ns, net_ns, vec_len (net_ns));
+
+  vec_free (tap_name);
+  vec_free (net_ns);
+
+  /* send it... */
+  S (mp);
+
+  /* Wait for a reply... */
+  W (ret);
+  return ret;
+}
+
+static int
+api_tap_delete_v2 (vat_main_t * vam)
+{
+  unformat_input_t *i = vam->input;
+  vl_api_tap_delete_v2_t *mp;
+  u32 sw_if_index = ~0;
+  u8 sw_if_index_set = 0;
+  int ret;
+
+  /* Parse args required to build the message */
+  while (unformat_check_input (i) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (i, "%U", api_unformat_sw_if_index, vam, &sw_if_index))
+	sw_if_index_set = 1;
+      else if (unformat (i, "sw_if_index %d", &sw_if_index))
+	sw_if_index_set = 1;
+      else
+	break;
+    }
+
+  if (sw_if_index_set == 0)
+    {
+      errmsg ("missing vpp interface name. ");
+      return -99;
+    }
+
+  /* Construct the API message */
+  M (TAP_DELETE_V2, mp);
 
   mp->sw_if_index = ntohl (sw_if_index);
 
@@ -12189,6 +12396,53 @@ api_sw_interface_tap_dump (vat_main_t * vam)
   print (vam->ofp, "\n%-16s %s", "dev_name", "sw_if_index");
   /* Get list of tap interfaces */
   M (SW_INTERFACE_TAP_DUMP, mp);
+  S (mp);
+
+  /* Use a control ping for synchronization */
+  MPING (CONTROL_PING, mp_ping);
+  S (mp_ping);
+
+  W (ret);
+  return ret;
+}
+
+static void vl_api_sw_interface_tap_v2_details_t_handler
+  (vl_api_sw_interface_tap_v2_details_t * mp)
+{
+  vat_main_t *vam = &vat_main;
+
+  print (vam->ofp, "%-16s %d",
+	 mp->dev_name, clib_net_to_host_u32 (mp->sw_if_index));
+}
+
+static void vl_api_sw_interface_tap_v2_details_t_handler_json
+  (vl_api_sw_interface_tap_v2_details_t * mp)
+{
+  vat_main_t *vam = &vat_main;
+  vat_json_node_t *node = NULL;
+
+  if (VAT_JSON_ARRAY != vam->json_tree.type)
+    {
+      ASSERT (VAT_JSON_NONE == vam->json_tree.type);
+      vat_json_init_array (&vam->json_tree);
+    }
+  node = vat_json_array_add (&vam->json_tree);
+
+  vat_json_init_object (node);
+  vat_json_object_add_uint (node, "sw_if_index", ntohl (mp->sw_if_index));
+  vat_json_object_add_string_copy (node, "dev_name", mp->dev_name);
+}
+
+static int
+api_sw_interface_tap_v2_dump (vat_main_t * vam)
+{
+  vl_api_sw_interface_tap_v2_dump_t *mp;
+  vl_api_control_ping_t *mp_ping;
+  int ret;
+
+  print (vam->ofp, "\n%-16s %s", "dev_name", "sw_if_index");
+  /* Get list of tap interfaces */
+  M (SW_INTERFACE_TAP_V2_DUMP, mp);
   S (mp);
 
   /* Use a control ping for synchronization */
@@ -22375,6 +22629,11 @@ _(tap_modify,                                                           \
 _(tap_delete,                                                           \
   "<vpp-if-name> | sw_if_index <id>")                                   \
 _(sw_interface_tap_dump, "")                                            \
+_(tap_create_v2,                                                        \
+  "name <name> [hw-addr <mac-addr>] [host-ns <name>] [rx-ring-size <num> [tx-ring-size <num>]") \
+_(tap_delete_v2,                                                        \
+  "<vpp-if-name> | sw_if_index <id>")                                   \
+_(sw_interface_tap_v2_dump, "")                                         \
 _(ip_table_add_del,                                                     \
   "table-id <n> [ipv6]\n")                                              \
 _(ip_add_del_route,                                                     \

src/vnet.am
@@ -868,13 +868,23 @@ API_FILES += vnet/pg/pg.api
 ########################################
 
 libvnet_la_SOURCES +=       \
+  vnet/devices/virtio/cli.c		\
+  vnet/devices/virtio/device.c		\
+  vnet/devices/virtio/node.c		\
+  vnet/devices/virtio/tap.c		\
+  vnet/devices/virtio/tapv2_api.c  	\
   vnet/devices/virtio/vhost-user.c	\
-  vnet/devices/virtio/vhost_user_api.c
+  vnet/devices/virtio/vhost_user_api.c	\
+  vnet/devices/virtio/virtio.c
+
 
 nobase_include_HEADERS +=     \
+  vnet/devices/virtio/virtio.h		\
+  vnet/devices/virtio/tapv2.api.h \
   vnet/devices/virtio/vhost-user.h	\
   vnet/devices/virtio/vhost_user.api.h
 
+API_FILES += vnet/devices/virtio/tapv2.api
 API_FILES += vnet/devices/virtio/vhost_user.api
 
 ########################################

src/vnet/api_errno.h
@@ -132,7 +132,9 @@ _(NAME_SERVER_NO_ADDRESSES, -139, "No addresses available")		\
 _(NAME_SERVER_NEXT_SERVER, -140, "Retry with new server")		\
 _(APP_CONNECT_FILTERED, -141, "Connect was filtered")			\
 _(ACL_IN_USE_INBOUND, -142, "Inbound ACL in use")			\
-_(ACL_IN_USE_OUTBOUND, -143, "Outbound ACL in use")
+_(ACL_IN_USE_OUTBOUND, -143, "Outbound ACL in use")                     \
+_(NAMESPACE_CREATE, -144, "Failed to create netlink namespace")         \
+_(VIRTIO_INIT, -145, "Failed to init virtio ring")
 
 typedef enum
 {

src/vnet/devices/virtio/cli.c
@@ -1,0 +1,300 @@
+/*
+ *------------------------------------------------------------------
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *------------------------------------------------------------------
+ */
+#include <stdint.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <inttypes.h>
+
+#include <vlib/vlib.h>
+#include <vlib/unix/unix.h>
+#include <vnet/ethernet/ethernet.h>
+#include <linux/virtio_net.h>
+#include <linux/vhost.h>
+#include <vnet/devices/virtio/virtio.h>
+#include <vnet/devices/virtio/tap.h>
+
+static clib_error_t *
+tap_create_command_fn (vlib_main_t * vm, unformat_input_t * input,
+		       vlib_cli_command_t * cmd)
+{
+  unformat_input_t _line_input, *line_input = &_line_input;
+  int rv;
+  tap_create_if_args_t args = { 0 };
+
+  /* Get a line of input. */
+  if (!unformat_user (input, unformat_line_input, line_input))
+    return clib_error_return (0, "Missing name <interface>");
+
+  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (line_input, "name %s", &args.name))
+	;
+      else if (unformat (line_input, "host-ns %s", &args.net_ns))
+	;
+      else if (unformat (line_input, "rx-ring-size %d", &args.rx_ring_sz))
+	;
+      else if (unformat (line_input, "tx-ring-size %d", &args.tx_ring_sz))
+	;
+      else if (unformat (line_input, "hw-addr %U",
+			 unformat_ethernet_address, args.hw_addr))
+	args.hw_addr_set = 1;
+      else
+	return clib_error_return (0, "unknown input `%U'",
+				  format_unformat_error, input);
+    }
+  unformat_free (line_input);
+
+  rv = tap_create_if (vm, &args);
+
+  vec_free (args.name);
+
+  if (rv == VNET_API_ERROR_SYSCALL_ERROR_1)
+    return clib_error_return_unix (0, "open '/dev/vhost-net'");
+  else if (rv == VNET_API_ERROR_SYSCALL_ERROR_2)
+    return clib_error_return_unix (0, "open '/dev/net/tun'");
+  else if (rv == VNET_API_ERROR_UNSUPPORTED)
+    return clib_error_return (0, "vhost-net backend doesn't support needed"
+			      " features");
+  else if (rv == VNET_API_ERROR_NAMESPACE_CREATE)
+    return clib_error_return (0, "failed to create netlink namespace");
+  else if (rv == VNET_API_ERROR_VIRTIO_INIT)
+    return clib_error_return (0, "failed to init virtio ring");
+  else if (rv == VNET_API_ERROR_INVALID_REGISTRATION)
+    return clib_error_return (0, "failed to register interface");
+  else if (rv != 0)
+    return clib_error_return (0, "error on creating tap interface");
+
+  return 0;
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (tap_create_command, static) = {
+  .path = "create tap",
+  .short_help = "create tap {name <if-name>} [hw-addr <mac-address>]"
+    "[rx-ring-size <size>] [tx-ring-size <size>] [host-ns <netns>]",
+  .function = tap_create_command_fn,
+};
+/* *INDENT-ON* */
+
+static clib_error_t *
+tap_delete_command_fn (vlib_main_t * vm, unformat_input_t * input,
+		       vlib_cli_command_t * cmd)
+{
+  unformat_input_t _line_input, *line_input = &_line_input;
+  u32 sw_if_index = ~0;
+  vnet_main_t *vnm = vnet_get_main ();
+  int rv;
+
+  /* Get a line of input. */
+  if (!unformat_user (input, unformat_line_input, line_input))
+    return clib_error_return (0, "Missing <interface>");
+
+  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (line_input, "sw_if_index %d", &sw_if_index))
+	;
+      else if (unformat (line_input, "%U", unformat_vnet_sw_interface,
+			 vnm, &sw_if_index))
+	;
+      else
+	return clib_error_return (0, "unknown input `%U'",
+				  format_unformat_error, input);
+    }
+  unformat_free (line_input);
+
+  if (sw_if_index == ~0)
+    return clib_error_return (0,
+			      "please specify interface name or sw_if_index");
+
+  rv = tap_delete_if (vm, sw_if_index);
+  if (rv == VNET_API_ERROR_INVALID_SW_IF_INDEX)
+    return clib_error_return (0, "not a tap interface");
+  else if (rv != 0)
+    return clib_error_return (0, "error on deleting tap interface");
+
+  return 0;
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (tap_delete__command, static) =
+{
+  .path = "delete tap",
+  .short_help = "delete tap {<interface> | sw_if_index <sw_idx>}",
+  .function = tap_delete_command_fn,
+};
+/* *INDENT-ON* */
+
+static clib_error_t *
+tap_show_command_fn (vlib_main_t * vm, unformat_input_t * input,
+		     vlib_cli_command_t * cmd)
+{
+  virtio_main_t *mm = &virtio_main;
+  virtio_if_t *vif;
+  vnet_main_t *vnm = vnet_get_main ();
+  int show_descr = 0;
+  clib_error_t *error = 0;
+  u32 hw_if_index, *hw_if_indices = 0;
+  virtio_vring_t *vring;
+  int i, j;
+  struct feat_struct
+  {
+    u8 bit;
+    char *str;
+  };
+  struct feat_struct *feat_entry;
+
+  static struct feat_struct feat_array[] = {
+#define _(s,b) { .str = #s, .bit = b, },
+    foreach_virtio_net_features
+#undef _
+    {.str = NULL}
+  };
+
+  struct feat_struct *flag_entry;
+  static struct feat_struct flags_array[] = {
+#define _(b,e,s) { .bit = b, .str = s, },
+    foreach_virtio_if_flag
+#undef _
+    {.str = NULL}
+  };
+
+  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat
+	  (input, "%U", unformat_vnet_hw_interface, vnm, &hw_if_index))
+	vec_add1 (hw_if_indices, hw_if_index);
+      else if (unformat (input, "descriptors"))
+	show_descr = 1;
+      else
+	{
+	  error = clib_error_return (0, "unknown input `%U'",
+				     format_unformat_error, input);
+	  goto done;
+	}
+    }
+
+  if (vec_len (hw_if_indices) == 0)
+    {
+      /* *INDENT-OFF* */
+      pool_foreach (vif, mm->interfaces,
+	  vec_add1 (hw_if_indices, vif->hw_if_index);
+      );
+      /* *INDENT-ON* */
+    }
+
+  for (hw_if_index = 0; hw_if_index < vec_len (hw_if_indices); hw_if_index++)
+    {
+      vnet_hw_interface_t *hi =
+	vnet_get_hw_interface (vnm, hw_if_indices[hw_if_index]);
+      vif = pool_elt_at_index (mm->interfaces, hi->dev_instance);
+      vlib_cli_output (vm, "interface %U", format_vnet_sw_if_index_name,
+		       vnm, vif->sw_if_index);
+      if (vif->name)
+	vlib_cli_output (vm, "  name \"%s\"", vif->name);
+      if (vif->net_ns)
+	vlib_cli_output (vm, "  host-ns \"%s\"", vif->net_ns);
+      vlib_cli_output (vm, "  flags 0x%x", vif->flags);
+      flag_entry = (struct feat_struct *) &flags_array;
+      while (flag_entry->str)
+	{
+	  if (vif->flags & (1ULL << flag_entry->bit))
+	    vlib_cli_output (vm, "    %s (%d)", flag_entry->str,
+			     flag_entry->bit);
+	  flag_entry++;
+	}
+      vlib_cli_output (vm, "  fd %d", vif->fd);
+      vlib_cli_output (vm, "  tap-fd %d", vif->tap_fd);
+      vlib_cli_output (vm, "  features 0x%lx", vif->features);
+      feat_entry = (struct feat_struct *) &feat_array;
+      while (feat_entry->str)
+	{
+	  if (vif->features & (1ULL << feat_entry->bit))
+	    vlib_cli_output (vm, "    %s (%d)", feat_entry->str,
+			     feat_entry->bit);
+	  feat_entry++;
+	}
+      vlib_cli_output (vm, "  remote-features 0x%lx", vif->remote_features);
+      feat_entry = (struct feat_struct *) &feat_array;
+      while (feat_entry->str)
+	{
+	  if (vif->remote_features & (1ULL << feat_entry->bit))
+	    vlib_cli_output (vm, "    %s (%d)", feat_entry->str,
+			     feat_entry->bit);
+	  feat_entry++;
+	}
+      vec_foreach_index (i, vif->vrings)
+      {
+	// RX = 0, TX = 1
+	vring = vec_elt_at_index (vif->vrings, i);
+	vlib_cli_output (vm, "  Virtqueue (%s)", (i & 1) ? "TX" : "RX");
+	vlib_cli_output (vm, "    qsz %d, last_used_idx %d, desc_in_use %d",
+			 vring->size, vring->last_used_idx,
+			 vring->desc_in_use);
+	vlib_cli_output (vm,
+			 "    avail.flags 0x%x avail.idx %d used.flags 0x%x used.idx %d",
+			 vring->avail->flags, vring->avail->idx,
+			 vring->used->flags, vring->used->idx);
+	vlib_cli_output (vm, "    kickfd %d, callfd %d", vring->kick_fd,
+			 vring->call_fd);
+	if (show_descr)
+	  {
+	    vlib_cli_output (vm, "\n  descriptor table:\n");
+	    vlib_cli_output (vm,
+			     "   id          addr         len  flags  next      user_addr\n");
+	    vlib_cli_output (vm,
+			     "  ===== ================== ===== ====== ===== ==================\n");
+	    vring = vif->vrings;
+	    for (j = 0; j < vring->size; j++)
+	      {
+		struct vring_desc *desc = &vring->desc[j];
+		vlib_cli_output (vm,
+				 "  %-5d 0x%016lx %-5d 0x%04x %-5d 0x%016lx\n",
+				 j, desc->addr,
+				 desc->len,
+				 desc->flags, desc->next, desc->addr);
+	      }
+	  }
+      }
+    }
+done:
+  vec_free (hw_if_indices);
+  return error;
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (tap_show_command, static) = {
+  .path = "show tap",
+  .short_help = "show tap {<interface>] [descriptors]",
+  .function = tap_show_command_fn,
+};
+/* *INDENT-ON* */
+
+clib_error_t *
+tap_cli_init (vlib_main_t * vm)
+{
+  return 0;
+}
+
+VLIB_INIT_FUNCTION (tap_cli_init);
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

commit 4b29b1095f56e4451136eb07fe3090fc8c877c6d

virtio: feature arc have higher priority than redirect
Type: fix
Fixes: 8389fb9

Change-Id: Ie159eb444b28b36a7af86049b80fba4e49be93cb
Signed-off-by: Damjan Marion <damarion@cisco.com>

MAINTAINERS
@@ -103,6 +103,7 @@ VNET Native Virtio Drivers
 I:	virtio
 Y:	src/vnet/devices/virtio/FEATURE.yaml
 M:	Mohsin Kazmi <sykazmi@cisco.com>
+M:	Damjan Marion <damarion@cisco.com>
 F:	src/vnet/devices/virtio/
 
 VNET Ethernet

src/vnet/devices/virtio/node.c
@@ -316,9 +316,10 @@ virtio_device_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 
 	  if (PREDICT_FALSE (vif->per_interface_next_index != ~0))
 	    next0 = vif->per_interface_next_index;
-	  else
-	    /* redirect if feature path enabled */
-	    vnet_feature_start_device_input_x1 (vif->sw_if_index, &next0, b0);
+
+	  /* redirect if feature path enabled */
+	  vnet_feature_start_device_input_x1 (vif->sw_if_index, &next0, b0);
+
 	  /* trace */
 	  VLIB_BUFFER_TRACE_TRAJECTORY_INIT (b0);
 