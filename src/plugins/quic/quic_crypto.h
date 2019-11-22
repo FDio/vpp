83ea36f05bfddb1dda454384a4bab719b7da22fd
MathiasRaoul <mathias.raoul@gmail.com>
2019-11-22 10:23:22 +0000

    quic: quicly encryption oflloading integration
    - Change quic packet allocator to allocate more memory at the end of the
    packet to store crypto offloading related data
    - encryption batching using vnet crypto
    -  Create patches from quicly PR #214 and #225
    
    Type: feature
    
    Change-Id: I7557fd457d7ba492329d5d8ed192509cbd727f9c
    Signed-off-by: MathiasRaoul <mathias.raoul@gmail.com>

--- src/plugins/quic/quic_crypto.h
+++ src/plugins/quic/quic_crypto.h
@@ -20,6 +20,40 @@
 
 extern ptls_cipher_suite_t *quic_crypto_cipher_suites[];
 
+void quic_crypto_batch_tx_packets ();
+void quic_crypto_batch_rx_packets ();
+
+void quic_crypto_finalize_send_packet (quicly_datagram_t * packet);
+void quic_crypto_finalize_send_packet_cb (quicly_finalize_send_packet_t *
+					  _self, quicly_conn_t * conn,
+					  ptls_cipher_context_t * hp,
+					  ptls_aead_context_t * aead,
+					  quicly_datagram_t * packet,
+					  size_t first_byte_at,
+					  size_t payload_from, int coalesced);
+int quic_crypto_decrypt_packet (quicly_conn_t * conn,
+				quicly_decoded_packet_t * packet,
+				struct sockaddr *dest_addr,
+				struct sockaddr *src_addr);
+
+typedef struct quic_finalize_send_packet_cb_ctx_
+{
+  size_t payload_from;
+  size_t first_byte_at;
+  ptls_cipher_context_t *hp;
+} quic_finalize_send_packet_cb_ctx;
+
+typedef struct quic_encrypt_cb_ctx_
+{
+  ptls_cipher_context_t *hp;
+  size_t payload_from;
+  size_t first_byte_at;
+  quicly_datagram_t *packet;
+  quic_finalize_send_packet_cb_ctx snd_ctx[5];
+  size_t snd_ctx_count;
+} quic_encrypt_cb_ctx;
+
+
 #endif /* __included_vpp_quic_crypto_h__ */
 
 /*
