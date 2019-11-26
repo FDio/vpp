5ab60abbe56699d5be918163ee44f419829ca9f5
Jon Loeliger <jdl@netgate.com>
2019-11-26 08:11:11 -0600

    build: Fix Subject-line feature list extraction
    Bang on sed until a better incantation appears.
    
    Change-Id: Ib8ad0996b6325db0fe983c86dd7dc553c9d388c4
    Type: fix
    Fixes: 26ce6ca1fe6f524a9049444fe8d55042fd7530a6
    Signed-off-by: Jon Loeliger <jdl@netgate.com>

--- extras/scripts/check_commit_msg.sh
+++ extras/scripts/check_commit_msg.sh
@@ -1,7 +1,8 @@
 #/bin/env bash
 
 KNOWN_FEATURES=$(cat MAINTAINERS | sed -ne 's/^I:[[:space:]]*//p')
-FEATURES=$(git show -s --format=%s --no-color | sed -e 's/\([a-z0-9 -]*\):.*/\1/')
+FEATURES=$(git show -s --format=%s --no-color \
+    | sed -ne 's/^\([a-z0-9 -]*\):.*$/\1/p')
 KNOWN_TYPES="feature fix refactor style docs test make"
 TYPE=$(git show -s --format=%b --no-color | sed -ne 's/^Type:[[:space:]]*//p')
 ERR="=============================== ERROR ==============================="
