FDio/vpp

commit 26ce6ca1fe6f524a9049444fe8d55042fd7530a6

misc: add check_commit_msg.sh script
Type: feature
Change-Id: I5e04991afd7500673db121e4529275e9aff15e65
Signed-off-by: Damjan Marion <damarion@cisco.com>

extras/scripts/check_commit_msg.sh
@@ -1,0 +1,50 @@
+#/bin/env bash
+
+KNOWN_FEATURES=$(cat MAINTAINERS | sed -ne 's/^I:[[:space:]]*//p')
+FEATURES=$(git show -s --format=%s --no-color | sed -e 's/\(.*\):.*/\1/')
+KNOWN_TYPES="fix refactor style docs test make"
+TYPE=$(git show -s --format=%b --no-color | sed -ne 's/^Type:[[:space:]]*//p')
+ERR="=============================== ERROR ==============================="
+
+# Chech that subject line contains at least one feature id
+if [ $(echo ${FEATURES} | wc -w) -eq 0 ]; then
+  echo $ERR
+  echo "git commit 'Subject:' line must contain at least one known feature id."
+  echo "feature id(s) must be listed before ':' and space delimited "
+  echo "if more then one is listed."
+  echo "Please reffer to MAINTAINERS file (I: lines) for known feature ids."
+  echo $ERR
+  exit 1
+fi
+
+# Check that feature ids in subject line are known
+for i in ${FEATURES}; do
+  is_known=false
+  for j in ${KNOWN_FEATURES}; do
+    [ "${i}" = "${j}" ] && is_known=true
+  done
+  if [ ${is_known} = "false" ] ; then
+    echo $ERR
+    echo "Unknown feature '${i}' in commit 'Subject:' line."
+    echo "Feature must exist in MAINTAINERS file. If this commit intruduces "
+    echo "new feature, then this commit must add new entry into the "
+    echo "MAINTAINERS file."
+    echo $ERR
+    exit 1
+  fi
+done
+
+# Check that Message body contains valid Type: entry
+is_known=false
+for i in ${KNOWN_TYPES}; do
+  [ "${i}" = "${TYPE}" ] && is_known=true
+done
+if [ ${is_known} = "false" ] ; then
+  echo $ERR
+  echo "Unknown commit type '${TYPE}' in commit message body."
+  echo "Commit message must contain known 'Type:' entry."
+  echo "Known types are: ${KNOWN_TYPES}"
+  echo $ERR
+  exit 1
+fi
+

commit 4409c561c38e5cf1ae8bde2f7a4de01db37a563b

build: Fix Subject-line feature list extraction
This line of code:

    sed -e 's/\([a-z0-9 -]*\):.*/\1/'

does not do anything like what it initially appears to do, nor what
is neeeded for it to do.  Specifically, in the case when the words
of the commit message do NOT match the pattern-piece that is left
of the colon, then nothing matches and the pattern is ignored and
subsequently the entire input line is simply echoed.

For example:
    $ echo 'Build some stuff: Ease commit message checking' \
	| sed -e 's/^\([a-z0 -]*\):.*/\1/'
    Build some stuff: Ease commit message checking

To make that clearer, perhaps:
    $ echo 'Build some stuff: Ease commit message checking' \
	| sed -e 's/^\([a-z0-9 -]*\):\(.*\)/\1 and \2/'
    Build some stuff: Ease commit message checking

But notice when the LHS of the colon pattern matches:
    $ echo 'build some stuff: Ease commit message checking' \
	| sed -e 's/^\([a-z0-9 -]*\):\(.*\)/\1 and \2/'
    build some stuff and  Ease commit message checking

So the key here will be to two-step it, with a split on the colon
first, then check for acceptable words sequences without strange
characters:
    $ echo 'build some !*(^*%^ stuff: Ease commit message checking' \
	| sed -e 's/^\([^:]*\):.*/\1/' \
	| sed -e 's/\([a-z0-9 -]*\).*/\1/'
    build some

Change-Id: Ib8ad0996b6325db0fe983c86dd7dc553c9d388c4
Type: fix
Fixes: 26ce6ca1fe6f524a9049444fe8d55042fd7530a6
Signed-off-by: Jon Loeliger <jdl@netgate.com>

extras/scripts/check_commit_msg.sh
@@ -1,7 +1,9 @@
 #/bin/env bash
 
 KNOWN_FEATURES=$(cat MAINTAINERS | sed -ne 's/^I:[[:space:]]*//p')
-FEATURES=$(git show -s --format=%s --no-color | sed -e 's/\([a-z0-9 -]*\):.*/\1/')
+FEATURES=$(git show -s --format=%s --no-color \
+        | sed -e 's/\([^:]*\):.*/\1/' \
+	| sed -e 's/\([a-z0-9 -]*\).*/\1/')
 KNOWN_TYPES="feature fix refactor style docs test make"
 TYPE=$(git show -s --format=%b --no-color | sed -ne 's/^Type:[[:space:]]*//p')
 ERR="=============================== ERROR ==============================="