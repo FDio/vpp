04338e85ae4979222029d18c314a9cc09d723572
Ole Troan <ot@cisco.com>
2019-12-06 14:23:10 +0100

    pppoe: use explicit types in api
    Type: fix
    Signed-off-by: Ole Troan <ot@cisco.com>
    Change-Id: Ifd1e1907740e55420dc040eb2afbbbf9887aea3c

--- src/plugins/pppoe/test/vpp_pppoe_interface.py
+++ src/plugins/pppoe/test/vpp_pppoe_interface.py
@@ -19,21 +19,17 @@ class VppPppoeInterface(VppInterface):
         self.decap_vrf_id = decap_vrf_id
 
     def add_vpp_config(self):
-        cip = socket.inet_pton(socket.AF_INET, self.client_ip)
-        cmac = mac_pton(self.client_mac)
         r = self.test.vapi.pppoe_add_del_session(
-                cip, cmac,
+                self.client_ip, self.client_mac,
                 session_id=self.session_id,
                 decap_vrf_id=self.decap_vrf_id)
         self.set_sw_if_index(r.sw_if_index)
         self.generate_remote_hosts()
 
     def remove_vpp_config(self):
-        cip = socket.inet_pton(socket.AF_INET, self.client_ip)
-        cmac = mac_pton(self.client_mac)
         self.unconfig()
         self.test.vapi.pppoe_add_del_session(
-                cip, cmac,
+                self.client_ip, self.client_mac,
                 session_id=self.session_id,
                 decap_vrf_id=self.decap_vrf_id,
                 is_add=0)
