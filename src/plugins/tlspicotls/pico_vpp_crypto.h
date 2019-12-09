1423522257b3136232f739625a61f55e82b53fc8
Simon Zhang <yuwei1.zhang@intel.com>
2019-12-09 19:58:39 +0800

    tls: Picotls engine symmetric crypto enhancement by vpp crypto framework
    Type: feature
    
    Signed-off-by: Simon Zhang <yuwei1.zhang@intel.com>
    Change-Id: I1d4fe75e5faf3fa2086d11020828345b173ebd03

---
+++ src/plugins/tlspicotls/pico_vpp_crypto.h
@@ -0,0 +1,9 @@
+#ifndef __included_pico_vpp_crypto_h__
+#define __included_pico_vpp_crypto_h__
+
+#include <picotls.h>
+
+extern ptls_cipher_suite_t * ptls_vpp_crypto_cipher_suites[];
+
+#endif /* __included_pico_vpp_crypto_h__ */
+
