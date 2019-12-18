ba0061feba13a11fc1c1af551bc4c26eccbd2f2c
Mohsin Kazmi <sykazmi@cisco.com>
2019-12-18 17:08:54 +0100

    tap: split gso and checksum offload functionality
    Type: refactor
    
    Change-Id: I0d4b79ef384c11c841576d264bfd8ccb21783e10
    Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>

--- src/vnet/devices/tap/tap.h
+++ src/vnet/devices/tap/tap.h
@@ -32,6 +32,7 @@ typedef struct
   u16 tx_ring_sz;
   u32 tap_flags;
 #define TAP_FLAG_GSO (1 << 0)
+#define TAP_FLAG_CSUM_OFFLOAD (1 << 1)
   u8 *host_namespace;
   u8 *host_if_name;
   mac_address_t host_mac_addr;
@@ -88,6 +89,9 @@ void tap_create_if (vlib_main_t * vm, tap_create_if_args_t * args);
 int tap_delete_if (vlib_main_t * vm, u32 sw_if_index);
 int tap_gso_enable_disable (vlib_main_t * vm, u32 sw_if_index,
 			    int enable_disable);
+int
+tap_csum_offload_enable_disable (vlib_main_t * vm, u32 sw_if_index,
+				 int enable_disable);
 int tap_dump_ifs (tap_interface_details_t ** out_tapids);
 
 #endif /* _VNET_DEVICES_VIRTIO_TAP_H_ */
