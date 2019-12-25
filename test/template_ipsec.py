ac7ea3f605f5ff0280b911659fd5441bad8e7f07
Neale Ranns <nranns@cisco.com>
2019-12-25 23:22:39 +0000

    ipsec: Async mode in the unit tests
    Type: test
    
    Change-Id: If4b5896566c152d002f73c9cb45f321f6009b203
    Signed-off-by: Neale Ranns <nranns@cisco.com>

--- test/template_ipsec.py
+++ test/template_ipsec.py
@@ -177,8 +177,11 @@ class TemplateIpsec(VppTestCase):
         super(TemplateIpsec, cls).tearDownClass()
 
     def setup_params(self):
-        self.ipv4_params = IPsecIPv4Params()
-        self.ipv6_params = IPsecIPv6Params()
+        # allow a baseclass to provide these
+        if not hasattr(self, 'ipv4_params'):
+            self.ipv4_params = IPsecIPv4Params()
+        if not hasattr(self, 'ipv6_params'):
+            self.ipv6_params = IPsecIPv6Params()
         self.params = {self.ipv4_params.addr_type: self.ipv4_params,
                        self.ipv6_params.addr_type: self.ipv6_params}
 
