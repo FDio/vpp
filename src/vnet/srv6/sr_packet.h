94279e6b2cde26d37851f4abdc49c3cd34029a9b
Tetsuya Murakami <tetsuya.mrk@gmail.com>
2020-01-09 14:22:04 -0800

    srv6-mobile
    Type: feature
    
    Plug-in for IPv6 Segment Routing Mobile
    
    Add new functions like GTP4.DT and GTP6.DT
    
    Signed-off-by: Tetsuya Murakami <tetsuya.mrk@gmail.com>
    Change-Id: I573a0c27bd463dd56a4d11b940941b8a8c826e08

--- src/vnet/srv6/sr_packet.h
+++ src/vnet/srv6/sr_packet.h
@@ -147,7 +147,14 @@ typedef struct
 
   /* The segment elts */
   ip6_address_t segments[0];
-} __attribute__ ((packed)) ip6_sr_header_t;
+} __attribute__((packed)) ip6_sr_header_t;
+
+typedef struct
+{
+  u8 type;
+  u8 length;
+  u8 value[0];
+} __attribute__((packed)) ip6_sr_tlv_t;
 
 /*
 * fd.io coding-style-patch-verification: ON
