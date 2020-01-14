6295263ee9b769f1271aa7ef53f510d2531db627
Alexander Chernavin <achernavin@netgate.com>
2020-01-14 06:11:42 -0500

    nat: fix dhcp client on outside interface with output feature
    There was an attempt to fix this problem in the commit:
    d3b8c861a44e70c197ab721fa3ce7f38bbeab7fd
    
    But checking the LOCALLY_ORIGINATED flag didn't work because this flag
    gets reset before it can reach the NAT nodes.
    
    With this commit, replace the check for the LOCALLY_ORIGINATED flag
    with a check to see if the packet is a DHCP broadcast.
    
    Type: fix
    
    Change-Id: I069c08a785b5988b10192f528e4f9c4c7cc2f8a3
    Signed-off-by: Alexander Chernavin <achernavin@netgate.com>

--- src/plugins/nat/in2out.c
+++ src/plugins/nat/in2out.c
@@ -1043,11 +1043,11 @@ snat_in2out_node_fn_inline (vlib_main_t * vm,
 		       * be able to use dhcp client on the outside interface
 		       */
 		      if (PREDICT_FALSE
-			  ((b0->flags & VNET_BUFFER_F_LOCALLY_ORIGINATED)
-			   && proto0 == SNAT_PROTOCOL_UDP
+			  (proto0 == SNAT_PROTOCOL_UDP
 			   && (vnet_buffer (b0)->ip.reass.l4_dst_port ==
 			       clib_host_to_net_u16
-			       (UDP_DST_PORT_dhcp_to_server))))
+			       (UDP_DST_PORT_dhcp_to_server))
+			   && ip0->dst_address.as_u32 == 0xffffffff))
 			goto trace00;
 		    }
 		  else
@@ -1251,11 +1251,11 @@ snat_in2out_node_fn_inline (vlib_main_t * vm,
 		       * be able to use dhcp client on the outside interface
 		       */
 		      if (PREDICT_FALSE
-			  ((b1->flags & VNET_BUFFER_F_LOCALLY_ORIGINATED)
-			   && proto1 == SNAT_PROTOCOL_UDP
+			  (proto1 == SNAT_PROTOCOL_UDP
 			   && (vnet_buffer (b1)->ip.reass.l4_dst_port ==
 			       clib_host_to_net_u16
-			       (UDP_DST_PORT_dhcp_to_server))))
+			       (UDP_DST_PORT_dhcp_to_server))
+			   && ip1->dst_address.as_u32 == 0xffffffff))
 			goto trace01;
 		    }
 		  else
@@ -1492,11 +1492,11 @@ snat_in2out_node_fn_inline (vlib_main_t * vm,
 		       * be able to use dhcp client on the outside interface
 		       */
 		      if (PREDICT_FALSE
-			  ((b0->flags & VNET_BUFFER_F_LOCALLY_ORIGINATED)
-			   && proto0 == SNAT_PROTOCOL_UDP
+			  (proto0 == SNAT_PROTOCOL_UDP
 			   && (vnet_buffer (b0)->ip.reass.l4_dst_port ==
 			       clib_host_to_net_u16
-			       (UDP_DST_PORT_dhcp_to_server))))
+			       (UDP_DST_PORT_dhcp_to_server))
+			   && ip0->dst_address.as_u32 == 0xffffffff))
 			goto trace0;
 		    }
 		  else
