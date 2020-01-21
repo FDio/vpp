6ed81620909387c7a55dbe03b152a75fdc219a64
Benoît Ganne <bganne@cisco.com>
2020-01-21 18:33:14 +0100

    api: check id is valid for bounce checking
    If the id is invalid we cannot check whether we must free the message or
    not, free it anyway.
    
    Type: fix
    
    Change-Id: Ie4426f601390d1e5e14c739f670e8c1e6e3aaf1e
    Signed-off-by: Benoît Ganne <bganne@cisco.com>
    (cherry picked from commit ff13e46215ab96df988310b4a20eddefad92de99)

--- src/vlibapi/api_shared.c
+++ src/vlibapi/api_shared.c
@@ -595,7 +595,7 @@ vl_msg_api_handler_with_vm_node (api_main_t * am,
    * Special-case, so we can e.g. bounce messages off the vnet
    * main thread without copying them...
    */
-  if (!(am->message_bounce[id]))
+  if (id >= vec_len (am->message_bounce) || !(am->message_bounce[id]))
     vl_msg_api_free (the_msg);
 
   if (PREDICT_FALSE (am->elog_trace_api_messages))
