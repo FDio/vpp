87a3aff89e8a1dfde96bb7bd8ce6660093e20084
Florin Coras <fcoras@cisco.com>
2020-01-22 07:32:12 -0800

    vcl: always report EPOLLHUP/EPOLLRDHUP on close
    Type: fix
    
    Change-Id: I3d24a7973c7113ffeb9109e89cda7fa960e73a5b
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/vcl/vppcom.c
+++ src/vcl/vppcom.c
@@ -2719,8 +2719,6 @@ vcl_epoll_wait_handle_mq_event (vcl_worker_t * wrk, session_event_t * e,
       if (!session)
 	break;
       session_events = session->vep.ev.events;
-      if (!((EPOLLHUP | EPOLLRDHUP) & session_events))
-	break;
       add_event = 1;
       events[*num_ev].events |= EPOLLHUP | EPOLLRDHUP;
       session_evt_data = session->vep.ev.data.u64;
@@ -2730,8 +2728,6 @@ vcl_epoll_wait_handle_mq_event (vcl_worker_t * wrk, session_event_t * e,
       if (!(session = vcl_session_get (wrk, sid)))
 	break;
       session_events = session->vep.ev.events;
-      if (!((EPOLLHUP | EPOLLRDHUP) & session_events))
-	break;
       add_event = 1;
       events[*num_ev].events |= EPOLLHUP | EPOLLRDHUP;
       session_evt_data = session->vep.ev.data.u64;
