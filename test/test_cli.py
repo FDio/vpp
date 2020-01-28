727e28d0065fd049bc447dc5e4b47b08bdb6bdbe
Paul Vinciguerra <pvinci@vinciconsulting.com>
2020-01-27 21:04:43 -0500

    vlib: add logging of comments
    Add cli option to log comments
    "cli log-comments <on|off>"
    
    2020/01/27 20:59:44:846 notice     cli/comment    this_is_comment_foo...
    2020/01/27 20:59:44:857 notice     cli/comment    this is comment bar...
    
    Type: feature
    
    Change-Id: Ideae95a7203e975f265fc263be8676b903befe59
    Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

--- test/test_cli.py
+++ test/test_cli.py
@@ -49,6 +49,24 @@ class TestCLI(VppTestCase):
         rv = self.vapi.papi.cli_inband(cmd='wait 10', _timeout=15)
         self.assertEqual(rv.retval, 0)
 
+    def test_comment_logging(self):
+        """ test comment logging """
+        c1 = "this_is_comment_foo..."
+        c1a = " baz"
+        c2 = "this is comment bar..."
+        rv = self.vapi.papi.cli_inband(cmd='cli log-comments on')
+        rv = self.vapi.papi.cli_inband(cmd=f'comment {c1+c1a}')
+        rv = self.vapi.papi.cli_inband(
+            cmd=f'comment {{{c2}}}')
+        log = self.vapi.papi.cli_inband(cmd="show log").reply
+
+        # without braces, the first term should be logged.
+        self.assertIn(c1, log)
+        # but not the term following the space.
+        self.assertNotIn(c1a, log)
+        # inside {} the line with spaces should be logged.
+        self.assertIn(c2, log)
+
 
 class TestCLIExtendedVapiTimeout(VppTestCase):
     maxDiff = None
