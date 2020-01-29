9ef9f01a8663df04a8e248f76678ea0c945b44c6
Dave Barach <dave@barachs.net>
2020-01-29 18:05:24 -0500

    vppinfra: improve clocks_per_second convergence
    Apply exponential smoothing to the clock rate update calculation in
    clib_time_verify_frequency(), with a half-life of 1 minute and a
    sampling frequency of 16 seconds. Within 5 minutes or so, the
    calculation converges
    
    With each rate recalculation: reset total_cpu_time based on the kernel
    timebase delta since vpp started, and the new clock rate
    
    Improve the "show clock [verbose]" debug CLI command.
    
    BFD echo + echo fail tests marked extended, until the BFD code can be
    reworked a bit.
    
    Type: fix
    
    Signed-off-by: Dave Barach <dave@barachs.net>
    Change-Id: I24e88a78819b12867736c875067b386ef6115c5c

--- test/test_bfd.py
+++ test/test_bfd.py
@@ -1132,6 +1132,7 @@ class BFD4TestCase(VppTestCase):
         self.assert_equal(udp_sport_tx, udp_sport_rx, "UDP source port (== "
                           "ECHO packet identifier for test purposes)")
 
+    @unittest.skipUnless(running_extended_tests, "part of extended tests")
     def test_echo(self):
         """ echo function """
         bfd_session_up(self)
@@ -1188,6 +1189,7 @@ class BFD4TestCase(VppTestCase):
             self.test_session.send_packet()
         self.assertTrue(echo_seen, "No echo packets received")
 
+    @unittest.skipUnless(running_extended_tests, "part of extended tests")
     def test_echo_fail(self):
         """ session goes down if echo function fails """
         bfd_session_up(self)
