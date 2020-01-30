b25a98d40cdbb65dfc9ec9105080ceaa8e6a8dd1
Klement Sekera <ksekera@cisco.com>
2020-01-30 12:06:29 +0000

    bfd: DO NOT MERGE remove Dave's hack to see if it still fails
    DO NOT MERGE
    
    Type: fix
    
    Change-Id: Iefd68da8d86ece9a1670bcc3671c5a1460f09d5c
    Signed-off-by: Klement Sekera <ksekera@cisco.com>

--- src/vnet/bfd/bfd_main.c
+++ src/vnet/bfd/bfd_main.c
@@ -874,11 +874,6 @@ bfd_init_control_frame (bfd_main_t * bm, bfd_session_t * bs,
       pkt->req_min_rx =
 	clib_host_to_net_u32 (bfd_clocks_to_usec
 			      (bm, bs->effective_required_min_rx_clocks));
-      if (clib_net_to_host_u32 (pkt->req_min_rx) < 1000000)
-	{
-	  clib_warning ("BUG: fix echo packet req_min_rx < 1000000");
-	  pkt->req_min_rx = clib_host_to_net_u32 (1000000);
-	}
     }
   else
     {
@@ -1332,7 +1327,8 @@ bfd_main_init (vlib_main_t * vm)
   bm->vlib_main = vm;
   bm->vnet_main = vnet_get_main ();
   clib_memset (&bm->wheel, 0, sizeof (bm->wheel));
-  bm->cpu_cps = vm->clib_time.clocks_per_second;
+  // quick and dirty hack to avoid fractions messing up calculations
+  bm->cpu_cps = (u64)vm->clib_time.clocks_per_second;
   BFD_DBG ("cps is %.2f", bm->cpu_cps);
   bm->default_desired_min_tx_clocks =
     bfd_usec_to_clocks (bm, BFD_DEFAULT_DESIRED_MIN_TX_USEC);
