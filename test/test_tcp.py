ada84bb0e02ce21611b3ee9be58e8099bac543e3
Jakub Grajciar <jgrajcia@cisco.com>
2020-01-31 09:35:29 +0100

    session: API cleanup
    Use consistent API types.
    
    Type: fix
    
    Signed-off-by: Jakub Grajciar <jgrajcia@cisco.com>
    Change-Id: Ib752a3425c076eb53bc5255a48039cbb74b1fcce
    Signed-off-by: Jakub Grajciar <jgrajcia@cisco.com>

--- test/test_tcp.py
+++ test/test_tcp.py
@@ -36,9 +36,9 @@ class TestTCP(VppTestCase):
             table_id += 1
 
         # Configure namespaces
-        self.vapi.app_namespace_add_del(namespace_id=b"0",
+        self.vapi.app_namespace_add_del(namespace_id="0",
                                         sw_if_index=self.loop0.sw_if_index)
-        self.vapi.app_namespace_add_del(namespace_id=b"1",
+        self.vapi.app_namespace_add_del(namespace_id="1",
                                         sw_if_index=self.loop1.sw_if_index)
 
     def tearDown(self):
