2dfed5ad560a43d63fc8fd7a1a4920c621bd519a
Jakub Grajciar <jgrajcia@cisco.com>
2020-02-07 11:30:26 +0100

    api: improve api string safety
    - Remove vl_api_from_api_string
      to prevent use of not nul-terminated strings.
    - Rename vl_api_from_api_to_vec -> vl_api_from_api_to_new_vec
      to imply a new vector is created. NOT nul terminated.
    - Add vl_api_from_api_to_new_c_string. Returns
      nul terminated string in a new vector.
    - Add vl_api_c_string_to_api_string. Convert nul terminated
      string to vl_api_string_t
    - Add vl_api_vec_to_api_string. Convert NON nul terminated
      vector to vl_api_string_t
    
    Type: fix
    
    Signed-off-by: Jakub Grajciar <jgrajcia@cisco.com>
    Change-Id: Iadd59b612c0d960a34ad0dd07a9d17f56435c6ea
    Signed-off-by: Jakub Grajciar <jgrajcia@cisco.com>

--- src/plugins/http_static/http_static_test.c
+++ src/plugins/http_static/http_static_test.c
@@ -111,11 +111,8 @@ api_http_static_enable (vat_main_t * vam)
 
   /* Construct the API message */
   M (HTTP_STATIC_ENABLE, mp);
-  vl_api_to_api_string (strnlen ((const char *) www_root, 256),
-			(const char *) www_root,
-			(vl_api_string_t *) & mp->www_root);
-  vl_api_to_api_string (strnlen ((const char *) uri, 256), (const char *) uri,
-			(vl_api_string_t *) & mp->uri);
+  vl_api_vec_to_api_string (www_root, (vl_api_string_t *) & mp->www_root);
+  vl_api_vec_to_api_string (uri, (vl_api_string_t *) & mp->uri);
   mp->fifo_size = ntohl (fifo_size);
   mp->cache_size_limit = ntohl (cache_size_limit);
   mp->prealloc_fifos = ntohl (prealloc_fifos);
