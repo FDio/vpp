ee174d9d74807cb380016b702fc192449e16465a
Mohsin Kazmi <sykazmi@cisco.com>
2020-02-14 17:51:04 +0000

    tap: add support for persistance
    Type: feature
    
    Change-Id: I775f53531972447ebae0d69b9e2dfeee84d115e5
    Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>

--- src/vpp/api/custom_dump.c
+++ src/vpp/api/custom_dump.c
@@ -582,7 +582,11 @@ static void *vl_api_tap_create_v2_t_print
   if ((mp->tap_flags) & 0x1)
     s = format (s, "gso-enabled ");
   if ((mp->tap_flags) & 0x2)
-    s = format (s, "csum-offload-enabled");
+    s = format (s, "csum-offload-enabled ");
+  if ((mp->tap_flags) & 0x4)
+    s = format (s, "persist ");
+  if ((mp->tap_flags) & 0x8)
+    s = format (s, "attach ");
   FINISH;
 }
 
