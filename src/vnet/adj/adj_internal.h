0a25200de65447dfcf40a89b0d7d6b237699fdb7
Neale Ranns <nranns@cisco.com>
2020-02-18 13:56:24 +0000

    fib: adjacency midchain teardown (VPP-1841)
    Type: fix
    
    Change-Id: I57f8bfbce4feed9d2775875cb8b1b729a47900a4
    Signed-off-by: Neale Ranns <nranns@cisco.com>
    (cherry picked from commit 24064d02aa9810ebc64c16dc778a179bb0ef5483)
    (cherry picked from commit 3ebebc3a2fe47f1222ba035e04ccd8caed0cf58f)

--- src/vnet/adj/adj_internal.h
+++ src/vnet/adj/adj_internal.h
@@ -124,6 +124,7 @@ extern void adj_glean_remove(fib_protocol_t proto,
 			     u32 sw_if_index);
 extern void adj_mcast_remove(fib_protocol_t proto,
 			     u32 sw_if_index);
+extern void adj_midchain_teardown(ip_adjacency_t *adj);
 
 extern u32 adj_dpo_get_urpf(const dpo_id_t *dpo);
 
