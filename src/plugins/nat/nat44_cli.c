1dba8ad3c2d8110b6572124f3237426eada77917
Klement Sekera <ksekera@cisco.com>
2020-02-20 11:40:50 +0000

    nat: ED port re-use algorithm (bihash version)
    Type: fix
    
    Change-Id: I11440c855eb35d2a6095dfe135e4ab5090f11ff3
    Signed-off-by: Klement Sekera <ksekera@cisco.com>

--- src/plugins/nat/nat44_cli.c
+++ src/plugins/nat/nat44_cli.c
@@ -252,8 +252,11 @@ nat44_show_hash_commnad_fn (vlib_main_t * vm, unformat_input_t * input,
   }
 
   if (sm->endpoint_dependent)
-    vlib_cli_output (vm, "%U", format_bihash_16_8, &nam->affinity_hash,
-		     verbose);
+    {
+      vlib_cli_output (vm, "%U", format_bihash_16_8, &nam->affinity_hash,
+		       verbose);
+      vlib_cli_output (vm, "%U", format_bihash_16_8, &sm->ed_ext_ports, 0);
+    }
   return 0;
 }
 
