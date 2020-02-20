39d0285fca412ea2921fe1ae98a231894cd193cf
Klement Sekera <ksekera@cisco.com>
2020-02-20 11:39:58 +0000

    vppinfra: fix minor cuckoo bugs and add cuckoo_16_8
    Type: improvement
    
    Change-Id: If1164d2eb81e9d4748436cb1bb8b164857d70565
    Signed-off-by: Klement Sekera <ksekera@cisco.com>

--- src/vppinfra/cuckoo_16_8.h
+++ src/vppinfra/cuckoo_16_8.h
@@ -14,14 +14,14 @@
  */
 #undef CLIB_CUCKOO_TYPE
 
-#define CLIB_CUCKOO_TYPE _8_8
+#define CLIB_CUCKOO_TYPE _16_8
 #define CLIB_CUCKOO_KVP_PER_BUCKET (4)
 #define CLIB_CUCKOO_LOG2_KVP_PER_BUCKET (2)
 #define CLIB_CUCKOO_BFS_MAX_STEPS (2000)
 #define CLIB_CUCKOO_BFS_MAX_PATH_LENGTH (8)
 
-#ifndef __included_cuckoo_8_8_h__
-#define __included_cuckoo_8_8_h__
+#ifndef __included_cuckoo_16_8_h__
+#define __included_cuckoo_16_8_h__
 
 #include <vppinfra/heap.h>
 #include <vppinfra/format.h>
@@ -46,72 +46,80 @@
 /** 8 octet key, 8 octet key value pair */
 typedef struct
 {
-  u64 key;   /**< the key */
+  u64 key[2];	/**< the key */
   u64 value; /**< the value */
-} clib_cuckoo_kv_8_8_t;
+} clib_cuckoo_kv_16_8_t;
 
-/** Decide if a clib_cuckoo_kv_8_8_t instance is free
+/** Decide if a clib_cuckoo_kv_16_8_t instance is free
     @param v- pointer to the (key,value) pair
 */
 always_inline int
-clib_cuckoo_kv_is_free_8_8 (const clib_cuckoo_kv_8_8_t * v)
+clib_cuckoo_kv_is_free_16_8 (const clib_cuckoo_kv_16_8_t * v)
 {
-  if (v->key == ~0ULL && v->value == ~0ULL)
+  if (v->key[0] == ~0ULL && v->value == ~0ULL)
     return 1;
   return 0;
 }
 
 always_inline void
-clib_cuckoo_kv_set_free_8_8 (clib_cuckoo_kv_8_8_t * v)
+clib_cuckoo_kv_set_free_16_8 (clib_cuckoo_kv_16_8_t * v)
 {
   clib_memset (v, 0xff, sizeof (*v));
 }
 
-/** Format a clib_cuckoo_kv_8_8_t instance
+/** Format a clib_cuckoo_kv_16_8_t instance
     @param s - u8 * vector under construction
     @param args (vararg) - the (key,value) pair to format
     @return s - the u8 * vector under construction
 */
 always_inline u8 *
-format_cuckoo_kvp_8_8 (u8 * s, va_list * args)
+format_cuckoo_kvp_16_8 (u8 * s, va_list * args)
 {
-  clib_cuckoo_kv_8_8_t *v = va_arg (*args, clib_cuckoo_kv_8_8_t *);
+  clib_cuckoo_kv_16_8_t *v = va_arg (*args, clib_cuckoo_kv_16_8_t *);
 
-  if (clib_cuckoo_kv_is_free_8_8 (v))
+  if (clib_cuckoo_kv_is_free_16_8 (v))
     {
       s = format (s, " -- empty -- ");
     }
   else
     {
-      s = format (s, "key %llu value %llu", v->key, v->value);
+      s =
+	format (s, "key %llu%llu value %llu", v->key[0], v->key[1], v->value);
     }
   return s;
 }
 
 always_inline u64
-clib_cuckoo_hash_8_8 (clib_cuckoo_kv_8_8_t * v)
+clib_cuckoo_hash_16_8 (clib_cuckoo_kv_16_8_t * v)
 {
-#if defined(clib_crc32c_uses_intrinsics) && !defined (__i386__)
-  return crc32_u64 (0, v->key);
+#ifdef clib_crc32c_uses_intrinsics
+  return clib_crc32c ((u8 *) v->key, 16);
 #else
-  return clib_xxhash (v->key);
+  u64 tmp = v->key[0] ^ v->key[1];
+  return clib_xxhash (tmp);
 #endif
 }
 
-/** Compare two clib_cuckoo_kv_8_8_t instances
+/** Compare two clib_cuckoo_kv_16_8_t instances
     @param a - first key
     @param b - second key
 */
 always_inline int
-clib_cuckoo_key_compare_8_8 (u64 a, u64 b)
+clib_cuckoo_key_compare_16_8 (u64 * a, u64 * b)
 {
-  return a == b;
+#if defined(CLIB_HAVE_VEC128) && defined(CLIB_HAVE_VEC128_UNALIGNED_LOAD_STORE)
+  u64x2 v;
+  v = u64x2_load_unaligned (a) ^ u64x2_load_unaligned (b);
+  return u64x2_is_all_zero (v);
+#else
+  return ((a[0] ^ b[0]) | (a[1] ^ b[1])) == 0;
+#endif
 }
 
 #undef __included_cuckoo_template_h__
 #include <vppinfra/cuckoo_template.h>
 
-#endif /* __included_cuckoo_8_8_h__ */
+#endif /* __included_cuckoo_16_8_h__ */
 
 /*
  * fd.io coding-style-patch-verification: ON
