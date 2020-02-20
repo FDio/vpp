d1586962a5f8f14fb81c930174d12d0453adaab8
Dave Barach <dave@barachs.net>
2020-02-20 16:17:58 -0500

    dhcp: update secondary radv_info structures
    For details, see the Jira ticket below. Fix gerrit 23350.
    
    Type: fix
    Fixes: 28a6eb7
    Ticket: VPP-1840
    
    Signed-off-by: Dave Barach <dave@barachs.net>
    Change-Id: Ic9248734bb330eadb302f8410e8db9c64723f075

--- src/vnet/ip6-nd/ip6_ra.c
+++ src/vnet/ip6-nd/ip6_ra.c
@@ -559,13 +559,18 @@ icmp6_router_solicitation (vlib_main_t * vm,
                               }
                             h.unused  = 0;
 
-                            clib_warning ("Prefix %U valid %u preferred %u",
-                                          format_ip6_address, &pr_info->prefix,
-                                          clib_net_to_host_u32 (h.valid_time),
-                                          clib_net_to_host_u32 (h.preferred_time));
+                            /* Handy for debugging, but too noisy... */
+                            if (0 && CLIB_DEBUG > 0)
+                              clib_warning
+                                ("send RA for prefix %U/%d "
+                                 "sw_if_index %d valid %u preferred %u",
+                                 format_ip6_address, &pr_info->prefix,
+                                 pr_info->prefix_len, sw_if_index0,
+                                 clib_net_to_host_u32 (h.valid_time),
+                                 clib_net_to_host_u32 (h.preferred_time));
 
                             if (h.valid_time == 0)
-                              clib_warning ("WARNING: valid_time 0!!!");
+                              clib_warning ("BUG: send RA with valid_time 0");
 
                             clib_memcpy(&h.dst_address, &pr_info->prefix,  sizeof(ip6_address_t));
 
@@ -1412,6 +1417,66 @@ ip6_ra_delegate_disable (index_t rai)
   mhash_free (&radv_info->address_to_prefix_index);
 
   pool_put (ip6_ra_pool, radv_info);
+}
+
+void
+ip6_ra_update_secondary_radv_info (ip6_address_t * address, u8 prefix_len,
+				   u32 primary_sw_if_index,
+				   u32 valid_time, u32 preferred_time)
+{
+  vlib_main_t *vm = vlib_get_main ();
+  static u32 *radv_indices;
+  ip6_ra_t *radv_info;
+  int i;
+  ip6_address_t mask;
+  ip6_address_mask_from_width (&mask, prefix_len);
+
+  vec_reset_length (radv_indices);
+  /* *INDENT-OFF* */
+  pool_foreach (radv_info, ip6_ra_pool,
+  ({
+    vec_add1 (radv_indices, radv_info - ip6_ra_pool);
+  }));
+  /* *INDENT-ON* */
+
+  /*
+   * If we have another customer for this prefix,
+   * tell the RA code about it...
+   */
+  for (i = 0; i < vec_len (radv_indices); i++)
+    {
+      ip6_radv_prefix_t *this_prefix;
+      radv_info = pool_elt_at_index (ip6_ra_pool, radv_indices[i]);
+
+      /* We already took care of these timers... */
+      if (radv_info->sw_if_index == primary_sw_if_index)
+	continue;
+
+      /* *INDENT-OFF* */
+      pool_foreach (this_prefix, radv_info->adv_prefixes_pool,
+      ({
+        if (this_prefix->prefix_len == prefix_len
+            && ip6_address_is_equal_masked (&this_prefix->prefix, address,
+                                            &mask))
+          {
+            int rv = ip6_ra_prefix (vm,
+                                    radv_info->sw_if_index,
+                                    address,
+                                    prefix_len,
+                                    0 /* use_default */,
+                                    valid_time,
+                                    preferred_time,
+                                    0 /* no_advertise */,
+                                    0 /* off_link */,
+                                    0 /* no_autoconfig */,
+                                    0 /* no_onlink */,
+                                    0 /* is_no */);
+            if (rv != 0)
+              clib_warning ("ip6_neighbor_ra_prefix returned %d", rv);
+          }
+      }));
+      /* *INDENT-ON*/
+    }
 }
 
 /* send a RA or update the timer info etc.. */
