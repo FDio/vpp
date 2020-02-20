4471bb81555c60a7c56e17141d42fc42e56ffc1f
Florin Coras <fcoras@cisco.com>
2020-02-20 05:45:31 +0000

    tcp: dynamic max timers per loop
    Type: improvement
    
    Signed-off-by: Florin Coras <fcoras@cisco.com>
    Change-Id: If0a634c277d4f8e4d32321f52253721a990bdb1d

--- src/vnet/tcp/tcp.h
+++ src/vnet/tcp/tcp.h
@@ -558,6 +558,9 @@ typedef struct tcp_worker_ctx_
   /* Fifo of pending timer expirations */
   u32 *pending_timers;
 
+  /* Max timers to be handled per dispatch loop */
+  u32 max_timers;
+
   /** worker timer wheel */
   tw_timer_wheel_16t_2w_512sl_t timer_wheel;
 
