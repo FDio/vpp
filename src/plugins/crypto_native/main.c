7499ee1a563c967c75078e6464ad08d11e0fc566
Fan Zhang <roy.fan.zhang@intel.com>
2020-02-24 09:21:01 +0000

    crypto: introduce async crypto infra
    Type: feature
    
    Signed-off-by: Damjan Marion <damarion@cisco.com>
    Signed-off-by: Filip Tehlar <ftehlar@cisco.com>
    Signed-off-by: Fan Zhang <roy.fan.zhang@intel.com>
    Signed-off-by: Piotr Bronowski <piotrx.bronowski@intel.com>
    Signed-off-by: Dariusz Kazimierski <dariuszx.kazimierski@intel.com>
    Signed-off-by: Piotr Kleski <piotrx.kleski@intel.com>
    Change-Id: I4c3fcccf55c36842b7b48aed260fef2802b5c54b

--- src/plugins/crypto_native/main.c
+++ src/plugins/crypto_native/main.c
@@ -55,6 +55,106 @@ crypto_native_key_handler (vlib_main_t * vm, vnet_crypto_key_op_t kop,
   cm->key_data[idx] = cm->key_fn[key->alg] (key);
 }
 
+u32
+crypto_native_get_pending_jobs (crypto_native_queue_t * q,
+				vnet_crypto_op_t * jobs[], u32 n_jobs)
+{
+  u32 i;
+  vnet_crypto_op_t *j;
+  u32 tail = q->tail;
+  u32 head = q->head;
+  u32 mask = q->size - 1;
+  u32 n_got = 0;
+
+  if (head == tail)
+    return 0;
+
+  for (i = tail; i < head; i++)
+    {
+      j = &q->jobs[i & mask];
+
+      if (clib_atomic_bool_cmp_and_swap (&j->status,
+					 VNET_CRYPTO_OP_STATUS_PENDING,
+					 VNET_CRYPTO_OP_STATUS_WORK_IN_PROGRESS))
+	{
+	  jobs[n_got++] = j;
+	  if (PREDICT_FALSE (n_got == n_jobs))
+	    break;
+	}
+    }
+  return n_got;
+}
+
+u32
+crypto_native_dequeue_ops (crypto_native_per_thread_data_t * ptd,
+			   crypto_native_queue_t * q,
+			   vnet_crypto_op_async_data_t post_jobs[],
+			   u32 n_jobs)
+{
+  vnet_crypto_op_id_t opt = q->op;
+  u32 head = q->head;
+  u32 mask = q->size - 1;
+  u32 n_total = 0;
+
+  while (1)
+    {
+      u32 tail = clib_atomic_load_acq_n (&q->tail);
+      vnet_crypto_op_t *job = &q->jobs[tail & mask];
+
+      if (head == tail)
+	{
+	  clib_bitmap_set_no_check (ptd->act_queues, opt, 0);
+	  break;
+	}
+
+      if (job->status >= VNET_CRYPTO_OP_STATUS_COMPLETED)
+	{
+	  post_jobs[n_total].bi = job->async_dst.bi;
+	  post_jobs[n_total].next = job->async_dst.next;
+	  post_jobs[n_total++].status = (u16) job->status;
+	  q->jobs[tail & mask].status = VNET_CRYPTO_OP_STATUS_IDLE;
+	  clib_atomic_fetch_add (&q->tail, 1);
+	}
+
+      if (n_total == n_jobs)
+	break;
+    }
+
+  return n_total;
+}
+
+u32
+crypto_native_enqueue_ops (vlib_main_t * vm, vnet_crypto_op_id_t opt,
+			   vnet_crypto_op_t * jobs[], u32 n_jobs)
+{
+  crypto_native_main_t *cm = &crypto_native_main;
+  crypto_native_per_thread_data_t *ptd =
+    vec_elt_at_index (cm->per_thread_data,
+		      vm->thread_index);
+  crypto_native_queue_t *q = ptd->queues[opt];
+
+  u32 n_enq = 0;
+  u32 head = q->head;
+  u32 mask = q->size - 1;
+
+  while (n_enq < n_jobs)
+    {
+      if (q->jobs[head & mask].status != VNET_CRYPTO_OP_STATUS_IDLE)
+	break;
+      jobs[n_enq]->status = VNET_CRYPTO_OP_STATUS_PENDING;
+      q->jobs[head & mask] = *jobs[n_enq];
+      head += 1;
+      n_enq += 1;
+    }
+
+  if (n_enq)
+    {
+      CLIB_MEMORY_STORE_BARRIER ();
+      q->head = head;
+    }
+  return n_enq;
+}
+
 clib_error_t *
 crypto_native_init (vlib_main_t * vm)
 {
@@ -112,6 +212,25 @@ crypto_native_init (vlib_main_t * vm)
   vnet_crypto_register_key_handler (vm, cm->crypto_engine_index,
 				    crypto_native_key_handler);
 
+  u32 sz =
+    CRYPTO_NATIVE_RING_SIZE * sizeof (vnet_crypto_op_t) +
+    sizeof (crypto_native_queue_t);
+  crypto_native_queue_t *q;
+  crypto_native_per_thread_data_t *ptd;
+
+  vec_foreach (ptd, cm->per_thread_data)
+  {
+    for (int opt = 0; opt < VNET_CRYPTO_N_OP_IDS; opt++)
+      {
+	q = clib_mem_alloc_aligned (sz, CLIB_CACHE_LINE_BYTES);
+	ptd->queues[opt] = q;
+	clib_memset_u8 (q, 0, sz);
+	q->size = CRYPTO_NATIVE_RING_SIZE;
+	q->op = opt;
+      }
+    clib_bitmap_vec_validate (ptd->act_queues, VNET_CRYPTO_N_OP_IDS);
+    vec_validate_aligned (ptd->jobs, VLIB_FRAME_SIZE, CLIB_CACHE_LINE_BYTES);
+  }
 
 error:
   if (error)
