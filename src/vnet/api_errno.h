ced15b6cbbfb2a859a60daff132360061f3242a5
Neale Ranns <nranns@cisco.com>
2020-02-24 13:32:30 +0000

    ip: ip_address_t uses ip46_address_t
    Type: improvement
    
    type re-use.
    
    Signed-off-by: Neale Ranns <nranns@cisco.com>
    Change-Id: Ic2a2e6babf9ae66a1e53aec53a6cd157e1893dc8

--- src/vnet/api_errno.h
+++ src/vnet/api_errno.h
@@ -80,7 +80,7 @@ _(SR_POLICY_NAME_NOT_PRESENT, -84, "Segment routing policy name required") \
 _(NOT_RUNNING_AS_ROOT, -85, "Not running as root") \
 _(ALREADY_CONNECTED, -86, "Connection to the data plane already exists") \
 _(UNSUPPORTED_JNI_VERSION, -87, "Unsupported JNI version") \
-_(FAILED_TO_ATTACH_TO_JAVA_THREAD, -88, "Failed to attach to Java thread") \
+_(IP_PREFIX_INVALID, -88, "IP prefix invalid (masked bits set in address") \
 _(INVALID_WORKER, -89, "Invalid worker thread")                         \
 _(LISP_DISABLED, -90, "LISP is disabled")                               \
 _(CLASSIFY_TABLE_NOT_FOUND, -91, "Classify table not found")            \
