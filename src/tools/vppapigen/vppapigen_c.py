a7c340e65e8d457f9fb0168a48ca8f529611f255
Ole Troan <ot@cisco.com>
2020-02-28 13:45:42 +0100

    vppapigen: generate define for module_semver
    Generate a semver define for API modules in the VPP side C generator.
    
    E.g in ipsec.api_types.h
    
    Type: improvement
    Signed-off-by: Ole Troan <ot@cisco.com>
    Change-Id: I2ab86154e85e26fb075c4e91a01ff3692640654b

--- src/tools/vppapigen/vppapigen_c.py
+++ src/tools/vppapigen/vppapigen_c.py
@@ -521,6 +521,15 @@ def generate_include_types(s, module, stream):
     write('#ifndef included_{module}_api_types_h\n'.format(module=module))
     write('#define included_{module}_api_types_h\n'.format(module=module))
 
+    if 'version' in s['Option']:
+        v = s['Option']['version']
+        (major, minor, patch) = v.split('.')
+        write('#define {module}_VERSION_{major}_{minor}_{patch} 1\n'
+              .format(module=module.upper(),
+                      major=major,
+                      minor=minor,
+                      patch=patch))
+
     if len(s['Import']):
         write('/* Imported API files */\n')
         for i in s['Import']:
