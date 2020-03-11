445281c368ed4602c065603c9857c1a4f846cc76
Jakub Grajciar <jgrajcia@cisco.com>
2020-03-11 12:47:32 +0100

    acl: API cleanup
    Use consistent API types.
    
    Type: fix
    
    Signed-off-by: Jakub Grajciar <jgrajcia@cisco.com>
    Change-Id: If90d753f129312400c4c3669bb86289d0c3e0d99

--- src/vnet/ip/ip_types_api.h
+++ src/vnet/ip/ip_types_api.h
@@ -30,9 +30,9 @@
  * These enum decode/encodes use 'int' as the type for the enum because
  * one cannot forward declare an enum
  */
-extern int ip_address_family_decode (int _af, ip_address_family_t * out);
+extern int ip_address_family_decode (u8 _af, ip_address_family_t * out);
 extern int ip_address_family_encode (ip_address_family_t af);
-extern int ip_proto_decode (int _af, ip_protocol_t * out);
+extern int ip_proto_decode (u8 _af, ip_protocol_t * out);
 extern int ip_proto_encode (ip_protocol_t af);
 extern ip_dscp_t ip_dscp_decode (u8 _dscp);
 extern u8 ip_dscp_encode (ip_dscp_t dscp);
