FDio/vpp

commit b49bc1ae6404f0dff373578786a9fd95d961e5cd

tap: add support for persistance
Type: feature

Change-Id: I775f53531972447ebae0d69b9e2dfeee84d115e5
Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>

src/vat/api_format.c
@@ -7371,6 +7371,10 @@ api_tap_create_v2 (vat_main_t * vam)
 	tap_flags |= TAP_FLAG_GSO;
       else if (unformat (i, "csum-offload"))
 	tap_flags |= TAP_FLAG_CSUM_OFFLOAD;
+      else if (unformat (i, "persist"))
+	tap_flags |= TAP_FLAG_PERSIST;
+      else if (unformat (i, "attach"))
+	tap_flags |= TAP_FLAG_ATTACH;
       else
 	break;
     }
@@ -20626,7 +20630,7 @@ _(l2_flags,                                                             \
 _(bridge_flags,                                                         \
   "bd_id <bridge-domain-id> [learn] [forward] [uu-flood] [flood] [arp-term] [disable]\n") \
 _(tap_create_v2,                                                        \
-  "id <num> [hw-addr <mac-addr>] [host-if-name <name>] [host-ns <name>] [num-rx-queues <num>] [rx-ring-size <num>] [tx-ring-size <num>] [host-bridge <name>] [host-mac-addr <mac-addr>] [host-ip4-addr <ip4addr/mask>] [host-ip6-addr <ip6addr/mask>] [host-mtu-size <mtu>] [gso | no-gso | csum-offload]") \
+  "id <num> [hw-addr <mac-addr>] [host-if-name <name>] [host-ns <name>] [num-rx-queues <num>] [rx-ring-size <num>] [tx-ring-size <num>] [host-bridge <name>] [host-mac-addr <mac-addr>] [host-ip4-addr <ip4addr/mask>] [host-ip6-addr <ip6addr/mask>] [host-mtu-size <mtu>] [gso | no-gso | csum-offload] [persist] [attach]") \
 _(tap_delete_v2,                                                        \
   "<vpp-if-name> | sw_if_index <id>")                                   \
 _(sw_interface_tap_v2_dump, "")                                         \

src/vnet/devices/netlink.c
@@ -243,7 +243,7 @@ vnet_netlink_add_ip4_addr (int ifindex, void *addr, int pfx_len)
   ifa.ifa_index = ifindex;
 
   vnet_netlink_msg_init (&m, RTM_NEWADDR,
-			 NLM_F_REQUEST | NLM_F_CREATE | NLM_F_EXCL,
+			 NLM_F_REQUEST | NLM_F_REPLACE,
 			 &ifa, sizeof (struct ifaddrmsg));
 
   vnet_netlink_msg_add_rtattr (&m, IFA_LOCAL, addr, 4);
@@ -266,7 +266,7 @@ vnet_netlink_add_ip6_addr (int ifindex, void *addr, int pfx_len)
   ifa.ifa_index = ifindex;
 
   vnet_netlink_msg_init (&m, RTM_NEWADDR,
-			 NLM_F_REQUEST | NLM_F_CREATE | NLM_F_EXCL,
+			 NLM_F_REQUEST | NLM_F_REPLACE,
 			 &ifa, sizeof (struct ifaddrmsg));
 
   vnet_netlink_msg_add_rtattr (&m, IFA_LOCAL, addr, 16);
@@ -291,7 +291,7 @@ vnet_netlink_add_ip4_route (void *dst, u8 dst_len, void *gw)
   rtm.rtm_dst_len = dst_len;
 
   vnet_netlink_msg_init (&m, RTM_NEWROUTE,
-			 NLM_F_REQUEST | NLM_F_CREATE | NLM_F_EXCL,
+			 NLM_F_REQUEST | NLM_F_REPLACE,
 			 &rtm, sizeof (struct rtmsg));
 
   vnet_netlink_msg_add_rtattr (&m, RTA_GATEWAY, gw, 4);
@@ -316,7 +316,7 @@ vnet_netlink_add_ip6_route (void *dst, u8 dst_len, void *gw)
   rtm.rtm_dst_len = dst_len;
 
   vnet_netlink_msg_init (&m, RTM_NEWROUTE,
-			 NLM_F_REQUEST | NLM_F_CREATE | NLM_F_EXCL,
+			 NLM_F_REQUEST | NLM_F_REPLACE,
 			 &rtm, sizeof (struct rtmsg));
 
   vnet_netlink_msg_add_rtattr (&m, RTA_GATEWAY, gw, 16);

src/vnet/devices/tap/FEATURE.yaml
@@ -3,6 +3,8 @@ name: Tap Device
 maintainer: damarion@cisco.com sluong@cisco.com sykazmi@cisco.com
 features:
   - Virtio
+  - persistence
+  - attach to existing tap at host
 description: "Create a tap v2 device interface, which connects to a
               tap interface on the host system."
 missing:

src/vnet/devices/tap/cli.c
@@ -92,6 +92,10 @@ tap_create_command_fn (vlib_main_t * vm, unformat_input_t * input,
 	    args.tap_flags |= TAP_FLAG_GSO;
 	  else if (unformat (line_input, "csum-offload"))
 	    args.tap_flags |= TAP_FLAG_CSUM_OFFLOAD;
+	  else if (unformat (line_input, "persist"))
+	    args.tap_flags |= TAP_FLAG_PERSIST;
+	  else if (unformat (line_input, "attach"))
+	    args.tap_flags |= TAP_FLAG_ATTACH;
 	  else if (unformat (line_input, "hw-addr %U",
 			     unformat_ethernet_address, args.mac_addr.bytes))
 	    args.mac_addr_set = 1;
@@ -131,7 +135,8 @@ VLIB_CLI_COMMAND (tap_create_command, static) = {
     "[host-bridge <bridge-name>] [host-ip4-addr <ip4addr/mask>] "
     "[host-ip6-addr <ip6-addr>] [host-ip4-gw <ip4-addr>] "
     "[host-ip6-gw <ip6-addr>] [host-mac-addr <host-mac-address>] "
-    "[host-if-name <name>] [host-mtu-size <size>] [no-gso|gso|csum-offload]",
+    "[host-if-name <name>] [host-mtu-size <size>] [no-gso|gso|csum-offload] "
+    "[persist] [attach]",
   .function = tap_create_command_fn,
 };
 /* *INDENT-ON* */

src/vnet/devices/tap/tap.c
@@ -91,6 +91,7 @@ tap_free (vlib_main_t * vm, virtio_if_t * vif)
 {
   virtio_main_t *mm = &virtio_main;
   tap_main_t *tm = &tap_main;
+  clib_error_t *err = 0;
   int i;
 
   /* *INDENT-OFF* */
@@ -102,6 +103,9 @@ tap_free (vlib_main_t * vm, virtio_if_t * vif)
     virtio_vring_free_tx (vm, vif, TX_QUEUE (i));
   /* *INDENT-ON* */
 
+  _IOCTL (vif->tap_fd, TUNSETPERSIST, (void *) (uintptr_t) 0);
+  tap_log_dbg (vif, "TUNSETPERSIST: unset");
+error:
   if (vif->tap_fd != -1)
     close (vif->tap_fd);
 
@@ -131,6 +135,7 @@ tap_create_if (vlib_main_t * vm, tap_create_if_args_t * args)
   int i;
   int old_netns_fd = -1;
   struct ifreq ifr = {.ifr_flags = IFF_TAP | IFF_NO_PI | IFF_VNET_HDR };
+  struct ifreq get_ifr = {.ifr_flags = 0 };
   size_t hdrsz;
   struct vhost_memory *vhost_mem = 0;
   virtio_if_t *vif = 0;
@@ -170,6 +175,39 @@ tap_create_if (vlib_main_t * vm, tap_create_if_args_t * args)
   vif->num_rxqs = args->num_rx_queues;
   num_q_pairs = clib_max (vif->num_rxqs, vif->num_txqs);
 
+  if (args->tap_flags & TAP_FLAG_ATTACH)
+    {
+      if (args->host_if_name != NULL)
+	{
+	  host_if_name = (char *) args->host_if_name;
+	  clib_memcpy (ifr.ifr_name, host_if_name,
+		       clib_min (IFNAMSIZ, strlen (host_if_name)));
+	}
+      else
+	{
+	  args->rv = VNET_API_ERROR_NO_MATCHING_INTERFACE;
+	  err = clib_error_return (0, "host_if_name is not provided");
+	  goto error;
+	}
+      if (args->host_namespace)
+	{
+	  old_netns_fd = open ("/proc/self/ns/net", O_RDONLY);
+	  if ((nfd = open_netns_fd ((char *) args->host_namespace)) == -1)
+	    {
+	      args->rv = VNET_API_ERROR_SYSCALL_ERROR_2;
+	      args->error = clib_error_return_unix (0, "open_netns_fd '%s'",
+						    args->host_namespace);
+	      goto error;
+	    }
+	  if (setns (nfd, CLONE_NEWNET) == -1)
+	    {
+	      args->rv = VNET_API_ERROR_SYSCALL_ERROR_3;
+	      args->error = clib_error_return_unix (0, "setns '%s'",
+						    args->host_namespace);
+	      goto error;
+	    }
+	}
+    }
   if ((vif->tap_fd = tfd = open ("/dev/net/tun", O_RDWR | O_NONBLOCK)) < 0)
     {
       args->rv = VNET_API_ERROR_SYSCALL_ERROR_2;
@@ -229,6 +267,33 @@ tap_create_if (vlib_main_t * vm, tap_create_if_args_t * args)
       err = clib_error_return_unix (0, "fcntl(tfd, F_SETFL, O_NONBLOCK)");
       tap_log_err (vif, "set nonblocking: %U", format_clib_error, err);
       goto error;
+    }
+
+  /*
+   * unset the persistence when attaching to existing
+   * interface
+   */
+  if (args->tap_flags & TAP_FLAG_ATTACH)
+    {
+      _IOCTL (tfd, TUNSETPERSIST, (void *) (uintptr_t) 0);
+      tap_log_dbg (vif, "TUNSETPERSIST: unset");
+    }
+
+  /* set the persistence */
+  if (args->tap_flags & TAP_FLAG_PERSIST)
+    {
+      _IOCTL (tfd, TUNSETPERSIST, (void *) (uintptr_t) 1);
+      tap_log_dbg (vif, "TUNSETPERSIST: set");
+
+      /* verify persistence is set, read the flags */
+      _IOCTL (tfd, TUNGETIFF, (void *) &get_ifr);
+      tap_log_dbg (vif, "TUNGETIFF: flags 0x%lx", get_ifr.ifr_flags);
+      if ((get_ifr.ifr_flags & IFF_PERSIST) == 0)
+	{
+	  args->rv = VNET_API_ERROR_SYSCALL_ERROR_2;
+	  args->error = clib_error_return (0, "persistence not supported");
+	  goto error;
+	}
     }
 
   tap_log_dbg (vif, "TUNSETVNETHDRSZ: fd %d vnet_hdr_sz %u", tfd, hdrsz);
@@ -291,46 +356,46 @@ tap_create_if (vlib_main_t * vm, tap_create_if_args_t * args)
 
   virtio_set_net_hdr_size (vif);
 
-  /* if namespace is specified, all further netlink messages should be executed
-     after we change our net namespace */
-  if (args->host_namespace)
+  if (!(args->tap_flags & TAP_FLAG_ATTACH))
     {
-      old_netns_fd = open ("/proc/self/ns/net", O_RDONLY);
-      if ((nfd = open_netns_fd ((char *) args->host_namespace)) == -1)
+      /* if namespace is specified, all further netlink messages should be executed
+         after we change our net namespace */
+      if (args->host_namespace)
 	{
-	  args->rv = VNET_API_ERROR_SYSCALL_ERROR_2;
-	  args->error = clib_error_return_unix (0, "open_netns_fd '%s'",
-						args->host_namespace);
-	  goto error;
-	}
-      args->error = vnet_netlink_set_link_netns (vif->ifindex, nfd,
-						 host_if_name);
-      if (args->error)
-	{
-	  args->rv = VNET_API_ERROR_NETLINK_ERROR;
-	  goto error;
-	}
-      if (setns (nfd, CLONE_NEWNET) == -1)
-	{
-	  args->rv = VNET_API_ERROR_SYSCALL_ERROR_3;
-	  args->error = clib_error_return_unix (0, "setns '%s'",
-						args->host_namespace);
-	  goto error;
-	}
-      if ((vif->ifindex = if_nametoindex (host_if_name)) == 0)
-	{
-	  args->rv = VNET_API_ERROR_SYSCALL_ERROR_3;
-	  args->error = clib_error_return_unix (0, "if_nametoindex '%s'",
-						host_if_name);
-	  goto error;
-	}
-    }
-  else
-    {
-      if (host_if_name)
-	{
-	  args->error = vnet_netlink_set_link_name (vif->ifindex,
+	  old_netns_fd = open ("/proc/self/ns/net", O_RDONLY);
+	  if ((nfd = open_netns_fd ((char *) args->host_namespace)) == -1)
+	    {
+	      args->rv = VNET_API_ERROR_SYSCALL_ERROR_2;
+	      args->error = clib_error_return_unix (0, "open_netns_fd '%s'",
+						    args->host_namespace);
+	      goto error;
+	    }
+	  args->error = vnet_netlink_set_link_netns (vif->ifindex, nfd,
+						     host_if_name);
+	  if (args->error)
+	    {
+	      args->rv = VNET_API_ERROR_NETLINK_ERROR;
+	      goto error;
+	    }
+	  if (setns (nfd, CLONE_NEWNET) == -1)
+	    {
+	      args->rv = VNET_API_ERROR_SYSCALL_ERROR_3;
+	      args->error = clib_error_return_unix (0, "setns '%s'",
+						    args->host_namespace);
+	      goto error;
+	    }
+	  if ((vif->ifindex = if_nametoindex (host_if_name)) == 0)
+	    {
+	      args->rv = VNET_API_ERROR_SYSCALL_ERROR_3;
+	      args->error = clib_error_return_unix (0, "if_nametoindex '%s'",
 						    host_if_name);
+	      goto error;
+	    }
+	}
+      else if (host_if_name)
+	{
+	  args->error =
+	    vnet_netlink_set_link_name (vif->ifindex, host_if_name);
 	  if (args->error)
 	    {
 	      args->rv = VNET_API_ERROR_NETLINK_ERROR;

src/vnet/devices/tap/tap.h
@@ -33,6 +33,8 @@ typedef struct
   u32 tap_flags;
 #define TAP_FLAG_GSO (1 << 0)
 #define TAP_FLAG_CSUM_OFFLOAD (1 << 1)
+#define TAP_FLAG_PERSIST (1 << 2)
+#define TAP_FLAG_ATTACH (1 << 3)
   u8 *host_namespace;
   u8 *host_if_name;
   mac_address_t host_mac_addr;

src/vnet/devices/tap/tapv2.api
@@ -28,6 +28,8 @@ import "vnet/ip/ip_types.api";
 enum tap_flags {
         TAP_FLAG_GSO = 1,
         TAP_FLAG_CSUM_OFFLOAD = 2,
+	TAP_FLAG_PERSIST = 4,
+	TAP_FLAG_ATTACH = 8,
 };
 
 /** \brief Initialize a new tap interface with the given parameters

src/vnet/devices/tap/tapv2_api.c
@@ -164,8 +164,6 @@ vl_api_tap_delete_v2_t_handler (vl_api_tap_delete_v2_t * mp)
 
   rv = tap_delete_if (vm, sw_if_index);
 
-
-
   rmp = vl_msg_api_alloc (sizeof (*rmp));
   rmp->_vl_msg_id = ntohs (VL_API_TAP_DELETE_V2_REPLY);
   rmp->context = mp->context;

commit f26b99c5732e61b725cff79dd2ec06224e4666b0

misc: Revert "tap: add support for persistance"
This reverts commit b49bc1ae6404f0dff373578786a9fd95d961e5cd.

Reason for revert: netlink error -2 when setting host gateway / default route. No workaround.

Type: fix
Fixes: b49bc1a

Change-Id: Ic4bb171e136d41bda1ccf30bf1acd1a2e8094462
Signed-off-by: Dave Barach <dave@barachs.net>

src/vat/api_format.c
@@ -7371,10 +7371,6 @@ api_tap_create_v2 (vat_main_t * vam)
 	tap_flags |= TAP_FLAG_GSO;
       else if (unformat (i, "csum-offload"))
 	tap_flags |= TAP_FLAG_CSUM_OFFLOAD;
-      else if (unformat (i, "persist"))
-	tap_flags |= TAP_FLAG_PERSIST;
-      else if (unformat (i, "attach"))
-	tap_flags |= TAP_FLAG_ATTACH;
       else
 	break;
     }
@@ -20630,7 +20626,7 @@ _(l2_flags,                                                             \
 _(bridge_flags,                                                         \
   "bd_id <bridge-domain-id> [learn] [forward] [uu-flood] [flood] [arp-term] [disable]\n") \
 _(tap_create_v2,                                                        \
-  "id <num> [hw-addr <mac-addr>] [host-if-name <name>] [host-ns <name>] [num-rx-queues <num>] [rx-ring-size <num>] [tx-ring-size <num>] [host-bridge <name>] [host-mac-addr <mac-addr>] [host-ip4-addr <ip4addr/mask>] [host-ip6-addr <ip6addr/mask>] [host-mtu-size <mtu>] [gso | no-gso | csum-offload] [persist] [attach]") \
+  "id <num> [hw-addr <mac-addr>] [host-if-name <name>] [host-ns <name>] [num-rx-queues <num>] [rx-ring-size <num>] [tx-ring-size <num>] [host-bridge <name>] [host-mac-addr <mac-addr>] [host-ip4-addr <ip4addr/mask>] [host-ip6-addr <ip6addr/mask>] [host-mtu-size <mtu>] [gso | no-gso | csum-offload]") \
 _(tap_delete_v2,                                                        \
   "<vpp-if-name> | sw_if_index <id>")                                   \
 _(sw_interface_tap_v2_dump, "")                                         \

src/vnet/devices/netlink.c
@@ -243,7 +243,7 @@ vnet_netlink_add_ip4_addr (int ifindex, void *addr, int pfx_len)
   ifa.ifa_index = ifindex;
 
   vnet_netlink_msg_init (&m, RTM_NEWADDR,
-			 NLM_F_REQUEST | NLM_F_REPLACE,
+			 NLM_F_REQUEST | NLM_F_CREATE | NLM_F_EXCL,
 			 &ifa, sizeof (struct ifaddrmsg));
 
   vnet_netlink_msg_add_rtattr (&m, IFA_LOCAL, addr, 4);
@@ -266,7 +266,7 @@ vnet_netlink_add_ip6_addr (int ifindex, void *addr, int pfx_len)
   ifa.ifa_index = ifindex;
 
   vnet_netlink_msg_init (&m, RTM_NEWADDR,
-			 NLM_F_REQUEST | NLM_F_REPLACE,
+			 NLM_F_REQUEST | NLM_F_CREATE | NLM_F_EXCL,
 			 &ifa, sizeof (struct ifaddrmsg));
 
   vnet_netlink_msg_add_rtattr (&m, IFA_LOCAL, addr, 16);
@@ -291,7 +291,7 @@ vnet_netlink_add_ip4_route (void *dst, u8 dst_len, void *gw)
   rtm.rtm_dst_len = dst_len;
 
   vnet_netlink_msg_init (&m, RTM_NEWROUTE,
-			 NLM_F_REQUEST | NLM_F_REPLACE,
+			 NLM_F_REQUEST | NLM_F_CREATE | NLM_F_EXCL,
 			 &rtm, sizeof (struct rtmsg));
 
   vnet_netlink_msg_add_rtattr (&m, RTA_GATEWAY, gw, 4);
@@ -316,7 +316,7 @@ vnet_netlink_add_ip6_route (void *dst, u8 dst_len, void *gw)
   rtm.rtm_dst_len = dst_len;
 
   vnet_netlink_msg_init (&m, RTM_NEWROUTE,
-			 NLM_F_REQUEST | NLM_F_REPLACE,
+			 NLM_F_REQUEST | NLM_F_CREATE | NLM_F_EXCL,
 			 &rtm, sizeof (struct rtmsg));
 
   vnet_netlink_msg_add_rtattr (&m, RTA_GATEWAY, gw, 16);

src/vnet/devices/tap/FEATURE.yaml
@@ -3,8 +3,6 @@ name: Tap Device
 maintainer: damarion@cisco.com sluong@cisco.com sykazmi@cisco.com
 features:
   - Virtio
-  - persistence
-  - attach to existing tap at host
 description: "Create a tap v2 device interface, which connects to a
               tap interface on the host system."
 missing:

src/vnet/devices/tap/cli.c
@@ -92,10 +92,6 @@ tap_create_command_fn (vlib_main_t * vm, unformat_input_t * input,
 	    args.tap_flags |= TAP_FLAG_GSO;
 	  else if (unformat (line_input, "csum-offload"))
 	    args.tap_flags |= TAP_FLAG_CSUM_OFFLOAD;
-	  else if (unformat (line_input, "persist"))
-	    args.tap_flags |= TAP_FLAG_PERSIST;
-	  else if (unformat (line_input, "attach"))
-	    args.tap_flags |= TAP_FLAG_ATTACH;
 	  else if (unformat (line_input, "hw-addr %U",
 			     unformat_ethernet_address, args.mac_addr.bytes))
 	    args.mac_addr_set = 1;
@@ -135,8 +131,7 @@ VLIB_CLI_COMMAND (tap_create_command, static) = {
     "[host-bridge <bridge-name>] [host-ip4-addr <ip4addr/mask>] "
     "[host-ip6-addr <ip6-addr>] [host-ip4-gw <ip4-addr>] "
     "[host-ip6-gw <ip6-addr>] [host-mac-addr <host-mac-address>] "
-    "[host-if-name <name>] [host-mtu-size <size>] [no-gso|gso|csum-offload] "
-    "[persist] [attach]",
+    "[host-if-name <name>] [host-mtu-size <size>] [no-gso|gso|csum-offload]",
   .function = tap_create_command_fn,
 };
 /* *INDENT-ON* */

src/vnet/devices/tap/tap.c
@@ -91,7 +91,6 @@ tap_free (vlib_main_t * vm, virtio_if_t * vif)
 {
   virtio_main_t *mm = &virtio_main;
   tap_main_t *tm = &tap_main;
-  clib_error_t *err = 0;
   int i;
 
   /* *INDENT-OFF* */
@@ -103,9 +102,6 @@ tap_free (vlib_main_t * vm, virtio_if_t * vif)
     virtio_vring_free_tx (vm, vif, TX_QUEUE (i));
   /* *INDENT-ON* */
 
-  _IOCTL (vif->tap_fd, TUNSETPERSIST, (void *) (uintptr_t) 0);
-  tap_log_dbg (vif, "TUNSETPERSIST: unset");
-error:
   if (vif->tap_fd != -1)
     close (vif->tap_fd);
 
@@ -135,7 +131,6 @@ tap_create_if (vlib_main_t * vm, tap_create_if_args_t * args)
   int i;
   int old_netns_fd = -1;
   struct ifreq ifr = {.ifr_flags = IFF_TAP | IFF_NO_PI | IFF_VNET_HDR };
-  struct ifreq get_ifr = {.ifr_flags = 0 };
   size_t hdrsz;
   struct vhost_memory *vhost_mem = 0;
   virtio_if_t *vif = 0;
@@ -175,39 +170,6 @@ tap_create_if (vlib_main_t * vm, tap_create_if_args_t * args)
   vif->num_rxqs = args->num_rx_queues;
   num_q_pairs = clib_max (vif->num_rxqs, vif->num_txqs);
 
-  if (args->tap_flags & TAP_FLAG_ATTACH)
-    {
-      if (args->host_if_name != NULL)
-	{
-	  host_if_name = (char *) args->host_if_name;
-	  clib_memcpy (ifr.ifr_name, host_if_name,
-		       clib_min (IFNAMSIZ, strlen (host_if_name)));
-	}
-      else
-	{
-	  args->rv = VNET_API_ERROR_NO_MATCHING_INTERFACE;
-	  err = clib_error_return (0, "host_if_name is not provided");
-	  goto error;
-	}
-      if (args->host_namespace)
-	{
-	  old_netns_fd = open ("/proc/self/ns/net", O_RDONLY);
-	  if ((nfd = open_netns_fd ((char *) args->host_namespace)) == -1)
-	    {
-	      args->rv = VNET_API_ERROR_SYSCALL_ERROR_2;
-	      args->error = clib_error_return_unix (0, "open_netns_fd '%s'",
-						    args->host_namespace);
-	      goto error;
-	    }
-	  if (setns (nfd, CLONE_NEWNET) == -1)
-	    {
-	      args->rv = VNET_API_ERROR_SYSCALL_ERROR_3;
-	      args->error = clib_error_return_unix (0, "setns '%s'",
-						    args->host_namespace);
-	      goto error;
-	    }
-	}
-    }
   if ((vif->tap_fd = tfd = open ("/dev/net/tun", O_RDWR | O_NONBLOCK)) < 0)
     {
       args->rv = VNET_API_ERROR_SYSCALL_ERROR_2;
@@ -267,33 +229,6 @@ tap_create_if (vlib_main_t * vm, tap_create_if_args_t * args)
       err = clib_error_return_unix (0, "fcntl(tfd, F_SETFL, O_NONBLOCK)");
       tap_log_err (vif, "set nonblocking: %U", format_clib_error, err);
       goto error;
-    }
-
-  /*
-   * unset the persistence when attaching to existing
-   * interface
-   */
-  if (args->tap_flags & TAP_FLAG_ATTACH)
-    {
-      _IOCTL (tfd, TUNSETPERSIST, (void *) (uintptr_t) 0);
-      tap_log_dbg (vif, "TUNSETPERSIST: unset");
-    }
-
-  /* set the persistence */
-  if (args->tap_flags & TAP_FLAG_PERSIST)
-    {
-      _IOCTL (tfd, TUNSETPERSIST, (void *) (uintptr_t) 1);
-      tap_log_dbg (vif, "TUNSETPERSIST: set");
-
-      /* verify persistence is set, read the flags */
-      _IOCTL (tfd, TUNGETIFF, (void *) &get_ifr);
-      tap_log_dbg (vif, "TUNGETIFF: flags 0x%lx", get_ifr.ifr_flags);
-      if ((get_ifr.ifr_flags & IFF_PERSIST) == 0)
-	{
-	  args->rv = VNET_API_ERROR_SYSCALL_ERROR_2;
-	  args->error = clib_error_return (0, "persistence not supported");
-	  goto error;
-	}
     }
 
   tap_log_dbg (vif, "TUNSETVNETHDRSZ: fd %d vnet_hdr_sz %u", tfd, hdrsz);
@@ -356,46 +291,46 @@ tap_create_if (vlib_main_t * vm, tap_create_if_args_t * args)
 
   virtio_set_net_hdr_size (vif);
 
-  if (!(args->tap_flags & TAP_FLAG_ATTACH))
+  /* if namespace is specified, all further netlink messages should be executed
+     after we change our net namespace */
+  if (args->host_namespace)
     {
-      /* if namespace is specified, all further netlink messages should be executed
-         after we change our net namespace */
-      if (args->host_namespace)
+      old_netns_fd = open ("/proc/self/ns/net", O_RDONLY);
+      if ((nfd = open_netns_fd ((char *) args->host_namespace)) == -1)
 	{
-	  old_netns_fd = open ("/proc/self/ns/net", O_RDONLY);
-	  if ((nfd = open_netns_fd ((char *) args->host_namespace)) == -1)
-	    {
-	      args->rv = VNET_API_ERROR_SYSCALL_ERROR_2;
-	      args->error = clib_error_return_unix (0, "open_netns_fd '%s'",
-						    args->host_namespace);
-	      goto error;
-	    }
-	  args->error = vnet_netlink_set_link_netns (vif->ifindex, nfd,
-						     host_if_name);
-	  if (args->error)
-	    {
-	      args->rv = VNET_API_ERROR_NETLINK_ERROR;
-	      goto error;
-	    }
-	  if (setns (nfd, CLONE_NEWNET) == -1)
-	    {
-	      args->rv = VNET_API_ERROR_SYSCALL_ERROR_3;
-	      args->error = clib_error_return_unix (0, "setns '%s'",
-						    args->host_namespace);
-	      goto error;
-	    }
-	  if ((vif->ifindex = if_nametoindex (host_if_name)) == 0)
-	    {
-	      args->rv = VNET_API_ERROR_SYSCALL_ERROR_3;
-	      args->error = clib_error_return_unix (0, "if_nametoindex '%s'",
-						    host_if_name);
-	      goto error;
-	    }
+	  args->rv = VNET_API_ERROR_SYSCALL_ERROR_2;
+	  args->error = clib_error_return_unix (0, "open_netns_fd '%s'",
+						args->host_namespace);
+	  goto error;
 	}
-      else if (host_if_name)
+      args->error = vnet_netlink_set_link_netns (vif->ifindex, nfd,
+						 host_if_name);
+      if (args->error)
 	{
-	  args->error =
-	    vnet_netlink_set_link_name (vif->ifindex, host_if_name);
+	  args->rv = VNET_API_ERROR_NETLINK_ERROR;
+	  goto error;
+	}
+      if (setns (nfd, CLONE_NEWNET) == -1)
+	{
+	  args->rv = VNET_API_ERROR_SYSCALL_ERROR_3;
+	  args->error = clib_error_return_unix (0, "setns '%s'",
+						args->host_namespace);
+	  goto error;
+	}
+      if ((vif->ifindex = if_nametoindex (host_if_name)) == 0)
+	{
+	  args->rv = VNET_API_ERROR_SYSCALL_ERROR_3;
+	  args->error = clib_error_return_unix (0, "if_nametoindex '%s'",
+						host_if_name);
+	  goto error;
+	}
+    }
+  else
+    {
+      if (host_if_name)
+	{
+	  args->error = vnet_netlink_set_link_name (vif->ifindex,
+						    host_if_name);
 	  if (args->error)
 	    {
 	      args->rv = VNET_API_ERROR_NETLINK_ERROR;

src/vnet/devices/tap/tap.h
@@ -33,8 +33,6 @@ typedef struct
   u32 tap_flags;
 #define TAP_FLAG_GSO (1 << 0)
 #define TAP_FLAG_CSUM_OFFLOAD (1 << 1)
-#define TAP_FLAG_PERSIST (1 << 2)
-#define TAP_FLAG_ATTACH (1 << 3)
   u8 *host_namespace;
   u8 *host_if_name;
   mac_address_t host_mac_addr;

src/vnet/devices/tap/tapv2.api
@@ -28,8 +28,6 @@ import "vnet/ip/ip_types.api";
 enum tap_flags {
         TAP_FLAG_GSO = 1,
         TAP_FLAG_CSUM_OFFLOAD = 2,
-	TAP_FLAG_PERSIST = 4,
-	TAP_FLAG_ATTACH = 8,
 };
 
 /** \brief Initialize a new tap interface with the given parameters

src/vnet/devices/tap/tapv2_api.c
@@ -164,6 +164,8 @@ vl_api_tap_delete_v2_t_handler (vl_api_tap_delete_v2_t * mp)
 
   rv = tap_delete_if (vm, sw_if_index);
 
+
+
   rmp = vl_msg_api_alloc (sizeof (*rmp));
   rmp->_vl_msg_id = ntohs (VL_API_TAP_DELETE_V2_REPLY);
   rmp->context = mp->context;

src/vpp/api/custom_dump.c
@@ -582,11 +582,7 @@ static void *vl_api_tap_create_v2_t_print
   if ((mp->tap_flags) & 0x1)
     s = format (s, "gso-enabled ");
   if ((mp->tap_flags) & 0x2)
-    s = format (s, "csum-offload-enabled ");
-  if ((mp->tap_flags) & 0x4)
-    s = format (s, "persist ");
-  if ((mp->tap_flags) & 0x8)
-    s = format (s, "attach ");
+    s = format (s, "csum-offload-enabled");
   FINISH;
 }
 