303ca88d6b4e138a82676607e44cb2de485eb559
John DeNisco <jdenisco@cisco.com>
2020-03-20 10:03:47 -0400

    docs: Fix build on fresh ubuntu 18.04
    Signed-off-by: John DeNisco <jdenisco@cisco.com>
    Change-Id: If842c92183a9cf4b5a9773dced3298005b327e1a

--- docs/scripts/sphinx-make.sh
+++ docs/scripts/sphinx-make.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-if [ "$1" == "venv" ]
+if [ "$1" == "dep" ]
 then
-    python3 -m pip install --user virtualenv
+    OS_ID=$(grep '^ID=' /etc/os-release  | cut -f2- -d= | sed -e 's/\"//g')
+    if [ "$OS_ID" == "ubuntu" ]
+    then
+	sudo apt-get install -y python3-pip
+    fi
+    exit 0
+elif [ "$1" == "venv" ]
+then
+    pip3 install --user virtualenv
     python3 -m virtualenv $VENV_DIR
     source $VENV_DIR/bin/activate;
-    python3 -m pip install -r $DOCS_DIR/etc/requirements.txt
+    pip3 install -r $DOCS_DIR/etc/requirements.txt
 else
     source $VENV_DIR/bin/activate;
     VERSION=`source $WS_ROOT/src/scripts/version`
