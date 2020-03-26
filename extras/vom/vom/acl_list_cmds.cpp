ca022d36188153c70a3f275977767ef12bb4d523
Ole Trøan <otroan@employees.org>
2020-03-26 18:08:35 +0000

    acl: revert acl: api cleanup
    This reverts commit aad1ee149403994194cf37cef4530b042ba7df3a.
    
    Reason for revert: Verify failure. Doesn't build.
    
    Type: fix
    Change-Id: I91b1b26ac43edde4853e4561a0083d0b3a06efee
    Signed-off-by: Ole Troan <ot@cisco.com>

--- extras/vom/vom/acl_list_cmds.cpp
+++ extras/vom/vom/acl_list_cmds.cpp
@@ -24,10 +24,9 @@ namespace list_cmds {
 static void
 to_vpp(const l2_rule& rule, vapi_type_macip_acl_rule& payload)
 {
-  payload.is_permit = (vapi_enum_acl_action)rule.action().value();
-  rule.src_ip().to_vpp((uint8_t*)&payload.src_prefix.address.af,
-                       (uint8_t*)&payload.src_prefix.address.un,
-                       &payload.src_prefix.len);
+  payload.is_permit = rule.action().value();
+  rule.src_ip().to_vpp(&payload.is_ipv6, payload.src_ip_addr,
+                       &payload.src_ip_prefix_len);
   rule.mac().to_bytes(payload.src_mac, 6);
   rule.mac_mask().to_bytes(payload.src_mac_mask, 6);
 }
@@ -35,15 +34,13 @@ to_vpp(const l2_rule& rule, vapi_type_macip_acl_rule& payload)
 static void
 to_vpp(const l3_rule& rule, vapi_type_acl_rule& payload)
 {
-  payload.is_permit = (vapi_enum_acl_action)rule.action().value();
-  rule.src().to_vpp((uint8_t*)&payload.src_prefix.address.af,
-                    (uint8_t*)&payload.src_prefix.address.un,
-                    &payload.src_prefix.len);
-  rule.dst().to_vpp((uint8_t*)&payload.dst_prefix.address.af,
-                    (uint8_t*)&payload.dst_prefix.address.un,
-                    &payload.dst_prefix.len);
+  payload.is_permit = rule.action().value();
+  rule.src().to_vpp(&payload.is_ipv6, payload.src_ip_addr,
+                    &payload.src_ip_prefix_len);
+  rule.dst().to_vpp(&payload.is_ipv6, payload.dst_ip_addr,
+                    &payload.dst_ip_prefix_len);
 
-  payload.proto = (vapi_enum_ip_proto)rule.proto();
+  payload.proto = rule.proto();
   payload.srcport_or_icmptype_first = rule.srcport_or_icmptype_first();
   payload.srcport_or_icmptype_last = rule.srcport_or_icmptype_last();
   payload.dstport_or_icmpcode_first = rule.dstport_or_icmpcode_first();
@@ -64,8 +61,8 @@ l3_update_cmd::issue(connection& con)
   payload.acl_index = m_hw_item.data().value();
   payload.count = m_rules.size();
   memset(payload.tag, 0, sizeof(payload.tag));
-  memcpy(
-    payload.tag, m_key.c_str(), std::min(m_key.length(), sizeof(payload.tag)));
+  memcpy(payload.tag, m_key.c_str(),
+         std::min(m_key.length(), sizeof(payload.tag)));
 
   auto it = m_rules.cbegin();
 
@@ -131,8 +128,8 @@ l2_update_cmd::issue(connection& con)
   // payload.acl_index = m_hw_item.data().value();
   payload.count = m_rules.size();
   memset(payload.tag, 0, sizeof(payload.tag));
-  memcpy(
-    payload.tag, m_key.c_str(), std::min(m_key.length(), sizeof(payload.tag)));
+  memcpy(payload.tag, m_key.c_str(),
+         std::min(m_key.length(), sizeof(payload.tag)));
 
   auto it = m_rules.cbegin();
 
