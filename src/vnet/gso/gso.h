2a8c8b4a2d2600254ee339fa18ee1573efdb07ea
Mohsin Kazmi <sykazmi@cisco.com>
2020-03-30 21:16:06 +0200

    gso: add vxlan tunnel support
    Type: feature
    
    Change-Id: I85f6ec77187a4983c66c5e22fd39fbb2cef82902
    Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>

--- src/vnet/gso/gso.h
+++ src/vnet/gso/gso.h
@@ -16,22 +16,7 @@
 #ifndef included_gso_h
 #define included_gso_h
 
-#include <vnet/ethernet/ethernet.h>
-#include <vnet/ip/ip4_packet.h>
-#include <vnet/ip/ip6_packet.h>
-#include <vnet/udp/udp_packet.h>
 #include <vnet/vnet.h>
-
-typedef struct
-{
-  i16 l2_hdr_offset;
-  i16 l3_hdr_offset;
-  i16 l4_hdr_offset;
-  u16 l4_hdr_sz;
-  i16 outer_l2_hdr_offset;
-  i16 outer_l3_hdr_offset;
-  i16 outer_l4_hdr_offset;
-} gso_header_offset_t;
 
 typedef struct
 {
@@ -43,84 +28,6 @@ typedef struct
 extern gso_main_t gso_main;
 
 int vnet_sw_interface_gso_enable_disable (u32 sw_if_index, u8 enable);
-
-static_always_inline gso_header_offset_t
-vnet_gso_header_offset_parser (vlib_buffer_t * b0, int is_ip6)
-{
-  gso_header_offset_t gho = { 0 };
-  u8 l4_proto = 0;
-  u8 l4_hdr_sz = 0;
-
-  if (PREDICT_TRUE ((b0->flags & (VNET_BUFFER_F_L2_HDR_OFFSET_VALID |
-				  VNET_BUFFER_F_L3_HDR_OFFSET_VALID |
-				  VNET_BUFFER_F_L4_HDR_OFFSET_VALID)) ==
-		    (VNET_BUFFER_F_L2_HDR_OFFSET_VALID |
-		     VNET_BUFFER_F_L3_HDR_OFFSET_VALID |
-		     VNET_BUFFER_F_L4_HDR_OFFSET_VALID)))
-    {
-      gho.l2_hdr_offset = vnet_buffer (b0)->l2_hdr_offset;
-      gho.l3_hdr_offset = vnet_buffer (b0)->l3_hdr_offset;
-      gho.l4_hdr_offset = vnet_buffer (b0)->l4_hdr_offset;
-      gho.l4_hdr_sz = vnet_buffer2 (b0)->gso_l4_hdr_sz;
-      return gho;
-    }
-
-  ethernet_header_t *eh = (ethernet_header_t *) vlib_buffer_get_current (b0);
-  u16 ethertype = clib_net_to_host_u16 (eh->type);
-  u16 l2hdr_sz = sizeof (ethernet_header_t);
-
-  if (ethernet_frame_is_tagged (ethertype))
-    {
-      ethernet_vlan_header_t *vlan = (ethernet_vlan_header_t *) (eh + 1);
-
-      ethertype = clib_net_to_host_u16 (vlan->type);
-      l2hdr_sz += sizeof (*vlan);
-      if (ethertype == ETHERNET_TYPE_VLAN)
-	{
-	  vlan++;
-	  ethertype = clib_net_to_host_u16 (vlan->type);
-	  l2hdr_sz += sizeof (*vlan);
-	}
-    }
-
-  gho.l2_hdr_offset = b0->current_data;
-  gho.l3_hdr_offset = l2hdr_sz;
-
-  if (PREDICT_TRUE (is_ip6 == 0))
-    {
-      ip4_header_t *ip4 =
-	(ip4_header_t *) (vlib_buffer_get_current (b0) + l2hdr_sz);
-      gho.l4_hdr_offset = l2hdr_sz + ip4_header_bytes (ip4);
-      l4_proto = ip4->protocol;
-    }
-  else if (PREDICT_TRUE (is_ip6))
-    {
-      ip6_header_t *ip6 =
-	(ip6_header_t *) (vlib_buffer_get_current (b0) + l2hdr_sz);
-      /* FIXME IPv6 EH traversal */
-      gho.l4_hdr_offset = l2hdr_sz + sizeof (ip6_header_t);
-      l4_proto = ip6->protocol;
-    }
-  if (l4_proto == IP_PROTOCOL_TCP)
-    {
-      tcp_header_t *tcp = (tcp_header_t *) (vlib_buffer_get_current (b0) +
-					    gho.l4_hdr_offset);
-      l4_hdr_sz = tcp_header_bytes (tcp);
-    }
-  else if (l4_proto == IP_PROTOCOL_UDP)
-    {
-      udp_header_t *udp = (udp_header_t *) (vlib_buffer_get_current (b0) +
-					    gho.l4_hdr_offset);
-      l4_hdr_sz = sizeof (*udp);
-    }
-
-  if (b0->flags & (VNET_BUFFER_F_IS_IP4 | VNET_BUFFER_F_IS_IP6))
-    {
-      gho.l4_hdr_sz = l4_hdr_sz;
-    }
-
-  return gho;
-}
 
 #endif /* included_gso_h */
 
