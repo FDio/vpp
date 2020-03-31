7c0648018eece80fd8770921080714ee2d041e83
Neale Ranns <nranns@cisco.com>
2020-03-31 09:21:29 -0400

    fib: midchain adjacency optimisations
    Type: improvement
    
     - inline some common encap fixup functions into the midchain
    rewrite node so we don't incur the cost of the virtual function call
     - change the copy 'guess' from ethernet_header (which will never happen) to an ip4 header
     - add adj-midchain-tx to multiarch sources
     - cache the feature arc config index (to save the cache miss going to fetch it)
     - don't check if features are enabled when taking the arc (since we know they are)
    
    the last two changes will also benefit normal adjacencies taking the arc (i.e. for NAT, ACLs, etc)
    
    Change-Id: Ida0af56a704302cf2d7797ded5f118a781e8acb7
    Signed-off-by: Neale Ranns <nranns@cisco.com>

--- src/vnet/feature/feature.h
+++ src/vnet/feature/feature.h
@@ -278,6 +278,23 @@ vnet_feature_arc_start_with_data (u8 arc, u32 sw_if_index, u32 * next,
   return 0;
 }
 
+static_always_inline void *
+vnet_feature_arc_start_w_cfg_index (u8 arc,
+				    u32 sw_if_index,
+				    u32 * next,
+				    vlib_buffer_t * b, u32 cfg_index)
+{
+  vnet_feature_main_t *fm = &feature_main;
+  vnet_feature_config_main_t *cm;
+  cm = &fm->feature_config_mains[arc];
+
+  vnet_buffer (b)->feature_arc_index = arc;
+  b->current_config_index = cfg_index;
+
+  return vnet_get_config_data (&cm->config_main, &b->current_config_index,
+			       next, 0);
+}
+
 static_always_inline void
 vnet_feature_arc_start (u8 arc, u32 sw_if_index, u32 * next0,
 			vlib_buffer_t * b0)
