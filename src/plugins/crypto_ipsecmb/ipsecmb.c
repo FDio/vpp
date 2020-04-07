877ba7bb33e638fffef071a43ccfaedf986e820b
Fan Zhang <roy.fan.zhang@intel.com>
2020-04-07 12:55:43 +0100

    crypto: introduce async crypto infra
    Type: feature
    
    Signed-off-by: Damjan Marion <damarion@cisco.com>
    Signed-off-by: Filip Tehlar <ftehlar@cisco.com>
    Signed-off-by: Fan Zhang <roy.fan.zhang@intel.com>
    Signed-off-by: Piotr Bronowski <piotrx.bronowski@intel.com>
    Signed-off-by: Dariusz Kazimierski <dariuszx.kazimierski@intel.com>
    Signed-off-by: Piotr Kleski <piotrx.kleski@intel.com>
    Change-Id: I4c3fcccf55c36842b7b48aed260fef2802b5c54b

--- src/plugins/crypto_ipsecmb/ipsecmb.c
+++ src/plugins/crypto_ipsecmb/ipsecmb.c
@@ -459,6 +459,10 @@ crypto_ipsecmb_key_handler (vlib_main_t * vm, vnet_crypto_key_op_t kop,
       clib_mem_free_s (imbm->key_data[idx]);
     }
 
+  /** TODO: add linked alg support **/
+  if (key->type == VNET_CRYPTO_KEY_TYPE_LINK)
+    return;
+
   kd = imbm->key_data[idx] = clib_mem_alloc_aligned (ad->data_size,
 						     CLIB_CACHE_LINE_BYTES);
 
