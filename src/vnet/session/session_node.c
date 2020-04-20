0cd9ab589d79cee680450bd1f74acfd1ee6f7099
Florin Coras <fcoras@cisco.com>
2020-04-20 21:07:06 +0000

    session: multiple dgrams per dispatch
    Type: improvement
    
    Signed-off-by: Florin Coras <fcoras@cisco.com>
    Change-Id: Ifa4916ffeaa30039e366011109bbd2e9c91a1b0b

--- src/vnet/session/session_node.c
+++ src/vnet/session/session_node.c
@@ -690,6 +690,9 @@ session_tx_fill_buffer (vlib_main_t * vm, session_tx_context_t * ctx,
 	    {
 	      offset = hdr->data_length + SESSION_CONN_HDR_LEN;
 	      svm_fifo_dequeue_drop (f, offset);
+	      if (ctx->left_to_snd > n_bytes_read)
+		svm_fifo_peek (ctx->s->tx_fifo, 0, sizeof (ctx->hdr),
+			       (u8 *) & ctx->hdr);
 	    }
 	}
       else
@@ -758,7 +761,10 @@ session_tx_set_dequeue_params (vlib_main_t * vm, session_tx_context_t * ctx,
 			       u32 max_segs, u8 peek_data)
 {
   u32 n_bytes_per_buf, n_bytes_per_seg;
+
+  n_bytes_per_buf = vlib_buffer_get_default_data_size (vm);
   ctx->max_dequeue = svm_fifo_max_dequeue_cons (ctx->s->tx_fifo);
+
   if (peek_data)
     {
       /* Offset in rx fifo from where to peek data */
@@ -773,15 +779,46 @@ session_tx_set_dequeue_params (vlib_main_t * vm, session_tx_context_t * ctx,
     {
       if (ctx->transport_vft->transport_options.tx_type == TRANSPORT_TX_DGRAM)
 	{
+	  u32 len;
+
 	  if (ctx->max_dequeue <= sizeof (ctx->hdr))
 	    {
 	      ctx->max_len_to_snd = 0;
 	      return;
 	    }
+
 	  svm_fifo_peek (ctx->s->tx_fifo, 0, sizeof (ctx->hdr),
 			 (u8 *) & ctx->hdr);
 	  ASSERT (ctx->hdr.data_length > ctx->hdr.data_offset);
-	  ctx->max_dequeue = ctx->hdr.data_length - ctx->hdr.data_offset;
+	  len = ctx->hdr.data_length - ctx->hdr.data_offset;
+
+	  /* Process multiple dgrams if smaller than min (max buffer, mss)
+	   * and all of them have the same length */
+	  if (len < clib_min (n_bytes_per_buf, ctx->sp.snd_mss))
+	    {
+	      u32 first_dgram_len, dgram_len, offset, max_offset;
+	      session_dgram_hdr_t hdr;
+
+	      ctx->sp.snd_mss = clib_min (ctx->sp.snd_mss, len);
+	      offset = len + sizeof (session_dgram_hdr_t);
+	      first_dgram_len = len;
+	      max_offset = clib_min (ctx->max_dequeue, 16 << 10);
+
+	      while (offset < max_offset)
+		{
+		  svm_fifo_peek (ctx->s->tx_fifo, offset, sizeof (ctx->hdr),
+				 (u8 *) & hdr);
+		  ASSERT (hdr.data_length > hdr.data_offset);
+		  dgram_len = hdr.data_length - hdr.data_offset;
+		  if (len + dgram_len > ctx->max_dequeue
+		      || first_dgram_len != dgram_len)
+		    break;
+		  len += dgram_len;
+		  offset += sizeof (hdr) + hdr.data_length;
+		}
+	    }
+
+	  ctx->max_dequeue = len;
 	}
     }
   ASSERT (ctx->max_dequeue > 0);
@@ -809,7 +846,6 @@ session_tx_set_dequeue_params (vlib_main_t * vm, session_tx_context_t * ctx,
       ctx->max_len_to_snd = max_segs * ctx->sp.snd_mss;
     }
 
-  n_bytes_per_buf = vlib_buffer_get_default_data_size (vm);
   ASSERT (n_bytes_per_buf > TRANSPORT_MAX_HDRS_LEN);
   if (ctx->n_segs_per_evt > 1)
     {
