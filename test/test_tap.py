002ad88b6d9a24f2b1371f6b21225a40e431982b
Paul Vinciguerra <pvinci@vinciconsulting.com>
2020-04-27 01:12:48 -0400

    tap: implement sw_interface_tap_v2_dump filtering by sw_if_index
    Type: feature
    
    Change-Id: I6f607f383dc77a71e8712124f7613b38b4ac065a
    Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

--- test/test_tap.py
+++ test/test_tap.py
@@ -19,6 +19,20 @@ class TestTAP(VppTestCase):
         tap0.add_vpp_config()
         self.assertTrue(tap0.query_vpp_config())
 
+    def test_tap_dump(self):
+        """ Test api dump w/ and w/o sw_if_index filtering"""
+        MAX_INSTANCES = 10
+        tap_instances = []
+        for instance in range(MAX_INSTANCES):
+            i = VppTAPInterface(self, tap_id=instance)
+            i.add_vpp_config()
+            tap_instances.append(i)
+        details = self.vapi.sw_interface_tap_v2_dump()
+        self.assertEqual(MAX_INSTANCES, len(details))
+        details = self.vapi.sw_interface_tap_v2_dump(
+            tap_instances[5].sw_if_index)
+        self.assertEqual(1, len(details))
+
 
 if __name__ == '__main__':
     unittest.main(testRunner=VppTestRunner)
