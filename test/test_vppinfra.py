c608bae48d494eadd499d8d947470a5a8b6e605e
Dave Barach <dave@barachs.net>
2020-04-27 18:38:36 -0400

    vppinfra: improve test coverage
    Bonus corner-case bugfix in bitmap.h, found during the exercise.
    Issue dates from 2001 or thereabouts. Please review this specific
    change carefully.
    
    Type: improvement
    
    Signed-off-by: Dave Barach <dave@barachs.net>
    Change-Id: Iaa0b63e9dc571dfe3d992197ac49ba4d93403c61

---
+++ test/test_vppinfra.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from framework import VppTestCase, VppTestRunner, running_extended_tests
+from framework import running_gcov_tests
+
+
+class TestVppinfra(VppTestCase):
+    """ Vppinfra Unit Test Cases """
+    worker_config = "workers 1"
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestVppinfra, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        super(TestVppinfra, cls).tearDownClass()
+
+    def setUp(self):
+        super(TestVppinfra, self).setUp()
+
+    def tearDown(self):
+        super(TestVppinfra, self).tearDown()
+
+    def test_bitmap_unittest(self):
+        """ Bitmap Code Coverage Test """
+        cmds = ["test bitmap"]
+
+        for cmd in cmds:
+            r = self.vapi.cli_return_response(cmd)
+            if r.retval != 0:
+                if hasattr(r, 'reply'):
+                    self.logger.info(cmd + " FAIL reply " + r.reply)
+                else:
+                    self.logger.info(cmd + " FAIL retval " + str(r.retval))
+
+if __name__ == '__main__':
+    unittest.main(testRunner=VppTestRunner)
