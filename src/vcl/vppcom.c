517d83a68c09eb63ee9829f75db51de756776157
Florin Coras <fcoras@cisco.com>
2020-04-30 02:59:55 +0000

    vcl: disconnect both flavors of bapi transport on destroy
    Type: fix
    
    Signed-off-by: Florin Coras <fcoras@cisco.com>
    Change-Id: I6697296b45c5816a31535b0cf44b8e726292b8bb

--- src/vcl/vppcom.c
+++ src/vcl/vppcom.c
@@ -1223,7 +1223,7 @@ void
 vppcom_app_destroy (void)
 {
   struct dlmallinfo mi;
-  vcl_worker_t *wrk;
+  vcl_worker_t *wrk, *current_wrk;
   mspace heap;
 
   if (!pool_elts (vcm->workers))
@@ -1231,15 +1231,18 @@ vppcom_app_destroy (void)
 
   vcl_evt (VCL_EVT_DETACH, vcm);
 
-  vcl_send_app_detach (vcl_worker_get_current ());
+  current_wrk = vcl_worker_get_current ();
 
   /* *INDENT-OFF* */
   pool_foreach (wrk, vcm->workers, ({
-    if (pool_elts (vcm->workers) == 1)
-      vl_client_disconnect_from_vlib ();
-    vcl_worker_cleanup (wrk, 0 /* notify vpp */ );
+    if (current_wrk != wrk)
+      vcl_worker_cleanup (wrk, 0 /* notify vpp */ );
   }));
   /* *INDENT-ON* */
+
+  vcl_send_app_detach (current_wrk);
+  vppcom_disconnect_from_vpp ();
+  vcl_worker_cleanup (current_wrk, 0 /* notify vpp */ );
 
   vcl_elog_stop (vcm);
 
