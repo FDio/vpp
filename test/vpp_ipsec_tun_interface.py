49bfbda3657907824141afb55b2924a9349cbf2f
Paul Vinciguerra <pvinci@vinciconsulting.com>
2020-05-02 09:58:09 -0400

    tests: refactor VppInterface
      - Set all optional fields to None and move defaults to .api
      - Refactor query_vpp_config. Subclasses now only need to
        define their 'dump' method as a class property.
      - Implement missing dump by sw_if_index functionality.
      - Remove affected function definitions from vpp_papi_provider.
    
    Type: test
    
    Change-Id: Ibcd8d3023919b93dacc1dbead7bbcd5914dc8c93
    Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

--- test/vpp_ipsec_tun_interface.py
+++ test/vpp_ipsec_tun_interface.py
@@ -8,9 +8,9 @@ class VppIpsecTunInterface(VppTunnelInterface):
 
     def __init__(self, test, parent_if, local_spi,
                  remote_spi, crypto_alg, local_crypto_key, remote_crypto_key,
-                 integ_alg, local_integ_key, remote_integ_key, salt=0,
-                 udp_encap=False,
-                 is_ip6=False,
+                 integ_alg, local_integ_key, remote_integ_key, salt=None,
+                 udp_encap=None,
+                 is_ip6=None,
                  dst=None):
         super(VppIpsecTunInterface, self).__init__(test, parent_if)
         self.local_spi = local_spi
@@ -22,6 +22,11 @@ class VppIpsecTunInterface(VppTunnelInterface):
         self.local_integ_key = local_integ_key
         self.remote_integ_key = remote_integ_key
         self.salt = salt
+
+        # save for __repr__
+        self.is_ip6 = is_ip6
+        self.dst = dst
+
         if is_ip6:
             self.local_ip = self.parent_if.local_ip6
             self.remote_ip = self.parent_if.remote_ip6
@@ -54,3 +59,12 @@ class VppIpsecTunInterface(VppTunnelInterface):
 
     def object_id(self):
         return "ipsec-tun-if-%d" % self._sw_if_index
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self._test}, {self.parent_if}, " \
+               f"{self.local_spi}, {self.remote_spi}, {self.crypto_alg}, " \
+               f"{self.local_crypto_key}, {self.remote_crypto_key}," \
+               f"{self.integ_alg}, {self.local_integ_key}, " \
+               f"{self.remote_integ_key}, salt={repr(self.salt)}," \
+               f"udp_encap={repr(self.udp_encap)}, " \
+               f"is_ip6={repr(self.is_ip6)}, dst={repr(self.dst)})"
