0792bb451c68b9f039e88ab5fb4ce3f73988b781
Damjan Marion <damarion@cisco.com>
2020-05-25 19:31:42 +0200

    pci: improve vfio logging
    Type: improvement
    Change-Id: Ic49a43651b80b79fa278e29964da5cb2ead2a818
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- src/vlib/linux/vfio.h
+++ src/vlib/linux/vfio.h
@@ -49,6 +49,8 @@ clib_error_t *vfio_map_physmem_page (vlib_main_t * vm, void *addr);
 clib_error_t *linux_vfio_group_get_device_fd (vlib_pci_addr_t * addr,
 					      int *fd, int *is_noiommu);
 
+format_function_t format_vfio_region_info;
+
 
 #endif /* included_vlib_linux_vfio_h */
 
