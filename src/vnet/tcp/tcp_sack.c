1ca689f7ab30b1f4cb94298b604fabe1ea407b74
Florin Coras <fcoras@cisco.com>
2020-05-25 19:15:56 +0000

    tcp: fix sack block validation on wrap
    Type: fix
    
    Signed-off-by: Florin Coras <fcoras@cisco.com>
    Change-Id: Ia9813ae09d14111dc8edac0fa6ab082e13ab6e2e

--- src/vnet/tcp/tcp_sack.c
+++ src/vnet/tcp/tcp_sack.c
@@ -326,7 +326,9 @@ tcp_rcv_sacks (tcp_connection_t * tc, u32 ack)
     {
       if (seq_lt (blk->start, blk->end)
 	  && seq_gt (blk->start, tc->snd_una)
-	  && seq_gt (blk->start, ack) && seq_leq (blk->end, tc->snd_nxt))
+	  && seq_gt (blk->start, ack)
+	  && seq_lt (blk->start, tc->snd_nxt)
+	  && seq_leq (blk->end, tc->snd_nxt))
 	{
 	  blk++;
 	  continue;
