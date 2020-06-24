90ecd3a0c4f4c364d57e633e1bbc2f7894860ec1
Nathan Skrzypczak <nathan.skrzypczak@gmail.com>
2020-06-24 16:20:15 +0200

    ip: Fix mfib route cli
    Type: fix
    
    When trying to add a route to a tunnel interface
    (e.g. GRE) with an mfib6 route, we hit an assert
    in debug images as rpath->frp_proto is not set.
    
    Change-Id: Iac4148a5ef8661fe8240f59eb848e5d5e309360d
    Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

--- src/vnet/ip/lookup.c
+++ src/vnet/ip/lookup.c
@@ -700,6 +700,7 @@ vnet_ip_mroute_cmd (vlib_main_t * vm,
 			 unformat_ip4_address, &pfx.fp_grp_addr.ip4))
 	{
 	  pfx.fp_proto = FIB_PROTOCOL_IP4;
+	  payload_proto = DPO_PROTO_IP4;
 	  pfx.fp_len = 64;
 	}
       else if (unformat (line_input, "%U %U",
@@ -708,6 +709,7 @@ vnet_ip_mroute_cmd (vlib_main_t * vm,
 			 unformat_ip6_address, &pfx.fp_grp_addr.ip6))
 	{
 	  pfx.fp_proto = FIB_PROTOCOL_IP6;
+	  payload_proto = DPO_PROTO_IP6;
 	  pfx.fp_len = 256;
 	}
       else if (unformat (line_input, "%U/%d",
@@ -716,6 +718,7 @@ vnet_ip_mroute_cmd (vlib_main_t * vm,
 	{
 	  clib_memset (&pfx.fp_src_addr.ip4, 0, sizeof (pfx.fp_src_addr.ip4));
 	  pfx.fp_proto = FIB_PROTOCOL_IP4;
+	  payload_proto = DPO_PROTO_IP4;
 	}
       else if (unformat (line_input, "%U/%d",
 			 unformat_ip6_address,
@@ -723,12 +726,14 @@ vnet_ip_mroute_cmd (vlib_main_t * vm,
 	{
 	  clib_memset (&pfx.fp_src_addr.ip6, 0, sizeof (pfx.fp_src_addr.ip6));
 	  pfx.fp_proto = FIB_PROTOCOL_IP6;
+	  payload_proto = DPO_PROTO_IP6;
 	}
       else if (unformat (line_input, "%U",
 			 unformat_ip4_address, &pfx.fp_grp_addr.ip4))
 	{
 	  clib_memset (&pfx.fp_src_addr.ip4, 0, sizeof (pfx.fp_src_addr.ip4));
 	  pfx.fp_proto = FIB_PROTOCOL_IP4;
+	  payload_proto = DPO_PROTO_IP4;
 	  pfx.fp_len = 32;
 	}
       else if (unformat (line_input, "%U",
@@ -736,6 +741,7 @@ vnet_ip_mroute_cmd (vlib_main_t * vm,
 	{
 	  clib_memset (&pfx.fp_src_addr.ip6, 0, sizeof (pfx.fp_src_addr.ip6));
 	  pfx.fp_proto = FIB_PROTOCOL_IP6;
+	  payload_proto = DPO_PROTO_IP6;
 	  pfx.fp_len = 128;
 	}
       else if (unformat (line_input, "via local Forward"))
