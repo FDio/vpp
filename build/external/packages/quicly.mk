ff6bed426bb5aa02e27fa42dde5da177e1becfbc
MathiasRaoul <mathias.raoul@gmail.com>
2020-07-06 10:22:29 +0000

    quic : quicly v0.1.1 update
    Change-Id: If76ef31c43b430eea2f7674539b2112aee0f351e
    Signed-off-by: MathiasRaoul <mathias.raoul@gmail.com>

--- build/external/packages/quicly.mk
+++ build/external/packages/quicly.mk
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-quicly_version := 0.1.0-vpp
+quicly_version := 0.1.1-vpp
 quicly_tarball := quicly_$(quicly_version).tar.gz
 quicly_tarball_md5sum := 223f62c4dda7cbb1d907956cafcfd3f0
 quicly_tarball_strip_dirs := 1
