ac0326fc5ae7ac4a8126bbc2f496a92fcfe4755e
Dave Barach <dave@barachs.net>
2020-07-14 18:30:05 -0400

    adl: move allow/deny list function to plugin
    Provide binary API compatibility support for the "cop" APIs until vpp
    21.01.
    
    Change the deprecation date in map.api to vpp 21.01.
    
    Type: refactor
    Signed-off-by: Dave Barach <dave@barachs.net>
    Change-Id: I0e60d96de4ae9ae4448f134cf257934126f3b760

--- src/plugins/adl/node.c
+++ src/plugins/adl/node.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Cisco and/or its affiliates.
+ * Copyright (c) 2016,2020 Cisco and/or its affiliates.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at:
@@ -13,48 +13,49 @@
  * limitations under the License.
  */
 
-#include <vnet/cop/cop.h>
+#include <plugins/adl/adl.h>
+#include <vnet/pg/pg.h>
 
 typedef struct {
   u32 next_index;
   u32 sw_if_index;
-} cop_input_trace_t;
+} adl_input_trace_t;
 
 /* packet trace format function */
-static u8 * format_cop_input_trace (u8 * s, va_list * args)
+static u8 * format_adl_input_trace (u8 * s, va_list * args)
 {
   CLIB_UNUSED (vlib_main_t * vm) = va_arg (*args, vlib_main_t *);
   CLIB_UNUSED (vlib_node_t * node) = va_arg (*args, vlib_node_t *);
-  cop_input_trace_t * t = va_arg (*args, cop_input_trace_t *);
-  
-  s = format (s, "COP_INPUT: sw_if_index %d, next index %d",
+  adl_input_trace_t * t = va_arg (*args, adl_input_trace_t *);
+
+  s = format (s, "ADL_INPUT: sw_if_index %d, next index %d",
               t->sw_if_index, t->next_index);
   return s;
 }
 
-#define foreach_cop_input_error \
-_(PROCESSED, "COP input packets processed")
+#define foreach_adl_input_error \
+_(PROCESSED, "Allow/Deny packets processed")
 
 typedef enum {
-#define _(sym,str) COP_INPUT_ERROR_##sym,
-  foreach_cop_input_error
+#define _(sym,str) ADL_INPUT_ERROR_##sym,
+  foreach_adl_input_error
 #undef _
-  COP_INPUT_N_ERROR,
-} cop_input_error_t;
+  ADL_INPUT_N_ERROR,
+} adl_input_error_t;
 
-static char * cop_input_error_strings[] = {
+static char * adl_input_error_strings[] = {
 #define _(sym,string) string,
-  foreach_cop_input_error
+  foreach_adl_input_error
 #undef _
 };
 
-VLIB_NODE_FN (cop_input_node) (vlib_main_t * vm,
+VLIB_NODE_FN (adl_input_node) (vlib_main_t * vm,
 		  vlib_node_runtime_t * node,
 		  vlib_frame_t * frame)
 {
   u32 n_left_from, * from, * to_next;
-  cop_feature_type_t next_index;
-  cop_main_t *cm = &cop_main;
+  adl_feature_type_t next_index;
+  adl_main_t *am = &adl_main;
 
   from = vlib_frame_vector_args (frame);
   n_left_from = frame->n_vectors;
@@ -74,17 +75,17 @@ VLIB_NODE_FN (cop_input_node) (vlib_main_t * vm,
           u32 next0, next1;
           u32 sw_if_index0, sw_if_index1;
           ethernet_header_t * en0, * en1;
-          cop_config_main_t * ccm0, * ccm1;
+          adl_config_main_t * ccm0, * ccm1;
           u32 advance0, advance1;
           int proto0, proto1;
-          
+
 	  /* Prefetch next iteration. */
 	  {
 	    vlib_buffer_t * p2, * p3;
-            
+
 	    p2 = vlib_get_buffer (vm, from[2]);
 	    p3 = vlib_get_buffer (vm, from[3]);
-            
+
 	    vlib_prefetch_buffer_header (p2, LOAD);
 	    vlib_prefetch_buffer_header (p3, LOAD);
 
@@ -106,68 +107,68 @@ VLIB_NODE_FN (cop_input_node) (vlib_main_t * vm,
           en0 = vlib_buffer_get_current (b0);
           en1 = vlib_buffer_get_current (b1);
 
-          sw_if_index0 = vnet_buffer(b0)->sw_if_index[VLIB_RX];
-          sw_if_index1 = vnet_buffer(b1)->sw_if_index[VLIB_RX];
+          sw_if_index0 = adl_buffer(b0)->sw_if_index[VLIB_RX];
+          sw_if_index1 = adl_buffer(b1)->sw_if_index[VLIB_RX];
 
-          proto0 = VNET_COP_DEFAULT;
-          proto1 = VNET_COP_DEFAULT;
+          proto0 = VNET_ADL_DEFAULT;
+          proto1 = VNET_ADL_DEFAULT;
           advance0 = 0;
           advance1 = 0;
 
           if (en0->type == clib_host_to_net_u16(ETHERNET_TYPE_IP4))
             {
-              proto0 = VNET_COP_IP4;
+              proto0 = VNET_ADL_IP4;
               advance0 = sizeof(ethernet_header_t);
             }
           else if (en0->type == clib_host_to_net_u16(ETHERNET_TYPE_IP6))
             {
-              proto0 = VNET_COP_IP6;
+              proto0 = VNET_ADL_IP6;
               advance0 = sizeof(ethernet_header_t);
             }
 
           if (en1->type == clib_host_to_net_u16(ETHERNET_TYPE_IP4))
             {
-              proto1 = VNET_COP_IP4;
+              proto1 = VNET_ADL_IP4;
               advance1 = sizeof(ethernet_header_t);
             }
           else if (en1->type == clib_host_to_net_u16(ETHERNET_TYPE_IP6))
             {
-              proto1 = VNET_COP_IP6;
+              proto1 = VNET_ADL_IP6;
               advance1 = sizeof(ethernet_header_t);
             }
 
-	  ccm0 = cm->cop_config_mains + proto0;
-	  ccm1 = cm->cop_config_mains + proto1;
-          vnet_buffer(b0)->cop.current_config_index = 
+	  ccm0 = am->adl_config_mains + proto0;
+	  ccm1 = am->adl_config_mains + proto1;
+          adl_buffer(b0)->adl.current_config_index =
             ccm0->config_index_by_sw_if_index [sw_if_index0];
 
-          vnet_buffer(b1)->cop.current_config_index = 
+          adl_buffer(b1)->adl.current_config_index =
             ccm1->config_index_by_sw_if_index [sw_if_index1];
 
           vlib_buffer_advance (b0, advance0);
           vlib_buffer_advance (b1, advance1);
 
           vnet_get_config_data (&ccm0->config_main,
-                                &vnet_buffer(b0)->cop.current_config_index,
+                                &adl_buffer(b0)->adl.current_config_index,
                                 &next0, 0 /* bytes of config data */);
 
           vnet_get_config_data (&ccm1->config_main,
-                                &vnet_buffer(b1)->cop.current_config_index,
+                                &adl_buffer(b1)->adl.current_config_index,
                                 &next1, 0 /* bytes of config data */);
 
-          if (PREDICT_FALSE((node->flags & VLIB_NODE_FLAG_TRACE) 
-                            && (b0->flags & VLIB_BUFFER_IS_TRACED))) 
+          if (PREDICT_FALSE((node->flags & VLIB_NODE_FLAG_TRACE)
+                            && (b0->flags & VLIB_BUFFER_IS_TRACED)))
             {
-              cop_input_trace_t *t = 
+              adl_input_trace_t *t =
                  vlib_add_trace (vm, node, b0, sizeof (*t));
               t->sw_if_index = sw_if_index0;
               t->next_index = next0;
             }
 
-          if (PREDICT_FALSE((node->flags & VLIB_NODE_FLAG_TRACE) 
-                            && (b1->flags & VLIB_BUFFER_IS_TRACED))) 
+          if (PREDICT_FALSE((node->flags & VLIB_NODE_FLAG_TRACE)
+                            && (b1->flags & VLIB_BUFFER_IS_TRACED)))
             {
-              cop_input_trace_t *t = 
+              adl_input_trace_t *t =
                  vlib_add_trace (vm, node, b1, sizeof (*t));
               t->sw_if_index = sw_if_index1;
               t->next_index = next1;
@@ -185,7 +186,7 @@ VLIB_NODE_FN (cop_input_node) (vlib_main_t * vm,
           u32 next0;
           u32 sw_if_index0;
           ethernet_header_t *en0;
-          cop_config_main_t *ccm0;
+          adl_config_main_t *ccm0;
           u32 advance0;
           int proto0;
 
@@ -199,7 +200,7 @@ VLIB_NODE_FN (cop_input_node) (vlib_main_t * vm,
 
 	  b0 = vlib_get_buffer (vm, bi0);
 
-          /* 
+          /*
            * Direct from the driver, we should be at offset 0
            * aka at &b0->data[0]
            */
@@ -207,41 +208,41 @@ VLIB_NODE_FN (cop_input_node) (vlib_main_t * vm,
 
           en0 = vlib_buffer_get_current (b0);
 
-          sw_if_index0 = vnet_buffer(b0)->sw_if_index[VLIB_RX];
+          sw_if_index0 = adl_buffer(b0)->sw_if_index[VLIB_RX];
 
-          proto0 = VNET_COP_DEFAULT;
+          proto0 = VNET_ADL_DEFAULT;
           advance0 = 0;
 
           if (en0->type == clib_host_to_net_u16(ETHERNET_TYPE_IP4))
             {
-              proto0 = VNET_COP_IP4;
+              proto0 = VNET_ADL_IP4;
               advance0 = sizeof(ethernet_header_t);
             }
           else if (en0->type == clib_host_to_net_u16(ETHERNET_TYPE_IP6))
             {
-              proto0 = VNET_COP_IP6;
+              proto0 = VNET_ADL_IP6;
               advance0 = sizeof(ethernet_header_t);
             }
 
-	  ccm0 = cm->cop_config_mains + proto0;
-          vnet_buffer(b0)->cop.current_config_index = 
+	  ccm0 = am->adl_config_mains + proto0;
+          adl_buffer(b0)->adl.current_config_index =
             ccm0->config_index_by_sw_if_index [sw_if_index0];
 
           vlib_buffer_advance (b0, advance0);
 
           vnet_get_config_data (&ccm0->config_main,
-                                &vnet_buffer(b0)->cop.current_config_index,
+                                &adl_buffer(b0)->adl.current_config_index,
                                 &next0, 0 /* bytes of config data */);
 
-          if (PREDICT_FALSE((node->flags & VLIB_NODE_FLAG_TRACE) 
-                            && (b0->flags & VLIB_BUFFER_IS_TRACED))) 
+          if (PREDICT_FALSE((node->flags & VLIB_NODE_FLAG_TRACE)
+                            && (b0->flags & VLIB_BUFFER_IS_TRACED)))
             {
-              cop_input_trace_t *t = 
+              adl_input_trace_t *t =
                  vlib_add_trace (vm, node, b0, sizeof (*t));
               t->sw_if_index = sw_if_index0;
               t->next_index = next0;
             }
-            
+
           /* verify speculative enqueue, maybe switch current next frame */
 	  vlib_validate_buffer_enqueue_x1 (vm, node, next_index,
 					   to_next, n_left_to_next,
@@ -250,36 +251,37 @@ VLIB_NODE_FN (cop_input_node) (vlib_main_t * vm,
 
       vlib_put_next_frame (vm, node, next_index, n_left_to_next);
     }
-  vlib_node_increment_counter (vm, cop_input_node.index, 
-                               COP_INPUT_ERROR_PROCESSED, frame->n_vectors);
+  vlib_node_increment_counter (vm, adl_input_node.index,
+                               ADL_INPUT_ERROR_PROCESSED, frame->n_vectors);
   return frame->n_vectors;
 }
 
-VLIB_REGISTER_NODE (cop_input_node) = {
-  .name = "cop-input",
+VLIB_REGISTER_NODE (adl_input_node) = {
+  .name = "adl-input",
   .vector_size = sizeof (u32),
-  .format_trace = format_cop_input_trace,
+  .format_trace = format_adl_input_trace,
+  .unformat_buffer = unformat_ethernet_header,
   .type = VLIB_NODE_TYPE_INTERNAL,
-  
-  .n_errors = ARRAY_LEN(cop_input_error_strings),
-  .error_strings = cop_input_error_strings,
 
-  .n_next_nodes = COP_RX_N_FEATURES,
+  .n_errors = ARRAY_LEN(adl_input_error_strings),
+  .error_strings = adl_input_error_strings,
+
+  .n_next_nodes = ADL_RX_N_FEATURES,
 
   /* edit / add dispositions here */
   .next_nodes = {
-    [IP4_RX_COP_WHITELIST] = "ip4-cop-whitelist",
-    [IP6_RX_COP_WHITELIST] = "ip6-cop-whitelist",
-    [DEFAULT_RX_COP_WHITELIST] = "default-cop-whitelist",
-    [IP4_RX_COP_INPUT] = "ip4-input",
-    [IP6_RX_COP_INPUT] = "ip6-input",
-    [DEFAULT_RX_COP_INPUT] = "ethernet-input",
-    [RX_COP_DROP] = "error-drop",
+    [IP4_RX_ADL_ALLOWLIST] = "ip4-adl-allowlist",
+    [IP6_RX_ADL_ALLOWLIST] = "ip6-adl-allowlist",
+    [DEFAULT_RX_ADL_ALLOWLIST] = "default-adl-allowlist",
+    [IP4_RX_ADL_INPUT] = "ip4-input",
+    [IP6_RX_ADL_INPUT] = "ip6-input",
+    [DEFAULT_RX_ADL_INPUT] = "ethernet-input",
+    [RX_ADL_DROP] = "error-drop",
   },
 };
 
-#define foreach_cop_stub                        \
-_(default-cop-whitelist, default_cop_whitelist)
+#define foreach_adl_stub                        \
+_(default-adl-allowlist, default_adl_allowlist)
 
 #define _(n,f)                                  \
                                                 \
@@ -304,10 +306,4 @@ VLIB_REGISTER_NODE (f##_input_node) = {         \
   .n_next_nodes = 0,                            \
 };
 
-foreach_cop_stub;
-
-
-
-
-
-
+foreach_adl_stub;
