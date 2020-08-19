535b38343d417e239022024475450178d819318d
Andrew Yourtchenko <ayourtch@gmail.com>
2020-08-19 12:39:45 +0000

    WIP: capo: Calico Policies plugin
    Type: feature
    
    Change-Id: I49d9c2ee985eb0d4a55ca672da9916dc4afa89ef
    Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>
    Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

---
+++ src/plugins/capo/capo.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2020 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef included_capo_h
+#define included_capo_h
+
+#include <vnet/ip/ip.h>
+#include <vnet/ip/ip_types_api.h>
+#include <capo/bihash_4_12.h>
+
+#include <capo/capo.api_enum.h>
+#include <capo/capo.api_types.h>
+
+#define CAPO_INVALID_INDEX ((u32)~0)
+
+typedef struct
+{
+  u16 start;
+  u16 end;
+} capo_port_range_t;
+
+typedef struct
+{
+  u32 *policies;
+  u32 pass_id;
+} capo_interface_config;
+
+typedef struct
+{
+  clib_bihash_4_12_t *if_config;	/* sw_if_index -> capo_interface_config */
+
+  /* API message ID base */
+  u16 msg_id_base;
+
+} capo_main_t;
+
+
+extern capo_main_t capo_main;
+
+#endif
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
