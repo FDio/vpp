d55faa4ecad25a321852659b37b974dfe208f675
jan_cavojsky <Jan.Cavojsky@pantheon.tech>
2020-08-20 15:41:24 +0200

    ikev2: prevent crash after no IP address
    Type: fix
    Ticket: VPP-1900
    
    After calling command "ikev2 initiate sa-init xy" with unset ip address
    of interface assigned to profile xy will be avoid crash of vpp.
    In this case is writen error message into console of vpp, but after
    setting ip address will be vpp works correctly again.
    Behavior of vpp in this use case is the same like when command
    "ikev2 initiate sa-init xy" is call with set ip address of interface
    assigned to profile xy.
    Request of connectivity is also retried.
    
    Signed-off-by: jan_cavojsky <Jan.Cavojsky@pantheon.tech>
    Change-Id: Ia1919c349e64b3a0a4198365e075e177e3ba3de5

--- src/plugins/ikev2/ikev2.c
+++ src/plugins/ikev2/ikev2.c
@@ -3659,6 +3659,7 @@ ikev2_initiate_sa_init (vlib_main_t * vm, u8 * name)
   clib_error_t *r;
   ip4_main_t *im = &ip4_main;
   ikev2_main_t *km = &ikev2_main;
+  int valid_ip = 0;
 
   p = ikev2_profile_index_by_name (name);
 
@@ -3681,14 +3682,23 @@ ikev2_initiate_sa_init (vlib_main_t * vm, u8 * name)
     u32 bi0 = 0;
     ip_lookup_main_t *lm = &im->lookup_main;
     u32 if_add_index0;
+    ip4_address_t no_ip = {
+      .as_u32 = 0,
+    };
+    ip4_address_t *if_ip = &no_ip;
     int len = sizeof (ike_header_t);
 
     /* Get own iface IP */
     if_add_index0 =
       lm->if_address_pool_index_by_sw_if_index[p->responder.sw_if_index];
-    ip_interface_address_t *if_add =
-      pool_elt_at_index (lm->if_address_pool, if_add_index0);
-    ip4_address_t *if_ip = ip_interface_address_get_address (lm, if_add);
+
+    if (!pool_is_free_index (lm->if_address_pool, if_add_index0))
+      {
+	valid_ip = 1;
+	ip_interface_address_t *if_add =
+	  pool_elt_at_index (lm->if_address_pool, if_add_index0);
+	if_ip = ip_interface_address_get_address (lm, if_add);
+      }
 
     bi0 = ikev2_get_new_ike_header_buff (vm, &ike0);
     if (!bi0)
@@ -3805,13 +3815,25 @@ ikev2_initiate_sa_init (vlib_main_t * vm, u8 * name)
     clib_memcpy_fast (sa0, &sa, sizeof (*sa0));
     hash_set (km->sa_by_ispi, sa0->ispi, sa0 - km->sais);
 
-    ikev2_send_ike (vm, if_ip, &p->responder.ip4, bi0, len,
-		    IKEV2_PORT, sa.dst_port, sa.sw_if_index);
+    if (valid_ip)
+      {
+	ikev2_send_ike (vm, if_ip, &p->responder.ip4, bi0, len,
+			IKEV2_PORT, sa.dst_port, sa.sw_if_index);
 
-    ikev2_elog_exchange ("ispi %lx rspi %lx IKEV2_EXCHANGE_SA_INIT sent to "
-			 "%d.%d.%d.%d", clib_host_to_net_u64 (sa0->ispi), 0,
-			 p->responder.ip4.as_u32);
+	ikev2_elog_exchange
+	  ("ispi %lx rspi %lx IKEV2_EXCHANGE_SA_INIT sent to " "%d.%d.%d.%d",
+	   clib_host_to_net_u64 (sa0->ispi), 0, p->responder.ip4.as_u32);
+      }
   }
+
+  if (!valid_ip)
+    {
+      r =
+	clib_error_return (0, "interface  %U hasn't any ip address",
+			   format_vnet_sw_if_index_name, vnet_get_main (),
+			   p->responder.sw_if_index);
+      return r;
+    }
 
   return 0;
 }
@@ -4260,6 +4282,26 @@ ikev2_process_pending_sa_init (ikev2_main_t * km)
     ikev2_send_ike (km->vlib_main, &sa->iaddr, &sa->raddr, bi0,
         vec_len (sa->last_sa_init_req_packet_data),
         ikev2_get_port (sa), IKEV2_PORT, sa->sw_if_index);
+
+    if(sa->iaddr.as_u32 || !sa->is_initiator)
+        continue;
+
+    ikev2_profile_t *p = pool_elt_at_index (km->profiles, sa->profile_index);
+    if (p)
+      {
+        ip_lookup_main_t *lm = &ip4_main.lookup_main;
+        u32 if_add_index0 =
+            lm->if_address_pool_index_by_sw_if_index[p->responder.sw_if_index];
+
+        if (!pool_is_free_index (lm->if_address_pool, if_add_index0))
+          {
+            ip_interface_address_t *if_add =
+                pool_elt_at_index (lm->if_address_pool, if_add_index0);
+            ip4_address_t *if_ip =
+                ip_interface_address_get_address (lm, if_add);
+            sa->iaddr.as_u32 = if_ip->as_u32;
+          }
+      }
   }));
   /* *INDENT-ON* */
 }
