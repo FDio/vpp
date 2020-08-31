0a34c96f7835958296649cc0ba2383c70f593bfe
Artem Glazychev <artem.glazychev@xored.com>
2020-08-31 17:12:30 +0700

    wireguard: initial implementation of wireguard protocol
    Type: feature
    
    The main information about plugin you can see in README.md
    
    Change-Id: I85eb0bfc033ccfb2045696398d8a108b1c64b8d9
    Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>
    Signed-off-by: Damjan Marion <damarion@cisco.com>
    Signed-off-by: Jim Thompson <jim@netgate.com>

---
+++ src/plugins/wireguard/wireguard_peer.c
@@ -0,0 +1,247 @@
+/*
+ * Copyright (c) 2020 Doc.ai and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vnet/fib/ip4_fib.h>
+#include <wireguard/wireguard_peer.h>
+#include <wireguard/wireguard_itf.h>
+#include <wireguard/wireguard_messages.h>
+#include <wireguard/wireguard_key.h>
+#include <wireguard/wireguard_send.h>
+#include <wireguard/wireguard.h>
+
+wg_peer_t *
+wg_peer_get (index_t peeri)
+{
+  return (pool_elt_at_index (wg_main.peers, peeri));
+}
+
+void
+wg_peer_init (vlib_main_t * vm, wg_peer_t * peer)
+{
+  wg_timers_init (peer, vlib_time_now (vm));
+  wg_peer_clear (vm, peer);
+}
+
+void
+wg_peer_clear (vlib_main_t * vm, wg_peer_t * peer)
+{
+  wg_timers_stop (peer);
+  noise_remote_clear (vm, &peer->remote);
+  peer->last_sent_handshake = vlib_time_now (vm) - (REKEY_TIMEOUT + 1);
+
+  clib_memset (&peer->cookie_maker, 0, sizeof (peer->cookie_maker));
+  peer->ip4_address.as_u32 = 0;
+
+  peer->persistent_keepalive_interval = 0;
+  peer->port = 0;
+  peer->timer_handshake_attempts = 0;
+  peer->timer_need_another_keepalive = false;
+  peer->is_dead = true;
+  vec_free (peer->allowed_ips);
+}
+
+static int
+wg_peer_fill (vlib_main_t * vm, wg_peer_t * peer,
+	      u32 table_id,
+	      ip4_address_t ip4, u16 port,
+	      u16 persistent_keepalive_interval,
+	      const fib_prefix_t * allowed_ips, u32 wg_sw_if_index)
+{
+  peer->sw_if_index = ~0;
+  peer->ip4_address.as_u32 = ip4.as_u32;
+  peer->port = port;
+  peer->table_id = table_id;
+  peer->persistent_keepalive_interval = persistent_keepalive_interval;
+  peer->tun_sw_if_index = wg_sw_if_index;
+  peer->last_sent_handshake = vlib_time_now (vm) - (REKEY_TIMEOUT + 1);
+
+  wg_itf_t *wgi = wg_itf_find_by_sw_if_index (wg_sw_if_index);
+
+  if (NULL == wgi)
+    return (VNET_API_ERROR_INVALID_INTERFACE);
+
+  /* *INDENT-OFF* */
+  fib_prefix_t pfx = {
+    .fp_addr = {
+      .ip4 = peer->ip4_address,
+    }
+    ,
+    .fp_len = 32,
+    .fp_proto = FIB_PROTOCOL_IP4,
+  };
+  /* *INDENT-ON* */
+
+  // FIXME
+  fib_node_index_t fib_entry_index =
+    fib_table_lookup (fib_table_find (FIB_PROTOCOL_IP4, peer->table_id),
+		      &pfx);
+
+  if (fib_entry_index != FIB_NODE_INDEX_INVALID)
+    {
+      peer->sw_if_index = fib_entry_get_resolving_interface (fib_entry_index);
+    }
+  peer->is_dead = false;
+
+  u32 ii;
+
+  vec_validate (peer->allowed_ips, vec_len (allowed_ips) - 1);
+
+  vec_foreach_index (ii, allowed_ips)
+  {
+    peer->allowed_ips[ii].prefix = allowed_ips[ii];
+
+    // ADD ROUTE
+  }
+
+  return (0);
+}
+
+int
+wg_peer_add (u32 tun_sw_if_index,
+	     const u8 public_key_64[NOISE_KEY_LEN_BASE64],
+	     u32 table_id,
+	     ip4_address_t endpoint,
+	     const fib_prefix_t * allowed_ips,
+	     u16 port, u16 persistent_keepalive, u32 * peer_index)
+{
+  wg_itf_t *wg_itf;
+  wg_peer_t *peer;
+  int rv;
+
+  vlib_main_t *vm = vlib_get_main ();
+
+  if (tun_sw_if_index == ~0)
+    return (VNET_API_ERROR_INVALID_SW_IF_INDEX);
+
+  wg_itf = wg_itf_find_by_sw_if_index (tun_sw_if_index);
+  if (!wg_itf)
+    return (VNET_API_ERROR_INVALID_SW_IF_INDEX);
+
+  u8 public_key[NOISE_PUBLIC_KEY_LEN];
+  if (!key_from_base64 (public_key_64, NOISE_KEY_LEN_BASE64, public_key))
+    return (VNET_API_ERROR_KEY_LENGTH);
+
+  /* *INDENT-OFF* */
+  pool_foreach (peer, wg_main.peers,
+  ({
+    if (!memcmp (peer->remote.r_public, public_key, NOISE_PUBLIC_KEY_LEN))
+    {
+      return (VNET_API_ERROR_ENTRY_ALREADY_EXISTS);
+    }
+  }));
+  /* *INDENT-ON* */
+
+  if (pool_elts (wg_main.peers) > MAX_PEERS)
+    return (VNET_API_ERROR_LIMIT_EXCEEDED);
+
+  pool_get (wg_main.peers, peer);
+
+  wg_peer_init (vm, peer);
+
+  rv = wg_peer_fill (vm, peer, table_id, endpoint, (u16) port,
+		     persistent_keepalive, allowed_ips, tun_sw_if_index);
+
+  if (rv)
+    {
+      wg_peer_clear (vm, peer);
+      pool_put (wg_main.peers, peer);
+      return (rv);
+    }
+
+  noise_remote_init (&peer->remote, peer - wg_main.peers, public_key,
+		     &wg_itf->local);
+  cookie_maker_init (&peer->cookie_maker, public_key);
+
+  vnet_feature_enable_disable ("ip4-output", "wg-output-tun",
+			       tun_sw_if_index, 1, 0, 0);
+
+  if (peer->persistent_keepalive_interval != 0)
+    {
+      wg_send_keepalive (vm, peer);
+    }
+
+  *peer_index = peer - wg_main.peers;
+
+  return (0);
+}
+
+int
+wg_peer_remove (index_t peeri)
+{
+  wg_main_t *wmp = &wg_main;
+  wg_peer_t *peer = NULL;
+
+  if (pool_is_free_index (wmp->peers, peeri))
+    return VNET_API_ERROR_NO_SUCH_ENTRY;
+
+  peer = pool_elt_at_index (wmp->peers, peeri);
+
+  vnet_feature_enable_disable ("ip4-output", "wg-output-tun",
+			       peer->tun_sw_if_index, 0, 0, 0);
+  wg_peer_clear (wmp->vlib_main, peer);
+  pool_put (wmp->peers, peer);
+
+  return (0);
+}
+
+void
+wg_peer_walk (wg_peer_walk_cb_t fn, void *data)
+{
+  index_t peeri;
+
+  /* *INDENT-OFF* */
+  pool_foreach_index(peeri, wg_main.peers,
+  {
+    if (WALK_STOP == fn(peeri, data))
+      break;
+  });
+  /* *INDENT-ON* */
+}
+
+u8 *
+format_wg_peer (u8 * s, va_list * va)
+{
+  index_t peeri = va_arg (*va, index_t);
+  u8 key_64[NOISE_KEY_LEN_BASE64];
+  wg_peer_allowed_ip_t *allowed_ip;
+  wg_peer_t *peer;
+
+  peer = wg_peer_get (peeri);
+
+  key_to_base64 (peer->remote.r_public, NOISE_PUBLIC_KEY_LEN, key_64);
+  s = format (s, "[%d] key:%=45s endpoint:[%U, %u] %U keep-alive:%d",
+	      peeri,
+	      key_64,
+	      format_ip4_address, &peer->ip4_address,
+	      peer->port,
+	      format_vnet_sw_if_index_name, vnet_get_main (),
+	      peer->tun_sw_if_index, peer->persistent_keepalive_interval);
+
+  s = format (s, "\n  allowed-ips:");
+  vec_foreach (allowed_ip, peer->allowed_ips)
+  {
+    s = format (s, " %U", format_fib_prefix, &allowed_ip->prefix);
+  }
+
+  return s;
+}
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
---
+++ src/plugins/wireguard/wireguard_send.c
@@ -0,0 +1,282 @@
+/*
+ * Copyright (c) 2020 Doc.ai and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vnet/vnet.h>
+#include <vnet/fib/ip6_fib.h>
+#include <vnet/fib/ip4_fib.h>
+#include <vnet/fib/fib_entry.h>
+#include <vnet/ip/ip6_link.h>
+#include <vnet/pg/pg.h>
+#include <vnet/udp/udp.h>
+#include <vppinfra/error.h>
+#include <wireguard/wireguard.h>
+#include <wireguard/wireguard_send.h>
+
+// This file has pieces of code for a future IP6 implementation.
+
+static int
+ip46_set_src_address (u32 sw_if_index, vlib_buffer_t * b0, int is_ip6)
+{
+  int res;
+  if (is_ip6)
+    {
+      ip6_header_t *ip6 = vlib_buffer_get_current (b0);
+      res = ip6_src_address_for_packet (sw_if_index,
+					&ip6->dst_address, &ip6->src_address);
+    }
+  else
+    {
+      ip4_main_t *im = &ip4_main;
+      ip4_header_t *ip4 = vlib_buffer_get_current (b0);
+      res = ip4_src_address_for_packet (&im->lookup_main,
+					sw_if_index, &ip4->src_address);
+      /* IP4 and IP6 paths have the inverse logic. Harmonize. */
+      res = !res;
+    }
+  return res;
+}
+
+static int
+ip46_fill_l3_header (ip46_address_t * ip46_dst, u32 sw_if_index,
+		     vlib_buffer_t * b0, u32 len, u16 port_src, u16 port_dst,
+		     int is_ip6)
+{
+//TODO IP6
+  if (!is_ip6)
+    {
+
+      ip4_header_t *ip4;
+      udp_header_t *udp0;
+
+      vlib_buffer_advance (b0, -sizeof (udp_header_t));
+      udp0 = vlib_buffer_get_current (b0);
+      vlib_buffer_advance (b0, -sizeof (ip4_header_t));
+      ip4 = vlib_buffer_get_current (b0);
+
+      /* Fill in ip4 header fields */
+      ip4->ip_version_and_header_length = 0x45;
+      ip4->tos = 0;
+      ip4->fragment_id = 0;
+      ip4->flags_and_fragment_offset = 0;
+      ip4->ttl = 0xff;
+      ip4->protocol = IP_PROTOCOL_UDP;
+
+      ip46_set_src_address (sw_if_index, b0, false);
+      ip4->dst_address.as_u32 = ip46_dst->ip4.as_u32;
+
+      udp0->src_port = clib_host_to_net_u16 (port_src);
+      udp0->dst_port = clib_host_to_net_u16 (port_dst);
+
+      udp0->length = clib_host_to_net_u16 (len + sizeof (udp_header_t));
+      udp0->checksum = 0;
+      b0->current_length =
+	len + sizeof (ip4_header_t) + sizeof (udp_header_t);
+      ip4->length = clib_host_to_net_u16 (b0->current_length);
+      ip4->checksum = ip4_header_checksum (ip4);
+    }
+  return (sizeof (ip4_header_t) + sizeof (udp_header_t));
+}
+
+static int
+ip46_enqueue_packet (vlib_main_t * vm, u32 bi0, int is_ip6)
+{
+  vlib_frame_t *f = 0;
+  u32 lookup_node_index =
+    is_ip6 ? ip6_lookup_node.index : ip4_lookup_node.index;
+
+  f = vlib_get_frame_to_node (vm, lookup_node_index);
+  /* f can not be NULL here - frame allocation failure causes panic */
+
+  u32 *to_next = vlib_frame_vector_args (f);
+  f->n_vectors = 1;
+  to_next[0] = bi0;
+
+  vlib_put_frame_to_node (vm, lookup_node_index, f);
+
+  return f->n_vectors;
+}
+
+static bool
+wg_create_buffer (vlib_main_t * vm, const u8 * packet, u32 packet_len,
+		  u32 * bi, wg_peer_t * peer, u16 src_port)
+{
+  u32 n_buf0 = 0;
+  vlib_buffer_t *b0;
+  ip46_address_t ip46;
+
+  if (~0 == peer->sw_if_index)
+    return false;
+
+  n_buf0 = vlib_buffer_alloc (vm, bi, 1);
+  if (!n_buf0)
+    return false;
+
+  b0 = vlib_get_buffer (vm, *bi);
+
+  u8 *payload = vlib_buffer_get_current (b0);
+  clib_memcpy (payload, packet, packet_len);
+
+  u16 port_src = src_port;
+  u16 port_dst = peer->port;
+
+  ip46_address_set_ip4 (&ip46, &peer->ip4_address);
+  ip46_fill_l3_header (&ip46, peer->sw_if_index, b0, packet_len, port_src,
+		       port_dst, false);
+  return true;
+}
+
+bool
+wg_send_handshake (vlib_main_t * vm, wg_peer_t * peer, bool is_retry)
+{
+  wg_main_t *wmp = &wg_main;
+  message_handshake_initiation_t packet;
+
+  if (!is_retry)
+    peer->timer_handshake_attempts = 0;
+
+  if (!wg_birthdate_has_expired (peer->last_sent_handshake,
+				 REKEY_TIMEOUT) || peer->is_dead)
+    {
+      return true;
+    }
+  if (noise_create_initiation (wmp->vlib_main,
+			       &peer->remote,
+			       &packet.sender_index,
+			       packet.unencrypted_ephemeral,
+			       packet.encrypted_static,
+			       packet.encrypted_timestamp))
+    {
+      f64 now = vlib_time_now (vm);
+      packet.header.type = MESSAGE_HANDSHAKE_INITIATION;
+      cookie_maker_mac (&peer->cookie_maker, &packet.macs, &packet,
+			sizeof (packet));
+      wg_timers_any_authenticated_packet_traversal (peer);
+      wg_timers_any_authenticated_packet_sent (peer);
+      peer->last_sent_handshake = now;
+      wg_timers_handshake_initiated (peer);
+    }
+  else
+    return false;
+  u32 bi0 = 0;
+  if (!wg_create_buffer
+      (vm, (u8 *) & packet, sizeof (packet), &bi0, peer, wmp->port_src))
+    return false;
+  ip46_enqueue_packet (vm, bi0, false);
+
+  return true;
+}
+
+bool
+wg_send_keepalive (vlib_main_t * vm, wg_peer_t * peer)
+{
+  wg_main_t *wmp = &wg_main;
+  u32 size_of_packet = message_data_len (0);
+  message_data_t *packet = clib_mem_alloc (size_of_packet);
+  u32 bi0 = 0;
+  bool ret = true;
+  enum noise_state_crypt state;
+
+  if (!peer->remote.r_current)
+    {
+      wg_send_handshake (vm, peer, false);
+      goto out;
+    }
+
+  state =
+    noise_remote_encrypt (wmp->vlib_main,
+			  &peer->remote,
+			  &packet->receiver_index,
+			  &packet->counter, NULL, 0, packet->encrypted_data);
+  switch (state)
+    {
+    case SC_OK:
+      break;
+    case SC_KEEP_KEY_FRESH:
+      wg_send_handshake (vm, peer, false);
+      break;
+    case SC_FAILED:
+      ret = false;
+      goto out;
+    default:
+      break;
+    }
+  packet->header.type = MESSAGE_DATA;
+
+  if (!wg_create_buffer
+      (vm, (u8 *) packet, size_of_packet, &bi0, peer, wmp->port_src))
+    {
+      ret = false;
+      goto out;
+    }
+
+  ip46_enqueue_packet (vm, bi0, false);
+  wg_timers_any_authenticated_packet_traversal (peer);
+  wg_timers_any_authenticated_packet_sent (peer);
+
+out:
+  clib_mem_free (packet);
+  return ret;
+}
+
+bool
+wg_send_handshake_response (vlib_main_t * vm, wg_peer_t * peer)
+{
+  wg_main_t *wmp = &wg_main;
+  message_handshake_response_t packet;
+
+  peer->last_sent_handshake = vlib_time_now (vm);
+
+  if (noise_create_response (wmp->vlib_main,
+			     &peer->remote,
+			     &packet.sender_index,
+			     &packet.receiver_index,
+			     packet.unencrypted_ephemeral,
+			     packet.encrypted_nothing))
+    {
+      f64 now = vlib_time_now (vm);
+      packet.header.type = MESSAGE_HANDSHAKE_RESPONSE;
+      cookie_maker_mac (&peer->cookie_maker, &packet.macs, &packet,
+			sizeof (packet));
+
+      if (noise_remote_begin_session (wmp->vlib_main, &peer->remote))
+	{
+	  wg_timers_session_derived (peer);
+	  wg_timers_any_authenticated_packet_traversal (peer);
+	  wg_timers_any_authenticated_packet_sent (peer);
+	  peer->last_sent_handshake = now;
+
+	  u32 bi0 = 0;
+	  if (!wg_create_buffer
+	      (vm, (u8 *) & packet, sizeof (packet), &bi0, peer,
+	       wmp->port_src))
+	    return false;
+
+	  ip46_enqueue_packet (vm, bi0, false);
+	}
+      else
+	return false;
+    }
+  else
+    return false;
+  return true;
+}
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
