FDio/vpp

commit e5f0050c7a5d411f96af6401797529d58825e2af

ip: optimize ip4_header_checksum
Type: improvement
Change-Id: I67bacb90a3dd8a9bd7beb4975ad0fe344675b65f
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/vnet/ip/ip4_packet.h
@@ -83,52 +83,6 @@ typedef struct
   ip4_address_t addr, mask;
 } ip4_address_and_mask_t;
 
-/* If address is a valid netmask, return length of mask. */
-always_inline uword
-ip4_address_netmask_length (const ip4_address_t * a)
-{
-  uword result = 0;
-  uword i;
-  for (i = 0; i < ARRAY_LEN (a->as_u8); i++)
-    {
-      switch (a->as_u8[i])
-	{
-	case 0xff:
-	  result += 8;
-	  break;
-	case 0xfe:
-	  result += 7;
-	  goto done;
-	case 0xfc:
-	  result += 6;
-	  goto done;
-	case 0xf8:
-	  result += 5;
-	  goto done;
-	case 0xf0:
-	  result += 4;
-	  goto done;
-	case 0xe0:
-	  result += 3;
-	  goto done;
-	case 0xc0:
-	  result += 2;
-	  goto done;
-	case 0x80:
-	  result += 1;
-	  goto done;
-	case 0x00:
-	  result += 0;
-	  goto done;
-	default:
-	  /* Not a valid netmask mask. */
-	  return ~0;
-	}
-    }
-done:
-  return result;
-}
-
 typedef union
 {
   struct
@@ -246,22 +200,61 @@ ip4_next_header (ip4_header_t * i)
 always_inline u16
 ip4_header_checksum (ip4_header_t * i)
 {
-  u16 save, csum;
-  ip_csum_t sum;
+  u16 *iphdr = (u16 *) i;
+  u32 sum = 0;
+  int option_len = (i->ip_version_and_header_length & 0xf) - 5;
 
-  save = i->checksum;
-  i->checksum = 0;
-  sum = ip_incremental_checksum (0, i, ip4_header_bytes (i));
-  csum = ~ip_csum_fold (sum);
+  sum += clib_net_to_host_u16 (iphdr[0]);
+  sum += clib_net_to_host_u16 (iphdr[1]);
+  sum += clib_net_to_host_u16 (iphdr[2]);
+  sum += clib_net_to_host_u16 (iphdr[3]);
+  sum += clib_net_to_host_u16 (iphdr[4]);
 
-  i->checksum = save;
+  sum += clib_net_to_host_u16 (iphdr[6]);
+  sum += clib_net_to_host_u16 (iphdr[7]);
+  sum += clib_net_to_host_u16 (iphdr[8]);
+  sum += clib_net_to_host_u16 (iphdr[9]);
 
-  /* Make checksum agree for special case where either
-     0 or 0xffff would give same 1s complement sum. */
-  if (csum == 0 && save == 0xffff)
-    csum = save;
+  if (PREDICT_FALSE (option_len > 0))
+    switch (option_len)
+      {
+      case 10:
+	sum += clib_net_to_host_u16 (iphdr[28]);
+	sum += clib_net_to_host_u16 (iphdr[29]);
+      case 9:
+	sum += clib_net_to_host_u16 (iphdr[26]);
+	sum += clib_net_to_host_u16 (iphdr[27]);
+      case 8:
+	sum += clib_net_to_host_u16 (iphdr[24]);
+	sum += clib_net_to_host_u16 (iphdr[25]);
+      case 7:
+	sum += clib_net_to_host_u16 (iphdr[22]);
+	sum += clib_net_to_host_u16 (iphdr[23]);
+      case 6:
+	sum += clib_net_to_host_u16 (iphdr[20]);
+	sum += clib_net_to_host_u16 (iphdr[21]);
+      case 5:
+	sum += clib_net_to_host_u16 (iphdr[18]);
+	sum += clib_net_to_host_u16 (iphdr[19]);
+      case 4:
+	sum += clib_net_to_host_u16 (iphdr[16]);
+	sum += clib_net_to_host_u16 (iphdr[17]);
+      case 3:
+	sum += clib_net_to_host_u16 (iphdr[14]);
+	sum += clib_net_to_host_u16 (iphdr[15]);
+      case 2:
+	sum += clib_net_to_host_u16 (iphdr[12]);
+	sum += clib_net_to_host_u16 (iphdr[13]);
+      case 1:
+	sum += clib_net_to_host_u16 (iphdr[10]);
+	sum += clib_net_to_host_u16 (iphdr[11]);
+      default:
+	break;
+      }
 
-  return csum;
+  sum = ((u16) sum) + (sum >> 16);
+  sum = ((u16) sum) + (sum >> 16);
+  return clib_host_to_net_u16 (~((u16) sum));
 }
 
 always_inline void

commit 7a60919d680d5e0eac2ad81c918063c99f883db6

ip: fix ip zero checksum verification
When checking for a valid IP header checksum, we must take care of the
special case 'zero' checksum, which can be represented by 2 values:
0x0000 and 0xffff. We must consider them equals.
This used to be taken care of in ip4_header_checksum(), but it is better
to do that in ip4_header_checksum_is_valid().

Type: fix
Fixes: e5f0050c7a5d411f96af6401797529d58825e2af

Change-Id: Iacc6b60645a834287b085aecb9e3fdb4554cf0cf
Signed-off-by: Benoît Ganne <bganne@cisco.com>

src/plugins/flowprobe/node.c
@@ -598,7 +598,7 @@ flowprobe_export_send (vlib_main_t * vm, vlib_buffer_t * b0,
 	udp->checksum = 0xffff;
     }
 
-  ASSERT (ip->checksum == ip4_header_checksum (ip));
+  ASSERT (ip4_header_checksum_is_valid (ip));
 
   /* Find or allocate a frame */
   f = fm->context[which].frames_per_worker[my_cpu_number];

src/plugins/ioam/udp-ping/udp_ping_export.c
@@ -152,7 +152,7 @@ udp_ping_send_flows (flow_report_main_t * frm, flow_report_t * fr,
 		  if (udp->checksum == 0)
 		    udp->checksum = 0xffff;
 
-		  ASSERT (ip->checksum == ip4_header_checksum (ip));
+		  ASSERT (ip4_header_checksum_is_valid (ip));
 
 		  to_next[0] = bi0;
 		  f->n_vectors++;
@@ -203,7 +203,7 @@ udp_ping_send_flows (flow_report_main_t * frm, flow_report_t * fr,
       if (udp->checksum == 0)
 	udp->checksum = 0xffff;
 
-      ASSERT (ip->checksum == ip4_header_checksum (ip));
+      ASSERT (ip4_header_checksum_is_valid (ip));
 
       to_next[0] = bi0;
       f->n_vectors++;

src/plugins/map/ip4_map.c
@@ -97,7 +97,7 @@ ip4_map_decrement_ttl (ip4_header_t * ip, u8 * error)
   *error = ttl <= 0 ? IP4_ERROR_TIME_EXPIRED : *error;
 
   /* Verify checksum. */
-  ASSERT (ip->checksum == ip4_header_checksum (ip));
+  ASSERT (ip4_header_checksum_is_valid (ip));
 }
 
 static u32

src/plugins/nat/nat_ipfix_logging.c
@@ -567,7 +567,7 @@ snat_ipfix_send (u32 thread_index, flow_report_main_t * frm,
 	udp->checksum = 0xffff;
     }
 
-  ASSERT (ip->checksum == ip4_header_checksum (ip));
+  ASSERT (ip4_header_checksum_is_valid (ip));
 
   vlib_put_frame_to_node (vm, ip4_lookup_node.index, f);
 }

src/plugins/ping/ping.c
@@ -474,8 +474,8 @@ ip4_icmp_echo_request (vlib_main_t * vm,
 	  ip0->checksum = ip_csum_fold (sum0);
 	  ip1->checksum = ip_csum_fold (sum1);
 
-	  ASSERT (ip0->checksum == ip4_header_checksum (ip0));
-	  ASSERT (ip1->checksum == ip4_header_checksum (ip1));
+	  ASSERT (ip4_header_checksum_is_valid (ip0));
+	  ASSERT (ip4_header_checksum_is_valid (ip1));
 
 	  p0->flags |= VNET_BUFFER_F_LOCALLY_ORIGINATED;
 	  p1->flags |= VNET_BUFFER_F_LOCALLY_ORIGINATED;
@@ -531,7 +531,7 @@ ip4_icmp_echo_request (vlib_main_t * vm,
 
 	  ip0->checksum = ip_csum_fold (sum0);
 
-	  ASSERT (ip0->checksum == ip4_header_checksum (ip0));
+	  ASSERT (ip4_header_checksum_is_valid (ip0));
 
 	  p0->flags |= VNET_BUFFER_F_LOCALLY_ORIGINATED;
 	}

src/vnet/ip/ip4_format.c
@@ -151,7 +151,7 @@ format_ip4_header (u8 * s, va_list * args)
   /* Check and report invalid checksums. */
   {
     u16 c = ip4_header_checksum (ip);
-    if (c != ip->checksum)
+    if (!ip4_header_checksum_is_valid__ (ip, c))
       s = format (s, " (should be 0x%04x)", clib_net_to_host_u16 (c));
   }
 

src/vnet/ip/ip4_forward.c
@@ -2063,7 +2063,7 @@ ip4_ttl_inc (vlib_buffer_t * b, ip4_header_t * ip)
   ttl += 1;
   ip->ttl = ttl;
 
-  ASSERT (ip->checksum == ip4_header_checksum (ip));
+  ASSERT (ip4_header_checksum_is_valid (ip));
 }
 
 /* Decrement TTL & update checksum.
@@ -2104,7 +2104,7 @@ ip4_ttl_and_checksum_check (vlib_buffer_t * b, ip4_header_t * ip, u16 * next,
     }
 
   /* Verify checksum. */
-  ASSERT ((ip->checksum == ip4_header_checksum (ip)) ||
+  ASSERT (ip4_header_checksum_is_valid (ip) ||
 	  (b->flags & VNET_BUFFER_F_OFFLOAD_IP_CKSUM));
 }
 

src/vnet/ip/ip4_packet.h
@@ -198,7 +198,7 @@ ip4_next_header (ip4_header_t * i)
 }
 
 always_inline u16
-ip4_header_checksum (ip4_header_t * i)
+ip4_header_checksum (const ip4_header_t * i)
 {
   int option_len = (i->ip_version_and_header_length & 0xf) - 5;
   uword sum = 0;
@@ -361,10 +361,22 @@ ip4_header_get_df (const ip4_header_t * ip4)
 	      clib_host_to_net_u16 (IP4_HEADER_FLAG_DONT_FRAGMENT)));
 }
 
-static inline uword
-ip4_header_checksum_is_valid (ip4_header_t * i)
+static_always_inline int
+ip4_header_checksum_is_valid__ (const ip4_header_t * i, const u16 csum)
 {
-  return i->checksum == ip4_header_checksum (i);
+  /*
+   * 1's complement notation has two representations for the number zero:
+   * normal zero (0x0000) and negative zero (0xffff). They should be treating
+   * as equal.
+   */
+  return (csum == i->checksum) | (0 == csum && 0xffff == i->checksum);
+}
+
+static_always_inline int
+ip4_header_checksum_is_valid (const ip4_header_t * i)
+{
+  const u16 csum = ip4_header_checksum (i);
+  return ip4_header_checksum_is_valid__ (i, csum);
 }
 
 #define ip4_partial_header_checksum_x1(ip0,sum0)			\

src/vnet/ip/ip4_pg.c
@@ -90,8 +90,8 @@ compute_length_and_or_checksum (vlib_main_t * vm,
 	  ip0->checksum = ~ip_csum_fold (sum0);
 	  ip1->checksum = ~ip_csum_fold (sum1);
 
-	  ASSERT (ip0->checksum == ip4_header_checksum (ip0));
-	  ASSERT (ip1->checksum == ip4_header_checksum (ip1));
+	  ASSERT (ip4_header_checksum_is_valid (ip0));
+	  ASSERT (ip4_header_checksum_is_valid (ip1));
 	}
     }
 
@@ -123,7 +123,7 @@ compute_length_and_or_checksum (vlib_main_t * vm,
 	  ip4_partial_header_checksum_x1 (ip0, sum0);
 	  ip0->checksum = ~ip_csum_fold (sum0);
 
-	  ASSERT (ip0->checksum == ip4_header_checksum (ip0));
+	  ASSERT (ip4_header_checksum_is_valid (ip0));
 	}
     }
 }

src/vnet/ipfix-export/flow_report_classify.c
@@ -321,7 +321,7 @@ ipfix_classify_send_flows (flow_report_main_t * frm,
 			udp->checksum = 0xffff;
 		    }
 
-		  ASSERT (ip->checksum == ip4_header_checksum (ip));
+		  ASSERT (ip4_header_checksum_is_valid (ip));
 
 		  to_next[0] = bi0;
 		  f->n_vectors++;
@@ -376,7 +376,7 @@ flush:
 	    udp->checksum = 0xffff;
 	}
 
-      ASSERT (ip->checksum == ip4_header_checksum (ip));
+      ASSERT (ip4_header_checksum_is_valid (ip));
 
       to_next[0] = bi0;
       f->n_vectors++;