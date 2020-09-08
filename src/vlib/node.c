FDio/vpp

commit 4b2946a884c1ffbce022c8121c6889ff39f242e1

vlib: vlib_get_node_by_name fails via API
Type: fix

Calling vlib_get_node_by_name via the VPE api
doesn't work due to hash weirdness. Haven't
gotten around the real cause of this. But this
fixes it.

Change-Id: I89f95dba2bcd9573b8f1f435e063e9dd57f9ca93
Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

src/vlib/node.c
@@ -47,8 +47,7 @@ vlib_get_node_by_name (vlib_main_t * vm, u8 * name)
   vlib_node_main_t *nm = &vm->node_main;
   uword *p;
   u8 *key = name;
-  if (!clib_mem_is_heap_object (vec_header (key, 0)))
-    key = format (0, "%s", key);
+  key = format (0, "%s", key);
   p = hash_get (nm->node_by_name, key);
   if (key != name)
     vec_free (key);

commit 81bb12667114f2e086bfe799ae3b9052384820ea

vlib: fix vlib_get_node_by_name()
Type: fix
Fixes: 4b2946a884c1ffbce022c8121c6889ff39f242e1

Change-Id: If95a9252990c7bb50bd9db4a3cec0e8a8b578c20
Signed-off-by: Benoît Ganne <bganne@cisco.com>

src/vlib/node.c
@@ -47,7 +47,17 @@ vlib_get_node_by_name (vlib_main_t * vm, u8 * name)
   vlib_node_main_t *nm = &vm->node_main;
   uword *p;
   u8 *key = name;
-  key = format (0, "%s", key);
+  /*
+   * We store node names as non-NULL terminated vectors in nm->node_by_name
+   * This creates interesting situations as we must be ready to accept:
+   *  - non-NULL terminated vectors
+   *  - NULL-terminated vectors
+   *  - NULL-terminated C strings
+   * In the last 2 cases we must ignore the terminating NULL
+   */
+  if (!clib_mem_is_vec (name)
+      || (vec_len (name) > 0 && name[vec_len (name) - 1] == 0))
+    key = format (0, "%s", name);
   p = hash_get (nm->node_by_name, key);
   if (key != name)
     vec_free (key);