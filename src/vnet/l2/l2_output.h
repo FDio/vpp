b9b8f95ae62684b7584a42a545c3ed43d015d55a
Neale Ranns <nranns@cisco.com>
2020-09-14 07:41:48 +0000

    misc: Delete GBP and its dependents.
    Type: improvement
    
    no longer in use.
    
    Signed-off-by: Neale Ranns <nranns@cisco.com>
    Change-Id: I2f30a4f04fd9a8635ce2d259b5fd5b0c85cee8c3

--- src/vnet/l2/l2_output.h
+++ src/vnet/l2/l2_output.h
@@ -81,9 +81,6 @@ extern vlib_node_registration_t l2output_node;
 #define foreach_l2output_feat \
  _(OUTPUT,            "interface-output")           \
  _(SPAN,              "span-l2-output")             \
- _(GBP_POLICY_LPM,    "gbp-policy-lpm")            \
- _(GBP_POLICY_PORT,   "gbp-policy-port")            \
- _(GBP_POLICY_MAC,    "gbp-policy-mac")             \
  _(CFM,               "feature-bitmap-drop")        \
  _(QOS,               "feature-bitmap-drop")        \
  _(ACL,               "l2-output-acl")              \
