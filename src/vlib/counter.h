90e5cf898e47bc6fa15a9659a8744478b655ac06
Sergey Matov <sergey.matov@travelping.com>
2020-09-15 13:12:21 +0400

    vlib: add decrement counter method
    Currently there is no way to decrease simple counter. To extend
    counters API new method allows to decrement counter by given value.
    This might be useful in implementation of statistics stored in stats
    segment.
    
    Change-Id: I7c08c62bffa6a2d50e9e2cf884f7b2d48538f34b
    Signed-off-by: Sergey Matov <sergey.matov@travelping.com>

--- src/vlib/counter.h
+++ src/vlib/counter.h
@@ -84,6 +84,18 @@ vlib_increment_simple_counter (vlib_simple_counter_main_t * cm,
   my_counters[index] += increment;
 }
 
+always_inline void
+vlib_decrement_simple_counter (vlib_simple_counter_main_t * cm,
+                               u32 thread_index, u32 index, u64 decrement)
+
+{
+  counter_t *my_counters;
+
+  my_counters = cm->counters[thread_index];
+  ASSERT (my_counters[index] >= decrement)
+  my_counters[index] -= decrement;
+}
+
 /** Set a simple counter
     @param cm - (vlib_simple_counter_main_t *) simple counter main pointer
     @param thread_index - (u32) the current cpu index
