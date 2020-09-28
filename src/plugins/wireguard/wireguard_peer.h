a880b276d9998fa06c92355c4144fe3ca574c915
Artem Glazychev <artem.glazychev@xored.com>
2020-09-28 19:54:35 +0700

    wireguard: fix license headers
    Type: fix
    
    Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>
    Change-Id: If5cc8894e0d1785dd04ab1e460e017580c9b41ce

--- src/plugins/wireguard/wireguard_peer.h
+++ src/plugins/wireguard/wireguard_peer.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020 Doc.ai and/or its affiliates.
+ * Copyright (c) 2020 Cisco and/or its affiliates.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at:
