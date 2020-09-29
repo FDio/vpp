0e5cbf949135ca240d320cc94efe404c148e1691
Florin Coras <fcoras@cisco.com>
2020-09-28 23:40:28 -0700

    svm: use new clib mem apis for segments
    Type: improvement
    
    Signed-off-by: Florin Coras <fcoras@cisco.com>
    Change-Id: I7debdcd06c7728abfb5cf817a1ccc37de495472c

--- src/vnet/session/segment_manager.c
+++ src/vnet/session/segment_manager.c
@@ -142,7 +142,9 @@ segment_manager_add_segment (segment_manager_t * sm, uword segment_size)
 
   fs->ssvm.ssvm_size = segment_size;
   fs->ssvm.name = seg_name;
-  fs->ssvm.requested_va = baseva;
+  /* clib mem allocator wants a page before the requested va */
+  fs->ssvm.requested_va = baseva + page_size;
+  clib_warning ("mem alloc va %x + 1 page %x", baseva, fs->ssvm.requested_va);
 
   if ((rv = ssvm_master_init (&fs->ssvm, props->segment_type)))
     {
@@ -192,7 +194,8 @@ segment_manager_del_segment (segment_manager_t * sm, fifo_segment_t * fs)
 
   if (ssvm_type (&fs->ssvm) != SSVM_SEGMENT_PRIVATE)
     {
-      clib_valloc_free (&smm->va_allocator, fs->ssvm.requested_va);
+      clib_valloc_free (&smm->va_allocator,
+			fs->ssvm.requested_va - clib_mem_get_page_size ());
 
       if (!segment_manager_app_detached (sm))
 	{
