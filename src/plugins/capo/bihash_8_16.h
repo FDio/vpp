7c1d41e5ef31ef5db6edf3c28d79c7e707b49aef
Aloys Augustin <aloaugus@cisco.com>
2020-09-29 13:20:41 +0200

    capo: WIP: policies matching
    Change-Id: I8d761670633903fb9a80cf61028352ddf5f481b7
    Signed-off-by: Aloys Augustin <aloaugus@cisco.com>
    Signed-off-by: MathiasRaoul <mathias.raoul@gmail.com>

--- src/plugins/capo/bihash_8_16.h
+++ src/plugins/capo/bihash_8_16.h
@@ -20,14 +20,14 @@
 #undef BIHASH_LAZY_INSTANTIATE
 #undef BIHASH_BUCKET_PREFETCH_CACHE_LINES
 
-#define BIHASH_TYPE _4_12
-#define BIHASH_KVP_PER_PAGE 7
+#define BIHASH_TYPE _8_16
+#define BIHASH_KVP_PER_PAGE 4
 #define BIHASH_KVP_AT_BUCKET_LEVEL 1
-#define BIHASH_LAZY_INSTANTIATE 1
+#define BIHASH_LAZY_INSTANTIATE 0
 #define BIHASH_BUCKET_PREFETCH_CACHE_LINES 2
 
-#ifndef __included_bihash_4_12_h__
-#define __included_bihash_4_12_h__
+#ifndef __included_bihash_8_16_h__
+#define __included_bihash_8_16_h__
 
 #include <vppinfra/heap.h>
 #include <vppinfra/format.h>
@@ -35,61 +35,58 @@
 #include <vppinfra/xxhash.h>
 #include <vppinfra/crc32.h>
 
-/** 4 octet key, 16 octet key value pair */
+/** 8 octet key, 24 octet key value pair */
 typedef struct
 {
-  u32 key;			/**< the key */
-  u32 value[3];			/**< the value */
-} clib_bihash_kv_4_12_t;
+  u64 key;			/**< the key */
+  u64 value[2];			/**< the value */
+} clib_bihash_kv_8_16_t;
 
-/** Decide if a clib_bihash_kv_4_12_t instance is free
+/** Decide if a clib_bihash_kv_8_16_t instance is free
     @param v- pointer to the (key,value) pair
 */
 static inline int
-clib_bihash_is_free_4_12 (clib_bihash_kv_4_12_t * v)
+clib_bihash_is_free_8_16 (clib_bihash_kv_8_16_t * v)
 {
-  if (v->key == ~0U && v->value[0] == ~0U && v->value[1] == ~0U &&
-      v->value[2] == ~0U)
+  if (v->key == ~0ULL && v->value[0] == ~0ULL && v->value[1] == ~0ULL)
     return 1;
   return 0;
 }
 
-/** Hash a clib_bihash_kv_4_12_t instance
+/** Hash a clib_bihash_kv_8_16_t instance
     @param v - pointer to the (key,value) pair, hash the key (only)
 */
 static inline u64
-clib_bihash_hash_4_12 (clib_bihash_kv_4_12_t * v)
+clib_bihash_hash_8_16 (clib_bihash_kv_8_16_t * v)
 {
   /* Note: to torture-test linear scan, make this fn return a constant */
 #ifdef clib_crc32c_uses_intrinsics
-  return clib_crc32c ((u8 *) & v->key, 4);
+  return clib_crc32c ((u8 *) & v->key, 8);
 #else
   return clib_xxhash (v->key);
 #endif
 }
 
-/** Format a clib_bihash_kv_4_12_t instance
+/** Format a clib_bihash_kv_8_16_t instance
     @param s - u8 * vector under construction
     @param args (vararg) - the (key,value) pair to format
     @return s - the u8 * vector under construction
 */
 static inline u8 *
-format_bihash_kvp_4_12 (u8 * s, va_list * args)
+format_bihash_kvp_8_16 (u8 * s, va_list * args)
 {
-  clib_bihash_kv_4_12_t *v = va_arg (*args, clib_bihash_kv_4_12_t *);
+  clib_bihash_kv_8_16_t *v = va_arg (*args, clib_bihash_kv_8_16_t *);
 
-  s =
-    format (s, "key %lu value %lu %lu %lu", v->key, v->value[0], v->value[1],
-	    v->value[2]);
+  s = format (s, "key %lu value %lu %lu", v->key, v->value[0], v->value[1]);
   return s;
 }
 
-/** Compare two clib_bihash_kv_4_12_t instances
+/** Compare two clib_bihash_kv_8_16_t instances
     @param a - first key
     @param b - second key
 */
 static inline int
-clib_bihash_key_compare_4_12 (u32 a, u32 b)
+clib_bihash_key_compare_8_16 (u64 a, u64 b)
 {
   return a == b;
 }
@@ -97,7 +94,7 @@ clib_bihash_key_compare_4_12 (u32 a, u32 b)
 #undef __included_bihash_template_h__
 #include <vppinfra/bihash_template.h>
 
-#endif /* __included_bihash_4_12_h__ */
+#endif /* __included_bihash_8_16_h__ */
 
 /*
  * fd.io coding-style-patch-verification: ON
