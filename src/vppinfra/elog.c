FDio/vpp

commit ab1a50cf7407e31097b550226c2b6954d6371bea

misc: add xxx_not_inline
vlib_main_t *vlib_get_main_not_inline(void)
vlib_thread_main_t *vlib_get_thread_main_not_inline(void)
elog_main_t *vlib_get_elog_main_not_inline()

Type: refactor
Signed-off-by: Dave Barach <dave@barachs.net>
Change-Id: I6de306d567283ad28ef34c9be0cf27452aecbf6c

src/vppinfra/elog.c
@@ -614,8 +614,8 @@ elog_string (elog_main_t * em, char *fmt, ...)
 elog_event_t *
 elog_get_events (elog_main_t * em)
 {
-  if (!em->events)
-    em->events = elog_peek_events (em);
+  vec_reset_length (em->events);
+  em->events = elog_peek_events (em);
   return em->events;
 }
 

src/vlib/main.c
@@ -2510,6 +2510,18 @@ VLIB_CLI_COMMAND (pcap_dispatch_trace_command, static) = {
 };
 /* *INDENT-ON* */
 
+vlib_main_t *
+vlib_get_main_not_inline (void)
+{
+  return vlib_get_main ();
+}
+
+elog_main_t *
+vlib_get_elog_main_not_inline ()
+{
+  return &vlib_global_main.elog_main;
+}
+
 /*
  * fd.io coding-style-patch-verification: ON
  *

src/vlib/main.h
@@ -489,6 +489,8 @@ typedef struct
 } vlib_pcap_dispatch_trace_args_t;
 
 int vlib_pcap_dispatch_trace_configure (vlib_pcap_dispatch_trace_args_t *);
+vlib_main_t *vlib_get_main_not_inline (void);
+elog_main_t *vlib_get_elog_main_not_inline ();
 
 #endif /* included_vlib_main_h */
 

src/vlib/threads.c
@@ -2010,6 +2010,12 @@ VLIB_CLI_COMMAND (f_command, static) =
 };
 /* *INDENT-ON* */
 
+vlib_thread_main_t *
+vlib_get_thread_main_not_inline (void)
+{
+  return vlib_get_thread_main ();
+}
+
 /*
  * fd.io coding-style-patch-verification: ON
  *

src/vlib/threads.h
@@ -632,7 +632,7 @@ vlib_process_signal_event_mt_helper (vlib_process_signal_event_mt_args_t *
 				     args);
 void vlib_rpc_call_main_thread (void *function, u8 * args, u32 size);
 void vlib_get_thread_core_numa (vlib_worker_thread_t * w, unsigned cpu_id);
-
+vlib_thread_main_t *vlib_get_thread_main_not_inline (void);
 
 #endif /* included_vlib_threads_h */
 

commit 1046b1a631fd4153bca09b8924e34206ab3cf0ee

vppinfra: memory leak be gone
Type: fix
Fixes: ab1a50cf7407e31097b550226c2b6954d6371bea / gerrit 29272

Signed-off-by: Dave Barach <dave@barachs.net>
Change-Id: Ic8146a66b54bb9ac6223cf984d4566554b73276d

src/vppinfra/elog.c
@@ -614,7 +614,7 @@ elog_string (elog_main_t * em, char *fmt, ...)
 elog_event_t *
 elog_get_events (elog_main_t * em)
 {
-  vec_reset_length (em->events);
+  vec_free (em->events);
   em->events = elog_peek_events (em);
   return em->events;
 }