0a0cc86aa12666c5f7ff387caadfa533259cf5be
Vladimir Ratnikov <vratnikov@netgate.com>
2020-11-11 08:00:48 -0500

    dpdk: allow configure individual VMBUS devices
    now startup.conf supports confuguration for VMBUS
    devices as for PCI devices for whitelisting/blacklisting
    
    dpdk { dev fa5a6e7a-cf3a-4b98-9569-addb479b84bc }
     with sub-configuration as for PCI devices
    dpdk { blacklist fa5a6e7a-cf3a-4b98-9569-addb479b84bc }
     where fa5a6e7a-cf3a-4b98-9569-addb479b84bc - example of UUID
    
    struct vlib_vmbus_addr_t changed to union with UUID described
     fields
    
    Added device_config_index_by_vmbus_addr
          blacklist_by_vmbus_addr
    to enumerate available device configs
    
    hash_key is as_u32[0] field(last 4 bytes of UUID)
    Lost of precision against full UUID, but 2^32 is enough
    to handle all the devices available
    
    Added is_blacklisted check while creating vnet devices in
    order to supress creation of dev if it's blacklisted
    
    Type: feature
    
    Signed-off-by: Vladimir Ratnikov <vratnikov@netgate.com>
    Change-Id: Id82611e54fed082190e488c7e5fbe14ecbe5b2ab

--- src/vlib/vmbus/vmbus.c
+++ src/vlib/vmbus/vmbus.c
@@ -25,9 +25,23 @@
 #include <net/if.h>
 
 /* this is a stub replaced by the Linux specfic version */
-vlib_vmbus_addr_t * __attribute__ ((weak)) vlib_vmbus_get_all_dev_addrs ()
+vlib_vmbus_addr_t *__clib_weak
+vlib_vmbus_get_all_dev_addrs ()
 {
   return NULL;
+}
+
+u8 *__clib_weak
+format_vlib_vmbus_addr (u8 * s, va_list * va)
+{
+  return 0;
+}
+
+uword
+  __clib_weak unformat_vlib_vmbus_addr (unformat_input_t * input,
+					va_list * args)
+{
+  return 0;
 }
 
 clib_error_t *
