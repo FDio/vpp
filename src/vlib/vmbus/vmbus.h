f020b369d969544e3691bf6b79cffe73a8d52e19
Vladimir Ratnikov <vratnikov@netgate.com>
2020-11-11 08:00:48 -0500

    dpdk: allow configure individual VMBUS devices
    now startup.conf supports confuguration for VMBUS
    devices as for PCI devices for whitelisting/blacklisting
    
    dpdk { dev fa5a6e7a-cf3a-4b98-9569-addb479b84bc }
     with sub-configuration as for PCI devices
    dpdk { blacklist fa5a6e7a-cf3a-4b98-9569-addb479b84bc }
     where fa5a6e7a-cf3a-4b98-9569-addb479b84bc - example of UUID
    
    struct vlib_vmbus_addr_t changed to union with UUID described
     fields
    
    Added device_config_index_by_vmbus_addr
          blacklist_by_vmbus_addr
    to enumerate available device configs
    
    hash_key is clock_and_node field(last 8 bytes of UUID)
    
    Type: feature
    
    Signed-off-by: Vladimir Ratnikov <vratnikov@netgate.com>
    Change-Id: Id82611e54fed082190e488c7e5fbe14ecbe5b2ab

--- src/vlib/vmbus/vmbus.h
+++ src/vlib/vmbus/vmbus.h
@@ -21,10 +21,12 @@
 
 #include <vlib/vlib.h>
 
-typedef struct
+typedef union
 {
   u8 guid[16];
+  u32 as_u32[4];
 } vlib_vmbus_addr_t;
+
 typedef u32 vlib_vmbus_dev_handle_t;
 
 vlib_vmbus_addr_t *vlib_vmbus_get_all_dev_addrs ();
@@ -33,6 +35,8 @@ uword vlib_vmbus_get_private_data (vlib_vmbus_dev_handle_t h);
 void vlib_vmbus_set_private_data (vlib_vmbus_dev_handle_t h,
 				  uword private_data);
 
+format_function_t format_vlib_vmbus_addr;
+unformat_function_t unformat_vlib_vmbus_addr;
 clib_error_t *vlib_vmbus_bind_to_uio (vlib_vmbus_addr_t * addr);
 #endif /* included_vlib_vmbus_h */
 
