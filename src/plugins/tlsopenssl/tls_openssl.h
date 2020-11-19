a02465b589ef75e87f6dcfe8a555ad73b6810123
Florin Coras <fcoras@cisco.com>
2020-11-19 13:38:26 -0800

    tls: dtls initial implementation
    Type: feature
    
    Basic dtls transport protocol implementation that relies on openssl
    wire protocol implementation.
    
    To test using vcl test apps, first ensure all arp entries are properly
    resolved and subsequently:
    
    server: vcl_server -p dtls 1234
    client: vcl_client -p dtls 6.0.1.1 1234 -U -N 2000000 -T 1460 -X
    
    Signed-off-by: Florin Coras <fcoras@cisco.com>
    Change-Id: I04b4516a8fe9ce85ba230bcdd891f33a900046ed

--- src/plugins/tlsopenssl/tls_openssl.h
+++ src/plugins/tlsopenssl/tls_openssl.h
@@ -13,6 +13,9 @@
  * limitations under the License.
  */
 
+#ifndef SRC_PLUGINS_TLSOPENSSL_TLS_OPENSSL_H_
+#define SRC_PLUGINS_TLSOPENSSL_TLS_OPENSSL_H_
+
 #include <openssl/ssl.h>
 #include <openssl/conf.h>
 #include <openssl/err.h>
@@ -23,6 +26,8 @@
 
 #define TLSO_CTRL_BYTES 1000
 #define TLSO_MIN_ENQ_SPACE (1 << 16)
+
+#define DTLSO_MAX_DGRAM 2000
 
 typedef struct tls_ctx_openssl_
 {
@@ -48,6 +53,9 @@ typedef struct openssl_main_
   openssl_ctx_t ***ctx_pool;
   openssl_listen_ctx_t *lctx_pool;
 
+  u8 **rx_bufs;
+  u8 **tx_bufs;
+
   /* API message ID base */
   u16 msg_id_base;
 
@@ -72,6 +80,8 @@ void openssl_async_node_enable_disable (u8 is_en);
 clib_error_t *tls_openssl_api_init (vlib_main_t * vm);
 int tls_openssl_set_ciphers (char *ciphers);
 int vpp_openssl_is_inflight (tls_ctx_t * ctx);
+
+#endif /* SRC_PLUGINS_TLSOPENSSL_TLS_OPENSSL_H_ */
 
 /*
  * fd.io coding-style-patch-verification: ON
