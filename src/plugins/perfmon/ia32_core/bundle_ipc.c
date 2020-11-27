6afc1ca36a5fed840cfcca59d7dd7462b2f31ba0
Damjan Marion <damarion@cisco.com>
2020-11-27 20:15:17 +0100

    perfmon2
    Change-Id: I2c14f82393d11fc05c6d229f5c58603ab5c0f14d
    Signed-off-by: Damjan Marion <damarion@cisco.com>

---
+++ src/plugins/perfmon/ia32_core/bundle_ipc.c
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2020 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vnet/vnet.h>
+#include <perfmon/perfmon.h>
+#include <perfmon/ia32_core/ia32_core.h>
+
+static u8 *
+format_ipc_node (u8 * s, va_list * args)
+{
+  __clib_unused perfmon_bundle_t *b = va_arg (*args, perfmon_bundle_t *);
+  perfmon_node_counters_t *nc = va_arg (*args, perfmon_node_counters_t *);
+  s = format (s, "%10.2f", (f64) nc->n_packets / nc->n_calls);
+  s = format (s, "%10.2f", (f64) nc->value[0] / nc->value[1]);
+  s = format (s, "%10.2f", (f64) nc->value[1] / nc->n_packets);
+  s = format (s, "%10.2f", (f64) nc->value[0] / nc->n_packets);
+  return s;
+}
+
+PERFMON_REGISTER_BUNDLE (ipc) = {
+  .name = "ipc",
+  .description = "instructions/packet, cycles/packet and IPC",
+  .source = "intel-ia32-core",
+  .type = PERFMON_BUNDLE_TYPE_NODE,
+  .events[0] = IA32_CORE_E_INST_RETIRED_ANY_P,
+  .events[1] = IA32_CORE_E_CPU_CLK_UNHALTED_THREAD_P,
+  .events[2] = IA32_CORE_E_CPU_CLK_UNHALTED_REF_TSC,
+  .n_events = 3,
+  .format_fn = format_ipc_node,
+  .column_headers = PERFMON_STRINGS ("pkt/call", "IPC", "clk/pkt", "inst/pkt"),
+};
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
--- src/plugins/perfmon/perfmon.h
+++ src/plugins/perfmon/perfmon.h
@@ -1,7 +1,5 @@
 /*
- * perfmon.h - performance monitor
- *
- * Copyright (c) 2018 Cisco Systems and/or its affiliates
+ * Copyright (c) 2020 Cisco and/or its affiliates.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at:
@@ -14,159 +12,166 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef __included_perfmon_h__
-#define __included_perfmon_h__
 
-#include <vnet/vnet.h>
-#include <vnet/ip/ip.h>
-#include <vnet/ethernet/ethernet.h>
-#include <vlib/log.h>
+#ifndef __perfmon_perfmon_h
+#define __perfmon_perfmon_h
 
-#include <vppinfra/hash.h>
-#include <vppinfra/error.h>
+#define PERF_MAX_EVENTS 7	/* 3 fixed and 4 programmable */
 
-#include <linux/perf_event.h>
-#include <perfmon/perfmon_intel.h>
+typedef enum
+{
+  PERFMON_BUNDLE_TYPE_UNKNOWN,
+  PERFMON_BUNDLE_TYPE_NODE,
+  PERFMON_BUNDLE_TYPE_THREAD,
+  PERFMON_BUNDLE_TYPE_SYSTEM,
+} perfmon_bundle_type_t;
 
-#define foreach_perfmon_event                                           \
-_(PERF_TYPE_HARDWARE, PERF_COUNT_HW_CPU_CYCLES, "cpu-cycles")           \
-_(PERF_TYPE_HARDWARE, PERF_COUNT_HW_INSTRUCTIONS, "instructions")       \
-_(PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_REFERENCES,                   \
-  "cache-references")                                                   \
-_(PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_MISSES, "cache-misses")       \
-_(PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_INSTRUCTIONS, "branches")    \
- _(PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_MISSES, "branch-misses")    \
-_(PERF_TYPE_HARDWARE, PERF_COUNT_HW_BUS_CYCLES, "bus-cycles")           \
-_(PERF_TYPE_HARDWARE, PERF_COUNT_HW_STALLED_CYCLES_FRONTEND,            \
-  "stall-frontend")                                                     \
-_(PERF_TYPE_HARDWARE, PERF_COUNT_HW_STALLED_CYCLES_BACKEND,             \
-  "stall-backend")                                                      \
-_(PERF_TYPE_HARDWARE, PERF_COUNT_HW_REF_CPU_CYCLES, "ref-cpu-cycles")   \
-_(PERF_TYPE_SOFTWARE, PERF_COUNT_SW_PAGE_FAULTS, "page-faults")         \
-_(PERF_TYPE_SOFTWARE, PERF_COUNT_SW_CONTEXT_SWITCHES, "context-switches") \
-_(PERF_TYPE_SOFTWARE, PERF_COUNT_SW_CPU_MIGRATIONS, "cpu-migrations")   \
-_(PERF_TYPE_SOFTWARE, PERF_COUNT_SW_PAGE_FAULTS_MIN, "minor-pagefaults") \
-_(PERF_TYPE_SOFTWARE, PERF_COUNT_SW_PAGE_FAULTS_MAJ, "major-pagefaults") \
-_(PERF_TYPE_SOFTWARE, PERF_COUNT_SW_EMULATION_FAULTS, "emulation-faults")
+typedef struct
+{
+  u32 type_from_instance:1;
+  u32 exclude_kernel:1;
+  union
+  {
+    u32 type;
+    u32 instance_type;
+  };
+  u64 config;
+  char *name;
+  char *description;
+} perfmon_event_t;
+
+typedef struct
+{
+  u32 type;
+  int cpu;
+  pid_t pid;
+  char *name;
+} perfmon_instance_t;
 
 typedef struct
 {
   char *name;
-  int pe_type;
-  int pe_config;
-} perfmon_event_config_t;
+  perfmon_instance_t *instances;
+} perfmon_instance_type_t;
 
-typedef enum
+struct perfmon_source;
+
+typedef clib_error_t *(perfmon_source_init_fn_t) (vlib_main_t * vm,
+						  struct perfmon_source *);
+typedef struct perfmon_source
 {
-  PERFMON_STATE_OFF = 0,
-  PERFMON_STATE_RUNNING,
-} perfmon_state_t;
+  char *name;
+  char *description;
+  struct perfmon_source *next;
+  perfmon_event_t *events;
+  u32 n_events;
+  perfmon_instance_type_t *instances_by_type;
+  format_function_t *format_config;
+  perfmon_source_init_fn_t *init_fn;
+} perfmon_source_t;
+
+struct perfmon_bundle;
+typedef clib_error_t *(perfmon_bundle_init_fn_t) (vlib_main_t * vm,
+						  struct perfmon_bundle *);
+typedef struct perfmon_bundle
+{
+  char *name;
+  char *description;
+  char *source;
+  perfmon_bundle_type_t type;
+  u32 events[PERF_MAX_EVENTS];
+  u32 n_events;
+
+  perfmon_bundle_init_fn_t *init_fn;
+
+  char **column_headers;
+  format_function_t *format_fn;
+
+  /* do not set manually */
+  perfmon_source_t *src;
+  struct perfmon_bundle *next;
+} perfmon_bundle_t;
 
 typedef struct
 {
-  u8 *thread_and_node_name;
-  u8 **counter_names;
-  u64 *counter_values;
-  u64 *vectors_this_counter;
-} perfmon_capture_t;
-
-typedef struct
-{
-  u8 *name;
-  u8 *value;
-} name_value_pair_t;
-
-typedef struct
-{
-  u64 ticks[2];
-  u64 vectors;
-} perfmon_counters_t;
-
-typedef struct
-{
-  CLIB_CACHE_LINE_ALIGN_MARK (cacheline0);
-
-  /* Current counters */
-  u64 c[2];
-
-  /* Current perf_event file descriptors, per thread */
-  int pm_fds[2];
-
-  /* mmap base of mapped struct perf_event_mmap_page */
-  u8 *perf_event_pages[2];
-
-  u32 rdpmc_indices[2];
-
-  /* vector of counters by node index */
-  perfmon_counters_t *counters;
-
+  CLIB_CACHE_LINE_ALIGN_MARK (cacheline);
+  u8 n_events;
+  u64 *counters;
+  int pmc_index[PERF_MAX_EVENTS];
+  u64 node_pre_counters[PERF_MAX_EVENTS];
+  struct perf_event_mmap_page *mmap_pages[PERF_MAX_EVENTS];
+  int group_fd;
 } perfmon_thread_t;
 
 typedef struct
 {
-  /* API message ID base */
-  u16 msg_id_base;
-
-  /* on/off switch for the periodic function */
-  volatile u8 state;
-
-  /* capture pool, hash table */
-  perfmon_capture_t *capture_pool;
-  uword *capture_by_thread_and_node_name;
-
-  /* vector of registered perfmon tables */
-  perfmon_intel_pmc_registration_t *perfmon_tables;
-
-  /* active table */
-  perfmon_intel_pmc_event_t *perfmon_table;
-
-  uword *pmc_event_by_name;
-
-  /* vector of single events to collect */
-  perfmon_event_config_t *single_events_to_collect;
-
-  /* vector of paired events to collect */
-  perfmon_event_config_t *paired_events_to_collect;
-
-  /* Base indices of synthetic event tuples */
-  u32 ipc_event_index;
-  u32 mispredict_event_index;
-
-  /* Length of time to capture a single event */
-  f64 timeout_interval;
-
-  /* Current event (index) being collected */
-  u32 current_event;
-  int n_active;
-  /* mmap size of (mapped) struct perf_event_mmap_page */
-  u32 page_size;
-
-  /* thread bitmap */
-  uword *thread_bitmap;
-
-  /* per-thread data */
-  perfmon_thread_t **threads;
-
-  /* Logging */
-  vlib_log_class_t log_class;
-
-  /* convenience */
-  vlib_main_t *vlib_main;
-  vnet_main_t *vnet_main;
-  ethernet_main_t *ethernet_main;
+  perfmon_thread_t *threads;
+  perfmon_bundle_t *bundles;
+  uword *bundle_by_name;
+  perfmon_source_t *sources;
+  uword *source_by_name;
+  perfmon_bundle_t *active_bundle;
+  int is_running;
+  int *group_fds;
+  int *fds_to_close;
+  perfmon_instance_type_t *default_instance_type;
+  perfmon_instance_type_t *active_instance_type;;
 } perfmon_main_t;
 
 extern perfmon_main_t perfmon_main;
 
-extern vlib_node_registration_t perfmon_periodic_node;
-uword *perfmon_parse_table (perfmon_main_t * pm, char *path, char *filename);
+#define PERFMON_REGISTER_SOURCE(x)                                           \
+  perfmon_source_t __perfmon_source_##x;                                     \
+static void __clib_constructor                                               \
+__perfmon_source_registration_##x (void)                                     \
+{                                                                            \
+  perfmon_main_t * pm = &perfmon_main;                                       \
+  __perfmon_source_##x.next = pm->sources;                                   \
+  pm->sources = & __perfmon_source_##x;                                      \
+}                                                                            \
+perfmon_source_t __perfmon_source_##x
 
-uword unformat_processor_event (unformat_input_t * input, va_list * args);
+#define PERFMON_REGISTER_BUNDLE(x)                                           \
+  perfmon_bundle_t __perfmon_bundle_##x;                                     \
+static void __clib_constructor                                               \
+__perfmon_bundle_registration_##x (void)                                     \
+{                                                                            \
+  perfmon_main_t * pm = &perfmon_main;                                       \
+  __perfmon_bundle_##x.next = pm->bundles;                                   \
+  pm->bundles = & __perfmon_bundle_##x;                                      \
+}                                                                            \
+perfmon_bundle_t __perfmon_bundle_##x
 
-/* Periodic function events */
-#define PERFMON_START 1
 
-#endif /* __included_perfmon_h__ */
+void perfmon_reset (vlib_main_t * vm);
+clib_error_t *perfmon_set (vlib_main_t * vm, perfmon_bundle_t *);
+clib_error_t *perfmon_start (vlib_main_t * vm);
+clib_error_t *perfmon_stop (vlib_main_t * vm);
+
+typedef struct
+{
+  u64 n_calls;
+  u64 n_packets;
+  u64 value[0];
+} __clib_aligned (8) perfmon_node_counters_t;
+
+typedef struct {
+  u64 nr;
+  u64 time_enabled;
+  u64 time_running;
+  u64 value[PERF_MAX_EVENTS];
+} perfmon_reading_t;
+
+static_always_inline perfmon_node_counters_t *
+perfmon_get_node_counters (perfmon_thread_t * pt, u32 node_index)
+{
+  return (perfmon_node_counters_t *) (pt->counters +
+				      node_index * (pt->n_events + 2));
+}
+
+#define PERFMON_STRINGS(...) (char*[]) {__VA_ARGS__, 0}
+
+#endif
 
 /*
  * fd.io coding-style-patch-verification: ON
