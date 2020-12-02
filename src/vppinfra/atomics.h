49ff03730e8273440576a3fe50938a0c2a00ca50
Florin Coras <fcoras@cisco.com>
2020-12-01 17:48:00 -0800

    svm: remove fifo segment spinlock
    Type: improvement
    
    Signed-off-by: Florin Coras <fcoras@cisco.com>
    Change-Id: I7f6043bd7180647e98891bd4b3a397a833d52e15

--- src/vppinfra/atomics.h
+++ src/vppinfra/atomics.h
@@ -38,6 +38,7 @@
 #define clib_atomic_bool_cmp_and_swap(addr,old,new) __sync_bool_compare_and_swap(addr, old, new)
 
 #define clib_atomic_cmp_and_swap_acq_relax_n(addr,exp,new,weak) __atomic_compare_exchange_n ((addr), (exp), (new), (weak), __ATOMIC_ACQUIRE, __ATOMIC_RELAXED)
+#define clib_atomic_cmp_and_swap_acq_relax(addr,exp,new,weak) __atomic_compare_exchange ((addr), (exp), (new), (weak), __ATOMIC_ACQUIRE, __ATOMIC_RELAXED)
 
 #define clib_atomic_test_and_set(a) __atomic_exchange_n(a, 1, __ATOMIC_ACQUIRE)
 #define clib_atomic_release(a) __atomic_store_n(a, 0, __ATOMIC_RELEASE)
