db49947f39242b4229b74f26a9d95553e5740f63
Paul Vinciguerra <pvinci@vinciconsulting.com>
2020-12-04 15:01:53 -0500

    vppapigen: py2 cleanup - remove subclassing of object
    Type: refactor
    
    Change-Id: I7136cb8ba101ea3917dacc31ceb3a76a31328301
    Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

--- src/tools/vppapigen/vppapigen.py
+++ src/tools/vppapigen/vppapigen.py
@@ -44,7 +44,7 @@ def exception_handler(exception_type, exception, traceback):
 #
 # Lexer
 #
-class VPPAPILexer(object):
+class VPPAPILexer:
     def __init__(self, filename):
         self.filename = filename
 
@@ -450,7 +450,7 @@ class Paths():
         self.paths = pathset
 
 
-class Coord(object):
+class Coord:
     """ Coordinates of a syntactic element. Consists of:
             - File name
             - Line number
@@ -477,7 +477,7 @@ class ParseError(Exception):
 #
 # Grammar rules
 #
-class VPPAPIParser(object):
+class VPPAPIParser:
     tokens = VPPAPILexer.tokens
 
     def __init__(self, filename, logger, revision=None):
