b65c0e1f940cce57f85344362272bc1bf7601ab9
Paul Vinciguerra <pvinci@vinciconsulting.com>
2020-12-09 19:12:47 +0000

    papi: refactor client to decouple dependency on transport
    Allow for new transports
    Deprecate the use of transport=None after 21.04
    
    * Refactors VppTestCase to use a generic VPPIOError instead of
      VppTransportShmemIOError. Removes import.
    * Removes global state from vpp_transport_shmem.py
    * Specifies the interface for future transports
    * Updates test/framework to use a base transport exception.
      Today the tests only properlt support shm.
    * Update vpp_papi_provider to use decoupled tranport.
    
    Type: refactor
    
    Change-Id: Iaa20253440433e823859af29b8642fb757021b58
    Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>
    (cherry picked from commit bff86090ae441322448e6dfe7dd8ade4d0e33da0)
    (cherry picked from commit d5bcb91bf603f002b0947d9f68ec0a38ba2a6300)

---
+++ src/vpp-api/python/vpp_papi/vpp_transport.py
@@ -0,0 +1,76 @@
+#  Copyright (c) 2020. Vinci Consulting Corp. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import abc
+
+import typing
+
+
+class VPPIOError(IOError):
+    """ exception communicating with vpp over shared memory """
+
+    def __init__(self, rv, descr):
+        self.rv = rv
+        self.desc = descr
+
+        super(VPPIOError, self).__init__(rv, descr)
+
+
+class MessageTableMixin(metaclass=abc.ABCMeta):
+
+    def __init__(self) -> None:
+        self.message_table = {}
+
+    @abc.abstractmethod
+    def get_msg_index(self, name: str) -> int:
+        pass
+
+    @abc.abstractmethod
+    def msg_table_max_index(self) -> int:
+        pass
+
+
+class BaseVppTransport(MessageTableMixin, metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def __init__(self, *, parent, ctx: typing.Dict) -> None:
+        super(BaseVppTransport, self).__init__()
+
+    @abc.abstractmethod
+    def connect(self, name: str, pfx: str, msg_handler, rx_qlen: int) -> int:
+        pass
+
+    @abc.abstractmethod
+    def disconnect(self) -> int:
+        pass
+
+    @abc.abstractmethod
+    def suspend(self) -> None:
+        pass
+
+    @abc.abstractmethod
+    def resume(self) -> None:
+        pass
+
+    @abc.abstractmethod
+    def get_callback(self, do_async: bool) -> typing.Callable:
+        pass
+
+    @abc.abstractmethod
+    def read(self, timeout: typing.Optional[int] = None) -> bytes:
+        pass
+
+    @abc.abstractmethod
+    def write(self, buf: bytes) -> int:
+        pass
+
