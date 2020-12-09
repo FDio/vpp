8f2ba8bb2c8e58a0c2224467c0153390585aa528
Paul Vinciguerra <pvinci@vinciconsulting.com>
2020-12-09 19:12:47 +0000

    papi: refactor client to decouple dependency on transport
    Allow for new transports
    Deprecate the use of transport=None after 21.04
    
    Add in helper classes
      VPPApiShmClient,
      VPPApiSocketClient
    
    Type: refactor
    
    Change-Id: Iaa20253440433e823859af29b8642fb757021b58
    Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

--- test/vpp_papi_provider.py
+++ test/vpp_papi_provider.py
@@ -10,7 +10,7 @@ import time
 from collections import deque
 
 from six import moves, iteritems
-from vpp_papi import VPPApiClient, mac_pton
+from vpp_papi import VPPApiSocketClient, VPPApiShmClient
 from hook import Hook
 from vpp_ip_route import MPLS_IETF_MAX_LABEL, MPLS_LABEL_INVALID
 
@@ -140,21 +140,25 @@ class VppPapiProvider(object):
         self._expect_api_retval = self._zero
         self._expect_stack = []
 
-        # install_dir is a class attribute. We need to set it before
-        # calling the constructor.
-        VPPApiClient.apidir = os.getenv('VPP_INSTALL_PATH')
-
         use_socket = False
         try:
             if os.environ['SOCKET'] == '1':
                 use_socket = True
         except KeyError:
             pass
+        if use_socket:
+            # install_dir is a class attribute. We need to set it before
+            # calling the constructor.
+            VPPApiSocketClient.apidir = os.getenv('VPP_INSTALL_PATH')
+            self.vpp = VPPApiSocketClient(logger=test_class.logger,
+                                          read_timeout=read_timeout,
+                                          server_address=test_class.api_sock)
+        else:
+            VPPApiShmClient.apidir = os.getenv('VPP_INSTALL_PATH')
+            self.vpp = VPPApiShmClient(logger=test_class.logger,
+                                       read_timeout=read_timeout,
+                                       server_address=test_class.api_sock)
 
-        self.vpp = VPPApiClient(logger=test_class.logger,
-                                read_timeout=read_timeout,
-                                use_socket=use_socket,
-                                server_address=test_class.api_sock)
         self._events = deque()
 
     def __enter__(self):
