181fdccd615bb486610c7e2c427375a89785a460
Paul Vinciguerra <pvinci@vinciconsulting.com>
2020-12-09 17:41:22 +0000

    papi: mark apifiles option of VPPApiClient as non-optional
    to be changed after the 21.01 release
    
    Use case1: simultaneous connections to muliple instances of VPP.
    Use case2: unit testing of vpp_papi
    
    testmode=False will be removed in its entirety. It is provided for as
    testmode = []
    
    Type: refactor
    
     # <feature-name>: <subject>
    
    Change-Id: Iaaa57fb6b2791bcff52776b05b37e56fa9572c74
    Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

--- test/vpp_papi_provider.py
+++ test/vpp_papi_provider.py
@@ -10,7 +10,7 @@ import time
 from collections import deque
 
 from six import moves, iteritems
-from vpp_papi import VPPApiClient, mac_pton
+from vpp_papi import VPPApiClient, VPPApiJSONFiles
 from hook import Hook
 from vpp_ip_route import MPLS_IETF_MAX_LABEL, MPLS_LABEL_INVALID
 
@@ -142,7 +142,8 @@ class VppPapiProvider(object):
 
         # install_dir is a class attribute. We need to set it before
         # calling the constructor.
-        VPPApiClient.apidir = os.getenv('VPP_INSTALL_PATH')
+        api_dir = os.getenv('VPP_INSTALL_PATH')
+        apifiles = VPPApiJSONFiles.find_api_files(api_dir=apidir)
 
         use_socket = False
         try:
@@ -151,7 +152,8 @@ class VppPapiProvider(object):
         except KeyError:
             pass
 
-        self.vpp = VPPApiClient(logger=test_class.logger,
+        self.vpp = VPPApiClient(apifiles=apifiles,
+                                logger=test_class.logger,
                                 read_timeout=read_timeout,
                                 use_socket=use_socket,
                                 server_address=test_class.api_sock)
