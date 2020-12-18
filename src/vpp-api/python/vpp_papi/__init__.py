bc194a1f9d6682c73e861522c80ec8b5da5ecc21
Paul Vinciguerra <pvinci@vinciconsulting.com>
2020-12-18 18:47:27 -0500

    papi: expose vpp_papi version to client
    Type: feature
    
    Change-Id: I73025427a58214a842245fceaa77daa7acd3e6f8
    Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

--- src/vpp-api/python/vpp_papi/__init__.py
+++ src/vpp-api/python/vpp_papi/__init__.py
@@ -9,3 +9,9 @@ from . macaddress import MACAddress, mac_pton, mac_ntop  # noqa: F401
 from .vpp_serializer import BaseTypes  # noqa: F401
 from .vpp_serializer import VPPEnumType, VPPType, VPPTypeAlias  # noqa: F401
 from .vpp_serializer import VPPMessage, VPPUnionType  # noqa: F401
+
+try:
+    import pkg_resources  # part of setuptools
+    __version__ = pkg_resources.get_distribution("vpp_papi").version
+except (ModuleNotFoundError, AttributeError):
+    """Not installed via setup/pip"""
