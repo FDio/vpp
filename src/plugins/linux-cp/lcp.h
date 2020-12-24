68b3efbcefc388a91ee808cd95ac9b56f6ab0cd3
Neale Ranns <nranns@cisco.com>
2020-12-24 09:16:09 +0000

    linux-cp: Linux Interface Mirroring for Control Plane Integration
    Type: feature
    
    please see FEATURE.yaml for details.
    
    Signed-off-by: Neale Ranns <nranns@cisco.com>
    Signed-off-by: Matthew Smith <mgsmith@netgate.com>
    Signed-off-by: Jon Loeliger <jdl@netgate.com>
    Signed-off-by: Pim van Pelt <pim@ipng.nl>
    Change-Id: I04a45c15c0838906aa787e06660fa29f39f755fa

---
+++ src/plugins/linux-cp/lcp.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2020 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef __LCP_H__
+#define __LCP_H__
+
+#include <vlib/vlib.h>
+
+#define LCP_NS_LEN 32
+
+typedef struct lcp_main_s
+{
+  u16 msg_id_base;		    /* API message ID base */
+  u8 default_namespace[LCP_NS_LEN]; /* default namespace if set */
+  int default_ns_fd;
+  u8 auto_intf;
+  /* Set when Unit testing */
+  u8 test_mode;
+} lcp_main_t;
+
+extern lcp_main_t lcp_main;
+
+/**
+ * Get/Set the default namespace for LCP host taps.
+ */
+int lcp_set_default_ns (u8 *ns);
+u8 *lcp_get_default_ns (void); /* Returns NULL or shared string */
+int lcp_get_default_ns_fd (void);
+
+#endif
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
