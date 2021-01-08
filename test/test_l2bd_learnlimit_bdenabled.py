40fe4cf08f2c2fb960056a2bea94311ef9a5c0a9
Jerome Tollet <jtollet@cisco.com>
2021-01-08 22:52:54 +0100

    l2: fix tests for learn_limit
    Type: fix
    
    Signed-off-by: Jerome Tollet <jtollet@cisco.com>
    Change-Id: I7cbbea2205ca7249ba19020b25c8657b42e09562

--- test/test_l2bd_learnlimit_bdenabled.py
+++ test/test_l2bd_learnlimit_bdenabled.py
@@ -63,14 +63,38 @@ class TestL2LearnLimit(VppTestCase):
         self.pg_start()
 
     def test_l2bd_learnlimit(self):
-        """ L2BD test without learn Limit
+        """ L2BD test with bridge domain limit
         """
-        hosts = self.create_hosts(self.pg_interfaces[0], 20, 1)
-        self.learn_hosts(self.pg_interfaces[0], 1, hosts)
-        lfs = self.vapi.l2_fib_table_dump(1)
+        self.vapi.want_l2_macs_events(enable_disable=1, learn_limit=1000)
+        self.vapi.bridge_domain_set_default_learn_limit(4)
+        self.vapi.bridge_domain_add_del(bd_id=3)
+        self.vapi.sw_interface_set_l2_bridge(
+            self.pg_interfaces[2].sw_if_index, bd_id=3)
 
-        # check that 20 macs are learned.
-        self.assertEqual(len(lfs), 20)
+        self.vapi.bridge_domain_set_learn_limit(2, 5)
+
+        hosts = self.create_hosts(self.pg_interfaces[1], 20, 2)
+        fhosts = self.create_hosts(self.pg_interfaces[2], 20, 3)
+
+        # inject 20 mac addresses on bd2
+        self.learn_hosts(self.pg_interfaces[1], 2, hosts)
+
+        # inject 20 macs address on bd3
+        self.learn_hosts(self.pg_interfaces[2], 3, fhosts)
+
+        lfs1 = self.vapi.l2_fib_table_dump(2)
+        lfs2 = self.vapi.l2_fib_table_dump(3)
+
+        # check that only 5 macs are learned.
+        self.assertEqual(len(lfs1), 5)
+
+        # check that only 4 macs are learned.
+        self.assertEqual(len(lfs2), 4)
+
+        self.vapi.sw_interface_set_l2_bridge(
+            rx_sw_if_index=self.pg_interfaces[2].sw_if_index,
+            bd_id=3, enable=0)
+        self.vapi.bridge_domain_add_del(is_add=0, bd_id=3)
 
     def setUp(self):
         super(TestL2LearnLimit, self).setUp()
