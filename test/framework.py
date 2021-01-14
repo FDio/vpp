b42e9303da587188fca78fb2011093feb2aedf9a
Andrew Yourtchenko <ayourtch@gmail.com>
2021-01-14 10:19:08 +0000

    tests: add generalized tags for tests, use them for run-solo tests
    We have accumulated several scenarios in prod or wishlists
    where it would be useful to have a general infra to say yes/no
    about a certain test, and potentially make decisions based on that,
    for example:
    
    - runs solo (aka 'time-dependent')
    - (wishlist) part of quick smoke-test set
    - (wishlist) intermittent failure unrelated to timing
    - (wishlist) test broken with a multi-worker config in vpp
    
    Refactor the current "run-solo" code to allow for this extension.
    
    Type: test
    
    Change-Id: Ia5b3810e57c0543753c8e0dc4dc0cfb4a30b36ac
    Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>
    Signed-off-by: Klement Sekera <ksekera@cisco.com>

--- test/framework.py
+++ test/framework.py
@@ -21,6 +21,7 @@ from threading import Thread, Event
 from inspect import getdoc, isclass
 from traceback import format_exception
 from logging import FileHandler, DEBUG, Formatter
+from enum import Enum
 
 import scapy.compat
 from scapy.packet import Raw
@@ -254,6 +255,15 @@ class KeepAliveReporter(object):
 
         self.pipe.send((desc, test.vpp_bin, test.tempdir, test.vpp.pid))
 
+class TestCaseTag(Enum):
+    RUN_SOLO = 1
+
+def tag_run_solo(cls):
+    try:
+        cls.test_tags.append(TestCaseTag.RUN_SOLO)
+    except AttributeError:
+        cls.test_tags = [ TestCaseTag.RUN_SOLO ]
+    return cls
 
 class VppTestCase(unittest.TestCase):
     """This subclass is a base class for VPP test cases that are implemented as
@@ -279,9 +289,18 @@ class VppTestCase(unittest.TestCase):
             return 0
 
     @classmethod
+    def has_tag(cls, tag):
+        """ if the test case has a given tag - return true """
+        try:
+            return tag in cls.test_tags
+        except AttributeError:
+            pass
+        return False
+
+    @classmethod
     def force_solo(cls):
         """ if the test case class is timing-sensitive - return true """
-        return False
+        return cls.has_tag(TestCaseTag.RUN_SOLO)
 
     @classmethod
     def instance(cls):
