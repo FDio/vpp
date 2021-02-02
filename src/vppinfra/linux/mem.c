e389d705022b57617393f93d72d48f67e03447d1
Florin Coras <fcoras@cisco.com>
2021-02-02 15:40:35 -0800

    vppinfra: fix mem map mt issue due to snprintf
    Type: fix
    
    Apparently snprintf is not thread safe!?
    
    Thread 6 "vpp_wk_3" received signal SIGSEGV, Segmentation fault.
    [Switching to Thread 0x7ffeed1a3700 (LWP 819375)]
    0x00007ffff58fc2b4 in __GI__IO_default_xsputn (n=<optimized out>,
    data=<optimized out>, f=<optimized out>) at genops.c:394
    394     genops.c: No such file or directory.
    (gdb) bt
    data=<optimized out>, f=<optimized out>) at genops.c:394
    n=9) at genops.c:370
    format=format@entry=0x7ffff5f833ce "%s",
        ap=ap@entry=0x7ffeec35e1d0, mode_flags=mode_flags@entry=2) at
    ../libio/libioP.h:948
    "", maxlen=<optimized out>, format=0x7ffff5f833ce "%s",
        args=args@entry=0x7ffeec35e1d0, mode_flags=2) at vsnprintf.c:114
    maxlen=<optimized out>, flag=<optimized out>, slen=<optimized out>,
        format=<optimized out>) at snprintf_chk.c:38
    out>, log2_page_sz=<optimized out>, size=<optimized out>,
        fd=<optimized out>, offset=<optimized out>, name=0x0) at
    /scratch/fcoras/vpp/src/vppinfra/linux/mem.c:502
    
    Signed-off-by: Florin Coras <fcoras@cisco.com>
    Change-Id: I8ff7cfed422273dbdc55068772e11f5e550a9b60

--- src/vppinfra/linux/mem.c
+++ src/vppinfra/linux/mem.c
@@ -493,13 +493,13 @@ clib_mem_vm_map_internal (void *base, clib_mem_page_sz_t log2_page_sz,
   hdr->prev = mm->last_map;
   mm->last_map = hdr;
 
+  snprintf (hdr->name, CLIB_VM_MAP_HDR_NAME_MAX_LEN - 1, "%s", (char *) name);
   map_unlock ();
 
   hdr->base_addr = (uword) base;
   hdr->log2_page_sz = log2_page_sz;
   hdr->num_pages = size >> log2_page_sz;
   hdr->fd = fd;
-  snprintf (hdr->name, CLIB_VM_MAP_HDR_NAME_MAX_LEN - 1, "%s", (char *) name);
   hdr->name[CLIB_VM_MAP_HDR_NAME_MAX_LEN - 1] = 0;
   mprotect (hdr, sys_page_sz, PROT_NONE);
 
