2af7d410e08444ac06e696525180e50b9ca59d1c
Brian Russell <brian@graphiant.com>
2021-02-10 13:53:42 +0000

    policer: add api to bind policer to worker
    Add a new api to allow a policer to be bound to
    a specific worker thread for thread handoff.
    
    Type: improvement
    Signed-off-by: Brian Russell <brian@graphiant.com>
    Change-Id: I2623a6827843c3d93c0d7b4ad7c2e13611ec1696

--- src/vnet/policer/policer.h
+++ src/vnet/policer/policer.h
@@ -15,6 +15,8 @@
 #ifndef __included_policer_h__
 #define __included_policer_h__
 
+#include <stdbool.h>
+
 #include <vlib/vlib.h>
 #include <vnet/vnet.h>
 
@@ -68,6 +70,7 @@ u8 *format_policer_instance (u8 * s, va_list * va);
 clib_error_t *policer_add_del (vlib_main_t *vm, u8 *name,
 			       qos_pol_cfg_params_st *cfg, u32 *policer_index,
 			       u8 is_add);
+int policer_bind_worker (u8 *name, u32 worker, bool bind);
 
 #endif /* __included_policer_h__ */
 
