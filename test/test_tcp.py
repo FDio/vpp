179e53f1b923c754b9236ca86c36deaefb3a8276
Florin Coras <fcoras@cisco.com>
2021-03-03 12:58:57 -0800

    hsa: fix builtin echo apps with multiple workers
    Type: fix
    
    Signed-off-by: Florin Coras <fcoras@cisco.com>
    Change-Id: I9507b5a9755e938b4d1da657bed3a8681a056427

--- test/test_tcp.py
+++ test/test_tcp.py
@@ -2,12 +2,10 @@
 
 import unittest
 
-from framework import tag_fixme_vpp_workers
 from framework import VppTestCase, VppTestRunner
 from vpp_ip_route import VppIpTable, VppIpRoute, VppRoutePath
 
 
-@tag_fixme_vpp_workers
 class TestTCP(VppTestCase):
     """ TCP Test Case """
 
