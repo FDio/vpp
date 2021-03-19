abc5f07f978104fd2dad2fc51e5197b022c62791
Govindarajan Mohandoss <govindarajan.mohandoss@arm.com>
2021-03-19 19:20:49 +0000

    ipsec: Performance improvement of ipsec4_output_node using flow cache
    Adding flow cache support to improve outbound IPv4/IPSec SPD lookup
    performance. Details about flow cache:
      Mechanism:
      1. First packet of a flow will undergo linear search in SPD
         table. Once a policy match is found, a new entry will be added
         into the flow cache. From 2nd packet onwards, the policy lookup
         will happen in flow cache.
      2. The flow cache is implemented using bihash without collision
         handling. This will avoid the logic to age out or recycle the old
         flows in flow cache. Whenever a collision occurs, old entry will
         be overwritten by the new entry. Worst case is when all the 256
         packets in a batch result in collision and fall back to linear
         search. Average and best case will be O(1).
      3. The size of flow cache is fixed and decided based on the number
         of flows to be supported. The default is set to 1 million flows.
         This can be made as a configurable option as a next step.
      4. Whenever a SPD rule is added/deleted by the control plane, the
         flow cache entries will be completely deleted (reset) in the
         control plane. The assumption here is that SPD rule add/del is not
         a frequent operation from control plane. Flow cache reset is done,
         by putting the data plane in fall back mode, to bypass flow cache
         and do linear search till the SPD rule add/delete operation is
         complete. Once the rule is successfully added/deleted, the data
         plane will be allowed to make use of the flow cache. The flow
         cache will be reset only after flushing out the inflight packets
         from all the worker cores using
         vlib_worker_wait_one_loop().
    
      Details about bihash usage:
      1. A new bihash template (16_8) is added to support IPv4 5 tuple.
         BIHASH_KVP_PER_PAGE and BIHASH_KVP_AT_BUCKET_LEVEL are set
         to 1 in the new template. It means only one KVP is supported
         per bucket.
      2. Collision handling is avoided by calling
         BV (clib_bihash_add_or_overwrite_stale) function.
         Through the stale callback function pointer, the KVP entry
         will be overwritten during collision.
      3. Flow cache reset is done using
         BV (clib_bihash_foreach_key_value_pair) function.
         Through the callback function pointer, the KVP value is reset
         to ~0ULL.
    
      MRR performance numbers with 1 core, 1 ESP Tunnel, null-encrypt,
      64B for different SPD policy matching indices:
    
      SPD Policy index    : 1          10         100        1000
      Throughput          : MPPS/MPPS  MPPS/MPPS  MPPS/MPPS  KPPS/MPPS
      (Baseline/Optimized)
      ARM Neoverse N1     : 5.2/4.84   4.55/4.84  2.11/4.84  329.5/4.84
      ARM TX2             : 2.81/2.6   2.51/2.6   1.27/2.6   176.62/2.6
      INTEL SKX           : 4.93/4.48  4.29/4.46  2.05/4.48  336.79/4.47
    
      Next Steps:
      Following can be made as a configurable option through startup
      conf at IPSec level:
      1. Enable/Disable Flow cache.
      2. Bihash configuration like number of buckets and memory size.
      3. Dual/Quad loop unroll can be applied around bihash to further
         improve the performance.
      4. The same flow cache logic can be applied for IPv6 as well as in
         IPSec inbound direction. A deeper and wider flow cache using
         bihash_40_8 can replace existing bihash_16_8, to make it
         common for both IPv4 and IPv6 in both outbound and
         inbound directions.
    
    Following changes are made based on the review comments:
    1. ON/OFF flow cache through startup conf. Default: OFF
    2. Flow cache stale entry detection using epoch counter.
    3. Avoid host order endianness conversion during flow cache
       lookup.
    4. Move IPSec startup conf to a common file.
    5. Added SPD flow cache unit test case
    
    Type: improvement
    Signed-off-by: mgovind <govindarajan.Mohandoss@arm.com>
                   Zachary Leaf <zachary.leaf@arm.com>
    Tested-by: Jieqiang Wang <jieqiang.wang@arm.com>
    Change-Id: I62b4d6625fbc6caf292427a5d2046aa5672b2006

---
+++ src/vnet/ipsec/ipsec_bihash_16_8.h
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2021 ARM Limited.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#undef BIHASH_TYPE
+#undef BIHASH_KVP_PER_PAGE
+#undef BIHASH_32_64_SVM
+#undef BIHASH_ENABLE_STATS
+#undef BIHASH_KVP_AT_BUCKET_LEVEL
+#undef BIHASH_LAZY_INSTANTIATE
+#undef BIHASH_BUCKET_PREFETCH_CACHE_LINES
+#undef BIHASH_USE_HEAP
+
+#define BIHASH_TYPE			   _16_8_1
+#define BIHASH_KVP_PER_PAGE		   1
+#define BIHASH_KVP_AT_BUCKET_LEVEL	   1
+#define BIHASH_LAZY_INSTANTIATE		   0
+#define BIHASH_BUCKET_PREFETCH_CACHE_LINES 2
+#define BIHASH_USE_HEAP			   1
+
+#ifndef __included_ipsec_bihash_16_8_1_h__
+#define __included_ipsec_bihash_16_8_1_h__
+
+#include <vppinfra/heap.h>
+#include <vppinfra/format.h>
+#include <vppinfra/pool.h>
+#include <vppinfra/xxhash.h>
+#include <vppinfra/crc32.h>
+
+typedef struct
+{
+  u64 key[2];
+  u64 value;
+} clib_bihash_kv_16_8_1_t;
+
+static inline int
+clib_bihash_is_free_16_8_1 (clib_bihash_kv_16_8_1_t *v)
+{
+  /* Free values are clib_memset to 0xff, check a bit... */
+  if (v->key[0] == ~0ULL && v->value == ~0ULL)
+    return 1;
+  return 0;
+}
+
+static inline u64
+clib_bihash_hash_16_8_1 (clib_bihash_kv_16_8_1_t *v)
+{
+#ifdef clib_crc32c_uses_intrinsics
+  return clib_crc32c ((u8 *) v->key, 16);
+#else
+  u64 tmp = v->key[0] ^ v->key[1];
+  return clib_xxhash (tmp);
+#endif
+}
+
+static inline u8 *
+format_bihash_kvp_16_8_1 (u8 *s, va_list *args)
+{
+  clib_bihash_kv_16_8_1_t *v = va_arg (*args, clib_bihash_kv_16_8_1_t *);
+
+  s = format (s, "key %llu %llu value %llu", v->key[0], v->key[1], v->value);
+  return s;
+}
+
+static inline int
+clib_bihash_key_compare_16_8_1 (u64 *a, u64 *b)
+{
+#if defined(CLIB_HAVE_VEC128) && defined(CLIB_HAVE_VEC128_UNALIGNED_LOAD_STORE)
+  u64x2 v;
+  v = u64x2_load_unaligned (a) ^ u64x2_load_unaligned (b);
+  return u64x2_is_all_zero (v);
+#else
+  return ((a[0] ^ b[0]) | (a[1] ^ b[1])) == 0;
+#endif
+}
+
+#undef __included_bihash_template_h__
+#include <vppinfra/bihash_template.h>
+
+#endif /* __included_ipsec_bihash_16_8_1_h__ */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
