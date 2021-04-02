bc80e8b4c3d4fa62016d66689afc911c2c0b9ab8
Damjan Marion <damarion@cisco.com>
2021-04-02 23:04:05 +0200

    misc: add build_static_vppctl.sh
    Type: make
    Change-Id: I47cf68ecd546cad05732a572579569594f96a1ff
    Signed-off-by: Damjan Marion <damarion@cisco.com>

---
+++ extras/scripts/build_static_vppctl.sh
@@ -0,0 +1,36 @@
+#/bin/env bash
+
+src=$(realpath $(dirname $0)/../..)/src
+tmp=$(mktemp -d)
+mkdir ${tmp}/vppinfra
+touch ${tmp}/vppinfra/config.h
+
+${CC:-cc} \
+ -O2 \
+ -flto \
+ -static \
+ -I ${src} \
+ -I ${tmp} \
+ -DHAVE_MEMFD_CREATE \
+ -DHAVE_GETCPU \
+ ${src}/vppinfra/backtrace.c \
+ ${src}/vppinfra/dlmalloc.c \
+ ${src}/vppinfra/elf.c \
+ ${src}/vppinfra/elf_clib.c \
+ ${src}/vppinfra/error.c \
+ ${src}/vppinfra/format.c \
+ ${src}/vppinfra/hash.c \
+ ${src}/vppinfra/mem.c \
+ ${src}/vppinfra/mem_dlmalloc.c \
+ ${src}/vppinfra/std-formats.c \
+ ${src}/vppinfra/string.c \
+ ${src}/vppinfra/socket.c \
+ ${src}/vppinfra/vec.c \
+ ${src}/vppinfra/unformat.c \
+ ${src}/vppinfra/unix-misc.c \
+ ${src}/vppinfra/linux/mem.c \
+ ${src}/vpp/app/vppctl.c \
+ -o vppctl
+
+rm ${tmp}/vppinfra/config.h
+rmdir ${tmp}/vppinfra ${tmp}
