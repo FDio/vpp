e212c58bde05cdc823ad57bbb04d5659d56e8da6
Mohsin Kazmi <sykazmi@cisco.com>
2021-04-30 16:16:43 +0200

    interface: add support for tx queue depth
    Type: feature
    
    Change-Id: Ie743f87468a40cd0fa2a9bf66104cb7ff0a5fe7b
    Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>

--- src/vnet/devices/virtio/device.c
+++ src/vnet/devices/virtio/device.c
@@ -982,13 +982,15 @@ VNET_DEVICE_CLASS_TX_FN (virtio_device_class) (vlib_main_t * vm,
   virtio_main_t *nm = &virtio_main;
   vnet_interface_output_runtime_t *rund = (void *) node->runtime_data;
   virtio_if_t *vif = pool_elt_at_index (nm->interfaces, rund->dev_instance);
-  u16 qid = vm->thread_index % vif->num_txqs;
+  u32 thread_index = vm->thread_index;
+  u16 qid = thread_index % vif->num_txqs;
   virtio_vring_t *vring = vec_elt_at_index (vif->txq_vrings, qid);
   u16 n_left = frame->n_vectors;
   u32 *buffers = vlib_frame_vector_args (frame);
   u32 to[GRO_TO_VECTOR_SIZE (n_left)];
   int packed = vif->is_packed;
   u16 n_vectors = frame->n_vectors;
+  u16 n_buffers_in_buffering = 0;
 
   clib_spinlock_lock_if_init (&vring->lockp);
 
@@ -1034,11 +1036,18 @@ retry:
 							      - n_left],
 							     n_left);
       n_left -= n_buffered;
+      n_buffers_in_buffering = virtio_vring_n_buffers (vring->buffering);
     }
   if (n_left)
     virtio_interface_drop_inline (vm, node->node_index,
 				  &buffers[n_vectors - n_left], n_left,
 				  VIRTIO_TX_ERROR_NO_FREE_SLOTS);
+
+  vlib_set_simple_counter (
+        vnet_main.interface_main.sw_if_counters +
+          vnet_interface_counter_tx_queue (TX_QUEUE_ACCESS (vring->queue_id)),
+        thread_index, vif->sw_if_index,
+        vring->desc_in_use + n_buffers_in_buffering);
 
   clib_spinlock_unlock_if_init (&vring->lockp);
 
