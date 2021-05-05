cabc06c251f1be7cb16312797407488972bee984
Ole Troan <ot@cisco.com>
2021-05-05 23:00:58 +0200

    vppapigen: include comments in json
    Type: feature
    Signed-off-by: Ole Troan <ot@cisco.com>
    Change-Id: Ibd796adea734b64d9209c5e18c5b9800cbaf62c6

--- src/tools/vppapigen/vppapigen_json.py
+++ src/tools/vppapigen/vppapigen_json.py
@@ -78,6 +78,7 @@ def walk_defs(s, is_message=False):
             c = {}
             c['crc'] = "{0:#0{1}x}".format(t.crc, 10)
             c['options'] = t.options
+            c['comment'] = t.comment
             d.append(c)
 
         r.append(d)
