FDio/vpp

commit bd376a8ce8425f45b23074398949e8055de29280

avf: Fix cksum offload / ipsec
VNET_BUFFER_F_OFFLOAD means 'check 2nd cacheline'
but we can still have TCP_CKSUM=0 & UDP_CKSUM=0

Type: fix

Change-Id: I42b5974f6d34b777aa959c06c67619a5c83c5805
Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

src/plugins/avf/output.c
@@ -63,9 +63,14 @@ avf_tx_prepare_cksum (vlib_buffer_t * b, u8 is_tso)
   vnet_buffer_oflags_t oflags = vnet_buffer (b)->oflags;
   u32 is_tcp = is_tso || oflags & VNET_BUFFER_OFFLOAD_F_TCP_CKSUM;
   u32 is_udp = !is_tso && oflags & VNET_BUFFER_OFFLOAD_F_UDP_CKSUM;
+
+  if (!is_tcp && !is_udp)
+    return 0;
+
   u32 is_ip4 = b->flags & VNET_BUFFER_F_IS_IP4;
   u32 is_ip6 = b->flags & VNET_BUFFER_F_IS_IP6;
-  ASSERT (!is_tcp || !is_udp);
+
+  ASSERT (!(is_tcp && is_udp));
   ASSERT (is_ip4 || is_ip6);
   i16 l2_hdr_offset = b->current_data;
   i16 l3_hdr_offset = vnet_buffer (b)->l3_hdr_offset;

commit e4959a7b3a5314237456bf71775a8f5554f30558

avf: fix coverity
Change-Id: I54025eb0e4ee861b2b2fed554b006405cba38bb9
Signed-off-by: Mohammed Hawari <mohammed@hawari.fr>
Type: fix
Fixes: bd376a8ce8425f45b23074398949e8055de29280

src/plugins/avf/output.c
@@ -81,14 +81,13 @@ avf_tx_prepare_cksum (vlib_buffer_t * b, u8 is_tso)
   ip6_header_t *ip6 = (void *) (b->data + l3_hdr_offset);
   tcp_header_t *tcp = (void *) (b->data + l4_hdr_offset);
   udp_header_t *udp = (void *) (b->data + l4_hdr_offset);
-  u16 l4_len =
-    is_tcp ? tcp_header_bytes (tcp) : is_udp ? sizeof (udp_header_t) : 0;
+  u16 l4_len = is_tcp ? tcp_header_bytes (tcp) : sizeof (udp_header_t);
   u16 sum = 0;
 
   flags |= AVF_TXD_OFFSET_MACLEN (l2_len) |
     AVF_TXD_OFFSET_IPLEN (l3_len) | AVF_TXD_OFFSET_L4LEN (l4_len);
   flags |= is_ip4 ? AVF_TXD_CMD_IIPT_IPV4 : AVF_TXD_CMD_IIPT_IPV6;
-  flags |= is_tcp ? AVF_TXD_CMD_L4T_TCP : is_udp ? AVF_TXD_CMD_L4T_UDP : 0;
+  flags |= is_tcp ? AVF_TXD_CMD_L4T_TCP : AVF_TXD_CMD_L4T_UDP;
 
   if (is_ip4)
     ip4->checksum = 0;
@@ -101,8 +100,6 @@ avf_tx_prepare_cksum (vlib_buffer_t * b, u8 is_tso)
 	ip6->payload_length = 0;
     }
 
-  if (is_tcp || is_udp)
-    {
       if (is_ip4)
 	{
 	  struct avf_ip4_psh psh = { 0 };
@@ -124,12 +121,12 @@ avf_tx_prepare_cksum (vlib_buffer_t * b, u8 is_tso)
 	  psh.l4len = is_tso ? 0 : ip6->payload_length;
 	  sum = ~ip_csum (&psh, sizeof (psh));
 	}
-    }
+
   /* ip_csum does a byte swap for some reason... */
   sum = clib_net_to_host_u16 (sum);
   if (is_tcp)
     tcp->checksum = sum;
-  else if (is_udp)
+  else
     udp->checksum = sum;
   return flags;
 }