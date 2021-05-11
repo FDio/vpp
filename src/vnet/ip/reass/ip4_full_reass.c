26b82f5205c46adedcb7da3aa1be2c65aca62a74
Damjan Marion <damarion@cisco.com>
2021-05-11 09:39:24 +0200

    vlib: handoff rework
    Type: improvement
    Change-Id: I1e199ae31e969154319e94c5cd286b8d8adc6660
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- src/vnet/ip/reass/ip4_full_reass.c
+++ src/vnet/ip/reass/ip4_full_reass.c
@@ -1840,9 +1840,8 @@ ip4_full_reass_handoff_node_inline (vlib_main_t * vm,
       ti += 1;
       b += 1;
     }
-  n_enq =
-    vlib_buffer_enqueue_to_thread (vm, fq_index, from, thread_indices,
-				   frame->n_vectors, 1);
+  n_enq = vlib_buffer_enqueue_to_thread (vm, node, fq_index, from,
+					 thread_indices, frame->n_vectors, 1);
 
   if (n_enq < frame->n_vectors)
     vlib_node_increment_counter (vm, node->node_index,
