32ea9805e6cc6fe4c8b3bf53e89895cb77e95476
Neale Ranns <neale@graphiant.com>
2021-05-13 15:28:27 +0000

    fib: During the mfib lookup set the unicast FIB index in the packet so that a uRPF check on a for-us packet is done in the correct VRF
    Type: fix
    
    Signed-off-by: Neale Ranns <neale@graphiant.com>
    Change-Id: Iafa6efea0d96962aa9136dccefc148a961f74476

--- src/vnet/mfib/mfib_forward.c
+++ src/vnet/mfib/mfib_forward.c
@@ -150,7 +150,9 @@ mfib_forward_lookup (vlib_main_t * vm,
             {
                 ip4_header_t * ip0;
 
-                fib_index0 = vec_elt (ip4_main.mfib_index_by_sw_if_index,
+		ip_lookup_set_buffer_fib_index (
+		  ip4_main.fib_index_by_sw_if_index, p0);
+		fib_index0 = vec_elt (ip4_main.mfib_index_by_sw_if_index,
                                       vnet_buffer(p0)->sw_if_index[VLIB_RX]);
                 ip0 = vlib_buffer_get_current (p0);
                 mfei0 = ip4_mfib_table_lookup(ip4_mfib_get(fib_index0),
@@ -162,7 +164,9 @@ mfib_forward_lookup (vlib_main_t * vm,
             {
                 ip6_header_t * ip0;
 
-                fib_index0 = vec_elt (ip6_main.mfib_index_by_sw_if_index,
+		ip_lookup_set_buffer_fib_index (
+		  ip6_main.fib_index_by_sw_if_index, p0);
+		fib_index0 = vec_elt (ip6_main.mfib_index_by_sw_if_index,
                                       vnet_buffer(p0)->sw_if_index[VLIB_RX]);
                 ip0 = vlib_buffer_get_current (p0);
                 mfei0 = ip6_mfib_table_fwd_lookup(ip6_mfib_get(fib_index0),
