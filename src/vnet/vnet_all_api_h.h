69b0fd1c1c87fa8d34973df0579fb43c264a321d
Filip Tehlar <ftehlar@cisco.com>
2021-06-04 13:37:00 +0000

    vxlan: api cleanup
    use autogenerated code
    
    Type: improvement
    
    Signed-off-by: Filip Tehlar <ftehlar@cisco.com>
    Change-Id: I163eefa86f3248260481181818d70fa1b6eaa220

--- src/vnet/vnet_all_api_h.h
+++ src/vnet/vnet_all_api_h.h
@@ -41,7 +41,6 @@
 #include <vnet/l2/l2.api.h>
 #include <vnet/span/span.api.h>
 #include <vnet/ip/ip.api.h>
-#include <vnet/vxlan/vxlan.api.h>
 #include <vnet/vxlan-gpe/vxlan_gpe.api.h>
 #include <vnet/bfd/bfd.api.h>
 #include <vnet/ipsec/ipsec.api.h>
