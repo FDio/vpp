3ca17ad262fa60bdca25b603a4abbb145f93ae0e
Mauricio Solis <mauricio.solisjr@tno.nl>
2021-06-06 09:38:25 -0500

    ioam: Updated iOAM plugin basedo on recent drafts
    Improvements based on:
    https://github.com/inband-oam/ietf/blob/master/drafts/versions/03/draft-ietf-ippm-ioam-ipv6-options-03.txt
    https://tools.ietf.org/html/draft-ietf-ippm-ioam-data-10
    
    Type: fix
    
    Change-Id: I9e8e8fe8be0f3cd3aee416965476484068b36036
    Signed-off-by: Mauricio Solis <mauricio.solisjr@tno.nl>

--- src/plugins/ioam/lib-trace/trace_api.c
+++ src/plugins/ioam/lib-trace/trace_api.c
@@ -37,14 +37,27 @@ static void vl_api_trace_profile_add_t_handler
   int rv = 0;
   vl_api_trace_profile_add_reply_t *rmp;
   trace_profile *profile = NULL;
+  trace_profile mp_profile;
 
   profile = trace_profile_find ();
+
+  mp_profile.namespace_id = mp->namespace_id;
+  mp_profile.num_elts = mp->num_elts;
+  mp_profile.node_id_short = mp->node_id_short;
+  mp_profile.node_id_wide = mp->node_id_wide;
+  mp_profile.app_data_short = mp->app_data_short;
+  mp_profile.app_data_wide = mp->app_data_wide;
+  mp_profile.option_type = mp->option_type;
+  mp_profile.trace_type = mp->trace_type;
+  mp_profile.node_type = mp->node_type;
+  mp_profile.ts_format = mp->ts_format;
+  mp_profile.queue_depth_type = mp->queue_depth_type;
+  mp_profile.opaque.len_schemeid = mp->len_schemeid;
+  mp_profile.opaque->data = mp->data;
+
   if (profile)
     {
-      rv =
-	trace_profile_create (profile, mp->trace_type, mp->num_elts,
-			      mp->trace_tsp, ntohl (mp->node_id),
-			      ntohl (mp->app_data));
+      rv = trace_profile_create (profile, &mp_profile);
       if (rv != 0)
 	goto ERROROUT;
     }
@@ -56,9 +69,8 @@ ERROROUT:
   REPLY_MACRO (VL_API_TRACE_PROFILE_ADD_REPLY);
 }
 
-
-static void vl_api_trace_profile_del_t_handler
-  (vl_api_trace_profile_del_t * mp)
+static void
+vl_api_trace_profile_del_t_handler (vl_api_trace_profile_del_t *mp)
 {
   int rv = 0;
   vl_api_trace_profile_del_reply_t *rmp;
@@ -77,20 +89,28 @@ static void vl_api_trace_profile_show_config_t_handler
   if (profile->valid)
     {
       REPLY_MACRO2 (VL_API_TRACE_PROFILE_SHOW_CONFIG_REPLY,
-		    rmp->trace_type = profile->trace_type;
+		    rmp->namespace_id = profile->namespace_id;
 		    rmp->num_elts = profile->num_elts;
-		    rmp->trace_tsp = profile->trace_tsp;
-		    rmp->node_id = htonl (profile->node_id);
-		    rmp->app_data = htonl (profile->app_data);
-	);
+		    rmp->node_id_short = profile->node_id_short;
+		    rmp->node_id_wide = profile->node_id_wide;
+		    rmp->app_data_short = profile->app_data_short;
+		    rmp->app_data_wide = profile->app_data_wide;
+		    rmp->option_type = profile->option_type;
+		    rmp->trace_type = profile->trace_type;
+		    rmp->node_type = profile->node_type;
+		    rmp->ts_format = profile->ts_format;
+		    rmp->queue_depth_type = profile->queue_depth_type;
+		    rmp->len_schemeid = profile->opaque.len_schemeid;
+		    rmp->data = profile->opaque.data;);
     }
   else
     {
-      REPLY_MACRO2 (VL_API_TRACE_PROFILE_SHOW_CONFIG_REPLY,
-		    rmp->trace_type = 0;
-		    rmp->num_elts = 0; rmp->trace_tsp = 0;
-		    rmp->node_id = 0; rmp->app_data = 0;
-	);
+      REPLY_MACRO2 (
+	VL_API_TRACE_PROFILE_SHOW_CONFIG_REPLY, rmp->namespace_id = 0;
+	rmp->num_elts = 0; rmp->node_id_short = 0; rmp->node_id_wide = 0;
+	rmp->app_data_short = 0; rmp->app_data_wide = 0; rmp->option_type = 0;
+	rmp->trace_type = 0; rmp->node_type = 0; rmp->ts_format = 0;
+	rmp->queue_depth_type = 0; rmp->len_schemeid = 0; rmp->data = 0;);
     }
 }
 
@@ -99,7 +119,6 @@ static clib_error_t *
 trace_init (vlib_main_t * vm)
 {
   trace_main_t *sm = &trace_main;
-
   bzero (sm, sizeof (trace_main));
   (void) trace_util_init ();
 
