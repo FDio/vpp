3dc105289cab3a2d9cec0e98e41bf285bf40e619
Ray Kinsella <mdr@ashroe.eu>
2021-06-09 11:35:26 +0100

    perfmon: add membw-bound bundle
    Added memory bandwidth boundedness bundled, closely related to cache-hierarchy.
    Works on ICX only, due to additional general counters.
    
    Type: improvement
    
    Signed-off-by: Ray Kinsella <mdr@ashroe.eu>
    Change-Id: Id385bd5f4e645ac020774e311c623afb64b79b1f

---
+++ src/plugins/perfmon/intel/bundle/membw_bound.c
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2020 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vnet/vnet.h>
+#include <vppinfra/linux/sysfs.h>
+#include <perfmon/perfmon.h>
+#include <perfmon/intel/core.h>
+
+static f64
+calculate_membw_bound (perfmon_stats_t *ts, int idx)
+{
+  if (!ts->n_packets)
+    return 0;
+
+  return (u64) ts->value[idx] / ts->n_packets;
+}
+
+static u64
+update_membw_bound (perfmon_stats_t *ts, int idx)
+{
+  return (f64) calculate_membw_bound (ts, idx);
+}
+
+static u8 *
+format_intel_membw_bound (u8 *s, va_list *args)
+{
+  perfmon_stats_t *ss = va_arg (*args, perfmon_stats_t *);
+  int row = va_arg (*args, int);
+  f64 sv = calculate_membw_bound (ss, row);
+
+  s = format (s, "%5.0f", sv);
+
+  return s;
+}
+
+static perfmon_cpu_supports_t membw_bound_cpu_supports[] = {
+  { clib_cpu_supports_avx512_bitalg,
+    PERFMON_BUNDLE_TYPE_NODE_FLAG | PERFMON_BUNDLE_TYPE_THREAD_FLAG },
+};
+
+PERFMON_REGISTER_BUNDLE (intel_core_membw_bound) = {
+  .name = "membw-bound",
+  .description = "memory bandwidth boundedness",
+  .source = "intel-core",
+  .type_flags = PERFMON_BUNDLE_TYPE_NODE_FLAG |
+		PERFMON_BUNDLE_TYPE_THREAD_FLAG,
+  .events[0] = INTEL_CORE_E_CPU_CLK_UNHALTED_THREAD_P,	      /* FIXED */
+  .events[1] = INTEL_CORE_E_CYCLE_ACTIVITY_CYCLES_NO_EXECUTE, /*CMask: 0xFF*/
+  .events[2] = INTEL_CORE_E_CYCLE_ACTIVITY_STALLS_MEM_ANY,    /*CMask: 0xFF*/
+  .events[3] = INTEL_CORE_E_CYCLE_ACTIVITY_STALLS_L1D_MISS,   /*CMask: 0xF*/
+  .events[4] = INTEL_CORE_E_L1D_PEND_MISS_FB_FULL,	      /*CMask: 0xF*/
+  .events[5] = INTEL_CORE_E_CYCLE_ACTIVITY_STALLS_L3_MISS,    /*CMask: 0xF*/
+  .events[6] = INTEL_CORE_E_SQ_MISC_SQ_FULL,		      /*CMask: 0xF*/
+  .n_events = 7,
+  .format_fn = format_intel_membw_bound,
+  .update_fn = update_membw_bound,
+  .cpu_supports = membw_bound_cpu_supports,
+  .n_cpu_supports = ARRAY_LEN (membw_bound_cpu_supports),
+  .column_headers = PERFMON_STRINGS ("Cycles/Packet", "Cycles Stall/Packet",
+				     "Mem Stall/Packet",
+				     "L1D Miss Stall/Packet", "FB Full/Packet",
+				     "L3 Miss Stall/Packet", "SQ Full/Packet"),
+};
