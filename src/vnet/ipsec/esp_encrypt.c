1477f43a2d20025b6734c94242c2d97f51284cdd
Arthur de Kerhor <arthurdekerhor@gmail.com>
2021-06-14 11:56:38 +0200

    gso: hardware offloads for ipip tunnels in dpdk
    When the dpdk device supports TSO for IPIP tunnels, we shift the
    responsability for the segmentation to the hardware itself
    
    Type: feature
    
    Change-Id: I17f5d71bf4c5f43a85ca3f2fbebfa1426b42ef69
    Signed-off-by: Arthur de Kerhor <arthurdekerhor@gmail.com>

--- src/vnet/ipsec/esp_encrypt.c
+++ src/vnet/ipsec/esp_encrypt.c
@@ -18,6 +18,7 @@
 #include <vnet/vnet.h>
 #include <vnet/api_errno.h>
 #include <vnet/ip/ip.h>
+#include <vnet/interface_output.h>
 
 #include <vnet/crypto/crypto.h>
 
@@ -636,6 +637,9 @@ esp_encrypt_inline (vlib_main_t *vm, vlib_node_runtime_t *node,
 
       if (is_tun)
 	{
+	  vnet_calc_checksums_inline (vm, b[0],
+				      b[0]->flags & VNET_BUFFER_F_IS_IP4,
+				      b[0]->flags & VNET_BUFFER_F_IS_IP6);
 	  /* we are on a ipsec tunnel's feature arc */
 	  vnet_buffer (b[0])->ipsec.sad_index =
 	    sa_index0 = ipsec_tun_protect_get_sa_out
