85de1513053adf335357665258fe7a949140fb8d
Mohsin Kazmi <sykazmi@cisco.com>
2021-06-30 18:26:25 +0000

    hash: add support for hashing infra
    Type: feature
    
    Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>
    Change-Id: I3652ae275385d9b1eb1b11f418e3a7e5fef2f556

---
+++ src/vnet/hash/hash.c
@@ -0,0 +1,110 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright(c) 2021 Cisco Systems, Inc.
+ */
+
+#include <vlib/vlib.h>
+#include <vnet/vnet.h>
+#include <vnet/interface.h>
+#include <vnet/hash/hash.h>
+
+vnet_hash_main_t vnet_hash_main;
+
+u8 *
+format_vnet_hash_type (u8 *s, va_list *args)
+{
+  vnet_hash_type_t htype = va_arg (*args, vnet_hash_type_t);
+
+#define _(name, bit, ss)                                                      \
+  if (htype & VNET_HASH_TYPE_##name)                                          \
+    s = format (s, "%s ", #name);
+  foreach_vnet_hash_types
+#undef _
+    return s;
+}
+
+u8 *
+format_vnet_hash (u8 *s, va_list *args)
+{
+  vnet_hash_function_registration_t *hash =
+    va_arg (*args, vnet_hash_function_registration_t *);
+
+  s = format (s, "[name: %s ", hash->name);
+  s = format (s, "type: %U ", format_vnet_hash_type, hash->type);
+  s = format (s, "description: %s]", hash->description);
+  return s;
+}
+
+uword
+unformat_vnet_hash_type (unformat_input_t *input, va_list *args)
+{
+  vnet_hash_type_t *htypep = va_arg (*args, vnet_hash_type_t *);
+  int rv = 0;
+  vnet_hash_type_t htype;
+
+  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (0)
+	;
+#define _(enum, bit, str)                                                     \
+  else if (unformat (input, str))                                             \
+  {                                                                           \
+    htype = (1 << bit);                                                       \
+    rv = 1;                                                                   \
+  }
+      foreach_vnet_hash_types
+#undef _
+	else break;
+    }
+  if (rv)
+    *htypep = htype;
+  return rv;
+}
+
+vnet_hash_func
+vnet_hash_function_from_default_type (vnet_hash_fn_type_t ftype)
+{
+  vnet_hash_function_registration_t *hash = vnet_hash_main.hash_registrations;
+  while (hash)
+    {
+      if (hash->type == VNET_HASH_TYPE_CRC32_5TUPLE)
+	break;
+      hash = hash->next;
+    }
+  return hash->function[ftype];
+}
+
+vnet_hash_func
+vnet_hash_function_from_type (vnet_hash_type_t htype,
+			      vnet_hash_fn_type_t ftype)
+{
+  vnet_hash_function_registration_t *hash = vnet_hash_main.hash_registrations;
+  while (hash)
+    {
+      if (hash->type == htype)
+	break;
+      hash = hash->next;
+    }
+  return hash->function[ftype];
+}
+
+vnet_hash_function_registration_t *
+vnet_hash_function_from_func (vnet_hash_func func, vnet_hash_fn_type_t ftype)
+{
+  vnet_hash_function_registration_t *hash = vnet_hash_main.hash_registrations;
+  while (hash)
+    {
+      if (hash->function[ftype] == func)
+	break;
+      hash = hash->next;
+    }
+  return hash;
+}
+
+static clib_error_t *
+vnet_hash_init (vlib_main_t *vm)
+{
+  return (0);
+}
+
+VLIB_INIT_FUNCTION (vnet_hash_init);
---
+++ src/vnet/hash/hash.h
@@ -0,0 +1,72 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright(c) 2021 Cisco Systems, Inc.
+ */
+
+#ifndef __VNET_HASH_H__
+#define __VNET_HASH_H__
+
+#include <vlib/vlib.h>
+
+#define foreach_vnet_hash_types _ (CRC32_5TUPLE, 0, "crc32-5tuple")
+
+typedef enum
+{
+#define _(f, n, s) VNET_HASH_TYPE_##f = (1 << n),
+  foreach_vnet_hash_types
+#undef _
+    VNET_HASH_TYPE_N,
+} vnet_hash_type_t;
+
+#define foreach_vnet_hash_fn_types                                            \
+  _ (ETHERNET, 0, "hash-fn-ethernet")                                         \
+  _ (IP, 1, "hash-fn-ip")
+
+typedef enum
+{
+#define _(f, n, s) VNET_HASH_FN_TYPE_##f,
+  foreach_vnet_hash_fn_types
+#undef _
+    VNET_HASH_FN_TYPE_N,
+} vnet_hash_fn_type_t;
+
+typedef void (*vnet_hash_func) (void **p, u32 *h, u32 n_packets);
+
+typedef struct vnet_hash_function_registration
+{
+  const char *name;
+  const char *description;
+  vnet_hash_type_t type;
+  vnet_hash_func function[VNET_HASH_FN_TYPE_N];
+
+  struct vnet_hash_function_registration *next;
+} vnet_hash_function_registration_t;
+
+typedef struct
+{
+  vnet_hash_function_registration_t *hash_registrations;
+} vnet_hash_main_t;
+
+extern vnet_hash_main_t vnet_hash_main;
+
+#define VNET_REGISTER_HASH_FUNCTION(x, ...)                                   \
+  __VA_ARGS__ vnet_hash_function_registration_t __vnet_hash_function_##x;     \
+  static void __clib_constructor __vnet_hash_function_registration_##x (void) \
+  {                                                                           \
+    vnet_hash_main_t *hm = &vnet_hash_main;                                   \
+    __vnet_hash_function_##x.next = hm->hash_registrations;                   \
+    hm->hash_registrations = &__vnet_hash_function_##x;                       \
+  }                                                                           \
+  __VA_ARGS__ vnet_hash_function_registration_t __vnet_hash_function_##x
+
+vnet_hash_func
+vnet_hash_function_from_default_type (vnet_hash_fn_type_t ftype);
+vnet_hash_func vnet_hash_function_from_type (vnet_hash_type_t htype,
+					     vnet_hash_fn_type_t ftype);
+vnet_hash_function_registration_t *
+vnet_hash_function_from_func (vnet_hash_func func, vnet_hash_fn_type_t ftype);
+format_function_t format_vnet_hash_type;
+format_function_t format_vnet_hash;
+unformat_function_t unformat_vnet_hash_type;
+
+#endif
