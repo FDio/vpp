FDio/vpp

commit e2fe097424fb169dfe01421ff17b8ccd0c26b4a6

fib: Source Address Selection
Type: feature

Use the FIB to provide SAS (in so far as it is today)
 - Use the glean adjacency as the record of the connected prefixes
 = there's a glean per-{interface, protocol, connected-prefix}
 - Keep the glean up to date with whatever the recieve host prefix is
(since it can change)

Signed-off-by: Neale Ranns <neale.ranns@cisco.com>
Change-Id: I0f3dd1edb1f3fc965af1c7c586709028eb9cdeac

src/plugins/ping/ping.c
@@ -18,7 +18,7 @@
 #include <vlib/vlib.h>
 #include <vnet/fib/ip6_fib.h>
 #include <vnet/fib/ip4_fib.h>
-#include <vnet/fib/fib_entry.h>
+#include <vnet/fib/fib_sas.h>
 #include <vnet/ip/ip6_link.h>
 #include <vnet/plugin/plugin.h>
 #include <vpp/app/version.h>
@@ -725,24 +725,22 @@ ip46_fill_l3_header (ip46_address_t * pa46, vlib_buffer_t * b0, int is_ip6)
     }
 }
 
-static int
+static bool
 ip46_set_src_address (u32 sw_if_index, vlib_buffer_t * b0, int is_ip6)
 {
-  int res;
+  bool res = false;
+
   if (is_ip6)
     {
       ip6_header_t *ip6 = vlib_buffer_get_current (b0);
-      res = ip6_src_address_for_packet (sw_if_index,
-					&ip6->dst_address, &ip6->src_address);
+
+      res = fib_sas6_get (sw_if_index, &ip6->dst_address, &ip6->src_address);
     }
   else
     {
-      ip4_main_t *im = &ip4_main;
       ip4_header_t *ip4 = vlib_buffer_get_current (b0);
-      res = ip4_src_address_for_packet (&im->lookup_main,
-					sw_if_index, &ip4->src_address);
-      /* IP4 and IP6 paths have the inverse logic. Harmonize. */
-      res = !res;
+
+      res = fib_sas4_get (sw_if_index, &ip4->dst_address, &ip4->src_address);
     }
   return res;
 }

src/plugins/igmp/igmp_pkt.c
@@ -16,6 +16,7 @@
  */
 
 #include <igmp/igmp_pkt.h>
+#include <vnet/fib/fib_sas.h>
 
 static void
 vlib_buffer_append (vlib_buffer_t * b, uword l)
@@ -76,8 +77,7 @@ igmp_pkt_build_ip_header (igmp_pkt_build_t * bk,
   ip4->protocol = IP_PROTOCOL_IGMP;
   ip4->tos = 0xc0;
 
-  ip4_src_address_for_packet (&ip4_main.lookup_main,
-			      bk->sw_if_index, &ip4->src_address);
+  fib_sas4_get (bk->sw_if_index, NULL, &ip4->src_address);
 
   vlib_buffer_append (b, sizeof (*ip4));
   bk->n_avail -= sizeof (*ip4);

src/plugins/unittest/fib_test.c
@@ -898,9 +898,6 @@ fib_test_v4 (void)
     adj = adj_get(ai);
     FIB_TEST((IP_LOOKUP_NEXT_GLEAN == adj->lookup_next_index),
              "attached interface adj is glean");
-    FIB_TEST((0 == ip46_address_cmp(&local_pfx.fp_addr,
-                                    &adj->sub_type.glean.receive_addr)),
-             "attached interface adj is receive ok");
 
     local_pfx.fp_len = 32;
     fib_table_entry_update_one_path(fib_index, &local_pfx,
@@ -937,6 +934,9 @@ fib_test_v4 (void)
                                              FIB_PROTOCOL_IP4,
                                              FIB_SOURCE_INTERFACE)),
              "2 Interface Source'd prefixes");
+    FIB_TEST((0 == ip46_address_cmp(&local_pfx.fp_addr,
+                                    &adj->sub_type.glean.rx_pfx.fp_addr)),
+             "attached interface adj is receive ok");
 
     /*
      * +2 interface routes +2 non-shared path-lists
@@ -4495,9 +4495,6 @@ fib_test_v6 (void)
     adj = adj_get(ai);
     FIB_TEST((IP_LOOKUP_NEXT_GLEAN == adj->lookup_next_index),
              "attached interface adj is glean");
-    FIB_TEST((0 == ip46_address_cmp(&local_pfx.fp_addr,
-                                    &adj->sub_type.glean.receive_addr)),
-             "attached interface adj is receive ok");
     dpo = fib_entry_contribute_ip_forwarding(fei);
     FIB_TEST((dpo->dpoi_index == ip6_fib_table_fwding_lookup(
                   1,
@@ -4535,6 +4532,9 @@ fib_test_v6 (void)
                   1,
                   &local_pfx.fp_addr.ip6)),
              "local-route; fwd and non-fwd tables match");
+    FIB_TEST((0 == ip46_address_cmp(&local_pfx.fp_addr,
+                                    &adj->sub_type.glean.rx_pfx.fp_addr)),
+             "attached interface adj is receive ok");
 
     /*
      * +2 entries. +2 unshared path-lists
@@ -5257,6 +5257,8 @@ fib_test_v6 (void)
 
     FIB_TEST((0 == adj_nbr_db_size()), "ADJ DB size is %d",
              adj_nbr_db_size());
+    FIB_TEST((0 == adj_glean_db_size()), "ADJ DB size is %d",
+             adj_glean_db_size());
 
     return (res);
 }

src/plugins/vrrp/node.c
@@ -12,6 +12,7 @@
 #include <vnet/ip/ip4_packet.h>
 #include <vnet/ip/ip6_link.h>
 #include <vnet/ethernet/arp_packet.h>
+#include <vnet/fib/fib_sas.h>
 #include <vppinfra/error.h>
 #include <vrrp/vrrp.h>
 #include <vrrp/vrrp_packet.h>
@@ -119,8 +120,7 @@ vrrp_vr_addr_cmp (vrrp_vr_t * vr, vrrp_header_t * pkt)
       peer_addr = &(((ip4_header_t *) pkt) - 1)->src_address;
       local_addr = &addr.ip4;
       addr_size = 4;
-      ip4_src_address_for_packet (&ip4_main.lookup_main,
-				  vrc->sw_if_index, local_addr);
+      fib_sas4_get (vrc->sw_if_index, NULL, local_addr);
     }
 
   return memcmp (local_addr, peer_addr, addr_size);

src/plugins/vrrp/vrrp_packet.c
@@ -14,6 +14,7 @@
 #include <vnet/adj/adj.h>
 #include <vnet/adj/adj_mcast.h>
 #include <vnet/fib/fib_table.h>
+#include <vnet/fib/fib_sas.h>
 #include <vnet/ip/igmp_packet.h>
 #include <vnet/ip/ip6_link.h>
 #include <vnet/ethernet/arp_packet.h>
@@ -107,8 +108,7 @@ vrrp_adv_l3_build (vrrp_vr_t * vr, vlib_buffer_t * b,
       ip4->ttl = 255;
       ip4->protocol = IP_PROTOCOL_VRRP;
       clib_memcpy (&ip4->dst_address, &dst->ip4, sizeof (dst->ip4));
-      ip4_src_address_for_packet (&ip4_main.lookup_main,
-				  vr->config.sw_if_index, &ip4->src_address);
+      fib_sas4_get (vr->config.sw_if_index, NULL, &ip4->src_address);
       ip4->length = clib_host_to_net_u16 (sizeof (*ip4) +
 					  vrrp_adv_payload_len (vr));
       ip4->checksum = ip4_header_checksum (ip4);
@@ -541,8 +541,7 @@ vrrp_igmp_pkt_build (vrrp_vr_t * vr, vlib_buffer_t * b)
 
   ip4 = vlib_buffer_get_current (b);
   clib_memcpy (ip4, &igmp_ip4_mcast, sizeof (*ip4));
-  ip4_src_address_for_packet (&ip4_main.lookup_main, vr->config.sw_if_index,
-			      &ip4->src_address);
+  fib_sas4_get (vr->config.sw_if_index, NULL, &ip4->src_address);
 
   vlib_buffer_chain_increase_length (b, b, sizeof (*ip4));
   vlib_buffer_advance (b, sizeof (*ip4));

src/vnet/CMakeLists.txt
@@ -1195,6 +1195,7 @@ list(APPEND VNET_SOURCES
   fib/fib_path_list.c
   fib/fib_path.c
   fib/fib_path_ext.c
+  fib/fib_sas.c
   fib/fib_source.c
   fib/fib_urpf_list.c
   fib/fib_attached_export.c
@@ -1213,6 +1214,7 @@ list(APPEND VNET_HEADERS
   fib/fib_node_list.h
   fib/fib_entry.h
   fib/fib_entry_delegate.h
+  fib/fib_sas.h
   fib/fib_source.h
 )
 

src/vnet/adj/adj.c
@@ -288,8 +288,7 @@ adj_last_lock_gone (ip_adjacency_t *adj)
 		       adj->rewrite_header.sw_if_index);
 	break;
     case IP_LOOKUP_NEXT_GLEAN:
-	adj_glean_remove(adj->ia_nh_proto,
-			 adj->rewrite_header.sw_if_index);
+	adj_glean_remove(adj);
 	break;
     case IP_LOOKUP_NEXT_MCAST_MIDCHAIN:
         adj_midchain_teardown(adj);

src/vnet/adj/adj.h
@@ -297,7 +297,7 @@ typedef struct ip_adjacency_t_
      */
     struct
     {
-      ip46_address_t receive_addr;
+      fib_prefix_t rx_pfx;
     } glean;
   } sub_type;
 

commit 8ee2be24b564b2596e10d40588be46fb6a8f7b86

ping: fix breakage around source address selection (sas)
https://gerrit.fd.io/r/c/vpp/+/30197 introduced SAS and inadvertently
broke ping in a variety of situations:

https://jira.fd.io/browse/VPP-1992
https://jira.fd.io/browse/VPP-1970
https://lists.fd.io/g/vpp-dev/topic/84038840

all of which seem to be rooted in situations where there's literally
nothing smarter ping can do for source address selection than
to pick the first IP on the interface.  This can happen for:

1. P2P interfaces, see attempted fix: https://gerrit.fd.io/r/c/vpp/+/32801
2. Interfaces with /32 IP addresses intentionally assigned

After some examination, it appears SAS is working as intended and
designed, and so is not the proper place to fix this problem.

Therefore, this fix simply has ping:
1. Try SAS first (because if it works, its clearly a better answer)
2. Fall back to the first IP of the correct AF on the interface

Ticket: VPP-1970
Ticket: VPP-1992
Fixes: e2fe097424fb169dfe01421ff17b8ccd0c26b4a6

Signed-off-by: Ed Warnicke <hagbard@gmail.com>
Change-Id: I9659f963778d1f07f787a8d8b38255e91692a7f8
Signed-off-by: Ed Warnicke <hagbard@gmail.com>

src/plugins/ping/ping.c
@@ -736,12 +736,32 @@ ip46_set_src_address (u32 sw_if_index, vlib_buffer_t * b0, int is_ip6)
       ip6_header_t *ip6 = vlib_buffer_get_current (b0);
 
       res = fib_sas6_get (sw_if_index, &ip6->dst_address, &ip6->src_address);
+      if (!res)
+	{
+	  ip6_address_t *tmp6;
+	  tmp6 = ip_interface_get_first_ip (sw_if_index, false);
+	  if (ip6)
+	    {
+	      ip6_address_copy (&ip6->src_address, tmp6);
+	      res = true;
+	    }
+	}
     }
   else
     {
       ip4_header_t *ip4 = vlib_buffer_get_current (b0);
 
       res = fib_sas4_get (sw_if_index, &ip4->dst_address, &ip4->src_address);
+      if (!res)
+	{
+	  ip6_address_t *tmp4;
+	  tmp4 = ip_interface_get_first_ip (sw_if_index, true);
+	  if (ip6)
+	    {
+	      ip6_address_copy (&ip4->src_address, tmp4);
+	      res = true;
+	    }
+	}
     }
   return res;
 }