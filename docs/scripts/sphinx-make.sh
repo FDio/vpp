b93782b36643714a8dcb53e2e396da642b1b0053
Dave Wallace <dwallacelf@gmail.com>
2021-08-17 15:11:43 -0400

    docs: fix missing dependency on pip-tools for docs-venv
    Type: fix
    
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>
    Change-Id: Iadb72fd0cb434a7ca2f6cbda7d78209f0746f0c6

--- docs/scripts/sphinx-make.sh
+++ docs/scripts/sphinx-make.sh
@@ -13,6 +13,7 @@ fi
 OS_ID=$(grep '^ID=' /etc/os-release  | cut -f2- -d= | sed -e 's/\"//g')
 OS_VERSION=$(grep '^VERSION_ID=' /etc/os-release  | cut -f2- -d= | sed -e 's/\"//g')
 PIP_VERSION=$(grep 'PIP_VERSION=' $WS_ROOT/test/Makefile | cut -d'=' -f2)
+PIP_TOOLS_VERSION=$(grep 'PIP_TOOLS_VERSION=' $WS_ROOT/test/Makefile | cut -d'=' -f2)
 
 if [ "$1" == "venv" ]
 then
@@ -21,6 +22,7 @@ then
     $PYTHON_INTERP -m venv $VENV_DIR
     source $VENV_DIR/bin/activate;
     $PYTHON_INTERP -m pip install pip==$PIP_VERSION
+    $PYTHON_INTERP -m pip install pip-tools==$PIP_TOOLS_VERSION
     $PYTHON_INTERP -m pip install -r $WS_ROOT/test/requirements-3.txt
 else
     [ -n "$(declare -f deactivate)" ] && deactivate
