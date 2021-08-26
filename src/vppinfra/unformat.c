d260dad239c5c5b1ca218e2c6706241226d7358b
Xiaoming Jiang <jiangxiaoming@outlook.com>
2021-08-26 08:57:33 +0000

    vppinfra: fix unformat matching with whole string may fail
    unformat function can't deal with whole string matching properly.
    for example: if input content is "abcde", the result of
    unformat(input, "ab") should be 0, but now 1
    
    Type: fix
    Signed-off-by: Xiaoming Jiang <jiangxiaoming@outlook.com>
    Change-Id: Ie42bd81fea36366bb66d596d1c0a8887031ca2e9

--- src/vppinfra/unformat.c
+++ src/vppinfra/unformat.c
@@ -703,14 +703,26 @@ match_input_with_format (unformat_input_t * input, const char *f)
   while (1)
     {
       cf = *f;
-      if (cf == 0 || cf == '%' || cf == ' ')
+      if (cf == '%' || cf == ' ')
 	break;
-      f++;
 
       ci = unformat_get_input (input);
 
-      if (cf != ci)
+      if (cf == 0)
+	{
+	  if (is_white_space (ci) || ci == UNFORMAT_END_OF_INPUT)
+	    {
+	      break;
+	    }
+	  else
+	    {
+	      return 0;
+	    }
+	}
+      else if (cf != ci)
 	return 0;
+
+      f++;
     }
   return f;
 }
