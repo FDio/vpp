d84e9ba2f2c467a0361ff672ce6271a3fe5b932d
Pim van Pelt <pim@ipng.nl>
2021-09-09 17:53:09 +0000

    linux-cp: Add VPP->Linux synchronization
    Part 1 -- notes in https://ipng.ch/s/articles/2021/08/13/vpp-2.html
    Add the ability for VPP to copy out (sync) its state from the dataplane
    to Linux Interface Pairs, when they exist. Gated by a configuration
    flag (linux-cp { lcp-sync }), and by a CLI option to toggle on/off,
    synchronize the following events:
    
    - Interface state changes
    - Interface MTU changes
    - Interface IPv4/IPv6 address add/deletion
    
    In VPP, subints can have any link state and MTU, orthogonal to their
    phy. In Linux, setting admin-down on a phy forces its children to be
    down as well. Also, in Linux, MTU of children must not exceed that of
    the phy. Add a state synchronizer which walks over phy+subints to
    ensure Linux and VPP end up in the same consistent state.
    
    Part 2 -- notes in https://ipng.ch/s/articles/2021/08/15/vpp-3.html
    Add the ability for VPP to autocreate sub-interfaces of existing Linux
    Interface pairs, when they exist. Gated by a configuration flag
    (linux-cp { lcp-auto-subint }), and by a CLI option to toggle on/off,
    synchronize the following events:
    
    - Sub-interface creation (dot1q, dot1ad, QinQ and QinAD)
    
    Other changes:
    - Add a few functions into netlink.[ch] to delete ip4 and ip6 addresses.
    - Remove a spurious logline (printing MTU).
    - Resolve a TODO around vnet_sw_interface_supports_addressing()
    
    Type: improvement
    Signed-off-by: Pim van Pelt <pim@ipng.nl>
    Change-Id: I34fc070e80af4013be58d7a8cbf64296cc760e4e

--- src/plugins/linux-cp/lcp.h
+++ src/plugins/linux-cp/lcp.h
@@ -24,8 +24,9 @@ typedef struct lcp_main_s
   u16 msg_id_base;		    /* API message ID base */
   u8 *default_namespace;	    /* default namespace if set */
   int default_ns_fd;
-  /* Set when Unit testing */
-  u8 test_mode;
+  u8 lcp_auto_subint; /* Automatically create/delete LCP sub-interfaces */
+  u8 lcp_sync;	      /* Automatically sync VPP changes to LCP */
+  u8 test_mode;	      /* Set when Unit testing */
 } lcp_main_t;
 
 extern lcp_main_t lcp_main;
