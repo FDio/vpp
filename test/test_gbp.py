b10e154c5b9a68e52ae8d75bacb726f12613e825
Dave Wallace <dwallacelf@gmail.com>
2021-09-14 16:24:01 -0400

    tests: relicense
    - Convert python test code from Apache 2.0 license to
      a dual Apache / GPLv2 license as required by use of
      scapy in framework.py
    
    Change-Id: I54756917edd14ffead0501b2e004a02ab21a0ca1
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- test/test_gbp.py
+++ test/test_gbp.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
+# Copyright (c) 2021 Cisco and/or its affiliates.
+#
+# SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+#
+# Licensed under the Apache License 2.0 or
+# GNU General Public License v2.0 or later;  you may not use this file
+# except in compliance with one of these Licenses. You
+# may obtain a copy of the Licenses at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#     https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+#
+# Note: If this file is linked with Scapy, which is GPLv2+, your use of it
+# must be under GPLv2+.  If at any point in the future it is no longer linked
+# with Scapy (or other GPLv2+ licensed software), you are free to choose
+# Apache 2.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import typing
 from socket import AF_INET6, inet_pton, inet_ntop
 import unittest
