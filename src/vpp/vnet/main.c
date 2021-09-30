bc4292328f80535efd11e927ee2c9f062b0b2847
Damjan Marion <damarion@cisco.com>
2021-09-30 20:04:14 +0200

    build: respect VPP_LIBRARY_DIR in plugin path
    target triplet is only used in the library path, so instead of using it
    repeatedly in the same places, just include it the library path. This
    also allows calling cmake with -DCMAKE_INSTALL_LIBDIR to override as
    needed.
    
    Type: fix
    Change-Id: I33f364fda88914f88f9b976cb83e6d3ff466f0bb
    Signed-off-by: Damjan Marion <damarion@cisco.com>
    Signed-off-by: Nick Brown <nickbroon@gmail.com>

--- src/vpp/vnet/main.c
+++ src/vpp/vnet/main.c
@@ -60,13 +60,17 @@ vpp_find_plugin_path ()
     return;
   *p = 0;
 
-  s = format (0, "%s/lib/" CLIB_TARGET_TRIPLET "/vpp_plugins:"
-	      "%s/lib/vpp_plugins", path, path);
+  s = format (0,
+	      "%s/" CLIB_LIB_DIR "/vpp_plugins:"
+	      "%s/lib/vpp_plugins",
+	      path, path);
   vec_add1 (s, 0);
   vlib_plugin_path = (char *) s;
 
-  s = format (0, "%s/lib/" CLIB_TARGET_TRIPLET "/vpp_api_test_plugins:"
-	      "%s/lib/vpp_api_test_plugins", path, path);
+  s = format (0,
+	      "%s/" CLIB_LIB_DIR "/vpp_api_test_plugins:"
+	      "%s/lib/vpp_api_test_plugins",
+	      path, path);
   vec_add1 (s, 0);
   vat_plugin_path = (char *) s;
 }
