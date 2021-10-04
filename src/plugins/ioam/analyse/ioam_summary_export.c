b967cb0388103d86c82709b278e015e641b06acf
Paul Atkins <patkins@graphiant.com>
2021-10-04 15:43:56 +0100

    ipfix-export: Change exp to use ip_address
    Modify the ipfix_exporter to use ip_address instead of the ipv4 specific
    version. Modify the current code so that it writes into the v4 specific
    part of the address, i.e. we are not yet fully supporting IPv6.  For the
    exporter configured via the original API (the one that is always in slot0)
    we will not support IPv6 addresses.
    
    Type: improvement
    Signed-off-by: Paul Atkins <patkins@graphiant.com>
    Change-Id: Ic9854ac62aaee76a7a55a958234c456fd9828c4c

--- src/plugins/ioam/analyse/ioam_summary_export.c
+++ src/plugins/ioam/analyse/ioam_summary_export.c
@@ -72,8 +72,8 @@ ioam_template_rewrite (ipfix_exporter_t *exp, flow_report_t *fr,
   ip->ip_version_and_header_length = 0x45;
   ip->ttl = 254;
   ip->protocol = IP_PROTOCOL_UDP;
-  ip->src_address.as_u32 = exp->src_address.as_u32;
-  ip->dst_address.as_u32 = exp->ipfix_collector.as_u32;
+  ip->src_address.as_u32 = exp->src_address.ip.ip4.as_u32;
+  ip->dst_address.as_u32 = exp->ipfix_collector.ip.ip4.as_u32;
   udp->src_port = clib_host_to_net_u16 (collector_port);
   udp->dst_port = clib_host_to_net_u16 (UDP_DST_PORT_ipfix);
   udp->length = clib_host_to_net_u16 (vec_len (rewrite) - sizeof (*ip));
