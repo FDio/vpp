eefcbeaf338711f276892d3d891bb4ab413d4e29
Mohsin Kazmi <sykazmi@cisco.com>
2021-10-05 13:52:42 +0000

    process: vpp process privileges and capabilities
    Type: feature
    
    DBGvpp# show privilege
    VPP process group id 0
    VPP process user id 0
    DBGvpp# create tap
    tap0
    DBGvpp# create tap
    tap1
    DBGvpp# set privilege gid 1000 uid 1000
    DBGvpp#
    DBGvpp# show privilege
    VPP process group id 1000
    VPP process user id 1000
    DBGvpp# create tap
    tap                  [error ]: tap2: tap_create_if: ioctl(TUNSETIFF): Operation not permitted
    tap                  [error ]: tap2: tap_create_if: ioctl(TUNSETIFF): Operation not permitted
    tap                  [error ]: tap2: tap_free: ioctl(TUNSETPERSIST): File descriptor in bad state
    create tap: ioctl(TUNSETIFF): Operation not permitted
    
    Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>
    Change-Id: I593d9557ed091a370e25dea061ad96cbe664b2a1

---
+++ src/plugins/process/privileges.c
@@ -0,0 +1,54 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright(c) 2021 Cisco Systems, Inc.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <plugins/process/process.h>
+#include <vlib/vlib.h>
+#include <vppinfra/format.h>
+
+int
+process_drop_privileges (u32 gid, u32 uid, char *chroot_dir)
+{
+  ASSERT (gid != 0 || uid != 0);
+
+  if (chroot_dir)
+    {
+      struct stat st = { 0 };
+      if (stat (chroot_dir, &st) == -1)
+	{
+	  if (mkdir (chroot_dir, 0700) != 0)
+	    return PROCESS_API_ERROR_MKDIR;
+	}
+      if (chown (chroot_dir, uid, gid) != 0)
+	return PROCESS_API_ERROR_CHOWN;
+      if (chdir (chroot_dir) != 0)
+	return PROCESS_API_ERROR_CHDIR;
+      if (chroot (chroot_dir) != 0)
+	return PROCESS_API_ERROR_CHROOT;
+    }
+
+  if (getuid () == 0)
+    {
+      if (setgid (gid) != 0)
+	return PROCESS_API_ERROR_SETGID;
+      if (setuid (uid) != 0)
+	return PROCESS_API_ERROR_SETUID;
+    }
+
+  if (setuid (0) != -1)
+    return PROCESS_API_ERROR_PRIVILEGES_REGAINED;
+
+  return 0;
+}
+
+void
+process_get_privileges (vlib_main_t *vm, u32 *gid, u32 *uid)
+{
+  *gid = getgid ();
+  *uid = getuid ();
+}
