31f192434660a6fe71663fd62172cb82fc79ede1
Andrew Yourtchenko <ayourtch@gmail.com>
2021-11-15 00:40:18 +0100

    misc: vppctl - remove the dependency on vppinfra
    56f54af21d18f9fdd471b81db77a3942b0aa4d9c introduced the new memcpy.
    It also made a vector support mandatory in order to compile anything
    that depends on vppinfra.
    
    This broke extras/scripts/build_static_vppctl.sh
    
    Since the vppctl is just a two-pronged epoll+basic telnet client handler,
    remove dependencies on the vpp infra libs, and trim the build script accordingly.
    
    Change-Id: I394bc65c485cbf8e7143a818ca0c86367bb15d90
    Type: improvement
    Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>

--- extras/scripts/build_static_vppctl.sh
+++ extras/scripts/build_static_vppctl.sh
@@ -3,25 +3,13 @@
 src=$(realpath $(dirname $0)/../..)/src
 
 ${CC:-cc} \
+ -Wall \
+ -Werror \
  -O2 \
  -flto \
  -static \
  -I ${src} \
- ${src}/vppinfra/backtrace.c \
- ${src}/vppinfra/dlmalloc.c \
- ${src}/vppinfra/elf.c \
- ${src}/vppinfra/elf_clib.c \
- ${src}/vppinfra/error.c \
- ${src}/vppinfra/format.c \
- ${src}/vppinfra/hash.c \
- ${src}/vppinfra/mem.c \
- ${src}/vppinfra/mem_dlmalloc.c \
- ${src}/vppinfra/std-formats.c \
- ${src}/vppinfra/string.c \
- ${src}/vppinfra/socket.c \
- ${src}/vppinfra/vec.c \
- ${src}/vppinfra/unformat.c \
- ${src}/vppinfra/unix-misc.c \
- ${src}/vppinfra/linux/mem.c \
+ -g \
  ${src}/vpp/app/vppctl.c \
  -o vppctl
+
