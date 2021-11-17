162dfc1b6d5d967ad7c63000625c64a9927c30b5
Benoît Ganne <bganne@cisco.com>
2021-11-17 17:55:50 +0100

    perfmon: fix non-NULL terminated C-string
    format() expects a NULL-terminated C-string as format string.
    
    Type: fix
    
    Change-Id: Ib428cf2debbf98850eed512907175f8ae8ba3c04
    Signed-off-by: Benoît Ganne <bganne@cisco.com>

--- src/plugins/perfmon/intel/uncore.c
+++ src/plugins/perfmon/intel/uncore.c
@@ -63,7 +63,7 @@ intel_uncore_instance_name_cmp (void *v1, void *v2)
   return strcmp (i1->name, i2->name);
 }
 
-static_always_inline u8 *
+static u8 *
 format_instance_name (intel_uncore_unit_type_t u, char *unit_fmt, u8 socket_id,
 		      u8 ubox)
 {
@@ -78,7 +78,7 @@ format_instance_name (intel_uncore_unit_type_t u, char *unit_fmt, u8 socket_id,
 	{
 	  u8 *fmt = 0;
 
-	  fmt = format (0, "%s (%s)", unit_fmt, (n->unit_names[ubox]));
+	  fmt = format (0, "%s (%s)%c", unit_fmt, (n->unit_names[ubox]), 0);
 	  s = format (0, (char *) fmt, socket_id, ubox);
 	  vec_free (fmt);
 
