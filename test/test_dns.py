a4550b77f718e209e59c2354d73384d54ee88160
Daniel Béreš <daniel.beres@pantheon.tech>
2021-11-24 15:08:11 +0000

    dns: support AAAA over IPV4
    Client should be able get AAAA records from ipv4 upstream.
    Ticket: VPP-1963
    Type: fix
    
    Change-Id: I64509ed02ab480cd747fb0740e0a53b609612767
    Signed-off-by: Daniel Béreš <daniel.beres@pantheon.tech>

--- test/test_dns.py
+++ test/test_dns.py
@@ -3,14 +3,10 @@
 import unittest
 
 from framework import VppTestCase, VppTestRunner
-from vpp_ip_route import VppIpTable, VppIpRoute, VppRoutePath
 from ipaddress import *
 
-import scapy.compat
-from scapy.contrib.mpls import MPLS
-from scapy.layers.inet import IP, UDP, TCP, ICMP, icmptypes, icmpcodes
+from scapy.layers.inet import IP, UDP
 from scapy.layers.l2 import Ether
-from scapy.packet import Raw
 from scapy.layers.dns import DNSRR, DNS, DNSQR
 
 
@@ -28,7 +24,7 @@ class TestDns(VppTestCase):
     def setUp(self):
         super(TestDns, self).setUp()
 
-        self.create_pg_interfaces(range(1))
+        self.create_pg_interfaces(range(2))
 
         for i in self.pg_interfaces:
             i.admin_up()
@@ -38,21 +34,43 @@ class TestDns(VppTestCase):
     def tearDown(self):
         super(TestDns, self).tearDown()
 
-    def create_stream(self, src_if):
+    def create_stream_req(self, src_if):
         """Create input packet stream for defined interface.
 
         :param VppInterface src_if: Interface to create packet stream for.
         """
-        good_request = (Ether(dst=src_if.local_mac, src=src_if.remote_mac) /
-                        IP(src=src_if.remote_ip4) /
-                        UDP(sport=1234, dport=53) /
-                        DNS(rd=1, qd=DNSQR(qname="bozo.clown.org")))
+        good_request_static = (
+                    Ether(dst=src_if.local_mac, src=src_if.remote_mac) /
+                    IP(src=src_if.remote_ip4, dst=src_if.local_ip4) /
+                    UDP(sport=1234, dport=53) /
+                    DNS(rd=1, qd=DNSQR(qname="bozo.clown.org")))
 
-        bad_request = (Ether(dst=src_if.local_mac, src=src_if.remote_mac) /
-                       IP(src=src_if.remote_ip4) /
-                       UDP(sport=1234, dport=53) /
-                       DNS(rd=1, qd=DNSQR(qname="no.clown.org")))
-        pkts = [good_request, bad_request]
+        good_request_AAAA_static = (
+                    Ether(dst=src_if.local_mac, src=src_if.remote_mac) /
+                    IP(src=src_if.remote_ip4, dst=src_if.local_ip4) /
+                    UDP(sport=1234, dport=53) /
+                    DNS(rd=1, qd=DNSQR(qname="bozo.clown.org", qtype="AAAA")))
+
+        good_request_dynamic = (
+                    Ether(dst=src_if.local_mac, src=src_if.remote_mac) /
+                    IP(src=src_if.remote_ip4, dst=src_if.local_ip4) /
+                    UDP(sport=1234, dport=53) /
+                    DNS(rd=1, qd=DNSQR(qname="no.clown.org")))
+        pkts = [good_request_static,
+                good_request_AAAA_static,
+                good_request_dynamic]
+        return pkts
+
+    def create_stream_res(self, src_if, dst_if):
+        """Response from DNS server to VPP. """
+        answer = (
+            Ether(dst=src_if.local_mac, src=src_if.remote_mac) /
+            IP(src=src_if.remote_ip4, dst=src_if.local_ip4) /
+            UDP(sport=53, dport=53053) /
+            DNS(id=1, qr=1, ra=1, rd=1,
+                qd=DNSQR(qname="no.clown.org", qtype="A"),
+                an=DNSRR(rrname="no.clown.org", ttl=600, rdata="4.3.2.1")))
+        pkts = [answer]
         return pkts
 
     def verify_capture(self, dst_if, capture):
@@ -63,47 +81,52 @@ class TestDns(VppTestCase):
         :param list capture: Captured packet stream.
         """
         self.logger.info("Verifying capture on interface %s" % dst_if.name)
-        for packet in capture:
-            dns = packet[DNS]
-            self.assertEqual(dns.an[0].rdata, '1.2.3.4')
+        dns = capture[0][DNS]
+        self.assertEqual(dns.an[0].rdata, '1.2.3.4')
+        dns = capture[1][DNS]
+        self.assertEqual(dns.an[0].rdata, '2001:db8:1::2')
+        dns = capture[2][DNS]
+        self.assertEqual(dns.an[0].rdata, '4.3.2.1')
 
     def test_dns_unittest(self):
         """ DNS Name Resolver Basic Functional Test """
 
-        # Set up an upstream name resolver. We won't actually go there
+        # Set up an upstream name resolver
         self.vapi.dns_name_server_add_del(
-            is_ip6=0, is_add=1, server_address=IPv4Address(u'8.8.8.8').packed)
+            is_ip6=0, is_add=1,
+            server_address=IPv4Address(self.pg1.remote_ip4).packed)
 
         # Enable name resolution
         self.vapi.dns_enable_disable(enable=1)
 
         # Manually add a static dns cache entry
-        self.logger.info(self.vapi.cli("dns cache add bozo.clown.org 1.2.3.4"))
+        self.logger.info(self.vapi.cli(
+            "dns cache add bozo.clown.org 1.2.3.4 2001:db8:1::2"))
 
         # Test the binary API
         rv = self.vapi.dns_resolve_name(name=b'bozo.clown.org')
         self.assertEqual(rv.ip4_address, IPv4Address(u'1.2.3.4').packed)
 
-        # Configure 127.0.0.1/8 on the pg interface
-        self.vapi.sw_interface_add_del_address(
-            sw_if_index=self.pg0.sw_if_index,
-            prefix="127.0.0.1/8")
-
         # Send a couple of DNS request packets, one for bozo.clown.org
-        # and one for no.clown.org which won't resolve
+        # and one for no.clown.org which will ask DNS server
 
-        pkts = self.create_stream(self.pg0)
+        pkts = self.create_stream_req(self.pg0)
         self.pg0.add_stream(pkts)
+
+        res_pkts = self.create_stream_res(self.pg1, self.pg0)
+        self.pg1.add_stream(res_pkts)
         self.pg_enable_capture(self.pg_interfaces)
 
         self.pg_start()
-        pkts = self.pg0.get_capture(1)
+        pkts = self.pg0.get_capture(3)
+        res_pkts = self.pg1.get_capture(1)
         self.verify_capture(self.pg0, pkts)
 
         # Make sure that the cache contents are correct
         str = self.vapi.cli("show dns cache verbose")
         self.assertIn('1.2.3.4', str)
-        self.assertIn('[P] no.clown.org:', str)
+        self.assertIn('2001:db8:1::2', str)
+        self.assertIn('4.3.2.1', str)
 
 if __name__ == '__main__':
     unittest.main(testRunner=VppTestRunner)
