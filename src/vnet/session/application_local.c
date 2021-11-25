d8dffb118c95703f85bf914328f00b4fbfbb6867
Florin Coras <fcoras@cisco.com>
2021-11-25 12:14:25 -0800

    session: only notify server on ct connect failure
    Type: fix
    
    Signed-off-by: Florin Coras <fcoras@cisco.com>
    Change-Id: Ifcb631ed2913e5f4fc318238de9100286a7990d2

--- src/vnet/session/application_local.c
+++ src/vnet/session/application_local.c
@@ -322,6 +322,13 @@ done:
   clib_rwlock_writer_unlock (&cm->app_segs_lock);
 }
 
+static void
+ct_session_force_disconnect_server (ct_connection_t *sct)
+{
+  sct->peer_index = ~0;
+  session_transport_closing_notify (&sct->connection);
+}
+
 int
 ct_session_connect_notify (session_t *ss, session_error_t err)
 {
@@ -341,9 +348,7 @@ ct_session_connect_notify (session_t *ss, session_error_t err)
   /* Client closed while waiting for reply from server */
   if (PREDICT_FALSE (!cct))
     {
-      session_transport_closing_notify (&sct->connection);
-      session_transport_delete_notify (&sct->connection);
-      ct_connection_free (sct);
+      ct_session_force_disconnect_server (sct);
       return 0;
     }
 
@@ -354,8 +359,10 @@ ct_session_connect_notify (session_t *ss, session_error_t err)
     goto connect_error;
 
   /*
-   * Alloc client session
+   * Alloc client session, server session assumed to be established
    */
+
+  ASSERT (ss->session_state >= SESSION_STATE_READY);
 
   cs = session_alloc (thread_index);
   ss = session_get (ss_index, thread_index);
@@ -372,7 +379,7 @@ ct_session_connect_notify (session_t *ss, session_error_t err)
   if ((err = app_worker_init_connected (client_wrk, cs)))
     {
       session_free (cs);
-      session_close (ss);
+      ct_session_force_disconnect_server (sct);
       err = SESSION_E_ALLOC;
       goto connect_error;
     }
@@ -383,7 +390,7 @@ ct_session_connect_notify (session_t *ss, session_error_t err)
     {
       segment_manager_dealloc_fifos (cs->rx_fifo, cs->tx_fifo);
       session_free (cs);
-      session_close (ss);
+      ct_session_force_disconnect_server (sct);
       goto cleanup_client;
     }
 
@@ -397,8 +404,6 @@ connect_error:
   app_worker_connect_notify (client_wrk, 0, err, cct->client_opaque);
 
 cleanup_client:
-
-  sct->peer_index = ~0;
 
   if (cct->client_rx_fifo)
     ct_session_dealloc_fifos (cct, cct->client_rx_fifo, cct->client_tx_fifo);
