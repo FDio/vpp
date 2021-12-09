FDio/vpp

commit fa1fb608b6092fd057b993cfd779bd9ee8306974

dpdk: enable vmxnet3 l4 checksum offload
Type: improvement

Signed-off-by: Florin Coras <fcoras@cisco.com>
Change-Id: Ia25e671084dd2c0010c0577649bf51ba6495b6ac

src/plugins/dpdk/device/init.c
@@ -622,6 +622,12 @@ dpdk_lib_init (dpdk_main_t * dm)
 	    case VNET_DPDK_PMD_VMXNET3:
 	      xd->port_type = VNET_DPDK_PORT_TYPE_ETH_1G;
 	      xd->port_conf.txmode.offloads |= DEV_TX_OFFLOAD_MULTI_SEGS;
+	      if (dm->conf->no_tx_checksum_offload == 0)
+		{
+		  xd->port_conf.txmode.offloads |= DEV_TX_OFFLOAD_TCP_CKSUM;
+		  xd->port_conf.txmode.offloads |= DEV_TX_OFFLOAD_UDP_CKSUM;
+		  xd->flags |= DPDK_DEVICE_FLAG_TX_OFFLOAD;
+		}
 	      break;
 
 	    case VNET_DPDK_PMD_AF_PACKET:

commit 1c8153f11c92f86d333b5ac0bae4213492500f8f

dpdk: enable ip csum offload for vmxnet3
Otherwise tcp csum offload fails although udp seems to work.

Type: fix
Fixes: fa1fb60

Signed-off-by: Florin Coras <fcoras@cisco.com>
Change-Id: Ie0651887b09920365806eaad776b0d13059faee8

src/plugins/dpdk/device/init.c
@@ -624,6 +624,7 @@ dpdk_lib_init (dpdk_main_t * dm)
 	      xd->port_conf.txmode.offloads |= DEV_TX_OFFLOAD_MULTI_SEGS;
 	      if (dm->conf->no_tx_checksum_offload == 0)
 		{
+		  xd->port_conf.txmode.offloads |= DEV_TX_OFFLOAD_IPV4_CKSUM;
 		  xd->port_conf.txmode.offloads |= DEV_TX_OFFLOAD_TCP_CKSUM;
 		  xd->port_conf.txmode.offloads |= DEV_TX_OFFLOAD_UDP_CKSUM;
 		  xd->flags |= DPDK_DEVICE_FLAG_TX_OFFLOAD;