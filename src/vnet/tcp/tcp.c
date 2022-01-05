539bea9cfe6ea8b14bd08eeec7697d567548ff60
Florin Coras <fcoras@cisco.com>
2022-01-04 21:44:53 -0800

    tcp: improvements
    Signed-off-by: Florin Coras <fcoras@cisco.com>
    Change-Id: I784613fecdca9e8fc0e261559bd033b936b6e077

--- src/vnet/tcp/tcp.c
+++ src/vnet/tcp/tcp.c
@@ -1379,6 +1379,8 @@ tcp_connection_tx_pacer_update (tcp_connection_t * tc)
 
   f64 srtt = clib_min ((f64) tc->srtt * TCP_TICK, tc->mrtt_us);
 
+  //  clib_warning ("pacing rate is %u srtt %.2f cwnd %u",
+  //  tcp_cc_get_pacing_rate (tc), srtt, tc->cwnd);
   transport_connection_tx_pacer_update (&tc->connection,
 					tcp_cc_get_pacing_rate (tc),
 					srtt * CLIB_US_TIME_FREQ);
@@ -1389,6 +1391,8 @@ tcp_connection_tx_pacer_reset (tcp_connection_t * tc, u32 window,
 			       u32 start_bucket)
 {
   f64 srtt = clib_min ((f64) tc->srtt * TCP_TICK, tc->mrtt_us);
+  //  clib_warning ("pacing rate is %u srtt %.2f cwnd %u",
+  //  tcp_cc_get_pacing_rate (tc), srtt, tc->cwnd);
   transport_connection_tx_pacer_reset (&tc->connection,
 				       tcp_cc_get_pacing_rate (tc),
 				       start_bucket,
--- src/vnet/tcp/tcp_input.c
+++ src/vnet/tcp/tcp_input.c
@@ -692,6 +692,7 @@ tcp_cc_init_congestion (tcp_connection_t * tc)
 static void
 tcp_cc_congestion_undo (tcp_connection_t * tc)
 {
+  clib_warning ("undo");
   tc->cwnd = tc->prev_cwnd;
   tc->ssthresh = tc->prev_ssthresh;
   tcp_cc_undo_recovery (tc);
@@ -751,6 +752,7 @@ tcp_cc_try_recover (tcp_connection_t *tc)
 
   if (tcp_cc_is_spurious_retransmit (tc))
     {
+      clib_warning ("spurious");
       tcp_cc_congestion_undo (tc);
       is_spurious = 1;
     }
@@ -762,6 +764,9 @@ tcp_cc_try_recover (tcp_connection_t *tc)
    * of the current recovery event with an updated snd_congestion */
   if (tc->sack_sb.sacked_bytes)
     {
+      tcp_recovery_off (tc);
+      clib_warning ("left congested");
+      tcp_cc_congestion (tc);
       tc->snd_congestion = tc->snd_nxt;
       return -1;
     }
