0b64a4f526cbd35fb9bedba58176b6f711cdd034
Florin Coras <fcoras@cisco.com>
2022-01-04 21:44:53 -0800

    tcp: improvements
    Signed-off-by: Florin Coras <fcoras@cisco.com>
    Change-Id: I784613fecdca9e8fc0e261559bd033b936b6e077

--- src/vnet/tcp/tcp_input.c
+++ src/vnet/tcp/tcp_input.c
@@ -692,6 +692,7 @@ tcp_cc_init_congestion (tcp_connection_t * tc)
 static void
 tcp_cc_congestion_undo (tcp_connection_t * tc)
 {
+  clib_warning ("undo");
   tc->cwnd = tc->prev_cwnd;
   tc->ssthresh = tc->prev_ssthresh;
   tcp_cc_undo_recovery (tc);
@@ -751,6 +752,7 @@ tcp_cc_recover (tcp_connection_t * tc)
 
   if (tcp_cc_is_spurious_retransmit (tc))
     {
+      clib_warning ("spurious");
       tcp_cc_congestion_undo (tc);
       is_spurious = 1;
     }
@@ -762,6 +764,9 @@ tcp_cc_recover (tcp_connection_t * tc)
    * of the current recovery event with an updated snd_congestion */
   if (tc->sack_sb.sacked_bytes)
     {
+      tcp_recovery_off (tc);
+      clib_warning ("left congested");
+      tcp_cc_congestion (tc);
       tc->snd_congestion = tc->snd_nxt;
       tcp_program_retransmit (tc);
       return is_spurious;
@@ -866,8 +871,7 @@ tcp_cc_handle_event (tcp_connection_t * tc, tcp_rate_sample_t * rs,
 	tcp_fastrecovery_first_on (tc);
 
       tc->rxt_delivered += tc->sack_sb.rxt_sacked;
-      tc->prr_delivered += tc->bytes_acked + tc->sack_sb.last_sacked_bytes
-	- tc->sack_sb.last_bytes_delivered;
+      tc->prr_delivered += rs->delivered;
     }
   else
     {
@@ -903,6 +907,8 @@ tcp_cc_handle_event (tcp_connection_t * tc, tcp_rate_sample_t * rs,
 	  return;
 	}
 
+      if (tcp_in_cong_recovery (tc))
+	clib_warning ("HOW??");
       /* Treat as congestion avoidance ack */
       tcp_cc_rcv_ack (tc, rs);
       return;
@@ -924,9 +930,13 @@ tcp_cc_handle_event (tcp_connection_t * tc, tcp_rate_sample_t * rs,
    * reset dupacks to 0. Also needed if in congestion recovery */
   tc->rcv_dupacks = 0;
 
-  if (tcp_in_recovery (tc))
-    tcp_cc_rcv_ack (tc, rs);
-  else
+//  if (tcp_in_recovery (tc))
+//    tcp_cc_rcv_ack (tc, rs);
+//  else
+//  tcp_cc_rcv_cong_ack (tc, TCP_CC_PARTIALACK, rs);
+
+  /* In (fast)recovery with sacks cwnd is controlled by prr */
+  if (!has_sack)
     tcp_cc_rcv_cong_ack (tc, TCP_CC_PARTIALACK, rs);
 }
 
@@ -1042,6 +1052,9 @@ process_ack:
 
   if (tc->cfg_flags & TCP_CFG_F_RATE_SAMPLE)
     tcp_bt_sample_delivery_rate (tc, &rs);
+  else
+    rs.delivered = tc->bytes_acked + tc->sack_sb.last_sacked_bytes
+	- tc->sack_sb.last_bytes_delivered;
 
   if (tc->bytes_acked + tc->sack_sb.last_sacked_bytes)
     {
