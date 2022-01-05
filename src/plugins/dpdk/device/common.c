FDio/vpp

commit dfc853c0ac2f8f15765a51268a53b023d4cf6c65

dpdk: keep buffer flags per-device
Type: improvement
Change-Id: Idb0b079df49c12643c9a93ee0effe011d3489068
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/plugins/dpdk/device/common.c
@@ -147,7 +147,12 @@ dpdk_device_setup (dpdk_device_t * xd)
     goto error;
 
   rte_eth_dev_set_mtu (xd->port_id, hi->max_packet_bytes);
+  xd->buffer_flags =
+    (VLIB_BUFFER_TOTAL_LENGTH_VALID | VLIB_BUFFER_EXT_HDR_VALID);
 
+  if (xd->port_conf.rxmode.offloads & DEV_RX_OFFLOAD_TCP_CKSUM)
+    xd->buffer_flags |=
+      (VNET_BUFFER_F_L4_CHECKSUM_COMPUTED | VNET_BUFFER_F_L4_CHECKSUM_CORRECT);
   if (xd->flags & DPDK_DEVICE_FLAG_ADMIN_UP)
     dpdk_device_start (xd);
 

src/plugins/dpdk/device/node.c
@@ -376,6 +376,7 @@ dpdk_device_input (vlib_main_t * vm, dpdk_main_t * dm, dpdk_device_t * xd,
   /* Update buffer template */
   vnet_buffer (bt)->sw_if_index[VLIB_RX] = xd->sw_if_index;
   bt->error = node->errors[DPDK_ERROR_NONE];
+  bt->flags = xd->buffer_flags;
   /* as DPDK is allocating empty buffers from mempool provided before interface
      start for each queue, it is safe to store this in the template */
   bt->buffer_pool_index = rxq->buffer_pool_index;

src/plugins/dpdk/device/dpdk.h
@@ -176,6 +176,7 @@ typedef struct
 
   u32 hw_if_index;
   u32 sw_if_index;
+  u32 buffer_flags;
 
   /* next node index if we decide to steal the rx graph arc */
   u32 per_interface_next_index;
@@ -330,13 +331,9 @@ typedef struct
 
 typedef struct
 {
-
   /* Devices */
   dpdk_device_t *devices;
   dpdk_per_thread_data_t *per_thread_data;
-
-  /* buffer flags template, configurable to enable/disable tcp / udp cksum */
-  u32 buffer_flags_template;
 
   /*
    * flag indicating that a posted admin up/down

src/plugins/dpdk/device/init.c
@@ -230,10 +230,6 @@ dpdk_lib_init (dpdk_main_t * dm)
   if (CLIB_DEBUG > 0)
     dpdk_log_notice ("DPDK drivers found %d ports...", nports);
 
-  if (dm->conf->enable_tcp_udp_checksum)
-    dm->buffer_flags_template &= ~(VNET_BUFFER_F_L4_CHECKSUM_CORRECT
-				   | VNET_BUFFER_F_L4_CHECKSUM_COMPUTED);
-
   /* vlib_buffer_t template */
   vec_validate_aligned (dm->per_thread_data, tm->n_vlib_mains - 1,
 			CLIB_CACHE_LINE_BYTES);
@@ -241,7 +237,6 @@ dpdk_lib_init (dpdk_main_t * dm)
     {
       dpdk_per_thread_data_t *ptd = vec_elt_at_index (dm->per_thread_data, i);
       clib_memset (&ptd->buffer_template, 0, sizeof (vlib_buffer_t));
-      ptd->buffer_template.flags = dm->buffer_flags_template;
       vnet_buffer (&ptd->buffer_template)->sw_if_index[VLIB_TX] = (u32) ~ 0;
     }
 
@@ -1886,12 +1881,6 @@ dpdk_init (vlib_main_t * vm)
   dm->conf = &dpdk_config_main;
 
   vec_add1 (dm->conf->eal_init_args, (u8 *) "vnet");
-
-  /* Default vlib_buffer_t flags, DISABLES tcp/udp checksumming... */
-  dm->buffer_flags_template = (VLIB_BUFFER_TOTAL_LENGTH_VALID |
-			       VLIB_BUFFER_EXT_HDR_VALID |
-			       VNET_BUFFER_F_L4_CHECKSUM_COMPUTED |
-			       VNET_BUFFER_F_L4_CHECKSUM_CORRECT);
 
   dm->stat_poll_interval = DPDK_STATS_POLL_INTERVAL;
   dm->link_state_poll_interval = DPDK_LINK_POLL_INTERVAL;

commit 4c76e96ab37b64ffff82e7f360811b54f5968972

dpdk: fix checksum computation
Type: fix
Fixes: dfc853c

Based on gerrit 26480 by chenmin.sun@intel.com

Signed-off-by: Florin Coras <fcoras@cisco.com>
Change-Id: Id8f2127f0fac5f555b38483714fe92c038875915

src/plugins/dpdk/device/common.c
@@ -146,11 +146,9 @@ dpdk_device_setup (dpdk_device_t * xd)
 
   rte_eth_dev_set_mtu (xd->port_id, hi->max_packet_bytes);
   xd->buffer_flags =
-    (VLIB_BUFFER_TOTAL_LENGTH_VALID | VLIB_BUFFER_EXT_HDR_VALID);
+    (VLIB_BUFFER_TOTAL_LENGTH_VALID | VLIB_BUFFER_EXT_HDR_VALID |
+     VNET_BUFFER_F_L4_CHECKSUM_COMPUTED | VNET_BUFFER_F_L4_CHECKSUM_CORRECT);
 
-  if (xd->port_conf.rxmode.offloads & DEV_RX_OFFLOAD_TCP_CKSUM)
-    xd->buffer_flags |=
-      (VNET_BUFFER_F_L4_CHECKSUM_COMPUTED | VNET_BUFFER_F_L4_CHECKSUM_CORRECT);
   if (xd->flags & DPDK_DEVICE_FLAG_ADMIN_UP)
     dpdk_device_start (xd);
 

src/plugins/dpdk/device/node.c
@@ -36,9 +36,12 @@ static char *dpdk_error_strings[] = {
 };
 
 /* make sure all flags we need are stored in lower 32 bits */
-STATIC_ASSERT ((u64) (PKT_RX_IP_CKSUM_BAD | PKT_RX_FDIR | PKT_RX_LRO) <
-		 (1ULL << 32),
+STATIC_ASSERT ((u64) (PKT_RX_IP_CKSUM_BAD | PKT_RX_L4_CKSUM_BAD | PKT_RX_FDIR |
+		      PKT_RX_LRO) < (1ULL << 32),
 	       "dpdk flags not in lower word, fix needed");
+
+STATIC_ASSERT (PKT_RX_L4_CKSUM_BAD == (1ULL << 3),
+	       "bit number of PKT_RX_L4_CKSUM_BAD is no longer 3!");
 
 static_always_inline uword
 dpdk_process_subseq_segs (vlib_main_t * vm, vlib_buffer_t * b,
@@ -449,6 +452,23 @@ dpdk_device_input (vlib_main_t * vm, dpdk_main_t * dm, dpdk_device_t * xd,
 	  if (xd->flags & DPDK_DEVICE_FLAG_RX_IP4_CKSUM &&
 	      (or_flags & PKT_RX_IP_CKSUM_BAD) == 0)
 	    f->flags |= ETH_INPUT_FRAME_F_IP4_CKSUM_OK;
+
+	  if (PREDICT_FALSE ((or_flags & PKT_RX_L4_CKSUM_BAD) &&
+			     (dm->conf->enable_tcp_udp_checksum)))
+	    {
+	      for (n = 0; n < n_rx_packets; n++)
+		{
+		  /* Check and reset VNET_BUFFER_F_L4_CHECKSUM_CORRECT flag
+		     if PKT_RX_L4_CKSUM_BAD is set.
+		     The magic num 3 is the bit number of PKT_RX_L4_CKSUM_BAD
+		     which is defined in DPDK.
+		     Have made a STATIC_ASSERT in this file to ensure this.
+		   */
+		  b0 = vlib_buffer_from_rte_mbuf (ptd->mbufs[n]);
+		  b0->flags ^= (ptd->flags[n] & PKT_RX_L4_CKSUM_BAD)
+			       << (VNET_BUFFER_F_LOG2_L4_CHECKSUM_CORRECT - 3);
+		}
+	    }
 	  vlib_frame_no_append (f);
 	}
       n_left_to_next -= n_rx_packets;