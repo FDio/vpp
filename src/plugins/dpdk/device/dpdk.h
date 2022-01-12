79caa31c12f2c109721ede0b6966b756ed4a22f3
Damjan Marion <damarion@cisco.com>
2022-01-12 18:46:29 +0100

    dpdk: improve logging
    Type: improvement
    Change-Id: If61d7409ff14b9f771c1dc8ec9f35e179cea7a28
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- src/plugins/dpdk/device/dpdk.h
+++ src/plugins/dpdk/device/dpdk.h
@@ -473,6 +473,8 @@ format_function_t format_dpdk_flow;
 format_function_t format_dpdk_rss_hf_name;
 format_function_t format_dpdk_rx_offload_caps;
 format_function_t format_dpdk_tx_offload_caps;
+format_function_t format_dpdk_burst_fn;
+format_function_t format_dpdk_rte_device;
 vnet_flow_dev_ops_function_t dpdk_flow_ops_fn;
 
 clib_error_t *unformat_rss_fn (unformat_input_t * input, uword * rss_fn);
