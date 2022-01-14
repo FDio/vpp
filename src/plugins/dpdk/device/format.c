FDio/vpp

commit 65105c95fe039d5fb1d3baec0affe1cd7be32505

dpdk: improve logging
Type: improvement
Change-Id: If61d7409ff14b9f771c1dc8ec9f35e179cea7a28
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/plugins/dpdk/device/format.c
@@ -491,15 +491,25 @@ format_dpdk_device_module_info (u8 * s, va_list * args)
   return s;
 }
 
-static const char *
-ptr2sname (void *p)
+u8 *
+format_dpdk_burst_fn (u8 *s, va_list *args)
 {
+  dpdk_device_t *xd = va_arg (*args, dpdk_device_t *);
+  vlib_rx_or_tx_t dir = va_arg (*args, vlib_rx_or_tx_t);
+  void *p;
   Dl_info info = { 0 };
+
+#if RTE_VERSION < RTE_VERSION_NUM(21, 11, 0, 0)
+#define rte_eth_fp_ops rte_eth_devices
+#endif
+
+  p = (dir == VLIB_TX) ? rte_eth_fp_ops[xd->port_id].tx_pkt_burst :
+			 rte_eth_fp_ops[xd->port_id].rx_pkt_burst;
 
   if (dladdr (p, &info) == 0)
     return 0;
 
-  return info.dli_sname;
+  return format (s, "%s", info.dli_sname);
 }
 
 static u8 *
@@ -512,6 +522,25 @@ format_switch_info (u8 * s, va_list * args)
     s = format (s, "name %s ", si->name);
 
   s = format (s, "domain id %d port id %d", si->domain_id, si->port_id);
+
+  return s;
+}
+
+u8 *
+format_dpdk_rte_device (u8 *s, va_list *args)
+{
+  struct rte_device *d = va_arg (*args, struct rte_device *);
+
+  if (!d)
+    return format (s, "not available");
+
+  s = format (s, "name: %s, numa: %d", d->name, d->numa_node);
+
+  if (d->driver)
+    s = format (s, ", driver: %s", d->driver->name);
+
+  if (d->bus)
+    s = format (s, ", bus: %s", d->bus->name);
 
   return s;
 }
@@ -630,12 +659,8 @@ format_dpdk_device (u8 * s, va_list * args)
 							      "");
     }
 
-#if RTE_VERSION < RTE_VERSION_NUM(21, 11, 0, 0)
-#define rte_eth_fp_ops rte_eth_devices
-#endif
-
-  s = format (s, "%Utx burst function: %s\n", format_white_space, indent + 2,
-	      ptr2sname (rte_eth_fp_ops[xd->port_id].tx_pkt_burst));
+  s = format (s, "%Utx burst function: %U\n", format_white_space, indent + 2,
+	      format_dpdk_burst_fn, xd, VLIB_RX);
 
   if (rte_eth_rx_burst_mode_get (xd->port_id, 0, &mode) == 0)
     {
@@ -646,7 +671,7 @@ format_dpdk_device (u8 * s, va_list * args)
     }
 
   s = format (s, "%Urx burst function: %s\n", format_white_space, indent + 2,
-	      ptr2sname (rte_eth_devices[xd->port_id].rx_pkt_burst));
+	      format_dpdk_burst_fn, xd, VLIB_TX);
 
   /* $$$ MIB counters  */
   {

src/plugins/dpdk/device/common.c
@@ -82,6 +82,9 @@ dpdk_device_setup (dpdk_device_t * xd)
 
   rte_eth_dev_info_get (xd->port_id, &dev_info);
 
+  dpdk_log_debug ("[%u] configuring device %U", xd->port_id,
+		  format_dpdk_rte_device, dev_info.device);
+
   /* create rx and tx offload wishlist */
   rxo = DEV_RX_OFFLOAD_IPV4_CKSUM;
   txo = 0;
@@ -118,8 +121,12 @@ dpdk_device_setup (dpdk_device_t * xd)
   rxo &= dev_info.rx_offload_capa;
   txo &= dev_info.tx_offload_capa;
 
+  dpdk_log_debug ("[%u] Supported RX offloads: %U", xd->port_id,
+		  format_dpdk_rx_offload_caps, dev_info.rx_offload_capa);
   dpdk_log_debug ("[%u] Configured RX offloads: %U", xd->port_id,
 		  format_dpdk_rx_offload_caps, rxo);
+  dpdk_log_debug ("[%u] Supported TX offloads: %U", xd->port_id,
+		  format_dpdk_tx_offload_caps, dev_info.tx_offload_capa);
   dpdk_log_debug ("[%u] Configured TX offloads: %U", xd->port_id,
 		  format_dpdk_tx_offload_caps, txo);
 
@@ -373,6 +380,11 @@ dpdk_device_start (dpdk_device_t * xd)
       dpdk_device_error (xd, "rte_eth_dev_start", rv);
       return;
     }
+
+  dpdk_log_debug ("[%u] RX burst function: %U", xd->port_id,
+		  format_dpdk_burst_fn, xd, VLIB_RX);
+  dpdk_log_debug ("[%u] TX burst function: %U", xd->port_id,
+		  format_dpdk_burst_fn, xd, VLIB_TX);
 
   dpdk_setup_interrupts (xd);
 

src/plugins/dpdk/device/dpdk.h
@@ -473,6 +473,8 @@ format_function_t format_dpdk_flow;
 format_function_t format_dpdk_rss_hf_name;
 format_function_t format_dpdk_rx_offload_caps;
 format_function_t format_dpdk_tx_offload_caps;
+format_function_t format_dpdk_burst_fn;
+format_function_t format_dpdk_rte_device;
 vnet_flow_dev_ops_function_t dpdk_flow_ops_fn;
 
 clib_error_t *unformat_rss_fn (unformat_input_t * input, uword * rss_fn);

commit f8dd9d8af135363631e4d3a366e6ccd89431aa99

dpdk: fix burst function output
Type: fix
Fixes: 65105c95f
Change-Id: I8dee4b560a49891f954d7eb8e79ea535cedeaa88
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/plugins/dpdk/device/format.c
@@ -506,8 +506,8 @@ format_dpdk_burst_fn (u8 *s, va_list *args)
   p = (dir == VLIB_TX) ? rte_eth_fp_ops[xd->port_id].tx_pkt_burst :
 			 rte_eth_fp_ops[xd->port_id].rx_pkt_burst;
 
-  if (dladdr (p, &info) == 0)
-    return 0;
+  if (dladdr (p, &info) == 0 || info.dli_sname == 0)
+    return format (s, "(not available)");
 
   return format (s, "%s", info.dli_sname);
 }
@@ -670,7 +670,7 @@ format_dpdk_device (u8 * s, va_list * args)
 							      "");
     }
 
-  s = format (s, "%Urx burst function: %s\n", format_white_space, indent + 2,
+  s = format (s, "%Urx burst function: %U\n", format_white_space, indent + 2,
 	      format_dpdk_burst_fn, xd, VLIB_TX);
 
   /* $$$ MIB counters  */