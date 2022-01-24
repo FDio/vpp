FDio/vpp

commit 3fca567ff438145e28dd1318ad5b1734c1091257

svm: queue sub: Add conditional timed wait
On reviece side svm queue only permits blocking and
non-blocking calls. This patch adds timed wait blocking
functionality which returns either on signal/event or
on given time out.

It also preserves the original behavior, so it will not
hurt client applications which are using svm queue.

Change-Id: Ic10632170330a80afb8bc781d4ccddfe4da2c69a
Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>

src/vpp-api/vapi/vapi.c
@@ -502,7 +502,8 @@ out:
 }
 
 vapi_error_e
-vapi_recv (vapi_ctx_t ctx, void **msg, size_t * msg_size)
+vapi_recv (vapi_ctx_t ctx, void **msg, size_t * msg_size,
+	   svm_q_conditional_wait_t cond, u32 time)
 {
   if (!ctx || !ctx->connected || !msg || !msg_size)
     {
@@ -519,7 +520,9 @@ vapi_recv (vapi_ctx_t ctx, void **msg, size_t * msg_size)
 
   svm_queue_t *q = am->vl_input_queue;
   VAPI_DBG ("doing shm queue sub");
-  int tmp = svm_queue_sub (q, (u8 *) & data, 0);
+
+  int tmp = svm_queue_sub (q, (u8 *) & data, cond, time);
+
   if (tmp == 0)
     {
 #if VAPI_DEBUG_ALLOC
@@ -700,7 +703,7 @@ vapi_dispatch_one (vapi_ctx_t ctx)
   VAPI_DBG ("vapi_dispatch_one()");
   void *msg;
   size_t size;
-  vapi_error_e rv = vapi_recv (ctx, &msg, &size);
+  vapi_error_e rv = vapi_recv (ctx, &msg, &size, SVM_Q_WAIT, 0);
   if (VAPI_OK != rv)
     {
       VAPI_DBG ("vapi_recv failed with rv=%d", rv);

src/svm/queue.c
@@ -26,6 +26,7 @@
 #include <vppinfra/format.h>
 #include <vppinfra/cache.h>
 #include <svm/queue.h>
+#include <vppinfra/time.h>
 #include <signal.h>
 
 /*
@@ -299,12 +300,14 @@ svm_queue_add2 (svm_queue_t * q, u8 * elem, u8 * elem2, int nowait)
  * svm_queue_sub
  */
 int
-svm_queue_sub (svm_queue_t * q, u8 * elem, int nowait)
+svm_queue_sub (svm_queue_t * q, u8 * elem, svm_q_conditional_wait_t cond,
+	       u32 time)
 {
   i8 *headp;
   int need_broadcast = 0;
+  int rc = 0;
 
-  if (nowait)
+  if (cond == SVM_Q_NOWAIT)
     {
       /* zero on success */
       if (pthread_mutex_trylock (&q->mutex))
@@ -317,14 +320,32 @@ svm_queue_sub (svm_queue_t * q, u8 * elem, int nowait)
 
   if (PREDICT_FALSE (q->cursize == 0))
     {
-      if (nowait)
+      if (cond == SVM_Q_NOWAIT)
 	{
 	  pthread_mutex_unlock (&q->mutex);
 	  return (-2);
 	}
-      while (q->cursize == 0)
+      else if (cond == SVM_Q_TIMEDWAIT)
 	{
-	  (void) pthread_cond_wait (&q->condvar, &q->mutex);
+	  struct timespec ts;
+	  ts.tv_sec = unix_time_now () + time;
+	  ts.tv_nsec = 0;
+	  while (q->cursize == 0 && rc == 0)
+	    {
+	      rc = pthread_cond_timedwait (&q->condvar, &q->mutex, &ts);
+	    }
+	  if (rc == ETIMEDOUT)
+	    {
+	      pthread_mutex_unlock (&q->mutex);
+	      return ETIMEDOUT;
+	    }
+	}
+      else
+	{
+	  while (q->cursize == 0)
+	    {
+	      (void) pthread_cond_wait (&q->condvar, &q->mutex);
+	    }
 	}
     }
 

src/svm/queue.h
@@ -36,6 +36,24 @@ typedef struct _svm_queue
   char data[0];
 } svm_queue_t;
 
+typedef enum
+{
+  /**
+   * blocking call
+   */
+  SVM_Q_WAIT = 0,
+
+  /**
+   * non-blocking call
+   */
+  SVM_Q_NOWAIT,
+
+  /**
+   * blocking call, return on signal or time-out
+   */
+  SVM_Q_TIMEDWAIT,
+} svm_q_conditional_wait_t;
+
 svm_queue_t *svm_queue_init (int nels,
 			     int elsize,
 			     int consumer_pid,
@@ -43,7 +61,8 @@ svm_queue_t *svm_queue_init (int nels,
 void svm_queue_free (svm_queue_t * q);
 int svm_queue_add (svm_queue_t * q, u8 * elem, int nowait);
 int svm_queue_add2 (svm_queue_t * q, u8 * elem, u8 * elem2, int nowait);
-int svm_queue_sub (svm_queue_t * q, u8 * elem, int nowait);
+int svm_queue_sub (svm_queue_t * q, u8 * elem, svm_q_conditional_wait_t cond,
+		   u32 time);
 int svm_queue_sub2 (svm_queue_t * q, u8 * elem);
 void svm_queue_lock (svm_queue_t * q);
 void svm_queue_unlock (svm_queue_t * q);

src/tests/vnet/session/tcp_echo.c
@@ -505,7 +505,7 @@ client_handle_event_queue (uri_tcp_test_main_t * utm)
 {
   session_fifo_event_t _e, *e = &_e;;
 
-  svm_queue_sub (utm->our_event_queue, (u8 *) e, 0 /* nowait */ );
+  svm_queue_sub (utm->our_event_queue, (u8 *) e, SVM_Q_WAIT, 0);
   switch (e->event_type)
     {
     case FIFO_EVENT_APP_RX:
@@ -530,7 +530,7 @@ client_rx_thread_fn (void *arg)
   utm->client_bytes_received = 0;
   while (1)
     {
-      svm_queue_sub (utm->our_event_queue, (u8 *) e, 0 /* nowait */ );
+      svm_queue_sub (utm->our_event_queue, (u8 *) e, SVM_Q_WAIT, 0);
       switch (e->event_type)
 	{
 	case FIFO_EVENT_APP_RX:
@@ -1027,7 +1027,7 @@ server_handle_event_queue (uri_tcp_test_main_t * utm)
 
   while (1)
     {
-      svm_queue_sub (utm->our_event_queue, (u8 *) e, 0 /* nowait */ );
+      svm_queue_sub (utm->our_event_queue, (u8 *) e, SVM_Q_WAIT, 0);
       switch (e->event_type)
 	{
 	case FIFO_EVENT_APP_RX:

src/tests/vnet/session/udp_echo.c
@@ -956,7 +956,7 @@ server_handle_event_queue (uri_udp_test_main_t * utm)
 
   while (1)
     {
-      svm_queue_sub (utm->our_event_queue, (u8 *) e, 0 /* nowait */ );
+      svm_queue_sub (utm->our_event_queue, (u8 *) e, SVM_Q_WAIT, 0);
       switch (e->event_type)
 	{
 	case FIFO_EVENT_APP_RX:

src/vlibapi/api_shared.c
@@ -758,7 +758,7 @@ vl_msg_api_queue_handler (svm_queue_t * q)
 {
   uword msg;
 
-  while (!svm_queue_sub (q, (u8 *) & msg, 0))
+  while (!svm_queue_sub (q, (u8 *) & msg, SVM_Q_WAIT, 0))
     vl_msg_api_handler ((void *) msg);
 }
 

src/vlibmemory/memory_client.c
@@ -218,7 +218,7 @@ vl_client_connect (const char *name, int ctx_quota, int input_queue_size)
       for (i = 0; i < 1000; i++)
 	{
 	  qstatus = svm_queue_sub (vl_input_queue, (u8 *) & rp,
-				   1 /* nowait */ );
+				   SVM_Q_NOWAIT, 0);
 	  if (qstatus == 0)
 	    goto read_one_msg;
 	  ts.tv_sec = 0;
@@ -305,7 +305,7 @@ vl_client_disconnect (void)
 	  am->shmem_hdr = 0;
 	  break;
 	}
-      if (svm_queue_sub (vl_input_queue, (u8 *) & rp, 1) < 0)
+      if (svm_queue_sub (vl_input_queue, (u8 *) & rp, SVM_Q_NOWAIT, 0) < 0)
 	continue;
 
       /* drain the queue */

src/vlibmemory/memory_shared.c
@@ -577,7 +577,7 @@ vl_map_shmem (const char *region_name, int is_vlib)
 
 	mutex_ok:
 	  am->vlib_rp = vlib_rp;
-	  while (svm_queue_sub (q, (u8 *) & old_msg, 1 /* nowait */ )
+	  while (svm_queue_sub (q, (u8 *) & old_msg, SVM_Q_NOWAIT, 0)
 		 != -2 /* queue underflow */ )
 	    {
 	      vl_msg_api_free_nolock ((void *) old_msg);

commit 58da8c898c1c66c6ebdbfb1e090e053eb054f7a9

api: vapi: don't ignore vapi_is_nonblocking call
Pass correct conditional based on how vapi is configured wrt blocking.

Type: fix
Fixes: 3fca567ff438145e28dd1318ad5b1734c1091257
Signed-off-by: Klement Sekera <ksekera@cisco.com>
Change-Id: I47adca19f104d7a758cb2940e93c9fd8c7cc9bfa

src/vpp-api/vapi/vapi.c
@@ -766,7 +766,9 @@ vapi_dispatch_one (vapi_ctx_t ctx)
   VAPI_DBG ("vapi_dispatch_one()");
   void *msg;
   uword size;
-  vapi_error_e rv = vapi_recv (ctx, &msg, &size, SVM_Q_WAIT, 0);
+  svm_q_conditional_wait_t cond =
+    vapi_is_nonblocking (ctx) ? SVM_Q_NOWAIT : SVM_Q_WAIT;
+  vapi_error_e rv = vapi_recv (ctx, &msg, &size, cond, 0);
   if (VAPI_OK != rv)
     {
       VAPI_DBG ("vapi_recv failed with rv=%d", rv);