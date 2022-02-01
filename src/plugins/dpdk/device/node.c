FDio/vpp

commit a0de7807405963d15216f3d89ee1c6edf678946a

dpdk: prefetch becomes prefetchw
Changed dpdk-input prefetch of vlib_buffer_t to prefetchw.

vlib_buffer_t was being prefetched without 'ownership', which may
cause a stall when the buffer is subsequently written to. This saves
4 clocks a packet when the buffer is shared a cross cores, and has no
impact when not sharing.

Type: improvement

Signed-off-by: Ray Kinsella <mdr@ashroe.eu>
Change-Id: I317af2a38ef536022e68552351a8507861f62dad

src/plugins/dpdk/device/node.c
@@ -97,13 +97,13 @@ dpdk_prefetch_buffer_x4 (struct rte_mbuf *mb[])
 {
   vlib_buffer_t *b;
   b = vlib_buffer_from_rte_mbuf (mb[0]);
-  clib_prefetch_load (b);
+  clib_prefetch_store (b);
   b = vlib_buffer_from_rte_mbuf (mb[1]);
-  clib_prefetch_load (b);
+  clib_prefetch_store (b);
   b = vlib_buffer_from_rte_mbuf (mb[2]);
-  clib_prefetch_load (b);
+  clib_prefetch_store (b);
   b = vlib_buffer_from_rte_mbuf (mb[3]);
-  clib_prefetch_load (b);
+  clib_prefetch_store (b);
 }
 
 /** \brief Main DPDK input node

commit a475b32ea704e8af1de168cdb820f008db384b44

dpdk: revert prefetch becomes prefetchw
This reverts commit a0de7807405963d15216f3d89ee1c6edf678946a.

Type: fix
Fixes: a0de7807405963d15216f3d89ee1c6edf678946a
Signed-off-by: Ray Kinsella <mdr@ashroe.eu>
Change-Id: Id8f37f25b53b380b2c2962cd7088562bcdfeca4e

src/plugins/dpdk/device/node.c
@@ -100,13 +100,13 @@ dpdk_prefetch_buffer_x4 (struct rte_mbuf *mb[])
 {
   vlib_buffer_t *b;
   b = vlib_buffer_from_rte_mbuf (mb[0]);
-  clib_prefetch_store (b);
+  clib_prefetch_load (b);
   b = vlib_buffer_from_rte_mbuf (mb[1]);
-  clib_prefetch_store (b);
+  clib_prefetch_load (b);
   b = vlib_buffer_from_rte_mbuf (mb[2]);
-  clib_prefetch_store (b);
+  clib_prefetch_load (b);
   b = vlib_buffer_from_rte_mbuf (mb[3]);
-  clib_prefetch_store (b);
+  clib_prefetch_load (b);
 }
 
 /** \brief Main DPDK input node