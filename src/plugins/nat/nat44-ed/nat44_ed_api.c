e4afe3f3c711f6d7172772d624569492264ff37b
Filip Varga <fivarga@cisco.com>
2022-02-23 15:45:48 -0800

    nat: VRF routing & FIB improvements
    This patch affects how destination fib is choosen during session
    creation. Default behavior of choosing fib based on output
    interfaces is kept.
    
    Configuration gives you the ability to change default behavior
    to direct or restrict traffic between different FIB tables.
    
    NAT specific VRF routing options:
    a) keeping communication in the same VRF
    b) option to add multiple destination VRFs
    c) option to control the resolution order of destination VRFs
    
    TX FIB resolution is based on looking up RX FIB entry in NATs
    VRF table and picking the first FIB that resolves
    destination address.
    
    Ticket: VPP-2009
    Type: improvement
    
    Change-Id: If500c48d7ce3466533ad9581c0847870788fc4fb
    Signed-off-by: Filip Varga <fivarga@cisco.com>

--- src/plugins/nat/nat44-ed/nat44_ed_api.c
+++ src/plugins/nat/nat44-ed/nat44_ed_api.c
@@ -163,6 +163,28 @@ vl_api_nat_worker_dump_t_handler (vl_api_nat_worker_dump_t * mp)
 }
 
 static void
+vl_api_nat44_ed_add_del_vrf_table_t_handler (
+  vl_api_nat44_ed_add_del_vrf_table_t *mp)
+{
+  snat_main_t *sm = &snat_main;
+  vl_api_nat44_ed_add_del_vrf_table_reply_t *rmp;
+  int rv = 0;
+
+  REPLY_MACRO (VL_API_NAT44_ED_ADD_DEL_VRF_TABLE_REPLY);
+}
+
+static void
+vl_api_nat44_ed_add_del_vrf_route_t_handler (
+  vl_api_nat44_ed_add_del_vrf_route_t *mp)
+{
+  snat_main_t *sm = &snat_main;
+  vl_api_nat44_ed_add_del_vrf_table_route_t *rmp;
+  int rv = 0;
+
+  REPLY_MACRO (VL_API_NAT44_ED_ADD_DEL_VRF_ROUTE_REPLY);
+}
+
+static void
 vl_api_nat44_set_session_limit_t_handler (vl_api_nat44_set_session_limit_t *
 					  mp)
 {
@@ -1192,6 +1214,74 @@ vl_api_nat44_show_running_config_t_handler (
       rmp->ipfix_logging_enabled = nat_ipfix_logging_enabled ();
       rmp->flags |= NAT44_IS_ENDPOINT_DEPENDENT;
     }));
+}
+
+static void
+vl_api_nat44_ed_add_del_vrf_table_t_handler (
+  vl_api_nat44_ed_add_del_vrf_table_t *mp)
+{
+  vl_api_nat44_ed_add_del_vrf_table_reply_t *rmp;
+  int rv = nat44_ed_add_del_vrf_table (clib_net_to_host_u32 (mp->table_vrf_id),
+				       mp->is_add);
+  REPLY_MACRO (VL_API_NAT44_ED_ADD_DEL_VRF_TABLE);
+}
+
+static void
+vl_api_nat44_ed_add_del_vrf_route_t_handler (
+  vl_api_nat44_ed_add_del_vrf_route_t *mp)
+{
+  vl_api_nat44_ed_add_del_vrf_route_t *rmp;
+  int rv =
+    nat44_ed_add_del_vrf_route (clib_net_to_host_u32 (mp->table_vrf_id),
+				clib_net_to_host_u32 (mp->vrf_id), mp->is_add);
+  REPLY_MACRO (VL_API_NAT44_ED_ADD_DEL_VRF_ROUTE);
+}
+
+static void nat44_ed_vrf_table_send_details (vl_api_registration_t *rp,
+					     u32 context, vrf_table_t *t);
+{
+  snat_main_t *sm = &snat_main;
+  vl_api_nat44_ed_vrf_table_details_t *mp;
+
+  u32 *vrf_ids = 0;
+  vrf_route_t *r;
+
+  mp = vl_msg_api_alloc_zero (sizeof (*mp) +
+			      sizeof (mp->vrf_ids[0]) * vec_len (t->vrf_ids));
+  mp->_vl_msg_id = ntohs (VL_API_NAT44_ED_TABLE_DETAILS + mp->msg_id_base);
+  mp->context = context;
+  mp->n_vrf_ids = clib_host_to_net_u32 (vec_len (t->vrf_ids));
+
+  pool_foreach (r, t->routes)
+    {
+      vec_add1 (vrf_ids, r->vrf_id);
+    }
+
+  // copy the records
+  clib_memcpy (mp->vrf_ids, vrf_ids,
+	       sizeof (mp->vrf_ids[0]) * vec_len (t->vrf_ids));
+
+  vec_free (vrf_ids);
+
+  // send the message
+  vl_api_snd_msg (rp, (u8 *), mp);
+}
+
+static void
+vl_api_nat44_ed_vrf_table_dump_t_handler (vl_api_nat44_ed_vrf_table_dump_t *mp)
+{
+  snat_main_t *sm = &snat_main;
+  vl_api_registration_t *rp;
+  vrf_table_t *t;
+
+  rp = vl_api_client_index_to_registration (mp->client_index);
+  if (rp == 0)
+    return;
+
+  pool_foreach (t, sm->vrf_tables)
+    {
+      nat44_ed_vrf_table_send_details (rp, mp->context, t);
+    }
 }
 
 /* user (internal host) key */
