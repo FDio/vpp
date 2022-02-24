ab9d930129f469fab6edccfdaf2536156fec86d5
Mohammed Hawari <mohammed@hawari.fr>
2022-02-24 15:19:01 +0100

    af_packet: fix tx stall by retrying failed sendto
    Change-Id: I6bed66f740b34673a4883eda1c7f7310c57e131b
    Type: fix
    Signed-off-by: Mohammed Hawari <mohammed@hawari.fr>

--- src/vnet/devices/af_packet/device.c
+++ src/vnet/devices/af_packet/device.c
@@ -192,7 +192,7 @@ VNET_DEVICE_CLASS_TX_FN (af_packet_device_class) (vlib_main_t * vm,
 
   CLIB_MEMORY_BARRIER ();
 
-  if (PREDICT_TRUE (n_sent))
+  if (PREDICT_TRUE (n_sent || apif->pending_kick))
     {
       apif->next_tx_frame = tx_frame;
 
@@ -208,7 +208,10 @@ VNET_DEVICE_CLASS_TX_FN (af_packet_device_class) (vlib_main_t * vm,
 			      AF_PACKET_TX_ERROR_TXRING_FATAL :
 			      AF_PACKET_TX_ERROR_TXRING_EAGAIN,
 			    n_sent);
+	  apif->pending_kick = 1;
 	}
+      else
+	apif->pending_kick = 0;
     }
 
   clib_spinlock_unlock_if_init (&apif->lockp);
