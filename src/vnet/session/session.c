e09bd48e354d8701091437cafda6b29163ed2d58
Florin Coras <fcoras@cisco.com>
2022-03-21 10:38:01 -0700

    session: linked list of events to be handled by main
    Minimize amount of rpcs from first worker to main
    
    Type: improvement
    
    Signed-off-by: Florin Coras <fcoras@cisco.com>
    Change-Id: I3794ff028a17d18b7bff69ede2b62e1e2d45ae77

--- src/vnet/session/session.c
+++ src/vnet/session/session.c
@@ -1896,6 +1896,8 @@ session_manager_main_enable (vlib_main_t * vm)
       wrk->new_head = clib_llist_make_head (wrk->event_elts, evt_list);
       wrk->old_head = clib_llist_make_head (wrk->event_elts, evt_list);
       wrk->pending_connects = clib_llist_make_head (wrk->event_elts, evt_list);
+      wrk->evts_pending_main =
+	clib_llist_make_head (wrk->event_elts, evt_list);
       wrk->vm = vlib_get_main_by_index (i);
       wrk->last_vlib_time = vlib_time_now (vm);
       wrk->last_vlib_us_time = wrk->last_vlib_time * CLIB_US_TIME_FREQ;
