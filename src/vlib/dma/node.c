7ec465a9223c42df27c2c2e605a56d5d8722ed55
Marvin Liu <yong.liu@intel.com>
2022-03-21 17:54:56 +0800

    vlib: introduce DMA infrastructure [RFC]
    This patch introduces DMA infrastructure into vlib. This is well known
    that large amount of memory movements will drain core resource. Nowadays
    more and more hardware accelerators were designed out for freeing core
    from this burden. Meanwhile some restrictions still remained when
    utilizing hardware accelerators, e.g. cross numa throughput will have a
    significant drop compared to same node. Normally the number of hardware
    accelerator instances will less than cores number, not to mention that
    applications number will even beyond the number of cores. Some hardware
    may support share virtual address with cores, while others are not.
    
    Here we introduce new DMA infrastructure which can fulfill the
    requirements of vpp applications like session and memif and in the
    meantime dealing with hardware limitations.
    
    Here is some design backgrounds:
    
      Backend is the abstract of resource which allocated from DMA device
      and can do some basic operations like configuration, DMA copy and
      result query.
    
      Config is the abstract of application DMA requirement.
    
      Datapath of backend may not thread safe, so one backend only assigned
      to dedicated thread.
    
      No need to assign multiple backends to same thread as one backend can
      consume all the bandwidth of DMA device. Multiple backends is also in
      consideration if there’s a requirement.
    
      DMA infra will not track inflight frames as backend can guarantee
      that callbacks are orderly called.
    
      One DMA polling thread will handle multiple configs, requests are
      distinguished by config index.
    
    DMA architecture:
    
       +----------+   +----------+           +----------+   +----------+
       | Config1  |   | Config2  |           | Config3  |   | Config4  |
       +----------+   +----------+           +----------+   +----------+
            ||             ||                     ||             ||
       +-------------------------+           +-------------------------+
       |  DMA polling thread A   |           |  DMA polling thread B   |
       +-------------------------+           +-------------------------+
       	       ||                      		      ||
               +----------+                          +----------+
               | Backend1 |                          | Backend2 |
               +----------+                          +----------+
    
    Type: feature
    
    Signed-off-by: Marvin Liu <yong.liu@intel.com>
    Change-Id: Ic8119f00a9d7ed639a6fcdcca7cdba6f8045770e

---
+++ src/vlib/dma/node.c
@@ -0,0 +1,92 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright(c) 2022 Cisco Systems, Inc.
+ */
+
+#include <stdbool.h>
+#include <vlib/vlib.h>
+#include <vlib/dma/dma.h>
+
+#define foreach_dma_op_status                                                 \
+  _ (IDLE, "idle")                                                            \
+  _ (PENDING, "pending")                                                      \
+  _ (WORK_IN_PROGRESS, "work-in-progress")                                    \
+  _ (COMPLETED, "completed")                                                  \
+  _ (FAIL_NO_HANDLER, "no-handler")                                           \
+  _ (FAIL_BAD_DESC, "bad-desc")                                               \
+  _ (FAIL_ENGINE_ERR, "engine-error")
+
+typedef enum
+{
+#define _(sym, str) VLIB_DMA_ERROR_##sym,
+  foreach_dma_op_status
+#undef _
+    VLIB_DMA_N_ERROR,
+} vlib_dma_error_t;
+
+static char *vlib_dma_error_strings[] = {
+#define _(sym, str) str,
+  foreach_dma_op_status
+#undef _
+};
+
+#define foreach_dma_dispatch_next _ (ERR_DROP, "error-drop")
+
+typedef enum
+{
+#define _(n, s) DMA_DISPATCH_NEXT_##n,
+  foreach_dma_dispatch_next
+#undef _
+    DMA_DISPATCH_N_NEXT,
+} dma_dispatch_next_t;
+
+// TODO: trace staff
+
+VLIB_NODE_FN (dma_dispatch_node)
+(vlib_main_t *vm, vlib_node_runtime_t *node, vlib_frame_t *frame)
+{
+  vlib_dma_main_t *dm = &dma_main;
+  vlib_dma_thread_t *thread = dm->threads + vm->thread_index;
+  vlib_dma_backend_t *p = thread->backend;
+  vlib_dma_frame_t *dma_frame = NULL;
+  vlib_dma_config_t *config;
+  u16 cnt = 0;
+
+  // for each active backend
+  // pull completed frames, update status
+  // for each config pending list
+  // do call back for first completed pending frame
+  if (p)
+    {
+      do
+	{
+	  cnt = p->fn->get_completed_count (p->ctx, &dma_frame);
+	  if (cnt && dma_frame->state == VLIB_DMA_FRAME_STATE_SUCCESS)
+	    {
+	      config = dm->configs + dma_frame->config_index;
+	      config->data.cb (vm, dma_frame->trans, dma_frame->n_transfers,
+			       dma_frame->opaque);
+	      config->n_transfers += dma_frame->n_transfers;
+	      pool_put (thread->frame_pool, dma_frame);
+	    }
+	}
+      while (cnt);
+    }
+  return 0;
+}
+
+VLIB_REGISTER_NODE (dma_dispatch_node) = {
+  .name = "dma-dispatch",
+  .type = VLIB_NODE_TYPE_INPUT,
+  .state = VLIB_NODE_STATE_POLLING,
+//  .format_trace = format_dma_dispatch_trace,
+
+  .n_errors = ARRAY_LEN(vlib_dma_error_strings),
+  .error_strings = vlib_dma_error_strings,
+
+  .n_next_nodes = DMA_DISPATCH_N_NEXT,
+  .next_nodes = {
+#define _(n, s) [DMA_DISPATCH_NEXT_##n] = s,
+      foreach_dma_dispatch_next
+#undef _
+  },
+};
