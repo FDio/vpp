1ce8f240cf7822315c3f8d1fad0b6d62926844c6
Ting Xu <ting.xu@intel.com>
2022-04-24 06:14:25 +0000

    packetforge: add packetforge plugin for generic flow
    Add a new plugin packetforge. This plugin is to support generic flow.
    Packetforge is a library to translate naming or json profile format flow
    pattern to the required input of generic flow, i.e. spec and mask. It
    can add a new flow rule, but still needs vnet flow command to enable,
    disable or delete the rule. Examples are shown below. Json profile
    examples and its spec can be found in the plugin.
    
    Naming format input:
    packetforge flow add pattern
    mac()/ipv4()/udp()/gtpu()/gtppsc(qfi=1)/ipv4() rss function default
    
    Json profile format input:
    packetforge flow add profile <path>/mac_ipv4.json redirect-to-queue 3
    
    Type: feature
    
    Signed-off-by: Ting Xu <ting.xu@intel.com>
    Change-Id: Ia9f539741c5dca27ff236f2bcc493c5dd48c0df1

---
+++ src/plugins/packetforge/base/PathNode.py
@@ -0,0 +1,26 @@
+from PathNodeField import *
+
+
+class PathNode:
+    def __init__(self):
+        self.Header = None
+        self.fields = []
+
+    def Create(token):
+        if token == None:
+            return None
+
+        config = PathNode()
+
+        if "header" in token:
+            config.Header = token["header"]
+            if config.Header == None:
+                return None
+
+        if "fields" in token:
+            fts = token["fields"]
+            if fts != None:
+                for ft in fts:
+                    config.fields.append(PathNodeField.Create(ft))
+
+        return config
---
+++ src/plugins/packetforge/base/ProtocolHeaderField.py
@@ -0,0 +1,35 @@
+import ExpressionConverter
+
+
+class ProtocolHeaderField:
+    def __init__(self, Size, Value, Mask, Field):
+        self.Size = Size
+        self.Value = Value
+        self.Mask = Mask
+        self.Field = Field
+
+    def UpdateValue(self, expression, auto):
+        if self.Field.IsReadonly and not auto:
+            return False
+
+        if expression != None:
+            ret, _ = ExpressionConverter.Verify(self.Field.Format, expression)
+            if not ret:
+                return False
+
+        self.Value = expression
+        return True
+
+    def UpdateMask(self, expression):
+        if expression != None:
+            ret, _ = ExpressionConverter.Verify(self.Field.Format, expression)
+            if not ret:
+                return False
+
+        self.Mask = expression
+        return True
+
+    def UpdateSize(self):
+        if self.Size:
+            return
+        self.Size = self.Field.Size
