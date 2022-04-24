fe0828d8058f2abe8e4f36582531d8ceab0f3cd6
Ting Xu <ting.xu@intel.com>
2022-04-24 06:14:25 +0000

    packetforge: add new plugins packetforge
    Add a new plugin packetforge. This plugin is to support generic flow.
    Packetforge is a library to translate naming or json profile format flow
    pattern to the required input if generic flow, spec and mask. It can
    add a new flow rule, but still needs vnet flow command to enable,disable
    or delete the rule. Examples are shown below. Json profile examples and
    can spec be found in the plugin.
    
    Naming format input:
    packetforge flow add pattern
    mac()/ipv4()/udp()/gtpu()/gtppsc(qfi=1)/ipv4() rss function default
    
    Json profile format input:
    packetforge flow add profile <path>/mac_ipv4_udp.json redirect-to-queue 3
    
    Type: feature
    
    Signed-off-by: Ting Xu <ting.xu@intel.com>
    Change-Id: Ia9f539741c5dca27ff236f2bcc493c5dd48c0df1

---
+++ src/plugins/packetforge/base/ForgeResult.py
@@ -0,0 +1,32 @@
+import ExpressionConverter
+
+class ForgeResult:
+
+    def __init__(self, Header, PacketBuffer, MaskBuffer):
+        self.Headers = Header
+        self.PacketBuffer = PacketBuffer
+        self.MaskBuffer = MaskBuffer
+
+    def ToJSON(self):
+        result = {}
+        result["Length"] = str(len(self.PacketBuffer))
+        result["Packet"] = ExpressionConverter.ByteArrayToString(self.PacketBuffer)
+        result["Mask"] = ExpressionConverter.ByteArrayToString(self.MaskBuffer)
+        result["Protocol Stack"] = []
+
+        for header in self.Headers:
+            head_info = {}
+            head_info["name"] = header.Name()
+            head_info["Fields"] = []
+            for field in header.fields:
+                if (field.Size == 0):
+                    continue
+                field_info = {}
+                field_info["name"] = field.Field.Name
+                field_info["size"] = str(field.Size)
+                field_info["value"] = field.Value
+                field_info["mask"] = field.Mask
+                head_info["Fields"].append(field_info)
+            result["Protocol Stack"].append(head_info)
+
+        return result
---
+++ src/plugins/packetforge/base/packetforge.py
@@ -0,0 +1,115 @@
+from Path import *
+from ParseGraph import *
+import re
+import os
+import json
+
+parsegraph_path = os.getcwd() + "/src/plugins/packetforge/base/parsegraph"
+
+def Forge(pattern, file_flag):
+    result = GetPath(pattern, file_flag)
+    if (result == None):
+        return '', ''
+
+    spec, mask = GetBinary(result.ToJSON())
+    return spec, mask
+
+def GetPath(pattern, file_flag):
+    pg = ParseGraph.Create(parsegraph_path)
+    if (pg == None):
+        print("error: create parsegraph failed")
+        return None
+
+    if (not file_flag):
+        token = ParsePattern(pattern)
+        if (token == None):
+            return None
+    else:
+        if (not os.path.exists(pattern)):
+            print("error: file not exist '%s' " % (pattern))
+            return
+        f = open(pattern, 'r', encoding='utf-8')
+        token = json.load(f)
+
+    path = Path.Create(token)
+    if (path == None):
+        print("error: path not exit")
+        return None
+
+    result = pg.Forge(path)
+    if (result == None):
+        print("error: result not available")
+        return None
+
+    return result
+
+def GetBinary(flow_info):
+    spec = ''.join(flow_info["Packet"])
+    mask = ''.join(flow_info["Mask"])
+    return spec, mask
+
+def ParseFields(item):
+    # get protocol name
+    prot = item.split('(')[0]
+    stack = {"header": prot}
+    # get fields contents
+    fields = re.findall(r'[(](.*?)[)]', item)
+    if (not fields):
+        print("error: invalid pattern")
+        return None
+    if (fields == ['']):
+        return stack
+    stack.update({"fields": []})
+    return ParseStack(stack, fields[0].split(','))
+
+def GetMask(item):
+    if ("format" in item):
+        format = item["format"]
+        if (format == "mac"):
+            mask = "ff.ff.ff.ff.ff.ff"
+        elif (format == "ipv4"):
+            mask = "255.255.255.255"
+        elif (format == "ipv6"):
+            mask = "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"
+        return mask
+    if ("size" in item):
+        mask = str((1 << int(item["size"])) - 1)
+    else:
+        print("mask error")
+    return mask
+
+# parse protocol headers and its fields. Available fields are defined in corresponding nodes.
+def ParseStack(stack, fields):
+    prot = stack["header"]
+    node_path = parsegraph_path + "/nodes/" + prot + ".json"
+    if (not os.path.exists(node_path)):
+        print("error file not exist '%s' " % (node_path))
+        return None
+    f = open(node_path, 'r', encoding='utf-8')
+    nodeinfo = json.load(f)
+    for field in fields:
+        fld_name = field.split('=')[0].strip()
+        fld_value = field.split('=')[-1].strip() if (len(field.split('=')) >= 2) else None
+        for item in nodeinfo["layout"]:
+            if fld_name == item["name"]:
+                mask = GetMask(item)
+                stack["fields"].append({"name": fld_name, "value": fld_value, "mask": mask})
+                break
+        if (not stack["fields"]):
+            print("warning: invalid field '%s'" % (fld_name))
+            return None
+
+    return stack
+
+def ParsePattern(pattern):
+    # create json template
+    json_tmp = {"type": "path", "stack": []}
+
+    items = pattern.split("/")
+    for item in items:
+        stack = ParseFields(item)
+        if (stack == None):
+            return None
+        json_tmp["stack"].append(stack)
+
+    return json_tmp
