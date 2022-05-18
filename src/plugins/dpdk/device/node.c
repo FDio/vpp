1c528fffa645eef33fd469a7026e4e4e124f84d6
Vratko Polak <vrpolak@cisco.com>
2022-05-18 14:16:04 +0200

    Experiment: Single call in dpdk_device_input
    Change-Id: I9b646b9ddf410c57e756fefd2b94a364d5358983
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- src/plugins/dpdk/device/node.c
+++ src/plugins/dpdk/device/node.c
@@ -363,18 +363,8 @@ dpdk_device_input (vlib_main_t * vm, dpdk_main_t * dm, dpdk_device_t * xd,
     return 0;
 
   /* get up to DPDK_RX_BURST_SZ buffers from PMD */
-  while (n_rx_packets < DPDK_RX_BURST_SZ)
-    {
-      u32 n_to_rx = clib_min (DPDK_RX_BURST_SZ - n_rx_packets, 32);
-
-      n = rte_eth_rx_burst (xd->port_id, queue_id, ptd->mbufs + n_rx_packets,
-			    n_to_rx);
-      n_rx_packets += n;
-
-      if (n < n_to_rx)
-	break;
-    }
-
+  n_rx_packets =
+    rte_eth_rx_burst (xd->port_id, queue_id, ptd->mbufs, DPDK_RX_BURST_SZ);
   if (n_rx_packets == 0)
     return 0;
 
