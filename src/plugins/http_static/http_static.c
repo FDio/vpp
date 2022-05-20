b0f36127a8eefdce4159d563da099e16b9246ca1
Damjan Marion <damarion@cisco.com>
2022-05-20 16:01:22 +0200

    api: replace print functions wth format
    Type: improvement
    Change-Id: I7f7050c19453a69a7fb6c5e62f8f57db847d9144
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- src/plugins/http_static/http_static.c
+++ src/plugins/http_static/http_static.c
@@ -27,7 +27,6 @@
 
 #include <vpp/api/types.h>
 
-#define vl_print(handle, ...) vlib_cli_output (handle, __VA_ARGS__)
 
 #define REPLY_MSG_ID_BASE hsm->msg_id_base
 #include <vlibapi/api_helper_macros.h>
