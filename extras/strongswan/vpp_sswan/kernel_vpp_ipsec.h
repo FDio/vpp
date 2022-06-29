83adfcf6f110e614717d2b950505dafaaca947b1
Gabriel Oginski <gabrielx.oginski@intel.com>
2022-06-29 12:54:30 +0000

    vpp-swan: Add plugin for vpp-swan
    Added plugin vpp-swan is a plugin that helps offloading
    Strongswan IPsec ESP process from Linux Kernel to VPP.
    
    Type: feature
    Signed-off-by: Gabriel Oginski <gabrielx.oginski@intel.com>
    Change-Id: Iec77945892453fac1890d3c49d7d86fc6b09c893

---
+++ extras/strongswan/vpp_sswan/kernel_vpp_ipsec.h
@@ -0,0 +1,30 @@
+#ifndef KERNEL_VPP_IPSEC_H_
+#define KERNEL_VPP_IPSEC_H_
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2018-2022 Cisco
+ */
+
+#include <kernel/kernel_ipsec.h>
+
+typedef struct kernel_vpp_ipsec_t kernel_vpp_ipsec_t;
+
+/**
+ * Implementation of the kernel ipsec interface using Netlink.
+ */
+struct kernel_vpp_ipsec_t
+{
+
+  /**
+   * Implements kernel_ipsec_t interface
+   */
+  kernel_ipsec_t interface;
+};
+
+/**
+ * Create a vpp kernel ipsec interface instance.
+ *
+ * @return          kernel_vpp_ipsec_t instance
+ */
+kernel_vpp_ipsec_t *kernel_vpp_ipsec_create ();
+
+#endif /** KERNEL_VPP_IPSEC_H_ @}*/
