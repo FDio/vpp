FDio/vpp

commit 534dfc1f18db74f4a2c78d62fe6893daba56dc86

build: set OS_ID_LIKE on to be OS_ID on Debian proper
OS_ID_LIKE from /etc/os-release is used to determine
the type of packaging. On Debian it ends up being empty,
so the "ninja package" does not work for out of tree plugin
builds.

Solution: if OS_ID_LIKE is not set, set it to OS_ID

Type: fix
Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>
Change-Id: I077d4fdb509ea94b187d11391b1f49edb94c4e30
Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>

src/cmake/pack.cmake
@@ -64,6 +64,11 @@ macro(add_vpp_packaging)
   set(CPACK_${type}_PACKAGE_DESCRIPTION "${ARG_DESCRIPTION}")
   set(CPACK_${type}_PACKAGE_RELEASE 1)
 
+  # Pure Debian does not set the "OS_ID_LIKE", it only sets "OS_ID"
+  if (OS_ID_LIKE MATCHES "")
+    set(OS_ID_LIKE "${OS_ID}")
+  endif()
+
   if(OS_ID_LIKE MATCHES "debian")
     set(CPACK_GENERATOR "DEB")
     set(type "DEBIAN")

commit 068808a61c1cb5daf62cbfd1aef791303c2a9ae8

build: fix the matching of Debian for the packages
534dfc1f18db74f4a2c78d62fe6893daba56dc86 has fixed the building of out-of-tree plugins
on Debian - but the matching logic added was incorrect, so building out-of-tree plugins
on Ubuntu got broken.

Use the proper regex matching - because the empty regex always matches, with
not desirable result.

Type: fix
Fixes: 534dfc1f18db74f4a2c78d62fe6893daba56dc86
Change-Id: Id93e3d8b8596661dd932996b2b3a21fcf19e9da3
Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>

src/cmake/pack.cmake
@@ -65,7 +65,7 @@ macro(add_vpp_packaging)
   set(CPACK_${type}_PACKAGE_RELEASE 1)
 
   # Pure Debian does not set the "OS_ID_LIKE", it only sets "OS_ID"
-  if (OS_ID_LIKE MATCHES "")
+  if (OS_ID_LIKE MATCHES "^$")
     set(OS_ID_LIKE "${OS_ID}")
   endif()
 