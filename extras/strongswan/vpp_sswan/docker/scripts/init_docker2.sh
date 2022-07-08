e6bec6a930c72cab106c1ae0ee219ae48fe8a908
Gabriel Oginski <gabrielx.oginski@intel.com>
2022-07-08 07:46:32 +0000

    vpp-swan: Add scripts for testing
    Added scripts to reparing setups for testing
    
    To prepare and run containers:
    sudo ./extras/strongswan/vpp_sswan/docker/run.sh prepare_containers
    
    To prepare setups:
    sudo ./extras/strongswan/vpp_sswan/docker/run.sh config
    
    To clean-up settups:
    sudo ./extras/strongswan/vpp_sswan/docker/run.sh clean
    
    To deleted all containers and images in Docker:
    sudo ./extras/strongswan/vpp_sswan/docker/run.sh deleted
    
    Type: feature
    Signed-off-by: Gabriel Oginski <gabrielx.oginski@intel.com>
    Change-Id: I77f01c0419dccc95f610046c8552ae825f2c7e12

---
+++ extras/strongswan/vpp_sswan/docker/scripts/init_docker2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Initialization Docker 2 - SSWAN in kernel"
+
+# tested only with 5.9.5 and 5.9.6 version of strongSwan
+VERSION_SSWAN=5.9.6
+
+curl -o ./strongswan-${VERSION_SSWAN}.tar.gz -LO https://github.com/strongswan/strongswan/archive/${VERSION_SSWAN}.tar.gz;
+tar -zxof ./strongswan-${VERSION_SSWAN}.tar.gz
+
+cd /root/strongswan-${VERSION_SSWAN}
+./autogen.sh
+./configure --prefix=/usr --sysconfdir=/etc --enable-libipsec --enable-systemd --enable-swanctl --disable-gmp --enable-openssl
+make -j$(nproc)
+sudo make install
+
+sudo cp /root/vpp_sswan/docker/configs/swanctl_docker2.conf /etc/swanctl/conf.d/swanctl.conf
+
+sudo systemctl daemon-reload
+sudo systemctl restart strongswan.service
+
+echo "### Loaded plugin in strogswan"
+sudo swanctl --stats
