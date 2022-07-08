b2ecb94d8aeb855a089107a6c23cea83b0be56fc
Gabriel Oginski <gabrielx.oginski@intel.com>
2022-07-08 07:46:32 +0000

    vpp-swan: Add scripts for testing
    Added scripts to reparing setups for testing
    
    To prepare and run containers:
    sudo ./extras/strongswan/vpp_sswan/docker/run.sh prepare_containers
    
    To prepare setups:
    sudo ./extras/strongswan/vpp_sswan/docker/run.sh config
    
    To clean-up settups:
    sudo ./extras/strongswan/vpp_sswan/docker/run.sh clean
    
    To deleted all containers and images in Docker:
    sudo ./extras/strongswan/vpp_sswan/docker/run.sh deleted
    
    Type: feature
    Signed-off-by: Gabriel Oginski <gabrielx.oginski@intel.com>
    Change-Id: I77f01c0419dccc95f610046c8552ae825f2c7e12

---
+++ extras/strongswan/vpp_sswan/docker/scripts/run_vpp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /root/vpp/
+make run-release STARTUP_CONF=/root/vpp_sswan/docker/configs/startup.conf &
+
+sleep 5
+
+sudo systemctl restart strongswan.service
+
+sleep 2
+
+echo "### Checking connections between VPP and Strongswan"
+/root/vpp/build-root/build-vpp-native/vpp/bin/vppctl -s /run/vpp/cli.sock sh api client
