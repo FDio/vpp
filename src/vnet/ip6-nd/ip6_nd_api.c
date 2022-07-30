1ec53d9dbac38467b11d65bb89f7c2d64ccd5a29
Takanori Hirano <me@hrntknr.net>
2022-07-31 04:57:07 +0900

    ip6-nd: add no-decrement flag
    Add the no-decrement flag to the ra options.
    This will prevent the valid/preferred lifetime from decreasing.
    
    Type: improvement
    Signed-off-by: Takanori Hirano <me@hrntknr.net>
    Change-Id: I8101ae6ed6522441d3d9620fc5dd4864af9b9f07

--- src/vnet/ip6-nd/ip6_nd_api.c
+++ src/vnet/ip6-nd/ip6_nd_api.c
@@ -210,12 +210,10 @@ static void
   no_autoconfig = mp->no_autoconfig == 1;
   no_onlink = mp->no_onlink == 1;
 
-  rv = ip6_ra_prefix (vm, ntohl (mp->sw_if_index),
-		      &pfx.fp_addr.ip6,
-		      pfx.fp_len, use_default,
-		      ntohl (mp->val_lifetime),
-		      ntohl (mp->pref_lifetime), no_advertise,
-		      off_link, no_autoconfig, no_onlink, is_no);
+  rv = ip6_ra_prefix (vm, ntohl (mp->sw_if_index), &pfx.fp_addr.ip6,
+		      pfx.fp_len, use_default, ntohl (mp->val_lifetime),
+		      ntohl (mp->pref_lifetime), no_advertise, off_link,
+		      no_autoconfig, 0, no_onlink, is_no);
 
   BAD_SW_IF_INDEX_LABEL;
   REPLY_MACRO (VL_API_SW_INTERFACE_IP6ND_RA_PREFIX_REPLY);
