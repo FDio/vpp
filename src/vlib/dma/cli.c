06035920a9df561f224453e56fa7a4620068387b
Marvin Liu <yong.liu@intel.com>
2022-08-17 09:38:40 +0800

    vlib: introduce DMA infrastructure
    This patch introduces DMA infrastructure into vlib. This is well known
    that large amount of memory movements will drain core resource. Nowadays
    more and more hardware accelerators were designed out for freeing core
    from this burden. Meanwhile some restrictions still remained when
    utilizing hardware accelerators, e.g. cross numa throughput will have a
    significant drop compared to same node. Normally the number of hardware
    accelerator instances will less than cores number, not to mention that
    applications number will even beyond the number of cores. Some hardware
    may support share virtual address with cores, while others are not.
    
    Here we introduce new DMA infrastructure which can fulfill the
    requirements of vpp applications like session and memif and in the
    meantime dealing with hardware limitations.
    
    Here is some design backgrounds:
    
      Backend is the abstract of resource which allocated from DMA device
      and can do some basic operations like configuration, DMA copy and
      result query.
    
      Config is the abstract of application DMA requirement. Application
      need to request an unique config index from DMA infrastructure. This
      unique config index is associated with backend resource. Two options
      cpu fallback and barrier before last can be specified in config.
      DMA transfer will be performed by CPU when backend is busy if cpu
      fallback option is enabled. DMA transfer callback will be in order
      if barrier before last option is enabled.
    
      We constructs all the stuffs that DMA transfer request needed into
      DMA batch. It contains the pattern of DMA descriptors and function
      pointers for submission and callback. One DMA transfer request need
      multiple times batch update and one time batch submission.
    
      DMA backends will assigned to config's workers threads equally. Lock
      will be used for thread-safety if same backends assigned to multiple
      threads. Backend node will check all the pending requests in worker
      thread and do callback with the pointer of DMA batch if transfer
      completed. Application can utilize cookie in DMA batch for selves
      usage.
    
    DMA architecture:
    
       +----------+   +----------+           +----------+   +----------+
       | Config1  |   | Config2  |           | Config1  |   | Config2  |
       +----------+   +----------+           +----------+   +----------+
            ||             ||                     ||             ||
       +-------------------------+           +-------------------------+
       |  DMA polling thread A   |           |  DMA polling thread B   |
       +-------------------------+           +-------------------------+
                   ||                                     ||
               +----------+                          +----------+
               | Backend1 |                          | Backend2 |
               +----------+                          +----------+
    
    Type: feature
    
    Signed-off-by: Marvin Liu <yong.liu@intel.com>
    Change-Id: I1725e0c26687985aac29618c9abe4f5e0de08ebf

---
+++ src/vlib/dma/cli.c
@@ -0,0 +1,160 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2022 Cisco Systems, Inc.
+ */
+
+#include <vlib/vlib.h>
+#include <vlib/physmem_funcs.h>
+#include <vlib/dma/dma.h>
+
+static clib_error_t *
+show_dma_backends_command_fn (vlib_main_t *vm, unformat_input_t *input,
+			      vlib_cli_command_t *cmd)
+{
+  vlib_dma_main_t *dm = &vlib_dma_main;
+
+  if (vec_len (dm->backends))
+    {
+      vlib_dma_backend_t *b;
+      vec_foreach (b, dm->backends)
+	vlib_cli_output (vm, "%s", b->name);
+    }
+  else
+    vlib_cli_output (vm, "No active DMA backends");
+
+  return 0;
+}
+
+VLIB_CLI_COMMAND (avf_create_command, static) = {
+  .path = "show dma backends",
+  .short_help = "show dma backends",
+  .function = show_dma_backends_command_fn,
+};
+
+static void
+test_dma_cb_fn (vlib_main_t *vm, vlib_dma_batch_t *b)
+{
+  fformat (stderr, "%s: cb %p cookie %lx\n", __func__, b,
+	   vlib_dma_batch_get_cookie (vm, b));
+}
+
+static clib_error_t *
+fill_random_data (void *buffer, uword size)
+{
+  uword seed = random_default_seed ();
+
+  uword remain = size;
+  const uword p = clib_mem_get_page_size ();
+  uword offset = 0;
+
+  clib_random_buffer_t rb;
+  clib_random_buffer_init (&rb, seed);
+
+  while (remain > 0)
+    {
+      uword fill_size = clib_min (p, remain);
+
+      clib_random_buffer_fill (&rb, fill_size);
+      void *rbuf = clib_random_buffer_get_data (&rb, fill_size);
+      clib_memcpy_fast (buffer + offset, rbuf, fill_size);
+      clib_random_buffer_free (&rb);
+
+      offset += fill_size;
+      remain -= fill_size;
+    }
+
+  return 0;
+}
+
+static clib_error_t *
+test_dma_command_fn (vlib_main_t *vm, unformat_input_t *input,
+		     vlib_cli_command_t *cmd)
+{
+  clib_error_t *err = 0;
+  vlib_dma_batch_t *b;
+  int config_index = -1;
+  u32 rsz, n_alloc, v;
+  u8 *from = 0, *to = 0;
+  vlib_dma_config_t cfg = { .max_transfers = 256,
+			    .max_transfer_size = 4096,
+			    .callback_fn = test_dma_cb_fn };
+
+  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (input, "transfers %u", &v))
+	cfg.max_transfers = v;
+      else if (unformat (input, "size %u", &v))
+	cfg.max_transfer_size = v;
+      else
+	return clib_error_return (0, "unknown input `%U'",
+				  format_unformat_error, input);
+    }
+
+  if ((config_index = vlib_dma_config_add (vm, &cfg)) < 0)
+    {
+      err = clib_error_return (0, "Unable to allocate dma config");
+      return err;
+    }
+
+  rsz = round_pow2 (cfg.max_transfer_size, CLIB_CACHE_LINE_BYTES);
+  n_alloc = rsz * cfg.max_transfers * 2;
+
+  if ((from = vlib_physmem_alloc_aligned_on_numa (
+	 vm, n_alloc, CLIB_CACHE_LINE_BYTES, vm->numa_node)) == 0)
+    {
+      err = clib_error_return (0, "Unable to allocate %u bytes of physmem",
+			       n_alloc);
+      return err;
+    }
+  to = from + n_alloc / 2;
+
+  u32 port_allocator_seed;
+
+  fill_random_data (from, cfg.max_transfers * rsz);
+
+  b = vlib_dma_batch_new (vm, config_index);
+  vlib_dma_batch_set_cookie (vm, b, 0x12345678);
+
+  port_allocator_seed = clib_cpu_time_now ();
+  int transfers = random_u32 (&port_allocator_seed) % cfg.max_transfers;
+  if (!transfers)
+    transfers = 1;
+  for (int i = 0; i < transfers; i++)
+    vlib_dma_batch_add (vm, b, to + i * rsz, from + i * rsz,
+			cfg.max_transfer_size);
+
+  vlib_dma_batch_submit (vm, b);
+  return err;
+}
+
+static clib_error_t *
+test_show_dma_fn (vlib_main_t *vm, unformat_input_t *input,
+		  vlib_cli_command_t *cmd)
+{
+  clib_error_t *err = 0;
+  int config_index = 0;
+  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (input, "config %u", &config_index))
+	;
+      else
+	return clib_error_return (0, "unknown input `%U'",
+				  format_unformat_error, input);
+    }
+
+  for (u32 i = 0; i < vlib_get_n_threads (); i++)
+    vlib_cli_output (vm, "Config %d %U", config_index, vlib_dma_config_info,
+		     config_index, vlib_get_main_by_index (i));
+  return err;
+}
+
+VLIB_CLI_COMMAND (test_dma_command, static) = {
+  .path = "test dma",
+  .short_help = "test dma [transfers <x> size <x>]",
+  .function = test_dma_command_fn,
+};
+
+VLIB_CLI_COMMAND (show_dma_command, static) = {
+  .path = "show dma",
+  .short_help = "show dma [config <x>]",
+  .function = test_show_dma_fn,
+};
