FDio/vpp

commit 561ae5d17b5565d0644a5249acc8fdb7a6f72b44

vppinfra: add clib_mem_set_numa_affinity()
Type: improvement
Change-Id: I81a7fb71c8ce0c0d22e326a4ddd01bc1c1aea5f7
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/vppinfra/linux/mem.c
@@ -366,7 +366,7 @@ legacy_memfd_create (u8 * name)
     {
       vec_reset_length (mm->error);
       mm->error = clib_error_return_unix (mm->error, "mkdtemp");
-      return -1;
+      return CLIB_MEM_ERROR;
     }
 
   if (mount ("none", mount_dir, "hugetlbfs", 0, NULL))
@@ -374,7 +374,7 @@ legacy_memfd_create (u8 * name)
       rmdir ((char *) mount_dir);
       vec_reset_length (mm->error);
       mm->error = clib_error_return_unix (mm->error, "mount");
-      return -1;
+      return CLIB_MEM_ERROR;
     }
 
   filename = format (0, "%s/%s%c", mount_dir, name, 0);
@@ -407,7 +407,7 @@ clib_mem_vm_create_fd (clib_mem_page_sz_t log2_page_size, char *fmt, ...)
   switch (log2_page_size)
     {
     case CLIB_MEM_PAGE_SZ_UNKNOWN:
-      return -1;
+      return CLIB_MEM_ERROR;
     case CLIB_MEM_PAGE_SZ_DEFAULT:
       memfd_flags = MFD_ALLOW_SEALING;
       break;
@@ -441,7 +441,7 @@ clib_mem_vm_create_fd (clib_mem_page_sz_t log2_page_size, char *fmt, ...)
       vec_reset_length (mm->error);
       mm->error = clib_error_return_unix (mm->error, "memfd_create");
       vec_free (s);
-      return -1;
+      return CLIB_MEM_ERROR;
     }
 
   vec_free (s);
@@ -452,7 +452,7 @@ clib_mem_vm_create_fd (clib_mem_page_sz_t log2_page_size, char *fmt, ...)
       vec_reset_length (mm->error);
       mm->error = clib_error_return_unix (mm->error, "fcntl (F_ADD_SEALS)");
       close (fd);
-      return -1;
+      return CLIB_MEM_ERROR;
     }
 
   return fd;
@@ -640,11 +640,11 @@ clib_mem_vm_unmap (void *base)
   clib_mem_vm_map_hdr_t *hdr = base - sys_page_sz;;
 
   if (mprotect (hdr, sys_page_sz, PROT_READ | PROT_WRITE) != 0)
-    return -1;
+    return CLIB_MEM_ERROR;
 
   size = hdr->num_pages << hdr->log2_page_sz;
   if (munmap ((void *) hdr->base_addr, size) != 0)
-    return -1;
+    return CLIB_MEM_ERROR;
 
   if (hdr->next)
     {
@@ -665,7 +665,7 @@ clib_mem_vm_unmap (void *base)
     mm->first_map = hdr->next;
 
   if (munmap (hdr, sys_page_sz) != 0)
-    return -1;
+    return CLIB_MEM_ERROR;
 
   return 0;
 }
@@ -806,6 +806,55 @@ clib_mem_vm_ext_map (clib_mem_vm_map_t * a)
 
 done:
   return err;
+}
+
+int
+clib_mem_set_numa_affinity (u8 numa_node, int force)
+{
+  clib_mem_main_t *mm = &clib_mem_main;
+  long unsigned int mask[16] = { 0 };
+  int mask_len = sizeof (mask) * 8 + 1;
+
+  /* no numa support */
+  if (mm->numa_node_bitmap == 0)
+    {
+      if (numa_node)
+	{
+	  vec_reset_length (mm->error);
+	  mm->error = clib_error_return (mm->error, "%s: numa not supported",
+					 (char *) __func__);
+	  return CLIB_MEM_ERROR;
+	}
+      else
+	return 0;
+    }
+
+  mask[0] = 1 << numa_node;
+
+  if (set_mempolicy (force ? MPOL_BIND : MPOL_PREFERRED, mask, mask_len))
+    goto error;
+
+  vec_reset_length (mm->error);
+  return 0;
+
+error:
+  vec_reset_length (mm->error);
+  mm->error = clib_error_return_unix (mm->error, (char *) __func__);
+  return CLIB_MEM_ERROR;
+}
+
+int
+clib_mem_set_default_numa_affinity ()
+{
+  clib_mem_main_t *mm = &clib_mem_main;
+
+  if (set_mempolicy (MPOL_DEFAULT, 0, 0))
+    {
+      vec_reset_length (mm->error);
+      mm->error = clib_error_return_unix (mm->error, (char *) __func__);
+      return CLIB_MEM_ERROR;
+    }
+  return 0;
 }
 
 /*

src/vppinfra/mem.h
@@ -54,6 +54,7 @@
 #define CLIB_MAX_MHEAPS 256
 #define CLIB_MAX_NUMAS 16
 #define CLIB_MEM_VM_MAP_FAILED ((void *) ~0)
+#define CLIB_MEM_ERROR (-1)
 
 typedef enum
 {
@@ -476,6 +477,8 @@ u64 *clib_mem_vm_get_paddr (void *mem, clib_mem_page_sz_t log2_page_size,
 			    int n_pages);
 void clib_mem_destroy_mspace (void *mspace);
 void clib_mem_destroy (void);
+int clib_mem_set_numa_affinity (u8 numa_node, int force);
+int clib_mem_set_default_numa_affinity ();
 
 typedef struct
 {

commit 803387b3615227674b6eb053e3b82a140719feea

vppinfra: fix set mempolicy for different cpu arch
On Ubuntu 22.04 aarch64 multiple NUMA node platform,
VPP debug version fail to start, this starts with kernel version 5.15
and above, following message pops out when this happens,

buffer [warn  ]: numa[1] falling back to non-hugepage backed buffer pool
(vlib_physmem_shared_map_create: pmalloc_map_pages:
failed to set mempolicy for numa node 1: Invalid argument)

Root cause:
x86 and aarch64 have different max numa node size by default.
1024 for x86 and 64 for aarch64, controlled by CONFIG_NODES_SHIFT.

VPP uses u64 mask[16] and mask_len(1025) for set_mempolicy syscall for
both x86 and aarch64,

On aarch64 platform, the larger mask_len(1025) will trigger out of bound
array access issue in kernel code (fixed in patch[1]),
and return undefined value for set_mempolicy syscall.
Then VPP gets error from this syscall and exit.

To fix this from VPP side without changing linux kernel,
set VPP's mask array size with proper mask_len size for
different CPU arch. We can get this max numa node size via libnuma's api
numa_num_possible_nodes().

[1] https://github.com/torvalds/linux/commit/000eca5d044d1ee23b4ca311793cf3fc528da6c6

Type: fix
Fixes: 561ae5d17b55 ("vppinfra: add clib_mem_set_numa_affinity()")

Signed-off-by: Tianyu Li <tianyu.li@arm.com>
Change-Id: If99569af0c0378d6fef78c7611d65a4aea0ea192

src/vppinfra/CMakeLists.txt
@@ -222,7 +222,7 @@ if(VPP_USE_EXTERNAL_LIBEXECINFO)
 endif()
 add_vpp_library(vppinfra
   SOURCES ${VPPINFRA_SRCS}
-  LINK_LIBRARIES m ${EXECINFO_LIB}
+  LINK_LIBRARIES m numa ${EXECINFO_LIB}
   INSTALL_HEADERS ${VPPINFRA_HEADERS}
   COMPONENT libvppinfra
   LTO

src/vppinfra/linux/mem.c
@@ -31,6 +31,7 @@
 #include <vppinfra/format.h>
 #include <vppinfra/clib_error.h>
 #include <vppinfra/linux/sysfs.h>
+#include <numa.h>
 
 #ifndef F_LINUX_SPECIFIC_BASE
 #define F_LINUX_SPECIFIC_BASE 1024
@@ -609,7 +610,9 @@ clib_mem_set_numa_affinity (u8 numa_node, int force)
 {
   clib_mem_main_t *mm = &clib_mem_main;
   long unsigned int mask[16] = { 0 };
-  int mask_len = sizeof (mask) * 8 + 1;
+  int numa_max_node_num = numa_num_possible_nodes ();
+  ASSERT (numa_max_node_num <= sizeof (mask) * 8);
+  int mask_len = numa_max_node_num + 1;
 
   /* no numa support */
   if (mm->numa_node_bitmap == 0)