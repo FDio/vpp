5de18c719372c0f2f8b698caae126b66a178fc2c
Huawei LI <lihuawei_zzu@163.com>
2022-09-29 11:28:12 +0800

    fib: fix crash when create vxlan/vxlan-gpe/geneve/gtpu tunnel.
    Fix vpp crash when create vxlan/vxlan-gpe/geneve/gtpu tunnel
    with 0.0.0.0 dst ip in debug build. The ASSERT should be move
    out of fib_prefix_from_ip46_addr, which may be called when
    create vxlan/vxlan-gpe/geneve/gtpu tunnel with 0.0.0.0 dst ip.
    
    How to reproduce:
    1. build debug vpp and run vpp
    2. create vxlan t src 192.168.0.2 dst 0.0.0.0 vni 1 instance 1
       create vxlan-gpe tunnel local 192.168.0.2 remote 0.0.0.0 vni 1
       create geneve tunnel local 192.168.0.2 remote 0.0.0.0 vni 1
       create gtpu tunnel src 192.168.0.2 dst 0.0.0.0 teid 1
    
    Type: fix
    
    Change-Id: I19972f6af588f4ff7fd17de1b16b9301e43d596f
    Signed-off-by: Huawei LI <lihuawei_zzu@163.com>

--- src/vnet/fib/fib_types.h
+++ src/vnet/fib/fib_types.h
@@ -276,8 +276,9 @@ extern void fib_prefix_normalize(const fib_prefix_t *p,
 /**
  * \brief Host prefix from ip
  */
-extern void fib_prefix_from_ip46_addr (const ip46_address_t *addr,
-			   fib_prefix_t *pfx);
+extern void fib_prefix_from_ip46_addr (fib_protocol_t fproto,
+		                       const ip46_address_t *addr,
+		                       fib_prefix_t *pfx);
 
 extern u8 * format_fib_prefix(u8 * s, va_list * args);
 extern u8 * format_fib_forw_chain_type(u8 * s, va_list * args);
