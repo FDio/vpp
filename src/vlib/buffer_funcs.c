FDio/vpp

commit f8631ce7e8886136b4543a7926ffdf1bc760fb11

buffers: protect against bad thread indices
There is a very rare bug in NAT processing that yeilds a thread
index of ~0.  When this happens, vlib_get_frame_queue_elt()
suffers a segfault and VPP quits.  Prevent an outright fault
by dropping the packet instead.

Type: fix
Signed-off-by: Jon Loeliger <jdl@netgate.com>
Change-Id: I48c7a268925bb821ea15e58db5d4bfb211c40c09

src/vlib/buffer_funcs.c
@@ -261,6 +261,9 @@ vlib_get_frame_queue_elt (vlib_frame_queue_main_t *fqm, u32 index,
   vlib_frame_queue_t *fq;
   u64 nelts, tail, new_tail;
 
+  if (index >= vec_len (fqm->vlib_frame_queues))
+    return 0;
+
   fq = fqm->vlib_frame_queues[index];
   ASSERT (fq);
   nelts = fq->nelts;

commit e739c87fa62cbc9f283ebf814e9a34c2e0285d33

buffers: fix buffer leak on enqueue to bad thread
After commit f8631ce7e8886136b4543a7926ffdf1bc760fb11 packets can be
dropped due invalid thread index even without drop_on_congestion set.
Under normal circumstances without drop_on_congestion set thread will
perform blocking wait until a frame queue ring slot is available - no
drop is possible.
So, avoid buffer leakage by freeing all the drop buffers, if any.

Type: fix
Fixes: f8631ce7e8886136b4543a7926ffdf1bc760fb11
Change-Id: I25f6919885bb8bce64395bb44d0df58a5795afe7
Signed-off-by: Vladislav Grishenko <themiron@yandex-team.ru>

src/vlib/buffer_funcs.c
@@ -343,7 +343,7 @@ more:
       goto more;
     }
 
-  if (drop_on_congestion && n_drop)
+  if (n_drop)
     vlib_buffer_free (vm, drop_list, n_drop);
 
   return n_packets - n_drop;