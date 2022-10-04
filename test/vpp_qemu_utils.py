b1645d7b1fb604998645d6546282dc95267568ea
Naveen Joy <najoy@cisco.com>
2022-10-04 14:22:05 -0700

    tests: gso qemu tests for tapv2
    Type: test
    
    Change-Id: I5f9d8cc80d20b4e34011fc8a87e35659bd9613bc
    Signed-off-by: Naveen Joy <najoy@cisco.com>

--- test/vpp_qemu_utils.py
+++ test/vpp_qemu_utils.py
@@ -6,10 +6,37 @@ import subprocess
 
 
 def create_namespace(ns):
+    """create one or more namespaces.
+
+    arguments:
+    ns -- a string value or an iterable of namespace names
+    """
+    if isinstance(ns, str):
+        namespaces = [ns]
+    else:
+        namespaces = ns
     try:
-        subprocess.run(["ip", "netns", "add", ns])
+        for namespace in namespaces:
+            subprocess.run(["ip", "netns", "add", namespace])
     except subprocess.CalledProcessError as e:
         raise Exception("Error creating namespace:", e.output)
+
+
+def delete_namespace(ns):
+    """delete one or more namespaces.
+
+    arguments:
+    ns -- a string value or an iterable of namespace names
+    """
+    if isinstance(ns, str):
+        namespaces = [ns]
+    else:
+        namespaces = ns
+    try:
+        for namespace in namespaces:
+            subprocess.run(["ip", "netns", "del", namespace])
+    except subprocess.CalledProcessError as e:
+        raise Exception("Error deleting namespace:", e.output)
 
 
 def list_namespace(ns):
