c2b72491bee7797ff05aec7a6fa0bf53e802c226
Florin Coras <fcoras@cisco.com>
2022-11-09 15:54:39 -0800

    session: transport endpt cleanup on owner thread
    Maintain a single writer multiple readers usage model for transport
    endpoints pool.
    
    Type: improvement
    
    Signed-off-by: Florin Coras <fcoras@cisco.com>
    Change-Id: I8555700ed725971341f145ea97f031042a298e83

--- src/vnet/session/transport.c
+++ src/vnet/session/transport.c
@@ -25,6 +25,7 @@ transport_proto_vft_t *tp_vfts;
 typedef struct local_endpoint_
 {
   transport_endpoint_t ep;
+  transport_proto_t proto;
   int refcnt;
 } local_endpoint_t;
 
@@ -32,6 +33,7 @@ typedef struct transport_main_
 {
   transport_endpoint_table_t local_endpoints_table;
   local_endpoint_t *local_endpoints;
+  u32 *lcl_endpts_freelist;
   u32 port_allocator_seed;
   clib_spinlock_t local_endpoints_lock;
 } transport_main_t;
@@ -427,34 +429,64 @@ transport_endpoint_alloc (void)
   local_endpoint_t *lep;
 
   ASSERT (vlib_get_thread_index () <= transport_cl_thread ());
+
+  /* Cleanup freelist if need be */
+  if (vec_len (tm->lcl_endpts_freelist))
+    {
+      local_endpoint_t *lep;
+      u32 *lep_indexp;
+
+      clib_spinlock_lock (&tm->local_endpoints_lock);
+
+      vec_foreach (lep_indexp, tm->lcl_endpts_freelist)
+	{
+	  lep = pool_elt_at_index (tm->local_endpoints, *lep_indexp);
+
+	  /* Port re-shared after attempt to cleanup */
+	  if (lep->refcnt > 0)
+	    continue;
+
+	  transport_endpoint_table_del (&tm->local_endpoints_table, lep->proto,
+					&lep->ep);
+	  transport_endpoint_free (*lep_indexp);
+	}
+
+      vec_reset_length (tm->lcl_endpts_freelist);
+
+      clib_spinlock_unlock (&tm->local_endpoints_lock);
+    }
+
   pool_get_aligned_safe (tm->local_endpoints, lep, 0);
   return lep;
 }
 
-void
-transport_endpoint_cleanup (u8 proto, ip46_address_t * lcl_ip, u16 port)
+int
+transport_release_local_endpoint (u8 proto, ip46_address_t *lcl_ip, u16 port)
 {
   transport_main_t *tm = &tp_main;
   local_endpoint_t *lep;
   u32 lepi;
 
-  /* Cleanup local endpoint if this was an active connect */
   lepi = transport_endpoint_lookup (&tm->local_endpoints_table, proto, lcl_ip,
 				    clib_net_to_host_u16 (port));
   if (lepi == ENDPOINT_INVALID_INDEX)
-    return;
+    return -1;
 
   lep = pool_elt_at_index (tm->local_endpoints, lepi);
+
+  /* Local endpoint no longer in use, program cleanup */
   if (!clib_atomic_sub_fetch (&lep->refcnt, 1))
     {
-      transport_endpoint_table_del (&tm->local_endpoints_table, proto,
-				    &lep->ep);
-
-      /* All workers can free connections. Synchronize access to pool */
+      /* All workers can free connections. Synchronize access to freelist */
       clib_spinlock_lock (&tm->local_endpoints_lock);
-      transport_endpoint_free (lepi);
+      vec_add1 (tm->lcl_endpts_freelist, lepi);
       clib_spinlock_unlock (&tm->local_endpoints_lock);
+
+      return 0;
     }
+
+  /* Not an error, just in idication that endpoint was not cleaned up */
+  return -1;
 }
 
 static int
@@ -475,6 +507,7 @@ transport_endpoint_mark_used (u8 proto, ip46_address_t *ip, u16 port)
   lep = transport_endpoint_alloc ();
   clib_memcpy_fast (&lep->ep.ip, ip, sizeof (*ip));
   lep->ep.port = port;
+  lep->proto = proto;
   lep->refcnt = 1;
 
   transport_endpoint_table_add (&tm->local_endpoints_table, proto, &lep->ep,
@@ -490,6 +523,9 @@ transport_share_local_endpoint (u8 proto, ip46_address_t * lcl_ip, u16 port)
   local_endpoint_t *lep;
   u32 lepi;
 
+  /* Active opens should call this only from a control thread, which are also
+   * used to allocate and free ports. So, pool has only one writer and
+   * potentially many readers. Listeners are allocated with barrier */
   lepi = transport_endpoint_lookup (&tm->local_endpoints_table, proto, lcl_ip,
 				    clib_net_to_host_u16 (port));
   if (lepi != ENDPOINT_INVALID_INDEX)
