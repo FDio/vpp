598dbeb8308dea33592bca595238e5b3e79465e2
Florin Coras <fcoras@cisco.com>
2022-11-11 11:37:36 -0800

    udp: refactor port allocation and sharing
    Type: improvement
    
    Signed-off-by: Florin Coras <fcoras@cisco.com>
    Change-Id: I4f7314ddf95d26f1939bd3772d29d011fb4cea47

--- src/vnet/udp/udp_input.c
+++ src/vnet/udp/udp_input.c
@@ -123,8 +123,10 @@ udp_connection_accept (udp_connection_t * listener, session_dgram_hdr_t * hdr,
       udp_connection_free (uc);
       return 0;
     }
-  udp_connection_share_port (clib_net_to_host_u16
-			     (uc->c_lcl_port), uc->c_is_ip4);
+  transport_share_local_endpoint (TRANSPORT_PROTO_UDP, &uc->c_lcl_ip,
+				  uc->c_lcl_port);
+  //   udp_connection_share_port (clib_net_to_host_u16
+  // 			     (uc->c_lcl_port), uc->c_is_ip4);
   return uc;
 }
 
