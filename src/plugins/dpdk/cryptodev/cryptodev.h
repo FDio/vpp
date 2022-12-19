92adf1ca86a55ee60de31bccb6fff0d2d70fc1c7
Xinyao Cai <xinyao.cai@intel.com>
2022-12-19 19:10:37 +0800

    dpdk: bump to dpdk 22.11
    This patch bumps DPDK version to 22.11.
    The DPDK build flag "enable_driver_sdk" is enabled to
    obtain the definition of rte_pci_device from DPDK internal header file,
    which is used during VPP startup.
    
    Type: feature
    Signed-off-by: Xinyao Cai <xinyao.cai@intel.com>
    Change-Id: Ie120346c332e362fd1a3148b316612ce102fd7e6

--- src/plugins/dpdk/cryptodev/cryptodev.h
+++ src/plugins/dpdk/cryptodev/cryptodev.h
@@ -81,10 +81,18 @@ typedef enum
   CRYPTODEV_N_OP_TYPES,
 } cryptodev_op_type_t;
 
+typedef
+#if RTE_VERSION >= RTE_VERSION_NUM(22, 11, 0, 0)
+  void
+#else
+  struct rte_cryptodev_sym_session
+#endif
+    cryptodev_session_t;
+
 /* Cryptodev session data, one data per direction per numa */
 typedef struct
 {
-  struct rte_cryptodev_sym_session ***keys;
+  cryptodev_session_t ***keys;
 } cryptodev_key_t;
 
 /* Replicate DPDK rte_cryptodev_sym_capability structure with key size ranges
@@ -125,7 +133,9 @@ typedef struct
 typedef struct
 {
   struct rte_mempool *sess_pool;
+#if RTE_VERSION < RTE_VERSION_NUM(22, 11, 0, 0)
   struct rte_mempool *sess_priv_pool;
+#endif
 } cryptodev_session_pool_t;
 
 typedef struct
@@ -163,7 +173,7 @@ typedef struct
       u16 aad_index;
       u8 *aad_buf;
       u64 aad_phy_addr;
-      struct rte_cryptodev_sym_session *reset_sess;
+      cryptodev_session_t *reset_sess;
     };
   };
   u16 cryptodev_id;
@@ -184,6 +194,7 @@ typedef struct
   u32 sess_sz;
   u32 drivers_cnt;
   u8 is_raw_api;
+  u8 driver_id;
 } cryptodev_main_t;
 
 extern cryptodev_main_t cryptodev_main;
