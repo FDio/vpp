b9fe75b379c3c603747083399e293382a4bda1dc
Benoît Ganne <bganne@cisco.com>
2023-01-05 10:56:26 +0100

    crypto: make it easier to diagnose keys use-after-free
    Type: improvement
    
    Change-Id: Ib98eba146e24e659acf3b9a228b81fcd641f4c67
    Signed-off-by: Benoît Ganne <bganne@cisco.com>

--- src/vnet/crypto/crypto.c
+++ src/vnet/crypto/crypto.c
@@ -469,12 +469,12 @@ vnet_crypto_key_del (vlib_main_t * vm, vnet_crypto_key_index_t index)
 
   if (key->type == VNET_CRYPTO_KEY_TYPE_DATA)
     {
-      clib_memset (key->data, 0, vec_len (key->data));
+      clib_memset (key->data, 0xfe, vec_len (key->data));
       vec_free (key->data);
     }
   else if (key->type == VNET_CRYPTO_KEY_TYPE_LINK)
     {
-      key->index_crypto = key->index_integ = 0;
+      key->index_crypto = key->index_integ = ~0;
     }
 
   pool_put (cm->keys, key);
