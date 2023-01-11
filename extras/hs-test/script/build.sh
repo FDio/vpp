a826129307d829de4a8086cb577a28d2eec4934b
Maros Ondrejicka <maros.ondrejicka@pantheon.tech>
2023-01-11 10:43:51 +0100

    hs-test: containerize hs-test build process
    Type: test
    Signed-off-by: Maros Ondrejicka <maros.ondrejicka@pantheon.tech>
    Change-Id: I95ec5dd7bd7e2a48b79adfd29cac9dd30c92cf2a

--- extras/hs-test/script/build.sh
+++ extras/hs-test/script/build.sh
@@ -2,13 +2,7 @@
 
 source vars
 
-bin=vpp-data/bin
-lib=vpp-data/lib
-
-mkdir -p ${bin} ${lib} || true
-
-cp ${VPP_WS}/build-root/build-vpp_debug-native/vpp/bin/* ${bin}
-cp -r ${VPP_WS}/build-root/build-vpp_debug-native/vpp/lib/x86_64-linux-gnu/* ${lib}
+docker build --build-arg UBUNTU_VERSION -t hs-test/build -f Dockerfile.build ../../
 
 docker build --build-arg UBUNTU_VERSION -t hs-test/vpp -f Dockerfile.vpp .
 docker build --build-arg UBUNTU_VERSION -t hs-test/nginx-ldp -f Dockerfile.nginx .
