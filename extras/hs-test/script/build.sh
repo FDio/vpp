724335f146d7b13bf06e63ccd81b1be5e9008401
Maros Ondrejicka <maros.ondrejicka@pantheon.tech>
2023-01-11 10:43:51 +0100

    hs-test: containerize hs-test build process
    VPP is built in dedicated `build` image so that its done
    in equal Ubuntu version as the images used to run tests.
    The build image has ~15GB size.
    
    Type: test
    Signed-off-by: Maros Ondrejicka <maros.ondrejicka@pantheon.tech>
    Change-Id: I95ec5dd7bd7e2a48b79adfd29cac9dd30c92cf2a

--- extras/hs-test/script/build.sh
+++ extras/hs-test/script/build.sh
@@ -2,13 +2,14 @@
 
 source vars
 
-bin=vpp-data/bin
-lib=vpp-data/lib
-
-mkdir -p ${bin} ${lib} || true
-
-cp ${VPP_WS}/build-root/build-vpp_debug-native/vpp/bin/* ${bin}
-cp -r ${VPP_WS}/build-root/build-vpp_debug-native/vpp/lib/x86_64-linux-gnu/* ${lib}
+docker build --build-arg UBUNTU_VERSION -t hs-test/build -f Dockerfile.build ../../
 
 docker build --build-arg UBUNTU_VERSION -t hs-test/vpp -f Dockerfile.vpp .
 docker build --build-arg UBUNTU_VERSION -t hs-test/nginx-ldp -f Dockerfile.nginx .
+
+mkdir -p ${bin} ${lib} || true
+
+container_id=$(docker create hs-test/build)
+docker cp ${container_id}:/vpp/build-root/build-vpp_debug-native/vpp/bin ./vpp-data/
+docker cp ${container_id}:/vpp/build-root/build-vpp_debug-native/vpp/lib/x86_64-linux-gnu ${lib}/
+docker rm $container_id
