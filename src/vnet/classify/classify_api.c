8ebf07805328ddb233001a60dfae762182e816fc
Maxime Peim <mpeim@cisco.com>
2023-01-13 08:04:55 +0000

    api: pcap capture api update
    Allow enabling and disabling pcap capture via the API.
    A little bug is fixed along the way in
    vl_api_classify_pcap_set_table_t_handler.
    
    Type: improvement
    
    Signed-off-by: Maxime Peim <mpeim@cisco.com>
    Change-Id: I096129c82aecdc82bee5dbfb5e19c76a51d80aab

--- src/vnet/classify/classify_api.c
+++ src/vnet/classify/classify_api.c
@@ -115,9 +115,8 @@ static void vl_api_classify_pcap_set_table_t_handler
   u32 table_index = ntohl (mp->table_index);
   u32 sw_if_index = ntohl (mp->sw_if_index);
 
-  if (sw_if_index == ~0
-      || sw_if_index >= vec_len (cm->classify_table_index_by_sw_if_index)
-      || (table_index != ~0 && pool_is_free_index (cm->tables, table_index)))
+  if (sw_if_index == ~0 ||
+      (table_index != ~0 && pool_is_free_index (cm->tables, table_index)))
     {
       rv = VNET_API_ERROR_INVALID_VALUE;
       goto out;
