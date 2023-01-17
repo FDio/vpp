f643b6f67111061e90ea41471b4b42c75b7a5988
Maros Ondrejicka <maros.ondrejicka@pantheon.tech>
2023-01-17 10:52:20 +0100

    hs-test: autodetect ubuntu version during build
    Since VPP binaries are being compiled on host system,
    it makes sense to autodetect Ubuntu version when building test images
    so that containers would be running version equal to host system.
    
    Type: test
    Signed-off-by: Maros Ondrejicka <maros.ondrejicka@pantheon.tech>
    Change-Id: I0e13d9ba1ddcd3ad5835bce1b8cccfc048e5e528

--- extras/hs-test/script/build.sh
+++ extras/hs-test/script/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ $(lsb_release -is) != Ubuntu ]; then
+	echo "Host stack test framework is supported only on Ubuntu"
+	exit 1
+fi
+
 source vars
 
 bin=vpp-data/bin
@@ -8,7 +13,13 @@ lib=vpp-data/lib
 mkdir -p ${bin} ${lib} || true
 
 cp ${VPP_WS}/build-root/build-vpp_debug-native/vpp/bin/* ${bin}
+res+=$?
 cp -r ${VPP_WS}/build-root/build-vpp_debug-native/vpp/lib/x86_64-linux-gnu/* ${lib}
+res+=$?
+if [ $res -ne 0 ]; then
+	echo "Failed to copy VPP files. Is VPP built? Try running 'make build' in VPP directory."
+	exit 1
+fi
 
 docker build --build-arg UBUNTU_VERSION -t hs-test/vpp -f docker/Dockerfile.vpp .
 docker build --build-arg UBUNTU_VERSION -t hs-test/nginx-ldp -f docker/Dockerfile.nginx .
