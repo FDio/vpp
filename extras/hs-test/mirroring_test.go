54a8b94398edcc704988522dc6e0b68d18690132
Maros Ondrejicka <mondreji@cisco.com>
2023-02-09 11:04:39 +0100

    hs-test: test mirroring with vpp+nginx proxy
    Type: test
    Signed-off-by: Maros Ondrejicka <mondreji@cisco.com>
    Change-Id: I27a917673b9ef31792d593d0b21e2631dc5c91d1

---
+++ extras/hs-test/mirroring_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/edwarnicke/exechelper"
+)
+
+func (s *MirroringSuite) TestMirroring() {
+	proxyAddress := s.netInterfaces[mirroringProxyInterfaceName].IP4AddressString()
+	clientNamespace := s.netInterfaces[mirroringClientInterfaceName].(*NetworkInterfaceVeth).networkNamespace
+
+	path := "/return_ok"
+
+	testCommand := "wrk -c 20 -t 10 -d 40 http://" + proxyAddress + ":80" + path
+	testCommand = "ip netns exec " + clientNamespace + " " + testCommand
+	s.log(testCommand)
+	o, _ := exechelper.Output(testCommand)
+	s.log(string(o))
+	s.assertNotEmpty(o)
+
+	// Check if log output from VPP contains 'no lcl port' warnings
+	logContent := s.getContainerByName(vppProxyContainerName).log()
+	s.assertNotContains(logContent, "no lcl port")
+}
