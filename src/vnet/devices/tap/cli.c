ba5f860f5f08b500c19c49314c13b72f5bfdaa70
Chen Yahui <goodluckwillcomesoon@gmail.com>
2023-02-21 21:00:13 +0800

    tap: add interface type check
    Running the show tun command with a non-tap interface causes the vpp
    process to crash.
    
    Therefore, the check of the interface type needs to be added.
    
    Type: fix
    
    Signed-off-by: Chen Yahui <goodluckwillcomesoon@gmail.com>
    Change-Id: I02a376247e66f92c79b7892d104659cddbb793f9

--- src/vnet/devices/tap/cli.c
+++ src/vnet/devices/tap/cli.c
@@ -331,12 +331,28 @@ tun_show_command_fn (vlib_main_t * vm, unformat_input_t * input,
   int show_descr = 0;
   clib_error_t *error = 0;
   u32 hw_if_index, *hw_if_indices = 0;
+  bool found = false;
 
   while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
     {
       if (unformat
 	  (input, "%U", unformat_vnet_hw_interface, vnm, &hw_if_index))
-	vec_add1 (hw_if_indices, hw_if_index);
+	{
+	  pool_foreach (vif, mm->interfaces)
+            if (hw_if_index == vif->hw_if_index)
+              {
+                found = true;
+                break;
+              }
+
+          if (!found)
+            {
+              error = clib_error_return (0, "not tun/tap interface");
+              goto done;
+            }
+
+          vec_add1 (hw_if_indices, vif->hw_if_index);
+	}
       else if (unformat (input, "descriptors"))
 	show_descr = 1;
       else
