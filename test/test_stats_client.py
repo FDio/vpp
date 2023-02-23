e2f014c8e42e520c0535b787a76ede9bcef7966d
Benoît Ganne <bganne@cisco.com>
2023-02-23 18:43:48 +0100

    stats: fix collector
    Type: fix
    Fixes: c5d81b91be30c9a69bb5333f2685f536246dd1f8
    
    Change-Id: I6e652a6401b9b81458344acc9e5518bacd3b91cb
    Signed-off-by: Benoît Ganne <bganne@cisco.com>

--- test/test_stats_client.py
+++ test/test_stats_client.py
@@ -4,13 +4,11 @@ import unittest
 import psutil
 from vpp_papi.vpp_stats import VPPStats
 
-from framework import tag_fixme_vpp_workers
 from framework import VppTestCase, VppTestRunner
 from scapy.layers.l2 import Ether
 from scapy.layers.inet import IP
 
 
-@tag_fixme_vpp_workers
 class StatsClientTestCase(VppTestCase):
     """Test Stats Client"""
 
