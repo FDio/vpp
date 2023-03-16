e300f912c62da1d01f631070d85b0c73fc2e4dbd
Ting Xu <ting.xu@intel.com>
2023-03-16 01:22:33 +0000

    packetforge: add option to show spec and mask only
    In some cases with Generic FLow, it is only required to show the pattern
    of spec and mask, but no need to add the flow. Therefore, add an option
    in packetforge so that users can show spec and mask only.
    
    Type: improvement
    
    Signed-off-by: Ting Xu <ting.xu@intel.com>
    Change-Id: I7b3040689eb82d0b58924712ee6fc9cfa0a42fa1

--- extras/packetforge/flow_create.py
+++ extras/packetforge/flow_create.py
@@ -32,12 +32,13 @@ API_FILE_SUFFIX = "*.api.json"
 def Main(argv):
     file_flag = False
     operation = None
+    show_result_only = False
     try:
         opts, args = getopt.getopt(
             argv,
-            "hf:p:a:i:I:",
+            "hf:p:a:i:I:s",
             [
-                "help=",
+                "help",
                 "add",
                 "del",
                 "file=",
@@ -45,17 +46,18 @@ def Main(argv):
                 "actions=",
                 "interface=",
                 "flow-index=",
+                "show-result-only"
             ],
         )
     except getopt.GetoptError:
         print(
-            "flow_create.py --add|del -f <file> -p <pattern> -a <actions> -i <interface> -I <flow-index>"
+            "flow_create.py --add|del -f <file> -p <pattern> -a <actions> -i <interface> -I <flow-index> -s <show-result-only>"
         )
         sys.exit()
     for opt, arg in opts:
         if opt == "-h":
             print(
-                "flow_create.py --add|del -f <file> -p <pattern> -a <actions> -i <interface> -I <flow-index>"
+                "flow_create.py --add|del -f <file> -p <pattern> -a <actions> -i <interface> -I <flow-index> -s <show-result-only>"
             )
             sys.exit()
         elif opt == "--add":
@@ -74,6 +76,8 @@ def Main(argv):
             iface = arg
         elif opt in ("-I", "--flow-index"):
             flow_index = arg
+        elif opt in ("-s", "--show-result-only"):
+            show_result_only = True
 
     if operation == None:
         print("Error: Please choose the operation: add or del")
@@ -84,9 +88,9 @@ def Main(argv):
             result = packetforge.Forge(pattern, actions, False)
         else:
             result = packetforge.Forge(json_file, actions, True)
-        return result, int(iface), operation, None
+        return result, int(iface), operation, None, show_result_only
     elif operation == "del":
-        return None, int(iface), operation, int(flow_index)
+        return None, int(iface), operation, int(flow_index), show_result_only
 
 
 def load_json_api_files(suffix=API_FILE_SUFFIX):
@@ -119,38 +123,44 @@ if __name__ == "__main__":
     print(vpp.api.show_version())
 
     # Parse the arguments
-    my_flow, iface, operation, del_flow_index = Main(sys.argv[1:])
+    my_flow, iface, operation, del_flow_index, show_result_only = Main(sys.argv[1:])
 
     # set inteface states
     vpp.api.sw_interface_set_flags(sw_if_index=iface, flags=1)
 
     if operation == "add":
-        # add flow
-        rv = vpp.api.flow_add_v2(flow=my_flow)
-        flow_index = rv[3]
-        print(rv)
+        if (show_result_only):
+            print(my_flow)
+        else:
+            # add flow
+            rv = vpp.api.flow_add_v2(flow=my_flow)
+            flow_index = rv[3]
+            print(rv)
 
-        # enable added flow
-        rv = vpp.api.flow_enable(flow_index=flow_index, hw_if_index=iface)
-        ena_res = rv[2]
-        # if enable flow fail, delete added flow
-        if ena_res:
-            print("Error: enable flow failed, delete flow")
-            rv = vpp.api.flow_del(flow_index=flow_index)
-        print(rv)
+            # enable added flow
+            rv = vpp.api.flow_enable(flow_index=flow_index, hw_if_index=iface)
+            ena_res = rv[2]
+            # if enable flow fail, delete added flow
+            if ena_res:
+                print("Error: enable flow failed, delete flow")
+                rv = vpp.api.flow_del(flow_index=flow_index)
+            print(rv)
 
     elif operation == "del":
-        # disable flow
-        rv = vpp.api.flow_disable(flow_index=del_flow_index, hw_if_index=iface)
-        dis_res = rv[2]
-        if dis_res:
-            print("Error: disable flow failed")
-            sys.exit()
-        print(rv)
+        if (show_result_only):
+            print("Cannot show spec and mask only in deletion")
+        else:
+            # disable flow
+            rv = vpp.api.flow_disable(flow_index=del_flow_index, hw_if_index=iface)
+            dis_res = rv[2]
+            if dis_res:
+                print("Error: disable flow failed")
+                sys.exit()
+            print(rv)
 
-        # delete flow
-        rv = vpp.api.flow_del(flow_index=del_flow_index)
-        print(rv)
+            # delete flow
+            rv = vpp.api.flow_del(flow_index=del_flow_index)
+            print(rv)
 
 # command example:
 # python flow_create.py --add -p "mac()/ipv4(src=1.1.1.1,dst=2.2.2.2)/udp()" -a "redirect-to-queue 3" -i 1
