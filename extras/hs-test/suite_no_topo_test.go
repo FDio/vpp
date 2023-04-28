1c6b5de7d652ff715fed8a8d5f26374ba754b005
Filip Tehlar <ftehlar@cisco.com>
2023-04-28 10:29:47 +0200

    WIP: hs-test: support for multiple workers
    Type: test
    
    Signed-off-by: Filip Tehlar <ftehlar@cisco.com>
    Change-Id: Ie90e4b02c268bc3ca40171b03829f5686fb83162

--- extras/hs-test/suite_no_topo_test.go
+++ extras/hs-test/suite_no_topo_test.go
@@ -1,5 +1,7 @@
 package main
 
+import "runtime"
+
 const (
 	singleTopoContainerVpp   = "vpp"
 	singleTopoContainerNginx = "nginx"
@@ -17,20 +19,31 @@ func (s *NoTopoSuite) SetupSuite() {
 	s.loadContainerTopology("single")
 }
 
+func generateCpuConfig() Stanza {
+	var c Stanza
+	if runtime.NumCPU() > 1 {
+		c.newStanza("cpu").
+			append("main-core 0").
+			append("corelist-workers 1").close()
+	}
+	return c
+}
+
 func (s *NoTopoSuite) SetupTest() {
 	s.skipIfUnconfiguring()
 	s.setupVolumes()
 	s.setupContainers()
 
 	// Setup test conditions
-	var startupConfig Stanza
+	var startupConfig, cpuConfig Stanza
 	startupConfig.
 		newStanza("session").
 		append("enable").
 		append("use-app-socket-api").close()
 
+	cpuConfig = generateCpuConfig()
 	container := s.getContainerByName(singleTopoContainerVpp)
-	vpp, _ := container.newVppInstance(startupConfig)
+	vpp, _ := container.newVppInstance(startupConfig, cpuConfig)
 	vpp.start()
 
 	tapInterface := s.netInterfaces[tapInterfaceName]
