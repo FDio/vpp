83f8e5cf8744467095eba23c578ea6a1f203ea9b
Vratko Polak <vrpolak@cisco.com>
2023-05-10 15:53:29 +0200

    Revert "investigating reassembly performance"
    This reverts commit 5322177af7a4b21f0db1f968577aa0dfe75b0125.
    
    Change-Id: I95b69d11453dfe08cf86bc0f6ce0302ebeb1d718
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- src/vnet/ip/ip_frag.c
+++ src/vnet/ip/ip_frag.c
@@ -134,7 +134,7 @@ ip4_frag_do_fragment (vlib_main_t * vm, u32 from_bi, u16 mtu,
     }
   else
     {
-      ip_frag_id = __atomic_fetch_add (&running_fragment_id, 1, __ATOMIC_ACQ_REL);
+      ip_frag_id = (++running_fragment_id);
       ip_frag_offset = 0;
       more = 0;
     }
@@ -413,7 +413,7 @@ ip6_frag_do_fragment (vlib_main_t * vm, u32 from_bi, u16 mtu,
     from_b->current_length - (l2unfragmentablesize + sizeof (ip6_header_t));
   u16 ptr = 0;
 
-  ip_frag_id = __atomic_fetch_add (&running_fragment_id, 1, __ATOMIC_ACQ_REL);
+  ip_frag_id = ++running_fragment_id;	// Fix
 
   /* Do the actual fragmentation */
   while (rem)
