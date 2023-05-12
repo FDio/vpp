cc977bcdcd9c8fcf1c09249fa353a497c3a7cfd1
Florin Coras <fcoras@cisco.com>
2023-05-12 16:35:14 -0700

    udp: improvements to connection format fn
    Print fib-index, next node index and opaque.
    
    Type: improvement
    
    Signed-off-by: Florin Coras <fcoras@cisco.com>
    Change-Id: Id2ff265c9acffc75f8b04fb9f26c6d571fc2ef98

--- src/vnet/udp/udp_cli.c
+++ src/vnet/udp/udp_cli.c
@@ -95,10 +95,13 @@ format_udp_vars (u8 * s, va_list * args)
 {
   udp_connection_t *uc = va_arg (*args, udp_connection_t *);
 
-  s = format (s, " index %u%U flags: %U", uc->c_c_index, format_udp_cfg_flags,
-	      uc, format_udp_connection_flags, uc);
+  s = format (s, " index %u%U flags: %U\n", uc->c_c_index,
+	      format_udp_cfg_flags, uc, format_udp_connection_flags, uc);
+  s = format (s, " fib_index: %u next_node: %u opaque: %u ", uc->c_fib_index);
   if (!(uc->flags & UDP_CONN_F_LISTEN))
-    s = format (s, " \n sw_if_index: %d, mss: %u\n", uc->sw_if_index, uc->mss);
+    s = format (s, " sw_if_index: %d mss: %u\n", uc->sw_if_index, uc->mss);
+  else
+    s = format (s, "\n");
 
   return s;
 }
