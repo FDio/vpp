d9b6ecb8fddc5c5d145c6a6252697d99c9969975
Simon Zolin <steelum@gmail.com>
2023-05-19 09:25:39 +0000

    fib: don't leave default 'dpo-drop' rule after 'sr steer'
    Steps to reproduce:
    
    1. Exec these commands on a new IP table:
    
            ip table add 77
            sr policy add bsid 2999:1:1:1:: next 2bbb:1:1:1:: encap
            sr steer l3 0.0.0.0/0 via bsid 2999:1:1:1:: fib-table 77
    
    2. Observe that default 'dpo-drop' rule isn't removed automatically:
    
            0.0.0.0/0
              unicast-ip4-chain
              [@0]: dpo-load-balance: ...
                [0] [@14]: dpo-load-balance: ...
                      [0] [@13]: SR: Segment List index:[0]
                    Segments:< 2bbb:1:1:1:: > - Weight: 1
                [1] [@0]: dpo-drop ip4
    
    Now this configuration will load-balance the traffic and drop half of the
     packets, which isn't correct.
    
    This is because:
    
    * sr_steering_policy() uses FIB_ENTRY_FLAG_LOOSE_URPF_EXEMPT flag, which gets
       passed through to fib_entry_src_api_add().
    * fib_entry_src_api_add() sets `src->fes_pl` index.
    * Then, fib_entry_src_api_path_add() calls fib_path_list_copy_and_path_add()
       which preserves default drop rule.
    
    This patch prevents fib_entry_src_api_add() from setting `fes_pl` index
     when FIB_ENTRY_FLAG_LOOSE_URPF_EXEMPT flag is used alone.
    Then, fib_entry_src_api_path_add() will call fib_path_list_create(),
     thus not preserving the default drop rule.
    
    Type: fix
    Change-Id: Ia34d3ea17aac6717c49f6d85cc54aa6b5d0581db
    Signed-off-by: Simon Zolin <steelum@gmail.com>

--- src/vnet/fib/fib_entry_src_api.c
+++ src/vnet/fib/fib_entry_src_api.c
@@ -134,7 +134,7 @@ fib_entry_src_api_add (fib_entry_src_t *src,
 		       dpo_proto_t proto,
 		       const dpo_id_t *dpo)
 {
-    if (FIB_ENTRY_FLAG_NONE != flags)
+    if (FIB_ENTRY_FLAG_LOOSE_URPF_EXEMPT != flags)
     {
 	src->fes_pl = fib_path_list_create_special(
 	                  proto,
