77dc18d8221b5a030a3fdf5873856667d398863d
Filip Tehlar <ftehlar@cisco.com>
2023-05-22 15:48:51 +0200

    hs-test: add vcl echo tests
    Type: test
    
    Signed-off-by: Filip Tehlar <ftehlar@cisco.com>
    Change-Id: I3e543cb292c7ed0f9d240f8f2473a1db07aff0fd

---
+++ extras/hs-test/raw_session_test.go
@@ -0,0 +1,37 @@
+package main
+
+func (s *VethsSuite) TestVppEchoQuic() {
+	s.skip("quic test skipping..")
+	s.testVppEcho("quic")
+}
+
+func (s *VethsSuite) TestVppEchoUdp() {
+	s.skip("udp echo currently broken in vpp, skipping..")
+	s.testVppEcho("udp")
+}
+
+func (s *VethsSuite) TestVppEchoTcp() {
+	s.testVppEcho("tcp")
+}
+
+func (s *VethsSuite) testVppEcho(proto string) {
+	serverVethAddress := s.netInterfaces["vppsrv"].ip4AddressString()
+	uri := proto + "://" + serverVethAddress + "/12344"
+
+	echoSrvContainer := s.getContainerByName("server-app")
+	serverCommand := "vpp_echo server TX=RX" +
+		" socket-name " + echoSrvContainer.getContainerWorkDir() + "/var/run/app_ns_sockets/1" +
+		" use-app-socket-api" +
+		" uri " + uri
+	s.log(serverCommand)
+	echoSrvContainer.execServer(serverCommand)
+
+	echoClnContainer := s.getContainerByName("client-app")
+
+	clientCommand := "vpp_echo client" +
+		" socket-name " + echoClnContainer.getContainerWorkDir() + "/var/run/app_ns_sockets/2" +
+		" use-app-socket-api uri " + uri
+	s.log(clientCommand)
+	o := echoClnContainer.exec(clientCommand)
+	s.log(o)
+}
