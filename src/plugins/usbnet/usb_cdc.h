0501057dee58afd75bdf1cba5860ca129c9c43c3
Damjan Marion <damarion@cisco.com>
2023-06-03 22:06:08 +0200

    usbnet: native device driver for USB network adapters
    Type: feature
    Change-Id: I889ac05d89dc1f1e9f4e11261476fa024f7edcc0
    Signed-off-by: Damjan Marion <damarion@cisco.com>

---
+++ src/plugins/usbnet/usb_cdc.h
@@ -0,0 +1,40 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2022 Cisco Systems, Inc.
+ */
+
+#ifndef _USB_CDC_H_
+#define _USB_CDC_H_
+
+#include <vppinfra/clib.h>
+#include <vppinfra/error_bootstrap.h>
+
+#define USB_CDC_DESC_SUBTYPE_HEADER 0x00
+#define USB_CDC_DESC_SUBTYPE_UNION  0x06
+
+typedef struct
+{
+  u8 bFunctionLength;
+  u8 bDescriptorType;
+  u8 bDescriptorSubtype;
+  u8 data[0];
+} __clib_packed usb_cdc_desc_hdr_t;
+STATIC_ASSERT_SIZEOF (usb_cdc_desc_hdr_t, 3);
+
+typedef struct
+{
+  usb_cdc_desc_hdr_t hdr;
+  u16 bcdCDC;
+} __clib_packed usb_cdc_heder_desc_t;
+
+STATIC_ASSERT_SIZEOF (usb_cdc_heder_desc_t, 5);
+
+typedef struct
+{
+  usb_cdc_desc_hdr_t hdr;
+  u8 bControlInterface;
+  u8 bSubordinateInterface[1];
+} __clib_packed usb_cdc_union_desc_t;
+
+STATIC_ASSERT_SIZEOF (usb_cdc_union_desc_t, 5);
+
+#endif /* _USB_CDC_H_ */
