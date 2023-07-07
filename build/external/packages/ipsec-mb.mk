d25e477a967e4a6dc03b453fc095bd0c169ad388
Ranjan Raj <ranjanx.raj@intel.com>
2023-07-07 06:09:50 +0000

    crypto-ipsecmb: bump intel-ipsec-mb version to 1.4
    Type: feature
    
    This patch update the Intel IPsec-MB lib to v1.4
    Remove v0.54 and v0.55 support, as the compatible IMB APIs
    are deprecated in v1.4
    
    Signed-off-by: Ranjan Raj <ranjanx.raj@intel.com>
    Change-Id: I01f71134c6bd17a68ec20b7bb4b0b0ff43fc644b

--- build/external/packages/ipsec-mb.mk
+++ build/external/packages/ipsec-mb.mk
@@ -11,14 +11,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ipsec-mb_version             := 1.3
+ipsec-mb_version             := 1.4
 ipsec-mb_tarball             := v$(ipsec-mb_version).tar.gz
-ipsec-mb_tarball_md5sum_0.54 := 258941f7ba90c275fcf9d19c622d2d21
-ipsec-mb_tarball_md5sum_0.55 := deca674bca7ae2282890e1fa7f953609
 ipsec-mb_tarball_md5sum_1.0  := 906e701937751e761671dc83a41cff65
 ipsec-mb_tarball_md5sum_1.1  := 3916471d3713d27e42473cb6af9c65e5
 ipsec-mb_tarball_md5sum_1.2  := f551d9c208893a436c1f5c146a615bd6
 ipsec-mb_tarball_md5sum_1.3  := d8692db9efe32a263b61f12ac0dca950
+ipsec-mb_tarball_md5sum_1.4  := fddba2611f822296ddd82d1c31d22b24
 
 ipsec-mb_tarball_md5sum      := $(ipsec-mb_tarball_md5sum_$(ipsec-mb_version))
 ipsec-mb_tarball_strip_dirs  := 1
