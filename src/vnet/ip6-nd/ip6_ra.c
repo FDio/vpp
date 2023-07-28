FDio/vpp

commit f094ce7653917b8c331a79c088ef63ea2679c40d

ip6-nd: initialize radv_info->send_radv to 1
Otherwise, the newly configured interface will never send RADV's.

See below. In the typical case, suppress = 0 and is_no = 0, which
propagates the current value of radv->send_radv:

  radv_info->send_radv =
    (suppress != 0) ? ((is_no != 0) ? 1 : 0) : radv_info->send_radv;

No other bit of code will set send_radv, at least in straightforward
ways.

Type:fix

Signed-off-by: Dave Barach <dave@barachs.net>
Change-Id: If9368155f7676460ca1f87729c2b3c453405d08d

src/vnet/ip6-nd/ip6_ra.c
@@ -1700,6 +1700,9 @@ ip6_ra_config (vlib_main_t * vm, u32 sw_if_index,
   if (!radv_info)
     return (VNET_API_ERROR_IP6_NOT_ENABLED);
 
+  /* Start off believing that we're going to send radv's */
+  radv_info->send_radv = 1;
+
   if ((max_interval != 0) && (min_interval == 0))
     min_interval = .75 * max_interval;
 

commit a589b56cd60a9766f48e3a329014030eeb07df22

ip6-nd: Revert "ip6-nd: initialize radv_info->send_radv to 1"
This reverts commit f094ce7653917b8c331a79c088ef63ea2679c40d.

 It's a wrong place to set send_radv option by default. In case
if other option should be changed - send_radv will be forced
to true.
 It's not actually hard to enable/disable send_radv by combining
suppress and is_no flags, or even cease

Type: fix
Fixes: f094ce7653917b8c331a79c088ef63ea2679c40d

Change-Id: I6615f9a8c623fb40880087d8edd5452174de2836
Signed-off-by: Vladimir Ratnikov <vratnikov@netgate.com>

src/vnet/ip6-nd/ip6_ra.c
@@ -1633,9 +1633,6 @@ ip6_ra_config (vlib_main_t * vm, u32 sw_if_index,
   if (!radv_info)
     return (VNET_API_ERROR_IP6_NOT_ENABLED);
 
-  /* Start off believing that we're going to send radv's */
-  radv_info->send_radv = 1;
-
   if ((max_interval != 0) && (min_interval == 0))
     min_interval = .75 * max_interval;
 