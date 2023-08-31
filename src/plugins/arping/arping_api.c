8af78b8e360f12ffb9e347868a6e820bee618b81
Ole Troan <otroan@employees.org>
2023-08-31 09:48:45 +0200

    arping: api to return responder mac address
    The new arping_acd call includes the responders mac address in the reply.
    Enabling a client doing address conflict detection to identify if it
    is itself that is replying or that it is another host uses the IP
    address.
    
    Type: feature
    Change-Id: Ia4bab2af1086f06ed71ba42e2e07368d4e330a27
    Signed-off-by: Ole Troan <otroan@employees.org>

--- src/plugins/arping/arping_api.c
+++ src/plugins/arping/arping_api.c
@@ -26,6 +26,7 @@
 
 #include <vlibapi/api.h>
 #include <vlibmemory/api.h>
+#include <vnet/ethernet/ethernet_types_api.h>
 
 /* define message IDs */
 #include <arping/arping.api_enum.h>
@@ -62,6 +63,36 @@ vl_api_arping_t_handler (vl_api_arping_t *mp)
 		({ rmp->reply_count = ntohl (args.reply_count); }));
 }
 
+static void
+vl_api_arping_acd_t_handler (vl_api_arping_acd_t *mp)
+{
+  vlib_main_t *vm = vlib_get_main ();
+  arping_main_t *am = &arping_main;
+  vl_api_arping_acd_reply_t *rmp;
+  arping_args_t args = { 0 };
+  int rv;
+
+  if (mp->sw_if_index != ~0)
+    VALIDATE_SW_IF_INDEX (mp);
+
+  ip_address_decode2 (&mp->address, &args.address);
+  args.interval = clib_net_to_host_f64 (mp->interval);
+  args.repeat = ntohl (mp->repeat);
+  args.is_garp = mp->is_garp;
+  args.sw_if_index = ntohl (mp->sw_if_index);
+  args.silence = 1;
+
+  arping_run_command (vm, &args);
+  rv = args.rv;
+
+  BAD_SW_IF_INDEX_LABEL;
+
+  REPLY_MACRO2 (VL_API_ARPING_ACD_REPLY, ({
+		  rmp->reply_count = ntohl (args.reply_count);
+		  mac_address_encode (&args.recv.from4.mac, rmp->mac_address);
+		}));
+}
+
 /* set tup the API message handling tables */
 #include <arping/arping.api.c>
 clib_error_t *
@@ -75,6 +106,7 @@ arping_plugin_api_hookup (vlib_main_t *vm)
 
   /* Mark API as mp safe */
   vl_api_set_msg_thread_safe (vam, am->msg_id_base + VL_API_ARPING, 1);
+  vl_api_set_msg_thread_safe (vam, am->msg_id_base + VL_API_ARPING_ACD, 1);
 
   return 0;
 }
