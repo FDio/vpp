69ac60a9217f675b8bda4cfee90b564bbe21d248
Benoît Ganne <bganne@cisco.com>
2023-09-01 16:24:55 +0200

    cnat: use static clients for snat
    Type: improvement
    
    Change-Id: I05bff5070e06e8a9942c716607973395a408e1cc
    Signed-off-by: Benoît Ganne <bganne@cisco.com>

--- src/plugins/cnat/cnat_node_vip.c
+++ src/plugins/cnat/cnat_node_vip.c
@@ -147,7 +147,7 @@ cnat_vip_feature_new_flow_inline (vlib_main_t *vm, vlib_buffer_t *b,
       clib_atomic_add_fetch (&ts->ts_session_refcnt, 1);
 
       cnat_rsession_create (rw, vnet_buffer2 (b)->session.generic_flow_id,
-			    CNAT_FIB_TABLE);
+			    CNAT_FIB_TABLE, 1 /* add client */);
     }
 
   return rw;
