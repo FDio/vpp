12dffc03ff44b20d511a0d4cb513ca82513af0ba
Benoît Ganne <bganne@cisco.com>
2023-09-13 17:21:04 +0200

    fib: log an error when destroying non-empty tables
    Destroying a non-empty fib table can lead to difficult to diagnose bugs,
    like dandling fib entries preventing entries to be inserting to the
    mtrie.
    Always log an error to help diagnose those issues.
    
    Type: improvement
    
    Change-Id: I2c771a80595035b440931b74cca4429af22161a2
    Signed-off-by: Benoît Ganne <bganne@cisco.com>

--- src/vnet/fib/ip6_fib.c
+++ src/vnet/fib/ip6_fib.c
@@ -160,10 +160,7 @@ ip6_fib_table_destroy (u32 fib_index)
     /*
      * validate no more routes.
      */
-#if CLIB_DEBUG > 0
-    if (0 != fib_table->ft_total_route_counts)
-        fib_table_assert_empty(fib_table);
-#endif
+    fib_table_assert_empty(fib_table);
 
     vec_foreach_index(source, fib_table->ft_src_route_counts)
     {
