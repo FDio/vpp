f482335c0ee2cdfd71008d16b729762a180b15b6
Damjan Marion <damarion@cisco.com>
2023-10-17 16:06:26 +0000

    dev: new device driver infra (experimental)
    Change-Id: I20c56e0d3103624407f18365c2bc1273dea5c199
    Signed-off-by: Damjan Marion <damarion@cisco.com>

---
+++ src/vnet/dev/mgmt.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2023 Cisco Systems, Inc.
+ */
+
+#ifndef _VNET_DEV_MGMT_H_
+#define _VNET_DEV_MGMT_H_
+
+#include <vppinfra/clib.h>
+
+#endif /* _VNET_DEV_MGMT_H_ */
