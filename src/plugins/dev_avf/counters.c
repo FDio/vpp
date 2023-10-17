b1e1c57017d20cdca9fa7253ba917eef6bf903e4
Damjan Marion <damarion@cisco.com>
2023-10-17 16:08:18 +0000

    avf: new driver using new dev infra (experimental)
    Type: feature
    Change-Id: I9ae0dbf28b4571a37c568b587b771f90c06f200d
    Signed-off-by: Damjan Marion <damarion@cisco.com>

---
+++ src/plugins/dev_avf/counters.c
@@ -0,0 +1,128 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2023 Cisco Systems, Inc.
+ */
+
+#include <vnet/vnet.h>
+#include <vnet/dev/dev.h>
+#include <vnet/dev/pci.h>
+#include <vnet/dev/counters.h>
+#include <dev_avf/avf.h>
+#include <dev_avf/virtchnl.h>
+#include <dev_avf/virtchnl_funcs.h>
+
+VLIB_REGISTER_LOG_CLASS (avf_log, static) = {
+  .class_name = "dev_avf",
+  .subclass_name = "counters",
+};
+
+typedef enum
+{
+  AVF_PORT_CTR_RX_BYTES,
+  AVF_PORT_CTR_TX_BYTES,
+  AVF_PORT_CTR_RX_PACKETS,
+  AVF_PORT_CTR_TX_PACKETS,
+  AVF_PORT_CTR_RX_DROPS,
+  AVF_PORT_CTR_TX_DROPS,
+  AVF_PORT_CTR_RX_UCAST,
+  AVF_PORT_CTR_TX_UCAST,
+  AVF_PORT_CTR_RX_MCAST,
+  AVF_PORT_CTR_TX_MCAST,
+  AVF_PORT_CTR_RX_BCAST,
+  AVF_PORT_CTR_TX_BCAST,
+  AVF_PORT_CTR_RX_UNKNOWN_PROTOCOL,
+  AVF_PORT_CTR_TX_ERRORS,
+} avf_port_counter_id_t;
+
+vnet_dev_counter_t avf_port_counters[] = {
+  VNET_DEV_CTR_RX_BYTES (AVF_PORT_CTR_RX_BYTES),
+  VNET_DEV_CTR_RX_PACKETS (AVF_PORT_CTR_RX_PACKETS),
+  VNET_DEV_CTR_RX_DROPS (AVF_PORT_CTR_RX_DROPS),
+  VNET_DEV_CTR_VENDOR (AVF_PORT_CTR_RX_UCAST, RX, PACKETS, "unicast"),
+  VNET_DEV_CTR_VENDOR (AVF_PORT_CTR_RX_MCAST, RX, PACKETS, "multicast"),
+  VNET_DEV_CTR_VENDOR (AVF_PORT_CTR_RX_BCAST, RX, PACKETS, "broadcast"),
+  VNET_DEV_CTR_VENDOR (AVF_PORT_CTR_RX_UNKNOWN_PROTOCOL, RX, PACKETS,
+		       "unknown protocol"),
+
+  VNET_DEV_CTR_TX_BYTES (AVF_PORT_CTR_TX_BYTES),
+  VNET_DEV_CTR_TX_PACKETS (AVF_PORT_CTR_TX_PACKETS),
+  VNET_DEV_CTR_TX_DROPS (AVF_PORT_CTR_TX_DROPS),
+  VNET_DEV_CTR_VENDOR (AVF_PORT_CTR_TX_UCAST, TX, PACKETS, "unicast"),
+  VNET_DEV_CTR_VENDOR (AVF_PORT_CTR_TX_MCAST, TX, PACKETS, "multicast"),
+  VNET_DEV_CTR_VENDOR (AVF_PORT_CTR_TX_BCAST, TX, PACKETS, "broadcast"),
+  VNET_DEV_CTR_VENDOR (AVF_PORT_CTR_TX_ERRORS, TX, PACKETS, "errors"),
+};
+
+void
+avf_port_add_counters (vlib_main_t *vm, vnet_dev_port_t *port)
+{
+  vnet_dev_port_add_counters (vm, port, avf_port_counters,
+			      ARRAY_LEN (avf_port_counters));
+}
+
+void
+avf_port_poll_stats (vlib_main_t *vm, vnet_dev_port_t *port)
+{
+  vnet_dev_rv_t rv;
+  vnet_dev_t *dev = port->dev;
+  virtchnl_eth_stats_t stats;
+  avf_port_t *ap = vnet_dev_get_port_data (port);
+  virtchnl_queue_select_t qs = { .vsi_id = ap->vsi_id };
+
+  rv = avf_vc_op_get_stats (vm, dev, &qs, &stats);
+
+  if (rv != VNET_DEV_OK)
+    return;
+
+  foreach_vnet_dev_counter (c, port->counter_main)
+    {
+      switch (c->user_data)
+	{
+	case AVF_PORT_CTR_RX_BYTES:
+	  vnet_dev_counter_value_update (vm, c, stats.rx_bytes);
+	  break;
+	case AVF_PORT_CTR_TX_BYTES:
+	  vnet_dev_counter_value_update (vm, c, stats.tx_bytes);
+	  break;
+	case AVF_PORT_CTR_RX_PACKETS:
+	  vnet_dev_counter_value_update (
+	    vm, c, stats.rx_unicast + stats.rx_broadcast + stats.rx_multicast);
+	  break;
+	case AVF_PORT_CTR_TX_PACKETS:
+	  vnet_dev_counter_value_update (
+	    vm, c, stats.tx_unicast + stats.tx_broadcast + stats.tx_multicast);
+	  break;
+	case AVF_PORT_CTR_RX_DROPS:
+	  vnet_dev_counter_value_update (vm, c, stats.rx_discards);
+	  break;
+	case AVF_PORT_CTR_TX_DROPS:
+	  vnet_dev_counter_value_update (vm, c, stats.tx_discards);
+	  break;
+	case AVF_PORT_CTR_RX_UCAST:
+	  vnet_dev_counter_value_update (vm, c, stats.rx_unicast);
+	  break;
+	case AVF_PORT_CTR_TX_UCAST:
+	  vnet_dev_counter_value_update (vm, c, stats.tx_unicast);
+	  break;
+	case AVF_PORT_CTR_RX_MCAST:
+	  vnet_dev_counter_value_update (vm, c, stats.rx_multicast);
+	  break;
+	case AVF_PORT_CTR_TX_MCAST:
+	  vnet_dev_counter_value_update (vm, c, stats.tx_multicast);
+	  break;
+	case AVF_PORT_CTR_RX_BCAST:
+	  vnet_dev_counter_value_update (vm, c, stats.rx_broadcast);
+	  break;
+	case AVF_PORT_CTR_TX_BCAST:
+	  vnet_dev_counter_value_update (vm, c, stats.tx_broadcast);
+	  break;
+	case AVF_PORT_CTR_RX_UNKNOWN_PROTOCOL:
+	  vnet_dev_counter_value_update (vm, c, stats.rx_unknown_protocol);
+	  break;
+	case AVF_PORT_CTR_TX_ERRORS:
+	  vnet_dev_counter_value_update (vm, c, stats.tx_errors);
+	  break;
+	default:
+	  ASSERT (0);
+	}
+    }
+}
---
+++ src/plugins/dev_avf/virtchnl_funcs.h
@@ -0,0 +1,241 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2023 Cisco Systems, Inc.
+ */
+
+#ifndef _AVF_VIRTCHNL_FUNCS_H_
+#define _AVF_VIRTCHNL_FUNCS_H_
+
+#include <vppinfra/clib.h>
+#include <vnet/dev/dev.h>
+#include <dev_avf/avf.h>
+
+#define VIRTCHNL_MSG_SZ(s, e, n) STRUCT_OFFSET_OF (s, e[(n) + 1])
+
+typedef struct
+{
+  virtchnl_op_t op;
+  u8 no_reply : 1;
+  u16 req_sz;
+  u16 resp_sz;
+  virtchnl_status_t status;
+  const void *req;
+  void *resp;
+} avf_virtchnl_req_t;
+
+vnet_dev_rv_t avf_virtchnl_req (vlib_main_t *, vnet_dev_t *,
+				avf_virtchnl_req_t *);
+
+static_always_inline vnet_dev_rv_t
+avf_vc_op_version (vlib_main_t *vm, vnet_dev_t *dev,
+		   const virtchnl_version_info_t *req,
+		   virtchnl_version_info_t *resp)
+{
+  avf_virtchnl_req_t vr = {
+    .op = VIRTCHNL_OP_VERSION,
+    .req = req,
+    .req_sz = sizeof (*req),
+    .resp = resp,
+    .resp_sz = sizeof (*resp),
+  };
+
+  return avf_virtchnl_req (vm, dev, &vr);
+}
+
+static_always_inline vnet_dev_rv_t
+avf_vc_op_reset_vf (vlib_main_t *vm, vnet_dev_t *dev)
+{
+  avf_virtchnl_req_t vr = {
+    .op = VIRTCHNL_OP_RESET_VF,
+    .no_reply = 1,
+  };
+
+  return avf_virtchnl_req (vm, dev, &vr);
+}
+
+static_always_inline vnet_dev_rv_t
+avf_vc_op_get_vf_resources (vlib_main_t *vm, vnet_dev_t *dev, const u32 *req,
+			    virtchnl_vf_resource_t *resp)
+{
+  avf_virtchnl_req_t vr = {
+    .op = VIRTCHNL_OP_GET_VF_RESOURCES,
+    .req = req,
+    .req_sz = sizeof (*req),
+    .resp = resp,
+    .resp_sz = sizeof (*resp),
+  };
+
+  return avf_virtchnl_req (vm, dev, &vr);
+}
+
+static_always_inline vnet_dev_rv_t
+avf_vc_op_enable_queues (vlib_main_t *vm, vnet_dev_t *dev,
+			 const virtchnl_queue_select_t *req)
+{
+  avf_virtchnl_req_t vr = {
+    .op = VIRTCHNL_OP_ENABLE_QUEUES,
+    .req = req,
+    .req_sz = sizeof (*req),
+  };
+  return avf_virtchnl_req (vm, dev, &vr);
+}
+
+static_always_inline vnet_dev_rv_t
+avf_vc_op_disable_queues (vlib_main_t *vm, vnet_dev_t *dev,
+			  const virtchnl_queue_select_t *req)
+{
+  avf_virtchnl_req_t vr = {
+    .op = VIRTCHNL_OP_DISABLE_QUEUES,
+    .req = req,
+    .req_sz = sizeof (*req),
+  };
+  return avf_virtchnl_req (vm, dev, &vr);
+}
+
+static_always_inline vnet_dev_rv_t
+avf_vc_op_config_vsi_queues (vlib_main_t *vm, vnet_dev_t *dev,
+			     const virtchnl_vsi_queue_config_info_t *req)
+{
+  avf_virtchnl_req_t vr = {
+    .op = VIRTCHNL_OP_CONFIG_VSI_QUEUES,
+    .req = req,
+    .req_sz = VIRTCHNL_MSG_SZ (virtchnl_vsi_queue_config_info_t, qpair,
+			       req->num_queue_pairs),
+  };
+
+  return avf_virtchnl_req (vm, dev, &vr);
+}
+
+static_always_inline vnet_dev_rv_t
+avf_vc_op_config_irq_map (vlib_main_t *vm, vnet_dev_t *dev,
+			  const virtchnl_irq_map_info_t *req)
+{
+  avf_virtchnl_req_t vr = {
+    .op = VIRTCHNL_OP_CONFIG_IRQ_MAP,
+    .req = req,
+    .req_sz =
+      VIRTCHNL_MSG_SZ (virtchnl_irq_map_info_t, vecmap, req->num_vectors),
+  };
+
+  return avf_virtchnl_req (vm, dev, &vr);
+}
+
+static_always_inline vnet_dev_rv_t
+avf_vc_op_config_rss_lut (vlib_main_t *vm, vnet_dev_t *dev,
+			  const virtchnl_rss_lut_t *req)
+{
+  avf_virtchnl_req_t vr = {
+    .op = VIRTCHNL_OP_CONFIG_RSS_LUT,
+    .req = req,
+    .req_sz = VIRTCHNL_MSG_SZ (virtchnl_rss_lut_t, lut, req->lut_entries),
+  };
+
+  return avf_virtchnl_req (vm, dev, &vr);
+}
+
+static_always_inline vnet_dev_rv_t
+avf_vc_op_config_rss_key (vlib_main_t *vm, vnet_dev_t *dev,
+			  const virtchnl_rss_key_t *req)
+{
+  avf_virtchnl_req_t vr = {
+    .op = VIRTCHNL_OP_CONFIG_RSS_KEY,
+    .req = req,
+    .req_sz = VIRTCHNL_MSG_SZ (virtchnl_rss_key_t, key, req->key_len),
+  };
+
+  return avf_virtchnl_req (vm, dev, &vr);
+}
+
+static_always_inline vnet_dev_rv_t
+avf_vc_op_config_promisc_mode (vlib_main_t *vm, vnet_dev_t *dev,
+			       const virtchnl_promisc_info_t *req)
+{
+  avf_virtchnl_req_t vr = {
+    .op = VIRTCHNL_OP_CONFIG_PROMISCUOUS_MODE,
+    .req = req,
+    .req_sz = sizeof (*req),
+  };
+
+  return avf_virtchnl_req (vm, dev, &vr);
+}
+
+static_always_inline vnet_dev_rv_t
+avf_vc_op_disable_vlan_stripping (vlib_main_t *vm, vnet_dev_t *dev)
+{
+  avf_virtchnl_req_t vr = {
+    .op = VIRTCHNL_OP_DISABLE_VLAN_STRIPPING,
+  };
+
+  return avf_virtchnl_req (vm, dev, &vr);
+}
+
+static_always_inline vnet_dev_rv_t
+avf_vc_op_add_eth_addr (vlib_main_t *vm, vnet_dev_t *dev,
+			const virtchnl_ether_addr_list_t *req)
+{
+  avf_virtchnl_req_t vr = {
+    .op = VIRTCHNL_OP_ADD_ETH_ADDR,
+    .req = req,
+    .req_sz =
+      VIRTCHNL_MSG_SZ (virtchnl_ether_addr_list_t, list, req->num_elements),
+  };
+
+  return avf_virtchnl_req (vm, dev, &vr);
+}
+
+static_always_inline vnet_dev_rv_t
+avf_vc_op_del_eth_addr (vlib_main_t *vm, vnet_dev_t *dev,
+			const virtchnl_ether_addr_list_t *req)
+{
+  avf_virtchnl_req_t vr = {
+    .op = VIRTCHNL_OP_DEL_ETH_ADDR,
+    .req = req,
+    .req_sz =
+      VIRTCHNL_MSG_SZ (virtchnl_ether_addr_list_t, list, req->num_elements),
+  };
+
+  return avf_virtchnl_req (vm, dev, &vr);
+}
+
+static_always_inline vnet_dev_rv_t
+avf_vc_op_get_offload_vlan_v2_caps (vlib_main_t *vm, vnet_dev_t *dev,
+				    virtchnl_vlan_caps_t *resp)
+{
+  avf_virtchnl_req_t vr = {
+    .op = VIRTCHNL_OP_GET_OFFLOAD_VLAN_V2_CAPS,
+    .resp = resp,
+    .resp_sz = sizeof (*resp),
+  };
+
+  return avf_virtchnl_req (vm, dev, &vr);
+}
+
+static_always_inline vnet_dev_rv_t
+avf_vc_op_get_stats (vlib_main_t *vm, vnet_dev_t *dev,
+		     const virtchnl_queue_select_t *req,
+		     virtchnl_eth_stats_t *resp)
+{
+  avf_virtchnl_req_t vr = {
+    .op = VIRTCHNL_OP_GET_STATS,
+    .req = req,
+    .req_sz = sizeof (*req),
+    .resp = resp,
+    .resp_sz = sizeof (*resp),
+  };
+
+  return avf_virtchnl_req (vm, dev, &vr);
+}
+
+static_always_inline vnet_dev_rv_t
+avf_vc_op_disable_vlan_stripping_v2 (vlib_main_t *vm, vnet_dev_t *dev,
+				     const virtchnl_vlan_setting_t *req)
+{
+  avf_virtchnl_req_t vr = {
+    .op = VIRTCHNL_OP_DISABLE_VLAN_STRIPPING_V2,
+    .req = req,
+    .req_sz = sizeof (*req),
+  };
+
+  return avf_virtchnl_req (vm, dev, &vr);
+}
+
+#endif /* _AVF_VIRTCHNL_FUNCS_H_ */
