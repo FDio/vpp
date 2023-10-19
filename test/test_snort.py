d303d25752fff8d2732970431d7e9aa1ef012ded
adrianvillin <avillin@cisco.com>
2023-10-19 12:15:52 +0200

    snort: Added a simple Snort plugin test to increase coverage.
    Type: improvement
    
    Change-Id: I96ec8b4347210672bc587407ab2fd0f0305ea486
    Signed-off-by: adrianvillin <avillin@cisco.com>

---
+++ test/test_snort.py
@@ -0,0 +1,51 @@
+from framework import VppTestCase, VppTestRunner
+import unittest
+from config import config
+
+
+@unittest.skipIf("snort" in config.excluded_plugins, "Exclude snort plugin test")
+class TestSnort(VppTestCase):
+    """Simple Snort plugin test"""
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestSnort, cls).setUpClass()
+        try:
+            cls.create_pg_interfaces(range(2))
+            for i in cls.pg_interfaces:
+                i.config_ip4().resolve_arp()
+                i.admin_up()
+        except:
+            cls.tearDownClass()
+            raise
+
+    @classmethod
+    def tearDownClass(cls):
+        for i in cls.pg_interfaces:
+            i.unconfig_ip4()
+            i.admin_down()
+        super(TestSnort, cls).tearDownClass()
+
+    def test_snort_cli(self):
+        # { cli command : part of the expected reply }
+        commands_replies = {"snort create-instance name snortTest queue-size 16 on-disconnect drop": "",
+                            "snort create-instance name snortTest2 queue-size 16 on-disconnect pass": "",
+                            "snort attach instance snortTest interface pg0 output": "",
+                            "snort attach instance snortTest2 interface pg1 input": "",
+                            "show snort instances": "snortTest",
+                            "show snort interfaces": "pg0",
+                            "show snort clients": "number of clients",
+                            "show snort mode": "input mode: interrupt",
+                            "snort mode polling": "",
+                            "snort mode interrupt": "",
+                            "snort detach interface pg0": "",
+                            "snort detach interface pg1": ""
+                            }
+
+        for command, reply in commands_replies.items():
+            actual_reply = self.vapi.cli(command)
+            self.assertIn(reply, actual_reply)
+
+
+if __name__ == "__main__":
+    unittest.main(testRunner=VppTestRunner)
