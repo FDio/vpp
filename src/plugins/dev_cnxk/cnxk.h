c6de494d48dee14faf01924a344475d4b0d7c620
Damjan Marion <damarion@cisco.com>
2023-10-23 18:36:18 +0200

    cnxk: native driver for Octeon CPUs from Marvell
    Change-Id: I6898625c4e8854f777407dac3159e4c639a54860
    Type: improvement
    Signed-off-by: Damjan Marion <damarion@cisco.com>

---
+++ src/plugins/dev_cnxk/cnxk.h
@@ -0,0 +1,168 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2023 Cisco Systems, Inc.
+ */
+
+#ifndef _CNXK_H_
+#define _CNXK_H_
+
+#include <vppinfra/clib.h>
+#include <vppinfra/error_bootstrap.h>
+#include <vppinfra/format.h>
+#include <vnet/vnet.h>
+#include <vnet/dev/dev.h>
+#include <roc/base/roc_api.h>
+
+typedef enum
+{
+  CNXK_DEVICE_TYPE_UNKNOWN = 0,
+  CNXK_DEVICE_TYPE_RVU_PF,
+  CNXK_DEVICE_TYPE_CPT_VF,
+} __clib_packed cnxk_device_type_t;
+
+typedef union
+{
+  union nix_rx_parse_u f;
+  u64 w[7];
+} cnxk_nix_rx_parse_t;
+
+STATIC_ASSERT_SIZEOF (cnxk_nix_rx_parse_t, 56);
+
+typedef struct
+{
+  CLIB_ALIGN_MARK (desc, 128);
+  struct nix_cqe_hdr_s hdr;
+  cnxk_nix_rx_parse_t parse;
+  struct nix_rx_sg_s sg0;
+  void *segs0[3];
+  struct nix_rx_sg_s sg1;
+  void *segs1[3];
+} cnxk_nix_rx_cqe_desc_t;
+
+STATIC_ASSERT_SIZEOF (cnxk_nix_rx_cqe_desc_t, 128);
+
+typedef struct
+{
+  cnxk_device_type_t type;
+  u8 nix_initialized : 1;
+  u8 status : 1;
+  u8 full_duplex : 1;
+  u32 speed;
+  struct plt_pci_device plt_pci_dev;
+  struct roc_cpt cpt;
+  struct roc_nix *nix;
+} cnxk_device_t;
+
+typedef struct
+{
+  u8 lf_allocated : 1;
+  u8 tm_initialized : 1;
+  u8 npc_initialized : 1;
+  struct roc_npc npc;
+} cnxk_port_t;
+
+typedef struct
+{
+  u8 npa_pool_initialized : 1;
+  u8 cq_initialized : 1;
+  u8 rq_initialized : 1;
+  u16 hdr_off;
+  u32 n_enq;
+  u64 aura_handle;
+  u64 lmt_base_addr;
+  CLIB_CACHE_LINE_ALIGN_MARK (data0);
+  struct roc_nix_cq cq;
+  struct roc_nix_rq rq;
+} cnxk_rxq_t;
+
+typedef struct
+{
+  CLIB_ALIGN_MARK (cl, 128);
+  union
+  {
+    struct npa_batch_alloc_status_s status;
+    u64 iova[16];
+  };
+} cnxk_npa_batch_alloc_cl128_t;
+
+STATIC_ASSERT_SIZEOF (cnxk_npa_batch_alloc_cl128_t, 128);
+
+typedef struct
+{
+  u8 sq_initialized : 1;
+  u8 npa_pool_initialized : 1;
+  u16 hdr_off;
+  u32 n_enq;
+  u64 aura_handle;
+  u64 io_addr;
+  void *lmt_addr;
+
+  cnxk_npa_batch_alloc_cl128_t *ba_buffer;
+  u8 ba_first_cl;
+  u8 ba_num_cl;
+  CLIB_CACHE_LINE_ALIGN_MARK (data0);
+  struct roc_nix_sq sq;
+} cnxk_txq_t;
+
+/* format.c */
+format_function_t format_cnxk_port_status;
+format_function_t format_cnxk_rx_trace;
+
+/* port.c */
+vnet_dev_rv_t cnxk_port_init (vlib_main_t *, vnet_dev_port_t *);
+vnet_dev_rv_t cnxk_port_start (vlib_main_t *, vnet_dev_port_t *);
+void cnxk_port_stop (vlib_main_t *, vnet_dev_port_t *);
+void cnxk_port_deinit (vlib_main_t *, vnet_dev_port_t *);
+vnet_dev_rv_t cnxk_port_cfg_change (vlib_main_t *, vnet_dev_port_t *,
+				    vnet_dev_port_cfg_change_req_t *);
+
+/* queue.c */
+vnet_dev_rv_t cnxk_rx_queue_alloc (vlib_main_t *, vnet_dev_rx_queue_t *);
+vnet_dev_rv_t cnxk_tx_queue_alloc (vlib_main_t *, vnet_dev_tx_queue_t *);
+void cnxk_rx_queue_free (vlib_main_t *, vnet_dev_rx_queue_t *);
+void cnxk_tx_queue_free (vlib_main_t *, vnet_dev_tx_queue_t *);
+vnet_dev_rv_t cnxk_rxq_init (vlib_main_t *, vnet_dev_rx_queue_t *);
+vnet_dev_rv_t cnxk_txq_init (vlib_main_t *, vnet_dev_tx_queue_t *);
+void cnxk_rxq_deinit (vlib_main_t *, vnet_dev_rx_queue_t *);
+void cnxk_txq_deinit (vlib_main_t *, vnet_dev_tx_queue_t *);
+format_function_t format_cnxk_rxq_info;
+format_function_t format_cnxk_txq_info;
+
+#define log_debug(dev, f, ...)                                                \
+  vlib_log (VLIB_LOG_LEVEL_DEBUG, cnxk_log.class, "%U: " f,                   \
+	    format_vnet_dev_addr, (dev), ##__VA_ARGS__)
+#define log_info(dev, f, ...)                                                 \
+  vlib_log (VLIB_LOG_LEVEL_INFO, cnxk_log.class, "%U: " f,                    \
+	    format_vnet_dev_addr, (dev), ##__VA_ARGS__)
+#define log_notice(dev, f, ...)                                               \
+  vlib_log (VLIB_LOG_LEVEL_NOTICE, cnxk_log.class, "%U: " f,                  \
+	    format_vnet_dev_addr, (dev), ##__VA_ARGS__)
+#define log_warn(dev, f, ...)                                                 \
+  vlib_log (VLIB_LOG_LEVEL_WARNING, cnxk_log.class, "%U: " f,                 \
+	    format_vnet_dev_addr, (dev), ##__VA_ARGS__)
+#define log_err(dev, f, ...)                                                  \
+  vlib_log (VLIB_LOG_LEVEL_ERR, cnxk_log.class, "%U: " f,                     \
+	    format_vnet_dev_addr, (dev), ##__VA_ARGS__)
+
+#define foreach_cnxk_tx_node_counter                                          \
+  _ (CHAIN_TOO_LONG, chain_too_logn, ERROR, "drop due to buffer chain > 3")   \
+  _ (NO_FREE_SLOTS, no_free_slots, ERROR, "no free tx slots")                 \
+  _ (AURA_BATCH_ALLOC_ISSUE_FAIL, aura_batch_alloc_issue_fail, ERROR,         \
+     "aura batch alloc issue failed")                                         \
+  _ (AURA_BATCH_ALLOC_NOT_READY, aura_batch_alloc_not_ready, ERROR,           \
+     "aura batch alloc not ready")
+
+typedef enum
+{
+#define _(f, n, s, d) CNXK_TX_NODE_CTR_##f,
+  foreach_cnxk_tx_node_counter
+#undef _
+} cnxk_tx_node_counter_t;
+
+typedef struct
+{
+  u32 sw_if_index;
+  u32 next_index;
+  cnxk_nix_rx_cqe_desc_t desc;
+} cnxk_rx_trace_t;
+
+#endif /* _CNXK_H_ */
---
+++ src/plugins/dev_cnxk/rx_node.c
@@ -0,0 +1,348 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2023 Cisco Systems, Inc.
+ */
+
+#include <vlib/vlib.h>
+#include <vnet/dev/dev.h>
+#include <vnet/ethernet/ethernet.h>
+#include <dev_cnxk/cnxk.h>
+
+#define BATCH_SZ 32
+
+typedef union
+{
+  struct
+  {
+    u64 tail : 20;
+    u64 head : 20;
+    u64 resv40 : 6;
+    u64 cq_err : 1;
+    u64 resv47 : 16;
+    u64 op_err : 1;
+  };
+  u64 as_u64;
+} cnxk_nix_lf_cq_op_status_t;
+
+STATIC_ASSERT_SIZEOF (cnxk_nix_lf_cq_op_status_t, 8);
+
+typedef struct
+{
+  u32 next_index;
+  u32 sw_if_index;
+  u32 hw_if_index;
+  u32 trace_count;
+  u32 n_traced;
+  cnxk_nix_rx_cqe_desc_t *next_desc;
+  u64 parse_w0_or;
+  u32 n_left_to_next;
+  u32 *to_next;
+  u32 n_rx_pkts;
+  u32 n_rx_bytes;
+  u32 n_segs;
+  u64 aura;
+  u64 reg;
+  u64 aura_op_free0_addr;
+} cnxk_rx_node_ctx_t;
+
+static_always_inline u32
+cnxk_rx_batch (vlib_main_t *vm, cnxk_rx_node_ctx_t *ctx,
+	       vnet_dev_rx_queue_t *rxq, u32 n)
+{
+  cnxk_rxq_t *crq = vnet_dev_get_rx_queue_data (rxq);
+  vlib_buffer_template_t bt = rxq->buffer_template;
+  u32 n_left;
+  cnxk_nix_rx_cqe_desc_t *d = ctx->next_desc;
+  vlib_buffer_t *b[4];
+
+  for (n_left = n; n_left >= 8; d += 4, n_left -= 4, ctx->to_next += 4)
+    {
+      clib_prefetch_store ((vlib_buffer_t *) d[4].segs0[0] - 1);
+      clib_prefetch_store ((vlib_buffer_t *) d[5].segs0[0] - 1);
+      b[0] = (vlib_buffer_t *) d[0].segs0[0] - 1;
+      clib_prefetch_store ((vlib_buffer_t *) d[6].segs0[0] - 1);
+      b[1] = (vlib_buffer_t *) d[1].segs0[0] - 1;
+      clib_prefetch_store ((vlib_buffer_t *) d[7].segs0[0] - 1);
+      b[2] = (vlib_buffer_t *) d[2].segs0[0] - 1;
+      b[3] = (vlib_buffer_t *) d[3].segs0[0] - 1;
+      ctx->to_next[0] = vlib_get_buffer_index (vm, b[0]);
+      ctx->to_next[1] = vlib_get_buffer_index (vm, b[1]);
+      ctx->to_next[2] = vlib_get_buffer_index (vm, b[2]);
+      ctx->to_next[3] = vlib_get_buffer_index (vm, b[3]);
+      b[0]->template = bt;
+      b[1]->template = bt;
+      b[2]->template = bt;
+      b[3]->template = bt;
+      ctx->n_rx_bytes += b[0]->current_length = d[0].sg0.seg1_size;
+      ctx->n_rx_bytes += b[1]->current_length = d[1].sg0.seg1_size;
+      ctx->n_rx_bytes += b[2]->current_length = d[2].sg0.seg1_size;
+      ctx->n_rx_bytes += b[3]->current_length = d[3].sg0.seg1_size;
+      ctx->n_segs += d[0].sg0.segs;
+      ctx->n_segs += d[1].sg0.segs;
+      ctx->n_segs += d[2].sg0.segs;
+      ctx->n_segs += d[3].sg0.segs;
+    }
+
+  for (; n_left; d += 1, n_left -= 1, ctx->to_next += 1)
+    {
+      b[0] = (vlib_buffer_t *) d->segs0[0] - 1;
+      ctx->to_next[0] = vlib_get_buffer_index (vm, b[0]);
+      b[0]->template = bt;
+      ctx->n_rx_bytes += b[0]->current_length = d[0].sg0.seg1_size;
+      ctx->n_segs += d[0].sg0.segs;
+    }
+
+  plt_write64 ((crq->cq.wdata | n), crq->cq.door);
+  ctx->n_rx_pkts += n;
+  ctx->n_left_to_next -= n;
+  return n;
+}
+
+typedef union
+{
+  struct
+  {
+    u64 aura : 20;
+    u64 _reseved20 : 12;
+    u64 count_eot : 1;
+    u64 _reserved33 : 30;
+    u64 fabs : 1;
+  };
+  u64 as_u64;
+} cnxk_npa_lf_aura_batch_free0_t;
+
+STATIC_ASSERT_SIZEOF (cnxk_npa_lf_aura_batch_free0_t, 8);
+
+typedef struct
+{
+  cnxk_npa_lf_aura_batch_free0_t w0;
+  u64 data[15];
+} cnxk_npa_lf_aura_batch_free_line_t;
+
+STATIC_ASSERT_SIZEOF (cnxk_npa_lf_aura_batch_free_line_t, 128);
+
+static_always_inline void
+cnxk_rxq_refill_batch (vlib_main_t *vm, u64 lmt_id, u64 addr,
+		       cnxk_npa_lf_aura_batch_free_line_t *lines, u32 *bi,
+		       cnxk_npa_lf_aura_batch_free0_t w0, u64 n_lines)
+{
+  u64 data;
+
+  for (u32 i = 0; i < n_lines; i++, bi += 15)
+    {
+      lines[i].w0 = w0;
+      vlib_get_buffers (vm, bi, (vlib_buffer_t **) lines[i].data, 15);
+    }
+
+  data = lmt_id | ((n_lines - 1) << 12) | ((1ULL << (n_lines * 3)) - 1) << 19;
+  roc_lmt_submit_steorl (data, addr);
+  cnxk_wmb ();
+}
+
+static_always_inline u32
+cnxk_rxq_refill (vlib_main_t *vm, vnet_dev_rx_queue_t *rxq, u16 n_refill)
+{
+  const u32 batch_max_lines = 16;
+  const u32 bufs_per_line = 15;
+  const u32 batch_max_bufs = 15 * 16;
+
+  u32 batch_bufs;
+  u32 buffer_indices[batch_max_bufs];
+  u64 lmt_addr, lmt_id, ah, addr, n_enq = 0;
+  u8 bpi = 0;
+  cnxk_rxq_t *crq = vnet_dev_get_rx_queue_data (rxq);
+  u32 n_lines, n_alloc;
+
+  if (n_refill < bufs_per_line)
+    return 0;
+
+  n_lines = n_refill / bufs_per_line;
+
+  ah = crq->aura_handle;
+  addr = roc_npa_aura_handle_to_base (ah) + NPA_LF_AURA_BATCH_FREE0;
+  addr |= 0x7 << 4;
+
+  lmt_addr = crq->lmt_base_addr;
+  lmt_id = vm->thread_index << ROC_LMT_LINES_PER_CORE_LOG2;
+  lmt_addr += lmt_id << ROC_LMT_LINE_SIZE_LOG2;
+
+  cnxk_npa_lf_aura_batch_free_line_t *lines =
+    (cnxk_npa_lf_aura_batch_free_line_t *) lmt_addr;
+
+  cnxk_npa_lf_aura_batch_free0_t w0 = {
+    .aura = roc_npa_aura_handle_to_aura (ah),
+    .count_eot = 1,
+  };
+
+  while (n_lines >= batch_max_lines)
+    {
+      n_alloc = vlib_buffer_alloc (vm, buffer_indices, batch_max_bufs);
+      if (PREDICT_FALSE (n_alloc < batch_max_bufs))
+	goto alloc_fail;
+      cnxk_rxq_refill_batch (vm, lmt_id, addr, lines, buffer_indices, w0,
+			     batch_max_lines);
+      n_lines -= batch_max_lines;
+      n_enq += batch_max_bufs;
+    }
+
+  if (n_lines == 0)
+    return n_enq;
+
+  batch_bufs = n_lines * bufs_per_line;
+  n_alloc = vlib_buffer_alloc_from_pool (vm, buffer_indices, batch_bufs, bpi);
+
+  if (PREDICT_FALSE (n_alloc < batch_bufs))
+    {
+    alloc_fail:
+      if (n_alloc >= bufs_per_line)
+	{
+	  u32 n_unalloc;
+	  n_lines = n_alloc / bufs_per_line;
+	  batch_bufs = n_lines * bufs_per_line;
+	  n_unalloc = n_alloc - batch_bufs;
+
+	  if (n_unalloc)
+	    vlib_buffer_unalloc_to_pool (vm, buffer_indices + batch_bufs,
+					 n_unalloc, bpi);
+	}
+      else
+	{
+	  if (n_alloc)
+	    vlib_buffer_unalloc_to_pool (vm, buffer_indices, n_alloc, bpi);
+	  return n_enq;
+	}
+    }
+
+  cnxk_rxq_refill_batch (vm, lmt_id, addr, lines, buffer_indices, w0, n_lines);
+  n_enq += batch_bufs;
+
+  return n_enq;
+}
+
+static_always_inline void
+cnxk_rx_trace (vlib_main_t *vm, vlib_node_runtime_t *node,
+	       cnxk_rx_node_ctx_t *ctx, cnxk_nix_rx_cqe_desc_t *d, u32 n_desc)
+{
+  u32 i = 0;
+  if (PREDICT_TRUE (ctx->trace_count == 0))
+    return;
+
+  while (ctx->n_traced < ctx->trace_count && i < n_desc)
+    {
+      vlib_buffer_t *b = (vlib_buffer_t *) d[i].segs0[0] - 1;
+
+      if (PREDICT_TRUE (vlib_trace_buffer (vm, node, ctx->next_index, b,
+					   /* follow_chain */ 0)))
+	{
+	  cnxk_rx_trace_t *tr = vlib_add_trace (vm, node, b, sizeof (*tr));
+	  tr->next_index = ctx->next_index;
+	  tr->sw_if_index = ctx->sw_if_index;
+	  tr->desc = d[i];
+	  ctx->n_traced++;
+	}
+      i++;
+    }
+}
+
+static_always_inline uword
+cnxk_rx_node_inline (vlib_main_t *vm, vlib_node_runtime_t *node,
+		     vlib_frame_t *frame, vnet_dev_port_t *port,
+		     vnet_dev_rx_queue_t *rxq, int with_flows)
+{
+  vnet_main_t *vnm = vnet_get_main ();
+  u32 thr_idx = vlib_get_thread_index ();
+  cnxk_rxq_t *crq = vnet_dev_get_rx_queue_data (rxq);
+  u32 n_desc, head, n, n_enq;
+  u32 cq_size = crq->cq.nb_desc;
+  u32 cq_mask = crq->cq.qmask;
+  cnxk_nix_rx_cqe_desc_t *descs = crq->cq.desc_base;
+  cnxk_nix_lf_cq_op_status_t status;
+  cnxk_rx_node_ctx_t _ctx = {
+    .next_index = rxq->next_index,
+    .sw_if_index = port->intf.sw_if_index,
+    .hw_if_index = port->intf.hw_if_index,
+  }, *ctx = &_ctx;
+
+  /* get head and tail from NIX_LF_CQ_OP_STATUS */
+  status.as_u64 = roc_atomic64_add_sync (crq->cq.wdata, crq->cq.status);
+  if (status.cq_err || status.cq_err)
+    return 0;
+
+  head = status.head;
+  n_desc = (status.tail - head) & cq_mask;
+
+  if (n_desc == 0)
+    return 0;
+
+  vlib_get_new_next_frame (vm, node, ctx->next_index, ctx->to_next,
+			   ctx->n_left_to_next);
+
+  ctx->trace_count = vlib_get_trace_count (vm, node);
+
+  while (1)
+    {
+      ctx->next_desc = descs + head;
+      n = clib_min (cq_size - head, clib_min (n_desc, ctx->n_left_to_next));
+      n = cnxk_rx_batch (vm, ctx, rxq, n);
+      cnxk_rx_trace (vm, node, ctx, descs + head, n);
+
+      if (ctx->n_left_to_next == 0)
+	break;
+
+      status.as_u64 = roc_atomic64_add_sync (crq->cq.wdata, crq->cq.status);
+      if (status.cq_err || status.cq_err)
+	break;
+
+      head = status.head;
+      n_desc = (status.tail - head) & cq_mask;
+      if (n_desc == 0)
+	break;
+    }
+
+  if (ctx->n_traced)
+    vlib_set_trace_count (vm, node, ctx->trace_count - ctx->n_traced);
+
+  if (PREDICT_TRUE (ctx->next_index == VNET_DEV_ETH_RX_PORT_NEXT_ETH_INPUT))
+    {
+      vlib_next_frame_t *nf;
+      vlib_frame_t *f;
+      ethernet_input_frame_t *ef;
+      cnxk_nix_rx_parse_t p = { .w[0] = ctx->parse_w0_or };
+      nf = vlib_node_runtime_get_next_frame (vm, node, ctx->next_index);
+      f = vlib_get_frame (vm, nf->frame);
+      f->flags = ETH_INPUT_FRAME_F_SINGLE_SW_IF_IDX;
+
+      ef = vlib_frame_scalar_args (f);
+      ef->sw_if_index = ctx->sw_if_index;
+      ef->hw_if_index = ctx->hw_if_index;
+
+      if (p.f.errcode == 0 && p.f.errlev == 0)
+	f->flags |= ETH_INPUT_FRAME_F_IP4_CKSUM_OK;
+
+      vlib_frame_no_append (f);
+    }
+
+  vlib_put_next_frame (vm, node, ctx->next_index, ctx->n_left_to_next);
+
+  n_enq = crq->n_enq - ctx->n_segs;
+  n_enq += cnxk_rxq_refill (vm, rxq, rxq->size - n_enq);
+  crq->n_enq = n_enq;
+
+  vlib_increment_combined_counter (
+    vnm->interface_main.combined_sw_if_counters + VNET_INTERFACE_COUNTER_RX,
+    thr_idx, ctx->hw_if_index, ctx->n_rx_pkts, ctx->n_rx_bytes);
+
+  return ctx->n_rx_pkts;
+}
+
+VNET_DEV_NODE_FN (cnxk_rx_node)
+(vlib_main_t *vm, vlib_node_runtime_t *node, vlib_frame_t *frame)
+{
+  u32 n_rx = 0;
+  foreach_vnet_dev_rx_queue_runtime (rxq, node)
+    {
+      vnet_dev_port_t *port = rxq->port;
+      n_rx += cnxk_rx_node_inline (vm, node, frame, port, rxq, 0);
+    }
+
+  return n_rx;
+}
