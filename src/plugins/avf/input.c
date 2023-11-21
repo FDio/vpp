FDio/vpp

commit 8b4d474abd62c623502ad9a4a279a9b4535ca0c1

avf: avoid eating our own tail
This happens in case we are not able to refill ring with new buffers.

Type: fix
Change-Id: I727e55c5b8482d9b8aeb4a9933a9a755fe21e55f
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/plugins/avf/input.c
@@ -428,6 +428,9 @@ no_more_desc:
   rxq->next = next;
   rxq->n_enqueued -= n_rx_packets + n_tail_desc;
 
+  /* avoid eating our own tail */
+  rxq->descs[(next + rxq->n_enqueued) & mask].qword[1] = 0;
+
 #if defined(CLIB_HAVE_VEC256) || defined(CLIB_HAVE_VEC128)
   or_qw1 |= or_q1x4[0] | or_q1x4[1] | or_q1x4[2] | or_q1x4[3];
 #endif

commit 25dd5a3b6b80cb9ba9432100d2c919bb05fb56ad

avf: put sentinel at correct place
The previous fix was adding the sentinel before refilling rx,
which gave the NIC time to overwrite it with a new descriptor.

Ticket: VPP-2087
Type: fix
Fixes: 8b4d474abd62c623502ad9a4a279a9b4535ca0c1

Change-Id: I32bde4a763a62fb66c5c3871d9f10af6066e2d47
Signed-off-by: Vratko Polak <vrpolak@cisco.com>

src/plugins/avf/input.c
@@ -126,6 +126,9 @@ avf_rxq_refill (vlib_main_t * vm, vlib_node_runtime_t * node, avf_rxq_t * rxq,
     }
 
   avf_tail_write (rxq->qrx_tail, slot);
+
+  /* RXQ can be smaller than 256 packets, especially if jumbo. */
+  rxq->descs[slot].qword[1] = 0;
 }
 
 
@@ -422,9 +425,6 @@ no_more_desc:
 
   rxq->next = next;
   rxq->n_enqueued -= n_rx_packets + n_tail_desc;
-
-  /* avoid eating our own tail */
-  rxq->descs[(next + rxq->n_enqueued) & mask].qword[1] = 0;
 
 #if defined(CLIB_HAVE_VEC256) || defined(CLIB_HAVE_VEC128)
   or_qw1 |= or_q1x4[0] | or_q1x4[1] | or_q1x4[2] | or_q1x4[3];