eef605f14eb00c3853591023c19d6d6bee50d455
Dmitry Valter <d-valter@yandex-team.com>
2024-01-05 15:17:29 +0000

    vppinfra: fix clib_array_mask_u32 OOB reads
    Partially revert f62ed3f9c1ec3e8db36f63d6a54f46b7bea43723
    Respect n_elts to avoid crashes with ASAN.
    
    Type: fix
    Fixes: f62ed3f9c1ec3e8db36f63d6a54f46b7bea43723
    Signed-off-by: Dmitry Valter <d-valter@yandex-team.com>
    Change-Id: Ic1708a3f33fe71ca752345b5c77b6ae7a2d42bcd

--- src/vppinfra/vector/array_mask.h
+++ src/vppinfra/vector/array_mask.h
@@ -17,56 +17,44 @@
 static_always_inline void
 clib_array_mask_u32 (u32 *src, u32 mask, u32 n_elts)
 {
+  u32 i;
 #if defined(CLIB_HAVE_VEC512)
   u32x16 mask16 = u32x16_splat (mask);
-  if (n_elts <= 16)
+
+  for (i = 0; i + 16 <= n_elts; i += 16)
+    *((u32x16u *) (src + i)) &= mask16;
+  n_elts -= i;
+  if (n_elts)
     {
       u32 m = pow2_mask (n_elts);
       u32x16 r = u32x16_mask_load_zero (src, m);
       u32x16_mask_store (r & mask16, src, m);
-      return;
     }
-  for (int i = 0; i < n_elts; i += 16)
-    *((u32x16u *) (src + i)) &= mask16;
-  *((u32x16u *) (src + n_elts - 16)) &= mask16;
+  return;
 #elif defined(CLIB_HAVE_VEC256)
   u32x8 mask8 = u32x8_splat (mask);
+
+  for (i = 0; i + 8 <= n_elts; i += 8)
+    *((u32x8u *) (src + i)) &= mask8;
+  n_elts -= i;
+  src += i;
 #if defined(CLIB_HAVE_VEC256_MASK_LOAD_STORE)
-  if (n_elts <= 8)
+  if (n_elts)
     {
       u32 m = pow2_mask (n_elts);
       u32x8 r = u32x8_mask_load_zero (src, m);
       u32x8_mask_store (r & mask8, src, m);
-      return;
     }
-#else
-  if (PREDICT_FALSE (n_elts < 4))
-    {
-      if (n_elts & 2)
-	{
-	  src[0] &= mask;
-	  src[1] &= mask;
-	  src += 2;
-	}
-      if (n_elts & 1)
-	src[0] &= mask;
-      return;
-    }
-  if (n_elts <= 8)
-    {
-      u32x4 mask4 = u32x4_splat (mask);
-      *(u32x4u *) src &= mask4;
-      *(u32x4u *) (src + n_elts - 4) &= mask4;
-    }
+  return;
 #endif
-
-  for (int i = 0; i < n_elts; i += 8)
-    *((u32x8u *) (src + i)) &= mask8;
-  *((u32x8u *) (src + n_elts - 8)) &= mask8;
 #elif defined(CLIB_HAVE_VEC128)
   u32x4 mask4 = u32x4_splat (mask);
 
-  if (PREDICT_FALSE (n_elts < 4))
+  for (i = 0; i + 4 <= n_elts; i += 4)
+    *((u32x4u *) (src + i)) &= mask4;
+  n_elts -= i;
+  src += i;
+  if (n_elts)
     {
       if (n_elts & 2)
 	{
@@ -78,19 +66,14 @@ clib_array_mask_u32 (u32 *src, u32 mask, u32 n_elts)
 	src[0] &= mask;
       return;
     }
-
-  for (int i = 0; i < n_elts; i += 4)
-    *((u32x4u *) (src + i)) &= mask4;
-  *((u32x4u *) (src + n_elts - 4)) &= mask4;
   return;
-#else
+#endif
   while (n_elts > 0)
     {
       src[0] &= mask;
       src++;
       n_elts--;
     }
-#endif
 }
 
 static_always_inline void
