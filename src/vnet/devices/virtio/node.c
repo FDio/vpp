61b4efc4dae3f60265ed2b6d8bdcad84b1c4e1cb
Tom Jones <thj@freebsd.org>
2024-01-30 09:06:47 +0000

    vnet: Provide platform specific if_tap headers
    Type: improvement
    Change-Id: Ia5cec0afc7f929491e495bb337493e64f752d75f
    Signed-off-by: Tom Jones <thj@freebsd.org>

--- src/vnet/devices/virtio/node.c
+++ src/vnet/devices/virtio/node.c
@@ -19,7 +19,11 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <net/if.h>
+#ifdef __linux__
 #include <linux/if_tun.h>
+#elif __FreeBSD__
+#include <net/if_tun.h>
+#endif /* __linux */
 #include <sys/ioctl.h>
 #include <sys/eventfd.h>
 
