fbf5f2b0300c654be5d1962bc207c87f49930faf
adrianvillin <avillin@cisco.com>
2024-02-13 03:26:25 -0500

    hs-test: shortened interface names to avoid character limit
    Type: test
    
    Change-Id: I09df597ccb8a3c4af47b8a36010afb81df533236
    Signed-off-by: adrianvillin <avillin@cisco.com>

--- extras/hs-test/echo_test.go
+++ extras/hs-test/echo_test.go
@@ -2,7 +2,7 @@ package main
 
 func (s *VethsSuite) TestEchoBuiltin() {
 	serverVpp := s.getContainerByName("server-vpp").vppInstance
-	serverVeth := s.netInterfaces["vppsrv"]
+	serverVeth := s.netInterfaces[serverInterfaceName]
 
 	serverVpp.vppctl("test echo server " +
 		" uri tcp://" + serverVeth.ip4AddressString() + "/1234")
@@ -33,7 +33,7 @@ func (s *VethsSuite) TestTcpWithLoss() {
 	clientVpp.vppctl("set nsim poll-main-thread delay 0.01 ms bandwidth 40 gbit" +
 		" packet-size 1400 packets-per-drop 1000")
 
-	clientVpp.vppctl("nsim output-feature enable-disable host-vppcln")
+	clientVpp.vppctl("nsim output-feature enable-disable " + s.netInterfaces[clientInterfaceName].name)
 
 	// Do echo test from client-vpp container
 	output := clientVpp.vppctl("test echo client uri tcp://%s/20022 verbose echo-bytes mbytes 50",
