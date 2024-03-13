33b0673ebc8303ee85d53bc729d8a4ee3501ef3a
Andrew Yourtchenko <ayourtch@gmail.com>
2024-03-13 12:16:11 +0100

    misc: fix the static vppctl build
    Change I58e1ae1c91f4a62e80eaf4e16e9932d8bab17c74 has introduced a reference to config.h,
    which is not there in a case of building a static standalone vppctl.
    
    Solution: create an empty file and add the path to it to include path,
    and clean up after the build.
    
    Type: fix
    Change-Id: I133235ba07e5c2e0d5669be9c2292cab0fdf436f
    Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>

--- extras/scripts/build_static_vppctl.sh
+++ extras/scripts/build_static_vppctl.sh
@@ -1,6 +1,17 @@
 #/bin/env bash
+set -eu
 
 src=$(realpath $(dirname $0)/../..)/src
+
+
+# Change-id I58e1ae1c91f4a62e80eaf4e16e9932d8bab17c74 introduced the usage of config.h
+# which we do not have here. Make an empty one.
+TMP_DIR_ROOT="/tmp/static_vppctl_build"
+TMP_DIR="${TMP_DIR_ROOT}/vpp/vnet/"
+EMPTY_CFG="${TMP_DIR}/config.h"
+
+mkdir -p "${TMP_DIR}"
+touch "${EMPTY_CFG}"
 
 ${CC:-cc} \
  -Wall \
@@ -9,7 +20,10 @@ ${CC:-cc} \
  -flto \
  -static \
  -I ${src} \
+ -I "${TMP_DIR_ROOT}" \
  -g \
  ${src}/vpp/app/vppctl.c \
  -o vppctl
+
+rm -rf "${TMP_DIR_ROOT}"
 
