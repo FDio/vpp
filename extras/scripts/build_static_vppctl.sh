3dff783dea1009890433a8d0f8e3135889562047
Andrew Yourtchenko <ayourtch@gmail.com>
2024-03-13 12:16:11 +0100

    misc: fix the static vppctl build
    Change I58e1ae1c91f4a62e80eaf4e16e9932d8bab17c74 has introduced a reference to config.h,
    which is not there in a case of building a static standalone vppctl.
    
    Solution: add a variable STATIC_VPPCTL which, when defined, avoids including
    the missing include file. Thanks a lot to Damjan for the suggestion.
    
    Type: fix
    Change-Id: I133235ba07e5c2e0d5669be9c2292cab0fdf436f
    Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>

--- extras/scripts/build_static_vppctl.sh
+++ extras/scripts/build_static_vppctl.sh
@@ -1,6 +1,10 @@
 #/bin/env bash
+set -eu
 
 src=$(realpath $(dirname $0)/../..)/src
+
+mkdir -p "${TMP_DIR}"
+touch "${EMPTY_CFG}"
 
 ${CC:-cc} \
  -Wall \
@@ -8,7 +12,9 @@ ${CC:-cc} \
  -O2 \
  -flto \
  -static \
+ -D STATIC_VPPCTL \
  -I ${src} \
+ -I "${TMP_DIR_ROOT}" \
  -g \
  ${src}/vpp/app/vppctl.c \
  -o vppctl
