e88fa2eca3b64b555155c91b11371aa10bc16c1b
Tom Jones <thj@freebsd.org>
2024-03-14 12:24:04 +0000

    acl: Add FreeBSD specific include to build
    Type: improvement
    Change-Id: I51345fb75843c67c6bf6a4c56380e1f0899c45b1
    Signed-off-by: Tom Jones <thj@freebsd.org>

--- src/plugins/acl/acl_test.c
+++ src/plugins/acl/acl_test.c
@@ -18,6 +18,10 @@
  *------------------------------------------------------------------
  */
 
+#ifdef __FreeBSD__
+#include <sys/endian.h>
+#endif /* __FreeBSD__ */
+
 #include <vat/vat.h>
 #include <vlibapi/api.h>
 #include <vlibmemory/api.h>
@@ -157,9 +161,13 @@ static void vl_api_acl_plugin_get_conn_table_max_entries_reply_t_handler
     (vl_api_acl_plugin_get_conn_table_max_entries_reply_t * mp)
     {
         vat_main_t * vam = acl_test_main.vat_main;
-        clib_warning("\nConn table max entries: %d",
-                    __bswap_64(mp->conn_table_max_entries) );
-        vam->result_ready = 1;
+	clib_warning ("\nConn table max entries: %d",
+#ifdef __linux__
+		      __bswap_64 (mp->conn_table_max_entries));
+#else
+		      bswap64 (mp->conn_table_max_entries));
+#endif /* __linux__ */
+	vam->result_ready = 1;
     }
 
 static inline u8 *
