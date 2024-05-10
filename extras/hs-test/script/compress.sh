bcbb52266055b3e3d8354e0c0452726a0ec9afde
Adrian Villin <avillin@cisco.com>
2024-05-10 04:19:35 -0400

    hs-test: fixed timed out tests passing in the CI
    Type: test
    
    Change-Id: Id05ea56bc5dfd80d42b8600cf11e763e25420bd0
    Signed-off-by: Adrian Villin <avillin@cisco.com>

--- extras/hs-test/script/compress.sh
+++ extras/hs-test/script/compress.sh
@@ -1,34 +1,37 @@
 #!/usr/bin/env bash
 
-if [ "${COMPRESS_FAILED_TEST_LOGS}" == "yes" -a -s "${HS_SUMMARY}/failed-summary.log" ]
+# if failed-summary.log is not empty, exit status = 1
+echo ${HS_ROOT}
+if [ -s "${HS_ROOT}/summary/failed-summary.log" ]
 then
-    echo -n "Copying docker logs..."
-    dirs=$(jq -r '.[0] | .SpecReports[] | select(.State == "failed") | .LeafNodeText' ${HS_SUMMARY}/report.json)
-    for dirName in $dirs; do
-        logDir=/tmp/hs-test/$dirName
-        if [ -d "$logDir" ]; then
-            mkdir -p $WORKSPACE/archives/summary
-            cp -r $logDir $WORKSPACE/archives/summary/
-        fi
-    done
-    echo "Done."
-
-    if [ -n "$WORKSPACE" ]
+    if [ "${COMPRESS_FAILED_TEST_LOGS}" == "yes" ]
     then
-        echo -n "Copying failed test logs into build log archive directory ($WORKSPACE/archives)... "
-        mkdir -p $WORKSPACE/archives/summary
-        cp -a ${HS_SUMMARY}/* $WORKSPACE/archives/summary
-    echo "Done."
+        echo -n "Copying docker logs..."
+        dirs=$(jq -r '.[0] | .SpecReports[] | select(.State == "failed") | .LeafNodeText' ${HS_ROOT}/summary/report.json)
+        for dirName in $dirs; do
+            logDir=/tmp/hs-test/$dirName
+            if [ -d "$logDir" ]; then
+                mkdir -p ${WORKSPACE}/archives/summary
+                cp -r $logDir ${WORKSPACE}/archives/summary/
+            fi
+        done
+        echo "Done."
+
+        if [ -n "${WORKSPACE}" ]
+        then
+            echo -n "Copying failed test logs into build log archive directory (${WORKSPACE}/archives)... "
+            mkdir -p ${WORKSPACE}/archives/summary
+            cp -a ${HS_ROOT}/* ${WORKSPACE}/archives/summary
+        echo "Done."
+        fi
+
+        echo -n "Compressing files in ${WORKSPACE}/archives from test runs... "
+        cd ${WORKSPACE}/archives
+        find . -type f \( -name "*.json" -o -name "*.log" \) -exec gzip {} \;
+        echo "Done."
+
+    else
+        echo "Not compressing files in temporary directories from test runs."
     fi
-
-    echo -n "Compressing files in $WORKSPACE/archives from test runs... "
-    cd $WORKSPACE/archives
-    find . -type f \( -name "*.json" -o -name "*.log" \) -exec gzip {} \;
-    echo "Done."
-
-else
-    echo "Not compressing files in temporary directories from test runs."
-    exit 0
+    exit 1
 fi
-
-exit 1
