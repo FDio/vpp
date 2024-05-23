FDio/vpp

commit 0a2fdc56694db3226dc99065955787db65b9da5a

octeon: fix rvu vf device id
Type: fix
Fixes: I1ea92f

Change-Id: Idf91b3f4cb3cc93081a282a14ffe2421a3628509
Signed-off-by: Monendra Singh Kushwaha <kmonendra@marvell.com>

src/plugins/dev_octeon/init.c
@@ -51,7 +51,8 @@ static struct
   }
 
   _ (0xa063, RVU_PF, "Marvell Octeon Resource Virtualization Unit PF"),
-  _ (0xa0f8, RVU_VF, "Marvell Octeon Resource Virtualization Unit VF"),
+  _ (0xa064, RVU_VF, "Marvell Octeon Resource Virtualization Unit VF"),
+  _ (0xa0f8, LBK_VF, "Marvell Octeon Loopback Unit VF"),
   _ (0xa0f7, SDP_VF, "Marvell Octeon System DPI Packet Interface Unit VF"),
   _ (0xa0f3, CPT_VF, "Marvell Octeon Cryptographic Accelerator Unit VF"),
 #undef _

src/plugins/dev_octeon/octeon.h
@@ -22,6 +22,7 @@ typedef enum
   OCT_DEVICE_TYPE_UNKNOWN = 0,
   OCT_DEVICE_TYPE_RVU_PF,
   OCT_DEVICE_TYPE_RVU_VF,
+  OCT_DEVICE_TYPE_LBK_VF,
   OCT_DEVICE_TYPE_SDP_VF,
   OCT_DEVICE_TYPE_CPT_VF,
 } __clib_packed oct_device_type_t;

commit 737f38678cac3912d633abfeaf6088abe6e4ca53

octeon: fix lbk vf initialization
Type: fix
Fixes: 0a2fdc56

Change-Id: I5d232a86be66edeec8b740a883104f5a22516697
Signed-off-by: Monendra Singh Kushwaha <kmonendra@marvell.com>

src/plugins/dev_octeon/init.c
@@ -245,6 +245,7 @@ oct_init (vlib_main_t *vm, vnet_dev_t *dev)
     {
     case OCT_DEVICE_TYPE_RVU_PF:
     case OCT_DEVICE_TYPE_RVU_VF:
+    case OCT_DEVICE_TYPE_LBK_VF:
     case OCT_DEVICE_TYPE_SDP_VF:
       return oct_init_nix (vm, dev);
 