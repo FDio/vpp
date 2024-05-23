0eb57165f44d5550c2e38b9d28b894bb10b233df
Damjan Marion <damarion@cisco.com>
2024-05-23 13:06:39 +0000

    vlib: traceback improvements
     - use libunwrap which seems to be industry standard
     - display traceback on console if running interactive or with syslog
       disabled (color output unless nocolor specified)
     - print hexdump of offending code
     - print library filename for each stack frame
    
    Change-Id: I61d3056251b87076be0578ccda300aa311c222ef
    Signed-off-by: Damjan Marion <damarion@cisco.com>

---
+++ src/vppinfra/format_ansi.h
@@ -0,0 +1,48 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2024 Cisco Systems, Inc.
+ */
+
+#ifndef __FORMAT_ANSI_H__
+#define __FORMAT_ANSI_H__
+
+#define ANSI_RESET	   "\x1b[0m"
+#define ANSI_BOLD	   "\x1b[1m"
+#define ANSI_ITALIC	   "\x1b[3m"
+#define ANSI_UNDERLINE	   "\x1b[4m"
+#define ANSI_BLINK	   "\x1b[5m"
+#define ANSI_FG_BLACK	   "\x1b[30m"
+#define ANSI_FG_RED	   "\x1b[31m"
+#define ANSI_FG_GREEN	   "\x1b[32m"
+#define ANSI_FG_YELLOW	   "\x1b[33m"
+#define ANSI_FG_BLUE	   "\x1b[34m"
+#define ANSI_FG_MAGENTA	   "\x1b[35m"
+#define ANSI_FG_CYAN	   "\x1b[36m"
+#define ANSI_FG_WHITE	   "\x1b[37m"
+#define ANSI_FG_DEFAULT	   "\x1b[39m"
+#define ANSI_BG_BLACK	   "\x1b[40m"
+#define ANSI_BG_RED	   "\x1b[41m"
+#define ANSI_BG_GREEN	   "\x1b[42m"
+#define ANSI_BG_YELLOW	   "\x1b[43m"
+#define ANSI_BG_BLUE	   "\x1b[44m"
+#define ANSI_BG_MAGENTA	   "\x1b[45m"
+#define ANSI_BG_CYAN	   "\x1b[46m"
+#define ANSI_BG_WHITE	   "\x1b[47m"
+#define ANSI_BG_DEFAULT	   "\x1b[49m"
+#define ANSI_FG_BR_BLACK   "\x1b[90m"
+#define ANSI_FG_BR_RED	   "\x1b[91m"
+#define ANSI_FG_BR_GREEN   "\x1b[92m"
+#define ANSI_FG_BR_YELLOW  "\x1b[93m"
+#define ANSI_FG_BR_BLUE	   "\x1b[94m"
+#define ANSI_FG_BR_MAGENTA "\x1b[95m"
+#define ANSI_FG_BR_CYAN	   "\x1b[96m"
+#define ANSI_FG_BR_WHITE   "\x1b[97m"
+#define ANSI_BG_BR_BLACK   "\x1b[100m"
+#define ANSI_BG_BR_RED	   "\x1b[101m"
+#define ANSI_BG_BR_GREEN   "\x1b[102m"
+#define ANSI_BG_BR_YELLOW  "\x1b[103m"
+#define ANSI_BG_BR_BLUE	   "\x1b[104m"
+#define ANSI_BG_BR_MAGENTA "\x1b[105m"
+#define ANSI_BG_BR_CYAN	   "\x1b[106m"
+#define ANSI_BG_BR_WHITE   "\x1b[107m"
+
+#endif /* __FORMAT_ANSI_H__ */
