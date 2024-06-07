83c6c6e7ca1afb339777ffb3c3febc014f11e9ef
hsandid <halsandi@cisco.com>
2024-06-07 18:11:48 +0200

    vlib: improve core pinning
    Type: feature
    
    Add keyword to translate cpu pinning configuration
    to match vpp launch affinity
    
    Change-Id: Ic748e5f4269525764b7adc92a8a5e4877e59c5a3
    Signed-off-by: hsandid <halsandi@cisco.com>

--- src/vppinfra/unix-misc.c
+++ src/vppinfra/unix-misc.c
@@ -66,8 +66,8 @@
 
 __clib_export __thread uword __os_thread_index = 0;
 __clib_export __thread uword __os_numa_index = 0;
-
-__clib_export clib_bitmap_t *os_get_cpu_affinity_bitmap (int pid);
+__clib_export cpu_set_t __os_affinity_cpu_set;
+__clib_export clib_bitmap_t *os_get_cpu_affinity_bitmap ();
 
 clib_error_t *
 clib_file_n_bytes (char *file, uword * result)
@@ -285,29 +285,29 @@ os_get_online_cpu_core_bitmap ()
 }
 
 __clib_export clib_bitmap_t *
-os_get_cpu_affinity_bitmap (int pid)
+os_get_cpu_affinity_bitmap ()
 {
 #if __linux
-  int index, ret;
-  cpu_set_t cpuset;
+  int cpu;
   uword *affinity_cpus;
 
-  clib_bitmap_alloc (affinity_cpus, sizeof (cpu_set_t));
+  clib_bitmap_alloc (affinity_cpus, __CPU_SETSIZE);
   clib_bitmap_zero (affinity_cpus);
 
-  CPU_ZERO_S (sizeof (cpu_set_t), &cpuset);
-
-  ret = sched_getaffinity (0, sizeof (cpu_set_t), &cpuset);
-
-  if (ret < 0)
+  if (__CPU_COUNT_S (sizeof (cpu_set_t), &__os_affinity_cpu_set) == 0)
     {
-      clib_bitmap_free (affinity_cpus);
-      return 0;
+      int ret;
+      ret = sched_getaffinity (0, sizeof (cpu_set_t), &__os_affinity_cpu_set);
+      if (ret < 0)
+	{
+	  clib_bitmap_free (affinity_cpus);
+	  return 0;
+	}
     }
 
-  for (index = 0; index < sizeof (cpu_set_t); index++)
-    if (CPU_ISSET_S (index, sizeof (cpu_set_t), &cpuset))
-      clib_bitmap_set (affinity_cpus, index, 1);
+  for (cpu = 0; cpu < __CPU_SETSIZE; cpu++)
+    if (__CPU_ISSET_S (cpu, __CPU_SETSIZE, &__os_affinity_cpu_set))
+      clib_bitmap_set (affinity_cpus, cpu, 1);
   return affinity_cpus;
 #elif defined(__FreeBSD__)
   cpuset_t mask;
@@ -330,6 +330,92 @@ os_get_cpu_affinity_bitmap (int pid)
 #else
   return NULL;
 #endif
+}
+
+__clib_export int
+os_translate_cpu_to_affinity_bitmap (int cpu)
+{
+  uword *affinity_bmp = os_get_cpu_affinity_bitmap ();
+  int cpu_it = 0;
+  int cpu_translate_it = 0;
+
+  if (!affinity_bmp || cpu == ~0)
+    return -1;
+
+  clib_bitmap_foreach (cpu_it, affinity_bmp)
+    {
+
+      if (cpu == cpu_translate_it)
+	{
+	  clib_bitmap_free (affinity_bmp);
+	  return cpu_it;
+	}
+
+      cpu_translate_it += 1;
+    }
+
+  clib_bitmap_free (affinity_bmp);
+  return -1;
+}
+
+__clib_export int
+os_translate_cpu_from_affinity_bitmap (int cpu_translated)
+{
+  uword *affinity_bmp = os_get_cpu_affinity_bitmap ();
+  int cpu_it = 0;
+  int cpu_translate_it = 0;
+
+  if (!affinity_bmp || cpu_translated == ~0)
+    return -1;
+
+  clib_bitmap_foreach (cpu_it, affinity_bmp)
+    {
+
+      if (cpu_translated == cpu_it)
+	{
+	  clib_bitmap_free (affinity_bmp);
+	  return cpu_translate_it;
+	}
+
+      cpu_translate_it += 1;
+    }
+
+  clib_bitmap_free (affinity_bmp);
+  return -1;
+}
+
+__clib_export clib_bitmap_t *
+os_translate_cpu_bmp_to_affinity_bitmap (clib_bitmap_t *cpu_bmp)
+{
+  uword *affinity_bmp = os_get_cpu_affinity_bitmap ();
+
+  if (!affinity_bmp)
+    return 0;
+
+  u32 cpu_count_relative = clib_bitmap_count_set_bits (affinity_bmp);
+  u32 cpu_max_corelist = clib_bitmap_last_set (cpu_bmp);
+
+  if (cpu_count_relative < cpu_max_corelist)
+    return 0;
+
+  uword *translated_cpulist;
+  clib_bitmap_alloc (translated_cpulist, __CPU_SETSIZE);
+  clib_bitmap_zero (translated_cpulist);
+
+  uword cpu_it;
+  uword cpu_translate_it = 0;
+
+  clib_bitmap_foreach (cpu_it, affinity_bmp)
+    {
+
+      if (clib_bitmap_get (cpu_bmp, cpu_translate_it))
+	clib_bitmap_set (translated_cpulist, cpu_it, 1);
+
+      cpu_translate_it++;
+    }
+
+  vec_free (affinity_bmp);
+  return translated_cpulist;
 }
 
 __clib_export clib_bitmap_t *
