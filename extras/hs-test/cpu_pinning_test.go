7b8e198ef8bdffc3cecf53aea4074849a788bf1a
Hadi Rayan Al-Sandid <halsandi@cisco.com>
2024-06-25 23:38:54 +0200

    hs-test: Add CPU pinning tests for translate keyword
    Type: test
    
    Added tests to cpu_pinning_suite.go to verify that
    VPP launches with configurations including the
    translate keyword.
    
    Change-Id: Ia6bef88ca22ebad5d37480230c8a84cabebab4a9
    Signed-off-by: Hadi Rayan Al-Sandid <halsandi@cisco.com>

--- extras/hs-test/cpu_pinning_test.go
+++ extras/hs-test/cpu_pinning_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func init() {
-	RegisterCpuPinningSoloTests(DefaultCpuConfigurationTest, SkipCoresTest)
+	RegisterCpuPinningSoloTests(DefaultCpuConfigurationTest, SkipCoresTest, MainCpuPinTest, WorkersNoMainThreadPinningTest, CoreListWorkersNoMainThreadPinningTest, WorkersMainThreadPinningTest, CoreListWorkersMainThreadPinningTest, TranslateAutoWorkersTest, TranslateAutoCoreListWorkersTest, TranslateAutoSkipCoresTest)
 }
 
 // TODO: Add more CPU configuration tests
@@ -16,15 +16,127 @@ func DefaultCpuConfigurationTest(s *CpuPinningSuite) {
 }
 
 func SkipCoresTest(s *CpuPinningSuite) {
-
-	skipCoresConfiguration := VppCpuConfig{
+	vppCpuConfig := VppCpuConfig{
 		PinMainCpu:         true,
 		PinWorkersCorelist: true,
+		TranslateCores:     false,
 		SkipCores:          1,
+	}
+
+	vpp := s.Containers.Vpp.VppInstance
+	vpp.CpuConfig = vppCpuConfig
+
+	s.AssertNil(vpp.Start())
+}
+
+func MainCpuPinTest(s *CpuPinningSuite) {
+	vppCpuConfig := VppCpuConfig{
+		PinMainCpu:         true,
+		PinWorkersCorelist: false,
+		TranslateCores:     false,
+		SkipCores:          0,
+	}
+
+	vpp := s.Containers.Vpp.VppInstance
+	vpp.CpuConfig = vppCpuConfig
+
+	s.AssertNil(vpp.Start())
+}
+
+func WorkersNoMainThreadPinningTest(s *CpuPinningSuite) {
+	skipCoresConfiguration := VppCpuConfig{
+		PinMainCpu:         false,
+		PinWorkersCorelist: false,
+		TranslateCores:     false,
+		SkipCores:          0,
 	}
 
 	vpp := s.Containers.Vpp.VppInstance
 	vpp.CpuConfig = skipCoresConfiguration
 
 	s.AssertNil(vpp.Start())
+}
+
+func CoreListWorkersNoMainThreadPinningTest(s *CpuPinningSuite) {
+	skipCoresConfiguration := VppCpuConfig{
+		PinMainCpu:         false,
+		PinWorkersCorelist: true,
+		TranslateCores:     false,
+		SkipCores:          0,
+	}
+
+	vpp := s.Containers.Vpp.VppInstance
+	vpp.CpuConfig = skipCoresConfiguration
+
+	s.AssertNotNil(vpp.Start()) /* vpp should fail during launch */
+}
+
+func WorkersMainThreadPinningTest(s *CpuPinningSuite) {
+	vppCpuConfig := VppCpuConfig{
+		PinMainCpu:         true,
+		PinWorkersCorelist: false,
+		TranslateCores:     false,
+		SkipCores:          0,
+	}
+
+	vpp := s.Containers.Vpp.VppInstance
+	vpp.CpuConfig = vppCpuConfig
+
+	s.AssertNil(vpp.Start())
+}
+
+func CoreListWorkersMainThreadPinningTest(s *CpuPinningSuite) {
+	vppCpuConfig := VppCpuConfig{
+		PinMainCpu:         true,
+		PinWorkersCorelist: true,
+		TranslateCores:     false,
+		SkipCores:          0,
+	}
+
+	vpp := s.Containers.Vpp.VppInstance
+	vpp.CpuConfig = vppCpuConfig
+
+	s.AssertNil(vpp.Start())
+}
+
+func TranslateAutoWorkersTest(s *CpuPinningSuite) {
+
+	translateCoresConfiguration := VppCpuConfig{
+		PinMainCpu:         true, /* main-thread must always be pinned when using translate mode */
+		PinWorkersCorelist: false,
+		TranslateCores:     true,
+		SkipCores:          0,
+	}
+	vpp := s.Containers.Vpp.VppInstance
+	vpp.CpuConfig = translateCoresConfiguration
+
+	s.AssertNil(vpp.Start())
+}
+
+func TranslateAutoCoreListWorkersTest(s *CpuPinningSuite) {
+
+	translateCoresConfiguration := VppCpuConfig{
+		PinMainCpu:         true, /* main-thread must always be pinned when using translate mode */
+		PinWorkersCorelist: true,
+		TranslateCores:     true,
+		SkipCores:          0,
+	}
+	vpp := s.Containers.Vpp.VppInstance
+	vpp.CpuConfig = translateCoresConfiguration
+
+	s.AssertNil(vpp.Start())
+}
+
+func TranslateAutoSkipCoresTest(s *CpuPinningSuite) {
+	translateCoresConfiguration := VppCpuConfig{
+		PinMainCpu:         true,
+		PinWorkersCorelist: false,
+		TranslateCores:     true,
+		SkipCores:          1,
+	}
+	vpp := s.Containers.Vpp.VppInstance
+	vpp.CpuConfig = translateCoresConfiguration
+
+	s.AssertNil(vpp.Start())
+
 }
