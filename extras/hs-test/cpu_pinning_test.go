460c7e5bd874c549423555f014a2cdf7dd0f3b37
Hadi Rayan Al-Sandid <halsandi@cisco.com>
2024-06-25 23:38:54 +0200

    hs-test: Add CPU pinning tests for translate keyword
    Type: test
    
    Added tests to cpu_pinning_suite.go to verify that
    VPP launches with configurations including the
    translate keyword.
    
    Change-Id: Ia6bef88ca22ebad5d37480230c8a84cabebab4a9
    Signed-off-by: Hadi Rayan Al-Sandid <halsandi@cisco.com>

--- extras/hs-test/cpu_pinning_test.go
+++ extras/hs-test/cpu_pinning_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func init() {
-	RegisterCpuPinningSoloTests(DefaultCpuConfigurationTest, SkipCoresTest)
+	RegisterCpuPinningSoloTests(DefaultCpuConfigurationTest, SkipCoresTest, TranslateAutoTest, TranslateAutoNoMainPinTest, TranslateAutoSkipCoresTest, TranslateNegativeAutoNoMainSet)
 }
 
 // TODO: Add more CPU configuration tests
@@ -20,6 +20,7 @@ func SkipCoresTest(s *CpuPinningSuite) {
 	skipCoresConfiguration := VppCpuConfig{
 		PinMainCpu:         true,
 		PinWorkersCorelist: true,
+		TranslateCores:     false,
 		SkipCores:          1,
 	}
 
@@ -27,4 +28,61 @@ func SkipCoresTest(s *CpuPinningSuite) {
 	vpp.CpuConfig = skipCoresConfiguration
 
 	s.AssertNil(vpp.Start())
+}
+
+func TranslateAutoTest(s *CpuPinningSuite) {
+
+	translateCoresConfiguration := VppCpuConfig{
+		PinMainCpu:         true,
+		PinWorkersCorelist: false,
+		TranslateCores:     true,
+		SkipCores:          0,
+	}
+	vpp := s.Containers.Vpp.VppInstance
+	vpp.CpuConfig = translateCoresConfiguration
+
+	s.AssertNil(vpp.Start())
+
+}
+
+func TranslateAutoNoMainPinTest(s *CpuPinningSuite) {
+	translateCoresConfiguration := VppCpuConfig{
+		PinMainCpu:         true, /* main-thread must always be pinned when using translate mode */
+		PinWorkersCorelist: false,
+		TranslateCores:     true,
+		SkipCores:          0,
+	}
+	vpp := s.Containers.Vpp.VppInstance
+	vpp.CpuConfig = translateCoresConfiguration
+
+	s.AssertNil(vpp.Start())
+
+}
+
+func TranslateAutoSkipCoresTest(s *CpuPinningSuite) {
+	translateCoresConfiguration := VppCpuConfig{
+		PinMainCpu:         true,
+		PinWorkersCorelist: false,
+		TranslateCores:     true,
+		SkipCores:          1,
+	}
+	vpp := s.Containers.Vpp.VppInstance
+	vpp.CpuConfig = translateCoresConfiguration
+
+	s.AssertNil(vpp.Start())
+
+}
+
+func TranslateNegativeAutoNoMainSet(s *CpuPinningSuite) {
+	translateCoresConfiguration := VppCpuConfig{
+		PinMainCpu:         false,
+		PinWorkersCorelist: false,
+		TranslateCores:     true,
+		SkipCores:          0,
+	}
+	vpp := s.Containers.Vpp.VppInstance
+	vpp.CpuConfig = translateCoresConfiguration
+
+	s.AssertNotNil(vpp.Start()) /* should fail */
+
 }
