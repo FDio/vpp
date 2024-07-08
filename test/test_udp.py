89adf4482d95cf4ab4fdb0cc669936c0c2c58c54
Steven Luong <sluong@cisco.com>
2024-07-08 11:21:23 -0700

    session: application namespace may reference a deleted vrf table
    lock the vrf table when adding an application namespace and
    unlock the vrf table when deleting an application namespace.
    
    Free the session rules table when no more application namespace
    uses it anymore to avoid memory leaks.
    
    Type: fix
    
    Change-Id: I10422c9a3b549bd4403962c925e29dd61a058eb0
    Signed-off-by: Steven Luong <sluong@cisco.com>

--- test/test_udp.py
+++ test/test_udp.py
@@ -721,6 +721,14 @@ class TestUDP(VppTestCase):
             i.unconfig_ip4()
             i.set_table_ip4(0)
             i.admin_down()
+        # Unconfigure namespaces - remove our locks to the vrf tables
+        self.vapi.app_namespace_add_del_v4(
+            is_add=0, namespace_id="0", sw_if_index=self.loop0.sw_if_index
+        )
+        self.vapi.app_namespace_add_del_v4(
+            is_add=0, namespace_id="1", sw_if_index=self.loop1.sw_if_index
+        )
+
         self.vapi.session_enable_disable(is_enable=0)
         super(TestUDP, self).tearDown()
 
