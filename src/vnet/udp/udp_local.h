ddd9a501c3573dddb67d41ecd53e3d8e9a96f5ff
Abdel Baig <abdbaig@cisco.com>
2024-09-03 11:52:20 -0400

    bfd: add support for multihop
    Type: feature
    
    Change-Id: If23f9cc9317e7528f3c8d66303457206843a12c5
    Signed-off-by: Abdel Baig <abdbaig@cisco.com>

--- src/vnet/udp/udp_local.h
+++ src/vnet/udp/udp_local.h
@@ -18,42 +18,43 @@
 
 #include <vnet/vnet.h>
 
-#define foreach_udp4_dst_port			\
-_ (53, dns)					\
-_ (67, dhcp_to_server)                          \
-_ (68, dhcp_to_client)                          \
-_ (500, ikev2)                                  \
-_ (2152, GTPU)					\
-_ (3784, bfd4)                                  \
-_ (3785, bfd_echo4)                             \
-_ (4341, lisp_gpe)                              \
-_ (4342, lisp_cp)                          	\
-_ (4500, ipsec)                                 \
-_ (4739, ipfix)                                 \
-_ (4789, vxlan)					\
-_ (4789, vxlan6)				\
-_ (48879, vxlan_gbp)				\
-_ (4790, VXLAN_GPE)				\
-_ (6633, vpath_3)				\
-_ (6081, geneve)				\
-_ (53053, dns_reply)
+#define foreach_udp4_dst_port                                                 \
+  _ (53, dns)                                                                 \
+  _ (67, dhcp_to_server)                                                      \
+  _ (68, dhcp_to_client)                                                      \
+  _ (500, ikev2)                                                              \
+  _ (2152, GTPU)                                                              \
+  _ (3784, bfd4)                                                              \
+  _ (3785, bfd_echo4)                                                         \
+  _ (4341, lisp_gpe)                                                          \
+  _ (4342, lisp_cp)                                                           \
+  _ (4500, ipsec)                                                             \
+  _ (4739, ipfix)                                                             \
+  _ (4784, bfd4_mh)                                                           \
+  _ (4789, vxlan)                                                             \
+  _ (4789, vxlan6)                                                            \
+  _ (48879, vxlan_gbp)                                                        \
+  _ (4790, VXLAN_GPE)                                                         \
+  _ (6633, vpath_3)                                                           \
+  _ (6081, geneve)                                                            \
+  _ (53053, dns_reply)
 
-
-#define foreach_udp6_dst_port                   \
-_ (53, dns6)					\
-_ (547, dhcpv6_to_server)                       \
-_ (546, dhcpv6_to_client)			\
-_ (2152, GTPU6)					\
-_ (3784, bfd6)                                  \
-_ (3785, bfd_echo6)                             \
-_ (4341, lisp_gpe6)                             \
-_ (4342, lisp_cp6)                          	\
-_ (48879, vxlan6_gbp)				\
-_ (4790, VXLAN6_GPE)                            \
-_ (6633, vpath6_3)				\
-_ (6081, geneve6)				\
-_ (8138, BIER)		         		\
-_ (53053, dns_reply6)
+#define foreach_udp6_dst_port                                                 \
+  _ (53, dns6)                                                                \
+  _ (547, dhcpv6_to_server)                                                   \
+  _ (546, dhcpv6_to_client)                                                   \
+  _ (2152, GTPU6)                                                             \
+  _ (3784, bfd6)                                                              \
+  _ (3785, bfd_echo6)                                                         \
+  _ (4341, lisp_gpe6)                                                         \
+  _ (4342, lisp_cp6)                                                          \
+  _ (48879, vxlan6_gbp)                                                       \
+  _ (4784, bfd6_mh)                                                           \
+  _ (4790, VXLAN6_GPE)                                                        \
+  _ (6633, vpath6_3)                                                          \
+  _ (6081, geneve6)                                                           \
+  _ (8138, BIER)                                                              \
+  _ (53053, dns_reply6)
 
 typedef enum
 {
