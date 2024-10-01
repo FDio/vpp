aacfc964ae16c27f5c23494a4b17f4710f181d0b
SuminSSon <itoodo12@gmail.com>
2024-10-02 00:53:44 +0900

    refactor(nat44): change argument order and parsing format for static mapping
    Change-Id: I1176bc93b82449bd0339ba0db5acf84af44ae41d
    Signed-off-by: SuminSSon <itoodo12@gmail.com>

--- src/plugins/nat/nat44-ed/nat44_ed_cli.c
+++ src/plugins/nat/nat44-ed/nat44_ed_cli.c
@@ -37,7 +37,7 @@ nat44_ed_enable_disable_command_fn (vlib_main_t *vm, unformat_input_t *input,
   unformat_input_t _line_input, *line_input = &_line_input;
   clib_error_t *error = 0;
 
-  nat44_config_t c = { 0 };
+  nat44_confinat44 add static mappingg_t c = { 0 };
   u8 enable_set = 0, enable = 0;
 
   if (!unformat_user (input, unformat_line_input, line_input))
@@ -814,9 +814,8 @@ nat44_show_interfaces_command_fn (vlib_main_t * vm, unformat_input_t * input,
 }
 
 static clib_error_t *
-add_static_mapping_command_fn (vlib_main_t * vm,
-			       unformat_input_t * input,
-			       vlib_cli_command_t * cmd)
+add_static_mapping_command_fn (vlib_main_t *vm, unformat_input_t *input,
+			       vlib_cli_command_t *cmd)
 {
   unformat_input_t _line_input, *line_input = &_line_input;
   vnet_main_t *vnm = vnet_get_main ();
@@ -830,37 +829,33 @@ add_static_mapping_command_fn (vlib_main_t * vm,
   ip_protocol_t proto = 0;
 
   if (!unformat_user (input, unformat_line_input, line_input))
-    return clib_error_return (0, NAT44_ED_EXPECTED_ARGUMENT);
+    return clib_error_return (0, "Expected arguments.");
 
   while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
     {
-      if (unformat (line_input, "local %U %u", unformat_ip4_address, &l_addr,
-		    &l_port))
-	{
-	  l_port_set = 1;
-	}
-      else
-	if (unformat (line_input, "local %U", unformat_ip4_address, &l_addr))
-	;
-      else if (unformat (line_input, "external %U %u", unformat_ip4_address,
-			 &e_addr, &e_port))
+      // 'external <addr>:<port>' 형식을 먼저 처리
+      if (unformat (line_input, "external %U:%u", unformat_ip4_address,
+		    &e_addr, &e_port))
 	{
 	  e_port_set = 1;
 	}
       else if (unformat (line_input, "external %U", unformat_ip4_address,
 			 &e_addr))
-	;
-      else if (unformat (line_input, "external %U %u",
-			 unformat_vnet_sw_interface, vnm, &sw_if_index,
-			 &e_port))
 	{
-	  flags |= NAT_SM_FLAG_SWITCH_ADDRESS;
-	  e_port_set = 1;
+	  // 포트가 생략되었을 경우 처리
+	  e_port_set = 0;
 	}
-      else if (unformat (line_input, "external %U",
-			 unformat_vnet_sw_interface, vnm, &sw_if_index))
+      // 'local <addr>:<port>' 형식을 처리
+      else if (unformat (line_input, "local %U:%u", unformat_ip4_address,
+			 &l_addr, &l_port))
 	{
-	  flags |= NAT_SM_FLAG_SWITCH_ADDRESS;
+	  l_port_set = 1;
+	}
+      else if (unformat (line_input, "local %U", unformat_ip4_address,
+			 &l_addr))
+	{
+	  // 포트가 생략되었을 경우 처리
+	  l_port_set = 0;
 	}
       else if (unformat (line_input, "exact %U", unformat_ip4_address,
 			 &pool_addr))
@@ -897,10 +892,10 @@ add_static_mapping_command_fn (vlib_main_t * vm,
 
   if (l_port_set != e_port_set)
     {
-      error = clib_error_return (0, "Either both ports are set or none.");
+      error = clib_error_return (
+	0, "Both local and external ports must be set or omitted together.");
       goto done;
     }
-
   if (!l_port_set)
     {
       flags |= NAT_SM_FLAG_ADDR_ONLY;
@@ -910,7 +905,6 @@ add_static_mapping_command_fn (vlib_main_t * vm,
       l_port = clib_host_to_net_u16 (l_port);
       e_port = clib_host_to_net_u16 (e_port);
     }
-
   if (is_add)
     {
       rv =
@@ -2104,28 +2098,31 @@ VLIB_CLI_COMMAND (nat44_show_interfaces_command, static) = {
  * @cliexpar
  * @cliexstart{nat44 add static mapping}
  * Static mapping allows hosts on the external network to initiate connection
- * to to the local network host.
- * To create static mapping between local host address 10.0.0.3 port 6303 and
- * external address 4.4.4.4 port 3606 for TCP protocol use:
- *  vpp# nat44 add static mapping tcp local 10.0.0.3 6303 external 4.4.4.4 3606
- * If not runnig "static mapping only" NAT plugin mode use before:
+ * to the local network host.
+ * To create static mapping between external address 4.4.4.4:3606 and
+ * local host address 10.0.0.3:6303 for TCP protocol use:
+ *  vpp# nat44 add static mapping tcp external 4.4.4.4:3606 local 10.0.0.3:6303
+ * If not running "static mapping only" NAT plugin mode use before:
  *  vpp# nat44 add address 4.4.4.4
- * To create address only static mapping between local and external address use:
- *  vpp# nat44 add static mapping local 10.0.0.3 external 4.4.4.4
- * To create ICMP static mapping between local and external with ICMP echo
+ * To create address only static mapping between external and local address
+use:
+ *  vpp# nat44 add static mapping external 4.4.4.4 local 10.0.0.3
+ * To create ICMP static mapping between external and local with ICMP echo
  * identifier 10 use:
- *  vpp# nat44 add static mapping icmp local 10.0.0.3 10 external 4.4.4.4 10
+ *  vpp# nat44 add static mapping icmp external 4.4.4.4:10 local 10.0.0.3:10
  * To force use of specific pool address, vrf independent
- *  vpp# nat44 add static mapping local 10.0.0.2 1234 external 10.0.2.2 1234 twice-nat exact 10.0.1.2
+ *  vpp# nat44 add static mapping external 10.0.2.2:1234 local 10.0.0.2:1234
+twice-nat exact 10.0.1.2
  * @cliexend
 ?*/
 VLIB_CLI_COMMAND (add_static_mapping_command, static) = {
   .path = "nat44 add static mapping",
   .function = add_static_mapping_command_fn,
-  .short_help =
-    "nat44 add static mapping tcp|udp|icmp local <addr> [<port|icmp-echo-id>] "
-    "external <addr> [<port|icmp-echo-id>] [vrf <table-id>] [twice-nat|self-twice-nat] "
-    "[out2in-only] [exact <pool-addr>] [del]",
+  .short_help = "nat44 add static mapping tcp|udp|icmp external "
+		"<addr>:<port|icmp-echo-id> "
+		"local <addr>:<port|icmp-echo-id> [vrf <table-id>] "
+		"[twice-nat|self-twice-nat] "
+		"[out2in-only] [exact <pool-addr>] [del]",
 };
 
 /*?
