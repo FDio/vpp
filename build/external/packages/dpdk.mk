FDio/vpp

commit 70522a1dc211013cc792f2c09e6a3500524dc1d9

dpdk: Enable dpdk build on FreeBSD
Type: improvement
Change-Id: If897f3bdfc131f8d7c01718057b54087eff02d7a
Signed-off-by: Tom Jones <thj@freebsd.org>

build/external/Makefile
@@ -49,12 +49,12 @@ AARCH64=$(filter aarch64,$(shell uname -m))
 include packages.mk
 include packages/ipsec-mb.mk
 include packages/quicly.mk
+include packages/dpdk.mk
 ifneq ($(shell uname), FreeBSD)
 include packages/rdma-core.mk
-include packages/dpdk.mk
 include packages/xdp-tools.mk
 include packages/octeon-roc.mk
-endif	# FreeBSD
+endif	# ! FreeBSD
 
 .PHONY: clean
 clean:
@@ -62,14 +62,14 @@ clean:
 
 .PHONY: install
 ifeq ($(shell uname), FreeBSD)
-install: $(if $(ARCH_X86_64), ipsec-mb-install) quicly-install
+install: $(if $(ARCH_X86_64), ipsec-mb-install) dpdk-install quicly-install
 else
 install: $(if $(ARCH_X86_64), ipsec-mb-install) dpdk-install rdma-core-install quicly-install xdp-tools-install $(if $(AARCH64), octeon-roc-install)
 endif	# FreeBSD
 
 .PHONY: config
 ifeq ($(shell uname), FreeBSD)
-config: $(if $(ARCH_X86_64), ipsec-mb-config) quicly-build
+config: $(if $(ARCH_X86_64), ipsec-mb-config) dpdk-config quicly-build
 else
 config: $(if $(ARCH_X86_64), ipsec-mb-config) dpdk-config rdma-core-config quicly-build
 endif	# FreeBSD

commit dda259e4d24bdcfef074d126a944aeda3a133086

build: fix dpdk mellanox driver build setting
- dpdk mellanox driver setting requires rdma-core.mk is included
  before dpdk.mk makefile.  Include check for rdma-core_version
  variable definition in dpdk.mk to prevent a re-occurance of this
  issue.

Type: fix
Fixes: 70522a1dc

Change-Id: I297cbd734ac7abe970eac7fb317d06b7acfc5a40
Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

build/external/Makefile
@@ -49,12 +49,12 @@ AARCH64=$(filter aarch64,$(shell uname -m))
 include packages.mk
 include packages/ipsec-mb.mk
 include packages/quicly.mk
-include packages/dpdk.mk
 ifneq ($(shell uname), FreeBSD)
 include packages/rdma-core.mk
 include packages/xdp-tools.mk
 include packages/octeon-roc.mk
 endif	# ! FreeBSD
+include packages/dpdk.mk
 
 .PHONY: clean
 clean:

build/external/packages/dpdk.mk
@@ -39,6 +39,9 @@ dpdk_depends		     := $(if $(ARCH_X86_64), ipsec-mb)
 else
 dpdk_depends		     := rdma-core $(if $(ARCH_X86_64), ipsec-mb)
 endif
+ifeq ($(rdma-core_version),)
+$(error Missing rdma-core_version)
+endif
 DPDK_MLX_DEFAULT             := $(shell if grep -q "rdma=$(rdma-core_version) dpdk=$(dpdk_version)" mlx_rdma_dpdk_matrix.txt; then echo 'y'; else echo 'n'; fi)
 DPDK_MLX4_PMD                ?= $(DPDK_MLX_DEFAULT)
 DPDK_MLX5_PMD                ?= $(DPDK_MLX_DEFAULT)