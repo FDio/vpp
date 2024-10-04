06d11636233dae99bd233ff65adf5ed2e2af5f11
Stanislav Zaikin <stanislav.zaikin@46labs.com>
2024-10-04 17:43:55 +0200

    af_xdp: remove deprecated api
    type: improvement
    
    Change-Id: Id53d94f9d5fe46eba3ed809e4c16136c0eb2befc
    Signed-off-by: Stanislav Zaikin <stanislav.zaikin@46labs.com>

--- src/plugins/af_xdp/api.c
+++ src/plugins/af_xdp/api.c
@@ -57,65 +57,6 @@ af_xdp_api_flags (vl_api_af_xdp_flag_t flags)
 }
 
 static void
-vl_api_af_xdp_create_t_handler (vl_api_af_xdp_create_t * mp)
-{
-  vlib_main_t *vm = vlib_get_main ();
-  af_xdp_main_t *rm = &af_xdp_main;
-  vl_api_af_xdp_create_reply_t *rmp;
-  af_xdp_create_if_args_t args;
-  int rv;
-
-  clib_memset (&args, 0, sizeof (af_xdp_create_if_args_t));
-
-  args.linux_ifname = mp->host_if[0] ? (char *) mp->host_if : 0;
-  args.name = mp->name[0] ? (char *) mp->name : 0;
-  args.prog = mp->prog[0] ? (char *) mp->prog : 0;
-  args.mode = af_xdp_api_mode (mp->mode);
-  args.flags = af_xdp_api_flags (mp->flags);
-  args.rxq_size = ntohs (mp->rxq_size);
-  args.txq_size = ntohs (mp->txq_size);
-  args.rxq_num = ntohs (mp->rxq_num);
-
-  af_xdp_create_if (vm, &args);
-  rv = args.rv;
-
-  REPLY_MACRO2 (VL_API_AF_XDP_CREATE_REPLY,
-		({ rmp->sw_if_index = ntohl (args.sw_if_index); }));
-}
-
-static void
-vl_api_af_xdp_create_v2_t_handler (vl_api_af_xdp_create_v2_t *mp)
-{
-  vlib_main_t *vm = vlib_get_main ();
-  af_xdp_main_t *rm = &af_xdp_main;
-  vl_api_af_xdp_create_v2_reply_t *rmp;
-  af_xdp_create_if_args_t args;
-  int rv;
-
-  clib_memset (&args, 0, sizeof (af_xdp_create_if_args_t));
-
-  args.linux_ifname = mp->host_if[0] ? (char *) mp->host_if : 0;
-  args.name = mp->name[0] ? (char *) mp->name : 0;
-  args.prog = mp->prog[0] ? (char *) mp->prog : 0;
-  args.netns = mp->namespace[0] ? (char *) mp->namespace : 0;
-  args.mode = af_xdp_api_mode (mp->mode);
-  args.flags = af_xdp_api_flags (mp->flags);
-  args.rxq_size = ntohs (mp->rxq_size);
-  args.txq_size = ntohs (mp->txq_size);
-  args.rxq_num = ntohs (mp->rxq_num);
-
-  af_xdp_create_if (vm, &args);
-  rv = args.rv;
-
-  /* clang-format off */
-  REPLY_MACRO2 (VL_API_AF_XDP_CREATE_V2_REPLY,
-    ({
-      rmp->sw_if_index = ntohl (args.sw_if_index);
-    }));
-  /* clang-format on */
-}
-
-static void
 vl_api_af_xdp_create_v3_t_handler (vl_api_af_xdp_create_v3_t *mp)
 {
   vlib_main_t *vm = vlib_get_main ();
