FDio/vpp

commit 4e518418964d0597df8a9b1f34b01d2f9500b47b

armada: introduce dev_armada plugin
Also retires old marvell plugin.

Change-Id: Icedec11f5661909058fdfe8d5fc455306adafacd
Type: feature
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/plugins/dev_armada/pp2/port.c
@@ -1,0 +1,280 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2023 Cisco Systems, Inc.
+ */
+
+#include <vnet/vnet.h>
+#include <vnet/ethernet/ethernet.h>
+#include <vnet/dev/dev.h>
+#include <vnet/dev/counters.h>
+#include <vnet/dev/bus/platform.h>
+#include <vppinfra/ring.h>
+#include <dev_armada/musdk.h>
+#include <dev_armada/pp2/pp2.h>
+
+VLIB_REGISTER_LOG_CLASS (mvpp2_log, static) = {
+  .class_name = "armada",
+  .subclass_name = "pp2-port",
+};
+
+vnet_dev_rv_t
+mvpp2_port_init (vlib_main_t *vm, vnet_dev_port_t *port)
+{
+  vnet_dev_t *dev = port->dev;
+  mvpp2_device_t *md = vnet_dev_get_data (dev);
+  mvpp2_port_t *mp = vnet_dev_get_port_data (port);
+  vnet_dev_rv_t rv = VNET_DEV_OK;
+  struct pp2_ppio_link_info li;
+  char match[16];
+  int mrv;
+
+  log_debug (port->dev, "");
+
+  snprintf (match, sizeof (match), "ppio-%d:%d", md->pp_id, port->port_id);
+
+  struct pp2_ppio_params ppio_params = {
+    .match = match,
+    .type = PP2_PPIO_T_NIC,
+    .eth_start_hdr = mp->is_dsa ? PP2_PPIO_HDR_ETH_DSA : PP2_PPIO_HDR_ETH,
+    .inqs_params = {
+      .num_tcs = 1,
+      .tcs_params[0] = {
+        .pkt_offset = 0,
+	.num_in_qs = 1,
+	.inqs_params = &(struct pp2_ppio_inq_params) { .size = 512 },
+	.pools[0][0] = md->thread[0].bpool,
+      },
+    },
+  };
+
+  foreach_vnet_dev_port_rx_queue (q, port)
+    {
+      struct pp2_ppio_outqs_params *oqs = &ppio_params.outqs_params;
+      oqs->outqs_params[0].weight = 1;
+      oqs->outqs_params[0].size = q->size;
+      oqs->num_outqs++;
+    }
+
+  mrv = pp2_ppio_init (&ppio_params, &mp->ppio);
+  if (mrv)
+    {
+      rv = VNET_DEV_ERR_INIT_FAILED;
+      log_err (dev, "port %u ppio '%s' init failed, rv %d", port->port_id,
+	       match, mrv);
+      goto done;
+    }
+  log_debug (dev, "port %u ppio '%s' init ok", port->port_id, match);
+
+  mrv = pp2_ppio_get_link_info (mp->ppio, &li);
+  if (mrv)
+    {
+      rv = VNET_DEV_ERR_INIT_FAILED;
+      log_err (dev, "failed to get link info for port %u, rv %d",
+	       port->port_id, mrv);
+      goto done;
+    }
+
+  log_debug (dev, "port %u %U", port->port_id, format_pp2_ppio_link_info, &li);
+
+done:
+  if (rv != VNET_DEV_OK)
+    mvpp2_port_stop (vm, port);
+  return rv;
+}
+
+void
+mvpp2_port_deinit (vlib_main_t *vm, vnet_dev_port_t *port)
+{
+  mvpp2_port_t *mp = vnet_dev_get_port_data (port);
+
+  log_debug (port->dev, "");
+
+  if (mp->ppio)
+    {
+      pp2_ppio_deinit (mp->ppio);
+      mp->ppio = 0;
+    }
+}
+
+void
+mvpp2_port_poll (vlib_main_t *vm, vnet_dev_port_t *port)
+{
+  mvpp2_port_t *mp = vnet_dev_get_port_data (port);
+  vnet_dev_t *dev = port->dev;
+  vnet_dev_port_state_changes_t changes = {};
+  struct pp2_ppio_link_info li;
+  int mrv;
+
+  mrv = pp2_ppio_get_link_info (mp->ppio, &li);
+
+  if (mrv)
+    {
+      log_debug (dev, "pp2_ppio_get_link_info: failed, rv %d", mrv);
+      return;
+    }
+
+  if (mp->last_link_info.up != li.up)
+    {
+      changes.change.link_state = 1;
+      changes.link_state = li.up != 0;
+      log_debug (dev, "link state changed to %u", changes.link_state);
+    }
+
+  if (mp->last_link_info.duplex != li.duplex)
+    {
+      changes.change.link_duplex = 1;
+      changes.full_duplex = li.duplex != 0;
+      log_debug (dev, "link full duplex changed to %u", changes.full_duplex);
+    }
+
+  if (mp->last_link_info.speed != li.speed)
+    {
+      u32 speeds[] = {
+	[MV_NET_LINK_SPEED_AN] = 0,
+	[MV_NET_LINK_SPEED_10] = 10000,
+	[MV_NET_LINK_SPEED_100] = 100000,
+	[MV_NET_LINK_SPEED_1000] = 1000000,
+	[MV_NET_LINK_SPEED_2500] = 2500000,
+	[MV_NET_LINK_SPEED_10000] = 10000000,
+      };
+
+      if (li.speed < ARRAY_LEN (speeds))
+	{
+	  changes.change.link_speed = 1;
+	  changes.link_speed = speeds[li.speed];
+	  log_debug (dev, "link speed changed to %u", changes.link_speed);
+	}
+    }
+
+  if (changes.change.any == 0)
+    return;
+
+  mp->last_link_info = li;
+
+  vnet_dev_port_state_change (vm, port, changes);
+}
+
+vnet_dev_rv_t
+mvpp2_port_start (vlib_main_t *vm, vnet_dev_port_t *port)
+{
+  mvpp2_port_t *mp = vnet_dev_get_port_data (port);
+  int mrv;
+
+  log_debug (port->dev, "");
+
+  mrv = pp2_ppio_enable (mp->ppio);
+  if (mrv)
+    {
+      log_err (port->dev, "pp2_ppio_enable() failed, rv %d", mrv);
+      return VNET_DEV_ERR_NOT_READY;
+    }
+
+  mp->is_enabled = 1;
+
+  vnet_dev_poll_port_add (vm, port, 0.5, mvpp2_port_poll);
+
+  return VNET_DEV_OK;
+}
+
+void
+mvpp2_port_stop (vlib_main_t *vm, vnet_dev_port_t *port)
+{
+  int rv;
+  mvpp2_port_t *mp = vnet_dev_get_port_data (port);
+
+  log_debug (port->dev, "");
+
+  if (mp->is_enabled)
+    {
+      vnet_dev_poll_port_remove (vm, port, mvpp2_port_poll);
+
+      rv = pp2_ppio_disable (mp->ppio);
+      if (rv)
+	log_err (port->dev, "pp2_ppio_disable() failed, rv %d", rv);
+
+      vnet_dev_port_state_change (vm, port,
+				  (vnet_dev_port_state_changes_t){
+				    .change.link_state = 1,
+				    .change.link_speed = 1,
+				    .link_speed = 0,
+				    .link_state = 0,
+				  });
+      mp->is_enabled = 0;
+    }
+}
+
+vnet_dev_rv_t
+mvpp2_port_cfg_change_validate (vlib_main_t *vm, vnet_dev_port_t *port,
+				vnet_dev_port_cfg_change_req_t *req)
+{
+  vnet_dev_rv_t rv = VNET_DEV_OK;
+
+  switch (req->type)
+    {
+    case VNET_DEV_PORT_CFG_PROMISC_MODE:
+    case VNET_DEV_PORT_CFG_ADD_SECONDARY_HW_ADDR:
+    case VNET_DEV_PORT_CFG_REMOVE_SECONDARY_HW_ADDR:
+      break;
+
+    default:
+      rv = VNET_DEV_ERR_NOT_SUPPORTED;
+    };
+
+  return rv;
+}
+
+vnet_dev_rv_t
+mvpp2_port_cfg_change (vlib_main_t *vm, vnet_dev_port_t *port,
+		       vnet_dev_port_cfg_change_req_t *req)
+{
+  mvpp2_port_t *mp = vnet_dev_get_port_data (port);
+  vnet_dev_rv_t rv = VNET_DEV_OK;
+  eth_addr_t addr;
+  int mrv;
+
+  switch (req->type)
+    {
+
+    case VNET_DEV_PORT_CFG_PROMISC_MODE:
+      mrv = pp2_ppio_set_promisc (mp->ppio, req->promisc);
+      if (mrv)
+	{
+	  log_err (port->dev, "pp2_ppio_set_promisc: failed, rv %d", mrv);
+	  rv = VNET_DEV_ERR_INTERNAL;
+	}
+      else
+	log_debug (port->dev, "pp2_ppio_set_promisc: promisc %u",
+		   req->promisc);
+      break;
+
+    case VNET_DEV_PORT_CFG_ADD_SECONDARY_HW_ADDR:
+      clib_memcpy (&addr, req->addr.eth_mac, sizeof (addr));
+      mrv = pp2_ppio_add_mac_addr (mp->ppio, addr);
+      if (mrv)
+	{
+	  log_err (port->dev, "pp2_ppio_add_mac_addr: failed, rv %d", mrv);
+	  rv = VNET_DEV_ERR_INTERNAL;
+	}
+      else
+	log_debug (port->dev, "pp2_ppio_add_mac_addr: %U added",
+		   format_ethernet_address, &addr);
+      break;
+
+    case VNET_DEV_PORT_CFG_REMOVE_SECONDARY_HW_ADDR:
+      clib_memcpy (&addr, req->addr.eth_mac, sizeof (addr));
+      mrv = pp2_ppio_remove_mac_addr (mp->ppio, addr);
+      if (mrv)
+	{
+	  log_err (port->dev, "pp2_ppio_remove_mac_addr: failed, rv %d", mrv);
+	  rv = VNET_DEV_ERR_INTERNAL;
+	}
+      else
+	log_debug (port->dev, "pp2_ppio_remove_mac_addr: %U added",
+		   format_ethernet_address, &addr);
+      break;
+
+    default:
+      return VNET_DEV_ERR_NOT_SUPPORTED;
+    };
+
+  return rv;
+}

MAINTAINERS
@@ -500,10 +500,10 @@ I:	memif
 M:	Damjan Marion <damarion@cisco.com>
 F:	src/plugins/memif/
 
-Plugin - Marvell MUSDK device driver
-I:	marvell
+Plugin - Marvell Armada device driver
+I:	armada
 M:	Damjan Marion <damarion@cisco.com>
-F:	src/plugins/marvell/
+F:	src/plugins/dev_armada/
 
 Plugin - performance counter
 I:	perfmon

src/plugins/dev_armada/CMakeLists.txt
@@ -1,0 +1,30 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright(c) 2022 Cisco Systems, Inc.
+
+
+find_path(MUSDK_INCLUDE_DIR NAMES mv_std.h)
+find_library(MUSDK_LIB NAMES libmusdk.a)
+
+if(NOT MUSDK_INCLUDE_DIR OR NOT MUSDK_LIB)
+  message(WARNING "Marvell MUSDK not found - dev_armada plugin disabled")
+  return()
+endif()
+
+get_filename_component(MUSDK_LIB_DIR ${MUSDK_LIB} DIRECTORY)
+set(MUSDK_LINK_FLAGS "-Wl,--whole-archive,${MUSDK_LIB_DIR}/libmusdk.a,--no-whole-archive")
+
+add_vpp_plugin(dev_armada
+  SOURCES
+  plugin.c
+  pp2/init.c
+  pp2/format.c
+  pp2/port.c
+  pp2/queue.c
+  pp2/rx.c
+  pp2/tx.c
+
+  LINK_FLAGS
+  ${MUSDK_LINK_FLAGS}
+)
+include_directories(${MUSDK_INCLUDE_DIR})
+

src/plugins/dev_armada/musdk.h
@@ -1,0 +1,22 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2023 Cisco Systems, Inc.
+ */
+
+#ifndef _MUSDK_H_
+#define _MUSDK_H_
+
+#define MVCONF_DBG_LEVEL	       0
+#define MVCONF_PP2_BPOOL_COOKIE_SIZE   32
+#define MVCONF_PP2_BPOOL_DMA_ADDR_SIZE 64
+#define MVCONF_DMA_PHYS_ADDR_T_SIZE    64
+#define MVCONF_SYS_DMA_UIO
+#define MVCONF_TYPES_PUBLIC
+#define MVCONF_DMA_PHYS_ADDR_T_PUBLIC
+
+#include <mv_std.h>
+#include <env/mv_sys_dma.h>
+#include <drivers/mv_pp2.h>
+#include <drivers/mv_pp2_bpool.h>
+#include <drivers/mv_pp2_ppio.h>
+
+#endif /* _MUSDK_H_ */

src/plugins/dev_armada/plugin.c
@@ -1,0 +1,12 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2023 Cisco Systems, Inc.
+ */
+
+#include <vlib/vlib.h>
+#include <vnet/plugin/plugin.h>
+#include <vpp/app/version.h>
+
+VLIB_PLUGIN_REGISTER () = {
+  .version = VPP_BUILD_VER,
+  .description = "Marvell Armada Drivers",
+};

src/plugins/dev_armada/pp2/format.c
@@ -1,0 +1,176 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2023 Cisco Systems, Inc.
+ */
+
+#include <vnet/vnet.h>
+#include <vnet/dev/dev.h>
+#include <vnet/dev/counters.h>
+#include <vnet/dev/bus/platform.h>
+#include <dev_armada/musdk.h>
+#include <dev_armada/pp2/pp2.h>
+
+static inline u32
+mrvl_get_u32_bits (void *start, int offset, int first, int last)
+{
+  u32 value = *(u32 *) (((u8 *) start) + offset);
+  if ((last == 0) && (first == 31))
+    return value;
+  value >>= last;
+  value &= (1 << (first - last + 1)) - 1;
+  return value;
+}
+
+u8 *
+format_pp2_ppio_link_info (u8 *s, va_list *args)
+{
+  struct pp2_ppio_link_info *li = va_arg (*args, struct pp2_ppio_link_info *);
+
+  char *port_duplex[] = {
+    [MV_NET_LINK_DUPLEX_HALF] = "half",
+    [MV_NET_LINK_DUPLEX_FULL] = "full",
+  };
+
+  u32 port_speeds[] = {
+    [MV_NET_LINK_SPEED_10] = 10,       [MV_NET_LINK_SPEED_100] = 100,
+    [MV_NET_LINK_SPEED_1000] = 1000,   [MV_NET_LINK_SPEED_2500] = 2500,
+    [MV_NET_LINK_SPEED_10000] = 10000,
+  };
+
+  char *port_phy_modes[] = {
+    [MV_NET_PHY_MODE_NONE] = "NONE",
+    [MV_NET_PHY_MODE_MII] = "MII",
+    [MV_NET_PHY_MODE_GMII] = "GMII",
+    [MV_NET_PHY_MODE_SGMII] = "SGMII",
+    [MV_NET_PHY_MODE_TBI] = "TBI",
+    [MV_NET_PHY_MODE_REVMII] = "REVMII",
+    [MV_NET_PHY_MODE_RMII] = "RMII",
+    [MV_NET_PHY_MODE_RGMII] = "RGMII",
+    [MV_NET_PHY_MODE_RGMII_ID] = "RGMII_ID",
+    [MV_NET_PHY_MODE_RGMII_RXID] = "RGMII_RXID",
+    [MV_NET_PHY_MODE_RGMII_TXID] = "RGMII_TXID",
+    [MV_NET_PHY_MODE_RTBI] = "RTBI",
+    [MV_NET_PHY_MODE_SMII] = "SMII",
+    [MV_NET_PHY_MODE_XGMII] = "XGMII",
+    [MV_NET_PHY_MODE_MOCA] = "MOCA",
+    [MV_NET_PHY_MODE_QSGMII] = "QSGMII",
+    [MV_NET_PHY_MODE_XAUI] = "XAUI",
+    [MV_NET_PHY_MODE_RXAUI] = "RXAUI",
+    [MV_NET_PHY_MODE_KR] = "KR",
+  };
+
+  s =
+    format (s, "duplex %s speed %d up %d phy_mode %s", port_duplex[li->duplex],
+	    port_speeds[li->speed], li->up, port_phy_modes[li->phy_mode]);
+
+  return s;
+}
+
+u8 *
+format_mvpp2_port_status (u8 *s, va_list *args)
+{
+  vnet_dev_format_args_t __clib_unused *a =
+    va_arg (*args, vnet_dev_format_args_t *);
+  vnet_dev_port_t *port = va_arg (*args, vnet_dev_port_t *);
+  mvpp2_port_t *mp = vnet_dev_get_port_data (port);
+  struct pp2_ppio_link_info li = {};
+
+  if (mp->ppio == 0 || pp2_ppio_get_link_info (mp->ppio, &li))
+    return format (s, "link info not available");
+
+  return format (s, "%U", format_pp2_ppio_link_info, &li);
+}
+
+u8 *
+format_mvpp2_dev_info (u8 *s, va_list *args)
+{
+  vnet_dev_format_args_t __clib_unused *a =
+    va_arg (*args, vnet_dev_format_args_t *);
+  vnet_dev_t *dev = va_arg (*args, vnet_dev_t *);
+  mvpp2_device_t *md = vnet_dev_get_data (dev);
+
+  format (s, "pp_id is %u", md->pp_id);
+  return s;
+}
+
+#define foreach_pp2_rx_desc_field                                             \
+  _ (0x00, 6, 0, l3_offset)                                                   \
+  _ (0x00, 12, 8, ip_hdlen)                                                   \
+  _ (0x00, 14, 13, ec)                                                        \
+  _ (0x00, 15, 15, es)                                                        \
+  _ (0x00, 19, 16, pool_id)                                                   \
+  _ (0x00, 21, 21, hwf_sync)                                                  \
+  _ (0x00, 22, 22, l4_chk_ok)                                                 \
+  _ (0x00, 23, 23, ip_frg)                                                    \
+  _ (0x00, 24, 24, ipv4_hdr_err)                                              \
+  _ (0x00, 27, 25, l4_info)                                                   \
+  _ (0x00, 30, 28, l3_info)                                                   \
+  _ (0x00, 31, 31, buf_header)                                                \
+  _ (0x04, 5, 0, lookup_id)                                                   \
+  _ (0x04, 8, 6, cpu_code)                                                    \
+  _ (0x04, 9, 9, pppoe)                                                       \
+  _ (0x04, 11, 10, l3_cast_info)                                              \
+  _ (0x04, 13, 12, l2_cast_info)                                              \
+  _ (0x04, 15, 14, vlan_info)                                                 \
+  _ (0x04, 31, 16, byte_count)                                                \
+  _ (0x08, 11, 0, gem_port_id)                                                \
+  _ (0x08, 13, 12, color)                                                     \
+  _ (0x08, 14, 14, gop_sop_u)                                                 \
+  _ (0x08, 15, 15, key_hash_enable)                                           \
+  _ (0x08, 31, 16, l4chk)                                                     \
+  _ (0x0c, 31, 0, timestamp)                                                  \
+  _ (0x10, 31, 0, buf_phys_ptr_lo)                                            \
+  _ (0x14, 7, 0, buf_phys_ptr_hi)                                             \
+  _ (0x14, 31, 8, key_hash)                                                   \
+  _ (0x18, 31, 0, buf_virt_ptr_lo)                                            \
+  _ (0x1c, 7, 0, buf_virt_ptr_hi)                                             \
+  _ (0x1c, 14, 8, buf_qset_no)                                                \
+  _ (0x1c, 15, 15, buf_type)                                                  \
+  _ (0x1c, 21, 16, mod_dscp)                                                  \
+  _ (0x1c, 24, 22, mod_pri)                                                   \
+  _ (0x1c, 25, 25, mdscp)                                                     \
+  _ (0x1c, 26, 26, mpri)                                                      \
+  _ (0x1c, 27, 27, mgpid)                                                     \
+  _ (0x1c, 31, 29, port_num)
+
+u8 *
+format_mvpp2_rx_desc (u8 *s, va_list *args)
+
+{
+  struct pp2_ppio_desc *d = va_arg (*args, struct pp2_ppio_desc *);
+  u32 indent = format_get_indent (s);
+  u32 r32;
+
+#define _(a, b, c, n)                                                         \
+  r32 = mrvl_get_u32_bits (d, a, b, c);                                       \
+  if (r32 > 9)                                                                \
+    s = format (s, "%s %u (0x%x)", #n, r32, r32);                             \
+  else                                                                        \
+    s = format (s, "%s %u", #n, r32);                                         \
+  if (format_get_indent (s) > 72)                                             \
+    s = format (s, "\n%U", format_white_space, indent + 2);                   \
+  else                                                                        \
+    s = format (s, " ");
+
+  foreach_pp2_rx_desc_field;
+  return s;
+}
+
+u8 *
+format_mvpp2_rx_trace (u8 *s, va_list *args)
+{
+  vlib_main_t *vm = va_arg (*args, vlib_main_t *);
+  vlib_node_t *node = va_arg (*args, vlib_node_t *);
+  mvpp2_rx_trace_t *t = va_arg (*args, mvpp2_rx_trace_t *);
+  vnet_main_t *vnm = vnet_get_main ();
+  u32 hw_if_index = t->rxq->port->intf.hw_if_index;
+  vnet_hw_interface_t *hi = vnet_get_hw_interface (vnm, hw_if_index);
+  u32 indent = format_get_indent (s);
+  struct pp2_ppio_desc *d = &t->desc;
+
+  s = format (s, "pp2: %v (%d) next-node %U", hi->name, hw_if_index,
+	      format_vlib_next_node_name, vm, node->index, t->rxq->next_index);
+  s = format (s, "\n%U%U", format_white_space, indent + 2,
+	      format_mvpp2_rx_desc, d);
+
+  return s;
+}

src/plugins/dev_armada/pp2/init.c
@@ -1,0 +1,343 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2023 Cisco Systems, Inc.
+ */
+
+#include <vnet/vnet.h>
+#include <vnet/dev/dev.h>
+#include <vnet/dev/counters.h>
+#include <vnet/dev/bus/platform.h>
+#include <vppinfra/ring.h>
+#include <dev_armada/musdk.h>
+#include <dev_armada/pp2/pp2.h>
+#include <vnet/ethernet/ethernet.h>
+#include <vnet/plugin/plugin.h>
+#include <vpp/app/version.h>
+
+#include <linux/if.h>
+#include <sys/ioctl.h>
+
+#define MV_SYS_DMA_MEM_SZ (2 << 20)
+
+VLIB_REGISTER_LOG_CLASS (mvpp2_log, static) = {
+  .class_name = "armada",
+  .subclass_name = "init",
+};
+
+static int num_pp2_in_use = 0;
+static int dma_mem_initialized = 0;
+static int global_pp2_initialized = 0;
+
+#define _(f, n, s, d)                                                         \
+  { .name = #n, .desc = d, .severity = VL_COUNTER_SEVERITY_##s },
+
+vlib_error_desc_t mvpp2_rx_node_counters[] = { foreach_mvpp2_rx_node_counter };
+vlib_error_desc_t mvpp2_tx_node_counters[] = { foreach_mvpp2_tx_node_counter };
+#undef _
+
+vnet_dev_node_t mvpp2_rx_node = {
+  .error_counters = mvpp2_rx_node_counters,
+  .n_error_counters = ARRAY_LEN (mvpp2_rx_node_counters),
+  .format_trace = format_mvpp2_rx_trace,
+};
+
+vnet_dev_node_t mvpp2_tx_node = {
+  .error_counters = mvpp2_tx_node_counters,
+  .n_error_counters = ARRAY_LEN (mvpp2_tx_node_counters),
+};
+
+static u8 *
+mvpp2_probe (vlib_main_t *vm, vnet_dev_bus_index_t bus_index, void *dev_info)
+{
+  vnet_dev_bus_platform_device_info_t *di = dev_info;
+
+  if (clib_dt_node_is_compatible (di->node, "marvell,armada-7k-pp22"))
+    return format (0, "Marvell Armada Packet Processor v2.2");
+  return 0;
+}
+static void
+mvpp2_global_deinit (vlib_main_t *vm, vnet_dev_t *dev)
+{
+  mvpp2_device_t *md = vnet_dev_get_data (dev);
+  log_debug (dev, "");
+  if (--num_pp2_in_use == 0)
+    {
+      if (global_pp2_initialized)
+	{
+	  for (u32 i = 0; i < ARRAY_LEN (md->thread); i++)
+	    if (md->thread[i].bpool)
+	      {
+		pp2_bpool_deinit (md->thread[i].bpool);
+		md->thread[i].bpool = 0;
+	      }
+	  for (u32 i = 0; i < ARRAY_LEN (md->hif); i++)
+	    if (md->hif[i])
+	      {
+		pp2_hif_deinit (md->hif[i]);
+		md->hif[i] = 0;
+	      }
+
+	  pp2_deinit ();
+	  global_pp2_initialized = 0;
+	}
+      if (dma_mem_initialized)
+	{
+	  mv_sys_dma_mem_destroy ();
+	  log_debug (0, "mv_sys_dma_mem_destroy()");
+	  dma_mem_initialized = 0;
+	}
+    }
+}
+
+static void
+mvpp2_deinit (vlib_main_t *vm, vnet_dev_t *dev)
+{
+  log_debug (dev, "");
+  mvpp2_global_deinit (vm, dev);
+}
+
+static vnet_dev_rv_t
+mvpp2_global_init (vlib_main_t *vm, vnet_dev_t *dev)
+{
+  mvpp2_device_t *md = vnet_dev_get_data (dev);
+  vnet_dev_rv_t rv = VNET_DEV_OK;
+  int mrv;
+  u16 free_hifs, free_bpools;
+  u16 n_threads = vlib_get_n_threads ();
+
+  struct pp2_init_params init_params = {
+    .hif_reserved_map = 0xf,
+    .bm_pool_reserved_map = 0x7,
+  };
+
+  if (num_pp2_in_use++)
+    return rv;
+
+  mrv = mv_sys_dma_mem_init (MV_SYS_DMA_MEM_SZ);
+  if (mrv < 0)
+    {
+      log_err (0, "mv_sys_dma_mem_init failed, err %d", mrv);
+      rv = VNET_DEV_ERR_INIT_FAILED;
+      goto done;
+    }
+
+  dma_mem_initialized = 1;
+  log_debug (0, "mv_sys_dma_mem_init(%u) ok", MV_SYS_DMA_MEM_SZ);
+
+  if ((mrv = pp2_init (&init_params)))
+    {
+      log_err (dev, "pp2_init failed, err %d", mrv);
+      rv = VNET_DEV_ERR_INIT_FAILED;
+      goto done;
+    }
+
+  log_debug (dev, "pp2_init() ok");
+
+  free_hifs = pow2_mask (MVPP2_NUM_HIFS) ^ init_params.hif_reserved_map;
+  free_bpools =
+    pow2_mask (MVPP2_NUM_BPOOLS) ^ init_params.bm_pool_reserved_map;
+
+  if (n_threads > count_set_bits (free_hifs))
+    {
+      log_err (dev, "no enough HIFs (needed %u available %u)", n_threads,
+	       count_set_bits (free_hifs));
+      rv = VNET_DEV_ERR_INIT_FAILED;
+      goto done;
+    }
+
+  for (u32 i = 0; i < n_threads; i++)
+    {
+      char match[16];
+      u8 index;
+      struct pp2_hif_params hif_params = {
+	.match = match,
+	.out_size = 2048,
+      };
+      struct pp2_bpool_params bpool_params = {
+	.match = match,
+	.buff_len = vlib_buffer_get_default_data_size (vm),
+      };
+
+      index = get_lowest_set_bit_index (free_hifs);
+      free_hifs ^= 1 << index;
+      snprintf (match, sizeof (match), "hif-%u", index);
+
+      mrv = pp2_hif_init (&hif_params, md->hif + i);
+      if (mrv < 0)
+	{
+	  log_err (dev, "pp2_hif_init failed for hif %u thread %u, err %d",
+		   index, i, mrv);
+	  rv = VNET_DEV_ERR_INIT_FAILED;
+	  goto done;
+	}
+      log_debug (dev, "pp2_hif_init(hif %u, thread %u) ok", index, i);
+
+      index = get_lowest_set_bit_index (free_bpools);
+      free_bpools ^= 1 << index;
+      snprintf (match, sizeof (match), "pool-%u:%u", md->pp_id, index);
+
+      mrv = pp2_bpool_init (&bpool_params, &md->thread[i].bpool);
+      if (mrv < 0)
+	{
+	  log_err (dev, "pp2_bpool_init failed for bpool %u thread %u, err %d",
+		   index, i, mrv);
+	  rv = VNET_DEV_ERR_INIT_FAILED;
+	  goto done;
+	}
+      log_debug (dev, "pp2_bpool_init(bpool %u, thread %u) pool-%u:%u ok",
+		 index, i, md->thread[i].bpool->pp2_id,
+		 md->thread[i].bpool->id);
+      for (u32 j = 0; j < ARRAY_LEN (md->thread[0].bre); j++)
+	md->thread[i].bre[j].bpool = md->thread[i].bpool;
+    }
+
+done:
+  return rv;
+}
+
+static vnet_dev_rv_t
+mvpp2_init (vlib_main_t *vm, vnet_dev_t *dev)
+{
+  mvpp2_device_t *md = vnet_dev_get_data (dev);
+  vnet_dev_rv_t rv = VNET_DEV_OK;
+  vnet_dev_bus_platform_device_data_t *dd = vnet_dev_get_bus_data (dev);
+  clib_dt_node_t *sc;
+  int pp_id = -1;
+
+  if (!clib_dt_node_is_compatible (dd->node, "marvell,armada-7k-pp22"))
+    return VNET_DEV_ERR_NOT_SUPPORTED;
+
+  sc = clib_dt_dereference_node (dd->node, "marvell,system-controller");
+
+  if (sc && vec_len (sc->path) > strlen ("/cpX/"))
+    {
+      if (strncmp ((char *) sc->path, "/cp0/", 4) == 0)
+	pp_id = 0;
+      else if (strncmp ((char *) sc->path, "/cp1/", 4) == 0)
+	pp_id = 1;
+    }
+
+  if (pp_id < 0)
+    return VNET_DEV_ERR_UNKNOWN_DEVICE;
+
+  if ((mvpp2_global_init (vm, dev)) != VNET_DEV_OK)
+    return rv;
+
+  md->pp_id = pp_id;
+
+  vec_foreach_pointer (cn, dd->node->child_nodes)
+    {
+      clib_dt_property_t *p;
+      char netdev_name[IFNAMSIZ];
+      struct ifreq s = {};
+      u8 ppio_id;
+      int fd, srv;
+
+      p = clib_dt_get_node_property_by_name (cn, "port-id");
+
+      if (!clib_dt_proprerty_is_u32 (p))
+	continue;
+
+      ppio_id = clib_dt_proprerty_get_u32 (p);
+      log_debug (dev, "found port with ppio id %u", ppio_id);
+
+      if (pp2_ppio_available (md->pp_id, ppio_id) == 0)
+	continue;
+
+      if (pp2_netdev_get_ifname (md->pp_id, ppio_id, netdev_name) < 0)
+	{
+	  log_warn (dev, "failed to get ifname, skipping port %u ", ppio_id);
+	  continue;
+	}
+
+      srv = -1;
+      if ((fd = socket (PF_INET, SOCK_DGRAM, IPPROTO_IP)) >= 0)
+	{
+	  strcpy (s.ifr_name, netdev_name);
+	  srv = ioctl (fd, SIOCGIFHWADDR, &s);
+	  close (fd);
+	}
+
+      if (srv < 0)
+	{
+	  log_warn (dev, "unable to get hw address, skipping port %u",
+		    ppio_id);
+	  continue;
+	}
+
+      log_debug (dev, "adding ppio %u (netdev name %s, hwaddr %U)", ppio_id,
+		 netdev_name, format_ethernet_address, s.ifr_addr.sa_data);
+
+      mvpp2_port_t mvpp2_port = {
+	.ppio_id = ppio_id,
+      };
+
+      vnet_dev_port_add_args_t port_add_args = {
+        .port = {
+          .attr = {
+            .type = VNET_DEV_PORT_TYPE_ETHERNET,
+            .max_rx_queues = PP2_PPIO_MAX_NUM_INQS,
+            .max_tx_queues = PP2_PPIO_MAX_NUM_OUTQS,
+            .max_supported_rx_frame_size = 9216,
+          },
+          .ops = {
+            .init = mvpp2_port_init,
+            .deinit = mvpp2_port_deinit,
+            .start = mvpp2_port_start,
+            .stop = mvpp2_port_stop,
+            .config_change = mvpp2_port_cfg_change,
+            .config_change_validate = mvpp2_port_cfg_change_validate,
+            .format_status = format_mvpp2_port_status,
+          },
+          .data_size = sizeof (mvpp2_port_t),
+          .initial_data = &mvpp2_port,
+        },
+    .rx_node = &mvpp2_rx_node,
+    .tx_node = &mvpp2_tx_node,
+    .rx_queue = {
+      .config = {
+        .data_size = sizeof (mvpp2_rxq_t),
+        .default_size = 512,
+        .multiplier = 32,
+        .min_size = 32,
+        .max_size = 4096,
+	.size_is_power_of_two = 1,
+      },
+    },
+    .tx_queue = {
+      .config = {
+        .data_size = sizeof (mvpp2_txq_t),
+        .default_size = 512,
+        .multiplier = 32,
+        .min_size = 32,
+        .max_size = 4096,
+	.size_is_power_of_two = 1,
+          },
+      .ops = {
+	  .alloc = mvpp2_txq_alloc,
+	  .free = mvpp2_txq_free,
+      },
+        },
+      };
+
+      vnet_dev_set_hw_addr_eth_mac (&port_add_args.port.attr.hw_addr,
+				    (u8 *) s.ifr_addr.sa_data);
+
+      vnet_dev_port_add (vm, dev, ppio_id, &port_add_args);
+    }
+
+  if (rv != VNET_DEV_OK)
+    mvpp2_deinit (vm, dev);
+  return rv;
+}
+
+VNET_DEV_REGISTER_DRIVER (pp2) = {
+  .name = "mvpp2",
+  .bus = PLATFORM_BUS_NAME,
+  .device_data_sz = sizeof (mvpp2_device_t),
+  .ops = {
+    .init = mvpp2_init,
+    .deinit = mvpp2_deinit,
+    .probe = mvpp2_probe,
+    .format_info = format_mvpp2_dev_info,
+  },
+};

commit 623147bfdaf55ea14569209d14d429d675f56a5f

armada: fix tx queue init
Type: fix
Fixes: 4e51841
Change-Id: Ife3cb43f54cc0700c469f89b0a5af0ff44143807
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/plugins/dev_armada/pp2/port.c
@@ -46,11 +46,11 @@ mvpp2_port_init (vlib_main_t *vm, vnet_dev_port_t *port)
     },
   };
 
-  foreach_vnet_dev_port_rx_queue (q, port)
+  foreach_vnet_dev_port_tx_queue (q, port)
     {
       struct pp2_ppio_outqs_params *oqs = &ppio_params.outqs_params;
-      oqs->outqs_params[0].weight = 1;
-      oqs->outqs_params[0].size = q->size;
+      oqs->outqs_params[q->queue_id].weight = 1;
+      oqs->outqs_params[q->queue_id].size = q->size;
       oqs->num_outqs++;
     }
 