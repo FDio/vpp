c3e1a04384ca059e9f3b73e9e5d3f1b2dc03e615
Damjan Marion <damarion@cisco.com>
2024-10-08 20:51:15 +0200

    armada: initial DSA support
    Type: feature
    Change-Id: Ic6578e0ded33ca3c6590c8e6127ec11a0f5b3057
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- src/plugins/dev_armada/pp2/pp2.h
+++ src/plugins/dev_armada/pp2/pp2.h
@@ -8,6 +8,7 @@
 #include <vppinfra/clib.h>
 #include <vppinfra/error_bootstrap.h>
 #include <vppinfra/format.h>
+#include <vppinfra/devicetree.h>
 #include <vnet/vnet.h>
 #include <vnet/dev/dev.h>
 
@@ -29,6 +30,52 @@
 #define MVPP2_NUM_BPOOLS       16
 #define MVPP2_MAX_THREADS      4
 #define MRVL_PP2_BUFF_BATCH_SZ 32
+#define MV_DSA_N_SRC	       32
+
+#define foreach_mv_dsa_tag_field                                              \
+  _ (12, vid)                                                                 \
+  _ (1, _zero13)                                                              \
+  _ (3, pri)                                                                  \
+  _ (1, cfi_dei)                                                              \
+  _ (1, _unused17)                                                            \
+  _ (1, src_is_lag)                                                           \
+  _ (5, src_port_or_lag)                                                      \
+  _ (5, src_dev)                                                              \
+  _ (1, src_tagged)                                                           \
+  _ (2, tag_type)
+
+typedef enum
+{
+  MV_DSA_TAG_TYPE_TO_CPU = 0,
+  MV_DSA_TAG_TYPE_FROM_CPU = 1,
+  MV_DSA_TAG_TYPE_TO_SNIFFER = 2,
+  MV_DSA_TAG_TYPE_FORWARD = 3
+} mv_dsa_tag_type_t;
+
+typedef union
+{
+  struct
+  {
+#define _(b, n) u32 (n) : (b);
+    foreach_mv_dsa_tag_field
+#undef _
+  };
+  u32 as_u32;
+} mv_dsa_tag_t;
+
+STATIC_ASSERT_SIZEOF (mv_dsa_tag_t, 4);
+
+static_always_inline mv_dsa_tag_t
+mv_dsa_tag_read (void *p)
+{
+  return (mv_dsa_tag_t){ .as_u32 = clib_net_to_host_u32 (*(u32u *) p) };
+}
+
+static_always_inline void
+mv_dsa_tag_write (void *p, mv_dsa_tag_t tag)
+{
+  ((mv_dsa_tag_t *) p)->as_u32 = clib_host_to_net_u32 (tag.as_u32);
+}
 
 typedef struct
 {
@@ -49,6 +96,13 @@ typedef struct
   struct pp2_ppio *ppio;
   u8 ppio_id;
   struct pp2_ppio_link_info last_link_info;
+  clib_dt_node_t *switch_node;
+  clib_dt_node_t *switch_port_node;
+
+  struct pp2_ppio_desc descs[VLIB_FRAME_SIZE];
+  struct pp2_ppio_desc *desc_ptrs[VLIB_FRAME_SIZE];
+  u8 valid_dsa_src_bitmap;
+  u16 dsa_to_sec_if[MV_DSA_N_SRC];
 } mvpp2_port_t;
 
 typedef struct
@@ -65,7 +119,8 @@ typedef struct
 typedef struct
 {
   struct pp2_ppio_desc desc;
-  vnet_dev_rx_queue_t *rxq;
+  u32 sw_if_index;
+  u16 next_index;
 } mvpp2_rx_trace_t;
 
 /* format.c */
@@ -74,12 +129,15 @@ format_function_t format_mvpp2_port_status;
 format_function_t format_mvpp2_dev_info;
 format_function_t format_mvpp2_rx_trace;
 format_function_t format_mvpp2_rx_desc;
+format_function_t format_mv_dsa_tag;
 
 /* port.c */
 vnet_dev_port_op_t mvpp2_port_init;
 vnet_dev_port_op_no_rv_t mvpp2_port_deinit;
 vnet_dev_port_op_t mvpp2_port_start;
 vnet_dev_port_op_no_rv_t mvpp2_port_stop;
+vnet_dev_port_op_with_ptr_t mvpp2_port_add_sec_if;
+vnet_dev_port_op_with_ptr_t mvpp2_port_del_sec_if;
 vnet_dev_rv_t mvpp2_port_cfg_change (vlib_main_t *, vnet_dev_port_t *,
 				     vnet_dev_port_cfg_change_req_t *);
 vnet_dev_rv_t
@@ -128,6 +186,7 @@ typedef enum
      "pp2_bpool_get_num_buffs error")                                         \
   _ (BPOOL_PUT_BUFFS, bpool_put_buffs, ERROR, "pp2_bpool_put_buffs error")    \
   _ (BUFFER_ALLOC, buffer_alloc, ERROR, "buffer alloc error")                 \
+  _ (UNKNOWN_DSA_SRC, unknown_dsa_src, ERROR, "unknown DSA source")           \
   _ (MAC_CE, mac_ce, ERROR, "MAC error (CRC error)")                          \
   _ (MAC_OR, mac_or, ERROR, "overrun error")                                  \
   _ (MAC_RSVD, mac_rsvd, ERROR, "unknown MAC error")                          \
