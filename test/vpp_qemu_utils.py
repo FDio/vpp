41378de07b105ba6b83c9cfde040f6c21583e08a
Ivan Ivanets <iivanets@cisco.com>
2024-10-25 20:29:55 +0000

    misc: Test code to debug the CI. DO NOT MERGE!
    Type: style
    
    Change-Id: Iea3bae200c55022543f5370da397838d03393da2
    Signed-off-by: Ivan Ivanets <iivanets@cisco.com>

--- test/vpp_qemu_utils.py
+++ test/vpp_qemu_utils.py
@@ -1,308 +1,209 @@
-#!/usr/bin/env python
-
-# Utility functions for QEMU tests ##
-
 import subprocess
 import sys
 import os
-import multiprocessing as mp
+import time
+from multiprocessing import Lock, Process
 
+lock = Lock()
 
 def can_create_namespaces(namespace="vpp_chk_4212"):
     """Check if the environment allows creating the namespaces"""
-
-    try:
-        result = subprocess.run(["ip", "netns", "add", namespace], capture_output=True)
-        if result.returncode != 0:
+    with lock:
+        try:
+            result = subprocess.run(["ip", "netns", "add", namespace], capture_output=True)
+            if result.returncode != 0:
+                return False
+            subprocess.run(["ip", "netns", "del", namespace], capture_output=True)
+            return True
+        except Exception:
             return False
-        result = subprocess.run(["ip", "netns", "del", namespace], capture_output=True)
-        if result.returncode != 0:
-            return False
-        return True
-    except Exception:
-        return False
 
 
 def create_namespace(ns):
-    """create one or more namespaces.
-
-    arguments:
-    ns -- a string value or an iterable of namespace names
-    """
+    """Create one or more namespaces."""
     if isinstance(ns, str):
         namespaces = [ns]
     else:
         namespaces = ns
-    try:
+
+    with lock:
         for namespace in namespaces:
-            result = subprocess.run(["ip", "netns", "add", namespace])
-            if result.returncode != 0:
-                raise Exception(f"Error while creating namespace {namespace}")
-    except subprocess.CalledProcessError as e:
-        raise Exception("Error creating namespace:", e.output)
+            retries = 3
+            for attempt in range(retries):
+                result = subprocess.run(["ip", "netns", "add", namespace], capture_output=True)
+                if result.returncode == 0:
+                    break
+                if attempt < retries - 1:
+                    time.sleep(1)
+                else:
+                    raise Exception(f"Failed to create namespace {namespace} after {retries} attempts")
 
 
 def add_namespace_route(ns, prefix, gw_ip):
-    """Add a route to a namespace.
-
-    arguments:
-    ns -- namespace string value
-    prefix -- NETWORK/MASK or "default"
-    gw_ip -- Gateway IP
-    """
-    try:
-        subprocess.run(
-            ["ip", "netns", "exec", ns, "ip", "route", "add", prefix, "via", gw_ip],
-            capture_output=True,
-        )
-    except subprocess.CalledProcessError as e:
-        raise Exception("Error adding route to namespace:", e.output)
+    """Add a route to a namespace."""
+    with lock:
+        retries = 3
+        for attempt in range(retries):
+            result = subprocess.run(
+                ["ip", "netns", "exec", ns, "ip", "route", "add", prefix, "via", gw_ip],
+                capture_output=True,
+            )
+            if result.returncode == 0:
+                break
+            if attempt < retries - 1:
+                time.sleep(1)
+            else:
+                raise Exception(f"Failed to add route to namespace {ns} after {retries} attempts")
 
 
 def delete_host_interfaces(*host_interface_names):
-    """Delete host interfaces.
+    """Delete host interfaces."""
+    with lock:
+        for host_interface_name in host_interface_names:
+            retries = 3
+            for attempt in range(retries):
+                result = subprocess.run(["ip", "link", "del", host_interface_name], capture_output=True)
+                if result.returncode == 0:
+                    break
+                if attempt < retries - 1:
+                    time.sleep(1)
+                else:
+                    raise Exception(f"Failed to delete host interface {host_interface_name} after {retries} attempts")
 
-    arguments:
-    host_interface_names - sequence of host interface names to be deleted
-    """
-    for host_interface_name in host_interface_names:
-        try:
-            subprocess.run(
-                ["ip", "link", "del", host_interface_name], capture_output=True
-            )
-        except subprocess.CalledProcessError as e:
-            raise Exception("Error deleting host interface:", e.output)
 
-
-def create_host_interface(
-    host_interface_name, vpp_interface_name, host_namespace, *host_ip_prefixes
-):
-    """Create a host interface of type veth.
-
-    arguments:
-    host_interface_name -- name of the veth interface on the host side
-    vpp_interface_name -- name of the veth interface on the VPP side
-    host_namespace -- host namespace into which the host_interface needs to be set
-    host_ip_prefixes -- a sequence of ip/prefix-lengths to be set
-                        on the host_interface
-    """
-    try:
-        process = subprocess.run(
-            [
-                "ip",
-                "link",
-                "add",
-                "name",
-                vpp_interface_name,
-                "type",
-                "veth",
-                "peer",
-                "name",
-                host_interface_name,
-            ],
-            capture_output=True,
-        )
-        if process.returncode != 0:
-            print(f"Error creating host interface: {process.stderr}")
-            sys.exit(1)
-
-        process = subprocess.run(
-            ["ip", "link", "set", host_interface_name, "netns", host_namespace],
-            capture_output=True,
-        )
-        if process.returncode != 0:
-            print(f"Error setting host interface namespace: {process.stderr}")
-            sys.exit(1)
-
-        process = subprocess.run(
-            ["ip", "link", "set", "dev", vpp_interface_name, "up"], capture_output=True
-        )
-        if process.returncode != 0:
-            print(f"Error bringing up the host interface: {process.stderr}")
-            sys.exit(1)
-
-        process = subprocess.run(
-            [
-                "ip",
-                "netns",
-                "exec",
-                host_namespace,
-                "ip",
-                "link",
-                "set",
-                "dev",
-                host_interface_name,
-                "up",
-            ],
-            capture_output=True,
-        )
-        if process.returncode != 0:
-            print(
-                f"Error bringing up the host interface in namespace: "
-                f"{process.stderr}"
-            )
-            sys.exit(1)
-
-        for host_ip_prefix in host_ip_prefixes:
-            process = subprocess.run(
-                [
-                    "ip",
-                    "netns",
-                    "exec",
-                    host_namespace,
-                    "ip",
-                    "addr",
-                    "add",
-                    host_ip_prefix,
-                    "dev",
-                    host_interface_name,
-                ],
+def create_host_interface(host_interface_name, vpp_interface_name, host_namespace, *host_ip_prefixes):
+    """Create a host interface of type veth."""
+    with lock:
+        retries = 3
+        for attempt in range(retries):
+            result = subprocess.run(
+                ["ip", "link", "add", "name", vpp_interface_name, "type", "veth", "peer", "name", host_interface_name],
                 capture_output=True,
             )
-            if process.returncode != 0:
-                print(
-                    f"Error setting ip prefix on the host interface: "
-                    f"{process.stderr}"
-                )
-                sys.exit(1)
-    except subprocess.CalledProcessError as e:
-        raise Exception("Error adding route to namespace:", e.output)
+            if result.returncode == 0:
+                break
+            if attempt < retries - 1:
+                time.sleep(1)
+            else:
+                raise Exception(f"Failed to create host interface {host_interface_name} after {retries} attempts")
+
+        result = subprocess.run(["ip", "link", "set", host_interface_name, "netns", host_namespace], capture_output=True)
+        if result.returncode != 0:
+            raise Exception(f"Error setting host interface namespace: {result.stderr.decode()}")
+
+        result = subprocess.run(["ip", "link", "set", "dev", vpp_interface_name, "up"], capture_output=True)
+        if result.returncode != 0:
+            raise Exception(f"Error bringing up the host interface: {result.stderr.decode()}")
+
+        result = subprocess.run(
+            ["ip", "netns", "exec", host_namespace, "ip", "link", "set", "dev", host_interface_name, "up"],
+            capture_output=True,
+        )
+        if result.returncode != 0:
+            raise Exception(f"Error bringing up the host interface in namespace: {result.stderr.decode()}")
+
+        for host_ip_prefix in host_ip_prefixes:
+            result = subprocess.run(
+                ["ip", "netns", "exec", host_namespace, "ip", "addr", "add", host_ip_prefix, "dev", host_interface_name],
+                capture_output=True,
+            )
+            if result.returncode != 0:
+                raise Exception(f"Error setting ip prefix on the host interface: {result.stderr.decode()}")
 
 
 def set_interface_mtu(namespace, interface, mtu, logger):
-    """set an mtu number on a linux device interface."""
+    """Set an MTU number on a linux device interface."""
     args = ["ip", "link", "set", "mtu", str(mtu), "dev", interface]
     if namespace:
         args = ["ip", "netns", "exec", namespace] + args
-    try:
-        logger.debug(
-            f"Setting mtu:{mtu} on linux interface:{interface} "
-            f"in namespace:{namespace}"
-        )
-        subprocess.run(args)
-    except subprocess.CalledProcessError as e:
-        raise Exception("Error updating mtu:", e.output)
+    with lock:
+        retries = 3
+        for attempt in range(retries):
+            result = subprocess.run(args, capture_output=True)
+            if result.returncode == 0:
+                break
+            if attempt < retries - 1:
+                time.sleep(1)
+            else:
+                raise Exception(f"Failed to set MTU on interface {interface} in namespace {namespace} after {retries} attempts")
 
 
 def enable_interface_gso(namespace, interface):
-    """enable gso offload on a linux device interface."""
+    """Enable GSO offload on a linux device interface."""
     args = ["ethtool", "-K", interface, "rx", "on", "tx", "on"]
     if namespace:
         args = ["ip", "netns", "exec", namespace] + args
-    try:
-        process = subprocess.run(args, capture_output=True)
-        if process.returncode != 0:
-            print(
-                f"Error enabling GSO offload on linux device interface: "
-                f"{process.stderr}"
-            )
-            sys.exit(1)
-    except subprocess.CalledProcessError as e:
-        raise Exception("Error enabling gso:", e.output)
+    with lock:
+        result = subprocess.run(args, capture_output=True)
+        if result.returncode != 0:
+            raise Exception(f"Error enabling GSO offload on interface {interface} in namespace {namespace}: {result.stderr.decode()}")
 
 
 def disable_interface_gso(namespace, interface):
-    """disable gso offload on a linux device interface."""
+    """Disable GSO offload on a linux device interface."""
     args = ["ethtool", "-K", interface, "rx", "off", "tx", "off"]
     if namespace:
         args = ["ip", "netns", "exec", namespace] + args
-    try:
-        process = subprocess.run(args, capture_output=True)
-        if process.returncode != 0:
-            print(
-                f"Error disabling GSO offload on linux device interface: "
-                f"{process.stderr}"
-            )
-            sys.exit(1)
-    except subprocess.CalledProcessError as e:
-        raise Exception("Error disabling gso:", e.output)
+    with lock:
+        result = subprocess.run(args, capture_output=True)
+        if result.returncode != 0:
+            raise Exception(f"Error disabling GSO offload on interface {interface} in namespace {namespace}: {result.stderr.decode()}")
 
 
 def delete_namespace(ns):
-    """delete one or more namespaces.
-
-    arguments:
-    namespaces -- a list of namespace names
-    """
+    """Delete one or more namespaces."""
     if isinstance(ns, str):
         namespaces = [ns]
     else:
         namespaces = ns
-    try:
+    with lock:
         for namespace in namespaces:
-            result = subprocess.run(
-                ["ip", "netns", "del", namespace], capture_output=True
-            )
-            if result.returncode != 0:
-                raise Exception(f"Error while deleting namespace {namespace}")
-    except subprocess.CalledProcessError as e:
-        raise Exception("Error deleting namespace:", e.output)
+            retries = 3
+            for attempt in range(retries):
+                result = subprocess.run(["ip", "netns", "del", namespace], capture_output=True)
+                if result.returncode == 0:
+                    break
+                if attempt < retries - 1:
+                    time.sleep(1)
+                else:
+                    raise Exception(f"Failed to delete namespace {namespace} after {retries} attempts")
 
 
 def list_namespace(ns):
-    """List the IP address of a namespace"""
-    try:
-        subprocess.run(["ip", "netns", "exec", ns, "ip", "addr"])
-    except subprocess.CalledProcessError as e:
-        raise Exception("Error listing namespace IP:", e.output)
+    """List the IP address of a namespace."""
+    with lock:
+        result = subprocess.run(["ip", "netns", "exec", ns, "ip", "addr"], capture_output=True)
+        if result.returncode != 0:
+            raise Exception(f"Error listing IP addresses in namespace {ns}: {result.stderr.decode()}")
 
 
 def libmemif_test_app(memif_sock_path, logger):
     """Build & run the libmemif test_app for memif interface testing."""
     test_dir = os.path.dirname(os.path.realpath(__file__))
     ws_root = os.path.dirname(test_dir)
-    libmemif_app = os.path.join(
-        ws_root, "extras", "libmemif", "build", "examples", "test_app"
-    )
+    libmemif_app = os.path.join(ws_root, "extras", "libmemif", "build", "examples", "test_app")
 
     def build_libmemif_app():
         if not os.path.exists(libmemif_app):
-            print(f"Building app:{libmemif_app} for memif interface testing")
+            logger.info(f"Building app:{libmemif_app} for memif interface testing")
             libmemif_app_dir = os.path.join(ws_root, "extras", "libmemif", "build")
-            if not os.path.exists(libmemif_app_dir):
-                os.makedirs(libmemif_app_dir)
+            os.makedirs(libmemif_app_dir, exist_ok=True)
             os.chdir(libmemif_app_dir)
-            try:
-                p = subprocess.run(["cmake", ".."], capture_output=True)
-                logger.debug(p.stdout)
-                if p.returncode != 0:
-                    print(f"libmemif app:{libmemif_app} cmake error:{p.stderr}")
-                    sys.exit(1)
-                p = subprocess.run(["make"], capture_output=True)
-                logger.debug(p.stdout)
-                if p.returncode != 0:
-                    print(f"Error building libmemif app:{p.stderr}")
-                    sys.exit(1)
-            except subprocess.CalledProcessError as e:
-                raise Exception("Error building libmemif_test_app:", e.output)
+            subprocess.run(["cmake", ".."], check=True)
+            subprocess.run(["make"], check=True)
 
     def start_libmemif_app():
-        """Restart once if the initial run fails."""
         max_tries = 2
         run = 0
-        if not os.path.exists(libmemif_app):
-            raise Exception(
-                f"Error could not locate the libmemif test app:{libmemif_app}"
-            )
-        args = [libmemif_app, "-b", "9216", "-s", memif_sock_path]
         while run < max_tries:
-            try:
-                process = subprocess.run(args, capture_output=True)
-                logger.debug(process.stdout)
-                if process.returncode != 0:
-                    msg = f"Error starting libmemif app:{libmemif_app}"
-                    logger.error(msg)
-                    raise Exception(msg)
-            except Exception:
-                msg = f"re-starting libmemif app:{libmemif_app}"
-                logger.error(msg)
-                continue
-            else:
+            result = subprocess.run([libmemif_app, "-b", "9216", "-s", memif_sock_path], capture_output=True)
+            if result.returncode == 0:
                 break
-            finally:
-                run += 1
+            logger.error(f"Restarting libmemif app due to error: {result.stderr.decode()}")
+            run += 1
+            time.sleep(1)
 
     build_libmemif_app()
-    process = mp.Process(target=start_libmemif_app)
+    process = Process(target=start_libmemif_app)
     process.start()
     return process
