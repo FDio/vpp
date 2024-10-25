633ed2dd87ffde2b56271a7291f1044edccd5e9b
Ivan Ivanets <iivanets@cisco.com>
2024-10-25 20:29:55 +0000

    tests: vpp_qemu_utils with concurrency handling
    Type: test
    
    Enhance vpp_qemu_utils functions with mutex locking, handle namespace and
    host interface existence, unique namespace/interface name, error
    handling and retries, check error code explicitly.
    
    Change-Id: Iea3bae200c55022543f5370da397838d03393da2
    Signed-off-by: Ivan Ivanets <iivanets@cisco.com>

--- test/asf/test_http_static.py
+++ test/asf/test_http_static.py
@@ -3,11 +3,15 @@ from asfframework import VppAsfTestCase, VppTestRunner
 import unittest
 import subprocess
 import tempfile
+import random
+import string
 from vpp_qemu_utils import (
     create_host_interface,
     delete_host_interfaces,
     create_namespace,
     delete_namespace,
+    generate_unique_namespace_name,
+    generate_unique_host_interface_name,
 )
 
 
@@ -28,23 +32,23 @@ class TestHttpStaticVapi(VppAsfTestCase):
         cls.temp2 = tempfile.NamedTemporaryFile()
         cls.temp2.write(b"Hello world2")
 
-        try:
-            create_namespace("HttpStatic")
-        except Exception:
-            cls.logger.warning("Unable to create a namespace, retrying.")
-            delete_namespace("HttpStatic")
-            create_namespace("HttpStatic")
+        cls.ns_name = generate_unique_namespace_name()
+        cls.host_name = generate_unique_host_interface_name()
 
-        create_host_interface("vppHost", "vppOut", "HttpStatic", "10.10.1.1/24")
+        create_namespace(cls.ns_name)
 
-        cls.vapi.cli("create host-interface name vppOut")
-        cls.vapi.cli("set int state host-vppOut up")
-        cls.vapi.cli("set int ip address host-vppOut 10.10.1.2/24")
+        out_suffix = "".join(random.choices(string.digits, k=7))
+        out_name = f"vppOut{out_suffix}"
+        create_host_interface(cls.host_name, out_name, cls.ns_name, "10.10.1.1/24")
+
+        cls.vapi.cli(f"create host-interface name {out_name}")
+        cls.vapi.cli(f"set int state host-{out_name} up")
+        cls.vapi.cli(f"set int ip address host-{out_name} 10.10.1.2/24")
 
     @classmethod
     def tearDownClass(cls):
-        delete_namespace("HttpStatic")
-        delete_host_interfaces("vppHost")
+        delete_namespace(cls.ns_name)
+        delete_host_interfaces(cls.host_name)
         cls.temp.close()
         cls.temp2.close()
         super(TestHttpStaticVapi, cls).tearDownClass()
@@ -61,7 +65,7 @@ class TestHttpStaticVapi(VppAsfTestCase):
                 "ip",
                 "netns",
                 "exec",
-                "HttpStatic",
+                self.ns_name,
                 "curl",
                 "-v",
                 f"10.10.1.2/{self.temp.name[5:]}",
@@ -77,7 +81,7 @@ class TestHttpStaticVapi(VppAsfTestCase):
                 "ip",
                 "netns",
                 "exec",
-                "HttpStatic",
+                self.ns_name,
                 "curl",
                 f"10.10.1.2/{self.temp2.name[5:]}",
             ],
@@ -103,23 +107,23 @@ class TestHttpStaticCli(VppAsfTestCase):
         cls.temp2 = tempfile.NamedTemporaryFile()
         cls.temp2.write(b"Hello world2")
 
-        try:
-            create_namespace("HttpStatic2")
-        except Exception:
-            cls.logger.warning("Unable to create namespace, retrying.")
-            delete_namespace("HttpStatic2")
-            create_namespace("HttpStatic2")
+        cls.ns_name = generate_unique_namespace_name()
+        cls.host_name = generate_unique_host_interface_name()
 
-        create_host_interface("vppHost2", "vppOut2", "HttpStatic2", "10.10.1.1/24")
+        create_namespace(cls.ns_name)
 
-        cls.vapi.cli("create host-interface name vppOut2")
-        cls.vapi.cli("set int state host-vppOut2 up")
-        cls.vapi.cli("set int ip address host-vppOut2 10.10.1.2/24")
+        out_suffix = "".join(random.choices(string.digits, k=7))
+        out_name = f"vppOut2{out_suffix}"
+        create_host_interface(cls.host_name, out_name, cls.ns_name, "10.10.1.1/24")
+
+        cls.vapi.cli(f"create host-interface name {out_name}")
+        cls.vapi.cli(f"set int state host-{out_name} up")
+        cls.vapi.cli(f"set int ip address host-{out_name} 10.10.1.2/24")
 
     @classmethod
     def tearDownClass(cls):
-        delete_namespace("HttpStatic2")
-        delete_host_interfaces("vppHost2")
+        delete_namespace(cls.ns_name)
+        delete_host_interfaces(cls.host_name)
         cls.temp.close()
         cls.temp2.close()
         super(TestHttpStaticCli, cls).tearDownClass()
@@ -135,7 +139,7 @@ class TestHttpStaticCli(VppAsfTestCase):
                 "ip",
                 "netns",
                 "exec",
-                "HttpStatic2",
+                self.ns_name,
                 "curl",
                 f"10.10.1.2/{self.temp.name[5:]}",
             ],
@@ -149,7 +153,7 @@ class TestHttpStaticCli(VppAsfTestCase):
                 "ip",
                 "netns",
                 "exec",
-                "HttpStatic2",
+                self.ns_name,
                 "curl",
                 f"10.10.1.2/{self.temp2.name[5:]}",
             ],
