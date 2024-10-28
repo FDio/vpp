cdb3136fad5817a28bb07dc9ad64db8f6ae62abb
Matus Fabian <matfabia@cisco.com>
2024-10-28 10:30:16 +0100

    http_static: always free ext_cfg
    Type: fix
    
    Change-Id: I655e93c0f1e1de65d6d3c3e0976e25a7ce11aafc
    Signed-off-by: Matus Fabian <matfabia@cisco.com>

--- src/plugins/hs_apps/http_client_cli.c
+++ src/plugins/hs_apps/http_client_cli.c
@@ -381,6 +381,7 @@ hcc_connect_rpc (void *rpc_args)
   if (rv)
     clib_warning (0, "connect returned: %U", format_session_error, rv);
 
+  session_endpoint_free_ext_cfgs (&a->sep_ext);
   vec_free (a);
   return rv;
 }
