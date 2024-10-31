47ce1cd213e4b73d376d55e1db7c2e16bd0934dc
Ivan Ivanets <iivanets@cisco.com>
2024-10-31 18:55:27 +0000

    tests: vpp_qemu_utils with concurrency handling
    Type: test
    
    Enhance vpp_qemu_utils functions with mutex locking, handle namespace
    and
    host interface existence, unique namespace/interface name, error
    handling and retries, check error code explicitly.
    
    Change-Id: I1ea66eeefbc1fee9b58e8b9886f4dd6fd8d33444
    Signed-off-by: Ivan Ivanets <iivanets@cisco.com>

--- test/asf/test_http_static.py
+++ test/asf/test_http_static.py
@@ -29,22 +29,22 @@ class TestHttpStaticVapi(VppAsfTestCase):
         cls.temp2.write(b"Hello world2")
 
         try:
-            create_namespace("HttpStatic")
+            create_namespace("HttpStatic1")
         except Exception:
             cls.logger.warning("Unable to create a namespace, retrying.")
-            delete_namespace("HttpStatic")
-            create_namespace("HttpStatic")
+            delete_namespace("HttpStatic1")
+            create_namespace("HttpStatic1")
 
-        create_host_interface("vppHost", "vppOut", "HttpStatic", "10.10.1.1/24")
+        create_host_interface("vppHost1", "vppOut1", "HttpStatic1", "10.10.1.1/24")
 
-        cls.vapi.cli("create host-interface name vppOut")
-        cls.vapi.cli("set int state host-vppOut up")
-        cls.vapi.cli("set int ip address host-vppOut 10.10.1.2/24")
+        cls.vapi.cli("create host-interface name vppOut1")
+        cls.vapi.cli("set int state host-vppOut1 up")
+        cls.vapi.cli("set int ip address host-vppOut1 10.10.1.2/24")
 
     @classmethod
     def tearDownClass(cls):
-        delete_namespace("HttpStatic")
-        delete_host_interfaces("vppHost")
+        delete_namespace("HttpStatic1")
+        delete_host_interfaces("vppHost1")
         cls.temp.close()
         cls.temp2.close()
         super(TestHttpStaticVapi, cls).tearDownClass()
@@ -61,7 +61,7 @@ class TestHttpStaticVapi(VppAsfTestCase):
                 "ip",
                 "netns",
                 "exec",
-                "HttpStatic",
+                "HttpStatic1",
                 "curl",
                 "-v",
                 f"10.10.1.2/{self.temp.name[5:]}",
@@ -77,7 +77,7 @@ class TestHttpStaticVapi(VppAsfTestCase):
                 "ip",
                 "netns",
                 "exec",
-                "HttpStatic",
+                "HttpStatic1",
                 "curl",
                 f"10.10.1.2/{self.temp2.name[5:]}",
             ],
