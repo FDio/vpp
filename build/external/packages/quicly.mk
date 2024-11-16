435bce0c2a1d5acf488a3fd4a2f96175ab10449d
Dave Wallace <dwallacelf@gmail.com>
2024-11-16 01:26:42 -0500

    quic: update to quicly version 0.1.5
    Type: improvement
    
    Change-Id: I7c9c91a9c4d3ac2499e42eedda0a5d395bdf5912
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- build/external/packages/quicly.mk
+++ build/external/packages/quicly.mk
@@ -11,9 +11,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-quicly_version := 0.1.4-vpp
+quicly_version := 0.1.5-vpp
 quicly_tarball := quicly_$(quicly_version).tar.gz
-quicly_tarball_md5sum := 5419a0f340c5572bcf682c08acc84c77
+quicly_tarball_md5sum := fc388ccde2f7d677b92ff3cb3fd387e4
 quicly_tarball_strip_dirs := 1
 quicly_url := https://github.com/vpp-quic/quicly/releases/download/v$(quicly_version)/quicly_$(quicly_version).tar.gz
 
