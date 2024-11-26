573a1f2ef82bfdc63ac3c6931761c6c47b1ea838
Klement Sekera <klement.sekera@gmail.com>
2024-11-26 15:42:41 +0100

    ip: fix ICMP inner payload parsing
    Add a check so that ICMP type is verified to be an error before parsing
    inner payload. If it's not an error, then the inner payload is not there.
    
    Type: fix
    Fixes: 46d0ff3945
    Change-Id: I5c7d8ddacb347ec030784f349064e66d63cd525e
    Signed-off-by: Klement Sekera <klement.sekera@gmail.com>

--- src/plugins/nat/nat44-ei/nat44_ei_in2out.c
+++ src/plugins/nat/nat44-ei/nat44_ei_in2out.c
@@ -21,6 +21,7 @@
 
 #include <vnet/vnet.h>
 #include <vnet/ip/ip.h>
+#include <vnet/ip/ip4_to_ip6.h>
 #include <vnet/ethernet/ethernet.h>
 #include <vnet/udp/udp_local.h>
 #include <vnet/fib/ip4_fib.h>
