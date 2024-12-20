87d82d01db04a9922fdef275b6b9fd7c2d80c746
Monendra Singh Kushwaha <kmonendra@marvell.com>
2024-12-20 23:26:43 +0530

    octeon: fix compilation for octeon
    This patch adapts new changes introduced in crypto framework
    as part of 0cf4eef7.
    
    Type: fix
    fixes: 0cf4eef7
    
    Change-Id: I41a17c9b340f54014c9d2ae21546ec39ccef3f43
    Signed-off-by: Monendra Singh Kushwaha <kmonendra@marvell.com>

--- src/plugins/dev_octeon/crypto.h
+++ src/plugins/dev_octeon/crypto.h
@@ -157,7 +157,7 @@ void oct_crypto_key_del_handler (vlib_main_t *vm,
 void oct_crypto_key_add_handler (vlib_main_t *vm,
 				 vnet_crypto_key_index_t key_index);
 
-void oct_crypto_key_handler (vlib_main_t *vm, vnet_crypto_key_op_t kop,
+void oct_crypto_key_handler (vnet_crypto_key_op_t kop,
 			     vnet_crypto_key_index_t idx);
 
 int oct_crypto_enqueue_linked_alg_enc (vlib_main_t *vm,
