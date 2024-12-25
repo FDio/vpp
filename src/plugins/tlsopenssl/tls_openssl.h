a902166e2140fdea4933f94006d11ced24a27f06
Varun Rapelly <vrapelly@marvell.com>
2024-12-25 06:49:46 +0100

    tls:async event handling enhancement
    This patch updates async processing logic.
    
    Type: enhancement
    
    Change-Id: I6366689fec7e29fa9850cb22e20ab3659bf5245a
    Signed-off-by: Varun Rapelly <vrapelly@marvell.com>

--- src/plugins/tlsopenssl/tls_openssl.h
+++ src/plugins/tlsopenssl/tls_openssl.h
@@ -40,6 +40,7 @@ typedef struct tls_ctx_openssl_
   SSL_CTX *client_ssl_ctx;
   SSL *ssl;
   u32 evt_index[SSL_ASYNC_EVT_MAX];
+  bool evt_alloc_flag[SSL_ASYNC_EVT_MAX];
   u32 total_async_write;
   BIO *rbio;
   BIO *wbio;
@@ -74,13 +75,13 @@ typedef struct openssl_main_
   u32 max_pipelines;
 } openssl_main_t;
 
-typedef int openssl_resume_handler (tls_ctx_t * ctx, session_t * tls_session);
+typedef int openssl_resume_handler (void *event, void *session);
+typedef int (*async_handlers) (void *event, void *session);
 
 tls_ctx_t *openssl_ctx_get_w_thread (u32 ctx_index, u8 thread_index);
-int vpp_tls_async_init_events (tls_ctx_t *ctx, openssl_resume_handler *handler,
-			       session_t *session);
-int vpp_tls_async_update_event (tls_ctx_t *ctx, int eagain,
-				ssl_async_evt_type_t type);
+int vpp_tls_async_init_event (tls_ctx_t *ctx, openssl_resume_handler *handler,
+			      transport_send_params_t *sp, session_t *session,
+			      ssl_async_evt_type_t evt_type, int wr_size);
 int vpp_tls_async_enqueue_event (openssl_ctx_t *ctx, int evt_type,
 				 transport_send_params_t *sp, int size);
 int tls_async_openssl_callback (SSL * s, void *evt);
@@ -91,11 +92,13 @@ void openssl_async_node_enable_disable (u8 is_en);
 clib_error_t *tls_openssl_api_init (vlib_main_t * vm);
 int tls_openssl_set_ciphers (char *ciphers);
 int vpp_openssl_is_inflight (tls_ctx_t * ctx);
-int openssl_read_from_ssl_into_fifo (svm_fifo_t *f, tls_ctx_t *ctx,
-				     u32 max_len);
+int openssl_read_from_ssl_into_fifo (svm_fifo_t *f, SSL *ssl);
 void openssl_handle_handshake_failure (tls_ctx_t *ctx);
 void openssl_confirm_app_close (tls_ctx_t *ctx);
 
+int tls_async_write_event_handler (void *event, void *session);
+int tls_async_read_event_handler (void *event, void *session);
+int tls_async_handshake_event_handler (void *event, void *session);
 #endif /* SRC_PLUGINS_TLSOPENSSL_TLS_OPENSSL_H_ */
 
 /*
