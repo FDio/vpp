6db9a1e6120da9315a8ec522710f7c9aee419f28
Vladimir Smirnov <civil.over@gmail.com>
2025-01-03 21:49:25 +0100

    build: Add VLIB_MAX_NELTS configure option
    Type: feature
    
    Add VLIB_MAX_NELTS configure time option, that allows
    to speicfy MAX_NELTS value, that is one of the factors
    limiting max amount of workers VPP can safely run.
    
    Set it by default to 64, as increasing the number requires
    increasing the heap and overall increased memory consumption.
    
    Increasing amount of workers further would require more work,
    as I keep encountering 'thread-local data' oom from glibc if
    I go too far above 128 threads.
    
    Change-Id: Iddfddce71d3c1811bbb962a7ab014802b9ed72a4
    Signed-off-by: Vladimir Smirnov <civil.over@gmail.com>

--- build-data/packages/vpp.mk
+++ build-data/packages/vpp.mk
@@ -33,6 +33,9 @@ endif
 ifneq ($(VPP_PLATFORM),)
 vpp_cmake_args += -DVPP_PLATFORM="$(VPP_PLATFORM)"
 endif
+ifneq ($(VPP_MAX_WORKERS),)
+vpp_cmake_args += -DVPP_MAX_WORKERS=$(VPP_MAX_WORKERS)
+endif
 ifneq ($(VPP_EXCLUDED_PLUGINS),)
 vpp_cmake_args += -DVPP_EXCLUDED_PLUGINS="$(VPP_EXCLUDED_PLUGINS)"
 endif
