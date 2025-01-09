f21d062fc4c55045dd271ab71c60ea819d2a4919
Vladimir Smirnov <civil.over@gmail.com>
2025-01-09 20:32:27 +0100

    dpdk: update rdma-core to 55.0
    That is not really required for DPDK 24.11.1, but
    newer version of RDMA contains some bugfixes.
    
    Type: feature
    
    Change-Id: I4cd6bc7608bbadd99cdddc7b97646df1583b6ce6
    Signed-off-by: Vladimir Smirnov <civil.over@gmail.com>

--- build/external/packages/rdma-core.mk
+++ build/external/packages/rdma-core.mk
@@ -23,9 +23,9 @@ RDMA_CORE_DEBUG?=n
 #       	2. Verify that the file build/external/dpdk_mlx_default.sh was generated
 #              and contains 'DPDK_MLX_DEFAULT=y'
 #
-rdma-core_version             := 52.0
+rdma-core_version             := 55.0
 rdma-core_tarball             := rdma-core-$(rdma-core_version).tar.gz
-rdma-core_tarball_sha256sum_52.0 := 1f0ce5f2462c982b20d21156707076278807a7adf4d10e9142f3be4bec1b2b83
+rdma-core_tarball_sha256sum_55.0 := 6f8b97267807cdae54845f542ee3d75de80fdc24fe2632f5db1573ecef132d0f
 rdma-core_tarball_sha256sum      := $(rdma-core_tarball_sha256sum_$(rdma-core_version))
 rdma-core_tarball_strip_dirs  := 1
 rdma-core_url                 := http://github.com/linux-rdma/rdma-core/releases/download/v$(rdma-core_version)/$(rdma-core_tarball)
