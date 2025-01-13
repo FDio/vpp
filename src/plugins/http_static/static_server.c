826490a7edc6358614ba56fa96cebf40475f064c
Semir Sionek <ssionek@cisco.com>
2025-01-13 09:00:40 -0500

    http_static: request body handling improvement
    The request body is now ignored for GET requests and a new configurable
    parameter has been introduced - max-body-size.
    It limits the requests' body size to a certain number of bytes for POST
    requests (512M by default). Any larger request is rejected with a 413.
    
    Type: improvement
    Change-Id: Ia97325e06d76a17f8a6b8f0a0d720560cd1eeb36
    Signed-off-by: Semir Sionek <ssionek@cisco.com>

--- src/plugins/http_static/static_server.c
+++ src/plugins/http_static/static_server.c
@@ -520,6 +520,7 @@ handle_request (hss_session_t *hs, http_req_method_t rt, u8 *target_path,
 static int
 hss_ts_rx_callback (session_t *ts)
 {
+  hss_main_t *hsm = &hss_main;
   hss_session_t *hs;
   u8 *target_path = 0, *target_query = 0, *data = 0;
   http_msg_t msg;
@@ -583,12 +584,20 @@ hss_ts_rx_callback (session_t *ts)
     }
 
   /* Read body */
-  if (msg.data.body_len)
+  if (msg.data.body_len && msg.method_type == HTTP_REQ_POST)
     {
-      vec_validate (data, msg.data.body_len - 1);
-      rv = svm_fifo_peek (ts->rx_fifo, msg.data.body_offset, msg.data.body_len,
-			  data);
-      ASSERT (rv == msg.data.body_len);
+      if (msg.data.body_len <= hsm->max_body_size)
+	{
+	  vec_validate (data, msg.data.body_len - 1);
+	  rv = svm_fifo_peek (ts->rx_fifo, msg.data.body_offset,
+			      msg.data.body_len, data);
+	  ASSERT (rv == msg.data.body_len);
+	}
+      else
+	{
+	  start_send_data (hs, HTTP_STATUS_CONTENT_TOO_LARGE);
+	  goto done;
+	}
     }
 
   /* Find and send data */
@@ -906,6 +915,7 @@ hss_create_command_fn (vlib_main_t *vm, unformat_input_t *input,
   hsm->fifo_size = 0;
   hsm->cache_size = 10 << 20;
   hsm->max_age = HSS_DEFAULT_MAX_AGE;
+  hsm->max_body_size = HSS_DEFAULT_MAX_BODY_SIZE;
   hsm->keepalive_timeout = HSS_DEFAULT_KEEPALIVE_TIMEOUT;
 
   /* Get a line of input. */
@@ -942,6 +952,8 @@ hss_create_command_fn (vlib_main_t *vm, unformat_input_t *input,
       else if (unformat (line_input, "url-handlers"))
 	hsm->enable_url_handlers = 1;
       else if (unformat (line_input, "max-age %d", &hsm->max_age))
+	;
+      else if (unformat (line_input, "max-body-size %lu", &hsm->max_body_size))
 	;
       else
 	{
