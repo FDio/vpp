de58477afe9e2a7d97df69f26333a65b3cebafeb
Guillaume Solignac <gsoligna@cisco.com>
2025-01-15 10:00:44 +0100

    build: use mcpu for custom platforms
    When using VPP_PLATFORM, we can specify specify mcpu to leverage the
    compiler tuning.
    
    Change-Id: If9fbd33cd4e3cb5844c1be86233fbc3d6591d6cd
    Signed-off-by: Guillaume Solignac <gsoligna@cisco.com>

--- src/cmake/platform/octeon10.cmake
+++ src/cmake/platform/octeon10.cmake
@@ -1,4 +1,4 @@
 
 set(VPP_PLATFORM_CACHE_LINE_SIZE 64)
-set(VPP_PLATFORM_MARCH_FLAGS -march=armv8.3-a+crypto+sve2-bitperm)
+set(VPP_PLATFORM_MARCH_FLAGS -mcpu=neoverse-n2)
 set(VPP_PLATFORM_BUFFER_ALIGN 128)
