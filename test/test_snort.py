FDio/vpp

commit 102575492c9199259aa5e468f21b46936d7a1ac4

snort: support multiple instances per interface
Implements load balancing between snort instances via flow hash.
New CLI commands have been made to support these changes:

snort attach instance <name1>
    [instance <name2> ... ] interface <ifname> [input|output|inout]
snort attach all-instances interface <ifname> [input|output|inout]
snort detach instance <name1> interface <ifname>
snort detach all-instances interface <ifname>

The output of "show snort interfaces" has an extra column to show the
direction of each attachment:

interface   instances   direction
Ethernet0:  snort1      inout
            snort2      inout
            snort3      inout
Ethernet1:  snort1      input
            snort3      output

To maintain backwards compatibility for the snort api, the
snort_interface_get api endpoint only returns one of the attached
instances and the snort_interface_detach endpoint detaches all
attached instances.

Type: improvement
Signed-off-by: Agathiyan Bragadeesh <agathiyan.bragadeesh2@arm.com>
Change-Id: I6b7c26c203496d6a1dba244620907f28c04bb478

src/plugins/snort/snort_api.c
@@ -185,7 +185,8 @@ vl_api_snort_interface_get_t_handler (vl_api_snort_interface_get_t *mp)
   snort_main_t *sm = snort_get_main ();
   vl_api_snort_interface_get_reply_t *rmp;
   u32 sw_if_index;
-  u32 *index;
+  u32 *instances;
+  u32 index;
   int rv = 0;
 
   sw_if_index = clib_net_to_host_u32 (mp->sw_if_index);
@@ -193,7 +194,7 @@ vl_api_snort_interface_get_t_handler (vl_api_snort_interface_get_t *mp)
   if (sw_if_index == INDEX_INVALID)
     {
       /* clang-format off */
-      if (vec_len (sm->instance_by_sw_if_index) == 0)
+      if (vec_len (sm->interfaces) == 0)
 	{
 	  REPLY_MACRO2 (VL_API_SNORT_INTERFACE_GET_REPLY, ({ rmp->cursor = ~0; }));
 	  return;
@@ -201,17 +202,36 @@ vl_api_snort_interface_get_t_handler (vl_api_snort_interface_get_t *mp)
 
       REPLY_AND_DETAILS_VEC_MACRO(
 	VL_API_SNORT_INTERFACE_GET_REPLY,
-	sm->instance_by_sw_if_index,
+	sm->interfaces,
 	mp, rmp, rv, ({
-          index = vec_elt_at_index (sm->instance_by_sw_if_index, cursor);
-          send_snort_interface_details (cursor, *index, rp, mp->context);
+          instances = vec_len(sm->interfaces[cursor].input_instance_indices) ?
+           sm->interfaces[cursor].input_instance_indices : sm->interfaces[cursor].output_instance_indices;
+          if (vec_len(instances) == 0)
+          {
+            index = ~0;
+          }
+          else {
+            index = instances[0];
+          }
+          send_snort_interface_details (cursor, index, rp, mp->context);
 	}))
       /* clang-format on */
     }
   else
     {
-      index = vec_elt_at_index (sm->instance_by_sw_if_index, sw_if_index);
-      if (snort_get_instance_by_index (index[0]))
+      instances =
+	vec_len (sm->interfaces[sw_if_index].input_instance_indices) ?
+	  sm->interfaces[sw_if_index].input_instance_indices :
+	  sm->interfaces[sw_if_index].output_instance_indices;
+      if (vec_len (instances) == 0)
+	{
+	  index = ~0;
+	}
+      else
+	{
+	  index = instances[0];
+	}
+      if (snort_get_instance_by_index (index))
 	{
 	  vl_api_registration_t *rp =
 	    vl_api_client_index_to_registration (mp->client_index);
@@ -221,7 +241,8 @@ vl_api_snort_interface_get_t_handler (vl_api_snort_interface_get_t *mp)
 	      return;
 	    }
 
-	  send_snort_interface_details (sw_if_index, *index, rp, mp->context);
+	  send_snort_interface_details (sw_if_index, *instances, rp,
+					mp->context);
 	}
       else
 	{
@@ -352,11 +373,9 @@ vl_api_snort_interface_detach_t_handler (vl_api_snort_interface_detach_t *mp)
   vlib_main_t *vm = vlib_get_main ();
   vl_api_snort_interface_detach_reply_t *rmp;
   u32 sw_if_index = clib_net_to_host_u32 (mp->sw_if_index);
-  int rv = VNET_API_ERROR_NO_MATCHING_INTERFACE;
+  int rv;
 
-  if (sw_if_index != INDEX_INVALID)
-    rv = snort_interface_enable_disable (vm, NULL, sw_if_index,
-					 0 /* is_enable */, SNORT_INOUT);
+  rv = snort_interface_disable_all (vm, sw_if_index);
 
   REPLY_MACRO (VL_API_SNORT_INTERFACE_DETACH_REPLY);
 }

test/test_snort.py
@@ -12,10 +12,10 @@ class TestSnort(VppTestCase):
     def setUpClass(cls):
         super(TestSnort, cls).setUpClass()
         try:
-            cls.create_pg_interfaces(range(2))
+            cls.create_pg_interfaces(range(4))
             for i in cls.pg_interfaces:
                 i.config_ip4().resolve_arp()
-                i.admin_up()
+                i.admin_down()
         except Exception:
             cls.tearDownClass()
             raise
@@ -24,7 +24,6 @@ class TestSnort(VppTestCase):
     def tearDownClass(cls):
         for i in cls.pg_interfaces:
             i.unconfig_ip4()
-            i.admin_down()
         super(TestSnort, cls).tearDownClass()
 
     def test_snort_cli(self):
@@ -36,14 +35,18 @@ class TestSnort(VppTestCase):
             "snort create-instance name snortTest2 queue-size 16 on-disconnect pass": "",
             "snort attach instance snortTest interface pg0 output": "",
             "snort attach instance snortTest2 interface pg1 input": "",
+            "snort attach all-instances interface pg2 inout": "",
+            "snort attach instance snortTest instance snortTest2 interface pg3 inout": "",
             "show snort instances": "snortTest",
             "show snort interfaces": "pg0",
             "show snort clients": "number of clients",
             "show snort mode": "input mode: interrupt",
             "snort mode polling": "",
             "snort mode interrupt": "",
-            "snort detach interface pg0": "",
-            "snort detach interface pg1": "",
+            "snort detach instance snortTest interface pg0": "",
+            "snort detach instance snortTest2 interface pg1": "",
+            "snort detach all-instances interface pg2": "",
+            "snort detach instance snortTest instance snortTest2 interface pg3": "",
             "snort delete instance snortTest": "",
         }
 
@@ -64,7 +67,7 @@ class TestSnortVapi(VppTestCase):
             for i in cls.pg_interfaces:
                 i.config_ip4()
                 i.resolve_arp()
-                i.admin_up()
+                i.admin_down()
         except Exception:
             cls.tearDownClass()
             raise
@@ -73,7 +76,6 @@ class TestSnortVapi(VppTestCase):
     def tearDownClass(cls):
         for i in cls.pg_interfaces:
             i.unconfig_ip4()
-            i.admin_down()
         super(TestSnortVapi, cls).tearDownClass()
 
     def test_snort_01_modes_set_interrupt(self):
@@ -109,20 +111,27 @@ class TestSnortVapi(VppTestCase):
         reply = self.vapi.snort_interface_attach(
             instance_index=0, sw_if_index=1, snort_dir=1
         )
-        try:
-            reply = self.vapi.snort_interface_attach(
-                instance_index=1, sw_if_index=1, snort_dir=1
-            )
-        except:
-            pass
-        else:
-            self.assertNotEqual(reply.retval, 0)
-
+        reply = self.vapi.snort_interface_attach(
+            instance_index=0, sw_if_index=2, snort_dir=2
+        )
         reply = self.vapi.snort_interface_attach(
             instance_index=1, sw_if_index=2, snort_dir=3
         )
         reply = self.vapi.cli("show snort interfaces")
         self.assertIn("snortTest0", reply)
+        self.assertIn("snortTest1", reply)
+        self.assertIn("input", reply)
+        self.assertIn("inout", reply)
+        self.assertIn("output", reply)
+        try:
+            reply = self.vapi.snort_interface_attach(
+                instance_index=1, sw_if_index=2, snort_dir=2
+            )
+        except:
+            pass
+        else:
+            self.assertNotEqual(reply.retval, 0)
+        reply = self.vapi.cli("show snort interfaces")
         self.assertIn("snortTest1", reply)
 
     def test_snort_05_delete_instance(self):
@@ -131,14 +140,13 @@ class TestSnortVapi(VppTestCase):
         reply = self.vapi.cli("show snort interfaces")
         self.assertNotIn("snortTest0", reply)
         self.assertIn("snortTest1", reply)
-        reply = self.vapi.cli("show snort interfaces")
         self.assertNotIn("pg0", reply)
         self.assertIn("pg1", reply)
 
     def test_snort_06_detach_if(self):
         """Interfaces can be detached"""
         try:
-            reply = self.vapi.snort_interface_detach(sw_if_index=1)
+            reply = self.vapi.snort_interface_detach(sw_if_index=3)
         except:
             pass
         else:

src/plugins/snort/cli.c
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: Apache-2.0
  * Copyright(c) 2021 Cisco Systems, Inc.
+ * Copyright(c) 2024 Arm Limited
  */
 
 #include <vlib/vlib.h>
@@ -14,6 +15,70 @@ format_snort_instance (u8 *s, va_list *args)
 	      i->shm_fd);
 
   return s;
+}
+
+static clib_error_t *
+snort_attach_detach_instance (vlib_main_t *vm, vnet_main_t *vnm,
+			      char *instance_name, u32 sw_if_index,
+			      int is_enable, snort_attach_dir_t dir)
+{
+  clib_error_t *err = NULL;
+  int rv = snort_interface_enable_disable (vm, instance_name, sw_if_index,
+					   is_enable, dir);
+  switch (rv)
+    {
+    case 0:
+      break;
+    case VNET_API_ERROR_FEATURE_ALREADY_ENABLED:
+      /* already attached to same instance */
+      break;
+    case VNET_API_ERROR_INVALID_INTERFACE:
+      err = clib_error_return (0,
+			       "interface %U is not assigned to snort "
+			       "instance %s!",
+			       format_vnet_sw_if_index_name, vnm, sw_if_index,
+			       instance_name);
+      break;
+    case VNET_API_ERROR_NO_SUCH_ENTRY:
+      err = clib_error_return (0, "unknown instance '%s'", instance_name);
+      break;
+    case VNET_API_ERROR_INSTANCE_IN_USE:
+      err = clib_error_return (
+	0, "interface %U is currently up, set state down first",
+	format_vnet_sw_if_index_name, vnm, sw_if_index);
+      break;
+    default:
+      err = clib_error_return (0, "snort_interface_enable_disable returned %d",
+			       rv);
+      break;
+    }
+  return err;
+}
+
+static clib_error_t *
+snort_detach_all_instance (vlib_main_t *vm, vnet_main_t *vnm, u32 sw_if_index)
+{
+  clib_error_t *err = NULL;
+  int rv = snort_interface_disable_all (vm, sw_if_index);
+  switch (rv)
+    {
+    case 0:
+      break;
+    case VNET_API_ERROR_INSTANCE_IN_USE:
+      err = clib_error_return (
+	0, "interface %U is currently up, set state down first",
+	format_vnet_sw_if_index_name, vnm, sw_if_index);
+      break;
+    case VNET_API_ERROR_INVALID_INTERFACE:
+      err = clib_error_return (0, "interface %U has no attached instances",
+			       format_vnet_sw_if_index_name, vnm, sw_if_index);
+      break;
+    default:
+      err =
+	clib_error_return (0, "snort_interface_disable_all returned %d", rv);
+      break;
+    }
+  return err;
 }
 
 static clib_error_t *
@@ -94,7 +159,7 @@ done:
 
 VLIB_CLI_COMMAND (snort_create_instance_command, static) = {
   .path = "snort create-instance",
-  .short_help = "snort create-instaince name <name> [queue-size <size>] "
+  .short_help = "snort create-instance name <name> [queue-size <size>] "
 		"[on-disconnect drop|pass]",
   .function = snort_create_instance_command_fn,
 };
@@ -217,11 +282,15 @@ snort_attach_command_fn (vlib_main_t *vm, unformat_input_t *input,
 {
   unformat_input_t _line_input, *line_input = &_line_input;
   vnet_main_t *vnm = vnet_get_main ();
-  clib_error_t *err = 0;
-  u8 *name = 0;
+  snort_main_t *sm = &snort_main;
+  snort_instance_t *si;
+  clib_error_t *err = NULL;
+  u8 *name = NULL;
+  u8 **names = NULL;
   u32 sw_if_index = ~0;
-  snort_attach_dir_t dir = SNORT_INOUT;
-  int rv = 0;
+  snort_attach_dir_t direction = SNORT_INOUT;
+  u8 is_all_instances = 0;
+  int i;
 
   /* Get a line of input. */
   if (!unformat_user (input, unformat_line_input, line_input))
@@ -233,13 +302,15 @@ snort_attach_command_fn (vlib_main_t *vm, unformat_input_t *input,
 		    vnm, &sw_if_index))
 	;
       else if (unformat (line_input, "instance %s", &name))
-	;
+	vec_add1 (names, name);
+      else if (unformat (line_input, "all-instances"))
+	is_all_instances = 1;
       else if (unformat (line_input, "input"))
-	dir = SNORT_INPUT;
+	direction = SNORT_INPUT;
       else if (unformat (line_input, "output"))
-	dir = SNORT_OUTPUT;
+	direction = SNORT_OUTPUT;
       else if (unformat (line_input, "inout"))
-	dir = SNORT_INOUT;
+	direction = SNORT_INOUT;
       else
 	{
 	  err = clib_error_return (0, "unknown input `%U'",
@@ -254,46 +325,53 @@ snort_attach_command_fn (vlib_main_t *vm, unformat_input_t *input,
       goto done;
     }
 
-  if (!name)
+  if (vec_len (names) == 0 && is_all_instances == 0)
     {
-      err = clib_error_return (0, "please specify instance name");
+      err = clib_error_return (0, "please specify instances");
       goto done;
     }
 
-  rv = snort_interface_enable_disable (vm, (char *) name, sw_if_index, 1, dir);
-
-  switch (rv)
+  if (is_all_instances)
     {
-    case 0:
-      break;
-    case VNET_API_ERROR_FEATURE_ALREADY_ENABLED:
-      /* already attached to same instance */
-      break;
-    case VNET_API_ERROR_INSTANCE_IN_USE:
-      err = clib_error_return (0,
-			       "interface %U already assigned to "
-			       "an instance",
-			       format_vnet_sw_if_index_name, vnm, sw_if_index);
-      break;
-    case VNET_API_ERROR_NO_SUCH_ENTRY:
-      err = clib_error_return (0, "unknown instance '%s'", name);
-      break;
-    default:
-      err = clib_error_return (0, "snort_interface_enable_disable returned %d",
-			       rv);
-      break;
+      if (vec_len (sm->instances) == 0)
+	{
+	  err = clib_error_return (0, "no snort instances have been created");
+	  goto done;
+	}
+
+      pool_foreach (si, sm->instances)
+	{
+	  snort_attach_detach_instance (vm, vnm, (char *) si->name,
+					sw_if_index, 1 /* is_enable */,
+					direction);
+	}
+    }
+  else
+    {
+      vec_foreach_index (i, names)
+	{
+	  snort_attach_detach_instance (vm, vnm, (char *) names[i],
+					sw_if_index, 1 /* is_enable */,
+					direction);
+	}
     }
 
 done:
-  vec_free (name);
+  vec_foreach_index (i, names)
+    {
+      vec_free (names[i]);
+    }
+  vec_free (names);
   unformat_free (line_input);
   return err;
 }
 
 VLIB_CLI_COMMAND (snort_attach_command, static) = {
   .path = "snort attach",
-  .short_help = "snort attach instance <name> interface <if-name> "
-		"[input|ouput|inout]",
+  .short_help =
+    "snort attach all-instances|(instance <name> [instance <name> [...]]) "
+    "interface <if-name> "
+    "[input|output|inout]",
   .function = snort_attach_command_fn,
 };
 
@@ -303,9 +381,12 @@ snort_detach_command_fn (vlib_main_t *vm, unformat_input_t *input,
 {
   unformat_input_t _line_input, *line_input = &_line_input;
   vnet_main_t *vnm = vnet_get_main ();
-  clib_error_t *err = 0;
+  clib_error_t *err = NULL;
+  u8 *name = NULL;
+  u8 **names = NULL;
   u32 sw_if_index = ~0;
-  int rv = 0;
+  u8 is_all_instances = 0;
+  int i = 0;
 
   /* Get a line of input. */
   if (!unformat_user (input, unformat_line_input, line_input))
@@ -313,8 +394,12 @@ snort_detach_command_fn (vlib_main_t *vm, unformat_input_t *input,
 
   while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
     {
-      if (unformat (line_input, "interface %U", unformat_vnet_sw_interface,
-		    vnm, &sw_if_index))
+      if (unformat (line_input, "instance %s", &name))
+	vec_add1 (names, name);
+      else if (unformat (line_input, "all-instances"))
+	is_all_instances = 1;
+      else if (unformat (line_input, "interface %U",
+			 unformat_vnet_sw_interface, vnm, &sw_if_index))
 	;
       else
 	{
@@ -330,32 +415,41 @@ snort_detach_command_fn (vlib_main_t *vm, unformat_input_t *input,
       goto done;
     }
 
-  rv = snort_interface_enable_disable (vm, 0, sw_if_index, 0, SNORT_INOUT);
-
-  switch (rv)
+  if (vec_len (names) == 0)
     {
-    case 0:
-      break;
-    case VNET_API_ERROR_INVALID_INTERFACE:
-      err = clib_error_return (0,
-			       "interface %U is not assigned to snort "
-			       "instance!",
-			       format_vnet_sw_if_index_name, vnm, sw_if_index);
-      break;
-    default:
-      err = clib_error_return (0, "snort_interface_enable_disable returned %d",
-			       rv);
-      break;
+      /* To maintain backwards compatibility */
+      is_all_instances = 1;
+    }
+
+  if (is_all_instances)
+    {
+      err = snort_detach_all_instance (vm, vnm, sw_if_index);
+    }
+  else
+    {
+      vec_foreach_index (i, names)
+	{
+	  snort_attach_detach_instance (vm, vnm, (char *) names[i],
+					sw_if_index, 0 /* is_enable */,
+					SNORT_INOUT);
+	}
     }
 
 done:
+  vec_foreach_index (i, names)
+    {
+      vec_free (names[i]);
+    }
+  vec_free (names);
   unformat_free (line_input);
   return err;
 }
 
 VLIB_CLI_COMMAND (snort_detach_command, static) = {
   .path = "snort detach",
-  .short_help = "snort detach interface <if-name>",
+  .short_help =
+    "snort detach all-instances|(instance <name> [instance <name> [...]]) "
+    "interface <if-name> ",
   .function = snort_detach_command_fn,
 };
 
@@ -384,17 +478,57 @@ snort_show_interfaces_command_fn (vlib_main_t *vm, unformat_input_t *input,
 {
   snort_main_t *sm = &snort_main;
   vnet_main_t *vnm = vnet_get_main ();
-  snort_instance_t *si;
-  u32 *index;
+  snort_interface_data_t *interface;
+  snort_instance_t *instance;
+  snort_attach_dir_t direction;
+  u32 instance_index;
+  u32 sw_if_index;
+  u8 is_input;
+  int i, j;
 
-  vlib_cli_output (vm, "interface\t\tsnort instance");
-  vec_foreach (index, sm->instance_by_sw_if_index)
+  vlib_cli_output (vm, "interface\tinstances\tdirection");
+  vec_foreach_index (sw_if_index, sm->interfaces)
     {
-      if (index[0] != ~0)
+      interface = vec_elt_at_index (sm->interfaces, sw_if_index);
+
+      /* Loop over input instances and prints all of them (with direction
+       * indicated), then continues over output instances while ignoring
+       * previously printed input instances */
+      for (i = 0; i < vec_len (interface->input_instance_indices) +
+			vec_len (interface->output_instance_indices);
+	   i++)
 	{
-	  si = vec_elt_at_index (sm->instances, index[0]);
-	  vlib_cli_output (vm, "%U:\t%s", format_vnet_sw_if_index_name, vnm,
-			   index - sm->instance_by_sw_if_index, si->name);
+	  is_input = i < vec_len (interface->input_instance_indices);
+
+	  instance_index =
+	    is_input ? interface->input_instance_indices[i] :
+		       interface->output_instance_indices
+			 [i - vec_len (interface->input_instance_indices)];
+
+	  /* When printing the output instances ignore the ones present in
+	   * input instances as we have already printed them */
+	  if (!is_input)
+	    {
+	      j =
+		vec_search (interface->input_instance_indices, instance_index);
+	      if (j != ~0)
+		continue;
+	    }
+
+	  instance = snort_get_instance_by_index (instance_index);
+	  direction = snort_get_instance_direction (instance_index, interface);
+	  if (i == 0)
+	    {
+	      vlib_cli_output (vm, "%U:\t%s\t\t%s",
+			       format_vnet_sw_if_index_name, vnm, sw_if_index,
+			       instance->name,
+			       snort_get_direction_name_by_enum (direction));
+	    }
+	  else
+	    {
+	      vlib_cli_output (vm, "\t\t%s\t\t%s", instance->name,
+			       snort_get_direction_name_by_enum (direction));
+	    }
 	}
     }
   return 0;

src/plugins/snort/enqueue.c
@@ -1,7 +1,10 @@
 /* SPDX-License-Identifier: Apache-2.0
  * Copyright(c) 2021 Cisco Systems, Inc.
+ * Copyright(c) 2024 Arm Limited
  */
 
+#include <vnet/ip/ip4_inlines.h>
+#include <vnet/ip/ip4_packet.h>
 #include <vlib/vlib.h>
 #include <vnet/feature/feature.h>
 #include <snort/snort.h>
@@ -56,6 +59,33 @@ static char *snort_enq_error_strings[] = {
 #undef _
 };
 
+static_always_inline u32
+get_snort_instance_index_ip4 (snort_main_t *sm, vlib_buffer_t *b, u32 fa_data)
+{
+  u32 hash;
+  u32 sw_if_index = vnet_buffer (b)->sw_if_index[VLIB_RX];
+  ip4_header_t *ip = NULL;
+  u32 *instances = (fa_data == SNORT_INPUT) ?
+		     sm->interfaces[sw_if_index].input_instance_indices :
+		     sm->interfaces[sw_if_index].output_instance_indices;
+  int n_instances = vec_len (instances);
+
+  if (n_instances == 1)
+    {
+      return instances[0];
+    }
+  ip = vlib_buffer_get_current (b);
+  hash = ip4_compute_flow_hash (ip, IP_FLOW_HASH_DEFAULT);
+  return instances[hash % n_instances];
+}
+
+static_always_inline snort_instance_t *
+get_snort_instance (snort_main_t *sm, vlib_buffer_t *b, u32 fa_data)
+{
+  u32 instance_index = get_snort_instance_index_ip4 (sm, b, fa_data);
+  return snort_get_instance_by_index (instance_index);
+}
+
 static_always_inline uword
 snort_enq_node_inline (vlib_main_t *vm, vlib_node_runtime_t *node,
 		       vlib_frame_t *frame, int with_trace)
@@ -66,26 +96,24 @@ snort_enq_node_inline (vlib_main_t *vm, vlib_node_runtime_t *node,
   u32 thread_index = vm->thread_index;
   u32 n_left = frame->n_vectors;
   u32 n_trace = 0;
-  u32 total_enq = 0, n_processed = 0;
+  u32 total_enq = 0, n_unprocessed = 0;
   u32 *from = vlib_frame_vector_args (frame);
   vlib_buffer_t *bufs[VLIB_FRAME_SIZE], **b = bufs;
   u16 nexts[VLIB_FRAME_SIZE], *next = nexts;
+  u32 unprocessed_bufs[VLIB_FRAME_SIZE];
 
   vlib_get_buffers (vm, from, bufs, n_left);
 
   while (n_left)
     {
-      u64 fa_data;
-      u32 instance_index, next_index, n;
-      u32 l3_offset;
-
-      fa_data =
-	*(u64 *) vnet_feature_next_with_data (&next_index, b[0], sizeof (u64));
-
-      instance_index = (u32) (fa_data & 0xffffffff);
-      l3_offset =
-	(fa_data >> 32) ? vnet_buffer (b[0])->ip.save_rewrite_length : 0;
-      si = vec_elt_at_index (sm->instances, instance_index);
+      u32 next_index, n;
+      /* fa_data is either SNORT_INPUT or SNORT_OUTPUT */
+      u32 fa_data =
+	*(u32 *) vnet_feature_next_with_data (&next_index, b[0], sizeof (u32));
+      u32 l3_offset = (fa_data == SNORT_INPUT) ?
+			0 :
+			vnet_buffer (b[0])->ip.save_rewrite_length;
+      si = get_snort_instance (sm, b[0], fa_data);
 
       /* if client isn't connected skip enqueue and take default action */
       if (PREDICT_FALSE (si->client_index == ~0))
@@ -95,7 +123,8 @@ snort_enq_node_inline (vlib_main_t *vm, vlib_node_runtime_t *node,
 	  else
 	    next[0] = next_index;
 	  next++;
-	  n_processed++;
+	  unprocessed_bufs[n_unprocessed] = from[0];
+	  n_unprocessed++;
 	}
       else
 	{
@@ -108,7 +137,7 @@ snort_enq_node_inline (vlib_main_t *vm, vlib_node_runtime_t *node,
 
 	  vlib_buffer_chain_linearize (vm, b[0]);
 
-	  /* If this pkt is traced, snapshoot the data */
+	  /* If this pkt is traced, snapshot the data */
 	  if (with_trace && b[0]->flags & VLIB_BUFFER_IS_TRACED)
 	    n_trace++;
 
@@ -125,12 +154,12 @@ snort_enq_node_inline (vlib_main_t *vm, vlib_node_runtime_t *node,
       b++;
     }
 
-  if (n_processed)
+  if (n_unprocessed)
     {
       vlib_node_increment_counter (vm, snort_enq_node.index,
-				   SNORT_ENQ_ERROR_NO_INSTANCE, n_processed);
-      vlib_buffer_enqueue_to_next (vm, node, vlib_frame_vector_args (frame),
-				   nexts, n_processed);
+				   SNORT_ENQ_ERROR_NO_INSTANCE, n_unprocessed);
+      vlib_buffer_enqueue_to_next (vm, node, unprocessed_bufs, nexts,
+				   n_unprocessed);
     }
 
   pool_foreach (si, sm->instances)

commit abbe908dcee89ba2ad26f1cb0331b5d7e83a1ec9

snort: validate sw_if_index in attach/detach api handlers
- fixes crash in vpp-debug-verify-master-ubuntu2204-x86_64 CI job
  in test_snort_06_detach_if testcase
- fix similar logic in attach handler
- verify snort direction in attach api message
- add tests verifying attribute validation in attach testcase

Type: fix
Fixes: 102575492c9199259aa5e468f21b46936d7a1ac4

Change-Id: I96fbeb0a7b84f2f238df15b20476ed4086251471
Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

src/plugins/snort/snort_api.c
@@ -1,3 +1,6 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright(c) 2025 Cisco Systems, Inc.
+ */
 #include <vlib/vlib.h>
 #include <vnet/plugin/plugin.h>
 #include <snort/snort.h>
@@ -80,17 +83,25 @@ vl_api_snort_interface_attach_t_handler (vl_api_snort_interface_attach_t *mp)
   u8 snort_dir = mp->snort_dir;
   int rv = VNET_API_ERROR_NO_SUCH_ENTRY;
 
-  if (sw_if_index == INDEX_INVALID)
-    rv = VNET_API_ERROR_NO_MATCHING_INTERFACE;
-  else
+  VALIDATE_SW_IF_INDEX (mp);
+  switch (snort_dir)
     {
-      instance = snort_get_instance_by_index (instance_index);
-      if (instance)
-	rv = snort_interface_enable_disable (vm, (char *) instance->name,
-					     sw_if_index, 1 /* is_enable */,
-					     snort_dir);
+    case SNORT_INPUT:
+    case SNORT_OUTPUT:
+    case SNORT_INOUT:
+      break;
+    default:
+      rv = VNET_API_ERROR_INVALID_DIRECTION;
+      goto bad_sw_if_index;
     }
-
+  instance = snort_get_instance_by_index (instance_index);
+  if (instance)
+    {
+      rv = snort_interface_enable_disable (vm, (char *) instance->name,
+					   sw_if_index, 1 /* is_enable */,
+					   snort_dir);
+    }
+  BAD_SW_IF_INDEX_LABEL;
   REPLY_MACRO (VL_API_SNORT_INTERFACE_ATTACH_REPLY);
 }
 
@@ -375,8 +386,10 @@ vl_api_snort_interface_detach_t_handler (vl_api_snort_interface_detach_t *mp)
   u32 sw_if_index = clib_net_to_host_u32 (mp->sw_if_index);
   int rv;
 
+  VALIDATE_SW_IF_INDEX (mp);
   rv = snort_interface_disable_all (vm, sw_if_index);
 
+  BAD_SW_IF_INDEX_LABEL;
   REPLY_MACRO (VL_API_SNORT_INTERFACE_DETACH_REPLY);
 }
 

src/vnet/error.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: Apache-2.0
- * Copyright(c) 2022 Cisco Systems, Inc.
+ * Copyright(c) 2025 Cisco Systems, Inc.
  */
 #ifndef included_vnet_error_h
 #define included_vnet_error_h
@@ -159,7 +159,8 @@
   _ (BUG, -168, "Bug")                                                        \
   _ (FEATURE_ALREADY_DISABLED, -169, "Feature already disabled")              \
   _ (FEATURE_ALREADY_ENABLED, -170, "Feature already enabled")                \
-  _ (INVALID_PREFIX_LENGTH, -171, "Invalid prefix length")
+  _ (INVALID_PREFIX_LENGTH, -171, "Invalid prefix length")                    \
+  _ (INVALID_DIRECTION, -172, "Invalid Direction")
 
 typedef enum
 {

test/test_snort.py
@@ -29,7 +29,6 @@ class TestSnort(VppTestCase):
     def test_snort_cli(self):
         # TODO: add a test with packets
         # { cli command : part of the expected reply }
-        print("TEST SNORT CLI")
         commands_replies = {
             "snort create-instance name snortTest queue-size 16 on-disconnect drop": "",
             "snort create-instance name snortTest2 queue-size 16 on-disconnect pass": "",
@@ -114,6 +113,18 @@ class TestSnortVapi(VppTestCase):
         reply = self.vapi.snort_interface_attach(
             instance_index=0, sw_if_index=2, snort_dir=2
         )
+        # verify attaching with an invalid direction is rejected
+        try:
+            reply = self.vapi.snort_interface_attach(
+                instance_index=1, sw_if_index=2, snort_dir=4
+            )
+        except:
+            pass
+        else:
+            self.assertNotEqual(reply.retval, 0)
+        reply = self.vapi.cli("show snort interfaces")
+        self.assertNotIn("snortTest1", reply)
+
         reply = self.vapi.snort_interface_attach(
             instance_index=1, sw_if_index=2, snort_dir=3
         )
@@ -123,9 +134,21 @@ class TestSnortVapi(VppTestCase):
         self.assertIn("input", reply)
         self.assertIn("inout", reply)
         self.assertIn("output", reply)
+
+        # verify attaching a previously attached interface is rejected
         try:
             reply = self.vapi.snort_interface_attach(
                 instance_index=1, sw_if_index=2, snort_dir=2
+            )
+        except:
+            pass
+        else:
+            self.assertNotEqual(reply.retval, 0)
+
+        # verify attaching an invalid sw_if_index is rejected
+        try:
+            reply = self.vapi.snort_interface_attach(
+                instance_index=1, sw_if_index=3, snort_dir=2
             )
         except:
             pass
@@ -145,6 +168,7 @@ class TestSnortVapi(VppTestCase):
 
     def test_snort_06_detach_if(self):
         """Interfaces can be detached"""
+        # verify detaching an invalid sw_if_index is rejected
         try:
             reply = self.vapi.snort_interface_detach(sw_if_index=3)
         except: