FDio/vpp

commit e40f8a90bb0c39986c198fca8ad9b0b3c1658401

sflow: initial checkin
This is an sFlow dataplane plugin that can sample
1-in-N packets from device-input, copying them to
a FIFO queue and servicing that queue from a main
process which formats them as Netlink PSAMPLEs,
to be picked up by a popular sidecar agent called
host-sflow.

Type: feature
Change-Id: Ic03456472e53309678f182dc8f74d3c81fb619e6
Signed-off-by: neil.mckee@inmon.com
Signed-off-by: pim@ipng.nl

src/plugins/sflow/CMakeLists.txt
@@ -1,0 +1,42 @@
+
+# Copyright (c) 2024 InMon Corp.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include_directories(${CMAKE_SOURCE_DIR}/vpp-api ${CMAKE_CURRENT_BINARY_DIR}/../../vpp-api)
+add_vpp_plugin(sflow
+  SOURCES
+  sflow.c
+  node.c
+  sflow_common.h
+  sflow.h
+  sflow_psample.c
+  sflow_psample.h
+  sflow_psample_fields.h
+  sflow_usersock.c
+  sflow_usersock.h
+  sflow_vapi.c
+  sflow_vapi.h
+
+  MULTIARCH_SOURCES
+  node.c
+
+  API_FILES
+  sflow.api
+
+  API_TEST_SOURCES
+  sflow_test.c
+
+  LINK_LIBRARIES
+  vppapiclient
+  vapiclient
+)

src/plugins/sflow/sflow.h
@@ -1,0 +1,199 @@
+/*
+ * Copyright (c) 2024 InMon Corp.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef __included_sflow_h__
+#define __included_sflow_h__
+
+#include <vnet/vnet.h>
+#include <vnet/ip/ip.h>
+#include <vnet/ethernet/ethernet.h>
+
+#include <vppinfra/hash.h>
+#include <vppinfra/error.h>
+#include <sflow/sflow_common.h>
+#include <sflow/sflow_vapi.h>
+#include <sflow/sflow_psample.h>
+#include <sflow/sflow_usersock.h>
+
+#define SFLOW_DEFAULT_SAMPLING_N   10000
+#define SFLOW_DEFAULT_POLLING_S	   20
+#define SFLOW_DEFAULT_HEADER_BYTES 128
+#define SFLOW_MAX_HEADER_BYTES	   256
+#define SFLOW_MIN_HEADER_BYTES	   64
+#define SFLOW_HEADER_BYTES_STEP	   32
+
+#define SFLOW_USE_VAPI
+#define SFLOW_FIFO_DEPTH  2048 // must be power of 2
+#define SFLOW_POLL_WAIT_S 0.001
+#define SFLOW_READ_BATCH  100
+
+// use PSAMPLE group number to distinguish VPP samples from others
+// (so that hsflowd will know to remap the ifIndex numbers if necessary)
+#define SFLOW_VPP_PSAMPLE_GROUP_INGRESS 3
+#define SFLOW_VPP_PSAMPLE_GROUP_EGRESS	4
+
+#define foreach_sflow_error                                                   \
+  _ (PROCESSED, "sflow packets processed")                                    \
+  _ (SAMPLED, "sflow packets sampled")                                        \
+  _ (DROPPED, "sflow packets dropped")                                        \
+  _ (PSAMPLE_SEND, "sflow PSAMPLE sent")                                      \
+  _ (PSAMPLE_SEND_FAIL, "sflow PSAMPLE send failed")
+
+typedef enum
+{
+#define _(sym, str) SFLOW_ERROR_##sym,
+  foreach_sflow_error
+#undef _
+    SFLOW_N_ERROR,
+} sflow_error_t;
+
+typedef struct
+{
+  u32 counters[SFLOW_N_ERROR];
+} sflow_err_ctrs_t;
+
+/* packet sample */
+typedef struct
+{
+  u32 samplingN;
+  u32 input_if_index;
+  u32 output_if_index;
+  u32 header_protocol;
+  u32 sampled_packet_size;
+  u32 header_bytes;
+  u8 header[SFLOW_MAX_HEADER_BYTES];
+} sflow_sample_t;
+
+// Define SPSC FIFO for sending samples worker-to-main.
+// (I did try to use VPP svm FIFO, but couldn't
+// understand why it was sometimes going wrong).
+typedef struct
+{
+  volatile u32 tx; // can change under consumer's feet
+  volatile u32 rx; // can change under producer's feet
+  sflow_sample_t samples[SFLOW_FIFO_DEPTH];
+} sflow_fifo_t;
+
+#define SFLOW_FIFO_NEXT(slot) ((slot + 1) & (SFLOW_FIFO_DEPTH - 1))
+static inline int
+sflow_fifo_enqueue (sflow_fifo_t *fifo, sflow_sample_t *sample)
+{
+  u32 curr_rx = clib_atomic_load_acq_n (&fifo->rx);
+  u32 curr_tx = fifo->tx; // clib_atomic_load_acq_n(&fifo->tx);
+  u32 next_tx = SFLOW_FIFO_NEXT (curr_tx);
+  if (next_tx == curr_rx)
+    return false; // full
+  memcpy (&fifo->samples[next_tx], sample, sizeof (*sample));
+  clib_atomic_store_rel_n (&fifo->tx, next_tx);
+  return true;
+}
+
+static inline int
+sflow_fifo_dequeue (sflow_fifo_t *fifo, sflow_sample_t *sample)
+{
+  u32 curr_rx = fifo->rx; // clib_atomic_load_acq_n(&fifo->rx);
+  u32 curr_tx = clib_atomic_load_acq_n (&fifo->tx);
+  if (curr_rx == curr_tx)
+    return false; // empty
+  memcpy (sample, &fifo->samples[curr_rx], sizeof (*sample));
+  u32 next_rx = SFLOW_FIFO_NEXT (curr_rx);
+  clib_atomic_store_rel_n (&fifo->rx, next_rx);
+  return true;
+}
+
+/* private to worker */
+typedef struct
+{
+  u32 smpN;
+  u32 skip;
+  u32 pool;
+  u32 seed;
+  u32 smpl;
+  u32 drop;
+  CLIB_CACHE_LINE_ALIGN_MARK (_fifo);
+  sflow_fifo_t fifo;
+} sflow_per_thread_data_t;
+
+typedef struct
+{
+  /* API message ID base */
+  u16 msg_id_base;
+
+  /* convenience */
+  vlib_main_t *vlib_main;
+  vnet_main_t *vnet_main;
+  ethernet_main_t *ethernet_main;
+
+  /* sampling state */
+  u32 samplingN;
+  u32 pollingS;
+  u32 headerB;
+  u32 total_threads;
+  sflow_per_interface_data_t *per_interface_data;
+  sflow_per_thread_data_t *per_thread_data;
+
+  /* psample channel (packet samples) */
+  SFLOWPS sflow_psample;
+  /* usersock channel (periodic counters) */
+  SFLOWUS sflow_usersock;
+#define SFLOW_NETLINK_USERSOCK_MULTICAST 29
+  /* dropmon channel (packet drops) */
+  // SFLOWDM sflow_dropmon;
+
+  /* sample-processing */
+  u32 now_mono_S;
+
+  /* running control */
+  int running;
+  u32 interfacesEnabled;
+
+  /* main-thread counters */
+  u32 psample_seq_ingress;
+  u32 psample_seq_egress;
+  u32 psample_send_drops;
+  u32 csample_send;
+  u32 csample_send_drops;
+  u32 unixsock_seq;
+
+  /* vapi query helper thread (transient) */
+  CLIB_CACHE_LINE_ALIGN_MARK (_vapi);
+  sflow_vapi_client_t vac;
+  int vapi_requests;
+
+} sflow_main_t;
+
+extern sflow_main_t sflow_main;
+
+extern vlib_node_registration_t sflow_node;
+
+static inline u32
+sflow_next_random_skip (sflow_per_thread_data_t *sfwk)
+{
+  /* skip==1 means "take the next packet" so this
+     fn must never return 0 */
+  if (sfwk->smpN <= 1)
+    return 1;
+  u32 lim = (2 * sfwk->smpN) - 1;
+  return (random_u32 (&sfwk->seed) % lim) + 1;
+}
+
+#endif /* __included_sflow_h__ */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

src/plugins/sflow/sflow_common.h
@@ -1,0 +1,44 @@
+/*
+ * Copyright (c) 2024 InMon Corp.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef __included_sflow_common_h__
+#define __included_sflow_common_h__
+
+#define SFLOW_USE_VAPI
+
+extern vlib_log_class_t sflow_logger;
+#define SFLOW_DBG(...)	  vlib_log_debug (sflow_logger, __VA_ARGS__);
+#define SFLOW_INFO(...)	  vlib_log_info (sflow_logger, __VA_ARGS__);
+#define SFLOW_NOTICE(...) vlib_log_notice (sflow_logger, __VA_ARGS__);
+#define SFLOW_WARN(...)	  vlib_log_warn (sflow_logger, __VA_ARGS__);
+#define SFLOW_ERR(...)	  vlib_log_err (sflow_logger, __VA_ARGS__);
+
+typedef struct
+{
+  u32 sw_if_index;
+  u32 hw_if_index;
+  u32 linux_if_index;
+  u32 polled;
+  int sflow_enabled;
+} sflow_per_interface_data_t;
+
+#endif /* __included_sflow_common_h__ */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

src/plugins/sflow/sflow_vapi.c
@@ -1,0 +1,221 @@
+/*
+ * Copyright (c) 2024 InMon Corp.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <sflow/sflow_vapi.h>
+
+#include <vlibapi/api.h>
+#include <vlibmemory/api.h>
+#include <vpp/app/version.h>
+#include <stdbool.h>
+
+#include <vapi/vapi.h>
+#include <vapi/memclnt.api.vapi.h>
+#include <vapi/vlib.api.vapi.h>
+
+#ifdef included_interface_types_api_types_h
+#define defined_vapi_enum_if_status_flags
+#define defined_vapi_enum_mtu_proto
+#define defined_vapi_enum_link_duplex
+#define defined_vapi_enum_sub_if_flags
+#define defined_vapi_enum_rx_mode
+#define defined_vapi_enum_if_type
+#define defined_vapi_enum_direction
+#endif
+#include <vapi/lcp.api.vapi.h>
+
+DEFINE_VAPI_MSG_IDS_LCP_API_JSON;
+
+static vapi_error_e
+my_pair_get_cb (struct vapi_ctx_s *ctx, void *callback_ctx, vapi_error_e rv,
+		bool is_last, vapi_payload_lcp_itf_pair_get_v2_reply *reply)
+{
+  // this is a no-op, but it seems like it's presence is still required.  For
+  // example, it is called if the pair lookup does not find anything.
+  return VAPI_OK;
+}
+
+static vapi_error_e
+my_pair_details_cb (struct vapi_ctx_s *ctx, void *callback_ctx,
+		    vapi_error_e rv, bool is_last,
+		    vapi_payload_lcp_itf_pair_details *details)
+{
+  sflow_per_interface_data_t *sfif =
+    (sflow_per_interface_data_t *) callback_ctx;
+  // Setting this here will mean it is sent to hsflowd with the interface
+  // counters.
+  sfif->linux_if_index = details->vif_index;
+  return VAPI_OK;
+}
+
+static vapi_error_e
+sflow_vapi_connect (sflow_vapi_client_t *vac)
+{
+  vapi_error_e rv = VAPI_OK;
+  vapi_ctx_t ctx = vac->vapi_ctx;
+  if (ctx == NULL)
+    {
+      // first time - open and connect.
+      if ((rv = vapi_ctx_alloc (&ctx)) != VAPI_OK)
+	{
+	  SFLOW_ERR ("vap_ctx_alloc() returned %d", rv);
+	}
+      else
+	{
+	  vac->vapi_ctx = ctx;
+	  if ((rv = vapi_connect_from_vpp (
+		 ctx, "api_from_sflow_plugin", SFLOW_VAPI_MAX_REQUEST_Q,
+		 SFLOW_VAPI_MAX_RESPONSE_Q, VAPI_MODE_BLOCKING, true)) !=
+	      VAPI_OK)
+	    {
+	      SFLOW_ERR ("vapi_connect_from_vpp() returned %d", rv);
+	    }
+	  else
+	    {
+	      // Connected - but is there a handler for the request we want to
+	      // send?
+	      if (!vapi_is_msg_available (ctx,
+					  vapi_msg_id_lcp_itf_pair_add_del_v2))
+		{
+		  SFLOW_WARN ("vapi_is_msg_available() returned false => "
+			      "linux-cp plugin not loaded");
+		  rv = VAPI_EUSER;
+		}
+	    }
+	}
+    }
+  return rv;
+}
+
+// in forked thread
+static void *
+get_lcp_itf_pairs (void *magic)
+{
+  sflow_vapi_client_t *vac = magic;
+  vapi_error_e rv = VAPI_OK;
+
+  sflow_per_interface_data_t *intfs = vac->vapi_itfs;
+  vlib_set_thread_name (SFLOW_VAPI_THREAD_NAME);
+  if ((rv = sflow_vapi_connect (vac)) != VAPI_OK)
+    {
+      vac->vapi_unavailable = true;
+    }
+  else
+    {
+      vapi_ctx_t ctx = vac->vapi_ctx;
+
+      for (int ii = 1; ii < vec_len (intfs); ii++)
+	{
+	  sflow_per_interface_data_t *sfif = vec_elt_at_index (intfs, ii);
+	  if (sfif && sfif->sflow_enabled)
+	    {
+	      // TODO: if we try non-blocking we might not be able to just pour
+	      // all the requests in here. Might be better to do them one at a
+	      // time - e.g. when we poll for counters.
+	      vapi_msg_lcp_itf_pair_get_v2 *msg =
+		vapi_alloc_lcp_itf_pair_get_v2 (ctx);
+	      if (msg)
+		{
+		  msg->payload.sw_if_index = sfif->sw_if_index;
+		  if ((rv = vapi_lcp_itf_pair_get_v2 (ctx, msg, my_pair_get_cb,
+						      sfif, my_pair_details_cb,
+						      sfif)) != VAPI_OK)
+		    {
+		      SFLOW_ERR ("vapi_lcp_itf_pair_get_v2 returned %d", rv);
+		      // vapi.h: "message must be freed by vapi_msg_free if not
+		      // consumed by vapi_send"
+		      vapi_msg_free (ctx, msg);
+		    }
+		}
+	    }
+	}
+      // We no longer disconnect or free the client structures
+      // vapi_disconnect_from_vpp (ctx);
+      // vapi_ctx_free (ctx);
+    }
+  // indicate that we are done - more portable that using pthread_tryjoin_np()
+  vac->vapi_request_status = (int) rv;
+  clib_atomic_store_rel_n (&vac->vapi_request_active, false);
+  // TODO: how to tell if heap-allocated data is stored separately per thread?
+  // And if so, how to tell the allocator to GC all data for the thread when it
+  // exits?
+  return (void *) rv;
+}
+
+int
+sflow_vapi_read_linux_if_index_numbers (sflow_vapi_client_t *vac,
+					sflow_per_interface_data_t *itfs)
+{
+
+#ifdef SFLOW_VAPI_TEST_PLUGIN_SYMBOL
+  // don't even fork the query thread if the symbol is not there
+  if (!vlib_get_plugin_symbol ("linux_cp_plugin.so", "lcp_itf_pair_get"))
+    {
+      return false;
+    }
+#endif
+  // previous query is done and results extracted?
+  int req_active = clib_atomic_load_acq_n (&vac->vapi_request_active);
+  if (req_active == false && vac->vapi_itfs == NULL)
+    {
+      // make a copy of the current interfaces vector for the lookup thread to
+      // write into
+      vac->vapi_itfs = vec_dup (itfs);
+      pthread_attr_t attr;
+      pthread_attr_init (&attr);
+      pthread_attr_setdetachstate (&attr, PTHREAD_CREATE_DETACHED);
+      pthread_attr_setstacksize (&attr, VLIB_THREAD_STACK_SIZE);
+      vac->vapi_request_active = true;
+      pthread_create (&vac->vapi_thread, &attr, get_lcp_itf_pairs, vac);
+      pthread_attr_destroy (&attr);
+      return true;
+    }
+  return false;
+}
+
+int
+sflow_vapi_check_for_linux_if_index_results (sflow_vapi_client_t *vac,
+					     sflow_per_interface_data_t *itfs)
+{
+  // request completed?
+  // TODO: if we use non-blocking mode do we have to call something here to
+  // receive results?
+  int req_active = clib_atomic_load_acq_n (&vac->vapi_request_active);
+  if (req_active == false && vac->vapi_itfs != NULL)
+    {
+      // yes, extract what we learned
+      // TODO: would not have to do this if vector were array of pointers
+      // to sflow_per_interface_data_t rather than an actual array, but
+      // it does mean we have very clear separation between the threads.
+      for (int ii = 1; ii < vec_len (vac->vapi_itfs); ii++)
+	{
+	  sflow_per_interface_data_t *sfif1 =
+	    vec_elt_at_index (vac->vapi_itfs, ii);
+	  sflow_per_interface_data_t *sfif2 = vec_elt_at_index (itfs, ii);
+	  if (sfif1 && sfif2 && sfif1->sflow_enabled && sfif2->sflow_enabled)
+	    sfif2->linux_if_index = sfif1->linux_if_index;
+	}
+      vec_free (vac->vapi_itfs);
+      vac->vapi_itfs = NULL;
+      return true;
+    }
+  return false;
+}
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

src/plugins/sflow/sflow_vapi.h
@@ -1,0 +1,52 @@
+/*
+ * Copyright (c) 2024 InMon Corp.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef __included_sflow_vapi_h__
+#define __included_sflow_vapi_h__
+
+#include <vnet/vnet.h>
+#include <sflow/sflow_common.h>
+
+#define SFLOW_VAPI_POLL_INTERVAL  5
+#define SFLOW_VAPI_MAX_REQUEST_Q  8
+#define SFLOW_VAPI_MAX_RESPONSE_Q 16
+#define SFLOW_VAPI_THREAD_NAME	  "sflow_vapi" // must be <= 15 characters
+
+// #define SFLOW_VAPI_TEST_PLUGIN_SYMBOL
+
+typedef struct
+{
+  volatile int vapi_request_active; // to sync main <-> vapi_thread
+  pthread_t vapi_thread;
+  sflow_per_interface_data_t *vapi_itfs;
+  int vapi_unavailable;
+  int vapi_request_status; // written by vapi_thread
+  void *vapi_ctx;
+} sflow_vapi_client_t;
+
+int sflow_vapi_read_linux_if_index_numbers (sflow_vapi_client_t *vac,
+					    sflow_per_interface_data_t *itfs);
+int
+sflow_vapi_check_for_linux_if_index_results (sflow_vapi_client_t *vac,
+					     sflow_per_interface_data_t *itfs);
+
+#endif /* __included_sflow_vapi_h__ */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

MAINTAINERS
@@ -903,3 +903,9 @@ Netmap
 I:	netmap
 M:	Tom Jones <thj@freebsd.org>
 F:	src/plugins/netmap/
+
+sFlow
+I:	sflow
+M:	Pim van Pelt <pim@ipng.nl>
+M:	Neil McKee <neil.mckee@inmon.com>
+F:	src/plugins/sflow/

src/plugins/sflow/FEATURE.yaml
@@ -1,0 +1,16 @@
+---
+name: sFlow
+maintainer: Neil McKee <neil.mckee@inmon.com>
+
+description: |-
+       This plugin implements the random packet-sampling and interface
+       telemetry streaming required to support standard sFlow export
+       on Linux platforms. The overhead incurred by this monitoring is
+       minimal, so that detailed, real-time traffic analysis can be
+       achieved even under high load conditions, with visibility into
+       any fields that appear in the packet headers. If the linux-cp
+       plugin is running then interfaces will be mapped to their
+       equivalent Linux tap ports.
+
+state: experimental
+properties: [CLI, MULTITHREAD]

commit 48087681474937976fdd432dec5f9bc18d0da45b

sflow: Update build rules
1) sFlow relies on Netlink PSAMPLE and USERSOCK make sure the headers are there.
2) sFlow plugin is not available on FreeBSD due to specific Netlink features.
3) The preprocessor flag SFLOW_USE_VAPI controls whether the sflow plugin
will attempt to contact the linux-cp plugin using the binary VAPI. It has
to be a preprocessor flag so it can suppress the "#include" statements
that define that linux-cp api, because those include files will be missing
if linux-cp is excluded from the build.

This "excluded_plugins" list in constructed in vpp/src/plugins/CMakeLists.txt
and can be used with:
make VPP_EXCLUDED_PLUGINS=linux-cp build build-release

Type: fix
Fixes: e40f8a9
Change-Id: If44929d285d27db7862910ecb4ec11ddd4591fd2
Signed-off-by: pim@ipng.nl

src/plugins/sflow/CMakeLists.txt
@@ -12,6 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+vpp_find_path(NETLINK_INCLUDE_DIR NAMES linux/netlink.h)
+if (NOT NETLINK_INCLUDE_DIR)
+  message(WARNING "netlink headers not found - sflow plugin disabled")
+  return()
+endif()
+
+if ("${CMAKE_SYSTEM_NAME}" STREQUAL "FreeBSD")
+  message(WARNING "sflow is not supported on FreeBSD - sflow plugin disabled")
+  return()
+endif()
+
+LIST(FIND excluded_plugins linux-cp exc_index)
+if(${exc_index} EQUAL "-1")
+  message(WARNING "sflow plugin - linux-cp plugin included: compiling VAPI calls")
+  add_compile_definitions(SFLOW_USE_VAPI)
+else()
+  message(WARNING "sflow plugin - linux-cp plugin excluded: not compiling VAPI calls")
+endif()
+
 include_directories(${CMAKE_SOURCE_DIR}/vpp-api ${CMAKE_CURRENT_BINARY_DIR}/../../vpp-api)
 add_vpp_plugin(sflow
   SOURCES

src/plugins/sflow/sflow.c
@@ -539,8 +539,10 @@ sflow_sampling_start (sflow_main_t *smp)
   smp->psample_seq_egress = 0;
   smp->psample_send_drops = 0;
 
+#ifdef SFLOW_USE_VAPI
   // reset vapi request count so that we make a request the first time
   smp->vapi_requests = 0;
+#endif
 
   /* open PSAMPLE netlink channel for writing packet samples */
   SFLOWPS_open (&smp->sflow_psample);

src/plugins/sflow/sflow.h
@@ -33,7 +33,6 @@
 #define SFLOW_MIN_HEADER_BYTES	   64
 #define SFLOW_HEADER_BYTES_STEP	   32
 
-#define SFLOW_USE_VAPI
 #define SFLOW_FIFO_DEPTH  2048 // must be power of 2
 #define SFLOW_POLL_WAIT_S 0.001
 #define SFLOW_READ_BATCH  100
@@ -165,12 +164,12 @@ typedef struct
   u32 csample_send;
   u32 csample_send_drops;
   u32 unixsock_seq;
-
+#ifdef SFLOW_USE_VAPI
   /* vapi query helper thread (transient) */
   CLIB_CACHE_LINE_ALIGN_MARK (_vapi);
   sflow_vapi_client_t vac;
   int vapi_requests;
-
+#endif
 } sflow_main_t;
 
 extern sflow_main_t sflow_main;

src/plugins/sflow/sflow_common.h
@@ -15,7 +15,7 @@
 #ifndef __included_sflow_common_h__
 #define __included_sflow_common_h__
 
-#define SFLOW_USE_VAPI
+// #define SFLOW_USE_VAPI (set by CMakeLists.txt)
 
 extern vlib_log_class_t sflow_logger;
 #define SFLOW_DBG(...)	  vlib_log_debug (sflow_logger, __VA_ARGS__);

src/plugins/sflow/sflow_vapi.c
@@ -15,6 +15,8 @@
 
 #include <sflow/sflow_vapi.h>
 
+#ifdef SFLOW_USE_VAPI
+
 #include <vlibapi/api.h>
 #include <vlibmemory/api.h>
 #include <vpp/app/version.h>
@@ -212,6 +214,9 @@ sflow_vapi_check_for_linux_if_index_results (sflow_vapi_client_t *vac,
     }
   return false;
 }
+
+#endif /* SFLOW_USE_VAPI */
+
 /*
  * fd.io coding-style-patch-verification: ON
  *

src/plugins/sflow/sflow_vapi.h
@@ -18,6 +18,8 @@
 #include <vnet/vnet.h>
 #include <sflow/sflow_common.h>
 
+#ifdef SFLOW_USE_VAPI
+
 #define SFLOW_VAPI_POLL_INTERVAL  5
 #define SFLOW_VAPI_MAX_REQUEST_Q  8
 #define SFLOW_VAPI_MAX_RESPONSE_Q 16
@@ -41,6 +43,7 @@ int
 sflow_vapi_check_for_linux_if_index_results (sflow_vapi_client_t *vac,
 					     sflow_per_interface_data_t *itfs);
 
+#endif /* SFLOW_USE_VAPI */
 #endif /* __included_sflow_vapi_h__ */
 
 /*