03dafe88f608aeea43d0cf02d1de74ae60582c31
Alok Mishra <almishra@marvell.com>
2025-01-23 16:33:38 +0500

    octeon: implement hardware traffic management
    This patch adds support for hw traffic management in
    OCTEON plugin.
    
    Type: feature
    
    Signed-off-by: Harish Kumar Malik <hmalik@marvell.com>
    Signed-off-by: Monendra Singh Kushwaha <kmonendra@marvell.com>
    Signed-off-by: Alok Mishra <almishra@marvell.com>
    Change-Id: I02f4c9128c45d07627b4e7d78bb34bd8bc3820b0

---
+++ src/plugins/dev_octeon/tm.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024 Marvell.
+ * SPDX-License-Identifier: Apache-2.0
+ * https://spdx.org/licenses/Apache-2.0.html
+ */
+
+#ifndef _OCT_TM_H_
+#define _OCT_TM_H_
+
+#include <vppinfra/format.h>
+#include <vnet/vnet.h>
+#include <vnet/tm/tm.h>
+
+#define NIX_TM_DFLT_RR_WT   71
+#define OCT_TM_NODE_ID_NULL -1
+#define OCT_TM_INVALID	    0
+
+#endif /* _OCT_TM_H_ */
