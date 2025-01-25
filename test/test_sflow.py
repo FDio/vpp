df91021cf4729e2281b0ea45d703b96ded0dce4c
Pim van Pelt <pim@ipng.nl>
2025-01-25 12:49:20 +0100

    sflow: replace VAPI with DLAPI
    Remove the requirement to coordinate between linux-cp and sflow at
    build time via cmake. Also, remove 350 lines of awkward thread-forking
    VAPI code. Replace it with a dynamically retrieved function from the
    linux-cp plugin, called lcp_itf_pair_get_vif_index_by_phy().
    
    Remove build inhibit based on Netlink headers, and only inhibit the
    build for FreeBSD. This plugin can now run regardless of Linux CP
    being built or loaded, and then return VPP ifIndex numbers instead.
    
    Also, fix a flaky test where non-ephemeral source ports throw off
    packet captures.
    
    Type: improvement
    Change-Id: I5486742fa0e343e382630a22672a05fe3dcf7836
    Signed-off-by: neil.mckee@inmon.com
    Signed-off-by: pim@ipng.nl

--- test/test_sflow.py
+++ test/test_sflow.py
@@ -108,7 +108,7 @@ class SFlowTestCase(VppTestCase):
             p = (
                 Ether(dst=src_if.local_mac, src=src_if.remote_mac)
                 / IP(src=src_if.remote_ip4, dst=dst_if.remote_ip4)
-                / UDP(sport=randint(1000, 2000), dport=5678)
+                / UDP(sport=randint(49152, 65535), dport=5678)
                 / Raw(payload)
             )
             # store a copy of the packet in the packet info
