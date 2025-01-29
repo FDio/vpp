744e6e5c77395348a282b6ecca9258c9d591e38c
Dave Wallace <dwallacelf@gmail.com>
2025-01-28 23:44:06 -0500

    quic: quic engine api
    - Add an api to the quic plugin to separate the quic library
      implementation from the vpp quic transport.
    - Move quicly library implementation to a separate plugin
      utilizing the quic engine api.
    
    Change-Id: If522f18150ef7d92dbdd5ff1a96a65703dea730c
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- src/plugins/quic_quicly/ptls_certs.h
+++ src/plugins/quic_quicly/ptls_certs.h
@@ -1,21 +1,9 @@
-/*
- * Copyright (c) 2019 Cisco and/or its affiliates.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright(c) 2025 Cisco Systems, Inc.
  */
 
-#ifndef __included_quic_certs_h__
-#define __included_quic_certs_h__
-
+#ifndef __included_ptls_certs_h__
+#define __included_ptls_certs_h__
 
 #include <picotls/openssl.h>
 #include <picotls/pembase64.h>
@@ -36,8 +24,7 @@ int load_bio_certificate_chain (ptls_context_t * ctx, const char *cert_data);
 
 int load_bio_private_key (ptls_context_t * ctx, const char *pk_data);
 
-
-#endif /* __included_quic_certs_h__ */
+#endif /* __included_ptls_certs_h__ */
 
 /*
  * fd.io coding-style-patch-verification: ON
