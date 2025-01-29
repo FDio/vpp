fe53afbb30b569c173feeabaed8467a54f88756a
Florin Coras <fcoras@cisco.com>
2025-01-29 01:27:20 -0500

    vppinfra: reduce rbtree size
    Use pool header opaque to store root index.
    
    Type: improvement
    
    Change-Id: Ibc295478b146b96826b1457eb6b83d5eb732e199
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/svm/svm_fifo.c
+++ src/svm/svm_fifo.c
@@ -533,7 +533,7 @@ f_find_node_rbtree (rb_tree_t * rt, u32 pos)
 {
   rb_node_t *cur, *prev;
 
-  cur = rb_node (rt, rt->root);
+  cur = rb_node (rt, rb_tree_root (rt));
   if (PREDICT_FALSE (rb_node_is_tnil (rt, cur)))
     return 0;
 
@@ -599,7 +599,7 @@ f_update_ooo_enq (svm_fifo_t * f, u32 start_pos, u32 end_pos)
       return;
     }
 
-  if (rt->root == RBTREE_TNIL_INDEX)
+  if (rb_tree_root (rt) == RBTREE_TNIL_INDEX)
     {
       c = f_tail_cptr (f);
       ASSERT (c->enq_rb_index == RBTREE_TNIL_INDEX);
@@ -648,7 +648,7 @@ f_update_ooo_deq (svm_fifo_t * f, u32 start_pos, u32 end_pos)
       return;
     }
 
-  if (rt->root == RBTREE_TNIL_INDEX)
+  if (rb_tree_root (rt) == RBTREE_TNIL_INDEX)
     {
       c = f_start_cptr (f);
       ASSERT (c->deq_rb_index == RBTREE_TNIL_INDEX);
