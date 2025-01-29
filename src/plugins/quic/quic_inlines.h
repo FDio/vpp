fba8262a47f7e9e9b2e3d6a98436c0420f60b9b9
Dave Wallace <dwallacelf@gmail.com>
2025-01-28 23:44:06 -0500

    quic: quic library api
    - Add an api to the quic plugin to separate the quic library
      implementation from the vpp quic transport.
    - Move quicly library implementation to a separate plugin
      utilizing the quic library api.
    
    Change-Id: If522f18150ef7d92dbdd5ff1a96a65703dea730c
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

---
+++ src/plugins/quic/quic_inlines.h
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright(c) 2025 Cisco Systems, Inc.
+ */
+
+#ifndef __included_quic_inlines_h__
+#define __included_quic_inlines_h__
+
+#include <quic/quic.h>
+
+static_always_inline int
+quic_lib_init_crypto_context (quic_lib_type_t lib_type,
+			      crypto_context_t *crctx, quic_ctx_t *ctx)
+{
+  if (!quic_vfts[lib_type].init_crypto_context)
+    {
+      QUIC_DBG (1, "init_crypto_context() not available for %s library\n",
+		quic_lib_type_str (lib_type));
+      return -1;
+    }
+  return (quic_vfts[lib_type].init_crypto_context (crctx, ctx));
+}
+
+static_always_inline quic_ctx_t *
+quic_lib_conn_ctx_get (quic_lib_type_t lib_type, void *conn)
+{
+  if (!quic_vfts[lib_type].conn_ctx_get)
+    {
+      QUIC_DBG (1, "conn_ctx_get() not available for %s library\n",
+		quic_lib_type_str (lib_type));
+      return NULL;
+    }
+  return (quic_vfts[lib_type].conn_ctx_get (conn));
+}
+#endif /* __included_quic_inlines_h__ */
