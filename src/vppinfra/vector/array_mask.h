e748ed8495f0b2558be137106167da87244ccee1
Mohsin Kazmi <sykazmi@cisco.com>
2025-02-06 19:29:31 +0000

    vppinfra: add support for flag test in vector of flags
    Type: improvement
    
    Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>
    Change-Id: Iddb5d5d54f21adba982323d537c91c3bbf5788b1

--- src/vppinfra/vector/array_mask.h
+++ src/vppinfra/vector/array_mask.h
@@ -6,6 +6,150 @@
 #define included_vector_array_mask_h
 #include <vppinfra/clib.h>
 
+static_always_inline u64
+clib_array_mask_flag_test_u32_x64_n (u64 bitmap, u32 *a, u32 v, u32 n_elts)
+{
+  u64 mask = 0;
+  CLIB_UNUSED (u32 i) = 0;
+#if defined(CLIB_HAVE_VEC512)
+  u32x16 v16 = u32x16_splat (v);
+  u32x16u *av = (u32x16u *) a;
+  mask = ((u64) _mm512_mask_test_epi32_mask (bitmap, av[0], v16) |
+	  (u64) _mm512_mask_test_epi32_mask (bitmap >> 16, av[1], v16) << 16 |
+	  (u64) _mm512_mask_test_epi32_mask (bitmap >> 32, av[2], v16) << 32 |
+	  (u64) _mm512_mask_test_epi32_mask (bitmap >> 48, av[3], v16) << 48);
+  return mask;
+#elif defined(CLIB_HAVE_VEC256)
+  u32x8 v8 = u32x8_splat (v);
+  u32x8 dst0, av;
+  for (; i + 8 <= n_elts; i += 8)
+    {
+      av = _mm256_loadu_si256 ((__m256i *) &a[i]);
+      dst0 = _mm256_cmpeq_epi32 (_mm256_and_si256 (av, v8),
+				 _mm256_setzero_si256 ());
+      mask |=
+	(u64) (0x000000FF & ~_mm256_movemask_ps (_mm256_castsi256_ps (dst0)))
+	<< i;
+    }
+#endif
+  for (; i < n_elts; i++)
+    {
+      if (a[i] & v)
+	mask |= (1ULL << i);
+    }
+  mask &= bitmap;
+  return mask;
+}
+
+static_always_inline void
+clib_array_mask_flag_test_u32 (u64 *bitmap, u32 *vec, u32 value,
+			       u64 *ret_bitmap, u32 n_elts)
+{
+  while (n_elts >= uword_bits)
+    {
+      ret_bitmap++[0] =
+	clib_array_mask_flag_test_u32_x64_n (bitmap++[0], vec, value, n_elts);
+      n_elts -= 64;
+      vec += 64;
+    }
+  if (PREDICT_TRUE (n_elts == 0))
+    return;
+  ret_bitmap[0] =
+    clib_array_mask_flag_test_u32_x64_n (bitmap[0], vec, value, n_elts);
+}
+
+static_always_inline u64
+clib_array_test_flag_u32_x64 (u32 *a, u32 v)
+{
+  u64 mask = 0;
+#if defined(CLIB_HAVE_VEC512)
+  u32x16 v16 = u32x16_splat (v);
+  u32x16u *av = (u32x16u *) a;
+  mask = ((u64) _mm512_test_epi32_mask (av[0], v16) |
+	  (u64) _mm512_test_epi32_mask (av[1], v16) << 16 |
+	  (u64) _mm512_test_epi32_mask (av[2], v16) << 32 |
+	  (u64) _mm512_test_epi32_mask (av[3], v16) << 48);
+#elif defined(CLIB_HAVE_VEC256)
+  u32x8 v8 = u32x8_splat (v);
+  u32x8 dst0, av;
+  for (u32 i = 0; i + 8 <= 64; i += 8)
+    {
+      av = _mm256_loadu_si256 ((__m256i *) &a[i]);
+      dst0 = _mm256_cmpeq_epi32 (_mm256_and_si256 (av, v8),
+				 _mm256_setzero_si256 ());
+      mask |=
+	(u64) (0x000000FF & ~_mm256_movemask_ps (_mm256_castsi256_ps (dst0)))
+	<< i;
+    }
+#else
+  for (u32 i = 0; i < 64; i++)
+    {
+      if (a[i] & v)
+	mask |= (1ULL << i);
+    }
+#endif
+  return mask;
+}
+
+static_always_inline u64
+clib_array_test_flag_u32_x64_n (u32 *a, u32 v, u32 n_elts)
+{
+  u64 mask = 0;
+  CLIB_UNUSED (u32 i) = 0;
+  CLIB_UNUSED (u64 data_mask) = pow2_mask (n_elts);
+#if defined(CLIB_HAVE_VEC512)
+  u32x16 v16 = u32x16_splat (v);
+  u32x16u *av = (u32x16u *) a;
+  mask = ((u64) _mm512_test_epi32_mask (
+	    u32x16_mask_load_zero (&av[0], data_mask), v16) |
+	  (u64) _mm512_test_epi32_mask (
+	    u32x16_mask_load_zero (&av[1], data_mask >> 16), v16)
+	    << 16 |
+	  (u64) _mm512_test_epi32_mask (
+	    u32x16_mask_load_zero (&av[2], data_mask >> 32), v16)
+	    << 32 |
+	  (u64) _mm512_test_epi32_mask (
+	    u32x16_mask_load_zero (&av[3], data_mask >> 48), v16)
+	    << 48);
+  n_elts = 0;
+#elif defined(CLIB_HAVE_VEC256)
+  u32x8 v8 = u32x8_splat (v);
+  u32x8 dst0, av;
+  for (; i + 8 <= n_elts; i += 8)
+    {
+      av = _mm256_loadu_si256 ((__m256i *) &a[i]);
+      dst0 =
+	_mm256_cmpeq_epi32 (_mm256_and_si256 (av, v8),
+			    _mm256_setzero_si256 ()); // Test flag presence
+      mask |=
+	(u64) (0x000000FF & ~_mm256_movemask_ps (_mm256_castsi256_ps (dst0)))
+	<< i;
+    }
+
+#endif
+  for (; i < n_elts; i++)
+    {
+      if (a[i] & v)
+	mask |= (1ULL << i);
+    }
+  return mask;
+}
+
+static_always_inline void
+clib_array_test_flag_u32 (u32 *vec, u32 value, u64 *bitmap, u32 n_elts)
+{
+  while (n_elts >= uword_bits)
+    {
+      bitmap++[0] = clib_array_test_flag_u32_x64 (vec, value);
+      n_elts -= 64;
+      vec += 64;
+    }
+  if (PREDICT_TRUE (n_elts == 0))
+    return;
+  bitmap[0] =
+    clib_array_test_flag_u32_x64_n (vec, value, n_elts) & pow2_mask (n_elts);
+}
+
 /** \brief Mask array of 32-bit elemments
 
     @param src source array of u32 elements
