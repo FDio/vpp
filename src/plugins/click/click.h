ffc1728cc5c407d150ed77336de9c32936fec903
Damjan Marion <damarion@cisco.com>
2025-02-20 20:02:51 +0100

    click: click plugin
    Change-Id: Id01d278689ffc8cfa66d1b00dc9405c5d6ea65e8
    Signed-off-by: Damjan Marion <damarion@cisco.com>

---
+++ src/plugins/click/click.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright(c) 2025 Cisco Systems, Inc.
+ */
+
+#ifndef __click_click_h__
+#define __click_click_h__
+
+#include <vlib/vlib.h>
+#include <click/vppclick.h>
+
+typedef struct
+{
+  vppclick_ctx_t *ctx;
+  f64 last_run;
+} click_instance_t;
+
+typedef struct
+{
+  click_instance_t *instances;
+} click_main_t;
+
+extern click_main_t click_main;
+extern vlib_node_registration_t click_input_node;
+
+#endif /* __click_click_h__ */
