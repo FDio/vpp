ac376bd21c91e4f1165774ad02bd88cd09a35b8d
Masih Nilforoush <m.nilforoush@x-ion.de>
2025-02-27 14:44:44 +0100

    gre: Add support for GRE keys in the GRE plugin
    The added feature enables the GRE plugin to create tunnels between the same endpoints,
    distinguishing them by the "key" value. It uses the standard 'key'
    parameter in the GRE header.
    Changes have been made to add support for CLI and API to create tunnels with a "key" value.
    
    The CLI syntax is as follows:
       create gre tunnel src src_IP_Address dst dst_IP_Address key key_value
    
    All existing GRE functionalities, such as tunnel type and mode, remain unchanged.
    GRE key support has been implemented for all non-ERSPAN tunnel types, including both IPv4 and IPv6.
    Additionally, modifications were made to the GRE packet header, data structure, and inbound/outbound
    packet processing to accommodate key configuration through CLI and API.
    
    Type: feature
    Change-Id: I222d585007fa264e7cc12c79d6ba9c63c044f133
    Signed-off-by: Masih Nilforoush <m.nilforoush@x-ion.de>

--- test/vpp_gre_interface.py
+++ test/vpp_gre_interface.py
@@ -17,6 +17,7 @@ class VppGreInterface(VppInterface):
         mode=None,
         flags=0,
         session=0,
+        gre_key=0,
     ):
         """Create VPP GRE interface"""
         super(VppGreInterface, self).__init__(test)
@@ -24,6 +25,7 @@ class VppGreInterface(VppInterface):
         self.t_dst = dst_ip
         self.t_outer_table = outer_table_id
         self.t_session = session
+        self.t_gre_key = gre_key
         self.t_flags = flags
         self.t_type = type
         if not self.t_type:
@@ -33,45 +35,112 @@ class VppGreInterface(VppInterface):
             self.t_mode = VppEnum.vl_api_tunnel_mode_t.TUNNEL_API_MODE_P2P
 
     def add_vpp_config(self):
-        r = self.test.vapi.gre_tunnel_add_del(
-            is_add=1,
-            tunnel={
-                "src": self.t_src,
-                "dst": self.t_dst,
-                "outer_table_id": self.t_outer_table,
-                "instance": 0xFFFFFFFF,
-                "type": self.t_type,
-                "mode": self.t_mode,
-                "flags": self.t_flags,
-                "session_id": self.t_session,
-            },
-        )
-        self.set_sw_if_index(r.sw_if_index)
+        if self.t_gre_key != 0:
+            r = self.test.vapi.gre_tunnel_add_del_v2(
+                is_add=1,
+                tunnel={
+                    "src": self.t_src,
+                    "dst": self.t_dst,
+                    "outer_table_id": self.t_outer_table,
+                    "instance": 0xFFFFFFFF,
+                    "type": self.t_type,
+                    "mode": self.t_mode,
+                    "flags": self.t_flags,
+                    "session_id": self.t_session,
+                    "key": self.t_gre_key,
+                },
+            )
+            self.set_sw_if_index(r.sw_if_index)
+        else:
+            r = self.test.vapi.gre_tunnel_add_del(
+                is_add=1,
+                tunnel={
+                    "src": self.t_src,
+                    "dst": self.t_dst,
+                    "outer_table_id": self.t_outer_table,
+                    "instance": 0xFFFFFFFF,
+                    "type": self.t_type,
+                    "mode": self.t_mode,
+                    "flags": self.t_flags,
+                    "session_id": self.t_session,
+                },
+            )
+            self.set_sw_if_index(r.sw_if_index)
+
         self.generate_remote_hosts()
         self.test.registry.register(self, self.test.logger)
         return self
 
     def remove_vpp_config(self):
         self.unconfig()
-        self.test.vapi.gre_tunnel_add_del(
-            is_add=0,
-            tunnel={
-                "src": self.t_src,
-                "dst": self.t_dst,
-                "outer_table_id": self.t_outer_table,
-                "instance": 0xFFFFFFFF,
-                "type": self.t_type,
-                "mode": self.t_mode,
-                "flags": self.t_flags,
-                "session_id": self.t_session,
-            },
-        )
+
+        if self.t_gre_key != 0:
+            self.test.vapi.gre_tunnel_add_del_v2(
+                is_add=0,
+                tunnel={
+                    "src": self.t_src,
+                    "dst": self.t_dst,
+                    "outer_table_id": self.t_outer_table,
+                    "instance": 0xFFFFFFFF,
+                    "type": self.t_type,
+                    "mode": self.t_mode,
+                    "flags": self.t_flags,
+                    "session_id": self.t_session,
+                    "key": self.t_gre_key,
+                },
+            )
+        else:
+            self.test.vapi.gre_tunnel_add_del(
+                is_add=0,
+                tunnel={
+                    "src": self.t_src,
+                    "dst": self.t_dst,
+                    "outer_table_id": self.t_outer_table,
+                    "instance": 0xFFFFFFFF,
+                    "type": self.t_type,
+                    "mode": self.t_mode,
+                    "flags": self.t_flags,
+                    "session_id": self.t_session,
+                },
+            )
 
     def object_id(self):
         return "gre-%d" % self.sw_if_index
 
     def query_vpp_config(self):
-        return self.test.vapi.gre_tunnel_dump(sw_if_index=self._sw_if_index)
+        if not hasattr(self, "sw_if_index") or self.sw_if_index == 0xFFFFFFFF:
+            return False
+
+        try:
+            if self.t_gre_key != 0:
+                dump = self.test.vapi.gre_tunnel_dump_v2(sw_if_index=self.sw_if_index)
+            else:
+                dump = self.test.vapi.gre_tunnel_dump(sw_if_index=self.sw_if_index)
+
+            if not dump:
+                return False
+
+            tunnel = dump[0].tunnel
+
+            if (
+                str(tunnel.src) != str(self.t_src)
+                or str(tunnel.dst) != str(self.t_dst)
+                or tunnel.outer_table_id != self.t_outer_table
+                or tunnel.type != self.t_type
+                or tunnel.mode != self.t_mode
+                or tunnel.flags != self.t_flags
+                or tunnel.session_id != self.t_session
+            ):
+                return False
+
+            if self.t_gre_key != 0:
+                if not hasattr(tunnel, "key") or tunnel.key != self.t_gre_key:
+                    return False
+
+            return True
+
+        except Exception:
+            return False
 
     @property
     def remote_ip(self):
