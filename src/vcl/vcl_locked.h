bd5ba003d3f125925a1b43de481f3b20a75f8e2f
Florin Coras <fcoras@cisco.com>
2025-03-13 01:56:35 -0400

    vcl: avoid ldp intercept of localhost sockets
    Type: improvement
    
    Change-Id: I668f8cca9a4750f5df079d3971f44aafb9aa69a1
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/vcl/vcl_locked.h
+++ src/vcl/vcl_locked.h
@@ -58,8 +58,8 @@ int vls_app_create (char *app_name);
 unsigned char vls_use_eventfd (void);
 unsigned char vls_mt_wrk_supported (void);
 int vls_use_real_epoll (void);
-int vls_set_libc_epfd (vls_handle_t ep_vlsh, int libc_epfd);
-int vls_get_libc_epfd (vls_handle_t ep_vlsh);
+int vls_set_libc_fd (vls_handle_t vlsh, int libc_fd);
+int vls_get_libc_fd (vls_handle_t vlsh);
 void vls_register_vcl_worker (void);
 
 #endif /* SRC_VCL_VCL_LOCKED_H_ */
