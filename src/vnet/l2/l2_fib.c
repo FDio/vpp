FDio/vpp

commit 47a3d9975fa3af7a7537b565d6511dadc0df61fb

l2: input performance
Type: improvement

- cache the values form the BD on the input config to avoid loading
- avoid the short write long read on the sequence number
- use vlib_buffer_enqueue_to_next

Signed-off-by: Neale Ranns <nranns@cisco.com>
Change-Id: I33442b9104b457e4c638d26e9ad3bc965687a0bc

src/vnet/l2/l2_fib.c
@@ -146,6 +146,26 @@ l2fib_table_dump (u32 bd_index,
   *l2fe_res = ctx.l2fe_res;
 }
 
+void
+l2_fib_extract_seq_num (l2fib_seq_num_t sn, u8 * bd_sn, u8 * if_sn)
+{
+  *bd_sn = sn >> 8;
+  *if_sn = sn & 0xff;
+}
+
+u8 *
+format_l2_fib_seq_num (u8 * s, va_list * a)
+{
+  l2fib_seq_num_t sn = va_arg (*a, int);
+  u8 bd_sn, if_sn;
+
+  l2_fib_extract_seq_num (sn, &bd_sn, &if_sn);
+
+  s = format (s, "%3d/%-3d", bd_sn, if_sn);
+
+  return (s);
+}
+
 typedef struct l2fib_show_walk_ctx_t_
 {
   u8 first_entry;
@@ -207,12 +227,12 @@ l2fib_show_walk_cb (BVT (clib_bihash_kv) * kvp, void *arg)
 	}
 
       vlib_cli_output (ctx->vm,
-		       "%=19U%=7d%=7d %3d/%-3d%=9v%=7s%=7s%=5s%=30U",
+		       "%=19U%=7d%=7d %U%=9v%=7s%=7s%=5s%=30U",
 		       format_ethernet_address, key.fields.mac,
 		       key.fields.bd_index,
 		       result.fields.sw_if_index == ~0
 		       ? -1 : result.fields.sw_if_index,
-		       result.fields.sn.bd, result.fields.sn.swif, s,
+		       format_l2_fib_seq_num, result.fields.sn, s,
 		       l2fib_entry_result_is_set_STATIC (&result) ? "*" : "-",
 		       l2fib_entry_result_is_set_FILTER (&result) ? "*" : "-",
 		       l2fib_entry_result_is_set_BVI (&result) ? "*" : "-",
@@ -404,16 +424,13 @@ VLIB_CLI_COMMAND (clear_l2fib_cli, static) = {
 };
 /* *INDENT-ON* */
 
-static inline l2fib_seq_num_t
+static l2fib_seq_num_t
 l2fib_cur_seq_num (u32 bd_index, u32 sw_if_index)
 {
   l2_bridge_domain_t *bd_config = l2input_bd_config (bd_index);
-  /* *INDENT-OFF* */
-  return (l2fib_seq_num_t) {
-    .swif = *l2fib_swif_seq_num (sw_if_index),
-    .bd = bd_config->seq_num,
-  };
-  /* *INDENT-ON* */
+
+  return l2_fib_mk_seq_num (bd_config->seq_num,
+			    l2_input_seq_num (sw_if_index));
 }
 
 /**
@@ -835,7 +852,7 @@ l2fib_start_ager_scan (vlib_main_t * vm)
 void
 l2fib_flush_int_mac (vlib_main_t * vm, u32 sw_if_index)
 {
-  *l2fib_swif_seq_num (sw_if_index) += 1;
+  l2_input_seq_num_inc (sw_if_index);
   l2fib_start_ager_scan (vm);
 }
 
@@ -989,7 +1006,8 @@ clib_error_t *
 l2fib_sw_interface_up_down (vnet_main_t * vnm, u32 sw_if_index, u32 flags)
 {
   l2_input_config_t *config = l2input_intf_config (sw_if_index);
-  if ((flags & VNET_SW_INTERFACE_FLAG_ADMIN_UP) == 0 && config->bridge)
+  if ((flags & VNET_SW_INTERFACE_FLAG_ADMIN_UP) == 0 &&
+      l2_input_is_bridge (config))
     l2fib_flush_int_mac (vnm->vlib_main, sw_if_index);
   return 0;
 }
@@ -1136,8 +1154,8 @@ l2fib_scan (vlib_main_t * vm, f64 start_time, u8 event_only)
 	      /* start aging processing */
 	      u32 bd_index = key.fields.bd_index;
 	      u32 sw_if_index = result.fields.sw_if_index;
-	      u16 sn = l2fib_cur_seq_num (bd_index, sw_if_index).as_u16;
-	      if (result.fields.sn.as_u16 != sn)
+	      u16 sn = l2fib_cur_seq_num (bd_index, sw_if_index);
+	      if (result.fields.sn != sn)
 		goto age_out;	/* stale mac */
 
 	      l2_bridge_domain_t *bd_config =

src/vnet/CMakeLists.txt
@@ -176,6 +176,7 @@ list(APPEND VNET_SOURCES
   l2/l2_flood.c
   l2/l2_fwd.c
   l2/l2_input.c
+  l2/l2_input_node.c
   l2/l2_input_vtr.c
   l2/l2_learn.c
   l2/l2_output.c
@@ -196,7 +197,7 @@ list(APPEND VNET_MULTIARCH_SOURCES
   l2/l2_patch.c
   l2/l2_in_out_feat_arc.c
   l2/l2_input_classify.c
-  l2/l2_input.c
+  l2/l2_input_node.c
   l2/l2_output_classify.c
   l2/l2_flood.c
   l2/l2_uu_fwd.c

src/vnet/dpo/dvr_dpo.c
@@ -110,7 +110,8 @@ dvr_dpo_add_or_lock (u32 sw_if_index,
 
         config = l2input_intf_config (sw_if_index);
 
-        if (config->bridge || config->xconnect)
+        if (l2_input_is_bridge(config) ||
+            l2_input_is_xconnect(config))
         {
             dd->dd_reinject = DVR_REINJECT_L2;
         }

src/vnet/ethernet/interface.c
@@ -496,10 +496,10 @@ simulated_ethernet_interface_tx (vlib_main_t * vm,
 
   /* Ordinarily, this is the only config lookup. */
   config = l2input_intf_config (vnet_buffer (b[0])->sw_if_index[VLIB_TX]);
-  next_index =
-    config->bridge ? VNET_SIMULATED_ETHERNET_TX_NEXT_L2_INPUT :
-    VNET_SIMULATED_ETHERNET_TX_NEXT_ETHERNET_INPUT;
-  new_tx_sw_if_index = config->bvi ? L2INPUT_BVI : ~0;
+  next_index = (l2_input_is_bridge (config) ?
+		VNET_SIMULATED_ETHERNET_TX_NEXT_L2_INPUT :
+		VNET_SIMULATED_ETHERNET_TX_NEXT_ETHERNET_INPUT);
+  new_tx_sw_if_index = l2_input_is_bvi (config) ? L2INPUT_BVI : ~0;
   new_rx_sw_if_index = vnet_buffer (b[0])->sw_if_index[VLIB_TX];
 
   while (n_left_from >= 4)
@@ -579,10 +579,10 @@ simulated_ethernet_interface_tx (vlib_main_t * vm,
 	{
 	  config = l2input_intf_config
 	    (vnet_buffer (b[0])->sw_if_index[VLIB_TX]);
-	  next_index =
-	    config->bridge ? VNET_SIMULATED_ETHERNET_TX_NEXT_L2_INPUT :
-	    VNET_SIMULATED_ETHERNET_TX_NEXT_ETHERNET_INPUT;
-	  new_tx_sw_if_index = config->bvi ? L2INPUT_BVI : ~0;
+	  next_index = (l2_input_is_bridge (config) ?
+			VNET_SIMULATED_ETHERNET_TX_NEXT_L2_INPUT :
+			VNET_SIMULATED_ETHERNET_TX_NEXT_ETHERNET_INPUT);
+	  new_tx_sw_if_index = l2_input_is_bvi (config) ? L2INPUT_BVI : ~0;
 	  new_rx_sw_if_index = vnet_buffer (b[0])->sw_if_index[VLIB_TX];
 	}
       next[0] = next_index;
@@ -602,11 +602,11 @@ simulated_ethernet_interface_tx (vlib_main_t * vm,
 	{
 	  config = l2input_intf_config
 	    (vnet_buffer (b[1])->sw_if_index[VLIB_TX]);
-	  next_index =
-	    config->bridge ? VNET_SIMULATED_ETHERNET_TX_NEXT_L2_INPUT :
-	    VNET_SIMULATED_ETHERNET_TX_NEXT_ETHERNET_INPUT;
+	  next_index = (l2_input_is_bridge (config) ?
+			VNET_SIMULATED_ETHERNET_TX_NEXT_L2_INPUT :
+			VNET_SIMULATED_ETHERNET_TX_NEXT_ETHERNET_INPUT);
 	  new_rx_sw_if_index = vnet_buffer (b[1])->sw_if_index[VLIB_TX];
-	  new_tx_sw_if_index = config->bvi ? L2INPUT_BVI : ~0;
+	  new_tx_sw_if_index = l2_input_is_bvi (config) ? L2INPUT_BVI : ~0;
 	}
       next[1] = next_index;
       vnet_buffer (b[1])->sw_if_index[VLIB_RX] = new_rx_sw_if_index;
@@ -625,11 +625,11 @@ simulated_ethernet_interface_tx (vlib_main_t * vm,
 	{
 	  config = l2input_intf_config
 	    (vnet_buffer (b[2])->sw_if_index[VLIB_TX]);
-	  next_index =
-	    config->bridge ? VNET_SIMULATED_ETHERNET_TX_NEXT_L2_INPUT :
-	    VNET_SIMULATED_ETHERNET_TX_NEXT_ETHERNET_INPUT;
+	  next_index = (l2_input_is_bridge (config) ?
+			VNET_SIMULATED_ETHERNET_TX_NEXT_L2_INPUT :
+			VNET_SIMULATED_ETHERNET_TX_NEXT_ETHERNET_INPUT);
 	  new_rx_sw_if_index = vnet_buffer (b[2])->sw_if_index[VLIB_TX];
-	  new_tx_sw_if_index = config->bvi ? L2INPUT_BVI : ~0;
+	  new_tx_sw_if_index = l2_input_is_bvi (config) ? L2INPUT_BVI : ~0;
 	}
       next[2] = next_index;
       vnet_buffer (b[2])->sw_if_index[VLIB_RX] = new_rx_sw_if_index;
@@ -648,11 +648,11 @@ simulated_ethernet_interface_tx (vlib_main_t * vm,
 	{
 	  config = l2input_intf_config
 	    (vnet_buffer (b[3])->sw_if_index[VLIB_TX]);
-	  next_index =
-	    config->bridge ? VNET_SIMULATED_ETHERNET_TX_NEXT_L2_INPUT :
-	    VNET_SIMULATED_ETHERNET_TX_NEXT_ETHERNET_INPUT;
+	  next_index = (l2_input_is_bridge (config) ?
+			VNET_SIMULATED_ETHERNET_TX_NEXT_L2_INPUT :
+			VNET_SIMULATED_ETHERNET_TX_NEXT_ETHERNET_INPUT);
 	  new_rx_sw_if_index = vnet_buffer (b[3])->sw_if_index[VLIB_TX];
-	  new_tx_sw_if_index = config->bvi ? L2INPUT_BVI : ~0;
+	  new_tx_sw_if_index = l2_input_is_bvi (config) ? L2INPUT_BVI : ~0;
 	}
       next[3] = next_index;
       vnet_buffer (b[3])->sw_if_index[VLIB_RX] = new_rx_sw_if_index;
@@ -676,10 +676,10 @@ simulated_ethernet_interface_tx (vlib_main_t * vm,
 	{
 	  config = l2input_intf_config
 	    (vnet_buffer (b[0])->sw_if_index[VLIB_TX]);
-	  next_index =
-	    config->bridge ? VNET_SIMULATED_ETHERNET_TX_NEXT_L2_INPUT :
-	    VNET_SIMULATED_ETHERNET_TX_NEXT_ETHERNET_INPUT;
-	  new_tx_sw_if_index = config->bvi ? L2INPUT_BVI : ~0;
+	  next_index = (l2_input_is_bridge (config) ?
+			VNET_SIMULATED_ETHERNET_TX_NEXT_L2_INPUT :
+			VNET_SIMULATED_ETHERNET_TX_NEXT_ETHERNET_INPUT);
+	  new_tx_sw_if_index = l2_input_is_bvi (config) ? L2INPUT_BVI : ~0;
 	  new_rx_sw_if_index = vnet_buffer (b[0])->sw_if_index[VLIB_TX];
 	}
       next[0] = next_index;

src/vnet/interface.c
@@ -42,7 +42,6 @@
 #include <vnet/fib/ip6_fib.h>
 #include <vnet/adj/adj.h>
 #include <vnet/adj/adj_mcast.h>
-#include <vnet/l2/l2_input.h>
 
 typedef enum vnet_interface_helper_flags_t_
 {
@@ -636,18 +635,6 @@ vnet_delete_sw_interface (vnet_main_t * vnm, u32 sw_if_index)
     pool_elt_at_index (im->sw_interfaces, sw_if_index);
 
   /* Check if the interface has config and is removed from L2 BD or XConnect */
-  vlib_main_t *vm = vlib_get_main ();
-  l2_input_config_t *config;
-  if (sw_if_index < vec_len (l2input_main.configs))
-    {
-      config = vec_elt_at_index (l2input_main.configs, sw_if_index);
-      if (config->xconnect)
-	set_int_l2_mode (vm, vnm, MODE_L3, config->output_sw_if_index, 0,
-			 L2_BD_PORT_TYPE_NORMAL, 0, 0);
-      if (config->xconnect || config->bridge)
-	set_int_l2_mode (vm, vnm, MODE_L3, sw_if_index, 0,
-			 L2_BD_PORT_TYPE_NORMAL, 0, 0);
-    }
   vnet_clear_sw_interface_tag (vnm, sw_if_index);
 
   /* Bring down interface in case it is up. */

src/vnet/interface_cli.c
@@ -342,14 +342,7 @@ show_sw_interfaces (vlib_main_t * vm,
   if (show_features)
     {
       vnet_interface_features_show (vm, sw_if_index, verbose);
-
-      l2_input_config_t *l2_input = l2input_intf_config (sw_if_index);
-      u32 fb = l2_input->feature_bitmap;
-      /* intf input features are masked by bridge domain */
-      if (l2_input->bridge)
-	fb &= l2input_bd_config (l2_input->bd_index)->feature_bitmap;
-      vlib_cli_output (vm, "\nl2-input:\n%U", format_l2_input_features, fb,
-		       1);
+      vlib_cli_output (vm, "%U", format_l2_input_features, sw_if_index, 1);
 
       l2_output_config_t *l2_output = l2output_intf_config (sw_if_index);
       vlib_cli_output (vm, "\nl2-output:");
@@ -448,19 +441,7 @@ show_sw_interfaces (vlib_main_t * vm,
 	     (si->flags & VNET_SW_INTERFACE_FLAG_ADMIN_UP) ? "up" : "dn");
 
 	/* Display any L2 info */
-	l2_input_config_t *l2_input = l2input_intf_config (si->sw_if_index);
-	if (l2_input->bridge)
-	  {
-	    bd_main_t *bdm = &bd_main;
-	    u32 bd_id = l2input_main.bd_configs[l2_input->bd_index].bd_id;
-	    vlib_cli_output (vm, "  L2 bridge bd-id %d idx %d shg %d %s",
-			     bd_id, bd_find_index (bdm, bd_id), l2_input->shg,
-			     l2_input->bvi ? "bvi" : " ");
-	  }
-	else if (l2_input->xconnect)
-	  vlib_cli_output (vm, "  L2 xconnect %U",
-			   format_vnet_sw_if_index_name, vnm,
-			   l2_input->output_sw_if_index);
+	vlib_cli_output (vm, "%U", format_l2_input, si->sw_if_index);
 
 	/* *INDENT-OFF* */
 	/* Display any IP4 addressing info */

src/vnet/interface_format.c
@@ -555,7 +555,7 @@ format_vnet_buffer_opaque (u8 * s, va_list * args)
 
   s = format (s,
 	      "l2.feature_bitmap_input: %U, L2.feature_bitmap_output: %U",
-	      format_l2_input_features, o->l2.feature_bitmap, 0,
+	      format_l2_input_feature_bitmap, o->l2.feature_bitmap, 0,
 	      format_l2_output_features, o->l2.feature_bitmap, 0);
   vec_add1 (s, '\n');
 

src/vnet/l2/l2_api.c
@@ -115,7 +115,7 @@ vl_api_l2_xconnect_dump_t_handler (vl_api_l2_xconnect_dump_t * mp)
   pool_foreach (swif, im->sw_interfaces,
   ({
     config = vec_elt_at_index (l2im->configs, swif->sw_if_index);
-    if (config->xconnect)
+    if (l2_input_is_xconnect(config))
       send_l2_xconnect_details (reg, mp->context, swif->sw_if_index,
                                 config->output_sw_if_index);
   }));
@@ -236,7 +236,7 @@ vl_api_l2fib_add_del_t_handler (vl_api_l2fib_add_del_t * mp)
 	    {
 	      l2_input_config_t *config;
 	      config = vec_elt_at_index (l2im->configs, sw_if_index);
-	      if (config->bridge == 0)
+	      if (!l2_input_is_bridge (config))
 		{
 		  rv = VNET_API_ERROR_INVALID_SW_IF_INDEX;
 		  goto bad_sw_if_index;

commit ebdc1071de4d9286c152c0548a0fb7a411f8d898

l2: stale cached value input->bd_seq_num
bd_seq_num is cached to l2_input_config to save a few cycles for lookup.
However, when the value is changed, we forgot to update the cache.

Type: fix
Fixes: 47a3d9975fa3af7a7537b565d6511dadc0df61fb

Change-Id: I553c61f4b072c74a6bc7fe1bf56d98ec881c833f
Signed-off-by: Steven Luong <sluong@cisco.com>

src/vnet/l2/l2_fib.c
@@ -893,14 +893,22 @@ l2fib_flush_int_mac (vlib_main_t * vm, u32 sw_if_index)
   l2fib_start_ager_scan (vm);
 }
 
+static void
+l2fib_bd_seq_num_inc (u32 bd_index)
+{
+  l2_bridge_domain_t *bd_config = l2input_bd_config (bd_index);
+
+  bd_config->seq_num += 1;
+  bd_input_walk (bd_index, l2input_recache, NULL);
+}
+
 /**
     Flush all non static MACs in a bridge domain
 */
 void
 l2fib_flush_bd_mac (vlib_main_t * vm, u32 bd_index)
 {
-  l2_bridge_domain_t *bd_config = l2input_bd_config (bd_index);
-  bd_config->seq_num += 1;
+  l2fib_bd_seq_num_inc (bd_index);
   l2fib_start_ager_scan (vm);
 }
 
@@ -910,10 +918,16 @@ l2fib_flush_bd_mac (vlib_main_t * vm, u32 bd_index)
 void
 l2fib_flush_all_mac (vlib_main_t * vm)
 {
+  l2input_main_t *mp = &l2input_main;
   l2_bridge_domain_t *bd_config;
-  vec_foreach (bd_config, l2input_main.bd_configs)
+  u32 bd_index;
+
+  vec_foreach_index (bd_index, mp->bd_configs)
+  {
+    bd_config = vec_elt_at_index (mp->bd_configs, bd_index);
     if (bd_is_valid (bd_config))
-    bd_config->seq_num += 1;
+      l2fib_bd_seq_num_inc (bd_index);
+  }
 
   l2fib_start_ager_scan (vm);
 }