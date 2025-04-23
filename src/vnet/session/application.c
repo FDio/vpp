822d179d39f1174ca85e0d2db0a7b8ec746efa51
Florin Coras <fcoras@cisco.com>
2025-04-23 00:49:14 -0400

    session: add session eventing infra for apps
    Basic framework/supporting infrastructure for now.
    
    Start evt app with default collector with:
     app evt-collector enable [uri <uri>]
    
    Add/del collectors (only one supported for now) with:
     app evt-collector add <uri>
    
    External applications can request eventing to default collector. Builtin
    applications can also provide custom eventing functions.
    
    Type: feature
    
    Change-Id: I3547bfc9b258b33a4e8c60c161de75c21533b7f1
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/vnet/session/application.c
+++ src/vnet/session/application.c
@@ -17,6 +17,7 @@
 #include <vnet/session/application_interface.h>
 #include <vnet/session/application_namespace.h>
 #include <vnet/session/application_local.h>
+#include <vnet/session/application_eventing.h>
 #include <vnet/session/session.h>
 #include <vnet/session/segment_manager.h>
 
@@ -854,6 +855,9 @@ application_alloc_and_init (app_init_args_t *a)
   if (opts[APP_OPTIONS_PCT_FIRST_ALLOC])
     props->pct_first_alloc = opts[APP_OPTIONS_PCT_FIRST_ALLOC];
   props->segment_type = seg_type;
+
+  if (opts[APP_OPTIONS_FLAGS] & APP_OPTIONS_FLAGS_LOG_COLLECTOR)
+    app->cb_fns.app_evt_callback = app_evt_collector_get_cb_fn ();
 
   /* Add app to lookup by api_client_index table */
   if (!application_is_builtin (app))
