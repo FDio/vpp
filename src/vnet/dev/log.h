073962333725df1078183718fbbee5aaa884034e
Vratko Polak <vrpolak@cisco.com>
2025-04-23 17:38:03 +0200

    experiment: log_info for CSIT investigation
    Ticket: vpp-3602
    
    Change-Id: I70024981bed893175ab08b51c2098f35e521a0db
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- src/vnet/dev/log.h
+++ src/vnet/dev/log.h
@@ -6,9 +6,11 @@
 #define _VNET_DEV_LOG_H_
 
 #define log_debug(dev, f, ...)                                                \
-  vlib_log (VLIB_LOG_LEVEL_DEBUG, dev_log.class, "%U" f, format_vnet_dev_log, \
+  vlib_log (VLIB_LOG_LEVEL_INFO, dev_log.class, "%U" f, format_vnet_dev_log,  \
 	    dev, clib_string_skip_prefix (__func__, "vnet_dev_"),             \
 	    ##__VA_ARGS__)
+#define log_info(f, ...)                                                      \
+  vlib_log (VLIB_LOG_LEVEL_INFO, dev_log.class, f, 0, ##__VA_ARGS__)
 #define log_notice(dev, f, ...)                                               \
   vlib_log (VLIB_LOG_LEVEL_NOTICE, dev_log.class, "%U" f,                     \
 	    format_vnet_dev_log, dev, 0, ##__VA_ARGS__)
