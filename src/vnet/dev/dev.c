412a43ecacc509250e8fae826855d15cc1545b95
Vratko Polak <vrpolak@cisco.com>
2025-04-24 17:23:44 +0200

    experiment: log_info for CSIT investigation
    Ticket: vpp-3602
    
    Change-Id: I70024981bed893175ab08b51c2098f35e521a0db
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- src/vnet/dev/dev.c
+++ src/vnet/dev/dev.c
@@ -267,14 +267,21 @@ vnet_dev_feature_update_cb (u32 sw_if_index, u8 arc_index, u8 is_enable,
   u32 next_index = ~0;
   int update_runtime = 0;
 
+  log_info ("starting");
+  log_info ("arc %u (%s) vdm %u (%s)", arc_index, vnet_get_feature_arc_index(arc_index), vdm->eth_port_rx_feature_arc_index, vnet_get_feature_arc_index(vdm->eth_port_rx_feature_arc_index));
+
   if (arc_index != vdm->eth_port_rx_feature_arc_index)
     return;
+
+  log_info ("new arc index %u", vdm->eth_port_rx_feature_arc_index);
 
   hw = vnet_get_sup_hw_interface (vnm, sw_if_index);
   di = vnet_dev_get_dev_instance (hw->dev_instance);
 
   if (!di)
     return;
+
+  log_info ("instance exists");
 
   intf = di->is_primary_if ?
 	   vnet_dev_port_get_primary_if (di->port) :
@@ -285,8 +292,12 @@ vnet_dev_feature_update_cb (u32 sw_if_index, u8 arc_index, u8 is_enable,
   if (port == 0 || intf->sw_if_index != sw_if_index)
     return;
 
+  log_debug (port->dev, "port exists");
+
   if (vnet_have_features (arc_index, sw_if_index))
     {
+      log_debug (port->dev, "intended arc enabled");
+
       cm = &fm->feature_config_mains[arc_index];
       current_config_index =
 	vec_elt (cm->config_index_by_sw_if_index, sw_if_index);
@@ -295,6 +306,7 @@ vnet_dev_feature_update_cb (u32 sw_if_index, u8 arc_index, u8 is_enable,
       if (intf->feature_arc == 0 || intf->rx_next_index != next_index ||
 	  intf->current_config_index != current_config_index)
 	{
+	  log_debug (port->dev, "config difference detected");
 	  intf->current_config_index = current_config_index;
 	  intf->rx_next_index = next_index;
 	  intf->feature_arc_index = arc_index;
@@ -304,8 +316,12 @@ vnet_dev_feature_update_cb (u32 sw_if_index, u8 arc_index, u8 is_enable,
     }
   else
     {
+      log_debug (port->dev, "intended arc not enabled");
+
       if (intf->feature_arc)
 	{
+	  log_debug (port->dev, "arc detected");
+
 	  intf->current_config_index = 0;
 	  intf->rx_next_index =
 	    intf->redirect_to_node ?
@@ -319,6 +335,8 @@ vnet_dev_feature_update_cb (u32 sw_if_index, u8 arc_index, u8 is_enable,
 
   if (update_runtime)
     {
+      log_debug (port->dev, "runtime update needed");
+
       foreach_vnet_dev_port_rx_queue (rxq, port)
 	vnet_dev_rx_queue_rt_request (
 	  vm, rxq,
