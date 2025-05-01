84b7a7d4e137927d22a3eddf9fe75958a4a0d70c
Ivan Ivanets <iivanets@cisco.com>
2025-05-01 17:32:21 +0300

    crypto: add new handlers for cbc/ctr+hmac
    Type: feature
    
    Support combined enc/dec/chain + auth algorithms with aes-cbc/ctr for
    openssl, native and ipsecmb crypto engines
    
    Change-Id: I4c970c57c3fce1e78fe25ea3d4a9c1de3b4de0c0
    Signed-off-by: Ivan Ivanets <iivanets@cisco.com>

--- src/crypto_engines/native/aes_cbc.c
+++ src/crypto_engines/native/aes_cbc.c
@@ -17,9 +17,8 @@
 
 #include <vlib/vlib.h>
 #include <vnet/plugin/plugin.h>
-#include <vnet/crypto/crypto.h>
-#include <native/crypto_native.h>
 #include <vppinfra/crypto/aes_cbc.h>
+#include "sha2.h"
 
 #if __GNUC__ > 4  && !__clang__ && CLIB_DEBUG == 0
 #pragma GCC optimize ("O3")
@@ -186,3 +185,53 @@ aes_cbc_key_exp_256 (vnet_crypto_key_t *key)
 foreach_aes_cbc_handler_type;
 #undef _
 
+extern u32 crypto_native_ops_hmac_sha2 (vlib_main_t *vm,
+					vnet_crypto_op_t *ops[], u32 n_ops,
+					vnet_crypto_op_chunk_t *chunks,
+					clib_sha2_type_t type);
+
+#define foreach_crypto_native_cbc_hmac_op                                     \
+  _ (128, 224, CLIB_SHA2_224, 14)                                             \
+  _ (192, 224, CLIB_SHA2_224, 14)                                             \
+  _ (256, 224, CLIB_SHA2_224, 14)                                             \
+  _ (128, 256, CLIB_SHA2_256, 16)                                             \
+  _ (192, 256, CLIB_SHA2_256, 16)                                             \
+  _ (256, 256, CLIB_SHA2_256, 16)
+
+#define _(k, b, clib_sha2, t)                                                 \
+  static u32 crypto_native_ops_enc_aes_##k##_cbc_hmac_sha##b##_tag##t (       \
+    vlib_main_t *vm, vnet_crypto_op_t *ops[], u32 n_ops)                      \
+  {                                                                           \
+    aes_ops_enc_aes_cbc (vm, ops, n_ops, AES_KEY_##k);                        \
+    crypto_native_ops_hmac_sha2 (vm, ops, n_ops, 0, clib_sha2);               \
+    return n_ops;                                                             \
+  }                                                                           \
+                                                                              \
+  static u32 crypto_native_ops_dec_aes_##k##_cbc_hmac_sha##b##_tag##t (       \
+    vlib_main_t *vm, vnet_crypto_op_t *ops[], u32 n_ops)                      \
+  {                                                                           \
+    crypto_native_ops_hmac_sha2 (vm, ops, n_ops, 0, clib_sha2);               \
+    aes_ops_dec_aes_cbc (vm, ops, n_ops, AES_KEY_##k);                        \
+    return n_ops;                                                             \
+  }                                                                           \
+                                                                              \
+  CRYPTO_NATIVE_OP_HANDLER (aes_##k##_cbc_hmac_sha##b##_tag##t##_enc) = {     \
+    .op_id = VNET_CRYPTO_OP_AES_##k##_CBC_SHA##b##_TAG##t##_ENC,              \
+    .fn = crypto_native_ops_enc_aes_##k##_cbc_hmac_sha##b##_tag##t,           \
+    .probe = aes_cbc_cpu_probe,                                               \
+  };                                                                          \
+                                                                              \
+  CRYPTO_NATIVE_OP_HANDLER (aes_##k##_cbc_hmac_sha##b##_tag##t##_dec) = {     \
+    .op_id = VNET_CRYPTO_OP_AES_##k##_CBC_SHA##b##_TAG##t##_DEC,              \
+    .fn = crypto_native_ops_dec_aes_##k##_cbc_hmac_sha##b##_tag##t,           \
+    .probe = aes_cbc_cpu_probe,                                               \
+  };                                                                          \
+                                                                              \
+  CRYPTO_NATIVE_KEY_HANDLER (aes_##k##_cbc_hmac_sha##b##_tag##t) = {          \
+    .alg_id = VNET_CRYPTO_ALG_AES_##k##_CBC_SHA##b##_TAG##t,                  \
+    .key_fn = aes_cbc_key_exp_##k,                                            \
+    .probe = aes_cbc_cpu_probe,                                               \
+  };
+
+foreach_crypto_native_cbc_hmac_op
+#undef _
--- src/crypto_engines/native/sha2.c
+++ src/crypto_engines/native/sha2.c
@@ -4,9 +4,7 @@
 
 #include <vlib/vlib.h>
 #include <vnet/plugin/plugin.h>
-#include <vnet/crypto/crypto.h>
-#include <native/crypto_native.h>
-#include <vppinfra/crypto/sha2.h>
+#include "sha2.h"
 
 static_always_inline u32
 crypto_native_ops_hash_sha2 (vlib_main_t *vm, vnet_crypto_op_t *ops[],
@@ -38,70 +36,6 @@ next:
     }
 
   return n_ops;
-}
-
-static_always_inline u32
-crypto_native_ops_hmac_sha2 (vlib_main_t *vm, vnet_crypto_op_t *ops[],
-			     u32 n_ops, vnet_crypto_op_chunk_t *chunks,
-			     clib_sha2_type_t type)
-{
-  crypto_native_main_t *cm = &crypto_native_main;
-  vnet_crypto_op_t *op = ops[0];
-  u32 n_left = n_ops;
-  clib_sha2_hmac_ctx_t ctx;
-  u8 buffer[64];
-  u32 sz, n_fail = 0;
-
-  for (; n_left; n_left--, op++)
-    {
-      clib_sha2_hmac_init (
-	&ctx, type, (clib_sha2_hmac_key_data_t *) cm->key_data[op->key_index]);
-      if (op->flags & VNET_CRYPTO_OP_FLAG_CHAINED_BUFFERS)
-	{
-	  vnet_crypto_op_chunk_t *chp = chunks + op->chunk_index;
-	  for (int j = 0; j < op->n_chunks; j++, chp++)
-	    clib_sha2_hmac_update (&ctx, chp->src, chp->len);
-	}
-      else
-	clib_sha2_hmac_update (&ctx, op->src, op->len);
-
-      clib_sha2_hmac_final (&ctx, buffer);
-
-      if (op->digest_len)
-	{
-	  sz = op->digest_len;
-	  if (op->flags & VNET_CRYPTO_OP_FLAG_HMAC_CHECK)
-	    {
-	      if ((memcmp (op->digest, buffer, sz)))
-		{
-		  n_fail++;
-		  op->status = VNET_CRYPTO_OP_STATUS_FAIL_BAD_HMAC;
-		  continue;
-		}
-	    }
-	  else
-	    clib_memcpy_fast (op->digest, buffer, sz);
-	}
-      else
-	{
-	  sz = clib_sha2_variants[type].digest_size;
-	  if (op->flags & VNET_CRYPTO_OP_FLAG_HMAC_CHECK)
-	    {
-	      if ((memcmp (op->digest, buffer, sz)))
-		{
-		  n_fail++;
-		  op->status = VNET_CRYPTO_OP_STATUS_FAIL_BAD_HMAC;
-		  continue;
-		}
-	    }
-	  else
-	    clib_memcpy_fast (op->digest, buffer, sz);
-	}
-
-      op->status = VNET_CRYPTO_OP_STATUS_COMPLETED;
-    }
-
-  return n_ops - n_fail;
 }
 
 static void *
