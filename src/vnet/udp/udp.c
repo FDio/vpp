ba2e65c4b3083e81e38fe2dde02441f35f8c4641
Florin Coras <fcoras@cisco.com>
2025-05-02 03:20:53 -0400

    session: add support for postponing transport cleanups
    Add new session layer api that allows transports to request postponed
    cleanups in session_input. Eventually this should become default
    especially if transport connections are to be scrapable before cleanup.
    
    Type: improvement
    
    Change-Id: I74beef41d5deed68efa664b78c1cf95e980b3bde
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/vnet/udp/udp.c
+++ src/vnet/udp/udp.c
@@ -108,8 +108,8 @@ udp_connection_cleanup (udp_connection_t * uc)
 void
 udp_connection_delete (udp_connection_t * uc)
 {
-  session_transport_delete_notify (&uc->connection);
-  udp_connection_cleanup (uc);
+  session_transport_delete_request (&uc->connection, udp_connection_cleanup);
+  //   udp_connection_cleanup (uc);
 }
 
 static void
