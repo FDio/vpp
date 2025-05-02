0c05c0149651c2514b20e9e4a574b92c8b14344c
Florin Coras <fcoras@cisco.com>
2025-05-02 03:20:53 -0400

    session: add support for postponing transport cleanups
    Add new session layer api that allows transports to request postponed
    cleanups in session_input. Eventually this should become default
    especially if transport connections are to be scrapable before cleanup.
    
    Type: improvement
    
    Change-Id: I74beef41d5deed68efa664b78c1cf95e980b3bde
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/vnet/session/session_input.c
+++ src/vnet/session/session_input.c
@@ -55,7 +55,12 @@ app_worker_del_all_events (app_worker_t *app_wrk)
 	    case SESSION_CTRL_EVT_CLEANUP:
 	      s = session_get (evt->as_u64[0] & 0xffffffff, thread_index);
 	      if (evt->as_u64[0] >> 32 != SESSION_CLEANUP_SESSION)
-		break;
+		{
+		  if (evt->as_u64[1])
+		    transport_cleanup_cb ((void *) evt->as_u64[1],
+					  session_get_transport (s));
+		  break;
+		}
 	      uword_to_pointer (evt->as_u64[1], void (*) (session_t * s)) (s);
 	      break;
 	    case SESSION_CTRL_EVT_HALF_CLEANUP:
@@ -245,7 +250,13 @@ app_worker_flush_events_inline (app_worker_t *app_wrk,
 		app->cb_fns.session_cleanup_callback (s, evt->as_u64[0] >> 32);
 	    }
 	  if (evt->as_u64[0] >> 32 != SESSION_CLEANUP_SESSION)
-	    break;
+	    {
+	      /* postponed cleanup requested */
+	      if (evt->as_u64[1])
+		transport_cleanup_cb ((void *) evt->as_u64[1],
+				      session_get_transport (s));
+	      break;
+	    }
 	  uword_to_pointer (evt->as_u64[1], void (*) (session_t * s)) (s);
 	  break;
 	case SESSION_CTRL_EVT_HALF_CLEANUP:
