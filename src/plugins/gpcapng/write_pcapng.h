a2b6b798ac48b62b8c58e5a2926c91a067098faa
Andrew Yourtchenko <ayourtch@gmail.com>
2025-05-06 14:23:31 +0200

    gpcapng: first draft
    A new plugin which allows flexible capturing of packets -
    with enhanced support for GENEVE tunnels,
    with multiple backends for writing the packets:
    
    - pcapng file
    - streaming HTTP PUT
    
    This PoC needs to be heavily edited still.
    
    Missing bits
    
    - time limit
    - packet counter limit
    - capture size limit (e.g. capture only headers)
    
    Type: feature
    Change-Id: I56afbb64ac1a352198bfb313d5804f02c31e9843
    Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>

---
+++ src/plugins/gpcapng/write_pcapng.h
@@ -0,0 +1,7 @@
+u8 *get_pcapng_shb_vec ();
+
+u8 *get_pcapng_idb (u8 **orig_block, u32 if_index, const char *if_name);
+
+int vec_add_pcapng_epb (u8 **vec_out, void *context, u32 if_index,
+			u64 timestamp, u32 orig_len, void *packet_data,
+			u32 packet_len);
