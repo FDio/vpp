30c6744285c52ad31b773714b6ff3dd80ac3ed19
Andrew Yourtchenko <ayourtch@gmail.com>
2025-05-06 14:23:31 +0200

    gpcapng: first draft
    A new plugin which allows flexible capturing of packets -
    with enhanced support for GENEVE tunnels,
    with multiple backends for writing the packets:
    
    - pcapng file
    - streaming HTTP PUT with chunked encoding
    
    This PoC needs to be heavily edited still.
    
    Missing bits
    
    - time limit
    - packet counter limit
    - capture size limit (e.g. capture only headers)
    
    Type: feature
    Change-Id: I56afbb64ac1a352198bfb313d5804f02c31e9843
    Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>

---
+++ src/plugins/gpcapng/exports.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2024 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef included_gpcapng_exports_h
+#define included_gpcapng_exports_h
+
+/*
+ * This file contains the declarations for external consumption,
+ * along with the necessary dependent includes.
+ */
+
+#define GPCAPNG_PLUGIN_EXTERNAL_EXPORTS
+
+#include <vlib/unix/plugin.h>
+
+#include "gpcapng_filter_api.h"
+#include "public_inlines.h"
+
+/* Export function declaration */
+clib_error_t *gpcapng_plugin_methods_vtable_init (gpcapng_plugin_methods_t *m);
+
+#endif /* included_gpcapng_exports_h */
