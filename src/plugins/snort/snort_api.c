7c439d236a96ad57bfb9a16a73ff8965d47edcbc
Damjan Marion <damarion@cisco.com>
2025-05-06 17:45:16 +0200

    snort: plugin rework
    Changes:
      - support for multiple qpairs per thread
      - ability for daq to connect to multiple instances
      - ability for daq to connect to specific qpairs
      - added ovnership to each qpair to avoid multiple clients using same
        queue
      - enabled packet rewrite
      - enqueue and dequeue nodes are decoupled from feature arcs so they
        can be used in different datapaths
      - feature arc doesn't support multiple-instances anymore, use multiple
        qpairs for load-balancing
    
    sample snort invocation:
    
    snort \
      <standard args> \
      --max-packet-threads 3 \  # number of threads snort will launch
      --daq-dir <daq dir> \     # path to dir with libdaq_bpp.so
      --daq vpp \               # enable VPP daq
      --daq-var <path> \        # optional, path to snort.sock
      --daq-var debug \         # optional, turn on debug prints
      -i vpp0:1 \               # attach instance to vpp0 instance qpair 1.0
      -i vpp0:1.1 \             # attach instance to vpp0 instance qpair 1.1
      -i vpp0:2.0,2.1,0 \       # attach instance to vpp0 instance qpairs 2.0, 2.1 and 0.0
    
    qpair is identified as x.y where x is thread_id and y is queue_id, .y
    part can be ommited if queue_id is 0.
    
    Type: feature
    Change-Id: I4f65b9a12ec71b115b9cb2da814c7918ddf6191d
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- src/plugins/snort/snort_api.c
+++ src/plugins/snort/snort_api.c
@@ -27,13 +27,6 @@ static u32 snort_base_msg_id;
 #include <vlibapi/api.h>
 #include <sys/eventfd.h>
 
-VLIB_REGISTER_LOG_CLASS (snort_log, static) = {
-  .class_name = "snort",
-};
-
-#define log_debug(fmt, ...) vlib_log_debug (snort_log.class, fmt, __VA_ARGS__)
-#define log_err(fmt, ...)   vlib_log_err (snort_log.class, fmt, __VA_ARGS__)
-
 static void
 vl_api_snort_instance_create_t_handler (vl_api_snort_instance_create_t *mp)
 {
@@ -46,8 +39,12 @@ vl_api_snort_instance_create_t_handler (vl_api_snort_instance_create_t *mp)
   u32 instance_index = ~0;
   snort_instance_t *si;
 
-  rv =
-    snort_instance_create (vm, name, min_log2 (queue_sz), drop_on_disconnect);
+  rv = snort_instance_create (vm,
+			      &(snort_instance_create_args_t){
+				.drop_on_disconnect = drop_on_disconnect,
+				.log2_queue_sz = min_log2 (queue_sz),
+			      },
+			      "%s", name);
 
   if ((si = snort_get_instance_by_name (name)))
     {
@@ -82,13 +79,19 @@ vl_api_snort_interface_attach_t_handler (vl_api_snort_interface_attach_t *mp)
   u32 sw_if_index = clib_net_to_host_u32 (mp->sw_if_index);
   u8 snort_dir = mp->snort_dir;
   int rv = VNET_API_ERROR_NO_SUCH_ENTRY;
+  int in = 0, out = 0;
 
   VALIDATE_SW_IF_INDEX (mp);
   switch (snort_dir)
     {
-    case SNORT_INPUT:
-    case SNORT_OUTPUT:
-    case SNORT_INOUT:
+    case 1:
+      in = 1;
+      break;
+    case 2:
+      out = 1;
+      break;
+    case 3:
+      in = out = 1;
       break;
     default:
       rv = VNET_API_ERROR_INVALID_ARGUMENT;
@@ -97,9 +100,8 @@ vl_api_snort_interface_attach_t_handler (vl_api_snort_interface_attach_t *mp)
   instance = snort_get_instance_by_index (instance_index);
   if (instance)
     {
-      rv = snort_interface_enable_disable (vm, (char *) instance->name,
-					   sw_if_index, 1 /* is_enable */,
-					   snort_dir);
+      rv = snort_interface_enable_disable (
+	vm, (char *) instance->name, sw_if_index, 1 /* is_enable */, in, out);
     }
   BAD_SW_IF_INDEX_LABEL;
   REPLY_MACRO (VL_API_SNORT_INTERFACE_ATTACH_REPLY);
@@ -116,7 +118,7 @@ send_snort_instance_details (const snort_instance_t *instance,
     VL_API_SNORT_INSTANCE_DETAILS, name_len, rp, context, ({
       rmp->instance_index = clib_host_to_net_u32 (instance->index);
       vl_api_vec_to_api_string (instance->name, &rmp->name);
-      rmp->snort_client_index = clib_host_to_net_u32 (instance->client_index);
+      rmp->snort_client_index = clib_host_to_net_u32 (0);
       rmp->shm_size = clib_host_to_net_u32 (instance->shm_size);
       rmp->shm_fd = clib_host_to_net_u32 (instance->shm_fd);
       rmp->drop_on_disconnect = instance->drop_on_disconnect;
@@ -174,98 +176,12 @@ vl_api_snort_instance_get_t_handler (vl_api_snort_instance_get_t *mp)
 }
 
 static void
-send_snort_interface_details (u32 sw_if_index, u32 instance_index,
-			      vl_api_registration_t *rp, u32 context)
-{
-  vl_api_snort_interface_details_t *rmp;
-
-  if (instance_index != ~0)
-    {
-      REPLY_MACRO_DETAILS4 (VL_API_SNORT_INTERFACE_DETAILS, rp, context, ({
-			      rmp->instance_index =
-				clib_host_to_net_u32 (instance_index);
-			      rmp->sw_if_index =
-				clib_host_to_net_u32 (sw_if_index);
-			    }));
-    }
-}
-
-static void
 vl_api_snort_interface_get_t_handler (vl_api_snort_interface_get_t *mp)
 {
-  snort_main_t *sm = snort_get_main ();
   vl_api_snort_interface_get_reply_t *rmp;
-  u32 sw_if_index;
-  u32 *instances;
-  u32 index;
   int rv = 0;
 
-  sw_if_index = clib_net_to_host_u32 (mp->sw_if_index);
-
-  if (sw_if_index == INDEX_INVALID)
-    {
-      /* clang-format off */
-      if (vec_len (sm->interfaces) == 0)
-	{
-	  REPLY_MACRO2 (VL_API_SNORT_INTERFACE_GET_REPLY, ({ rmp->cursor = ~0; }));
-	  return;
-	}
-
-      REPLY_AND_DETAILS_VEC_MACRO(
-	VL_API_SNORT_INTERFACE_GET_REPLY,
-	sm->interfaces,
-	mp, rmp, rv, ({
-          instances = vec_len(sm->interfaces[cursor].input_instance_indices) ?
-           sm->interfaces[cursor].input_instance_indices : sm->interfaces[cursor].output_instance_indices;
-          if (vec_len(instances) == 0)
-          {
-            index = ~0;
-          }
-          else {
-            index = instances[0];
-          }
-          send_snort_interface_details (cursor, index, rp, mp->context);
-	}))
-      /* clang-format on */
-    }
-  else
-    {
-      instances =
-	vec_len (sm->interfaces[sw_if_index].input_instance_indices) ?
-	  sm->interfaces[sw_if_index].input_instance_indices :
-	  sm->interfaces[sw_if_index].output_instance_indices;
-      if (vec_len (instances) == 0)
-	{
-	  index = ~0;
-	}
-      else
-	{
-	  index = instances[0];
-	}
-      if (snort_get_instance_by_index (index))
-	{
-	  vl_api_registration_t *rp =
-	    vl_api_client_index_to_registration (mp->client_index);
-
-	  if (rp == NULL)
-	    {
-	      return;
-	    }
-
-	  send_snort_interface_details (sw_if_index, *instances, rp,
-					mp->context);
-	}
-      else
-	{
-	  rv = VNET_API_ERROR_NO_SUCH_ENTRY;
-	}
-
-      /* clang-format off */
-      REPLY_MACRO2 (VL_API_SNORT_INTERFACE_GET_REPLY, ({
-        rmp->cursor = INDEX_INVALID;
-      }));
-      /* clang-format on */
-    }
+  REPLY_MACRO2 (VL_API_SNORT_INTERFACE_GET_REPLY, ({ rmp->cursor = ~0; }));
 }
 
 static void
@@ -274,23 +190,12 @@ send_snort_client_details (const snort_client_t *client,
 {
   snort_main_t *sm = snort_get_main ();
   vl_api_snort_client_details_t *rmp;
-  snort_instance_t *instance;
 
-  if (client->instance_index == ~0)
-    {
-      return;
-    }
-
-  instance = pool_elt_at_index (sm->instances, client->instance_index);
-  if (instance)
-    {
-      REPLY_MACRO_DETAILS4 (VL_API_SNORT_CLIENT_DETAILS, rp, context, ({
-			      rmp->instance_index =
-				clib_host_to_net_u32 (client->instance_index);
-			      rmp->client_index =
-				clib_host_to_net_u32 (client - sm->clients);
-			    }));
-    }
+  REPLY_MACRO_DETAILS4 (VL_API_SNORT_CLIENT_DETAILS, rp, context, ({
+			  rmp->instance_index = 0;
+			  rmp->client_index =
+			    clib_host_to_net_u32 (client - sm->clients);
+			}));
 }
 
 static void
@@ -347,21 +252,11 @@ static void
 vl_api_snort_client_disconnect_t_handler (vl_api_snort_client_disconnect_t *mp)
 {
   vlib_main_t *vm = vlib_get_main ();
-  snort_main_t *sm = snort_get_main ();
-  snort_client_t *client;
   vl_api_snort_client_disconnect_reply_t *rmp;
   u32 client_index = clib_net_to_host_u32 (mp->snort_client_index);
   int rv = 0;
 
-  if (pool_is_free_index (sm->clients, client_index))
-    {
-      rv = VNET_API_ERROR_NO_SUCH_ENTRY;
-    }
-  else
-    {
-      client = pool_elt_at_index (sm->clients, client_index);
-      rv = snort_instance_disconnect (vm, client->instance_index);
-    }
+  rv = snort_client_disconnect (vm, client_index);
 
   REPLY_MACRO (VL_API_SNORT_CLIENT_DISCONNECT_REPLY);
 }
@@ -370,10 +265,8 @@ static void
 vl_api_snort_instance_disconnect_t_handler (
   vl_api_snort_instance_disconnect_t *mp)
 {
-  vlib_main_t *vm = vlib_get_main ();
   vl_api_snort_instance_disconnect_reply_t *rmp;
-  u32 instance_index = clib_net_to_host_u32 (mp->instance_index);
-  int rv = snort_instance_disconnect (vm, instance_index);
+  int rv = VNET_API_ERROR_UNSUPPORTED;
 
   REPLY_MACRO (VL_API_SNORT_INSTANCE_DISCONNECT_REPLY);
 }
@@ -396,28 +289,17 @@ vl_api_snort_interface_detach_t_handler (vl_api_snort_interface_detach_t *mp)
 static void
 vl_api_snort_input_mode_get_t_handler (vl_api_snort_input_mode_get_t *mp)
 {
-  snort_main_t *sm = &snort_main;
   vl_api_snort_input_mode_get_reply_t *rmp;
-  int rv = 0;
+  int rv = VNET_API_ERROR_UNSUPPORTED;
 
-  REPLY_MACRO2 (VL_API_SNORT_INPUT_MODE_GET_REPLY, ({
-		  rmp->snort_mode = clib_host_to_net_u32 (sm->input_mode);
-		}));
+  REPLY_MACRO (VL_API_SNORT_INPUT_MODE_GET_REPLY);
 }
 
 static void
 vl_api_snort_input_mode_set_t_handler (vl_api_snort_input_mode_set_t *mp)
 {
-  vlib_main_t *vm = vlib_get_main ();
   vl_api_snort_input_mode_set_reply_t *rmp;
-  u8 mode = mp->input_mode;
-  int rv = 0;
-
-  if (mode != VLIB_NODE_STATE_INTERRUPT && mode != VLIB_NODE_STATE_POLLING)
-    {
-      clib_error_return (0, "invalid input mode %u", mode);
-    }
-  snort_set_node_mode (vm, mode);
+  int rv = VNET_API_ERROR_UNSUPPORTED;
 
   REPLY_MACRO (VL_API_SNORT_INPUT_MODE_SET_REPLY);
 }
