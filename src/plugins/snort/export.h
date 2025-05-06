e070068b768cbd72173a945d9954d1200c69bb08
Damjan Marion <damarion@cisco.com>
2025-05-06 17:45:16 +0200

    snort: plugin rework
    Changes:
      - support for multiple qpairs per thread
      - ability for daq to connect to multiple instances
      - ability for daq to connect to specific qpairs
      - added ownership to each qpair to avoid multiple clients using same one
      - enabled packet rewrite
      - enqueue and dequeue nodes are decoupled from feature arcs so they
        can be used in different datapaths
      - feature arc doesn't support multiple-instances anymore, use multiple
        qpairs for load-balancing
      - per-instance drop decisions based on drop bitmap
    
    sample snort invocation:
    
    snort \
      <standard args> \
      --max-packet-threads 3 \  # number of threads snort will launch
      --daq-dir <daq dir> \     # path to dir with libdaq_bpp.so
      --daq vpp \               # enable VPP daq
      --daq-var <path> \        # optional, path to snort.sock
      --daq-var debug \         # optional, turn on debug prints
      -i vpp0:1 \               # attach instance to vpp0 instance qpair 1.0
      -i vpp0:1.1 \             # attach instance to vpp0 instance qpair 1.1
      -i vpp0:2.0,2.1,0 \       # attach instance to vpp0 instance qpairs 2.0, 2.1 and 0.0
    
    qpair is identified as x.y where x is thread_id and y is queue_id, .y
    part can be ommited if queue_id is 0.
    
    Type: feature
    
    Change-Id: I4f65b9a12ec71b115b9cb2da814c7918ddf6191d
    Signed-off-by: Damjan Marion <damarion@cisco.com>
    Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>

---
+++ src/plugins/snort/export.h
@@ -0,0 +1,37 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright(c) 2021 Cisco Systems, Inc.
+ * Copyright(c) 2024 Arm Limited
+ */
+
+#ifndef __snort_export_h__
+#define __snort_export_h__
+
+#include <vppinfra/error.h>
+#include <vppinfra/socket.h>
+#include <vppinfra/file.h>
+#include <vlib/vlib.h>
+
+typedef struct
+{
+  u8 log2_queue_sz;
+  u8 drop_on_disconnect;
+  u8 qpairs_per_thread;
+  u8 drop_bitmap; /* bits indexed by verdict, 0 = pass, 1 = drop */
+} snort_instance_create_args_t;
+
+typedef u16 snort_instance_index_t;
+
+typedef int (snort_instance_create_fn_t) (vlib_main_t *vm,
+					  snort_instance_create_args_t *args,
+					  char *fmt, ...);
+typedef int (snort_instance_delete_fn_t) (
+  vlib_main_t *vm, snort_instance_index_t instance_index);
+
+typedef int (snort_instance_get_index_by_name_fn_t) (
+  vlib_main_t *vm, const char *name, snort_instance_index_t *instance_index);
+
+snort_instance_create_fn_t snort_instance_create;
+snort_instance_delete_fn_t snort_instance_delete;
+snort_instance_get_index_by_name_fn_t snort_instance_get_index_by_name;
+
+#endif /* __snort_export_h__ */
