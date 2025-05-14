eb98d606be959082325b775cef304d8eb2139e70
Vratko Polak <vrpolak@cisco.com>
2025-05-14 16:28:59 +0200

    experiment: VCL timing
    Change-Id: Ibf4a27b93eb5b51960a075eddc9ab132397212fb
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- src/vcl/vppcom.c
+++ src/vcl/vppcom.c
@@ -1282,7 +1282,7 @@ vcl_worker_wait_mq (vcl_worker_t *wrk, u32 session_handle,
    * as we'd like to only be woken up on events, but since multiple threads may
    * be waiting on the same mq, events may be missed. If waiting on session io
    * events, return if session is ready. Otherwise return if mq has event */
-  while (svm_msg_q_timedwait (wrk->app_event_queue, 1e-3))
+  while (svm_msg_q_timedwait (wrk->app_event_queue, 1e-2))
     {
       if (s)
 	{
