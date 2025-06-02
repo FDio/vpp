55061bcfdad8b50e75fde0ae6812fd169e89b354
Florin Coras <fcoras@cisco.com>
2025-06-02 13:58:46 -0400

    hs-test: use base image container for container builds
    Avoid multiple updates/downloads of dependencies.
    
    Type: improvement
    
    Change-Id: Ic770dc00a42cb5aa8c780aebb72beef390200363
    Signed-off-by: Florin Coras <fcoras@cisco.com>
    Signed-off-by: Matus Fabian <matfabia@cisco.com>

--- extras/hs-test/script/build_curl.sh
+++ extras/hs-test/script/build_curl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 wget https://github.com/stunnel/static-curl/releases/download/8.5.0/curl-static-"$TARGETARCH"-8.5.0.tar.xz
 tar -xvf ./curl-static-"$TARGETARCH"-8.5.0.tar.xz
 cp curl /usr/bin/curl
