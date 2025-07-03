49719f66e66a9db070bd1fecdfe291579e3df3fb
hedi bouattour <hedibouattour2010@gmail.com>
2025-07-03 12:56:00 +0000

    cnat: support encapsulation and session cleanup on backend deletion
    *The writeback path is extended to reconstruct the correct return session
    key for encapsulated packets by using the original 5-tuple stored at
    session creation time, rather than parsing the (now encapsulated) packet
    headers directly.
    
    *To support stable backend references from sessions, cnat_ep_trk_t structs
    are moved from per-translation embedded vectors into a global pool
    (cnat_ep_trk_pool). Sessions track their backend by pool index.
    When a backend is removed, it is marked for deletion and
    the session scanner cleans up all sessions referencing it
    before freeing the backend.
    
    Type: improvement
    Change-Id: Ibfc14bc9d9bf06a817700ece911867fcab5cdb2c
    Signed-off-by: hedi bouattour <hedibouattour2010@gmail.com>

--- src/plugins/cnat/cnat_inline.h
+++ src/plugins/cnat/cnat_inline.h
@@ -53,7 +53,6 @@ always_inline index_t
 cnat_timestamp_alloc (u32 fib_index, bool is_v6)
 {
   cnat_timestamp_mpool_t *ctm = &cnat_timestamps;
-  int *sessions_per_vrf = is_v6 ? ctm->sessions_per_vrf_ip6 : ctm->sessions_per_vrf_ip4;
   u32 log2_pool_sz = ctm->log2_pool_sz;
   u32 pool_sz = 1 << log2_pool_sz;
   cnat_timestamp_t *pool;
@@ -62,9 +61,9 @@ cnat_timestamp_alloc (u32 fib_index, bool is_v6)
   u32 pidx;
 
   clib_rwlock_writer_lock (&ctm->ts_lock);
-
+  int *sessions_per_vrf = is_v6 ? ctm->sessions_per_vrf_ip6 : ctm->sessions_per_vrf_ip4;
   if (PREDICT_FALSE (vec_elt (sessions_per_vrf, fib_index) <= 0))
-    goto err; /* too many sessions... */
+    goto err;
 
   pidx = clib_bitmap_first_set (ctm->ts_free);
   if (PREDICT_FALSE (pidx >= vec_len (ctm->ts_pools)))
@@ -180,12 +179,21 @@ cnat_lookup_create_or_return (vlib_buffer_t *b, int rv, cnat_bihash_kv_t *bkey,
 			      bool alloc_if_not_found)
 {
   vnet_buffer2 (b)->session.flags = 0;
-  cnat_session_t *session = (cnat_session_t *) bvalue;
-  if (rv)
+  cnat_session_t *ksession = (cnat_session_t *) bkey;
+  if (PREDICT_TRUE (!rv))
+    {
+      cnat_session_t *session = (cnat_session_t *) bvalue;
+      vnet_buffer2 (b)->session.generic_flow_id = session->value.cs_session_index;
+      vnet_buffer2 (b)->session.state = session->value.cs_flags & CNAT_SESSION_IS_RETURN ?
+					  CNAT_LOOKUP_IS_RETURN :
+					  CNAT_LOOKUP_IS_OK;
+      cnat_timestamp_update (session->value.cs_session_index, now);
+    }
+  /* avoid garbage traffic */
+  else if (ksession->key.cs_5tuple.iproto != 0)
     {
       if (!alloc_if_not_found)
 	goto err;
-      cnat_session_t *ksession = (cnat_session_t *) bkey;
       index_t session_index = cnat_timestamp_new (now, ksession->key.fib_index, is_v6);
       ASSERT ((session_index < CNAT_MAX_SESSIONS || INDEX_INVALID == session_index));
       if (PREDICT_FALSE (session_index >= CNAT_MAX_SESSIONS))
@@ -196,14 +204,11 @@ cnat_lookup_create_or_return (vlib_buffer_t *b, int rv, cnat_bihash_kv_t *bkey,
       vnet_buffer2 (b)->session.generic_flow_id = ksession->value.cs_session_index;
       vnet_buffer2 (b)->session.state = CNAT_LOOKUP_IS_NEW;
       cnat_log_session_create (ksession);
-    }
-  else if (session->key.cs_5tuple.iproto != 0)
-    {
-      vnet_buffer2 (b)->session.generic_flow_id = session->value.cs_session_index;
-      vnet_buffer2 (b)->session.state = session->value.cs_flags & CNAT_SESSION_IS_RETURN ?
-					  CNAT_LOOKUP_IS_RETURN :
-					  CNAT_LOOKUP_IS_OK;
-      cnat_timestamp_update (session->value.cs_session_index, now);
+      cnat_timestamp_t *ts = cnat_timestamp_get (session_index);
+      // we put the original 5tuple in the rewrite of the session to use it
+      // later in the writeback if there is no nat (i.e no actual rewrites) in
+      // the case where we have actual rewrites this is going to be overridden
+      ts->cts_rewrites[CNAT_LOCATION_INPUT].tuple = ksession->key.cs_5tuple;
     }
   else
     goto err;
@@ -214,4 +219,45 @@ err:
   vnet_buffer2 (b)->session.generic_flow_id = 0;
   vnet_buffer2 (b)->session.state = CNAT_LOOKUP_IS_ERR;
 }
+
+/* INPUT node stores FWD@INPUT + RETURN@OUTPUT, OUTPUT node stores the reverse.
+ * FIB node stores both at FIB. This table maps a location to where its paired
+ * return rewrite lives. */
+static const cnat_session_location_t cnat_paired_location[] = {
+  [CNAT_LOCATION_INPUT] = CNAT_LOCATION_OUTPUT,
+  [CNAT_LOCATION_OUTPUT] = CNAT_LOCATION_INPUT,
+  [CNAT_LOCATION_FIB] = CNAT_LOCATION_FIB,
+};
+
+/* Fill session->key.cs_5tuple (swapped) and session->key.fib_index from the
+ * best available rewrite pair in the timestamp for the given direction.
+ * Priority: OUTPUT > FIB > INPUT. Falls back to the INPUT rewrite when no
+ * valid entry is found. */
+static_always_inline void
+cnat_get_rsession_from_ts (cnat_timestamp_t *ts, const cnat_timestamp_direction_t dir,
+			   cnat_session_t *session)
+{
+  cnat_timestamp_direction_t rdir = (dir == CNAT_IS_FWD) ? CNAT_IS_RETURN : CNAT_IS_FWD;
+  u8 locations[] = { CNAT_LOCATION_OUTPUT, CNAT_LOCATION_FIB, CNAT_LOCATION_INPUT };
+  int i;
+  for (i = 0; i < ARRAY_LEN (locations); i++)
+    {
+      u8 rw_index = locations[i] + dir;
+      u8 rrw_index = cnat_paired_location[locations[i]] + rdir;
+      u8 rw_bm = (1 << rw_index) | (1 << rrw_index);
+      if ((ts->ts_rw_bm & rw_bm) != rw_bm)
+	continue;
+      cnat_timestamp_rewrite_t *rw = &ts->cts_rewrites[rw_index];
+      if (rw->cts_flags & CNAT_TS_RW_FLAG_NO_NAT)
+	continue;
+      if (rw->tuple.iproto == 0)
+	continue;
+      cnat_5tuple_copy (&session->key.cs_5tuple, &rw->tuple, 1 /* swap */);
+      /* TODO: storing fib index in rewrite is only used in CNAT_LOCATION_FIB for now */
+      if (locations[i] == CNAT_LOCATION_FIB)
+	session->key.fib_index = ts->cts_rewrites[rrw_index].fib_index;
+      return;
+    }
+}
+
 #endif
