46bf686d835dd3ecf515d264653c8cf1f17ec592
Vladimir Zhigulin <vladimir.jigulin@travelping.com>
2025-07-04 11:10:44 +0200

    vlib: fix typo in ASAN stack size calculation
    Type: fix
    
    Change-Id: I771ca783854f704fc333a6dd857831ffe5d70bd3
    Signed-off-by: Vladimir Zhigulin <vladimir.jigulin@travelping.com>

--- src/vlib/node_funcs.h
+++ src/vlib/node_funcs.h
@@ -59,7 +59,7 @@ vlib_process_start_switch_stack (vlib_main_t * vm, vlib_process_t * p)
 #ifdef CLIB_SANITIZE_ADDR
   void *stack = p ? (void *) p->stack : vlib_thread_stacks[vm->thread_index];
   u32 stack_bytes =
-    p ? (1ULL < p->log2_n_stack_bytes) : VLIB_THREAD_STACK_SIZE;
+    p ? (1ULL << p->log2_n_stack_bytes) : VLIB_THREAD_STACK_SIZE;
   __sanitizer_start_switch_fiber (&vm->asan_stack_save, stack, stack_bytes);
 #endif
 }
