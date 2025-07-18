18fd9366555e341738f0fb14cf7693a4c0c5c6fc
Matus Fabian <matfabia@cisco.com>
2025-07-18 13:04:07 -0400

    http: h2 client multiplexing
    Type: improvement
    
    Change-Id: I768df864cbda26b0901528789b52a33e788c2258
    Signed-off-by: Matus Fabian <matfabia@cisco.com>

--- extras/hs-test/http2_test.go
+++ extras/hs-test/http2_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/edwarnicke/exechelper"
 	"strconv"
 	"strings"
 	"time"
@@ -11,7 +12,7 @@ import (
 
 func init() {
 	RegisterH2Tests(Http2TcpGetTest, Http2TcpPostTest, Http2MultiplexingTest, Http2TlsTest, Http2ContinuationTxTest, Http2ServerMemLeakTest,
-		Http2ClientGetTest, Http2ClientPostTest, Http2ClientPostPtrTest, Http2ClientGetRepeatTest)
+		Http2ClientGetTest, Http2ClientPostTest, Http2ClientPostPtrTest, Http2ClientGetRepeatTest, Http2ClientMultiplexingTest)
 	RegisterH2MWTests(Http2MultiplexingMWTest)
 	RegisterVethTests(Http2CliTlsTest, Http2ClientContinuationTest)
 }
@@ -231,6 +232,25 @@ func Http2ClientGetRepeatTest(s *Http2Suite) {
 	cmd := fmt.Sprintf("http client http2 repeat %d uri %s", 10, uri)
 	o := vpp.Vppctl(cmd)
 	s.Log(o)
+}
+
+func Http2ClientMultiplexingTest(s *Http2Suite) {
+	vpp := s.Containers.Vpp.VppInstance
+	serverAddress := s.HostAddr() + ":" + s.Ports.Port2
+
+	s.CreateNginxServer()
+	s.AssertNil(s.Containers.NginxServer.Start())
+
+	uri := "https://" + serverAddress + "/httpTestFile"
+	cmd := fmt.Sprintf("http client http2 streams %d repeat %d uri %s", 10, 10, uri)
+	o := vpp.Vppctl(cmd)
+	s.Log(o)
+	s.AssertContains(o, "10 request(s)")
+	logPath := s.Containers.NginxServer.GetHostWorkDir() + "/" + s.Containers.NginxServer.Name + "-access.log"
+	logContents, err := exechelper.Output("cat " + logPath)
+	s.Log(string(logContents))
+	s.AssertNil(err)
+	s.AssertContains(string(logContents), "conn_reqs=10")
 }
 
 func Http2ClientContinuationTest(s *VethsSuite) {
