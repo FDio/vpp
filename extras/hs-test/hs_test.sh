6f3cc4d3104ffd115295dc1214f9280a9fd383f5
Adrian Villin <avillin@cisco.com>
2025-07-21 14:08:14 +0200

    hs-test: containerize ginkgo
    - ginkgo will run in a container
    - replaced '--network=host' with '--network=container:ginkgo'
    - removed network namespaces from HST
    - updated goimports
    - fixed state hashes
    
    Type: improvement
    
    Change-Id: I5f4c5aa93437f40b36a8eb2ba3d6486cdfe23e09
    Signed-off-by: Adrian Villin <avillin@cisco.com>

--- extras/hs-test/hs_test.sh
+++ extras/hs-test/hs_test.sh
@@ -17,6 +17,7 @@ skip_names=()
 dryrun=
 no_color=
 perf=
+hs_root=
 
 for i in "$@"
 do
@@ -125,6 +126,9 @@ case "${i}" in
     --timeout=*)
         args="$args -timeout ${i#*=}"
         ;;
+    --hs_root=*)
+        hs_root="${i#*=}"
+        cd $hs_root
 esac
 done
 
@@ -185,6 +189,9 @@ fi
 if [ -n "${BUILD_NUMBER}" ]; then
        ginkgo_args="$ginkgo_args --no-color"
 fi
+
+mkdir -p .go_cache
+make build-go
 
 mkdir -p summary
 # shellcheck disable=SC2086
