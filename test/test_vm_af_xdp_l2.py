8cb1294c000eb8ce977962c384a509c1cd5408ec
Naveen Joy <najoy@cisco.com>
2025-07-21 14:59:34 -0700

    tests: af_xdp interface tests
    Type: test
    
    Change-Id: I81019d2ac8b793c11b22deab8a103b37b88eb75e
    Signed-off-by: Naveen Joy <najoy@cisco.com>

---
+++ test/test_vm_af_xdp_l2.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import unittest
+from framework import VppTestCase
+from vm_vpp_interfaces import (
+    TestSelector,
+    TestVPPInterfacesQemu,
+    generate_vpp_interface_tests,
+)
+from asfframework import VppTestRunner
+from vm_test_config import test_config
+import fcntl
+import time
+
+
+class TestVPPInterfacesQemuAfXDPL2(TestVPPInterfacesQemu, VppTestCase):
+    """Test af_xdp interfaces in L2 mode for IPv4/v6."""
+
+    # Set test_id(s) to run from vm_test_config
+    # The expansion of these numbers are included in the test docstring
+    tests_to_run = "28"
+
+    @classmethod
+    def setUpClass(cls):
+        # Create lock file to prevent concurrent test runs of af_xdp tests
+        # as they interfere with each other
+        cls.lock_file_path = "/tmp/vpp_af_xdp_test.lock"
+        cls.lock_file = open(cls.lock_file_path, "w")
+
+        # Wait for lock
+        attempt = 0
+        while True:
+            try:
+                fcntl.flock(cls.lock_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
+                break
+            except IOError:
+                attempt += 1
+                if attempt > 120:  # Wait up to 2 minutes
+                    raise Exception("Could not acquire lock for AF_XDP tests")
+                time.sleep(1)
+
+        super(TestVPPInterfacesQemuAfXDPL2, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            super(TestVPPInterfacesQemuAfXDPL2, cls).tearDownClass()
+        finally:
+            # Release lock
+            if hasattr(cls, "lock_file"):
+                fcntl.flock(cls.lock_file, fcntl.LOCK_UN)
+                cls.lock_file.close()
+
+    def tearDown(self):
+        super(TestVPPInterfacesQemuAfXDPL2, self).tearDown()
+
+
+SELECTED_TESTS = TestVPPInterfacesQemuAfXDPL2.tests_to_run
+tests = filter(TestSelector(SELECTED_TESTS).filter_tests, test_config["tests"])
+generate_vpp_interface_tests(tests, TestVPPInterfacesQemuAfXDPL2)
+
+if __name__ == "__main__":
+    unittest.main(testRunner=VppTestRunner)
