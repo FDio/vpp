d436b097bf592faa72d4c7ac1c3fb51472d0e6d0
Benoît Ganne <bganne@cisco.com>
2025-07-25 16:22:23 +0200

    cnat: fix rsession sport collision probing
    When trying to find an unused port, we should consume the full 16-bits
    each time and not only 2 (which was probably a typo for 2-bytes...).
    
    Type: fix
    
    Change-Id: I79bde6ddebde83767072a2ac4bf289496c2d78ed
    Signed-off-by: Benoît Ganne <bganne@cisco.com>

--- src/plugins/cnat/cnat_node.h
+++ src/plugins/cnat/cnat_node.h
@@ -983,7 +983,7 @@ cnat_rsession_create (cnat_timestamp_rewrite_t *rw, u32 flow_id, u32 ret_fib_ind
 	      int rv = cnat_bihash_add_del (&cnat_session_db, &rkey, 2 /* no overwrite */);
 	      if (!rv)
 		goto out;  /* success ! */
-	      hash_ >>= 2; /* try next port... */
+	      hash_ >>= 16; /* try next port... */
 	    }
 	}
 
