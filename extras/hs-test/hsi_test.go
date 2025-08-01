be3f22bca2a3ee568ea01f94c4e0849fc5bed3d9
Matus Fabian <matfabia@cisco.com>
2025-08-01 12:08:59 -0400

    hs-test: add hsi and transparent proxy test
    Type: test
    
    Change-Id: Ie7fdc987ee300cf0d3dc5aed31dd28a972f0c394
    Signed-off-by: Matus Fabian <matfabia@cisco.com>

---
+++ extras/hs-test/hsi_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	"strconv"
+
+	. "fd.io/hs-test/infra"
+	. "github.com/onsi/ginkgo/v2"
+)
+
+func init() {
+	RegisterHsiTests(HsiTransparentProxyTest)
+}
+
+func HsiTransparentProxyTest(s *HsiSuite) {
+	s.SetupNginxServer()
+	vpp := s.Containers.Vpp.VppInstance
+	s.Log(vpp.Vppctl("set interface feature host-" + s.Interfaces.Client.Name() + " hsi4-in arc ip4-unicast"))
+	s.Log(vpp.Vppctl("set interface feature host-" + s.Interfaces.Server.Name() + " hsi4-in arc ip4-unicast"))
+	s.Log(vpp.Vppctl("test proxy server server-uri tcp://0.0.0.0:%d client-uri tcp://%s:%d",
+		s.Ports.Server, s.ServerAddr(), s.Ports.Server))
+
+	query := "httpTestFile"
+	finished := make(chan error, 1)
+	defer func() { os.Remove(query) }()
+	go func() {
+		defer GinkgoRecover()
+		s.StartWget(finished, s.ServerAddr(), strconv.Itoa(int(s.Ports.Server)), query, s.NetNamespaces.Client)
+	}()
+	s.AssertNil(<-finished)
+}
