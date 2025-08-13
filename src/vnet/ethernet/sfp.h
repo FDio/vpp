16be54411ca03020ad78ed91e12a5353e4c8170d
Pim van Pelt <pim@ipng.nl>
2025-08-14 00:02:34 +0200

    vnet: add SFF8472 and SFF8636 diagnostics
    Using device-class.eeprom_read_function, extract the type of
    EEPROM, and create a parser for SFF8472 (SFP+/SFP/SFP28) and
    SFF8636 (QSFP+/QSFP28/QSFP-DD) to show module diagnostics.
    
    When available, read EEPROM page A2h and report on DDM values:
    show int transceiver <itf> eeprom
    show int transceiver <itf> module [verbose]
    show int transceiver <itf> diag [verbose]
    
    Type: improvement
    Change-Id: Iae41b9753f31bc1a8d32b2c42d396cd743864147
    Signed-off-by: pim@ipng.nl

--- src/vnet/ethernet/sfp.h
+++ src/vnet/ethernet/sfp.h
@@ -17,6 +17,7 @@
 #define included_vnet_optics_sfp_h
 
 #include <vppinfra/format.h>
+#include <vnet/interface.h>
 
 #define foreach_sfp_id				\
   _ (UNKNOWN, "unknown")			\
@@ -66,18 +67,24 @@ typedef struct
   u8 ext_module_codes;
   u8 vendor_oui[3];
   u8 vendor_part_number[16];
-  u8 vendor_revision[2];
-  /* 16 bit value network byte order. */
-  u8 wavelength_or_att[2];
-  u8 wavelength_tolerance_or_att[2];
-  u8 max_case_temp;
-  u8 cc_base;
 
-  u8 link_codes;
-  u8 options[3];
+  /* NOTE: SFF8472 defines vendor revision as 4 bytes, followed by 2 bytes of
+   * wavelength_or_att SFF8636 defines vendor revision as 2 bytes, followed by
+   * u16 wavelength_or_att, then u16 wavelength_tolerance
+   */
+  u8 vendor_revision[4]; /* SFF8636 has wavelength in vendor_revision[2+3] */
+  u16 wavelength_or_att; /* SFF8472 has wavelength here; SFF8636 has
+			    wavelength_tolerance here */
+
+  u8 reserved_62; /* Byte 62: Reserved */
+  u8 cc_base;	  /* Byte 63: checksum for first 64 bytes */
+  u8 option_values[2];
+  u8 signalling_rate_max;
+  u8 signalling_rate_min;
   u8 vendor_serial_number[16];
   u8 vendor_date_code[8];
-  u8 reserved92[3];
+  u8 diag_monitoring_type; /* Byte 92 */
+  u8 reserved93[2];
   u8 checksum_63_to_94;
   u8 vendor_specific[32];
   u8 reserved128[384];
@@ -95,6 +102,16 @@ sfp_eeprom_is_valid (sfp_eeprom_t * e)
     sum += ((u8 *) e)[i];
   return sum == e->cc_base;
 }
+
+/* Show the EEPROM module information */
+void sfp_eeprom_module (vlib_main_t *vm, vnet_interface_eeprom_t *eeprom,
+			u8 is_terse);
+void sfp_eeprom_diagnostics (vlib_main_t *vm, vnet_interface_eeprom_t *eeprom,
+			     u8 is_terse);
+
+/* Base SFP EEPROM decoding function */
+void sfp_eeprom_decode_base (vlib_main_t *vm, sfp_eeprom_t *se, u8 is_terse,
+			     vnet_interface_eeprom_type_t eeprom_type);
 
 /* _ (byte_index, bit_index, name) */
 #define foreach_sfp_compatibility		\
@@ -128,9 +145,58 @@ typedef enum
     SFP_N_COMPATIBILITY,
 } sfp_compatibility_t;
 
-u32 sfp_is_comatible (sfp_eeprom_t * e, sfp_compatibility_t c);
+#define foreach_sfp_encoding                                                  \
+  _ (0x01, "8B/10B")                                                          \
+  _ (0x02, "4B/5B")                                                           \
+  _ (0x03, "NRZ")                                                             \
+  _ (0x04, "4B/5B (FC-100)")                                                  \
+  _ (0x05, "Manchester")                                                      \
+  _ (0x06, "64B/66B")                                                         \
+  _ (0x07, "256B/257B")                                                       \
+  _ (0x08, "PAM4")
+
+typedef enum
+{
+#define _(v, s) SFP_ENCODING_##v = v,
+  foreach_sfp_encoding
+#undef _
+} sfp_encoding_t;
+
+#define foreach_sfp_connector                                                 \
+  _ (0x01, "SC")                                                              \
+  _ (0x02, "Fibre Channel Style 1 copper")                                    \
+  _ (0x03, "Fibre Channel Style 2 copper")                                    \
+  _ (0x04, "BNC/TNC")                                                         \
+  _ (0x05, "Fibre Channel coaxial")                                           \
+  _ (0x06, "Fiber Jack")                                                      \
+  _ (0x07, "LC")                                                              \
+  _ (0x08, "MT-RJ")                                                           \
+  _ (0x09, "MU")                                                              \
+  _ (0x0A, "SG")                                                              \
+  _ (0x0B, "Optical pigtail")                                                 \
+  _ (0x0C, "MPO 1x12 Parallel Optic")                                         \
+  _ (0x0D, "MPO 2x16 Parallel Optic")                                         \
+  _ (0x20, "HSSDC II")                                                        \
+  _ (0x21, "Copper pigtail")                                                  \
+  _ (0x22, "RJ45")                                                            \
+  _ (0x23, "No separable connector")                                          \
+  _ (0x24, "MXC 2x16")                                                        \
+  _ (0x25, "CS optical connector")                                            \
+  _ (0x26, "SN optical connector")                                            \
+  _ (0x27, "MPO 2x12 Parallel Optic")                                         \
+  _ (0x28, "MPO 1x16 Parallel Optic")
+
+typedef enum
+{
+#define _(v, s) SFP_CONNECTOR_##v = v,
+  foreach_sfp_connector
+#undef _
+} sfp_connector_t;
 
 format_function_t format_sfp_eeprom;
+format_function_t format_sfp_id;
+format_function_t format_sfp_encoding;
+format_function_t format_sfp_connector;
 
 #endif /* included_vnet_optics_sfp_h */
 
---
+++ src/vnet/ethernet/sfp_sff8472.h
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2025 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __included_ethernet_sff8472_h__
+#define __included_ethernet_sff8472_h__
+
+#include <vlib/vlib.h>
+#include <vppinfra/types.h>
+#include <vnet/ethernet/sfp.h>
+
+/* SFF-8472 A2 page - Diagnostic fields */
+typedef struct
+{
+  /* Alarm and warning thresholds (bytes 0-55) */
+  u16 temp_high_alarm;	     /* 0-1: Temperature high alarm */
+  u16 temp_low_alarm;	     /* 2-3: Temperature low alarm */
+  u16 temp_high_warning;     /* 4-5: Temperature high warning */
+  u16 temp_low_warning;	     /* 6-7: Temperature low warning */
+  u16 voltage_high_alarm;    /* 8-9: Voltage high alarm */
+  u16 voltage_low_alarm;     /* 10-11: Voltage low alarm */
+  u16 voltage_high_warning;  /* 12-13: Voltage high warning */
+  u16 voltage_low_warning;   /* 14-15: Voltage low warning */
+  u16 bias_high_alarm;	     /* 16-17: Bias high alarm */
+  u16 bias_low_alarm;	     /* 18-19: Bias low alarm */
+  u16 bias_high_warning;     /* 20-21: Bias high warning */
+  u16 bias_low_warning;	     /* 22-23: Bias low warning */
+  u16 tx_power_high_alarm;   /* 24-25: TX power high alarm */
+  u16 tx_power_low_alarm;    /* 26-27: TX power low alarm */
+  u16 tx_power_high_warning; /* 28-29: TX power high warning */
+  u16 tx_power_low_warning;  /* 30-31: TX power low warning */
+  u16 rx_power_high_alarm;   /* 32-33: RX power high alarm */
+  u16 rx_power_low_alarm;    /* 34-35: RX power low alarm */
+  u16 rx_power_high_warning; /* 36-37: RX power high warning */
+  u16 rx_power_low_warning;  /* 38-39: RX power low warning */
+  u8 reserved_40_55[16];     /* 40-55: Reserved/Other fields */
+  /* Calibration constants for external calibration (bytes 56-95) */
+  u32 rx_power_cal[5];	/* 56-75: RX power calibration coefficients (IEEE 754
+			   float) */
+  u16 tx_bias_slope;	/* 76-77: TX bias slope calibration */
+  u16 tx_bias_offset;	/* 78-79: TX bias offset calibration */
+  u16 tx_power_slope;	/* 80-81: TX power slope calibration */
+  u16 tx_power_offset;	/* 82-83: TX power offset calibration */
+  u16 temp_slope;	/* 84-85: Temperature slope calibration */
+  u16 temp_offset;	/* 86-87: Temperature offset calibration */
+  u16 voltage_slope;	/* 88-89: Voltage slope calibration */
+  u16 voltage_offset;	/* 90-91: Voltage offset calibration */
+  u8 reserved_92_95[4]; /* 92-95: Reserved */
+  /* Real-time diagnostic values (bytes 96-105) */
+  u16 temperature;	    /* 96-97: Temperature */
+  u16 vcc;		    /* 98-99: Supply voltage */
+  u16 tx_bias;		    /* 100-101: TX bias current */
+  u16 tx_power;		    /* 102-103: TX average optical power */
+  u16 rx_power;		    /* 104-105: RX average optical power */
+  u8 reserved_106_109[4];   /* 106-109: Reserved */
+  u8 status_control[2];	    /* 110-111: Status/Control */
+  u8 alarm1;		    /* 112: Temp, VCC, TX Bias, TX Power alarms */
+  u8 alarm2;		    /* 113: Rx, Laser Temp, TEC current alarms */
+  u8 tx_input_equalization; /* 114: Tx Input equalization HIGH / LOW */
+  u8 rx_output_emphasis;    /* 115: Rx output emphasis HIGH / LOW */
+  u8 warning1;		    /* 116: Temp, VCC, TX Bias, TX power warnings */
+  u8 warning2;		    /* 117: Rx, Laser Temp, TEC current warning */
+  u8 emc_status[2];	    /* 118-119: Extended Module Control status */
+  u8 reserved_120_126[7];   /* 120-126: Vendor specific Locations */
+  u8 page_select;	    /* 127: Page Select */
+} sff8472_diag_t;
+
+STATIC_ASSERT (sizeof (sff8472_diag_t) == 128,
+	       "sff8472_diag_t must be 128 bytes");
+
+/* Function declarations */
+void sff8472_decode_diagnostics (vlib_main_t *vm,
+				 vnet_interface_eeprom_t *eeprom, u8 is_terse);
+
+#endif /* __included_ethernet_sff8472_h__ */
