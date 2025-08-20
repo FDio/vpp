FDio/vpp

commit fc7b794758fbdd9bcae337e90255c1fc1e548808

vppinfra: unify thread_index handling
use typedef for thread_index instead  if u16, u32, uword....

Type: improvement
Change-Id: Ib31a0f918035e07d8f53c46a3d80b9ad8b3df129
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/vnet/interface_cli.c
@@ -1694,7 +1694,7 @@ VLIB_CLI_COMMAND (show_interface_rx_placement, static) = {
 };
 clib_error_t *
 set_hw_interface_rx_placement (u32 hw_if_index, u32 queue_id,
-			       u32 thread_index, u8 is_main)
+			       clib_thread_index_t thread_index, u8 is_main)
 {
   vnet_main_t *vnm = vnet_get_main ();
   vnet_device_main_t *vdm = &vnet_device_main;
@@ -1731,7 +1731,7 @@ set_interface_rx_placement (vlib_main_t *vm, unformat_input_t *input,
   vnet_main_t *vnm = vnet_get_main ();
   u32 hw_if_index = (u32) ~ 0;
   u32 queue_id = (u32) 0;
-  u32 thread_index = (u32) ~ 0;
+  clib_thread_index_t thread_index = CLIB_INVALID_THREAD_INDEX;
   u8 is_main = 0;
 
   if (!unformat_user (input, unformat_line_input, line_input))
@@ -1831,11 +1831,12 @@ set_hw_interface_tx_queue (u32 hw_if_index, u32 queue_id, uword *bitmap)
   vlib_thread_main_t *vtm = vlib_get_thread_main ();
   vnet_hw_if_tx_queue_t *txq;
   u32 queue_index;
-  u32 thread_index;
+  clib_thread_index_t thread_index;
 
   /* highest set bit in bitmap should not exceed last worker thread index */
   thread_index = clib_bitmap_last_set (bitmap);
-  if ((thread_index != ~0) && (thread_index >= vtm->n_vlib_mains))
+  if ((thread_index != CLIB_INVALID_THREAD_INDEX) &&
+      (thread_index >= vtm->n_vlib_mains))
     return VNET_API_ERROR_INVALID_VALUE;
 
   queue_index =

commit 9fe0a35a1871bd5527c11e53d81a06898fa39ab5

vnet: fix set interfaces rx-placement cli
 - unformat() parses 32-bit integers whereas clib_thread_t is a 16-bit
   integers, yielding stack overflow
 - no need to pass thread_index when parsing main
 - validate thread_index otherwise ~0 + 1 arithmetic done in
   set_hw_interface_rx_placement() selects main thread by default

Type: fix
Fixes: fc7b794758fbdd9bcae337e90255c1fc1e548808

Change-Id: I36dcba977573a32f6f91e6c5470966f35cce30ed
Signed-off-by: Benoît Ganne <bganne@cisco.com>

src/vnet/interface_cli.c
@@ -1731,7 +1731,7 @@ set_interface_rx_placement (vlib_main_t *vm, unformat_input_t *input,
   vnet_main_t *vnm = vnet_get_main ();
   u32 hw_if_index = (u32) ~ 0;
   u32 queue_id = (u32) 0;
-  clib_thread_index_t thread_index = CLIB_INVALID_THREAD_INDEX;
+  u32 thread_index = (u32) ~0;
   u8 is_main = 0;
 
   if (!unformat_user (input, unformat_line_input, line_input))
@@ -1744,7 +1744,7 @@ set_interface_rx_placement (vlib_main_t *vm, unformat_input_t *input,
 	;
       else if (unformat (line_input, "queue %d", &queue_id))
 	;
-      else if (unformat (line_input, "main", &thread_index))
+      else if (unformat (line_input, "main"))
 	is_main = 1;
       else if (unformat (line_input, "worker %d", &thread_index))
 	;
@@ -1761,6 +1761,9 @@ set_interface_rx_placement (vlib_main_t *vm, unformat_input_t *input,
 
   if (hw_if_index == (u32) ~ 0)
     return clib_error_return (0, "please specify valid interface name");
+
+  if (thread_index == (u32) ~0 && !is_main)
+    return clib_error_return (0, "please specify valid worker thread or main");
 
   error = set_hw_interface_rx_placement (hw_if_index, queue_id, thread_index,
 					 is_main);