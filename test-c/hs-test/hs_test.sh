99932d8c63bbc9fe462516a0df0e0042b0c2bd7a
Adrian Villin <avillin@cisco.com>
2025-09-08 16:29:39 +0200

    hs-test: move hs-test to vpp/test-c
    - separated cluster tests from hs-test and moved them into
      test-c/kube-test
    - cleaned up kube-test and hs-test
    - kube-test setup-cluster.sh improvements
    - Makefile in extras/hs-test is temporary
    - kube-test stability improvements
    
    Type: test
    
    Change-Id: Iee9fc732ccd303d4b4635d329f673c03f95a4dd4
    Signed-off-by: Adrian Villin <avillin@cisco.com>

---
+++ test-c/hs-test/hs_test.sh
@@ -0,0 +1,219 @@
+#!/usr/bin/env bash
+
+args=
+focused_test=0
+persist_set=0
+dryrun_set=0
+coverage_set=0
+unconfigure_set=0
+debug_set=0
+leak_check_set=0
+debug_build=
+ginkgo_args=
+tc_names=()
+skip_names=()
+dryrun=
+no_color=
+hs_root=
+
+for i in "$@"
+do
+case "${i}" in
+    --persist=*)
+        persist="${i#*=}"
+        if [ "$persist" = "true" ]; then
+            args="$args -persist"
+            persist_set=1
+        fi
+        ;;
+    --coverage=*)
+        coverage="${i#*=}"
+        if [ "$coverage" = "true" ]; then
+            args="$args -coverage"
+            coverage_set=1
+        fi
+        ;;
+    --debug=*)
+        debug="${i#*=}"
+        if [ "$debug" = "true" ]; then
+            args="$args -debug"
+            debug_set=1
+        fi
+        ;;
+    --debug_build=*)
+        debug_build="${i#*=}"
+        if [ "$debug_build" = "true" ]; then
+            args="$args -debug_build"
+        fi
+        ;;
+    --verbose=*)
+        verbose="${i#*=}"
+        if [ "$verbose" = "true" ]; then
+            args="$args -verbose"
+        fi
+        ;;
+    --unconfigure=*)
+        unconfigure="${i#*=}"
+        if [ "$unconfigure" = "true" ]; then
+            args="$args -unconfigure"
+            unconfigure_set=1
+        fi
+        ;;
+    --cpus=*)
+        args="$args -cpus ${i#*=}"
+        ;;
+    --vpp_cpus=*)
+        args="$args -vpp_cpus ${i#*=}"
+        ;;
+    --vppsrc=*)
+        args="$args -vppsrc ${i#*=}"
+        ;;
+    --test=*)
+        tc_list="${i#*=}"
+        ginkgo_args="$ginkgo_args -v"
+        if [ "$tc_list" != "all" ]; then
+            focused_test=1
+            IFS=',' read -r -a tc_names <<< "$tc_list"
+            args="$args -verbose"
+        fi
+        ;;
+    --skip=*)
+        skip_list="${i#*=}"
+        IFS=',' read -r -a skip_names <<< "$skip_list"
+        ;;
+    --parallel=*)
+        ginkgo_args="$ginkgo_args -procs=${i#*=}"
+        ;;
+    --ginkgo_timeout=*)
+        ginkgo_args="$ginkgo_args --timeout=${i#*=}"
+        ;;
+    --repeat=*)
+        ginkgo_args="$ginkgo_args --repeat=${i#*=}"
+        ;;
+    --cpu0=*)
+        cpu0="${i#*=}"
+        if [ "$cpu0" = "true" ]; then
+            args="$args -cpu0"
+        fi
+        ;;
+    --dryrun=*)
+        dryrun="${i#*=}"
+        if [ "$dryrun" = "true" ]; then
+            args="$args -dryrun"
+            dryrun_set=1
+        fi
+        ;;
+    --leak_check=*)
+        leak_check="${i#*=}"
+        if [ "$leak_check" = "true" ]; then
+            args="$args -leak_check"
+            leak_check_set=1
+        fi
+        ;;
+    --no_color=*)
+        no_color="${i#*=}"
+        if [ "$no_color" = "true" ]; then
+            ginkgo_args="$ginkgo_args --no-color"
+        fi
+        ;;
+    --timeout=*)
+        args="$args -timeout ${i#*=}"
+        ;;
+    --hs_root=*)
+        hs_root="${i#*=}"
+        cd $hs_root
+esac
+done
+
+if [ ${#tc_names[@]} -gt 1 ]
+then
+    focused_test=0
+fi
+
+for name in "${tc_names[@]}"; do
+    ginkgo_args="$ginkgo_args --focus $name"
+done
+
+for skip in "${skip_names[@]}"; do
+    ginkgo_args="$ginkgo_args --skip $skip"
+done
+
+if [ $focused_test -eq 0 ] && { [ $persist_set -eq 1 ] || [ $dryrun_set -eq 1 ]; }; then
+    echo -e "\e[1;31mpersist/dryrun flag is not supported while running all tests!\e[1;0m"
+    exit 2
+fi
+
+if [ $unconfigure_set -eq 1 ] && [ $focused_test -eq 0 ]; then
+    echo -e "\e[1;31ma single test has to be specified when unconfigure is set\e[1;0m"
+    exit 2
+fi
+
+if [ $persist_set -eq 1 ] && [ $unconfigure_set -eq 1 ]; then
+    echo -e "\e[1;31msetting persist flag and unconfigure flag is not allowed\e[1;0m"
+    exit 2
+fi
+
+if [ $focused_test -eq 0 ] && [ $debug_set -eq 1 ]; then
+    echo -e "\e[1;31mVPP debug flag is not supported while running all tests!\e[1;0m"
+    exit 2
+fi
+
+args="$args -whoami $(whoami)"
+
+if [ $leak_check_set -eq 1 ]; then
+  if [ $focused_test -eq 0 ]; then
+    echo -e "\e[1;31ma single test has to be specified via TEST var when leak_check is set\e[1;0m"
+    exit 2
+  else
+    if [[ $tc_list != *"MemLeak"* ]]; then
+        echo -e "\e[1;31ma none of the selected tests are memleak tests\e[1;0m"
+        exit 2
+    fi
+  fi
+fi
+
+if [ -n "${BUILD_NUMBER}" ]; then
+        ginkgo_args="$ginkgo_args --no-color"
+fi
+
+mkdir -p .go_cache
+make build-go
+
+mkdir -p summary
+rm summary/*
+# shellcheck disable=SC2086
+CMD="go run github.com/onsi/ginkgo/v2/ginkgo --json-report=summary/report.json $ginkgo_args -- $args"
+echo "$CMD"
+$CMD
+exit_status=$?
+
+if [ $exit_status != 0 ]; then
+    jq -r '.[0] | .SpecReports[] | select((.State == "failed") or (.State == "timedout") or (.State == "panicked")) | select(.Failure != null) |
+"TestName:
+    \(.LeafNodeText)
+Suite:
+    \(.Failure.FailureNodeLocation.FileName)
+Message:\n"
++ (
+    if .ReportEntries? then
+        (.ReportEntries[] | select(.Name | contains("Backtrace")) |
+        "\tFull Back Trace:
+\(.Value.Representation | ltrimstr("{{red}}") | rtrimstr("{{/}}"))"
+        ) // "\(.Failure.Message)"
+    else
+        "parse error"
+    end
+)
++ (
+    if .Failure.Message == "A spec timeout occurred" then
+        "\n"
+    else
+        "\nFull Stack Trace:
+\(.Failure.Location.FullStackTrace)\n"
+    end
+)' summary/report.json > summary/failed-summary.log \
+&& echo "Summary generated -> summary/failed-summary.log"
+    exit $exit_status
+else
+    exit $exit_status
+fi
---
+++ test-c/kube-test/infra/utils.go
@@ -0,0 +1,152 @@
+package kube_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"time"
+)
+
+func boolPtr(b bool) *bool {
+	return &b
+}
+
+func int64Ptr(integer int64) *int64 {
+	return &integer
+}
+
+func GetTestFilename() string {
+	_, filename, _, _ := runtime.Caller(2)
+	return filepath.Base(filename)
+}
+
+var testCounter uint16
+var startTime time.Time = time.Now()
+
+func TestCounterFunc() {
+	testCounter++
+	fmt.Printf("Test counter: %d\n"+
+		"Time elapsed: %.2fs\n",
+		testCounter, time.Since(startTime).Seconds())
+}
+
+type IPerfResult struct {
+	Start struct {
+		Timestamp struct {
+			Time string `json:"time"`
+		} `json:"timestamp"`
+		Connected []struct {
+			Socket     int    `json:"socket"`
+			LocalHost  string `json:"local_host"`
+			LocalPort  int    `json:"local_port"`
+			RemoteHost string `json:"remote_host"`
+			RemotePort int    `json:"remote_port"`
+		} `json:"connected"`
+		Version string `json:"version"`
+		Details struct {
+			Protocol string `json:"protocol"`
+		} `json:"test_start"`
+	} `json:"start"`
+	End struct {
+		TcpSent *struct {
+			MbitsPerSecond float64 `json:"bits_per_second"`
+			MBytes         float64 `json:"bytes"`
+		} `json:"sum_sent,omitempty"`
+		TcpReceived *struct {
+			MbitsPerSecond float64 `json:"bits_per_second"`
+			MBytes         float64 `json:"bytes"`
+		} `json:"sum_received,omitempty"`
+		Udp *struct {
+			MbitsPerSecond float64 `json:"bits_per_second"`
+			JitterMs       float64 `json:"jitter_ms,omitempty"`
+			LostPackets    int     `json:"lost_packets,omitempty"`
+			Packets        int     `json:"packets,omitempty"`
+			LostPercent    float64 `json:"lost_percent,omitempty"`
+			MBytes         float64 `json:"bytes"`
+		} `json:"sum,omitempty"`
+	} `json:"end"`
+}
+
+func (s *BaseSuite) ParseJsonIperfOutput(jsonResult []byte) IPerfResult {
+	var result IPerfResult
+
+	// VCL/LDP debugging can pollute output so find the first occurrence of a curly brace to locate the start of JSON data
+	jsonStart := -1
+	jsonEnd := len(jsonResult)
+	braceCount := 0
+	for i := 0; i < len(jsonResult); i++ {
+		if jsonResult[i] == '{' {
+			if jsonStart == -1 {
+				jsonStart = i
+			}
+			braceCount++
+		} else if jsonResult[i] == '}' {
+			braceCount--
+			if braceCount == 0 {
+				jsonEnd = i + 1
+				break
+			}
+		}
+	}
+	jsonResult = jsonResult[jsonStart:jsonEnd]
+
+	// remove iperf warning line if present
+	if strings.Contains(string(jsonResult), "warning") {
+		index := strings.Index(string(jsonResult), "\n")
+		jsonResult = jsonResult[index+1:]
+	}
+
+	err := json.Unmarshal(jsonResult, &result)
+	s.AssertNil(err)
+
+	if result.Start.Details.Protocol == "TCP" {
+		result.End.TcpSent.MbitsPerSecond = result.End.TcpSent.MbitsPerSecond / 1000000
+		result.End.TcpSent.MBytes = result.End.TcpSent.MBytes / 1000000
+		result.End.TcpReceived.MbitsPerSecond = result.End.TcpReceived.MbitsPerSecond / 1000000
+		result.End.TcpReceived.MBytes = result.End.TcpReceived.MBytes / 1000000
+	} else {
+		result.End.Udp.MBytes = result.End.Udp.MBytes / 1000000
+		result.End.Udp.MbitsPerSecond = result.End.Udp.MbitsPerSecond / 1000000
+	}
+
+	return result
+}
+
+func (s *BaseSuite) LogJsonIperfOutput(result IPerfResult) {
+	s.Log("\n*******************************************\n"+
+		"%s\n"+
+		"[%s] %s:%d connected to %s:%d\n"+
+		"Started:  %s\n",
+		result.Start.Version,
+		result.Start.Details.Protocol,
+		result.Start.Connected[0].LocalHost, result.Start.Connected[0].LocalPort,
+		result.Start.Connected[0].RemoteHost, result.Start.Connected[0].RemotePort,
+		result.Start.Timestamp.Time)
+
+	if result.Start.Details.Protocol == "TCP" {
+		s.Log("Transfer (sent):     %.2f MBytes\n"+
+			"Bitrate  (sent):     %.2f Mbits/sec\n"+
+			"Transfer (received): %.2f MBytes\n"+
+			"Bitrate  (received): %.2f Mbits/sec",
+			result.End.TcpSent.MBytes,
+			result.End.TcpSent.MbitsPerSecond,
+			result.End.TcpReceived.MBytes,
+			result.End.TcpReceived.MbitsPerSecond)
+	} else {
+		s.Log("Transfer:     %.2f MBytes\n"+
+			"Bitrate:      %.2f Mbits/sec\n"+
+			"Jitter:       %.3f ms\n"+
+			"Packets:      %d\n"+
+			"Packets lost: %d\n"+
+			"Percent lost: %.2f%%",
+			result.End.Udp.MBytes,
+			result.End.Udp.MbitsPerSecond,
+			result.End.Udp.JitterMs,
+			result.End.Udp.Packets,
+			result.End.Udp.LostPackets,
+			result.End.Udp.LostPercent)
+	}
+	s.Log("*******************************************\n")
+}
