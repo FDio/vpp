9d259f818e02d9cf9e7d234bedf51544b8dd951b
Vratko Polak <vrpolak@cisco.com>
2025-09-08 15:20:42 +0200

    experiment: abort instead of exit
    In CI, some crashes fail to create core. This should force it.
    
    Change-Id: I45943423e3d391dd915535a9316ec786f5f8fc86
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- src/vppinfra/error.c
+++ src/vppinfra/error.c
@@ -140,9 +140,9 @@ _clib_error (int how_to_die, const char *function_name, uword line_number,
   vec_free (msg);
 
   if (how_to_die & CLIB_ERROR_ABORT)
-    debugger ();
+    abort ();
   if (how_to_die & CLIB_ERROR_FATAL)
-    error_exit (1);
+    abort ();
 }
 
 __clib_export clib_error_t *
@@ -229,7 +229,7 @@ _clib_error_report (clib_error_t * errors)
       vec_free (msg);
 
       if (errors->flags & CLIB_ERROR_ABORT)
-	debugger ();
+	abort ();
       if (errors->flags & CLIB_ERROR_FATAL)
 	error_exit (1);
 
