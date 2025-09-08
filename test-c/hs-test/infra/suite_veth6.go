af7797afff327b46cec6ec8d3502fcb05f17528f
Adrian Villin <avillin@cisco.com>
2025-09-08 16:29:39 +0200

    hs-test: move hs-test to vpp/test-c [WiP]
    Type: test
    
    Change-Id: Iee9fc732ccd303d4b4635d329f673c03f95a4dd4
    Signed-off-by: Adrian Villin <avillin@cisco.com>

---
+++ test-c/hs-test/infra/suite_veth6.go
@@ -0,0 +1,164 @@
+package hst
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"strings"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+)
+
+var veth6Tests = map[string][]func(s *Veths6Suite){}
+var veth6SoloTests = map[string][]func(s *Veths6Suite){}
+
+type Veths6Suite struct {
+	HstSuite
+	Interfaces struct {
+		Server *NetInterface
+		Client *NetInterface
+	}
+	Containers struct {
+		ServerVpp *Container
+		ClientVpp *Container
+		ServerApp *Container
+		ClientApp *Container
+	}
+	Ports struct {
+		Port1 string
+	}
+}
+
+func RegisterVeth6Tests(tests ...func(s *Veths6Suite)) {
+	veth6Tests[GetTestFilename()] = tests
+}
+func RegisterSoloVeth6Tests(tests ...func(s *Veths6Suite)) {
+	veth6SoloTests[GetTestFilename()] = tests
+}
+
+func (s *Veths6Suite) SetupSuite() {
+	time.Sleep(1 * time.Second)
+	s.HstSuite.SetupSuite()
+	s.ConfigureNetworkTopology("2peerVeth6")
+	s.LoadContainerTopology("2peerVeth")
+	s.Interfaces.Client = s.GetInterfaceByName("cln")
+	s.Interfaces.Server = s.GetInterfaceByName("srv")
+	s.Containers.ServerVpp = s.GetContainerByName("server-vpp")
+	s.Containers.ClientVpp = s.GetContainerByName("client-vpp")
+	s.Containers.ServerApp = s.GetContainerByName("server-app")
+	s.Containers.ClientApp = s.GetContainerByName("client-app")
+	s.Ports.Port1 = s.GeneratePort()
+}
+
+func (s *Veths6Suite) SetupTest() {
+	s.HstSuite.SetupTest()
+
+	// Setup test conditions
+	var sessionConfig Stanza
+	sessionConfig.
+		NewStanza("session").
+		Append("enable").
+		Append("use-app-socket-api")
+
+	if strings.Contains(CurrentSpecReport().LeafNodeText, "InterruptMode") {
+		sessionConfig.Append("use-private-rx-mqs").Close()
+		s.Log("**********************INTERRUPT MODE**********************")
+	} else {
+		sessionConfig.Close()
+	}
+
+	// ... For server
+	serverVpp, err := s.Containers.ServerVpp.newVppInstance(s.Containers.ServerVpp.AllocatedCpus, sessionConfig)
+	s.AssertNotNil(serverVpp, fmt.Sprint(err))
+
+	// ... For client
+	clientVpp, err := s.Containers.ClientVpp.newVppInstance(s.Containers.ClientVpp.AllocatedCpus, sessionConfig)
+	s.AssertNotNil(clientVpp, fmt.Sprint(err))
+
+	s.SetupServerVpp()
+	s.SetupClientVpp()
+	if *DryRun {
+		s.LogStartedContainers()
+		s.Skip("Dry run mode = true")
+	}
+}
+
+func (s *Veths6Suite) SetupServerVpp() {
+	serverVpp := s.Containers.ServerVpp.VppInstance
+	s.AssertNil(serverVpp.Start())
+
+	idx, err := serverVpp.createAfPacket(s.Interfaces.Server, true)
+	s.AssertNil(err, fmt.Sprint(err))
+	s.AssertNotEqual(0, idx)
+}
+
+func (s *Veths6Suite) SetupClientVpp() {
+	clientVpp := s.GetContainerByName("client-vpp").VppInstance
+	s.AssertNil(clientVpp.Start())
+
+	idx, err := clientVpp.createAfPacket(s.Interfaces.Client, true)
+	s.AssertNil(err, fmt.Sprint(err))
+	s.AssertNotEqual(0, idx)
+}
+
+var _ = Describe("Veths6Suite", Ordered, ContinueOnFailure, func() {
+	var s Veths6Suite
+	BeforeAll(func() {
+		s.SetupSuite()
+	})
+	BeforeEach(func() {
+		s.SetupTest()
+	})
+	AfterAll(func() {
+		s.TeardownSuite()
+
+	})
+	AfterEach(func() {
+		s.TeardownTest()
+	})
+
+	// https://onsi.github.io/ginkgo/#dynamically-generating-specs
+	for filename, tests := range veth6Tests {
+		for _, test := range tests {
+			test := test
+			pc := reflect.ValueOf(test).Pointer()
+			funcValue := runtime.FuncForPC(pc)
+			testName := filename + "/" + strings.Split(funcValue.Name(), ".")[2]
+			It(testName, func(ctx SpecContext) {
+				s.Log(testName + ": BEGIN")
+				test(&s)
+			}, SpecTimeout(TestTimeout))
+		}
+	}
+})
+
+var _ = Describe("Veths6SuiteSolo", Ordered, ContinueOnFailure, Serial, func() {
+	var s Veths6Suite
+	BeforeAll(func() {
+		s.SetupSuite()
+	})
+	BeforeEach(func() {
+		s.SetupTest()
+	})
+	AfterAll(func() {
+		s.TeardownSuite()
+	})
+	AfterEach(func() {
+		s.TeardownTest()
+	})
+
+	// https://onsi.github.io/ginkgo/#dynamically-generating-specs
+	for filename, tests := range veth6SoloTests {
+		for _, test := range tests {
+			test := test
+			pc := reflect.ValueOf(test).Pointer()
+			funcValue := runtime.FuncForPC(pc)
+			testName := filename + "/" + strings.Split(funcValue.Name(), ".")[2]
+			It(testName, Label("SOLO"), func(ctx SpecContext) {
+				s.Log(testName + ": BEGIN")
+				test(&s)
+			}, SpecTimeout(TestTimeout))
+		}
+	}
+})
---
+++ test-c/kube-test/go.mod
@@ -0,0 +1,65 @@
+module fd.io/kube-test
+
+go 1.23.8
+
+toolchain go1.23.10
+
+require (
+	github.com/onsi/ginkgo/v2 v2.23.3
+	github.com/onsi/gomega v1.37.0
+	gopkg.in/yaml.v3 v3.0.1
+	k8s.io/api v0.30.2
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/emicklei/go-restful/v3 v3.11.2 // indirect
+	github.com/go-openapi/jsonpointer v0.21.0 // indirect
+	github.com/go-openapi/jsonreference v0.21.0 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/gnostic-models v0.6.8 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/moby/spdystream v0.2.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/mxk/go-flowrate v0.0.0-20140419014527-cca7078d478f // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
+	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/oauth2 v0.18.0 // indirect
+	golang.org/x/term v0.30.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340 // indirect
+	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
+	sigs.k8s.io/yaml v1.4.0 // indirect
+)
+
+require (
+	github.com/a8m/envsubst v1.4.2
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-openapi/swag v0.23.0 // indirect
+	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
+	github.com/google/pprof v0.0.0-20241210010833-40e02aabc2ad // indirect
+	github.com/imdario/mergo v0.3.16 // indirect
+	github.com/joho/godotenv v1.5.1
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	golang.org/x/tools v0.30.0 // indirect
+	google.golang.org/protobuf v1.36.5 // indirect
+	k8s.io/apimachinery v0.30.2
+	k8s.io/client-go v0.30.2
+	k8s.io/klog/v2 v2.120.1 // indirect
+	k8s.io/utils v0.0.0-20240502163921-fe8a2dddb1d0 // indirect
+)
