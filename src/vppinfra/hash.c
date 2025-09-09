ef72be5bccf446cf4c5617bcee19ee41ff36d3cf
Vratko Polak <vrpolak@cisco.com>
2025-09-09 15:41:34 +0200

    fib: guard against reallocs
    Worker ARP can be reading vectors, so barrier is needed on API reallocs.
    
    Ticket: vpp-3628
    Type: fix
    
    Change-Id: Ib84a8f11a5585c133f917e9c1661fd887174faec
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- src/vppinfra/hash.c
+++ src/vppinfra/hash.c
@@ -650,6 +650,12 @@ hash_dup (void *old)
   return hash_resize_internal (old, vec_len (old), 0);
 }
 
+__clib_export u8
+_hash_will_expand (hash_t *h, void *v)
+{
+  return (4 * (h->elts + 1) > 3 * vec_len (v));
+}
+
 __clib_export void *
 _hash_set3 (void *v, uword key, void *value, void *old_value)
 {
@@ -664,7 +670,7 @@ _hash_set3 (void *v, uword key, void *value, void *old_value)
   if (!(h->flags & HASH_FLAG_NO_AUTO_GROW))
     {
       /* Resize when 3/4 full. */
-      if (4 * (h->elts + 1) > 3 * vec_len (v))
+      if (_hash_will_expand (h, v))
 	v = hash_resize (v, 2 * vec_len (v));
     }
 
