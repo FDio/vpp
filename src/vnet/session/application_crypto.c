2eb245a2d2d558507b11ddb2992f87b48ef6fd09
Florin Coras <fcoras@cisco.com>
2025-09-09 23:04:00 -0400

    session tls: add support for configurable trusted cas
    Type: feature
    
    Change-Id: Ic6727d03a369cf09d4397da6bff998d1811a6a61
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/vnet/session/application_crypto.c
+++ src/vnet/session/application_crypto.c
@@ -45,6 +45,36 @@ app_cert_key_pair_get_default ()
   return app_cert_key_pair_get (0);
 }
 
+app_crypto_ca_trust_t *
+app_crypto_get_ca_trust (u32 app_wrk_index, u32 ca_trust_index)
+{
+  app_worker_t *app_wrk;
+  application_t *app;
+
+  app_wrk = app_worker_get (app_wrk_index);
+  app = application_get (app_wrk->app_index);
+
+  return app_get_crypto_ca_trust (app, ca_trust_index);
+}
+
+app_crypto_ca_trust_int_ctx_t *
+app_crypto_alloc_int_ca_trust (app_crypto_ca_trust_t *ct,
+			       clib_thread_index_t thread_index)
+{
+  if (!ct->cti)
+    vec_validate (ct->cti, vlib_num_workers ());
+  return vec_elt_at_index (ct->cti, thread_index);
+}
+
+app_crypto_ca_trust_int_ctx_t *
+app_crypto_get_int_ca_trust (app_crypto_ca_trust_t *ct,
+			     clib_thread_index_t thread_index)
+{
+  if (vec_len (ct->cti) <= thread_index)
+    return 0;
+  return vec_elt_at_index (ct->cti, thread_index);
+}
+
 int
 vnet_app_add_cert_key_pair (vnet_app_add_cert_key_pair_args_t *a)
 {
@@ -189,6 +219,25 @@ void
 app_crypto_ctx_free (app_crypto_ctx_t *crypto_ctx)
 {
   app_crypto_wrk_t *crypto_wrk;
+
+  if (crypto_ctx->ca_trust_store)
+    {
+      app_crypto_ca_trust_t *ct;
+      pool_foreach (ct, crypto_ctx->ca_trust_store)
+	{
+	  app_crypto_ca_trust_int_ctx_t *cti;
+	  vec_foreach (cti, ct->cti)
+	    {
+	      if (cti->cleanup_cb)
+		(cti->cleanup_cb) (cti);
+	      cti->ca_store = 0;
+	    }
+	  vec_free (ct->cti);
+	}
+      vec_free (ct->ca_chain);
+      vec_free (ct->crl);
+      pool_free (crypto_ctx->ca_trust_store);
+    }
 
   vec_foreach (crypto_wrk, crypto_ctx->wrk)
     pool_free (crypto_wrk->reqs);
