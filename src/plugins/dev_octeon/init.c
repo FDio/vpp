c9b50af790856ff1714ae891e379ae2d2e0c798c
Damjan Marion <damarion@cisco.com>
2025-09-15 15:24:08 +0200

    octeon: add option to specify RSS flowkey bitmap
    Type: improvement
    
    Change-Id: I0fa660c96a44ede0fcb435af560544bbc0f8da04
    Signed-off-by: Damjan Marion <damarion@cisco.com>
    Signed-off-by: Monendra Singh Kushwaha <kmonendra@marvell.com>

--- src/plugins/dev_octeon/init.c
+++ src/plugins/dev_octeon/init.c
@@ -88,6 +88,15 @@ static vnet_dev_arg_t oct_port_args[] = {
     .default_val.boolean = false,
   },
   {
+    .id = OCT_PORT_ARG_RSS_FLOW_KEY,
+    .name = "rss_flow_key",
+    .desc = "RSS Flow Key Bitmap, applicable to network devices only",
+    .type = VNET_DEV_ARG_TYPE_UINT32,
+    .default_val.uint32 = FLOW_KEY_TYPE_IPV4 | FLOW_KEY_TYPE_IPV6 |
+			  FLOW_KEY_TYPE_TCP | FLOW_KEY_TYPE_UDP |
+			  FLOW_KEY_TYPE_SCTP,
+  },
+  {
     .id = OCT_PORT_ARG_END,
     .name = "end",
     .desc = "Argument end",
