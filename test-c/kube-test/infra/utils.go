c3f2c76d4218acf35bd3f40dffd57962d2a80322
Aritra Basu <aritrbas@cisco.com>
2025-09-18 14:02:29 -0700

    kube-test: added felix tests for calico in kube-test
    Type: test
    
    Change-Id: Idd8d61686cbc146e1837299f60074ab523bbbccb
    Signed-off-by: Aritra Basu <aritrbas@cisco.com>

--- test-c/kube-test/infra/utils.go
+++ test-c/kube-test/infra/utils.go
@@ -1,12 +1,16 @@
 package kube_test
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"os/exec"
 	"path/filepath"
 	"runtime"
 	"strings"
 	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func boolPtr(b bool) *bool {
@@ -15,6 +19,10 @@ func boolPtr(b bool) *bool {
 
 func int64Ptr(integer int64) *int64 {
 	return &integer
+}
+
+func int32Ptr(i int32) *int32 {
+	return &i
 }
 
 func GetTestFilename() string {
@@ -149,4 +157,79 @@ func (s *BaseSuite) LogJsonIperfOutput(result IPerfResult) {
 			result.End.Udp.LostPercent)
 	}
 	s.Log("*******************************************\n")
+}
+
+// findNodePod finds a pod with the given prefix running on the specified node
+func (s *KubeSuite) findNodePod(nodeName, podPrefix, namespace string) (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	s.Log("Searching for pods with prefix '%s' in namespace '%s' on node '%s'", podPrefix, namespace, nodeName)
+
+	pods, err := s.ClientSet.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{
+		FieldSelector: fmt.Sprintf("spec.nodeName=%s", nodeName),
+	})
+	if err != nil {
+		s.Log("Failed to list pods in namespace %s: %v", namespace, err)
+		return "", err
+	}
+
+	for _, pod := range pods.Items {
+		if strings.HasPrefix(pod.Name, podPrefix) {
+			s.Log("Found pod %s on node %s", pod.Name, nodeName)
+			return pod.Name, nil
+		}
+	}
+
+	return "", fmt.Errorf("pod with prefix '%s' not found on node '%s' in namespace '%s'", podPrefix, nodeName, namespace)
+}
+
+// execInPod executes a command in a specific container within a pod
+func (s *KubeSuite) execInPod(namespace, podName, containerName string, command ...string) ([]byte, error) {
+	kubectlArgs := append([]string{
+		"exec",
+		"-n", namespace,
+		"-c", containerName,
+		podName,
+		"--",
+	}, command...)
+
+	cmd := exec.Command("kubectl", kubectlArgs...)
+	s.Log(cmd.String())
+	return cmd.CombinedOutput()
+}
+
+// ExecInKindNodeContainer executes a command inside a container within a KinD node
+// This uses kubectl exec to access pods running on the specified KinD node
+// kindNodeName: name of the KinD node (e.g., "kind-worker", "kind-worker2")
+// containerName: name of the container within the pod (e.g., "vpp", "agent")
+// command: the command to execute as separate arguments
+func (s *KubeSuite) ExecInKindNodeContainer(kindNodeName, containerName string, command ...string) ([]byte, error) {
+	// Find the Calico VPP pod on the specified node
+	podName, err := s.findNodePod(kindNodeName, "calico-vpp-node", "calico-vpp-dataplane")
+	if err != nil {
+		s.Log("Failed to find Calico VPP pod on %s: %v", kindNodeName, err)
+		return nil, err
+	}
+
+	// Execute the command in the pod
+	output, err := s.execInPod("calico-vpp-dataplane", podName, containerName, command...)
+	if err != nil {
+		s.Log("Command failed on %s in container %s: %v", kindNodeName, containerName, err)
+		return output, err
+	}
+
+	return output, nil
+}
+
+// ExecVppctlInKindNode executes a vppctl command in the Calico VPP container on a KinD node
+func (s *KubeSuite) ExecVppctlInKindNode(kindNodeName string, vppctlArgs ...string) ([]byte, error) {
+	// Use the full path to vppctl and add the socket path as per the sample code
+	vppctlPath := "/usr/bin/vppctl"
+	vppSockPath := "/var/run/vpp/cli.sock"
+
+	command := []string{vppctlPath, "-s", vppSockPath}
+	command = append(command, vppctlArgs...)
+
+	return s.ExecInKindNodeContainer(kindNodeName, "vpp", command...)
 }
