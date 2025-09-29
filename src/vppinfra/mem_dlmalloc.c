cd46e875e78d59b209f98bfd2872fe83400396f1
Vratko Polak <vrpolak@cisco.com>
2025-09-29 14:21:42 +0200

    vppinfra vlib: delayed free for safety
    Change-Id: Ia0bd43167d79c47224a7c33b0ee3be2915b05d3d
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- src/vppinfra/mem_dlmalloc.c
+++ src/vppinfra/mem_dlmalloc.c
@@ -804,6 +804,39 @@ clib_mem_heap_free (void *heap, void *p)
 }
 
 __clib_export __clib_flatten void
+clib_mem_heap_free_mt_safe (void *heap, void *p)
+{
+  clib_mem_heap_t *h;
+  clib_delayed_free_entry_t *e;
+  vlib_main_t *vm = vlib_get_main ();
+
+  /*
+   * For now, only the main thread can perform mt-safe reallocations.
+   * This could be relaxed later if a multi-writer system is implemented.
+   */
+  ASSERT (vlib_get_thread_index () == 0);
+
+  /* If no workers, no need for delayed free. */
+  if (vlib_num_workers () == 0)
+    {
+      clib_mem_heap_free (heap, p);
+      return;
+    }
+
+  if (heap == 0)
+    h = clib_mem_get_per_cpu_heap ();
+  else
+    h = heap;
+
+  /* Allocate the element in the vector. */
+  vec_add2 (vm->pending_frees, e, 1);
+
+  e->ptr_to_free = p;
+  e->heap = h;
+  e->type = CLIB_DELAYED_FREE_DLMALLOC;
+}
+
+__clib_export __clib_flatten void
 clib_mem_free (void *p)
 {
   clib_mem_heap_free (0, p);
