ecd20196b4efd2e263ab395b17211db5e9f60171
Matus Fabian <matfabia@cisco.com>
2025-10-06 13:03:14 -0400

    session: move tls_alpn_proto_t to transport_types
    Type: refactor
    
    Change-Id: I0a314ba5111f6c706296d4f949f8b107492e188a
    Signed-off-by: Matus Fabian <matfabia@cisco.com>

--- src/vnet/tls/tls_types.h
+++ src/vnet/tls/tls_types.h
@@ -7,47 +7,6 @@
 
 #include <vppinfra/types.h>
 
-#define foreach_tls_alpn_protos                                               \
-  _ (NONE, "none")                                                            \
-  _ (HTTP_1_1, "http/1.1")                                                    \
-  _ (HTTP_2, "h2")                                                            \
-  _ (HTTP_3, "h3")                                                            \
-  _ (IMAP, "imap")                                                            \
-  _ (POP3, "pop3")                                                            \
-  _ (SMB2, "smb")                                                             \
-  _ (TURN, "stun.turn")                                                       \
-  _ (STUN, "stun.nat-discovery")                                              \
-  _ (WEBRTC, "webrtc")                                                        \
-  _ (CWEBRTC, "c-webrtc")                                                     \
-  _ (FTP, "ftp")                                                              \
-  _ (MANAGE_SIEVE, "managesieve")                                             \
-  _ (COAP_TLS, "coap")                                                        \
-  _ (COAP_DSTL, "co")                                                         \
-  _ (XMPP_CLIENT, "xmpp-client")                                              \
-  _ (XMPP_SERVER, "xmpp-server")                                              \
-  _ (ACME_TLS_1, "acme-tls/1")                                                \
-  _ (MQTT, "mqtt")                                                            \
-  _ (DNS_OVER_TLS, "dot")                                                     \
-  _ (NTSKE_1, "ntske/1")                                                      \
-  _ (SUN_RPC, "sunrpc")                                                       \
-  _ (IRC, "irc")                                                              \
-  _ (NNTP, "nntp")                                                            \
-  _ (NNSP, "nnsp")                                                            \
-  _ (DOQ, "doq")                                                              \
-  _ (SIP_2, "sip/2")                                                          \
-  _ (TDS_8_0, "tds/8.0")                                                      \
-  _ (DICOM, "dicom")                                                          \
-  _ (POSTGRESQL, "postgresql")                                                \
-  _ (RADIUS_1_0, "radius/1.0")                                                \
-  _ (RADIUS_1_1, "radius/1.1")
-
-typedef enum tls_alpn_proto_
-{
-#define _(sym, str) TLS_ALPN_PROTO_##sym,
-  foreach_tls_alpn_protos
-#undef _
-} __clib_packed tls_alpn_proto_t;
-
 typedef struct tls_alpn_proto_id_
 {
   u8 len;
