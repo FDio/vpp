e2d1350333ae7445689af61f441123b2607e558e
Adrian Villin <avillin@cisco.com>
2025-10-07 09:09:41 -0400

    hs-test: run iperf server as daemon
    Type: test
    
    Change-Id: I325accfa89a6ae7cf69b29cd9a471fc859696999
    Signed-off-by: Adrian Villin <avillin@cisco.com>

--- test-c/hs-test/iperf_linux_test.go
+++ test-c/hs-test/iperf_linux_test.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	. "fd.io/hs-test/infra"
-	. "github.com/onsi/ginkgo/v2"
 )
 
 func init() {
@@ -16,33 +14,18 @@ func IperfUdpLinuxTest(s *IperfSuite) {
 	serverIpAddress := s.Interfaces.Server.Ip4AddressString()
 	clientIpAddress := s.Interfaces.Client.Ip4AddressString()
 
-	clnCh := make(chan error)
-	stopServerCh := make(chan struct{})
-	srvCh := make(chan error, 1)
-	clnRes := make(chan []byte, 1)
-
-	defer func() {
-		stopServerCh <- struct{}{}
-	}()
-
-	go func() {
-		defer GinkgoRecover()
-		cmd := "iperf3 -4 -s -B " + serverIpAddress + " -p " + s.Ports.Port1
-		s.StartServerApp(s.Containers.Server, "iperf3", cmd, srvCh, stopServerCh)
-	}()
-	err := <-srvCh
-	s.AssertNil(err, fmt.Sprint(err))
+	cmd := fmt.Sprintf("iperf3 -4 -s -1 -D -B %s -p %s --logfile %s",
+		serverIpAddress, s.Ports.Port1, s.IperfLogFileName(s.Containers.Server))
+	o, err := s.Containers.Server.Exec(false, cmd)
+	s.AssertNil(err, o)
 	s.Log("server running")
 
-	go func() {
-		defer GinkgoRecover()
-		cmd := "iperf3 -c " + serverIpAddress + " -B " + clientIpAddress +
-			" -u -l 1460 -b 10g -J -p " + s.Ports.Port1
-		s.StartClientApp(s.Containers.Client, cmd, clnCh, clnRes)
-	}()
-	s.AssertChannelClosed(time.Minute*3, clnCh)
-	output := <-clnRes
-	result := s.ParseJsonIperfOutput(output)
+	cmd = "iperf3 -c " + serverIpAddress + " -B " + clientIpAddress +
+		" -u -l 1460 -b 10g -J -p " + s.Ports.Port1
+	o, err = s.Containers.Client.Exec(false, cmd)
+
+	s.AssertNil(err, o)
+	result := s.ParseJsonIperfOutput([]byte(o))
 	s.LogJsonIperfOutput(result)
 	s.AssertIperfMinTransfer(result, 400)
 }
