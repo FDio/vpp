64968f8a8b627215435fe0485eb67b33f7588670
Matus Fabian <matfabia@cisco.com>
2025-10-16 10:04:34 -0400

    session: add vnet_connect_stream
    Ask transport to open stream on existing connection, stream must be
    opened on same thread as parent connection. For internall
    applications stream is opened instantaneously.
    Transport protocl must implement connect_stream callback.
    This is useful for protocols like HTTP/2, HTTP/3 or QUIC which use
    streams to provide lightweight, ordered byte-stream abstraction to an
    application, they are created by sending data.
    
    Type: improvement
    
    Change-Id: I4b6b5a003a6e1c56135cb26e067c42956ba5ae06
    Signed-off-by: Matus Fabian <matfabia@cisco.com>

--- src/vnet/session/session.c
+++ src/vnet/session/session.c
@@ -505,6 +505,26 @@ session_alloc_for_connection (transport_connection_t * tc)
   return s;
 }
 
+static session_t *
+session_alloc_for_stream (transport_connection_t *tc,
+			  session_handle_t parent_handle)
+{
+  session_t *s, *ps;
+  clib_thread_index_t thread_index = tc->thread_index;
+
+  ASSERT (thread_index == vlib_get_thread_index ());
+
+  ps = session_get_from_handle (parent_handle);
+  s = session_alloc (thread_index);
+  s->session_type = ps->session_type;
+  session_set_state (s, SESSION_STATE_CLOSED);
+
+  /* Attach transport to session and vice versa */
+  s->connection_index = tc->c_index;
+  tc->s_index = s->session_index;
+  return s;
+}
+
 session_t *
 session_alloc_for_half_open (transport_connection_t *tc)
 {
@@ -1347,6 +1367,57 @@ session_open (session_endpoint_cfg_t *rmt, session_handle_t *rsh)
   transport_service_type_t tst;
   tst = transport_protocol_service_type (rmt->transport_proto);
   return session_open_srv_fns[tst](rmt, rsh);
+}
+
+/**
+ * Ask transport to open stream on existing connection.
+ */
+int
+session_open_stream (session_endpoint_cfg_t *sep, session_handle_t *rsh)
+{
+  transport_connection_t *tc;
+  transport_endpoint_cfg_t *tep;
+  app_worker_t *app_wrk;
+  session_t *s;
+  u32 conn_index;
+  int rv;
+
+  app_wrk = app_worker_get (sep->app_wrk_index);
+  tep = session_endpoint_to_transport_cfg (sep);
+  rv = transport_connect_stream (sep->transport_proto, tep, &conn_index);
+  if (rv < 0)
+    {
+      SESSION_DBG ("Transport failed to open stream.");
+      if (app_worker_application_is_builtin (app_wrk))
+	return rv;
+      return app_worker_connect_notify (app_wrk, 0, rv, sep->opaque);
+    }
+
+  tc =
+    transport_get_connection (sep->transport_proto, conn_index,
+			      session_thread_from_handle (sep->parent_handle));
+
+  /* allocate session and fifos now */
+  s = session_alloc_for_stream (tc, sep->parent_handle);
+  s->app_wrk_index = app_wrk->wrk_index;
+  s->opaque = sep->opaque;
+  session_set_state (s, SESSION_STATE_READY);
+  s->flags |= SESSION_F_STREAM;
+  if ((rv = app_worker_init_connected (app_wrk, s)))
+    {
+      session_free (s);
+      if (app_worker_application_is_builtin (app_wrk))
+	return rv;
+      return app_worker_connect_notify (app_wrk, 0, rv, sep->opaque);
+    }
+
+  *rsh = session_handle (s);
+
+  /* builtin apps are synchronous */
+  if (app_worker_application_is_builtin (app_wrk))
+    return SESSION_E_NONE;
+
+  return app_worker_connect_notify (app_wrk, s, SESSION_E_NONE, sep->opaque);
 }
 
 /**
