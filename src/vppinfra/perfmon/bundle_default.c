0794a31a48bf662f9b12ddfb903d42d53b9fb746
Damjan Marion <damarion@cisco.com>
2025-10-28 17:48:15 +0100

    vppinfra: fix perfmon default bundle to work on aatch64
    Type: fix
    Change-Id: I5f9094be6bc2660340b2742a243475ec85dcbee8
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- src/vppinfra/perfmon/bundle_default.c
+++ src/vppinfra/perfmon/bundle_default.c
@@ -9,7 +9,7 @@
 static u8 *
 format_perfmon_bundle_default (u8 *s, va_list *args)
 {
-  clib_perfmon_ctx_t *ctx = va_arg (*args, clib_perfmon_ctx_t *);
+  clib_perfmon_ctx_t __clib_unused *ctx = va_arg (*args, clib_perfmon_ctx_t *);
   clib_perfmon_capture_t *c = va_arg (*args, clib_perfmon_capture_t *);
   u32 col = va_arg (*args, int);
   u64 *d = c->data;
@@ -17,28 +17,30 @@ format_perfmon_bundle_default (u8 *s, va_list *args)
   switch (col)
     {
     case 0:
+      return format (s, "%5.2f", (f64) d[1] / d[0]);
+    case 1:
+      return format (s, "%8u", d[0]);
+    case 2:
+      return format (s, "%8.2f", (f64) d[0] / c->n_ops);
+    case 3:
+      return format (s, "%8u", d[1]);
+    case 4:
+      return format (s, "%8.2f", (f64) d[1] / c->n_ops);
+    case 5:
+      return format (s, "%9u", d[2]);
+    case 6:
+      return format (s, "%9.2f", (f64) d[2] / c->n_ops);
+    case 7:
+      return format (s, "%10u", d[3]);
+    case 8:
+      return format (s, "%10.2f", (f64) d[3] / c->n_ops);
+#ifdef __x86_64__
+    case 9:
       if (ctx->ref_clock > 0)
-	return format (s, "%8.1f", (f64) d[0] / d[1] * (ctx->ref_clock / 1e9));
+	return format (s, "%8.1f", (f64) d[0] / d[4] * (ctx->ref_clock / 1e9));
       else
 	return s;
-    case 1:
-      return format (s, "%5.2f", (f64) d[2] / d[0]);
-    case 2:
-      return format (s, "%8u", d[0]);
-    case 3:
-      return format (s, "%8.2f", (f64) d[0] / c->n_ops);
-    case 4:
-      return format (s, "%8u", d[2]);
-    case 5:
-      return format (s, "%8.2f", (f64) d[2] / c->n_ops);
-    case 6:
-      return format (s, "%9u", d[3]);
-    case 7:
-      return format (s, "%9.2f", (f64) d[3] / c->n_ops);
-    case 8:
-      return format (s, "%10u", d[4]);
-    case 9:
-      return format (s, "%10.2f", (f64) d[4] / c->n_ops);
+#endif
     default:
       return s;
     }
@@ -49,13 +51,22 @@ CLIB_PERFMON_BUNDLE (default) = {
   .desc = "IPC, Clocks/Operatiom, Instr/Operation, Branch Total & Miss",
   .type = PERF_TYPE_HARDWARE,
   .config[0] = PERF_COUNT_HW_CPU_CYCLES,
-  .config[1] = PERF_COUNT_HW_REF_CPU_CYCLES,
-  .config[2] = PERF_COUNT_HW_INSTRUCTIONS,
-  .config[3] = PERF_COUNT_HW_BRANCH_INSTRUCTIONS,
-  .config[4] = PERF_COUNT_HW_BRANCH_MISSES,
+  .config[1] = PERF_COUNT_HW_INSTRUCTIONS,
+  .config[2] = PERF_COUNT_HW_BRANCH_INSTRUCTIONS,
+  .config[3] = PERF_COUNT_HW_BRANCH_MISSES,
+#ifdef __x86_64__
+  .config[4] = PERF_COUNT_HW_REF_CPU_CYCLES,
   .n_events = 5,
+#else
+  .n_events = 4,
+#endif
   .format_fn = format_perfmon_bundle_default,
-  .column_headers = CLIB_STRING_ARRAY ("Freq", "IPC", "Clks", "Clks/Op",
-				       "Inst", "Inst/Op", "Brnch", "Brnch/Op",
-				       "BrMiss", "BrMiss/Op"),
+  .column_headers = CLIB_STRING_ARRAY ("IPC", "Clks", "Clks/Op", "Inst",
+				       "Inst/Op", "Brnch", "Brnch/Op",
+				       "BrMiss", "BrMiss/Op"
+#ifdef __x86_64__
+				       ,
+				       "Freq"
+#endif
+				       ),
 };
