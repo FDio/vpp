01415b0e4a09f25f31c7041e35c6c557d8024e60
Florin Coras <fcoras@cisco.com>
2025-10-29 19:15:49 -0400

    hsa: do not initialize heap in vcl test server
    Type: fix
    
    Change-Id: Ib2da1aedf260197bd4552873a2b190e50aa6e7ff
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/plugins/hs_apps/vcl/vcl_test_server.c
+++ src/plugins/hs_apps/vcl/vcl_test_server.c
@@ -865,7 +865,6 @@ main (int argc, char **argv)
   vcl_test_main_t *vt = &vcl_test_main;
   int rv, i;
 
-  clib_mem_init (0, 64 << 20);
   vsm->server_cfg.port = VCL_TEST_SERVER_PORT;
   vsm->server_cfg.workers = 1;
   vsm->active_workers = 0;
