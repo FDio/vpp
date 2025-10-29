c238ad67ffc00d2bcdee056f23bf1e49006fd8aa
Damjan Marion <damarion@cisco.com>
2025-10-29 20:19:02 +0100

    vppinfra: remove per-cpu and per-numa heap concept
    They all point to single heap....
    
    thread and numa awareness should be part of heap inplementation.
    
    Type: improvement
    Change-Id: Ifc97d092f2327d271503b709c5f75fa4ae60170a
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- src/vppinfra/mem_dlmalloc.c
+++ src/vppinfra/mem_dlmalloc.c
@@ -650,7 +650,7 @@ static inline void *
 clib_mem_heap_alloc_inline (void *heap, uword size, uword align,
 			    int os_out_of_memory_on_failure)
 {
-  clib_mem_heap_t *h = heap ? heap : clib_mem_get_per_cpu_heap ();
+  clib_mem_heap_t *h = heap ? heap : clib_mem_get_heap ();
   void *p;
 
   align = clib_max (CLIB_MEM_MIN_ALIGN, align);
@@ -734,7 +734,7 @@ clib_mem_heap_realloc_aligned (void *heap, void *p, uword new_size,
 			       uword align)
 {
   uword old_alloc_size;
-  clib_mem_heap_t *h = heap ? heap : clib_mem_get_per_cpu_heap ();
+  clib_mem_heap_t *h = heap ? heap : clib_mem_get_heap ();
   void *new;
 
   ASSERT (count_set_bits (align) == 1);
@@ -793,7 +793,7 @@ clib_mem_realloc (void *p, uword new_size)
 __clib_export __clib_flatten uword
 clib_mem_heap_is_heap_object (void *heap, void *p)
 {
-  clib_mem_heap_t *h = heap ? heap : clib_mem_get_per_cpu_heap ();
+  clib_mem_heap_t *h = heap ? heap : clib_mem_get_heap ();
   return mspace_is_heap_object (h->mspace, p);
 }
 
@@ -806,7 +806,7 @@ clib_mem_is_heap_object (void *p)
 __clib_export __clib_flatten void
 clib_mem_heap_free (void *heap, void *p)
 {
-  clib_mem_heap_t *h = heap ? heap : clib_mem_get_per_cpu_heap ();
+  clib_mem_heap_t *h = heap ? heap : clib_mem_get_heap ();
   uword size = clib_mem_size (p);
 
   /* Make sure object is in the correct heap. */
