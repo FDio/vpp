FDio/vpp

commit 0bbad0fc924d4fe990746e512a1500293c2cef06

vppinfra: remove per-cpu and per-numa heap concept
They all point to single heap....

thread and numa awareness should be part of heap inplementation.

Type: improvement
Change-Id: Ifc97d092f2327d271503b709c5f75fa4ae60170a
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/vppinfra/mem.h
@@ -114,6 +114,10 @@ typedef struct clib_mem_heap_t clib_mem_heap_t;
 
 typedef struct
 {
+  /* main and active heap */
+  clib_mem_heap_t *main_heap;
+  clib_mem_heap_t *active_heap;
+
   /* log2 system page size */
   clib_mem_page_sz_t log2_page_sz;
 
@@ -125,12 +129,6 @@ typedef struct
 
   /* bitmap of available numa nodes */
   u32 numa_node_bitmap;
-
-  /* per CPU heaps */
-  void *per_cpu_mheaps[CLIB_MAX_MHEAPS];
-
-  /* per NUMA heaps */
-  void *per_numa_mheaps[CLIB_MAX_NUMAS];
 
   /* memory maps */
   clib_mem_vm_map_hdr_t *first_map, *last_map;
@@ -161,59 +159,6 @@ clib_mem_unpoison (const void volatile *p, uword s)
 #ifdef CLIB_SANITIZE_ADDR
   ASAN_UNPOISON_MEMORY_REGION (p, s);
 #endif
-}
-
-always_inline clib_mem_heap_t *
-clib_mem_get_per_cpu_heap (void)
-{
-  int cpu = os_get_thread_index ();
-  return clib_mem_main.per_cpu_mheaps[cpu];
-}
-
-always_inline void *
-clib_mem_set_per_cpu_heap (void *new_heap)
-{
-  int cpu = os_get_thread_index ();
-  void *old = clib_mem_main.per_cpu_mheaps[cpu];
-  clib_mem_main.per_cpu_mheaps[cpu] = new_heap;
-  return old;
-}
-
-always_inline void *
-clib_mem_get_per_numa_heap (u32 numa_id)
-{
-  ASSERT (numa_id < ARRAY_LEN (clib_mem_main.per_numa_mheaps));
-  return clib_mem_main.per_numa_mheaps[numa_id];
-}
-
-always_inline void *
-clib_mem_set_per_numa_heap (void *new_heap)
-{
-  int numa = os_get_numa_index ();
-  void *old = clib_mem_main.per_numa_mheaps[numa];
-  clib_mem_main.per_numa_mheaps[numa] = new_heap;
-  return old;
-}
-
-always_inline void
-clib_mem_set_thread_index (void)
-{
-  /*
-   * Find an unused slot in the per-cpu-mheaps array,
-   * and grab it for this thread. We need to be able to
-   * push/pop the thread heap without affecting other thread(s).
-   */
-  int i;
-  if (__os_thread_index != 0)
-    return;
-  for (i = 0; i < ARRAY_LEN (clib_mem_main.per_cpu_mheaps); i++)
-    if (clib_atomic_bool_cmp_and_swap (&clib_mem_main.per_cpu_mheaps[i],
-				       0, clib_mem_main.per_cpu_mheaps[0]))
-      {
-	os_set_thread_index (i);
-	break;
-      }
-  ASSERT (__os_thread_index > 0);
 }
 
 /* Memory allocator which calls os_out_of_memory() when it fails */
@@ -260,13 +205,15 @@ void clib_mem_free_s (void *p);
 always_inline clib_mem_heap_t *
 clib_mem_get_heap (void)
 {
-  return clib_mem_get_per_cpu_heap ();
+  return clib_mem_main.active_heap;
 }
 
 always_inline clib_mem_heap_t *
 clib_mem_set_heap (clib_mem_heap_t * heap)
 {
-  return clib_mem_set_per_cpu_heap (heap);
+  clib_mem_heap_t *old_heap = clib_mem_main.active_heap;
+  clib_mem_main.active_heap = heap;
+  return old_heap;
 }
 
 void clib_mem_destroy_heap (clib_mem_heap_t * heap);

src/vppinfra/mem_dlmalloc.c
@@ -277,6 +277,8 @@ clib_mem_init_internal (clib_mem_init_ex_args_t *a)
 				     a->log2_page_sz, 1 /*is_locked */,
 				     "main heap");
 
+  ASSERT (clib_mem_main.main_heap == 0);
+  clib_mem_main.main_heap = h;
   clib_mem_set_heap (h);
 
   if (mheap_trace_main.lock == 0)
@@ -312,11 +314,12 @@ __clib_export void
 clib_mem_destroy (void)
 {
   mheap_trace_main_t *tm = &mheap_trace_main;
-  clib_mem_heap_t *heap = clib_mem_get_heap ();
+  clib_mem_heap_t *heap = clib_mem_main.main_heap;
 
   if (heap->mspace == tm->current_traced_mheap)
     mheap_trace (heap, 0);
 
+  clib_mem_main.main_heap = 0;
   destroy_mspace (heap->mspace);
   clib_mem_vm_unmap (heap);
 }
@@ -650,7 +653,7 @@ static inline void *
 clib_mem_heap_alloc_inline (void *heap, uword size, uword align,
 			    int os_out_of_memory_on_failure)
 {
-  clib_mem_heap_t *h = heap ? heap : clib_mem_get_per_cpu_heap ();
+  clib_mem_heap_t *h = heap ? heap : clib_mem_get_heap ();
   void *p;
 
   align = clib_max (CLIB_MEM_MIN_ALIGN, align);
@@ -734,7 +737,7 @@ clib_mem_heap_realloc_aligned (void *heap, void *p, uword new_size,
 			       uword align)
 {
   uword old_alloc_size;
-  clib_mem_heap_t *h = heap ? heap : clib_mem_get_per_cpu_heap ();
+  clib_mem_heap_t *h = heap ? heap : clib_mem_get_heap ();
   void *new;
 
   ASSERT (count_set_bits (align) == 1);
@@ -793,7 +796,7 @@ clib_mem_realloc (void *p, uword new_size)
 __clib_export __clib_flatten uword
 clib_mem_heap_is_heap_object (void *heap, void *p)
 {
-  clib_mem_heap_t *h = heap ? heap : clib_mem_get_per_cpu_heap ();
+  clib_mem_heap_t *h = heap ? heap : clib_mem_get_heap ();
   return mspace_is_heap_object (h->mspace, p);
 }
 
@@ -806,7 +809,7 @@ clib_mem_is_heap_object (void *p)
 __clib_export __clib_flatten void
 clib_mem_heap_free (void *heap, void *p)
 {
-  clib_mem_heap_t *h = heap ? heap : clib_mem_get_per_cpu_heap ();
+  clib_mem_heap_t *h = heap ? heap : clib_mem_get_heap ();
   uword size = clib_mem_size (p);
 
   /* Make sure object is in the correct heap. */

src/plugins/hs_apps/sapi/vpp_echo.c
@@ -453,7 +453,6 @@ echo_handle_data (echo_main_t * em, echo_session_t * s, u8 * rx_buf)
 static void *
 echo_data_thread_fn (void *arg)
 {
-  clib_mem_set_thread_index ();	/* First thing to do in client thread */
   echo_main_t *em = &echo_main;
   u32 N = em->n_clients;
   u32 n = (N + em->n_rx_threads - 1) / em->n_rx_threads;
@@ -874,7 +873,6 @@ echo_print_periodic_stats (echo_main_t * em)
 static void *
 echo_mq_thread_fn (void *arg)
 {
-  clib_mem_set_thread_index ();	/* First thing to do in client thread */
   svm_msg_q_msg_t *msg_vec = 0;
   echo_main_t *em = &echo_main;
   session_event_t *e;

src/plugins/unittest/session_test.c
@@ -2363,7 +2363,7 @@ session_test_mq_basic (vlib_main_t * vm, unformat_input_t * input)
 static f32
 session_get_memory_usage (void)
 {
-  clib_mem_heap_t *heap = clib_mem_get_per_cpu_heap ();
+  clib_mem_heap_t *heap = clib_mem_get_heap ();
   u8 *s = 0;
   char *ss;
   f32 used = 0.0;

src/vcl/vcl_private.c
@@ -284,9 +284,6 @@ vcl_worker_alloc_and_init ()
   /* Grab lock before selecting mem thread index */
   clib_spinlock_lock (&vcm->workers_lock);
 
-  /* Use separate heap map entry for worker */
-  clib_mem_set_thread_index ();
-
   if (pool_elts (vcm->workers) == vcm->cfg.max_workers)
     {
       VDBG (0, "max-workers %u limit reached", vcm->cfg.max_workers);

src/vlib/cli.c
@@ -809,8 +809,6 @@ show_memory_usage (vlib_main_t * vm,
   int api_segment = 0, stats_segment = 0, main_heap = 0, numa_heaps = 0;
   int map = 0;
   clib_error_t *error;
-  u32 index = 0;
-  int i;
   uword clib_mem_trace_enable_disable (uword enable);
   uword was_enabled;
 
@@ -889,35 +887,11 @@ show_memory_usage (vlib_main_t * vm,
 	 */
 	was_enabled = clib_mem_trace_enable_disable (0);
 
-	foreach_vlib_main ()
-	  {
-	    vlib_cli_output (vm, "%sThread %d %s\n", index ? "\n" : "", index,
-			     vlib_worker_threads[index].name);
-	    vlib_cli_output (vm, "  %U\n", format_clib_mem_heap,
-			     mm->per_cpu_mheaps[index], verbose);
-	    index++;
-	  }
+	vlib_cli_output (vm, "  %U\n", format_clib_mem_heap, mm->main_heap,
+			 verbose);
 
 	/* Restore the trace flag */
 	clib_mem_trace_enable_disable (was_enabled);
-      }
-    if (numa_heaps)
-      {
-	for (i = 0; i < ARRAY_LEN (mm->per_numa_mheaps); i++)
-	  {
-	    if (mm->per_numa_mheaps[i] == 0)
-	      continue;
-	    if (mm->per_numa_mheaps[i] == mm->per_cpu_mheaps[i])
-	      {
-		vlib_cli_output (vm, "Numa %d uses the main heap...", i);
-		continue;
-	      }
-	    was_enabled = clib_mem_trace_enable_disable (0);
-
-	    vlib_cli_output (vm, "Numa %d:", i);
-	    vlib_cli_output (vm, "  %U\n", format_clib_mem_heap,
-			     mm->per_numa_mheaps[index], verbose);
-	  }
       }
     if (map)
       {
@@ -1008,13 +982,11 @@ enable_disable_memory_trace (vlib_main_t * vm,
 			     unformat_input_t * input,
 			     vlib_cli_command_t * cmd)
 {
-  clib_mem_main_t *mm = &clib_mem_main;
   unformat_input_t _line_input, *line_input = &_line_input;
   int enable = 1;
   int api_segment = 0;
   int stats_segment = 0;
   int main_heap = 0;
-  u32 numa_id = ~0;
   void *oldheap;
 
   if (!unformat_user (input, unformat_line_input, line_input))
@@ -1030,8 +1002,6 @@ enable_disable_memory_trace (vlib_main_t * vm,
 	stats_segment = 1;
       else if (unformat (line_input, "main-heap"))
 	main_heap = 1;
-      else if (unformat (line_input, "numa-heap %d", &numa_id))
-	;
       else
 	{
 	  unformat_free (line_input);
@@ -1040,12 +1010,11 @@ enable_disable_memory_trace (vlib_main_t * vm,
     }
   unformat_free (line_input);
 
-  if ((api_segment + stats_segment + main_heap + (enable == 0)
-       + (numa_id != ~0)) == 0)
+  if ((api_segment + stats_segment + main_heap + (enable == 0)) == 0)
     {
-      return clib_error_return
-	(0, "Need one of main-heap, stats-segment, api-segment,\n"
-	 "numa-heap <nn> or disable");
+      return clib_error_return (
+	0, "Need one of main-heap, stats-segment, api-segment,\n"
+	   "or disable");
     }
 
   /* Turn off current trace, if any */
@@ -1088,23 +1057,6 @@ enable_disable_memory_trace (vlib_main_t * vm,
       current_traced_heap = clib_mem_get_heap ();
       clib_mem_trace (main_heap);
     }
-
-  if (numa_id != ~0)
-    {
-      if (numa_id >= ARRAY_LEN (mm->per_numa_mheaps))
-	return clib_error_return (0, "Numa %d out of range", numa_id);
-      if (mm->per_numa_mheaps[numa_id] == 0)
-	return clib_error_return (0, "Numa %d heap not active", numa_id);
-
-      if (mm->per_numa_mheaps[numa_id] == clib_mem_get_heap ())
-	return clib_error_return (0, "Numa %d uses the main heap...",
-				  numa_id);
-      current_traced_heap = mm->per_numa_mheaps[numa_id];
-      oldheap = clib_mem_set_heap (current_traced_heap);
-      clib_mem_trace (1);
-      clib_mem_set_heap (oldheap);
-    }
-
 
   return 0;
 }

src/vlib/threads.c
@@ -518,36 +518,13 @@ vlib_get_thread_core_numa (vlib_worker_thread_t * w, unsigned cpu_id)
 static clib_error_t *
 vlib_launch_thread_int (void *fp, vlib_worker_thread_t * w, unsigned cpu_id)
 {
-  clib_mem_main_t *mm = &clib_mem_main;
-  vlib_thread_main_t *tm = &vlib_thread_main;
   pthread_t worker;
   pthread_attr_t attr;
   cpu_set_t cpuset;
   void *(*fp_arg) (void *) = fp;
-  void *numa_heap;
 
   w->cpu_id = cpu_id;
   vlib_get_thread_core_numa (w, cpu_id);
-
-  /* Set up NUMA-bound heap if indicated */
-  if (mm->per_numa_mheaps[w->numa_id] == 0)
-    {
-      /* If the user requested a NUMA heap, create it... */
-      if (tm->numa_heap_size)
-	{
-	  clib_mem_set_numa_affinity (w->numa_id, 1 /* force */ );
-	  numa_heap = clib_mem_create_heap (0 /* DIY */ , tm->numa_heap_size,
-					    1 /* is_locked */ ,
-					    "numa %u heap", w->numa_id);
-	  clib_mem_set_default_numa_affinity ();
-	  mm->per_numa_mheaps[w->numa_id] = numa_heap;
-	}
-      else
-	{
-	  /* Or, use the main heap */
-	  mm->per_numa_mheaps[w->numa_id] = w->thread_mheap;
-	}
-    }
 
       CPU_ZERO (&cpuset);
       CPU_SET (cpu_id, &cpuset);
@@ -586,7 +563,7 @@ start_workers (vlib_main_t * vm)
   u32 n_vlib_mains = tm->n_vlib_mains;
   u32 worker_thread_index;
   u32 stats_err_entry_index = fvm->error_main.stats_err_entry_index;
-  clib_mem_heap_t *main_heap = clib_mem_get_per_cpu_heap ();
+  clib_mem_heap_t *main_heap = clib_mem_get_heap ();
   vlib_stats_register_mem_heap (main_heap);
 
   vec_reset_length (vlib_worker_threads);

src/vlibmemory/memory_client.c
@@ -75,7 +75,6 @@ rx_thread_fn (void *arg)
   if (setjmp (mm->rx_thread_jmpbuf) == 0)
     {
       mm->rx_thread_jmpbuf_valid = 1;
-      clib_mem_set_thread_index ();
       while (1)
 	vl_msg_api_queue_handler (q);
     }

commit 344c7fa93c7b5169894aed8f1c230bfecc7c3129

vppinfra: thread safe active heap tracking
Type: fix
Fixes: 0bbad0fc924d4fe990746e512a1500293c2cef06

Change-Id: I7b021dc36526cdb88d81d2bbb46db0957607dd98
Signed-off-by: Matus Fabian <matfabia@cisco.com>

src/vppinfra/mem.h
@@ -116,7 +116,7 @@ typedef struct
 {
   /* main and active heap */
   clib_mem_heap_t *main_heap;
-  clib_mem_heap_t *active_heap;
+  clib_mem_heap_t *active_heap[CLIB_MAX_MHEAPS];
 
   /* log2 system page size */
   clib_mem_page_sz_t log2_page_sz;
@@ -205,14 +205,16 @@ void clib_mem_free_s (void *p);
 always_inline clib_mem_heap_t *
 clib_mem_get_heap (void)
 {
-  return clib_mem_main.active_heap;
+  int cpu = os_get_thread_index ();
+  return clib_mem_main.active_heap[cpu];
 }
 
 always_inline clib_mem_heap_t *
 clib_mem_set_heap (clib_mem_heap_t * heap)
 {
-  clib_mem_heap_t *old_heap = clib_mem_main.active_heap;
-  clib_mem_main.active_heap = heap;
+  int cpu = os_get_thread_index ();
+  clib_mem_heap_t *old_heap = clib_mem_main.active_heap[cpu];
+  clib_mem_main.active_heap[cpu] = heap;
   return old_heap;
 }
 

src/vppinfra/mem_dlmalloc.c
@@ -270,6 +270,7 @@ static void *
 clib_mem_init_internal (clib_mem_init_ex_args_t *a)
 {
   clib_mem_heap_t *h;
+  int i;
 
   clib_mem_main_init ();
 
@@ -279,7 +280,8 @@ clib_mem_init_internal (clib_mem_init_ex_args_t *a)
 
   ASSERT (clib_mem_main.main_heap == 0);
   clib_mem_main.main_heap = h;
-  clib_mem_set_heap (h);
+  for (i = 0; i < CLIB_MAX_MHEAPS; i++)
+    clib_mem_main.active_heap[i] = h;
 
   if (mheap_trace_main.lock == 0)
     {