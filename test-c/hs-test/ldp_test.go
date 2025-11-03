2e2fdf683e7122e895fb32be8e9525bf28188682
Adrian Villin <avillin@cisco.com>
2025-11-03 10:32:54 -0500

    hs-test: simplify iperf tests
    - running as daemon where possible
    
    Type: test
    
    Change-Id: Ie8fbb84609d109ecf95b52e452a6a1e75012d42f
    Signed-off-by: Adrian Villin <avillin@cisco.com>

--- test-c/hs-test/ldp_test.go
+++ test-c/hs-test/ldp_test.go
@@ -90,33 +90,20 @@ func LdpIperfUdpTest(s *LdpSuite) {
 
 func ldPreloadIperf(s *LdpSuite, extraClientArgs string) IPerfResult {
 	serverVethAddress := s.Interfaces.Server.Ip4AddressString()
-	stopServerCh := make(chan struct{}, 1)
-	srvCh := make(chan error, 1)
-	clnCh := make(chan error)
-	clnRes := make(chan []byte, 1)
 
-	defer func() {
-		stopServerCh <- struct{}{}
-	}()
+	// running as daemon makes reorder tests unstable
+	cmd := fmt.Sprintf("sh -c \"iperf3 -4 -s --one-off -B %s -p %s --logfile %s\"",
+		serverVethAddress, s.Ports.Port1, s.IperfLogFileName(s.Containers.ServerApp))
+	s.Containers.ServerApp.ExecServer(true, cmd)
 
-	go func() {
-		defer GinkgoRecover()
-		cmd := fmt.Sprintf("sh -c \"iperf3 -4 -s -B %s -p %s > %s 2>&1\"", serverVethAddress, s.Ports.Port1, s.IperfLogFileName(s.Containers.ServerApp))
-		s.StartServerApp(s.Containers.ServerApp, "iperf3", cmd, srvCh, stopServerCh)
-	}()
+	cmd = fmt.Sprintf("iperf3 -c %s -B %s -l 1460 -b 10g -J -p %s %s", serverVethAddress, s.Interfaces.Client.Ip4AddressString(), s.Ports.Port1, extraClientArgs)
+	o, err := s.Containers.ClientApp.Exec(true, cmd)
 
-	err := <-srvCh
-	s.AssertNil(err, fmt.Sprint(err))
+	fileLog, _ := s.Containers.ServerApp.Exec(false, "cat "+s.IperfLogFileName(s.Containers.ServerApp))
+	s.Log("*** Server logs: \n%s\n***", fileLog)
 
-	go func() {
-		defer GinkgoRecover()
-		cmd := fmt.Sprintf("iperf3 -c %s -B %s -l 1460 -b 10g -J -p %s %s", serverVethAddress, s.Interfaces.Client.Ip4AddressString(), s.Ports.Port1, extraClientArgs)
-		s.StartClientApp(s.Containers.ClientApp, cmd, clnCh, clnRes)
-	}()
-
-	s.AssertChannelClosed(time.Minute*4, clnCh)
-	output := <-clnRes
-	result := s.ParseJsonIperfOutput(output)
+	s.AssertNil(err, o)
+	result := s.ParseJsonIperfOutput([]byte(o))
 	s.LogJsonIperfOutput(result)
 
 	return result
