c4a470b7dd87f2b93090e0fffdee53db1a69b30f
Mohammed Hawari <mohammed@hawari.fr>
2025-11-05 17:19:58 +0100

    vppinfra: introduce dynamic_shuffle SIMD routines
    This change introduces support for shuffle routines that use a non
    constexpr mask
    
    Change-Id: I32be770f05caf8db69ec25984e07efbb8607c749
    Type: improvement
    Signed-off-by: Mohammed Hawari <mohammed@hawari.fr>

---
+++ src/vppinfra/test/dynamic_shuffle.c
@@ -0,0 +1,152 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright(c) 2025 Cisco Systems, Inc.
+ */
+#pragma GCC diagnostic ignored "-Wpsabi"
+#include <vppinfra/format.h>
+#include <vppinfra/random.h>
+#include <vppinfra/test/test.h>
+#include <vppinfra/vector.h>
+
+#include <string.h>
+
+#define NUM_TEST_ITERS	256
+#define PERF_BATCH_SIZE 256
+
+#define TEST_SHUFFLE_MASK(func, type, scalar_t, lanes, select_mask)           \
+  do                                                                          \
+    {                                                                         \
+      const scalar_t select_mask_local = (scalar_t) (select_mask);            \
+      const scalar_t invalid_mask_local = (scalar_t) (~select_mask_local);    \
+                                                                              \
+      inline type local_insert (type x, scalar_t y, int pos)                  \
+      {                                                                       \
+	x[pos] = y;                                                           \
+	return x;                                                             \
+      }                                                                       \
+      for (u32 iter = 0; iter < NUM_TEST_ITERS; ++iter)                       \
+	{                                                                     \
+	  type data = { 0 };                                                  \
+	  type index = { 0 };                                                 \
+                                                                              \
+	  for (u32 lane = 0; lane < (lanes); ++lane)                          \
+	    {                                                                 \
+	      data[lane] = (scalar_t) random_u32 (&seed);                     \
+                                                                              \
+	      scalar_t base =                                                 \
+		(scalar_t) (random_u32 (&seed) & select_mask_local);          \
+	      if (random_u32 (&seed) & 1)                                     \
+		index[lane] = base;                                           \
+	      else                                                            \
+		index[lane] = (scalar_t) ~0;                                  \
+	    }                                                                 \
+                                                                              \
+	  type actual = func (data, index);                                   \
+	  type expected = actual;                                             \
+                                                                              \
+	  for (u32 lane = 0; lane < (lanes); ++lane)                          \
+	    {                                                                 \
+	      scalar_t idx_val = index[lane];                                 \
+	      expected = local_insert (expected,                              \
+				       (idx_val & invalid_mask_local) ?       \
+					 (scalar_t) 0 :                       \
+					 data[idx_val & select_mask_local],   \
+				       lane);                                 \
+	    }                                                                 \
+                                                                              \
+	  if (memcmp (&expected, &actual, sizeof (expected)) != 0)            \
+	    {                                                                 \
+	      for (u32 lane = 0; lane < (lanes); ++lane)                      \
+		{                                                             \
+		  if (expected[lane] != actual[lane])                         \
+		    return clib_error_return (                                \
+		      err,                                                    \
+		      "%s mismatch iter %u lane %u index=0x%llx "             \
+		      "expected=0x%llx actual=0x%llx",                        \
+		      #func, iter, lane, (unsigned long long) index[lane],    \
+		      (unsigned long long) expected[lane],                    \
+		      (unsigned long long) actual[lane]);                     \
+		}                                                             \
+	    }                                                                 \
+	}                                                                     \
+    }                                                                         \
+  while (0)
+
+static clib_error_t *
+test_clib_dynamic_shuffle (clib_error_t *err)
+{
+  u32 seed = 0x9e3779b9;
+
+  TEST_SHUFFLE_MASK (u8x8_shuffle_dynamic, u8x8, u8, 8, 0x7);
+  TEST_SHUFFLE_MASK (u16x4_shuffle_dynamic, u16x4, u16, 4, 0x3);
+  TEST_SHUFFLE_MASK (u8x16_shuffle_dynamic, u8x16, u8, 16, 0x0f);
+  TEST_SHUFFLE_MASK (u16x8_shuffle_dynamic, u16x8, u16, 8, 0x7);
+  TEST_SHUFFLE_MASK (u32x4_shuffle_dynamic, u32x4, u32, 4, 0x3);
+  TEST_SHUFFLE_MASK (u8x32_shuffle_dynamic, u8x32, u8, 32, 0x1f);
+  TEST_SHUFFLE_MASK (u16x16_shuffle_dynamic, u16x16, u16, 16, 0x0f);
+  TEST_SHUFFLE_MASK (u32x8_shuffle_dynamic, u32x8, u32, 8, 0x7);
+
+  return err;
+}
+
+#undef TEST_SHUFFLE_MASK
+
+#define foreach_dynamic_shuffle_perf_case(_)                                  \
+  _ (u8x8, u8, 8, 0x7, 0x12345678)                                            \
+  _ (u16x4, u16, 4, 0x3, 0x2468ace0)                                          \
+  _ (u8x16, u8, 16, 0x0f, 0xf00dcafe)                                         \
+  _ (u16x8, u16, 8, 0x7, 0xdeadbeef)                                          \
+  _ (u32x4, u32, 4, 0x3, 0x13579bdf)                                          \
+  _ (u32x8, u32, 8, 0x7, 0x0badf00d)                                          \
+  _ (u8x32, u8, 32, 0x1f, 0xc001c0de)                                         \
+  _ (u16x16, u16, 16, 0x0f, 0x42424242)
+
+#define _(vec_t, scalar_t, lanes, select_mask, seed_init)                     \
+  void __test_perf_fn perftest_shuffle_##vec_t (test_perf_t *tp)              \
+  {                                                                           \
+    vec_t *data = test_mem_alloc (sizeof (*data) * PERF_BATCH_SIZE);          \
+    vec_t *indices = test_mem_alloc (sizeof (*indices) * PERF_BATCH_SIZE);    \
+    u32 seed = seed_init;                                                     \
+    const scalar_t select_mask_local = (scalar_t) (select_mask);              \
+    const scalar_t invalid_value = (scalar_t) ~0;                             \
+                                                                              \
+    for (u32 i = 0; i < PERF_BATCH_SIZE; ++i)                                 \
+      {                                                                       \
+	for (u32 lane = 0; lane < (lanes); ++lane)                            \
+	  {                                                                   \
+	    data[i][lane] = (scalar_t) random_u32 (&seed);                    \
+	    scalar_t base =                                                   \
+	      (scalar_t) (random_u32 (&seed) & select_mask_local);            \
+	    if (random_u32 (&seed) & 1)                                       \
+	      indices[i][lane] = base;                                        \
+	    else                                                              \
+	      indices[i][lane] = invalid_value;                               \
+	  }                                                                   \
+      }                                                                       \
+                                                                              \
+    test_perf_event_enable (tp);                                              \
+    for (u32 i = 0; i < tp->n_ops; ++i)                                       \
+      {                                                                       \
+	u32 pos = i & (PERF_BATCH_SIZE - 1);                                  \
+	vec_t res = vec_t##_shuffle_dynamic (data[pos], indices[pos]);        \
+	data[pos] = res;                                                      \
+      }                                                                       \
+    test_perf_event_disable (tp);                                             \
+                                                                              \
+    tp->arg0 = 0;                                                             \
+  }
+foreach_dynamic_shuffle_perf_case (_)
+#undef _
+
+#define _(vec_t, scalar_t, lanes, select_mask, seed_init)                     \
+  { .name = #vec_t " shuffle (per op)",                                       \
+    .n_ops = 32768,                                                           \
+    .fn = perftest_shuffle_##vec_t },
+
+  REGISTER_TEST (clib_dynamic_shuffle) = {
+    .name = "clib_dynamic_shuffle",
+    .fn = test_clib_dynamic_shuffle,
+    .perf_tests = (test_perf_t[]){ foreach_dynamic_shuffle_perf_case (_){} },
+  };
+
+#undef _
+#undef foreach_dynamic_shuffle_perf_case
--- src/vppinfra/vector_sse42.h
+++ src/vppinfra/vector_sse42.h
@@ -578,6 +578,83 @@ u64x2_clmul64 (u64x2 a, const int a_hi, u64x2 b, const int b_hi)
   return p;
 }
 #endif
+#define CLIB_VEC128_INSERT_DEFINED
+
+#define u8x16_insert(v, x, i) (u8x16) _mm_insert_epi8 ((__m128i) v, x, i)
+#define u16x8_insert(v, x, i) (u16x8) _mm_insert_epi16 ((__m128i) v, x, i)
+#define u32x4_insert(v, x, i) (u32x4) _mm_insert_epi32 ((__m128i) v, x, i)
+#define u64x2_insert(v, x, i) (u64x2) _mm_insert_epi64 ((__m128i) v, x, i)
+
+#define CLIB_VEC64_DYNAMIC_SHUFFLE_DEFINED
+static_always_inline u8x8
+u8x8_shuffle_dynamic (u8x8 v, u8x8 indices)
+{
+  return (u8x8) _mm_shuffle_pi8 ((__m64) v, (__m64) indices);
+}
+
+static_always_inline u16x4
+u16x4_shuffle_dynamic (u16x4 v, u16x4 indices)
+{
+  u16x8 res;
+  /* indices_16 = {i0, i1, i2, i3}
+     indices_8  = {2*i0, 2*i0 + 1,
+		   2*i1, 2*i1 + 1,
+		   2*i2, 2*i2 + 1,
+		   2*i3, 2*i3 + 1}
+  */
+  u16x8 v2 = (u16x8) _mm_set1_epi64 ((__m64) v);
+  u16x8 indices2 = (u16x8) _mm_set1_epi64 ((__m64) indices);
+  u16x8 dbled = indices2 << 1;
+  u16x8 plus_one = dbled + 1;
+  u8x16 dbled_u8 = (u8x16) _mm_packs_epi16 ((__m128i) dbled, (__m128i) dbled);
+  u8x16 plus_one_u8 =
+    (u8x16) _mm_packs_epi16 ((__m128i) plus_one, (__m128i) plus_one);
+  u8x16 indices_8 =
+    (u8x16) _mm_unpacklo_epi8 ((__m128i) dbled_u8, (__m128i) plus_one_u8);
+  res = (u16x8) _mm_shuffle_epi8 ((__m128i) v2, (__m128i) indices_8);
+  return (u16x4) _mm_movepi64_pi64 ((__m128i) res);
+}
+
+#define CLIB_VEC128_DYNAMIC_SHUFFLE_DEFINED
+#define u8x16_shuffle_dynamic(v, i)                                           \
+  (u8x16) _mm_shuffle_epi8 ((__m128i) v, (__m128i) i)
+
+static_always_inline u16x8
+u16x8_shuffle_dynamic (u16x8 v, u16x8 indices)
+{
+  u16x8 res;
+  /* indices_16 = {i0, i1, i2, i3, i4, i5, i6, i7}
+     indices_8  = {2*i0, 2*i0 + 1,
+		   2*i1, 2*i1 + 1,
+		   2*i2, 2*i2 + 1,
+		   2*i3, 2*i3 + 1,
+		   2*i4, 2*i4 + 1,
+		   2*i5, 2*i5 + 1,
+		   2*i6, 2*i6 + 1,
+		   2*i7, 2*i7 + 1}
+  */
+  u16x8 dbled = indices << 1;
+  u16x8 plus_one = dbled + 1;
+  u8x16 dbled_u8 = (u8x16) _mm_packs_epi16 ((__m128i) dbled, (__m128i) dbled);
+  u8x16 plus_one_u8 =
+    (u8x16) _mm_packs_epi16 ((__m128i) plus_one, (__m128i) plus_one);
+  u8x16 indices_8 =
+    (u8x16) _mm_unpacklo_epi8 ((__m128i) dbled_u8, (__m128i) plus_one_u8);
+  res = (u16x8) _mm_shuffle_epi8 ((__m128i) v, (__m128i) indices_8);
+  return res;
+}
+
+static_always_inline u32x4
+u32x4_shuffle_dynamic (u32x4 v, u32x4 indices)
+{
+  u32x4 fourtupled = indices << 2;
+  const u8x16 mask1 = { 0, 0, 0, 0, 4, 4, 4, 4, 8, 8, 8, 8, 12, 12, 12, 12 };
+  const u8x16 offset = { 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 };
+  u8x16 new_mask =
+    (u8x16) _mm_shuffle_epi8 ((__m128i) fourtupled, (__m128i) mask1);
+  new_mask += offset;
+  return (u32x4) _mm_shuffle_epi8 ((__m128i) v, (__m128i) new_mask);
+}
 
 #endif /* included_vector_sse2_h */
 
