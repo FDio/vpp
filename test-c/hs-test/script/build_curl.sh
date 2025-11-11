f027adfc7a0c66313d47308206f392de29f38a39
Matus Fabian <matfabia@cisco.com>
2025-11-11 12:41:09 -0500

    hs-test: bump curl version to 8.17.0
    Type: test
    
    Change-Id: I5edd4f3de642c7ee15f43c1f0e7eee2821921b66
    Signed-off-by: Matus Fabian <matfabia@cisco.com>

--- test-c/hs-test/script/build_curl.sh
+++ test-c/hs-test/script/build_curl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
 OS_ARCH="$(uname -m)"
-wget -t 2 https://github.com/stunnel/static-curl/releases/download/8.15.0/curl-linux-"${OS_ARCH}"-glibc-8.15.0.tar.xz
-tar -xvf ./curl-linux-"${OS_ARCH}"-glibc-8.15.0.tar.xz
+wget -t 2 https://github.com/stunnel/static-curl/releases/download/8.17.0/curl-linux-"${OS_ARCH}"-glibc-8.17.0.tar.xz
+tar -xvf ./curl-linux-"${OS_ARCH}"-glibc-8.17.0.tar.xz
 cp curl /usr/bin/curl
