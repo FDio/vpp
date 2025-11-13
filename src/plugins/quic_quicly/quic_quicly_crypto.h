60d0f08440dfc5409499ced1b501a5ffc0693aa8
Ivan Ivanets <iivanets@cisco.com>
2025-11-13 21:02:37 +0200

    crypto: replace  key indices with direct pointers
    Type: feature
    
    Remove the linked key index mechanism from the crypto engine to simplify
    key management. Update the crypto backend API to pass key structures and
    key data directly instead of using key indices. Modify crypto encrypt/decrypt
    operations to accept direct pointers to key data rather than
    vnet_crypto_key_index references.
    
    Change-Id: I9116d2fe8d3c3a99cf158608272ccbace6fb1166
    Signed-off-by: Ivan Ivanets <iivanets@cisco.com>

--- src/plugins/quic_quicly/quic_quicly_crypto.h
+++ src/plugins/quic_quicly/quic_quicly_crypto.h
@@ -2,123 +2,96 @@
  * Copyright(c) 2025 Cisco Systems, Inc.
  */
 
-#ifndef __included_quic_quicly_crypto_h__
-#define __included_quic_quicly_crypto_h__
+#ifndef __included_quic_quicly_h__
+#define __included_quic_quicly_h__
 
 #include <quic/quic.h>
-#include <quicly.h>
-#include <vnet/crypto/crypto.h>
-#include <picotls/openssl.h>
-#include <vppinfra/bihash_24_8.h>
-#include <quic_quicly/quic_quicly.h>
+#include <quic_quicly/ptls_certs.h>
 #include <vnet/session/session.h>
+#include <quicly.h>
+#include <quicly/constants.h>
+#include <quicly/defaults.h>
+#include <picotls.h>
+#include <picotls/openssl.h>
 
-static_always_inline crypto_context_t *
-quic_quicly_crypto_context_get (u32 cr_index, u32 thread_index)
+/* Taken from quicly.c */
+#define QUICLY_QUIC_BIT 0x40
+
+#define QUICLY_PACKET_TYPE_INITIAL                                            \
+  (QUICLY_LONG_HEADER_BIT | QUICLY_QUIC_BIT | 0)
+#define QUICLY_PACKET_TYPE_0RTT                                               \
+  (QUICLY_LONG_HEADER_BIT | QUICLY_QUIC_BIT | 0x10)
+#define QUICLY_PACKET_TYPE_HANDSHAKE                                          \
+  (QUICLY_LONG_HEADER_BIT | QUICLY_QUIC_BIT | 0x20)
+#define QUICLY_PACKET_TYPE_RETRY                                              \
+  (QUICLY_LONG_HEADER_BIT | QUICLY_QUIC_BIT | 0x30)
+#define QUICLY_PACKET_TYPE_BITMASK 0xf0
+
+typedef struct quic_quicly_rx_packet_ctx_
 {
-  ASSERT (QUIC_CRCTX_CTX_INDEX_DECODE_THREAD (cr_index) == thread_index);
+#define _(type, name) type name;
+  foreach_quic_rx_pkt_ctx_field
+#undef _
+    quicly_decoded_packet_t packet;
+  u8 data[QUIC_MAX_PACKET_SIZE];
+  union
+  {
+    struct sockaddr sa;
+    struct sockaddr_in6 sa6;
+  };
+  socklen_t salen;
+  session_dgram_hdr_t ph;
+} quic_quicly_rx_packet_ctx_t;
+
+/* single-entry session cache */
+typedef struct quic_quicly_session_cache_
+{
+  ptls_encrypt_ticket_t super;
+  uint8_t id[32];
+  ptls_iovec_t data;
+} quic_quicly_session_cache_t;
+
+typedef struct quic_quicly_main_
+{
+  quic_main_t *qm;
+  ptls_cipher_suite_t ***quic_ciphers;
+  vnet_crypto_op_keys_t *per_thread_crypto_keys;
+  clib_bihash_16_8_t connection_hash; /**< quic connection id -> conn handle */
+  /* to handle packets that do not use the server generated CID, src CID ->
+   * conn handle, NOTE: we use only connected UDP for now */
+  clib_bihash_24_8_t conn_accepting_hash;
+  quic_quicly_session_cache_t session_cache;
+  quicly_cid_plaintext_t *next_cid;
+  clib_bihash_24_8_t *crypto_ctx_hash;
+  uword *available_crypto_engines; /**< Bitmap for registered engines */
+  u8 vnet_crypto_enabled;
+} quic_quicly_main_t;
+
+extern quic_quicly_main_t quic_quicly_main;
+extern const quicly_stream_callbacks_t quic_quicly_stream_callbacks;
+extern quic_ctx_t *quic_quicly_get_conn_ctx (void *conn);
+extern void quic_quicly_check_quic_session_connected (quic_ctx_t *ctx);
+
+static_always_inline quic_ctx_t *
+quic_quicly_get_quic_ctx (u32 ctx_index, u32 thread_index)
+{
   return pool_elt_at_index (
-    quic_wrk_ctx_get (quic_quicly_main.qm, thread_index)->crypto_ctx_pool,
-    QUIC_CRCTX_CTX_INDEX_DECODE_INDEX (cr_index));
+    quic_wrk_ctx_get (quic_quicly_main.qm, thread_index)->ctx_pool, ctx_index);
 }
 
-#define QUIC_IV_LEN 17
-
-#if OPENSSL_VERSION_NUMBER >= 0x30000000L
-#include <openssl/provider.h>
-
-#define quic_quicly_load_openssl3_legacy_provider()                           \
-  do                                                                          \
-    {                                                                         \
-      (void) OSSL_PROVIDER_load (NULL, "legacy");                             \
-    }                                                                         \
-  while (0)
-#else
-#define quic_quicly_load_openssl3_legacy_provider()
-#endif
-
-typedef struct crypto_key_
+static_always_inline quic_session_connected_t
+quic_quicly_is_session_connected (quic_ctx_t *ctx)
 {
-  vnet_crypto_alg_t algo;
-  u8 key[32];
-  u16 key_len;
-} crypto_key_t;
+  quic_session_connected_t session_connected = QUIC_SESSION_CONNECTED_NONE;
 
-struct aead_crypto_context_t
-{
-  ptls_aead_context_t super;
-  EVP_CIPHER_CTX *evp_ctx;
-  uint8_t static_iv[PTLS_MAX_IV_SIZE];
-  vnet_crypto_op_t op;
-  crypto_key_t key;
-
-  vnet_crypto_op_id_t id;
-  uint8_t iv[PTLS_MAX_IV_SIZE];
-};
-
-struct cipher_context_t
-{
-  ptls_cipher_context_t super;
-  vnet_crypto_op_t op;
-  vnet_crypto_op_id_t id;
-  crypto_key_t key;
-};
-
-typedef struct quic_quicly_on_client_hello_
-{
-  ptls_on_client_hello_t super;
-  u32 lctx_index;
-} quic_quicly_on_client_hello_t;
-
-typedef struct quic_quicly_crypto_context_data_
-{
-  quicly_context_t quicly_ctx;
-  char cid_key[QUIC_IV_LEN];
-  ptls_context_t ptls_ctx;
-  quic_quicly_on_client_hello_t client_hello_ctx;
-} quic_quicly_crypto_context_data_t;
-
-static_always_inline u8
-quic_quicly_register_cipher_suite (crypto_engine_type_t type,
-				   ptls_cipher_suite_t **ciphers)
-{
-  quic_quicly_main_t *qqm = &quic_quicly_main;
-  u8 rv = 1;
-
-  if (!qqm->quic_ciphers)
+  if (quicly_connection_is_ready (ctx->conn))
     {
-      vec_validate (qqm->quic_ciphers, type);
+      session_connected = quicly_is_client (ctx->conn) ?
+			    QUIC_SESSION_CONNECTED_CLIENT :
+			    QUIC_SESSION_CONNECTED_SERVER;
     }
-  if (!qqm->quic_ciphers[type])
-    {
-      QUIC_DBG (3,
-		"Register cipher suite: engine_type %U (%u), cipher_suites %p",
-		format_crypto_engine, type, type, ciphers);
-      clib_bitmap_set (qqm->available_crypto_engines, type, 1);
-      qqm->quic_ciphers[type] = ciphers;
-    }
-  else
-    {
-      QUIC_DBG (3,
-		"Cipher suite already registered: engine_type %U (%u), "
-		"cipher_suites %p",
-		format_crypto_engine, type, type, ciphers);
-      rv = 0;
-    }
-  return rv;
+
+  return (session_connected);
 }
 
-extern quicly_crypto_engine_t quic_quicly_crypto_engine;
-extern ptls_cipher_suite_t *quic_quicly_crypto_cipher_suites[];
-int quic_quicly_crypto_context_init (quic_ctx_t *ctx);
-void quic_quicly_crypto_context_free (u32 crypto_context_index,
-				      u8 thread_index);
-extern int quic_quicly_app_cert_key_pair_delete (app_cert_key_pair_t *ckpair);
-extern int quic_quicly_encrypt_ticket_cb (ptls_encrypt_ticket_t *_self,
-					  ptls_t *tls, int is_encrypt,
-					  ptls_buffer_t *dst,
-					  ptls_iovec_t src);
-extern void
-quic_quicly_crypto_decrypt_packet (quic_ctx_t *qctx,
-				   quic_quicly_rx_packet_ctx_t *pctx);
-#endif /* __included_quic_quicly_crypto_h__ */
+#endif /* __included_quic_quicly_h__ */
--- src/plugins/wireguard/wireguard_chachapoly.c
+++ src/plugins/wireguard/wireguard_chachapoly.c
@@ -20,8 +20,7 @@
 bool
 wg_chacha20poly1305_calc (vlib_main_t *vm, u8 *src, u32 src_len, u8 *dst,
 			  u8 *aad, u32 aad_len, u64 nonce,
-			  vnet_crypto_op_id_t op_id,
-			  vnet_crypto_key_index_t key_index)
+			  vnet_crypto_op_id_t op_id, uword keys)
 {
   vnet_crypto_op_t _op, *op = &_op;
   u8 iv[12];
@@ -47,7 +46,7 @@ wg_chacha20poly1305_calc (vlib_main_t *vm, u8 *src, u32 src_len, u8 *dst,
   op->len = src_len;
 
   op->dst = dst;
-  op->key_index = key_index;
+  op->keys = keys;
   op->aad = aad;
   op->aad_len = aad_len;
   op->iv = iv;
@@ -78,16 +77,17 @@ wg_xchacha20poly1305_encrypt (vlib_main_t *vm, u8 *src, u32 src_len, u8 *dst,
   for (i = 0; i < (sizeof (derived_key) / sizeof (derived_key[0])); i++)
     (derived_key[i]) = clib_host_to_little_u32 ((derived_key[i]));
 
-  uint32_t key_idx;
+  vnet_crypto_op_keys_t keys = { .integ = NULL };
 
-  key_idx =
-    vnet_crypto_key_add (vm, VNET_CRYPTO_ALG_CHACHA20_POLY1305,
-			 (uint8_t *) derived_key, CHACHA20POLY1305_KEY_SIZE);
+  keys.crypto = vnet_crypto_key_add_obj (vm, VNET_CRYPTO_ALG_CHACHA20_POLY1305,
+					 (uint8_t *) derived_key,
+					 CHACHA20POLY1305_KEY_SIZE);
 
   wg_chacha20poly1305_calc (vm, src, src_len, dst, aad, aad_len, h_nonce,
-			    VNET_CRYPTO_OP_CHACHA20_POLY1305_ENC, key_idx);
+			    VNET_CRYPTO_OP_CHACHA20_POLY1305_ENC,
+			    (uword) &keys);
 
-  vnet_crypto_key_del (vm, key_idx);
+  vnet_crypto_key_del_obj (vm, keys.crypto);
   wg_secure_zero_memory (derived_key, CHACHA20POLY1305_KEY_SIZE);
 }
 
@@ -108,17 +108,17 @@ wg_xchacha20poly1305_decrypt (vlib_main_t *vm, u8 *src, u32 src_len, u8 *dst,
   for (i = 0; i < (sizeof (derived_key) / sizeof (derived_key[0])); i++)
     (derived_key[i]) = clib_host_to_little_u32 ((derived_key[i]));
 
-  uint32_t key_idx;
+  vnet_crypto_op_keys_t keys = { .integ = NULL };
 
-  key_idx =
-    vnet_crypto_key_add (vm, VNET_CRYPTO_ALG_CHACHA20_POLY1305,
-			 (uint8_t *) derived_key, CHACHA20POLY1305_KEY_SIZE);
+  keys.crypto = vnet_crypto_key_add_obj (vm, VNET_CRYPTO_ALG_CHACHA20_POLY1305,
+					 (uint8_t *) derived_key,
+					 CHACHA20POLY1305_KEY_SIZE);
 
-  ret =
-    wg_chacha20poly1305_calc (vm, src, src_len, dst, aad, aad_len, h_nonce,
-			      VNET_CRYPTO_OP_CHACHA20_POLY1305_DEC, key_idx);
+  ret = wg_chacha20poly1305_calc (vm, src, src_len, dst, aad, aad_len, h_nonce,
+				  VNET_CRYPTO_OP_CHACHA20_POLY1305_DEC,
+				  (uword) &keys);
 
-  vnet_crypto_key_del (vm, key_idx);
+  vnet_crypto_key_del_obj (vm, keys.crypto);
   wg_secure_zero_memory (derived_key, CHACHA20POLY1305_KEY_SIZE);
 
   return ret;
