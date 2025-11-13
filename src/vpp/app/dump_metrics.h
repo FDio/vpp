cd5e3a49db240e95c7ef8c7552173e28239fb868
Jimmy Björklund <jimmy.bjorklund@hotmail.se>
2025-11-13 15:01:26 +0000

    vpp: Added vpp_prometheus_push and fixed vpp_prometheus_export to pickup stats after vpp restart
    Type: improvement
    
    This commit contains a fix to vpp_prometheus_export so that stats dont become stale after vpp has bin restarted and
    fixed unitialized size for getpeername.
    Added a new service vpp_prometheuse_push that can push stats to prometheus push gateway using http or https with or without
    basic auth.
    
    Change-Id: I6654ab07780b01ea160d2afa5d1e6f60bad82519
    Signed-off-by: Jimmy Björklund <jimmy.bjorklund@hotmail.se>

---
+++ src/vpp/app/dump_metrics.h
@@ -0,0 +1,8 @@
+#ifndef DUMP_METRICS_H
+#define DUMP_METRICS_H
+#include <vpp-api/client/stat_client.h>
+#include <vlib/vlib.h>
+#include <stdio.h>
+void dump_metrics (FILE *stream, u8 **patterns, u8 v2,
+		   stat_client_main_t *shm);
+#endif /* DUMP_METRICS_H */
