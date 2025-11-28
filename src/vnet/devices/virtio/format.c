f5dea76e7a1555da85b938ea9f087609be5941d2
Damjan Marion <damarion@cisco.com>
2025-11-28 23:23:36 +0000

    tap: rework and move to plugins
     - decouple it from virtio-pci, common code more hurts than helps
     - fix memory ordering issues on both rx and tx nodes
     - removed excessive unnecesary inlining
     - improve performance by using buffer templates
         - no feature-arc processing per packet
     - SIMD next index calc for TUN
     - start offset for TUN is changed from 0 to +14
       - vnet hdr now fits into same cacheline
       - same for eventual l2 rewrite on the packet
     - moved to plugin
     - dlsym based header file so other plugins can invoke tap APIs
     - implemented freelist for tx descriptor alloc instead of out-of-order hack
    
    Type: improvement
    Change-Id: I0d801838be77ac043cd62d62e88383e92d6bc8ad
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- src/vnet/devices/virtio/format.c
+++ src/vnet/devices/virtio/format.c
@@ -21,16 +21,8 @@ format_virtio_device_name (u8 * s, va_list * args)
   if (vif->initial_if_name)
     return format (s, "%s", vif->initial_if_name);
 
-  if (vif->type == VIRTIO_IF_TYPE_TAP)
-    s = format (s, "tap%u", vif->id);
-  else if (vif->type == VIRTIO_IF_TYPE_PCI)
-    s = format (s, "virtio-%x/%x/%x/%x", vif->pci_addr.domain,
-		vif->pci_addr.bus, vif->pci_addr.slot,
-		vif->pci_addr.function);
-  else if (vif->type == VIRTIO_IF_TYPE_TUN)
-    s = format (s, "tun%u", vif->id);
-  else
-    s = format (s, "virtio-%lu", vif->dev_instance);
+  s = format (s, "virtio-%x/%x/%x/%x", vif->pci_addr.domain, vif->pci_addr.bus,
+	      vif->pci_addr.slot, vif->pci_addr.function);
 
   return s;
 }
@@ -40,14 +32,7 @@ format_virtio_log_name (u8 * s, va_list * args)
 {
   virtio_if_t *vif = va_arg (*args, virtio_if_t *);
 
-  if (vif->type == VIRTIO_IF_TYPE_TAP)
-    s = format (s, "tap%u", vif->id);
-  else if (vif->type == VIRTIO_IF_TYPE_TUN)
-    s = format (s, "tun%u", vif->id);
-  else if (vif->type == VIRTIO_IF_TYPE_PCI)
-    s = format (s, "%U", format_vlib_pci_addr, &vif->pci_addr);
-  else
-    s = format (s, "virtio-%lu", vif->dev_instance);
+  s = format (s, "%U", format_vlib_pci_addr, &vif->pci_addr);
 
   return s;
 }
