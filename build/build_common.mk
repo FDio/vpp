FDio/vpp

commit c2bf6b6a4512ba5c5890c98c09f196d17ce79691

build: fix pkgconfig path for vpp-ext-deps package
Type: improvement
Change-Id: I77e986eb1591dd73181be2bffbc6b75f3582ea18
Signed-off-by: Mohammad Mahdi Nemati Haravani <nemati.mahdi255@gmail.com>

.gitignore
@@ -19,29 +19,37 @@
 /build-root/test-coverage*/
 /build-root/test/
 /build-config.mk
+/build/.ccache
+/build/.cmake
+/build/build.ninja
+/build/cmake*
+/build/CMakeCache.txt
+/build/CMakeFiles
 /build/external/*.tar.gz
 /build/external/*.tar.xz
-/build/external/vpp-*
-/build/external/dpdk_mlx_default.sh
-/build/external/downloads/
 /build/external/deb/_build/
+/build/external/deb/debian/.debhelper/
 /build/external/deb/debian/changelog
 /build/external/deb/debian/debhelper-build-stamp
-/build/external/deb/debian/tmp/
+/build/external/deb/debian/files
 /build/external/deb/debian/files/
-/build/external/deb/debian/.debhelper/
+/build/external/deb/debian/tmp/
 /build/external/deb/debian/vpp-ext-deps*
-/build/optional/downloads/
+/build/external/downloads/
+/build/external/dpdk_mlx_default.sh
+/build/external/vpp-*
 /build/optional/*.tar.gz
 /build/optional/*.tar.xz
-/build/optional/vpp-*
 /build/optional/deb/_build/
+/build/optional/deb/debian/.debhelper/
 /build/optional/deb/debian/changelog
 /build/optional/deb/debian/debhelper-build-stamp
-/build/optional/deb/debian/tmp/
 /build/optional/deb/debian/files/
-/build/optional/deb/debian/.debhelper/
+/build/optional/deb/debian/tmp/
 /build/optional/deb/debian/vpp-ext-deps*
+/build/optional/downloads/
+/build/optional/vpp-*
+/build/startup.conf
 /path_setup
 /tools/
 # start autotools ignore

build/build_common.mk
@@ -21,6 +21,7 @@ DL_CACHE_DIR = $(HOME)/Downloads
 MAKE_ARGS ?= -j
 BUILD_DIR        ?= $(CURDIR)/_build
 INSTALL_DIR      ?= $(CURDIR)/_install
+ROOT_DIR 		 ?= $(DESTDIR)
 DOWNLOAD_DIR     ?= $(CURDIR)/downloads
 PKG_VERSION ?= $(shell git describe --abbrev=0 --match 'v[0-9]*' | cut -d- -f1 | cut -dv -f2 | cut -d. -f1,2)
 SOURCE_DATE_EPOCH ?= $(shell git log -1 --pretty=%ct .)
@@ -33,6 +34,7 @@ endif	# FreeBSD
 
 B := $(BUILD_DIR)
 I := $(INSTALL_DIR)
+R := $(ROOT_DIR)
 D := $(DOWNLOAD_DIR)
 ifeq ($(WORKSPACE),)
 L := $(B)

build/external/deb/debian/rules
@@ -10,7 +10,8 @@ VERSION = $(shell dpkg-parsechangelog | sed -nr '/^Version:/s/Version: //p')
 BASE_VER = $(word 1, $(subst -, ,$(VERSION)))
 
 export BUILD_DIR=$(CURDIR)/_build
-export INSTALL_DIR=$(CURDIR)/debian/tmp/opt/vpp/external/$(shell uname -m)/
+export INSTALL_DIR=/opt/vpp/external/$(shell uname -m)/
+export DESTDIR=$(CURDIR)/debian/tmp
 
 MAKE_ARGS=-C ..
 
@@ -26,8 +27,12 @@ override_dh_auto_configure:
 
 override_dh_install:
 	make $(MAKE_ARGS) install
-	@mkdir -p $(INSTALL_DIR)
+	@mkdir -p $(DESTDIR)/$(INSTALL_DIR)
+	@mkdir -p $(DESTDIR)/etc/ld.so.conf.d/
+	@echo "$(INSTALL_DIR)/lib" > $(DESTDIR)/etc/ld.so.conf.d/vpp.conf
+	@echo "$(INSTALL_DIR)/lib64" >> $(DESTDIR)/etc/ld.so.conf.d/vpp.conf
+	dh_install -p$(PKG) --autodest /etc
 	dh_install -p$(PKG) --autodest /opt
 
 override_dh_shlibdeps:
-	dh_shlibdeps -l$(INSTALL_DIR)lib/:$(INSTALL_DIR)lib64
+	dh_shlibdeps -l$(DESTDIR)/$(INSTALL_DIR)lib/:$(DESTDIR)/$(INSTALL_DIR)lib64/

build/external/packages/dpdk.mk
@@ -180,6 +180,8 @@ DPDK_MESON_ARGS = \
 	--libdir lib \
 	--prefix $(dpdk_install_dir) \
 	-Dtests=false \
+	-Dc_args="-I$(dpdk_root_dir)/$(dpdk_install_dir)/include" \
+	-Dc_link_args="-L$(dpdk_root_dir)/$(dpdk_install_dir)/lib" \
 	-Denable_driver_sdk=true \
 	"-Ddisable_drivers=$(DPDK_DRIVERS_DISABLED)" \
 	"-Ddisable_libs=$(DPDK_LIBS_DISABLED)" \
@@ -201,10 +203,7 @@ define dpdk_config_cmds
 	source ../dpdk-meson-venv/bin/activate && \
 	(if ! ls $(PIP_DOWNLOAD_DIR)meson* ; then pip3 download -d $(PIP_DOWNLOAD_DIR) -f $(DL_CACHE_DIR) meson==0.57.2 setuptools wheel pyelftools; fi) && \
 	pip3 install --no-index --find-links=$(PIP_DOWNLOAD_DIR) meson==0.57.2 pyelftools && \
-	PKG_CONFIG_PATH=$(dpdk_install_dir)/lib/pkgconfig meson setup $(dpdk_src_dir) \
-		$(dpdk_build_dir) \
-		$(DPDK_MESON_ARGS) \
-			| tee $(dpdk_config_log) && \
+	meson setup $(dpdk_src_dir) $(dpdk_build_dir) $(DPDK_MESON_ARGS) | tee $(dpdk_config_log) && \
 	deactivate && \
 	echo "DPDK post meson configuration" && \
 	echo "Altering rte_build_config.h" && \
@@ -227,7 +226,7 @@ define dpdk_install_cmds
 	cd $(dpdk_build_dir) && \
 	source ../dpdk-meson-venv/bin/activate && \
 	meson install && \
-	cd $(dpdk_install_dir)/lib && \
+	cd $(dpdk_root_dir)/$(dpdk_install_dir)/lib && \
 	echo "GROUP ( $$(ls librte*.a ) )" > libdpdk.a && \
 	rm -rf librte*.so librte*.so.* dpdk/*/librte*.so dpdk/*/librte*.so.* && \
 	deactivate

build/external/packages/ipsec-mb.mk
@@ -32,7 +32,7 @@ define  ipsec-mb_build_cmds
 	  SAFE_PARAM=n \
 	  SAFE_LOOKUP=n \
 	  SAFE_DATA=n \
-	  PREFIX=$(ipsec-mb_install_dir) \
+	  PREFIX=$(ipsec-mb_root_dir)/$(ipsec-mb_install_dir) \
 	  EXTRA_CFLAGS="-g -msse4.2" > $(ipsec-mb_build_log)
 endef
 
@@ -52,10 +52,14 @@ define  ipsec-mb_install_cmds
 		exit 1; \
 		fi \
 	fi
-	@mkdir -p $(ipsec-mb_install_dir)/include
-	@mkdir -p $(ipsec-mb_install_dir)/lib
-	@cp $(ipsec-mb_src_dir)/lib/intel-ipsec-mb.h $(ipsec-mb_install_dir)/include
-	@cp $(ipsec-mb_src_dir)/lib/libIPSec_MB.a $(ipsec-mb_install_dir)/lib
+	@$(MAKE) -C $(ipsec-mb_src_dir)/lib install \
+		V=1 \
+		NOLDCONFIG=y \
+		SHARED=n \
+		SAFE_PARAM=n \
+		SAFE_LOOKUP=n \
+		SAFE_DATA=n \
+		PREFIX=$(ipsec-mb_root_dir)/$(ipsec-mb_install_dir) >> $(ipsec-mb_install_log)
 endef
 
 $(eval $(call package,ipsec-mb))

build/external/packages/rdma-core.mk
@@ -39,7 +39,9 @@ define  rdma-core_config_cmds
 	cd $(rdma-core_build_dir) && \
 	  $(CMAKE) -G Ninja $(rdma-core_src_dir) \
 	    -DENABLE_STATIC=1 -DENABLE_RESOLVE_NEIGH=0 -DNO_PYVERBS=1 -DENABLE_VALGRIND=0\
-	    -DCMAKE_BUILD_TYPE=$(RDMA_BUILD_TYPE) -DCMAKE_INSTALL_PREFIX=$(rdma-core_install_dir) \
+	    -DCMAKE_BUILD_TYPE=$(RDMA_BUILD_TYPE) \
+		-DCMAKE_INSTALL_PREFIX=$(rdma-core_install_dir) \
+		-DCMAKE_INSTALL_SYSCONFDIR=/etc/vpp \
 	    -DCMAKE_INSTALL_LIBDIR=lib \
 	    -DCMAKE_INSTALL_RUNDIR:PATH=/run \
 	    -DCMAKE_C_FLAGS='-fPIC' -DNO_MAN_PAGES=ON | tee $(rdma-core_config_log)

build/external/packages/xdp-tools.mk
@@ -25,15 +25,15 @@ define  xdp-tools_config_cmds
 endef
 
 define  xdp-tools_build_cmds
-	@cd ${xdp-tools_src_dir} && $(MAKE) CC=gcc V=1 BUILD_STATIC_ONLY=y > $(xdp-tools_build_log)
+	@cd ${xdp-tools_src_dir} && $(MAKE) CC=gcc V=1 BUILD_STATIC_ONLY=y PREFIX='$(xdp-tools_install_dir)' > $(xdp-tools_build_log)
 endef
 
 define  xdp-tools_install_cmds
 	@rm -f $(xdp-tools_install_log)
 	@cd ${xdp-tools_src_dir} && \
-		$(MAKE) -C lib/libbpf/src install V=1 BUILD_STATIC_ONLY=y PREFIX='' DESTDIR='$(xdp-tools_install_dir)' >> $(xdp-tools_install_log)
+		$(MAKE) -C lib/libbpf/src install V=1 BUILD_STATIC_ONLY=y PREFIX='$(xdp-tools_install_dir)' >> $(xdp-tools_install_log)
 	@cd ${xdp-tools_src_dir} && \
-		$(MAKE) libxdp_install V=1 BUILD_STATIC_ONLY=y PREFIX='' DESTDIR='$(xdp-tools_install_dir)' >> $(xdp-tools_install_log)
+		$(MAKE) -C lib/libxdp install V=1 BUILD_STATIC_ONLY=y PREFIX='$(xdp-tools_install_dir)' >> $(xdp-tools_install_log)
 endef
 
 $(eval $(call package,xdp-tools))

build/packages_common.mk
@@ -21,6 +21,7 @@ $1_src_dir ?= $(B)/src-$1
 $1_patch_dir ?= $(CURDIR)/patches/$1_$($1_version)
 $1_build_dir ?= $(B)/build-$1
 $1_install_dir ?= $(I)
+$1_root_dir ?= $(R)
 $1_config_log ?= $(L)/$1.config.log
 $1_build_log ?= $(L)/$1.build.log
 $1_install_log ?= $(L)/$1.install.log

commit 99d9df4ac6254408d3eeea8424f33bedbbb6d085

build: revert "fix pkgconfig path for vpp-ext-deps package"
Reason for revert: breaks make install-ext-deps, see https://lists.fd.io/g/vpp-dev/topic/116663717#msg26615

Type: fix
Fixes: c2bf6b6a4512ba5c5890c98c09f196d17ce79691.

Change-Id: Iadaa08f57de2859e3e77a1105540ce1dfc004f64
Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

.gitignore
@@ -19,37 +19,29 @@
 /build-root/test-coverage*/
 /build-root/test/
 /build-config.mk
-/build/.ccache
-/build/.cmake
-/build/build.ninja
-/build/cmake*
-/build/CMakeCache.txt
-/build/CMakeFiles
 /build/external/*.tar.gz
 /build/external/*.tar.xz
+/build/external/vpp-*
+/build/external/dpdk_mlx_default.sh
+/build/external/downloads/
 /build/external/deb/_build/
-/build/external/deb/debian/.debhelper/
 /build/external/deb/debian/changelog
 /build/external/deb/debian/debhelper-build-stamp
-/build/external/deb/debian/files
-/build/external/deb/debian/files/
 /build/external/deb/debian/tmp/
+/build/external/deb/debian/files/
+/build/external/deb/debian/.debhelper/
 /build/external/deb/debian/vpp-ext-deps*
-/build/external/downloads/
-/build/external/dpdk_mlx_default.sh
-/build/external/vpp-*
+/build/optional/downloads/
 /build/optional/*.tar.gz
 /build/optional/*.tar.xz
+/build/optional/vpp-*
 /build/optional/deb/_build/
-/build/optional/deb/debian/.debhelper/
 /build/optional/deb/debian/changelog
 /build/optional/deb/debian/debhelper-build-stamp
-/build/optional/deb/debian/files/
 /build/optional/deb/debian/tmp/
+/build/optional/deb/debian/files/
+/build/optional/deb/debian/.debhelper/
 /build/optional/deb/debian/vpp-ext-deps*
-/build/optional/downloads/
-/build/optional/vpp-*
-/build/startup.conf
 /path_setup
 /tools/
 # start autotools ignore

build/build_common.mk
@@ -21,7 +21,6 @@ DL_CACHE_DIR = $(HOME)/Downloads
 MAKE_ARGS ?= -j
 BUILD_DIR        ?= $(CURDIR)/_build
 INSTALL_DIR      ?= $(CURDIR)/_install
-ROOT_DIR 		 ?= $(DESTDIR)
 DOWNLOAD_DIR     ?= $(CURDIR)/downloads
 PKG_VERSION ?= $(shell git describe --abbrev=0 --match 'v[0-9]*' | cut -d- -f1 | cut -dv -f2 | cut -d. -f1,2)
 SOURCE_DATE_EPOCH ?= $(shell git log -1 --pretty=%ct .)
@@ -34,7 +33,6 @@ endif	# FreeBSD
 
 B := $(BUILD_DIR)
 I := $(INSTALL_DIR)
-R := $(ROOT_DIR)
 D := $(DOWNLOAD_DIR)
 ifeq ($(WORKSPACE),)
 L := $(B)

build/external/deb/debian/rules
@@ -10,8 +10,7 @@ VERSION = $(shell dpkg-parsechangelog | sed -nr '/^Version:/s/Version: //p')
 BASE_VER = $(word 1, $(subst -, ,$(VERSION)))
 
 export BUILD_DIR=$(CURDIR)/_build
-export INSTALL_DIR=/opt/vpp/external/$(shell uname -m)/
-export DESTDIR=$(CURDIR)/debian/tmp
+export INSTALL_DIR=$(CURDIR)/debian/tmp/opt/vpp/external/$(shell uname -m)/
 
 MAKE_ARGS=-C ..
 
@@ -27,12 +26,8 @@ override_dh_auto_configure:
 
 override_dh_install:
 	make $(MAKE_ARGS) install
-	@mkdir -p $(DESTDIR)/$(INSTALL_DIR)
-	@mkdir -p $(DESTDIR)/etc/ld.so.conf.d/
-	@echo "$(INSTALL_DIR)/lib" > $(DESTDIR)/etc/ld.so.conf.d/vpp.conf
-	@echo "$(INSTALL_DIR)/lib64" >> $(DESTDIR)/etc/ld.so.conf.d/vpp.conf
-	dh_install -p$(PKG) --autodest /etc
+	@mkdir -p $(INSTALL_DIR)
 	dh_install -p$(PKG) --autodest /opt
 
 override_dh_shlibdeps:
-	dh_shlibdeps -l$(DESTDIR)/$(INSTALL_DIR)lib/:$(DESTDIR)/$(INSTALL_DIR)lib64/
+	dh_shlibdeps -l$(INSTALL_DIR)lib/:$(INSTALL_DIR)lib64

build/external/packages/dpdk.mk
@@ -180,8 +180,6 @@ DPDK_MESON_ARGS = \
 	--libdir lib \
 	--prefix $(dpdk_install_dir) \
 	-Dtests=false \
-	-Dc_args="-I$(dpdk_root_dir)/$(dpdk_install_dir)/include" \
-	-Dc_link_args="-L$(dpdk_root_dir)/$(dpdk_install_dir)/lib" \
 	-Denable_driver_sdk=true \
 	"-Ddisable_drivers=$(DPDK_DRIVERS_DISABLED)" \
 	"-Ddisable_libs=$(DPDK_LIBS_DISABLED)" \
@@ -203,7 +201,10 @@ define dpdk_config_cmds
 	source ../dpdk-meson-venv/bin/activate && \
 	(if ! ls $(PIP_DOWNLOAD_DIR)meson* ; then pip3 download -d $(PIP_DOWNLOAD_DIR) -f $(DL_CACHE_DIR) meson==0.57.2 setuptools wheel pyelftools; fi) && \
 	pip3 install --no-index --find-links=$(PIP_DOWNLOAD_DIR) meson==0.57.2 pyelftools && \
-	meson setup $(dpdk_src_dir) $(dpdk_build_dir) $(DPDK_MESON_ARGS) | tee $(dpdk_config_log) && \
+	PKG_CONFIG_PATH=$(dpdk_install_dir)/lib/pkgconfig meson setup $(dpdk_src_dir) \
+		$(dpdk_build_dir) \
+		$(DPDK_MESON_ARGS) \
+			| tee $(dpdk_config_log) && \
 	deactivate && \
 	echo "DPDK post meson configuration" && \
 	echo "Altering rte_build_config.h" && \
@@ -226,7 +227,7 @@ define dpdk_install_cmds
 	cd $(dpdk_build_dir) && \
 	source ../dpdk-meson-venv/bin/activate && \
 	meson install && \
-	cd $(dpdk_root_dir)/$(dpdk_install_dir)/lib && \
+	cd $(dpdk_install_dir)/lib && \
 	echo "GROUP ( $$(ls librte*.a ) )" > libdpdk.a && \
 	rm -rf librte*.so librte*.so.* dpdk/*/librte*.so dpdk/*/librte*.so.* && \
 	deactivate

build/external/packages/ipsec-mb.mk
@@ -32,7 +32,7 @@ define  ipsec-mb_build_cmds
 	  SAFE_PARAM=n \
 	  SAFE_LOOKUP=n \
 	  SAFE_DATA=n \
-	  PREFIX=$(ipsec-mb_root_dir)/$(ipsec-mb_install_dir) \
+	  PREFIX=$(ipsec-mb_install_dir) \
 	  EXTRA_CFLAGS="-g -msse4.2" > $(ipsec-mb_build_log)
 endef
 
@@ -52,14 +52,10 @@ define  ipsec-mb_install_cmds
 		exit 1; \
 		fi \
 	fi
-	@$(MAKE) -C $(ipsec-mb_src_dir)/lib install \
-		V=1 \
-		NOLDCONFIG=y \
-		SHARED=n \
-		SAFE_PARAM=n \
-		SAFE_LOOKUP=n \
-		SAFE_DATA=n \
-		PREFIX=$(ipsec-mb_root_dir)/$(ipsec-mb_install_dir) >> $(ipsec-mb_install_log)
+	@mkdir -p $(ipsec-mb_install_dir)/include
+	@mkdir -p $(ipsec-mb_install_dir)/lib
+	@cp $(ipsec-mb_src_dir)/lib/intel-ipsec-mb.h $(ipsec-mb_install_dir)/include
+	@cp $(ipsec-mb_src_dir)/lib/libIPSec_MB.a $(ipsec-mb_install_dir)/lib
 endef
 
 $(eval $(call package,ipsec-mb))

build/external/packages/rdma-core.mk
@@ -39,9 +39,7 @@ define  rdma-core_config_cmds
 	cd $(rdma-core_build_dir) && \
 	  $(CMAKE) -G Ninja $(rdma-core_src_dir) \
 	    -DENABLE_STATIC=1 -DENABLE_RESOLVE_NEIGH=0 -DNO_PYVERBS=1 -DENABLE_VALGRIND=0\
-	    -DCMAKE_BUILD_TYPE=$(RDMA_BUILD_TYPE) \
-		-DCMAKE_INSTALL_PREFIX=$(rdma-core_install_dir) \
-		-DCMAKE_INSTALL_SYSCONFDIR=/etc/vpp \
+	    -DCMAKE_BUILD_TYPE=$(RDMA_BUILD_TYPE) -DCMAKE_INSTALL_PREFIX=$(rdma-core_install_dir) \
 	    -DCMAKE_INSTALL_LIBDIR=lib \
 	    -DCMAKE_INSTALL_RUNDIR:PATH=/run \
 	    -DCMAKE_C_FLAGS='-fPIC' -DNO_MAN_PAGES=ON | tee $(rdma-core_config_log)

build/external/packages/xdp-tools.mk
@@ -25,15 +25,15 @@ define  xdp-tools_config_cmds
 endef
 
 define  xdp-tools_build_cmds
-	@cd ${xdp-tools_src_dir} && $(MAKE) CC=gcc V=1 BUILD_STATIC_ONLY=y PREFIX='$(xdp-tools_install_dir)' > $(xdp-tools_build_log)
+	@cd ${xdp-tools_src_dir} && $(MAKE) CC=gcc V=1 BUILD_STATIC_ONLY=y > $(xdp-tools_build_log)
 endef
 
 define  xdp-tools_install_cmds
 	@rm -f $(xdp-tools_install_log)
 	@cd ${xdp-tools_src_dir} && \
-		$(MAKE) -C lib/libbpf/src install V=1 BUILD_STATIC_ONLY=y PREFIX='$(xdp-tools_install_dir)' >> $(xdp-tools_install_log)
+		$(MAKE) -C lib/libbpf/src install V=1 BUILD_STATIC_ONLY=y PREFIX='' DESTDIR='$(xdp-tools_install_dir)' >> $(xdp-tools_install_log)
 	@cd ${xdp-tools_src_dir} && \
-		$(MAKE) -C lib/libxdp install V=1 BUILD_STATIC_ONLY=y PREFIX='$(xdp-tools_install_dir)' >> $(xdp-tools_install_log)
+		$(MAKE) libxdp_install V=1 BUILD_STATIC_ONLY=y PREFIX='' DESTDIR='$(xdp-tools_install_dir)' >> $(xdp-tools_install_log)
 endef
 
 $(eval $(call package,xdp-tools))

build/packages_common.mk
@@ -21,7 +21,6 @@ $1_src_dir ?= $(B)/src-$1
 $1_patch_dir ?= $(CURDIR)/patches/$1_$($1_version)
 $1_build_dir ?= $(B)/build-$1
 $1_install_dir ?= $(I)
-$1_root_dir ?= $(R)
 $1_config_log ?= $(L)/$1.config.log
 $1_build_log ?= $(L)/$1.build.log
 $1_install_log ?= $(L)/$1.install.log