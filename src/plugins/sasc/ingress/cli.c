42c6886f38f5afe72de388ca77a7d3f77a8d110f
Hadi Rayan Al-Sandid <halsandi@cisco.com>
2025-12-10 17:07:06 +0100

    sasc: CLI commands to enable/disable feature on intf
    * 'set sasc interface..' and 'delete sasc interface..'
    CLI commands have been introduced enable/disable
    sasc on either input or output feature arc of specific
    interfaces
    
    Type: improvement
    
    Change-Id: I8968efbd2970c7a66030cd8cb0249043abed05ce
    Signed-off-by: Hadi Rayan Al-Sandid <halsandi@cisco.com>

--- src/plugins/sasc/ingress/cli.c
+++ src/plugins/sasc/ingress/cli.c
@@ -32,13 +32,18 @@ sasc_interface_input_enable_command_fn(vlib_main_t *vm, unformat_input_t *input,
             goto done;
         }
     }
-    if (sw_if_index == ~0 || tenant_id == ~0) {
-        err = clib_error_return(0, "missing arguments");
+    if (sw_if_index == ~0) {
+        err = clib_error_return(0, "no valid interface provided");
         goto done;
     }
-    int rv = sasc_interface_input_enable_disable(sw_if_index, tenant_id, output_arc, true);
+
+    if (tenant_id == ~0) {
+        err = clib_error_return(0, "cannot enable without valid tenant_id");
+        goto done;
+    }
+    int rv = sasc_interface_input_enable_disable(sw_if_index, tenant_id, output_arc, 1 /* is_enable */);
     if (rv != 0) {
-        err = clib_error_return(0, "could not enable interface");
+        err = clib_error_return(0, "error when enabling sasc on interface");
     }
 
 done:
@@ -50,6 +55,50 @@ VLIB_CLI_COMMAND(sasc_interface_input_enable_command, static) = {
     .path = "set sasc ingress interface",
     .short_help = "set sasc ingress interface <interface> tenant <tenant-id> [output]",
     .function = sasc_interface_input_enable_command_fn,
+};
+
+/*
+ *  disable sasc ingress interface <interface>
+ */
+static clib_error_t *
+sasc_interface_input_disable_command_fn(vlib_main_t *vm, unformat_input_t *input, vlib_cli_command_t *cmd) {
+    unformat_input_t line_input_, *line_input = &line_input_;
+    clib_error_t *err = 0;
+    u32 sw_if_index = ~0, tenant_id = ~0;
+    bool output_arc = false;
+
+    if (!unformat_user(input, unformat_line_input, line_input))
+        return 0;
+
+    while (unformat_check_input(line_input) != UNFORMAT_END_OF_INPUT) {
+        if (unformat(line_input, "%U", unformat_vnet_sw_interface, vnet_get_main(), &sw_if_index))
+            ;
+        else if (unformat(line_input, "output"))
+            output_arc = true;
+        else {
+            err = unformat_parse_error(line_input);
+            goto done;
+        }
+    }
+    if (sw_if_index == ~0) {
+        err = clib_error_return(0, "no valid interface provided");
+        goto done;
+    }
+
+    int rv = sasc_interface_input_enable_disable(sw_if_index, tenant_id, output_arc, 0 /*is_enable=*/);
+    if (rv != 0) {
+        err = clib_error_return(0, "error when disabling sasc on interface");
+    }
+
+done:
+    unformat_free(line_input);
+    return err;
+}
+
+VLIB_CLI_COMMAND(sasc_interface_input_disable_command, static) = {
+    .path = "delete sasc ingress interface",
+    .short_help = "delete sasc ingress interface <interface> [output]",
+    .function = sasc_interface_input_disable_command_fn,
 };
 
 /*
