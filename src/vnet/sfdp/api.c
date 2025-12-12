FDio/vpp

commit a74f0ef0a54e821da1737743af64960354b74bf7

sfdp: StateFul Data Plane
Change-Id: I229548afbae609f8a15047821e811874aa788a53
Type: feature
Signed-off-by: Mohammed Hawari <mohammed@hawari.fr>

src/vnet/sfdp/api.c
@@ -1,0 +1,266 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2025 Cisco Systems, Inc.
+ */
+
+#include <vnet/sfdp/sfdp.h>
+
+#include <vlibapi/api.h>
+#include <vlibmemory/api.h>
+#include <vnet/ip/ip_types_api.h>
+#include <vnet/format_fns.h>
+#include <vnet/sfdp/sfdp.api_enum.h>
+#include <vnet/sfdp/sfdp.api_types.h>
+#include <vnet/sfdp/sfdp_types_funcs.h>
+
+#define REPLY_MSG_ID_BASE sfdp->msg_id_base
+#include <vlibapi/api_helper_macros.h>
+
+static void
+vl_api_sfdp_tenant_add_del_t_handler (vl_api_sfdp_tenant_add_del_t *mp)
+{
+  sfdp_main_t *sfdp = &sfdp_main;
+  u32 tenant_id = clib_net_to_host_u32 (mp->tenant_id);
+  u32 context_id =
+    mp->context_id == ~0 ? tenant_id : clib_net_to_host_u32 (mp->context_id);
+  u8 is_del = mp->is_del;
+  clib_error_t *err =
+    sfdp_tenant_add_del (sfdp, tenant_id, context_id, is_del);
+  vl_api_sfdp_tenant_add_del_reply_t *rmp;
+  int rv = err ? -1 : 0;
+  REPLY_MACRO (VL_API_SFDP_TENANT_ADD_DEL_REPLY);
+}
+
+static void
+vl_api_sfdp_set_services_t_handler (vl_api_sfdp_set_services_t *mp)
+{
+  sfdp_main_t *sfdp = &sfdp_main;
+  u32 tenant_id = clib_net_to_host_u32 (mp->tenant_id);
+  sfdp_bitmap_t bitmap = 0;
+  u8 idx = 0;
+  u8 dir = sfdp_api_direction (mp->dir);
+  int rv;
+  for (uword i = 0; i < mp->n_services; i++)
+    {
+      char *cstring = (char *) mp->services[i].data;
+      unformat_input_t tmp;
+      unformat_init_string (&tmp, cstring,
+			    strnlen (cstring, sizeof (mp->services[0].data)));
+      rv = unformat_user (&tmp, unformat_sfdp_service, &idx);
+      unformat_free (&tmp);
+      if (!rv)
+	{
+	  rv = -1;
+	  goto fail;
+	}
+      bitmap |= (1ULL << idx);
+    }
+  clib_error_t *err = sfdp_set_services (sfdp, tenant_id, bitmap, dir);
+  vl_api_sfdp_set_services_reply_t *rmp;
+  rv = err ? -1 : 0;
+fail:
+  REPLY_MACRO (VL_API_SFDP_SET_SERVICES_REPLY);
+}
+
+static void
+vl_api_sfdp_set_timeout_t_handler (vl_api_sfdp_set_timeout_t *mp)
+{
+  sfdp_main_t *sfdp = &sfdp_main;
+  u32 tenant_id = clib_net_to_host_u32 (mp->tenant_id);
+  u32 timeout_id = clib_net_to_host_u32 (mp->timeout_id);
+  u32 timeout_value = clib_net_to_host_u32 (mp->timeout_value);
+  clib_error_t *err =
+    sfdp_set_timeout (sfdp, tenant_id, timeout_id, timeout_value);
+  vl_api_sfdp_set_timeout_reply_t *rmp;
+  int rv = err ? -1 : 0;
+  REPLY_MACRO (VL_API_SFDP_SET_TIMEOUT_REPLY);
+}
+
+static void
+vl_api_sfdp_set_sp_node_t_handler (vl_api_sfdp_set_sp_node_t *mp)
+{
+  vl_api_sfdp_set_sp_node_reply_t *rmp;
+  sfdp_main_t *sfdp = &sfdp_main;
+  u32 tenant_id = clib_net_to_host_u32 (mp->tenant_id);
+  u8 sp_node = sfdp_api_sp_node (mp->sp_node);
+  u32 node_index = clib_net_to_host_u32 (mp->node_index);
+
+  clib_error_t *err = sfdp_set_sp_node (sfdp, tenant_id, sp_node, node_index);
+  int rv = err ? -1 : 0;
+  REPLY_MACRO (VL_API_SFDP_SET_SP_NODE_REPLY);
+}
+
+static void
+vl_api_sfdp_set_icmp_error_node_t_handler (
+  vl_api_sfdp_set_icmp_error_node_t *mp)
+{
+  vl_api_sfdp_set_icmp_error_node_reply_t *rmp;
+  sfdp_main_t *sfdp = &sfdp_main;
+  u32 tenant_id = clib_net_to_host_u32 (mp->tenant_id);
+  u8 is_ip6 = mp->is_ip6;
+  u32 node_index = clib_net_to_host_u32 (mp->node_index);
+
+  clib_error_t *err =
+    sfdp_set_icmp_error_node (sfdp, tenant_id, is_ip6, node_index);
+  int rv = err ? -1 : 0;
+  REPLY_MACRO (VL_API_SFDP_SET_ICMP_ERROR_NODE_REPLY);
+}
+
+static vl_api_sfdp_session_state_t
+sfdp_session_state_encode (sfdp_session_state_t x)
+{
+  switch (x)
+    {
+    case SFDP_SESSION_STATE_FSOL:
+      return SFDP_API_SESSION_STATE_FSOL;
+    case SFDP_SESSION_STATE_ESTABLISHED:
+      return SFDP_API_SESSION_STATE_ESTABLISHED;
+    case SFDP_SESSION_STATE_TIME_WAIT:
+      return SFDP_API_SESSION_STATE_TIME_WAIT;
+    default:
+      return -1;
+    }
+};
+
+static void
+sfdp_send_session_details (vl_api_registration_t *rp, u32 context,
+			   u32 session_index, u32 thread_index,
+			   sfdp_session_t *session)
+{
+  sfdp_main_t *sfdp = &sfdp_main;
+  vlib_main_t *vm = vlib_get_main ();
+  vl_api_sfdp_session_details_t *mp;
+  sfdp_session_ip46_key_t skey;
+  sfdp_tenant_t *tenant;
+  u32 tenant_id;
+  f64 now = vlib_time_now (vm);
+  size_t msg_size;
+  u8 n_keys = sfdp_session_n_keys (session);
+  tenant = sfdp_tenant_at_index (sfdp, session->tenant_idx);
+  tenant_id = tenant->tenant_id;
+  msg_size = sizeof (*mp) + sizeof (mp->keys[0]) * n_keys;
+
+  mp = vl_msg_api_alloc_zero (msg_size);
+  mp->_vl_msg_id = ntohs (VL_API_SFDP_SESSION_DETAILS + sfdp->msg_id_base);
+
+  /* fill in the message */
+  mp->context = context;
+  mp->session_id = clib_host_to_net_u64 (session->session_id);
+  mp->thread_index = clib_host_to_net_u32 (thread_index);
+  mp->tenant_id = clib_host_to_net_u32 (tenant_id);
+  mp->session_idx = clib_host_to_net_u32 (session_index);
+  mp->session_type = sfdp_session_type_encode (session->type);
+  mp->protocol = ip_proto_encode (session->proto);
+  mp->state = sfdp_session_state_encode (session->state);
+  mp->remaining_time =
+    sfdp->expiry_callbacks.session_remaining_time (session, now);
+  mp->forward_bitmap =
+    clib_host_to_net_u64 (session->bitmaps[SFDP_FLOW_FORWARD]);
+  mp->reverse_bitmap =
+    clib_host_to_net_u64 (session->bitmaps[SFDP_FLOW_REVERSE]);
+  mp->n_keys = n_keys;
+  for (int i = 0; i < n_keys; i++)
+    {
+      if ((i == 0 &&
+	   session->key_flags & SFDP_SESSION_KEY_FLAG_PRIMARY_VALID_IP4) ||
+	  (i == 1 &&
+	   session->key_flags & SFDP_SESSION_KEY_FLAG_SECONDARY_VALID_IP4))
+	{
+	  sfdp_normalise_ip4_key (session, &skey.key4, i);
+	  sfdp_session_ip46_key_encode (&skey, IP46_TYPE_IP4, &mp->keys[i]);
+	}
+      if ((i == 0 &&
+	   session->key_flags & SFDP_SESSION_KEY_FLAG_PRIMARY_VALID_IP6) ||
+	  (i == 1 &&
+	   session->key_flags & SFDP_SESSION_KEY_FLAG_SECONDARY_VALID_IP6))
+	{
+	  sfdp_normalise_ip6_key (session, &skey.key6, i);
+	  sfdp_session_ip46_key_encode (&skey, IP46_TYPE_IP6, &mp->keys[i]);
+	}
+    }
+  vl_api_send_msg (rp, (u8 *) mp);
+}
+
+static void
+vl_api_sfdp_session_dump_t_handler (vl_api_sfdp_session_dump_t *mp)
+{
+  sfdp_main_t *sfdp = &sfdp_main;
+  sfdp_session_t *session;
+  uword session_index;
+  vl_api_registration_t *rp;
+  rp = vl_api_client_index_to_registration (mp->client_index);
+  if (rp == 0)
+    return;
+
+  sfdp_foreach_session (sfdp, session_index, session)
+  {
+    sfdp_send_session_details (rp, mp->context, session_index,
+			       session->owning_thread_index, session);
+  }
+}
+
+static void
+sfdp_send_tenant_details (vl_api_registration_t *rp, u32 context,
+			  u16 tenant_index, sfdp_tenant_t *tenant)
+{
+  sfdp_main_t *sfdp = &sfdp_main;
+  vl_api_sfdp_tenant_details_t *mp;
+  sfdp_timeout_t *timeout;
+
+  size_t msg_size;
+  msg_size = sizeof (*mp) + SFDP_MAX_TIMEOUTS * sizeof (mp->timeout[0]);
+
+  mp = vl_msg_api_alloc_zero (msg_size);
+  mp->_vl_msg_id = ntohs (VL_API_SFDP_TENANT_DETAILS + sfdp->msg_id_base);
+
+  /* fill in the message */
+  mp->context = context;
+  mp->context_id = clib_host_to_net_u32 (tenant->context_id);
+  mp->index = clib_host_to_net_u32 (tenant_index);
+  mp->forward_bitmap =
+    clib_host_to_net_u64 (tenant->bitmaps[SFDP_FLOW_FORWARD]);
+  mp->reverse_bitmap =
+    clib_host_to_net_u64 (tenant->bitmaps[SFDP_FLOW_REVERSE]);
+  mp->n_timeout = clib_host_to_net_u32 (SFDP_MAX_TIMEOUTS);
+  sfdp_foreach_timeout (sfdp, timeout)
+  {
+    u32 idx = timeout - sfdp->timeouts;
+    mp->timeout[idx] = clib_host_to_net_u32 (tenant->timeouts[idx]);
+  }
+
+  vl_api_send_msg (rp, (u8 *) mp);
+}
+
+static void
+vl_api_sfdp_tenant_dump_t_handler (vl_api_sfdp_tenant_dump_t *mp)
+{
+  sfdp_main_t *sfdp = &sfdp_main;
+  sfdp_tenant_t *tenant;
+  u16 tenant_index;
+  vl_api_registration_t *rp;
+  rp = vl_api_client_index_to_registration (mp->client_index);
+  if (rp == 0)
+    return;
+
+  pool_foreach_index (tenant_index, sfdp->tenants)
+    {
+      tenant = sfdp_tenant_at_index (sfdp, tenant_index);
+      sfdp_send_tenant_details (rp, mp->context, tenant_index, tenant);
+    }
+}
+
+#include <vnet/sfdp/sfdp.api.c>
+static clib_error_t *
+sfdp_plugin_api_hookup (vlib_main_t *vm)
+{
+  sfdp_main_t *sfdp = &sfdp_main;
+  sfdp->msg_id_base = setup_message_id_table ();
+  return 0;
+}
+VLIB_API_INIT_FUNCTION (sfdp_plugin_api_hookup);
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

src/vnet/sfdp/cli.c
@@ -1,0 +1,551 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2025 Cisco Systems, Inc.
+ */
+
+#include <vnet/plugin/plugin.h>
+#include <vnet/vnet.h>
+#include <vnet/sfdp/sfdp.h>
+#include <vnet/sfdp/service.h>
+
+/*
+ * add CLI:
+ * sfdp tenant <add/del> <tenant-id>
+ *
+ * it creates entry in the tenant pool. Default service chains in both
+ * directions is "sfdp-drop"
+ *
+ *
+ * add CLI:
+ * set sfdp services tenant <tenant-id> (SERVICE_NAME)+ <forward|reverse>
+ *
+ * configure tenant with a service chain for a given direction (forward or
+ * reverse)
+ *
+ */
+
+static clib_error_t *
+sfdp_tenant_add_del_command_fn (vlib_main_t *vm, unformat_input_t *input,
+				vlib_cli_command_t *cmd)
+{
+  unformat_input_t line_input_, *line_input = &line_input_;
+  clib_error_t *err = 0;
+  sfdp_main_t *sfdp = &sfdp_main;
+  u8 is_del = 0;
+  u32 tenant_id = ~0;
+  u32 context_id = ~0;
+  if (!unformat_user (input, unformat_line_input, line_input))
+    return 0;
+  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (line_input, "add %d", &tenant_id))
+	is_del = 0;
+      else if (unformat (line_input, "del %d", &tenant_id))
+	is_del = 1;
+      else if (unformat (line_input, "context %d", &context_id))
+	;
+      else
+	{
+	  err = unformat_parse_error (line_input);
+	  goto done;
+	}
+    }
+  if (tenant_id == ~0)
+    {
+      err = clib_error_return (0, "missing tenant id");
+      goto done;
+    }
+  if (context_id == ~0)
+    context_id = tenant_id;
+  err = sfdp_tenant_add_del (sfdp, tenant_id, context_id, is_del);
+done:
+  unformat_free (line_input);
+  return err;
+}
+
+static clib_error_t *
+sfdp_set_services_command_fn (vlib_main_t *vm, unformat_input_t *input,
+			      vlib_cli_command_t *cmd)
+{
+  unformat_input_t line_input_, *line_input = &line_input_;
+  clib_error_t *err = 0;
+  sfdp_main_t *sfdp = &sfdp_main;
+  u32 tenant_id = ~0;
+  sfdp_bitmap_t bitmap = 0;
+  u8 direction = ~0;
+
+  if (!unformat_user (input, unformat_line_input, line_input))
+    return 0;
+  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (line_input, "tenant %d", &tenant_id))
+	;
+      else if (unformat_user (line_input, unformat_sfdp_service_bitmap,
+			      &bitmap))
+	;
+      else if (unformat (line_input, "forward"))
+	direction = SFDP_FLOW_FORWARD;
+      else if (unformat (line_input, "reverse"))
+	direction = SFDP_FLOW_REVERSE;
+      else
+	{
+	  err = unformat_parse_error (line_input);
+	  goto done;
+	}
+    }
+  if (tenant_id == ~0)
+    {
+      err = clib_error_return (0, "missing tenant id");
+      goto done;
+    }
+  if (direction == (u8) ~0)
+    {
+      err = clib_error_return (0, "missing direction");
+      goto done;
+    }
+  sfdp_set_services (sfdp, tenant_id, bitmap, direction);
+done:
+  unformat_free (line_input);
+  return err;
+}
+
+static_always_inline u32
+table_format_insert_sfdp_service (table_t *t,
+				  sfdp_service_registration_t *service, u32 n)
+{
+  table_format_cell (t, n, 0, "%s", service->node_name);
+  table_set_cell_align (t, n, 0, TTAA_LEFT);
+  table_format_cell (t, n, 1, "%u", *(service->index_in_bitmap));
+  table_set_cell_align (t, n, 1, TTAA_CENTER);
+  table_format_cell (t, n, 2, "%s", (service->is_terminal) ? "T" : "");
+  table_set_cell_align (t, n, 1, TTAA_CENTER);
+  return n + 1;
+}
+
+static clib_error_t *
+sfdp_show_services_fn (vlib_main_t *vm, unformat_input_t *input,
+		       vlib_cli_command_t *cmd)
+{
+  sfdp_service_main_t *vsm = &sfdp_service_main;
+  sfdp_service_registration_t ***services_for_scope;
+
+  vec_foreach (services_for_scope, vsm->services_per_scope_index)
+    {
+      table_t service_table_ = {}, *service_table = &service_table_;
+      u32 scope_index = services_for_scope - vsm->services_per_scope_index;
+      sfdp_service_registration_t **service;
+      table_format_title (service_table,
+			  "Registered SFDP services for scope '%s'",
+			  vsm->scope_names[scope_index]);
+      table_add_header_col (service_table, 3, "Node name", "Index",
+			    "Terminal");
+
+      u32 n = 0;
+      vec_foreach (service, *services_for_scope)
+	{
+	  n = table_format_insert_sfdp_service (service_table, *service, n);
+	}
+      vlib_cli_output (vm, "%U", format_table, service_table);
+      vlib_cli_output (vm, "%u / 64 registered services", n);
+      table_free (service_table);
+    }
+  return 0;
+}
+
+static clib_error_t *
+sfdp_set_timeout_command_fn (vlib_main_t *vm, unformat_input_t *input,
+			     vlib_cli_command_t *cmd)
+{
+  unformat_input_t line_input_, *line_input = &line_input_;
+  clib_error_t *err = 0;
+  sfdp_main_t *sfdp = &sfdp_main;
+  u32 tenant_id = ~0;
+  u32 timeout_idx = ~0;
+  u32 timeout_val = ~0;
+
+  if (!unformat_user (input, unformat_line_input, line_input))
+    return 0;
+  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (line_input, "tenant %d", &tenant_id))
+	;
+      else if (unformat (line_input, "%U %d", unformat_sfdp_timeout_name,
+			 &timeout_idx, &timeout_val))
+	;
+      else
+	{
+	  err = unformat_parse_error (line_input);
+	  goto done;
+	}
+    }
+  if (tenant_id == ~0)
+    {
+      err = clib_error_return (0, "missing tenant id");
+      goto done;
+    }
+  if (timeout_idx == ~0)
+    {
+      err = clib_error_return (0, "missing timeout");
+      goto done;
+    }
+
+  err = sfdp_set_timeout (sfdp, tenant_id, timeout_idx, timeout_val);
+done:
+  unformat_free (line_input);
+  return err;
+}
+
+static clib_error_t *
+sfdp_set_sp_node_command_fn (vlib_main_t *vm, unformat_input_t *input,
+			     vlib_cli_command_t *cmd)
+{
+  unformat_input_t line_input_, *line_input = &line_input_;
+  clib_error_t *err = 0;
+  sfdp_main_t *sfdp = &sfdp_main;
+  u32 tenant_id = ~0;
+  u32 sp_idx = ~0;
+  u32 node_index = ~0;
+
+  if (!unformat_user (input, unformat_line_input, line_input))
+    return 0;
+  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (line_input, "tenant %d", &tenant_id))
+	;
+      else if (unformat (line_input, "node %U", unformat_vlib_node, vm,
+			 &node_index))
+	;
+      else if (unformat (line_input, "%U", unformat_sfdp_sp_node, &sp_idx))
+	;
+      else
+	{
+	  err = unformat_parse_error (line_input);
+	  goto done;
+	}
+    }
+  if (tenant_id == ~0)
+    {
+      err = clib_error_return (0, "missing tenant id");
+      goto done;
+    }
+  if (node_index == ~0)
+    {
+      err = clib_error_return (0, "missing node");
+      goto done;
+    }
+  if (sp_idx == ~0)
+    {
+      err = clib_error_return (0, "missing slow-path");
+      goto done;
+    }
+
+  err = sfdp_set_sp_node (sfdp, tenant_id, sp_idx, node_index);
+done:
+  unformat_free (line_input);
+  return err;
+}
+
+static clib_error_t *
+sfdp_set_icmp_error_node_command_fn (vlib_main_t *vm, unformat_input_t *input,
+				     vlib_cli_command_t *cmd)
+{
+  unformat_input_t line_input_, *line_input = &line_input_;
+  clib_error_t *err = 0;
+  sfdp_main_t *sfdp = &sfdp_main;
+  u32 tenant_id = ~0;
+  u32 node_index = ~0;
+  u8 ip46 = 0;
+
+  if (!unformat_user (input, unformat_line_input, line_input))
+    return 0;
+  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (line_input, "tenant %d", &tenant_id))
+	;
+      else if (unformat (line_input, "node %U", unformat_vlib_node, vm,
+			 &node_index))
+	;
+      else if (unformat (line_input, "ip4"))
+	ip46 = 1;
+      else if (unformat (line_input, "ip6"))
+	ip46 = 2;
+      else
+	{
+	  err = unformat_parse_error (line_input);
+	  goto done;
+	}
+    }
+  if (tenant_id == ~0)
+    {
+      err = clib_error_return (0, "missing tenant id");
+      goto done;
+    }
+  if (node_index == ~0)
+    {
+      err = clib_error_return (0, "missing node");
+      goto done;
+    }
+  if (ip46 == 0)
+    {
+      err = clib_error_return (0, "missing adress family");
+      goto done;
+    }
+
+  err = sfdp_set_icmp_error_node (sfdp, tenant_id, ip46 - 1, node_index);
+
+done:
+  unformat_free (line_input);
+  return err;
+}
+
+static clib_error_t *
+sfdp_show_sessions_command_fn (vlib_main_t *vm, unformat_input_t *input,
+			       vlib_cli_command_t *cmd)
+{
+  unformat_input_t line_input_, *line_input = &line_input_;
+  clib_error_t *err = 0;
+  sfdp_main_t *sfdp = &sfdp_main;
+  sfdp_session_t *session;
+  u32 session_index;
+  sfdp_tenant_t *tenant;
+  u32 tenant_id = ~0;
+  u32 max_output_value = 20;
+  bool is_show_all = false;
+  f64 now = vlib_time_now (vm);
+
+  if (unformat_user (input, unformat_line_input, line_input))
+    {
+      while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+	{
+	  if (unformat (line_input, "tenant %u", &tenant_id))
+	    ;
+	  else if (unformat (line_input, "max %u", &max_output_value))
+	    ;
+	  else if (unformat (line_input, "unsafe-show-all"))
+	    is_show_all = true;
+	  else
+	    {
+	      err = unformat_parse_error (line_input);
+	      break;
+	    }
+	}
+      unformat_free (line_input);
+    }
+
+  if (!is_show_all && max_output_value == 0)
+    err = clib_error_return (0, "Please specify a positive integer for max");
+
+  if (!err)
+    {
+      table_t session_table_ = {}, *session_table = &session_table_;
+      u32 n = 0;
+      sfdp_table_format_add_header_col (session_table);
+      sfdp_foreach_session (sfdp, session_index, session)
+      {
+	tenant = sfdp_tenant_at_index (sfdp, session->tenant_idx);
+	if (tenant_id != ~0 && tenant_id != tenant->tenant_id)
+	  continue;
+	n = sfdp_table_format_insert_session (session_table, n, session_index,
+					      session, tenant->tenant_id, now);
+
+	if (!is_show_all && n >= max_output_value)
+	  break;
+      }
+      vlib_cli_output (vm, "%U", format_table, session_table);
+      if (n < pool_elts (sfdp->sessions))
+	{
+	  vlib_cli_output (vm, "Only %u sessions displayed, %u ignored", n,
+			   pool_elts (sfdp->sessions) - n);
+	}
+
+      table_free (session_table);
+    }
+
+  return err;
+}
+
+static clib_error_t *
+sfdp_show_session_detail_command_fn (vlib_main_t *vm, unformat_input_t *input,
+				     vlib_cli_command_t *cmd)
+{
+  unformat_input_t line_input_, *line_input = &line_input_;
+  clib_error_t *err = 0;
+  sfdp_main_t *sfdp = &sfdp_main;
+  clib_bihash_kv_8_8_t kv = { 0 };
+  f64 now = vlib_time_now (vm);
+  u32 session_index;
+  u64 session_id;
+  if (unformat_user (input, unformat_line_input, line_input))
+    {
+      if (unformat_check_input (line_input) == UNFORMAT_END_OF_INPUT ||
+	  unformat (line_input, "0x%X", sizeof (session_id), &session_id) == 0)
+	err = unformat_parse_error (line_input);
+      unformat_free (line_input);
+    }
+  else
+    err = clib_error_return (0, "No session id provided");
+
+  if (!err)
+    {
+      kv.key = session_id;
+      if (!clib_bihash_search_inline_8_8 (&sfdp->session_index_by_id, &kv))
+	{
+	  session_index = sfdp_session_index_from_lookup (kv.value);
+	  vlib_cli_output (vm, "%U", format_sfdp_session_detail, session_index,
+			   now);
+	}
+      else
+	{
+	  err =
+	    clib_error_return (0, "Session id 0x%llx not found", session_id);
+	}
+    }
+  return err;
+}
+
+static clib_error_t *
+sfdp_show_tenant_detail_command_fn (vlib_main_t *vm, unformat_input_t *input,
+				    vlib_cli_command_t *cmd)
+{
+  unformat_input_t line_input_, *line_input = &line_input_;
+  clib_error_t *err = 0;
+  sfdp_main_t *sfdp = &sfdp_main;
+  sfdp_tenant_t *tenant;
+  u32 tenant_id = ~0;
+  u16 tenant_idx;
+  u8 detail = 0;
+  if (unformat_user (input, unformat_line_input, line_input))
+    {
+      while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+	{
+	  if (unformat (line_input, "%d detail", &tenant_id))
+	    detail = 1;
+	  else if (unformat (line_input, "%d", &tenant_id))
+	    ;
+	  else
+	    {
+	      err = unformat_parse_error (line_input);
+	      break;
+	    }
+	}
+      unformat_free (line_input);
+    }
+  if (err)
+    return err;
+
+  pool_foreach_index (tenant_idx, sfdp->tenants)
+    {
+      tenant = sfdp_tenant_at_index (sfdp, tenant_idx);
+
+      if (tenant_id != ~0 && tenant->tenant_id != tenant_id)
+	continue;
+
+      vlib_cli_output (vm, "Tenant %d", tenant->tenant_id);
+      vlib_cli_output (vm, "  %U", format_sfdp_tenant, sfdp, tenant_idx,
+		       tenant);
+      if (detail)
+	vlib_cli_output (vm, "  %U", format_sfdp_tenant_extra, sfdp,
+			 tenant_idx, tenant);
+    }
+
+  return err;
+}
+
+static clib_error_t *
+sfdp_show_sfdp_status_command_fn (vlib_main_t *vm, unformat_input_t *input,
+				  vlib_cli_command_t *cmd)
+{
+  sfdp_main_t *sfdp = &sfdp_main;
+  u64 max_sessions = sfdp_num_sessions ();
+  u64 free_sessions = sfdp_remaining_sessions_in_pool ();
+  u64 active_sessions = sfdp_real_active_sessions ();
+  f64 active_percent = (((f64) (100)) * active_sessions) / max_sessions;
+
+  vlib_cli_output (vm, "sfdp status:\n");
+  vlib_cli_output (vm, "  max sessions: %lu\n", max_sessions);
+  vlib_cli_output (vm, "  active sessions: %lu (%.2f%%)\n", active_sessions,
+		   active_percent);
+  vlib_cli_output (vm, "  free sessions: %lu\n", free_sessions);
+  vlib_cli_output (vm, "  eviction sessions margin: %u\n",
+		   sfdp->eviction_sessions_margin);
+  vlib_cli_output (vm, "  max sessions cache per thread: %lu\n",
+		   sfdp_num_sessions_cache_per_thread ());
+  vlib_cli_output (vm, "  max tenants: %llu\n", 1ULL << sfdp->log2_tenants);
+
+  // iterate over all threads
+  sfdp_per_thread_data_t *ptd;
+  vec_foreach (ptd, sfdp->per_thread_data)
+    {
+      u32 index = ptd - sfdp->per_thread_data;
+      vlib_cli_output (vm, "    [%u] active sessions: %lu\n", index,
+		       ptd->n_sessions);
+      vlib_cli_output (vm, "    [%u] cached sessions: %lu\n", index,
+		       vec_len (ptd->session_freelist));
+    }
+
+  return NULL;
+}
+
+VLIB_CLI_COMMAND (sfdp_tenant_add_del_command, static) = {
+  .path = "sfdp tenant",
+  .short_help = "sfdp tenant <add|del> <tenant-id> context <context-id>",
+  .function = sfdp_tenant_add_del_command_fn,
+};
+
+VLIB_CLI_COMMAND (sfdp_set_services_command, static) = {
+  .path = "set sfdp services",
+  .short_help = "set sfdp services tenant <tenant-id>"
+		" [SERVICE_NAME]+ <forward|reverse>",
+  .function = sfdp_set_services_command_fn,
+};
+
+VLIB_CLI_COMMAND (sfdp_show_services_command, static) = {
+  .path = "show sfdp services",
+  .short_help = "show sfdp services",
+  .function = sfdp_show_services_fn,
+};
+
+VLIB_CLI_COMMAND (show_sfdp_sessions_command, static) = {
+  .path = "show sfdp session-table",
+  .short_help = "show sfdp session-table [tenant <tenant-id>] "
+		"[max <max_value>] [unsafe-show-all]",
+  .function = sfdp_show_sessions_command_fn,
+};
+
+VLIB_CLI_COMMAND (show_sfdp_detail_command, static) = {
+  .path = "show sfdp session-detail",
+  .short_help = "show sfdp session-detail 0x<session-id>",
+  .function = sfdp_show_session_detail_command_fn,
+};
+
+VLIB_CLI_COMMAND (show_sfdp_tenant, static) = {
+  .path = "show sfdp tenant",
+  .short_help = "show sfdp tenant [<tenant-id> [detail]]",
+  .function = sfdp_show_tenant_detail_command_fn,
+};
+
+VLIB_CLI_COMMAND (sfdp_show_sfdp_status_command, static) = {
+  .path = "show sfdp status",
+  .short_help = "show sfdp status",
+  .function = sfdp_show_sfdp_status_command_fn,
+};
+
+VLIB_CLI_COMMAND (sfdp_set_timeout_command, static) = {
+  .path = "set sfdp timeout",
+  .short_help = "set sfdp timeout tenant <tenant-id>"
+		" <timeout-name> <timeout-value>",
+  .function = sfdp_set_timeout_command_fn
+};
+
+VLIB_CLI_COMMAND (sfdp_set_sp_node_command, static) = {
+  .path = "set sfdp sp-node",
+  .short_help = "set sfdp sp-node tenant <tenant-id>"
+		" <sp-name> node <node-name>",
+  .function = sfdp_set_sp_node_command_fn
+};
+
+VLIB_CLI_COMMAND (sfdp_set_icmp_error_node_command, static) = {
+  .path = "set sfdp icmp-error-node",
+  .short_help = "set sfdp icmp-error-node tenant <tenant-id>"
+		" <ip4|ip6> node <node-name>",
+  .function = sfdp_set_icmp_error_node_command_fn
+};

commit b5529cc800af12b4c40e623b6e597b2edaa7156d

sfdp: use a new type for session direction
I am not sure what was wrong with u8,
but CSIT testing was unable to access the reverse direction using PAPI.

As the direction value is used as an index to array of bitmaps,
ptrdiff_t was chosed to be the base type of the internal enum.

Type: fix
Fixes: a74f0ef0a54e821da1737743af64960354b74bf7

Change-Id: Iede4f8d0595dbdedb71e1d33a82264fc903fed29
Signed-off-by: Vratko Polak <vrpolak@cisco.com>

src/vnet/sfdp/api.c
@@ -37,7 +37,7 @@ vl_api_sfdp_set_services_t_handler (vl_api_sfdp_set_services_t *mp)
   u32 tenant_id = clib_net_to_host_u32 (mp->tenant_id);
   sfdp_bitmap_t bitmap = 0;
   u8 idx = 0;
-  u8 dir = sfdp_api_direction (mp->dir);
+  sfdp_flow_direction_t dir = sfdp_api_direction (mp->dir);
   int rv;
   for (uword i = 0; i < mp->n_services; i++)
     {

src/vnet/sfdp/cli.c
@@ -71,7 +71,7 @@ sfdp_set_services_command_fn (vlib_main_t *vm, unformat_input_t *input,
   sfdp_main_t *sfdp = &sfdp_main;
   u32 tenant_id = ~0;
   sfdp_bitmap_t bitmap = 0;
-  u8 direction = ~0;
+  sfdp_flow_direction_t direction = SFDP_FLOW_DIRECTION_INVALID;
 
   if (!unformat_user (input, unformat_line_input, line_input))
     return 0;
@@ -97,7 +97,7 @@ sfdp_set_services_command_fn (vlib_main_t *vm, unformat_input_t *input,
       err = clib_error_return (0, "missing tenant id");
       goto done;
     }
-  if (direction == (u8) ~0)
+  if (direction == SFDP_FLOW_DIRECTION_INVALID)
     {
       err = clib_error_return (0, "missing direction");
       goto done;

src/vnet/sfdp/sfdp.c
@@ -282,8 +282,11 @@ sfdp_tenant_add_del (sfdp_main_t *sfdp, u32 tenant_id, u32 context_id,
 
 clib_error_t *
 sfdp_set_services (sfdp_main_t *sfdp, u32 tenant_id, sfdp_bitmap_t bitmap,
-		   u8 direction)
+		   sfdp_flow_direction_t direction)
 {
+  if (directon < SFDP_FLOW_FORWARD || direction > SFDP_FLOW_REVERSE)
+    return clib_error_return (0, "Invalid direction: %d", direction);
+
   sfdp_init_main_if_needed (sfdp);
   clib_bihash_kv_8_8_t kv = { .key = tenant_id, .value = 0 };
   sfdp_tenant_t *tenant;

src/vnet/sfdp/sfdp.h
@@ -113,12 +113,13 @@ typedef enum
     SFDP_TENANT_DATA_N_COUNTER
 } sfdp_tenant_data_counter_index_t;
 
-enum
+typedef enum : ptrdiff_t
 {
+  SFDP_FLOW_INVALID = -1,
   SFDP_FLOW_FORWARD = 0,
   SFDP_FLOW_REVERSE = 1,
   SFDP_FLOW_F_B_N = 2
-};
+} sfdp_flow_direction_t;
 
 enum
 {
@@ -126,6 +127,7 @@ enum
   SFDP_SESSION_KEY_SECONDARY,
   SFDP_SESSION_N_KEY
 };
+
 /* Flags to determine key validity in the session */
 #define foreach_sfdp_session_key_flag                                         \
   _ (PRIMARY_VALID_IP4, 0x1, "primary-valid-ip4")                             \
@@ -644,10 +646,10 @@ sfdp_session_from_flow_index (u32 flow_index)
   return flow_index >> 1;
 }
 
-static_always_inline u32
+static_always_inline sfdp_flow_direction_t
 sfdp_direction_from_flow_index (u32 flow_index)
 {
-  return (flow_index & 0x1);
+  return (sfdp_flow_direction_t) (flow_index & 0x1);
 }
 
 static_always_inline sfdp_tenant_t *
@@ -853,7 +855,7 @@ int sfdp_create_session_with_scope_index (vlib_main_t *vm, vlib_buffer_t *b,
 clib_error_t *sfdp_tenant_add_del (sfdp_main_t *sfdp, u32 tenant_id,
 				   u32 context_id, u8 is_del);
 clib_error_t *sfdp_set_services (sfdp_main_t *sfdp, u32 tenant_id,
-				 sfdp_bitmap_t bitmap, u8 direction);
+				 sfdp_bitmap_t bitmap, sfdp_flow_direction_t direction);
 clib_error_t *sfdp_set_timeout (sfdp_main_t *sfdp, u32 tenant_id,
 				u32 timeout_idx, u32 timeout_val);
 

src/vnet/sfdp/sfdp_types_funcs.h
@@ -9,7 +9,8 @@
 #include <vnet/sfdp/sfdp_types.api_types.h>
 #include <vnet/sfdp/sfdp_types.api_enum.h>
 #include <vnet/ip/ip_types_api.h>
-static_always_inline u8
+
+static_always_inline sfdp_flow_direction_t
 sfdp_api_direction (vl_api_sfdp_session_direction_t dir)
 {
   switch (dir)
@@ -17,9 +18,9 @@ sfdp_api_direction (vl_api_sfdp_session_direction_t dir)
     case SFDP_API_FORWARD:
       return SFDP_FLOW_FORWARD;
     case SFDP_API_REVERSE:
-      return SFDP_API_REVERSE;
+      return SFDP_FLOW_REVERSE;
     }
-  return SFDP_FLOW_FORWARD;
+  return SFDP_FLOW_INVALID;
 }
 
 static_always_inline vl_api_sfdp_session_type_t