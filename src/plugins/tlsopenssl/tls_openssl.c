2f1c6105ac12f919d18697fa9ad2b812bf12cb08
Steven Luong <sluong@cisco.com>
2025-12-19 10:46:09 -0800

    session: make transport to use application's segment manager
    Suppress transport from creating its own segment manager. Instead,
    it uses its corresponding application's segment manager.
    
    Type: improvement
    
    Change-Id: I485a9ca202c392d9016bb7dc1bf872e57a601cf4
    Signed-off-by: Steven Luong <sluong@cisco.com>

--- src/plugins/tlsopenssl/tls_openssl.c
+++ src/plugins/tlsopenssl/tls_openssl.c
@@ -303,6 +303,7 @@ openssl_ctx_handshake_rx (tls_ctx_t *ctx, session_t *tls_session)
   openssl_ctx_t *oc = (openssl_ctx_t *) ctx;
   int rv = 0, err;
 
+  ctx->app_wrk_connect_index = session_connect_originated_get_app_wrk_connect_index (tls_session);
   while (SSL_in_init (oc->ssl))
     {
       if (ctx->flags & TLS_CONN_F_RESUME)
