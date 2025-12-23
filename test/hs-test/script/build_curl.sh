23bc2328e62530847cf3280f6d4e57543cbb74f6
Dave Wallace <dwallacelf@gmail.com>
2025-12-22 22:19:27 -0500

    tests: cache curl tarball in hst builds
    Type: test
    
    Change-Id: I83a3d01df758fd6d4e561e78195b8907801f8d56
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- test/hs-test/script/build_curl.sh
+++ test/hs-test/script/build_curl.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -x
+CURL_VERSION="8.17.0"
+CURL_TARBALL=curl-linux-"${OS_ARCH}"-glibc-"$CURL_VERSION".tar.xz
 OS_ARCH="$(uname -m)"
-wget -t 2 https://github.com/stunnel/static-curl/releases/download/8.17.0/curl-linux-"${OS_ARCH}"-glibc-8.17.0.tar.xz
-tar -xvf ./curl-linux-"${OS_ARCH}"-glibc-8.17.0.tar.xz
+DOWNLOADS_DIR=~/Downloads
+mkdir -p "$DOWNLOADS_DIR"
+pushd "$DOWNLOADS_DIR"
+if [ ! -f "$CURL_TARBALL" ] ; then
+  wget -t 2 https://github.com/stunnel/static-curl/releases/download/"$CURL_VERSION"/"$CURL_TARBALL"
+fi
+tar -xvf ./"$CURL_TARBALL"
 cp curl /usr/bin/curl
+popd
