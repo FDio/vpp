FDio/vpp

commit c52364afcc874f123b37371fcf78696a49ea5bda

tap: add support for VHOST_SET_FORK_FROM_OWNER to improve performance on kernel >= 6.4
This patch addresses performance degradation issues with TAP interfaces
on Linux kernels >= 6.4 where the vhost subsystem switched from kthread
to vhost_task workers (commit 6e890c5d5021).

The new VHOST_SET_FORK_FROM_OWNER ioctl (introduced in kernel 6.12+)
allows userspace to explicitly request kthread mode workers, which
provides better performance characteristics.

Changes:
- Add VHOST_SET_FORK_FROM_OWNER and VHOST_GET_FORK_FROM_OWNER ioctl
  definitions to vhost.h
- Call VHOST_SET_FORK_FROM_OWNER with KTHREAD mode BEFORE VHOST_SET_OWNER
  (as required by the kernel API)
- Display the vhost worker mode in "show tap" command output by
  calling VHOST_GET_FORK_FROM_OWNER directly (no storage in tap_if_t)
- Gracefully handle kernels that do not support this ioctl
  (returns ENOTTY or EINVAL)
- Add appropriate debug and warning logging

This change is backward compatible and will:
- Use kthread mode on kernels >= 6.12 with CONFIG_VHOST_ENABLE_FORK_OWNER_CONTROL=y
- Show "vhost-worker-mode: kthread" or "task" in show tap output
- Show "not supported" on older kernels

Type: improvement
Fixes: Performance issues on Linux kernel >= 6.4
Change-Id: I92ebe900cf640622853d3c97a7ed5fe17e37a6bb
Signed-off-by: Jerome Tollet <jtollet@cisco.com>

src/plugins/tap/cli.c
@@ -52,6 +52,28 @@ tap_show (vlib_main_t *vm, u32 *hw_if_indices, u8 show_descr, int is_tun)
 	str = format (str, " %d", tif->vhost_fds[i]);
       vlib_cli_output (vm, "  vhost-fds%v", str);
       vec_free (str);
+      /* Display vhost worker fork mode if available.
+       * We only check the first vhost_fd since all fds for this interface
+       * share the same fork mode (configured at interface creation). */
+      if (vec_len (tif->vhost_fds) > 0)
+	{
+	  int fork_mode = -1;
+	  if (ioctl (tif->vhost_fds[0], VHOST_GET_FORK_FROM_OWNER,
+		     &fork_mode) == 0)
+	    {
+	      if (fork_mode == VHOST_FORK_OWNER_KTHREAD)
+		vlib_cli_output (vm, "  vhost-worker-mode: kthread");
+	      else if (fork_mode == VHOST_FORK_OWNER_TASK)
+		vlib_cli_output (vm, "  vhost-worker-mode: task");
+	      else
+		vlib_cli_output (vm, "  vhost-worker-mode: unknown (%d)",
+				 fork_mode);
+	    }
+	  else
+	    {
+	      vlib_cli_output (vm, "  vhost-worker-mode: not supported");
+	    }
+	}
       vec_foreach_index (i, tif->tap_fds)
 	str = format (str, " %d", tif->tap_fds[i]);
       vlib_cli_output (vm, "  tap-fds%v", str);

src/plugins/tap/tap.c
@@ -14,6 +14,7 @@
 #include <sys/ioctl.h>
 #include <linux/ethtool.h>
 #include <net/if_arp.h>
+#include <errno.h>
 
 #include <vlib/vlib.h>
 #include <vlib/file.h>
@@ -644,6 +645,35 @@ tap_create_if (vlib_main_t *vm, tap_create_if_args_t *args)
 	}
       vec_add1 (tif->vhost_fds, vfd);
       log_dbg (tif, "open vhost-net fd %d qpair %u", vfd, i);
+      /* Try to set vhost worker mode to kthread for better performance.
+       * IMPORTANT: This IOCTL must be called BEFORE VHOST_SET_OWNER.
+       * Only available on kernel >= 6.12 with
+       * CONFIG_VHOST_ENABLE_FORK_OWNER_CONTROL=y */
+      int fork_mode = VHOST_FORK_OWNER_KTHREAD;
+      if (ioctl (vfd, VHOST_SET_FORK_FROM_OWNER, &fork_mode) == 0)
+	{
+	  if (i == 0)
+	    log_dbg (tif,
+		     "VHOST_SET_FORK_FROM_OWNER: fd %u mode %d (kthread mode "
+		     "requested)",
+		     vfd, fork_mode);
+	}
+      else if (errno == ENOTTY || errno == EINVAL)
+	{
+	  /* IOCTL not supported by this kernel, continue with default worker
+	   * mode */
+	  if (i == 0)
+	    log_dbg (tif, "VHOST_SET_FORK_FROM_OWNER not supported, using "
+			  "default worker mode (task)");
+	}
+      else
+	{
+	  /* Unexpected error, log warning but continue */
+	  if (i == 0)
+	    log_warn (
+	      tif, "VHOST_SET_FORK_FROM_OWNER failed (continuing with default "
+		   "mode)");
+	}
       _IOCTL (vfd, VHOST_SET_OWNER, 0);
       log_dbg (tif, "VHOST_SET_OWNER: fd %u", vfd);
     }

src/plugins/tap/vhost.h
@@ -66,3 +66,10 @@ typedef struct
 #define VHOST_SET_VRING_CALL  _IOW (VHOST_VIRTIO, 0x21, vhost_vring_file_t)
 #define VHOST_SET_VRING_ERR   _IOW (VHOST_VIRTIO, 0x22, vhost_vring_file_t)
 #define VHOST_NET_SET_BACKEND _IOW (VHOST_VIRTIO, 0x30, vhost_vring_file_t)
+
+/* vhost worker fork mode control (kernel >= 6.12 with
+ * CONFIG_VHOST_ENABLE_FORK_OWNER_CONTROL=y) */
+#define VHOST_FORK_OWNER_KTHREAD  0
+#define VHOST_FORK_OWNER_TASK	  1
+#define VHOST_SET_FORK_FROM_OWNER _IOW (VHOST_VIRTIO, 0x84, int)
+#define VHOST_GET_FORK_FROM_OWNER _IOR (VHOST_VIRTIO, 0x85, int)

commit 1165be3c5a7026f0a0f120dae0c566cc69972cbf

tap: fix for owner type
Type: fix
Fixes: c52364af
Signed-off-by: Jerome Tollet <jtollet@cisco.com>
Change-Id: I36f446a9bf9271e84a2153643338504955cd81f8

src/plugins/tap/cli.c
@@ -57,7 +57,7 @@ tap_show (vlib_main_t *vm, u32 *hw_if_indices, u8 show_descr, int is_tun)
        * share the same fork mode (configured at interface creation). */
       if (vec_len (tif->vhost_fds) > 0)
 	{
-	  int fork_mode = -1;
+	  u8 fork_mode = 0;
 	  if (ioctl (tif->vhost_fds[0], VHOST_GET_FORK_FROM_OWNER,
 		     &fork_mode) == 0)
 	    {

src/plugins/tap/tap.c
@@ -649,7 +649,7 @@ tap_create_if (vlib_main_t *vm, tap_create_if_args_t *args)
        * IMPORTANT: This IOCTL must be called BEFORE VHOST_SET_OWNER.
        * Only available on kernel >= 6.12 with
        * CONFIG_VHOST_ENABLE_FORK_OWNER_CONTROL=y */
-      int fork_mode = VHOST_FORK_OWNER_KTHREAD;
+      u8 fork_mode = VHOST_FORK_OWNER_KTHREAD;
       if (ioctl (vfd, VHOST_SET_FORK_FROM_OWNER, &fork_mode) == 0)
 	{
 	  if (i == 0)

src/plugins/tap/vhost.h
@@ -71,5 +71,5 @@ typedef struct
  * CONFIG_VHOST_ENABLE_FORK_OWNER_CONTROL=y) */
 #define VHOST_FORK_OWNER_KTHREAD  0
 #define VHOST_FORK_OWNER_TASK	  1
-#define VHOST_SET_FORK_FROM_OWNER _IOW (VHOST_VIRTIO, 0x84, int)
-#define VHOST_GET_FORK_FROM_OWNER _IOR (VHOST_VIRTIO, 0x85, int)
+#define VHOST_SET_FORK_FROM_OWNER _IOW (VHOST_VIRTIO, 0x84, u8)
+#define VHOST_GET_FORK_FROM_OWNER _IOR (VHOST_VIRTIO, 0x85, u8)