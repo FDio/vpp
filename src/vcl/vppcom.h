4f764f842329ff1ec68b4e36b974ecca7d5a0df8
Matus Fabian <matfabia@cisco.com>
2026-01-01 15:14:00 -0500

    vcl: add vppcom_session_terminate
    vcl application can now terminate (reset) session
    
    Type: improvement
    
    Change-Id: I121beefc448ead119aeb2b9d30cc3ef12f2a61e7
    Signed-off-by: Matus Fabian <matfabia@cisco.com>

--- src/vcl/vppcom.h
+++ src/vcl/vppcom.h
@@ -250,6 +250,7 @@ extern void vppcom_app_destroy (void);
 extern int vppcom_session_create (uint8_t proto, uint8_t is_nonblocking);
 extern int vppcom_session_shutdown (uint32_t session_handle, int how);
 extern int vppcom_session_close (uint32_t session_handle);
+extern int vppcom_session_terminate (uint32_t session_handle);
 extern int vppcom_session_bind (uint32_t session_handle, vppcom_endpt_t * ep);
 extern int vppcom_session_listen (uint32_t session_handle, uint32_t q_len);
 
