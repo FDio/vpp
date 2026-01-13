FDio/vpp

commit 6a4514c12e169e0f49719d8d9c3dc8bd1f10ab31

octeon: add switch_header_type argument
This commit adds a new argument, switch_header_type, to the octeon port
config, which can be used to tell the NPC about the type of underlying
switch header (edsa, etc), so that it can properly skip it when
performing RSS computations.

Type: improvement

Change-Id: I80f05ea07f5c466bb13b30c2d353b9d0876ad497
Signed-off-by: Yoann Desmouceaux <ydesmouc@cisco.com>
Signed-off-by: Damjan Marion <damarion@cisco.com>
Signed-off-by: Monendra Singh Kushwaha <kmonendra@marvell.com>

src/plugins/dev_octeon/init.c
@@ -82,24 +82,35 @@ static vnet_dev_arg_t oct_port_args[] = {
   {
     .id = OCT_PORT_ARG_EN_ETH_PAUSE_FRAME,
     .name = "eth_pause_frame",
-    .desc = "Enable ethernet pause frame support, applicable to network "
-	    "devices only",
+    .desc = "Enable ethernet pause frame support",
     .type = VNET_DEV_ARG_TYPE_BOOL,
     .default_val.boolean = false,
   },
   {
     .id = OCT_PORT_ARG_RSS_FLOW_KEY,
     .name = "rss_flow_key",
-    .desc = "RSS Flow Key Bitmap, applicable to network devices only",
-    .type = VNET_DEV_ARG_TYPE_UINT32,
+    .desc = "RSS Flow Key Bitmap",
+    .type = VNET_DEV_ARG_TYPE_HEX32,
     .default_val.uint32 = FLOW_KEY_TYPE_IPV4 | FLOW_KEY_TYPE_IPV6 |
 			  FLOW_KEY_TYPE_TCP | FLOW_KEY_TYPE_UDP |
 			  FLOW_KEY_TYPE_SCTP,
   },
   {
-    .id = OCT_PORT_ARG_END,
-    .name = "end",
-    .desc = "Argument end",
+    .id = OCT_PORT_ARG_SWITCH_HEADER_TYPE,
+    .name = "switch_header_type",
+    .desc = "Type of header used by underlying switch (none, edsa, higig, "
+	    "len_90b, exdsa, vlan_exdsa)",
+    .type = VNET_DEV_ARG_TYPE_ENUM,
+    .enum_vals = VNET_DEV_ARG_ENUM_VALS (
+      { .val = ROC_PRIV_FLAGS_DEFAULT, .name = "none" },
+      { .val = ROC_PRIV_FLAGS_EDSA, .name = "edsa" },
+      { .val = ROC_PRIV_FLAGS_HIGIG, .name = "higig" },
+      { .val = ROC_PRIV_FLAGS_LEN_90B, .name = "len_90b" },
+      { .val = ROC_PRIV_FLAGS_EXDSA, .name = "exdsa" },
+      { .val = ROC_PRIV_FLAGS_VLAN_EXDSA, .name = "vlan_exdsa" }, ),
+    .default_val.enum_val = ROC_PRIV_FLAGS_DEFAULT,
+  },
+  {
     .type = VNET_DEV_ARG_END,
   },
 };
@@ -244,7 +255,7 @@ oct_init_nix (vlib_main_t *vm, vnet_dev_t *dev)
       },
       .data_size = sizeof (oct_port_t),
       .initial_data = &oct_port,
-      .args = oct_port_args,
+      .args = roc_nix_is_vf_or_sdp(cd->nix) == 0 ? oct_port_args : 0,
     },
     .rx_node = &oct_rx_node,
     .tx_node = &oct_tx_node,

src/plugins/dev_octeon/port.c
@@ -134,6 +134,8 @@ oct_port_init (vlib_main_t *vm, vnet_dev_port_t *port)
 	is_pause_frame_enable = true;
       else if (arg->id == OCT_PORT_ARG_RSS_FLOW_KEY)
 	cp->rss_flowkey = vnet_dev_arg_get_uint32 (arg);
+      else if (arg->id == OCT_PORT_ARG_SWITCH_HEADER_TYPE)
+	cp->switch_header_type = vnet_dev_arg_get_enum (arg);
     }
 
   if ((rrv = roc_nix_lf_alloc (nix, ifs->num_rx_queues, ifs->num_tx_queues,
@@ -194,6 +196,12 @@ oct_port_init (vlib_main_t *vm, vnet_dev_port_t *port)
       return oct_roc_err (dev, rrv, "roc_npc_init() failed");
     }
   cp->npc_initialized = 1;
+
+  if ((rrv = roc_nix_switch_hdr_set (nix, cp->switch_header_type, 0, 0, 0)))
+    {
+      oct_port_deinit (vm, port);
+      return oct_roc_err (dev, rrv, "roc_nix_switch_hdr_set() failed");
+    }
 
   foreach_vnet_dev_port_rx_queue (q, port)
     if (q->enabled)

src/plugins/dev_octeon/octeon.h
@@ -34,6 +34,7 @@ typedef enum
 {
   OCT_PORT_ARG_EN_ETH_PAUSE_FRAME = 1,
   OCT_PORT_ARG_RSS_FLOW_KEY = 2,
+  OCT_PORT_ARG_SWITCH_HEADER_TYPE = 3,
   OCT_PORT_ARG_END
 } oct_port_args_t;
 
@@ -85,6 +86,7 @@ typedef struct
   struct roc_npc npc;
   oct_flow_entry_t *flow_entries;
   u32 rss_flowkey;
+  u64 switch_header_type;
 } oct_port_t;
 
 typedef struct

commit 7d67d48a34a04e4516d0efa7f3889dad35b1ba2c

octeon: fix port specific arguments
Type: fix
fixes: 6a4514c1

Change-Id: Iefe6962f2bcab20c445d05e6d406ed42e678a93b
Signed-off-by: Monendra Singh Kushwaha <kmonendra@marvell.com>

src/plugins/dev_octeon/init.c
@@ -110,6 +110,40 @@ static clib_arg_t oct_port_args[] = {
   },
 };
 
+static clib_arg_t oct_port_args_rvu_vf[] = {
+  {
+    .name = "eth_pause_frame",
+    .desc = "Enable ethernet pause frame support",
+    .type = CLIB_ARG_TYPE_BOOL,
+    .default_val.boolean = false,
+  },
+  {
+    .name = "rss_flow_key",
+    .desc = "RSS Flow Key Bitmap",
+    .type = CLIB_ARG_TYPE_HEX32,
+    .default_val.uint32 = FLOW_KEY_TYPE_IPV4 | FLOW_KEY_TYPE_IPV6 |
+			  FLOW_KEY_TYPE_TCP | FLOW_KEY_TYPE_UDP |
+			  FLOW_KEY_TYPE_SCTP,
+  },
+  {
+    .type = CLIB_ARG_END,
+  },
+};
+
+static clib_arg_t oct_port_args_lbk_sdp[] = {
+  {
+    .name = "rss_flow_key",
+    .desc = "RSS Flow Key Bitmap",
+    .type = CLIB_ARG_TYPE_HEX32,
+    .default_val.uint32 = FLOW_KEY_TYPE_IPV4 | FLOW_KEY_TYPE_IPV6 |
+			  FLOW_KEY_TYPE_TCP | FLOW_KEY_TYPE_UDP |
+			  FLOW_KEY_TYPE_SCTP,
+  },
+  {
+    .type = CLIB_ARG_END,
+  },
+};
+
 static clib_arg_t oct_dev_args[] = {
   {
     .name = "n_desc",
@@ -175,6 +209,17 @@ oct_config_args (vlib_main_t *vm, vnet_dev_driver_t *drv)
     }
 
   return 0;
+}
+
+static clib_arg_t *
+oct_dev_get_port_args (oct_device_t *cd)
+{
+  if (roc_nix_is_lbk (cd->nix) || roc_nix_is_sdp (cd->nix))
+    return oct_port_args_lbk_sdp;
+  else if (!roc_nix_is_pf (cd->nix))
+    return oct_port_args_rvu_vf;
+  else
+    return oct_port_args;
 }
 
 static vnet_dev_rv_t
@@ -247,7 +292,7 @@ oct_init_nix (vlib_main_t *vm, vnet_dev_t *dev)
       },
       .data_size = sizeof (oct_port_t),
       .initial_data = &oct_port,
-      .args = roc_nix_is_vf_or_sdp(cd->nix) == 0 ? oct_port_args : 0,
+      .args = oct_dev_get_port_args (cd),
     },
     .rx_node = &oct_rx_node,
     .tx_node = &oct_tx_node,

src/plugins/dev_octeon/port.c
@@ -112,6 +112,22 @@ oct_port_pause_flow_control_init (vlib_main_t *vm, vnet_dev_port_t *port)
   return VNET_DEV_OK;
 }
 
+static int
+oct_port_is_arg_valid (clib_args_handle_t h, char *fmt, ...)
+{
+  va_list va;
+  int idx;
+
+  va_start (va, fmt);
+  idx = clib_args_find_arg_by_name (h, fmt, &va);
+  va_end (va);
+
+  if (idx >= 0)
+    return true;
+
+  return false;
+}
+
 vnet_dev_rv_t
 oct_port_init (vlib_main_t *vm, vnet_dev_port_t *port)
 {
@@ -129,12 +145,15 @@ oct_port_init (vlib_main_t *vm, vnet_dev_port_t *port)
 
   if (port->args)
     {
-      if (clib_args_get_bool_val_by_name (port->args, "eth_pause_frame"))
+      if (oct_port_is_arg_valid (port->args, "eth_pause_frame") &&
+	  clib_args_get_bool_val_by_name (port->args, "eth_pause_frame"))
 	is_pause_frame_enable = true;
-      cp->rss_flowkey =
-	clib_args_get_uint32_val_by_name (port->args, "rss_flow_key");
-      cp->switch_header_type =
-	clib_args_get_enum_val_by_name (port->args, "switch_header_type");
+      if (oct_port_is_arg_valid (port->args, "rss_flow_key"))
+	cp->rss_flowkey =
+	  clib_args_get_uint32_val_by_name (port->args, "rss_flow_key");
+      if (oct_port_is_arg_valid (port->args, "switch_header_type"))
+	cp->switch_header_type =
+	  clib_args_get_enum_val_by_name (port->args, "switch_header_type");
     }
 
   if ((rrv = roc_nix_lf_alloc (nix, ifs->num_rx_queues, ifs->num_tx_queues,

src/vppinfra/args.c
@@ -20,7 +20,7 @@ clib_args_alloc (void)
   return h;
 }
 
-static int
+__clib_export int
 clib_args_find_arg_by_name (clib_args_handle_t h, char *fmt, va_list *va)
 {
   clib_arg_t *a;

src/vppinfra/args.h
@@ -100,6 +100,7 @@ typedef clib_args_t *clib_args_handle_t;
     __VA_ARGS__, CLIB_ARG_END ()                                              \
   }
 
+int clib_args_find_arg_by_name (clib_args_handle_t h, char *fmt, va_list *va);
 u32 clib_args_get_uint32_val_by_name (clib_args_handle_t h, char *fmt, ...);
 int clib_args_get_bool_val_by_name (clib_args_handle_t h, char *fmt, ...);
 int clib_args_get_enum_val_by_name (clib_args_handle_t h, char *fmt, ...);