861c5f4ef770cca46dbd7bf729c500eeada715b3
Filip Tehlar <ftehlar@meter.com>
2026-01-14 12:33:56 +0100

    ipsec: fix incorrect return types
    The x_encode() functions claim to return an enum type, but sometimes
    also return VNET_API_ERRORs.
    
    This change brings it in alignment with x_decode() functions: return an
    int, and return the output through an argument.
    
    Type: fix
    Change-Id: Ieb50c2b925485fa4b2233c84970c520c016e5cb8
    Signed-off-by: Filip Tehlar <ftehlar@meter.com>
    Signed-off-by: Ram Subramanian <ram@meter.com>

--- src/vnet/ipsec/ipsec_types_api.h
+++ src/vnet/ipsec/ipsec_types_api.h
@@ -14,16 +14,15 @@
 
 extern int ipsec_proto_decode (vl_api_ipsec_proto_t in,
 			       ipsec_protocol_t * out);
-extern vl_api_ipsec_proto_t ipsec_proto_encode (ipsec_protocol_t p);
+extern int ipsec_proto_encode (ipsec_protocol_t p, vl_api_ipsec_proto_t *out);
 
 extern int ipsec_crypto_algo_decode (vl_api_ipsec_crypto_alg_t in,
 				     ipsec_crypto_alg_t * out);
-extern vl_api_ipsec_crypto_alg_t ipsec_crypto_algo_encode (ipsec_crypto_alg_t
-							   c);
+extern int ipsec_crypto_algo_encode (ipsec_crypto_alg_t c, vl_api_ipsec_crypto_alg_t *out);
 
 extern int ipsec_integ_algo_decode (vl_api_ipsec_integ_alg_t in,
 				    ipsec_integ_alg_t * out);
-extern vl_api_ipsec_integ_alg_t ipsec_integ_algo_encode (ipsec_integ_alg_t i);
+extern int ipsec_integ_algo_encode (ipsec_integ_alg_t i, vl_api_ipsec_integ_alg_t *out);
 
 extern void ipsec_key_decode (const vl_api_key_t * key, ipsec_key_t * out);
 extern void ipsec_key_encode (const ipsec_key_t * in, vl_api_key_t * out);
