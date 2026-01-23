8fc2ebe562c2d19c8a514a581ae95e0f55998b47
Vladimir Lavor <vladimir.lavor@pantheon.tech>
2026-01-23 14:37:47 +0100

    support for wireguard psk
    Change-Id: I9bccb04c7ac8d430912b3a6fd696ac1173413615
    Signed-off-by: Vladimir lavor <vladimir.lavor@pantheon.tech>

--- src/plugins/wireguard/wireguard_peer.h
+++ src/plugins/wireguard/wireguard_peer.h
@@ -117,6 +117,7 @@ typedef struct wg_peer_table_bind_ctx_t_
 } wg_peer_table_bind_ctx_t;
 
 int wg_peer_add (u32 tun_sw_if_index,
+         const u8 preshared_key[NOISE_SYMMETRIC_KEY_LEN], bool preshared_key_set,
 		 const u8 public_key_64[NOISE_PUBLIC_KEY_LEN],
 		 u32 table_id,
 		 const ip46_address_t * endpoint,
