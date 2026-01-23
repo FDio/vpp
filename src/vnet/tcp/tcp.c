be0e3fc732ab2b9d9827a466be06de640e9be8f4
Florin Coras <fcoras@cisco.com>
2026-01-22 22:59:02 -0500

    tcp: handle active and passive open lcl port conflicts
    Mark listened on ip:port pairs, if ip is non-zero, as used, to force lcl
    port allocation for active opens to check 5-tuples.
    
    Also, avoid trying to cleanup lcl ports of accepted connections.
    
    Type: improvement
    
    Change-Id: Ia8572c4b8ed4551a4d954e5e844760e5a8b357f5
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/vnet/tcp/tcp.c
+++ src/vnet/tcp/tcp.c
@@ -133,6 +133,16 @@ tcp_connection_bind (u32 session_index, transport_endpoint_cfg_t *lcl)
 
   tcp_connection_timers_init (listener);
 
+  if (!ip_is_zero (&lcl->ip, 1))
+    {
+      if (transport_mark_used_local_endpoint (TRANSPORT_PROTO_TCP, listener->c_fib_index,
+					      &listener->c_lcl_ip, listener->c_lcl_port))
+	transport_share_local_endpoint (TRANSPORT_PROTO_TCP, listener->c_fib_index,
+					&listener->c_lcl_ip, listener->c_lcl_port);
+    }
+  else
+    listener->cfg_flags |= TCP_CFG_F_NO_ENDPOINT;
+
   TCP_EVT (TCP_EVT_BIND, listener);
 
   return listener->c_c_index;
@@ -151,6 +161,10 @@ tcp_connection_unbind (u32 listener_index)
   tcp_connection_t *tc;
 
   tc = pool_elt_at_index (tm->listener_pool, listener_index);
+
+  if (!(tc->cfg_flags & TCP_CFG_F_NO_ENDPOINT))
+    transport_release_local_endpoint (TRANSPORT_PROTO_TCP, tc->c_fib_index, &tc->c_lcl_ip,
+				      tc->c_lcl_port);
 
   TCP_EVT (TCP_EVT_UNBIND, tc);
 
