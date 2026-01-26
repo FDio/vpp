9f4735dbff10c88e5f3916eed037ae3f1fb684b3
Hadi Rayan Al-Sandid <halsandi@cisco.com>
2026-01-26 10:04:23 +0100

    tracepath: introduced tracepath plugin
    * Introduces CLI commands to display path of traced packets
    through VPP node graph.
    
    * Allows to filter VPP trace to only displays packet
    following a specific path through the VPP node graph.
    
    Type: feature
    
    Change-Id: I4bd36f7e9f094e7cc17971dda305b47f2f7ab28d
    Signed-off-by: Hadi Rayan Al-Sandid <halsandi@cisco.com>

---
+++ src/plugins/tracepath/plugin.c
@@ -0,0 +1,13 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2025 Cisco and/or its affiliates.
+ */
+
+#include <vlib/vlib.h>
+#include <vnet/plugin/plugin.h>
+#include <vpp/app/version.h>
+
+VLIB_PLUGIN_REGISTER () = {
+  .version = VPP_BUILD_VER,
+  .description = "Display traces based on packet paths",
+};
