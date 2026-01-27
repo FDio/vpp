f05b99b1cdee23f917f6c07899b40fef0ce1c0d5
Moinak Bhattacharyya <moinakb001@gmail.com>
2026-01-27 17:16:30 -0600

    iouring: Add io_uring plugin to allow polling usage of io_uring
    - Initialize a new io_uring plugin to better interface with the kernel (maybe eventually replacing epoll)
    - Node-based: individual io_uring ring per thread, queue and submit in whichever node or process is necessary, reap in a node registered with uring
    - Added command-line tests for several commands
    - This is just a v1, further changes on top of this will add registered buffer, file support etc.
    
    Type: improvement
    
    Change-Id: I3c2fb389124bc13df1333230e022693f6a6a6964
    Signed-off-by: Moinak Bhattacharyya <moinakb001@gmail.com>

---
+++ build/external/packages/liburing.mk
@@ -0,0 +1,36 @@
+# Copyright (c) 2026 Moinak Bhattacharyya <moinakb001@gmail.com>
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+liburing_version             := 2.13
+liburing_tarball             := liburing-$(liburing_version).tar.gz
+liburing_tarball_sha256sum_2.13 := 618e34dbea408fc9e33d7c4babd746036dbdedf7fce2496b1178ced0f9b5b357
+
+liburing_tarball_sha256sum      := $(liburing_tarball_sha256sum_$(liburing_version))
+liburing_tarball_strip_dirs  := 1
+liburing_url                 := https://github.com/axboe/liburing/archive/refs/tags/$(liburing_tarball)
+
+define  liburing_config_cmds
+	@true
+endef
+
+define  liburing_build_cmds
+	@cd ${liburing_src_dir} && $(MAKE) CC=gcc CXX=g++ > $(liburing_build_log)
+endef
+
+define  liburing_install_cmds
+	@rm -f $(liburing_install_log)
+	@cd ${liburing_src_dir} && \
+		$(MAKE) install PREFIX='$(liburing_install_dir)' >> $(liburing_install_log)
+endef
+
+$(eval $(call package,liburing))
