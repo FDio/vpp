FDio/vpp

commit 1b794cf1b639534b58ea7eab4baadff11bd3d19e

linux-cp: populate mapping vif-sw_if_index only for default-ns
When custom netns is used we don't need to populate the mapping
ifindex->sw_if_index otherwise netlink events in default-ns can apply settings
to wrong interfaces. Most of the lcp functions however wasn't working for such
interfaces and neither it was fixed by this patch.

Type: fix
Change-Id: I74a8a4e332753f9a40fc291d489dfd7fc51cbef3
Signed-off-by: Stanislav Zaikin <stanislav.zaikin@46labs.com>

src/plugins/linux-cp/lcp_interface.c
@@ -258,7 +258,11 @@ lcp_itf_pair_add (u32 host_sw_if_index, u32 phy_sw_if_index, u8 *host_name,
   vec_validate_init_empty (lip_db_by_host, host_sw_if_index, INDEX_INVALID);
   lip_db_by_phy[phy_sw_if_index] = lipi;
   lip_db_by_host[host_sw_if_index] = lipi;
-  hash_set (lip_db_by_vif, host_index, lipi);
+
+  if (clib_strcmp ((char *) ns, (char *) lcp_get_default_ns ()) == 0)
+    {
+      hash_set (lip_db_by_vif, host_index, lipi);
+    }
 
   lip->lip_host_sw_if_index = host_sw_if_index;
   lip->lip_phy_sw_if_index = phy_sw_if_index;
@@ -1094,7 +1098,7 @@ lcp_itf_pair_create (u32 phy_sw_if_index, u8 *host_if_name,
    * This controls whether the host can RX/TX.
    */
   sw = vnet_get_sw_interface (vnm, phy_sw_if_index);
-  lip = lcp_itf_pair_get (lcp_itf_pair_find_by_vif (vif_index));
+  lip = lcp_itf_pair_get (lcp_itf_pair_find_by_phy (phy_sw_if_index));
   LCP_ITF_PAIR_INFO ("pair create: %U sw-flags %u hw-flags %u",
 		     format_lcp_itf_pair, lip, sw->flags, hw->flags);
   vnet_sw_interface_admin_up (vnm, host_sw_if_index);

commit 58216459ae708fbfd45bd99265264d1fca5bb044

linux-cp: fix clib_c11_violation when creating interface
When creating an interface without a namespace or without a namespace
set, either or both the first and second parameters to the clib_strcmp
call in lcp_itf_pair_add can be NULL, triggering clib_c11_violation
errors.

Fix this by checking both for NULL before calling clib_strcmp on them.

Type: fix
Fixes: 1b794cf1b639534b58ea7eab4baadff11bd3d19e
Change-Id: I2c2b5e90b6004b92e9e6437b7c63f44567caaef7
Signed-off-by: Rob Shearman <robertshearman@gmail.com>

src/plugins/linux-cp/lcp_interface.c
@@ -264,7 +264,7 @@ lcp_itf_pair_add (u32 host_sw_if_index, u32 phy_sw_if_index, u8 *host_name,
   lip_db_by_phy[phy_sw_if_index] = lipi;
   lip_db_by_host[host_sw_if_index] = lipi;
 
-  if (clib_strcmp ((char *) ns, (char *) lcp_get_default_ns ()) == 0)
+  if (ns && lcp_get_default_ns () && clib_strcmp ((char *) ns, (char *) lcp_get_default_ns ()) == 0)
     {
       hash_set (lip_db_by_vif, host_index, lipi);
     }