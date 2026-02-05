FDio/vpp

commit 9106a84d357f255aaa868f6924ae1c37c1862d29

experiemnts: dev
Change-Id: Ief5d4089aae34ecbaef339b4e3de6f3aa55ddc29
Signed-off-by: Vratko Polak <vrpolak@cisco.com>

src/vnet/dev/dev.c
@@ -287,9 +287,9 @@ vnet_dev_dma_mem_alloc (vlib_main_t *vm, vnet_dev_t *dev, u32 size, u32 align,
       return rv;
     }
 
+  *pp = p;
   log_debug (dev, "%u bytes va %p dma-addr 0x%lx numa %u align %u", size, *pp,
 	     vnet_dev_get_dma_addr (vm, dev, *pp), dev->numa_node, align);
-  *pp = p;
   vec_foreach_index (i, dev->dma_allocs)
     if (dev->dma_allocs[i] == 0)
       {

commit 065fab89d0ddc69e737db6cd38c205b86b35bdb5

dev: avoid null pointer in debug log
For some reason, only some testbeds (2n-zn2 with AVF) failed in RC1.
Still, the log statement has no business parsing unitialized *pp.

Type: fix
Fixes: 9106a84d357f255aaa868f6924ae1c37c1862d29

Change-Id: Ief5d4089aae34ecbaef339b4e3de6f3aa55ddc29
Signed-off-by: Vratko Polak <vrpolak@cisco.com>

src/vnet/dev/dev.c
@@ -287,9 +287,9 @@ vnet_dev_dma_mem_alloc (vlib_main_t *vm, vnet_dev_t *dev, u32 size, u32 align,
       return rv;
     }
 
+  *pp = p;
   log_debug (dev, "%u bytes va %p dma-addr 0x%lx numa %u align %u", size, *pp,
 	     vnet_dev_get_dma_addr (vm, dev, *pp), dev->numa_node, align);
-  *pp = p;
   vec_foreach_index (i, dev->dma_allocs)
     if (dev->dma_allocs[i] == 0)
       {