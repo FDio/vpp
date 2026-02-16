7cd6d8c402316fcab74008954d0528321e68d111
Jimmy Björklund <jimmy.bjorklund@hotmail.se>
2026-02-16 09:45:18 +0000

    vpp: Fixed regration of MultiGigaEthernet0_0_rx => MultiGigaEthernet0_0-rx.
    Type: fix
    
    When fixing the case that some interface names have / in the names the code also changed
    - to _ with caused issue reported: https://github.com/FDio/vpp/issues/3686
    
    Change-Id: I88eb578917a29f8b7329ee56c672dab658ab9331
    Signed-off-by: Jimmy Björklund <jimmy.bjorklund@hotmail.se>

--- src/vpp/app/dump_metrics.h
+++ src/vpp/app/dump_metrics.h
@@ -3,6 +3,6 @@
 #include <vpp-api/client/stat_client.h>
 #include <vlib/vlib.h>
 #include <stdio.h>
-void dump_metrics (FILE *stream, u8 **patterns, u8 v2,
-		   stat_client_main_t *shm);
+void dump_metrics (FILE *stream, u8 **patterns, u8 v2, stat_client_main_t *shm);
+void print_metric_v2 (FILE *stream, stat_segment_data_t *res);
 #endif /* DUMP_METRICS_H */
