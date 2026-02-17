99e85e5bd68b8bde883b005b6b6c9a5d2c36ab63
Nathan Skrzypczak <nathan.skrzypczak@gmail.com>
2026-02-17 14:49:43 +0100

    ip6: fix show ip6-ll cli if selector
    This patch fixes the show ip6-ll CLI to bring
    back the interface selection. With this patch you can use
    `show ip6-ll tap0` to restrict the output to a single fib.
    
    This also allows passing the prefix length so that
    you can express `show ip6-ll tap0 fe80::/10`
    
    This is useful when seeking through a large number of LL
    interfaces.
    
    Type: improvement
    
    Change-Id: I8bd29a911ca1f5c4f77d122da94f81597db7d230
    Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

--- src/vnet/ip/ip6_ll_table.c
+++ src/vnet/ip/ip6_ll_table.c
@@ -23,13 +23,6 @@ ip6_ll_fib_get (u32 sw_if_index)
 }
 
 fib_node_index_t
-ip6_ll_table_lookup (const ip6_ll_prefix_t * prefix)
-{
-  return (ip6_fib_table_lookup (ip6_ll_fib_get (prefix->ilp_sw_if_index),
-				&prefix->ilp_addr, 128));
-}
-
-fib_node_index_t
 ip6_ll_table_lookup_exact_match (const ip6_ll_prefix_t * prefix)
 {
   return (ip6_fib_table_lookup_exact_match
@@ -155,16 +148,6 @@ ip6_ll_table_entry_delete (const ip6_ll_prefix_t * ilp)
     }
 }
 
-static void
-ip6_ll_table_show_one (vlib_main_t * vm, ip6_ll_prefix_t * ilp, int detail)
-{
-  vlib_cli_output (vm, "%U",
-		   format_fib_entry,
-		   ip6_ll_table_lookup (ilp),
-		   (detail ?
-		    FIB_ENTRY_FORMAT_DETAIL2 : FIB_ENTRY_FORMAT_DETAIL));
-}
-
 typedef struct ip6_ll_show_ctx_t_
 {
   fib_node_index_t *entries;
@@ -201,38 +184,58 @@ ip6_ll_table_show_all (vlib_main_t * vm, u32 fib_index)
   vec_free (ctx.entries);
 }
 
+static void
+ip6_ll_show_one_fib (vlib_main_t *vm, u32 sw_if_index, ip6_address_t *matching_address,
+		     u32 mask_len, int verbose)
+{
+  fib_table_t *fib_table;
+  u32 fib_index;
+
+  fib_index = ip6_ll_table.ilt_fibs[sw_if_index];
+  if (~0 == fib_index)
+  return;
+
+  fib_table = fib_table_get (fib_index, FIB_PROTOCOL_IP6);
+  if (!(fib_table->ft_flags & FIB_TABLE_FLAG_IP6_LL))
+  return;
+
+  ip6_fib_table_show (vm, fib_table, !verbose);
+  if (verbose == FIB_ENTRY_FORMAT_BRIEF)
+  return;
+
+  if (matching_address == NULL)
+  ip6_ll_table_show_all (vm, fib_index);
+  else
+  {
+    fib_node_index_t fei = ip6_fib_table_lookup (fib_index, matching_address, mask_len);
+    vlib_cli_output (vm, "%U", format_fib_entry, fei, verbose);
+  }
+}
+
 static clib_error_t *
 ip6_ll_show_fib (vlib_main_t * vm,
 		 unformat_input_t * input, vlib_cli_command_t * cmd)
 {
-  fib_table_t *fib_table;
-  int verbose, matching;
-  ip6_address_t matching_address;
-  u32 mask_len = 128;
-  u32 sw_if_index = ~0;
-  int detail = 0;
+  ip6_address_t _matching_address, *matching_address = NULL;
   vnet_main_t *vnm = vnet_get_main ();
-  u32 fib_index;
-
-  verbose = 1;
-  matching = 0;
+  u32 sw_if_index = ~0;
+  u32 mask_len = 128;
+  int verbose = FIB_ENTRY_FORMAT_DETAIL;
 
   while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
     {
       if (unformat (input, "brief") ||
 	  unformat (input, "summary") || unformat (input, "sum"))
-	verbose = 0;
+	verbose = FIB_ENTRY_FORMAT_BRIEF;
 
       else if (unformat (input, "detail") || unformat (input, "det"))
-	detail = 1;
+	verbose = FIB_ENTRY_FORMAT_DETAIL2;
 
-      else if (unformat (input, "%U/%d",
-			 unformat_ip6_address, &matching_address, &mask_len))
-	matching = 1;
+      else if (unformat (input, "%U/%d", unformat_ip6_address, &_matching_address, &mask_len))
+	matching_address = &_matching_address;
 
-      else
-	if (unformat (input, "%U", unformat_ip6_address, &matching_address))
-	matching = 1;
+      else if (unformat (input, "%U", unformat_ip6_address, &_matching_address))
+	matching_address = &_matching_address;
       else if (unformat (input, "%U",
 			 unformat_vnet_sw_interface, vnm, &sw_if_index))
 	;
@@ -240,41 +243,13 @@ ip6_ll_show_fib (vlib_main_t * vm,
 	break;
     }
 
-  vec_foreach_index (sw_if_index, ip6_ll_table.ilt_fibs)
-  {
-    fib_index = ip6_ll_table.ilt_fibs[sw_if_index];
-    if (~0 == fib_index)
-      continue;
-
-    fib_table = fib_table_get (fib_index, FIB_PROTOCOL_IP6);
-
-    if (!(fib_table->ft_flags & FIB_TABLE_FLAG_IP6_LL))
-      continue;
-
-    ip6_fib_table_show (vm, fib_table, !verbose);
-    if (!verbose)
-      continue;
-
-    if (!matching)
+  if (sw_if_index != ~0)
+    ip6_ll_show_one_fib (vm, sw_if_index, matching_address, mask_len, verbose);
+  else
+    vec_foreach_index (sw_if_index, ip6_ll_table.ilt_fibs)
       {
-	ip6_ll_table_show_all (vm, fib_index);
-      }
-    else
-      {
-	if (~0 == sw_if_index)
-	  {
-	    vlib_cli_output (vm, "specify the interface");
-	  }
-	else
-	  {
-	    ip6_ll_prefix_t ilp = {
-	      .ilp_addr = matching_address,
-	      .ilp_sw_if_index = sw_if_index,
-	    };
-	    ip6_ll_table_show_one (vm, &ilp, detail);
-	  }
-      }
-  };
+	ip6_ll_show_one_fib (vm, sw_if_index, matching_address, mask_len, verbose);
+      };
 
   return 0;
 }
