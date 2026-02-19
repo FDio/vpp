cb786487c2878f94b0d369dbcb8aed27a96528eb
Nicolas PLANEL <nplanel@cisco.com>
2026-02-19 16:34:10 +0100

    sfdp: offload lookup via vnet flow mark
    CLI now accepts: set sfdp interface-input <interface> tenant <tenant-id> [ip6] [lookup-offload] [disable]
    
    Type: feature
    Change-Id: Ie1398b3aead57161d7940d21e3eae5a627355abc
    Signed-off-by: Nicolas PLANEL <nplanel@cisco.com>

--- src/vnet/sfdp/expiry/expiry.c
+++ src/vnet/sfdp/expiry/expiry.c
@@ -2,13 +2,40 @@
  * Copyright (c) 2025 Cisco Systems, Inc.
  */
 
+#include "vnet/sfdp/sfdp.h"
 #include <vnet/sfdp/expiry/expiry.h>
 #include <vnet/sfdp/timer/timer.h>
 
 #include <vlib/vlib.h>
+#include <vppinfra/elog.h>
 #include <vnet/sfdp/sfdp_funcs.h>
+#include <vnet/flow/flow.h>
+#include <vnet/sfdp/lookup/lookup.h>
 
 u8 static expiry_is_enabled = 0;
+
+#define foreach_sfdp_expire_error                                                                  \
+  _ (NODE_CALLED, "node-called", INFO, "node called")                                              \
+  _ (EXPIRED, "expired", INFO, "session expired")                                                  \
+  _ (REQUESTED_EVICTION, "requested-eviction", INFO, "requested eviction")                         \
+  _ (FLOW_OFFLOAD_UNSET, "flow-offload-unset", ERROR, "flow offload unset")                        \
+  _ (FLOW_OFFLOAD_DISABLE_FAILED, "flow-offload-disable-failed", ERROR,                            \
+     "flow offload disable failed")                                                                \
+  _ (FLOW_OFFLOAD_DELETE_FAILED, "flow-offload-delete-failed", ERROR, "flow offload delete failed")
+
+typedef enum
+{
+#define _(sym, name, sev, str) SFDP_EXPIRE_ERROR_##sym,
+  foreach_sfdp_expire_error
+#undef _
+    SFDP_EXPIRE_N_ERROR,
+} sfdp_expire_error_t;
+
+static vlib_error_desc_t sfdp_expire_error_descriptors[] = {
+#define _(sym, name, sev, str) { name, str, VL_COUNTER_SEVERITY_##sev },
+  foreach_sfdp_expire_error
+#undef _
+};
 
 int
 sfdp_set_expiry_callbacks (const sfdp_expiry_callbacks_t *callbacks)
@@ -51,6 +78,42 @@ sfdp_enable_disable_expiry_node (u8 is_disable, int skip_main)
     }
 }
 
+static int
+sfdp_flow_offload_delete (vlib_main_t *vm, vlib_node_runtime_t *node, u32 session_index,
+			  u8 direction)
+{
+  vnet_main_t *vnm = &vnet_main;
+  sfdp_main_t *sfdp = &sfdp_main;
+  sfdp_session_t *session = pool_elt_at_index (sfdp->sessions, session_index);
+  int rv = 0;
+
+  if (session->flow_index[direction] == ~0)
+    {
+      vlib_node_increment_counter (vm, node->node_index, SFDP_EXPIRE_ERROR_FLOW_OFFLOAD_UNSET, 1);
+      return -1;
+    }
+
+  rv = vnet_flow_disable (vnm, session->flow_index[direction], session->rx_hw_if_index);
+  if (rv != 0)
+    {
+      vlib_node_increment_counter (vm, node->node_index,
+				   SFDP_LOOKUP_ERROR_FLOW_OFFLOAD_DISABLE_FAILED, 1);
+      return rv;
+    }
+
+  rv = vnet_flow_del (vnm, session->flow_index[direction]);
+  if (rv != 0)
+    {
+      vlib_node_increment_counter (vm, node->node_index,
+				   SFDP_LOOKUP_ERROR_FLOW_OFFLOAD_DELETE_FAILED, 1);
+      return rv;
+    }
+
+  session->flow_index[direction] = ~0;
+
+  return 0;
+}
+
 void
 sfdp_enable_disable_expiry (u8 is_disable)
 {
@@ -71,25 +134,6 @@ sfdp_enable_disable_expiry (u8 is_disable)
       expiry_is_enabled = false;
     }
 }
-
-#define foreach_sfdp_expire_error                                             \
-  _ (NODE_CALLED, "node-called", INFO, "node called")                         \
-  _ (EXPIRED, "expired", INFO, "session expired")                             \
-  _ (REQUESTED_EVICTION, "requested-eviction", INFO, "requested eviction")
-
-typedef enum
-{
-#define _(sym, name, sev, str) SFDP_EXPIRE_ERROR_##sym,
-  foreach_sfdp_expire_error
-#undef _
-    SFDP_EXPIRE_N_ERROR,
-} sfdp_expire_error_t;
-
-static vlib_error_desc_t sfdp_expire_error_descriptors[] = {
-#define _(sym, name, sev, str) { name, str, VL_COUNTER_SEVERITY_##sev },
-  foreach_sfdp_expire_error
-#undef _
-};
 
 VLIB_NODE_FN (sfdp_expire_node)
 (vlib_main_t *vm, vlib_node_runtime_t *node, vlib_frame_t *frame)
@@ -127,6 +171,14 @@ VLIB_NODE_FN (sfdp_expire_node)
   vec_foreach (session_index, ptd->expired_sessions)
     {
       sfdp_session_t *session = sfdp_session_at_index (*session_index);
+      if (session->flow_index[SFDP_FLOW_FORWARD] != ~0)
+	{
+	  sfdp_flow_offload_delete (vm, node, *session_index, SFDP_FLOW_FORWARD);
+	}
+      if (session->flow_index[SFDP_FLOW_REVERSE] != ~0)
+	{
+	  sfdp_flow_offload_delete (vm, node, *session_index, SFDP_FLOW_REVERSE);
+	}
       sfdp_session_remove (sfdp, ptd, session, thread_index, *session_index);
     }
 
