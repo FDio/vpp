b1ad26be4df0cddc6fc6bea28a3c45ec9aaf5396
Nicolas PLANEL <nplanel@cisco.com>
2026-02-19 16:34:10 +0100

    sfdp: offload lookup via vnet flow mark
    CLI now accepts: set sfdp interface-input <interface> tenant <tenant-id> [ip6] [offload] [disable]
    
    Change-Id: Ie1398b3aead57161d7940d21e3eae5a627355abc
    Signed-off-by: Nicolas PLANEL <nplanel@cisco.com>

--- src/plugins/sfdp_services/base/interface_input/interface_input.h
+++ src/plugins/sfdp_services/base/interface_input/interface_input.h
@@ -18,11 +18,12 @@ typedef enum
 typedef struct
 {
   u16 *tenant_idx_by_sw_if_idx[SFDP_INTERFACE_INPUT_PROTO_N_TYPES]; /* vec */
+  u8 *offload_enabled_by_sw_if_idx[SFDP_INTERFACE_INPUT_PROTO_N_TYPES]; /* vec */
   u16 msg_id_base;
 } sfdp_interface_input_main_t;
 
 extern sfdp_interface_input_main_t sfdp_interface_input_main;
 
 clib_error_t *sfdp_interface_input_set_tenant (sfdp_interface_input_main_t *vim, u32 sw_if_index,
-					       u32 tenant_id, u8 is_ip6, u8 unset);
+					       u32 tenant_id, u8 is_ip6, u8 offload, u8 unset);
 #endif /* __included_sfdp_interface_input_h__ */
