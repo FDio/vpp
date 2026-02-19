0f939dd93aa62541afab30e647160ca668a7d753
Florin Coras <fcoras@cisco.com>
2026-02-18 21:07:36 -0500

    vppinfra: fix memory-trace assert
    Type: fix
    
    Change-Id: I047d1a8262c25c3a0cda57d2daedce11c27ca790
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- test/asf/test_vppinfra.py
+++ test/asf/test_vppinfra.py
@@ -33,6 +33,38 @@ class TestVppinfra(VppAsfTestCase):
                 self.logger.critical(error)
                 self.assertNotIn("failed", error)
 
+    def test_memory_trace(self):
+        """Memory Trace Test"""
+        # Enable memory tracing on main-heap
+        r = self.vapi.cli_return_response("memory-trace on main-heap")
+        self.assertEqual(r.retval, 0, "Failed to enable memory-trace")
+
+        # Do some allocations by creating a loopback interface
+        r = self.vapi.cli_return_response("loopback create")
+        self.assertEqual(r.retval, 0, "Failed to create loopback")
+
+        # Show memory trace (should have some allocations)
+        r = self.vapi.cli_return_response("show memory main-heap")
+        self.assertEqual(r.retval, 0, "Failed to show memory")
+
+        # Disable memory tracing - this is where the bug would trigger
+        # an assertion failure in clib_mem_trace_main_free()
+        r = self.vapi.cli_return_response("memory-trace off")
+        self.assertEqual(r.retval, 0, "Failed to disable memory-trace")
+
+        # Verify we can still do operations after disabling
+        r = self.vapi.cli_return_response("show memory main-heap")
+        self.assertEqual(r.retval, 0, "Failed to show memory after trace off")
+
+        # Test enable/disable on multiple heaps
+        r = self.vapi.cli_return_response(
+            "memory-trace on main-heap api-segment stats-segment"
+        )
+        self.assertEqual(r.retval, 0, "Failed to enable memory-trace on all heaps")
+
+        r = self.vapi.cli_return_response("memory-trace off")
+        self.assertEqual(r.retval, 0, "Failed to disable memory-trace on all heaps")
+
 
 if __name__ == "__main__":
     unittest.main(testRunner=VppTestRunner)
