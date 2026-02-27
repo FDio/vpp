2c9560c68961471d46f0bf320785f2876abd792e
Damjan Marion <damarion@cisco.com>
2026-02-27 10:26:02 +0100

    udp-echo: add checksum regenerate options
    Type: improvement
    Change-Id: Ia9348adad437cff1db0e50dd8d84e40279f1baca
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- src/plugins/udp-echo/cli.c
+++ src/plugins/udp-echo/cli.c
@@ -15,6 +15,8 @@ udp_echo_enable_disable_command_fn (vlib_main_t *vm, unformat_input_t *input,
   udp_echo_main_t *uem = &udp_echo_main;
   u32 port = 0;
   int enable = 1;
+  u8 regen_udp_cksum = uem->regen_udp_cksum;
+  u8 regen_ip_cksum = uem->regen_ip_cksum;
 
   while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
     {
@@ -22,6 +24,10 @@ udp_echo_enable_disable_command_fn (vlib_main_t *vm, unformat_input_t *input,
 	enable = 0;
       else if (unformat (input, "port %d", &port))
 	;
+      else if (unformat (input, "regen-udp-cksum"))
+	regen_udp_cksum = 1;
+      else if (unformat (input, "regen-ip-cksum"))
+	regen_ip_cksum = 1;
       else
 	break;
     }
@@ -46,6 +52,8 @@ udp_echo_enable_disable_command_fn (vlib_main_t *vm, unformat_input_t *input,
       udp_register_dst_port (vm, port, udp_echo_node.index, 1 /* is_ip4 */);
       uem->port = port;
       uem->enabled = 1;
+      uem->regen_udp_cksum = regen_udp_cksum;
+      uem->regen_ip_cksum = regen_ip_cksum;
     }
   else
     {
@@ -61,7 +69,7 @@ udp_echo_enable_disable_command_fn (vlib_main_t *vm, unformat_input_t *input,
 
 VLIB_CLI_COMMAND (udp_echo_enable_disable_command, static) = {
   .path = "udp-echo",
-  .short_help = "udp-echo [port <port>] [disable]",
+  .short_help = "udp-echo [port <port>] [regen-udp-cksum] [regen-ip-cksum] [disable]",
   .function = udp_echo_enable_disable_command_fn,
 };
 
