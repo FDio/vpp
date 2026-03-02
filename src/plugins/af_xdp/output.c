16b013a4340f48a5c4225cd78de5454bea4e6783
Jerome Tollet <jtollet@cisco.com>
2026-03-02 10:36:14 +0000

    af_xdp: add initial multi-buffer support
    This change adds initial AF_XDP multi-buffer support in VPP.
    
    Implemented:
    - add CLI/API create-interface options: multi-buffer/no-multi-buffer
    - add multi-buffer RX/TX paths with XDP_PKT_CONTD handling
    - add dedicated eBPF section xdp.frags
      (extras/bpf/af_xdp_mb.bpf.c)
    - in device.c, auto-select xdp.frags only when multi-buffer is enabled
      and no user prog is provided
    - update xdp-tools dependency to newer upstream through VPP deps flow
    
    Known limitations / remaining work:
    - UDP performance/loss still needs tuning and repeated benchmarking
    - broader non-regression still required (long runs, more NIC/env)
    
    Change-Id: If996a83d9def356e047b4fd3f540ec5c713654b1
    Signed-off-by: Jerome Tollet <jtollet@cisco.com>

--- src/plugins/af_xdp/output.c
+++ src/plugins/af_xdp/output.c
@@ -123,6 +123,122 @@ af_xdp_device_output_tx_db (vlib_main_t * vm,
   clib_spinlock_unlock_if_init (&txq->syscall_lock);
 }
 
+static_always_inline void
+af_xdp_device_output_free_mb (vlib_main_t *vm, af_xdp_txq_t *txq)
+{
+  const __u64 * compl ;
+  const u32 size = txq->cq.size;
+  const u32 mask = size - 1;
+  u32 bis[VLIB_FRAME_SIZE], *bi = bis;
+  u32 n_wrap, idx;
+  u32 n = xsk_ring_cons__peek (&txq->cq, ARRAY_LEN (bis), &idx);
+  const u32 n_free = n;
+
+  if (!n_free)
+    return;
+
+  compl = xsk_ring_cons__comp_addr (&txq->cq, idx);
+  n = clib_min (n_free, size - (idx & mask));
+  n_wrap = n_free - n;
+
+wrap_around:
+  while (n >= 1)
+    {
+      bi[0] = compl [0] >> CLIB_LOG2_CACHE_LINE_BYTES;
+      ASSERT (vlib_buffer_is_known (vm, bi[0]) == VLIB_BUFFER_KNOWN_ALLOCATED);
+      compl += 1;
+      bi += 1;
+      n -= 1;
+    }
+
+  if (n_wrap)
+    {
+      compl = xsk_ring_cons__comp_addr (&txq->cq, 0);
+      n = n_wrap;
+      n_wrap = 0;
+      goto wrap_around;
+    }
+
+  xsk_ring_cons__release (&txq->cq, n_free);
+  vlib_buffer_free_no_next (vm, bis, n_free);
+}
+
+static_always_inline u32
+af_xdp_device_buffer_n_segs (vlib_main_t *vm, vlib_buffer_t *b)
+{
+  u32 n = 1;
+
+  while (b->flags & VLIB_BUFFER_NEXT_PRESENT)
+    {
+      b = vlib_get_buffer (vm, b->next_buffer);
+      n++;
+    }
+
+  return n;
+}
+
+static_always_inline u32
+af_xdp_device_output_tx_try_mb (vlib_main_t *vm, af_xdp_txq_t *txq, u32 n_tx, u32 *bi,
+				u32 *n_tx_desc)
+{
+  const uword start = vm->buffer_main->buffer_mem_start;
+  u32 n_desc_budget, n_desc = 0, n_enq = 0, idx, pidx;
+
+  n_desc_budget = xsk_prod_nb_free (&txq->tx, 16);
+  if (PREDICT_FALSE (n_desc_budget == 0))
+    goto out;
+
+  for (pidx = 0; pidx < n_tx; pidx++)
+    {
+      vlib_buffer_t *b = vlib_get_buffer (vm, bi[pidx]);
+      u32 n_segs = af_xdp_device_buffer_n_segs (vm, b);
+
+      if (n_desc + n_segs > n_desc_budget)
+	break;
+
+      n_desc += n_segs;
+      n_enq++;
+    }
+
+  if (PREDICT_FALSE (n_desc == 0))
+    goto out;
+
+  if (PREDICT_FALSE (xsk_ring_prod__reserve (&txq->tx, n_desc, &idx) != n_desc))
+    {
+      n_enq = n_desc = 0;
+      goto out;
+    }
+
+  for (pidx = 0; pidx < n_enq; pidx++)
+    {
+      vlib_buffer_t *b = vlib_get_buffer (vm, bi[pidx]);
+
+      while (1)
+	{
+	  struct xdp_desc *desc = xsk_ring_prod__tx_desc (&txq->tx, idx++);
+	  u64 offset = (sizeof (vlib_buffer_t) + b->current_data) << XSK_UNALIGNED_BUF_OFFSET_SHIFT;
+	  u64 addr = pointer_to_uword (b) - start;
+
+	  desc->addr = offset | addr;
+	  desc->len = b->current_length;
+	  if (b->flags & VLIB_BUFFER_NEXT_PRESENT)
+	    {
+	      desc->options = XDP_PKT_CONTD;
+	      b = vlib_get_buffer (vm, b->next_buffer);
+	    }
+	  else
+	    {
+	      desc->options = 0;
+	      break;
+	    }
+	}
+    }
+
+out:
+  *n_tx_desc = n_desc;
+  return n_enq;
+}
+
 static_always_inline u32
 af_xdp_device_output_tx_try (vlib_main_t * vm,
 			     const vlib_node_runtime_t * node,
@@ -170,6 +286,7 @@ wrap_around:
       addr = pointer_to_uword (b[0]) - start;
       desc[0].addr = offset | addr;
       desc[0].len = b[0]->current_length;
+      desc[0].options = 0;
 
       vlib_prefetch_buffer_header (b[5], LOAD);
       offset =
@@ -178,6 +295,7 @@ wrap_around:
       addr = pointer_to_uword (b[1]) - start;
       desc[1].addr = offset | addr;
       desc[1].len = b[1]->current_length;
+      desc[1].options = 0;
 
       vlib_prefetch_buffer_header (b[6], LOAD);
       offset =
@@ -186,6 +304,7 @@ wrap_around:
       addr = pointer_to_uword (b[2]) - start;
       desc[2].addr = offset | addr;
       desc[2].len = b[2]->current_length;
+      desc[2].options = 0;
 
       vlib_prefetch_buffer_header (b[7], LOAD);
       offset =
@@ -194,6 +313,7 @@ wrap_around:
       addr = pointer_to_uword (b[3]) - start;
       desc[3].addr = offset | addr;
       desc[3].len = b[3]->current_length;
+      desc[3].options = 0;
 
       desc += 4;
       b += 4;
@@ -221,6 +341,7 @@ wrap_around:
       addr = pointer_to_uword (b[0]) - start;
       desc[0].addr = offset | addr;
       desc[0].len = b[0]->current_length;
+      desc[0].options = 0;
       desc += 1;
       b += 1;
       n -= 1;
@@ -248,7 +369,7 @@ VNET_DEVICE_CLASS_TX_FN (af_xdp_device_class) (vlib_main_t * vm,
   const int shared_queue = tf->shared_queue;
   af_xdp_txq_t *txq = vec_elt_at_index (ad->txqs, tf->queue_id);
   u32 *from;
-  u32 n, n_tx;
+  u32 n, n_tx, n_desc;
   int i;
 
   from = vlib_frame_vector_args (frame);
@@ -257,16 +378,29 @@ VNET_DEVICE_CLASS_TX_FN (af_xdp_device_class) (vlib_main_t * vm,
   if (shared_queue)
     clib_spinlock_lock (&txq->lock);
 
-  for (i = 0, n = 0; i < AF_XDP_TX_RETRIES && n < n_tx; i++)
+  for (i = 0, n = 0, n_desc = 0; i < AF_XDP_TX_RETRIES && n < n_tx; i++)
     {
-      u32 n_enq;
-      af_xdp_device_output_free (vm, node, txq);
-      n_enq =
-	af_xdp_device_output_tx_try (vm, node, ad, txq, n_tx - n, from + n);
+      u32 n_enq, n_desc_enq;
+
+      if (ad->flags & AF_XDP_DEVICE_F_MULTI_BUFFER)
+	af_xdp_device_output_free_mb (vm, txq);
+      else
+	af_xdp_device_output_free (vm, node, txq);
+
+      if (ad->flags & AF_XDP_DEVICE_F_MULTI_BUFFER)
+	n_enq = af_xdp_device_output_tx_try_mb (vm, txq, n_tx - n, from + n, &n_desc_enq);
+      else
+	{
+	  n_enq = af_xdp_device_output_tx_try (vm, node, ad, txq, n_tx - n, from + n);
+	  n_desc_enq = n_enq;
+	}
+
       n += n_enq;
+      n_desc += n_desc_enq;
     }
 
-  af_xdp_device_output_tx_db (vm, node, ad, txq, n);
+  if (n_desc)
+    af_xdp_device_output_tx_db (vm, node, ad, txq, n_desc);
 
   if (shared_queue)
     clib_spinlock_unlock (&txq->lock);
