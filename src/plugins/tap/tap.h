FDio/vpp

commit cb19c526dd1c907569a00936c35e8c1b663008dd

tap: rework and move to plugins
 - decouple it from virtio-pci, common code more hurts than helps
 - fix memory ordering issues on both rx and tx nodes
 - removed excessive unnecesary inlining
 - improve performance by using buffer templates
     - no feature-arc processing per packet
 - SIMD next index calc for TUN
 - start offset for TUN is changed from 0 to +14
   - vnet hdr now fits into same cacheline
   - same for eventual l2 rewrite on the packet
 - moved to plugin
 - dlsym based header file so other plugins can invoke tap APIs
 - implemented freelist for tx descriptor alloc instead of out-of-order hack

Type: improvement
Change-Id: I0d801838be77ac043cd62d62e88383e92d6bc8ad
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/plugins/tap/tap.h
@@ -1,0 +1,148 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2017-2025 Cisco and/or its affiliates.
+ */
+#pragma once
+
+#include <vlib/vlib.h>
+#include <vlib/unix/plugin.h>
+#include <vnet/ethernet/mac_address.h>
+#include <vnet/ip/ip_packet.h>
+#include <vnet/ip/ip4_packet.h>
+#include <vnet/ip/ip6_packet.h>
+#include <vppinfra/error.h>
+
+#define foreach_tapv2_flags                                                   \
+  _ (GSO, 0)                                                                  \
+  _ (CSUM_OFFLOAD, 1)                                                         \
+  _ (PERSIST, 2)                                                              \
+  _ (ATTACH, 3)                                                               \
+  _ (TUN, 4)                                                                  \
+  _ (GRO_COALESCE, 5)                                                         \
+  _ (PACKED, 6)                                                               \
+  _ (IN_ORDER, 7)                                                             \
+  _ (CONSISTENT_QP, 8)
+
+typedef enum
+{
+#define _(a, b) TAP_FLAG_##a = (1 << b),
+  foreach_tapv2_flags
+#undef _
+} tap_flag_t;
+
+typedef struct
+{
+  u32 id;
+  u32 auto_id_offset;
+  u8 mac_addr_set;
+  mac_address_t mac_addr;
+  u16 num_rx_queues;
+  u16 num_tx_queues;
+  u16 rx_ring_sz;
+  u16 tx_ring_sz;
+  tap_flag_t tap_flags;
+  u8 *host_namespace;
+  u8 *if_name;
+  u8 *host_if_name;
+  mac_address_t host_mac_addr;
+  u8 *host_bridge;
+  ip4_address_t host_ip4_addr;
+  u8 host_ip4_prefix_len;
+  ip4_address_t host_ip4_gw;
+  u8 host_ip4_gw_set;
+  ip6_address_t host_ip6_addr;
+  u8 host_ip6_prefix_len;
+  ip6_address_t host_ip6_gw;
+  u8 host_ip6_gw_set;
+  u8 host_mtu_set;
+  u32 host_mtu_size;
+
+  /* return */
+  u32 sw_if_index;
+  int rv;
+  clib_error_t *error;
+} tap_create_if_args_t;
+
+typedef void (tap_create_if_fn_t) (vlib_main_t *vm,
+				   tap_create_if_args_t *args);
+typedef int (tap_delete_if_fn_t) (vlib_main_t *vm, u32 sw_if_index);
+typedef int (tap_set_carrier_fn_t) (u32 hw_if_index, u32 carrier_up);
+typedef int (tap_set_speed_fn_t) (u32 hw_if_index, u32 speed);
+typedef unsigned int (tap_get_ifindex_fn_t) (vlib_main_t *vm, u32 sw_if_index);
+typedef int (tap_is_tun_fn_t) (vlib_main_t *vm, u32 sw_if_index);
+
+#ifdef TAP_PLUGIN_INTERNAL
+
+tap_create_if_fn_t tap_create_if;
+tap_delete_if_fn_t tap_delete_if;
+tap_set_carrier_fn_t tap_set_carrier;
+tap_set_speed_fn_t tap_set_speed;
+tap_get_ifindex_fn_t tap_get_ifindex;
+tap_is_tun_fn_t tap_is_tun;
+
+#else /* TAP_PLUGIN_INTERNAL */
+
+static inline void
+tap_create_if (vlib_main_t *vm, tap_create_if_args_t *args)
+{
+  tap_create_if_fn_t *fn = (tap_create_if_fn_t *) vlib_get_plugin_symbol (
+    "tap_plugin.so", "tap_create_if");
+  if (!fn)
+    {
+      args->error = clib_error_return (0, "tap plugin not loaded");
+      return;
+    }
+  fn (vm, args);
+}
+
+static inline int
+tap_delete_if (vlib_main_t *vm, u32 sw_if_index)
+{
+  tap_delete_if_fn_t *fn = (tap_delete_if_fn_t *) vlib_get_plugin_symbol (
+    "tap_plugin.so", "tap_delete_if");
+  if (!fn)
+    return -1;
+  return fn (vm, sw_if_index);
+}
+
+static inline int
+tap_set_carrier (u32 hw_if_index, u32 carrier_up)
+{
+  tap_set_carrier_fn_t *fn = (tap_set_carrier_fn_t *) vlib_get_plugin_symbol (
+    "tap_plugin.so", "tap_set_carrier");
+  if (!fn)
+    return -1;
+  return fn (hw_if_index, carrier_up);
+}
+
+static inline int
+tap_set_speed (u32 hw_if_index, u32 speed)
+{
+  tap_set_speed_fn_t *fn = (tap_set_speed_fn_t *) vlib_get_plugin_symbol (
+    "tap_plugin.so", "tap_set_speed");
+  if (!fn)
+    return -1;
+  return fn (hw_if_index, speed);
+}
+
+static inline unsigned int
+tap_get_ifindex (vlib_main_t *vm, u32 sw_if_index)
+{
+  tap_get_ifindex_fn_t *fn = (tap_get_ifindex_fn_t *) vlib_get_plugin_symbol (
+    "tap_plugin.so", "tap_get_ifindex");
+  if (!fn)
+    return ~0;
+  return fn (vm, sw_if_index);
+}
+
+static inline int
+tap_is_tun (vlib_main_t *vm, u32 sw_if_index)
+{
+  tap_is_tun_fn_t *fn =
+    (tap_is_tun_fn_t *) vlib_get_plugin_symbol ("tap_plugin.so", "tap_is_tun");
+  if (!fn)
+    return 0;
+  return fn (vm, sw_if_index);
+}
+
+#endif /* TAP_PLUGIN_INTERNAL */

src/vnet/devices/tap/tap.h
@@ -1,129 +1,0 @@
-/*
- *------------------------------------------------------------------
- * Copyright (c) 2017 Cisco and/or its affiliates.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *------------------------------------------------------------------
- */
-
-#ifndef _VNET_DEVICES_VIRTIO_TAP_H_
-#define _VNET_DEVICES_VIRTIO_TAP_H_
-
-#ifndef MIN
-#define MIN(x,y) (((x)<(y))?(x):(y))
-#endif
-
-#define foreach_tapv2_flags                                                   \
-  _ (GSO, 0)                                                                  \
-  _ (CSUM_OFFLOAD, 1)                                                         \
-  _ (PERSIST, 2)                                                              \
-  _ (ATTACH, 3)                                                               \
-  _ (TUN, 4)                                                                  \
-  _ (GRO_COALESCE, 5)                                                         \
-  _ (PACKED, 6)                                                               \
-  _ (IN_ORDER, 7)                                                             \
-  _ (CONSISTENT_QP, 8)
-
-typedef enum
-{
-#define _(a, b) TAP_FLAG_##a = (1 << b),
-  foreach_tapv2_flags
-#undef _
-} tap_flag_t;
-
-typedef struct
-{
-  u32 id;
-  u32 auto_id_offset;
-  u8 mac_addr_set;
-  mac_address_t mac_addr;
-  u16 num_rx_queues;
-  u16 num_tx_queues;
-  u16 rx_ring_sz;
-  u16 tx_ring_sz;
-  u32 tap_flags;
-  u8 *host_namespace;
-  u8 *if_name;
-  u8 *host_if_name;
-  mac_address_t host_mac_addr;
-  u8 *host_bridge;
-  ip4_address_t host_ip4_addr;
-  u8 host_ip4_prefix_len;
-  ip4_address_t host_ip4_gw;
-  u8 host_ip4_gw_set;
-  ip6_address_t host_ip6_addr;
-  u8 host_ip6_prefix_len;
-  ip6_address_t host_ip6_gw;
-  u8 host_ip6_gw_set;
-  u8 host_mtu_set;
-  u32 host_mtu_size;
-
-  /* return */
-  u32 sw_if_index;
-  int rv;
-  clib_error_t *error;
-} tap_create_if_args_t;
-
-/** TAP interface details struct */
-typedef struct
-{
-  u32 id;
-  u32 sw_if_index;
-  u32 tap_flags;
-  u8 dev_name[64];
-  u16 tx_ring_sz;
-  u16 rx_ring_sz;
-  mac_address_t host_mac_addr;
-  u8 host_if_name[64];
-  u8 host_namespace[64];
-  u8 host_bridge[64];
-  ip4_address_t host_ip4_addr;
-  u8 host_ip4_prefix_len;
-  ip6_address_t host_ip6_addr;
-  u8 host_ip6_prefix_len;
-  u32 host_mtu_size;
-} tap_interface_details_t;
-
-typedef struct
-{
-  /* logging */
-  vlib_log_class_t log_default;
-
-  /* bit-map of in-use IDs */
-  uword *tap_ids;
-
-  /* host mtu size, configurable through startup.conf */
-  int host_mtu_size;
-  u16 msg_id_base;
-} tap_main_t;
-
-extern tap_main_t tap_main;
-void tap_create_if (vlib_main_t * vm, tap_create_if_args_t * args);
-int tap_delete_if (vlib_main_t * vm, u32 sw_if_index);
-int tap_gso_enable_disable (vlib_main_t * vm, u32 sw_if_index,
-			    int enable_disable, int packet_coalesce);
-int
-tap_csum_offload_enable_disable (vlib_main_t * vm, u32 sw_if_index,
-				 int enable_disable);
-int tap_dump_ifs (tap_interface_details_t ** out_tapids);
-int tap_set_carrier (u32 hw_if_index, u32 carrier_up);
-int tap_set_speed (u32 hw_if_index, u32 speed);
-
-#endif /* _VNET_DEVICES_VIRTIO_TAP_H_ */
-
-/*
- * fd.io coding-style-patch-verification: ON
- *
- * Local Variables:
- * eval: (c-set-style "gnu")
- * End:
- */

MAINTAINERS
@@ -117,11 +117,11 @@ F:	src/plugins/af_packet
 
 VNET TAP Drivers
 I:	tap
-Y:	src/vnet/devices/tap/FEATURE.yaml
+Y:	src/plugins/tap/FEATURE.yaml
 M:	Damjan Marion <damarion@cisco.com>
 M:	Steven Luong <sluong@cisco.com>
 M:	Mohsin Kazmi <mohsin.kazmi14@gmail.com>
-F:	src/vnet/devices/tap/
+F:	src/plugins/tap/
 
 VNET Vhost User Driver
 I:	vhost

extras/kube-test/docker/Dockerfile.vpp
@@ -25,6 +25,7 @@ COPY \
    $DIR/tlsopenssl_plugin.so \
    $DIR/mactime_plugin.so \
    $DIR/arping_plugin.so \
+   $DIR/tap_plugin.so \
    /usr/lib/$OS_ARCH-linux-gnu/vpp_plugins/
 
 COPY vpp-data/bin/vpp /usr/bin/

src/plugins/linux-cp/lcp_interface.c
@@ -33,8 +33,7 @@
 #include <vnet/adj/adj_mcast.h>
 #include <vnet/udp/udp.h>
 #include <vnet/tcp/tcp.h>
-#include <vnet/devices/tap/tap.h>
-#include <vnet/devices/virtio/virtio.h>
+#include <tap/tap.h>
 #include <vnet/devices/netlink.h>
 #include <vlibapi/api_helper_macros.h>
 #include <vnet/ipsec/ipsec_punt.h>
@@ -1023,16 +1022,13 @@ lcp_itf_pair_create (u32 phy_sw_if_index, u8 *host_if_name,
 	.rx_ring_sz = 256,
 	.tx_ring_sz = 256,
 	.host_if_name = host_if_name,
-	.host_namespace = 0,
-	.rv = 0,
-	.error = NULL,
+	.tap_flags = host_if_type == LCP_ITF_HOST_TUN ? TAP_FLAG_TUN : 0,
       };
       ethernet_interface_t *ei;
       u32 host_sw_mtu_size;
 
-      if (host_if_type == LCP_ITF_HOST_TUN)
-	args.tap_flags |= TAP_FLAG_TUN;
-      else
+      /* align with public TAP flag bit for TUN */
+      if (host_if_type != LCP_ITF_HOST_TUN)
 	{
 	  ei = pool_elt_at_index (ethernet_main.interfaces, hw->hw_instance);
 	  mac_address_copy (&args.host_mac_addr, &ei->address.mac);
@@ -1071,13 +1067,6 @@ lcp_itf_pair_create (u32 phy_sw_if_index, u8 *host_if_name,
       vnet_sw_interface_set_mtu (vnm, args.sw_if_index, host_sw_mtu_size);
 
       /*
-       * get the hw and ethernet of the tap
-       */
-      hw = vnet_get_sup_hw_interface (vnm, args.sw_if_index);
-      virtio_main_t *mm = &virtio_main;
-      virtio_if_t *vif = pool_elt_at_index (mm->interfaces, hw->dev_instance);
-
-      /*
        * Leave the TAP permanently up on the VPP side.
        * This TAP will be shared by many sub-interface.
        * Therefore we can't use it to manage admin state.
@@ -1089,7 +1078,7 @@ lcp_itf_pair_create (u32 phy_sw_if_index, u8 *host_if_name,
 	  ei->flags |= ETHERNET_INTERFACE_FLAG_STATUS_L3;
 	}
 
-      vif_index = vif->ifindex;
+      vif_index = tap_get_ifindex (vm, args.sw_if_index);
       host_sw_if_index = args.sw_if_index;
     }
 

src/plugins/tap/CMakeLists.txt
@@ -1,0 +1,28 @@
+# SPDX-License-Identifier: Apache-2.0
+
+add_compile_options(-Wmissing-prototypes)
+add_compile_options(-Wmissing-declarations)
+
+add_vpp_plugin(tap
+  SOURCES
+    plugin.c
+    cli.c
+    tap.c
+    tap_pre_input.c
+    format.c
+    tx_node.c
+    rx_node.c
+    tapv2_api.c
+
+  MULTIARCH_SOURCES
+    rx_node.c
+    tx_node.c
+
+  INSTALL_HEADERS
+    tap.h
+
+  API_FILES
+    tapv2.api
+
+  SUPPORTED_OS_LIST Linux
+)

src/plugins/tap/cli.c
@@ -1,18 +1,6 @@
 /*
- *------------------------------------------------------------------
- * Copyright (c) 2016 Cisco and/or its affiliates.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *------------------------------------------------------------------
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2016-2025 Cisco and/or its affiliates.
  */
 #include <stdint.h>
 #include <net/if.h>
@@ -25,12 +13,140 @@
 #include <vnet/ip/ip4_packet.h>
 #include <vnet/ip/ip6_packet.h>
 #include <vnet/ip/format.h>
-#include <vnet/devices/virtio/virtio.h>
-#include <vnet/devices/tap/tap.h>
+#include <tap/internal.h>
 
+static void
+tap_show (vlib_main_t *vm, u32 *hw_if_indices, u8 show_descr, int is_tun)
+{
+  u32 i, j, hw_if_index;
+  tap_if_t *tif;
+  vnet_main_t *vnm = &vnet_main;
+  tap_main_t *tm = &tap_main;
+  tap_txq_t *txq;
+  tap_rxq_t *rxq;
+
+  if (!hw_if_indices)
+    return;
+
+  for (hw_if_index = 0; hw_if_index < vec_len (hw_if_indices); hw_if_index++)
+    {
+      vnet_hw_interface_t *hi =
+	vnet_get_hw_interface (vnm, hw_if_indices[hw_if_index]);
+      tif = pool_elt_at_index (tm->interfaces, hi->dev_instance);
+      vlib_cli_output (vm, "Interface: %U (ifindex %d)",
+		       format_vnet_hw_if_index_name, vnm,
+		       hw_if_indices[hw_if_index], tif->hw_if_index);
+      u8 *str = 0;
+      if (tif->host_if_name)
+	vlib_cli_output (vm, "  name \"%s\"", tif->host_if_name);
+      if (tif->net_ns)
+	vlib_cli_output (vm, "  host-ns \"%s\"", tif->net_ns);
+      if (tif->host_mtu_size)
+	vlib_cli_output (vm, "  host-mtu-size \"%d\"", tif->host_mtu_size);
+      if (!is_tun)
+	vlib_cli_output (vm, "  host-mac-addr: %U", format_ethernet_address,
+			 tif->host_mac_addr);
+      vlib_cli_output (vm, "  host-carrier-up: %u", tif->host_carrier_up);
+
+      vec_foreach_index (i, tif->vhost_fds)
+	str = format (str, " %d", tif->vhost_fds[i]);
+      vlib_cli_output (vm, "  vhost-fds%v", str);
+      vec_free (str);
+      vec_foreach_index (i, tif->tap_fds)
+	str = format (str, " %d", tif->tap_fds[i]);
+      vlib_cli_output (vm, "  tap-fds%v", str);
+      vec_free (str);
+
+      vlib_cli_output (vm, "  gso-enabled %d", tif->gso_enabled);
+      vlib_cli_output (vm, "  csum-enabled %d", tif->csum_offload_enabled);
+      vlib_cli_output (vm, "  packet-coalesce %d", tif->packet_coalesce);
+      if (!is_tun)
+	vlib_cli_output (vm, "  mac Address: %U", format_ethernet_address,
+			 tif->mac_addr);
+      vlib_cli_output (vm, "  device instance: %u", tif->dev_instance);
+      vlib_cli_output (vm, "  admin state: %s", tif->admin_up ? "up" : "down");
+      vlib_cli_output (vm, "  features 0x%lx\n     %U", tif->features,
+		       format_virtio_features, tif->features);
+      vlib_cli_output (vm, "  remote-features 0x%lx\n    %U",
+		       tif->remote_features, format_virtio_features,
+		       tif->remote_features);
+      vlib_cli_output (vm, "  Number of RX Virtqueue  %u",
+		       vec_len (tif->rx_queues));
+      vlib_cli_output (vm, "  Number of TX Virtqueue  %u",
+		       vec_len (tif->tx_queues));
+      vec_foreach_index (i, tif->rx_queues)
+	{
+	  rxq = tap_get_rx_queue (tif, i);
+	  vlib_cli_output (vm, "  Virtqueue (RX) %d", rxq->queue_id);
+	  vlib_cli_output (
+	    vm, "    qsz %d, last_used_idx %d, desc_next %d, desc_in_use %d",
+	    rxq->queue_size, rxq->last_used_idx, rxq->desc_next,
+	    rxq->desc_in_use);
+	  vlib_cli_output (
+	    vm,
+	    "    avail.flags 0x%x avail.idx %d used.flags 0x%x used.idx %d",
+	    rxq->avail->flags, rxq->avail->idx, rxq->used->flags,
+	    rxq->used->idx);
+	  vlib_cli_output (vm, "    kickfd %d, callfd %d", rxq->kick_fd,
+			   rxq->call_fd);
+	  if (show_descr)
+	    {
+	      vlib_cli_output (vm, "\n  descriptor table:\n");
+	      vlib_cli_output (vm, "   id          addr         len  flags  "
+				   "next/id      user_addr\n");
+	      vlib_cli_output (vm, "  ===== ================== ===== ====== "
+				   "======= ==================\n");
+	      for (j = 0; j < rxq->queue_size; j++)
+		{
+		  vnet_virtio_vring_desc_t *desc = &rxq->desc[j];
+		  vlib_cli_output (
+		    vm, "  %-5d 0x%016lx %-5d 0x%04x %-8d 0x%016lx\n", j,
+		    desc->addr, desc->len, desc->flags, desc->next,
+		    desc->addr);
+		}
+	    }
+	}
+      vec_foreach_index (i, tif->tx_queues)
+	{
+	  txq = tap_get_tx_queue (tif, i);
+	  vlib_cli_output (vm, "  Virtqueue (TX) %d", txq->queue_id);
+	  vlib_cli_output (
+	    vm, "    qsz %d, last_used_idx %d, desc_next %d, desc_in_use %d",
+	    txq->queue_size, txq->last_used_idx, txq->desc_next,
+	    txq->desc_in_use);
+	  vlib_cli_output (
+	    vm,
+	    "    avail.flags 0x%x avail.idx %d used.flags 0x%x used.idx %d",
+	    txq->avail->flags, txq->avail->idx, txq->used->flags,
+	    txq->used->idx);
+	  vlib_cli_output (vm, "    kickfd %d", txq->kick_fd);
+	  if (txq->flow_table)
+	    {
+	      vlib_cli_output (vm, "    %U", gro_flow_table_format,
+			       txq->flow_table);
+	    }
+	  if (show_descr)
+	    {
+	      vlib_cli_output (vm, "\n  descriptor table:\n");
+	      vlib_cli_output (vm, "   id          addr         len  flags  "
+				   "next/id      user_addr\n");
+	      vlib_cli_output (vm, "  ===== ================== ===== ====== "
+				   "======== ==================\n");
+	      for (j = 0; j < txq->queue_size; j++)
+		{
+		  vnet_virtio_vring_desc_t *desc = &txq->desc[j];
+		  vlib_cli_output (
+		    vm, "  %-5d 0x%016lx %-5d 0x%04x %-8d 0x%016lx\n", j,
+		    desc->addr, desc->len, desc->flags, desc->next,
+		    desc->addr);
+		}
+	    }
+	}
+    }
+}
 static clib_error_t *
-tap_create_command_fn (vlib_main_t * vm, unformat_input_t * input,
-		       vlib_cli_command_t * cmd)
+tap_create_command_fn (vlib_main_t *vm, unformat_input_t *input,
+		       vlib_cli_command_t *cmd)
 {
   unformat_input_t _line_input, *line_input = &_line_input;
   tap_create_if_args_t args = { 0 };
@@ -52,8 +168,8 @@ tap_create_command_fn (vlib_main_t * vm, unformat_input_t * input,
 	    ;
 	  else if (unformat (line_input, "if-name %s", &args.if_name))
 	    ;
-	  else
-	    if (unformat (line_input, "host-if-name %s", &args.host_if_name))
+	  else if (unformat (line_input, "host-if-name %s",
+			     &args.host_if_name))
 	    ;
 	  else if (unformat (line_input, "host-ns %s", &args.host_namespace))
 	    ;
@@ -85,9 +201,8 @@ tap_create_command_fn (vlib_main_t * vm, unformat_input_t * input,
 	    args.rx_ring_sz = tmp;
 	  else if (unformat (line_input, "tx-ring-size %d", &tmp))
 	    args.tx_ring_sz = tmp;
-	  else
-	    if (unformat
-		(line_input, "host-mtu-size %d", &args.host_mtu_size))
+	  else if (unformat (line_input, "host-mtu-size %d",
+			     &args.host_mtu_size))
 	    args.host_mtu_set = 1;
 	  else if (unformat (line_input, "no-gso"))
 	    args.tap_flags &= ~TAP_FLAG_GSO;
@@ -124,7 +239,7 @@ tap_create_command_fn (vlib_main_t * vm, unformat_input_t * input,
 
   if (ip_addr_set && args.host_bridge)
     return clib_error_return (0, "Please specify either host ip address or "
-			      "host bridge");
+				 "host bridge");
 
   tap_create_if (vm, &args);
 
@@ -138,7 +253,6 @@ tap_create_command_fn (vlib_main_t * vm, unformat_input_t * input,
   vec_free (args.host_bridge);
 
   return args.error;
-
 }
 
 VLIB_CLI_COMMAND (tap_create_command, static) = {
@@ -156,8 +270,8 @@ VLIB_CLI_COMMAND (tap_create_command, static) = {
 };
 
 static clib_error_t *
-tap_delete_command_fn (vlib_main_t * vm, unformat_input_t * input,
-		       vlib_cli_command_t * cmd)
+tap_delete_command_fn (vlib_main_t *vm, unformat_input_t *input,
+		       vlib_cli_command_t *cmd)
 {
   unformat_input_t _line_input, *line_input = &_line_input;
   u32 sw_if_index = ~0;
@@ -172,8 +286,8 @@ tap_delete_command_fn (vlib_main_t * vm, unformat_input_t * input,
     {
       if (unformat (line_input, "sw_if_index %d", &sw_if_index))
 	;
-      else if (unformat (line_input, "%U", unformat_vnet_sw_interface,
-			 vnm, &sw_if_index))
+      else if (unformat (line_input, "%U", unformat_vnet_sw_interface, vnm,
+			 &sw_if_index))
 	;
       else
 	return clib_error_return (0, "unknown input `%U'",
@@ -194,89 +308,18 @@ tap_delete_command_fn (vlib_main_t * vm, unformat_input_t * input,
   return 0;
 }
 
-VLIB_CLI_COMMAND (tap_delete__command, static) =
-{
+VLIB_CLI_COMMAND (tap_delete__command, static) = {
   .path = "delete tap",
   .short_help = "delete tap {<interface> | sw_if_index <sw_idx>}",
   .function = tap_delete_command_fn,
 };
 
 static clib_error_t *
-tap_offload_command_fn (vlib_main_t * vm, unformat_input_t * input,
-			vlib_cli_command_t * cmd)
+tap_show_command_fn (vlib_main_t *vm, unformat_input_t *input,
+		     vlib_cli_command_t *cmd)
 {
-  unformat_input_t _line_input, *line_input = &_line_input;
-  u32 sw_if_index = ~0;
-  vnet_main_t *vnm = vnet_get_main ();
-  int gso_enable = 0, gso_disable = 0, is_gro_coalesce = 0;
-  int csum_offload_enable = 0, csum_offload_disable = 0;
-  int rv = 0;
-
-  /* Get a line of input. */
-  if (!unformat_user (input, unformat_line_input, line_input))
-    return clib_error_return (0, "Missing <interface>");
-
-  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
-    {
-      if (unformat (line_input, "sw_if_index %d", &sw_if_index))
-	;
-      else if (unformat (line_input, "%U", unformat_vnet_sw_interface,
-			 vnm, &sw_if_index))
-	;
-      else if (unformat (line_input, "gso-enable"))
-	{
-	  gso_enable = 1;
-	  if (unformat (line_input, "gro-coalesce"))
-	    is_gro_coalesce = 1;
-	}
-      else if (unformat (line_input, "gso-disable"))
-	gso_disable = 1;
-      else if (unformat (line_input, "csum-offload-enable"))
-	csum_offload_enable = 1;
-      else if (unformat (line_input, "csum-offload-disable"))
-	csum_offload_disable = 1;
-      else
-	return clib_error_return (0, "unknown input `%U'",
-				  format_unformat_error, input);
-    }
-  unformat_free (line_input);
-
-  if (sw_if_index == ~0)
-    return clib_error_return (0,
-			      "please specify interface name or sw_if_index");
-
-  if (gso_enable)
-    rv = tap_gso_enable_disable (vm, sw_if_index, 1, is_gro_coalesce);
-  else if (csum_offload_enable)
-    rv = tap_csum_offload_enable_disable (vm, sw_if_index, 1);
-  else if (gso_disable)
-    rv = tap_gso_enable_disable (vm, sw_if_index, 0, 0);
-  else if (csum_offload_disable)
-    rv = tap_csum_offload_enable_disable (vm, sw_if_index, 0);
-
-  if (rv == VNET_API_ERROR_INVALID_SW_IF_INDEX)
-    return clib_error_return (0, "not a tap interface");
-  else if (rv != 0)
-    return clib_error_return (0, "error on configuring GSO on tap interface");
-
-  return 0;
-}
-
-VLIB_CLI_COMMAND (tap_offload_command, static) =
-{
-  .path = "set tap offload",
-  .short_help = "set tap offload {<interface> | sw_if_index <sw_idx>}"
-    " <gso-enable [gro-coalesce]  | gso-disable | csum-offload-enable |"
-    "csum-offload-disable>",
-  .function = tap_offload_command_fn,
-};
-
-static clib_error_t *
-tap_show_command_fn (vlib_main_t * vm, unformat_input_t * input,
-		     vlib_cli_command_t * cmd)
-{
-  virtio_main_t *mm = &virtio_main;
-  virtio_if_t *vif;
+  tap_main_t *tm = &tap_main;
+  tap_if_t *tif;
   vnet_main_t *vnm = vnet_get_main ();
   int show_descr = 0;
   clib_error_t *error = 0;
@@ -284,8 +327,8 @@ tap_show_command_fn (vlib_main_t * vm, unformat_input_t * input,
 
   while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
     {
-      if (unformat
-	  (input, "%U", unformat_vnet_hw_interface, vnm, &hw_if_index))
+      if (unformat (input, "%U", unformat_vnet_hw_interface, vnm,
+		    &hw_if_index))
 	vec_add1 (hw_if_indices, hw_if_index);
       else if (unformat (input, "descriptors"))
 	show_descr = 1;
@@ -299,11 +342,11 @@ tap_show_command_fn (vlib_main_t * vm, unformat_input_t * input,
 
   if (vec_len (hw_if_indices) == 0)
     {
-      pool_foreach (vif, mm->interfaces)
-	  vec_add1 (hw_if_indices, vif->hw_if_index);
+      pool_foreach (tif, tm->interfaces)
+	vec_add1 (hw_if_indices, tif->hw_if_index);
     }
 
-  virtio_show (vm, hw_if_indices, show_descr, VIRTIO_IF_TYPE_TAP);
+  tap_show (vm, hw_if_indices, show_descr, 0);
 
 done:
   vec_free (hw_if_indices);
@@ -317,11 +360,11 @@ VLIB_CLI_COMMAND (tap_show_command, static) = {
 };
 
 static clib_error_t *
-tun_show_command_fn (vlib_main_t * vm, unformat_input_t * input,
-		     vlib_cli_command_t * cmd)
+tun_show_command_fn (vlib_main_t *vm, unformat_input_t *input,
+		     vlib_cli_command_t *cmd)
 {
-  virtio_main_t *mm = &virtio_main;
-  virtio_if_t *vif;
+  tap_main_t *tm = &tap_main;
+  tap_if_t *tif;
   vnet_main_t *vnm = vnet_get_main ();
   int show_descr = 0;
   clib_error_t *error = 0;
@@ -329,8 +372,8 @@ tun_show_command_fn (vlib_main_t * vm, unformat_input_t * input,
 
   while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
     {
-      if (unformat
-	  (input, "%U", unformat_vnet_hw_interface, vnm, &hw_if_index))
+      if (unformat (input, "%U", unformat_vnet_hw_interface, vnm,
+		    &hw_if_index))
 	vec_add1 (hw_if_indices, hw_if_index);
       else if (unformat (input, "descriptors"))
 	show_descr = 1;
@@ -344,11 +387,11 @@ tun_show_command_fn (vlib_main_t * vm, unformat_input_t * input,
 
   if (vec_len (hw_if_indices) == 0)
     {
-      pool_foreach (vif, mm->interfaces)
-          vec_add1 (hw_if_indices, vif->hw_if_index);
+      pool_foreach (tif, tm->interfaces)
+	vec_add1 (hw_if_indices, tif->hw_if_index);
     }
 
-  virtio_show (vm, hw_if_indices, show_descr, VIRTIO_IF_TYPE_TUN);
+  tap_show (vm, hw_if_indices, show_descr, 1);
 
 done:
   vec_free (hw_if_indices);
@@ -361,18 +404,10 @@ VLIB_CLI_COMMAND (tun_show_command, static) = {
   .function = tun_show_command_fn,
 };
 
-clib_error_t *
-tap_cli_init (vlib_main_t * vm)
+static clib_error_t *
+tap_cli_init (vlib_main_t *vm)
 {
   return 0;
 }
 
 VLIB_INIT_FUNCTION (tap_cli_init);
-
-/*
- * fd.io coding-style-patch-verification: ON
- *
- * Local Variables:
- * eval: (c-set-style "gnu")
- * End:
- */

commit fcf78a8af2b5bede5070979e1c759341db96ba3b

tap: set rv during creation when plugin not loaded
Type: fix
Fixes: cb19c526dd

When the inline function tap_create_if() fails to resolve the symbol
tap_create_if in tap_plugin.so, it sets args->error but not args->rv.

The only external caller of the function has always determined whether
the call succeeded by checking for a negative value of rv. So when the
tap plugin is not loaded, the calling function lcp_itf_pair_create()
falsely believes that tap_create_if() succeeded and proceeds to pass
args.sw_if_index to other calls, which results in a seg fault.

All other cases where args->error is set also have args->rv set to some
vnet error code. Set rv to VNET_API_ERROR_FEATURE_DISABLED in this case.

Signed-off-by: Matthew Smith <mgsmith@netgate.com>
Change-Id: I29a841f539b189ff3c9b1568b5c7733f6f259e21

src/plugins/tap/tap.h
@@ -89,6 +89,7 @@ tap_create_if (vlib_main_t *vm, tap_create_if_args_t *args)
     "tap_plugin.so", "tap_create_if");
   if (!fn)
     {
+      args->rv = VNET_API_ERROR_FEATURE_DISABLED;
       args->error = clib_error_return (0, "tap plugin not loaded");
       return;
     }