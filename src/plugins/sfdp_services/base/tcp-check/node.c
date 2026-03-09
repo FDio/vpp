FDio/vpp

commit 3aea68b574d5ba62a2bd26cd940ef1b6cc47cfb5

sfdp_services: add basic support for time-wait
+ Add fifth timeout value to vnet/sfdp/timer,
leaving only 3 slots available for custom timeouts.

- Do not add further logic to tcp-check
for example for FIN seen when in TIME_WAIT.

Type: feature
Ticket: vpp-3693

Change-Id: I23b411b9b1295a7e86021e10b5e26567619a13af
Signed-off-by: Vratko Polak <vrpolak@cisco.com>

src/plugins/sfdp_services/base/tcp-check/node.c
@@ -96,6 +96,7 @@ update_state_one_pkt (sfdp_tw_t *tw, sfdp_tenant_t *tenant,
   u32 seqnum = clib_net_to_host_u32 (tcph->seq_number);
   u32 next_timeout = 0;
   u8 remove_session = 0;
+  u8 time_wait = 0;
   if (PREDICT_FALSE (tcp_session->version != session->session_version))
     {
       tcp_session->version = session->session_version;
@@ -201,13 +202,16 @@ update_state_one_pkt (sfdp_tw_t *tw, sfdp_tenant_t *tenant,
   if ((nsf[0] & (SFDP_TCP_CHECK_SESSION_FLAG_SEEN_ACK_TO_FIN_INIT)) &&
       (nsf[0] & SFDP_TCP_CHECK_SESSION_FLAG_SEEN_ACK_TO_FIN_RESP))
     {
-      nsf[0] = SFDP_TCP_CHECK_SESSION_FLAG_REMOVING;
-      remove_session = 1;
+      nsf[0] = SFDP_TCP_CHECK_SESSION_FLAG_TIME_WAIT;
+      session->state = SFDP_SESSION_STATE_TIME_WAIT;
+      time_wait = 1;
     }
 out:
   tcp_session->flags = nsf[0];
   if (remove_session)
     next_timeout = 0;
+  else if (time_wait)
+    next_timeout = tenant->timeouts[SFDP_TIMEOUT_TIME_WAIT];
   else if (nsf[0] & SFDP_TCP_CHECK_SESSION_FLAG_ESTABLISHED)
     next_timeout = tenant->timeouts[SFDP_TIMEOUT_TCP_ESTABLISHED];
   else if (nsf[0] & SFDP_TCP_CHECK_SESSION_FLAG_BLOCKED)

src/plugins/sfdp_services/base/tcp-check/tcp_check.api
@@ -18,6 +18,7 @@ enumflag sfdp_tcp_check_session_flags : u32
   SFDP_API_TCP_CHECK_SESSION_FLAG_ESTABLISHED = 0x80,
   SFDP_API_TCP_CHECK_SESSION_FLAG_REMOVING = 0x100,
   SFDP_API_TCP_CHECK_SESSION_FLAG_BLOCKED = 0x200,
+  SFDP_API_TCP_CHECK_SESSION_FLAG_TIME_WAIT = 0x400,
 };
 
 define sfdp_tcp_session_dump

src/plugins/sfdp_services/base/tcp-check/tcp_check.h
@@ -8,17 +8,18 @@
 #include <vlib/vlib.h>
 #include <vnet/sfdp/sfdp.h>
 /* Convention: uppercase relates to responder lowercase to initiator */
-#define foreach_sfdp_tcp_check_session_flag                                   \
-  _ (WAIT_FOR_RESP_SYN, 0, "S")                                               \
-  _ (WAIT_FOR_INIT_ACK_TO_SYN, 1, "a")                                        \
-  _ (WAIT_FOR_RESP_ACK_TO_SYN, 2, "A")                                        \
-  _ (SEEN_FIN_INIT, 3, "f")                                                   \
-  _ (SEEN_FIN_RESP, 4, "F")                                                   \
-  _ (SEEN_ACK_TO_FIN_INIT, 5, "r")                                            \
-  _ (SEEN_ACK_TO_FIN_RESP, 6, "R")                                            \
-  _ (ESTABLISHED, 7, "U")                                                     \
-  _ (REMOVING, 8, "D")                                                        \
-  _ (BLOCKED, 9, "X")
+#define foreach_sfdp_tcp_check_session_flag                                                        \
+  _ (WAIT_FOR_RESP_SYN, 0, "S")                                                                    \
+  _ (WAIT_FOR_INIT_ACK_TO_SYN, 1, "a")                                                             \
+  _ (WAIT_FOR_RESP_ACK_TO_SYN, 2, "A")                                                             \
+  _ (SEEN_FIN_INIT, 3, "f")                                                                        \
+  _ (SEEN_FIN_RESP, 4, "F")                                                                        \
+  _ (SEEN_ACK_TO_FIN_INIT, 5, "r")                                                                 \
+  _ (SEEN_ACK_TO_FIN_RESP, 6, "R")                                                                 \
+  _ (ESTABLISHED, 7, "U")                                                                          \
+  _ (REMOVING, 8, "D")                                                                             \
+  _ (BLOCKED, 9, "X")                                                                              \
+  _ (TIME_WAIT, 10, "T")
 
 typedef enum
 {

src/vnet/sfdp/timer/timer.h
@@ -33,11 +33,12 @@ typedef struct
   u32 __unused;
 } __attribute__ ((may_alias)) sfdp_session_timer_t;
 
-#define foreach_sfdp_timeout                                                  \
-  _ (EMBRYONIC, 5, "embryonic")                                               \
-  _ (ESTABLISHED, 120, "established")                                         \
-  _ (TCP_ESTABLISHED, 3600, "tcp-established")                                \
-  _ (SECURITY, 30, "security")
+#define foreach_sfdp_timeout                                                                       \
+  _ (EMBRYONIC, 5, "embryonic")                                                                    \
+  _ (ESTABLISHED, 120, "established")                                                              \
+  _ (TCP_ESTABLISHED, 3600, "tcp-established")                                                     \
+  _ (SECURITY, 30, "security")                                                                     \
+  _ (TIME_WAIT, 120, "time-wait")
 
 typedef enum
 {

commit d557b77cccaeb86874074e3aac37a11c8a6a996a

sfdp_services: handle new corner case in tcp-check
Type: fix
Fixes: 3aea68b574d5ba62a2bd26cd940ef1b6cc47cfb5
Ticket: vpp-3693

Change-Id: I3fef31de2e2d1bcd1440f9010ceec3d7a3e85853
Signed-off-by: Vratko Polak <vrpolak@cisco.com>

src/plugins/sfdp_services/base/tcp-check/node.c
@@ -119,7 +119,7 @@ update_state_one_pkt (sfdp_tw_t *tw, sfdp_tenant_t *tenant,
       if (flags & SFDP_TCP_CHECK_TCP_FLAGS_SYN)
 	{
 	  /* New session, must be a SYN otherwise bad */
-	  if (sf[0] == 0)
+	  if (sf[0] == 0 || sf[0] & SFDP_TCP_CHECK_SESSION_FLAG_TIME_WAIT)
 	    nsf[0] = SFDP_TCP_CHECK_SESSION_FLAG_WAIT_FOR_RESP_SYN |
 		     SFDP_TCP_CHECK_SESSION_FLAG_WAIT_FOR_RESP_ACK_TO_SYN;
 	  else
@@ -141,11 +141,13 @@ update_state_one_pkt (sfdp_tw_t *tw, sfdp_tenant_t *tenant,
 	}
       if (flags & SFDP_TCP_CHECK_TCP_FLAGS_FIN)
 	{
-	  /*If we were up, we are not anymore */
+	  /* If we were established or time-wait, we are not anymore */
 	  nsf[0] &= ~SFDP_TCP_CHECK_SESSION_FLAG_ESTABLISHED;
+	  nsf[0] &= ~SFDP_TCP_CHECK_SESSION_FLAG_TIME_WAIT;
 	  /*Seen our FIN, wait for the other FIN and for an ACK*/
 	  tcp_session->fin_num[SFDP_FLOW_FORWARD] = seqnum + 1;
 	  nsf[0] |= SFDP_TCP_CHECK_SESSION_FLAG_SEEN_FIN_INIT;
+	  nsf[0] &= ~SFDP_TCP_CHECK_SESSION_FLAG_SEEN_ACK_TO_FIN_INIT;
 	}
       if (flags & SFDP_TCP_CHECK_TCP_FLAGS_RST)
 	{
@@ -177,11 +179,13 @@ update_state_one_pkt (sfdp_tw_t *tw, sfdp_tenant_t *tenant,
 	}
       if (flags & SFDP_TCP_CHECK_TCP_FLAGS_FIN)
 	{
-	  /*If we were up, we are not anymore */
+	  /* If we were established or time-wait, we are not anymore */
 	  nsf[0] &= ~SFDP_TCP_CHECK_SESSION_FLAG_ESTABLISHED;
+	  nsf[0] &= ~SFDP_TCP_CHECK_SESSION_FLAG_TIME_WAIT;
 	  /* Seen our FIN, wait for the other FIN and for an ACK */
 	  tcp_session->fin_num[SFDP_FLOW_REVERSE] = seqnum + 1;
 	  nsf[0] |= SFDP_TCP_CHECK_SESSION_FLAG_SEEN_FIN_RESP;
+	  nsf[0] &= ~SFDP_TCP_CHECK_SESSION_FLAG_SEEN_ACK_TO_FIN_RESP;
 	}
       if (flags & SFDP_TCP_CHECK_TCP_FLAGS_RST)
 	{
@@ -218,6 +222,7 @@ out:
     next_timeout = tenant->timeouts[SFDP_TIMEOUT_SECURITY];
   else
     next_timeout = tenant->timeouts[SFDP_TIMEOUT_EMBRYONIC];
+  /* TODO: Distinguish FIN-WAIT states in the last branch. */
 
   sfdp_session_timer_update_maybe_past (tw, SFDP_SESSION_TIMER (session),
 					current_time, next_timeout);