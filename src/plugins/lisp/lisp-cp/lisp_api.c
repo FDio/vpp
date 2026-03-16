7fdd034d9d0d67b59ad343d33ad6515757f8bae6
Florin Coras <fcoras@cisco.com>
2026-03-16 17:04:34 -0400

    lisp: cap hmac key copy length in eid table dump
    Type: fix
    
    Change-Id: Id1799477d18a5b0c1b305549f0b955bc1dbf523c
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/plugins/lisp/lisp-cp/lisp_api.c
+++ src/plugins/lisp/lisp-cp/lisp_api.c
@@ -657,7 +657,7 @@ send_lisp_eid_table_details (mapping_t * mapit,
   rmp->context = context;
   rmp->vni = clib_host_to_net_u32 (gid_address_vni (gid));
   rmp->key.id = clib_host_to_net_u16 (mapit->key_id);
-  memcpy (rmp->key.key, mapit->key, vec_len (mapit->key));
+  memcpy (rmp->key.key, mapit->key, clib_min (vec_len (mapit->key), sizeof (rmp->key.key)));
   vl_api_send_msg (reg, (u8 *) rmp);
 }
 
