a898b7b15f71a54aa2fc1e8de0ca05859afac8df
Florin Coras <fcoras@cisco.com>
2026-04-03 01:30:19 -0400

    quic: handle zero timeout for sessions in hs state
    Don't try to read session if still in handshake state.
    
    Type: fix
    
    Change-Id: I2b3a491659b579b10921475ef8d2398f9140939f
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/plugins/quic/quic_timer.h
+++ src/plugins/quic/quic_timer.h
@@ -44,7 +44,8 @@ quic_update_timer (quic_worker_ctx_t *wc, quic_ctx_t *ctx,
   next_interval = next_timeout - wc->time_now;
   if (next_timeout == 0 || next_interval <= 0)
     {
-      if (ctx->c_s_index == QUIC_SESSION_INVALID || ctx->flags & QUIC_F_NO_APP_SESSION)
+      if (ctx->c_s_index == QUIC_SESSION_INVALID || ctx->flags & QUIC_F_NO_APP_SESSION ||
+	  ctx->conn_state == QUIC_CONN_STATE_HANDSHAKE)
 	{
 	  next_interval = 1;
 	}
