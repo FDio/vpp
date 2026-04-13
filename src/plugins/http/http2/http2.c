d5c89f346845bdff643b4fbeda6fb4672651c527
Florin Coras <fcoras@cisco.com>
2026-04-13 03:16:39 -0400

    session: explicit session and transport formatting
    Support session and transport formatting for conn-id, state, details
    explicitly
    
    Type: improvement
    
    Change-Id: I841f74e9e7ef43421190c4538c9200ce438d1823
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/plugins/http/http2/http2.c
+++ src/plugins/http/http2/http2.c
@@ -2666,19 +2666,35 @@ http2_format_req (u8 *s, va_list *args)
   u32 req_index = va_arg (*args, u32);
   clib_thread_index_t thread_index = va_arg (*args, u32);
   http_ctx_t *hc = va_arg (*args, http_ctx_t *);
-  u32 verbose = va_arg (*args, u32);
+  transport_fmt_req_t fmt = transport_fmt_req_decode (va_arg (*args, u32));
+  uword explicit = transport_fmt_req_is_explicit (fmt);
   http_ctx_t *req;
 
   req = http_ctx_get_w_thread (req_index, thread_index);
 
-  s = format (s, "%-" SESSION_CLI_ID_LEN "U", format_http2_req, req, hc);
-  if (verbose)
+  if (!explicit)
     {
-      s = format (s, "%-" SESSION_CLI_STATE_LEN "U", format_http2_stream_state,
-		  req->stream_state);
-      if (verbose > 1)
-	s = format (s, "\n%U", format_http2_req_vars, req, hc);
+      s = format (s, "%-" SESSION_CLI_ID_LEN "U", format_http2_req, req, hc);
+      if (fmt.level)
+	{
+	  s = format (s, "%-" SESSION_CLI_STATE_LEN "U", format_http2_stream_state,
+		      req->stream_state);
+	  if (fmt.level > 1)
+	    s = format (s, "\n%U", format_http2_req_vars, req, hc);
+	}
+      return s;
     }
+
+  if (fmt.conn_id)
+    s = format (s, "%U", format_http2_req, req, hc);
+  if (fmt.transport_state)
+    {
+      if (fmt.conn_id)
+	s = format (s, "\t");
+      s = format (s, "%U", format_http2_stream_state, req->stream_state);
+    }
+  if (fmt.transport_detail)
+    s = format (s, "\n%U", format_http2_req_vars, req, hc);
 
   return s;
 }
