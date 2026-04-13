937de7e160798c4ab62c1ed3003666da677b2497
Florin Coras <fcoras@cisco.com>
2026-04-13 16:40:30 -0400

    session: improve tree cli indenting
    Type: improvement
    
    Change-Id: Ia5a8c04b338b9ff54b0e5354ad5b635b8c9fa5f1
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/vnet/session/session_cli.c
+++ src/vnet/session/session_cli.c
@@ -84,7 +84,7 @@ format_session_flags (u8 *s, va_list *args)
 static session_fmt_req_t
 session_fmt_req_normalize (session_fmt_req_t fmt)
 {
-  if (!(fmt.as_u32 & ~SESSION_FMT_REQ_LEVEL_MASK))
+  if (!(fmt.as_u32 & SESSION_FMT_REQ_F_MASK))
     {
       fmt.conn_id = 1;
       if (fmt.level >= 1)
@@ -121,6 +121,8 @@ format_session_io (u8 *s, session_t *ss)
 static u8 *
 format_session_transport (u8 *s, session_t *ss, u32 tp, session_fmt_req_t fmt)
 {
+  u32 conn_id_indent = fmt.conn_id_indent * 2;
+  u32 conn_id_width;
   uword wants_state, wants_io, use_cols;
 
   if (fmt.transport_detail && !fmt.rx_tx)
@@ -131,11 +133,16 @@ format_session_transport (u8 *s, session_t *ss, u32 tp, session_fmt_req_t fmt)
   wants_state = fmt.transport_state;
   wants_io = fmt.rx_tx;
   use_cols = wants_state || wants_io;
+  conn_id_width =
+    (conn_id_indent < SESSION_CLI_ID_WIDTH) ? SESSION_CLI_ID_WIDTH - conn_id_indent : 0;
 
   if (fmt.conn_id)
     {
+      if (conn_id_indent)
+	s = format (s, "%U", format_white_space, conn_id_indent);
+
       if (use_cols)
-	s = format (s, "%-" SESSION_CLI_ID_LEN "U", format_transport_connection, tp,
+	s = format (s, "%-*U", (int) conn_id_width, format_transport_connection, tp,
 		    ss->connection_index, ss->thread_index, TRANSPORT_FMT_REQ_F_CONN_ID);
       else
 	s = format (s, "%U", format_transport_connection, tp, ss->connection_index,
@@ -171,9 +178,11 @@ format_session (u8 *s, va_list *args)
   session_t *ss = va_arg (*args, session_t *);
   session_fmt_req_t fmt = { .as_u32 = va_arg (*args, int) };
   u32 tp = session_get_transport_proto (ss);
+  u32 conn_id_indent;
   u8 *str = 0;
 
   fmt = session_fmt_req_normalize (fmt);
+  conn_id_indent = fmt.conn_id_indent * 2;
 
   if (ss->session_state >= SESSION_STATE_TRANSPORT_DELETED)
     {
@@ -197,6 +206,8 @@ format_session (u8 *s, va_list *args)
     {
       if (fmt.rx_tx)
 	str = format_session_io (0, ss);
+      if (conn_id_indent)
+	s = format (s, "%U", format_white_space, conn_id_indent);
       s = format (s, "%U%v", format_transport_listen_connection, tp, ss->connection_index,
 		  ss->thread_index, fmt.level, str);
       if (fmt.level > 1)
@@ -208,6 +219,8 @@ format_session (u8 *s, va_list *args)
 	{
 	  if (fmt.rx_tx)
 	    str = format_session_io (0, ss);
+	  if (conn_id_indent)
+	    s = format (s, "%U", format_white_space, conn_id_indent);
 	  s = format (s, "%U", format_transport_half_open_connection, tp, ss->connection_index,
 		      ss->thread_index, fmt.level);
 	  s = format (s, "%v", str);
@@ -650,9 +663,10 @@ session_cli_show_session_tree (vlib_main_t *vm, u8 force_print)
 		pool_elt_at_index (st->nodes, vec_pop (node_stack));
 	      session_t *s = session_get (node->session_index, thread_index);
 	      u32 *child_index;
+	      session_fmt_req_t fmt = { .level = 1 };
 
-	      vlib_cli_output (vm, "%*s%U", node->depth * 2, "",
-			       format_session, s, 1);
+	      fmt.conn_id_indent = clib_min (node->depth, SESSION_FMT_REQ_CONN_ID_INDENT_MAX);
+	      vlib_cli_output (vm, "%U", format_session, s, fmt.as_u32);
 
 	      vec_foreach (child_index, node->children)
 		vec_add1 (node_stack, *child_index);
