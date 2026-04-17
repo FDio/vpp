FDio/vpp

commit 3a9c3ebcc1a818031efdf83d552bfb21206d2eae

dev: use the endian reply macro
Checkstyle also forces the new indentation.

Type: fix
Fixes: ddf6cec37027547ff7cc61e15bb8080664d41514

Change-Id: Ife96928d6ca30ba94e1c423d557d6ed9d68eca2b
Signed-off-by: Vratko Polak <vrpolak@cisco.com>

src/vnet/dev/dev_api.c
@@ -54,17 +54,17 @@ vl_api_dev_attach_t_handler (vl_api_dev_attach_t *mp)
 
   vec_free (a.args);
 
-  REPLY_MACRO3 (VL_API_DEV_ATTACH_REPLY, vec_len (error_string), ({
-		  rmp->retval = rv;
-		  if (error_string)
-		    {
-		      rmp->dev_index = ~0;
-		      vl_api_vec_to_api_string (error_string,
-						&rmp->error_string);
-		    }
-		  else
-		    rmp->dev_index = a.dev_index;
-		}));
+  REPLY_MACRO3_END (VL_API_DEV_ATTACH_REPLY, vec_len (error_string), ({
+		      rmp->retval = rv;
+		      if (error_string)
+			{
+			  rmp->dev_index = ~0;
+			  vl_api_vec_to_api_string (error_string,
+						    &rmp->error_string);
+			}
+		      else
+			rmp->dev_index = a.dev_index;
+		    }));
 
   vec_free (a.args);
   vec_free (error_string);
@@ -86,12 +86,12 @@ vl_api_dev_detach_t_handler (vl_api_dev_detach_t *mp)
   if (rv != VNET_DEV_OK)
     error_string = format (0, "%U", format_vnet_dev_rv, rv);
 
-  REPLY_MACRO3 (VL_API_DEV_DETACH_REPLY, vec_len (error_string), ({
-		  rmp->retval = rv;
-		  if (error_string)
-		    vl_api_vec_to_api_string (error_string,
-					      &rmp->error_string);
-		}));
+  REPLY_MACRO3_END (VL_API_DEV_DETACH_REPLY, vec_len (error_string), ({
+		      rmp->retval = rv;
+		      if (error_string)
+			vl_api_vec_to_api_string (error_string,
+						  &rmp->error_string);
+		    }));
 
   vec_free (error_string);
 }
@@ -128,17 +128,17 @@ vl_api_dev_create_port_if_t_handler (vl_api_dev_create_port_if_t *mp)
 
   vec_free (a.args);
 
-  REPLY_MACRO3 (VL_API_DEV_CREATE_PORT_IF_REPLY, vec_len (error_string), ({
-		  rmp->retval = rv;
-		  if (error_string)
-		    {
-		      rmp->sw_if_index = ~0;
-		      vl_api_vec_to_api_string (error_string,
-						&rmp->error_string);
-		    }
-		  else
-		    rmp->sw_if_index = a.sw_if_index;
-		}));
+  REPLY_MACRO3_END (VL_API_DEV_CREATE_PORT_IF_REPLY, vec_len (error_string), ({
+		      rmp->retval = rv;
+		      if (error_string)
+			{
+			  rmp->sw_if_index = ~0;
+			  vl_api_vec_to_api_string (error_string,
+						    &rmp->error_string);
+			}
+		      else
+			rmp->sw_if_index = a.sw_if_index;
+		    }));
 
   vec_free (a.args);
   vec_free (error_string);
@@ -160,12 +160,12 @@ vl_api_dev_remove_port_if_t_handler (vl_api_dev_remove_port_if_t *mp)
   if (rv != VNET_DEV_OK)
     error_string = format (0, "%U", format_vnet_dev_rv, rv);
 
-  REPLY_MACRO3 (VL_API_DEV_REMOVE_PORT_IF_REPLY, vec_len (error_string), ({
-		  rmp->retval = rv;
-		  if (error_string)
-		    vl_api_vec_to_api_string (error_string,
-					      &rmp->error_string);
-		}));
+  REPLY_MACRO3_END (VL_API_DEV_REMOVE_PORT_IF_REPLY, vec_len (error_string), ({
+		      rmp->retval = rv;
+		      if (error_string)
+			vl_api_vec_to_api_string (error_string,
+						  &rmp->error_string);
+		    }));
 
   vec_free (error_string);
 }

commit 5aa7828d2d18b78c96afcbe25bf26455a6edb955

dev: correct endiannes of API string inputs
Type: fix
Fixes: 3a9c3ebcc1a818031efdf83d552bfb21206d2eae

Change-Id: I2298bee0ebea91b65014f8449b04f59fa5c5d482
Signed-off-by: Vratko Polak <vrpolak@cisco.com>

src/vnet/dev/dev_api.c
@@ -49,7 +49,7 @@ vl_api_dev_attach_t_handler (vl_api_dev_attach_t *mp)
   a.flags.n = mp->flags;
   snprintf (a.device_id, sizeof (a.device_id), "%s", (char *) mp->device_id);
   snprintf (a.driver_name, sizeof (a.driver_name), "%s", (char *) mp->driver_name);
-  vec_add (a.args, mp->args.buf, mp->args.length);
+  a.args = vl_api_from_api_to_new_vec (mp, &mp->args);
 
   rv = vnet_dev_api_attach (vm, &a);
 
@@ -123,7 +123,7 @@ vl_api_dev_create_port_if_t_handler (vl_api_dev_create_port_if_t *mp)
 #undef _
 
   snprintf (a.intf_name, sizeof (a.intf_name), "%s", (char *) mp->intf_name);
-  vec_add (a.args, mp->args.buf, mp->args.length);
+  a.args = vl_api_from_api_to_new_vec (mp, &mp->args);
 
   rv = vnet_dev_api_create_port_if (vm, &a);
 