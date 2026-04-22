5c1ea2e7738e49b320152a36254cfd7c4d2dcc1b
Jerome Tollet <jtollet@cisco.com>
2026-04-22 22:00:10 +0000

    iavf: fix native TSO datapath
    Fix native iavf TSO by addressing several bugs:
    
    - correct TSO context and data descriptor field masks
    - track TX buffer ownership per descriptor, so context descriptors
      and extra descriptors generated from one GSO buffer are not freed
      as independent buffers
    - compute TSO header length relative to current_data, matching
      vlib_buffer_length_in_chain()
    - fix ring index wrap-around in TX queue teardown
    - program virtchnl rxq.max_pkt_size with the L2 frame size expected
      by Intel VFs, including FCS and VLAN headroom
    
    This change is intentionally limited to the native iavf driver. The
    TCP tx fifo issue that can generate empty chained buffers is handled
    in a separate TCP change.
    
    Type: fix
    Signed-off-by: Jerome Tollet <jtollet@cisco.com>
    Change-Id: I199042e27434fec384d0cf0709cd252bb4e0b395

--- src/drivers/iavf/iavf.h
+++ src/drivers/iavf/iavf.h
@@ -17,6 +17,9 @@
 #define IAVF_RX_MAX_DESC_IN_CHAIN 5
 #define IAVF_MAX_RSS_KEY_SIZE	  52
 #define IAVF_MAX_RSS_LUT_SIZE	  64
+#define IAVF_RX_FRAME_FCS_SIZE	  4
+#define IAVF_RX_FRAME_VLAN_TAGS	  2
+#define IAVF_TX_RS_SLOT_RING_SIZE 32
 #define IIAVF_AQ_POLL_INTERVAL	  0.2
 #define IIAVF_AQ_BUF_SIZE	  4096
 
@@ -66,7 +69,6 @@ typedef struct
   u16 *rs_slots;
   iavf_tx_desc_t *tmp_descs;
   u32 *tmp_bufs;
-  u32 *ph_bufs;
 } iavf_txq_t;
 
 typedef struct
@@ -194,8 +196,19 @@ typedef struct
   iavf_rx_tail_t tails[IAVF_RX_VECTOR_SZ];
 } iavf_rt_data_t;
 
-#define foreach_iavf_tx_node_counter                                          \
-  _ (SEG_SZ_EXCEEDED, seg_sz_exceeded, ERROR, "segment size exceeded")        \
+#define foreach_iavf_tx_node_counter                                                               \
+  _ (SEG_SZ_EXCEEDED, seg_sz_exceeded, ERROR, "segment size exceeded")                             \
+  _ (GSO_HDR_LINEARIZED, gso_hdr_linearized, INFO, "gso headers linearized")                       \
+  _ (GSO_NOT_NEEDED, gso_not_needed, INFO, "gso not needed")                                       \
+  _ (GSO_NO_L4_HDR, gso_no_l4_hdr, ERROR, "gso without l4 header offset")                          \
+  _ (GSO_BAD_HDR, gso_bad_hdr, ERROR, "gso bad header length")                                     \
+  _ (GSO_BAD_HDR_OFFSET, gso_bad_hdr_offset, ERROR, "gso bad l4 header offset")                    \
+  _ (GSO_BAD_L4_HDR_SZ, gso_bad_l4_hdr_sz, ERROR, "gso bad l4 header size")                        \
+  _ (GSO_HDR_LINEARIZE_FAIL, gso_hdr_linearize_fail, ERROR, "gso header linearize failed")         \
+  _ (GSO_BAD_PAYLOAD, gso_bad_payload, ERROR, "gso bad payload length")                            \
+  _ (GSO_BAD_MSS, gso_bad_mss, ERROR, "gso mss out of range")                                      \
+  _ (GSO_BAD_TLEN, gso_bad_tlen, ERROR, "gso tso length out of range")                             \
+  _ (CHAIN_TOO_LONG, chain_too_long, ERROR, "tx chain too long")                                   \
   _ (NO_FREE_SLOTS, no_free_slots, ERROR, "no free tx slots")
 
 typedef enum
