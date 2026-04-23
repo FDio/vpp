f1f83593f083c1797e86db8660f5edf26ff65414
Vratko Polak <vrpolak@cisco.com>
2026-04-23 13:18:43 +0000

    build: add platform cmake file for icelake
    + Do not prefer vector width.
    - Fow now, using and outdated parent for testing convenience.
    
    Change-Id: I7d1471b223d9037a55d18f4b5f74e011d94ba07f
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- src/cmake/platform/sapphirerapids.cmake
+++ src/cmake/platform/sapphirerapids.cmake
@@ -1,4 +1,4 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright(c) 2025 Cisco Systems, Inc.
 
-set(VPP_PLATFORM_CFLAGS -march=sapphirerapids -mtune=sapphirerapids -mprefer-vector-width=512)
+set(VPP_PLATFORM_CFLAGS -march=sapphirerapids -mtune=sapphirerapids)
