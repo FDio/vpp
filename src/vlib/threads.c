FDio/vpp

commit 91cc2319e4ce9ede882f49d7765815a03fe3bea6

vlib: add one-time barrier release callbacks
Type: improvement
Change-Id: I88b618ce11232243a5b42767c0e08d61ea197d5d
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/vlib/threads.c
@@ -30,6 +30,24 @@ vl (void *p)
 vlib_worker_thread_t *vlib_worker_threads;
 vlib_thread_main_t vlib_thread_main;
 
+void
+vlib_worker_thread_register_one_time_release_fn (vlib_worker_thread_one_time_release_fn_t *fn,
+						 uword arg)
+{
+  vlib_global_main_t *vgm = vlib_get_global_main ();
+  vlib_worker_thread_one_time_release_fn_elt_t *elt;
+
+  ASSERT (fn);
+
+  elt = clib_mem_alloc_no_fail (sizeof (*elt));
+  elt[0] = (vlib_worker_thread_one_time_release_fn_elt_t){
+    .fn = fn,
+    .arg = arg,
+  };
+
+  elt->next = __atomic_exchange_n (&vgm->worker_thread_one_time_release_fns, elt, __ATOMIC_ACQ_REL);
+}
+
 /*
  * Barrier tracing can be enabled on a normal build to collect information
  * on barrier use, including timings and call stacks.  Deliberately not
@@ -1452,6 +1470,22 @@ vlib_worker_thread_barrier_release (vlib_main_t * vm)
 			     (vlib_get_n_threads () - 1));
       now = vlib_time_now (vm);
       t_update_main = now - vm->barrier_epoch;
+    }
+
+  if (PREDICT_FALSE (vgm->worker_thread_one_time_release_fns != 0))
+    {
+      vlib_worker_thread_one_time_release_fn_elt_t *rfn, *rfn_next;
+
+      rfn = vgm->worker_thread_one_time_release_fns;
+      vgm->worker_thread_one_time_release_fns = 0;
+
+      while (rfn)
+	{
+	  rfn_next = rfn->next;
+	  rfn->fn (vm, rfn->arg);
+	  clib_mem_free (rfn);
+	  rfn = rfn_next;
+	}
     }
 
   deadline = now + BARRIER_SYNC_TIMEOUT;

src/vlib/main.h
@@ -246,6 +246,15 @@ typedef struct vlib_main_t
 #endif
 } vlib_main_t;
 
+typedef void (vlib_worker_thread_one_time_release_fn_t) (vlib_main_t *vm, uword arg);
+
+typedef struct vlib_worker_thread_one_time_release_fn_elt_t
+{
+  struct vlib_worker_thread_one_time_release_fn_elt_t *next;
+  vlib_worker_thread_one_time_release_fn_t *fn;
+  uword arg;
+} vlib_worker_thread_one_time_release_fn_elt_t;
+
 typedef struct vlib_global_main_t
 {
   CLIB_CACHE_LINE_ALIGN_MARK (cacheline0);
@@ -267,6 +276,9 @@ typedef struct vlib_global_main_t
 
   /* post-mortem callbacks */
   void (**post_mortem_callbacks) (void);
+
+  /* One-time callbacks invoked before releasing the worker barrier */
+  vlib_worker_thread_one_time_release_fn_elt_t *worker_thread_one_time_release_fns;
 
   /*
    * Need to call vlib_worker_thread_node_runtime_update before

src/vlib/threads.h
@@ -165,6 +165,8 @@ u32 vlib_frame_queue_main_init (u32 node_index, u32 frame_queue_nelts);
 void vlib_worker_thread_barrier_sync_int (vlib_main_t * vm,
 					  const char *func_name);
 void vlib_worker_thread_barrier_release (vlib_main_t * vm);
+void vlib_worker_thread_register_one_time_release_fn (vlib_worker_thread_one_time_release_fn_t *fn,
+						      uword arg);
 u8 vlib_worker_thread_barrier_held (void);
 void vlib_worker_thread_initial_barrier_sync_and_release (vlib_main_t * vm);
 void vlib_worker_thread_node_refork (void);

commit 93e030cfc9a3b7151135f14898682453ac08d3a8

vlib: fix one-time barrier release callbacks
If there are no worker threads, vlib_worker_thread_barrier_release()
early exits and never calls deferred release functions.

Type: fix
Fixes: 91cc2319e4ce9ede882f49d7765815a03fe3bea6

Change-Id: I4978cca2e0c1bed269e47af4e62f0cb04bca1585
Signed-off-by: Benoît Ganne <bganne@cisco.com>

src/vlib/threads.c
@@ -39,6 +39,13 @@ vlib_worker_thread_register_one_time_release_fn (vlib_worker_thread_one_time_rel
 
   ASSERT (fn);
 
+  if (vlib_get_n_threads () < 2)
+    {
+      /* No worker threads, call the function directly */
+      fn (vgm->vlib_mains[0], arg);
+      return;
+    }
+
   elt = clib_mem_alloc_no_fail (sizeof (*elt));
   elt[0] = (vlib_worker_thread_one_time_release_fn_elt_t){
     .fn = fn,