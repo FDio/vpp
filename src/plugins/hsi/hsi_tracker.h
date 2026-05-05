3aa5f49303b6af8ed03915f47a07b6e2ef2b6432
Florin Coras <fcoras@cisco.com>
2026-05-05 14:36:49 -0400

    hsi: basic support for session tracking
    Type: feature
    
    Change-Id: Ie01a103191c1ae00e92251bbebbf9cf69402dcb9
    Signed-off-by: Florin Coras <fcoras@cisco.com>

---
+++ src/plugins/hsi/hsi_tracker.h
@@ -0,0 +1,53 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2026 Cisco and/or its affiliates.
+ */
+
+#ifndef SRC_PLUGINS_HSI_HSI_TRACKER_H_
+#define SRC_PLUGINS_HSI_HSI_TRACKER_H_
+
+#include <hsi/hsi.h>
+#include <vnet/buffer.h>
+#include <vnet/tcp/tcp_types.h>
+#include <vnet/udp/udp.h>
+
+typedef enum hsi_tcp_tracked_action_
+{
+  HSI_TCP_TRACKED_ACTION_FORWARD,
+  HSI_TCP_TRACKED_ACTION_CONSUME,
+  HSI_TCP_TRACKED_ACTION_DROP,
+} hsi_tcp_tracked_action_t;
+
+hsi_tcp_tracked_action_t hsi_tcp_handle_tracked_connection (vlib_main_t *vm, vlib_buffer_t *b,
+							    tcp_connection_t *tc, void *ip_hdr,
+							    tcp_header_t *tcp_hdr, u8 is_ip4);
+int hsi_tcp_try_complete_drain (tcp_connection_t *tc);
+hsi_tcp_tracked_action_t hsi_tcp_drain_cache_buffer (vlib_main_t *vm, vlib_buffer_t *b,
+						     tcp_connection_t *tc, void *ip_hdr,
+						     tcp_header_t *tcp_hdr, u8 is_ip4);
+void hsi_tcp_drain_update_time (f64 time_now, u8 thread_index);
+
+static_always_inline hsi_tcp_tracked_action_t
+hsi_tcp_tracked_connection_action (vlib_main_t *vm, vlib_buffer_t *b, tcp_connection_t *tc,
+				   void *ip_hdr, tcp_header_t *tcp_hdr, u8 is_ip4)
+{
+  tcp_state_t state;
+
+  ASSERT (tc->cfg_flags & TCP_CFG_F_TRACKED);
+
+  state = tc->state;
+  if (state != TCP_STATE_CLOSED && hsi_tcp_try_complete_drain (tc))
+    state = TCP_STATE_CLOSED;
+
+  if (state != TCP_STATE_CLOSED)
+    {
+      return hsi_tcp_drain_cache_buffer (vm, b, tc, ip_hdr, tcp_hdr, is_ip4);
+    }
+
+  return hsi_tcp_handle_tracked_connection (vm, b, tc, ip_hdr, tcp_hdr, is_ip4);
+}
+
+void hsi_udp_handle_tracked_connection (vlib_main_t *vm, vlib_buffer_t *b, udp_connection_t *uc,
+					void *ip_hdr, udp_header_t *udp_hdr, u8 is_ip4);
+
+#endif /* SRC_PLUGINS_HSI_HSI_TRACKER_H_ */
--- test/hs-test/hsi_test.go
+++ test/hs-test/hsi_test.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 
 	. "fd.io/hs-test/infra"
 	. "github.com/onsi/ginkgo/v2"
 )
 
 func init() {
-	RegisterHsiSoloTests(HsiTransparentProxyTest)
+	RegisterHsiSoloTests(HsiTransparentProxyTest, HsiProxyLiteOffloadTest,
+		HsiProxyLiteDrainOffloadTest)
 }
 
 func HsiTransparentProxyTest(s *HsiSuite) {
@@ -28,4 +33,108 @@ func HsiTransparentProxyTest(s *HsiSuite) {
 		StartWget(finished, s.ServerAddr(), strconv.Itoa(int(s.Ports.Server)), query, s.NetNamespaces.Client)
 	}()
 	AssertNil(<-finished)
+}
+
+func startCurlHttpRequest(uri, netNs, expectedRespCode string, timeout int, args ...string) <-chan error {
+	finished := make(chan error, 1)
+	go func() {
+		defer GinkgoRecover()
+		StartCurl(finished, uri, netNs, expectedRespCode, timeout, args)
+	}()
+	return finished
+}
+
+func waitProxyLiteTracked(vpp *VppInstance, cancel func()) string {
+	var lastOutput string
+	deadline := time.Now().Add(5 * time.Second)
+	for time.Now().Before(deadline) {
+		output := vpp.Vppctl("show proxy-lite")
+		if output != "" {
+			lastOutput = output
+		}
+		if strings.Contains(output, "hsi tracked 1") {
+			return output
+		}
+		if strings.Contains(output, "failed 1") || strings.Contains(output, "skipped-pending 1") {
+			cancel()
+			AssertFail("proxy-lite hsi offload failed before completion:\n%s", output)
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	cancel()
+	AssertFail("timed out waiting for proxy-lite hsi offload; last output:\n%s", lastOutput)
+	return lastOutput
+}
+
+func assertProxyLiteSessionsCleaned(s *HsiSuite) {
+	vpp := s.Containers.Vpp.VppInstance
+	proxyClientConn := fmt.Sprintf("[T] %s:%d->%s", s.ServerAddr(), s.Ports.Server,
+		s.Interfaces.Client.Host.Ip4AddressString())
+	proxyTargetConn := fmt.Sprintf("->%s:%d", s.ServerAddr(), s.Ports.Server)
+	for range 10 {
+		sessions := vpp.Vppctl("show session verbose 2")
+		if !strings.Contains(sessions, proxyClientConn) &&
+			!strings.Contains(sessions, proxyTargetConn) {
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+	sessions := vpp.Vppctl("show session verbose 2")
+	Log(sessions)
+	AssertNotContains(sessions, proxyClientConn, "client-proxy session not cleaned up")
+	AssertNotContains(sessions, proxyTargetConn, "proxy-server session not cleaned up")
+}
+
+func HsiProxyLiteOffloadTest(s *HsiSuite) {
+	s.SetupNginxServer()
+	vpp := s.Containers.Vpp.VppInstance
+	Log(vpp.Vppctl("proxy-lite server-uri tcp://0.0.0.0:%d client-uri tcp://%s:%d hsi-offload-pending",
+		s.Ports.Server, s.ServerAddr(), s.Ports.Server))
+
+	uri := fmt.Sprintf("http://%s:%d/64B", s.ServerAddr(), s.Ports.Server)
+	finished := startCurlHttpRequest(uri, s.NetNamespaces.Client, "200 OK", 20)
+	waitProxyLiteTracked(vpp, func() {})
+	AssertNil(<-finished)
+
+	output := vpp.Vppctl("show proxy-lite")
+	Log(output)
+	AssertContains(output, "hsi tracked 1")
+	AssertContains(output, "failed 0")
+	AssertContains(output, "skipped-pending 0")
+
+	assertProxyLiteSessionsCleaned(s)
+}
+
+func HsiProxyLiteDrainOffloadTest(s *HsiSuite) {
+	s.SetupNginxServer()
+	vpp := s.Containers.Vpp.VppInstance
+	Log(vpp.Vppctl("proxy-lite server-uri tcp://0.0.0.0:%d client-uri tcp://%s:%d hsi-offload-pending fifo-size 4k",
+		s.Ports.Server, s.ServerAddr(), s.Ports.Server))
+
+	uploadFile, err := os.CreateTemp("", "hsi-proxy-lite-upload-*")
+	AssertNil(err)
+	defer os.Remove(uploadFile.Name())
+	_, err = uploadFile.Write(bytes.Repeat([]byte("0123456789abcdef"), 64*1024))
+	AssertNil(err)
+	AssertNil(uploadFile.Close())
+
+	uri := fmt.Sprintf("http://%s:%d/upload/hsi-proxy-lite-upload", s.ServerAddr(), s.Ports.Server)
+	finished := startCurlHttpRequest(uri, s.NetNamespaces.Client, "201", 60,
+		"-T", uploadFile.Name(), "-H", "Expect:", "--limit-rate", "256k")
+
+	output := waitProxyLiteTracked(vpp, func() {})
+	AssertContains(output, "pending 1")
+
+	AssertNil(<-finished)
+
+	output = vpp.Vppctl("show proxy-lite")
+	Log(output)
+	AssertContains(output, "hsi tracked 1")
+	AssertContains(output, "failed 0")
+	AssertContains(output, "skipped-pending 0")
+	AssertContains(output, "pending 1")
+
+	assertProxyLiteSessionsCleaned(s)
 }
