26b6644f826256a804c97e31e56395ef73a9ecd3
Samuel Benko <sbenko@cisco.com>
2026-05-07 09:11:08 +0200

    hs-test: add app-namespace VCL echo tests
    Add AppNsVclEchoTcpTest and AppNsVclEchoUdpTest. Each mirrors
    testVclEcho but additionally calls "app ns add" on both VPP
    instances and threads ns_id through to getVclConfig, so the VCL
    client and server both attach to a non-default app namespace.
    
    Type: test
    Change-Id: Idc1fa17e4f78c4698a32e1ccd7285fb5643b1206
    Signed-off-by: Samuel Benko <sbenko@cisco.com>

---
+++ test/hs-test/app_namespace_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+
+	. "fd.io/hs-test/infra"
+)
+
+func init() {
+	RegisterVethTests(AppNsVclEchoTcpTest, AppNsVclEchoUdpTest)
+}
+
+func AppNsVclEchoTcpTest(s *VethsSuite) {
+	testAppNsVclEcho(s, "tcp", "1")
+}
+
+func AppNsVclEchoUdpTest(s *VethsSuite) {
+	testAppNsVclEcho(s, "udp", "1")
+}
+
+func testAppNsVclEcho(s *VethsSuite, proto, nsId string) {
+	s.SetupAppContainers()
+
+	srvVppCont := s.Containers.ServerVpp
+	srvAppCont := s.Containers.ServerApp
+	serverVethAddress := s.Interfaces.Server.Ip4AddressString()
+
+	srvVppCont.VppInstance.Vppctl("app ns add id %s secret %s if %s",
+		nsId, nsId, s.Interfaces.Server.VppName())
+	srvAppCont.CreateFile("/vcl.conf", getVclConfig(srvVppCont, nsId))
+	srvAppCont.AddEnvVar("VCL_CONFIG", "/vcl.conf")
+
+	vclSrvCmd := fmt.Sprintf("vcl_test_server -p %s -B %s %s > %s 2>&1",
+		proto, serverVethAddress, s.Ports.Port1, VclTestSrvLogFileName(srvAppCont))
+	srvAppCont.ExecServer(true, WrapCmdWithLineBuffering(vclSrvCmd))
+	srvVppCont.VppInstance.WaitForApp("vcl_test_server", 3)
+
+	defaultNsApps := srvVppCont.VppInstance.Vppctl("show app ns id default")
+	AssertNotContains(defaultNsApps, "vcl_test_server")
+
+	clnVppCont := s.Containers.ClientVpp
+	clnVppCont.VppInstance.Vppctl("app ns add id %s secret %s if %s",
+		nsId, nsId, s.Interfaces.Client.VppName())
+
+	echoClnContainer := s.GetTransientContainerByName("client-app")
+	echoClnContainer.CreateFile("/vcl.conf", getVclConfig(echoClnContainer, nsId))
+	echoClnContainer.AddEnvVar("VCL_CONFIG", "/vcl.conf")
+
+	testClientCommand := fmt.Sprintf("vcl_test_client -X -S -p %s %s %s 2>&1 | tee %s",
+		proto, serverVethAddress, s.Ports.Port1, VclTestClnLogFileName(echoClnContainer))
+
+	o, err := echoClnContainer.Exec(true, WrapCmdWithLineBuffering(testClientCommand))
+	Log("****** Client output:\n%s\n******", o)
+
+	oSrv, errSrv := srvAppCont.Exec(false, "cat %s", VclTestSrvLogFileName(srvAppCont))
+	Log("****** Server output:\n%s\n******", oSrv)
+
+	AssertNil(err, o)
+	AssertNotContains(o, "aborting test")
+	AssertNil(errSrv, oSrv)
+}
