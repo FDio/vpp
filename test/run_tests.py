a3e1b082f7b4d00bb0276e4a95ec6f25caf0967a
Dave Wallace <dwallacelf@gmail.com>
2026-05-14 12:10:36 -0400

    tests: add suffix to failed_test file
    - Without the suffix, the failed_test file does not have the correct
      mime type to be decompressed in a browser when selected from AWS s3
      url in the CI log upload directory.  Adding '.log' suffix fixes this issue.
    
    Type: test
    
    Change-Id: I5e8d7a3059ed5df74881ebda94bc94e27ad462ff
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- test/run_tests.py
+++ test/run_tests.py
@@ -1038,7 +1038,7 @@ class AllResults(dict):
                         print_test_ids.append(failed_test_id)
 
         if print_test_ids and write_failed_file:
-            with open(os.path.join(config.failed_dir, "failed_tests"), "w") as f:
+            with open(os.path.join(config.failed_dir, "failed_tests.log"), "w") as f:
                 print(",".join(print_test_ids), file=f)
 
         if self.testsuites_cut_short:
