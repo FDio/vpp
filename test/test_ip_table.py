FDio/vpp

commit ff570d3d07ebe07a5107b44d50c54fc4a57359dc

fib: make mfib optional
In some cases we do not need multicast support. Making it optional helps
scaling to high number of VRFs, by reducing the control plane operations
and memory consumption.

Type: improvement

Change-Id: Ib34ed3fe2806e2f4624981da4e4a3c49c69f70be
Signed-off-by: Benoît Ganne <bganne@cisco.com>

src/vnet/interface_api.c
@@ -579,7 +579,7 @@ ip_table_bind (fib_protocol_t fproto, u32 sw_if_index, u32 table_id)
   fib_index = fib_table_find (fproto, table_id);
   mfib_index = mfib_table_find (fproto, table_id);
 
-  if (~0 == fib_index || ~0 == mfib_index)
+  if (~0 == fib_index)
     {
       return (VNET_API_ERROR_NO_SUCH_FIB);
     }
@@ -601,7 +601,8 @@ ip_table_bind (fib_protocol_t fproto, u32 sw_if_index, u32 table_id)
   /* clang-format on */
 
   fib_table_bind (fproto, sw_if_index, fib_index);
-  mfib_table_bind (fproto, sw_if_index, mfib_index);
+  if (mfib_index != ~0)
+    mfib_table_bind (fproto, sw_if_index, mfib_index);
 
   return (0);
 }

src/vnet/ip/ip_api.c
@@ -636,7 +636,8 @@ vl_api_ip_table_add_del_t_handler (vl_api_ip_table_add_del_t * mp)
 
   if (mp->is_add)
     {
-      ip_table_create (fproto, table_id, 1, mp->table.name);
+      ip_table_create (fproto, table_id, 1 /* is_api */, 1 /* create_mfib */,
+		       mp->table.name);
     }
   else
     {
@@ -644,6 +645,28 @@ vl_api_ip_table_add_del_t_handler (vl_api_ip_table_add_del_t * mp)
     }
 
   REPLY_MACRO (VL_API_IP_TABLE_ADD_DEL_REPLY);
+}
+
+void
+vl_api_ip_table_add_del_v2_t_handler (vl_api_ip_table_add_del_v2_t *mp)
+{
+  vl_api_ip_table_add_del_v2_reply_t *rmp;
+  fib_protocol_t fproto =
+    (mp->table.is_ip6 ? FIB_PROTOCOL_IP6 : FIB_PROTOCOL_IP4);
+  u32 table_id = ntohl (mp->table.table_id);
+  int rv = 0;
+
+  if (mp->is_add)
+    {
+      ip_table_create (fproto, table_id, 1 /* is_api */, mp->create_mfib,
+		       mp->table.name);
+    }
+  else
+    {
+      ip_table_delete (fproto, table_id, 1);
+    }
+
+  REPLY_MACRO (VL_API_IP_TABLE_ADD_DEL_V2_REPLY);
 }
 
 void
@@ -661,7 +684,8 @@ vl_api_ip_table_allocate_t_handler (vl_api_ip_table_allocate_t *mp)
   if (~0 == table_id)
     rv = VNET_API_ERROR_EAGAIN;
   else
-    ip_table_create (fproto, table_id, 1, mp->table.name);
+    ip_table_create (fproto, table_id, 1 /* is_api */, 1 /* create_mfib */,
+		     mp->table.name);
 
   REPLY_MACRO2 (VL_API_IP_TABLE_ALLOCATE_REPLY, {
     clib_memcpy_fast (&rmp->table, &mp->table, sizeof (mp->table));
@@ -915,8 +939,8 @@ vl_api_ip_route_lookup_v2_t_handler (vl_api_ip_route_lookup_v2_t *mp)
 }
 
 void
-ip_table_create (fib_protocol_t fproto,
-		 u32 table_id, u8 is_api, const u8 * name)
+ip_table_create (fib_protocol_t fproto, u32 table_id, u8 is_api,
+		 u8 create_mfib, const u8 *name)
 {
   u32 fib_index, mfib_index;
   vnet_main_t *vnm = vnet_get_main ();
@@ -936,16 +960,23 @@ ip_table_create (fib_protocol_t fproto,
        * their own unicast tables.
        */
       fib_index = fib_table_find (fproto, table_id);
-      mfib_index = mfib_table_find (fproto, table_id);
-
       /*
        * Always try to re-lock in case the fib was deleted by an API call
        * but was not yet freed because some other locks were held
        */
       fib_table_find_or_create_and_lock_w_name (
 	fproto, table_id, (is_api ? FIB_SOURCE_API : FIB_SOURCE_CLI), name);
-      mfib_table_find_or_create_and_lock_w_name (
-	fproto, table_id, (is_api ? MFIB_SOURCE_API : MFIB_SOURCE_CLI), name);
+
+      if (create_mfib)
+	{
+	  /* same for mfib, if needs be */
+	  mfib_index = mfib_table_find (fproto, table_id);
+	  mfib_table_find_or_create_and_lock_w_name (
+	    fproto, table_id, (is_api ? MFIB_SOURCE_API : MFIB_SOURCE_CLI),
+	    name);
+	}
+      else
+	mfib_index = 0;
 
       if ((~0 == fib_index) || (~0 == mfib_index))
 	call_elf_section_ip_table_callbacks (vnm, table_id, 1 /* is_add */ ,
@@ -1655,9 +1686,10 @@ vl_api_ip_table_replace_begin_t_handler (vl_api_ip_table_replace_begin_t * mp)
     rv = VNET_API_ERROR_NO_SUCH_FIB;
   else
     {
+      u32 mfib_index = mfib_table_find (fproto, ntohl (mp->table.table_id));
       fib_table_mark (fib_index, fproto, FIB_SOURCE_API);
-      mfib_table_mark (mfib_table_find (fproto, ntohl (mp->table.table_id)),
-		       fproto, MFIB_SOURCE_API);
+      if (mfib_index != INDEX_INVALID)
+	mfib_table_mark (mfib_index, fproto, MFIB_SOURCE_API);
     }
   REPLY_MACRO (VL_API_IP_TABLE_REPLACE_BEGIN_REPLY);
 }
@@ -1677,10 +1709,10 @@ vl_api_ip_table_replace_end_t_handler (vl_api_ip_table_replace_end_t * mp)
     rv = VNET_API_ERROR_NO_SUCH_FIB;
   else
     {
+      u32 mfib_index = mfib_table_find (fproto, ntohl (mp->table.table_id));
       fib_table_sweep (fib_index, fproto, FIB_SOURCE_API);
-      mfib_table_sweep (mfib_table_find
-			(fproto, ntohl (mp->table.table_id)), fproto,
-			MFIB_SOURCE_API);
+      if (mfib_index != INDEX_INVALID)
+	mfib_table_sweep (mfib_index, fproto, MFIB_SOURCE_API);
     }
   REPLY_MACRO (VL_API_IP_TABLE_REPLACE_END_REPLY);
 }
@@ -1703,6 +1735,7 @@ vl_api_ip_table_flush_t_handler (vl_api_ip_table_flush_t * mp)
       vnet_main_t *vnm = vnet_get_main ();
       vnet_interface_main_t *im = &vnm->interface_main;
       vnet_sw_interface_t *si;
+      u32 mfib_index;
 
       /* Shut down interfaces in this FIB / clean out intfc routes */
       pool_foreach (si, im->sw_interfaces)
@@ -1717,8 +1750,10 @@ vl_api_ip_table_flush_t_handler (vl_api_ip_table_flush_t * mp)
       }
 
       fib_table_flush (fib_index, fproto, FIB_SOURCE_API);
-      mfib_table_flush (mfib_table_find (fproto, ntohl (mp->table.table_id)),
-			fproto, MFIB_SOURCE_API);
+
+      mfib_index = mfib_table_find (fproto, ntohl (mp->table.table_id));
+      if (mfib_index != INDEX_INVALID)
+	mfib_table_flush (mfib_index, fproto, MFIB_SOURCE_API);
     }
 
   REPLY_MACRO (VL_API_IP_TABLE_FLUSH_REPLY);

src/plugins/unittest/session_test.c
@@ -133,7 +133,8 @@ session_create_lookpback (u32 table_id, u32 * sw_if_index,
 
   if (table_id != 0)
     {
-      ip_table_create (FIB_PROTOCOL_IP4, table_id, 0, 0);
+      ip_table_create (FIB_PROTOCOL_IP4, table_id, 0 /* is_api */,
+		       1 /* create_mfib */, 0);
       ip_table_bind (FIB_PROTOCOL_IP4, *sw_if_index, table_id);
     }
 

src/vnet/ip/ip.api
@@ -57,6 +57,23 @@ autoreply define ip_table_add_del
   vl_api_ip_table_t table;
 };
 
+/** \brief Add / del table request - version 2
+           A table can be added multiple times, but need be deleted only once.
+    @param client_index - opaque cookie to identify the sender
+    @param context - sender context, to match reply w/ request
+    @param table - the FIB table to add or del
+    @param create_mfib - whether to create mfib or not
+    @param is_add - add or del
+*/
+autoreply define ip_table_add_del_v2
+{
+  u32 client_index;
+  u32 context;
+  vl_api_ip_table_t table;
+  bool create_mfib [default=true];
+  bool is_add [default=true];
+};
+
 /** \brief Allocate an unused table
            A table can be added multiple times.
            If a large number of tables are in use (millions), this API might

src/vnet/ip/ip.h
@@ -262,7 +262,7 @@ extern vlib_node_registration_t ip4_inacl_node;
 extern vlib_node_registration_t ip6_inacl_node;
 
 void ip_table_create (fib_protocol_t fproto, u32 table_id, u8 is_api,
-		      const u8 * name);
+		      u8 create_mfib, const u8 *name);
 
 void ip_table_delete (fib_protocol_t fproto, u32 table_id, u8 is_api);
 

src/vnet/ip/ip_test.c
@@ -464,6 +464,60 @@ api_ip_table_add_del (vat_main_t *vam)
 }
 
 static int
+api_ip_table_add_del_v2 (vat_main_t *vam)
+{
+  unformat_input_t *i = vam->input;
+  vl_api_ip_table_add_del_v2_t *mp;
+  u8 create_mfib = 1;
+  u32 table_id = ~0;
+  u8 is_ipv6 = 0;
+  u8 is_add = 1;
+  int ret = 0;
+
+  /* Parse args required to build the message */
+  while (unformat_check_input (i) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (i, "ipv6"))
+	is_ipv6 = 1;
+      else if (unformat (i, "del"))
+	is_add = 0;
+      else if (unformat (i, "add"))
+	is_add = 1;
+      else if (unformat (i, "table %d", &table_id))
+	;
+      else if (unformat (i, "no-mfib"))
+	create_mfib = 0;
+      else
+	{
+	  clib_warning ("parse error '%U'", format_unformat_error, i);
+	  return -99;
+	}
+    }
+
+  if (~0 == table_id)
+    {
+      errmsg ("missing table-ID");
+      return -99;
+    }
+
+  /* Construct the API message */
+  M (IP_TABLE_ADD_DEL_V2, mp);
+
+  mp->table.table_id = ntohl (table_id);
+  mp->table.is_ip6 = is_ipv6;
+  mp->is_add = is_add;
+  mp->create_mfib = create_mfib;
+
+  /* send it... */
+  S (mp);
+
+  /* Wait for a reply... */
+  W (ret);
+
+  return ret;
+}
+
+static int
 api_ip_table_replace_begin (vat_main_t *vam)
 {
   unformat_input_t *i = vam->input;

src/vnet/ip/lookup.c
@@ -419,10 +419,12 @@ vnet_ip_table_cmd (vlib_main_t * vm,
   unformat_input_t _line_input, *line_input = &_line_input;
   clib_error_t *error = NULL;
   u32 table_id, is_add;
+  u8 create_mfib;
   u8 *name = NULL;
 
   is_add = 1;
   table_id = ~0;
+  create_mfib = 1;
 
   /* Get a line of input. */
   if (!unformat_user (main_input, unformat_line_input, line_input))
@@ -438,6 +440,8 @@ vnet_ip_table_cmd (vlib_main_t * vm,
 	is_add = 1;
       else if (unformat (line_input, "name %s", &name))
 	;
+      else if (unformat (line_input, "no-mfib"))
+	create_mfib = 0;
       else
 	{
 	  error = unformat_parse_error (line_input);
@@ -459,7 +463,8 @@ vnet_ip_table_cmd (vlib_main_t * vm,
 		  table_id = ip_table_get_unused_id (fproto);
 		  vlib_cli_output (vm, "%u\n", table_id);
 		}
-	      ip_table_create (fproto, table_id, 0, name);
+	      ip_table_create (fproto, table_id, 0 /* is_api */, create_mfib,
+			       name);
 	    }
 	  else
 	    {

test/test_ip4_vrf_multi_instance.py
@@ -195,7 +195,7 @@ class TestIp4VrfMultiInst(VppTestCase):
 
         for i in range(count):
             vrf_id = i + start
-            self.vapi.ip_table_add_del(is_add=1, table={"table_id": vrf_id})
+            self.vapi.ip_table_add_del_v2(is_add=1, table={"table_id": vrf_id})
             self.logger.info("IPv4 VRF ID %d created" % vrf_id)
             if vrf_id not in self.vrf_list:
                 self.vrf_list.append(vrf_id)
@@ -249,7 +249,7 @@ class TestIp4VrfMultiInst(VppTestCase):
         self.logger.info("IPv4 VRF ID %d reset finished" % vrf_id)
         self.logger.debug(self.vapi.ppcli("show ip fib"))
         self.logger.debug(self.vapi.ppcli("show ip neighbors"))
-        self.vapi.ip_table_add_del(is_add=0, table={"table_id": vrf_id})
+        self.vapi.ip_table_add_del_v2(is_add=0, table={"table_id": vrf_id})
 
     def create_stream(self, src_if, packet_sizes):
         """

commit 21d07017bdc2e8a3f73b39b375c74913f0ac31eb

interface: no-mfib table bind and create fixes
Fix an mfib table refcount leak when re-binding an interface that
previously had a table with an mfib table so that now the interface is
bound to a table without an mfib table.

Additionally, avoid firing the ip_table_add_del_functions with
is_add=1 twice for the same table if it was initially created with
create_mfib=false and later is created again with create_mfib=true
(with intervening delete), since doing so could surprise application
code.

Type: fix
Fixes: ff570d3d07ebe07a5107b44d50c54fc4a57359dc
Change-Id: Ie6332272f2d3534c95c58db2fdfb9978ad475b5b
Signed-off-by: Rob Shearman <robertshearman@gmail.com>

src/vnet/interface_api.c
@@ -522,6 +522,30 @@ mfib_table_bind (fib_protocol_t fproto, u32 sw_if_index, u32 mfib_index)
 {
   u32 table_id;
 
+  if (mfib_index == ~0)
+    {
+      /*
+       * Target table has no MFIB: still drop the interface's prior MFIB
+       * lock and clear the slot to avoid leaking MFIB_SOURCE_INTERFACE and
+       * leaving multicast forwarding on the old table).
+       */
+      if (FIB_PROTOCOL_IP6 == fproto)
+	{
+	  if (0 != ip6_main.mfib_index_by_sw_if_index[sw_if_index])
+	    mfib_table_unlock (ip6_main.mfib_index_by_sw_if_index[sw_if_index], FIB_PROTOCOL_IP6,
+			       MFIB_SOURCE_INTERFACE);
+	  ip6_main.mfib_index_by_sw_if_index[sw_if_index] = 0;
+	}
+      else
+	{
+	  if (0 != ip4_main.mfib_index_by_sw_if_index[sw_if_index])
+	    mfib_table_unlock (ip4_main.mfib_index_by_sw_if_index[sw_if_index], FIB_PROTOCOL_IP4,
+			       MFIB_SOURCE_INTERFACE);
+	  ip4_main.mfib_index_by_sw_if_index[sw_if_index] = 0;
+	}
+      return;
+    }
+
   table_id = mfib_table_get_table_id (mfib_index, fproto);
   ASSERT (table_id != ~0);
 
@@ -591,8 +615,7 @@ ip_table_bind (fib_protocol_t fproto, u32 sw_if_index, u32 table_id)
   /* clang-format on */
 
   fib_table_bind (fproto, sw_if_index, fib_index);
-  if (mfib_index != ~0)
-    mfib_table_bind (fproto, sw_if_index, mfib_index);
+  mfib_table_bind (fproto, sw_if_index, mfib_index);
 
   return (0);
 }

src/vnet/ip/ip_api.c
@@ -936,7 +936,7 @@ void
 ip_table_create (fib_protocol_t fproto, u32 table_id, u8 is_api,
 		 u8 create_mfib, const u8 *name)
 {
-  u32 fib_index, mfib_index;
+  u32 fib_index;
   vnet_main_t *vnm = vnet_get_main ();
 
   /*
@@ -964,15 +964,13 @@ ip_table_create (fib_protocol_t fproto, u32 table_id, u8 is_api,
       if (create_mfib)
 	{
 	  /* same for mfib, if needs be */
-	  mfib_index = mfib_table_find (fproto, table_id);
 	  mfib_table_find_or_create_and_lock_w_name (
 	    fproto, table_id, (is_api ? MFIB_SOURCE_API : MFIB_SOURCE_CLI),
 	    name);
 	}
-      else
-	mfib_index = 0;
 
-      if ((~0 == fib_index) || (~0 == mfib_index))
+      /* Only fire add callbacks for a newly created table */
+      if (~0 == fib_index)
 	call_elf_section_ip_table_callbacks (vnm, table_id, 1 /* is_add */ ,
 					     vnm->ip_table_add_del_functions);
     }

test/test_ip_table.py
@@ -1,0 +1,116 @@
+#!/usr/bin/env python3
+"""IP table tests"""
+
+import unittest
+
+from framework import VppTestCase
+from asfframework import VppTestRunner
+
+
+class TestIpTableCli(VppTestCase):
+    """Exercise ip table / set interface ip table CLI paths."""
+
+    IP_TABLE_ID_1 = 1
+    IP_TABLE_ID_2 = 2
+
+    pg0 = None
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestIpTableCli, cls).setUpClass()
+
+        cls.create_pg_interfaces([0])
+        cls.pg0.admin_up()
+
+    @classmethod
+    def tearDownClass(cls):
+        super(TestIpTableCli, cls).tearDownClass()
+
+    def setUp(self):
+        super(TestIpTableCli, self).setUp()
+
+    def tearDown(self):
+        super(TestIpTableCli, self).tearDown()
+        self.vapi.cli(f"set interface ip table {self.pg0.name} 0")
+        self.vapi.cli(f"ip table del {self.IP_TABLE_ID_1}")
+        self.vapi.cli(f"ip table del {self.IP_TABLE_ID_2}")
+
+    def _cli(self, cmd):
+        return self.vapi.cli(cmd)
+
+    def test_ip_table_cli_no_mfib_then_full_mfib(self):
+        """ip table add <id> no-mfib then ip table add <id> (same id).
+
+        Regression for double ip_table add-callback invocation when MFIB is
+        added in a second step for an existing unicast-only table.
+        """
+        tid = self.IP_TABLE_ID_1
+        r1 = self.vapi.cli(f"ip table add {tid} no-mfib")
+        r2 = self.vapi.cli(f"ip table add {tid}")
+        self.assertNotIn("error", (r1 or "").lower(), msg=f"first add: {r1!r}")
+        self.assertNotIn("error", (r2 or "").lower(), msg=f"second add: {r2!r}")
+
+        fib_sum = self.vapi.cli("show ip fib summary")
+        self.assertIn(
+            f"ipv4-VRF:{tid}",
+            fib_sum,
+            "unicast FIB for table should exist after both CLI adds",
+        )
+        mfib_sum = self.vapi.cli("show ip mfib summary")
+        self.assertIn(
+            f"ipv4-VRF:{tid}",
+            mfib_sum,
+            "MFIB for table should exist after second CLI add",
+        )
+
+    def test_ip_table_cli_bind_no_mfib_releases_prior_mfib(self):
+        """bind to full VRF then to no-mfib VRF; deleted VRF MFIB must go.
+
+        Regression for MFIB_SOURCE_INTERFACE leak and stale mfib_index_by_sw_if
+        when ip_table_bind skipped mfib_table_bind for NO_MFIB targets.
+        """
+        t_full = self.IP_TABLE_ID_1
+        t_no_mfib = self.IP_TABLE_ID_2
+        self.vapi.cli(f"ip table add {t_full}")
+        self.vapi.cli(f"set interface ip table {self.pg0.name} {t_full}")
+        self.vapi.cli(f"ip table add {t_no_mfib} no-mfib")
+        self.vapi.cli(f"set interface ip table {self.pg0.name} {t_no_mfib}")
+        self.vapi.cli(f"ip table del {t_full}")
+
+        mfib_sum = self.vapi.cli("show ip mfib summary")
+        self.assertNotIn(
+            f"ipv4-VRF:{t_full}",
+            mfib_sum,
+            "MFIB for a deleted table must not remain after rebinding the "
+            "interface to a no-mfib table (interface lock must be dropped)",
+        )
+
+    def test_ip6_table_cli_no_mfib_then_full_mfib(self):
+        """ip6 table add <id> no-mfib then ip6 table add <id>."""
+        tid = self.IP_TABLE_ID_1
+        r1 = self.vapi.cli(f"ip6 table add {tid} no-mfib")
+        r2 = self.vapi.cli(f"ip6 table add {tid}")
+        self.assertNotIn("error", (r1 or "").lower(), msg=f"first add: {r1!r}")
+        self.assertNotIn("error", (r2 or "").lower(), msg=f"second add: {r2!r}")
+
+    def test_ip6_table_cli_bind_no_mfib_releases_prior_mfib(self):
+        """set interface ip6 table through full VRF then no-mfib VRF."""
+        t_full = self.IP_TABLE_ID_1
+        t_no_mfib = self.IP_TABLE_ID_2
+        self.vapi.cli(f"ip6 table add {t_full}")
+        self.vapi.cli(f"set interface ip6 table {self.pg0.name} {t_full}")
+        self.vapi.cli(f"ip6 table add {t_no_mfib} no-mfib")
+        self.vapi.cli(f"set interface ip6 table {self.pg0.name} {t_no_mfib}")
+        self.vapi.cli(f"ip6 table del {t_full}")
+
+        mfib_sum = self.vapi.cli("show ip6 mfib summary")
+        self.assertNotIn(
+            f"ipv6-VRF:{t_full}",
+            mfib_sum,
+            "IPv6 MFIB for a deleted table must not remain after rebinding "
+            "to a no-mfib table",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main(testRunner=VppTestRunner)