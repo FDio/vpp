FDio/vpp

commit cb9cadad578297ffd78fa8a33670bdf1ab669e7e

Initial commit of vpp code.
Change-Id: Ib246f1fbfce93274020ee93ce461e3d8bd8b9f17
Signed-off-by: Ed Warnicke <eaw@cisco.com>

vppinfra/vppinfra/test_format.c
@@ -1,0 +1,186 @@
+/*
+ * Copyright (c) 2015 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+  Copyright (c) 2001, 2002, 2003 Eliot Dresselhaus
+
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include <vppinfra/format.h>
+
+static int verbose;
+static u8 * test_vec;
+
+static u8 * format_test1 (u8 * s, va_list * va)
+{
+  uword x = va_arg (*va, uword);
+  f64 y = va_arg (*va, f64);
+  return format (s, "%12d %12f%12.4e", x, y, y);
+}
+
+static int expectation (const char * exp, char * fmt, ...)
+{
+  int ret = 0;
+
+  va_list va;
+  va_start (va, fmt);
+  test_vec = va_format (test_vec, fmt, &va);
+  va_end (va);
+
+  vec_add1(test_vec, 0);
+  if (strcmp(exp, (char *) test_vec))
+    {
+      fformat (stdout, "FAIL: %s (expected vs. result)\n\"%s\"\n\"%v\"\n",
+	       fmt, exp, test_vec);
+      ret = 1;
+    }
+  else if (verbose)
+    fformat (stdout, "PASS: %s\n", fmt);
+  vec_delete (test_vec, vec_len(test_vec), 0);
+  return ret;
+}
+
+int test_format_main (unformat_input_t * input)
+{
+  int ret = 0;
+  u8 * food = format (0, "food");
+
+  ret |= expectation ("foo", "foo");
+  ret |= expectation ("foo", "%s", "foo");
+  ret |= expectation ("9876", "%d", 9876);
+  ret |= expectation ("-9876", "%wd", (word) -9876);
+  ret |= expectation ("98765432", "%u", 98765432);
+  ret |= expectation ("1200ffee", "%x", 0x1200ffee);
+  ret |= expectation ("BABEBABE", "%X", 0xbabebabe);
+  ret |= expectation ("10%a", "%d%%%c", 10, 'a');
+  ret |= expectation ("123456789abcdef0", "%016Lx", 0x123456789abcdef0LL);
+  ret |= expectation ("00000123", "%08x", 0x123);
+  ret |= expectation ("             23           23    2.3037e1",
+		      "%40U", format_test1, 23, 23.0367);
+  ret |= expectation ("left      ", "%-10s", "left");
+  ret |= expectation ("  center  ", "%=10s", "center");
+  ret |= expectation ("     right", "%+10s", "right");
+  ret |= expectation ("123456", "%.0f", 123456.);
+  ret |= expectation ("1234567.0", "%.1f", 1234567.);
+  ret |= expectation ("foo", "%.*s", 3, "food");
+  ret |= expectation ("food      ", "%.*s", 10, "food          ");
+  ret |= expectation ("(nil)", "%.*s", 3, (void *) 0);
+  ret |= expectation ("foo", "%.*v", 3, food);
+  ret |= expectation ("foobar", "%.*v%s", 3, food, "bar");
+  ret |= expectation ("foo bar", "%S", "foo_bar");
+  vec_free (food);
+  vec_free (test_vec);
+  return ret;
+}
+
+typedef struct {
+  int a, b;
+} foo_t;
+
+static u8 * format_foo (u8 * s, va_list * va)
+{
+  foo_t * foo = va_arg (*va, foo_t *);
+  return format (s, "{a %d, b %d}", foo->a, foo->b);
+}
+
+static uword unformat_foo (unformat_input_t * i, va_list * va)
+{
+  foo_t * foo = va_arg (*va, foo_t *);
+  return unformat (i, "{%D,%D}",
+		   sizeof (foo->a), &foo->a,
+		   sizeof (foo->b), &foo->b);
+}
+
+int test_unformat_main (unformat_input_t * input)
+{
+  u32 v[8];
+  long l;
+  long long ll;
+  f64 f;
+  u8 * s;
+  foo_t foo = {.a = ~0, .b = ~0};
+
+  v[0] = v[1] = 0;
+
+  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (input, "01 %d %d", &v[0], &v[1]))
+	fformat (stdout, "got 01 %d %d\n", v[0], v[1]);
+      else if (unformat (input, "d %d", &v[0]))
+	fformat (stdout, "got it d %d\n", v[0]);
+      else if (unformat (input, "ld %ld", &l))
+	fformat (stdout, "got it ld %ld\n", l);
+      else if (unformat (input, "lld %lld", &ll))
+	fformat (stdout, "got it lld %lld\n", ll);
+      else if (unformat (input, "string %s", &s))
+	fformat (stdout, "got string `%s'\n", s);
+      else if (unformat (input, "float %f", &f))
+	fformat (stdout, "got float `%.4f'\n", f);
+      else if (unformat (input, "foo %U", unformat_foo, &foo))
+	fformat (stdout, "got a foo `%U'\n", format_foo, &foo);
+      else if (unformat (input, "ignore-me1"))
+	fformat (stdout, "got an `ignore-me1'\n");
+      else if (unformat (input, "ignore-me2"))
+	fformat (stdout, "got an `ignore-me2'\n");
+      else if (unformat (input, "gi%d_%d@-", &v[0], &v[1]))
+	fformat (stdout, "got `gi%d_%d@-'\n", v[0], v[1]);
+      else if (unformat (input, "%_%d.%d.%d.%d%_->%_%d.%d.%d.%d%_",
+			 &v[0], &v[1], &v[2], &v[3],
+			 &v[4], &v[5], &v[6], &v[7]))
+	fformat (stdout, "got %d.%d.%d.%d -> %d.%d.%d.%d",
+		 v[0], v[1], v[2], v[3],
+		 v[4], v[5], v[6], v[7]);
+      else
+	{
+	  clib_warning ("unknown input `%U'\n",
+			format_unformat_error, input);
+	  return 1;
+	}
+    }
+
+  return 0;
+}
+
+#ifdef CLIB_UNIX
+int main (int argc, char * argv [])
+{
+  unformat_input_t i;
+
+  verbose = (argc > 1);
+  unformat_init_command_line (&i, argv);
+
+  if (unformat (&i, "unformat"))
+    return test_unformat_main (&i);
+  else
+    return test_format_main (&i);
+}
+#endif

commit a2dcad2123cb6633e533c462c38615006e48944c

vppinfra: error for out of range integer during unformat
Instead of silently truncating in unformat_integer, which is called
when integer format specifiers are used, check the value fits into the
range of the destination type and error if not.

Type: fix
Fixes: cb9cadad578297ffd78fa8a33670bdf1ab669e7e
Change-Id: I6d77d7ad2378caa201213756ad5f877819ace524
Signed-off-by: Rob Shearman <robertshearman@gmail.com>

src/vppinfra/unformat.c
@@ -379,6 +379,51 @@ unformat_line_input (unformat_input_t * i, va_list * va)
 #define UNFORMAT_INTEGER_SIGNED		1
 #define UNFORMAT_INTEGER_UNSIGNED	0
 
+/* True if the parsed value fits the destination without truncation.
+   For signed formats @value is the value before unary minus. */
+static int
+unformat_integer_value_ok (uword value, uword sign, uword is_signed, uword data_bytes)
+{
+  if (is_signed == UNFORMAT_INTEGER_UNSIGNED)
+    {
+      switch (data_bytes)
+	{
+	case 1:
+	  return value <= CLIB_U8_MAX;
+	case 2:
+	  return value <= CLIB_U16_MAX;
+	case 4:
+	  return value <= CLIB_U32_MAX;
+	case 8:
+	  return value <= CLIB_U64_MAX;
+	default:
+	  return 0;
+	}
+    }
+
+  switch (data_bytes)
+    {
+    case 1:
+      if (!sign)
+	return value <= CLIB_I8_MAX;
+      return value <= (uword) CLIB_I8_MAX + 1;
+    case 2:
+      if (!sign)
+	return value <= CLIB_I16_MAX;
+      return value <= (uword) CLIB_I16_MAX + 1;
+    case 4:
+      if (!sign)
+	return value <= CLIB_I32_MAX;
+      return value <= (uword) CLIB_I32_MAX + 1;
+    case 8:
+      if (!sign)
+	return value <= CLIB_I64_MAX;
+      return value <= (uword) CLIB_I64_MAX + 1;
+    default:
+      return 0;
+    }
+}
+
 static uword
 unformat_integer (unformat_input_t * input,
 		  va_list * va, uword base, uword is_signed, uword data_bytes)
@@ -464,35 +509,40 @@ unformat_integer (unformat_input_t * input,
     }
 
 done:
-  if (sign)
-    value = -value;
-
   if (n_digits > 0)
     {
-      void *v = va_arg (*va, void *);
-
       if (data_bytes == ~0)
 	data_bytes = sizeof (int);
 
-      switch (data_bytes)
-	{
-	case 1:
-	  *(u8 *) v = value;
-	  break;
-	case 2:
-	  *(u16 *) v = value;
-	  break;
-	case 4:
-	  *(u32 *) v = value;
-	  break;
-	case 8:
-	  *(u64 *) v = value;
-	  break;
-	default:
-	  goto error;
-	}
+      if (!unformat_integer_value_ok (value, sign, is_signed, data_bytes))
+	goto error;
 
-      return 1;
+      if (sign)
+	value = -value;
+
+      {
+	void *v = va_arg (*va, void *);
+
+	switch (data_bytes)
+	  {
+	  case 1:
+	    *(u8 *) v = value;
+	    break;
+	  case 2:
+	    *(u16 *) v = value;
+	    break;
+	  case 4:
+	    *(u32 *) v = value;
+	    break;
+	  case 8:
+	    *(u64 *) v = value;
+	    break;
+	  default:
+	    goto error;
+	  }
+
+	return 1;
+      }
     }
 
 error:

src/vppinfra/test_format.c
@@ -55,8 +55,8 @@ test_format_main (unformat_input_t * input)
   ret |= expectation ("10%a", "%d%%%c", 10, 'a');
   ret |= expectation ("123456789abcdef0", "%016Lx", 0x123456789abcdef0LL);
   ret |= expectation ("00000123", "%08x", 0x123);
-  ret |= expectation ("             23           23    2.3037e1",
-		      "%40U", format_test1, 23, 23.0367);
+  ret |=
+    expectation ("             23    23.036700    2.3036e1", "%40U", format_test1, 23, 23.0367);
   ret |= expectation ("left      ", "%-10s", "left");
   ret |= expectation ("  center  ", "%=10s", "center");
   ret |= expectation ("     right", "%+10s", "right");
@@ -70,6 +70,94 @@ test_format_main (unformat_input_t * input)
   ret |= expectation ("foo bar", "%S", "foo_bar");
   vec_free (food);
   vec_free (test_vec);
+  return ret;
+}
+
+/* Integer unformat accumulates in uword then stores to u8/u16/u32/u64.
+   Values wider than the destination must be rejected (no silent truncation). */
+static int
+test_unformat_integer_width (void)
+{
+  int ret = 0;
+  unformat_input_t in;
+  u32 u, a0, a1, a2, a3;
+
+#if uword_bits == 64
+#define U64_2_32 "4294967296"
+  unformat_init_cstring (&in, U64_2_32);
+  u = 0xdeadbeef;
+  if (unformat (&in, "%u", &u))
+    {
+      fformat (stdout, "FAIL: %%u " U64_2_32 " must be rejected (got u=%u)\n", u);
+      ret = 1;
+    }
+  unformat_free (&in);
+
+  unformat_init_cstring (&in, U64_2_32);
+  u = 0xdeadbeef;
+  if (unformat (&in, "%d", &u))
+    {
+      fformat (stdout, "FAIL: %%d " U64_2_32 " must be rejected\n");
+      ret = 1;
+    }
+  unformat_free (&in);
+
+  unformat_init_cstring (&in, "4294967297");
+  u = 0xdeadbeef;
+  if (unformat (&in, "%d", &u))
+    {
+      fformat (stdout, "FAIL: %%d 4294967297 must be rejected\n");
+      ret = 1;
+    }
+  unformat_free (&in);
+
+  unformat_init_cstring (&in, U64_2_32 ".0.0.1");
+  a0 = a1 = a2 = a3 = 111;
+  if (unformat (&in, "%d.%d.%d.%d", &a0, &a1, &a2, &a3))
+    {
+      fformat (stdout, "FAIL: first octet " U64_2_32 " must not parse as u32 octet\n");
+      ret = 1;
+    }
+  unformat_free (&in);
+#undef U64_2_32
+#endif /* uword_bits == 64 */
+
+  unformat_init_cstring (&in, "4294967295");
+  u = 0;
+  if (!unformat (&in, "%u", &u) || u != 4294967295u)
+    {
+      fformat (stdout, "FAIL: %%u UINT32_MAX decimal\n");
+      ret = 1;
+    }
+  unformat_free (&in);
+
+  unformat_init_cstring (&in, "4294967295");
+  u = 0;
+  if (unformat (&in, "%d", &u))
+    {
+      fformat (stdout, "FAIL: %%d 4294967295 must be rejected for int store\n");
+      ret = 1;
+    }
+  unformat_free (&in);
+
+  unformat_init_cstring (&in, "2147483647");
+  u = 0;
+  if (!unformat (&in, "%d", &u) || u != 2147483647u)
+    {
+      fformat (stdout, "FAIL: %%d INT32_MAX\n");
+      ret = 1;
+    }
+  unformat_free (&in);
+
+  unformat_init_cstring (&in, "2147483648");
+  u = 0;
+  if (unformat (&in, "%d", &u))
+    {
+      fformat (stdout, "FAIL: %%d 2147483648 must be rejected\n");
+      ret = 1;
+    }
+  unformat_free (&in);
+
   return ret;
 }
 
@@ -147,6 +235,7 @@ int
 main (int argc, char *argv[])
 {
   unformat_input_t i;
+  int ret;
 
   clib_mem_init (0, 3ULL << 30);
 
@@ -156,6 +245,10 @@ main (int argc, char *argv[])
   if (unformat (&i, "unformat"))
     return test_unformat_main (&i);
   else
-    return test_format_main (&i);
+    {
+      ret = test_format_main (&i);
+      ret |= test_unformat_integer_width ();
+      return ret;
+    }
 }
 #endif