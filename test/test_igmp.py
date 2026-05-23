FDio/vpp

commit 947ea6222dad1ef04595c34273e9231395aef443

IGMP improvements
- Enable/Disable an interface for IGMP
- improve logging
- refactor common code
- no orphaned timers
- IGMP state changes in main thread only
- Large groups split over multiple state-change reports
- SSM range configuration API.
- more tests

Change-Id: If5674f1044e7e97274a711f47807c9ba689d7b9a
Signed-off-by: Neale Ranns <nranns@cisco.com>

src/vnet/ip/igmp_packet.h
@@ -63,32 +63,46 @@ typedef enum
 #define _(n,f) IGMP_TYPE_##f = n,
   foreach_igmp_type
 #undef _
-} igmp_type_t;
+} __attribute__ ((packed)) igmp_type_t;
 
 typedef struct
 {
-  igmp_type_t type:8;
+  igmp_type_t type;
 
   u8 code;
 
   u16 checksum;
 } igmp_header_t;
 
-typedef struct
+/**
+ * Calculate the maximum response time allowed from the header.
+ *  - RFC 3367 Section 4.1.1
+ */
+always_inline f64
+igmp_header_get_max_resp_time (const igmp_header_t * header)
 {
-  /* membership_query, version <= 2 reports. */
-  igmp_header_t header;
+  f64 qqi;
 
-  /* Multicast destination address. */
-  ip4_address_t dst;
-} igmp_message_t;
+  if (header->code < 128)
+    qqi = header->code;
+  else
+    {
+      u8 mant = header->code << 4;
+      u8 exp = (header->code & 0x7) << 1;
+
+      qqi = ((mant | 0x10) << (exp + 3));
+    }
+
+  /* Querier's Query Interval (QQI), is represented in units of seconds */
+  return (qqi / 10);
+}
 
 typedef struct
 {
   /* type 0x11 (IGMPv3) */
   igmp_header_t header;
 
-  ip4_address_t dst;
+  ip4_address_t group_address;
 
   /* Reserved, Suppress Router-Side Processing flag and
      Querier's Robustness Variable RRRRSQQQ. */
@@ -101,11 +115,25 @@ typedef struct
   ip4_address_t src_addresses[0];
 } igmp_membership_query_v3_t;
 
+always_inline u32
+igmp_membership_query_v3_length (const igmp_membership_query_v3_t * q)
+{
+  return (sizeof (*q) +
+	  (sizeof (ip4_address_t) *
+	   clib_net_to_host_u16 (q->n_src_addresses)));
+}
+
+always_inline int
+igmp_membership_query_v3_is_geeral (const igmp_membership_query_v3_t * q)
+{
+  return (0 == q->group_address.as_u32);
+}
+
 #define foreach_igmp_membership_group_v3_type	\
-  _ (1, mode_is_filter_include)			\
-  _ (2, mode_is_filter_exclude)			\
-  _ (3, change_to_filter_include)		\
-  _ (4, change_to_filter_exclude)		\
+  _ (1, mode_is_include)			\
+  _ (2, mode_is_exclude)			\
+  _ (3, change_to_include)                      \
+  _ (4, change_to_exclude)                      \
   _ (5, allow_new_sources)			\
   _ (6, block_old_sources)
 
@@ -114,11 +142,11 @@ typedef enum
 #define _(n,f) IGMP_MEMBERSHIP_GROUP_##f = n,
   foreach_igmp_membership_group_v3_type
 #undef _
-} igmp_membership_group_v3_type_t;
+} __attribute__ ((packed)) igmp_membership_group_v3_type_t;
 
 typedef struct
 {
-  igmp_membership_group_v3_type_t type:8;
+  igmp_membership_group_v3_type_t type;
 
   /* Number of 32 bit words of aux data after source addresses. */
   u8 n_aux_u32s;
@@ -126,11 +154,19 @@ typedef struct
   /* Number of source addresses that follow. */
   u16 n_src_addresses;
 
-  /* Destination multicast address. */
-  ip4_address_t dst_address;
+  /* Destination multicast group address. */
+  ip4_address_t group_address;
 
   ip4_address_t src_addresses[0];
 } igmp_membership_group_v3_t;
+
+always_inline u32
+igmp_membership_group_v3_length (const igmp_membership_group_v3_t * g)
+{
+  return (sizeof (*g) +
+	  (sizeof (ip4_address_t) *
+	   clib_net_to_host_u16 (g->n_src_addresses)));
+}
 
 always_inline igmp_membership_group_v3_t *
 igmp_membership_group_v3_next (igmp_membership_group_v3_t * g)
@@ -152,6 +188,24 @@ typedef struct
 
   igmp_membership_group_v3_t groups[0];
 } igmp_membership_report_v3_t;
+
+always_inline u32
+igmp_membership_report_v3_length (const igmp_membership_report_v3_t * r)
+{
+  const igmp_membership_group_v3_t *g;
+  u32 len, ii, glen;
+
+  len = sizeof (igmp_membership_report_v3_t);
+  g = r->groups;
+
+  for (ii = 0; ii < clib_net_to_host_u16 (r->n_groups); ii++)
+    {
+      glen = igmp_membership_group_v3_length (g);
+      g = (const igmp_membership_group_v3_t *) (((u8 *) g) + glen);
+      len += glen;
+    }
+  return (len);
+}
 
 /* IP6 flavor of IGMP is called MLD which is embedded in ICMP6. */
 typedef struct

src/plugins/igmp.am
@@ -15,10 +15,18 @@ vppplugins_LTLIBRARIES += igmp_plugin.la
 
 igmp_plugin_la_SOURCES =		\
 	igmp/igmp.c			\
-	igmp/cli.c			\
+	igmp/igmp_query.c		\
+	igmp/igmp_report.c		\
+	igmp/igmp_group.c		\
+	igmp/igmp_src.c			\
+	igmp/igmp_config.c		\
+	igmp/igmp_cli.c			\
 	igmp/igmp_api.c			\
+	igmp/igmp_input.c		\
 	igmp/igmp_plugin.api.h		\
-	igmp/input.c			\
+	igmp/igmp_timer.c		\
+	igmp/igmp_pkt.c			\
+	igmp/igmp_ssm_range.c		\
 	igmp/igmp_format.c
 
 nobase_apiinclude_HEADERS += 		\

src/plugins/igmp/igmp.api
@@ -1,3 +1,4 @@
+/* Hey Emacs use -*- mode: C -*- */
 /*
  *------------------------------------------------------------------
  * Copyright (c) 2017 Cisco and/or its affiliates.
@@ -16,51 +17,92 @@
  */
 
 option version = "1.0.0";
+import "vnet/ip/ip_types.api";
 
-/** \brief 
-    Used by a 'host' to enable the recption/listening of packets for a specific
-    multicast group
-    @param client_index - opaque cookie to identify the sender
-    @param context - sender context, to match reply w/ request
-    @param enable - if set, enable igmp messages on configuration
-    @param sw_if_index - interface sw index
-    @param saddr - source address
-    @param gaddr - group address
-*/
+/**
+ * @brief Filter mode
+ */
+enum filter_mode {
+  EXCLUDE = 0,
+  INCLUDE = 1,
+};
+
+
+/**
+ * @brief
+ *   Used by a 'host' to enable the reception/listening of packets for a specific
+ *   multicast group
+ *
+ *  For each socket on which IPMulticastListen has been invoked, the
+ *  system records the desired multicast reception state for that socket.
+ *  That state conceptually consists of a set of records of the form:
+ *
+ *     (interface, multicast-address, filter-mode, source-list)
+ *
+ *  The socket state evolves in response to each invocation of
+ *  IPMulticastListen on the socket, as follows:
+ *
+ *  o If the requested filter mode is INCLUDE *and* the requested source
+ *    list is empty, then the entry corresponding to the requested
+ *    interface and multicast address is deleted if present.  If no such
+ *    entry is present, the request is ignored.
+ *
+ *  o If the requested filter mode is EXCLUDE *or* the requested source
+ *    list is non-empty, then the entry corresponding to the requested
+ *    interface and multicast address, if present, is changed to contain
+ *    the requested filter mode and source list.  If no such entry is
+ *    present, a new entry is created, using the parameters specified in
+ *    the request.
+ *
+ *   @param client_index - opaque cookie to identify the sender
+ *   @param context - sender context, to match reply w/ request
+ *   @param sw_if_index - interface sw index
+ *   @param filter - filter mode
+ *   @param saddr - source address
+ *   @param gaddr - group address
+ */
+typeonly define igmp_group
+{
+  vl_api_filter_mode_t filter;
+  u8 n_srcs;
+  u32 sw_if_index;
+  vl_api_ip4_address_t gaddr;
+  vl_api_ip4_address_t saddrs[n_srcs];
+};
 autoreply define igmp_listen
 {
   u32 client_index;
   u32 context;
 
-  u8 enable;
-  u32 sw_if_index;
-  u8 saddr[4];
-  u8 gaddr[4];
+  vl_api_igmp_group_t group;
 };
 
-/** \brief 
-    Used by a 'router' to enable the recption of IGMP packets and the
-    construction of group state for hosts on the link
-    multicast group
-    @param client_index - opaque cookie to identify the sender
-    @param context - sender context, to match reply w/ request
-    @param enable - if set, enable igmp messages on configuration
-    @param sw_if_index - interface sw index
-*/
+/**
+ * @brief
+ *   Used by a 'router' and 'host' to enable the recption of IGMP packets.
+ *   For hosts this must be called once before igmp_listen.
+ *
+ *   @param client_index - opaque cookie to identify the sender
+ *   @param context - sender context, to match reply w/ request
+ *   @param enable - if set, enable igmp messages on configuration
+ *   @param mode - Host (1) or router (0) mode
+ *   @param sw_if_index - interface sw index
+ */
 autoreply define igmp_enable_disable
 {
   u32 client_index;
   u32 context;
 
   u8 enable;
+  u8 mode;
   u32 sw_if_index;
 };
 
-/** \brief dump (S,G)s from interface
-    @param client_index - opaque cookie to identify the sender
-    @param context - sender context, to match reply w/ request
-    @param sw_if_index - interface sw index
-    @param dump_all - get (S,G)s from all interfaces
+/**
+ * @brief dump (S,G)s from interface
+ * @param client_index - opaque cookie to identify the sender
+ * @param context - sender context, to match reply w/ request
+ * @param sw_if_index - interface sw index (~0 for all)
 */
 define igmp_dump
 {
@@ -68,22 +110,22 @@ define igmp_dump
   u32 context;
 
   u32 sw_if_index;
-  u8 dump_all;
 };
 
-/** \brief igmp details
-    @param context - sender context, to match reply w/ request
-    @param sw_if_index - interface sw index
-    @param saddr - source address
-    @param gaddr - group address
-*/
+/**
+ * @brief igmp details
+ * @param context - sender context, to match reply w/ request
+ * @param sw_if_index - interface sw index
+ * @param saddr - source address
+ * @param gaddr - group address
+ */
 define igmp_details
 {
   u32 context;
 
   u32 sw_if_index;
-  u8 saddr[4];
-  u8 gaddr[4];
+  vl_api_ip4_address_t saddr;
+  vl_api_ip4_address_t gaddr;
 };
 
 /** \brief remove all (S,G)s from an interface
@@ -99,12 +141,13 @@ autoreply define igmp_clear_interface
   u32 sw_if_index;
 };
 
-/** \brief register for igmp events
-    @param client_index - opaque cookie to identify the sender
-    @param context - sender context, to match reply w/ request
-    @param pid - sender's pid
-    @param enable - 1 enable, 0 disable igmp events
-*/
+/**
+ * @brief register for igmp events
+ * @param client_index - opaque cookie to identify the sender
+ * @param context - sender context, to match reply w/ request
+ * @param pid - sender's pid
+ * @param enable - 1 enable, 0 disable igmp events
+ */
 autoreply define want_igmp_events
 {
   u32 client_index;
@@ -119,23 +162,69 @@ service {
     events igmp_event;
 };
 
-/** \brief igmp event details
-    @param client_index - opaque cookie to identify the sender
-    @param context - sender context, to match reply w/ request
-    @param sw_if_index - interface sw index
-    @param saddr - source address
-    @param gaddr - group address
-    @param is_join - if set source is joining the group, else leaving
-*/
+/**
+ * @brief igmp event details
+ * @param client_index - opaque cookie to identify the sender
+ * @param context - sender context, to match reply w/ request
+ * @param sw_if_index - interface sw index
+ * @param saddr - source address
+ * @param gaddr - group address
+ *@param filter - filter mode
+ */
 define igmp_event
 {
   u32 context;
 
   u32 sw_if_index;
-  u8 saddr[4];
-  u8 gaddr[4];
-  u8 is_join;
+  vl_api_filter_mode_t filter;
+  vl_api_ip4_address_t saddr;
+  vl_api_ip4_address_t gaddr;
 };
+
+/**
+ * @brief enum to specify either ASM or SSM semantics
+ */
+enum group_prefix_type
+{
+  ASM = 0,
+  SSM = 1,
+};
+
+/**
+ * @brief Definition of a Group prefix and its type
+ */
+typedef group_prefix
+{
+  vl_api_group_prefix_type_t type;
+  vl_api_prefix_t prefix;
+};
+
+/**
+ * @brief Configure a prefix for SSM or ASM semantics
+ * @param address - Prefix address
+ * @param address_length - Prefix length
+ */
+autoreply define igmp_group_prefix_set
+{
+  u32 client_index;
+  u32 context;
+
+  vl_api_group_prefix_t gp;
+};
+
+define igmp_group_prefix_dump
+{
+  u32 client_index;
+  u32 context;
+};
+
+define igmp_group_prefix_details
+{
+  u32 context;
+
+  vl_api_group_prefix_t gp;
+};
+
 
 /*
  * Local Variables:

commit 09b8159f0fa903c73f96da542f76e5e7c8d6dead

ip: fix IGMPv3 MR validation and parsing with aux data
If aux data is present in membership groups that are part of IGMPv3
membership reports it is ignored and not used for length validation or
when parsing membership groups, causing it to be incorrectly
interpreted as a following group.

Fix this by including the number of aux u32s as part of the membership
group length calculation. Also remove igmp_membership_group_v3_next
which is dead code and uses g->n_src_addresses in an endian-incorrect
way.

Also fix the calculation of the maximum response time by extracting
the correct bits for the mantissa and exponent, since for a value of
0xff the correct max response time is ~53 minutes whereas the previous
incorrect calculation yielded ~36 days.

Type: fix
Fixes: 947ea6222dad1ef04595c34273e9231395aef443
Change-Id: Ib434ffecbe26789c0feff5d3caa04f8815c803ab
Signed-off-by: Rob Shearman <robertshearman@gmail.com>

src/vnet/ip/igmp_packet.h
@@ -55,8 +55,14 @@ igmp_header_get_max_resp_time (const igmp_header_t * header)
     qqi = header->code;
   else
     {
-      u8 mant = header->code << 4;
-      u8 exp = (header->code & 0x7) << 1;
+      /*
+       *  0 1 2 3 4 5 6 7
+       * +-+-+-+-+-+-+-+-+
+       * |1| exp | mant  |
+       * +-+-+-+-+-+-+-+-+
+       */
+      u8 mant = header->code & 0xf;
+      u8 exp = (header->code >> 4) & 0x7;
 
       qqi = ((mant | 0x10) << (exp + 3));
     }
@@ -131,17 +137,8 @@ typedef struct
 always_inline u32
 igmp_membership_group_v3_length (const igmp_membership_group_v3_t * g)
 {
-  return (sizeof (*g) +
-	  (sizeof (ip4_address_t) *
-	   clib_net_to_host_u16 (g->n_src_addresses)));
-}
-
-always_inline igmp_membership_group_v3_t *
-igmp_membership_group_v3_next (igmp_membership_group_v3_t * g)
-{
-  return ((void *) g
-	  + g->n_src_addresses * sizeof (g->src_addresses[0])
-	  + g->n_aux_u32s * sizeof (u32));
+  return (sizeof (*g) + (sizeof (ip4_address_t) * clib_net_to_host_u16 (g->n_src_addresses))) +
+	 g->n_aux_u32s * sizeof (u32);
 }
 
 typedef struct

test/test_igmp.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import socket
+import struct
 import unittest
 
 from scapy.layers.l2 import Ether
@@ -1019,6 +1021,55 @@ class TestIgmp(VppTestCase):
         self.vapi.igmp_enable_disable(self.pg0.sw_if_index, 0, IGMP_MODE.HOST)
         self.vapi.igmp_enable_disable(self.pg2.sw_if_index, 0, IGMP_MODE.ROUTER)
         self.assertFalse(find_mroute(self, "239.1.1.1", "0.0.0.0", 32))
+
+    def test_igmpv3_report_aux_data(self):
+        """Two-group IGMPv3 report with aux data"""
+
+        # Craft aux so a mis-positioned "group 2" has n_src=0 (minimize inner reads).
+        fake_type = 2  # mode_is_exclude
+        fake_n_aux = 0
+        fake_n_src = 0
+        fake_group = socket.inet_aton("239.1.1.99")
+        aux = struct.pack("!BBH", fake_type, fake_n_aux, fake_n_src) + fake_group
+
+        group1 = (
+            IGMPv3gr(rtype=2, auxdlen=len(aux) // 4, maddr="239.1.1.1", srcaddrs=[])
+            / aux
+        )
+        group2 = IGMPv3gr(rtype=2, maddr="239.1.1.1")
+        pkt = (
+            Ether(src=self.pg0.remote_mac, dst=self.pg0.local_mac)
+            / IP(
+                src=self.pg0.remote_ip4,
+                dst="224.0.0.22",
+                proto=2,
+                ttl=1,
+                tos=0xC0,
+                options=[
+                    IPOption(
+                        copy_flag=1,
+                        optclass="control",
+                        option="router_alert",
+                        length=4,
+                    )
+                ],
+            )
+            / IGMPv3()
+            / IGMPv3mr(records=[group1, group2])
+        )
+        self.vapi.cli("test igmp timers query 1 src 3 leave 1")
+        self.vapi.igmp_enable_disable(self.pg0.sw_if_index, 1, IGMP_MODE.ROUTER)
+
+        self.send_and_assert_no_replies(
+            self.pg0,
+            pkt,
+            remark="IGMPv3 report with aux data and two group records",
+        )
+
+        # Spoofed second group (239.1.1.99) must not be learned from aux mis-parse.
+        dump = self.vapi.igmp_dump(self.pg0.sw_if_index)
+        for s in dump:
+            self.assertNotEqual(str(s.gaddr), "239.1.1.99")
 
 
 if __name__ == "__main__":