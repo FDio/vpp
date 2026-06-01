eac96997187edac6ce48d4a7f6820cd4d4400ab1
Kristof Provost <kprovost@netgate.com>
2026-06-01 15:12:13 +0000

    http_static: support disabling the server via the API
    Type: feature
    Change-Id: I65706103e043019987f5bdfa9d6de3df6f2cc504
    Signed-off-by: Kristof Provost <kprovost@netgate.com>

--- test/asf/test_http_static.py
+++ test/asf/test_http_static.py
@@ -110,6 +110,44 @@ class TestHttpStaticVapi(VppAsfTestCase):
         )
         self.assertIn(b"Hello world2", process.stdout)
 
+        self.vapi.http_static_disable_v5()
+        self.temp2.seek(0)
+        process = subprocess.run(
+            [
+                "ip",
+                "netns",
+                "exec",
+                self.ns_name,
+                "curl",
+                "--noproxy",
+                "10.10.1.2",
+                f"10.10.1.2/{self.temp2.name[5:]}",
+            ],
+            capture_output=True,
+        )
+        self.assertNotIn(b"Hello world2", process.stdout)
+
+        # Now enable again
+        self.vapi.http_static_enable_v5(
+            www_root="/tmp",
+            uri="tcp://0.0.0.0/80",
+        )
+        self.temp2.seek(0)
+        process = subprocess.run(
+            [
+                "ip",
+                "netns",
+                "exec",
+                self.ns_name,
+                "curl",
+                "--noproxy",
+                "10.10.1.2",
+                f"10.10.1.2/{self.temp2.name[5:]}",
+            ],
+            capture_output=True,
+        )
+        self.assertIn(b"Hello world2", process.stdout)
+
 
 @unittest.skipIf(
     "http_static" in config.excluded_plugins, "Exclude HTTP Static Server plugin tests"
