FDio/vpp

commit 5ea54cd1da80ebd2bcc27193368dbe5dea814c6f

ip: ip6 frags should have as much space for headers as original
Type: improvement

Change-Id: I10c924a391379151a09e2b81b364a4d796f751ec
Signed-off-by: Florin Coras <fcoras@cisco.com>

src/vnet/ip/ip_frag.c
@@ -442,8 +442,10 @@ ip6_frag_do_fragment (vlib_main_t * vm, u32 from_bi, u16 mtu,
       vec_add1 (*buffer, to_bi);
       frag_set_sw_if_index (to_b, org_from_b);
 
-      /* Copy ip6 header */
-      clib_memcpy_fast (to_b->data, org_from_packet,
+      /* Make sure we have as much space for headers as the original and copy
+       * ip6 header */
+      to_data = vlib_buffer_make_headroom (to_b, org_from_b->current_data);
+      clib_memcpy_fast (to_data, org_from_packet,
 			l2unfragmentablesize + sizeof (ip6_header_t));
       to_ip6 = vlib_buffer_get_current (to_b) + l2unfragmentablesize;
       to_frag_hdr = (ip6_frag_hdr_t *) (to_ip6 + 1);

commit ea75da0939d92e44517cc7a7ea070d4678aa10b0

ip: fix adjacent packet overwrite with ip6 frags
The call to vlib_buffer_make_headroom on each buffer allocated for an
ip6 fragment is problematic in two ways:
1. If org_from_b->current_data is positive then max doesn't take this
   into account so if mtu > 2048 this can result in a length that is too
   long and payload data overwriting the adjacent buffer with the next
   index.
2. If org_from_b->current_data is negative then since
   vlib_buffer_make_headroom takes a u8, the value is implicitly
   converted and becomes a large negative value. This case is observed in
   the test_ip6.TestIPv6PathMTU test. Again, with a large mtu this
   results in a length that is too long and the payload data overwriting
   the adjacent buffer with the next index.

Fix both of these by clamping the amount of headroom to make to a
minimum of 0 and then taking the headroom into account when computing
the max fragment payload length.

Type: fix
Fixes: 5ea54cd1da80ebd2bcc27193368dbe5dea814c6f
Change-Id: I3f8a4991f57a176d734f9a4b72ad564490f04b31
Signed-off-by: Rob Shearman <robertshearman@gmail.com>

src/vnet/ip/ip_frag.c
@@ -370,6 +370,7 @@ ip6_frag_do_fragment (vlib_main_t * vm, u32 from_bi, u16 mtu,
   u16 len, max, rem, ip_frag_id;
   u8 *org_from_packet;
   u16 head_bytes;
+  u8 headroom_bytes;
 
   from_b = vlib_get_buffer (vm, from_bi);
 
@@ -386,9 +387,10 @@ ip6_frag_do_fragment (vlib_main_t * vm, u32 from_bi, u16 mtu,
 
   head_bytes =
     (sizeof (ip6_header_t) + sizeof (ip6_frag_hdr_t) + l2unfragmentablesize);
+  headroom_bytes = clib_max (0, from_b->current_data);
   rem = clib_net_to_host_u16 (ip6->payload_length);
-  max = (clib_min (mtu, vlib_buffer_get_default_data_size (vm)) - head_bytes) &
-	~0x7;
+  max =
+    (clib_min (mtu, vlib_buffer_get_default_data_size (vm) - headroom_bytes) - head_bytes) & ~0x7;
 
   if (rem >
       (vlib_buffer_length_in_chain (vm, from_b) - sizeof (ip6_header_t)))
@@ -432,7 +434,7 @@ ip6_frag_do_fragment (vlib_main_t * vm, u32 from_bi, u16 mtu,
 
       /* Make sure we have as much space for headers as the original and copy
        * ip6 header */
-      to_data = vlib_buffer_make_headroom (to_b, org_from_b->current_data);
+      to_data = vlib_buffer_make_headroom (to_b, headroom_bytes);
       clib_memcpy_fast (to_data, org_from_packet,
 			l2unfragmentablesize + sizeof (ip6_header_t));
       to_ip6 = vlib_buffer_get_current (to_b) + l2unfragmentablesize;