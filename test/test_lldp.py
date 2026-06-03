FDio/vpp

commit de4582b4c25f30412a8eab5f7741e577a1387fec

VPP-344 Add LLDP feature
Change-Id: Iac96773e7f9028c0f09c3388893e69f67177439d
Signed-off-by: Klement Sekera <ksekera@cisco.com>

vnet/vnet/lldp/lldp_input.c
@@ -1,0 +1,257 @@
+/*
+ * Copyright (c) 2011-2016 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * @file
+ * @brief LLDP packet parsing implementation
+ */
+#include <vnet/lldp/lldp_node.h>
+#include <vnet/lldp/lldp_protocol.h>
+
+lldp_tlv_code_t
+lldp_tlv_get_code (const lldp_tlv_t * tlv)
+{
+  return tlv->head.byte1 >> 1;
+}
+
+void
+lldp_tlv_set_code (lldp_tlv_t * tlv, lldp_tlv_code_t code)
+{
+  tlv->head.byte1 = (tlv->head.byte1 & 1) + (code << 1);
+}
+
+u16
+lldp_tlv_get_length (const lldp_tlv_t * tlv)
+{
+  return (((u16) (tlv->head.byte1 & 1)) << 8) + tlv->head.byte2;
+}
+
+void
+lldp_tlv_set_length (lldp_tlv_t * tlv, u16 length)
+{
+  tlv->head.byte2 = length & ((1 << 8) - 1);
+  if (length > (1 << 8) - 1)
+    {
+      tlv->head.byte1 |= 1;
+    }
+  else
+    {
+      tlv->head.byte1 &= (1 << 8) - 2;
+    }
+}
+
+lldp_main_t lldp_main;
+
+static int
+lldp_packet_scan (lldp_main_t * lm, lldp_intf_t * n, const lldp_tlv_t * pkt)
+{
+  lldp_error_t e = LLDP_ERROR_NONE;
+  const lldp_tlv_t *tlv = pkt;
+
+/* first check if the header fits in before extracting data from it */
+#define TLV_VIOLATES_PKT_BOUNDARY(pkt, tlv)                               \
+    (((((u8 *)tlv) + sizeof(lldp_tlv_t)) > ((u8 *)pkt + vec_len(pkt))) || \
+     ((((u8 *)tlv) + lldp_tlv_get_length(tlv)) > ((u8 *)pkt + vec_len(pkt))))
+
+  /* first tlv is always chassis id, followed by port id and ttl tlvs */
+  if (TLV_VIOLATES_PKT_BOUNDARY (pkt, tlv) ||
+      LLDP_TLV_NAME (chassis_id) != lldp_tlv_get_code (tlv))
+    {
+      return LLDP_ERROR_BAD_TLV;
+    }
+
+  u16 l = lldp_tlv_get_length (tlv);
+  if (l < STRUCT_SIZE_OF (lldp_chassis_id_tlv_t, subtype) +
+      LLDP_MIN_CHASS_ID_LEN ||
+      l > STRUCT_SIZE_OF (lldp_chassis_id_tlv_t, subtype) +
+      LLDP_MAX_CHASS_ID_LEN)
+    {
+      return LLDP_ERROR_BAD_TLV;
+    }
+
+  u8 chid_subtype = ((lldp_chassis_id_tlv_t *) tlv)->subtype;
+  u8 *chid = ((lldp_chassis_id_tlv_t *) tlv)->id;
+  u8 chid_len = l - STRUCT_SIZE_OF (lldp_chassis_id_tlv_t, subtype);
+
+  tlv = (lldp_tlv_t *) ((u8 *) tlv + STRUCT_SIZE_OF (lldp_tlv_t, head) + l);
+
+  if (TLV_VIOLATES_PKT_BOUNDARY (pkt, tlv) ||
+      LLDP_TLV_NAME (port_id) != lldp_tlv_get_code (tlv))
+    {
+      return LLDP_ERROR_BAD_TLV;
+    }
+  l = lldp_tlv_get_length (tlv);
+  if (l < STRUCT_SIZE_OF (lldp_port_id_tlv_t, subtype) +
+      LLDP_MIN_PORT_ID_LEN ||
+      l > STRUCT_SIZE_OF (lldp_chassis_id_tlv_t, subtype) +
+      LLDP_MAX_PORT_ID_LEN)
+    {
+      return LLDP_ERROR_BAD_TLV;
+    }
+
+  u8 portid_subtype = ((lldp_port_id_tlv_t *) tlv)->subtype;
+  u8 *portid = ((lldp_port_id_tlv_t *) tlv)->id;
+  u8 portid_len = l - STRUCT_SIZE_OF (lldp_port_id_tlv_t, subtype);
+
+  tlv = (lldp_tlv_t *) ((u8 *) tlv + STRUCT_SIZE_OF (lldp_tlv_t, head) + l);
+
+  if (TLV_VIOLATES_PKT_BOUNDARY (pkt, tlv) ||
+      LLDP_TLV_NAME (ttl) != lldp_tlv_get_code (tlv))
+    {
+      return LLDP_ERROR_BAD_TLV;
+    }
+  l = lldp_tlv_get_length (tlv);
+  if (l != STRUCT_SIZE_OF (lldp_ttl_tlv_t, ttl))
+    {
+      return LLDP_ERROR_BAD_TLV;
+    }
+  u16 ttl = ntohs (((lldp_ttl_tlv_t *) tlv)->ttl);
+  tlv = (lldp_tlv_t *) ((u8 *) tlv + STRUCT_SIZE_OF (lldp_tlv_t, head) + l);
+  while (!TLV_VIOLATES_PKT_BOUNDARY (pkt, tlv) &&
+	 LLDP_TLV_NAME (pdu_end) != lldp_tlv_get_code (tlv))
+    {
+      switch (lldp_tlv_get_code (tlv))
+	{
+#define F(num, type, str)         \
+    case LLDP_TLV_NAME(type):     \
+        /* ignore optional TLV */ \
+        break;
+	  foreach_lldp_optional_tlv_type (F);
+#undef F
+	default:
+	  return LLDP_ERROR_BAD_TLV;
+	}
+      if (e)
+	{
+	  return e;
+	}
+      tlv = (lldp_tlv_t *) ((u8 *) tlv + STRUCT_SIZE_OF (lldp_tlv_t, head) +
+			    lldp_tlv_get_length (tlv));
+    }
+  /* last tlv is pdu_end */
+  if (TLV_VIOLATES_PKT_BOUNDARY (pkt, tlv) ||
+      LLDP_TLV_NAME (pdu_end) != lldp_tlv_get_code (tlv) ||
+      0 != lldp_tlv_get_length (tlv))
+    {
+      return LLDP_ERROR_BAD_TLV;
+    }
+  /* LLDP PDU validated, now store data */
+  if (n->chassis_id)
+    {
+      _vec_len (n->chassis_id) = 0;
+    }
+  vec_add (n->chassis_id, chid, chid_len);
+  n->chassis_id_subtype = chid_subtype;
+  if (n->port_id)
+    {
+      _vec_len (n->port_id) = 0;
+    }
+  vec_add (n->port_id, portid, portid_len);
+  n->port_id_subtype = portid_subtype;
+  n->ttl = ttl;
+  return LLDP_ERROR_NONE;
+}
+
+lldp_intf_t *
+lldp_get_intf (lldp_main_t * lm, u32 hw_if_index)
+{
+  uword *p = hash_get (lm->intf_by_hw_if_index, hw_if_index);
+
+  if (p)
+    {
+      return pool_elt_at_index (lm->intfs, p[0]);
+    }
+  return NULL;
+}
+
+lldp_intf_t *
+lldp_create_intf (lldp_main_t * lm, u32 hw_if_index)
+{
+
+  uword *p;
+  lldp_intf_t *n;
+  p = hash_get (lm->intf_by_hw_if_index, hw_if_index);
+
+  if (p == 0)
+    {
+      pool_get (lm->intfs, n);
+      memset (n, 0, sizeof (*n));
+      n->hw_if_index = hw_if_index;
+      hash_set (lm->intf_by_hw_if_index, n->hw_if_index, n - lm->intfs);
+    }
+  else
+    {
+      n = pool_elt_at_index (lm->intfs, p[0]);
+    }
+  return n;
+}
+
+/*
+ * lldp input routine
+ */
+lldp_error_t
+lldp_input (vlib_main_t * vm, vlib_buffer_t * b0, u32 bi0)
+{
+  lldp_main_t *lm = &lldp_main;
+  lldp_error_t e;
+
+  /* find our interface */
+  lldp_intf_t *n = lldp_get_intf (lm, vnet_buffer (b0)->sw_if_index[VLIB_RX]);
+
+  if (!n)
+    {
+      /* lldp disabled on this interface, we're done */
+      return LLDP_ERROR_DISABLED;
+    }
+
+  /* Actually scan the packet */
+  e = lldp_packet_scan (lm, n, vlib_buffer_get_current (b0));
+
+  if (LLDP_ERROR_NONE == e)
+    {
+      n->last_heard = vlib_time_now (vm);
+    }
+
+  return e;
+}
+
+/*
+ * setup function
+ */
+static clib_error_t *
+lldp_init (vlib_main_t * vm)
+{
+  clib_error_t *error;
+  lldp_main_t *lm = &lldp_main;
+
+  if ((error = vlib_call_init_function (vm, lldp_template_init)))
+    return error;
+
+  lm->vlib_main = vm;
+  lm->vnet_main = vnet_get_main ();
+  lm->msg_tx_hold = 4;		/* default value per IEEE 802.1AB-2009 */
+  lm->msg_tx_interval = 30;	/* default value per IEEE 802.1AB-2009 */
+
+  return 0;
+}
+
+VLIB_INIT_FUNCTION (lldp_init);
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

vnet/Makefile.am
@@ -230,6 +230,18 @@ nobase_include_HEADERS +=			\
   vnet/cdp/cdp_protocol.h
 
 ########################################
+# Layer 2 / LLDP
+########################################
+libvnet_la_SOURCES +=				\
+  vnet/lldp/lldp_input.c				\
+  vnet/lldp/lldp_node.c				\
+  vnet/lldp/lldp_output.c	\
+  vnet/lldp/lldp_cli.c
+
+nobase_include_HEADERS +=			\
+  vnet/lldp/lldp_protocol.h
+
+########################################
 # Layer 2/3 "classify"
 ########################################
 libvnet_la_SOURCES +=				\

vnet/vnet/lldp/dir.dox
@@ -1,0 +1,18 @@
+/*
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ @dir vnet/vnet/lldp
+ @brief Link Layer Discovery Protocol (LLDP) implementation
+*/

commit cd7d4cfbda8dd998db4fd9386514e19425f5edba

lldp: fix TLV validation
A bad packet with a TLV length inconsistent with the packet length can
cause a read beyond the end of the packet buffer due to the code
incorrectly assuming the packet header is a vec. Fix this by passing
the packet length into the TLV validation function and use it
accordingly instead of vec_len.

Type: fix
Fixes: de4582b4c25f30412a8eab5f7741e577a1387fec
Change-Id: I699740d94b36899c6133f1dc6203e406f8c5d908
Signed-off-by: Rob Shearman <robertshearman@gmail.com>

src/plugins/lldp/lldp_input.c
@@ -108,16 +108,17 @@ lldp_tlv_set_length (lldp_tlv_t * tlv, u16 length)
 lldp_main_t lldp_main;
 
 static int
-lldp_packet_scan (u32 hw_if_index, const lldp_tlv_t * pkt)
+lldp_packet_scan (u32 hw_if_index, const lldp_tlv_t *pkt, u32 pkt_len)
 {
   const lldp_tlv_t *tlv = pkt;
 
-#define TLV_VIOLATES_PKT_BOUNDARY(pkt, tlv)                               \
-  (((((u8 *)tlv) + sizeof (lldp_tlv_t)) > ((u8 *)pkt + vec_len (pkt))) || \
-   ((((u8 *)tlv) + lldp_tlv_get_length (tlv)) > ((u8 *)pkt + vec_len (pkt))))
+#define TLV_VIOLATES_PKT_BOUNDARY(pkt, pkt_len, tlv)                                               \
+  (((((u8 *) (tlv)) + sizeof (lldp_tlv_t)) > ((u8 *) (pkt) + (pkt_len))) ||                        \
+   ((((u8 *) (tlv)) + STRUCT_SIZE_OF (lldp_tlv_t, head) + lldp_tlv_get_length (tlv)) >             \
+    ((u8 *) (pkt) + (pkt_len))))
 
   /* first tlv is always chassis id, followed by port id and ttl tlvs */
-  if (TLV_VIOLATES_PKT_BOUNDARY (pkt, tlv) ||
+  if (TLV_VIOLATES_PKT_BOUNDARY (pkt, pkt_len, tlv) ||
       LLDP_TLV_NAME (chassis_id) != lldp_tlv_get_code (tlv))
     {
       return LLDP_ERROR_BAD_TLV;
@@ -138,7 +139,7 @@ lldp_packet_scan (u32 hw_if_index, const lldp_tlv_t * pkt)
 
   tlv = (lldp_tlv_t *) ((u8 *) tlv + STRUCT_SIZE_OF (lldp_tlv_t, head) + l);
 
-  if (TLV_VIOLATES_PKT_BOUNDARY (pkt, tlv) ||
+  if (TLV_VIOLATES_PKT_BOUNDARY (pkt, pkt_len, tlv) ||
       LLDP_TLV_NAME (port_id) != lldp_tlv_get_code (tlv))
     {
       return LLDP_ERROR_BAD_TLV;
@@ -158,7 +159,7 @@ lldp_packet_scan (u32 hw_if_index, const lldp_tlv_t * pkt)
 
   tlv = (lldp_tlv_t *) ((u8 *) tlv + STRUCT_SIZE_OF (lldp_tlv_t, head) + l);
 
-  if (TLV_VIOLATES_PKT_BOUNDARY (pkt, tlv) ||
+  if (TLV_VIOLATES_PKT_BOUNDARY (pkt, pkt_len, tlv) ||
       LLDP_TLV_NAME (ttl) != lldp_tlv_get_code (tlv))
     {
       return LLDP_ERROR_BAD_TLV;
@@ -170,7 +171,7 @@ lldp_packet_scan (u32 hw_if_index, const lldp_tlv_t * pkt)
     }
   u16 ttl = ntohs (((lldp_ttl_tlv_t *) tlv)->ttl);
   tlv = (lldp_tlv_t *) ((u8 *) tlv + STRUCT_SIZE_OF (lldp_tlv_t, head) + l);
-  while (!TLV_VIOLATES_PKT_BOUNDARY (pkt, tlv) &&
+  while (!TLV_VIOLATES_PKT_BOUNDARY (pkt, pkt_len, tlv) &&
 	 LLDP_TLV_NAME (pdu_end) != lldp_tlv_get_code (tlv))
     {
       switch (lldp_tlv_get_code (tlv))
@@ -188,9 +189,8 @@ lldp_packet_scan (u32 hw_if_index, const lldp_tlv_t * pkt)
 			    lldp_tlv_get_length (tlv));
     }
   /* last tlv is pdu_end */
-  if (TLV_VIOLATES_PKT_BOUNDARY (pkt, tlv) ||
-      LLDP_TLV_NAME (pdu_end) != lldp_tlv_get_code (tlv) ||
-      0 != lldp_tlv_get_length (tlv))
+  if (TLV_VIOLATES_PKT_BOUNDARY (pkt, pkt_len, tlv) ||
+      LLDP_TLV_NAME (pdu_end) != lldp_tlv_get_code (tlv) || 0 != lldp_tlv_get_length (tlv))
     {
       return LLDP_ERROR_BAD_TLV;
     }
@@ -256,8 +256,8 @@ lldp_input (vlib_main_t * vm, vlib_buffer_t * b0, u32 bi0)
     }
 
   /* Actually scan the packet */
-  e = lldp_packet_scan (sw_interface->hw_if_index,
-			vlib_buffer_get_current (b0));
+  e = lldp_packet_scan (sw_interface->hw_if_index, vlib_buffer_get_current (b0),
+			vlib_buffer_length_in_chain (vm, b0));
 
   return e;
 }

test/test_lldp.py
@@ -1,8 +1,10 @@
 from asfframework import VppTestRunner
 from framework import VppTestCase
 import unittest
+from struct import pack
 from config import config
 from scapy.layers.l2 import Ether
+from scapy.packet import Raw
 from scapy.contrib.lldp import (
     LLDPDUChassisID,
     LLDPDUPortID,
@@ -94,6 +96,49 @@ class TestLldpCli(VppTestCase):
         self.vapi.cli("set interface lldp pg0 disable")
         reply = self.vapi.cli("show lldp")
         self.assertNotIn("pg0", reply)
+
+    def test_lldp_truncated_optional_tlv_bounds(self):
+        """Optional TLV length must not exceed the buffer"""
+
+        def lldp_tlv(typ, length, payload):
+            b1 = (typ << 1) | ((length >> 8) & 1)
+            b2 = length & 0xFF
+            return bytes([b1, b2]) + payload
+
+        # VLIB_BUFFER_DEFAULT_DATA_SIZE is 2048, so fill up to just before that so the last TLV
+        # claims to finish beyond the end of the buffer.
+        filler_tlvs = lldp_tlv(127, 0, b"") * 1004
+        trunc_sys_name = lldp_tlv(5, 0x1FF, b"\x00")  # claims 511 value octets, sends 1
+        lldp_payload = (
+            lldp_tlv(1, 7, bytes([4]) + bytes.fromhex("010203040506"))
+            + lldp_tlv(2, 7, bytes([3]) + bytes.fromhex("0708090a0b0c"))
+            + lldp_tlv(3, 2, pack(">H", 120))
+            + filler_tlvs
+            + trunc_sys_name
+        )
+        pkt = Ether(
+            src="ff:ff:ff:ff:00:00",
+            dst="01:80:C2:00:00:03",
+            type=0x88CC,
+        ) / Raw(load=lldp_payload)
+
+        self.vapi.cli("set lldp system-name VPP tx-hold 4 tx-interval 10")
+        self.vapi.cli(
+            f"set interface lldp pg0 port-desc vtf:pg0 mgmt-ip4 {self.pg0.local_ip4}"
+        )
+
+        err_path = "/err/lldp-input/lldp packets with bad TLVs"
+        pre = self.statistics.get_err_counter(err_path)
+
+        self.pg0.add_stream([pkt])
+        self.pg_start()
+
+        post = self.statistics.get_err_counter(err_path)
+        self.assertGreater(
+            post,
+            pre,
+            "truncated system name TLV must increment bad TLV error counter",
+        )
 
 
 @unittest.skipIf("lldp" in config.excluded_plugins, "Exclude lldp plugin tests")