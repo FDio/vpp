FDio/vpp

commit e95c1657798b5ad76a6c885b093df18a4f7e9754

ip: frags should have as much space for headers as original
Type: improvement

Change-Id: Ic8ef26f588a7c161abbe63b3887a76faa32872de
Signed-off-by: Florin Coras <fcoras@cisco.com>

src/vnet/ip/ip_frag.c
@@ -173,8 +173,9 @@ ip4_frag_do_fragment (vlib_main_t * vm, u32 from_bi, u16 mtu,
       vec_add1 (*buffer, to_bi);
       frag_set_sw_if_index (to_b, org_from_b);
 
-      /* Copy ip4 header */
-      to_data = vlib_buffer_get_current (to_b);
+      /* Make sure we have as much space for headers as the original and copy
+       * ip4 header */
+      to_data = vlib_buffer_make_headroom (to_b, org_from_b->current_data);
       clib_memcpy_fast (to_data, org_from_packet, head_bytes);
       to_ip4 = (ip4_header_t *) (to_data + l2unfragmentablesize);
       to_data = (void *) (to_ip4 + 1);

commit 51c7c3b02a5fd9cb79f5edb0113bd38ec6669cc1

ip: fix adjacent packet overwrite with ip frags
The call to vlib_buffer_make_headroom on each buffer allocated for an
ip fragment is problematic in two ways:
1. If org_from_b->current_data is positive then max doesn't take this
   into account so if mtu > 2048 this can result in a length that is too
   long and payload data overwriting the adjacent buffer with the next
   index.
2. If org_from_b->current_data is negative then since
   vlib_buffer_make_headroom takes a u8, the value is implicitly
   converted and becomes a large negative value. Again, with a large
   mtu this results in a length that is too long and the payload data
   overwriting the adjacent buffer with the next index.

Fix both of these by clamping the amount of headroom to make to a
minimum of 0 and then taking the headroom into account when computing
the max fragment payload length.

Type: fix
Fixes: e95c1657798b5ad76a6c885b093df18a4f7e9754
Change-Id: I57960bbbc4e7c6814beb9ea41cd10341f264be8c
Signed-off-by: Rob Shearman <robertshearman@gmail.com>

src/vnet/ip/ip_frag.c
@@ -85,7 +85,7 @@ ip4_frag_do_fragment (vlib_main_t * vm, u32 from_bi, u16 mtu,
   vlib_buffer_t *from_b;
   ip4_header_t *ip4;
   u16 len, max, rem, ip_frag_id, ip_frag_offset, head_bytes;
-  u8 *org_from_packet, more;
+  u8 *org_from_packet, more, headroom_bytes;
 
   from_b = vlib_get_buffer (vm, from_bi);
 
@@ -102,8 +102,9 @@ ip4_frag_do_fragment (vlib_main_t * vm, u32 from_bi, u16 mtu,
 
   rem = clib_net_to_host_u16 (ip4->length) - sizeof (ip4_header_t);
   head_bytes = sizeof (ip4_header_t) + l2unfragmentablesize;
-  max = (clib_min (mtu, vlib_buffer_get_default_data_size (vm)) - head_bytes) &
-	~0x7;
+  headroom_bytes = clib_max (0, from_b->current_data);
+  max =
+    (clib_min (mtu, vlib_buffer_get_default_data_size (vm) - headroom_bytes) - head_bytes) & ~0x7;
 
   if (rem >
       (vlib_buffer_length_in_chain (vm, from_b) - sizeof (ip4_header_t)))
@@ -163,7 +164,7 @@ ip4_frag_do_fragment (vlib_main_t * vm, u32 from_bi, u16 mtu,
 
       /* Make sure we have as much space for headers as the original and copy
        * ip4 header */
-      to_data = vlib_buffer_make_headroom (to_b, org_from_b->current_data);
+      to_data = vlib_buffer_make_headroom (to_b, headroom_bytes);
       clib_memcpy_fast (to_data, org_from_packet, head_bytes);
       to_ip4 = (ip4_header_t *) (to_data + l2unfragmentablesize);
       to_data = (void *) (to_ip4 + 1);