cf7beb074809607f1b6a5e9e63782b1a7b20c3c0
Kristof Provost <kprovost@netgate.com>
2026-06-09 13:34:10 +0000

    http_static: retrieve status information
    Type: feature
    Change-Id: I5fb22c99c8bff92ad0724b103eb20d0143113ede
    Signed-off-by: Kristof Provost <kprovost@netgate.com>

--- test/asf/test_http_static.py
+++ test/asf/test_http_static.py
@@ -130,7 +130,7 @@ class TestHttpStaticVapi(VppAsfTestCase):
         # Now enable again
         self.vapi.http_static_enable_v5(
             www_root="/tmp",
-            uri="tcp://0.0.0.0/80",
+            uri="http://0.0.0.0/80",
         )
         self.temp2.seek(0)
         process = subprocess.run(
@@ -147,6 +147,10 @@ class TestHttpStaticVapi(VppAsfTestCase):
             capture_output=True,
         )
         self.assertIn(b"Hello world2", process.stdout)
+
+        reply = self.vapi.http_static_get_v5()
+        self.assertEqual(reply.www_root, "/tmp")
+        self.assertEqual(reply.uri, "http://0.0.0.0/80")
 
 
 @unittest.skipIf(
