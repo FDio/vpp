eb12e687d3dc9b90dbdf1c3ac26a691b025bb620
Kristof Provost <kprovost@netgate.com>
2026-06-09 13:34:10 +0000

    http_static: retrieve status information
    Type: feature
    Change-Id: I5fb22c99c8bff92ad0724b103eb20d0143113ede
    Signed-off-by: Kristof Provost <kprovost@netgate.com>

--- src/plugins/http_static/http_static_test.c
+++ src/plugins/http_static/http_static_test.c
@@ -28,6 +28,27 @@ http_static_test_main_t http_static_test_main;
 #define __plugin_msg_base http_static_test_main.msg_id_base
 #include <vlibapi/vat_helper_macros.h>
 
+static void
+vl_api_http_static_get_reply_t_handler (vl_api_http_static_get_reply_t *mp)
+{
+}
+
+static int
+api_http_static_get (vat_main_t *vam)
+{
+  vl_api_http_static_get_t *mp;
+  int ret;
+
+  M (HTTP_STATIC_GET, mp);
+  /* send it... */
+  S (mp);
+
+  /* Wait for a reply... */
+  W (ret);
+
+  return ret;
+}
+
 static int
 api_http_static_disable (vat_main_t *vam)
 {
