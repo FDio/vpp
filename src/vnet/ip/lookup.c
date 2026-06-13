FDio/vpp

commit 52456fca3d922d71098d4c1aef8dcee6c5459f8c

CLI extension to add multiple (S,G)s at once and time it
Change-Id: Id17060fd0e8ac80c8cf1999b0b82d0241b3b969a
Signed-off-by: Neale Ranns <nranns@cisco.com>

src/vnet/ip/lookup.c
@@ -784,7 +784,10 @@ vnet_ip_mroute_cmd (vlib_main_t * vm,
   u32 fib_index;
   mfib_itf_flags_t iflags = 0;
   mfib_entry_flags_t eflags = 0;
+  u32 gcount, scount, ss, gg, incr;
+  f64 timet[2];
 
+  gcount = scount = 1;
   vnm = vnet_get_main ();
   is_del = 0;
   table_id = 0;
@@ -804,6 +807,10 @@ vnet_ip_mroute_cmd (vlib_main_t * vm,
 	is_del = 1;
       else if (unformat (line_input, "add"))
 	is_del = 0;
+      else if (unformat (line_input, "scount %d", &scount))
+	;
+      else if (unformat (line_input, "gcount %d", &gcount))
+	;
       else if (unformat (line_input, "%U %U",
 			 unformat_ip4_address,
 			 &pfx.fp_src_addr.ip4,
@@ -885,23 +892,82 @@ vnet_ip_mroute_cmd (vlib_main_t * vm,
 	}
     }
 
-  if (is_del && 0 == rpath.frp_weight)
+  timet[0] = vlib_time_now (vm);
+
+  if (FIB_PROTOCOL_IP4 == pfx.fp_proto)
     {
-      mfib_table_entry_delete (fib_index, &pfx, MFIB_SOURCE_CLI);
-    }
-  else if (eflags)
-    {
-      mfib_table_entry_update (fib_index, &pfx, MFIB_SOURCE_CLI, eflags);
+      incr = 1 << (32 - (pfx.fp_len % 32));
     }
   else
     {
-      if (is_del)
-	mfib_table_entry_path_remove (fib_index,
-				      &pfx, MFIB_SOURCE_CLI, &rpath);
-      else
-	mfib_table_entry_path_update (fib_index,
-				      &pfx, MFIB_SOURCE_CLI, &rpath, iflags);
+      incr = 1 << (128 - (pfx.fp_len % 128));
     }
+
+  for (ss = 0; ss < scount; ss++)
+    {
+      for (gg = 0; gg < gcount; gg++)
+	{
+	  if (is_del && 0 == rpath.frp_weight)
+	    {
+	      /* no path provided => route delete */
+	      mfib_table_entry_delete (fib_index, &pfx, MFIB_SOURCE_CLI);
+	    }
+	  else if (eflags)
+	    {
+	      mfib_table_entry_update (fib_index, &pfx, MFIB_SOURCE_CLI,
+				       eflags);
+	    }
+	  else
+	    {
+	      if (is_del)
+		mfib_table_entry_path_remove (fib_index,
+					      &pfx, MFIB_SOURCE_CLI, &rpath);
+	      else
+		mfib_table_entry_path_update (fib_index,
+					      &pfx, MFIB_SOURCE_CLI, &rpath,
+					      iflags);
+	    }
+
+	  if (FIB_PROTOCOL_IP4 == pfx.fp_proto)
+	    {
+	      pfx.fp_grp_addr.ip4.as_u32 =
+		clib_host_to_net_u32 (incr +
+				      clib_net_to_host_u32 (pfx.
+							    fp_grp_addr.ip4.
+							    as_u32));
+	    }
+	  else
+	    {
+	      int bucket = (incr < 64 ? 0 : 1);
+	      pfx.fp_grp_addr.ip6.as_u64[bucket] =
+		clib_host_to_net_u64 (incr +
+				      clib_net_to_host_u64 (pfx.
+							    fp_grp_addr.ip6.as_u64
+							    [bucket]));
+
+	    }
+	}
+      if (FIB_PROTOCOL_IP4 == pfx.fp_proto)
+	{
+	  pfx.fp_src_addr.ip4.as_u32 =
+	    clib_host_to_net_u32 (1 +
+				  clib_net_to_host_u32 (pfx.fp_src_addr.
+							ip4.as_u32));
+	}
+      else
+	{
+	  pfx.fp_src_addr.ip6.as_u64[1] =
+	    clib_host_to_net_u64 (1 +
+				  clib_net_to_host_u64 (pfx.fp_src_addr.
+							ip6.as_u64[1]));
+	}
+    }
+
+  timet[1] = vlib_time_now (vm);
+
+  if (scount > 1 || gcount > 1)
+    vlib_cli_output (vm, "%.6e routes/sec",
+		     (scount * gcount) / (timet[1] - timet[0]));
 
 done:
   return error;

commit 5ba6541e0d9ce727f4a87eddabeb8c1c920f2de8

ip: fix ip mroute bulk insertion CLI for certain inputs
The ip mroute bulk insertion CLI has three issues relating to the
increment value:
1. The calculation of the increment suffers from a shift-counter
   overflow if the prefix length is 32 or more (including for IPv4/IPv6
   (S,G)s where special values are used of 64 and 256 respectively). This
   is undefined behaviour in C.
2. Missing prefix length validation for (*,G) mroutes.
3. Incorrect bucket selection for IPv6 increments

Fix (1) by using a u64 so that the largest shifts of 32 for IPv4 and
63 for IPv6 are both valid.

Fix (2) by returning an error if the prefix length is invalid for the
address family, accounting for the special prefix lengths for (S,G)
routes.

Fix (3) by incrementing the least-significant half of the address if
len > 64 instead of incr >= 64.

Type: fix
Fixes: 52456fca3d922d71098d4c1aef8dcee6c5459f8c
Change-Id: I51f819f66d48a3aff916d441742f8e9be055299a
Signed-off-by: Rob Shearman <robertshearman@gmail.com>

src/vnet/ip/lookup.c
@@ -757,7 +757,9 @@ vnet_ip_mroute_cmd (vlib_main_t * vm,
   mfib_prefix_t pfx;
   u32 fib_index;
   mfib_entry_flags_t eflags = 0;
-  u32 gcount, scount, ss, gg, incr;
+  u32 gcount, scount, ss, gg;
+  u64 incr;
+  u32 ip6_bucket = 0;
   f64 timet[2];
   u32 rpf_id = MFIB_RPF_ID_NONE;
 
@@ -859,6 +861,20 @@ vnet_ip_mroute_cmd (vlib_main_t * vm,
 	}
     }
 
+  /*
+   * A multicast group prefix is /0../32 (IPv4) or /0../128 (IPv6); a full
+   * (S,G) host is encoded with the sentinel lengths 64 and 256 respectively
+   * (see mfib_prefix_is_host()). Reject anything else rather than passing an
+   * out-of-range length down to the MFIB - cf. fib_prefix_validate() on the
+   * unicast 'ip route' path.
+   */
+  if ((FIB_PROTOCOL_IP4 == pfx.fp_proto && pfx.fp_len > 32 && 64 != pfx.fp_len) ||
+      (FIB_PROTOCOL_IP6 == pfx.fp_proto && pfx.fp_len > 128 && 256 != pfx.fp_len))
+    {
+      error = clib_error_return (0, "Invalid prefix len: %d", pfx.fp_len);
+      goto done;
+    }
+
   if (~0 == table_id)
     {
       /*
@@ -879,13 +895,30 @@ vnet_ip_mroute_cmd (vlib_main_t * vm,
 
   timet[0] = vlib_time_now (vm);
 
+  /*
+   * The group address is stepped by the size of the group prefix for each
+   * entry inserted, so consecutive entries do not overlap. For a full group
+   * address - a /32 (IPv4) or /128 (IPv6), or the (S,G) sentinel lengths 64
+   * and 256 - the step is 1. The step is computed in 64-bit arithmetic to
+   * avoid an out-of-range shift (undefined behaviour, e.g. 1 << 32). For IPv6
+   * it is applied to whichever 64-bit half of the address holds the prefix
+   * boundary (as_u64[0] is the high half, as_u64[1] the low half).
+   */
   if (FIB_PROTOCOL_IP4 == pfx.fp_proto)
     {
-      incr = 1 << (32 - (pfx.fp_len % 32));
+      u32 len = (pfx.fp_len > 32 ? 32 : pfx.fp_len);
+      incr = ((u64) 1) << (32 - len);
     }
   else
     {
-      incr = 1 << (128 - (pfx.fp_len % 128));
+      u32 len = (pfx.fp_len > 128 ? 128 : pfx.fp_len);
+      if (len > 64)
+	{
+	  ip6_bucket = 1;
+	  incr = ((u64) 1) << (128 - len);
+	}
+      else
+	incr = (0 == len ? 0 : (((u64) 1) << (64 - len)));
     }
 
   for (ss = 0; ss < scount; ss++)
@@ -914,21 +947,13 @@ vnet_ip_mroute_cmd (vlib_main_t * vm,
 
 	  if (FIB_PROTOCOL_IP4 == pfx.fp_proto)
 	    {
-	      pfx.fp_grp_addr.ip4.as_u32 =
-		clib_host_to_net_u32 (incr +
-				      clib_net_to_host_u32 (pfx.
-							    fp_grp_addr.ip4.
-							    as_u32));
+	      pfx.fp_grp_addr.ip4.as_u32 = clib_host_to_net_u32 (
+		(u32) (incr + clib_net_to_host_u32 (pfx.fp_grp_addr.ip4.as_u32)));
 	    }
 	  else
 	    {
-	      int bucket = (incr < 64 ? 0 : 1);
-	      pfx.fp_grp_addr.ip6.as_u64[bucket] =
-		clib_host_to_net_u64 (incr +
-				      clib_net_to_host_u64 (pfx.
-							    fp_grp_addr.ip6.as_u64
-							    [bucket]));
-
+	      pfx.fp_grp_addr.ip6.as_u64[ip6_bucket] = clib_host_to_net_u64 (
+		incr + clib_net_to_host_u64 (pfx.fp_grp_addr.ip6.as_u64[ip6_bucket]));
 	    }
 	}
       if (FIB_PROTOCOL_IP4 == pfx.fp_proto)

test/test_ip_mcast.py
@@ -14,6 +14,7 @@ from vpp_ip_route import (
 )
 from vpp_gre_interface import VppGreInterface
 from vpp_papi import VppEnum
+from vpp_papi_provider import CliFailedCommandError
 from config import config
 
 from scapy.packet import Raw
@@ -1248,6 +1249,60 @@ class TestIPMcast(VppTestCase):
         self.verify_capture_ip4(self.pg2, tx)
         self.verify_capture_ip4(self.pg3, tx)
         self.pg0.assert_nothing_captured(remark="IP multicast packets forwarded on PG0")
+
+    def mfib_groups(self, is_ip6):
+        """Return the set of group-address strings currently in the MFIB."""
+        af = "ip6" if is_ip6 else "ip4"
+        groups = set()
+        for e in self.vapi.ip_mroute_dump(0, is_ip6):
+            groups.add(str(getattr(e.route.prefix.grp_address, af)))
+        return groups
+
+    def test_mroute_bulk_ip4_group(self):
+        """Bulk insert of IPv4 (*,G)/32 groups installs consecutive groups"""
+        self.vapi.cli("ip mroute add gcount 8 232.1.1.1/32 via pg0 Forward")
+
+        groups = self.mfib_groups(is_ip6=False)
+        self.logger.info("installed IPv4 groups: %s" % sorted(groups))
+
+        # clean up before asserting so the table is empty for the next test
+        self.vapi.cli("ip mroute del gcount 8 232.1.1.1/32 via pg0 Forward")
+
+        expected = {"232.1.1.%d" % (1 + i) for i in range(8)}
+        self.assertEqual(expected, expected.intersection(groups))
+
+    def test_mroute_bulk_ip6_group(self):
+        """Bulk insert of IPv6 (*,G)/128 groups installs consecutive groups"""
+        self.vapi.cli("ip mroute add gcount 8 ff05::1/128 via pg0 Forward")
+
+        groups = self.mfib_groups(is_ip6=True)
+        self.logger.info("installed IPv6 groups: %s" % sorted(groups))
+
+        self.vapi.cli("ip mroute del gcount 8 ff05::1/128 via pg0 Forward")
+
+        expected = {"ff05::%d" % (1 + i) for i in range(8)}
+        self.assertEqual(expected, expected.intersection(groups))
+
+    def test_mroute_bulk_ip6_sg(self):
+        """Bulk insert of IPv6 (S,G) state installs consecutive groups"""
+        self.vapi.cli("ip mroute add gcount 8 2001:db8::1 ff05::1 via pg0 Forward")
+
+        groups = self.mfib_groups(is_ip6=True)
+        self.logger.info("installed IPv6 (S,G) groups: %s" % sorted(groups))
+
+        self.vapi.cli("ip mroute del gcount 8 2001:db8::1 ff05::1 via pg0 Forward")
+
+        expected = {"ff05::%d" % (1 + i) for i in range(8)}
+        self.assertEqual(expected, expected.intersection(groups))
+
+    def test_mroute_invalid_prefix_len(self):
+        """An out-of-range mroute prefix length is rejected, not installed"""
+        with self.assertRaises(CliFailedCommandError):
+            self.vapi.cli("ip mroute add 239.1.1.0/33 via pg0 Forward")
+
+        groups = self.mfib_groups(is_ip6=False)
+        self.logger.info("groups after /33 attempt: %s" % sorted(groups))
+        self.assertNotIn("239.1.1.0", groups)
 
 
 if __name__ == "__main__":