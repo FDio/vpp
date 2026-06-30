FDio/vpp

commit be0ece80276b5dfe538abdaa96004074d11cdca3

http_static: support disabling the server via the API
Type: feature
Change-Id: I65706103e043019987f5bdfa9d6de3df6f2cc504
Signed-off-by: Kristof Provost <kprovost@netgate.com>

src/plugins/http_static/static_server.c
@@ -1112,6 +1112,36 @@ hss_create (vlib_main_t *vm)
   return 0;
 }
 
+int
+hss_destroy (vlib_main_t *vm)
+{
+  hss_main_t *hsm = &hss_main;
+  hss_listener_t *l;
+  int ret;
+
+  pool_foreach (l, hsm->listeners)
+    {
+      ret = hss_listener_del (l);
+      if (ret != 0)
+	{
+	  clib_warning ("failed to delete a listener: %d", ret);
+	  return -1;
+	}
+    }
+
+  vnet_app_detach_args_t _da = {}, *da = &_da;
+  da->app_index = hsm->app_index;
+  if (vnet_application_detach (da))
+    {
+      clib_warning ("failed to detach http_static app");
+      return -1;
+    }
+  hsm->app_index = ~0;
+  hsm->is_init = 0;
+
+  return 0;
+}
+
 static clib_error_t *
 hss_create_command_fn (vlib_main_t *vm, unformat_input_t *input,
 		       vlib_cli_command_t *cmd)
@@ -1123,6 +1153,19 @@ hss_create_command_fn (vlib_main_t *vm, unformat_input_t *input,
   char *uri = 0;
   u64 seg_size;
   int rv;
+
+  /* Get a line of input. */
+  if (!unformat_user (input, unformat_line_input, line_input))
+    goto no_input;
+
+  if (unformat (line_input, "disable"))
+    {
+      rv = hss_destroy (vm);
+      if (rv != 0)
+	{
+	  error = clib_error_return (0, "failed to disable server %d", rv);
+	}
+    }
 
   if (hsm->app_index != (u32) ~0)
     return clib_error_return (0, "http static server already initialized...");
@@ -1137,10 +1180,6 @@ hss_create_command_fn (vlib_main_t *vm, unformat_input_t *input,
   l->rx_buff_thresh = HSS_DEFAULT_RX_BUFFER_THRESH;
   l->keepalive_timeout = HSS_DEFAULT_KEEPALIVE_TIMEOUT;
   l->flags = 0;
-
-  /* Get a line of input. */
-  if (!unformat_user (input, unformat_line_input, line_input))
-    goto no_input;
 
   while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
     {
@@ -1257,7 +1296,7 @@ done:
  * [fifo-size <nbytes>] [prealloc-fifos <nn>] [debug <nn>] [uri <uri>]
  * [www-root <path>] [url-handlers] [cache-size <nn>] [max-age <nseconds>]
  * [max-req-body-size <nn>] [rx-buff-thresh <nn>] [keepalive-timeout <nn>]
- * [ptr-thresh <nn>] [http1-only] [http3]}
+ * [ptr-thresh <nn>] [http1-only] [http3] [disable]}
 ?*/
 VLIB_CLI_COMMAND (hss_create_command, static) = {
   .path = "http static server",

src/plugins/http_static/http_static.api
@@ -81,3 +81,11 @@ autoreply define http_static_enable_v5 {
     /* The bind URI */
     string uri[256];
 };
+
+autoreply define http_static_disable {
+    /* Client identifier, set from api_main.my_client_index */
+    u32 client_index;
+
+    /* Arbitrary context, so client can match reply to request */
+    u32 context;
+};

src/plugins/http_static/http_static.c
@@ -138,6 +138,18 @@ vl_api_http_static_enable_v5_t_handler (vl_api_http_static_enable_v5_t *mp)
   REPLY_MACRO (VL_API_HTTP_STATIC_ENABLE_V5_REPLY);
 }
 
+static void
+vl_api_http_static_disable_t_handler (vl_api_http_static_disable_t *mp)
+{
+  hss_main_t *hsm = &hss_main;
+  vl_api_http_static_disable_reply_t *rmp;
+  int rv;
+
+  rv = hss_destroy (hsm->vlib_main);
+
+  REPLY_MACRO (VL_API_HTTP_STATIC_DISABLE_REPLY);
+}
+
 #include <http_static/http_static.api.c>
 static clib_error_t *
 hss_api_init (vlib_main_t *vm)

src/plugins/http_static/http_static.h
@@ -220,6 +220,7 @@ typedef struct
 extern hss_main_t hss_main;
 
 int hss_create (vlib_main_t *vm);
+int hss_destroy (vlib_main_t *vm);
 
 static inline hss_listener_t *
 hss_listener_get (u32 l_index)

src/plugins/http_static/http_static_test.c
@@ -29,6 +29,22 @@ http_static_test_main_t http_static_test_main;
 #include <vlibapi/vat_helper_macros.h>
 
 static int
+api_http_static_disable (vat_main_t *vam)
+{
+  vl_api_http_static_disable_t *mp;
+  int ret;
+
+  M (HTTP_STATIC_DISABLE, mp);
+  /* send it... */
+  S (mp);
+
+  /* Wait for a reply... */
+  W (ret);
+
+  return ret;
+}
+
+static int
 api_http_static_enable_v4 (vat_main_t *vam)
 {
   unformat_input_t *line_input = vam->input;

test/asf/test_http_static.py
@@ -110,6 +110,44 @@ class TestHttpStaticVapi(VppAsfTestCase):
         )
         self.assertIn(b"Hello world2", process.stdout)
 
+        self.vapi.http_static_disable()
+        self.temp2.seek(0)
+        process = subprocess.run(
+            [
+                "ip",
+                "netns",
+                "exec",
+                self.ns_name,
+                "curl",
+                "--noproxy",
+                "10.10.1.2",
+                f"10.10.1.2/{self.temp2.name[5:]}",
+            ],
+            capture_output=True,
+        )
+        self.assertNotIn(b"Hello world2", process.stdout)
+
+        # Now enable again
+        self.vapi.http_static_enable_v5(
+            www_root="/tmp",
+            uri="tcp://0.0.0.0/80",
+        )
+        self.temp2.seek(0)
+        process = subprocess.run(
+            [
+                "ip",
+                "netns",
+                "exec",
+                self.ns_name,
+                "curl",
+                "--noproxy",
+                "10.10.1.2",
+                f"10.10.1.2/{self.temp2.name[5:]}",
+            ],
+            capture_output=True,
+        )
+        self.assertIn(b"Hello world2", process.stdout)
+
 
 @unittest.skipIf(
     "http_static" in config.excluded_plugins, "Exclude HTTP Static Server plugin tests"
@@ -197,6 +235,21 @@ class TestHttpStaticCli(VppAsfTestCase):
         self.logger.info(self.vapi.cli("show http static server cache"))
         self.logger.info(self.vapi.cli("clear http static cache"))
         self.logger.info(self.vapi.cli("show http static server sessions"))
+
+        # Now turn the server off again
+        self.vapi.cli("http static server disable")
+        process = subprocess.run(
+            [
+                "ip",
+                "netns",
+                "exec",
+                self.ns_name,
+                "curl",
+                f"10.10.1.2/{self.temp2.name[5:]}",
+            ],
+            capture_output=True,
+        )
+        self.assertNotIn(b"Hello world2", process.stdout)
 
 
 if __name__ == "__main__":

commit e99de62d27e464176f726d1719010a2afe0f82e7

http_static: fix disable CLI
When disabling the server exit the function, don't immediately re-enable
it.

Fixes: be0ece80276b5dfe538abdaa96004074d11cdca3
Type: fix
Change-Id: I9cfd93cc50a98d64f2af297f453cade110a8439a
Signed-off-by: Kristof Provost <kprovost@netgate.com>

src/plugins/http_static/static_server.c
@@ -1165,6 +1165,7 @@ hss_create_command_fn (vlib_main_t *vm, unformat_input_t *input,
 	{
 	  error = clib_error_return (0, "failed to disable server %d", rv);
 	}
+	goto done;
     }
 
   if (hsm->app_index != (u32) ~0)