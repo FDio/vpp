5c12349940aa99380e9c410bc4a689cf420fd72f
Florin Coras <fcoras@cisco.com>
2026-07-03 22:02:20 -0400

    tcp: include delivered data in ssrb limit
    If pipe drops under ssthresh, allow one extra segment per data delivered
    on top of packet conservation. This allows pipe to re-inflate to
    ssthresh after heavy loss.
    
    Change-Id: Id9605cf5c92f790599fa9208c08e8a9707550174
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/vnet/tcp/tcp_input.c
+++ src/vnet/tcp/tcp_input.c
@@ -600,6 +600,7 @@ tcp_cc_init_congestion (tcp_connection_t * tc)
   tc->snd_rxt_bytes = 0;
   tc->rxt_delivered = 0;
   tc->prr_delivered = 0;
+  tc->prev_prr_delivered = 0;
   tc->prr_start = tc->snd_una;
   tc->prev_ssthresh = tc->ssthresh;
   tc->prev_cwnd = tc->cwnd;
@@ -695,6 +696,7 @@ tcp_cc_exit_recovery (tcp_connection_t *tc)
   tc->snd_rxt_bytes = 0;
   tc->snd_rxt_ts = 0;
   tc->prr_delivered = 0;
+  tc->prev_prr_delivered = 0;
   tc->rtt_ts = 0;
   tc->flags &= ~TCP_CONN_RXT_PENDING;
 
