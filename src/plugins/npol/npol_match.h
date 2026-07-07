1038974276e61ffbd14db392eb680c84837018c4
Onong Tayeng <otayeng@cisco.com>
2026-07-07 14:16:43 +0530

    npol: export flow decision counters
    Add per-interface flow decision counters for NPOL allow and deny results
    on the CNAT slow-path hooks. Expose separate RX and TX counters so consumers
    can distinguish policy decisions by direction without treating them as packet
    or byte counters.
    
    Change-Id: I0a7c6eea7508220fca6ebc998ebd21f2dc5dcbc7
    Signed-off-by: Onong Tayeng <otayeng@cisco.com>

--- src/plugins/npol/npol_match.h
+++ src/plugins/npol/npol_match.h
@@ -5,10 +5,24 @@
 #ifndef included_npol_match_h
 #define included_npol_match_h
 
+#include <vlib/counter.h>
 #include <npol/npol_ipset.h>
 #include <npol/npol_policy.h>
 #include <npol/npol_rule.h>
 #include <cnat/cnat_feature_hook.h>
+
+typedef enum
+{
+  NPOL_FLOW_COUNTER_RX_ALLOW,
+  NPOL_FLOW_COUNTER_RX_DENY,
+  NPOL_FLOW_COUNTER_TX_ALLOW,
+  NPOL_FLOW_COUNTER_TX_DENY,
+  NPOL_N_FLOW_COUNTERS,
+} npol_flow_counter_t;
+
+extern vlib_simple_counter_main_t npol_flow_counters[NPOL_N_FLOW_COUNTERS];
+
+void npol_flow_counters_validate (u32 sw_if_index);
 
 int npol_match_func (u32 sw_if_index, u32 is_inbound, cnat_5tuple_t *pkt_5tuple, int is_ip6,
 		     u8 *r_action);
