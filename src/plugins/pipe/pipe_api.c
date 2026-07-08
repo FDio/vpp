6e74125cb7cbfc3018924c7976855f3102092806
Anton Blazhko <ablazhko@cisco.com>
2026-07-08 14:10:12 +0000

    devices: Convert PIPE to plugin
    Move PIPE from vnet core (src/vnet/devices/pipe) to a loadable plugin
    (src/plugins/pipe). Remove pipe sources from the vnet build.
    
    Decouple ethernet-input from pipe by adding a subinterface config
    registration hook in ethernet (ethernet_register_subint_config_fn /
    ethernet_subint_config_get).
    ethernet-input now resolves PIPE subint config via this hook
    instead of calling pipe_get() directly.
    
    The plugin registers its subint callback at init and keeps API hookup
    in a separate VLIB_API_INIT_FUNCTION.
    
    Skip pipe tests when the plugin is excluded from the test image.
    
    Type: improvement
    
    Change-Id: I9125bb355ed9819ae78888b3a20d7116a3c922f6
    Signed-off-by: Anton Blazhko <ablazhko@cisco.com>

--- src/plugins/pipe/pipe_api.c
+++ src/plugins/pipe/pipe_api.c
@@ -4,11 +4,11 @@
 
 #include <vnet/vnet.h>
 #include <vlibmemory/api.h>
-#include <vnet/devices/pipe/pipe.h>
+#include <pipe/pipe.h>
 
 #include <vnet/format_fns.h>
-#include <vnet/devices/pipe/pipe.api_enum.h>
-#include <vnet/devices/pipe/pipe.api_types.h>
+#include <pipe/pipe.api_enum.h>
+#include <pipe/pipe.api_types.h>
 
 #define REPLY_MSG_ID_BASE msg_id_base
 #include <vlibapi/api_helper_macros.h>
@@ -17,7 +17,7 @@ extern vpe_api_main_t vpe_api_main;
 static u16 msg_id_base;
 
 static void
-vl_api_pipe_create_t_handler (vl_api_pipe_create_t * mp)
+vl_api_pipe_create_t_handler (vl_api_pipe_create_t *mp)
 {
   vl_api_pipe_create_reply_t *rmp;
   u32 parent_sw_if_index;
@@ -26,19 +26,18 @@ vl_api_pipe_create_t_handler (vl_api_pipe_create_t * mp)
   u8 is_specified = mp->is_specified;
   u32 user_instance = ntohl (mp->user_instance);
 
-  rv = vnet_create_pipe_interface (is_specified, user_instance,
-				   &parent_sw_if_index, pipe_sw_if_index);
+  rv =
+    vnet_create_pipe_interface (is_specified, user_instance, &parent_sw_if_index, pipe_sw_if_index);
 
-  REPLY_MACRO2(VL_API_PIPE_CREATE_REPLY,
-  ({
-    rmp->sw_if_index = ntohl (parent_sw_if_index);
-    rmp->pipe_sw_if_index[0] = ntohl (pipe_sw_if_index[0]);
-    rmp->pipe_sw_if_index[1] = ntohl (pipe_sw_if_index[1]);
-  }));
+  REPLY_MACRO2 (VL_API_PIPE_CREATE_REPLY, ({
+		  rmp->sw_if_index = ntohl (parent_sw_if_index);
+		  rmp->pipe_sw_if_index[0] = ntohl (pipe_sw_if_index[0]);
+		  rmp->pipe_sw_if_index[1] = ntohl (pipe_sw_if_index[1]);
+		}));
 }
 
 static void
-vl_api_pipe_delete_t_handler (vl_api_pipe_delete_t * mp)
+vl_api_pipe_delete_t_handler (vl_api_pipe_delete_t *mp)
 {
   vl_api_pipe_delete_reply_t *rmp;
   int rv;
@@ -55,8 +54,7 @@ typedef struct pipe_dump_walk_t_
 } pipe_dump_walk_t;
 
 static walk_rc_t
-pipe_send_details (u32 parent_sw_if_index,
-		   u32 pipe_sw_if_index[2], u32 instance, void *args)
+pipe_send_details (u32 parent_sw_if_index, u32 pipe_sw_if_index[2], u32 instance, void *args)
 {
   pipe_dump_walk_t *ctx = args;
   vl_api_pipe_details_t *mp;
@@ -79,7 +77,7 @@ pipe_send_details (u32 parent_sw_if_index,
 }
 
 static void
-vl_api_pipe_dump_t_handler (vl_api_pipe_dump_t * mp)
+vl_api_pipe_dump_t_handler (vl_api_pipe_dump_t *mp)
 {
   vl_api_registration_t *reg;
 
@@ -95,9 +93,9 @@ vl_api_pipe_dump_t_handler (vl_api_pipe_dump_t * mp)
   pipe_walk (pipe_send_details, &ctx);
 }
 
-#include <vnet/devices/pipe/pipe.api.c>
+#include <pipe/pipe.api.c>
 static clib_error_t *
-pipe_api_hookup (vlib_main_t * vm)
+pipe_api_hookup (vlib_main_t *vm)
 {
   /*
    * Set up the (msg_name, crc, message-id) table
