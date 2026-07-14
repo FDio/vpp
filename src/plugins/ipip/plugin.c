dd1ae92ed6e05745b2bf88565eb70d7626eafb62
Oleh Poluianskyi <opoluian@cisco.com>
2026-07-14 16:09:12 +0000

    ipip: move to the plugin
    Change-Id: I7fae7939bd6ade47cb29e962e0631f93cff04ef8
    Signed-off-by: Oleh Poluianskyi <opoluian@cisco.com>

---
+++ src/plugins/ipip/plugin.c
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2023 Cisco and/or its affiliates.
+ */
+
+/* plugin.c: ipip */
+
+#include <vlib/vlib.h>
+#include <vnet/plugin/plugin.h>
+#include <vpp/app/version.h>
+
+// register ipip plugin
+VLIB_PLUGIN_REGISTER () =
+{
+  .version = VPP_BUILD_VER,
+  .description = "Tunnel protocol: IP-in-IP (ipip) plugin"
+};
