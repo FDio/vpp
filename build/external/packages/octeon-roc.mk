39a7d24a4ca06878c80f2dfd2a1dc74c29a9bdf3
Monendra Singh Kushwaha <kmonendra@marvell.com>
2026-08-03 12:48:38 +0530

    octeon: update octeon roc version
    Type: feature
    
    Change-Id: I55a7872a9378d6ff1742e46f9e7af9d47762ea32
    Signed-off-by: Monendra Singh Kushwaha <kmonendra@marvell.com>

--- build/external/packages/octeon-roc.mk
+++ build/external/packages/octeon-roc.mk
@@ -2,9 +2,9 @@
 # SPDX-License-Identifier: Apache-2.0
 # https://spdx.org/licenses/Apache-2.0.html
 
-octeon-roc_version             := 26.05
+octeon-roc_version             := 26.09.01
 octeon-roc_tarball             := v$(octeon-roc_version).tar.gz
-octeon-roc_tarball_sha256sum   := e81d8277e891260df59f2f9fa6bc3abcfade42d9751ae577f94dc37274224c6a
+octeon-roc_tarball_sha256sum   := be3d304340512f5b8dd72dc568c4622a8f53bab25e17e967bf2d5eb06a38b527
 
 octeon-roc_tarball_strip_dirs  := 1
 octeon-roc_url                 := https://github.com/MarvellEmbeddedProcessors/marvell-octeon-roc/archive/refs/tags/$(octeon-roc_tarball)
