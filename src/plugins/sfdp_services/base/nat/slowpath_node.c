FDio/vpp

commit 28dbf5aee6bfd783e7336d561a1fe03f455c7e9b

sfdp_services: plugin with basic SFDP services
Change-Id: Id33e9f40fcc20d38c9749aada26a1b345b3ad027
Type: feature
Signed-off-by: Mohammed Hawari <mohammed@hawari.fr>

src/plugins/sfdp_services/base/nat/slowpath_node.c
@@ -1,0 +1,289 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2025 Cisco Systems, Inc.
+ */
+
+#include <vlib/vlib.h>
+#include <sfdp_services/base/nat/nat.h>
+#include <vnet/sfdp/service.h>
+#include <vnet/sfdp/sfdp_funcs.h>
+#define foreach_sfdp_nat_slowpath_error _ (DROP, "drop")
+
+typedef enum
+{
+#define _(sym, str) SFDP_NAT_SLOWPATH_ERROR_##sym,
+  foreach_sfdp_nat_slowpath_error
+#undef _
+    SFDP_NAT_SLOWPATH_N_ERROR,
+} sfdp_nat_slowpath_error_t;
+
+static char *sfdp_nat_slowpath_error_strings[] = {
+#define _(sym, string) string,
+  foreach_sfdp_nat_slowpath_error
+#undef _
+};
+
+typedef struct
+{
+  u32 flow_id;
+  u32 thread_index;
+} sfdp_nat_slowpath_trace_t;
+
+format_function_t format_sfdp_bitmap;
+
+SFDP_SERVICE_DECLARE (drop)
+static u8 *
+format_sfdp_nat_slowpath_trace (u8 *s, va_list *args)
+{
+  vlib_main_t __clib_unused *vm = va_arg (*args, vlib_main_t *);
+  vlib_node_t __clib_unused *node = va_arg (*args, vlib_node_t *);
+  sfdp_nat_slowpath_trace_t *t = va_arg (*args, sfdp_nat_slowpath_trace_t *);
+  /* FIXME: This is a scam, the session-idx can be invalid at format time!*/
+  sfdp_session_t *session = sfdp_session_at_index (t->flow_id >> 1);
+  u32 scope_index = session->scope_index;
+
+  s = format (s, "sfdp-nat-output: flow-id %u (session %u, %s)\n", t->flow_id,
+	      t->flow_id >> 1, t->flow_id & 0x1 ? "reverse" : "forward");
+  s = format (s, "  new forward service chain: %U\n", format_sfdp_bitmap,
+	      scope_index, session->bitmaps[SFDP_FLOW_FORWARD]);
+  s = format (s, "  new reverse service chain: %U\n", format_sfdp_bitmap,
+	      scope_index, session->bitmaps[SFDP_FLOW_REVERSE]);
+
+  return s;
+}
+SFDP_SERVICE_DECLARE (nat_late_rewrite)
+SFDP_SERVICE_DECLARE (nat_early_rewrite)
+SFDP_SERVICE_DECLARE (nat_output)
+static_always_inline void
+nat_slow_path_process_one (sfdp_main_t *sfdp, u32 *fib_index_by_sw_if_index,
+			   u16 thread_index, nat_main_t *nm,
+			   nat_tenant_t *tenant, u32 session_index,
+			   nat_rewrite_data_t *nat_session,
+			   sfdp_session_t *session, u16 *to_next,
+			   vlib_buffer_t **b)
+{
+  uword l3_sum_delta_forward = 0;
+  uword l4_sum_delta_forward = 0;
+  uword l3_sum_delta_reverse = 0;
+  uword l4_sum_delta_reverse = 0;
+  sfdp_session_ip46_key_t new_key46 = { 0 };
+  sfdp_session_ip4_key_t *new_key = &new_key46.key4;
+  new_key46.key4 = session->keys[SFDP_SESSION_KEY_PRIMARY].key4;
+  u8 pseudo_dir = session->pseudo_dir[SFDP_SESSION_KEY_PRIMARY];
+  u8 proto = session->proto;
+  u8 n_retries = 0;
+  u32 *ip4_key_src_addr =
+    pseudo_dir ? &new_key->ip4_key.ip_addr_hi : &new_key->ip4_key.ip_addr_lo;
+  u32 ip4_old_src_addr;
+  u32 ip4_new_src_addr;
+  u16 *ip4_key_src_port;
+  u16 *ip4_key_dst_port;
+  u16 ip4_old_port;
+  u16 ip4_new_port;
+
+  nat_alloc_pool_t *pool;
+  u32 src_addr_index;
+  u64 h;
+  u32 pseudo_flow_index;
+  u32 old_fib_index;
+
+  if (PREDICT_FALSE (!(tenant->flags & NAT_TENANT_FLAG_SNAT)))
+    {
+      sfdp_buffer (b[0])->service_bitmap = SFDP_SERVICE_MASK (drop);
+      goto end_of_packet;
+    }
+
+  pool = pool_elt_at_index (nm->alloc_pool, tenant->out_alloc_pool_idx);
+
+  if (PREDICT_FALSE (session->session_version == nat_session->version))
+    {
+      /* NAT State is already created, certainly a packet in flight. Refresh
+       * bitmap */
+      sfdp_buffer (b[0])->service_bitmap =
+	session->bitmaps[b[0]->flow_id & 0x1];
+      goto end_of_packet;
+    }
+
+  /* TODO: handle case with many addresses in pool (slowpath) */
+  if (PREDICT_FALSE (pool->num > NAT_ALLOC_POOL_ARRAY_SZ))
+    ASSERT (0);
+
+  new_key->context_id = tenant->reverse_context;
+
+  /* Allocate a new source */
+  ip4_old_src_addr = *ip4_key_src_addr;
+  src_addr_index = ip4_old_src_addr % pool->num;
+  ip4_new_src_addr = pool->addr[src_addr_index].as_u32;
+  *ip4_key_src_addr = ip4_new_src_addr;
+  pseudo_dir = sfdp_renormalise_ip4_key (new_key, pseudo_dir);
+  pseudo_flow_index = (session_index << 1) | (pseudo_dir & 0x1);
+  /* Allocate a new port */
+  ip4_key_src_port =
+    pseudo_dir ? &new_key->ip4_key.port_hi : &new_key->ip4_key.port_lo;
+  ip4_key_dst_port =
+    pseudo_dir ? &new_key->ip4_key.port_lo : &new_key->ip4_key.port_hi;
+  ip4_old_port = *ip4_key_src_port;
+
+  /* First try with original src port */
+  ip4_new_port = ip4_old_port;
+  while ((++n_retries) < 5 && sfdp_session_try_add_secondary_key (
+				sfdp, thread_index, pseudo_flow_index,
+				&new_key46, IP46_TYPE_IP4, &h))
+    {
+      /* Use h to try a different port */
+      u32 h2 = h;
+      u64 reduced = h2;
+      reduced *= 64512ULL;
+      reduced >>= 32;
+      ip4_new_port = clib_host_to_net_u16 (1024 + reduced);
+      *ip4_key_src_port = ip4_new_port;
+      if (PREDICT_FALSE (proto == IP_PROTOCOL_ICMP))
+	*ip4_key_dst_port = ip4_new_port;
+    }
+
+  if (n_retries == 5)
+    {
+      /* Port allocation failure */
+      /* TODO: do the sensible thing, drop the packet + increase counters */
+      sfdp_buffer (b[0])->service_bitmap = SFDP_SERVICE_MASK (drop);
+      goto end_of_packet;
+    }
+
+  /* Build the rewrites in both directions */
+  l3_sum_delta_forward =
+    ip_csum_add_even (l3_sum_delta_forward, ip4_new_src_addr);
+  l3_sum_delta_forward =
+    ip_csum_sub_even (l3_sum_delta_forward, ip4_old_src_addr);
+
+  l4_sum_delta_forward = ip_csum_add_even (l4_sum_delta_forward, ip4_new_port);
+  l4_sum_delta_forward = ip_csum_sub_even (l4_sum_delta_forward, ip4_old_port);
+
+  l3_sum_delta_reverse =
+    ip_csum_add_even (l3_sum_delta_reverse, ip4_old_src_addr);
+  l3_sum_delta_reverse =
+    ip_csum_sub_even (l3_sum_delta_reverse, ip4_new_src_addr);
+
+  l4_sum_delta_reverse = ip_csum_add_even (l4_sum_delta_reverse, ip4_old_port);
+  l4_sum_delta_reverse = ip_csum_sub_even (l4_sum_delta_reverse, ip4_new_port);
+
+  old_fib_index = vec_elt (fib_index_by_sw_if_index,
+			   vnet_buffer (b[0])->sw_if_index[VLIB_RX]);
+  nat_session[0].version = session->session_version;
+  nat_session[1].version = session->session_version;
+
+  if (PREDICT_TRUE (proto != IP_PROTOCOL_ICMP))
+    {
+      nat_session[0].ops =
+	NAT_REWRITE_OP_SADDR | NAT_REWRITE_OP_SPORT | NAT_REWRITE_OP_TXFIB;
+      nat_session[0].rewrite.sport = ip4_new_port;
+      nat_session[1].ops =
+	NAT_REWRITE_OP_DADDR | NAT_REWRITE_OP_DPORT | NAT_REWRITE_OP_TXFIB;
+      nat_session[1].rewrite.dport = ip4_old_port;
+    }
+  else
+    {
+      nat_session[0].ops =
+	NAT_REWRITE_OP_SADDR | NAT_REWRITE_OP_ICMP_ID | NAT_REWRITE_OP_TXFIB;
+      nat_session[0].rewrite.icmp_id = ip4_new_port;
+      nat_session[1].ops =
+	NAT_REWRITE_OP_DADDR | NAT_REWRITE_OP_ICMP_ID | NAT_REWRITE_OP_TXFIB;
+      nat_session[1].rewrite.icmp_id = ip4_old_port;
+    }
+
+  nat_session[0].rewrite.saddr.as_u32 = ip4_new_src_addr;
+  nat_session[0].rewrite.fib_index = tenant->fib_index;
+  nat_session[0].rewrite.proto = proto;
+  nat_session[0].l3_csum_delta = l3_sum_delta_forward;
+  nat_session[0].l4_csum_delta = l4_sum_delta_forward;
+
+  nat_session[1].rewrite.daddr.as_u32 = ip4_old_src_addr;
+  nat_session[1].rewrite.fib_index = old_fib_index;
+  nat_session[1].rewrite.proto = proto;
+  nat_session[1].l3_csum_delta = l3_sum_delta_reverse;
+  nat_session[1].l4_csum_delta = l4_sum_delta_reverse;
+
+  sfdp_buffer (b[0])->service_bitmap |= SFDP_SERVICE_MASK (nat_late_rewrite);
+  session->bitmaps[SFDP_FLOW_FORWARD] &= ~SFDP_SERVICE_MASK (nat_output);
+  session->bitmaps[SFDP_FLOW_REVERSE] &= ~SFDP_SERVICE_MASK (nat_output);
+  session->bitmaps[SFDP_FLOW_FORWARD] |= SFDP_SERVICE_MASK (nat_late_rewrite);
+  session->bitmaps[SFDP_FLOW_REVERSE] |= SFDP_SERVICE_MASK (nat_early_rewrite);
+
+end_of_packet:
+  sfdp_next (b[0], to_next);
+  return;
+}
+
+VLIB_NODE_FN (sfdp_nat_slowpath_node)
+(vlib_main_t *vm, vlib_node_runtime_t *node, vlib_frame_t *frame)
+{
+
+  vlib_buffer_t *bufs[VLIB_FRAME_SIZE], **b = bufs;
+  sfdp_main_t *sfdp = &sfdp_main;
+  ip4_main_t *im = &ip4_main;
+  nat_main_t *nat = &nat_main;
+  u32 thread_index = vlib_get_thread_index ();
+  sfdp_session_t *session;
+  nat_tenant_t *tenant;
+  u32 session_idx;
+  u32 tenant_idx;
+  nat_rewrite_data_t *nat_rewrites; /* rewrite data in both directions */
+  u32 *from = vlib_frame_vector_args (frame);
+  u32 n_left = frame->n_vectors;
+  u16 next_indices[VLIB_FRAME_SIZE], *to_next = next_indices;
+
+  vlib_get_buffers (vm, from, bufs, n_left);
+  while (n_left > 0)
+    {
+      session_idx = sfdp_session_from_flow_index (b[0]->flow_id);
+      session = sfdp_session_at_index (session_idx);
+      tenant_idx = sfdp_buffer (b[0])->tenant_index;
+      nat_rewrites = vec_elt_at_index (nat->flows, session_idx << 1);
+      tenant = vec_elt_at_index (nat->tenants, tenant_idx);
+
+      // nat_slow_path_process_one (tenant, nat_rewrites, session, to_next, b);
+      nat_slow_path_process_one (sfdp, im->fib_index_by_sw_if_index,
+				 thread_index, nat, tenant, session_idx,
+				 nat_rewrites, session, to_next, b);
+      n_left -= 1;
+      b += 1;
+      to_next += 1;
+    }
+  vlib_buffer_enqueue_to_next (vm, node, from, next_indices, frame->n_vectors);
+  if (PREDICT_FALSE ((node->flags & VLIB_NODE_FLAG_TRACE)))
+    {
+      int i;
+      b = bufs;
+      n_left = frame->n_vectors;
+      for (i = 0; i < n_left; i++)
+	{
+	  if (b[0]->flags & VLIB_BUFFER_IS_TRACED)
+	    {
+	      sfdp_nat_slowpath_trace_t *t =
+		vlib_add_trace (vm, node, b[0], sizeof (*t));
+	      t->flow_id = b[0]->flow_id;
+	      t->thread_index = thread_index;
+	      b++;
+	    }
+	  else
+	    break;
+	}
+    }
+  return frame->n_vectors;
+}
+
+VLIB_REGISTER_NODE (sfdp_nat_slowpath_node) = {
+  .name = "sfdp-nat-output",
+  .vector_size = sizeof (u32),
+  .format_trace = format_sfdp_nat_slowpath_trace,
+  .type = VLIB_NODE_TYPE_INTERNAL,
+
+  .n_errors = ARRAY_LEN (sfdp_nat_slowpath_error_strings),
+  .error_strings = sfdp_nat_slowpath_error_strings
+};
+
+SFDP_SERVICE_DEFINE (nat_output) = {
+  .node_name = "sfdp-nat-output",
+  .runs_before = SFDP_SERVICES ("sfdp-geneve-output", "sfdp-nat-late-rewrite"),
+  .runs_after = SFDP_SERVICES ("sfdp-drop", "sfdp-l4-lifecycle",
+			       "sfdp-tcp-check"),
+  .is_terminal = 0
+};

MAINTAINERS
@@ -223,6 +223,11 @@ M:      Mohammed Hawari <mohammed@hawari.fr>
 M:      Ole Troan <otroan@employees.org>
 F:      src/vnet/sfdp/
 
+Plugin StateFul Data Plane Services
+I:      sfdp_services
+M:      Mohammed Hawari <mohammed@hawari.fr>
+F:      src/plugins/sfdp_services
+
 Plugin - Crypto - native
 I:	crypto-native
 M:	Damjan Marion <damarion@cisco.com>

src/plugins/sfdp_services/CMakeLists.txt
@@ -1,0 +1,63 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2025 Cisco Systems, Inc.
+#
+
+#include_directories(${CMAKE_SOURCE_DIR})
+include_directories(${CMAKE_CURRENT_SOURCE_DIR})
+
+add_vpp_plugin(sfdp_services
+  SOURCES
+  base/tcp-check/node.c
+  base/tcp-check/tcp_check.c
+  base/tcp-check/format.c
+  base/tcp-check/cli.c
+  base/tcp-check/api.c
+
+  base/l4-lifecycle/node.c
+
+  base/nat/nat.c
+  base/nat/cli.c
+  base/nat/api.c
+  base/nat/format.c
+  base/nat/fastpath_node.c
+  base/nat/slowpath_node.c
+  base/nat/external_input_node.c
+
+  base/interface_input/interface_input.c
+  base/interface_input/cli.c
+  base/interface_input/api.c
+  base/interface_input/node.c
+
+  acl/acl_sample.c
+  acl/cli.c
+  acl/node.c
+
+  dot1q/dot1q.c
+
+  geneve/gateway.c
+  geneve/cli.c
+  geneve/api.c
+  geneve/geneve_input/node.c
+  geneve/geneve_output/node.c
+
+  reass/reass.c
+  reass/sv_reass_node.c
+  #reass/full_reass_node.c
+
+  MULTIARCH_SOURCES
+  base/tcp-check/node.c
+  base/l4-lifecycle/node.c
+  base/interface_input/node.c
+  acl/node.c
+  geneve/geneve_input/node.c
+  geneve/geneve_output/node.c
+  dot1q/dot1q.c
+  reass/sv_reass_node.c
+  #reass/full_reass_node.c
+
+  API_FILES
+  base/nat/nat.api
+  base/tcp-check/tcp_check.api
+  base/interface_input/interface_input.api
+  geneve/gateway.api
+)

src/plugins/sfdp_services/acl/acl_sample.c
@@ -1,0 +1,74 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2025 Cisco Systems, Inc.
+ */
+
+#include <acl/acl_sample.h>
+
+static void
+acl_sample_validate_tenant_lc (u16 tenant_idx)
+{
+  sfdp_acl_main_t *vam = &sfdp_acl_main;
+  sfdp_main_t *sfdp = &sfdp_main;
+  sfdp_tenant_t *tenant = sfdp_tenant_at_index (sfdp, tenant_idx);
+  u32 lc;
+
+  if (vec_elt_at_index (vam->lc_by_tenant_idx, tenant_idx)[0] != ~0)
+    return;
+
+  lc = acl_plugin.get_lookup_context_index (vam->acl_user_id,
+					    tenant->tenant_id, 0);
+  vec_elt_at_index (vam->lc_by_tenant_idx, tenant_idx)[0] = lc;
+};
+
+clib_error_t *
+sfdp_acl_sample_tenant_set_acl (sfdp_acl_main_t *vam, u64 tenant_id,
+				u32 acl_index, bool disable)
+{
+  sfdp_main_t *sfdp = &sfdp_main;
+  clib_error_t *err = 0;
+  clib_bihash_kv_8_8_t kv = { .key = tenant_id, .value = 0 };
+  u16 tenant_idx;
+  u32 lc;
+  u32 *acl_vec = 0;
+  if (clib_bihash_search_inline_8_8 (&sfdp->tenant_idx_by_id, &kv))
+    {
+      err = clib_error_return (0, "Invalid tenant id: %llu", tenant_id);
+      return err;
+    }
+  tenant_idx = kv.value;
+  acl_sample_validate_tenant_lc (tenant_idx);
+  lc = vam->lc_by_tenant_idx[tenant_idx];
+  if (!disable)
+    vec_add1 (acl_vec, acl_index);
+  acl_plugin.set_acl_vec_for_context (lc, acl_vec);
+  vec_free (acl_vec);
+  return err;
+}
+
+static clib_error_t *
+acl_sample_init (vlib_main_t *vm)
+{
+  sfdp_acl_main_t *vam = &sfdp_acl_main;
+  sfdp_main_t *sfdp = &sfdp_main;
+  clib_error_t *err = acl_plugin_exports_init (&acl_plugin);
+  int i;
+  if (err)
+    return err;
+
+  vam->acl_user_id =
+    acl_plugin.register_user_module ("sfdp ACL plugin", "tenant id", NULL);
+
+  vec_validate (vam->lc_by_tenant_idx, (1ULL << sfdp->log2_tenants) - 1);
+
+  vec_foreach_index (i, vam->lc_by_tenant_idx)
+    vam->lc_by_tenant_idx[i] = ~0;
+
+  return 0;
+}
+
+VLIB_MAIN_LOOP_ENTER_FUNCTION (acl_sample_init) = {
+
+};
+
+sfdp_acl_main_t sfdp_acl_main;
+acl_plugin_methods_t acl_plugin;

src/plugins/sfdp_services/acl/acl_sample.h
@@ -1,0 +1,24 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2025 Cisco Systems, Inc.
+ */
+
+#ifndef __included_acl_sample_h__
+#define __included_acl_sample_h__
+
+#include <vlib/vlib.h>
+#include <vnet/sfdp/sfdp.h>
+#include <acl/exports.h>
+
+typedef struct
+{
+  u32 acl_user_id;
+  u32 *lc_by_tenant_idx; /* vec */
+} sfdp_acl_main_t;
+
+clib_error_t *sfdp_acl_sample_tenant_set_acl (sfdp_acl_main_t *vam,
+					      u64 tenant_id, u32 acl_index,
+					      bool disable);
+
+extern sfdp_acl_main_t sfdp_acl_main;
+extern acl_plugin_methods_t acl_plugin;
+#endif

src/plugins/sfdp_services/acl/cli.c
@@ -1,0 +1,52 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2025 Cisco Systems, Inc.
+ */
+
+#include <acl/acl_sample.h>
+
+static clib_error_t *
+sfdp_acl_sample_set_fn (vlib_main_t *vm, unformat_input_t *input,
+			vlib_cli_command_t *cmd)
+{
+  sfdp_acl_main_t *vam = &sfdp_acl_main;
+  unformat_input_t line_input_, *line_input = &line_input_;
+
+  clib_error_t *err = 0;
+  u32 sw_if_index = ~0;
+  u32 tenant_id = ~0;
+  u32 acl_index = ~0;
+  u8 disable = 0;
+
+  if (!unformat_user (input, unformat_line_input, line_input))
+    return 0;
+
+  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (line_input, "tenant %d", &tenant_id))
+	;
+
+      else if (unformat (line_input, "acl_index %d", &acl_index))
+	;
+      else if (unformat (line_input, "disable"))
+	disable = 1;
+      else if (unformat (line_input, "%U", unformat_vnet_sw_interface,
+			 vnet_get_main (), &sw_if_index))
+	;
+      else
+	{
+	  err = unformat_parse_error (line_input);
+	  goto done;
+	}
+    }
+  err = sfdp_acl_sample_tenant_set_acl (vam, tenant_id, acl_index, disable);
+done:
+  unformat_free (line_input);
+  return err;
+}
+
+VLIB_CLI_COMMAND (sfdp_acl_sample_set_cmd, static) = {
+  .path = "set sfdp acl",
+  .short_help =
+    "set sfdp acl tenant <tenant-id> acl_index <acl_index> [disable]",
+  .function = sfdp_acl_sample_set_fn,
+};

src/plugins/sfdp_services/acl/node.c
@@ -1,0 +1,195 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2025 Cisco Systems, Inc.
+ */
+
+#include <vlib/vlib.h>
+#include <acl/acl_sample.h>
+#include <vnet/sfdp/service.h>
+#include <vnet/sfdp/sfdp_funcs.h>
+
+#define foreach_sfdp_acl_sample_error _ (DROP, "drop")
+
+typedef enum
+{
+#define _(sym, str) SFDP_ACL_SAMPLE_ERROR_##sym,
+  foreach_sfdp_acl_sample_error
+#undef _
+    SFDP_ACL_SAMPLE_N_ERROR,
+} sfdp_acl_sample_error_t;
+
+static char *sfdp_acl_sample_error_strings[] = {
+#define _(sym, string) string,
+  foreach_sfdp_acl_sample_error
+#undef _
+};
+
+typedef struct
+{
+  u32 thread_index;
+  u32 flow_id;
+  u8 matched;
+  u8 action;
+} sfdp_acl_sample_trace_t;
+
+static u8 *
+format_sfdp_acl_sample_trace (u8 *s, va_list *args)
+{
+  vlib_main_t __clib_unused *vm = va_arg (*args, vlib_main_t *);
+  vlib_node_t __clib_unused *node = va_arg (*args, vlib_node_t *);
+  sfdp_acl_sample_trace_t *t = va_arg (*args, sfdp_acl_sample_trace_t *);
+  const char *action_str[] = { "deny", "permit", "permit+reflect" };
+
+  s = format (
+    s, "sfdp-acl-sample: flow-id %u (session %u, %s) status: %s, action: %s\n",
+    t->flow_id, t->flow_id >> 1, t->flow_id & 0x1 ? "reverse" : "forward",
+    t->matched ? "matched" : "unmatched",
+    t->matched ? action_str[t->action] : "<none>");
+  return s;
+}
+
+SFDP_SERVICE_DECLARE (drop)
+SFDP_SERVICE_DECLARE (sfdp_acl_sample)
+static_always_inline void
+sfdp_acl_sample_process_one (sfdp_acl_main_t *vam, sfdp_session_t *session,
+			     u8 dir, u16 *to_next, vlib_buffer_t **b,
+			     u8 *matched, u8 *action)
+{
+  u16 tenant_idx = session->tenant_idx;
+  u32 lc_index = vam->lc_by_tenant_idx[tenant_idx];
+  fa_5tuple_opaque_t fa_5tuple;
+  u32 match_acl_index = ~0;
+  u32 match_acl_pos = ~0;
+  u32 match_rule_index = ~0;
+  u32 trace_bitmap = 0;
+
+  if (lc_index == ~0)
+    goto end_of_packet;
+
+  acl_plugin_fill_5tuple_inline (acl_plugin.p_acl_main, lc_index, b[0], 0, 1,
+				 0, &fa_5tuple);
+
+  if (acl_plugin_match_5tuple_inline (
+	acl_plugin.p_acl_main, lc_index, &fa_5tuple, 0, action, &match_acl_pos,
+	&match_acl_index, &match_rule_index, &trace_bitmap))
+    {
+      matched[0] = 1;
+      if (action[0] == 0)
+	{
+	  /* Drop this flow, in this direction, forever */
+	  session->bitmaps[dir] |= SFDP_SERVICE_MASK (drop);
+	  sfdp_buffer (b[0])->service_bitmap = SFDP_SERVICE_MASK (drop);
+	}
+      else if (action[0] == 1)
+	/* Allow this packet only */
+	;
+      else
+	{
+	  /* Allow this flow and cache the decision in forward and reverse
+	   * direction */
+	  session->bitmaps[SFDP_FLOW_FORWARD] &=
+	    ~SFDP_SERVICE_MASK (sfdp_acl_sample);
+	  session->bitmaps[SFDP_FLOW_REVERSE] &=
+	    ~SFDP_SERVICE_MASK (sfdp_acl_sample);
+	}
+    }
+  else
+    {
+      matched[0] = 0;
+    }
+
+end_of_packet:
+  sfdp_next (b[0], to_next);
+  return;
+}
+
+static_always_inline u16
+sfdp_acl_sample_inline (vlib_main_t *vm, vlib_node_runtime_t *node,
+			vlib_frame_t *frame)
+{
+
+  vlib_buffer_t *bufs[VLIB_FRAME_SIZE], **b = bufs;
+  sfdp_acl_main_t *vam = &sfdp_acl_main;
+  u32 thread_index = vlib_get_thread_index ();
+  sfdp_session_t *session;
+  u32 session_idx;
+  u8 dir;
+  u32 *from = vlib_frame_vector_args (frame);
+  u32 n_left = frame->n_vectors;
+  u16 next_indices[VLIB_FRAME_SIZE], *to_next = next_indices;
+  u8 matched[VLIB_FRAME_SIZE], action[VLIB_FRAME_SIZE];
+  u8 *m = matched;
+  u8 *a = action;
+  vlib_get_buffers (vm, from, bufs, n_left);
+  while (n_left > 0)
+    {
+      session_idx = sfdp_session_from_flow_index (b[0]->flow_id);
+      dir = sfdp_direction_from_flow_index (b[0]->flow_id);
+      session = sfdp_session_at_index (session_idx);
+
+      sfdp_acl_sample_process_one (vam, session, dir, to_next, b, m, a);
+      n_left -= 1;
+      b += 1;
+      to_next += 1;
+      m += 1;
+      a += 1;
+    }
+  vlib_buffer_enqueue_to_next (vm, node, from, next_indices, frame->n_vectors);
+  if (PREDICT_FALSE ((node->flags & VLIB_NODE_FLAG_TRACE)))
+    {
+      int i;
+      b = bufs;
+      m = matched;
+      a = action;
+      n_left = frame->n_vectors;
+      for (i = 0; i < n_left; i++)
+	{
+	  if (b[0]->flags & VLIB_BUFFER_IS_TRACED)
+	    {
+	      sfdp_acl_sample_trace_t *t =
+		vlib_add_trace (vm, node, b[0], sizeof (*t));
+	      t->flow_id = b[0]->flow_id;
+	      t->thread_index = thread_index;
+	      t->matched = m[0];
+	      t->action = a[0];
+	      b++;
+	      m++;
+	      a++;
+	    }
+	  else
+	    break;
+	}
+    }
+  return frame->n_vectors;
+}
+
+VLIB_NODE_FN (sfdp_acl_sample_node)
+(vlib_main_t *vm, vlib_node_runtime_t *node, vlib_frame_t *frame)
+{
+  return sfdp_acl_sample_inline (vm, node, frame);
+}
+
+VLIB_REGISTER_NODE (sfdp_acl_sample_node) = {
+  .name = "sfdp-acl-sample",
+  .vector_size = sizeof (u32),
+  .format_trace = format_sfdp_acl_sample_trace,
+  .type = VLIB_NODE_TYPE_INTERNAL,
+
+  .n_errors = ARRAY_LEN (sfdp_acl_sample_error_strings),
+  .error_strings = sfdp_acl_sample_error_strings
+};
+
+SFDP_SERVICE_DEFINE (sfdp_acl_sample) = {
+  .node_name = "sfdp-acl-sample",
+  .runs_before = SFDP_SERVICES ("sfdp-geneve-output", "ip4-lookup"),
+  .runs_after = SFDP_SERVICES ("sfdp-drop", "sfdp-l4-lifecycle",
+			       "sfdp-tcp-check"),
+  .is_terminal = 0
+};
+
+SFDP_SERVICE_DEFINE (ip4_lookup) = {
+  .node_name = "ip4-lookup",
+  .runs_before = SFDP_SERVICES (0),
+  .runs_after = SFDP_SERVICES ("sfdp-drop", "sfdp-l4-lifecycle",
+			       "sfdp-tcp-check"),
+  .is_terminal = 1
+};

src/plugins/sfdp_services/base/interface_input/api.c
@@ -1,0 +1,49 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2025 Cisco Systems, Inc.
+ */
+
+#include <vnet/sfdp/sfdp.h>
+
+#include <sfdp_services/base/interface_input/interface_input.h>
+#include <vlibapi/api.h>
+#include <vlibmemory/api.h>
+#include <vnet/ip/ip_types_api.h>
+
+#include <vnet/format_fns.h>
+#include <sfdp_services/base/interface_input/interface_input.api_enum.h>
+#include <sfdp_services/base/interface_input/interface_input.api_types.h>
+
+#define REPLY_MSG_ID_BASE vim->msg_id_base
+#include <vlibapi/api_helper_macros.h>
+
+static void
+vl_api_sfdp_interface_input_set_t_handler (
+  vl_api_sfdp_interface_input_set_t *mp)
+{
+  sfdp_interface_input_main_t *vim = &sfdp_interface_input_main;
+  u32 sw_if_index = clib_net_to_host_u32 (mp->sw_if_index);
+  u32 tenant_id = clib_net_to_host_u32 (mp->tenant_id);
+  u8 unset = mp->is_disable;
+  clib_error_t *err =
+    sfdp_interface_input_set_tenant (vim, sw_if_index, tenant_id, unset);
+  int rv = err ? -1 : 0;
+  vl_api_sfdp_interface_input_set_reply_t *rmp;
+  REPLY_MACRO (VL_API_SFDP_INTERFACE_INPUT_SET_REPLY);
+}
+
+#include <sfdp_services/base/interface_input/interface_input.api.c>
+static clib_error_t *
+sfdp_interface_input_api_hookup (vlib_main_t *vm)
+{
+  sfdp_interface_input_main_t *vim = &sfdp_interface_input_main;
+  vim->msg_id_base = setup_message_id_table ();
+  return 0;
+}
+VLIB_API_INIT_FUNCTION (sfdp_interface_input_api_hookup);
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

commit 8149a8cc1384121ca91e0661e72623842ab8fc4b

sfdp_services: support larger NAT pools
The old code only had a TODO and an assert.

Type: fix
Fixes: 28dbf5aee6bfd783e7336d561a1fe03f455c7e9b

Change-Id: Ifbcc203cc95765274c8835ae23a3d961c78116d2
Signed-off-by: Vratko Polak <vrpolak@cisco.com>

src/plugins/sfdp_services/base/nat/slowpath_node.c
@@ -103,16 +103,21 @@ nat_slow_path_process_one (sfdp_main_t *sfdp, u32 *fib_index_by_sw_if_index,
       goto end_of_packet;
     }
 
-  /* TODO: handle case with many addresses in pool (slowpath) */
-  if (PREDICT_FALSE (pool->num > NAT_ALLOC_POOL_ARRAY_SZ))
-    ASSERT (0);
-
   new_key->context_id = tenant->reverse_context;
 
   /* Allocate a new source */
   ip4_old_src_addr = *ip4_key_src_addr;
   src_addr_index = ip4_old_src_addr % pool->num;
-  ip4_new_src_addr = pool->addr[src_addr_index].as_u32;
+  if (src_addr_index < NAT_ALLOC_POOL_ARRAY_SZ)
+    {
+      ip4_new_src_addr = pool->addr[src_addr_index].as_u32;
+    }
+  else
+    {
+      ASSERT (pool->remaining != 0);
+      ASSERT (vec_len (pool->remaining) > src_addr_index - NAT_ALLOC_POOL_ARRAY_SZ);
+      ip4_new_src_addr = pool->remaining[src_addr_index - NAT_ALLOC_POOL_ARRAY_SZ].as_u32;
+    }
   *ip4_key_src_addr = ip4_new_src_addr;
   pseudo_dir = sfdp_renormalise_ip4_key (new_key, pseudo_dir);
   pseudo_flow_index = (session_index << 1) | (pseudo_dir & 0x1);