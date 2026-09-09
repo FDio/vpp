c29f2cdcf2c7b1493292d8b744d0645b01e4ff6a
Florin Coras <fcoras@cisco.com>
2026-09-09 01:25:26 -0400

    tcp: discount replaced bytes in retransmit samples
    Type: improvement
    
    Change-Id: Iea694c1db29a7a7f00fdc5ec963ddaf8de191297
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- src/vnet/tcp/tcp_bt.h
+++ src/vnet/tcp/tcp_bt.h
@@ -86,6 +86,10 @@ void tcp_bt_track_tx (tcp_connection_t * tc, u32 len);
 /**
  * Track a tcp retransmission
  *
+ * Must be called after snd_rxt_bytes is advanced by end - start. The saved
+ * flight includes the new transmission and discounts any active copy it
+ * replaces.
+ *
  * @param tc	tcp connection
  * @param start	start sequence number
  * @param end	end sequence number
