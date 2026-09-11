a6d54c577c50da5ea880d4690d92a7b12fc40048
Vratko Polak <vrpolak@cisco.com>
2026-09-11 11:33:17 +0000

    experiments: tcp resets
    Change-Id: I06a07da1a6ef6cf86482e09ecd85c5b3f8bae720
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- src/vnet/tcp/tcp_input.c
+++ src/vnet/tcp/tcp_input.c
@@ -2740,6 +2740,7 @@ tcp_input_set_error_next (tcp_main_t * tm, u16 * next, u32 * error, u8 is_ip4)
     {
       *next = TCP_INPUT_NEXT_RESET;
       *error = TCP_ERROR_NO_LISTENER;
+      abort(1);
     }
 }
 
