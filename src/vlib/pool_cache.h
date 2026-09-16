78a43499aab5451097fabade65c6310539e84d9b
Maxime Peim <maxime.peim@gmail.com>
2026-09-16 14:14:05 +0200

    vlib: pool-cache prefill and foreach macro
    Add an helper to prefill the global free-list.
    Add a best-effort macro to iterate over currently allocated elements
    
    Change-Id: I218ba1cc9df95b294be5618e75c497f991132bd7
    Signed-off-by: Maxime Peim <maxime.peim@gmail.com>

--- src/vlib/pool_cache.h
+++ src/vlib/pool_cache.h
@@ -124,6 +124,7 @@ void _pool_cache_init_with_batch (vlib_pool_cache_t *c, char *name,
 #define pool_cache_init(C, N, F, L, A, T)                                                          \
   _pool_cache_init_with_batch ((C), (N), (F), (L), (A), 0, sizeof (T), __alignof__ (T))
 void pool_cache_free (vlib_pool_cache_t *c);
+void pool_cache_prefill (vlib_pool_cache_t *c, u32 n_elts);
 void _pool_cache_refill (vlib_pool_cache_t *c, vlib_pool_cache_thread_t *pt);
 void _pool_cache_flush (vlib_pool_cache_t *c, vlib_pool_cache_thread_t *pt, u32 n_cached);
 u8 *pool_cache_format_element (u8 *s, vlib_pool_cache_t *c, u32 index);
@@ -304,5 +305,53 @@ pool_cache_put_index (vlib_pool_cache_t *c, u32 index)
 
   _pool_cache_publish_free (c, index, pidx, eidx, sp);
 }
+
+static_always_inline void
+_pool_cache_foreach_yield (vlib_main_t *vm, f64 *start)
+{
+  /* yields for 2 clock ticks every 1 tick to avoid blocking threads for too long
+   * when dumping huge data structures */
+  f64 now = vlib_time_now (vm);
+  if (now - *start > 11e-6)
+    {
+      vlib_process_suspend (vm, 21e-6);
+      *start = vlib_time_now (vm);
+    }
+}
+
+/*
+ * Best-effort diagnostic iterator: allocation state may change after it is
+ * checked, so the body may observe recycled or inconsistent payload data and
+ * requires external quiescence to be sanitizer-safe.
+ *
+ * The iterator yields with vlib_process_suspend() and therefore must only be
+ * called from VLIB process context.
+ */
+#define vlib_foreach_pool_cache(_e, _c, _b)                                                        \
+  do                                                                                               \
+    {                                                                                              \
+      vlib_main_t *vm = vlib_get_main ();                                                          \
+      f64 start = vlib_time_now (vm);                                                              \
+      u32 n_subpools = clib_atomic_load_relax_n (&(_c)->n_subpools);                               \
+                                                                                                   \
+      vlib_pool_cache_subpool_t *sp;                                                               \
+      for (int pidx = 0; pidx < n_subpools; pidx++)                                                \
+	{                                                                                          \
+	  sp = _pool_cache_subpool_at ((_c), pidx);                                                \
+	  for (int eidx = 0; eidx < (_c)->subpool_size; eidx++)                                    \
+	    {                                                                                      \
+	      _pool_cache_foreach_yield (vm, &start);                                              \
+	      u32 index = _pool_cache_encode_index (pidx, eidx);                                   \
+	      if (pool_cache_is_free_index ((_c), index))                                          \
+		continue;                                                                          \
+	      (_e) = _pool_cache_elt_at_indices ((_c), pidx, eidx);                                \
+	      {                                                                                    \
+		(_b);                                                                              \
+	      }                                                                                    \
+	    }                                                                                      \
+	}                                                                                          \
+    }                                                                                              \
+  }                                                                                                \
+  while (0)
 
 #endif /* included_vlib_pool_cache_h */
