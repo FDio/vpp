b8f6d3d72b8ceda19841ead14fca6f3ded4ba9be
Aritra Basu <aritrbas@cisco.com>
2026-09-17 02:24:07 -0400

    cnat: skip output SNAT for unsupported protocols
    The output feature can be visited again after a midchain
    adjacency encapsulates a packet. cnat_make_buffer_5tuple()
    leaves the tuple empty for unsupported outer protocols such
    as IP-in-IP. The output feature nevertheless passes that
    tuple to the SNAT policy. Protocol zero is treated as a new
    SNAT flow, port allocation fails, and the packet is dropped.
    
    Type: fix
    
    Change-Id: Ia180d2724d0e08ac2d50a5f7ea17156414bb8413
    Signed-off-by: Aritra Basu <aritrbas@cisco.com>

--- test/test_cnat.py
+++ test/test_cnat.py
@@ -990,6 +990,45 @@ class TestCNatTranslation(CnatCommonTestCase):
         self.cnat_encap()
         self.cnat_enable_features(0)
 
+    def test_cnat_snat_excluded_with_encap(self):
+        """CNAT output must not SNAT an already encapsulated packet"""
+        self.translations = []
+        self.mbtranslations = []
+        self.cnat_enable_features()
+        self.make_encap()
+
+        policies = VppEnum.vl_api_cnat_snat_policies_t
+        policy_tables = VppEnum.vl_api_cnat_snat_policy_table_t
+        exclude_prefix = ip_network(f"{self.pg2.remote_hosts[0].ip4}/24", strict=False)
+
+        self.vapi.cnat_set_snat_addresses(
+            snat_ip4="4.4.4.4",
+            sw_if_index=INVALID_INDEX,
+        )
+        self.vapi.cnat_set_snat_policy(policy=policies.CNAT_POLICY_IF_PFX)
+        self.vapi.cnat_snat_policy_add_del_if(
+            sw_if_index=self.pg0.sw_if_index,
+            is_add=1,
+            table=policy_tables.CNAT_POLICY_INCLUDE_V4,
+        )
+        self.vapi.cnat_snat_policy_add_del_exclude_pfx(prefix=exclude_prefix, is_add=1)
+
+        try:
+            ctx = CnatTestContext(self, UDP, is_v6=False)
+            ctx.cnat_send(self.pg0, "3.3.3.3", 1234, self.pg2, "2.2.2.2", 4455)
+            ctx.cnat_expect(self.pg1, 0, 1234, self.pg2, 0, 4455)
+        finally:
+            self.vapi.cnat_snat_policy_add_del_exclude_pfx(
+                prefix=exclude_prefix, is_add=0
+            )
+            self.vapi.cnat_snat_policy_add_del_if(
+                sw_if_index=self.pg0.sw_if_index,
+                is_add=0,
+                table=policy_tables.CNAT_POLICY_INCLUDE_V4,
+            )
+            self.vapi.cnat_set_snat_addresses(sw_if_index=INVALID_INDEX)
+            self.cnat_enable_features(0)
+
     def test_cnat_with_encap_vxlan(self):
         # """ CNat Translation ENCAP """
         self._make_remote_backend_translation_v4(CNAT_TR_FLAG_NO_CLIENT)
